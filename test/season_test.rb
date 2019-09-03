822f03ff3442838a36cb9fbcff3afb19ad4c052f
Brian Bower <brian@brianbower.com>
2019-09-03 16:04:33 -0600

    Add module frameworks
    Co-authored-by: Jonathan Patterson <jonpatt92@gmail.com>
    Co-authored-by: Allison Moore <33507055+m3gacrypt0@users.noreply.github.com>

---
+++ test/season_test.rb
@@ -0,0 +1,47 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+relative_require 'season'
+
+class SeasonTest < Minitest::Test
+
+  # Name of the team with the biggest decrease between regular season and postseason win percentage.	Return: String
+  def test_biggest_bust
+    # code goes here!
+  end
+
+  # Name of the team with the biggest increase between regular season and postseason win percentage.	Return: String
+  def test_biggest_surprise
+    # code goes here!
+  end
+
+  # Name of the Coach with the best win percentage for the season. Return:	String
+  def test_winningest_coach
+    # code goes here!
+  end
+
+  # Name of the Coach with the worst win percentage for the season. Return:	String
+  def test_worst_coach
+    # code goes here!
+  end
+
+  # Name of the Team with the best ratio of shots to goals for the season. Return:	String
+  def test_most_accurate_team
+    # code goes here!
+  end
+
+  # Name of the Team with the worst ratio of shots to goals for the season. Return:	String
+  def test_least_accurate_team
+    # code goes here!
+  end
+
+  # Name of the Team with the most tackles in the season. Return:	String
+  def test_most_tackles
+    # code goes here!
+  end
+
+  # Name of the Team with the fewest tackles in the season. Return:	String
+  def test_fewest_tackles
+    # code goes here!
+  end
+
+end
