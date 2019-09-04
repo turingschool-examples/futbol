0349470ca327eab0d17906639cd67772f546e9c4
David Lopez <davidclopez1@live.com>
2019-09-04 14:20:00 -0600

    Add ability to access data from_csv
    Co-authored-by: Alice Post <26877629+ap2322@users.noreply.github.com>
    Co-authored-by: Tyler Balogh <52803986+t-balogh@users.noreply.github.com>

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,19 +1,22 @@
 class StatTracker
-  attr_reader :teams
-  def self.from_csv(hash)
-    @teams = []
-    CSV.foreach(hash[:teams]) do |row|
-       @teams << Team.new(row)
+
+  def self.from_csv(file_paths)
+    @all_teams = Array.new
+    @all_games = Array.new
+    @all_game_teams = Array.new
+    self.generate_data(file_paths[:teams], Team, @all_teams)
+    self.generate_data(file_paths[:games], Game, @all_games)
+    self.generate_data(file_paths[:teams], GameTeam, @all_game_teams)
+    data = {
+      teams: @all_teams[1..-1],
+      games: @all_games[1..-1],
+      game_teams: @all_game_teams[1..-1]
+    }
+  end
+
+  def self.generate_data(location ,obj_type, array)
+    CSV.foreach(location) do |row|
+      array << obj_type.new(row)
     end
   end
-
-  def initialize
-    @teams
-  end
-
-  # def team_objects(location)
-  #   CSV.foreach(location) do |row|
-  #     require "pry"; binding.pry
-  #     Team.new(row)
-  #   end
 end
