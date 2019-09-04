ed40e9801973074cb8eecadf71a7cf896efce405
Brian Bower <brian@brianbower.com>
2019-09-04 12:54:15 -0600

    Add highest_total_score test
    Co-authored-by: Jonathan Patterson <jonpatt92@gmail.com>
    Co-authored-by: Allison Moore <33507055+m3gacrypt0@users.noreply.github.com>

--- test/games_test.rb
+++ test/games_test.rb
@@ -1,13 +1,29 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require_relative 'games'
+require './lib/stat_tracker'
+require './lib/games'
 
 class GamesTest < Minitest::Test
+
+  def setup
+    @game_path = './data/games.csv'
+    @team_path = './data/teams.csv'
+    @game_teams_path = './data/game_teams.csv'
+
+    @locations = {
+      games: @game_path,
+      teams: @team_path,
+      game_teams: @game_teams_path
+    }
+
+    @stat_tracker = StatTracker.from_csv(@locations)
+  end
+
 
   #Highest sum of the winning and losing teams’ scores	Integer
   #BB
   def test_highest_total_score
-
+    assert_equal 11, @stat_tracker.highest_total_score
   end
 
   # #Lowest sum of the winning and losing teams’ scores	Integer
