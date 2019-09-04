c067c1df78e9ea1074b242501d4a06d28c4c9530
Hillary Stewart <hillarystewart4427@gmail.com>
2019-09-04 16:53:24 -0600

    Refactor Game class initialize method
    Co-Authored-By: Laura Schulz <lrs8810@users.noreply.github.com>
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>

--- lib/game.rb
+++ lib/game.rb
@@ -1,14 +1,16 @@
 class Game
+  attr_reader :away_goals, :home_goals
+
   def initialize(game)
-    @game_id = game[:game_id]
-    @season = game[:season]
-    @type = game[:type]
-    @date_time = game[:date_time]
-    @away_team_id = game[:away_team_id]
-    @home_team_id = game[:home_team_id]
-    @away_goals = game[:away_goals]
-    @home_goals = game[:home_goals]
-    @venue = game[:venue]
-    @venue_link = game[:venue_link]
+    @game_id = game["game_id"].to_i
+    @season = game["season"].to_i
+    @type = game["type"]
+    @date_time = game["date_time"]
+    @away_team_id = game["away_team_id"].to_i
+    @home_team_id = game["home_team_id"].to_i
+    @away_goals = game["away_goals"].to_i
+    @home_goals = game["home_goals"].to_i
+    @venue = game["venue"]
+    @venue_link = game["venue_link"]
   end
 end
