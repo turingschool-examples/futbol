3a2b8a7156b2fb1affd7242b1d000c9d11c7441b
Mary Lang <mcl86@cornell.edu>
2019-09-04 16:18:36 -0600

    Changes

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,5 +1,10 @@
 class StatTracker
 
+  def initialize
+
+  end
+
+
   def StatTracker.from_csv(locations)
     # output csv data in correct format
     # call module methods ?
---
+++ test/stat_tracker_test.rb
@@ -0,0 +1,2 @@
+require './test_helper'
+require_relative './lib/stat_tracker'
