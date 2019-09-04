9912f6e1def8d5932f505f8fd1d5e37f98fdef4e
Carleigh Crockett <capleugh@gmail.com>
2019-09-04 12:57:15 -0600

    Add game class and game test
    Co-authored-by: Ryan Hantak <47759923+rhantak@users.noreply.github.com>
    Co-authored-by: John Travers <46035439+johnktravers@users.noreply.github.com>
    Co-authored-by: David Barriga-Gomez <50917634+davidbarriga-gomez@users.noreply.github.com>

---
+++ lib/game.rb
@@ -0,0 +1,25 @@
+class Game
+  attr_reader :game_id,
+              :season,
+              :type,
+              :date_time,
+              :away_team_id,
+              :home_team_id,
+              :away_goals,
+              :home_goals,
+              :venue,
+              :venue_link
+
+  def initialize(game_info)
+    @game_id = game_info[0].to_i
+    @season = game_info[1].to_i
+    @type = game_info[2]
+    @date_time = game_info[3]
+    @away_team_id = game_info[4].to_i
+    @home_team_id = game_info[5].to_i
+    @away_goals = game_info[6].to_i
+    @home_goals = game_info[7].to_i
+    @venue = game_info[8]
+    @venue_link = game_info[9]
+  end
+end
