a8ee9097e8c4ad8e8b379ef1731b74fe8d0d3b40
Hillary Stewart <hillarystewart4427@gmail.com>
2019-09-04 15:28:31 -0600

    Add self.from_csv method to StatTracker
    Co-Authored-By: Laura Schulz <lrs8810@users.noreply.github.com>
    Co-Authored-By: Tyla Devon <tyladevon@users.noreply.github.com>
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,6 +1,26 @@
+require 'csv'
+require_relative './game'
+
 class StatTracker
+  
   def self.from_csv(locations)
-    locations
-    require 'pry'; binding.pry
+    games = []
+    CSV.foreach(locations[:games]) do |row|
+      game = Hash.new
+      game[:game_id] = row[0]
+      game[:season] = row[1]
+      game[:type] = row[2]
+      game[:date_time] = row[3]
+      game[:away_team_id] = row[4]
+      game[:home_team_id] = row[5]
+      game[:away_goals] = row[6]
+      game[:home_goals] = row[7]
+      game[:venue] = row[8]
+      game[:venue_link] = row[9]
+      game = Game.new(game)
+      games << game
+    end
+    games.shift
+    games
   end
 end
