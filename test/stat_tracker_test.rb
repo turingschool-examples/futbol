a4a95f5468303480a98b4596490955ff148081fa
Hillary Stewart <hillarystewart4427@gmail.com>
2019-09-04 16:52:34 -0600

    Add test for highest_total_score & instance test for StatTracker
    Co-Authored-By: Laura Schulz <lrs8810@users.noreply.github.com>
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -16,7 +16,10 @@ class StatTrackerTest < Minitest::Test
   end
 
   def test_it_exists
-    assert_equal @locations, @stat_tracker
+    assert_instance_of StatTracker, @stat_tracker
   end
 
+  def test_it_can_show_highest_total_score
+    assert_equal 11, @stat_tracker.highest_total_score
+  end
 end
