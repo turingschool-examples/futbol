3d9b375d2e6a93914b92b0f0ee2f5bc342708202
Graham Thompson <grwthomps@gmail.com>
2019-09-04 11:41:51 -0600

    Create stat_tracker and stat_tracker_test files
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>
    Co-Authored-By: Laura Schulz <lrs8810@users.noreply.github.com>
    Co-Authored-By: Tyla Devon <tyladevon@users.noreply.github.com>

---
+++ lib/stat_tracker.rb
@@ -0,0 +1,6 @@
+class StatTracker
+  def self.from_csv(locations)
+    locations
+    require 'pry'; binding.pry
+  end
+end
---
+++ test/stat_tracker_test.rb
@@ -0,0 +1,22 @@
+require './test/test_helper'
+require './lib/stat_tracker'
+
+class StatTrackerTest < Minitest::Test
+
+  def setup
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+    @locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+    @stat_tracker = StatTracker.from_csv(@locations)
+  end
+
+  def test_it_exists
+    assert_equal @locations, @stat_tracker
+  end
+
+end
