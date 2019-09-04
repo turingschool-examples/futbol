2820f3f034577a9639f8e7c6513d139ac540d27a
Hillary Stewart <hillarystewart4427@gmail.com>
2019-09-04 16:54:01 -0600

    Add highest_total_score method and refactor self.from_csv method
    Co-Authored-By: Laura Schulz <lrs8810@users.noreply.github.com>
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -2,25 +2,25 @@ require 'csv'
 require_relative './game'
 
 class StatTracker
-  
+
+  def initialize(games)
+    @games = games
+  end
+
   def self.from_csv(locations)
     games = []
-    CSV.foreach(locations[:games]) do |row|
-      game = Hash.new
-      game[:game_id] = row[0]
-      game[:season] = row[1]
-      game[:type] = row[2]
-      game[:date_time] = row[3]
-      game[:away_team_id] = row[4]
-      game[:home_team_id] = row[5]
-      game[:away_goals] = row[6]
-      game[:home_goals] = row[7]
-      game[:venue] = row[8]
-      game[:venue_link] = row[9]
-      game = Game.new(game)
-      games << game
+    CSV.foreach(locations[:games], :headers=> true) do |row|
+      games.push(Game.new(row))
     end
-    games.shift
-    games
+    StatTracker.new(games)
+  end
+
+  def highest_total_score
+    sum = 0
+    @games.each do |game|
+      new_sum = game.away_goals + game.home_goals
+      sum = new_sum if new_sum > sum
+    end
+    sum
   end
 end
