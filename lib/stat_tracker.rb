6af71fae9ec6d032261d993a07467bd5d489ced6
David Lopez <davidclopez1@live.com>
2019-09-04 14:44:44 -0600

    Add refactored way for accessing data
    Co-authored-by: Alice Post <26877629+ap2322@users.noreply.github.com>
    Co-authored-by: Tyler Balogh <52803986+t-balogh@users.noreply.github.com>

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,18 +1,21 @@
 class StatTracker
 
+  def initialize(team_array, game_array, game_teams_array)
+    @all_teams = team_array
+    @all_games = game_array
+    @all_game_teams = game_teams_array
+  end
+
   def self.from_csv(file_paths)
-    @all_teams = Array.new
-    @all_games = Array.new
-    @all_game_teams = Array.new
+    all_teams = Array.new
+    all_games = Array.new
+    all_game_teams = Array.new
     self.generate_data(file_paths[:teams], Team, @all_teams)
     self.generate_data(file_paths[:games], Game, @all_games)
     self.generate_data(file_paths[:teams], GameTeam, @all_game_teams)
-    data = {
-      teams: @all_teams[1..-1],
-      games: @all_games[1..-1],
-      game_teams: @all_game_teams[1..-1]
-    }
+    self.new(all_teams, all_games, all_game_teams)
   end
+
 
   def self.generate_data(location ,obj_type, array)
     CSV.foreach(location) do |row|
