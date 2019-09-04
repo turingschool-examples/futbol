24f95fc61a56f1a9944ecfa3123bc75b1d4ba607
Tyler Balogh <tycbalogh@gmail.com>
2019-09-04 15:26:27 -0600

    Add attr_reader and ignore first line of csv files
    Co-authored-by: Alice Post <26877629+ap2322@users.noreply.github.com>
    Co-authored-by: David Lopez <43835133+Not-Zorro@users.noreply.github.com>

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,18 +1,19 @@
 class StatTracker
+  attr_reader :all_teams, :all_games, :all_game_teams
 
   def initialize(team_array, game_array, game_teams_array)
-    @all_teams = team_array
-    @all_games = game_array
-    @all_game_teams = game_teams_array
+    @all_teams = team_array[1..-1]
+    @all_games = game_array[1..-1]
+    @all_game_teams = game_teams_array[1..-1]
   end
 
   def self.from_csv(file_paths)
     all_teams = Array.new
     all_games = Array.new
     all_game_teams = Array.new
-    self.generate_data(file_paths[:teams], Team, @all_teams)
-    self.generate_data(file_paths[:games], Game, @all_games)
-    self.generate_data(file_paths[:teams], GameTeam, @all_game_teams)
+    self.generate_data(file_paths[:teams], Team, all_teams)
+    self.generate_data(file_paths[:games], Game, all_games)
+    self.generate_data(file_paths[:game_teams], GameTeam, all_game_teams)
     self.new(all_teams, all_games, all_game_teams)
   end
 
