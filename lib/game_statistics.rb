ca00a8db8661cd775b74ea2c0f2aeba255c061c0
Laura Schulz <lrs8810@gmail.com>
2019-09-05 15:17:51 -0600

    Add methods for percentage home wins, percentage ties, and count of games by season
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -25,4 +25,38 @@ module GameStatistics
     end
     sum
   end
+
+  def percentage_home_wins
+    wins = 0
+    home_games = 0
+    @game_teams.each do |game|
+      if game.hoa == "home" && game.result == "WIN"
+        wins += 1
+        home_games += 1
+      elsif game.hoa == "home" && game.result == "LOSS"
+        home_games += 1
+      end
+    end
+    (wins / home_games).to_f.round(2)
+    # require 'pry'; binding.pry
+  end
+
+  def percentage_ties
+    (@games.find_all do |game|
+      game.away_goals == game.home_goals
+    end.length.to_f / @games.length).round(2)
+  end
+
+  def count_of_games_by_season
+    total_games = Hash.new(0)
+    @games.each do |game|
+      if !total_games.has_key?(game.season)
+        total_games[game.season] = 1
+      else
+        total_games[game.season] += 1
+      end
+    end
+    total_games
+  end
+
 end
