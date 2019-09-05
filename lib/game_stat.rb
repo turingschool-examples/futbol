f86c79fc398d0b90b1bc1b672d5c4e6645e3b719
Tyler Balogh <tycbalogh@gmail.com>
2019-09-05 14:27:05 -0600

    Create biggest_blowout, percentage_home and visitor wins and tests
    Co-authored-by: Alice Post <26877629+ap2322@users.noreply.github.com>
    Co-authored-by: David Lopez <43835133+Not-Zorro@users.noreply.github.com>

--- lib/game_stat.rb
+++ lib/game_stat.rb
@@ -9,4 +9,19 @@ module GameStat
     lowest_game = all_games.min_by { |game| game.home_goals + game.away_goals }
     lowest_game.home_goals + lowest_game.away_goals
   end
+
+  def biggest_blowout
+    bb_game = all_games.max_by{ |game| (game.home_goals - game.away_goals).abs}
+    (bb_game.home_goals - bb_game.away_goals).abs
+  end
+
+  def percentage_home_wins
+    all_games.count { |game| game.home_goals > game.away_goals} \
+      / all_games.length.to_f * 100
+  end
+
+  def percentage_visitor_wins
+    all_games.count { |game| game.home_goals < game.away_goals} \
+      / all_games.length.to_f * 100
+  end
 end
--- test/game_stat_test.rb
+++ test/game_stat_test.rb
@@ -29,4 +29,16 @@ class GameStatTest < Minitest::Test
   def test_lowest_total_score
     assert_equal 0, @stat_tracker.lowest_total_score
   end
+
+  def test_biggest_blowout
+    assert_equal 499, @stat_tracker.biggest_blowout
+  end
+
+  def test_percentage_home_wins
+    assert_equal 42.11, @stat_tracker.percentage_home_wins.round(2)
+  end
+
+  def test_percentage_visitor_wins
+    assert_equal 52.63, @stat_tracker.percentage_visitor_wins.round(2)
+  end
 end
