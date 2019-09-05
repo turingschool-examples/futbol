84d69a77245e0748ea9cbb89150bb9fafbc300ca
Laura Schulz <lrs8810@gmail.com>
2019-09-05 14:08:43 -0600

    Add teams and game_teams to StatTracker class
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>
    Co-Authored-By: Tyla Devon <tyladevon@users.noreply.github.com>

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,10 +1,16 @@
 require 'csv'
 require_relative './game'
+require_relative './team'
+require_relative './game_team'
+
 
 class StatTracker
+  attr_reader :games, :teams, :game_teams
 
-  def initialize(games)
+  def initialize(games, teams, game_teams)
     @games = games
+    @teams = teams
+    @game_teams = game_teams
   end
 
   def self.from_csv(locations)
@@ -12,7 +18,18 @@ class StatTracker
     CSV.foreach(locations[:games], :headers=> true) do |row|
       games.push(Game.new(row))
     end
-    StatTracker.new(games)
+
+    teams = []
+    CSV.foreach(locations[:teams], :headers=> true) do |row|
+      teams.push(Team.new(row))
+    end
+
+    game_teams = []
+    CSV.foreach(locations[:game_teams], :headers=> true) do |row|
+      game_teams.push(GameTeam.new(row))
+    end
+
+    StatTracker.new(games, teams, game_teams)
   end
 
   def highest_total_score
