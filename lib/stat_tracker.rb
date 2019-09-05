9de63f10bcc166d2b997190836aab5565d2e4a45
John Travers <johnk.travers@gmail.com>
2019-09-05 13:34:41 -0600

    Change csv.read to csv.foreach
    Co-authored-by: Ryan Hantak <47759923+rhantak@users.noreply.github.com>
    Co-authored-by: David Barriga-Gomez <50917634+DavidBarriga-Gomez@users.noreply.github.com>
    Co-authored-by: Carleigh Crockett <47065120+Capleugh@users.noreply.github.com>

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -16,24 +16,18 @@ class StatTracker
 
   def self.from_csv(locations)
     teams = []
-    CSV.read(locations[:teams]).each_with_index do |line, index|
-      unless index == 0
-        teams << Team.new(line)
-      end
+    CSV.foreach(locations[:teams], :headers => true) do |line|
+      teams << Team.new(line)
     end
 
     games = []
-    CSV.read(locations[:games]).each_with_index do |line, index|
-      unless index == 0
-        games << Game.new(line)
-      end
+    CSV.foreach(locations[:games], :headers => true) do |line|
+      games << Game.new(line)
     end
 
     game_teams = []
-    CSV.read(locations[:game_teams]).each_with_index do |line, index|
-      unless index == 0
-        game_teams << GameTeam.new(line)
-      end
+    CSV.foreach(locations[:game_teams], :headers => true) do |line|
+      game_teams << GameTeam.new(line)
     end
 
     StatTracker.new(teams, games, game_teams)
