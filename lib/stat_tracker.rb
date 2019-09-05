4c93c983289b08273f555fe4d333887c217d58e0
Allison E. Moore <m3gacrypt0@gmail.com>
2019-09-05 14:43:18 -0600

    Update StatTracker Class to load new data structure by file
    Co-authored-by: Brian Bower <38663414+BabsLabs@users.noreply.github.com>
    Co-authored-by: Jonathan Patterson <jonpatt92@gmail.com>

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,80 +1,137 @@
 require 'csv'
+require_relative 'game'
+require_relative 'team'
+require_relative 'game_team'
 require_relative 'gameable'
-require_relative 'league'
-#require_relative 'teamable'
+require_relative 'leagueable'
+require_relative 'teamable'
+require_relative 'seasonable'
 
 class StatTracker
   include Gameable
-  include League
-  #include Teamable
+  include Leagueable
+  include Teamable
+  include Seasonable
 
-  attr_reader :data
+  attr_reader :games, :teams, :game_teams
 
   def initialize
-    @data = []
-    @games = [] #array of hashes
-    @teams = [] #array of hashes
-    @game_teams = [] #array of hashes
+    @games = [] #array of hashes with values equal to Game objects
+    @teams = [] #array of hashes with values equal to Team objects
+    @game_teams = [] #array of hashes with values equal to GameTeams objects
   end
 
   def self.from_csv(locations)
     stat_tracker = StatTracker.new
-    i = 0
 
-    locations.each_pair do |k, v|
-      x = 0
-      stat_tracker.data[i] = Hash.new
-      CSV.foreach(v, headers: true) do |row|
-          stat_tracker.data[i].store(v.to_s + x.to_s, row.to_hash)
-          x += 1
-      end
+    # creates unique Game objects as hashes in @games instance variable
+    CSV.foreach(locations[:games], headers: true) do |row|
+      stat_tracker.games << {row.fetch("game_id") => Game.new(row.fetch("game_id"),
+                                                      row.fetch("season"),
+                                                      row.fetch("type"),
+                                                      row.fetch("date_time"),
+                                                      row.fetch("away_team_id"),
+                                                      row.fetch("home_team_id"),
+                                                      row.fetch("away_goals").to_i,
+                                                      row.fetch("home_goals").to_i,
+                                                      row.fetch("venue"),
+                                                      row.fetch("venue_link")
+                                                      ) }
+    end
 
-      i += 1
+    # creates unique Team objects as hashes in @teams instance variable
+    CSV.foreach(locations[:teams], headers: true) do |row|
+      stat_tracker.games << {row.fetch("team_id") => Team.new(row.fetch("team_id"),
+                                                      row.fetch("franchiseId"),
+                                                      row.fetch("teamName"),
+                                                      row.fetch("abbreviation"),
+                                                      row.fetch("Stadium"),
+                                                      row.fetch("link")
+                                                      ) }
+    end
+
+    # creates unique Game_Teams objects as hashes in @games_teams instance variable
+    CSV.foreach(locations[:game_teams], headers: true) do |row|
+      stat_tracker.games << {row.fetch("game_id") => Game_Team.new(row.fetch("team_id"),
+                                                      row.fetch("HoA"),
+                                                      row.fetch("result"),
+                                                      row.fetch("settled_in"),
+                                                      row.fetch("head_coach"),
+                                                      row.fetch("goals").to_i,
+                                                      row.fetch("shots").to_i,
+                                                      row.fetch("tackles").to_i,
+                                                      row.fetch("pim").to_i,
+                                                      row.fetch("powerPlayOpportunities").to_i,
+                                                      row.fetch("powerPlayGoals").to_i,
+                                                      row.fetch("faceOffWinPercentage").to_f,
+                                                      row.fetch("giveaways").to_i,
+                                                      row.fetch("takeaways").to_i
+                                                      ) }
     end
 
     stat_tracker
 
   end
 
+  # First Attempt
+  # def self.from_csv(locations)
+  #   stat_tracker = StatTracker.new
+  #   i = 0
+  #
+  #   locations.each_pair do |k, v|
+  #     x = 0
+  #     stat_tracker.data[i] = Hash.new
+  #     CSV.foreach(v, headers: true) do |row|
+  #         stat_tracker.data[i].store(v.to_s + x.to_s, row.to_hash)
+  #         x += 1
+  #     end
+  #
+  #     i += 1
+  #   end
+  #
+  #   stat_tracker
+  #
+  # end
+
   #to access the values from these helper methods use this convention:
   #stat_tracker.access_by_id("game","2012030223")[0]["<row key>"]
 
-  def access_by_id(file, id) #returns hash of games data for row by id
-
-    if file == "game"
-      index = 0
-      key = "game_id"
-    elsif file == "teams"
-      index = 1
-      key = "team_id"
-    elseif file == "game_teams"
-      index = 2
-      key = "game_id"
-    else
-      return puts "Invalid file, id pair."
-    end
-
-    storage = @data[index].select do |k, v|
-      v[key] == id
-    end
-
-    storage.values
-  end
-
-  #to access data
-  #stat_tracker.teams = array of hashes of data from the file "teams"
-  #stat_tracker.teams[key] = value
-
-  def games
-    @games = @data[0].values
-  end
-
-  def teams
-    @teams = @data[1].values
-  end
-
-  def game_teams
-    @game_teams = @data[2].values
-  end
+  # def access_by_id(file, id) #returns hash of games data for row by id
+  #
+  #   if file == "game"
+  #     index = 0
+  #     key = "game_id"
+  #   elsif file == "teams"
+  #     index = 1
+  #     key = "team_id"
+  #   elseif file == "game_teams"
+  #     index = 2
+  #     key = "game_id"
+  #   else
+  #     return puts "Invalid file, id pair."
+  #   end
+  #
+  #   storage = @data[index].select do |k, v|
+  #     v[key] == id
+  #   end
+  #
+  #   storage.values
+  # end
+  #
+  # #to access data
+  # #stat_tracker.teams = array of hashes of data from the file "teams"
+  # #stat_tracker.teams[key] = value
+  #
+  # def games
+  #   @games = @data[0].values
+  # end
+  #
+  # def teams
+  #   @teams = @data[1].values
+  # end
+  #
+  # def game_teams
+  #   @game_teams = @data[2].values
+  # end
 
 end
