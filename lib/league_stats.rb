4289a8ae12dfd4ef401a04d51ea9e22ba2859b51
Zac Isaacson <isaacson.zm@gmail.com>
2019-09-05 14:02:51 -0600

    Minor issues
    Co-authored-by: Mary Lang <51488670+mcat56@users.noreply.github.com>
    Co-authored-by: Scott Regenthal <50811220+freeheeling@users.noreply.github.com>
    Co-authored-by: Sebastian Sloan <51456013+sasloan@users.noreply.github.com>

---
+++ lib/league_stats.rb
@@ -0,0 +1,61 @@
+module League
+
+# Total teams in data -integer
+  def count_of_teams
+    @game_objs.sum do 
+  end
+
+# Team with highest average goals scored per game, all seasons -string
+  def best_offense
+
+  end
+# Team with lowest average goals scored per game, all seasons -string
+  def worst_offense
+
+  end
+# Team with lowest number of goals allowed per game, all seasons -string
+  def best_defense
+
+  end
+
+# Team with highest number of goals allowed per game, all seasons -string
+  def worst_defense
+
+  end
+
+# Name of away team with highest average score per game, all seasons -String
+  def highest_scoring_visitor
+
+  end
+
+# Name of home team with highest average score per game, all seasons -string
+  def highest_scoring_home_team
+
+  end
+
+# Name of away team with lowest average score per game, all seasons -string
+  def lowest_scoring_visitior
+
+  end
+
+# Name of home team with lowest average score per game, all seasons -string
+  def lowest_scoring_home_team
+
+  end
+
+# Name of team with highest win percentage, all seasons -string
+  def winningest_team
+
+  end
+
+# Name of team with biggest difference between home and away win percentages -string
+  def best_fans
+
+  end
+
+# List of names of all teams with better away records(wins) than home records(wins) -array
+  def worst_fans
+
+  end
+
+end
---
+++ test/games_test.rb
@@ -0,0 +1,34 @@
+require_relative '../lib/games'
+require_relative 'test_helper'
+
+class GameTest < Minitest::Test
+
+  def setup
+    @line_2 = Game.new("2012030221,20122013,Postseason,5/16/13,3,6,2,3,Toyota Stadium,/api/v1/venues/null")
+  end
+
+  def test_it_exists
+    assert_instance_of Game, @line_2
+  end
+
+  def test_attributes
+    assert_equal 2012030221, @line_2.game_id
+    assert_equal 20122013, @line_2.season
+    assert_equal "Postseason", @line_2.type
+    assert_equal "5/16/13", @line_2.date_time
+    assert_equal 3, @line_2.away_team_id
+    assert_equal 6, @line_2.home_team_id
+    assert_equal 2, @line_2.away_goals
+    assert_equal 3, @line_2.home_goals
+    assert_equal "Toyota Stadium", @line_2.venue
+    assert_equal "/api/v1/venues/null", @line_2.venue_link
+  end
+
+
+
+
+
+
+
+
+end
