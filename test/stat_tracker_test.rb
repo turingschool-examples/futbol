607bcea36614bc34eaffd2f6d964763d45116a30
Carleigh Crockett <capleugh@gmail.com>
2019-09-05 16:19:25 -0600

    Add methods for biggest blowout and percentage home wins
    Co-authored-by: Ryan Hantak <47759923+rhantak@users.noreply.github.com>
    Co-authored-by: John Travers <46035439+johnktravers@users.noreply.github.com>
    Co-authored-by: David Barriga-Gomez <50917634+davidbarriga-gomez@users.noreply.github.com>

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -48,7 +48,13 @@ class StatTrackerTest < Minitest::Test
     assert_equal 2, @stat_tracker.lowest_total_score
   end
 
+  def test_biggest_blowout
+    assert_equal 3, @stat_tracker.biggest_blowout
+  end
 
+  def test_percentage_home_wins
+    assert_equal 65.00, @stat_tracker.percentage_home_wins
+  end
 
   def test_count_of_games_by_season
     expected = {"20122013"=>4,
@@ -59,4 +65,6 @@ class StatTrackerTest < Minitest::Test
               }
     assert_equal expected, @stat_tracker.count_of_games_by_season
   end
+
+
 end
