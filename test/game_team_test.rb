ccadc772039981180f5e3cbc66832f65173e889c
Tyler Balogh <tycbalogh@gmail.com>
2019-09-05 13:24:48 -0600

    Fix test data
    Co-authored-by: Alice Post <26877629+ap2322@users.noreply.github.com>
    Co-authored-by: David Lopez <43835133+Not-Zorro@users.noreply.github.com>

--- test/game_team_test.rb
+++ test/game_team_test.rb
@@ -19,14 +19,14 @@ class GameTeamTest < Minitest::Test
     assert_equal 'LOSS', @game_team.result
     assert_equal 'OT', @game_team.settled_in
     assert_equal 'John Tortorella', @game_team.head_coach
-    assert_equal '2', @game_team.goals
-    assert_equal '8', @game_team.shots
-    assert_equal '44', @game_team.tackles
-    assert_equal '8', @game_team.pim
-    assert_equal '3', @game_team.power_play_opportunities
-    assert_equal '0', @game_team.power_play_goals
-    assert_equal '44.8', @game_team.face_off_win_percentage
-    assert_equal '17', @game_team.giveaways
-    assert_equal '7', @game_team.takeaways
+    assert_equal 2, @game_team.goals
+    assert_equal 8, @game_team.shots
+    assert_equal 44, @game_team.tackles
+    assert_equal 8, @game_team.pim
+    assert_equal 3, @game_team.power_play_opportunities
+    assert_equal 0, @game_team.power_play_goals
+    assert_equal 44.8, @game_team.face_off_win_percentage
+    assert_equal 17, @game_team.giveaways
+    assert_equal 7, @game_team.takeaways
   end
 end
