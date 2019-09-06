9bd0b5c0e3054f7f3889e14f203719b53f7ba5a8
Brian Bower <brian@brianbower.com>
2019-09-06 12:33:45 -0600

    Add stat_tracker tests for existing and initialization

---
+++ test/stat_tracker_test.rb
@@ -0,0 +1,23 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+# require './test/test_helper'
+require './lib/stat_tracker'
+# require 'pry'
+
+class StatTrackerTest < Minitest::Test
+
+  def setup
+    @stat_tracker = StatTracker.new
+  end
+
+  def test_it_exists
+    assert_instance_of StatTracker, @stat_tracker
+  end
+
+  def test_initialization
+    assert_equal ({}), @stat_tracker.games
+    assert_equal ({}), @stat_tracker.teams
+    assert_equal [], @stat_tracker.game_teams
+  end
+
+end
