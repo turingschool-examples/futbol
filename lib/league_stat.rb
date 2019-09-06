619398fc368e49c62947fffac18f67f205114e3b
David Lopez <ldavid.x.lopezl@gmail.com>
2019-09-06 10:02:26 -0600

    Fixes wrong result for best_offence

--- lib/league_stat.rb
+++ lib/league_stat.rb
@@ -7,14 +7,19 @@ module LeagueStat
   def team_id_and_goals
     team_id_goals = Hash.new(0)
     all_game_teams.each do |game_team|
-      team_id_goals[game_team.team_id] += game_team.goals
+      if team_id_goals.has_key?(game_team.team_id) == false
+        team_id_goals[game_team.team_id] = {goals: game_team.goals, game_count: 1}
+      else
+        team_id_goals[game_team.team_id][:goals] += game_team.goals
+        team_id_goals[game_team.team_id][:game_count] += 1
+      end
     end
     team_id_goals
   end
 
   def best_offense
-    best_offense = team_id_and_goals.max_by do |id, goals|
-      goals
+    best_offense = team_id_and_goals.max_by do |id, hash|
+      hash[:goals] / hash[:game_count].to_f
     end
     all_teams[best_offense[0]].team_name
   end
