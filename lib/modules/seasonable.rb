fd8dd44a5b57a8d349ee24c3a0353a4a48cfe6ca
Daniel <danmoran.pro@gmail.com>
2019-09-07 11:00:03 -0600

    Add skeleton methods for modules.
    Co-authored-by: Michael Cooper <8922930+CoopTang@users.noreply.github.com>
    Co-authored-by: Rachel Lew <48839191+rlew421@users.noreply.github.com>

---
+++ lib/modules/seasonable.rb
@@ -0,0 +1,35 @@
+module Seasonable
+
+
+  def biggest_bust(season_id)
+
+  end
+
+  def biggest_surprise(season_id)
+
+  end
+
+  def winningest_coach(season_id)
+
+  end
+
+  def worst_coach(season_id)
+
+  end
+
+  def most_accurate_team(season_id)
+
+  end
+
+  def least_accurate_team(season_id)
+
+  end
+
+  def most_tackles(season_id)
+
+  end
+
+  def fewest_tackles(season_id)
+
+  end 
+end
