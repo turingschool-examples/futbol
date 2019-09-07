8178824c64ce1f84627704f94466121f65f0f5f3
Tyler Balogh <tycbalogh@gmail.com>
2019-09-06 19:10:28 -0600

    Refactor team_id_and_goals to read nested hash all_game_teams
    Co-authored-by: Alice Post <26877629+ap2322@users.noreply.github.com>

--- lib/league_stat.rb
+++ lib/league_stat.rb
@@ -5,11 +5,21 @@ module LeagueStat
   end
 
   def team_id_and_goals
-    @team_id_goals ||= all_game_teams.each_with_object(Hash.new) do |game_team, team_id_goals|
-      team_id_goals[game_team.team_id] ||= {goals: 0, game_count: 0}
-      team_id_goals[game_team.team_id][:goals] += game_team.goals
-      team_id_goals[game_team.team_id][:game_count] += 1
-    end
+    @team_id_goals ||=
+      all_game_teams.each_with_object(Hash.new) do |(game_id, home_away), team_id_goals|
+        home_away.each do |key, game_team|
+        # home_away.each do |key, game_team|
+          team_id_goals[game_team.team_id] ||= {goals: 0, game_count: 0, opponent_goals: 0}
+          team_id_goals[game_team.team_id][:goals] += game_team.goals
+          team_id_goals[game_team.team_id][:game_count] += 1
+          team_id_goals[game_team.team_id][:game_id] = game_team.game_id
+          if key == "away"
+            team_id_goals[game_team.team_id][:opponent_goals] += home_away["home"].goals
+          elsif key == "home"
+            team_id_goals[game_team.team_id][:opponent_goals] += home_away["away"].goals
+          end
+        end
+      end
   end
 
   def best_offense
@@ -27,7 +37,10 @@ module LeagueStat
   end
 
   def best_defense
-    
+    best_defense = team_id_and_goals.min_by do |id, hash|
+      hash[:opponent_goals] / hash[:game_count].to_f
+    end
+    all_teams[best_defense[0]].team_name
   end
 
   def worst_defense
