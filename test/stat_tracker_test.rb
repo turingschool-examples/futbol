22d03412bb3b09058917377772efa35852f03f08
Graham Thompson <grwthomps@gmail.com>
2019-09-07 11:43:40 -0600

    Add tests for count_of_teams and best_offense methods
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>
    Co-Authored-By: Laura Schulz <lrs8810@users.noreply.github.com>
    Co-Authored-By: Tyla Devon <tyladevon@users.noreply.github.com>

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -70,4 +70,12 @@ class StatTrackerTest < Minitest::Test
     }
     assert_equal expected, @stat_tracker.average_goals_by_season
   end
+
+  def test_count_of_teams
+    assert_equal 32, @stat_tracker.count_of_teams
+  end
+
+  def test_best_offense
+    assert_equal "Reign FC", @stat_tracker.best_offense
+  end
 end
