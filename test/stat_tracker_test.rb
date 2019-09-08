3a234f8e79fd00fda4efd51a0109ed0339835e37
Hillary Stewart <hillarystewart4427@gmail.com>
2019-09-08 12:26:18 -0600

    Add test for highest scoring visitor, highest scoring home team, lowest scoring visitor, lowest scoring home team, winningest team and best fans
    Co-Authored-By: Laura Schulz <lrs8810@users.noreply.github.com>
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -90,4 +90,32 @@ class StatTrackerTest < Minitest::Test
   def test_worst_defense
     assert_equal "Columbus Crew SC", @stat_tracker.worst_defense
   end
+
+  def test_highest_scoring_visitor
+    assert_equal "FC Dallas", @stat_tracker.highest_scoring_visitor
+  end
+
+  def test_highest_scoring_home_team
+    assert_equal "Reign FC", @stat_tracker.highest_scoring_home_team
+  end
+
+  def test_lowest_scoring_visitor
+    assert_equal "San Jose Earthquakes", @stat_tracker.lowest_scoring_visitor
+  end
+
+  def test_lowest_scoring_home_team
+    assert_equal "Utah Royals FC", @stat_tracker.lowest_scoring_home_team
+  end
+
+  def test_winningest_team
+    assert_equal "Reign FC", @stat_tracker.winningest_team
+  end
+
+  def test_best_fans
+    assert_equal "San Jose Earthquakes", @stat_tracker.best_fans
+  end
+
+  def test_worst_fans
+    assert_equal ["Houston Dynamo"], @stat_tracker.best_fans
+  end
 end
