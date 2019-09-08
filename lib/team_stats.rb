06209f3d825e09a654278f1a8daa2f5a2fd22299
John Travers <johnk.travers@gmail.com>
2019-09-08 13:15:54 -0600

    Reorder team statistics methods
    Co-authored-by: Ryan Hantak <47759923+rhantak@users.noreply.github.com>
    Co-authored-by: David Barriga-Gomez <50917634+DavidBarriga-Gomez@users.noreply.github.com>
    Co-authored-by: Carleigh Crockett <47065120+Capleugh@users.noreply.github.com>

--- lib/team_stats.rb
+++ lib/team_stats.rb
@@ -1,75 +1,5 @@
 module TeamStats
 
-  def favorite_opponent(team_id)
-    opponent_id = opponent_stats(team_id).min_by do |tm_id, counts|
-      counts[:wins] / counts[:games].to_f
-    end[0]
-
-    @teams[opponent_id].team_name
-  end
-
-  def rival(team_id)
-    opponent_id = opponent_stats(team_id).min_by do |tm_id, counts|
-      counts[:losses] / counts[:games].to_f
-    end[0]
-
-    @teams[opponent_id].team_name
-  end
-
-  def head_to_head(team_id)
-    record = Hash.new
-    opponent_stats = opponent_stats(team_id)
-
-    opponent_stats.each do |tm_id, counts|
-      record[@teams[tm_id].team_name] = (counts[:losses] / counts[:games].to_f).round(2)
-    end
-
-    record
-  end
-
-  # def seasonal_summary(team_id)
-  #   team_id = team_id.to_i
-  #   season_summary = Hash.new { Hash.new { |h,k| h[k] = Hash.new(0) } }
-  #   opponent_stats = opponent_stats(team_id)
-  #
-  #   @seasons.each do |season|
-  #     @games.each do |game_id, game|
-  #       if game.season == season && game.type == "Postseason"
-  #         # season_summary[season][:postseason]
-  #       elsif game.season == season && game.type == "Regular Season"
-  #         # season_summary[season][:regular_season]
-  #       end
-  #     end
-  #   end
-  #
-  #   season_summary[]
-  # end
-
-  def most_goals_scored(team_id)
-    team_id = team_id.to_i
-    most_goals = 0
-    @game_teams.each do |game_id, game_teams|
-      game_teams.each do |game_team|
-        if game_team.team_id == team_id
-          most_goals = game_team.goals if game_team.goals > most_goals
-        end
-      end
-    end
-    most_goals
-  end
-
-  def fewest_goals_scored(team_id)
-    team_id = team_id.to_i
-    fewest_goals = 50
-    @game_teams.each do |game_id, game_teams|
-      game_teams.each do |game_team|
-        if game_team.team_id == team_id
-          fewest_goals = game_team.goals if game_team.goals < fewest_goals
-        end
-      end
-    end
-    fewest_goals
-
   def best_season(team_id)
     team_id = team_id.to_i
     team_games = @games.find_all {|game_id, game| game.home_team_id == team_id || game.away_team_id == team_id}
@@ -123,7 +53,7 @@ module TeamStats
         season[game.season][:season_wins] += 1
       end
     end
-   
+
     worst_season = ""
     worst_season_percent = 101
     seasons.each do |season_id|
@@ -139,4 +69,76 @@ module TeamStats
   def average_win_percentage(team_id)
     #
   end
+
+  def most_goals_scored(team_id)
+    team_id = team_id.to_i
+    most_goals = 0
+    @game_teams.each do |game_id, game_teams|
+      game_teams.each do |game_team|
+        if game_team.team_id == team_id
+          most_goals = game_team.goals if game_team.goals > most_goals
+        end
+      end
+    end
+    most_goals
+  end
+
+  def fewest_goals_scored(team_id)
+    team_id = team_id.to_i
+    fewest_goals = 50
+    @game_teams.each do |game_id, game_teams|
+      game_teams.each do |game_team|
+        if game_team.team_id == team_id
+          fewest_goals = game_team.goals if game_team.goals < fewest_goals
+        end
+      end
+    end
+    fewest_goals
+  end
+
+  def favorite_opponent(team_id)
+    opponent_id = opponent_stats(team_id).min_by do |tm_id, counts|
+      counts[:wins] / counts[:games].to_f
+    end[0]
+
+    @teams[opponent_id].team_name
+  end
+
+  def rival(team_id)
+    opponent_id = opponent_stats(team_id).min_by do |tm_id, counts|
+      counts[:losses] / counts[:games].to_f
+    end[0]
+
+    @teams[opponent_id].team_name
+  end
+
+  def head_to_head(team_id)
+    record = Hash.new
+    opponent_stats = opponent_stats(team_id)
+
+    opponent_stats.each do |tm_id, counts|
+      record[@teams[tm_id].team_name] = (counts[:losses] / counts[:games].to_f).round(2)
+    end
+
+    record
+  end
+
+  # def seasonal_summary(team_id)
+  #   team_id = team_id.to_i
+  #   season_summary = Hash.new { Hash.new { |h,k| h[k] = Hash.new(0) } }
+  #   opponent_stats = opponent_stats(team_id)
+  #
+  #   @seasons.each do |season|
+  #     @games.each do |game_id, game|
+  #       if game.season == season && game.type == "Postseason"
+  #         # season_summary[season][:postseason]
+  #       elsif game.season == season && game.type == "Regular Season"
+  #         # season_summary[season][:regular_season]
+  #       end
+  #     end
+  #   end
+  #
+  #   season_summary[]
+  # end
+
 end
