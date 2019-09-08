ac46c2193c3f79bfdc7170fb44a79f83bf8666ce
Zac Isaacson <isaacson.zm@gmail.com>
2019-09-07 21:22:10 -0600

    Add season stats tests

--- test/season_stats_test.rb
+++ test/season_stats_test.rb
@@ -7,14 +7,48 @@ require_relative '../lib/game_teams'
 class SeasonStatsTest < MiniTest::Test
 
   def setup
-    locations = { games: './data/dummy_games.csv', teams: './data/teams.csv', game_teams: './data/dummy_game_teams.csv' }
+    locations = { games: './data/games.csv', teams: './data/teams.csv', game_teams: './data/game_teams.csv' }
     @stat_tracker = StatTracker.from_csv(locations)
   end
 
   def test_biggest_bust
+    assert_equal "Montreal Impact", @stat_tracker.biggest_bust("20132014")
+    assert_equal "Sporting Kansas City", @stat_tracker.biggest_bust("20142015")
+  end
 
+  def test_biggest_surprise
+    assert_equal "FC Cincinnati", @stat_tracker.biggest_surprise("20132014")
+    assert_equal "Minnesota United FC", @stat_tracker.biggest_surprise("20142015")
+  end
 
+  def test_winningest_coach
+    assert_equal "Claude Julien", @stat_tracker.winningest_coach("20132014")
+    assert_equal "Alain Vigneault", @stat_tracker.winningest_coach("20142015")
+  end
 
+  def test_worst_coach
+    assert_equal "Peter Laviolette", @stat_tracker.worst_coach("20132014")
+    assert_equal "Craig MacTavish", @stat_tracker.worst_coach("20142015")
+  end
 
+  def test_most_accurate_team
+    assert_equal "Real Salt Lake", @stat_tracker.most_accurate_team("20132014")
+    assert_equal "Toronto FC", @stat_tracker.most_accurate_team("20142015")
+  end
+
+  def test_least_accurate_team
+    assert_equal "New York City FC", @stat_track.least_accurate_team("20132014")
+    assert_equal "Columbus Crew SC", @stat_track.least_accurate_team("20142015")
+  end
+
+  def test_most_tackles
+    assert_equal "FC Cincinnati", @stat_tracker.most_tackles("20132014")
+    assert_equal "Seattle Sounders FC", @stat_track.most_tackles("20142015")
+  end
+
+  def test_fewest_tackles
+    assert_equal "Atlanta United", @stat_tracker.fewest_tackles("20132014")
+    assert_equal "Orlando City SC", @stat_tracker.fewest_tackles("20142015")
+  end
 
 end
