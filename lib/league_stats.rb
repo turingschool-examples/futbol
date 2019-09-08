9e99b54165104bd9cd04cd12ca7c40e980b97353
Carleigh Crockett <capleugh@gmail.com>
2019-09-07 18:25:48 -0600

    Add data to dummy files and method for highest_scoring_home_team
    Co-authored-by: Ryan Hantak <47759923+rhantak@users.noreply.github.com>
    Co-authored-by: John Travers <46035439+johnktravers@users.noreply.github.com>
    Co-authored-by: David Barriga-Gomez <50917634+davidbarriga-gomez@users.noreply.github.com>

--- lib/league_stats.rb
+++ lib/league_stats.rb
@@ -3,7 +3,7 @@ module LeagueStats
   def count_of_teams
     @teams.length
     #works fine but when we refactor we should consider doing this with uniq so that
-    #if there are repetitive teams it still works. 
+    #if there are repetitive teams it still works.
   end
 
   def best_offense
@@ -19,12 +19,12 @@ module LeagueStats
     end[0]
     @teams[lowest_average_number_of_goals_id].team_name
   end
-  
+
   def best_defense
     best_def_id = @team_result_count.min_by do |team_id, counts|
       counts[:goals_allowed] / counts[:games].to_f
     end[0]
-    
+
     @teams[best_def_id].team_name
   end
 
@@ -46,7 +46,7 @@ module LeagueStats
 
   def winningest_team
     winning_team_id = @team_result_count.max_by do |team_id, counts|
-      (counts[:home_wins] + counts[:away_wins]) / counts[:games].to_f if counts[:games] != 0
+      (counts[:home_wins] + counts[:away_wins]) / counts[:games].to_f
     end[0]
 
     @teams[winning_team_id].team_name
@@ -62,7 +62,7 @@ module LeagueStats
 
   def worst_fans
     worst_fans_teams = @team_result_count.find_all do |team_id, counts|
-      counts[:away_wins] > counts[:home_wins]
+      (counts[:away_wins] / counts[:away_games]) > (counts[:home_wins] / counts[:home_games])
     end
 
     team_names = []
@@ -74,28 +74,33 @@ module LeagueStats
   end
 
   def highest_scoring_home_team
+    highest_scoring_home_team_id = @team_result_count.max_by do |team_id, counts|
+      counts[:home_goals] / counts[:home_games].to_f
+    end[0]
+    @teams[highest_scoring_home_team_id].team_name
+
     # set these to 0 because we're just counting them
-    num_home_goals = Hash.new(0)
-    num_games = Hash.new(0)
-
-    @games.each do |game_id, game|
-
-      # for each team, we want the name of the team with the highest average score per game across all seasons when they are home
-
-      num_games[game.home_team_id] += 1
-      num_home_goals[game.home_team_id] += game.home_goals
-    end
-
-    greatest = [nil, 0]
-    @teams.each do |team_id, team|
-      if num_games[team_id] > 0
-        average = num_home_goals[team_id].to_f / num_games[team_id]
-        greatest = [team_id, average] if average > greatest[1]
-      end
-    end
-
-    @teams[greatest[0]].team_name
-
+    # num_home_goals = Hash.new(0)
+    # num_games = Hash.new(0)
+    #
+    # @games.each do |game_id, game|
+    #
+    #   # for each team, we want the name of the team with the highest average score per game across all seasons when they are home
+    #
+    #   num_games[game.home_team_id] += 1
+    #   num_home_goals[game.home_team_id] += game.home_goals
+    # end
+    #
+    # greatest = [nil, 0]
+    # @teams.each do |team_id, team|
+    #   if num_games[team_id] > 0
+    #     average = num_home_goals[team_id].to_f / num_games[team_id]
+    #     greatest = [team_id, average] if average > greatest[1]
+    #   end
+    # end
+    #
+    # @teams[greatest[0]].team_name
+    #
 
 
     # # want to take each return value, find the max, and then return the team name associated with the team_id
@@ -103,4 +108,18 @@ module LeagueStats
     #   average
     # end
   end
+
+  # def lowest_scoring_visitor
+  #   lowest_scoring_visitor_id = @team_result_count.min_by do |team_id, counts|
+  #     counts[:away_goals] / counts[:away_games].to_f
+  #   end[0]
+  #   @teams[lowest_scoring_visitor_id].team_name
+  # end
+  #
+  # def lowest_scoring_home_team
+  #   lowest_scoring_home_team_id = @team_result_count.min_by do |team_id, counts|
+  #     counts[:home_goals] / counts[:home_games].to_f
+  #   end[0]
+  #   @teams[lowest_scoring_home_team_id].team_name
+  # end
 end
