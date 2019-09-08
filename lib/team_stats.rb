b82f75888c82413b40f42e55d4bb9f82d7e8c023
Scott Regenthal <scottregenthal@gmail.com>
2019-09-08 16:08:57 -0600

    Add teams stats file (WIP)
    Co-authored-by: Zac Isaacson <50255174+zacisaacson@users.noreply.github.com>
    Co-authored-by: Mary Lang <51488670+mcat56@users.noreply.github.com>
    Co-authored-by: Sebastian Sloan <sebastiansloan94@gmail.com>

---
+++ lib/team_stats.rb
@@ -0,0 +1,103 @@
+module TeamStats
+
+  def team_info(team_id)
+    info = {}
+    info["team_id"] = @teams[team_id].team_id
+    info["franchiseId"] = @teams[team_id].franchiseId
+    info["teamName"] = @teams[team_id].teamName
+    info["abbreviation"] = @teams[team_id].abbreviation
+    info["Stadium"] = @teams[team_id].stadium
+    info["link"] = @teams[team_id].link
+    info
+  end
+
+  def worst_and_best_season(team_id)
+    seasons = Hash.new(0)
+    game_ids = []
+    @game_teams[team_id].each {|game| game_ids << game.game_id}
+    @game_count_by_season = Hash.new(0)
+    game_ids.each do |game_id|
+      if @games[game_id].home_team_id == team_id
+        @game_count_by_season[@games[game_id].season] += 1
+        if @games[game_id].home_goals > @games[game_id].away_goals
+          seasons[@games[game_id].season] += 1
+        end
+      elsif @games[game_id].away_team_id == team_id
+        @game_count_by_season[@games[game_id].season] += 1
+        if @games[game_id].away_goals > @games[game_id].home_goals
+          seasons[@games[game_id].season] += 1
+        end
+      end
+    end
+    seasons = seasons.each do |season, wins|
+    seasons[season] = ((wins.to_f / @game_count_by_season[season]) * 100).round(2)
+    end
+    max = seasons.max_by {|id, count| count}[0]
+    min = seasons.min_by {|id, count| count}[0]
+    @min_max = [min, max]
+  end
+
+  def best_season(team_id)
+    worst_and_best_season(team_id)[1]
+  end
+
+  def worst_season(team_id)
+    worst_and_best_season(team_id)[0]
+  end
+
+  def average_win_percentage(team_id)
+    (calculate_percents[2][team_id]/100).round(2)
+  end
+
+  def most_goals_scored(team_id)
+    @game_teams[team_id].max_by{ |object| object.goals }.goals
+  end
+
+  def fewest_goals_scored(team_id)
+    @game_teams[team_id].min_by{ |object| object.goals }.goals
+  end
+
+  def best_and_worst_opponent(team_id)
+    games = []
+    @games.each do |game_id, game|
+      if team_id == game.home_team_id || team_id == game.away_team_id
+        games << game
+      end
+    end
+    opponent_games = Hash.new(0)
+    opponent_wins = Hash.new(0)
+    games.each do |game|
+      if game.home_team_id == team_id
+        opponent_games[game.away_team_id] += 1
+        opponent_wins[game.away_team_id] += 0
+        if game.away_goals > game.home_goals
+          opponent_wins[game.away_team_id] += 1
+        end
+      elsif game.away_team_id == team_id
+        opponent_games[game.home_team_id] += 1
+        opponent_wins[game.home_team_id] += 0
+        if game.home_goals > game.away_goals
+          opponent_wins[game.home_team_id] += 1
+        end
+      end
+    end
+    opponent_percent = {}
+    opponent_wins.each {|opponent, wins|
+      opponent_percent[opponent] = ((wins.to_f / opponent_games[opponent]) * 100).round(2)
+    }
+    min = opponent_percent.min_by{ |opp, percent| percent }[0]
+    max = opponent_percent.max_by{ |opp, percent| percent }[0]
+    @teams[min].teamName
+    @teams[max].teamName
+    [@teams[min].teamName, @teams[max].teamName]
+  end
+
+  def favorite_opponent(team_id)
+    best_and_worst_opponent(team_id)[0]
+  end
+
+  def rival(team_id)
+    best_and_worst_opponent(team_id)[1]
+  end
+
+end
--- test/team_stats_test.rb
+++ test/team_stats_test.rb
@@ -12,191 +12,196 @@ class TeamStatsTest < MiniTest::Test
   end
 
 # hash with key/value pairs for each of the attributes of a team – Hash
-  def test_team_info
-    expected = {
-      "team_id" => "18",
-      "franchiseId" => "34",
-      "teamName" => "Minnesota United FC",
-      "abbreviation" => "MIN",
-      "Stadium" => "Allianz Field",
-      "link" => "/api/v1/teams/18"
-    }
-    assert_equal expected, @stat_tracker.team_info("18")
-  end
+  # def test_team_info
+  #   skip
+  #   expected = {
+  #     "team_id" => "18",
+  #     "franchiseId" => "34",
+  #     "teamName" => "Minnesota United FC",
+  #     "abbreviation" => "MIN",
+  #     "Stadium" => "Allianz Field",
+  #     "link" => "/api/v1/teams/18"
+  #   }
+  #   assert_equal expected, @stat_tracker.team_info("18")
+  # end
 
 # season with the highest win percentage for a team – Integer
-  def test_best_season
-    assert_equal "20132014", @stat_tracker.best_season("6")
-  end
-
-# season with the lowest win percentage for a team – Integer
-  def test_worst_season
-    assert_equal "20142015", @stat_tracker.worst_season("6")
-  end
-
-# average win percentage of all games for a team – Float
-  def test_average_win_percentage
-    assert_equal 0.49, @stat_tracker.average_win_percentage("6")
-  end
-
-# highest number of goals a particular team has scored in a single game – Integer
-  def test_most_goals_scored
-    assert_equal 7, @stat_tracker.most_goals_scored("18")
-  end
-
-# lowest numer of goals a particular team has scored in a single game – Integer
-  def test_fewest_goals_scored
-    assert_equal 0, @stat_tracker.fewest_goals_scored("18")
-  end
-
-# name of the opponent that has the lowest win percentage against the given
-# team – String
+  # def test_best_season
+  #   assert_equal "20132014", @stat_tracker.best_season("6")
+  # end
+#
+# # season with the lowest win percentage for a team – Integer
+  # def test_worst_season
+  #   assert_equal "20142015", @stat_tracker.worst_season("6")
+  # end
+#
+# # average win percentage of all games for a team – Float
+  # def test_average_win_percentage
+  #   assert_equal 0.49, @stat_tracker.average_win_percentage("6")
+  # end
+#
+# # highest number of goals a particular team has scored in a single game – Integer
+  # def test_most_goals_scored
+  #   assert_equal 7, @stat_tracker.most_goals_scored("18")
+  # end
+#
+# # lowest numer of goals a particular team has scored in a single game – Integer
+  # def test_fewest_goals_scored
+  #   assert_equal 0, @stat_tracker.fewest_goals_scored("18")
+  # end
+#
+# # name of the opponent that has the lowest win percentage against the given
+# # team – String
   def test_favorite_opponent
     assert_equal "DC United", @stat_tracker.favorite_opponent("18")
   end
-
-# name of the opponent that has the highest win percentage against the given
-# team – String
+#
+# # name of the opponent that has the highest win percentage against the given
+# # team – String
   def test_rival
     assert_equal "Houston Dash", @stat_tracker.rival("18")
   end
-
-# biggest difference between team goals and opponent goals for a win for the
-# given team – Integer
-  def test_biggest_team_blowout
-    assert_equal 5, @stat_tracker.biggest_team_blowout("18")
-  end
-
-#  biggest difference between team goals and opponent goals for a loss for the
-# given team – Integer
-  def test_worst_loss
-    assert_equal 4, @stat_tracker.worst_loss("18")
-  end
-
-# record (as a hash - win/loss) against all opponents with the opponents’ names
-# as keys and the win percentage against that opponent as a value – Hash
-  def test_head_to_head
-    expected = {
-     "Atlanta United"=>0.5,
-     "Chicago Fire"=>0.3,
-     "FC Cincinnati"=>0.39,
-     "DC United"=>0.8,
-     "FC Dallas"=>0.4,
-     "Houston Dynamo"=>0.4,
-     "Sporting Kansas City"=>0.25,
-     "LA Galaxy"=>0.29,
-     "Los Angeles FC"=>0.44,
-     "Montreal Impact"=>0.33,
-     "New England Revolution"=>0.47,
-     "New York City FC"=>0.6,
-     "New York Red Bulls"=>0.4,
-     "Orlando City SC"=>0.37,
-     "Portland Timbers"=>0.3,
-     "Philadelphia Union"=>0.44,
-     "Real Salt Lake"=>0.42,
-     "San Jose Earthquakes"=>0.33,
-     "Seattle Sounders FC"=>0.5,
-     "Toronto FC"=>0.33,
-     "Vancouver Whitecaps FC"=>0.44,
-     "Chicago Red Stars"=>0.48,
-     "Houston Dash"=>0.1,
-     "North Carolina Courage"=>0.2,
-     "Orlando Pride"=>0.47,
-     "Portland Thorns FC"=>0.45,
-     "Reign FC"=>0.33,
-     "Sky Blue FC"=>0.3,
-     "Utah Royals FC"=>0.6,
-     "Washington Spirit FC"=>0.67,
-     "Columbus Crew SC"=>0.5
-    }
-    assert_equal expected, @stat_tracker.head_to_head("18")
-  end
-
-# for each season that the team has played, a hash that has two keys
-# (:regular_season and :postseason), that each point to a hash with the
-# following keys: :win_percentage, :total_goals_scored, :total_goals_against,
-# :average_goals_scored, :average_goals_against – Hash
-  def test_seasonal_summary
-    expected = {"20162017"=>
-       {:postseason=>
-         {:win_percentage=>0.59,
-          :total_goals_scored=>48,
-          :total_goals_against=>40,
-          :average_goals_scored=>2.18,
-          :average_goals_against=>1.82},
-        :regular_season=>
-         {:win_percentage=>0.38,
-          :total_goals_scored=>180,
-          :total_goals_against=>170,
-          :average_goals_scored=>2.2,
-          :average_goals_against=>2.07}},
-      "20172018"=>
-       {:postseason=>
-         {:win_percentage=>0.54,
-          :total_goals_scored=>29,
-          :total_goals_against=>28,
-          :average_goals_scored=>2.23,
-          :average_goals_against=>2.15},
-        :regular_season=>
-         {:win_percentage=>0.44,
-          :total_goals_scored=>187,
-          :total_goals_against=>162,
-          :average_goals_scored=>2.28,
-          :average_goals_against=>1.98}},
-      "20132014"=>
-       {:postseason=>
-         {:win_percentage=>0.0,
-          :total_goals_scored=>0,
-          :total_goals_against=>0,
-          :average_goals_scored=>0.0,
-          :average_goals_against=>0.0},
-        :regular_season=>
-         {:win_percentage=>0.38,
-          :total_goals_scored=>166,
-          :total_goals_against=>177,
-          :average_goals_scored=>2.02,
-          :average_goals_against=>2.16}},
-      "20122013"=>
-       {:postseason=>
-         {:win_percentage=>0.0,
-          :total_goals_scored=>0,
-          :total_goals_against=>0,
-          :average_goals_scored=>0.0,
-          :average_goals_against=>0.0},
-        :regular_season=>
-         {:win_percentage=>0.25,
-          :total_goals_scored=>85,
-          :total_goals_against=>103,
-          :average_goals_scored=>1.77,
-          :average_goals_against=>2.15}},
-      "20142015"=>
-       {:postseason=>
-         {:win_percentage=>0.67,
-          :total_goals_scored=>17,
-          :total_goals_against=>13,
-          :average_goals_scored=>2.83,
-          :average_goals_against=>2.17},
-        :regular_season=>
-         {:win_percentage=>0.5,
-          :total_goals_scored=>186,
-          :total_goals_against=>162,
-          :average_goals_scored=>2.27,
-          :average_goals_against=>1.98}},
-      "20152016"=>
-       {:postseason=>
-         {:win_percentage=>0.36,
-          :total_goals_scored=>25,
-          :total_goals_against=>33,
-          :average_goals_scored=>1.79,
-          :average_goals_against=>2.36},
-        :regular_season=>
-         {:win_percentage=>0.45,
-          :total_goals_scored=>178,
-          :total_goals_against=>159,
-          :average_goals_scored=>2.17,
-          :average_goals_against=>1.94}}
-    }
-    assert_equal expected, @stat_tracker.seasonal_summary("18")
-  end
+#
+# # biggest difference between team goals and opponent goals for a win for the
+# # given team – Integer
+#   def test_biggest_team_blowout
+#     skip
+#     assert_equal 5, @stat_tracker.biggest_team_blowout("18")
+#   end
+#
+# #  biggest difference between team goals and opponent goals for a loss for the
+# # given team – Integer
+#   def test_worst_loss
+#     skip
+#     assert_equal 4, @stat_tracker.worst_loss("18")
+#   end
+#
+# # record (as a hash - win/loss) against all opponents with the opponents’ names
+# # as keys and the win percentage against that opponent as a value – Hash
+#   def test_head_to_head
+#     skip
+#     expected = {
+#      "Atlanta United"=>0.5,
+#      "Chicago Fire"=>0.3,
+#      "FC Cincinnati"=>0.39,
+#      "DC United"=>0.8,
+#      "FC Dallas"=>0.4,
+#      "Houston Dynamo"=>0.4,
+#      "Sporting Kansas City"=>0.25,
+#      "LA Galaxy"=>0.29,
+#      "Los Angeles FC"=>0.44,
+#      "Montreal Impact"=>0.33,
+#      "New England Revolution"=>0.47,
+#      "New York City FC"=>0.6,
+#      "New York Red Bulls"=>0.4,
+#      "Orlando City SC"=>0.37,
+#      "Portland Timbers"=>0.3,
+#      "Philadelphia Union"=>0.44,
+#      "Real Salt Lake"=>0.42,
+#      "San Jose Earthquakes"=>0.33,
+#      "Seattle Sounders FC"=>0.5,
+#      "Toronto FC"=>0.33,
+#      "Vancouver Whitecaps FC"=>0.44,
+#      "Chicago Red Stars"=>0.48,
+#      "Houston Dash"=>0.1,
+#      "North Carolina Courage"=>0.2,
+#      "Orlando Pride"=>0.47,
+#      "Portland Thorns FC"=>0.45,
+#      "Reign FC"=>0.33,
+#      "Sky Blue FC"=>0.3,
+#      "Utah Royals FC"=>0.6,
+#      "Washington Spirit FC"=>0.67,
+#      "Columbus Crew SC"=>0.5
+#     }
+#     assert_equal expected, @stat_tracker.head_to_head("18")
+#   end
+#
+# # for each season that the team has played, a hash that has two keys
+# # (:regular_season and :postseason), that each point to a hash with the
+# # following keys: :win_percentage, :total_goals_scored, :total_goals_against,
+# # :average_goals_scored, :average_goals_against – Hash
+#   def test_seasonal_summary
+#     skip
+#     expected = {"20162017"=>
+#        {:postseason=>
+#          {:win_percentage=>0.59,
+#           :total_goals_scored=>48,
+#           :total_goals_against=>40,
+#           :average_goals_scored=>2.18,
+#           :average_goals_against=>1.82},
+#         :regular_season=>
+#          {:win_percentage=>0.38,
+#           :total_goals_scored=>180,
+#           :total_goals_against=>170,
+#           :average_goals_scored=>2.2,
+#           :average_goals_against=>2.07}},
+#       "20172018"=>
+#        {:postseason=>
+#          {:win_percentage=>0.54,
+#           :total_goals_scored=>29,
+#           :total_goals_against=>28,
+#           :average_goals_scored=>2.23,
+#           :average_goals_against=>2.15},
+#         :regular_season=>
+#          {:win_percentage=>0.44,
+#           :total_goals_scored=>187,
+#           :total_goals_against=>162,
+#           :average_goals_scored=>2.28,
+#           :average_goals_against=>1.98}},
+#       "20132014"=>
+#        {:postseason=>
+#          {:win_percentage=>0.0,
+#           :total_goals_scored=>0,
+#           :total_goals_against=>0,
+#           :average_goals_scored=>0.0,
+#           :average_goals_against=>0.0},
+#         :regular_season=>
+#          {:win_percentage=>0.38,
+#           :total_goals_scored=>166,
+#           :total_goals_against=>177,
+#           :average_goals_scored=>2.02,
+#           :average_goals_against=>2.16}},
+#       "20122013"=>
+#        {:postseason=>
+#          {:win_percentage=>0.0,
+#           :total_goals_scored=>0,
+#           :total_goals_against=>0,
+#           :average_goals_scored=>0.0,
+#           :average_goals_against=>0.0},
+#         :regular_season=>
+#          {:win_percentage=>0.25,
+#           :total_goals_scored=>85,
+#           :total_goals_against=>103,
+#           :average_goals_scored=>1.77,
+#           :average_goals_against=>2.15}},
+#       "20142015"=>
+#        {:postseason=>
+#          {:win_percentage=>0.67,
+#           :total_goals_scored=>17,
+#           :total_goals_against=>13,
+#           :average_goals_scored=>2.83,
+#           :average_goals_against=>2.17},
+#         :regular_season=>
+#          {:win_percentage=>0.5,
+#           :total_goals_scored=>186,
+#           :total_goals_against=>162,
+#           :average_goals_scored=>2.27,
+#           :average_goals_against=>1.98}},
+#       "20152016"=>
+#        {:postseason=>
+#          {:win_percentage=>0.36,
+#           :total_goals_scored=>25,
+#           :total_goals_against=>33,
+#           :average_goals_scored=>1.79,
+#           :average_goals_against=>2.36},
+#         :regular_season=>
+#          {:win_percentage=>0.45,
+#           :total_goals_scored=>178,
+#           :total_goals_against=>159,
+#           :average_goals_scored=>2.17,
+#           :average_goals_against=>1.94}}
+#     }
+#     assert_equal expected, @stat_tracker.seasonal_summary("18")
+#   end
 
 end
