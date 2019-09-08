b9769003d3bfb5fc8df87a7e36ca16c66b666cad
John Travers <johnk.travers@gmail.com>
2019-09-08 12:58:00 -0600

    Add opponent_stats helper method
    Co-authored-by: Ryan Hantak <47759923+rhantak@users.noreply.github.com>
    Co-authored-by: David Barriga-Gomez <50917634+DavidBarriga-Gomez@users.noreply.github.com>
    Co-authored-by: Carleigh Crockett <47065120+Capleugh@users.noreply.github.com>

--- lib/helper_methods.rb
+++ lib/helper_methods.rb
@@ -28,4 +28,31 @@ module HelperMethods
     team_result_count
   end
 
+  def opponent_stats(team_id)
+    team_id = team_id.to_i
+    opponent_count = Hash.new { |h,k| h[k] = Hash.new(0) }
+
+    @game_teams.each do |game_id, game_teams|
+      game_teams.each do |game_team|
+        if game_team.team_id == team_id
+
+          if game_team.hoa == "away"
+            opponent_id = @games[game_id].home_team_id
+            opponent_count[opponent_id][:games] += 1
+            opponent_count[opponent_id][:wins] += 1 if game_team.result == "LOSS"
+            opponent_count[opponent_id][:losses] += 1 if game_team.result == "WIN"
+
+          elsif game_team.hoa == "home"
+            opponent_id = @games[game_id].away_team_id
+            opponent_count[opponent_id][:games] += 1
+            opponent_count[opponent_id][:wins] += 1 if game_team.result == "LOSS"
+            opponent_count[opponent_id][:losses] += 1 if game_team.result == "WIN"
+          end
+        end
+      end
+    end
+
+    opponent_count
+  end
+
 end
