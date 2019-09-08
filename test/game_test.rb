dc861037ad9813718c5bf7cd0b0f205e140c2469
Brian Bower <brian@brianbower.com>
2019-09-07 22:53:20 -0600

    Update class tests with initialization tests - they are skipped for now

--- test/game_test.rb
+++ test/game_test.rb
@@ -8,12 +8,25 @@ require './lib/game'
 class GameTest < Minitest::Test
 
   def setup
-    @game = Game.new
+    @sample_data = '2012030221,20122013,Postseason,5/16/13,3,6,2,3,Toyota Stadium,/api/v1/venues/null'
+    @game = Game.new(@sample_data)
   end
 
   def test_it_exists
-    skip
     assert_instance_of Game, @game
+  end
+
+  def test_initialization
+    skip
+    assert_equal 2012030221, @game.game_id
+    assert_equal '20122013', @game.season
+    assert_equal 'Postseason', @game.type
+    assert_equal "5/16/13", @game.date_time
+    assert_equal 3, @game.away_team_id
+    assert_equal 6, @game.home_team_id
+    assert_equal 2, @game.away_goals
+    assert_equal 3, @game.home_goals
+    assert_equal 'Toyota Stadium', @game.venue
   end
 
 end
