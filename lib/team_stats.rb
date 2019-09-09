918ef5f2b0d6b7ba90c400bd9bc3ab10f13cde7a
Ryan Hantak <rhantak@gmail.com>
2019-09-09 15:01:59 -0600

    Refactor best/worst season methods
    Co-authored-by: John Travers <46035439+johnktravers@users.noreply.github.com>
    Co-authored-by: Carleigh Crockett <47065120+capleugh@users.noreply.github.com>
    Co-authored-by: David Barriga-Gomez <50917634+DavidBarriga-Gomez@users.noreply.github.com>

--- lib/team_stats.rb
+++ lib/team_stats.rb
@@ -12,60 +12,32 @@ module TeamStats
 
   def best_season(team_id)
     team_games = @games.find_all {|game_id, game| game.home_team_id == team_id || game.away_team_id == team_id}
-
     season = Hash.new { |h,k| h[k] = Hash.new(0) }
 
     team_games.each do |game_id, game|
       season[game.season][:season_games] += 1
-      if game.home_team_id == team_id && game.home_goals > game.away_goals
+      if home_win?(team_id, game)
         season[game.season][:season_wins] += 1
-      elsif game.away_team_id == team_id && game.away_goals > game.home_goals
+      elsif away_win?(team_id, game)
         season[game.season][:season_wins] += 1
       end
     end
-
-    best_season = ""
-    best_season_percent = 0.0
-    season.keys.each do |season_id|
-      season_percent = (season[season_id][:season_wins] / season[season_id][:season_games].to_f) * 100
-      if season_percent > best_season_percent
-        best_season = season_id
-        best_season_percent = season_percent
-      end
-    end
-    best_season
+    season.max_by {|season_id, tallies| tallies[:season_wins] / tallies[:season_games].to_f}[0]
   end
 
   def worst_season(team_id)
     team_games = @games.find_all {|game_id, game| game.home_team_id == team_id || game.away_team_id == team_id}
-    seasons = []
-    team_games.each do |game_id, game|
-      seasons << game.season
-    end
-    seasons = seasons.uniq
-    season = Hash.new
-    seasons.each do |season_id|
-      season[season_id] = {season_wins: 0, season_games: 0}
-    end
+    season = Hash.new { |h,k| h[k] = Hash.new(0) }
+
     team_games.each do |game_id, game|
       season[game.season][:season_games] += 1
-      if game.home_team_id == team_id && game.home_goals > game.away_goals
+      if home_win?(team_id, game)
         season[game.season][:season_wins] += 1
-      elsif game.away_team_id == team_id && game.away_goals > game.home_goals
+      elsif away_win?(team_id, game)
         season[game.season][:season_wins] += 1
       end
     end
-
-    worst_season = ""
-    worst_season_percent = 101
-    seasons.each do |season_id|
-      season_percent = (season[season_id][:season_wins] / season[season_id][:season_games].to_f) * 100
-      if season_percent < worst_season_percent
-        worst_season = season_id
-        worst_season_percent = season_percent
-      end
-    end
-    worst_season
+    season.min_by {|season_id, tallies| tallies[:season_wins] / tallies[:season_games].to_f}[0]
   end
 
   def average_win_percentage(team_id)
@@ -158,5 +130,4 @@ module TeamStats
 
     season_summary
   end
-
 end
