19828c48cc5db53912feae0cbc1d2fde321c80d8
Tyla Devon <tyladevon@gmail.com>
2019-09-09 12:03:53 -0600

    Add test for favorite_opponent
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>
    Co-Authored-By: Laura Schulz <lrs8810@users.noreply.github.com>

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -150,4 +150,8 @@ class StatTrackerTest < Minitest::Test
   def test_fewest_goals_scored
     assert_equal 0, @stat_tracker.fewest_goals_scored("18")
   end
+
+  def test_favorite_opponent
+    assert_equal "DC United", @stat_tracker.favorite_opponent("18")
+  end
 end
