7aef7c91ca16538b556a925ce5e8ac6adb402f4f
Laura Schulz <lrs8810@gmail.com>
2019-09-10 17:41:37 -0600

    Add least accurate team, most accurate team, winningest coach, worst coach methods to SeasonStatistics module
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>

--- lib/season_statistics.rb
+++ lib/season_statistics.rb
@@ -49,4 +49,85 @@ module SeasonStatistics
     end
     @teams[fewest_tackles_team_id].team_name
   end
+
+  def least_accurate_team(season_id)
+    filtered_games = @game_teams.find_all do |game|
+      @games[game.game_id].season == season_id
+    end
+
+    games_by_teams = filtered_games.group_by(&:team_id)
+
+    lowest_ratio_team_id = nil
+    lowest_ratio = 10
+    games_by_teams.each do |team_id, games_arr|
+      shot_ratio = games_arr.sum(&:goals).to_f / games_arr.sum(&:shots)
+      # require 'pry'; binding.pry
+      if shot_ratio < lowest_ratio
+        lowest_ratio_team_id = team_id
+        lowest_ratio = shot_ratio
+      end
+    end
+    @teams[lowest_ratio_team_id].team_name
+  end
+
+  def most_accurate_team(season_id)
+    filtered_games = @game_teams.find_all do |game|
+      @games[game.game_id].season == season_id
+    end
+
+    games_by_teams = filtered_games.group_by(&:team_id)
+
+    highest_ratio_team_id = nil
+    highest_ratio = 10
+    games_by_teams.each do |team_id, games_arr|
+      shot_ratio = games_arr.sum(&:shots).to_f / games_arr.sum(&:goals)
+      if shot_ratio < highest_ratio
+        highest_ratio_team_id = team_id
+        highest_ratio = shot_ratio
+      end
+    end
+    @teams[highest_ratio_team_id].team_name
+  end
+
+  def winningest_coach(season_id)
+    filtered_games = @game_teams.find_all do |game|
+      @games[game.game_id].season == season_id
+    end
+
+    games_by_coaches = filtered_games.group_by(&:head_coach)
+
+    most_wins_coach_name = nil
+    highest_win_percentage = 0
+    games_by_coaches.each do |coach_name, games_arr|
+      win_percentage = (games_arr.find_all do |game|
+        game.result == "WIN"
+      end.length).to_f / games_arr.length
+      if win_percentage > highest_win_percentage
+        most_wins_coach_name = coach_name
+        highest_win_percentage = win_percentage
+      end
+    end
+    most_wins_coach_name
+  end
+
+  def worst_coach(season_id)
+    filtered_games = @game_teams.find_all do |game|
+      @games[game.game_id].season == season_id
+    end
+
+    games_by_coaches = filtered_games.group_by(&:head_coach)
+
+    fewest_wins_coach_name = nil
+    lowest_win_percentage = 100
+    games_by_coaches.each do |coach_name, games_arr|
+      win_percentage = (games_arr.find_all do |game|
+        game.result == "WIN"
+      end.length).to_f / games_arr.length
+      if win_percentage < lowest_win_percentage
+        fewest_wins_coach_name = coach_name
+        lowest_win_percentage = win_percentage
+      end
+    end
+    fewest_wins_coach_name
+  end
 end
