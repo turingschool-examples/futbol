5ca8cb49c8d406f9a504f416e08ed414192c1c6f
Brian Bower <brian@brianbower.com>
2019-09-10 14:33:54 -0600

    Add passing best_fans method

--- lib/teamable.rb
+++ lib/teamable.rb
@@ -99,26 +99,56 @@ module Teamable
 
   #Name of the opponent that has the lowest win percentage against the given team.	String
   #BB
+  #crafty - min_by_value  ???
   def favorite_opponent(team_id)
 
-    opponents_percent_hash = Hash.new(0.00)
+    # creates a hash full of opponent ids and sets the values to 0
+    opponents_number_of_wins = Hash.new(0.00)
     games_for_team_helper(team_id).each do |game|
-      opponents_percent_hash.store(game.away_team_id, 0.00) if game.away_team_id != team_id
-      opponents_percent_hash.store(game.home_team_id, 0.00) if game.home_team_id != team_id
+      opponents_number_of_wins.store(game.away_team_id, 0.00) if game.away_team_id != team_id
+      opponents_number_of_wins.store(game.home_team_id, 0.00) if game.home_team_id != team_id
     end
 
-    # opponents hash and win percents
-    # min by over that hashes values
-
-    opponents_percent_hash.each_key do |key|
-      opponents_total_wins = total_wins_helper(team_id, key)
+    # set the value to the number of wins over
+    opponents_number_of_wins.each do |key, value|
+       opponents_number_of_wins[key] = total_wins_array_helper(team_id, key)
     end
-    opponents_total_wins
 
-      # total_wins_array_helper(team_id, opponent) #it is the length
-      # total_games_array_helper # will get me the
+    # creates a hash full of opponent ids and sets the values to 0
+    opponents_number_of_games = Hash.new(0.00)
+    games_for_team_helper(team_id).each do |game|
+      opponents_number_of_games.store(game.away_team_id, 0.00) if game.away_team_id != team_id
+      opponents_number_of_games.store(game.home_team_id, 0.00) if game.home_team_id != team_id
+    end
 
-      #crafty - min_by_value  ???
+    # set the value to the number of games played
+    opponents_number_of_games.each do |key, value|
+       opponents_number_of_games[key] = total_games_array_helper(team_id, key)
+    end
+
+    # make a new hash to check the percent of wins against the passed in team
+    opponents_percentage_of_wins = Hash.new(0.00)
+    games_for_team_helper(team_id).each do |game|
+      opponents_percentage_of_wins.store(game.away_team_id, 0.00) if game.away_team_id != team_id
+      opponents_percentage_of_wins.store(game.home_team_id, 0.00) if game.home_team_id != team_id
+    end
+
+    # Check for matching team ids and return the percent of games won for that team
+    opponents_percentage_of_wins.each do |key_3, value_3|
+      opponents_number_of_wins.each do |key_1, value_1|
+        opponents_number_of_games.each do |key_2, value_2|
+          if key_1 == key_2 && key_1 == key_3
+            opponents_percentage_of_wins[key_3] = (value_1 / value_2)
+          end
+        end
+      end
+    end
+
+    # find the max percent id from the opponents_percentage_of_wins hash
+    favorite_opponent_id = opponents_percentage_of_wins.max_by{|k,v| v}
+    favorite_opponent = favorite_opponent_id[0]
+
+    team_name_finder_helper(favorite_opponent)
 
   end
 
