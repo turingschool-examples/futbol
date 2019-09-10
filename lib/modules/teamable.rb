790198c63baac778d8791642dd624d5a3974dd90
Daniel <danmoran.pro@gmail.com>
2019-09-10 13:59:59 -0600

    Added tests to teamable_test and implemented methods
    Co-authored-by: Michael Cooper <8922930+CoopTang@users.noreply.github.com>
    Co-authored-by: Rachel Lew <48839191+rlew421@users.noreply.github.com>

--- lib/modules/teamable.rb
+++ lib/modules/teamable.rb
@@ -1,17 +1,29 @@
 module Teamable
 
   def team_info(team_id)
-
+   {
+      "team_id" => teams[team_id].id,
+      "team_name" => teams[team_id].name,
+      "franchise_id" => teams[team_id].franchise_id,
+      "abbreviation" => teams[team_id].abbreviation,
+      "link" => teams[team_id].link
+    }
   end
 
   def best_season(team_id)
-
+    seasons
+      .values
+      .max_by {|season| season.teams[team_id].total_wins / season.teams[team_id].games.length.to_f}
+      .id
   end
 
   def worst_season(team_id)
-
+    seasons
+      .values
+      .min_by {|season| season.teams[team_id].total_wins / season.teams[team_id].games.length.to_f}
+      .id
   end
-
+  
   def average_win_percentage(team_id)
 
   end
--- test/module_test/teamable_test.rb
+++ test/module_test/teamable_test.rb
@@ -2,6 +2,7 @@ require "minitest/autorun"
 require "minitest/pride"
 require "./lib/modules/teamable"
 require "./lib/stat_tracker"
+require "pry"
 
 
 class TeamableModuleTest < Minitest::Test
@@ -21,52 +22,104 @@ class TeamableModuleTest < Minitest::Test
   end
 
   def test_team_info
-    assert_equal hash, @stat_tracker.team_info(team_id)
+    expected_hash = {
+      "team_id" => "3" ,
+      "team_name" => "Houston Dynamo",
+      "franchise_id" => "10",
+      "abbreviation" => "HOU",
+      "link" => "/api/v1/teams/3"
+    }
+    assert_equal expected_hash, @stat_tracker.team_info("3")
   end
 
   def test_best_season
-    assert_equal int, @stat_tracker.best_season(team_id)
+    assert_equal "20132014", @stat_tracker.best_season("3")
   end
 
   def test_worst_season
-    assert_equal int, @stat_tracker.worst_season(team_id)
+    assert_equal "20142015", @stat_tracker.worst_season("3")
   end
 
   def test_average_win_percentage
-    assert_equal float, @stat_tracker.average_win_percentage(team_id)
+    skip
+    assert_equal 0.57, @stat_tracker.average_win_percentage("3")
   end
 
   def test_most_goals_scored
-    assert_equal int, @stat_tracker.most_goals_scored(team_id)
+    skip
+    assert_equal 3, @stat_tracker.most_goals_scored("3")
   end
 
   def test_fewest_goals_scored
-    assert_equal int, @stat_tracker.fewest_goals_scored(team_id)
+    skip
+    assert_equal 0, @stat_tracker.fewest_goals_scored("3")
   end
 
   def test_favorite_opponent
-    assert_equal "string", @stat_tracker.favorite_opponent(team_id)
+    skip
+    assert_equal "Chicago Fire", @stat_tracker.favorite_opponent("3")
   end
 
   def test_rival
-    assert_equal "string", @stat_tracker.rival(team_id)
+    skip
+    assert_equal "Sporting Kansas City", @stat_tracker.rival("3")
   end
 
   def test_biggest_team_blowout
-    assert_equal int, @stat_tracker.biggest_blowout(team_id)
+    skip
+    assert_equal 2, @stat_tracker.biggest_blowout("3")
   end
 
   def test_worst_loss
-    assert_equal int, @stat_tracker.worst_loss(team_id)
+    skip
+    assert_equal 2, @stat_tracker.worst_loss("3")
   end
 
   def test_head_to_head
-
-    assert_equal expected_hash, @stat_tracker.head_to_head(team_id)
+    skip
+    expected_hash = {
+      "Chicago Fire" => 1.0,
+      "Sporting Kansas City" => 0.6
+    }
+    assert_equal expected_hash, @stat_tracker.head_to_head("3")
   end
 
   def test_seasonal_summary
-
-    assert_equal expected_hash, @stat_tracker.seasonal_summary(team_id)
+    skip
+    expected_hash = {
+     "20132014" => {
+       regular_season: {
+         win_percentage: nil,
+         total_goals_scored: nil,
+         total_goals_against: nil,
+         average_goals_scored: nil,
+         average_goals_against: nil
+        },
+       postseason:{
+         win_percentage: nil,
+         total_goals_scored: nil,
+         total_goals_against: nil,
+         average_goals_scored: nil,
+         average_goals_against: nil
+       }
+     },
+    "20142015" => {
+       regular_season: {
+         win_percentage: nil,
+         total_goals_scored: nil,
+         total_goals_against: nil,
+         average_goals_scored: nil,
+         average_goals_against: nil
+        },
+       postseason:{
+         win_percentage:nil ,
+         total_goals_scored: nil,
+         total_goals_against: nil,
+         average_goals_scored: nil,
+         average_goals_against: nil
+       }
+      }
+     }
+    assert_equal expected_hash, @stat_tracker.seasonal_summary("3")
   end
 end
