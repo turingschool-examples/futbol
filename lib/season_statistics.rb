b6ca02ecaa26e2fcbc894753aeb6c0e60fe2f355
Laura Schulz <lrs8810@gmail.com>
2019-09-10 16:55:13 -0600

    Add method for fewest tackles to SeasonStatistics module
    Co-Authored-By: Hillary <hillstew@users.noreply.github.com>
    Co-Authored-By: Graham Thompson <grwthomps@gmail.com>

--- lib/season_statistics.rb
+++ lib/season_statistics.rb
@@ -30,4 +30,23 @@ module SeasonStatistics
     end
     @teams[most_tackles_team_id].team_name
   end
+
+  def fewest_tackles(season_id)
+    filtered_games = @game_teams.find_all do |game|
+      @games[game.game_id].season == season_id
+    end
+
+    games_by_teams = filtered_games.group_by(&:team_id)
+
+    fewest_tackles_team_id = nil
+    fewest_tackles = 100_000
+    games_by_teams.each do |team_id, games_arr|
+      tackles = games_arr.sum {|game| game.tackles}
+      if tackles < fewest_tackles
+        fewest_tackles_team_id = team_id
+        fewest_tackles = tackles
+      end
+    end
+    @teams[fewest_tackles_team_id].team_name
+  end
 end
