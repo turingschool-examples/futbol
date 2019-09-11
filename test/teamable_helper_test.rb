a7defa904bdd1d31bdc81104a663c99f373ce8af
Jon Patterson <JonPatt92@gmail.com>
2019-09-11 15:33:23 -0600

    all but two teamable_helper methods tested

--- test/teamable_helper_test.rb
+++ test/teamable_helper_test.rb
@@ -25,23 +25,27 @@ class TeamableHelperTest < Minitest::Test
   end
 
   def test_season_win_loss_helper
-  assert_equal , @stat_tracker.season_win_loss_helper
+    expected_hash = {"20122013"=>{:wins=>17, :games=>48}, "20132014"=>{:wins=>15, :games=>82}, "20142015"=>{:wins=>15, :games=>82}, "20152016"=>{:wins=>32, :games=>82}, "20162017"=>{:wins=>30, :games=>82}, "20172018"=>{:wins=>20, :games=>82}}
+
+  assert_equal expected_hash, @stat_tracker.season_win_loss_helper("7")
   end
 
   def test_season_win_percentage_helper
-  assert_equal , @stat_tracker.season_win_percentage_helper
+    expected_hash = {"20122013"=>0.35, "20132014"=>0.18, "20142015"=>0.18, "20152016"=>0.39, "20162017"=>0.37, "20172018"=>0.24}
+
+  assert_equal expected_hash, @stat_tracker.season_win_percentage_helper("7")
   end
 
-  #get games for a team_id
-  def test_games_for_team_helper
-  assert_equal , @stat_tracker.games_for_team_helper
-  end
+  # needs fixture file
+  # def test_games_for_team_helper
+  # assert_equal , @stat_tracker.games_for_team_helper
+  # end
 
   def test_total_wins_array_helper
-  assert_equal , @stat_tracker.total_wins_array_helper
+  assert_equal 6.0, @stat_tracker.total_wins_array_helper("7", "14")
   end
 
-  def test_total_games_array_helper
-  assert_equal , @stat_tracker.total_games_array_helper
-  end
+  # def test_total_games_array_helper
+  # assert_equal , @stat_tracker.total_games_array_helper
+  # end
 end
