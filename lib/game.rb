b2375f07b34628c20b019b783c39568d229f03a4
Becky Robran <rerobran@gmail.com>
2019-10-22 16:45:52 -0600

    Adds game CSV and initialize

---
+++ lib/game.rb
@@ -0,0 +1,31 @@
+class Game
+  attr_reader :game_id, :team_id, :hoa, :result, :settled_in, :head_coach, :goals,
+      :shots, :tackles, :pim, :powerPlayOpportunities, :powerPlayGoals,
+      :faceOffWinPercentage, :giveaways, :takeaways
+
+  def initialize(game_info)
+    @game_id = game_info[:game_id].to_i
+    @team_id = game_info[:team_id]
+    @hoa = game_info[:description]
+    @result = game_info[:unit_price]
+    @settled_in = game_info[:settled_in]
+    @head_coach = game_info[:head_coach]
+    @goals = game_info[:goals]
+    @shots = game_info[:shots]
+    @tackles = game_info[:tackles]
+    @pim = game_info[:pim]
+    @powerPlayOpportunities = game_info[:powerPlayOpportunities]
+    @powerPlayGoals = game_info[:powerPlayGoals]
+    @faceOffWinPercentage = game_info[:faceOffWinPercentage]
+    @giveaways = game_info[:giveaways]
+    @takeaways = game_info[:takeaways]
+  end
+
+  def self.all
+    csv = CSV.read "./data/games.csv", headers: true, header_converters: symbols
+
+    csv.map do |row|
+      Item.new(row)
+    end
+  end
+end
