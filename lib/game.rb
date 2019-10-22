353092d58691a296b92925c6631f37c8ccc28cee
Becky Robran <rerobran@gmail.com>
2019-10-22 16:42:35 -0600

    Add thought process for setting up methods

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,2 @@
 require "csv"
 require 'lib/stat_tracker'
-
-contents = CSV.open './data/games.csv', headers: true, header_converters: :symbol
-contents.foreach do |row|
-  game_id = row[:game_id]
-  team_id = row[:team_id]
-  puts test
-  # "#{game_id} #{team_id}"
-end
--- lib/playground.rb
+++ lib/playground.rb
@@ -4,16 +4,9 @@ require './lib/stat_tracker'
 
 games = []
 contents = CSV.foreach './data/games.csv', headers: true, header_converters: :symbol do |row|
-  hash = {}
-  # game_id = row[:game_id].to_i
-  # team_id = row[:team_id].to_i
-  away_goals = row[:away_goals].to_i
-  home_goals = row[:home_goals].to_i
-  hash[:away_goals] = away_goals
-  hash[:home_goals] = home_goals
-  hash[:sum_goals] = away_goals + home_goals
-  hash[:sesson]
-games << hash
+
+contents.map.do |row|
+  Game.new(stats)
 
 end
-puts games
+puts games.first
