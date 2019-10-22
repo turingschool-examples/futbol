f32b295142489f5862041fbc46ba83f7dcc5768b
Paul Debevec <paulmdebevec@gmail.com>
2019-10-22 16:42:57 -0600

    Created Game class and initialize

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,31 @@
-require "csv"
-require 'lib/stat_tracker'
+class Game
+  attr_reader :game_id, :team_id, :hoa, :result, :settled_in, :head_coach, :goals,
+      :shots, :tackles, :pim, :powerPlayOpportunities, :powerPlayGoals,
+      :faceOffWinPercentage, :giveaways, :takeaways
 
-contents = CSV.open './data/games.csv', headers: true, header_converters: :symbol
-contents.foreach do |row|
-  game_id = row[:game_id]
-  team_id = row[:team_id]
-  puts test
-  # "#{game_id} #{team_id}"
+  def initialize(game_info)
+    @game_id = game_info[:game_id].to_i
+    @team_id = game_info[:team_id]
+    @hoa = game_info[:description]
+    @result = game_info[:unit_price]
+    @settled_in = game_info[:settled_in]
+    @head_coach = game_info[:head_coach]
+    @goals = game_info[:goals]
+    @shots = game_info[:shots]
+    @tackles = game_info[:tackles]
+    @pim = game_info[:pim]
+    @powerPlayOpportunities = game_info[:powerPlayOpportunities]
+    @powerPlayGoals = game_info[:powerPlayGoals]
+    @faceOffWinPercentage = game_info[:faceOffWinPercentage]
+    @giveaways = game_info[:giveaways]
+    @takeaways = game_info[:takeaways]
+  end
+
+  def self.all
+    csv = CSV.read "./data/games.csv", headers: true, header_converters: symbols
+
+    csv.map do |row|
+      Item.new(row)
+    end
+  end
 end
