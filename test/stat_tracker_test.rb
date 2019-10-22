dbc7c09909e30ac878a66a31b74e0b9ca00c60bd
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-10-21 20:39:49 -0600

    Add tests for initialization
    Co-Authored-By: Sebastian Sloan <sasloan@users.noreply.github.com>
    Co-Authored-By: Ray <itemniner@users.noreply.github.com>
    Co-Authored-By: Phil DeLong <philjdelong@users.noreply.github.com>

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -3,15 +3,21 @@ require_relative '../lib/stat_tracker'
 
 class StatTrackerTest < Minitest::Test
   def setup
-    locations = {
-                  games: '../data/games.csv',
-                  teams: '../data/teams.csv',
-                  game_teams: '../data/game_teams.csv'
+    @locations = {
+                  games: '../data/dummy_games.csv',
+                  teams: '../data/dummy_teams.csv',
+                  game_teams: '../data/dummy_game_teams.csv'
                 }
-    @stat_tracker = StatTracker.from_csv(locations)
+    @stat_tracker = StatTracker.from_csv(@locations)
   end
 
   def test_it_exists
     assert_instance_of StatTracker, @stat_tracker
+  end
+
+  def test_it_initializes_with_attributes
+    assert_equal @locations[:games], @stat_tracker.games_path
+    assert_equal @locations[:teams], @stat_tracker.teams_path
+    assert_equal @locations[:game_teams], @stat_tracker.game_teams_path
   end
 end
