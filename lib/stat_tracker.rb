13bc2192506f527f7a42b87690ffa6b90ea1a786
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-10-21 20:40:32 -0600

    Change stat_tracker to store CSV data in attributes
    Co-Authored-By: Sebastian Sloan <sasloan@users.noreply.github.com>
    Co-Authored-By: Ray <itemniner@users.noreply.github.com>
    Co-Authored-By: Phil DeLong <philjdelong@users.noreply.github.com>

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,8 +1,13 @@
 class StatTracker
-  def initialize
+  attr_reader :games, :teams, :game_teams
+
+  def initialize(locations)
+    @games = CSV.read(locations[:games])
+    @teams = CSV.read(locations[:teams])
+    @game_teams = CSV.read(locations[:game_teams])
   end
 
   def self.from_csv(locations)
-    self.new
+    self.new(locations)
   end
 end
