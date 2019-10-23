f0a34cdcf995ba67666d3acb49cb1c4b75c5a310
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-10-23 15:15:51 -0600

    Refactor to make game_repo an attribute

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,6 +1,8 @@
 require_relative './game_collection'
 
 class StatTracker
+  attr_reader :game_repo
+
   def self.from_csv(file_paths)
     game_path = file_paths[:games]
     teams_path = file_paths[:teams]
@@ -10,27 +12,19 @@ class StatTracker
   end
 
   def initialize(game_path, teams_path, game_teams_path)
-    @game_path = game_path
     @teams_path = teams_path
     @game_teams_path = game_teams_path
-  end
-
-  def game_repo
-    GameCollection.new(@game_path)
+    @game_repo = GameCollection.new(game_path)
   end
 
   def count_of_games_by_season
     games_per_season = Hash.new{0}
-    game_repo.games.each do |game|
-      games_per_season[game.season] += 1
-    end
+    @game_repo.games.each { |game| games_per_season[game.season] += 1 }
     games_per_season.sort.to_h
   end
 
   def average_goals_per_game
-    total_goals = game_repo.games.sum do |game|
-      game.total_score
-    end
-    (total_goals.to_f/game_repo.total_games).round(2)
+    total_goals = @game_repo.games.sum { |game| game.total_score }
+    (total_goals.to_f/@game_repo.total_games).round(2)
   end
 end
