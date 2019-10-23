4054670339ee0996119f51887304dddc2d73a4aa
Becky Robran <rerobran@gmail.com>
2019-10-23 14:52:53 -0600

    Fixes initialize path file
    Co-Authored-By: WrenS <wrenisit@users.noreply.github.com>
    Co-Authored-By: Travis Borgsmiller <TravisBorgsmiller@users.noreply.github.com>
    Co-Authored-By: Paul Debevec <PaulDebevec@users.noreply.github.com>

--- lib/game_stat_collection.rb
+++ lib/game_stat_collection.rb
@@ -1,16 +1,21 @@
 require 'csv'
 require_relative 'game'
+require_relative 'stat_tracker'
 
 class GameStatCollection
+  attr_reader :game_instances
 
   def initialize(csv_file_path)
     @csv_file_path = csv_file_path
+    @game_instances = all_stats
   end
 
   def all_stats
-    csv = CSV.read '#{@csv_file_path}', headers: true, header_converters: :symbols
-    csv.map do |row|
-      Game.new(row)
+    game_objects = []
+    csv = CSV.read("#{@csv_file_path}", headers: true, header_converters: :symbol)
+      csv.map do |row|
+      game_objects <<  Game.new(row)
     end
+    game_objects
   end
 end
