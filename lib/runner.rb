fd6657a817dd6b0586801b7a2f624b746e0135b6
Becky Robran <rerobran@gmail.com>
2019-10-23 14:53:36 -0600

    Instatiates game instances into array
    Co-Authored-By: WrenS <wrenisit@users.noreply.github.com>
    Co-Authored-By: Travis Borgsmiller <TravisBorgsmiller@users.noreply.github.com>
    Co-Authored-By: Paul Debevec <PaulDebevec@users.noreply.github.com>

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,4 +1,6 @@
+require 'csv'
 require './lib/stat_tracker'
+
 
 game_path = './data/games.csv'
 team_path = './data/teams.csv'
@@ -11,4 +13,6 @@ locations = {
 }
 
 stat_tracker = StatTracker.from_csv(locations)
-puts stat_tracker.game
+ game_array = stat_tracker.game
+require "pry"; binding.pry
+game_array.game_instances.first
