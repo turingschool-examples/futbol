891c5ddc09bc28585183a5e8caa31225f503f408
Nathan Keller <keller.nathan@gmail.com>
2019-10-23 08:34:46 -0600

    Adjust syntax game_teams

--- lib/game_teams.rb
+++ lib/game_teams.rb
@@ -5,21 +5,21 @@ class GameTeams
   :goals, :shots, :tackles, :pim, :powerPlayOpportunities, :powerPlayGoals,
   :faceOffWinPercentage, :giveaways, :takeaways
 
-  def initialize(game_team_info)
-    @game_id = game_team_info[:game_id].to_i
-    @team_id = game_team_info[:team_id].to_i
-    @HoA = game_team_info[:HoA]
-    @result = game_team_info[:result]
-    @settled_in = game_team_info[:settled_in]
-    @head_coach = game_team_info[:head_coach]
-    @goals = game_team_info[:goals].to_i
-    @shots = game_team_info[:shots].to_i
-    @tackles = game_team_info[:tackles].to_i
-    @pim = game_team_info[:pim].to_i
-    @powerPlayOpportunities = game_team_info[:powerPlayOpportunities].to_i
-    @powerPlayGoals = game_team_info[:powerPlayGoals].to_i
-    @faceOffWinPercentage = game_team_info[:faceOffWinPercentage].to_f
-    @giveaways = game_team_info[:giveaways].to_i
-    @takeaways = game_team_info[:takeaways].to_i
+  def initialize(game_teams_info)
+    @game_id = game_teams_info[:game_id].to_i
+    @team_id = game_teams_info[:team_id].to_i
+    @HoA = game_teams_info[:HoA]
+    @result = game_teams_info[:result]
+    @settled_in = game_teams_info[:settled_in]
+    @head_coach = game_teams_info[:head_coach]
+    @goals = game_teams_info[:goals].to_i
+    @shots = game_teams_info[:shots].to_i
+    @tackles = game_teams_info[:tackles].to_i
+    @pim = game_teams_info[:pim].to_i
+    @powerPlayOpportunities = game_teams_info[:powerPlayOpportunities].to_i
+    @powerPlayGoals = game_teams_info[:powerPlayGoals].to_i
+    @faceOffWinPercentage = game_teams_info[:faceOffWinPercentage].to_f
+    @giveaways = game_teams_info[:giveaways].to_i
+    @takeaways = game_teams_info[:takeaways].to_i
   end
 end
