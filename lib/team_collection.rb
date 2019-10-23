90d4acfb05154ace71514141397a6453cfb8200c
Becky Robran <rerobran@gmail.com>
2019-10-23 15:23:48 -0600

    "Adds team_collection file with helper method"

---
+++ lib/team_collection.rb
@@ -0,0 +1,21 @@
+require 'csv'
+require_relative 'team'
+require_relative 'stat_tracker'
+
+class TeamCollection
+  attr_reader :team_instances
+
+  def initialize(team_path)
+    @team_path = team_path
+    @team_instances = all_teams
+  end
+
+  def all_teams
+    team_objects = []
+    csv = CSV.read("#{@team_path}", headers: true, header_converters: :symbol)
+      csv.map do |row|
+      game_objects <<  Team.new(row)
+    end
+    team_objects
+  end
+end
