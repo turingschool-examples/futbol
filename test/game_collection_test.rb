0215e81760a21bfaa656acefecc10c7fc9e8b586
Ryan Allen <rallenwasd@gmail.com>
2019-10-23 17:48:49 -0600

    Add test for method average_goals_by_season

--- test/game_collection_test.rb
+++ test/game_collection_test.rb
@@ -34,8 +34,18 @@ class GameCollectionTest < Minitest::Test
     assert_equal count_games_by_season_list, @game_collection.count_of_games_by_season
   end
 
-  def test_it_can_average_goals_per_game
-  # 15 away goals, 14 home goals, (29 total goals / 7 games)
+  def test_it_can_calculate_average_goals_per_game
+    # 15 away goals, 14 home goals, (29 total goals / 7 games)
     assert_equal (29/7.0).round(2), @game_collection.average_goals_per_game
+  end
+
+  def test_it_can_calculate_average_goals_by_season
+    # 2012/2013 17 goals / 4 games
+    # 2014/2015 12 goals / 3 games
+    count_goals_by_season_list = {
+      20122013 => (17/4.0).round(2),
+      20142015 => (13/3.0).round(2)
+    }
+    assert_equal count_goals_by_season_list, @game_collection.average_goals_by_season
   end
 end
