e2a0c4a6bf10762721792d52285ec22731b4fc11
Becky Robran <rerobran@gmail.com>
2019-10-22 20:48:17 -0600

    Adds initialzie and file paths

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,6 +1,21 @@
+require './lib/game_stat_collection'
+
 class StatTracker
 
     def self.from_csv(locations)
-      StatTracker.new
+      game_path = locations[:games]
+      team_path = locations[:teams]
+      game_teams_path = locations[:game]
+      StatTracker.new(game_path, team_path, game_teams_path)
+    end
+
+    def initialize(game_path, team_path, game_teams_path)
+      @game_path = game_path
+      @team_path = team_path
+      @game_teams_path = game_teams_path
+    end
+
+    def game
+      GameStatCollection.new(@game_path)
     end
 end
