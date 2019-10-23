48483cf58e5eb94c494b3cc812eff42c37e39a41
Alison Vermeil <alison.minton@gmail.com>
2019-10-23 14:06:23 -0600

    Add attribute test and methods to game class
    Co-Authored-By: LainMcGrath <Lain.McGrath@gusto.com>

--- test/game_test.rb
+++ test/game_test.rb
@@ -25,5 +25,18 @@ class GameTest < Minitest::Test
   def test_it_exists
     assert_instance_of Game, @game
   end
+
+  def test_it_has_attributes
+    assert_equal 2012030221, @game.game_id
+    assert_equal 20122013, @game.season
+    assert_equal "Postseason", @game.type
+    assert_equal "5/16/13", @game.date_time
+    assert_equal 3, @game.away_team_id
+    assert_equal 6, @game.home_team_id
+    assert_equal 2, @game.away_goals
+    assert_equal 3, @game.home_goals
+    assert_equal "Toyota Stadium", @game.venue
+    assert_equal "/api/v1/venues/null", @game.venue_link
+  end
   #add test for attributes
 end
