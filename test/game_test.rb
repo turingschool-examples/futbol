9ac4b1f2c413ba396f23509a05dfb6c283a15100
Alison Vermeil <alison.minton@gmail.com>
2019-10-23 14:59:24 -0600

    Refactor game test file to test with data from csv and update game class attributes to convert numbers in strings to integers
    Co-Authored-By: Ryan Allen <rallenwasd@gmail.com>
    Co-Authored-By: LainMcGrath <Lain.McGrath@gusto.com>

--- test/game_test.rb
+++ test/game_test.rb
@@ -1,42 +1,32 @@
 require 'csv'
-require 'minitest/autorun'
-require 'minitest/pride'
+require './test/test_helper'
 require './lib/game'
 
 class GameTest < Minitest::Test
 
   def setup
-    @game = Game.new({
-      :game_id => 2012030221,
-      :season => 20122013,
-      :type => "Postseason",
-      :date_time => "5/16/13",
-      :away_team_id => 3,
-      :home_team_id => 6,
-      :away_goals => 2,
-      :home_goals => 3,
-      :venue => "Toyota Stadium",
-      :venue_link => "/api/v1/venues/null"
-      })
-
+    csv = CSV.read('./data/games_sample.csv', headers: true, header_converters: :symbol)
+    @games = csv.map do |row|
+      Game.new(row)
+    end
   end
 
-
   def test_it_exists
-    assert_instance_of Game, @game
+    @games.each do |game|
+      assert_instance_of Game, game
+    end
   end
 
   def test_it_has_attributes
-    assert_equal 2012030221, @game.game_id
-    assert_equal 20122013, @game.season
-    assert_equal "Postseason", @game.type
-    assert_equal "5/16/13", @game.date_time
-    assert_equal 3, @game.away_team_id
-    assert_equal 6, @game.home_team_id
-    assert_equal 2, @game.away_goals
-    assert_equal 3, @game.home_goals
-    assert_equal "Toyota Stadium", @game.venue
-    assert_equal "/api/v1/venues/null", @game.venue_link
+    assert_equal 2012030221, @games.first.game_id
+    assert_equal 20122013, @games.first.season
+    assert_equal "Postseason", @games.first.type
+    assert_equal "5/16/13", @games.first.date_time
+    assert_equal 3, @games.first.away_team_id
+    assert_equal 6, @games.first.home_team_id
+    assert_equal 2, @games.first.away_goals
+    assert_equal 3, @games.first.home_goals
+    assert_equal "Toyota Stadium", @games.first.venue
+    assert_equal "/api/v1/venues/null", @games.first.venue_link
   end
-  #add test for attributes
 end
