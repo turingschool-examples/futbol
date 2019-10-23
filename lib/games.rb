24666d3f972cc6f63d43b59e87f713476ce8b4df
Harrison Levin <harrison483@gmail.com>
2019-10-23 11:47:24 -0600

    edits to files
    puting methods in stat_tracker

--- lib/games.rb
+++ lib/games.rb
@@ -1,15 +1,8 @@
 require 'csv'
 
-<<<<<<< HEAD:lib/game.rb
 
-class Game
+class Games
   attr_reader :game_id, :season, :type, :date_time, :away_team_id, :home_team_id, :away_goals, :home_goals, :venue, :venue_link
-  #redundant?
-  @@games = []
-=======
-class Games < Teams
-  attr_reader :game_id, :season, :type, :date_time, :away_team_id, :home_team_id, :away_goals, :home_goals, :venue, :venue_link
->>>>>>> 236c5765973f3290f6f87164b7e3195f87521c75:lib/games.rb
 
   def initialize(game_id, season, type, date_time, away_team_id, home_team_id, away_goals, home_goals, venue, venue_link)
     @game_id = game_id
@@ -24,67 +17,22 @@ class Games < Teams
     @venue_link = venue_link
   end
 
-  def self.games
-
-  end
-
   def self.create(file_path)
     @games = []
     CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
-<<<<<<< HEAD:lib/game.rb
-      binding.pry
-      @@games << self.new(row[:game_id],
-=======
       @games << self.new(row[:game_id],
->>>>>>> 236c5765973f3290f6f87164b7e3195f87521c75:lib/games.rb
-                                    row[:season],
-                                    row[:type],
-                                    row[:date_time],
-                                    row[:away_team_id],
-                                    row[:home_team_id],
-                                    row[:away_goals].to_i,
-                                    row[:home_goals].to_i,
-                                    row[:venue],
-                                    row[:venue_link])
+                        row[:season],
+                        row[:type],
+                        row[:date_time],
+                        row[:away_team_id],
+                        row[:home_team_id],
+                        row[:away_goals].to_i,
+                        row[:home_goals].to_i,
+                        row[:venue],
+                        row[:venue_link])
     end
     @games
   end
 
-  def highest_total_score
-    games.max_by do |game|
-      game.away_goals + game.home_goals
-    end
-  end
 
-  def lowest_total_score
-    games.min_by do |game|
-      game.away_goals + game.home_goals
-    end
-  end
-
-  def biggest_blowout
-    games.max_by do |game|
-      (game.away_goals - game.home_goals).abs
-    end
-  end
-
-  def percentage_home_wins
-  end
-
-  def percentage_visitor_wins
-  end
-
-  def percentage_ties
-  end
-
-  def count_of_games_by_season
-  end
-
-  def average_goals_per_game
-  end
-
-  def average_goals_by_season
-  end
 end
-
-
--- lib/teams.rb
+++ lib/teams.rb
@@ -1,6 +1,6 @@
 require 'csv'
 
-class Teams < GameTeams
+class Teams
   attr_reader :team_id, :franchiseId, :teamName, :abbreviation, :stadium, :link
 
   def initialize(team_id, franchiseId, teamName, abbreviation, stadium, link)
