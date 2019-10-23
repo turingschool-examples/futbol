2d55bec059c9378c8e884e954421260a896d7bc2
Alison Vermeil <alison.minton@gmail.com>
2019-10-22 21:04:58 -0600

    Add game_test and game class file

---
+++ test/game_test.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/game'
+
+class GameTest < Minitest::Test
+
+  def setup
+    @game = Game.new({
+      :game_id => 2012030221,
+      :season => 20122013,
+      :type => "Postseason",
+      :date_time => "5/16/13",
+      :away_team_id => 3,
+      :home_team_id => 6,
+      :away_goals => 2,
+      :home_goals => 3,
+      :venue => "Toyota Stadium",
+      :venue_link => "/api/v1/venues/null"
+      })
+
+  end
+
+
+  def test_it_exists
+    assert_instance_of Game, @game
+  end
+
+  #add test for attributes 
+end
