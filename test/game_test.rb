75e5ea57bbe627ff41ef20cbad56c009904e60f4
Becky Robran <rerobran@gmail.com>
2019-10-23 14:54:19 -0600

    Adds first two tests
    Co-Authored-By: WrenS <wrenisit@users.noreply.github.com>
    Co-Authored-By: Travis Borgsmiller <TravisBorgsmiller@users.noreply.github.com>
    Co-Authored-By: Paul Debevec <PaulDebevec@users.noreply.github.com>

--- test/game_test.rb
+++ test/game_test.rb
@@ -1,9 +1,20 @@
 require './test/test_helper'
 require './lib/stat_tracker'
 require './lib/game'
+require './lib/game_stat_collection'
 
 class GameTest < Minitest::Test
+
+  def setup
+    csv_path = (CSV.read "./data/games.csv", headers: true, header_converters: :symbol)
+    @game = Game.new(csv_path)
+  end
+
   def test_it_exists
-    
+    assert_instance_of Game, @game
+  end
+
+  def test_it_initializes
+    assert_equal 2012030221, @game.game_id.first.to_i
   end
 end
