e0838bd9b505ec1f674358b76131ee60d4fdb959
msimon42 <msimon@uab.edu>
2019-10-23 14:57:15 -0600

    Add method to generate hash of all seasons

--- lib/helper_methods.rb
+++ lib/helper_methods.rb
@@ -5,6 +5,12 @@ require_relative 'stat_tracker'
 module HelperMethods
   def calculate_total_score(game)
     game.away_goals + game.home_goals
+  end
+
+  def generate_seasons_hash(games)
+    avg_by_season = Hash.new
+    seasons = games.map {|game| game.season}.uniq
+    seasons.each {|season| avg_by_season[season] = 0}
   end   
 
 end
