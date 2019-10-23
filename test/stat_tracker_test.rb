850e11273c5a7520ed954d0134f70d4892988535
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-10-22 23:28:35 -0600

    Add tests

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -1,5 +1,20 @@
 require './test/test_helper'
 
 class StatTrackerTest < Minitest::Test
+  def setup
+    test_paths = {
+      games: "./test/data/game_sample.csv",
+      teams: "./test/data/team_sample.csv",
+      game_teams: "./test/data/game_teams_sample.csv"
+    }
+    @stat_tracker = StatTracker.from_csv(test_paths)
+  end
 
+  def test_it_exists
+    assert_instance_of StatTracker, @stat_tracker
+  end
+
+  def test_games
+    assert_instance_of GameCollection, @stat_tracker.games
+  end
 end
