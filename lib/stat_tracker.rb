830a9d45eddbfed1939ed3e8baad0b2f1e718e19
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-10-24 15:18:44 -0600

    Add wrapper methods for iteration 2 methods
    Co-Authored-By: Phil DeLong <philjdelong@users.noreply.github.com>
    Co-Authored-By: Sebastian Sloan <sasloan@users.noreply.github.com>

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -10,4 +10,40 @@ class StatTracker
   def self.from_csv(file_paths)
     self.new(file_paths)
   end
+
+  def highest_total_score
+    @games.highest_total_score
+  end
+
+  def lowest_total_score
+    @games.lowest_total_score
+  end
+
+  def biggest_blowout
+    @games_teams.biggest_blowout
+  end
+
+  def percentage_home_wins
+    @games_teams.percentage_home_wins
+  end
+
+  def percentage_visitor_wins
+    @games_teams.percentage_visitor_wins
+  end
+
+  def percentage_ties
+    @games_teams.percentage_ties
+  end
+
+  def count_of_games_by_season
+    @games.count_of_games_by_season
+  end
+
+  def average_goals_per_game
+    @games.average_goals_per_game
+  end
+
+  def average_goals_by_season
+    @games.average_goals_by_season
+  end
 end
