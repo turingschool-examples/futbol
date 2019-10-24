6ea9edc01b39cd450e2e391955414b189b448cef
Alison Vermeil <alison.minton@gmail.com>
2019-10-24 16:46:27 -0600

    Change instance variable format for consistency
    Co-Authored-By: Lain McGratH <lain.mcgrath@gusto.com>
    Co-Authored-By: Ryan Allen <rallenwasd@gmail.com>

--- lib/team.rb
+++ lib/team.rb
@@ -1,10 +1,10 @@
 class Team
-  attr_reader :team_id, :franchiseid, :teamname, :abbreviation, :stadium, :link
+  attr_reader :team_id, :franchise_id, :team_name, :abbreviation, :stadium, :link
 
   def initialize(team_info)
-    @team_id = team_info[:team_id]
-    @franchiseid = team_info[:franchiseid]
-    @teamname = team_info[:teamname]
+    @team_id = team_info[:team_id].to_i
+    @franchise_id = team_info[:franchiseid].to_i
+    @team_name = team_info[:teamname]
     @abbreviation = team_info[:abbreviation]
     @stadium = team_info[:stadium]
     @link = team_info[:link]
