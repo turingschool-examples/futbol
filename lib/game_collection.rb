39404cc1778c78084cdc75766ea395278e169a99
Alison Vermeil <alison.minton@gmail.com>
2019-10-23 19:35:28 -0600

    Add percentage_visitor_wins method and change RV of percentage_home_wins to float

--- lib/game_collection.rb
+++ lib/game_collection.rb
@@ -42,6 +42,13 @@ class GameCollection
     home_wins = @total_games.find_all do |game|
       game.home_goals > game.away_goals
     end
-    home_wins.length.to_f / @total_games.length * 100
+    (home_wins.length.to_f / @total_games.length * 100).round(2)
+  end
+
+  def percentage_visitor_wins
+    visitor_wins = @total_games.find_all do |game|
+      game.away_goals > game.home_goals
+    end
+    (visitor_wins.length.to_f / @total_games.length * 100).round(2)
   end
 end
