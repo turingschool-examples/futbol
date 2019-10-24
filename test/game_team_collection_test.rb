12d44c3b6c6f1e3b1f464ad90cbcf3e2be4073f0
Alison Vermeil <alison.minton@gmail.com>
2019-10-24 13:27:19 -0600

    Add game_team_collection tests and class

---
+++ test/game_team_collection_test.rb
@@ -0,0 +1,20 @@
+require './test/test_helper'
+require './lib/game_team'
+require './lib/game_team_collection'
+
+class GameTeamCollectionTest < Minitest::Test
+
+  def setup
+    @game_team_collection = GameTeamCollection.new("./data/game_teams_sample.csv")
+  end
+
+  def test_it_exists
+    assert_instance_of GameTeamCollection, @game_team_collection
+  end
+
+  def test_it_has_total_game_team
+    @game_team_collection.create_game_teams("./data/game_teams_sample.csv")
+    assert_equal 9, @game_team_collection.total_game_teams.length
+  end
+
+end
