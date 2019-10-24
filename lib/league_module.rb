c106a78bbe7b957aede47605d50c3bde1a5feb8b
msimon42 <msimon@uab.edu>
2019-10-24 15:45:14 -0600

    Add helper methods

--- lib/league_module.rb
+++ lib/league_module.rb
@@ -4,6 +4,9 @@ module LeagueModule
   end
 
   def best_offense
+    # goals_by_team = Hash.new
+    # teams.each {|team| goals_by_team[team] = 0}
+
 
   end
 
@@ -15,5 +18,48 @@ module LeagueModule
 
   end
 
+  def worst_defense
 
+  end
+
+  def highest_scoring_visitor
+
+  end
+
+  def lowest_scoring_visitor
+
+  end
+
+  def lowest_scoring_home_team
+
+  end
+
+  def winningest_team
+
+  end
+
+  def best_fans
+
+  end
+
+  def worst_fans
+
+  end
+
+
+  ##Helper Methods##
+  def generate_avg_goals_by_team
+    games_by_team = game_teams.group_by do |game|
+      game.team_id
+    end
+    games_by_team.transform_values do |val|
+      games = val.length
+      val.map {|v| v.goals}.reduce {|sum, num| sum + num} / val.length
+    end
+    games_by_team
+  end
+
+  def convert_ids_to_team_name(id)
+
+  end   
 end
