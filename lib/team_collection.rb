c63f36dc87017d83401579ef87a539af7b1c0581
Nathan Keller <keller.nathan@gmail.com>
2019-10-24 12:37:21 -0600

    Update team collection to be able to put team objs into array

--- lib/team_collection.rb
+++ lib/team_collection.rb
@@ -2,16 +2,19 @@ require_relative 'team'
 require 'CSV'
 
 class TeamCollection
-  attr_reader :teams
+  attr_reader :teams, :team_objs
 
   def initialize(csv_file_path)
+    @team_objs = []
     @teams = create_teams(csv_file_path)
   end
 
   def create_teams(csv_file_path)
     csv = CSV.read("#{csv_file_path}", headers: true, header_converters: :symbol)
+    team_obj = 0
     csv.map do |row|
-      Team.new(row)
+      team_obj = Team.new(row)
+      @team_objs << team_obj
     end
   end
 end
