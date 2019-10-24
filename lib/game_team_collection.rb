402e0d6a285c8e425ea5ebb84592bde252962c49
Nathan Keller <keller.nathan@gmail.com>
2019-10-24 14:24:10 -0600

    Refactor foreach

--- lib/game_team_collection.rb
+++ lib/game_team_collection.rb
@@ -2,24 +2,22 @@ require './lib/game_team'
 require 'CSV'
 
 class GameTeamCollection
-  attr_reader :game_team, :game_team_objs
+  attr_reader :game_team
 
   def initialize(csv_file_path)
-    @game_team_objs = []
     @game_team = create_game_teams(csv_file_path)
   end
 
   def create_game_teams(csv_file_path)
-    csv = CSV.read("#{csv_file_path}", headers: true, header_converters: :symbol)
-    game_team_obj = 0
+    csv = CSV.foreach("#{csv_file_path}", headers: true, header_converters: :symbol)
     csv.map do |row|
-      game_team_obj = GameTeam.new(row)
-      @game_team_objs << game_team_obj
+      GameTeam.new(row)
     end
   end
 
-  def count_of_game_teams
-    @game_team.team_id.uniq.count
+  def count_of_teams
+    team_id_array = @game_team.map { |gt| gt.team_id }
+    team_id_array.uniq.length
   end
 
 end
