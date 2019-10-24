397360eee083a125e37e3ad4bb33729516a1cf7b
Paul Debevec <paulmdebevec@gmail.com>
2019-10-24 08:58:25 -0600

    Added total_scores method

--- lib/game_collection.rb
+++ lib/game_collection.rb
@@ -5,17 +5,28 @@ require_relative 'stat_tracker'
 class GameCollection
   attr_reader :game_instances
 
-  def initialize(game_path)
-    @game_path = game_path
-    @game_instances = all_games
+  def initialize(csv_file_path)
+    @csv_file_path = csv_file_path
+    @game_instances = all_stats
   end
 
-  def all_games
+  def all_stats
     game_objects = []
-    csv = CSV.read("#{@game_path}", headers: true, header_converters: :symbol)
+    csv = CSV.read("#{@csv_file_path}", headers: true, header_converters: :symbol)
       csv.map do |row|
       game_objects <<  Game.new(row)
     end
     game_objects
   end
+
+# Returns an array that contains every game score both winners and losers added together
+  def total_scores
+    total_scores_array = []
+      @game_instances.each do |game|
+        total_goals = game.away_goals + game.home_goals
+        total_scores_array << total_goals
+      end
+    total_scores_array
+  end
+
 end
