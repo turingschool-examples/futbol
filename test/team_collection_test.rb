5344d3fb39757147a9f7b1a7f57e52f6e5805556
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-10-23 22:42:00 -0600

    Add total_teams test

--- test/team_collection_test.rb
+++ test/team_collection_test.rb
@@ -8,4 +8,8 @@ class TeamCollectionTest < Minitest::Test
   def test_it_exists
     assert_instance_of TeamCollection, @team_collection
   end
+
+  def test_total_teams
+    assert_equal 10, @team_collection.total_teams
+  end
 end
