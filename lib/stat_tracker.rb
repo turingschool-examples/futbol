3eef91adbc19ff7624579b806378eec57671955d
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-10-23 21:32:38 -0600

    Add 2 more games to dummy. Add lowest total score method and tests
    Co-Authored-By: Madelyn Romero <madelynrr@users.noreply.github.com>
    Co-Authored-By: Nathan K <nkeller1@users.noreply.github.com>
    Co-Authored-By: Will Meighan <Will-Meighan@users.noreply.github.com>

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -34,5 +34,23 @@ class StatTracker
     highest_total = games.game_objs.max_by{ |game| game.away_goals + game.home_goals }
     highest_total.away_goals + highest_total.home_goals
   end
+
+  def lowest_total_score
+    lowest_total = games.game_objs.min_by{ |game| game.away_goals + game.home_goals }
+    lowest_total.away_goals + lowest_total.home_goals
+  end
+
+# Average number of goals scored in a game organized in a hash with season
+# names (e.g. 20122013) as keys and a float representing the average number of
+# goals in a game for that season as a key (rounded to the nearest 100th)
+  def average_goals_by_season
+    require 'pry'; binding.pry
+    avg_per_season = games.game_objs.map do |game|
+      game.away_goals + game.home_goals
+
+    # games.game_objs.reduce({}) do |avg_goals_by_season, season|
+    #   avg_goals_by_season[season] =
+    #
+  end
 end
 
--- test/game_stat_tests.rb
+++ test/game_stat_tests.rb
@@ -12,6 +12,22 @@ class StatTrackerTest < Minitest::Test
   end
 
   def test_it_can_calculate_highest_goal_total
-    assert_equal 5, @stat_tracker.highest_total_score
+    assert_equal 7, @stat_tracker.highest_total_score
+  end
+
+  def test_it_can_calculate_lowest_goal_total
+    assert_equal 2, @stat_tracker.lowest_total_score
+  end
+
+  def test_it_can_get_average_goals_per_season
+    expected = {
+      "20122013" => 3.60,
+      "20132014" => 5.00,
+      "20142015" => 2.00,
+      "20152016" => 6.00,
+      "20162017" => 3.00,
+      "20172018" => 4.00
+    }
+    assert_equal expected, @stat_tracker.average_goals_by_season
   end
 end
