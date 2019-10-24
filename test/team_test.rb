62b9a678172ab7e2d2c0313ea0f1dc55295373bf
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-10-23 22:46:36 -0600

    Change camelCase to snake_case

--- test/team_test.rb
+++ test/team_test.rb
@@ -6,8 +6,8 @@ class TeamTest < Minitest::Test
 def setup
   team_info = {
     team_id: "1",
-    franchiseId: "23",
-    teamName: "Atlanta United",
+    franchise_id: "23",
+    team_name: "Atlanta United",
     abbreviation: "ATL",
     stadium: "Mercedes-Benz Stadium",
     link: "/api/v1/teams/1"
@@ -21,8 +21,8 @@ end
 
   def test_initialize
     assert_equal "1", @team.team_id
-    assert_equal "23", @team.franchiseId
-    assert_equal "Atlanta United", @team.teamName
+    assert_equal "23", @team.franchise_id
+    assert_equal "Atlanta United", @team.team_name
     assert_equal "ATL", @team.abbreviation
     assert_equal "Mercedes-Benz Stadium", @team.stadium
     assert_equal "/api/v1/teams/1", @team.link
