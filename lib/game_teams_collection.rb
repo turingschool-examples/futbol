fe6e8d22297846ded3e1d3f2aa253dc79b0eafff
Nathan Keller <keller.nathan@gmail.com>
2019-10-24 12:45:23 -0600

    Update game_team_collection to similar to game and team collections

--- lib/game_teams_collection.rb
+++ lib/game_teams_collection.rb
@@ -2,16 +2,19 @@ require './lib/game_team'
 require 'CSV'
 
 class GameTeamsCollection
-  attr_reader :game_teams
+  attr_reader :game_teams, :game_teams_objs
 
   def initialize(csv_file_path)
+    @game_teams_objs = []
     @game_teams = create_game_teams(csv_file_path)
   end
 
   def create_game_teams(csv_file_path)
     csv = CSV.read("#{csv_file_path}", headers: true, header_converters: :symbol)
+    game_team_obj = 0
     csv.map do |row|
-       GameTeams.new(row)
+      game_team_obj = GameTeams.new(row)
+      @game_teams_objs << game_team_obj
     end
   end
 end
