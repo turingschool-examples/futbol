82d105c0da274f94af8af0738cdaf1216bddec49
Ryan Allen <rallenwasd@gmail.com>
2019-10-25 11:57:59 -0600

    Refactor attributes and add test_it_has_a_win_percentage

--- test/team_test.rb
+++ test/team_test.rb
@@ -1,13 +1,23 @@
 require 'csv'
 require './test/test_helper'
 require './lib/team'
+require 'mocha/minitest'
+require './lib/game_team'
 
 class TeamsTest < Minitest::Test
 
   def setup
+    csv = CSV.read('./test/data/game_teams_sample.csv', headers: true, header_converters: :symbol)
+    all_games = csv.map do |row|
+      GameTeam.new(row)
+    end
+
     csv = CSV.read('./test/data/teams_sample.csv', headers: true, header_converters: :symbol)
     @teams = csv.map do |row|
-      Team.new(row)
+      all_team_games = all_games.find_all do |game|
+        row[:team_id] == game.team_id
+      end
+      Team.new(row, all_team_games)
     end
   end
 
@@ -19,12 +29,16 @@ class TeamsTest < Minitest::Test
 
   def test_it_has_attributes
     team = @teams.first
+    assert_equal "3", team.team_id
+    assert_equal 10, team.franchise_id
+    assert_equal "Houston Dynamo", team.team_name
+    assert_equal "HOU", team.abbreviation
+  end
 
-    assert_equal 1, team.team_id
-    assert_equal 23, team.franchise_id
-    assert_equal "Atlanta United", team.team_name
-    assert_equal "ATL", team.abbreviation
-    assert_equal "Mercedes-Benz Stadium", team.stadium
-    assert_equal "/api/v1/teams/1", team.link
+  def test_it_has_a_win_percentage
+    team = @teams.find {|team| team.team_id == "8"}
+    # 1 win 3 losses 1 tie
+    win_percentage = (1 / 5.0).round(2)
+    assert_equal win_percentage, team.win_percentage
   end
 end
