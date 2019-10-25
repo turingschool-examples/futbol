231a7432ea5bc2a8551bdc41fad627367d15971f
Madelyn Romero <m.romero013@gmail.com>
2019-10-25 15:03:25 -0600

    Add average goals per game method to game collection to reduce lines in stat tracker and improve encapsulation of project

--- lib/game_collection.rb
+++ lib/game_collection.rb
@@ -12,4 +12,11 @@ class GameCollection
     csv = CSV.foreach("#{csv_file_path}", headers: true, header_converters: :symbol)
     csv.map { |row| Game.new(row) }
   end
+
+  def avg_goals_per_game
+    goals = @games.reduce(0) do |total_goals, game|
+      total_goals += (game.away_goals + game.home_goals)
+    end
+    (goals.to_f / @games.length).round(2)
+  end
 end
