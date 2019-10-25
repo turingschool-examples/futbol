5e5b89826b0ded3e7e6503dc8178702c41b2f55e
Ryan Allen <rallenwasd@gmail.com>
2019-10-25 12:03:16 -0600

    Add functionality for team instance to initialize with all_team_games and add method winningest_team with all tests passing

--- lib/team_collection.rb
+++ lib/team_collection.rb
@@ -1,17 +1,35 @@
 require 'csv'
 require_relative './team.rb'
+require_relative './game_team.rb'
 
 class TeamCollection
   attr_reader :total_teams
 
-  def initialize(team_path)
+  def initialize(team_path, game_team_path)
+    @total_games = create_game_team(game_team_path)
     @total_teams = create_teams(team_path)
+  end
+
+  def create_game_team(game_team_path)
+    csv = CSV.read(game_team_path, headers: true, header_converters: :symbol)
+    csv.map do |row|
+      GameTeam.new(row)
+    end
   end
 
   def create_teams(team_path)
     csv = CSV.read(team_path, headers: true, header_converters: :symbol)
     csv.map do |row|
-      Team.new(row)
+      all_team_games = @total_games.find_all do |game|
+        row[:team_id] == game.team_id
+      end
+      Team.new(row, all_team_games)
     end
+  end
+
+  def winningest_team
+    @total_teams.max_by do |team|
+      team.win_percentage
+    end.team_name
   end
 end
