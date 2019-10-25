b756be6f48b97c8e92f3367550f3f995db3352d2
Sebastian Sloan <51456013+sasloan@users.noreply.github.com>
2019-10-25 16:12:10 -0600

    per to by

--- lib/games_collection.rb
+++ lib/games_collection.rb
@@ -25,7 +25,7 @@ class GamesCollection
     seasons_of_games.values.map {|value| value.length}
   end
 
-  def count_of_games_per_season
+  def count_of_games_by_season
     target_hash = {}
     unique_seasons.each_with_index do |season, index|
       target_hash[season] = number_of_games_in_each_season[index]
