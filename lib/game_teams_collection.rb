0377bb77486e51e1c68762fc35da89c03228e245
Wren Steitle <lauren.steitle@gmail.com>
2019-10-25 17:13:11 -0600

    Update all tests to pass and fix branches

--- lib/game_teams_collection.rb
+++ lib/game_teams_collection.rb
@@ -2,7 +2,70 @@ require 'csv'
 require_relative 'game_teams'
 require_relative 'stat_tracker'
 
-class GameTeamCollection
-  
+class GameTeamsCollection
 
+  def initialize(game_teams_path)
+    @game_teams_path = game_teams_path
+    @game_teams_collection_instances = all_game_teams
+  end
+
+  def all_game_teams
+    csv = CSV.read("#{@game_teams_path}", headers: true, header_converters: :symbol)
+      csv.map do |row|
+       GameTeams.new(row)
+    end
+  end
+
+  def percentage_home_wins
+    #require 'pry'; binding.pry
+   all_results = []
+   @game_teams_collection_instances.each do |instance|
+     all_results << instance.result
+   end
+   number_of_results = all_results.length
+
+   result_win = []
+   @game_teams_collection_instances.each do |instance|
+     if instance.result.include?("WIN")
+       result_win << instance.result
+     end
+   end
+   number_of_wins = result_win.length
+   home_win_percentage = number_of_wins.to_f / number_of_results
+ end
+
+ def percentage_vistor_wins
+   all_results = []
+   @game_teams_collection_instances.each do |instance|
+     all_results << instance.result
+   end
+   number_of_results = all_results.length
+
+   result_loss = []
+   @game_teams_collection_instances.each do |instance|
+     if instance.result.include?("LOSS")
+       result_loss << instance.result
+     end
+   end
+   number_of_losses = result_loss.length
+   home_win_percentage = number_of_losses.to_f / number_of_results
+ end
+
+ def percentage_ties
+   all_results = []
+   @game_teams_collection_instances.each do |instance|
+     all_results << instance.result
+   end
+   number_of_results = all_results.length
+
+   result_tie = []
+   @game_teams_collection_instances.each do |instance|
+     if instance.result.include?("TIE")
+       result_tie << instance.result
+     end
+
+   end
+   number_of_ties = result_tie.length
+   home_win_percentage = number_of_ties.to_f / number_of_results
+ end
 end
--- test/game_teams_collection_test.rb
+++ test/game_teams_collection_test.rb
@@ -6,26 +6,23 @@ require './lib/game_teams'
 require './lib/stat_tracker'
 require './lib/game_teams_collection'
 
-class GameTeamsCollectionTest < Minitest::Test
+class GameTeamsCollectionTest < MiniTest::Test
   def test_it_exists
     new_game_tracker_instance = GameTeamsCollection.new('./dummy_data/dummy_game_teams.csv')
-    assert_instance_of GameTeams, new_game_tracker_instance
+    assert_instance_of GameTeamsCollection, new_game_tracker_instance
   end
 
   def test_percentage_home_wins
-
     new_game_tracker_instance = GameTeamsCollection.new('./dummy_data/dummy_game_teams.csv')
     assert_equal 0.40, new_game_tracker_instance.percentage_home_wins
   end
 
   def test_percentage_vistor_wins
-
     new_game_tracker_instance = GameTeamsCollection.new('./dummy_data/dummy_game_teams.csv')
     assert_equal 0.60, new_game_tracker_instance.percentage_vistor_wins
   end
 
   def test_percentage_ties
-
     new_game_tracker_instance = GameTeamsCollection.new('./dummy_data/dummy_game_teams.csv')
     assert_equal 0, new_game_tracker_instance.percentage_ties
   end
