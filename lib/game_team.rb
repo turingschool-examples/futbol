b4a357c8d29ecf4be9853b38530f0e72be85e96f
Ryan Allen <rallenwasd@gmail.com>
2019-10-25 12:01:52 -0600

    Refactor team_id to not convert to integer

--- lib/game_team.rb
+++ lib/game_team.rb
@@ -18,7 +18,7 @@ class GameTeam
 
   def initialize(game_team_info)
     @game_id = game_team_info[:game_id].to_i
-    @team_id = game_team_info[:team_id].to_i
+    @team_id = game_team_info[:team_id]
     @hoa = game_team_info[:hoa]
     @result = game_team_info[:result]
     @settled_in = game_team_info[:settled_in]
