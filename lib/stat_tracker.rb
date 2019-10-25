7a15a339a88e85e09e820608783771e1227ff79d
Ryan Allen <rallenwasd@gmail.com>
2019-10-25 16:07:15 -0600

    Add additional requirement and methods lowest_total_score, biggest_blowout, percentage_home_wins, percentage_visitor_wins, percentage_ties, count_of_games_by_season_list, average_goals_per_game, average_goals_by_season, winningest_team

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,4 +1,5 @@
 require_relative './game_collection'
+require_relative './team_collection'
 
 class StatTracker
   attr_reader :stats
@@ -18,20 +19,44 @@ class StatTracker
 
     self.new(game_path, team_path, game_team_path)
   end
-  #
-  # def games
-  #   @games_collection = GameCollection.new(@game_path)
-  #   #in game collection is where we would make new game instances
-  # end
-  # def teams
-  #   TeamCollection.new(@team_path)
-  # end
-  #
-  # def game_teams
-  #   GameTeamCollection.new(@game_team_path)
-  # end
 
   def highest_total_score
     @games_collection.highest_total_score
+  end
+
+  def lowest_total_score
+    @games_collection.lowest_total_score
+  end
+
+  def biggest_blowout
+    @games_collection.biggest_blowout
+  end
+
+  def percentage_home_wins
+    @games_collection.percentage_home_wins
+  end
+
+  def percentage_visitor_wins
+    @games_collection.percentage_visitor_wins
+  end
+
+  def percentage_ties
+    @games_collection.percentage_ties
+  end
+
+  def count_of_games_by_season_list
+    @games_collection.count_of_games_by_season
+  end
+
+  def average_goals_per_game
+    @games_collection.average_goals_per_game
+  end
+
+  def average_goals_by_season
+    @games_collection.average_goals_by_season
+  end
+
+  def winningest_team
+    @teams_collection.winningest_team
   end
 end
