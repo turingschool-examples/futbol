6520a400377f611c64b7246db0d065d8b0a5a01c
Ryan Allen <rallenwasd@gmail.com>
2019-10-25 12:00:17 -0600

    Refactor attributes and add win_percentage method with tests passing

--- lib/team.rb
+++ lib/team.rb
@@ -1,13 +1,18 @@
 class Team
   attr_reader :team_id, :franchise_id, :team_name, :abbreviation, :stadium, :link
 
-  def initialize(team_info)
-    @team_id = team_info[:team_id].to_i
+  def initialize(team_info, all_team_games)
+    @team_id = team_info[:team_id]
     @franchise_id = team_info[:franchiseid].to_i
     @team_name = team_info[:teamname]
     @abbreviation = team_info[:abbreviation]
-    @stadium = team_info[:stadium]
-    @link = team_info[:link]
+    @all_team_games = all_team_games
   end
 
+  def win_percentage
+    win_count = @all_team_games.count do |game|
+      game.result == "WIN"
+    end
+    (win_count / @all_team_games.length.to_f).round(2)
+  end
 end
