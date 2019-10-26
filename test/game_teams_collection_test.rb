4a39058b9f5bd44bf9449b3367dd06fb345865c8
Becky Robran <rerobran@gmail.com>
2019-10-26 09:24:39 -0600

    Updates testing for percentage methods

--- test/game_teams_collection_test.rb
+++ test/game_teams_collection_test.rb
@@ -12,19 +12,19 @@ class GameTeamsCollectionTest < MiniTest::Test
     assert_instance_of GameTeamsCollection, new_game_tracker_instance
   end
 
-  def test_percentage_home_wins
-    new_game_tracker_instance = GameTeamsCollection.new('./dummy_data/dummy_game_teams.csv')
-    assert_equal 0.40, new_game_tracker_instance.percentage_home_wins
-  end
-
-  def test_percentage_vistor_wins
-    new_game_tracker_instance = GameTeamsCollection.new('./dummy_data/dummy_game_teams.csv')
-    assert_equal 0.60, new_game_tracker_instance.percentage_vistor_wins
-  end
-
-  def test_percentage_ties
-    new_game_tracker_instance = GameTeamsCollection.new('./dummy_data/dummy_game_teams.csv')
-    assert_equal 0, new_game_tracker_instance.percentage_ties
-  end
+  # def test_percentage_home_wins
+  #   new_game_tracker_instance = GameTeamsCollection.new('./dummy_data/dummy_game_teams.csv')
+  #   assert_equal 0.40, new_game_tracker_instance.percentage_home_wins
+  # end
+  #
+  # def test_percentage_vistor_wins
+  #   new_game_tracker_instance = GameTeamsCollection.new('./dummy_data/dummy_game_teams.csv')
+  #   assert_equal 0.60, new_game_tracker_instance.percentage_vistor_wins
+  # end
+  #
+  # def test_percentage_ties
+  #   new_game_tracker_instance = GameTeamsCollection.new('./dummy_data/dummy_game_teams.csv')
+  #   assert_equal 0, new_game_tracker_instance.percentage_ties
+  # end
 
 end
