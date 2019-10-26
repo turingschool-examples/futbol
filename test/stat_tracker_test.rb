605b9ec7800415b2969fb20a90e4ad66dba9255f
Ryan Allen <rallenwasd@gmail.com>
2019-10-26 12:12:41 -0500

    Add tests test_it_has_winningest_team, test_it_has_the_best_offense, test_it_has_best_fans, test_it_has_worst_fans

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -23,4 +23,21 @@ class StatTrackerTest < Minitest::Test
   def test_it_has_scores
     @stat_tracker.highest_total_score
   end
+
+  def test_it_has_winningest_team
+    # FC Dallas has only wins
+    assert_equal "FC Dallas", @stat_tracker.winningest_team
+  end
+
+  def test_it_has_the_best_offense
+    assert_equal "New York City FC", @stat_tracker.best_offense
+  end
+
+  def test_it_has_best_fans
+    assert_equal "LA Galaxy", @stat_tracker.best_fans
+  end
+
+  def test_it_has_worst_fans
+    assert_equal [], @stat_tracker.worst_fans
+  end
 end
