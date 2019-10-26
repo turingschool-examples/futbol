be6939ce9942b155eb9281c548e76d2d80a8deec
Nathan Keller <keller.nathan@gmail.com>
2019-10-26 16:26:14 -0600

    create test for winningest team

--- test/game_team_collection_test.rb
+++ test/game_team_collection_test.rb
@@ -5,11 +5,20 @@ require './lib/stat_tracker'
 class GameTeamCollectionTest < Minitest::Test
 
   def setup
-    @game_team_collection= GameTeamCollection.new("./test/test_game_team_data.csv")
+    @game_team_collection= GameTeamCollection.new("./test/dummy_game_team_data.csv")
+  end
+
+  def test_it_exists
+    assert_instance_of GameTeamCollection, @game_team_collection
   end
 
   def test_count_of_teams
     assert_equal 7, @game_team_collection.count_of_teams
   end
+
+  def test_winningest_team
+    assert_equal "LA Galaxy", @game_team_collection.winningest_team
+  end
+
 
 end
