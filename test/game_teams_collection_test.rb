e85b35ca1126b5169168039d9d87f8cfd40534f2
Wren Steitle <lauren.steitle@gmail.com>
2019-10-26 11:58:15 -0600

    Clean up code comments

--- test/game_teams_collection_test.rb
+++ test/game_teams_collection_test.rb
@@ -10,25 +10,8 @@ class GameTeamsCollectionTest < MiniTest::Test
     assert_instance_of GameTeamsCollection, new_game_tracker_instance
   end
 
-  # def test_percentage_home_wins
-  #   new_game_tracker_instance = GameTeamsCollection.new('./dummy_data/dummy_game_teams.csv')
-  #   assert_equal 0.40, new_game_tracker_instance.percentage_home_wins
-  # end
-  #
-  # def test_percentage_vistor_wins
-  #   new_game_tracker_instance = GameTeamsCollection.new('./dummy_data/dummy_game_teams.csv')
-  #   assert_equal 0.60, new_game_tracker_instance.percentage_vistor_wins
-  # end
-  #
-  # def test_percentage_ties
-  #   new_game_tracker_instance = GameTeamsCollection.new('./dummy_data/dummy_game_teams.csv')
-  #   assert_equal 0, new_game_tracker_instance.percentage_ties
-  # end
-
   def test_winningest_team
-    #this is in progress
     new_game_tracker_instance = GameTeamsCollection.new('./dummy_data/dummy_game_teams.csv')
-
     assert_equal "6", new_game_tracker_instance.winningest_team_id
   end
 end
