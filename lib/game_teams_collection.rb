566ef4e0e5663665f1cc6032c7becbe5eb2b4fc5
Becky Robran <rerobran@gmail.com>
2019-10-26 09:10:15 -0600

    "Updates Game Collection methods"

--- lib/game_teams_collection.rb
+++ lib/game_teams_collection.rb
@@ -16,56 +16,44 @@ class GameTeamsCollection
     end
   end
 
-  def percentage_home_wins
-    #require 'pry'; binding.pry
-   all_results = []
-   @game_teams_collection_instances.each do |instance|
-     all_results << instance.result
-   end
-   number_of_results = all_results.length
-
-   result_win = []
-   @game_teams_collection_instances.each do |instance|
-     if instance.result.include?("WIN")
-       result_win << instance.result
-     end
-   end
-   number_of_wins = result_win.length
-   home_win_percentage = number_of_wins.to_f / number_of_results
- end
-
- def percentage_vistor_wins
-   all_results = []
-   @game_teams_collection_instances.each do |instance|
-     all_results << instance.result
-   end
-   number_of_results = all_results.length
-
-   result_loss = []
-   @game_teams_collection_instances.each do |instance|
-     if instance.result.include?("LOSS")
-       result_loss << instance.result
-     end
-   end
-   number_of_losses = result_loss.length
-   home_win_percentage = number_of_losses.to_f / number_of_results
- end
-
- def percentage_ties
-   all_results = []
-   @game_teams_collection_instances.each do |instance|
-     all_results << instance.result
-   end
-   number_of_results = all_results.length
-
-   result_tie = []
-   @game_teams_collection_instances.each do |instance|
-     if instance.result.include?("TIE")
-       result_tie << instance.result
-     end
-
-   end
-   number_of_ties = result_tie.length
-   home_win_percentage = number_of_ties.to_f / number_of_results
- end
+ #  def home_wins
+ #    @game_teams_collection_instances.count do |game_team|
+ #      game_team.result == "WIN"
+ #    end
+ #  end
+ #
+ # def percentage_vistor_wins
+ #   all_results = []
+ #   @game_teams_collection_instances.each do |instance|
+ #     all_results << instance.result
+ #   end
+ #   number_of_results = all_results.length
+ #
+ #   result_loss = []
+ #   @game_teams_collection_instances.each do |instance|
+ #     if instance.result.include?("LOSS")
+ #       result_loss << instance.result
+ #     end
+ #   end
+ #   number_of_losses = result_loss.length
+ #   home_win_percentage = number_of_losses.to_f / number_of_results
+ # end
+ #
+ # def percentage_ties
+ #   all_results = []
+ #   @game_teams_collection_instances.each do |instance|
+ #     all_results << instance.result
+ #   end
+ #   number_of_results = all_results.length
+ #
+ #   result_tie = []
+ #   @game_teams_collection_instances.each do |instance|
+ #     if instance.result.include?("TIE")
+ #       result_tie << instance.result
+ #     end
+ #
+ #   end
+ #   number_of_ties = result_tie.length
+ #   home_win_percentage = number_of_ties.to_f / number_of_results
+ # end
 end
