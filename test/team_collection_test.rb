1beac0bffa07c3e5c00f46f6ccbb30430e37f03a
Alison Vermeil <alison.minton@gmail.com>
2019-10-26 09:53:33 -0600

    Add tests for count_of_teams, best/worst offense and put in order according to spec

--- test/team_collection_test.rb
+++ test/team_collection_test.rb
@@ -16,13 +16,29 @@ class TeamsCollectionTest < Minitest::Test
     assert_equal 8, @total_teams.total_teams.length
   end
 
-  def test_it_has_winningest_team
-    # FC Dallas has only wins
-    assert_equal "FC Dallas", @total_teams.winningest_team
+  def test_it_has_count_of_teams
+    assert_equal 8, @total_teams.total_teams.length
   end
 
   def test_it_has_the_best_offense
     assert_equal "New York City FC", @total_teams.best_offense
+  end
+
+  def test_it_has_the_worst_offense
+    assert_equal "Sporting Kansas City", @total_teams.worst_offense
+  end
+
+  def test_it_has_the_best_defense
+    assert_equal "FC Dallas", @total_teams.best_defense
+  end
+
+  def test_it_has_the_worst_defense
+    assert_equal "Houston Dynamo", @total_teams.worst_defense 
+  end
+
+  def test_it_has_winningest_team
+    # FC Dallas has only wins
+    assert_equal "FC Dallas", @total_teams.winningest_team
   end
 
   def test_it_has_best_fans
