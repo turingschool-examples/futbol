742a90961230a0fcc521532cdd2205c5e29e5153
Ryan Allen <rallenwasd@gmail.com>
2019-10-27 14:30:42 -0500

    Refactor all test files to use new data sample set as of 10/27/19 with all tests passing

--- test/game_collection_test.rb
+++ test/game_collection_test.rb
@@ -3,7 +3,6 @@ require './lib/game'
 require './lib/game_collection'
 
 class GameCollectionTest < Minitest::Test
-
   def setup
     @game_collection = GameCollection.new("./test/data/games_sample.csv")
   end
@@ -14,53 +13,49 @@ class GameCollectionTest < Minitest::Test
 
   def test_it_has_total_games
     @game_collection.create_games('./test/data/games_sample.csv')
-    assert_equal 8, @game_collection.total_games.length
+    assert_equal 20, @game_collection.total_games.length
   end
 
   def test_it_can_calculate_highest_total_score
-    assert_equal 5, @game_collection.highest_total_score
+    assert_equal 10, @game_collection.highest_total_score
   end
 
   def test_it_can_calculate_lowest_total_score
-    assert_equal 2, @game_collection.lowest_total_score
+    assert_equal 0, @game_collection.lowest_total_score
   end
 
   def test_it_can_calculate_biggest_blowout
-    assert_equal 2, @game_collection.biggest_blowout
+    assert_equal 6, @game_collection.biggest_blowout
   end
 
   def test_it_can_calculate_percentage_home_wins
-    assert_equal 0.25, @game_collection.percentage_home_wins
+    assert_equal 0.45, @game_collection.percentage_home_wins
   end
 
   def test_it_can_calculate_percentage_visitor_wins
-    assert_equal 0.50, @game_collection.percentage_visitor_wins
+    assert_equal 0.40, @game_collection.percentage_visitor_wins
   end
 
   def test_it_can_calculate_percentage_ties
-    #i think we can just put the expected value here and not do math in the assertion
-    assert_equal (2/8.0).round(2), @game_collection.percentage_ties
+    assert_equal 0.15, @game_collection.percentage_ties
   end
 
   def test_it_can_calculate_count_of_games_by_season
     count_games_by_season_list = {
-      "20122013" => 5,
-      "20142015" => 3
+      "20172018" => 10,
+      "20152016" => 10
     }
     assert_equal count_games_by_season_list, @game_collection.count_of_games_by_season
   end
 
   def test_it_can_calculate_average_goals_per_game
-    #(31 total goals / 8 games)
-    assert_equal (31/8.0).round(2), @game_collection.average_goals_per_game
+    assert_equal 4.60, @game_collection.average_goals_per_game
   end
 
   def test_it_can_calculate_average_goals_by_season
-    # 2012/2013 19 goals / 5 games
-    # 2014/2015 12 goals / 3 games
     count_goals_by_season_list = {
-      "20122013" => (19/5.0).round(2),
-      "20142015" => (12/3.0).round(2)
+      "20172018" => 4.10,
+      "20152016" => 5.10
     }
     assert_equal count_goals_by_season_list, @game_collection.average_goals_by_season
   end
--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -20,99 +20,101 @@ class StatTrackerTest < Minitest::Test
     assert_instance_of StatTracker, stat_tracker
   end
 
-  #what happened to testing it can be made from csv?
+  def test_it_from_csv
+    # need test for self.from_csv method
+  end
 
   def test_it_has_highest_total_score
-    assert_equal 5, @stat_tracker.highest_total_score
+    assert_equal 10, @stat_tracker.highest_total_score
   end
 
   def test_it_has_lowest_total_score
-    assert_equal 2, @stat_tracker.lowest_total_score
+    assert_equal 0, @stat_tracker.lowest_total_score
   end
 
   def test_it_has_biggest_blowout
-    assert_equal 2, @stat_tracker.biggest_blowout
+    assert_equal 6, @stat_tracker.biggest_blowout
   end
 
   def test_it_has_percentage_home_wins
-    assert_equal 0.25, @stat_tracker.percentage_home_wins
+    assert_equal 0.45, @stat_tracker.percentage_home_wins
   end
 
   def test_it_has_percentage_visitor_wins
-    assert_equal 0.50, @stat_tracker.percentage_visitor_wins
+    assert_equal 0.40, @stat_tracker.percentage_visitor_wins
   end
 
   def test_it_has_percentage_ties
-    assert_equal 0.25, @stat_tracker.percentage_ties
+    assert_equal 0.15, @stat_tracker.percentage_ties
   end
 
   def test_it_has_count_of_games_by_season
     count_games_by_season_list = {
-      "20122013" => 5,
-      "20142015" => 3
+      "20172018" => 10,
+      "20152016" => 10
     }
     assert_equal count_games_by_season_list, @stat_tracker.count_of_games_by_season
   end
 
   def test_it_has_average_goals_per_game
-    assert_equal 3.88, @stat_tracker.average_goals_per_game
+    assert_equal 4.60, @stat_tracker.average_goals_per_game
   end
 
   def test_it_has_average_goals_by_season
     count_goals_by_season_list = {
-      "20122013" => (19/5.0).round(2),
-      "20142015" => (12/3.0).round(2)
+      "20172018" => 4.10,
+      "20152016" => 5.1
     }
     assert_equal count_goals_by_season_list, @stat_tracker.average_goals_by_season
   end
 
   def test_it_has_count_of_teams
-    assert_equal 8, @stat_tracker.count_of_teams
+    assert_equal 3, @stat_tracker.count_of_teams
   end
 
   def test_it_has_best_offense
-    assert_equal "New York City FC", @stat_tracker.best_offense
+    assert_equal "FC Cincinnati", @stat_tracker.best_offense
   end
 
   def test_it_has_worst_offense
-    assert_equal "Sporting Kansas City", @stat_tracker.worst_offense
+    assert_equal "Atlanta United", @stat_tracker.worst_offense
   end
 
   def test_it_has_best_defense
-    assert_equal "FC Dallas", @stat_tracker.best_defense
+    assert_equal "Atlanta United", @stat_tracker.best_defense
   end
 
   def test_it_has_the_worst_defense
-    assert_equal "Houston Dynamo", @stat_tracker.worst_defense
+    assert_equal "Chicago Fire", @stat_tracker.worst_defense
   end
 
   def test_it_has_winningest_team
-    assert_equal "FC Dallas", @stat_tracker.winningest_team
+    assert_equal "FC Cincinnati", @stat_tracker.winningest_team
   end
 
   def test_it_has_best_fans
-    assert_equal "LA Galaxy", @stat_tracker.best_fans
+    assert_equal "FC Cincinnati", @stat_tracker.best_fans
   end
 
   def test_it_has_worst_fans
     #need to include more data so we an actually get a list of the worst teams?
-    assert_equal [], @stat_tracker.worst_fans
+    assert_equal ["FC Cincinnati"], @stat_tracker.worst_fans
   end
 
   def test_it_has_winningest_team
     # FC Dallas has only wins
-    assert_equal "FC Dallas", @stat_tracker.winningest_team
+    assert_equal "Atlanta United", @stat_tracker.winningest_team
   end
 
   def test_it_has_the_best_offense
-    assert_equal "New York City FC", @stat_tracker.best_offense
+    assert_equal "FC Cincinnati", @stat_tracker.best_offense
   end
 
   def test_it_has_best_fans
-    assert_equal "LA Galaxy", @stat_tracker.best_fans
+    assert_equal "FC Cincinnati", @stat_tracker.best_fans
   end
 
   def test_it_has_worst_fans
-    assert_equal [], @stat_tracker.worst_fans
+    assert_equal ["Chicago Fire"], @stat_tracker.worst_fans
   end
 end
