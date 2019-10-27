e725b52f3351583da776a4d7fa00649c9769fd22
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-10-27 14:22:37 -0600

    Update test to reflect new win_percentage maths

--- test/games_collection_test.rb
+++ test/games_collection_test.rb
@@ -201,10 +201,10 @@ class GamesCollectionTest < Minitest::Test
   end
 
   def test_it_can_get_total_wins_across_seasons
-    assert_equal 9, @games_collection.total_wins_across_seasons("5")
+    assert_equal 8, @games_collection.total_wins_across_seasons("5")
   end
 
   def test_it_can_calculate_average_win_percentage_across_seasons
-    assert_equal 0.45, @games_collection.average_win_percentage("5")
+    assert_equal 0.40, @games_collection.average_win_percentage("5")
   end
 end
