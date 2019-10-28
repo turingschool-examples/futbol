2aa0b335f684c2125fbfdb7c5ee323da76921aab
Becky Robran <rerobran@gmail.com>
2019-10-28 10:37:51 -0600

    Adds a file path require

--- test/team_collection_test.rb
+++ test/team_collection_test.rb
@@ -3,6 +3,8 @@ require 'csv'
 require './lib/team'
 require './lib/stat_tracker'
 require './lib/team_collection'
+require './lib/game_collection'
+require './lib/game'
 
 class TeamCollectionTest < MiniTest::Test
 
@@ -15,15 +17,31 @@ class TeamCollectionTest < MiniTest::Test
   end
 
   def test_the_count_of_teams
-    assert_equal 15, @team_instance.count_of_teams
+    assert_equal 16, @team_instance.count_of_teams
   end
-
-  def test_highest_scoring_visitor
-    assert_equal "Dallas", @team_instance.highest_scoring_visitor
-  end 
 
   def test_winningest_team_method
     assert_equal "FC Dallas", @team_instance.winningest_team("6")
     assert_equal "LA Galaxy", @team_instance.winningest_team("17")
+  end
+
+  def test_highest_scoring_visitor
+    game_collection = GameCollection.new('./dummy_data/dummy_games.csv')
+    assert_equal "FC Dallas", @team_instance.highest_scoring_visitor(game_collection.highest_visitor_id)
+  end
+
+  def test_highest_scoring_home_team
+    game_collection = GameCollection.new('./dummy_data/dummy_games.csv')
+    assert_equal "DC United", @team_instance.highest_scoring_home_team(game_collection.highest_home_id)
+  end
+
+  def test_lowest_scoring_visitor
+    game_collection = GameCollection.new('./dummy_data/dummy_games.csv')
+    assert_equal "Toronto FC", @team_instance.lowest_scoring_visitor(game_collection.lowest_visitor_id)
+  end
+
+  def test_lowest_scoring_home_team
+    game_collection = GameCollection.new('./dummy_data/dummy_games.csv')
+    assert_equal "Sporting Kansas City", @team_instance.lowest_scoring_home_team(game_collection.lowest_home_id)
   end
 end
