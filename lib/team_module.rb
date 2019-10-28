cdbc25890b8562aa86245539d68556be06d63d1d
msimon42 <msimon@uab.edu>
2019-10-28 16:50:06 -0600

    Add helper methods

--- lib/team_module.rb
+++ lib/team_module.rb
@@ -1,7 +1,7 @@
 require 'pry'
 module TeamModule
   def team_info(team)
-    team_obj = teams.select {|team_obj| team_obj.teamname == team}[0]
+    team_obj = self.convert_team_name_to_obj(team)
     team_info = {
       Name: team_obj.teamname,
       Team_id: team_obj.team_id,
@@ -12,7 +12,8 @@ module TeamModule
     team_info
   end
 
-  def best_season
+  def best_season(team)
+    self.generate_win_percentage_season(team)
 
   end
 
@@ -24,5 +25,18 @@ module TeamModule
 
   end
 
+  ##Helper Methods
+  def generate_win_percentage_season(team)
+     team_obj = self.convert_team_name_to_obj(team)
+     seasons = self.generate_seasons_hash(games)
+     games_by_team = game_teams.group_by do |game|
+       game.team_id
+     end
+     binding.pry
+  end
+
+  def convert_team_name_to_obj(team)
+    team_obj = teams.select {|team_obj| team_obj.teamname == team}[0]
+  end
 
 end
