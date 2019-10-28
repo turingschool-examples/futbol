5a3bff3bd97e20a0ba904914b8263c39f845a869
msimon42 <msimon@uab.edu>
2019-10-28 15:20:24 -0600

    Add team info method

--- lib/team_module.rb
+++ lib/team_module.rb
@@ -1,6 +1,15 @@
+require 'pry'
 module TeamModule
-  def team_info
-
+  def team_info(team)
+    team_obj = teams.select {|team_obj| team_obj.teamname == team}[0]
+    team_info = {
+      Name: team_obj.teamname,
+      Team_id: team_obj.team_id,
+      Franchise_id: team_obj.franchiseid,
+      Abbreviation: team_obj.abbreviation,
+      Link: team_obj.link
+    }
+    team_info
   end
 
   def best_season
@@ -13,7 +22,7 @@ module TeamModule
 
   def average_win_percentage
 
-  end         
+  end
 
 
 end
