e1f1bb6ec6a34e793c0caf6c35067bcaa9469926
Travis Borgsmiller <travisborgsmiller@gmail.com>
2019-10-27 18:36:10 -0600

    Update test helper to pride format

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 require 'simplecov'
 SimpleCov.start
 require 'minitest/autorun'
-require 'minitest/reporters'
-Minitest::Reporters.use!
+require 'minitest/pride'
+# require 'minitest/reporters'
+# Minitest::Reporters.use!
 require 'pry'
