37aee11259fe2e71c7f230f25fac989ed80644d7
Madelyn Romero <m.romero013@gmail.com>
2019-10-29 11:23:14 -0600

    Change name of highest_scoring_visitor test

--- test/team_collection_test.rb
+++ test/team_collection_test.rb
@@ -1,4 +1,5 @@
 require_relative 'test_helper'
+require_relative '../lib/game_collection'
 require_relative '../lib/team_collection'
 require_relative '../lib/stat_tracker'
 
@@ -10,6 +11,7 @@ class TeamCollectionTest < Minitest::Test
     assert_instance_of TeamCollection, team_collection
   end
 
-  def test_it_can_find_highest_scoring_visitor
+  def test_it_can_return_name_of_highest_scoring_visitor
+    assert_equal "Orlando City SC", @game_collection.highest_scoring_visitor
   end
 end
