3e6428ce4c6a763f4b03c49566ad5a24362b213f
msimon42 <msimon@uab.edu>
2019-10-29 14:14:44 -0600

    Add test file for season module

---
+++ test/season_test.rb
@@ -0,0 +1,10 @@
+require 'minitest'
+require 'minitest/autorun'
+require './test/test_helper'
+require './lib/stat_tracker'
+require_relative '../lib/season_module'
+
+class SeasonTest < Minitest::Test
+
+
+end
