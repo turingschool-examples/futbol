09912e73ef09fed37a3ee7ab336f8bb4d6447d93
Lain McGratH <lain.mcgrath@gusto.com>
2019-10-29 11:42:46 -0600

    Edited franchise_id to be a string, not integer

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -135,8 +135,12 @@ class StatTrackerTest < Minitest::Test
   end
 
   def test_it_can_create_team_info
-    team_information = {:team_id=>"26", :franchise_id=>14, :team_name=>"FC Cincinnati",
-                        :abbreviation=>"CIN", :link=>"/api/v1/teams/26"}
+    team_information = {"team_id"=>"26",
+                        "franchise_id" =>"14",
+                        "team_name"=>"FC Cincinnati",
+                        "abbreviation"=>"CIN",
+                        "link"=>"/api/v1/teams/26"
+                      }
     assert_equal team_information, @stat_tracker.team_info("26")
   end
 
