4a36a45cde472465b16cba165985bc59bf1f9b44
Becky Robran <rerobran@gmail.com>
2019-10-29 15:25:13 -0600

    Add return value at end of each block

--- lib/game_module.rb
+++ lib/game_module.rb
@@ -8,6 +8,8 @@ module GameModule
       @game_counter_visitor[game.away_team_id] += 1.0
       @away_goals[game.away_team_id] += game.away_goals.to_f
     end
+    @game_counter_visitor
+    @away_goals
   end
 
   def home_score_maker
@@ -18,19 +20,25 @@ module GameModule
       @game_counter_home[game.home_team_id] += 1.0
       @home_goals[game.home_team_id] += game.home_goals.to_f
     end
+    @game_counter_home
+    @home_goals
   end
 
   def id_goal_counter_away
+    visitor_score_maker
     @id_identifier = Hash.new(0)
     @away_goals.each do |key, value|
       @id_identifier[key] = (value / @game_counter_visitor[key])
     end
+      @id_identifier
   end
 
   def id_goal_counter_home
+    home_score_maker
     @id_identifier_home = Hash.new(0)
     @home_goals.each do |key, value|
       @id_identifier_home[key] = (value / @game_counter_home[key])
     end
+    @id_identifier_home
   end
 end
