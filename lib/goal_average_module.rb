c912628912b0d44bc5c3ff74dc6fd20c73d258bc
Madelyn Romero <m.romero013@gmail.com>
2019-10-29 18:13:43 -0600

    Edit team name method to snake case instead of camel case

--- lib/goal_average_module.rb
+++ lib/goal_average_module.rb
@@ -3,8 +3,7 @@ module GoalAverage
   def highest_scoring_visitor
     @teams.teams.find do |team|
     if @games.away_team_id_for_highest_average_goals.to_i == team.team_id
-      require "pry"; binding.pry
-    team.teamName
+    team.team_name
     end
     end
   end
