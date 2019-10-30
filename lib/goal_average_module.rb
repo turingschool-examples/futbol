8617ec73b5a78e0463c4239609e3503de9ccef3e
Madelyn Romero <m.romero013@gmail.com>
2019-10-30 16:01:12 -0600

    Add return for teamName

--- lib/goal_average_module.rb
+++ lib/goal_average_module.rb
@@ -3,7 +3,7 @@ module GoalAverage
   def highest_scoring_visitor
     @teams.teams.find do |team|
     if @games.away_team_id_for_highest_average_goals.to_i == team.team_id
-    team.teamName
+    return team.teamName
     end
     end
   end
