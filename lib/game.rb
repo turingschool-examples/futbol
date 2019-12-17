02df776810d2a1ce1289ab4868cc71f2657edbfc
Nathan Keller <keller.nathan@gmail.com>
2019-12-17 16:03:25 -0700

    Uopdate to add type to game

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,6 @@
 class Game
   attr_reader :game_id, :season, :away_team_id,
-              :home_team_id, :away_goals, :home_goals
+              :home_team_id, :away_goals, :home_goals, :type
 
   def initialize(game_info)
     @game_id = game_info[:game_id].to_i
@@ -9,5 +9,6 @@ class Game
     @home_team_id = game_info[:home_team_id].to_i
     @away_goals = game_info[:away_goals].to_i
     @home_goals = game_info[:home_goals].to_i
+    @type =  game_info[:type]
   end
 end
