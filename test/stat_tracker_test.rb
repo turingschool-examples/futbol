c88361a0c0e5d70862901c8f8cfff21cfcf55bca
Steven Anderson <steven.a.anderson2012@gmail.com>
2019-12-17 08:55:08 -0700

    Commit for use later

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -1,7 +1,17 @@
 require_relative 'test_helper'
 
-class NewClassTest < Minitest::Test
+class StatTrackerTest < Minitest::Test
   def setup
+
+  end
+
+  def test_a_stat_tracker_exists
+    stat_tracker = StatTracker.new('games', 'teams', 'team_games')
+
+    assert_instance_of StatTracker, stat_tracker
+  end
+
+  def test_can_create_stat_tracker_from_csv
 
   end
 end
