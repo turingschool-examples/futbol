f47f24ba99f814332cd6f112e27019e48f71d1fa
Steven Anderson <steven.a.anderson2012@gmail.com>
2019-12-18 15:42:51 -0700

    Add full function to games_collection and its test

--- test/game_collection_test.rb
+++ test/game_collection_test.rb
@@ -11,14 +11,14 @@ class GameCollectionTest < Minitest::Test
     assert_instance_of GameCollection, @collection
   end
 
-  def test_game_collection_has_teams
+  def test_game_collection_has_games
     assert_instance_of Array, @collection.games
-    assert_equal 8, @collection.teams.length
+    assert_equal 8, @collection.games.length
   end
 
   def test_game_collection_can_create_games_from_csv
     assert_instance_of Game, @game
     assert_equal 'Postseason', @game.type
-    assert_equal '2012030221', @game.game_id
+    assert_equal '20122013', @game.season
   end
 end
