27c4d42707824cdd967c1bb02044174418b24100
Nathan Keller <keller.nathan@gmail.com>
2019-12-17 19:32:27 -0700

    Create module CSVloadable. adjust game collection to include and use module

--- lib/games_collection.rb
+++ lib/games_collection.rb
@@ -1,7 +1,9 @@
 require 'csv'
 require_relative 'game'
+require_relative 'csvloadable'
 
 class GamesCollection
+  include CsvLoadable
   attr_reader :games
 
   def initialize(csv_file_path)
@@ -9,9 +11,6 @@ class GamesCollection
   end
 
   def create_games(csv_file_path)
-    csv = CSV.read("#{csv_file_path}", headers: true, header_converters: :symbol)
-    csv.map do |row|
-       Game.new(row)
-    end
+    create_instances(csv_file_path, Game)
   end
 end
