9ea15831dd91a6ea0e657cfb13864834a487e701
Maria Ronauli <mronauli@gmail.com>
2019-12-18 14:29:05 -0700

    add lowest/highest total score and biggest_blowout methods

--- lib/game_collection.rb
+++ lib/game_collection.rb
@@ -13,4 +13,33 @@ class GameCollection
 
     csv.map { |row| Game.new(row) }
   end
+
+  def highest_total_score
+    # highest_score = 0
+    # @games.each do |game|
+    #   if game.total_score > highest_score
+    #     highest_score = game.total_score
+    #     end
+    #   end
+    #   highest_score
+    highest_score = @games.max_by do |game|
+      game.total_score
+    end.total_score
+      highest_score
+
+  end
+
+  def lowest_total_score
+    lowest_score = @games.min_by do |game|
+      game.total_score
+    end.total_score
+      lowest_score
+  end
+
+  def biggest_blowout
+      games_difference = @games.max_by do |game|
+        game.difference_between_score
+    end.difference_between_score
+    games_difference
+  end
 end
