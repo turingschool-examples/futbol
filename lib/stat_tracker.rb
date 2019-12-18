71c7a65cb51a003c1c3ae785f87777e0a6e5dbab
David Atkins <atki1080@gmail.com>
2019-12-17 18:30:09 -0700

    Add stat_tracker.rb

---
+++ lib/stat_tracker.rb
@@ -0,0 +1,33 @@
+require './lib/game_collection'
+require './lib/team_collection'
+require './lib/game_teams_collection'
+
+class StatTracker
+  attr_reader :game_path, :team_path, :game_teams_path
+
+  def self.from_csv(file_paths)
+    game_path = file_paths[:games]
+    team_path = file_paths[:teams]
+    game_teams_path = file_paths[:game_teams]
+
+    StatTracker.new(game_path, team_path, game_teams_path)
+  end
+
+  def initialize(game_path, team_path, game_teams_path)
+    @game_path = game_path
+    @team_path = team_path
+    @game_teams_path = game_teams_path
+  end
+
+  def game_collection
+    GameCollection.new(@game_path)
+  end
+
+  def team_collection
+    TeamCollection.new(@team_path)
+  end
+
+  def game_teams_collection
+    GameTeamsCollection.new(@game_teams_path)
+  end
+end
