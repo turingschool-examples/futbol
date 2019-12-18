6eef329bf67a710f894e8da01875c6e118fb06f4
David Atkins <atki1080@gmail.com>
2019-12-17 21:16:23 -0700

    Add average_goals_by_season test case

--- test/game_collection_test.rb
+++ test/game_collection_test.rb
@@ -31,4 +31,15 @@ class GameCollectionTest < Minitest::Test
     #to-do: change to account for dummy test stats
     assert_equal 6, @game_collection.games_by_season.length
   end
+
+  def test_it_can_calculate_average_goals_by_season
+    #to-do: change to account for dummy test stats
+    avg_goals_by_season = @game_collection.average_goals_by_season
+    assert_equal 4.12, avg_goals_by_season["20122013"]
+    assert_equal 4.19, avg_goals_by_season["20132014"]
+    assert_equal 4.14, avg_goals_by_season["20142015"]
+    assert_equal 4.16, avg_goals_by_season["20152016"]
+    assert_equal 4.23, avg_goals_by_season["20162017"]
+    assert_equal 4.44, avg_goals_by_season["20172018"]
+  end
 end
