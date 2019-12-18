3c881720331f6f435e3473b6f7aa4b70f51d623f
Nathan Keller <keller.nathan@gmail.com>
2019-12-17 18:42:54 -0700

    Create stat tracker, gamecollection and gamecollection test. shell built for stattracker test

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,4 +1,19 @@
+require_relative 'games_collection'
+
 class StatTracker
+  attr_reader :games_path, :teams_path, :game_teams_path
 
+  def self.from_csv(file_path)
+    games_path = file_paths[:games]
+    teams_path = file_paths[:teams]
+    game_teams_path = file_paths[:game_teams]
 
+    StatTracker.new(games_path, teams_path, game_teams_path)
+  end
+
+  def initialize(games_path, teams_path, game_teams_path)
+    @games_path = games_path
+    @teams_path = teams_path
+    @game_teams_path = game_teams_path
+  end
 end
---
+++ test/games_collections_test.rb
@@ -0,0 +1,26 @@
+require_relative 'testhelper'
+require_relative '../lib/games_collection'
+
+class GamesCollectionTest < Minitest::Test
+
+  def setup
+    @gamescollection = GamesCollection.new("./test/fixtures/games_trunc.csv")
+    @game = @gamescollection.games.first
+  end
+
+  def test_it_exists
+    assert_instance_of GamesCollection, @gamescollection
+  end
+
+  def test_attributes
+    assert_equal Array, @gamescollection.games.class
+    assert_equal 11, @gamescollection.games.length
+  end
+
+  def test_it_can_create_games_from_csv
+    assert_instance_of Game, @game
+    assert_equal 2012030224, @game.game_id
+    assert_equal 3, @game.away_goals
+    assert_equal "Postseason", @game.type
+  end
+end
