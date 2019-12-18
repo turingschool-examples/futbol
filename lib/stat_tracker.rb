73f2382bdc10552e42420d578753460687caf0f8
Jordan <jordan.wallace.williams@gmail.com>
2019-12-18 16:40:43 -0700

    Update stat tracker class and associated tests

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,19 +1,5 @@
 require 'CSV'
 
 class StatTracker
-  attr_reader :games, :teams, :game_teams
-
-  def self.from_csv(locations)
-    games = CSV.read(locations[:games], headers: true, header_converters: :symbol)
-    teams = CSV.read(locations[:teams], headers: true, header_converters: :symbol)
-    game_teams = CSV.read(locations[:game_teams], headers: true, header_converters: :symbol)
-
-    StatTracker.new(games, teams, game_teams)
-  end
-
-  def initialize(games, teams, game_teams)
-    @games = games
-    @teams = teams
-    @game_teams = game_teams
-  end
+  
 end
