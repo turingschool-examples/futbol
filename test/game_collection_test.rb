d47b861243ae23c9bc0b7cd3ac8285798b4fc69c
David Atkins <atki1080@gmail.com>
2019-12-18 14:50:42 -0700

    Update tests to work with truncated data

--- test/game_collection_test.rb
+++ test/game_collection_test.rb
@@ -23,23 +23,21 @@ class GameCollectionTest < Minitest::Test
   end
 
   def test_it_can_calculate_average_goals_per_game
-    expected = 4.22 #to-do: set up dummy test b/c idk if this is accurate
+    expected = 4.31 #to-do: set up dummy test b/c idk if this is accurate
     assert_equal  expected, @game_collection.average_goals_per_game
   end
 
   def test_it_can_store_games_by_season
     #to-do: change to account for dummy test stats
-    assert_equal 6, @game_collection.games_by_season.length
+    assert_equal 4, @game_collection.games_by_season.length
   end
 
   def test_it_can_calculate_average_goals_by_season
     #to-do: change to account for dummy test stats
     avg_goals_by_season = @game_collection.average_goals_by_season
-    assert_equal 4.12, avg_goals_by_season["20122013"]
-    assert_equal 4.19, avg_goals_by_season["20132014"]
-    assert_equal 4.14, avg_goals_by_season["20142015"]
-    assert_equal 4.16, avg_goals_by_season["20152016"]
-    assert_equal 4.23, avg_goals_by_season["20162017"]
-    assert_equal 4.44, avg_goals_by_season["20172018"]
+    assert_equal 4.43, avg_goals_by_season["20132014"]
+    assert_equal 3.43, avg_goals_by_season["20142015"]
+    assert_equal 4.64, avg_goals_by_season["20152016"]
+    assert_equal 4.8, avg_goals_by_season["20162017"]
   end
 end
