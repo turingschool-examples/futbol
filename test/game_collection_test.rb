cea14f8adc1678d47fd503ce5918b1b815293383
David Atkins <atki1080@gmail.com>
2019-12-17 21:00:36 -0700

    Add average_goals_per_game test case

--- test/game_collection_test.rb
+++ test/game_collection_test.rb
@@ -21,4 +21,9 @@ class GameCollectionTest < Minitest::Test
     assert_equal 2, @game.away_goals
     assert_equal 3, @game.home_goals
   end
+
+  def test_it_can_calculate_average_goals_per_game
+    expected = 4.22 #to-do: set up dummy test b/c idk if this is accurate
+    assert_equal  expected, @game_collection.average_goals_per_game
+  end
 end
