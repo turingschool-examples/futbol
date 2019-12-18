b6145a6bf61aadd96c68976220d6e821e50fca97
Tyler Tomlinson <twtomlinson89@gmail.com>
2019-12-18 15:16:11 -0700

    Fixed syntax to pass rake test

--- lib/game_collection.rb
+++ lib/game_collection.rb
@@ -34,26 +34,26 @@ class GameCollection
       game.home_goals == game.away_goals
     end
     (ties_count.to_f / @games.length).round(2)
+  end
 
   def highest_total_score
     highest_score = @games.max_by do |game|
       game.total_score
     end.total_score
-      highest_score
+    highest_score
   end
 
   def lowest_total_score
     lowest_score = @games.min_by do |game|
       game.total_score
     end.total_score
-      lowest_score
+    lowest_score
   end
 
   def biggest_blowout
-      games_difference = @games.max_by do |game|
-        game.difference_between_score
+    games_difference = @games.max_by do |game|
+      game.difference_between_score
     end.difference_between_score
     games_difference
   end
-  end 
 end
