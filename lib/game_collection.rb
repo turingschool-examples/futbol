daf351f7647b33d3ea9b8e2d3c881e419b32934e
David Atkins <atki1080@gmail.com>
2019-12-17 21:00:53 -0700

    Add average_goals_per_game method

--- lib/game_collection.rb
+++ lib/game_collection.rb
@@ -13,4 +13,13 @@ class GameCollection
 
     csv.map { |row| Game.new(row) }
   end
+
+  def average_goals_per_game
+    total_goals = @games.reduce(0) do |sum, game|
+      sum += game.away_goals
+      sum += game.home_goals
+      sum
+    end
+    (total_goals.to_f / @games.length).round(2)
+  end
 end
