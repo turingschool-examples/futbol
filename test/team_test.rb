6d6e2183a51c9562085fb346a85200699620272e
Jordan Sewell <jrsewell400@gmail.com>
2019-12-17 17:58:29 -0700

    Setup team class and test file, also created game and game_team files and tests

---
+++ test/team_test.rb
@@ -0,0 +1,26 @@
+require 'minitest/pride'
+require 'minitest/autorun'
+require './lib/team'
+# require_relative '../data/dummy_team'
+
+class TeamTest < Minitest::Test
+
+  def setup
+    @team = Team.from_csv('./data/dummy_team.csv')
+
+  end
+
+  def test_it_exists
+    assert_instance_of Team, @team.first
+  end
+
+  def test_it_has_attributes
+    assert_equal 1, @team.first.team_id
+    assert_equal 23, @team.first.franchiseid
+    assert_equal "Atlanta United", @team.first.teamname
+    assert_equal "ATL", @team.first.abbreviation
+    assert_equal "Mercedes-Benz Stadium", @team.first.stadium
+    assert_equal "/api/v1/teams/1", @team.first.link
+  end
+
+end
