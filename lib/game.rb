4ccc326b08ccb40e7750de2b875ba4e6cef233ae
Pierce A (Him/He, BE) <56050969+palworth@users.noreply.github.com>
2019-12-18 15:08:59 -0700

    Revert "Pa/iteration 2 methods"

--- lib/game.rb
+++ lib/game.rb
@@ -8,19 +8,19 @@ class Game
               :away_goals,
               :home_goals,
               :venue
-  def initialize(game_info)
-    @game_id = game_info[:game_id]
-    @season = game_info[:season]
-    @type = game_info[:type]
-    @date_time = game_info[:date_time]
-    @away_team_id = game_info[:away_team_id].to_i
-    @home_team_id = game_info[:home_team_id].to_i
-    @away_goals = game_info[:away_goals].to_i
-    @home_goals = game_info[:home_goals].to_i
-    @venue = game_info[:venue]
-  end
+def initialize(game_info)
+  @game_id = game_info[:game_id]
+  @season = game_info[:season]
+  @type = game_info[:type]
+  @date_time = game_info[:date_time]
+  @away_team_id = game_info[:away_team_id].to_i
+  @home_team_id = game_info[:home_team_id].to_i
+  @away_goals = game_info[:away_goals].to_i
+  @home_goals = game_info[:home_goals].to_i
+  @venue = game_info[:venue]
 
-  def games_per_season
-require "pry"; binding.pry
-  end 
+
+
+end
+
 end
