14b3f6526148c656c7ba4e0f196f3135ba44a416
Ben Fox <benfox1216@gmail.com>
2019-12-18 14:19:40 -0700

    Add count_of_games_by_season method to Game class, w/ tests working

--- lib/game.rb
+++ lib/game.rb
@@ -17,19 +17,42 @@ class Game
 attr_reader :game_id, :season, :type, :date_time, :away_team_id, :home_team_id, :away_goals, :home_goals, :venue
 
   def initialize(game_info)
-    @game_id = game_info[:game_id].to_i
-    @season = game_info[:season].to_i
+    @game_id = game_info[:game_id]
+    @season = game_info[:season]
     @type = game_info[:type]
     @date_time = game_info[:date_time]
-    @away_team_id = game_info[:away_team_id].to_i
-    @home_team_id = game_info[:home_team_id].to_i
+    @away_team_id = game_info[:away_team_id]
+    @home_team_id = game_info[:home_team_id]
     @away_goals = game_info[:away_goals].to_i
     @home_goals = game_info[:home_goals].to_i
     @venue = game_info[:venue]
   end
   
+  def self.count_of_games_by_season
+    
+    season_games = {}
+    number_of_games = 0
+    
+    all_games_sorted = @@all_games.sort_by(&:season)
+    
+    all_games_sorted.each do |game|
+      if season_games.keys.include?(game.season) == false
+        number_of_games = 0
+        season_games[game.season] = (number_of_games += 1)
+      else
+        season_games[game.season] = (number_of_games += 1)
+      end
+    end
+    
+    season_games
+  end
+  
   def average_goals_by_season
     # Need to figure out how to add all away_goals and home_goals for each season, then divide that by number of games in that season. Finally, turn that into a hash with seasons as keys, and the averages as a matching value.
+    seasons = @@all_games.map do |game|
+      game.season
+    end.uniq
+        
     total_goals_per_game = @@all_games.map do |game|
       game.home_goals + game.away_goals
     end
