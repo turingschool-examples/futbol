4e135cb04819b4d7561a69e165639b75a48a7a44
Maria Ronauli <mronauli@gmail.com>
2019-12-18 20:22:51 -0700

    add team_id method and attribute

--- lib/game_teams.rb
+++ lib/game_teams.rb
@@ -1,7 +1,8 @@
 class GameTeams
-  attr_reader :hoa, :result
+  attr_reader :team_id, :hoa, :result
 
   def initialize(game_teams_info)
+    @team_id = game_teams_info[:team_id]
     @hoa = game_teams_info[:hoa]
     @result = game_teams_info[:result]
   end
