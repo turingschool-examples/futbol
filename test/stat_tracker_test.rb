36b860ad69a7c4a16bda4e52812aca80e4325fd7
Mariana Cid <cidmariana5@gmail.com>
2019-12-19 16:24:41 -0700

    Update stat tracker argument from csv file paths to hash containing file paths.

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -4,7 +4,7 @@ require_relative '../lib/stat_tracker'
 class StatTrackerTest < Minitest::Test
 
   def setup
-    @stat_tracker = StatTracker.new("./test/fixtures/games_trunc.csv", "./test/fixtures/teams_trunc.csv", "./test/fixtures/game_teams_trunc.csv")
+    @stat_tracker = StatTracker.from_csv({ :games => "./test/fixtures/games_trunc.csv", :teams => "./test/fixtures/teams_trunc.csv", :game_teams => "./test/fixtures/game_teams_trunc.csv"})
   end
 
   def test_it_exists
