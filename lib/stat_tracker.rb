3d984f3a9773344be9ddad63e63104f5d11744fa
Jordan <jordan.wallace.williams@gmail.com>
2019-12-18 19:46:17 -0700

    Add biggest blowout method and associated tests

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,11 +1,29 @@
-require 'CSV'
+require 'csv'
 
 class StatTracker
-  attr_reader :games, :teams, :game_teams
+  attr_reader :games_collection, :teams_collection, :game_teams_collection
 
   def initialize(games, teams)
-    @games = games
-    @teams = teams
+    @games_collection = games
+    @teams_collection = teams
     # @game_team = game_team
+  end
+
+  def biggest_blowout
+    blowout = {}
+    @games_collection.games.find_all do |game|
+      margin = (game.home_goals.to_i - game.away_goals.to_i).abs
+      if blowout.empty?
+        blowout[game] = margin
+      elsif margin > blowout.values[0]
+        blowout.clear
+        blowout[game] = margin
+      end
+    end
+    blowout.values.last
+  end
+
+  def count_of_games_by_season
+    
   end
 end
