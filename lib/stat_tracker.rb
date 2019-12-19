36f35f72c278c5e161937e60a4f78343bd7e8baa
Jordan <jordan.wallace.williams@gmail.com>
2019-12-18 17:05:18 -0700

    Add stat tracker framework

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,5 +1,11 @@
 require 'CSV'
 
 class StatTracker
-  
+  attr_reader :games, :teams, :game_teams
+
+  def initialize(games, teams)
+    @games = games
+    @teams = teams
+    # @game_team = game_team
+  end
 end
--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -9,15 +9,25 @@ require './lib/game_team'
 require './lib/game_collection'
 require './lib/team_collection'
 require './lib/game_team_collection'
-require 'csv'
 
 class StatTrackerTest < Minitest::Test
   def setup
-    @new_tracker = StatTracker.new()
+    game_path = './test/fixtures/games_truncated.csv'
+    team_path = './test/fixtures/teams_truncated.csv'
+    # game_team_path = './test/fixtures/game_teams_truncated.csv'
+    games = GameCollection.new(game_path)
+    teams = TeamCollection.new(team_path)
+    # game_team = GameTeamCollection.new(game_team_path)
+    @new_tracker = StatTracker.new(games, teams)
   end
 
   def test_stat_tracker_exists
     assert_instance_of StatTracker, @new_tracker
   end
 
+  def test_that_data_can_be_passed_to_stat_tracker_attributes
+    assert_instance_of GameCollection, @new_tracker.games
+    assert_instance_of TeamCollection, @new_tracker.teams
+    # assert_instance_of GameCollection, @new_tracker.games
+  end
 end
