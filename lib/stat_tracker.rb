666988a07b5520106ddc3d8f020e25ef6917508c
Jordan Williams <iEv0lv3@users.noreply.github.com>
2019-12-19 08:58:36 -0700

    Update instance variable names
    Updated variable names and attr_readers to prepare for other merges.

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,22 +1,23 @@
 require 'csv'
 
 class StatTracker
+  attr_reader :games_collection, :teams_collection, :game_teams_collection
 
-  def initialize(game_collection, team_collection)
-    @game_collection = game_collection
-    @team_collection = team_collection
+  def initialize(games_collection, teams_collection)
+    @games_collection = games_collection
+    @teams_collection = teams_collection
     # @game_team = game_team
   end
 
   def highest_total_score
-    total_scores = @game_collection.games.map do |game|
+    total_scores = @games_collection.games.map do |game|
       game.away_goals.to_i + game.home_goals.to_i
     end
     total_scores.max
   end
 
   def lowest_total_score
-    total_scores = @game_collection.games.map do |game|
+    total_scores = @games_collection.games.map do |game|
       game.away_goals.to_i + game.home_goals.to_i
     end
     total_scores.min
