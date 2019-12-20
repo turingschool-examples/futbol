02b8c0b522a51c50ba478086ee04949b2be14357
Steven Anderson <steven.a.anderson2012@gmail.com>
2019-12-20 09:01:54 -0700

    Add game_team pointer to collection super class

--- lib/game_team_collection.rb
+++ lib/game_team_collection.rb
@@ -1,20 +1,9 @@
 require 'csv'
 require_relative 'game_team'
+require_relative 'collection'
 
-class GameTeamCollection
-  attr_accessor :game_teams
-
+class GameTeamCollection < Collection
   def initialize(csv_file_path)
-    @game_teams = create_game_teams(csv_file_path)
-  end
-
-  def from_csv(csv_file_path)
-    CSV.read(csv_file_path, headers: true, header_converters: :symbol)
-  end
-
-  def create_game_teams(csv_file_path)
-    from_csv(csv_file_path).map do |row|
-      GameTeam.new(row)
-    end
+    super(csv_file_path, GameTeam)
   end
 end
