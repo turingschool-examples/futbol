5b76eb60cbd4526e8e5176bc32df2a57ddec472f
Jordan <jordan.wallace.williams@gmail.com>
2019-12-19 20:15:08 -0700

    Add collection super class

---
+++ lib/collection.rb
@@ -0,0 +1,18 @@
+class Collection
+  attr_reader :collection
+
+  def initialize(csv_file_path, collection_type)
+    @collection = create_collection(csv_file_path, collection_type)
+  end
+
+  def from_csv(csv_file_path)
+    CSV.read(csv_file_path, headers: true, header_converters: :symbol)
+  end
+
+  def create_collection(csv_file_path, collection_type)
+    from_csv(csv_file_path).reduce({}) do |hash, row|
+      hash[row.first.last] = collection_type.new(row)
+      hash
+    end
+  end
+end
