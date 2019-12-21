606e06929ce53db31faaae44f743033059fcc2f0
Jordan <jordan.wallace.williams@gmail.com>
2019-12-21 14:52:47 -0700

    Update all methods to fully support new super class, 93% coverage

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -19,21 +19,20 @@ class StatTracker
 
   def average_goals_per_game
     sum = 0
-    @games_collection.games.each do |game|
-      sum += (game.away_goals.to_i + game.home_goals.to_i)
+    @games_collection.collection.each do |game|
+      sum += (game[1].away_goals.to_i + game[1].home_goals.to_i)
     end
-    (sum.to_f / @games_collection.games.length).round(2)
+    (sum.to_f / @games_collection.collection.length).round(2)
   end
 
   def average_goals_by_season
     sums = {}
     averages = {}
-
-    @games_collection.games.each do |game|
-      if !sums.key?(game.season)
-        sums[game.season] = (game.home_goals.to_i + game.away_goals.to_i)
+    @games_collection.collection.each do |game|
+      if !sums.key?(game[1].season)
+        sums[game[1].season] = (game[1].home_goals.to_i + game[1].away_goals.to_i)
       else
-        sums[game.season] += (game.home_goals.to_i + game.away_goals.to_i)
+        sums[game[1].season] += (game[1].home_goals.to_i + game[1].away_goals.to_i)
       end
     end
 
@@ -45,15 +44,15 @@ class StatTracker
   end
 
   def highest_total_score
-    total_scores = @games_collection.games.map do |game|
-      game.away_goals.to_i + game.home_goals.to_i
+    total_scores = @games_collection.collection.map do |game|
+      game[1].away_goals.to_i + game[1].home_goals.to_i
     end
     total_scores.max
   end
 
   def lowest_total_score
-    total_scores = @games_collection.games.map do |game|
-      game.away_goals.to_i + game.home_goals.to_i
+    total_scores = @games_collection.collection.map do |game|
+      game[1].away_goals.to_i + game[1].home_goals.to_i
     end
     total_scores.min
   end
@@ -82,18 +81,18 @@ class StatTracker
 
   def percentage_ties
     ties_sum = 0.0
-    @games_collection.games.each do |game|
-      ties_sum += 1 if game.home_goals == game.away_goals
+    @games_collection.collection.each do |game|
+      ties_sum += 1 if game[1].home_goals == game[1].away_goals
     end
-    (ties_sum / @games_collection.games.length).round(2)
+    (ties_sum / @games_collection.collection.length).round(2)
   end
 
   def percentage_home_wins
     home_wins = 0
-    total_games = @games_collection.games.length
+    total_games = @games_collection.collection.length
 
-    @games_collection.games.each do |game|
-      if game.home_goals.to_i > game.away_goals.to_i
+    @games_collection.collection.each do |game|
+      if game[1].home_goals.to_i > game[1].away_goals.to_i
         home_wins += 1
       end
     end
@@ -102,10 +101,10 @@ class StatTracker
 
   def percentage_visitor_wins
     visitor_wins = 0
-    total_games = @games_collection.games.length
+    total_games = @games_collection.collection.length
 
-    @games_collection.games.each do |game|       
-      if game.home_goals.to_i < game.away_goals.to_i
+    @games_collection.collection.each do |game|       
+      if game[1].home_goals.to_i < game[1].away_goals.to_i
         visitor_wins += 1
       end
     end
