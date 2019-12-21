bb7cc63ad7bb1392c7fb24717b1a327b49e6a159
Jordan <jordan.wallace.williams@gmail.com>
2019-12-21 12:36:02 -0700

    Update test setup, plus biggest blowout and count of games by season tests

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -3,16 +3,17 @@ require 'csv'
 require './lib/stat_tracker'
 require './lib/game'
 require './lib/team'
-require './lib/game_team'
+require './lib/game_teams'
+require './lib/collection'
 require './lib/game_collection'
 require './lib/team_collection'
 require './lib/game_teams_collection'
 
 class StatTrackerTest < Minitest::Test
   def setup
-    game_path = './data/fixtures/games_truncated.csv'
-    team_path = './data/fixtures/teams_truncated.csv'
-    game_teams_path = './data/fixtures/game_teams_truncated.csv'
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
 
     locations = {
                   games: game_path,
@@ -20,25 +21,25 @@ class StatTrackerTest < Minitest::Test
                   game_teams: game_teams_path
     }
 
-    @new_tracker = StatTracker.from_csv(locations)
+    @stat_tracker = StatTracker.from_csv(locations)
   end
 
   def test_stat_tracker_exists
     skip
-    assert_instance_of StatTracker, @new_tracker
+    assert_instance_of StatTracker, @stat_tracker
   end
 
   def test_that_data_can_be_passed_to_stat_tracker_attributes
     skip
-    assert_instance_of GameCollection, @new_tracker.games_collection
-    assert_instance_of TeamCollection, @new_tracker.teams_collection
-    # assert_instance_of GameCollection, @new_tracker.games
+    assert_instance_of GameCollection, @stat_tracker.games_collection
+    assert_instance_of TeamCollection, @stat_tracker.teams_collection
+    # assert_instance_of GameCollection, @stat_tracker.games
   end
 
   def test_stat_tracker_average_goals_per_game
     skip
-    assert_instance_of Float, @new_tracker.average_goals_per_game
-    assert_equal 4.15, @new_tracker.average_goals_per_game
+    assert_instance_of Float, @stat_tracker.average_goals_per_game
+    assert_equal 4.15, @stat_tracker.average_goals_per_game
   end
 
   def test_stat_tracker_average_goals_by_season
@@ -52,62 +53,61 @@ class StatTrackerTest < Minitest::Test
       "20132014"=>4.27
     }
 
-    assert_equal average_hash, @new_tracker.average_goals_by_season
+    assert_equal average_hash, @stat_tracker.average_goals_by_season
   end
 
   def test_highest_total_score
     skip
-    assert_instance_of Integer, @new_tracker.highest_total_score
-    assert_equal 10, @new_tracker.highest_total_score
+    assert_instance_of Integer, @stat_tracker.highest_total_score
+    assert_equal 10, @stat_tracker.highest_total_score
   end
 
   def test_lowest_total_score
     skip
-    assert_instance_of Integer, @new_tracker.lowest_total_score
-    assert_equal 1, @new_tracker.lowest_total_score
+    assert_instance_of Integer, @stat_tracker.lowest_total_score
+    assert_equal 1, @stat_tracker.lowest_total_score
   end
 
   def test_biggest_blowout_method_can_look_at_game_scores
-    skip
-    assert_equal 5, @new_tracker.biggest_blowout
+    assert_equal 8, @stat_tracker.biggest_blowout
   end
 
   def test_count_of_games_by_season
     count_game_hash = {
-      "20122013"=>33, 
-      "20142015"=>49, 
-      "20152016"=>90, 
-      "20162017"=>73, 
-      "20172018"=>68, 
-      "20132014"=>52
+      "20122013"=>806,
+      "20132014"=>1323,
+      "20142015"=>1319,
+      "20152016"=>1321,
+      "20162017"=>1317, 
+      "20172018"=>1355, 
     }
-    assert_equal count_game_hash, @new_tracker.count_of_games_by_season
+    assert_equal count_game_hash, @stat_tracker.count_of_games_by_season
   end
 
   def test_percentage_ties_method
     skip
-    assert_instance_of Float, @new_tracker.percentage_ties
-    assert_equal 0.21, @new_tracker.percentage_ties
+    assert_instance_of Float, @stat_tracker.percentage_ties
+    assert_equal 0.21, @stat_tracker.percentage_ties
   end
 
   def test_percentage_home_wins
     skip
-    team_1_home = @new_tracker.percentage_home_wins
+    team_1_home = @stat_tracker.percentage_home_wins
     assert_instance_of Float, team_1_home
     assert_equal 0.4, team_1_home
 
-    team_2_home = @new_tracker.percentage_home_wins
+    team_2_home = @stat_tracker.percentage_home_wins
     assert_instance_of Float, team_2_home
     assert_equal 0.4, team_2_home
   end
 
   def test_percentage_visitor_wins
     skip
-    team_1_visitor = @new_tracker.percentage_visitor_wins
+    team_1_visitor = @stat_tracker.percentage_visitor_wins
     assert_instance_of Float, team_1_visitor
     assert_equal 0.39, team_1_visitor
 
-    team_2_visitor = @new_tracker.percentage_visitor_wins
+    team_2_visitor = @stat_tracker.percentage_visitor_wins
     assert_instance_of Float, team_2_visitor
     assert_equal 0.39, team_2_visitor
   end
