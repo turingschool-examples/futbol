61a737655888e018bedf4aea7219e5e7e61a5967
Jordan <jordan.wallace.williams@gmail.com>
2019-12-21 12:34:11 -0700

    Update filename to match naming conventions and add logic for use with collection

--- lib/game_teams_collection.rb
+++ lib/game_teams_collection.rb
@@ -1,17 +1,8 @@
-# frozen_string_literal: true
-
+require_relative 'game_teams'
 require 'csv'
 
-class GameTeamCollection
-  attr_accessor :game_teams
-  attr_reader :game_teams_file_path
-
-  def initialize
-    @game_teams = nil
-    @game_teams_file_path = './data/game_teams.csv'
-  end
-
-  def from_csv
-    @game_teams = CSV.read(@game_teams_file_path, headers: true, header_converters: :symbol)
+class GameTeamCollection < Collection
+  def initialize(csv_file_path)
+    super(csv_file_path, GameTeams)
   end
 end
