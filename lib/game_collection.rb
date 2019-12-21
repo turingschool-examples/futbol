6f5cfc6144851941f63f870da1dd18220d03ace0
Jordan <jordan.wallace.williams@gmail.com>
2019-12-21 11:56:24 -0700

    Update game collection for use with collection super class

--- lib/game_collection.rb
+++ lib/game_collection.rb
@@ -1,21 +1,8 @@
-require_relative './game'
+require_relative 'game'
 require 'csv'
-require 'matrix'
 
-class GameCollection
-  attr_accessor :games
-
+class GameCollection < Collection
   def initialize(csv_file_path)
-    @games = create_games(csv_file_path)
-  end
-
-  def from_csv(csv_file_path)
-    CSV.read(csv_file_path, headers: true, header_converters: :symbol)
-  end
-
-  def create_games(csv_file_path)
-    from_csv(csv_file_path).map do |row|
-      Game.new(row)
-    end
+    super(csv_file_path, Game)
   end
 end
