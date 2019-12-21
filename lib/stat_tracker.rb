22cd5a989798705fbcaae8fe1f6fe661e880882d
Jordan <jordan.wallace.williams@gmail.com>
2019-12-21 15:29:26 -0700

    Add all necessary require relative files

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,4 +1,11 @@
 require 'csv'
+require_relative 'game'
+require_relative 'team'
+require_relative 'game_teams'
+require_relative 'collection'
+require_relative 'game_collection'
+require_relative 'team_collection'
+require_relative 'game_teams_collection'
 
 class StatTracker
   attr_reader :games_collection, :teams_collection, :game_teams_collection
