f5d12a74d9d8200c07aa798711e260e390cd7f3a
Jordan <jordan.wallace.williams@gmail.com>
2019-12-21 11:55:01 -0700

    Update game_team_collection to game_teams_collection

---
+++ lib/game_teams_collection.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'csv'
+
+class GameTeamCollection
+  attr_accessor :game_teams
+  attr_reader :game_teams_file_path
+
+  def initialize
+    @game_teams = nil
+    @game_teams_file_path = './data/game_teams.csv'
+  end
+
+  def from_csv
+    @game_teams = CSV.read(@game_teams_file_path, headers: true, header_converters: :symbol)
+  end
+end
