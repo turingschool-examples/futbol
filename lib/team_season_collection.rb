8ed7af5308eb4cb04f03207af8d5a1a84ee3b5a8
Jordan <jordan.wallace.williams@gmail.com>
2019-12-23 17:43:45 -0700

    Update create collection logic to parse data

--- lib/team_season_collection.rb
+++ lib/team_season_collection.rb
@@ -11,14 +11,16 @@ class TeamSeasonCollection < Collection
   end
 
   def create_collection(csv_file_path, collection_type)
-    from_csv(csv_file_path).reduce(Hash.new({})) do |hash, row|
-      season_hash = Hash.new([])
-      season_hash[row[:season]] += [collection_type.new(row)]
-
-      collection_helper_away(row, hash, season_hash)
-
-      collection_helper_home(row, hash, season_hash)
-
+    from_csv(csv_file_path).reduce(Hash.new(Hash.new([]))) do |hash, row|
+      # require 'pry'; binding.pry
+      # season_hash = { row[:home_team_id] => { row[:season] => ([] << collection_type.new(row)) } }
+      hash[row[:home_team_id]] = { row[:season] => (hash[row[:home_team_id]][row[:season]] += [collection_type.new(row)]) }
+      hash[row[:away_team_id]] = { row[:season] => (hash[row[:away_team_id]][row[:season]] += [collection_type.new(row)]) }
+      # nest1 = season_hash.dig(row[:home_team_id])
+      # nest2 = season_hash.dig(row[:home_team_id], row[:season])
+      # require 'pry'; binding.pry
+      # hash.merge!(season_hash){ |k, old_val, new_val| hash[k] = nest1.keys[0] = old_val.append(new_val) }
+      # require 'pry'; binding.pry
       hash
     end
   end
