91f8150d3026dfea1b05fcc29618f774ce01041f
Jordan <jordan.wallace.williams@gmail.com>
2019-12-24 17:18:17 -0700

    Update all team season methods into small helper methods

--- lib/modules/gatherable.rb
+++ lib/modules/gatherable.rb
@@ -44,4 +44,41 @@ module Gatherable
 
     hash[row[:away_team_id]][row[:season]] += season_hash.values.flatten(1)
   end
+
+  def team_hash(row, team_id)
+    season_hash = { team_id => { row[:season] => [] } }
+    season_hash
+  end
+
+  def season_hash(row, collection_type, season_hash, team_id)
+    season_hash[team_id] = { row[:season] => (season_hash[team_id][row[:season]] += [collection_type.new(row)]) }
+    season_hash
+  end
+
+  def season_data_hash(row, season_hash, team_id)
+    season_hash[team_id].values.flatten!
+  end
+
+  def team_key(season_hash)
+    key = season_hash.keys[0]
+    key
+  end
+
+  def season_key(season_hash, key)
+    season_key = season_hash[key].keys[0]
+    season_key
+  end
+
+  def season_parse(key, season_key, season_hash, season_data, hash)
+    if hash.key?(key) && hash[key].key?(season_key)
+      hash[key][season_key] << season_data
+    elsif hash.key?(key) && !hash[key].key?(season_key)
+      { hash[key] => hash[key][season_key] = season_data }
+    elsif !hash.has_key?(key) && !hash.empty?
+      hash[key] = { season_key => season_data }
+    elsif hash.empty?
+      hash = { key => { season_key => season_data } }
+    end
+    hash
+  end
 end
