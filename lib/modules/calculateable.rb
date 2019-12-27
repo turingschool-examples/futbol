a6a19d34cc3f4bef37c39d08fb0f2fcb050a8f4f
Jordan <jordan.wallace.williams@gmail.com>
2019-12-27 16:39:03 -0700

    Add logic framework for new methods using team season collection

--- lib/modules/calculateable.rb
+++ lib/modules/calculateable.rb
@@ -1,4 +1,8 @@
+require_relative 'gatherable'
+
 module Calculateable
+  include Gatherable
+
   def team_average_goals(goals_hash)
     average_goals = {}
     goals_hash.each do |team, tot_score|
@@ -6,5 +10,35 @@ module Calculateable
     end
 
     average_goals
+  end
+
+  def team_total_seasons(team_id)
+    @team_season_collection.collection[team_id].size
+  end
+
+  def team_season_keys(team_id)
+    { team_id => @team_season_collection.collection[team_id].keys }
+  end
+
+  def game_goals(team_id, game)
+    require 'pry'; binding.pry
+    if team_id == game.home_team_id
+      game.home_goals
+    else
+      game.away_goals
+    end
+  end
+
+  def team_season_total_goals(team_id)
+    @team_season_collection.collection[team_id].reduce({}) do |hash, season|
+      require 'pry'; binding.pry
+      goals = season[1].flatten.map do |game|
+        game_goals(team_id, game)
+        require 'pry'; binding.pry
+      end
+      hash = { team_id = season[0] => goals }
+      require 'pry'; binding.pry
+      hash
+    end
   end
 end
