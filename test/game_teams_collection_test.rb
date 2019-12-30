8ef3d6e147cba46c484c225ac4347ff6d16d2ef9
Kevin McGrevey <niv3kmcg@gmail.com>
2019-12-30 09:40:47 -0700

    Add test for updated hoa stat

--- test/game_teams_collection_test.rb
+++ test/game_teams_collection_test.rb
@@ -16,9 +16,21 @@ class GameTeamsCollectionTest < Minitest::Test
     assert_instance_of GameTeam, @game_teams_collection.game_teams.first
     assert_equal "Claude Julien", @game_teams_collection.game_teams.first.head_coach
     assert_equal 4, @game_teams_collection.game_teams.first.goals
+    assert_equal "away", @game_teams_collection.game_teams.first.hoa
   end
 
   def test_find_id_of_winningest_team
     assert_equal 6, @game_teams_collection.winningest_team_id
+  end
+
+  def test_find_id_of_team_with_best_fans
+    skip
+    # assert_equal expected, actual
+    best_fans
+  end
+
+  def test_all_are_home_games
+    skip
+    assert_equal expected, @game_teams_collection.hoa_game_sorter("home")
   end
 end
