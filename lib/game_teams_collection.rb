3903e866c047b5a28b258a454baa80f71befe801
Kevin McGrevey <niv3kmcg@gmail.com>
2019-12-30 21:48:15 -0700

    Refactor for improved modularity

--- lib/game_teams_collection.rb
+++ lib/game_teams_collection.rb
@@ -9,6 +9,7 @@ class GameTeamsCollection
 
   def initialize(game_teams_path)
     @game_teams = create_game_teams(game_teams_path)
+    all_games_by_team
   end
 
   def create_game_teams(game_teams_path)
@@ -16,36 +17,35 @@ class GameTeamsCollection
   end
 
   def winningest_team_id
-    team_records = all_games_by_team.reduce({}) do |records, games|
-      wlt_percent_calculator(games[1], "WIN")
-      records[games[0]] = @wlt_percentage
-      records
-    end
+    team_records = percent_sort_by_id(all_games_by_team)
     team_records.max_by { |team_id, percentage| percentage }[0]
   end
 
   def best_fans_team_id
-    home_games_teams = hoa_game_sorter("home")
-    away_games_teams = hoa_game_sorter("away")
+    # home_games_teams = hoa_game_sorter("home")
 
-    home_win_pcts = home_games_teams.reduce({}) do |records, games|
-      wlt_percent_calculator(games[1], "WIN")
-      records[games[0]] = @wlt_percentage
-      records
-    end
+    # home_win_pcts = home_games_teams.reduce({}) do |records, games|
+    #   wlt_percent_calculator(games[1], "WIN")
+    #   records[games[0]] = @wlt_percentage
+    #   records
+    # end
+    # home_win_pcts = percent_sort_by_id(home_games_teams)
 
-    away_win_pcts = away_games_teams.reduce({}) do |records, games|
-      wlt_percent_calculator(games[1], "WIN")
-      records[games[0]] = @wlt_percentage
-      records
-    end
+    # away_win_pcts = away_games_teams.reduce({}) do |records, games|
+    #   wlt_percent_calculator(games[1], "WIN")
+    #   records[games[0]] = @wlt_percentage
+    #   records
+    # end
+    # away_win_pcts = percent_sort_by_id(away_games_teams)
 
-    home_win_pcts.keys.max_by do |key|
-      home_win_pcts[key] - away_win_pcts[key]
+    # @home_win_pcts.keys.max_by do |key|
+    #   @home_win_pcts[key] - away_win_pcts[key]
+    # end
+    home_win_percents.keys.max_by do |key|
+      home_win_percents[key] - away_win_percents[key]
     end
   end
 
-  # module candidate?
   def wlt_percent_calculator(games_array, wlt)
     wlt_total = (games_array.find_all { |game| game.result == wlt }).length
     percentage = (wlt_total.to_f / games_array.length.to_f).round(3)
@@ -62,5 +62,23 @@ class GameTeamsCollection
       records[games[0]] = games[1].find_all {|game| game.hoa == h_a }
       records
     end
+  end
+
+  def percent_sort_by_id(to_sort)
+    to_sort.reduce({}) do |records, games|
+      wlt_percent_calculator(games[1], "WIN")
+      records[games[0]] = @wlt_percentage
+      records
+    end
+  end
+
+  def home_win_percents
+    home_games_teams = hoa_game_sorter("home")
+    percent_sort_by_id(home_games_teams)
+  end
+
+  def away_win_percents
+    away_games_teams = hoa_game_sorter("away")
+    percent_sort_by_id(away_games_teams)
   end
 end
