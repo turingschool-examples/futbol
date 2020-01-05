2a025ad2ca5134b935cdb8674054d6dd69a54613
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-01-04 18:55:24 -0700

    Added team_info method.

--- lib/team.rb
+++ lib/team.rb
@@ -5,4 +5,8 @@ attr_reader  :franchise_id, :team_name, :abbreviation, :link
   def initialize(team)
     @team = {team_id: team[:team_id], franchise_id: team[:franchise_id], team_name: team[:team_name], abbreviation: team[:abbreviation], link: team[:link]}
   end
-end 
+
+  def team_info
+    @team
+  end
+end
