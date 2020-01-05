b036200884363e37fb52a6e7aab7def0241c2cab
Maria Ronauli <mronauli@gmail.com>
2020-01-04 19:12:46 -0700

    add tests for refactored stat collecting methods into game_stats.rb

--- test/game_collection_test.rb
+++ test/game_collection_test.rb
@@ -23,10 +23,6 @@ class GameCollectionTest < Minitest::Test
     assert_equal 3, @game.home_goals
   end
 
-  def test_it_can_calculate_average_goals_per_game
-    assert_equal  4.31, @game_collection.average_goals_per_game
-  end
-
   def test_it_can_store_games_by_season
     assert_equal 4, @game_collection.game_lists_by_season.length
   end
@@ -34,37 +30,5 @@ class GameCollectionTest < Minitest::Test
   def test_games_by_season
     expected = {"20162017" => 4, "20142015" => 6, "20152016" => 10, "20132014" => 6}
     assert_equal expected, @game_collection.games_by_season
-  end
-
-  def test_it_can_calculate_average_goals_by_season
-    avg_goals_by_season = @game_collection.average_goals_by_season
-    assert_equal 4.33, avg_goals_by_season["20132014"]
-    assert_equal 3.5, avg_goals_by_season["20142015"]
-    assert_equal 4.6, avg_goals_by_season["20152016"]
-    assert_equal 4.75, avg_goals_by_season["20162017"]
-  end
-
-  def test_it_can_calculate_percentage_home_wins
-    assert_equal 0.38, @game_collection.percentage_home_wins
-  end
-
-  def test_it_can_calculate_percentage_visitor_wins
-    assert_equal 0.58, @game_collection.percentage_visitor_wins
-  end
-
-  def test_it_can_calculate_percentage_ties
-    assert_equal 0.04, @game_collection.percentage_ties
-  end
-
-  def test_it_can_get_the_sum_of_highest_winning_and_losing_team_score
-    assert_equal 7, @game_collection.highest_total_score
-  end
-
-  def test_it_can_get_the_sum_of_lowest_winning_and_losing_team_score
-    assert_equal 2, @game_collection.lowest_total_score
-  end
-
-  def test_it_can_get_biggest_blowout
-    assert_equal 3, @game_collection.biggest_blowout
   end
 end
