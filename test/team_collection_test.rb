e3bb9edd44a2f34dcc0ee4c56e6381a7ee66417f
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-01-04 18:59:56 -0700

    Added test for creating games from cvs.

--- test/team_collection_test.rb
+++ test/team_collection_test.rb
@@ -15,4 +15,10 @@ class TeamCollectionTest < Minitest::Test
   def test_it_has_attributes
     assert_instance_of Array, @teams_collection.teams_collection
   end
+
+  def test_it_can_create_games_from_csv
+    assert_instance_of Team, @teams
+    assert_equal "1", @teams.team_id
+    assert_equal "Atanta United", @teams.team_name
+  end
 end
