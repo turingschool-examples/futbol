1c3e7f91febfbf10463adfcbae5109d727a69d63
Fred Rondina <fredrondina96@gmail.com>
2020-01-06 15:14:59 -0700

    added ablility to fetch team object from integer

--- lib/modules/season_searchable.rb
+++ lib/modules/season_searchable.rb
@@ -1,3 +1,14 @@
+require 'pry'
 module SeasonSearchable
+
+  def best_season(team_id)
+    winning_season = 0
+    team = teams.find {|team| team.team_id == team_id}
+    binding.pry
+    team.stats_by_season.each do |season|
+      binding.pry
+      # stat_tracker.teams[team].stats_by_season[season][reg_season][win_percentage]
+    end
+  end
 
 end
