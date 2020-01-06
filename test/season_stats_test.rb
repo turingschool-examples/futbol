5b5891cd7c74f71addf8da26cd0a790cca8e59f5
Steve Anderson <steven.a.anderson2012@gmail.com>
2020-01-06 15:08:00 -0700

    Removed pry
    Removed an erroneous pry.

--- test/season_stats_test.rb
+++ test/season_stats_test.rb
@@ -34,7 +34,6 @@ class SeasonStatsTest < Minitest::Test
   end
 
   def test_season_stats_can_get_winningest_coach
-    require 'pry'; binding.pry
     assert_equal 'Claude Julien', @stat_tracker.winningest_coach('20132014')
     assert_equal 'Alain Vigneault', @stat_tracker.winningest_coach('20142015')
   end
