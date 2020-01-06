42886239f83118fe3e230f60b28835d70eda361d
Maria Ronauli <mronauli@gmail.com>
2020-01-06 16:40:47 -0700

    fix bugs

--- lib/team_collection.rb
+++ lib/team_collection.rb
@@ -1,20 +1,24 @@
-require_relative "team"
-require_relative "csv_loadable"
+require_relative 'team'
+require_relative 'csv_loadable'
 
 class TeamCollection
   include CsvLoadable
 
-  attr_reader :teams
+  attr_reader :teams_array
 
   def initialize(file_path)
-    @teams = create_teams(file_path)
+    @teams_array = create_teams_array(file_path)
   end
 
-  def create_games(file_path)
-    # csv = CSV.read(file_path, headers: true, header_converters: :symbol)
-    #
-    # csv.map { |row| Game.new(row) }
-
+  def create_teams_array(file_path)
     load_from_csv(file_path, Team)
+  end
+
+  def number_of_teams
+    @teams_array.length
+  end
+
+  def team_name_by_id(team_id)
+    @teams_array.find {|team| team.team_id.to_i == team_id}.team_name
   end
 end
