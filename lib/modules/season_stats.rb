5ddab92abea13f9d8dbe388fce70540967f41632
Anthony Gomes <code@tonygom.es>
2020-01-06 10:40:53 -0700

    Save work

--- lib/modules/season_stats.rb
+++ lib/modules/season_stats.rb
@@ -19,17 +19,4 @@ module SeasonStats
     get_team_name_by_id(team_id.first)
   end
 
-  def fewest_tackles(season_id)
-    team_tackles = {}
-    @games.collection.each do |game|
-      if game.last.season == season_id
-        team_tackles[game.last.away_team_id] = game.last.away_tackles.to_i
-        team_tackles[game.last.home_team_id] = game.last.home_tackles.to_i
-      end
-    end
-
-    team_id = team_tackles.min_by { |team| team.last}
-
-    get_team_name_by_id(team_id.first)
-  end
 end
