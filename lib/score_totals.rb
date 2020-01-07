71cc4547287575fa3623f472e8de5f6974860ad4
Daniel Selph <daniel.p.selph@gmail.com>
2020-01-07 10:52:13 -0700

    Setup complete for test and file

--- lib/score_totals.rb
+++ lib/score_totals.rb
@@ -1,3 +1,31 @@
+require_relative 'game'
+
 class ScoreTotals
+  @@all_games
+
+  def self.all_games
+    @@all_games
+  end
+
+  def self.from_csv(file_path)
+    csv = CSV.read("#{file_path}", headers: true, header_converters: :symbol)
+    @@all_games = csv.map do |row|
+                    Game.new(row)
+                  end
+  end
+
+attr_reader :game_id, :season, :type, :date_time, :away_team_id, :home_team_id, :away_goals, :home_goals, :venue
+
+  def initialize(game_info)
+    @game_id = game_info[:game_id]
+    @season = game_info[:season]
+    @type = game_info[:type]
+    @date_time = game_info[:date_time]
+    @away_team_id = game_info[:away_team_id]
+    @home_team_id = game_info[:home_team_id]
+    @away_goals = game_info[:away_goals].to_i
+    @home_goals = game_info[:home_goals].to_i
+    @venue = game_info[:venue]
+  end
 
 end
--- test/score_totals_test.rb
+++ test/score_totals_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/game'
-require './lib/score_totals'
+require_relative 'game'
+require_relative 'score_totals'
 
 class ScoreTotalsTest < Minitest::Test
   def setup
@@ -19,5 +19,35 @@ class ScoreTotalsTest < Minitest::Test
     @game_path = './test/dummy/games_trunc.csv'
     @games = Game.from_csv(@game_path)
     @csv_game = @games[4]
+  end
+
+  def test_it_exists
+    assert_instance_of Game, @game
+  end
+
+  def test_it_has_attributes
+    assert_instance_of Game, @game
+    assert_equal "201203022015", @game.game_id
+    assert_equal "20192020", @game.season
+    assert_equal "Preseason", @game.type
+    assert_equal "12/16/19", @game.date_time
+    assert_equal "3", @game.away_team_id
+    assert_equal "10", @game.home_team_id
+    assert_equal 4, @game.away_goals
+    assert_equal 1, @game.home_goals
+    assert_equal "Mercedes Benz Superdome", @game.venue
+  end
+
+  def test_it_reads_csv
+    assert_instance_of Game, @csv_game
+    assert_equal "2012030222", @csv_game.game_id
+    assert_equal "20122013", @csv_game.season
+    assert_equal "Postseason", @csv_game.type
+    assert_equal "5/19/13", @csv_game.date_time
+    assert_equal "3", @csv_game.away_team_id
+    assert_equal "6", @csv_game.home_team_id
+    assert_equal 2, @csv_game.away_goals
+    assert_equal 3, @csv_game.home_goals
+    assert_equal "Toyota Stadium", @csv_game.venue
   end
 end
