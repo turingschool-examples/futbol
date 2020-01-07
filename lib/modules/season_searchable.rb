a233cd17f8fb0852fc9aa50c1c8b9699ec31cf46
Fred Rondina <fredrondina96@gmail.com>
2020-01-06 18:49:24 -0700

    added worst season method

--- lib/modules/season_searchable.rb
+++ lib/modules/season_searchable.rb
@@ -2,13 +2,38 @@ require 'pry'
 module SeasonSearchable
 
   def best_season(team_id)
-    winning_season = 0
-    team = teams.find {|team| team.team_id == team_id}
+    team = teams.find {|team| team.team_id.to_s == team_id}
     binding.pry
-    team.stats_by_season.each do |season|
-      binding.pry
-      # stat_tracker.teams[team].stats_by_season[season][reg_season][win_percentage]
+    season_wins = team.stats_by_season.reduce({}) do |acc, (season, values)|
+      # binding.pry
+      if (values[:postseason][:win_percentage] > 0)
+        binding.pry
+      acc[season] = ((values[:regular_season][:win_percentage] + values[:postseason][:win_percentage]) / 2).round(2)
+      else
+        binding.pry
+      acc[season] = (values[:regular_season][:win_percentage])
+      end
+    acc
     end
+    binding.pry
+    return season_wins.max_by{|season, win_percentage| win_percentage}.first
   end
 
+  def worst_season(team_id)
+    team = teams.find {|team| team.team_id.to_s == team_id}
+    binding.pry
+    season_wins = team.stats_by_season.reduce({}) do |acc, (season, values)|
+      # binding.pry
+      if (values[:postseason][:win_percentage] > 0)
+        binding.pry
+      acc[season] = ((values[:regular_season][:win_percentage] + values[:postseason][:win_percentage]) / 2).round(2)
+      else
+        binding.pry
+      acc[season] = (values[:regular_season][:win_percentage])
+      end
+    acc
+    end
+    binding.pry
+    return season_wins.min_by{|season, win_percentage| win_percentage}.first
+  end
 end
