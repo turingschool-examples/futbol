624fd01b0309485b0f6464761754ca99bcc7d73e
Anthony Gomes <code@tonygom.es>
2020-01-07 14:26:33 -0700

    Save work

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -28,7 +28,6 @@ class StatTrackerTest < Minitest::Test
   end
 
   def test_stat_tracker_exists
-    skip
     stat_tracker = mock('StatTracker')
     stat_tracker.stubs(:klass).returns('StatTracker')
 
@@ -36,7 +35,6 @@ class StatTrackerTest < Minitest::Test
   end
 
   def test_that_collections_are_the_correct_class_type
-    skip
     stat_tracker = mock('StatTracker')
     stat_tracker.stubs(:klass).returns('KlassType')
 
@@ -56,13 +54,11 @@ class StatTrackerTest < Minitest::Test
   end
 
   def test_stat_tracker_average_goals_per_game
-    skip
     assert_instance_of Float, @stat_tracker.average_goals_per_game
     assert_equal 4.22, @stat_tracker.average_goals_per_game
   end
 
   def test_stat_tracker_average_goals_by_season
-    skip
     average_hash = {
       '20122013' => 4.12,
       '20132014' => 4.19,
@@ -76,26 +72,22 @@ class StatTrackerTest < Minitest::Test
   end
 
   def test_highest_total_score
-    skip
     assert_instance_of Integer, @stat_tracker.highest_total_score
     assert_equal 11, @stat_tracker.highest_total_score
   end
 
   def test_lowest_total_score
-    skip
     assert_instance_of Integer, @stat_tracker.lowest_total_score
     assert_equal 0, @stat_tracker.lowest_total_score
   end
 
   def test_biggest_blowout_margin
-    skip
     stat_tracker = mock('StatTracker')
     stat_tracker.stubs(:biggest_blowout).returns(8)
     assert_equal 8, stat_tracker.biggest_blowout
   end
 
   def test_count_of_games_by_season
-    skip
     stat_tracker = mock('StatTracker')
     stat_tracker.stubs(:count_of_games_by_season).returns(
       '20122013' => 806,
@@ -109,7 +101,6 @@ class StatTrackerTest < Minitest::Test
   end
 
   def test_percentage_ties_method
-    skip
     assert_instance_of Float, @stat_tracker.percentage_ties
     assert_equal 0.2, @stat_tracker.percentage_ties
   end
@@ -127,109 +118,76 @@ class StatTrackerTest < Minitest::Test
   end
 
   def test_stat_tracker_gets_count_of_teams
-    skip
     assert_equal 32, @stat_tracker.count_of_teams
   end
 
   def test_stat_tracker_can_get_best_offense
-    skip
     assert_instance_of String, @stat_tracker.best_offense
     assert_equal 'Reign FC', @stat_tracker.best_offense
   end
 
   def test_stat_tracker_can_get_worst_offense
-    skip
     assert_instance_of String, @stat_tracker.worst_offense
     assert_equal 'Utah Royals FC', @stat_tracker.worst_offense
   end
 
   def test_stat_tracker_can_get_best_defense
-    skip
     assert_instance_of String, @stat_tracker.best_defense
     assert_equal 'FC Cincinnati', @stat_tracker.best_defense
   end
 
   def test_stat_tracker_can_get_worst_defense
-    skip
     assert_instance_of String, @stat_tracker.worst_defense
     assert_equal 'Columbus Crew SC', @stat_tracker.worst_defense
   end
 
   def test_stat_tracker_can_get_highest_scoring_visitor
-    skip
     assert_instance_of String,  @stat_tracker.highest_scoring_visitor
     assert_equal 'FC Dallas', @stat_tracker.highest_scoring_visitor
   end
 
   def test_stat_tracker_can_get_highest_scoring_home
-    skip
     assert_instance_of String, @stat_tracker.highest_scoring_home_team
     assert_equal 'Reign FC', @stat_tracker.highest_scoring_home_team
   end
 
   def test_stat_tracker_can_get_lowest_scoring_visitor
-    skip
     assert_instance_of String, @stat_tracker.lowest_scoring_visitor
     assert_equal 'San Jose Earthquakes', @stat_tracker.lowest_scoring_visitor
   end
 
   def test_stat_tracker_can_get_lowest_scoring_home
-    skip
     assert_instance_of String, @stat_tracker.lowest_scoring_home_team
     assert_equal 'Utah Royals FC', @stat_tracker.lowest_scoring_home_team
   end
 
   def test_stat_tracker_can_get_winningest_team
-    skip
     assert_instance_of String, @stat_tracker.winningest_team
     assert_equal 'Reign FC', @stat_tracker.winningest_team
   end
 
   def test_stat_tracker_can_get_best_fans
-    skip
     assert_instance_of String, @stat_tracker.best_fans
     assert_equal 'San Jose Earthquakes', @stat_tracker.best_fans
   end
 
   def test_stat_tracker_can_get_worst_fans
-    skip
     assert_equal ['Houston Dynamo', 'Utah Royals FC'], @stat_tracker.worst_fans
   end
 
   def test_stat_tracker_can_get_most_goals_scored
-    skip
-    assert_instance_of Integer, @stat_tracker.most_goals_scored('6')
-    assert_equal 6, @stat_tracker.most_goals_scored('6')
-
-    assert_instance_of Integer, @stat_tracker.most_goals_scored('3')
-    assert_equal 6, @stat_tracker.most_goals_scored('3')
-
-    assert_instance_of Integer, @stat_tracker.most_goals_scored('17')
-    assert_equal 6, @stat_tracker.most_goals_scored('17')
+    assert_instance_of Integer, @stat_tracker.most_goals_scored('18')
+    assert_equal 7, @stat_tracker.most_goals_scored('18')
   end
 
   def test_stat_tracker_can_get_fewest_goals_scored
-    skip
-    assert_instance_of Integer, @stat_tracker.fewest_goals_scored('6')
-    assert_equal 0, @stat_tracker.fewest_goals_scored('6')
-
-    assert_instance_of Integer, @stat_tracker.fewest_goals_scored('3')
-    assert_equal 0, @stat_tracker.fewest_goals_scored('3')
-
-    assert_instance_of Integer, @stat_tracker.fewest_goals_scored('17')
-    assert_equal 0, @stat_tracker.fewest_goals_scored('17')
+    assert_instance_of Integer, @stat_tracker.fewest_goals_scored('18')
+    assert_equal 0, @stat_tracker.fewest_goals_scored('18')
   end
 
   def test_stat_tracker_can_get_worst_loss
-    skip
-    assert_instance_of Integer, @stat_tracker.worst_loss('3')
-    assert_equal 5, @stat_tracker.worst_loss('3')
-
-    assert_instance_of Integer, @stat_tracker.worst_loss('6')
-    assert_equal 5, @stat_tracker.worst_loss('6')
-
-    assert_instance_of Integer, @stat_tracker.worst_loss('17')
-    assert_equal 7, @stat_tracker.worst_loss('17')   
+    assert_instance_of Integer, @stat_tracker.worst_loss('18')
+    assert_equal 4, @stat_tracker.worst_loss('18')   
   end
 
   def test_stat_tracker_can_get_most_tackles
