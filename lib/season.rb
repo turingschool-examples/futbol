a6244b6b63fc9f71870f0aa428574cec7a6549f8
Kelly Bard <kellyibard@gmail.com>
2020-01-07 10:54:14 -0700

    working on iteration 4

--- lib/season.rb
+++ lib/season.rb
@@ -1,8 +1,9 @@
 require 'csv'
 require_relative './game'
+require 'pry'
 
 class Season < Game
-  attr_reader :hoa, :result
+  attr_reader :hoa, :result, :season, :team_id, :goals, :shots, :game_id
 
   @@all_seasons = []
 
@@ -17,13 +18,14 @@ class Season < Game
   end
 
   def initialize(league_info)
-    @game_id = league_info[:game_id]
+    @game_id = league_info[:game_id].to_i
     @team_id = league_info[:team_id].to_i
     @hoa = league_info[:hoa]
     @result = league_info[:result]
     @settled_in = league_info[:settled_in]
     @head_coach = league_info[:head_coach]
     @shots = league_info[:shots].to_i
+    @goals = league_info[:goals].to_i
     @tackles = league_info[:tackles].to_i
     @pim = league_info[:pim].to_i
     @power_play_opportunities = league_info[:powerplayopportunities].to_i
@@ -65,7 +67,7 @@ class Season < Game
       end
       acc
     end
-    require "pry"; binding.pry
+    # require "pry"; binding.pry
   end
 
   # def self.home_win_percentage
@@ -84,4 +86,57 @@ class Season < Game
   def self.winningest_team
 
   end
+
+  def self.seasons_filter(season_id)
+    @@all_games.map do |game|
+      game.game_id if game.season == season_id
+    end.compact.uniq
+
+  end
+
+  def self.most_accurate_team(season_id)	#Name of the Team with the best ratio of shots to goals for the season	String
+    team_accuracy_hash = shot_to_goal_ratio_per_game(season_id)
+    team_accuracy_average_hash = average_shots_per_goal_by_team(team_accuracy_hash)
+    binding.pry
+    max_value_team(team_accuracy_average_hash)
+  end
+
+  def self.least_accurate_team(season_id)	#Name of the Team with the best ratio of shots to goals for the season	String
+    team_accuracy_hash = shot_to_goal_ratio_per_game(season_id)
+    team_accuracy_average_hash = average_shots_per_goal_by_team(team_accuracy_hash)
+    min_value_team(team_accuracy_average_hash)
+  end
+
+
+  def self.shot_to_goal_ratio_per_game(season_id)
+    games_in_season = seasons_filter(season_id)
+    shot_to_goal_ratio_per_game_by_season = @@all_seasons.reduce (Hash.new([])) do |acc, season|
+      # binding.pry
+      if games_in_season.include?(season.game_id) && acc.keys.include?(season.team_id)
+        acc[season.team_id] << (season.shots/season.goals)
+      elsif games_in_season.include?(season.game_id)
+        acc[season.team_id] = (season.shots/season.goals)
+      end
+      acc
+    end.compact
+  end
+
+  def self.average_shots_per_goal_by_team(hash)
+      hash.each do |key, value|
+        hash[key] = (value.sum / value.count.to_f).round(2)
+      end
+  end
+
+  def self.max_value_team(team_accuracy_average_hash)
+    team_id = team_accuracy_average_hash.key(team_accuracy_average_hash.values.max)
+    team = @@all_teams.find {|team| team.team_name if team.team_id == team_id}
+    team.team_name
+  end
+
+  def self.min_value_team(team_accuracy_average_hash)
+    team_id = team_accuracy_average_hash.key(team_accuracy_average_hash.values.min)
+    team = @@all_teams.find {|team| team.team_name if team.team_id == team_id}
+    team.team_name
+  end
+
 end
--- test/season_test.rb
+++ test/season_test.rb
@@ -7,6 +7,8 @@ class SeasonTest < Minitest::Test
   def setup
     Season.from_csv('./data/game_teams_dummy.csv')
     @season = Season.all[0]
+    Game.from_csv('./data/games_dummy.csv')
+    @game = Game.all[0]
   end
 
   def test_it_exists
@@ -14,14 +16,17 @@ class SeasonTest < Minitest::Test
   end
 
   def test_it_can_determine_total_home_games_played_by_team_id
+    skip
     assert_equal ({6=>2, 12=>1, 10=>1, 21=>1, 53=>2, 1=>2, 24=>1, 4=>2}), Season.total_home_games_played
   end
 
   def test_it_can_determine_total_away_games_played_by_team_id
+    skip
     assert_equal ({3=>2, 23=>2, 5=>1, 19=>1, 28=>1, 25=>1, 2=>1, 10=>1, 6=>1}), Season.total_away_games_played
   end
 
   def test_it_can_determine_total_home_win_percentage
+    skip
     assert_equal ({6=>2, 12=>1, 10=>1, 21=>1, 53=>2, 1=>2, 24=>1, 4=>2}), Season.home_win_percentage
   end
 
@@ -44,4 +49,26 @@ class SeasonTest < Minitest::Test
     skip
     assert_equal "sfjghdfjhg", Game.winningest_team
   end
+
+  def test_it_can_determine_home_win_percentage
+
+  end
+
+  def test_it_can_filter_games_by_season
+    assert_equal ([2012030221, 2012030222, 2012030223, 2012030224, 2012020387]), Season.seasons_filter("20122013")
+  end
+
+  def test_case_name_it_can_find_shot_to_goal_ratio_per_game
+    assert_equal ({3=>4, 6=>4}), Season.shot_to_goal_ratio_per_game("20122013")
+  end
+
+  def test_it_can_find_average_shots_per_goal_by_team
+    Season.most_accurate_team("20122013")
+    assert_equal ("x"), Season.average_shots_per_goal_by_team
+  end
+
+  # def test_it_can_find_the_most_accurate_team_by_season
+  #   assert_equal (""), Season.most_accurate_team("20122013")
+  # end
+
 end
