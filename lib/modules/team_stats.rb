5288ebddfe364b3aef50d4d3c3eebfc15ce7347a
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-01-06 18:19:44 -0700

    Add function and tests for iteration 5 methods

--- lib/modules/team_stats.rb
+++ lib/modules/team_stats.rb
@@ -1,3 +1,70 @@
+require_relative './calculateable'
+require_relative './gatherable'
+
 module TeamStats
-  # logic
+  include Calculateable
+  include Gatherable
+
+  def favorite_opponent(team_id)
+    team_wins = get_wins_by_opponent(team_id)
+    team_games = get_total_games_by_opponent(team_id)
+    team_average_by_opponent = get_average_by_opponent(team_wins, team_games)
+
+    team_ident = team_average_by_opponent.max_by { |_opp_team, percent| percent }[0]
+    team_name = get_team_name_by_id(team_ident)
+
+    return team_name
+  end
+
+  def rival(team_id)
+    team_wins = get_wins_by_opponent(team_id)
+    team_games = get_total_games_by_opponent(team_id)
+    team_average_by_opponent = get_average_by_opponent(team_wins, team_games)
+
+    team_ident = team_average_by_opponent.min_by { |_opp_team, percent| percent }[0]
+    team_name = get_team_name_by_id(team_ident)
+
+    return team_name
+  end
+
+  def head_to_head(team_id)
+    team_wins = get_wins_by_opponent(team_id)
+    team_games = get_total_games_by_opponent(team_id)
+    team_average_by_opponent = get_average_by_opponent(team_wins, team_games)
+
+    avg_by_team_name = team_average_by_opponent.map do |key, value|
+      [key = get_team_name_by_id(key), value]
+    end.to_h
+    
+    avg_by_team_name.sort.to_h
+  end
+
+  def get_total_games_by_opponent(team_id)
+    @games.collection.inject(Hash.new(0)) do |team_games, game|
+      if game[1].home_team_id == team_id
+        team_games[game[1].away_team_id] += 1
+      elsif game[1].away_team_id == team_id
+        team_games[game[1].home_team_id] += 1
+      end
+      team_games
+    end
+  end
+
+  def get_wins_by_opponent(team_id)
+    @games.collection.inject(Hash.new(0)) do |opp_wins, game|
+      if (game[1].home_team_id == team_id) && (game[1].home_goals > game[1].away_goals)
+        opp_wins[game[1].away_team_id] += 1
+      elsif (game[1].away_team_id == team_id) && (game[1].away_goals > game[1].home_goals)
+        opp_wins[game[1].home_team_id] += 1
+      end
+      opp_wins
+    end
+  end
+
+  def get_average_by_opponent(team_wins, team_games)
+    team_wins.inject(Hash.new(0)) do |win_perc, win|
+      win_perc[win.first] = (win.last.to_f / team_games[win.first]).round(2)
+      win_perc
+    end
+  end
 end
