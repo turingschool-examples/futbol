2919681d0dcf1365628b41c63077ab1a2d6c9395
Fred Rondina <fredrondina96@gmail.com>
2020-01-07 13:11:20 -0700

    Removed prys

--- lib/modules/season_searchable.rb
+++ lib/modules/season_searchable.rb
@@ -84,14 +84,6 @@ module SeasonSearchable
     season_wins(team_id).min_by{|season, win_percentage| win_percentage}.first
   end
 
-  # def average_win_percentage(team_id)
-  #     win_array = season_wins(team_id).reduce([]) do |acc, (season, values)|
-  #       acc << values
-  #       acc
-  #     end
-  #   return (win_array.sum / win_array.length).round(2)
-  # end
-
   def season_wins(team_id)
     team = teams.find {|team| team.team_id.to_s == team_id}
     season_wins = team.stats_by_season.reduce({}) do |acc, (season, values)|
