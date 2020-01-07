48c2a6862e63553e10acc00f943c558c9cd4868f
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-01-07 13:10:22 -0700

    Fix stat_tracker_tests to have tackles tests

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -212,4 +212,27 @@ class StatTrackerTest < Minitest::Test
     assert_instance_of Integer, @stat_tracker.worst_loss('17')
     assert_equal 7, @stat_tracker.worst_loss('17')
   end
+
+  def test_stat_tracker_can_get_most_tackles
+    assert_instance_of String, @stat_tracker.most_tackles('20172018')
+    assert_equal "Minnesota United FC", @stat_tracker.most_tackles('20172018')
+
+    assert_instance_of String, @stat_tracker.most_tackles('20132014')
+    assert_equal "Sporting Kansas City", @stat_tracker.most_tackles('20132014')
+
+    assert_instance_of String, @stat_tracker.most_tackles('20152016')
+    assert_equal "Vancouver Whitecaps FC", @stat_tracker.most_tackles('20152016')
+
+  end
+
+  def test_stat_tracker_can_get_fewest_tackles
+    assert_instance_of String, @stat_tracker.fewest_tackles('20172018')
+    assert_equal "New England Revolution", @stat_tracker.fewest_tackles('20172018')
+
+    assert_instance_of String, @stat_tracker.fewest_tackles('20132014')
+    assert_equal "Montreal Impact", @stat_tracker.fewest_tackles('20132014')
+
+    assert_instance_of String, @stat_tracker.fewest_tackles('20152016')
+    assert_equal "Philadelphia Union", @stat_tracker.fewest_tackles('20152016')
+  end
 end
