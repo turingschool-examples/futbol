5c45333f41fc07e4b63b6bd00e621844d8a31006
Jesse Gietzen <gietzenjesse@gmail.com>
2020-02-15 13:11:15 -0700

    add attr_readers

--- lib/team.rb
+++ lib/team.rb
@@ -1,5 +1,12 @@
 class Team
 
+  attr_reader :team_id,
+              :franchise_id,
+              :team_name,
+              :abbreviation,
+              :stadium,
+              :link
+
   def initialize(details)
     @team_id = details[:team_id].to_i
     @franchise_id = details[:franchiseId].to_i
