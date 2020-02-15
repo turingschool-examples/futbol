8d70fdbfb30f73e6e5d90edf7678d61d8f3d4588
Jesse Gietzen <gietzenjesse@gmail.com>
2020-02-15 13:10:32 -0700

    add details test

--- test/teams_test.rb
+++ test/teams_test.rb
@@ -20,4 +20,15 @@ class TeamTest < Minitest::Test
     assert_instance_of Team, team
   end
 
+  def test_it_has_details
+    team = Team.new(@atlanta)
+
+    assert_equal 1, team.team_id
+    assert_equal 23, team.franchise_id
+    assert_equal "Atlanta United", team.team_name
+    assert_equal "ATL", team.abbreviation
+    assert_equal "Mercedes-Benz Stadium", team.stadium
+    assert_equal ".api/v1/teams/1", team.link
+  end
+
 end
