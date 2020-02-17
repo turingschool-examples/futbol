77ca5c7b45f0bcf95fd7da3806d86d51bad8e867
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-17 15:33:50 -0700

    Fix indentation

--- lib/data_module.rb
+++ lib/data_module.rb
@@ -2,8 +2,7 @@ require 'csv'
 
 module DataLoadable
 
-def csv_data(file_path)
-  CSV.open(file_path, headers: :first_row, header_converters: :symbol).map(&:to_h)
-end
-
+  def csv_data(file_path)
+    CSV.open(file_path, headers: :first_row, header_converters: :symbol).map(&:to_h)
+  end
 end
