d80178e226a321b71b0ff776c9538d1747b38171
Zach Holcomb <zachholcombmusic@gmail.com>
2020-02-16 23:28:45 -0700

    Update format on game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -10,14 +10,14 @@ class Game
               :venue
 
   def initialize(info_params)
-    @game_id = info_params[:game_id]
-    @season = info_params[:season]
+    @game_id = info_params[:game_id].to_i
+    @season = info_params[:season].to_i
     @type = info_params[:type]
     @date_time = info_params[:date_time]
-    @away_team_id = info_params[:away_team_id]
-    @home_team_id = info_params[:home_team_id]
-    @away_goals = info_params[:away_goals]
-    @home_goals = info_params[:home_goals]
+    @away_team_id = info_params[:away_team_id].to_i
+    @home_team_id = info_params[:home_team_id].to_i
+    @away_goals = info_params[:away_goals].to_i
+    @home_goals = info_params[:home_goals].to_i
     @venue = info_params[:venue]
   end
 end
