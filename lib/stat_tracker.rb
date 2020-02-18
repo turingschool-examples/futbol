12750852d758a3ecce85c5d52d34a6fa89d6c390
Colin Alexander <colintalex@gmail.com>
2020-02-18 16:31:01 -0700

    Adds StatTracker class with initialize and self.from_csv (in progress).

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,10 +1,29 @@
+require 'CSV'
+
 class StatTracker
+    @@all_teams = []
 
-  def initialize
-
+  def self.all
+    @all_teams
   end
 
   def self.from_csv(locations)
-    
+    teams_csv = CSV.read(locations[:teams], headers: true, header_converters: :symbol)
+    game_teams_csv = CSV.read(locations[:game_teams], headers: true, header_converters: :symbol)
+
+    @@all_games = games_csv.map do |row|
+      Game.new(row)
+    end
+
+    StatTracker.new(@@all_games)
   end
+
+  def initialize(games_csv)
+    @games_csv = games_csv
+    @teams_csv = 0
+    @game_teams_csv = 0
+  end
+
+
+
 end
