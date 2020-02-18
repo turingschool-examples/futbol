290ae27592b049176755879f9520e3c838329361
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-17 19:54:17 -0700

    Change require names

--- lib/game_stats.rb
+++ lib/game_stats.rb
@@ -1,10 +1,11 @@
 require_relative 'game'
-require_relative 'data_module'
+require_relative 'data_loadable'
 
 class GameStats
   include DataLoadable
+  attr_reader :games
 
-  def initialize(file_path)
-    @games = csv_data('./data/games_truncated.csv')
+  def initialize(file_path, object)
+    @games = csv_data('./data/games_truncated.csv', Game)
   end
 end
