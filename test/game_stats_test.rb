aa6b9ba13958f97718fe0ce2659dc36b694a5428
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-17 19:55:06 -0700

    Change require names Passing Object as argument in game_stats.

--- test/game_stats_test.rb
+++ test/game_stats_test.rb
@@ -1,5 +1,5 @@
 require_relative 'test_helper'
-require './lib/data_module'
+require './lib/data_loadable'
 require './lib/game'
 require './lib/game_stats'
 
@@ -7,10 +7,14 @@ require './lib/game_stats'
 class GameStatsTest < Minitest::Test
 
   def setup
-    @game_stats = GameStats.new("./data/games_truncated.csv")
+    @game_stats = GameStats.new("./data/games_truncated.csv", Game)
   end
 
   def test_it_exists
     assert_instance_of GameStats, @game_stats
+  end
+
+  def test_game_objects_are_new_instances_of_game_class
+    assert_instance_of Game, @game_stats.games.first
   end
 end
