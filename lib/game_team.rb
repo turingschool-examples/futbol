7a45e2431644a0f7cdedf94401e28a9b93797ab5
David Tran <davidttemail@gmail.com>
2020-02-18 13:57:04 -0700

    Create game methods test

---
+++ lib/game_team.rb
@@ -0,0 +1,48 @@
+class GameTeam
+  @@game_teams = {}
+
+  def self.add(game_team)
+    @@game_teams[game_team.game_id] = {} if !@@game_teams.has_key?(game_team.game_id)
+    @@game_teams[game_team.game_id][game_team.team_id] = game_team
+  end
+
+  def self.all
+    @@game_teams
+  end
+
+
+  attr_reader :game_id,
+              :team_id,
+              :hoa,
+              :result,
+              :settled_in,
+              :head_coach,
+              :goals,
+              :shots,
+              :tackles,
+              :pim,
+              :power_play_opportunities,
+              :power_play_goals,
+              :face_off_win_percentage,
+              :giveaways,
+              :takeaways
+
+  def initialize(data)
+    @game_id = data[:game_id]
+    @team_id = data[:team_id]
+    @hoa = data[:hoa]
+    @result = data[:result]
+    @settled_in = data[:settled_in]
+    @head_coach = data[:head_coach]
+    @goals = data[:goals]
+    @shots = data[:shots]
+    @tackles = data[:tackles]
+    @pim = data[:pim]
+    @power_play_opportunities = data[:powerplayopportunities]
+    @power_play_goals = data[:powerplaygoals]
+    @face_off_win_percentage = data[:faceoffwinpercentage]
+    @giveaways = data[:giveaways]
+    @takeaways = data[:takeaways]
+  end
+
+end
