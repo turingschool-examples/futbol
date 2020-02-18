9c1fd29b3ee37ce8d7830a33035e5e7afe22f9f1
Ethan Hocking <eshocking253@gmail.com>
2020-02-18 13:39:25 -0700

    Add attr_readers for all game data attributes

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,15 @@
 class Game
+  attr_reader :id,
+              :season,
+              :type,
+              :date_time,
+              :away_team_id,
+              :home_team_id,
+              :away_goals,
+              :home_goals,
+              :venue,
+              :venue_link
+
 
   def initialize(game_params)
     @id             = game_params[:id]
@@ -13,6 +24,6 @@ class Game
     @venue_link     = game_params[:venue_link]
   end
 
-  
+
 
 end
--- test/game_test.rb
+++ test/game_test.rb
@@ -24,6 +24,21 @@ class GameTest < Minitest::Test
     assert_instance_of Game, @game
   end
 
+  def test_it_has_attributes
+
+    assert_equal 2012030221,            @game.id
+    assert_equal 20122013,              @game.season
+    assert_equal "Postseason",          @game.type
+    assert_equal "5/16/13",             @game.date_time
+    assert_equal 3,                     @game.away_team_id
+    assert_equal 6,                     @game.home_team_id
+    assert_equal 2,                     @game.away_goals
+    assert_equal 3,                     @game.home_goals
+    assert_equal "Toyota Stadium",      @game.venue
+    assert_equal "/api/v1/venues/null", @game.venue_link
+
+  end
+
 end
 
 # === DONE ===
