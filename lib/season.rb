9e933d9f4e3e8abb6360ce654efcdf8e02f80a2d
Alex Lynch <mlynch5187@gmail.com>
2020-02-18 14:56:28 -0700

    Work on average_goals_per_season method

--- lib/season.rb
+++ lib/season.rb
@@ -1,12 +1,29 @@
 require "CSV"
+require_relative './game'
 
 class Season
 attr_reader :id, :games_in_season
 
+  # def self.from_csv(season, gt_path)
+  #   season_ids = []
+	# 	season_storage = []
+	# 	CSV.foreach(season_path, :headers => true, header_converters: :symbol) do |row|
+	# 		season_ids.push(row[1])
+	# 	end
+	# 	season_ids.uniq.each {|id| season_storage.push(Season.new({id: id, path: season_path}, gt_path))}
+	# 	season_storage
+  # end
+
   def initialize(season_info)
     @id = season_info[:id]
-    # @games_in_season = games_getter(season_info[:path])
+    @games_in_season = games_getter(season_info[:path])
   end
+
+  #create games_getter method
+
+  # def games_getter
+  #
+  # end
 
   def count_of_games_by_season
 		games_by_season = {}
@@ -14,13 +31,8 @@ attr_reader :id, :games_in_season
 		games_by_season
 	end
 
- #  def games_getter
- #  game_sorted = Hash.new { |hash, key| hash[key] = [] }
- #   CSV.foreach(games_path, :headers => true, header_converters: :symbol) do |row|
- #     if row[1] == @id
- #       game_sort_hash[(row[2])].push(row[0])
- #     end
- #   end
- #   games_sorted
- # end
+  def average_goals_by_season
+    # total_goals = Game.all.map {|game| game.total_score}
+    # return ((total_goals.sum.to_f / Game.length).round(2))
+  end
 end
