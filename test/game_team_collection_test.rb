347cbad1295ce4405453f5097b6bcd2aea289ca8
Zach Holcomb <zachholcombmusic@gmail.com>
2020-02-18 13:52:36 -0700

    Changed file architecture to have collections of games, teams, and gameteams, removed that code from stat_tracker

---
+++ test/game_team_collection_test.rb
@@ -0,0 +1,20 @@
+require_relative 'test_helper'
+require './lib/game_team_collection'
+require './lib/game_team'
+
+class GameTeamCollectionTest < Minitest::Test
+  def setup
+    file_path = './test/fixtures/truncated_game_teams.csv'
+    @game_team_collection = GameTeamCollection.new(file_path)
+  end
+
+  def test_it_exists
+    assert_instance_of GameTeamCollection, @game_team_collection
+  end
+
+  def test_it_has_list_of_game_teams
+    assert_instance_of Array, @game_team_collection.game_team_list
+    assert_instance_of GameTeam, @game_team_collection.game_team_list.first
+    assert_equal 200, @game_team_collection.game_team_list.length
+  end
+end
