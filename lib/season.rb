bba028d9553c7a289c21451bb5ee5035b46bc2d2
Alex Lynch <mlynch5187@gmail.com>
2020-02-18 14:18:12 -0700

    Start season_test

--- lib/season.rb
+++ lib/season.rb
@@ -1,8 +1,12 @@
 require "CSV"
-require_relative './game'
 
 class Season
-attr_reader :id, :games_by_type, :games_unsorted
+attr_reader :id, :games_in_season
+
+  def initialize(season_hash)
+    @id = season_has[:id]
+    @games_in_season = games_gather(season_hash[:path])
+  end
 
 
 end
---
+++ test/season_test.rb
@@ -0,0 +1,16 @@
+require './test_helper'
+require_relative '../lib/season'
+
+class SeasonTest < MiniTest::Test
+  def method_name
+    @season = Season.new(season_id)
+  end
+
+  def test_season_is_created_with_id
+    assert_instance_of Season, @season
+    assert_equal 20122013, @season.id
+    assert_equal "Postseason", @season.type
+  end
+
+
+end
