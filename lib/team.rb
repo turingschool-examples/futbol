3d67d4caf3e4101ee7c0ce44f853884b4ea1124b
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-02-18 14:22:36 -0700

    Test for attributes Change of method names to be able to call from csv headers

--- lib/team.rb
+++ lib/team.rb
@@ -1,11 +1,11 @@
 class Team
   attr_reader :team_id,
-              :franchise_id,
-              :team_name
-              
+              :franchiseId,
+              :teamName
+
   def initialize(attributes)
     @team_id = attributes[:team_id]
-    @franchise_id = attributes[:franchiseId]
-    @team_name = attributes[:teamName]
+    @franchiseId = attributes[:franchiseId]
+    @teamName = attributes[:teamName]
   end
 end
