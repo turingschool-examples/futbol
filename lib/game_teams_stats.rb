7b9689477038d033653ddea616cbff8c754ca6f9
Alex Latham <alexlthm@gmail.com>
2020-02-17 19:51:23 -0700

    Relcate method to calculate percentage home wins to GameTeamsStats class, create corresponding test file

---
+++ lib/game_teams_stats.rb
@@ -0,0 +1,24 @@
+require './lib/data_module'
+
+class GameTeamsStats
+  include DataLoadable
+
+  def initialize(game_teams_path_param)
+    @game_teams_path = game_teams_path_param
+  end
+
+  def percentage_home_wins
+    game_teams = csv_data(@game_teams_path)
+
+    home_wins = game_teams.count do |game|
+      game[:hoa] == "home" && game[:result] == "WIN"
+    end
+
+    home_games = game_teams.count do |game|
+      game[:hoa] == "home"
+    end
+
+    (100 * home_wins.fdiv(home_games)).round(2)
+  end
+
+end
