4c47d62cf6428e6bdb31c5b398b74d953f66398d
Alex Latham <alexlthm@gmail.com>
2020-02-17 19:50:04 -0700

    Remove methods and tests from iteration 1, rename game_path to games_path to match file name convention

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -3,46 +3,16 @@ require './lib/data_module'
 
 class StatTracker
   include DataLoadable
-  attr_accessor :game_path, :team_path, :game_teams_path
+  attr_accessor :games_path, :team_path, :game_teams_path
 
   def self.from_csv(locations)
     StatTracker.new(locations)
   end
 
   def initialize(locations)
-    @game_path = locations[:games]
+    @games_path = locations[:games]
     @team_path = locations[:teams]
     @game_teams_path = locations[:game_teams]
   end
 
-  def percentage_home_wins
-    game_teams = csv_data(@game_teams_path)
-
-    home_wins = game_teams.count do |game|
-      game[:hoa] == "home" && game[:result] == "WIN"
-    end
-
-    home_games = game_teams.count do |game|
-      game[:hoa] == "home"
-    end
-
-    (100 * home_wins.fdiv(home_games)).round(2)
-  end
-
-  def percentage_ties
-    games = csv_data(@game_path)
-
-    ties = games.count do |game|
-      game[:away_goals] == game[:home_goals]
-    end
-
-    (100 * ties.fdiv(games.length)).round(2)
-  end
-
-  def count_of_games_by_season
-    csv_data(@game_path).reduce(Hash.new(0)) do |games_by_season, game|
-      games_by_season[game[:season]] += 1
-      games_by_season
-    end
-  end
 end
--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -20,27 +20,9 @@ class StatTrackerTest < Minitest::Test
   end
 
   def test_it_can_initialize_from_correct_file_plath
-    assert_equal './data/games_truncated.csv', @stat_tracker.game_path
+    assert_equal './data/games_truncated.csv', @stat_tracker.games_path
     assert_equal './data/game_teams_truncated.csv', @stat_tracker.game_teams_path
     assert_equal './data/teams.csv', @stat_tracker.team_path
   end
 
-  def test_it_can_calculate_percentage_home_wins
-    assert_equal 66.67, @stat_tracker.percentage_home_wins
-  end
-
-  def test_it_can_calculate_percentage_ties
-    assert_equal 14.29, @stat_tracker.percentage_ties
-  end
-
-  def test_it_can_count_games_by_season
-    games_by_season = {
-      "20152016" => 3,
-      "20132014" => 2,
-      "20142015" => 1,
-      "20162017" => 1
-    }
-
-    assert_equal games_by_season, @stat_tracker.count_of_games_by_season
-  end
 end
