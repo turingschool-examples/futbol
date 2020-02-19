3e8d6331bfe14770e31162466080c054a4c0a1d6
Anthony Gomes <code@tonygom.es>
2020-02-18 18:20:00 -0700

    Add TeamStats test & class files

---
+++ test/team_stats_test.rb
@@ -0,0 +1,45 @@
+require_relative 'test_helper'
+# require './fixtures/teams_truncated'
+require './data/teams_sample.csv'
+require './lib/team.rb'
+
+class TeamStatsTest < MiniTest::Test
+  def setup
+  end
+
+  def test_it_can_get_team_info
+  end
+
+  def test_it_can_get_best_season
+  end
+
+  def test_it_can_get_worst_season
+  end
+
+  def test_it_can_get_average_win_percentage
+  end
+
+  def test_it_can_get_most_goals_scored
+  end
+
+  def test_it_can_get_fewest_goals_scored
+  end
+
+  def test_it_can_get_favorite_opponent
+  end
+
+  def test_it_can_get_rival
+  end
+
+  def test_it_can_get_biggest_team_blowout
+  end
+
+  def test_it_can_get_worst_loss
+  end
+
+  def test_it_can_get_head_to_head
+  end
+
+  def test_it_can_get_seasonal_summary
+  end
+end
