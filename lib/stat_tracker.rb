794ae8b27e62f49267c4a5d8fa0c45526b0c73cf
Javier Aguilar <jvr.agx@gmail.com>
2020-02-18 18:01:15 -0700

    Pass test_from_csv

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,5 +1,25 @@
+require './lib/game_collection'
+
 class StatTracker
 
-  
+  attr_reader :game_path, :team_path, :game_team_path
+
+  def initialize(game_path, team_path, game_team_path)
+    @game_path = game_path,
+    @team_path = team_path,
+    @game_team_path = game_team_path
+  end
+
+  def self.from_csv(locations)
+    @game_path = locations[:games]
+    @team_path = locations[:teams]
+    @game_team_path = locations[:game_teams]
+
+    StatTracker.new(@game_path, @team_path, @game_team_path)
+  end
+
+  def game_collection
+    GameCollection.new(@game_path)
+  end
 
 end
--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -1,7 +1,23 @@
 require "./test/test_helper"
+require "./lib/stat_tracker"
+require "CSV"
 
 class StatTrackerTest < Minitest::Test
 
+  def test_from_csv
 
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    stat_tracker = StatTracker.from_csv(locations)
+    assert_instance_of StatTracker, stat_tracker
+  end
 
 end
