8199522edeeb9878e51edb96d1fd2a1b445d44a0
Maxwell Baird <bairdm71@gmail.com>
2020-02-19 08:43:57 -0700

    added a test to check find_name

--- test/team_stats_test.rb
+++ test/team_stats_test.rb
@@ -14,4 +14,9 @@ class TeamStatsTest < Minitest::Test
     assert_instance_of TeamStats, @team_stats
   end
 
+  def test_it_can_find_team_name
+    assert_equal 'Chicago Red Stars', @team_stats.find_name(25)
+    assert_equal 'New York Red Bulls', @team_stats.find_name(8)
+  end
+
 end
