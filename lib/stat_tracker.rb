464cab3e30c27917fff83afd16b8980928eac1f7
Javier Aguilar <jvr.agx@gmail.com>
2020-02-18 21:20:33 -0700

    Move methods from game collection to stat_tracker

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,25 +1,36 @@
-require './lib/game_collection'
+require_relative './game_collection'
+require 'CSV'
 
 class StatTracker
 
   attr_reader :game_path, :team_path, :game_team_path
 
   def initialize(game_path, team_path, game_team_path)
-    @game_path = game_path,
-    @team_path = team_path,
+    @game_path = game_path
+    @team_path = team_path
     @game_team_path = game_team_path
   end
 
   def self.from_csv(locations)
-    @game_path = locations[:games]
-    @team_path = locations[:teams]
-    @game_team_path = locations[:game_teams]
-
-    StatTracker.new(@game_path, @team_path, @game_team_path)
+    StatTracker.new(locations[:games], locations[:teams], locations[:game_teams])
   end
 
   def game_collection
     GameCollection.new(@game_path)
   end
 
+  def percentage_home_wins
+    count = game_collection.games.count {|game| game.home_goals > game.away_goals}
+    (count.to_f / game_collection.games.length).round(2)
+  end
+
+  def percentage_visitor_wins
+    count = game_collection.games.count {|game| game.away_goals > game.home_goals}
+    (count.to_f / game_collection.games.length).round(2)
+  end
+
+  def percentage_ties
+    count = game_collection.games.count {|game| game.away_goals == game.home_goals}
+    (count.to_f / game_collection.games.length).round(2)
+  end
 end
