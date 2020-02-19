41661ec20d9473e7f7922bf977484a56bb58d2b4
Anthony Gomes <code@tonygom.es>
2020-02-18 18:41:01 -0700

    Save work

--- test/team_stats_test.rb
+++ test/team_stats_test.rb
@@ -5,9 +5,24 @@ require './lib/team.rb'
 
 class TeamStatsTest < MiniTest::Test
   def setup
+    @team = Team.new
+    @team_collection = Team.all
   end
 
   def test_it_can_get_team_info
+    expected = {
+      team_id: 7,
+      franchise_id: 19,
+      teamName: 'Utah Royals FC',
+      abbreviation: 'URF',
+      Stadium: 'Rio Tinto Stadium',
+      link: '/api/v1/teams/7'
+    }
+
+    new_team = mock('team')
+    new_team.stubs(:team_info).returns(expected)
+
+    assert_equal expected, new_team.team_info
   end
 
   def test_it_can_get_best_season
@@ -23,6 +38,7 @@ class TeamStatsTest < MiniTest::Test
   end
 
   def test_it_can_get_fewest_goals_scored
+    assert_equal 0, Team.all.fewest_goals_scored('7')
   end
 
   def test_it_can_get_favorite_opponent
