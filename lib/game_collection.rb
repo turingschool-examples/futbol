024888704072b0b0c5831e08786757785e12c873
Colin Alexander <colintalex@gmail.com>
2020-02-18 17:45:47 -0700

    Adds working Game_Collection class

---
+++ lib/game_collection.rb
@@ -0,0 +1,16 @@
+require './lib/game'
+require 'CSV'
+
+class GameCollection
+  attr_reader :games
+  def initialize(game_file_path)
+    @games = create_games(game_file_path)
+  end
+
+  def create_games(game_file_path)
+    @game_data = CSV.read(game_file_path, headers: true, header_converters: :symbol)
+    @game_data.map do |row|
+      Game.new(row.to_h)
+    end
+  end
+end
