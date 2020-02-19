9c89e2e196d633598f53c95681e3855f11c416d3
Mariana Cid <cidmariana5@gmail.com>
2020-02-19 16:16:20 -0700

    Created Method to instantiate game with test.

--- test/game_collection_test.rb
+++ test/game_collection_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/game_collection'
 
+
 class GameCollectionTest < Minitest::Test
   def setup
     @csv_file_path = "./test/fixtures/games_truncated.csv"
@@ -16,4 +17,20 @@ class GameCollectionTest < Minitest::Test
     assert_equal [], @game_collection.games
     assert_equal "./test/fixtures/games_truncated.csv", @game_collection.csv_file_path
   end
+
+  def test_it_instantiate_a_game_object
+    info = {
+      game_id: "2012030221", season: "20122013", type: "Postseason",
+      date_time: "5/16/13", away_team_id: "3", home_team_id: "6", away_goals: "2",
+      home_goals: "3", venue: "Toyota Stadium", venue_link: "/api/v1/venues/null"
+    }
+    game = @game_collection.instantiate_game(info)
+
+    assert_instance_of Game, game
+    assert_equal 2012030221, game.game_id
+    assert_equal "Postseason", game.type
+    assert_equal "5/16/13", game.date_time
+    assert_equal 3, game.away_team_id
+  end
+  
 end
