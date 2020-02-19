309f65eea087e9396985cd4f4d537e61302e4de6
“Krista <stadler.krista@gmail..com>
2020-02-18 20:56:13 -0700

    Tested it can add team with failures and successfully tested it loads all teams from csv

--- test/team_test.rb
+++ test/team_test.rb
@@ -9,7 +9,7 @@ require "./lib/game_team"
 class TeamTest < Minitest::Test
 
   def setup
-    StatTracker.create_items("./test/fixtures/teams_sample.csv", Game)
+    StatTracker.create_items("./test/fixtures/teams_sample.csv", Team)
 
     @team1 = Team.new({
       team_id: 1,
@@ -33,6 +33,25 @@ class TeamTest < Minitest::Test
     assert_equal "ATL", @team1.abbreviation
     assert_equal "Mercedes-Benz Stadium", @team1.stadium
     assert_equal "/api/v1/teams/1", @team1.link
+  end
+
+  def test_it_can_add_team
+    skip
+    assert_instance_of Hash, Team.all
+    assert_equal 32, Team.all.length
+    assert_instance_of Team, Team.all[26]
+
+    assert_equal 26, Team.all[26].team_id
+    assert_equal 14, Team.all[26].franchise_id
+    assert_equal "FC Cincinnati", Team.all[26].team_name
+    assert_equal "CIN", Team.all[26].abbreviation
+    assert_equal "Nippert Stadium", Team.all[26].stadium
+    assert_equal "/api/v1/teams/26", Team.all[26].link
+  end
+
+  def test_it_loads_all_teams_from_csv
+    assert_equal 1, Team.all[1].team_id
+    assert_equal 53, Team.all[53].team_id
   end
 
 end
