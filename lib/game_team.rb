2ba0ff44867552ec801c5ef2490af71b9eb12d2d
michelle <foleymichelle9@gmail.com>
2020-02-18 18:05:14 -0700

    add game_team class and test

---
+++ lib/game_team.rb
@@ -0,0 +1,54 @@
+require 'csv'
+
+class GameTeam
+
+  @@all = []
+
+  def self.create_game_team(csv_file_path)
+    csv = CSV.read(csv_file_path, headers: true, header_converters: :symbol)
+
+    all_game_team = csv.map do |row|
+      GameTeam.new(row)
+    end
+
+    @@all = all_game_team
+  end
+
+  def self.all
+    @@all
+  end
+
+  attr_reader :game_id,
+              :team_id,
+              :hoa,
+              :result,
+              :settled_in,
+              :head_coach,
+              :goals,
+              :shots,
+              :tackles,
+              :pim,
+              :power_play_opportunities,
+              :power_play_goals,
+              :face_off_win_percentage,
+              :giveaways,
+              :takeaways
+
+  def initialize(game_team_parameter)
+    @game_id = game_team_parameter[:game_id].to_i
+    @team_id = game_team_parameter[:team_id].to_i
+    @hoa = game_team_parameter[:hoa]
+    @result = game_team_parameter[:result]
+    @settled_in = game_team_parameter[:settled_in]
+    @head_coach = game_team_parameter[:head_coach]
+    @goals = game_team_parameter[:goals].to_i
+    @shots = game_team_parameter[:shots].to_i
+    @tackles = game_team_parameter[:tackles].to_i
+    @pim = game_team_parameter[:pim].to_i
+    @power_play_opportunities = game_team_parameter[:powerplayopportunities].to_i
+    @power_play_goals = game_team_parameter[:powerplaygoals].to_i
+    @face_off_win_percentage = game_team_parameter[:faceoffwinpercentage].to_f
+    @giveaways = game_team_parameter[:giveaways].to_i
+    @takeaways = game_team_parameter[:takeaways].to_i
+  end
+end
---
+++ test/game_team_test.rb
@@ -0,0 +1,32 @@
+require_relative 'test_helper'
+require './lib/game_team'
+
+class GameTeamTest < Minitest::Test
+
+  def setup
+    @game_teams = GameTeam.create_game_team('./test/fixtures/game_teams_truncated.csv')
+    @game_team = GameTeam.all[2]
+  end
+
+  def test_it_exists
+    assert_instance_of GameTeam, @game_team
+  end
+
+  def test_it_has_attributes
+    assert_equal 2012030222, @game_team.game_id
+    assert_equal 3, @game_team.team_id
+    assert_equal 'away', @game_team.hoa
+    assert_equal 'LOSS', @game_team.result
+    assert_equal 'REG', @game_team.settled_in
+    assert_equal 'John Tortorella', @game_team.head_coach
+    assert_equal 2, @game_team.goals
+    assert_equal 9, @game_team.shots
+    assert_equal 33, @game_team.tackles
+    assert_equal 11, @game_team.pim
+    assert_equal 5, @game_team.power_play_opportunities
+    assert_equal 0, @game_team.power_play_goals
+    assert_equal 51.7, @game_team.face_off_win_percentage
+    assert_equal 1, @game_team.giveaways
+    assert_equal 4, @game_team.takeaways
+  end
+end
