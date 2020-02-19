11f76cf5c4c78832b05f1fa022cde92b44432ed2
Javier Aguilar <jvr.agx@gmail.com>
2020-02-18 17:59:11 -0700

    Passes tests in game_collection

--- lib/game_collection.rb
+++ lib/game_collection.rb
@@ -1,5 +1,34 @@
 class GameCollection
 
-  
+  attr_reader :games
 
+  def initialize(csv_file_path)
+    @games = create_games(csv_file_path)
+  end
+
+  def create_games(csv_file_path)
+    csv = CSV.read(csv_file_path, headers: true, header_converters: :symbol)
+    csv.map do |row|
+       # Game.new(row)
+       row
+    end
+  end
+
+  def percentage_home_wins
+    count = 0
+    @games.each {|game| count += 1 if game[:home_goals] > game[:away_goals]}
+    ((count / @games.length.to_f) * 100).round(2)
+  end
+
+  def percentage_visitor_wins
+    count = 0
+    @games.each {|game| count += 1 if game[:away_goals] > game[:home_goals]}
+    ((count / @games.length.to_f) * 100).round(2)
+  end
+
+  def percentage_ties
+    count = 0
+    @games.each {|game| count += 1 if game[:away_goals] == game[:home_goals]}
+    ((count / @games.length.to_f) * 100).round(2)
+  end
 end
--- test/game_collection_test.rb
+++ test/game_collection_test.rb
@@ -1,6 +1,40 @@
 require "./test/test_helper"
+require "./lib/stat_tracker"
 require "./lib/game_collection"
+require 'mocha/minitest'
+require "CSV"
 
 class GameCollectionTest < Minitest::Test
 
+  def setup
+    @game_collection = GameCollection.new("./test/fixtures/games_truncated.csv")
+  end
+
+  def test_it_exists
+    assert_instance_of GameCollection, @game_collection
+  end
+
+  def test_it_has_attributes
+    assert_instance_of Array, @game_collection.games
+    assert_equal 10, @game_collection.games.length
+  end
+
+  def test_it_can_create_games_from_csv
+    game = mock('game')
+    @game_collection.stubs(:games).returns(game)
+
+    assert_equal game, @game_collection.games
+  end
+
+  def test_calculates_percentage_home_wins
+    assert_equal 40.0, @game_collection.percentage_home_wins
+  end
+
+  def test_calculates_percentage_visitor_wins
+    assert_equal 50.0, @game_collection.percentage_visitor_wins
+  end
+
+  def test_calculates_percentage_ties
+    assert_equal 10.0, @game_collection.percentage_ties
+  end
 end
