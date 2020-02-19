e50ea04e327c6699c2e2bb3c18dcf2132d8bd739
Javier Aguilar <jvr.agx@gmail.com>
2020-02-18 21:22:52 -0700

    Remove methods that should be stat_tracker

--- lib/game_collection.rb
+++ lib/game_collection.rb
@@ -1,3 +1,5 @@
+require_relative './game'
+
 class GameCollection
 
   attr_reader :games
@@ -9,26 +11,8 @@ class GameCollection
   def create_games(csv_file_path)
     csv = CSV.read(csv_file_path, headers: true, header_converters: :symbol)
     csv.map do |row|
-       # Game.new(row)
-       row
+       Game.new(row)
     end
   end
 
-  def percentage_home_wins
-    count = 0
-    @games.each {|game| count += 1 if game[:home_goals] > game[:away_goals]}
-    ((count / @games.length.to_f) * 100).round(2)
-  end
-
-  def percentage_visitor_wins
-    count = 0
-    @games.each {|game| count += 1 if game[:away_goals] > game[:home_goals]}
-    ((count / @games.length.to_f) * 100).round(2)
-  end
-
-  def percentage_ties
-    count = 0
-    @games.each {|game| count += 1 if game[:away_goals] == game[:home_goals]}
-    ((count / @games.length.to_f) * 100).round(2)
-  end
 end
