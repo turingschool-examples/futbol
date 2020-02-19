7b4da097c124ab7761fa0757f17b18fcb63d53c6
Anthony Gomes <code@tonygom.es>
2020-02-18 22:50:14 -0700

    Save test prework and psuedocode

--- lib/team_stats.rb
+++ lib/team_stats.rb
@@ -1,3 +1,25 @@
 class TeamStats
 
+# team_info(team_id)
+  # @@team[team_id]
+
+# best_season & worst_season
+  # the first 4 characters of game_id indicate the season
+    # @@game_teams.game_id.to_s.slice(0..3)
+  # if team_id
+    # season_hash[4-game_id] = (+wins / +total_games).max_by
+    # season_hash[4-game_id] = (+losses / +total_games).min_by
+    # 4-game_id + (4-game.id.to_i + 1).to_s
+
+  # average_win_percentage
+    # @@game_teams[:team_id] += total_games
+    # (@@game_teams[:team_id] += WINs) / total_games
+
+  # most_goals_scored & fewest_goals
+    # if @@game_teams[:team_id] << goals_array
+    # goals_array.sort.first
+    # goals_array.sort.last
+
+  # favorite_opponent
+    #
 end
--- test/team_stats_test.rb
+++ test/team_stats_test.rb
@@ -1,13 +1,12 @@
-require_relative 'test_helper'
-# require './fixtures/teams_truncated'
-# require './data/teams_sample.csv'
-require './lib/team.rb'
+require_relative "./test_helper"
+require "./lib/stat_tracker"
+require "./lib/game"
+require "./lib/team"
+require "./lib/game_team"
 
 class TeamStatsTest < MiniTest::Test
   def setup
-    require 'pry'; binding.pry
-    @team = Team.new
-    @team_collection = Team.all
+    StatTracker.create_items("./test/fixtures/teams_sample.csv", Team)
   end
 
   def test_it_can_get_team_info
@@ -20,43 +19,50 @@ class TeamStatsTest < MiniTest::Test
       link: '/api/v1/teams/7'
     }
 
-    new_team = mock('team')
-    new_team.stubs(:team_info).returns(expected)
-
-    assert_equal expected, new_team.team_info
+    assert_equal expected, new_team.team_info(7)
   end
 
   def test_it_can_get_best_season
+    assert_equal 'season', @@team.best_season(7)
   end
 
   def test_it_can_get_worst_season
+    assert_equal 'season', @@team.best_season(7)
   end
 
   def test_it_can_get_average_win_percentage
+    assert_equal 0.00, @@team.average_win_percentage(7)
   end
 
   def test_it_can_get_most_goals_scored
+    assert_equal 4, @@team.most_goals_scored(7)
   end
 
   def test_it_can_get_fewest_goals_scored
-    assert_equal 0, Team.all.fewest_goals_scored('7')
+    assert_equal 0, @@team.fewest_goals_scored(7)
   end
 
   def test_it_can_get_favorite_opponent
+    assert_equal 'team_name', @@team.favorite_opponent(7)
   end
 
   def test_it_can_get_rival
+    assert_equal 'team_name', @@team.favorite_opponent(7)
   end
 
   def test_it_can_get_biggest_team_blowout
+    assert_equal 0, @@team.biggest_team_blowout(7)
   end
 
   def test_it_can_get_worst_loss
+    assert_equal 0, @@team.worst_loss(7)
   end
 
   def test_it_can_get_head_to_head
+    assert_equal {}, @@team.head_to_head(7)
   end
 
   def test_it_can_get_seasonal_summary
+    assert_equal {}, @@team.seasonal_summary(7)
   end
 end
