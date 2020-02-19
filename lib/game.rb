f7f4befe8a43fb3105c61b539cc28878fe890758
Daniel Atkinson <atkinson.daniel7@gmail.com>
2020-02-18 17:33:32 -0700

    Complete Game class and run tests

---
+++ lib/game.rb
@@ -0,0 +1,17 @@
+class Game
+  attr_reader :game_id, :season, :type, :date_time, :away_team_id, :home_team_id,
+               :home_goals, :away_goals, :venue, :venue_link
+
+  def initialize(game_params)
+    @game_id = game_params[:game_id].to_i
+    @season = game_params[:season].to_i
+    @type = game_params[:type]
+    @date_time = game_params[:date_time]
+    @away_team_id = game_params[:away_team_id].to_i
+    @home_team_id = game_params[:home_team_id].to_i
+    @home_goals = game_params[:home_goals].to_i
+    @away_goals = game_params[:away_goals].to_i
+    @venue = game_params[:venue]
+    @venue_link = game_params[:venue_link]
+  end
+end
