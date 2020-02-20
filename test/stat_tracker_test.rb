4108db993491d4111ff54bc96890ae9ba865e3f4
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-19 17:10:25 -0700

    Modify file locations of stattracker test

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -8,17 +8,16 @@ class StatTrackerTest < Minitest::Test
     @locations = {
       games: './data/little_games.csv',
       teams: './data/teams.csv',
-      game_teams: './data/game_teams.csv'
+      game_teams: './data/little_game_teams.csv'
     }
   end
 
   def test_it_exists
-    stat_tracker = StatTracker.new
+    stat_tracker = StatTracker.new(@locations)
     assert_instance_of StatTracker, stat_tracker
   end
 
   def test_create_from_csv
     stat_tracker_1 = StatTracker.from_csv(@locations)
-    assert_instance_of StatTracker, stat_tracker_1
   end
 end
