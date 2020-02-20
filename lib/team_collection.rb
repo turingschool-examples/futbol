a7694d63cd05d054a4719ecee4b0a896814ec5b1
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-19 22:04:45 -0700

    Create team collection class

---
+++ lib/team_collection.rb
@@ -0,0 +1,17 @@
+require './lib/team'
+require 'CSV'
+
+class TeamCollection
+  attr_reader :teams
+
+  def initialize(team_file_path)
+    @teams = create_teams(team_file_path)
+  end
+
+  def create_teams(team_file_path)
+    @team_data = CSV.read(team_file_path, headers: true, header_converters: :symbol)
+    @team_data.map do |row|
+      Team.new(row.to_h)
+    end
+  end
+end
