56479077e9e62083171db23e6931509c636a8f91
Stephanie Friend <stephaniefriend91@gmail.com>
2020-02-20 15:34:05 -0700

    Start implementing methods into the stat tracker

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,11 +1,10 @@
-require 'CSV'
-require_relative 'team'
-require_relative 'game'
+require 'csv'
+require_relative 'season_win'
 
 class StatTracker
-  attr_reader :games, :teams, :game_teams
+  attr_reader :games_file, :teams_file, :game_teams_file
 
-  def initialize(games_file, teams_, game_teams)
+  def initialize(games_file, teams_file, game_teams_file)
     @games_file = games_file
     @teams_file = teams_file
     @game_teams_file = game_teams_file
@@ -17,5 +16,9 @@ class StatTracker
     game_teams_file = locations_params[:game_teams]
 
     StatTracker.new(games_file, teams_file, game_teams_file)
+  end
+
+  def team_info(team_id)
+    SeasonWin.new(team_id)
   end
 end
