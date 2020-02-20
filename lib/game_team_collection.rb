7f28460d96c96707e5782deb16d1dc45ea7aa148
Ryan Camp <cmpprg@icloud.com>
2020-02-19 17:18:50 -0700

    Create attr_readers/variables for game team collection

--- lib/game_team_collection.rb
+++ lib/game_team_collection.rb
@@ -1,6 +1,8 @@
 class GameTeamCollection
 
+  attr_reader :games_by_teams, :csv_file_path
   def initialize(csv_file_path)
-    
+    @games_by_teams = []
+    @csv_file_path = csv_file_path
   end
 end
--- test/game_team_collection_test.rb
+++ test/game_team_collection_test.rb
@@ -9,8 +9,13 @@ class GameTeamCollectionTest < Minitest::Test
     @file_path = "./test/fixtures/game_teams_truncated"
     @game_team_collection = GameTeamCollection.new(@file_path)
   end
-  
+
   def test_it_exists
     assert_instance_of GameTeamCollection, @game_team_collection
+  end
+
+  def test_it_has_attributes
+    assert_equal [], @game_team_collection.games_by_teams
+    assert_equal "./test/fixtures/game_teams_truncated", @game_team_collection.csv_file_path
   end
 end
