3a535a00d9f7a37f77fa66387d21eca1e083b10f
Mariana Cid <cidmariana5@gmail.com>
2020-02-20 15:06:17 -0700

    Refactored all files to include simpleCov.

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -1,6 +1,7 @@
+require './test/test_helper'
 require "minitest/autorun"
 require "minitest/pride"
-require "./lib/futbol_dal"
+require "./lib/stat_tracker"
 
 class FutbolDALTest < Minitest::Test
   def test_it_exists
@@ -9,9 +10,9 @@ class FutbolDALTest < Minitest::Test
             team: "./test/fixtures/teams_truncated.csv",
             game_team: "./test/fixtures/game_teams_truncated.csv"
             }
-    futbol_dal = FutbolDAL.from_csv(info)
+    stat_tracker = StatTracker.from_csv(info)
 
-    assert_instance_of FutbolDAL, futbol_dal
+    assert_instance_of StatTracker, stat_tracker
   end
 
   def test_it_has_attributes
@@ -20,10 +21,10 @@ class FutbolDALTest < Minitest::Test
             team: "./test/fixtures/teams_truncated.csv",
             game_team: "./test/fixtures/game_teams_truncated.csv"
             }
-    futbol_dal = FutbolDAL.from_csv(info)
+    stat_tracker = StatTracker.from_csv(info)
 
-    assert_instance_of TeamCollection, futbol_dal.team_collection
-    assert_instance_of GameCollection, futbol_dal.game_collection
-    assert_instance_of GameTeamCollection, futbol_dal.game_team_collection
+    assert_instance_of TeamCollection, stat_tracker.team_collection
+    assert_instance_of GameCollection, stat_tracker.game_collection
+    assert_instance_of GameTeamCollection, stat_tracker.game_team_collection
   end
 end
