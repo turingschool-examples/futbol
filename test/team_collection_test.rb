676fcae4f24d1d41fe079b2e313d3f4e475f5524
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-20 11:35:37 -0700

    Test team objects are created

--- test/team_collection_test.rb
+++ test/team_collection_test.rb
@@ -11,7 +11,13 @@ class TeamCollectionTest < Minitest::Test
   end
 
   def test_it_exists
+
     assert_instance_of TeamCollection, @team_collection
+  end
+
+  def test_it_creates_team_objects
+
+    assert_instance_of Team, @team_collection.teams.first
     assert_equal 32, @team_collection.teams.length
   end
 end
