d85944cf9677642a93893e655e348ce615d60fea
Ethan Hocking <eshocking253@gmail.com>
2020-02-20 16:12:21 -0700

    Update tests, Add initialization and create_teams methods

--- lib/team_collection.rb
+++ lib/team_collection.rb
@@ -1,3 +1,19 @@
+require_relative './team'
+require 'CSV'
+
 class TeamCollection
-  
+
+  attr_reader :teams
+
+  def initialize(csv_file_path)
+    @teams = create_teams(csv_file_path)
+  end
+
+  def create_teams(csv_file_path)
+    csv = CSV.read(csv_file_path, headers: true, header_converters: :symbol)
+    csv.map do |row|
+       Team.new(row)
+    end
+  end
+
 end
--- test/team_collection_test.rb
+++ test/team_collection_test.rb
@@ -1,5 +1,44 @@
 require "./test/test_helper"
+require "mocha/minitest"
+require "./lib/team_collection"
 
 class TeamCollectionTest < Minitest::Test
-  
+
+  def test_it_exists
+    TeamCollection.stubs(:create_teams).returns(Array.new(32, mock('team')))
+    stubbed_team_collection = TeamCollection.new("./data/teams.csv")
+    assert_instance_of TeamCollection, stubbed_team_collection
+  end
+
+  def test_it_has_attributes
+    TeamCollection.stubs(:create_teams).returns(Array.new(32, mock('team')))
+    stubbed_team_collection = TeamCollection.new("./data/teams.csv")
+
+    assert_instance_of Array, stubbed_team_collection.teams
+    assert_equal 32, stubbed_team_collection.teams.length
+  end
+
+  def test_csv_loads
+    team_collection = TeamCollection.new("./data/teams.csv")
+
+    team_a = team_collection.teams.first
+    team_z = team_collection.teams.last
+
+
+    assert_equal 1,                       team_a.team_id
+    assert_equal 23,                      team_a.franchiseId
+    assert_equal "Atlanta United",        team_a.teamName
+    assert_equal "ATL",                   team_a.abbreviation
+    assert_equal "Mercedes-Benz Stadium", team_a.stadium
+    assert_equal "/api/v1/teams/1",       team_a.link
+
+    assert_equal 53,                      team_z.team_id
+    assert_equal 28,                      team_z.franchiseId
+    assert_equal "Columbus Crew SC",      team_z.teamName
+    assert_equal "CCS",                   team_z.abbreviation
+    assert_equal "Mapfre Stadium",        team_z.stadium
+    assert_equal "/api/v1/teams/53",      team_z.link
+
+  end
+
 end
