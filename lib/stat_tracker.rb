abd509a76ab64f5070ebcf86ec023d7b7f5cac0f
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-02-19 21:57:27 -0700

    Pass best_offense test

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -18,4 +18,32 @@ class StatTracker
       CSV.foreach(file, csv_options) { |row| item_class.add(item_class.new(row.to_hash)) }
   end
 
+  def count_of_teams
+    Team.all.count
+  end
+
+  def best_offense
+    teams = change_data_to_array(Team)
+    games = change_data_to_array(Game)
+
+    best_team = teams.max_by do |team|
+      games_with_team = games.select do |game|
+        game.home_team_id == team.team_id || game.away_team_id == team.team_id
+      end
+      if !games_with_team.empty?
+        total_score = games_with_team.sum do |game|
+          game.home_team_id == team.team_id ? game.home_goals : game.away_goals
+        end
+        total_score / games_with_team.count
+      else
+        0
+      end
+    end
+    best_team.team_name
+  end
+
+  def change_data_to_array(data_class)
+    data_class.all.values
+  end
+
 end
