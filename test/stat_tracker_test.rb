ea8ed4d71de74d12bff823b5a7a619a3e024ccb3
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-20 15:12:07 -0700

    Remove simplecov and lower case csv

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -1,8 +1,10 @@
 require './lib/stat_tracker'
-require './lib/game'
-require './lib/game_teams'
 require './lib/game_collection'
 require './lib/game_teams_collection'
+require './lib/game_teams'
+require './lib/game'
+require './lib/team_collection'
+require './lib/team'
 require 'minitest/autorun'
 require 'minitest/pride'
 
@@ -20,10 +22,24 @@ class StatTrackerTest < Minitest::Test
     assert_instance_of StatTracker, stattracker
   end
 
-  def test_it_can_load_collections_of_data
+  def test_it_can_load_collections_of_game_team_data
     stattracker = StatTracker.new(@locations)
     stattracker.load_game_team_data
     assert_instance_of GameTeamsCollection, stattracker.gtc
     assert_equal GameTeams, stattracker.gtc.game_teams.first.class
+  end
+
+  def test_it_can_load_collections_of_game_data
+    stattracker = StatTracker.new(@locations)
+    stattracker.load_game_data
+    assert_instance_of GameCollection, stattracker.game_collection
+    assert_equal Game, stattracker.game_collection.games.first.class
+  end
+
+  def test_it_can_load_collections_of_team_data
+    stattracker = StatTracker.new(@locations)
+    stattracker.load_team_data
+    assert_instance_of TeamCollection, stattracker.team_collection
+    assert_equal Team, stattracker.team_collection.teams.first.class
   end
 end
