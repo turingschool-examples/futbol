dc6a87cd9c46c97427d8090fe66415c83466bb46
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-20 15:14:15 -0700

    Add functionality to stattracker to generate multiple collections

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,14 +1,24 @@
 class StatTracker
-  attr_reader :game_collection, :gtc
+  attr_reader :game_collection, :team_collection, :gtc
   def initialize(locations)
-    @game_data = locations[:games]
-    @teams_data = locations[:teams]
-    @game_teams_data = locations[:game_teams]
+    @game_file_path = locations[:games]
+    @teams_file_path = locations[:teams]
+    @game_teams_file_path = locations[:game_teams]
     @gtc = nil
+    @game_collection = nil
+    @team_collection = nil
   end
 
   def load_game_team_data
-    @gtc = GameTeamsCollection.new(@game_teams_data)
+    @gtc = GameTeamsCollection.new(@game_teams_file_path)
+  end
+
+  def load_game_data
+    @game_collection = GameCollection.new(@game_file_path)
+  end
+
+  def load_team_data
+    @team_collection = TeamCollection.new(@teams_file_path)
   end
 
 end
