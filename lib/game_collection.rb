5a050c5e7574f2927c03a359336ee788ef7acf52
Jesse Gietzen <gietzenjesse@gmail.com>
2020-02-20 14:49:35 -0700

    add create pct data and pct total games

--- lib/game_collection.rb
+++ lib/game_collection.rb
@@ -1,10 +1,11 @@
 require 'csv'
 
 class GameCollection
-  attr_reader :games_list
+  attr_reader :games_list, :pct_data
 
   def initialize(file_path)
     @games_list = create_games(file_path)
+    @pct_data = Hash.new { |hash, key| hash[key] = 0 }
   end
 
   def create_games(file_path)
@@ -14,4 +15,23 @@ class GameCollection
       Game.new(row)
     end
   end
+
+  def create_pct_data
+    @games_list.each do |game|
+      @pct_data[:total_games] += 1
+      if game.home_goals == game.away_goals
+        @pct_data[:ties] += 1
+      elsif game.home_goals > game.away_goals
+        @pct_data[:home_wins] += 1
+      else
+        @pct_data[:away_wins] += 1
+      end
+    end
+    @pct_data
+  end
+
+  def pct_of_total_games(outcome_type)
+    (@pct_data[outcome_type] / @pct_data[:total_games].to_f) * 100
+  end
+
 end
