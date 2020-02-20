dc5143f66b21a94ee4080486fa00f6d56abddc2d
Ethan Hocking <eshocking253@gmail.com>
2020-02-20 08:57:52 -0700

    Add attributes to Team class

--- lib/team.rb
+++ lib/team.rb
@@ -1,6 +1,17 @@
 class Team
+  attr_reader :team_id,
+              :franchiseId,
+              :teamName,
+              :abbreviation,
+              :stadium,
+              :link
+
   def initialize (team_params)
-
+    @team_id        = team_params[:team_id]
+    @franchiseId    = team_params[:franchiseId]
+    @teamName       = team_params[:teamName]
+    @abbreviation   = team_params[:abbreviation]
+    @stadium        = team_params[:stadium]
+    @link           = team_params[:link]
   end
-
 end
--- test/team_test.rb
+++ test/team_test.rb
@@ -1,20 +1,30 @@
 require "./test/test_helper"
 require_relative "../lib/team"
 
-class TeamTest < Minitest::Test 
+class TeamTest < Minitest::Test
 
   def setup
     @team = Team.new({
-      team_id: 1,
-      franchiseId: 23,
-      teamName: "Atlanta United",
-      abbreviation: "ATL",
-      stadium: "Mercedes-Benz Stadium",
-      link: "/api/v1/teams/1"
+      team_id:        1,
+      franchiseId:    23,
+      teamName:       "Atlanta United",
+      abbreviation:   "ATL",
+      stadium:        "Mercedes-Benz Stadium",
+      link:           "/api/v1/teams/1"
       })
   end
 
   def test_it_exists
     assert_instance_of Team, @team
+  end
+
+  def test_it_has_attributes
+    assert_equal @team.team_id,        1
+    assert_equal @team.franchiseId,    23
+    assert_equal @team.teamName,       "Atlanta United"
+    assert_equal @team.abbreviation,   "ATL"
+    assert_equal @team.stadium,        "Mercedes-Benz Stadium"
+    assert_equal @team.link,           "/api/v1/teams/1"
+
   end
 end
