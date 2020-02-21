c9b34b71ff6c8c7b918f079344e598b561c29bda
Stephanie Friend <stephaniefriend91@gmail.com>
2020-02-21 16:48:16 -0700

    Write more robust testing for main methods

--- lib/season_win.rb
+++ lib/season_win.rb
@@ -4,15 +4,14 @@ require_relative 'game_collection'
 
 class SeasonWin
 
-  attr_reader :team_id
-
-  def initialize(team_id)
-    @team_id = team_id
+  def initialize(team_file_path, game_team_file_path, game_file_path)
+    @game_collection = GameCollection.new(game_file_path)
+    @team_collection = TeamCollection.new(team_file_path)
+    @game_team_collection = GameTeamCollection.new(game_team_file_path)
   end
 
   def team_info(team_id)
-    team_collection = TeamCollection.new('./data/teams.csv')
-    team_collection.teams_list.reduce({}) do |acc, team|
+    @team_collection.teams_list.reduce({}) do |acc, team|
       if team_id == team.team_id.to_s
         acc = {"team_id" => team.team_id.to_s,
                "franchise_id" => team.franchise_id.to_s,
@@ -25,60 +24,56 @@ class SeasonWin
   end
 
   def best_season(team_id)
-    max_value = total_wins_by_team_per_season(team_id).max_by do |key, value|
+    max_value = average_wins_by_team_per_season(team_id).max_by do |key, value|
       value
     end
     max_value.first
   end
 
   def worst_season(team_id)
-    min_value = total_wins_by_team_per_season(team_id).min_by do |key, value|
+    min_value = average_wins_by_team_per_season(team_id).min_by do |key, value|
       value
     end
     min_value.first
   end
 
-  def game_id_by_season(team_id)
-    game_collection = GameCollection.new('./data/games.csv')
-    game_collection.games_list.reduce({}) do |acc, game|
-      game_id = game.game_id if game.away_team_id.to_s == team_id || game.home_team_id.to_s == team_id
-      next acc if game_id.nil?
-      if acc.include?(game.season)
-        acc[game.season] = acc[game.season] << game_id.to_s
-      else
-        acc[game.season] = [game_id.to_s]
-      end
-      acc
-    end
-  end
-
   def total_games_by_season(team_id)
-    total_games = {}
-    game_id_by_season(team_id).map do |season, game_id|
-      total_games[season] = game_id.length
-    end
-    total_games
+    games =  @game_team_collection.game_team_list.map do |game_team|
+      if game_team.team_id.to_s == team_id
+        game_team.game_id.to_s
+      end
+    end.compact
+    grouped_games = group_arrays_by_season(games)
+    transform_key_into_season(grouped_games)
   end
 
   def winning_game_ids(team_id)
-    game_team_collection = GameTeamCollection.new('./data/game_teams.csv')
-    game_team_collection.game_team_list.map do |game_team|
+    wins =  @game_team_collection.game_team_list.map do |game_team|
       if (game_team.team_id.to_s == team_id) && (game_team.result == "WIN")
         game_team.game_id.to_s
       end
     end.compact
+    grouped_wins = group_arrays_by_season(wins)
+    transform_key_into_season(grouped_wins)
   end
 
-  def total_wins_by_team_per_season(team_id)
-    wins = winning_game_ids(team_id).group_by do |game_id|
+  def group_arrays_by_season(game_id_array)
+    game_id_array.group_by do |game_id|
       game_id[0..3]
     end
-    total_won_games = {}
-    wins.map do |key, value|
-      total_won_games[key + (key.to_i + 1).to_s] = value.length
+  end
+
+  def transform_key_into_season(team_collection)
+    total = {}
+    team_collection.map do |key, value|
+      total[key + (key.to_i + 1).to_s] = value.length
     end
+    total
+  end
+
+  def average_wins_by_team_per_season(team_id)
     final_total_won_games = {}
-    total_won_games.map do |key, value|
+    winning_game_ids(team_id).map do |key, value|
       total_games = total_games_by_season(team_id)[key]
       if total_games != nil
         final_total_won_games[key] = ((value.to_f / total_games) * 100).round(2)
--- test/season_win_test.rb
+++ test/season_win_test.rb
@@ -4,7 +4,10 @@ require './lib/season_win'
 class SeasonWinTest < Minitest::Test
 
   def setup
-    @season_win = SeasonWin.new("18")
+    team_file_path = './data/teams.csv'
+    game_team_file_path = './test/fixtures/truncated_game_teams.csv'
+    game_file_path = './test/fixtures/truncated_games.csv'
+    @season_win = SeasonWin.new(team_file_path, game_team_file_path, game_file_path)
   end
 
   def test_it_exists
@@ -21,19 +24,31 @@ class SeasonWinTest < Minitest::Test
   end
 
   def test_it_can_return_best_season
-    assert_equal "20132014", @season_win.best_season("6")
+    assert_equal "20142015", @season_win.best_season("3")
   end
 
   def test_it_can_return_worst_season
-    assert_equal "20142015", @season_win.worst_season("6")
+    assert_equal "20122013", @season_win.worst_season("3")
   end
 
-  def test_it_can_return_game_id_by_season
-    assert_instance_of Hash, @season_win.game_id_by_season("6")
-    assert_equal ["20122013", "20172018", "20132014", "20142015", "20152016", "20162017"], @season_win.game_id_by_season("6").keys
-    assert_equal 70, @season_win.game_id_by_season("6")["20122013"].length
-    assert_equal "2012030221", @season_win.game_id_by_season("6")["20122013"].first
+  def test_it_can_return_total_games_played
+    assert_instance_of Hash, @season_win.total_games_by_season("3")
+    assert_equal 12, @season_win.total_games_by_season("3")["20122013"]
+    assert_equal 11, @season_win.total_games_by_season("3")["20142015"]
+    assert_equal 5, @season_win.total_games_by_season("3")["20152016"]
   end
 
+  def test_it_can_return_winning_game_ids
+    assert_instance_of Hash, @season_win.winning_game_ids("3")
+    assert_equal 2, @season_win.winning_game_ids("3").length
+    assert_equal 2, @season_win.winning_game_ids("3")["20122013"]
+    assert_equal 8, @season_win.winning_game_ids("3")["20142015"]
+    assert_equal ["20122013", "20142015"], @season_win.winning_game_ids("3").keys
+  end
 
+  def test_it_can_return_average_wins
+    assert_instance_of Hash, @season_win.average_wins_by_team_per_season("3")
+    assert_equal 16.67, @season_win.average_wins_by_team_per_season("3")["20122013"]
+    assert_equal 72.73, @season_win.average_wins_by_team_per_season("3")["20142015"]
+  end
 end
