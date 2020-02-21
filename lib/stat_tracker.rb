ccad27ec224d9d28c1add147e28ae286f44430b5
Colin Alexander <colintalex@gmail.com>
2020-02-20 22:30:47 -0700

    Cleans up code and extra lines. No code changes.

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -4,15 +4,6 @@ require './lib/game_collection'
 require './lib/team_collection'
 
 class StatTracker
-  attr_reader :game_collection, :team_collection, :gtc
-  def initialize(raw_data)
-    @game_data = raw_data[:game_data]
-    @team_data = raw_data[:team_data]
-    @game_teams_data = raw_data[:game_teams_data]
-    @gtc = nil
-    @game_collection = nil
-    @team_collection = nil
-  end
 
   def self.from_csv(locations)
     raw_data = {}
@@ -23,21 +14,20 @@ class StatTracker
     StatTracker.new(raw_data)
   end
 
-  def load_data
-    load_game_data
-    load_team_data
-    load_game_team_data
+  attr_reader :game_collection, :team_collection, :gtc
+  def initialize(raw_data)
+    @game_data = raw_data[:game_data]
+    @team_data = raw_data[:team_data]
+    @game_teams_data = raw_data[:game_teams_data]
+    @gtc = nil
+    @game_collection = nil
+    @team_collection = nil
+    construct_collections
   end
 
-  def load_game_team_data
+  def construct_collections
     @gtc = GameTeamsCollection.new(@game_teams_data)
-  end
-
-  def load_game_data
     @game_collection = GameCollection.new(@game_data)
-  end
-
-  def load_team_data
     @team_collection = TeamCollection.new(@team_data)
   end
 
