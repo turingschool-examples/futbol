a9b9b23417a51841f0c762668357ddc4e246baa3
Kelsha_Darby <ohanaipo@gmail.com>
2020-02-20 18:01:36 -0700

    Add helper methods for best_offense. Add test for helper methods.

--- lib/league_statistics.rb
+++ lib/league_statistics.rb
@@ -1,151 +1,192 @@
+require './lib/game'
+
 class LeagueStatistics
 
-  def count_of_teams
-    @teams.all.size
+  def initialize
+    @teams = Team.all
+    @games = Game.all
+    @game_teams = GameTeam.all
   end
-end
 
-# best_offense
-# 	Name of the team with the highest average number of goals scored
-#   per game across all seasons.
-# 	String
-def goal_average # module?
-  game_teams.goals per game
-  goals_per_game = @game_teams.game_id.each(&:goals)
-end
+  def count_of_teams
+    @teams.size
+  end
 
-def best_offense
-  max_by average
-  return match of game_teams.team_id and team.id
-end
+  def find_team_names(team_id)
+    match_team = @teams.find do |team|
+      team.team_id == team_id
+    end
+    match_team.team_name
+  end
+# =====================================================
+  def goals_per_team
+    @game_teams.reduce({}) do |goals_per_team, game_team|
+      goals_per_team[game_team.team_id] = [] if goals_per_team[game_team.team_id].nil?
+      goals_per_team[game_team.team_id] << game_team.goals
+      goals_per_team
+    end
+  end
 
-# worst_offense
-# 	Name of the team with the lowest average number of goals scored
-#   per game across all seasons.
-# 	String
-def average #module?
-  game_teams.goals
-end
+  def sum_goals_per_team
+    goals_per_team.transform_values do |goals|
+      goals.sum
+    end
+  end
 
-def worst_offense
-  min_by average
-  return match of game_teams.id and team.id
-end
+  def average_goals_per_team
+    require "pry"; binding.pry
+    # sum_goals_per_team
+  end
 
-# best_defense
-# 	Name of the team with the lowest average number of goals
-#   allowed per game across all seasons.
-# 	String
-#scored against them
-def average #module?
-  games_teams.goals(for home and away)
 end
-
-def best_defense
-  min_by average
-  return teams.team_name by game_teams.id
-end
-
-# worst_defense
-# 	Name of the team with the highest average number of goals
-#   allowed per game across all seasons.
-# 	String
-def average #module?
-  games_teams.goals(for home and away)
-end
-
-def worst_defense
-  max_by average
-  return teams.team_name by game_teams.id
-end
-
-# highest_scoring_visitor
-# 	Name of the team with the highest average score per game
-#   across all seasons when they are away.
-# 	String
-def average #module?
-  games_teams.goals(away)
-end
-
-def highest_scoring_visitor
-  max_by average
-  return teams.team_name by game_teams.id
-end
-
-# highest_scoring_home_team
-# 	Name of the team with the highest average score per game
-#   across all seasons when they are home.
-# 	String
-def average #module?
-  games_teams.goals(home)
-end
-
-def highest_scoring_home_team
-  max_by average
-  return teams.team_name by game_teams.id
-end
-
-# lowest_scoring_visitor
-# 	Name of the team with the lowest average score per game
-#   across all seasons when they are a visitor.
-# 	String
-def average #module?
-  games_teams.goals(away)
-end
-
-def lowest_scoring_visitor
-  min_by average
-  return teams.team_name by game_teams.id
-end
-
-# lowest_scoring_home_team
-# 	Name of the team with the lowest average score per game across all seasons when they are at home.
-# 	String
-def average #module?
-  games_teams.goals(home)
-end
-
-def lowest_scoring_home_team
-  min_by average
-  return teams.team_name by game_teams.id
-end
-
-# winningest_team
-# 	Name of the team with the highest win percentage across
-#   all seasons.
-# 	String
-def percentage # module?
-  game_teams.result == win
-end
-
-def winningest_team
-  max_by percentage
-  return teams.team_name by game_teams.team_id
-end
-
-# best_fans
-# 	Name of the team with biggest difference between home
-#   and away win percentages.
-# 	String
-def percentage # module?
-  game_teams.result == win (one for home, one for away?)
-end
-
-def best_fans
-  max_by away_percentage - home_percentage
-  return teams.team_name by game_teams.team_id
-end
-
-# worst_fans
-# 	List of names of all teams with better away records
-#   than home records.
-# 	Array
-def percentage # module?
-  game_teams.result == win (one for home, one for away?)
-end
-
-def worst_fans
-  find_all
-  away_percentage > home_percentage
-  match array with teams.team_name by game_teams.team_id
-end
+  #
+  # def best_offense
+  #   max_by average
+  #   return match of game_teams.team_id and team.id
+  # end
+# end
+#
+# # best_offense
+# # 	Name of the team with the highest average number of goals scored
+# #   per game across all seasons.
+# # 	String
+# def goal_average # module?
+#   game_teams.goals per game
+#   goals_per_game = @game_teams.game_id.each(&:goals)
+# end
+#
+# def best_offense
+#   max_by average
+#   return match of game_teams.team_id and team.id
+# end
+# #
+# # # worst_offense
+# # 	Name of the team with the lowest average number of goals scored
+# #   per game across all seasons.
+# # 	String
+# def average #module?
+#   game_teams.goals
+# end
+#
+# def worst_offense
+#   min_by average
+#   return match of game_teams.id and team.id
+# end
+#
+# # best_defense
+# # 	Name of the team with the lowest average number of goals
+# #   allowed per game across all seasons.
+# # 	String
+# #scored against them
+# def average #module?
+#   games_teams.goals(for home and away)
+# end
+#
+# def best_defense
+#   min_by average
+#   return teams.team_name by game_teams.id
+# end
+#
+# # worst_defense
+# # 	Name of the team with the highest average number of goals
+# #   allowed per game across all seasons.
+# # 	String
+# def average #module?
+#   games_teams.goals(for home and away)
+# end
+#
+# def worst_defense
+#   max_by average
+#   return teams.team_name by game_teams.id
+# end
+#
+# # highest_scoring_visitor
+# # 	Name of the team with the highest average score per game
+# #   across all seasons when they are away.
+# # 	String
+# def average #module?
+#   games_teams.goals(away)
+# end
+#
+# def highest_scoring_visitor
+#   max_by average
+#   return teams.team_name by game_teams.id
+# end
+#
+# # highest_scoring_home_team
+# # 	Name of the team with the highest average score per game
+# #   across all seasons when they are home.
+# # 	String
+# def average #module?
+#   games_teams.goals(home)
+# end
+#
+# def highest_scoring_home_team
+#   max_by average
+#   return teams.team_name by game_teams.id
+# end
+#
+# # lowest_scoring_visitor
+# # 	Name of the team with the lowest average score per game
+# #   across all seasons when they are a visitor.
+# # 	String
+# def average #module?
+#   games_teams.goals(away)
+# end
+#
+# def lowest_scoring_visitor
+#   min_by average
+#   return teams.team_name by game_teams.id
+# end
+#
+# # lowest_scoring_home_team
+# # 	Name of the team with the lowest average score per game across all seasons when they are at home.
+# # 	String
+# def average #module?
+#   games_teams.goals(home)
+# end
+#
+# def lowest_scoring_home_team
+#   min_by average
+#   return teams.team_name by game_teams.id
+# end
+#
+# # winningest_team
+# # 	Name of the team with the highest win percentage across
+# #   all seasons.
+# # 	String
+# def percentage # module?
+#   game_teams.result == win
+# end
+#
+# def winningest_team
+#   max_by percentage
+#   return teams.team_name by game_teams.team_id
+# end
+#
+# # best_fans
+# # 	Name of the team with biggest difference between home
+# #   and away win percentages.
+# # 	String
+# def percentage # module?
+#   game_teams.result == win (one for home, one for away?)
+# end
+#
+# def best_fans
+#   max_by away_percentage - home_percentage
+#   return teams.team_name by game_teams.team_id
+# end
+#
+# # worst_fans
+# # 	List of names of all teams with better away records
+# #   than home records.
+# # 	Array
+# def percentage # module?
+#   game_teams.result == win (one for home, one for away?)
+# end
+#
+# def worst_fans
+#   find_all
+#   away_percentage > home_percentage
+#   match array with teams.team_name by game_teams.team_id
+# end
--- test/league_statistics_test.rb
+++ test/league_statistics_test.rb
@@ -11,55 +11,108 @@ class Test < Minitest::Test
     game_path = './test/fixtures/games_truncated.csv'
     team_path = './test/fixtures/teams_truncated.csv'
     game_teams_path = './test/fixtures/game_teams_truncated.csv'
+    Game.create_games(game_path)
+    Team.create_teams(team_path)
+    GameTeam.create_game_teams(game_teams_path)
 
-    @stat_tracker = StatTracker.from_csv(game_path, team_path, game_teams_path)
+    @league = LeagueStatistics.new
   end
 
   def test_it_can_count_teams
-    assert_equal 5, @league.count_of_teams
+    assert_equal 32, @league.count_of_teams
+  end
+
+  def test_it_can_find_team_names
+    assert_equal "FC Dallas", @league.find_team_names(6)
+  end
+
+  def test_it_can_calculate_goals_per_team
+    expected = {
+      3=>[2, 2, 1, 2, 1],
+      6=>[3, 3, 2, 3, 3, 3, 4, 2, 1],
+      5=>[0, 1, 1, 0],
+      17=>[1, 2, 3, 2, 1, 3, 1],
+      16=>[2, 1, 1, 0, 2, 2, 2],
+      9=>[2, 1, 4],
+      8=>[2, 3, 1, 2]
+    }
+    assert_equal expected, @league.goals_per_team
+  end
+
+  def test_it_can_sum_goals_per_team
+    expected = {
+      3=>8,
+      6=>24,
+      5=>2,
+      17=>13,
+      16=>10,
+      9=>7,
+      8=>8
+    }
+    assert_equal expected, @league.sum_goals_per_team
+  end
+
+  def test_it_can_average_goals_per_team
+    assert_equal [], @league.average_goals_per_team
+  end
+
+  def test_it_can_average_goals_per_team
+    skip
+    assert_equal 12, @league.average_goals_per_team
   end
 
   def test_it_can_tell_best_offense
+    skip
     assert_equal "FC Dallas", @league.best_offense
   end
 
   def test_it_can_tell_worst_offense
+    skip
     assert_equal "", @league.worst_offense
   end
 
   def test_it_can_tell_best_defense
+    skip
     assert_equal "", @league.best_defense
   end
 
   def test_it_can_tell_worst_defense
+    skip
     assert_equal "", @league.worst_defense
   end
 
   def test_it_can_tell_highest_scoring_visitor
+    skip
     assert_equal "", @league.highest_scoring_visitor
   end
 
   def test_highest_scoring_home_team
+    skip
     assert_equal "", @league.highest_scoring_home_team
   end
 
   def test_lowest_scoring_visitor
+    skip
     assert_equal "", @league.lowest_scoring_visitor
   end
 
   def test_it_can_tell_lowest_scoring_home_team
+    skip
     assert_equal "", @league.lowest_scoring_home_team
   end
 
   def test_it_can_tell_winningest_team
+    skip
     assert_equal "", @league.winningest_team
   end
 
   def test_it_can_tell_best_fans
+    skip
     assert_equal "", @league.best_fans
   end
 
   def test_it_can_tell_worst_fans
+    skip
     assert_equal [], @league.worst_fans
   end
 end
