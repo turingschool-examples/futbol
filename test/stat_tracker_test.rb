1235709b01c5cd1c7ad5271172aabfa2e5994b0a
Colin Alexander <colintalex@gmail.com>
2020-02-20 22:31:40 -0700

    Adjusts test method for refacted StatTracker class.

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -15,31 +15,22 @@ class StatTrackerTest < Minitest::Test
       teams: './data/teams.csv',
       game_teams: './data/little_game_teams.csv'
     }
+    @stat_tracker = StatTracker.from_csv(@locations)
   end
 
   def test_it_exists
-    stattracker = StatTracker.new(@locations)
-    assert_instance_of StatTracker, stattracker
+    assert_instance_of StatTracker, @stat_tracker
   end
 
   def test_it_can_load_collections_of_game_team_data
-    stattracker = StatTracker.from_csv(@locations)
-    stattracker.load_game_team_data
-    assert_instance_of GameTeamsCollection, stattracker.gtc
-    assert_equal GameTeams, stattracker.gtc.game_teams.first.class
+    @stat_tracker = StatTracker.from_csv(@locations)
+    @stat_tracker.construct_collections
+    assert_instance_of GameTeamsCollection, @stat_tracker.gtc
+    assert_equal GameTeams, @stat_tracker.gtc.game_teams.first.class
+    assert_instance_of GameCollection, @stat_tracker.game_collection
+    assert_equal Game, @stat_tracker.game_collection.games.first.class
+    assert_instance_of TeamCollection, @stat_tracker.team_collection
+    assert_equal Team, @stat_tracker.team_collection.teams.first.class
   end
 
-  def test_it_can_load_collections_of_game_data
-    stattracker = StatTracker.from_csv(@locations)
-    stattracker.load_game_data
-    assert_instance_of GameCollection, stattracker.game_collection
-    assert_equal Game, stattracker.game_collection.games.first.class
-  end
-
-  def test_it_can_load_collections_of_team_data
-    stattracker = StatTracker.from_csv(@locations)
-    stattracker.load_team_data
-    assert_instance_of TeamCollection, stattracker.team_collection
-    assert_equal Team, stattracker.team_collection.teams.first.class
-  end
 end
