d7f3e6da421aec0a990fd27ef295a5b11699b817
Maxwell Baird <bairdm71@gmail.com>
2020-02-21 13:38:08 -0700

    added an attr_reader for test

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -6,6 +6,7 @@ require_relative 'game'
 
 class StatTracker
   include DataLoadable
+  attr_reader :games_path, :team_path, :game_teams_path
 
   def self.from_csv(locations)
     StatTracker.new(locations)
