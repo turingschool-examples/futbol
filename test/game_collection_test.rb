4299043294e97d8babd3dc41f723a77b19aacbad
Colin Alexander <colintalex@gmail.com>
2020-02-20 23:03:04 -0700

    Resolves testing gaps from refactoring from_csv

--- test/game_collection_test.rb
+++ test/game_collection_test.rb
@@ -6,12 +6,16 @@ require 'csv'
 class GameCollectionTest < Minitest::Test
   def setup
     @game_file_path = './data/little_games.csv'
-    @game_collection = GameCollection.new(@game_file_path)
+    @game_data = CSV.read('./data/little_games.csv',
+                 headers: true,
+                 header_converters: :symbol)
+    @game_collection = GameCollection.new(@game_data)
 
   end
 
   def test_it_exists
     assert_instance_of GameCollection, @game_collection
+    assert_instance_of Game, @game_collection.games.first
     assert_equal 4, @game_collection.games.length
   end
 end
