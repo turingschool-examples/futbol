ff3728937b33ddd79820c922410340050cbc3c52
Colin Alexander <colintalex@gmail.com>
2020-02-20 20:08:48 -0700

    Fixes test methods, calls .from_csv instead of .new

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -23,21 +23,21 @@ class StatTrackerTest < Minitest::Test
   end
 
   def test_it_can_load_collections_of_game_team_data
-    stattracker = StatTracker.new(@locations)
+    stattracker = StatTracker.from_csv(@locations)
     stattracker.load_game_team_data
     assert_instance_of GameTeamsCollection, stattracker.gtc
     assert_equal GameTeams, stattracker.gtc.game_teams.first.class
   end
 
   def test_it_can_load_collections_of_game_data
-    stattracker = StatTracker.new(@locations)
+    stattracker = StatTracker.from_csv(@locations)
     stattracker.load_game_data
     assert_instance_of GameCollection, stattracker.game_collection
     assert_equal Game, stattracker.game_collection.games.first.class
   end
 
   def test_it_can_load_collections_of_team_data
-    stattracker = StatTracker.new(@locations)
+    stattracker = StatTracker.from_csv(@locations)
     stattracker.load_team_data
     assert_instance_of TeamCollection, stattracker.team_collection
     assert_equal Team, stattracker.team_collection.teams.first.class
