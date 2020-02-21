140cfc0b5c039b18676b1662624563b7d0d930cf
Maxwell Baird <bairdm71@gmail.com>
2020-02-21 10:48:25 -0700

    added spacing

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -24,57 +24,75 @@ class StatTracker
   def count_of_teams
     @team_stats.count_of_teams
   end
+
   def best_offense
     @team_stats.find_name(@game_teams_stats.best_offense)
   end
+
   def worst_offense
     @team_stats.find_name(@game_teams_stats.worst_offense)
   end
+
   def highest_scoring_visitor
     @team_stats.find_name(@game_teams_stats.highest_scoring_visitor)
   end
+
   def highest_scoring_home_team
     @team_stats.find_name(@game_teams_stats.highest_scoring_home_team)
   end
+
   def lowest_scoring_visitor
     @team_stats.find_name(@game_teams_stats.lowest_scoring_visitor)
   end
+
   def lowest_scoring_home_team
     @team_stats.find_name(@game_teams_stats.lowest_scoring_home_team)
   end
+
   def winningest_team
     @team_stats.find_name(@games_stats.winningest_team)
   end
+
   def best_fans
     @team_stats.find_name(@game_teams_stats.best_fans)
   end
+
   def worst_fans
     @game_teams_stats.worst_fans.map {|fan| @team_stats.find_name(fan)}
   end
+
   def percentage_ties
     @games_stats.percentage_ties
   end
+
   def count_of_games_by_season
     @games_stats.count_of_games_by_season
   end
+
   def percentage_home_wins
     @games_stats.percentage_home_wins
   end
+
   def percentage_visitor_wins
     @games_stats.percentage_visitor_wins
   end
+
   def average_goals_per_game
     @games_stats.average_goals_per_game
   end
+
   def average_goals_by_season
     @games_stats.average_goals_by_season
   end
+
   def highest_total_score
     @games_stats.highest_total_score
   end
+
   def lowest_total_score
     @games_stats.lowest_total_score
   end
+
   def biggest_blowout
     @games_stats.biggest_blowout
   end
