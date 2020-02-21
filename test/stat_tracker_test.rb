75702012345393e4a8269d045a0f9200acd98d2f
Mike Hernandez <hernandezmichael321@gmail.com>
2020-02-20 19:21:18 -0700

    Create test for highest score

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -15,31 +15,32 @@ class StatTrackerTest < Minitest::Test
       teams: './data/teams.csv',
       game_teams: './data/little_game_teams.csv'
     }
+    @stattracker = StatTracker.new(@locations)
+    @stattracker.load_game_data
+    @stattracker.load_game_team_data
+    @stattracker.load_team_data
   end
 
   def test_it_exists
-    stattracker = StatTracker.new(@locations)
-    assert_instance_of StatTracker, stattracker
+    assert_instance_of StatTracker, @stattracker
   end
 
   def test_it_can_load_collections_of_game_team_data
-    stattracker = StatTracker.new(@locations)
-    stattracker.load_game_team_data
-    assert_instance_of GameTeamsCollection, stattracker.gtc
-    assert_equal GameTeams, stattracker.gtc.game_teams.first.class
+    assert_instance_of GameTeamsCollection, @stattracker.gtc
+    assert_equal GameTeams, @stattracker.gtc.game_teams.first.class
   end
 
   def test_it_can_load_collections_of_game_data
-    stattracker = StatTracker.new(@locations)
-    stattracker.load_game_data
-    assert_instance_of GameCollection, stattracker.game_collection
-    assert_equal Game, stattracker.game_collection.games.first.class
+    assert_instance_of GameCollection, @stattracker.game_collection
+    assert_equal Game, @stattracker.game_collection.games.first.class
   end
 
   def test_it_can_load_collections_of_team_data
-    stattracker = StatTracker.new(@locations)
-    stattracker.load_team_data
-    assert_instance_of TeamCollection, stattracker.team_collection
-    assert_equal Team, stattracker.team_collection.teams.first.class
+    assert_instance_of TeamCollection, @stattracker.team_collection
+    assert_equal Team, @stattracker.team_collection.teams.first.class
+  end
+
+  def test_it_can_return_the_highest_total_score
+    assert_equal 5, @stattracker.highest_total_score
   end
 end
