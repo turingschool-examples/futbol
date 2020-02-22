dbfabfcd1dcf4d59d750b51618909863bad670f9
David Tran <davidttemail@gmail.com>
2020-02-22 13:28:15 -0700

    Refactor methods into helper methods

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,4 +1,8 @@
-# require "./lib/modules/calculable"
+require 'csv'
+# require './lib/modules/calculable'
+require_relative 'game'
+require_relative 'game_team'
+require_relative 'team'
 
 class StatTracker
   # include Calculable
@@ -28,7 +32,7 @@ class StatTracker
       if function == "add"
         total_scores << value.home_goals + value.away_goals
       elsif function == "subtract"
-        total_scores << value.home_goals - value.away_goals
+        total_scores << (value.home_goals - value.away_goals).abs
       end
     end
     total_scores.uniq.sort
@@ -43,7 +47,7 @@ class StatTracker
   end
 
   def biggest_blowout
-    sort(Game, "subtract").min.abs
+    sort(Game, "subtract").max
   end
 
   def games_outcome_percent(outcome = nil)
@@ -57,7 +61,7 @@ class StatTracker
         games_count += 1
       end
     end
-    (games_count / Game.all.length * 100).round(2)
+    (games_count / Game.all.length).round(2)
   end
 
   def percentage_home_wins
@@ -73,11 +77,10 @@ class StatTracker
   end
 
   def count_of_games_by_season
-    count = Game.all.values.reduce(Hash.new(0)) do |games_by_season, game|
-      games_by_season[game.season] += 1
+    Game.all.values.reduce(Hash.new(0)) do |games_by_season, game|
+      games_by_season[game.season.to_s] += 1
       games_by_season
     end
-    count
   end
 
   def average_goals_per_game
@@ -109,11 +112,11 @@ class StatTracker
   end
 
   def average_goals_by_season
-    Game.all.values.reduce(Hash.new(0)) do |goals_by_season, game|
+    Game.all.each_value.reduce(Hash.new(0)) do |goals_by_season, game|
       goals = total_goals_per_season(game.season)
       games = number_of_games_per_season(game.season)
 
-      goals_by_season[game.season] = (goals / games).round(2)
+      goals_by_season[game.season.to_s] = (goals / games).round(2)
       goals_by_season
     end
   end
