9bce37f3570dad7722340b23779bd3cb48e3c2be
Jack Puchalla <Jackpuchallalab@gmail.com>
2020-02-25 11:06:42 -0700

    refactored test_biggest_blowout

--- test/team_stat_test.rb
+++ test/team_stat_test.rb
@@ -26,9 +26,9 @@ class TeamStatTest < Minitest::Test
   def test_lowest_total_score
     assert_equal 0, @game.lowest_total_score
   end
-  #
-  # def test_biggest_blowout
-  #   assert_equal 8, @game.biggest_blowout
-  # end
+
+  def test_biggest_blowout
+    assert_equal 3, @game.biggest_blowout
+  end
 
 end
