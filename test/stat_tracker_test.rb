5e6697726f4bd316fd0e74f221add395c77bcb10
David Tran <davidttemail@gmail.com>
2020-02-25 15:03:20 -0700

    Resolve merge conflicts with iteration 5

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -80,99 +80,99 @@ class StatTrackerTest < Minitest::Test
       "link" => "/api/v1/teams/18"
     }
 
-    assert_instance_of Hash, @stat_tracker.team_info("18")
-    assert_equal expected, @stat_tracker.team_info("18")
-    assert_equal 5, @stat_tracker.team_info("18").count
-    assert_equal 5, @stat_tracker.team_info("5").count
+    assert_instance_of Hash, @@stat_tracker.team_info("18")
+    assert_equal expected, @@stat_tracker.team_info("18")
+    assert_equal 5, @@stat_tracker.team_info("18").count
+    assert_equal 5, @@stat_tracker.team_info("5").count
   end
 
   def test_it_can_get_best_season
-    assert_equal "20142015", @stat_tracker.best_season("3")
-    assert_equal "20162017", @stat_tracker.best_season("5")
-    assert_equal "20132014", @stat_tracker.best_season("6")
+    assert_equal "20142015", @@stat_tracker.best_season("3")
+    assert_equal "20162017", @@stat_tracker.best_season("5")
+    assert_equal "20132014", @@stat_tracker.best_season("6")
   end
 
   def test_it_can_get_worst_season
-    assert_equal "20122013", @stat_tracker.worst_season("3")
-    assert_equal "20122013", @stat_tracker.worst_season("5")
-    assert_equal "20142015", @stat_tracker.worst_season("6")
+    assert_equal "20122013", @@stat_tracker.worst_season("3")
+    assert_equal "20122013", @@stat_tracker.worst_season("5")
+    assert_equal "20142015", @@stat_tracker.worst_season("6")
   end
 
   def test_it_can_get_average_win_percentage_by_team_id
-    assert_equal 0.36, @stat_tracker.average_win_percentage("1")
-    assert_equal 0.43, @stat_tracker.average_win_percentage("3")
-    assert_equal 0.48, @stat_tracker.average_win_percentage("5")
-    assert_equal 0.49, @stat_tracker.average_win_percentage("6")
+    assert_equal 0.36, @@stat_tracker.average_win_percentage("1")
+    assert_equal 0.43, @@stat_tracker.average_win_percentage("3")
+    assert_equal 0.48, @@stat_tracker.average_win_percentage("5")
+    assert_equal 0.49, @@stat_tracker.average_win_percentage("6")
   end
 
   def test_it_can_get_most_goals_scored_by_team_id
-    assert_equal 6, @stat_tracker.most_goals_scored("3")
-    assert_equal 6, @stat_tracker.most_goals_scored("6")
-    assert_equal 7, @stat_tracker.most_goals_scored("18")
+    assert_equal 6, @@stat_tracker.most_goals_scored("3")
+    assert_equal 6, @@stat_tracker.most_goals_scored("6")
+    assert_equal 7, @@stat_tracker.most_goals_scored("18")
   end
 
   def test_it_can_get_fewest_goals_scored
-    assert_equal 0, @stat_tracker.fewest_goals_scored("3")
-    assert_equal 0, @stat_tracker.fewest_goals_scored("6")
-    assert_equal 0, @stat_tracker.fewest_goals_scored("18")
+    assert_equal 0, @@stat_tracker.fewest_goals_scored("3")
+    assert_equal 0, @@stat_tracker.fewest_goals_scored("6")
+    assert_equal 0, @@stat_tracker.fewest_goals_scored("18")
   end
 
   def test_it_can_get_biggest_team_blowout
-    assert_equal 4, @stat_tracker.biggest_team_blowout("3")
-    assert_equal 4, @stat_tracker.biggest_team_blowout("6")
-    assert_equal 5, @stat_tracker.biggest_team_blowout("18")
+    assert_equal 4, @@stat_tracker.biggest_team_blowout("3")
+    assert_equal 4, @@stat_tracker.biggest_team_blowout("6")
+    assert_equal 5, @@stat_tracker.biggest_team_blowout("18")
   end
 
   def test_it_can_get_worst_loss
-    assert_equal 5, @stat_tracker.worst_loss("3")
-    assert_equal 5, @stat_tracker.worst_loss("6")
-    assert_equal 4, @stat_tracker.worst_loss("18")
+    assert_equal 5, @@stat_tracker.worst_loss("3")
+    assert_equal 5, @@stat_tracker.worst_loss("6")
+    assert_equal 4, @@stat_tracker.worst_loss("18")
   end
 
   def test_it_can_get_all_game_teams_by_team_id_in_array
-    assert_instance_of Array, @stat_tracker.all_game_teams_by_team_id("3")
-    assert_equal 531, @stat_tracker.all_game_teams_by_team_id("3").length
+    assert_instance_of Array, @@stat_tracker.all_game_teams_by_team_id("3")
+    assert_equal 531, @@stat_tracker.all_game_teams_by_team_id("3").length
 
-    assert_instance_of Array, @stat_tracker.all_game_teams_by_team_id("5")
-    assert_equal 552, @stat_tracker.all_game_teams_by_team_id("5").length
+    assert_instance_of Array, @@stat_tracker.all_game_teams_by_team_id("5")
+    assert_equal 552, @@stat_tracker.all_game_teams_by_team_id("5").length
   end
 
   def test_it_can_get_all_games_by_team_id_in_array
-    assert_instance_of Array, @stat_tracker.all_games_by_team_id("3")
-    assert_equal 531, @stat_tracker.all_games_by_team_id("3").length
+    assert_instance_of Array, @@stat_tracker.all_games_by_team_id("3")
+    assert_equal 531, @@stat_tracker.all_games_by_team_id("3").length
 
-    assert_instance_of Array, @stat_tracker.all_games_by_team_id("5")
-    assert_equal 552, @stat_tracker.all_games_by_team_id("5").length
+    assert_instance_of Array, @@stat_tracker.all_games_by_team_id("5")
+    assert_equal 552, @@stat_tracker.all_games_by_team_id("5").length
   end
 
   def test_it_can_get_total_results_by_team_id
-    assert_instance_of Array, @stat_tracker.total_results_by_team_id("3")
-    assert_equal 531, @stat_tracker.total_results_by_team_id("3").length
+    assert_instance_of Array, @@stat_tracker.total_results_by_team_id("3")
+    assert_equal 531, @@stat_tracker.total_results_by_team_id("3").length
 
-    assert_instance_of Array, @stat_tracker.total_results_by_team_id("5")
-    assert_equal 552, @stat_tracker.total_results_by_team_id("5").length
+    assert_instance_of Array, @@stat_tracker.total_results_by_team_id("5")
+    assert_equal 552, @@stat_tracker.total_results_by_team_id("5").length
   end
 
   def test_it_can_get_all_goals_scored_by_team_id
-    assert_instance_of Array, @stat_tracker.all_goals_scored_by_team_id("3")
-    assert_equal 531, @stat_tracker.all_goals_scored_by_team_id("3").length
-    assert_equal 2, @stat_tracker.all_goals_scored_by_team_id("3").first
-    assert_equal 3, @stat_tracker.all_goals_scored_by_team_id("6").first
-    assert_equal 0, @stat_tracker.all_goals_scored_by_team_id("5").first
+    assert_instance_of Array, @@stat_tracker.all_goals_scored_by_team_id("3")
+    assert_equal 531, @@stat_tracker.all_goals_scored_by_team_id("3").length
+    assert_equal 2, @@stat_tracker.all_goals_scored_by_team_id("3").first
+    assert_equal 3, @@stat_tracker.all_goals_scored_by_team_id("6").first
+    assert_equal 0, @@stat_tracker.all_goals_scored_by_team_id("5").first
   end
 
   def test_it_can_get_score_differences_by_team_id
-    assert_equal 531, @stat_tracker.score_differences_by_team_id("3").length
-    assert_equal (-1), @stat_tracker.score_differences_by_team_id("3").first
+    assert_equal 531, @@stat_tracker.score_differences_by_team_id("3").length
+    assert_equal (-1), @@stat_tracker.score_differences_by_team_id("3").first
 
-    assert_equal 552, @stat_tracker.score_differences_by_team_id("5").length
-    assert_equal (-3), @stat_tracker.score_differences_by_team_id("5").first
+    assert_equal 552, @@stat_tracker.score_differences_by_team_id("5").length
+    assert_equal (-3), @@stat_tracker.score_differences_by_team_id("5").first
   end
 
   def test_it_can_get_team_name
-    assert_equal "Houston Dynamo", @stat_tracker.get_team_name("3")
-    assert_equal "FC Dallas", @stat_tracker.get_team_name("6")
-    assert_equal "Reign FC", @stat_tracker.get_team_name("54")
+    assert_equal "Houston Dynamo", @@stat_tracker.get_team_name("3")
+    assert_equal "FC Dallas", @@stat_tracker.get_team_name("6")
+    assert_equal "Reign FC", @@stat_tracker.get_team_name("54")
   end
 
   def test_it_can_get_win_percentage_by_season
@@ -185,8 +185,8 @@ class StatTrackerTest < Minitest::Test
       "20162017"=>0.083
     }
 
-    assert_instance_of Hash, @stat_tracker.win_percentage_by_season("3")
-    assert_equal expected, @stat_tracker.win_percentage_by_season("3")
+    assert_instance_of Hash, @@stat_tracker.win_percentage_by_season_by_team_id("3")
+    assert_equal expected, @@stat_tracker.win_percentage_by_season_by_team_id("3")
   end
 
   def test_it_can_calc_win_percentage
