c934be0bfaf28b9ac6517a70573693245e50f7ed
Jesse Gietzen <gietzenjesse@gmail.com>
2020-02-25 16:41:33 -0700

    fix stat_tracker_tests

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -169,19 +169,19 @@ class StatTrackerTest < Minitest::Test
   end
 
   def test_it_returns_highest_scoring_visitor
-    assert_equal "FC Cincinnati", @stat_tracker.highest_scoring_visitor
+    assert_equal "FC Dallas", @stat_tracker.highest_scoring_visitor
   end
 
   def test_it_returns_highest_scoring_home_team
-    assert_equal "FC Cincinnati", @stat_tracker.highest_scoring_home_team
+    assert_equal "Reign FC", @stat_tracker.highest_scoring_home_team
   end
 
   def test_it_returns_lowest_scoring_away_team
-    assert_equal "FC Cincinnati", @stat_tracker.lowest_scoring_visitor
+    assert_equal "San Jose Earthquakes", @stat_tracker.lowest_scoring_visitor
   end
 
   def test_it_returns_lowest_scoring_home_team
-    assert_equal "FC Cincinnati", @stat_tracker.lowest_scoring_home_team
+    assert_equal "Utah Royals FC", @stat_tracker.lowest_scoring_home_team
   end
 
 end
