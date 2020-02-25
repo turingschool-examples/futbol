a4ac1b7b243b7c2d8a8646a5ac4db7781f3d57ce
Stephanie Friend <stephaniefriend91@gmail.com>
2020-02-25 11:32:04 -0700

    Move head to head method over to scored goal stat

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -6,7 +6,6 @@ require_relative 'season_stat_coach'
 require_relative 'summary'
 require_relative 'season_stat_team'
 require_relative 'league_stat'
-require_relative 'game_collection'
 
 class StatTracker
   attr_reader :games_file, :teams_file, :game_teams_file
@@ -76,8 +75,8 @@ class StatTracker
   end
 
   def head_to_head(team_id)
-    summary = Summary.new(@teams_file, @game_teams_file, @games_file)
-    summary.head_to_head(team_id)
+    scored_goal_stat = ScoredGoalStat.new(@teams_file, @game_teams_file, @games_file)
+    scored_goal_stat.head_to_head(team_id)
   end
 
   def count_of_games_by_season
@@ -136,14 +135,4 @@ class StatTracker
     season.fewest_tackles(season_param)
   end
 
-  def count_of_teams
-    league_stat = LeagueStat.new(@teams_file, @games_file)
-    league_stat.count_of_teams
-  end
-
-  def percentage_home_wins
-    game_collection = GameCollection.new(@games_file)
-    game_collection.create_pct_data
-    game_collection.pct_of_total_games(:home_wins)
-  end
 end
