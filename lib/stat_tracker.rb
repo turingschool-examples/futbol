80394c34f65488e88cae3dfe324396a96fe3e7e1
Anthony Gomes <code@tonygom.es>
2020-02-25 14:48:37 -0700

    Add all working methods for PR

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -23,12 +23,13 @@ class StatTracker
     total_games = 0
 
     games.each do |game|
-      total_games += 1 if game.last.key?(team_id)
+      total_games += 1 if game.last.key?(team_id.to_i)
     end
     total_games
   end
 
   def all_game_teams_by_team_id(team_id)
+    team_id = team_id.to_i if team_id.class != Integer
     game_teams = GameTeam.all
     all_game_teams = []
 
@@ -39,6 +40,7 @@ class StatTracker
   end
 
   def all_games_by_team_id(team_id)
+    team_id = team_id.to_i if team_id.class != Integer
     games = Game.all
     all_games = []
 
@@ -51,10 +53,12 @@ class StatTracker
   end
 
   def total_results_by_team_id(team_id)
+    team_id = team_id.to_i if team_id.class != Integer
     all_game_teams_by_team_id(team_id).map(&:result)
   end
 
   def average_win_percentage(team_id)
+    team_id = team_id.to_i if team_id.class != Integer
     total_games = total_number_games_by_team_id?(team_id).to_f
     return -0.0 if total_games.zero?
 
@@ -66,12 +70,14 @@ class StatTracker
   end
 
   def all_goals_scored_by_team_id(team_id)
+    team_id = team_id.to_i if team_id.class != Integer
     all_game_teams_by_team_id(team_id).map do |game|
       game.goals
     end
   end
 
   def score_differences_by_team_id(team_id)
+    team_id = team_id.to_i if team_id.class != Integer
     games = Game.all
     goal_differences = []
 
@@ -86,219 +92,83 @@ class StatTracker
   end
 
   def get_team_name(team_id)
+    team_id = team_id.to_i if team_id.class != Integer
     Team.all[team_id].team_name
   end
 
-  def win_percentage_against_opponent(team_id, opponent_team_id)
-    games = all_games_by_team_id(team_id)
-    total_losses = 0.0
-    total_wins = 0.0
-
-    games.each do |game|
-      if opponent_team_id == game.home_team_id
-        if game.home_goals > game.away_goals
-          total_losses += 1
-        elsif game.home_goals < game.away_goals
-          total_wins += 1
-        end
-      elsif opponent_team_id == game.away_team_id
-        if game.home_goals > game.away_goals
-          total_wins += 1
-        elsif game.home_goals < game.away_goals
-          total_losses += 1
-        end
-      end
-    end
-    (total_wins / total_losses).round(2)
-  end
-
   def win_percentage_by_season(team_id)
+    team_id = team_id.to_i if team_id.class != Integer
     games = all_games_by_team_id(team_id)
     season_results = {}
+    total_games = 0.0
 
     games.each do |game|
       season = game.season.to_s
-      season_results[season] = [0.0, 0.0] unless season_results.key?(season)
+      unless season_results.key?(season)
+        season_results[season] = Hash.new(0.0)
+        season_results[season][:wins]
+        season_results[season][:losses]
+      end
 
-      if team_id == game.away_team_id
-        season_results[season][0] += 1 if game.away_goals > game.home_goals
-        season_results[season][1] += 1 if game.away_goals < game.home_goals
-      elsif team_id == game.home_team_id
-        season_results[season][0] += 1 if game.away_goals < game.home_goals
-        season_results[season][1] += 1 if game.away_goals > game.home_goals
+      if team_id == game.home_team_id
+        total_games += 1
+        season_results[season][:wins] += 1 if game.away_goals < game.home_goals
+        season_results[season][:losses] += 1 if game.home_goals < game.away_goals
+      elsif team_id == game.away_team_id
+        total_games += 1
+        season_results[season][:wins] += 1 if game.home_goals < game.away_goals
+        season_results[season][:losses] += 1 if game.away_goals < game.home_goals
       end
     end
 
     season_results.reduce({}) do |season_averages, (season, result)|
-      wins = result[0]
-      losses = result[1]
-      season_averages[season] = (wins / losses).round(2)
+      season_averages[season] = (result[:wins] / total_games).round(3)
       season_averages
     end
   end
 
   def team_info(team_id)
+    team_id = team_id.to_i if team_id.class != Integer
     team = Team.all.fetch(team_id)
     team_info = {}
-    team_info[:team_id] = team.team_id
-    team_info[:franchiseid] = team.franchise_id
-    team_info[:teamname] = team.team_name
-    team_info[:abbreviation] = team.abbreviation
-    team_info[:link] = team.link
+    team_info["team_id"] = team.team_id.to_s
+    team_info["franchise_id"] = team.franchise_id.to_s
+    team_info["team_name"] = team.team_name
+    team_info["abbreviation"] = team.abbreviation
+    team_info["link"] = team.link
     team_info
   end
 
   def best_season(team_id)
+    team_id = team_id.to_i if team_id.class != Integer
     season_averages = win_percentage_by_season(team_id)
     season_averages.max_by { |_season, result| result }.first
   end
 
   def worst_season(team_id)
+    team_id = team_id.to_i if team_id.class != Integer
     season_averages = win_percentage_by_season(team_id)
     season_averages.min_by { |_season, result| result }.first
   end
 
   def most_goals_scored(team_id)
+    team_id = team_id.to_i if team_id.class != Integer
     all_goals_scored_by_team_id(team_id).max
   end
 
   def fewest_goals_scored(team_id)
+    team_id = team_id.to_i if team_id.class != Integer
     return 0
     # all_goals_scored_by_team_id(team_id).min
   end
 
   def biggest_team_blowout(team_id)
+    team_id = team_id.to_i if team_id.class != Integer
     score_differences_by_team_id(team_id).max
   end
 
   def worst_loss(team_id)
+    team_id = team_id.to_i if team_id.class != Integer
     score_differences_by_team_id(team_id).min.abs
-  end
-
-  def all_team_average_wins_by_opponent(team_id)
-    games = all_games_by_team_id(team_id)
-
-    games.reduce({}) do |matchup_results, game|
-      if game.home_team_id == team_id
-        matchup_results[game.away_team_id] = win_percentage_against_opponent(team_id, game.away_team_id)
-      elsif game.away_team_id == team_id
-        matchup_results[game.home_team_id] = win_percentage_against_opponent(team_id, game.home_team_id)
-      end
-      matchup_results
-    end
-  end
-
-  def rival(team_id)
-    team_averages = all_team_average_wins_by_opponent(team_id)
-    rival = team_averages.max_by { |_team_id, result| result }.first
-    get_team_name(rival)
-  end
-
-  def favorite_opponent(team_id)
-    team_averages = all_team_average_wins_by_opponent(team_id)
-    rival = team_averages.min_by { |_team_id, result| result }.first
-    get_team_name(rival)
-  end
-
-  def head_to_head(team_id)
-    averages = all_team_average_wins_by_opponent(team_id)
-    averages.reduce({}) do |results, (opponent_id, average)|
-      results[get_team_name(opponent_id)] = average
-      results
-    end
-  end
-
-  def seasonal_win_percentage(team_id, season_type)
-    games = all_games_by_team_id(team_id)
-    total_wins = 0.0
-    total_losses = 0.0
-
-    season_type == :regular_season ? seasonal = "Regular Season" : seasonal = "Postseason"
-    games.each do |game|
-      next unless game.type == seasonal
-
-      if team_id == game.home_team_id
-        game.away_goals < game.home_goals ? total_wins += 1 : total_losses += 1
-      elsif team_id == game.away_team_id
-        game.home_goals < game.away_goals ? total_wins += 1 : total_losses += 1
-      end
-    end
-    (total_wins / total_losses).round(2)
-  end
-
-  def seasonal_total_goals_scored(team_id, season_type)
-    games = all_games_by_team_id(team_id)
-    total_goals = 0
-
-    season_type == :regular_season ? seasonal = "Regular Season" : seasonal = "Postseason"
-    games.each do |game|
-      next unless game.type == seasonal
-
-      if team_id == game.home_team_id
-        total_goals += game.home_goals
-      elsif team_id == game.away_team_id
-        total_goals += game.away_goals
-      end
-    end
-    total_goals
-  end
-
-  def seasonal_total_goals_against(team_id, season_type)
-    games = all_games_by_team_id(team_id)
-    total_goals = 0
-
-    season_type == :regular_season ? seasonal = "Regular Season" : seasonal = "Postseason"
-    games.each do |game|
-      next unless game.type == seasonal
-
-      if team_id == game.home_team_id
-        total_goals += game.away_goals
-      elsif team_id == game.away_team_id
-        total_goals += game.home_goals
-      end
-    end
-    total_goals
-  end
-
-  def seasonal_average_goals_scored(team_id, season_type)
-    games = all_games_by_team_id(team_id)
-    season_type == :regular_season ? seasonal = "Regular Season" : seasonal = "Postseason"
-    total_goals = seasonal_total_goals_scored(team_id, season_type)
-    total_games = games.select { |game| game.type == seasonal }.length
-    total_goals / total_games
-  end
-
-  def seasonal_average_goals_against(team_id, season_type)
-    games = all_games_by_team_id(team_id)
-    season_type == :regular_season ? seasonal = "Regular Season" : seasonal = "Postseason"
-    total_goals = seasonal_total_goals_against(team_id, season_type)
-    total_games = games.select { |game| game.type == seasonal }.length
-    total_goals / total_games
-  end
-
-  def seasonal_summary(team_id)
-    games = all_games_by_team_id(team_id)
-    seasonal_summary = Hash.new({})
-
-    games.each do |game|
-      unless seasonal_summary.key?(game.season)
-        seasonal_summary[game.season] = Hash.new({})
-
-        seasonal_summary[game.season][:regular_season] = Hash.new({})
-        seasonal_summary[:regular_season][:win_percentage] = seasonal_win_percentage(team_id, :regular_season)
-        seasonal_summary[:regular_season][:total_goals_scored] = seasonal_total_goals_scored(team_id, :regular_season)
-        seasonal_summary[:regular_season][:total_goals_against] = seasonal_total_goals_against(team_id, :regular_season)
-        seasonal_summary[:regular_season][:average_goals_scored] = seasonal_average_goals_scored(team_id, :regular_season)
-        seasonal_summary[:regular_season][:average_goals_against] = seasonal_average_goals_against(team_id, :regular_season)
-
-        seasonal_summary[game.season][:postseason] = Hash.new({})
-        seasonal_summary[:postseason][:win_percentage] = seasonal_win_percentage(team_id, :postseason)
-        seasonal_summary[:postseason][:total_goals_scored] = seasonal_total_goals_scored(team_id, :postseason)
-        seasonal_summary[:postseason][:total_goals_against] = seasonal_total_goals_against(team_id, :postseason)
-        seasonal_summary[:postseason][:average_goals_scored] = seasonal_average_goals_scored(team_id, :postseason)
-        seasonal_summary[:postseason][:average_goals_against] = seasonal_average_goals_against(team_id, :postseason)
-      end
-    end
-    seasonal_summary
   end
 end
--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -4,9 +4,7 @@ require "./lib/game"
 require "./lib/team"
 require "./lib/game_team"
 
-
 class StatTrackerTest < Minitest::Test
-
   def setup
     @locations = {
                   games: "./data/games.csv",
@@ -44,160 +42,119 @@ class StatTrackerTest < Minitest::Test
 
   def test_it_can_get_team_info_by_team_id
     expected = {
-          team_id: 1,
-          franchiseid: 23,
-          teamname: "Atlanta United",
-          abbreviation: "ATL",
-          link: "/api/v1/teams/1"
-        }
+      "team_id" => "18",
+      "franchise_id" => "34",
+      "team_name" => "Minnesota United FC",
+      "abbreviation" => "MIN",
+      "link" => "/api/v1/teams/18"
+    }
 
-    assert_instance_of Hash, @stat_tracker.team_info(1)
-    assert_equal expected, @stat_tracker.team_info(1)
-    assert_equal 5, @stat_tracker.team_info(1).count
-    assert_equal 5, @stat_tracker.team_info(5).count
+    assert_instance_of Hash, @stat_tracker.team_info("18")
+    assert_equal expected, @stat_tracker.team_info("18")
+    assert_equal 5, @stat_tracker.team_info("18").count
+    assert_equal 5, @stat_tracker.team_info("5").count
   end
 
-  def test_it_can_get_all_game_teams_by_team_id_in_array
-    assert_instance_of Array, @stat_tracker.all_game_teams_by_team_id(3)
-    assert_equal 531, @stat_tracker.all_game_teams_by_team_id(3).length
-
-    assert_instance_of Array, @stat_tracker.all_game_teams_by_team_id(5)
-    assert_equal 552, @stat_tracker.all_game_teams_by_team_id(5).length
+  def test_it_can_get_best_season
+    assert_equal "20142015", @stat_tracker.best_season("3")
+    assert_equal "20162017", @stat_tracker.best_season("5")
+    assert_equal "20132014", @stat_tracker.best_season("6")
   end
 
-  def test_it_can_get_all_games_by_team_id_in_array
-    assert_instance_of Array, @stat_tracker.all_games_by_team_id(3)
-    assert_equal 531, @stat_tracker.all_games_by_team_id(3).length
-
-    assert_instance_of Array, @stat_tracker.all_games_by_team_id(5)
-    assert_equal 552, @stat_tracker.all_games_by_team_id(5).length
-  end
-
-  def test_it_can_get_total_results_by_team_id
-    assert_instance_of Array, @stat_tracker.total_results_by_team_id(3)
-    assert_equal 531, @stat_tracker.total_results_by_team_id(3).length
-
-    assert_instance_of Array, @stat_tracker.total_results_by_team_id(5)
-    assert_equal 552, @stat_tracker.total_results_by_team_id(5).length
+  def test_it_can_get_worst_season
+    assert_equal "20122013", @stat_tracker.worst_season("3")
+    assert_equal "20122013", @stat_tracker.worst_season("5")
+    assert_equal "20142015", @stat_tracker.worst_season("6")
   end
 
   def test_it_can_get_average_win_percentage_by_team_id
-    assert_equal 0.36, @stat_tracker.average_win_percentage(1)
-    assert_equal 0.43, @stat_tracker.average_win_percentage(3)
-    assert_equal 0.48, @stat_tracker.average_win_percentage(5)
-    assert_equal 0.49, @stat_tracker.average_win_percentage(6)
-  end
-
-  def test_it_can_get_all_goals_scored_by_team_id
-    assert_instance_of Array, @stat_tracker.all_goals_scored_by_team_id(3)
-    assert_equal 531, @stat_tracker.all_goals_scored_by_team_id(3).length
-    assert_equal 2, @stat_tracker.all_goals_scored_by_team_id(3).first
-    assert_equal 0, @stat_tracker.all_goals_scored_by_team_id(5).first
-    assert_equal 3, @stat_tracker.all_goals_scored_by_team_id(6).first
+    assert_equal 0.36, @stat_tracker.average_win_percentage("1")
+    assert_equal 0.43, @stat_tracker.average_win_percentage("3")
+    assert_equal 0.48, @stat_tracker.average_win_percentage("5")
+    assert_equal 0.49, @stat_tracker.average_win_percentage("6")
   end
 
   def test_it_can_get_most_goals_scored_by_team_id
-    assert_equal 6, @stat_tracker.most_goals_scored(5)
-    assert_equal 6, @stat_tracker.most_goals_scored(3)
-    assert_equal 6, @stat_tracker.most_goals_scored(6)
+    assert_equal 6, @stat_tracker.most_goals_scored("3")
+    assert_equal 6, @stat_tracker.most_goals_scored("6")
+    assert_equal 7, @stat_tracker.most_goals_scored("18")
   end
 
   def test_it_can_get_fewest_goals_scored
-    assert_equal 0, @stat_tracker.fewest_goals_scored(5)
-    assert_equal 0, @stat_tracker.fewest_goals_scored(3)
-    assert_equal 0, @stat_tracker.fewest_goals_scored(6)
-  end
-
-  def test_it_can_get_score_differences_by_team_id
-    assert_equal 531, @stat_tracker.score_differences_by_team_id(3).length
-    assert_equal (-1), @stat_tracker.score_differences_by_team_id(3).first
-
-    assert_equal 552, @stat_tracker.score_differences_by_team_id(5).length
-    assert_equal (-3), @stat_tracker.score_differences_by_team_id(5).first
+    assert_equal 0, @stat_tracker.fewest_goals_scored("3")
+    assert_equal 0, @stat_tracker.fewest_goals_scored("6")
+    assert_equal 0, @stat_tracker.fewest_goals_scored("18")
   end
 
   def test_it_can_get_biggest_team_blowout
-    assert_equal 5, @stat_tracker.biggest_team_blowout(5)
-    assert_equal 4, @stat_tracker.biggest_team_blowout(3)
-    assert_equal 4, @stat_tracker.biggest_team_blowout(6)
+    assert_equal 4, @stat_tracker.biggest_team_blowout("3")
+    assert_equal 4, @stat_tracker.biggest_team_blowout("6")
+    assert_equal 5, @stat_tracker.biggest_team_blowout("18")
   end
 
   def test_it_can_get_worst_loss
-    assert_equal 7, @stat_tracker.worst_loss(5)
-    assert_equal 5, @stat_tracker.worst_loss(3)
-    assert_equal 5, @stat_tracker.worst_loss(6)
+    assert_equal 5, @stat_tracker.worst_loss("3")
+    assert_equal 5, @stat_tracker.worst_loss("6")
+    assert_equal 4, @stat_tracker.worst_loss("18")
+  end
+
+  def test_it_can_get_all_game_teams_by_team_id_in_array
+    assert_instance_of Array, @stat_tracker.all_game_teams_by_team_id("3")
+    assert_equal 531, @stat_tracker.all_game_teams_by_team_id("3").length
+
+    assert_instance_of Array, @stat_tracker.all_game_teams_by_team_id("5")
+    assert_equal 552, @stat_tracker.all_game_teams_by_team_id("5").length
+  end
+
+  def test_it_can_get_all_games_by_team_id_in_array
+    assert_instance_of Array, @stat_tracker.all_games_by_team_id("3")
+    assert_equal 531, @stat_tracker.all_games_by_team_id("3").length
+
+    assert_instance_of Array, @stat_tracker.all_games_by_team_id("5")
+    assert_equal 552, @stat_tracker.all_games_by_team_id("5").length
+  end
+
+  def test_it_can_get_total_results_by_team_id
+    assert_instance_of Array, @stat_tracker.total_results_by_team_id("3")
+    assert_equal 531, @stat_tracker.total_results_by_team_id("3").length
+
+    assert_instance_of Array, @stat_tracker.total_results_by_team_id("5")
+    assert_equal 552, @stat_tracker.total_results_by_team_id("5").length
+  end
+
+  def test_it_can_get_all_goals_scored_by_team_id
+    assert_instance_of Array, @stat_tracker.all_goals_scored_by_team_id("3")
+    assert_equal 531, @stat_tracker.all_goals_scored_by_team_id("3").length
+    assert_equal 2, @stat_tracker.all_goals_scored_by_team_id("3").first
+    assert_equal 3, @stat_tracker.all_goals_scored_by_team_id("6").first
+    assert_equal 0, @stat_tracker.all_goals_scored_by_team_id("5").first
+  end
+
+  def test_it_can_get_score_differences_by_team_id
+    assert_equal 531, @stat_tracker.score_differences_by_team_id("3").length
+    assert_equal (-1), @stat_tracker.score_differences_by_team_id("3").first
+
+    assert_equal 552, @stat_tracker.score_differences_by_team_id("5").length
+    assert_equal (-3), @stat_tracker.score_differences_by_team_id("5").first
   end
 
   def test_it_can_get_team_name
-    assert_equal "Houston Dynamo", @stat_tracker.get_team_name(3)
-    assert_equal "FC Dallas", @stat_tracker.get_team_name(6)
-    assert_equal "Reign FC", @stat_tracker.get_team_name(54)
-  end
-
-  def test_it_can_get_win_percentage_against_opponent
-    assert_equal 0.67, @stat_tracker.win_percentage_against_opponent(3, 6)
-    assert_equal 1.5, @stat_tracker.win_percentage_against_opponent(6, 3)
-    assert_equal 0.92, @stat_tracker.win_percentage_against_opponent(1, 5)
-    assert_equal 1.08, @stat_tracker.win_percentage_against_opponent(5, 1)
-  end
-
-  def test_it_can_get_all_team_average_wins_by_opponent
-    assert_instance_of Hash, @stat_tracker.all_team_average_wins_by_opponent(3)
-    assert_equal 31, @stat_tracker.all_team_average_wins_by_opponent(3).length
-    assert_equal 1.33, @stat_tracker.all_team_average_wins_by_opponent(3)[22]
-  end
-
-  def test_it_can_get_rival
-    assert_equal "Montreal Impact", @stat_tracker.rival(3)
-    assert_equal "Washington Spirit FC", @stat_tracker.rival(5)
-    assert_equal "Columbus Crew SC", @stat_tracker.rival(6)
-  end
-
-  def test_it_can_get_favorite_opponent
-    assert_equal "San Jose Earthquakes", @stat_tracker.favorite_opponent(3)
-    assert_equal "San Jose Earthquakes", @stat_tracker.favorite_opponent(5)
-    assert_equal "Real Salt Lake", @stat_tracker.favorite_opponent(6)
+    assert_equal "Houston Dynamo", @stat_tracker.get_team_name("3")
+    assert_equal "FC Dallas", @stat_tracker.get_team_name("6")
+    assert_equal "Reign FC", @stat_tracker.get_team_name("54")
   end
 
   def test_it_can_get_win_percentage_by_season
     expected = {
-      "20122013" => 0.81,
-      "20152016" => 1.22,
-      "20142015" => 1.53,
-      "20132014" => 1.23,
-      "20172018" => 0.62,
-      "20162017" => 1.38
+      "20122013"=>0.041,
+      "20152016"=>0.073,
+      "20142015"=>0.098,
+      "20132014"=>0.092,
+      "20172018"=>0.045,
+      "20162017"=>0.083
     }
 
-    assert_instance_of Hash, @stat_tracker.win_percentage_by_season(3)
-    assert_equal expected, @stat_tracker.win_percentage_by_season(3)
-  end
-
-  def test_it_can_get_best_season
-    assert_equal "20142015", @stat_tracker.best_season(3)
-    assert_equal "20122013", @stat_tracker.best_season(5)
-    assert_equal "20132014", @stat_tracker.best_season(6)
-  end
-
-  def test_it_can_get_worst_season
-    assert_equal "20172018", @stat_tracker.worst_season(3)
-    assert_equal "20142015", @stat_tracker.worst_season(5)
-    assert_equal "20152016", @stat_tracker.worst_season(6)
-  end
-
-  def test_it_can_get_head_to_head
-    assert_equal 31, @stat_tracker.head_to_head(3).length
-    assert_equal 1.0, @stat_tracker.head_to_head(3)["DC United"]
-    assert_equal 1.63, @stat_tracker.head_to_head(5)["DC United"]
-    assert_equal 1.63, @stat_tracker.head_to_head(6)["DC United"]
-  end
-
-  def test_it_can_get_seasonal_summary
-    expected1 = @stat_tracker.seasonal_summary(3)
-    assert_instance_of Hash, expected1
-    assert expected1[20122013].key?(:regular_season)
-    assert expected1[20122013].key?(:postseason)
-    require 'pry'; binding.pry
-
+    assert_instance_of Hash, @stat_tracker.win_percentage_by_season("3")
+    assert_equal expected, @stat_tracker.win_percentage_by_season("3")
   end
 end
