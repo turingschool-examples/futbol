db81e98f13792d672a011a7d3943de4ee106419a
Colin Alexander <colintalex@gmail.com>
2020-02-26 16:23:45 -0700

    Adds tests for refactor to team_collection_test

--- test/team_collection_test.rb
+++ test/team_collection_test.rb
@@ -1,7 +1,7 @@
 require 'csv'
 require 'minitest/autorun'
 require 'minitest/pride'
-require_relative './team_collection'
+require_relative '../lib/team_collection'
 
 class TeamCollectionTest < Minitest::Test
 
@@ -22,5 +22,21 @@ class TeamCollectionTest < Minitest::Test
 
     assert_instance_of Team, @team_collection.teams.first
     assert_equal 32, @team_collection.teams.length
+  end
+
+  def test_it_knows_team_info
+    expected = {
+      "team_id" => "3",
+      "franchise_id" => "10",
+      "team_name" => "Houston Dynamo",
+      "abbreviation" => "HOU",
+      "link" => "/api/v1/teams/3"
+      }
+    assert_equal expected, @team_collection.team_info("3")
+  end
+
+  def test_it_can_return_a_team
+    assert_equal Team, @team_collection.retrieve_team(18).class
+    assert_equal 18, @team_collection.retrieve_team(18).team_id
   end
 end
