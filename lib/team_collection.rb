70f9b4cbb97a651e45580126f8abfb53e0745422
Colin Alexander <colintalex@gmail.com>
2020-02-26 16:23:23 -0700

    Adds team_info test

--- lib/team_collection.rb
+++ lib/team_collection.rb
@@ -17,4 +17,19 @@ class TeamCollection
   def count_of_teams
     teams.length
   end
+
+  def team_info(team_num)
+  info = {}
+  team_obj = retrieve_team(team_num.to_i)
+    info["team_id"] = team_obj.team_id.to_s
+    info["franchise_id"] = team_obj.franchiseid.to_s
+    info["team_name"] = team_obj.teamname
+    info["abbreviation"] = team_obj.abbreviation
+    info["link"] = team_obj.link
+    info
+  end
+
+  def retrieve_team(team_num)
+    @teams.find { |team_obj| team_obj.team_id == team_num }
+  end
 end
