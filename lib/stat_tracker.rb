b881e3ab6ce89b69dc6d99f9c9209a65f3d0b0d9
Colin Alexander <colintalex@gmail.com>
2020-02-26 19:22:34 -0700

    Changes requires to fix rake

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,8 +1,12 @@
 require 'csv'
-require_relative './game_teams_collection'
-require_relative './game_collection'
-require_relative './team_collection'
+require_relative 'game_teams_collection'
+require_relative 'game_teams'
+require_relative 'game_collection'
+require_relative 'game'
+require_relative 'team_collection'
+require_relative 'team'
 require_relative '../modules/nameable.rb'
+
 class StatTracker
   include Nameable
 
