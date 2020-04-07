8608ecf90a19786932cdee02855877f87a87391d
Antonio Jackson <antoniojackson@gmail.com>
2020-04-07 15:00:58 -0600

    Updating game_stats_collection_test to test for attributes

--- test/game_stats_collection_test.rb
+++ test/game_stats_collection_test.rb
@@ -9,4 +9,9 @@ class GameStatsCollectionTest < Minitest::Test
     assert_instance_of GameStatsCollection, @game_stats_collection
   end
 
+  def test_it_has_attributes
+    assert_instance_of Array, @game_stats_collection.game_stats
+    assert_equal 217, @game_stats_collection.game_stats.length
+  end
+
 end
