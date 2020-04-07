bdcbacfa11bc0168fe5d178db753621b0f0c8857
Melanie Tran <melaniexx02@gmail.com>
2020-04-07 15:40:06 -0600

    Created and passed the setup for team collection

--- lib/team_collection.rb
+++ lib/team_collection.rb
@@ -4,13 +4,15 @@ require_relative 'team'
 class TeamCollection
   attr_reader :teams
 
-  def initialize(team_data)
-    @teams = create_teams(team_data)
+  def initialize(csv_file_path)
+    @teams = create_teams(csv_file_path)
   end
 
-  def create_teams(team_data)
-    team_data.map do |row|
-      Team.new(row.to_h)
+  def create_teams(csv_file_path)
+    csv = CSV.read("#{csv_file_path}", headers: true, header_converters: :symbol)
+
+    csv.map do |row|
+      Team.new(row)
     end
   end
 end
--- test/team_collection_test.rb
+++ test/team_collection_test.rb
@@ -3,7 +3,7 @@ require_relative 'test_helper'
 class TeamCollectionTest < Minitest::Test
   def setup
     @team_collection = TeamCollection.new('./data/teams.csv')
-    @team = @team_collection.teams[2]
+    @team = @team_collection.teams[21]
   end
 
   def test_it_exists
@@ -17,6 +17,10 @@ class TeamCollectionTest < Minitest::Test
 
   def test_it_can_create_teams_from_csv
     assert_instance_of Team, @team
-    
+    assert_equal "VAN", @team.abbreviation
+    assert_equal 0, @team.franchiseid
+    assert_equal "/api/v1/teams/21", @team.link
+    assert_equal 21, @team.team_id
+    assert_equal "Vancouver Whitecaps FC", @team.teamname
   end
 end
