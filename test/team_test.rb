144196ac678a14dae2d9a3ab4b188e1f5b21f79a
Melanie Tran <melaniexx02@gmail.com>
2020-04-07 13:40:08 -0600

    Created test it exists

--- test/team_test.rb
+++ test/team_test.rb
@@ -1,4 +1,19 @@
 require_relative 'test_helper'
 
 class TeamTest < Minitest::Test
+  def setup
+    team_info = {
+      :team_id => "1",
+      :franchiseId => "23",
+      :teamName => "Atlanta United",
+      :abbreviation => "ATL",
+      :Stadium => "Mercedes-Benz Stadium",
+      :link => "/api/v1/teams/1"
+    }
+    @team = Team.new(team_info)
+  end
+
+  def test_it_exists
+    assert_instance_of Team, @team
+  end
 end
