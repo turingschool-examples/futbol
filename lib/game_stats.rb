0e56c63242e1be7af4dbc702d3e71439c77f361b
Antonio Jackson <antoniojackson@gmail.com>
2020-04-07 15:47:32 -0600

    Updating GameStats class and GameStatsTest attributes to reflect the downcased symbols being passed in from the CSV file

--- lib/game_stats.rb
+++ lib/game_stats.rb
@@ -1,12 +1,12 @@
 class GameStats
-  attr_reader :game_id, :team_id, :HoA, :result, :settled_in, :head_coach,
-              :goals, :shots, :tackles, :pim, :powerPlayOpportunities,
-              :powerPlayGoals, :faceOffWinPercentage, :giveaways, :takeaways
+  attr_reader :game_id, :team_id, :home_away, :result, :settled_in, :head_coach,
+              :goals, :shots, :tackles, :pim, :power_play_opportunities,
+              :power_play_goals, :face_off_win_percentage, :giveaways, :takeaways
 
   def initialize(game_stats)
     @game_id = game_stats[:game_id].to_i
     @team_id = game_stats[:team_id].to_i
-    @HoA = game_stats[:HoA]
+    @home_away = game_stats[:hoa]
     @result = game_stats[:result]
     @settled_in = game_stats[:settled_in]
     @head_coach = game_stats[:head_coach]
@@ -14,9 +14,9 @@ class GameStats
     @shots = game_stats[:shots].to_i
     @tackles = game_stats[:tackles].to_i
     @pim = game_stats[:pim].to_i
-    @powerPlayOpportunities = game_stats[:powerPlayOpportunities].to_i
-    @powerPlayGoals = game_stats[:powerPlayGoals].to_i
-    @faceOffWinPercentage = game_stats[:faceOffWinPercentage].to_f
+    @power_play_opportunities = game_stats[:powerplayopportunities].to_i
+    @power_play_goals = game_stats[:powerplaygoals].to_i
+    @face_off_win_percentage = game_stats[:faceoffwinpercentage].to_f
     @giveaways = game_stats[:giveaways].to_i
     @takeaways = game_stats[:takeaways].to_i
   end
--- test/game_stats_test.rb
+++ test/game_stats_test.rb
@@ -5,7 +5,7 @@ class GameStatsTest < Minitest::Test
   def setup
     @info = { :game_id => "2012030221",
               :team_id => 3,
-              :HoA => "away",
+              :hoa => "away",
               :result => "LOSS",
               :settled_in => "OT",
               :head_coach => "John Tortorella",
@@ -13,9 +13,9 @@ class GameStatsTest < Minitest::Test
               :shots => 8,
               :tackles => 44,
               :pim => 8,
-              :powerPlayOpportunities => 3,
-              :powerPlayGoals => 0,
-              :faceOffWinPercentage => 44.8,
+              :powerplayopportunities => 3,
+              :powerplaygoals => 0,
+              :faceoffwinpercentage => 44.8,
               :giveaways => 17,
               :takeaways => 7}
       @game_stats = GameStats.new(@info)
@@ -28,7 +28,7 @@ class GameStatsTest < Minitest::Test
   def test_it_has_attributes
     assert_equal 2012030221, @game_stats.game_id
     assert_equal 3, @game_stats.team_id
-    assert_equal "away", @game_stats.HoA
+    assert_equal "away", @game_stats.home_away
     assert_equal "LOSS", @game_stats.result
     assert_equal "OT", @game_stats.settled_in
     assert_equal "John Tortorella", @game_stats.head_coach
@@ -36,9 +36,9 @@ class GameStatsTest < Minitest::Test
     assert_equal 8, @game_stats.shots
     assert_equal 44, @game_stats.tackles
     assert_equal 8, @game_stats.pim
-    assert_equal 3, @game_stats.powerPlayOpportunities
-    assert_equal 0, @game_stats.powerPlayGoals
-    assert_equal 44.8, @game_stats.faceOffWinPercentage
+    assert_equal 3, @game_stats.power_play_opportunities
+    assert_equal 0, @game_stats.power_play_goals
+    assert_equal 44.8, @game_stats.face_off_win_percentage
     assert_equal 17, @game_stats.giveaways
     assert_equal 7, @game_stats.takeaways
   end
