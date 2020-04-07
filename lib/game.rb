b598ff48c7f625f7b9fd52e2e38fc570176ad27e
Jane Greene <janegreenecasa@gmail.com>
2020-04-06 20:08:09 -0600

    Added test and method for csv in game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,16 +1,29 @@
 class Game
-  attr_reader :game_id, :season, :type, :date_time, :away_team_id, :home_team_id, :away_goals, :home_goals, :venue, :venue_link 
+  @@all_games = []
+  def self.from_csv(file_path)
+      csv = CSV.read("#{file_path}", headers: true, header_converters: :symbol)
+      @@all_games = csv.map do |row|
+        game = Game.new(row)
+      end
+  end
+
+  def self.all_games
+    @@all_games
+  end
+
+  attr_reader :game_id, :season, :type, :date_time, :away_team_id, :home_team_id, :away_goals, :home_goals, :venue, :venue_link
 
   def initialize(info)
-    @game_id = info[:game_id]
+    @game_id = info[:game_id].to_i
     @season = info[:season]
     @type = info[:type]
     @date_time = info[:date_time]
-    @away_team_id = info[:away_team_id]
-    @home_team_id = info[:home_team_id]
-    @away_goals = info[:away_goals]
-    @home_goals = info[:home_goals]
+    @away_team_id = info[:away_team_id].to_i
+    @home_team_id = info[:home_team_id].to_i
+    @away_goals = info[:away_goals].to_i
+    @home_goals = info[:home_goals].to_i
     @venue = info[:venue]
     @venue_link = info[:venue_link]
   end
+
 end
--- test/game_test.rb
+++ test/game_test.rb
@@ -5,13 +5,15 @@ require './lib/game'
 
 
 class GameTest < Minitest::Test
+
   def test_it_exists
-  #game_id,season,type,date_time,away_team_id,home_team_id,away_goals,home_goals,venue,venue_link
-  game = Game.new({game_id: 123, season: "20122013", type: "Postseason", date_time: "6/5/13", away_team_id: 1, home_team_id: 1, away_goals: 1, home_goals: 1, venue: "Toyta", venue_link: "link" })
-  assert_instance_of Game, game
+    game = Game.new({game_id: 123, season: "20122013", type: "Postseason", date_time: "6/5/13", away_team_id: 1, home_team_id: 1, away_goals: 1, home_goals: 1, venue: "Toyta", venue_link: "link" })
+    assert_instance_of Game, game
   end
+
   def test_has_attributes
     game = Game.new({game_id: 123, season: "20122013", type: "Postseason", date_time: "6/5/13", away_team_id: 1, home_team_id: 1, away_goals: 1, home_goals: 1, venue: "Toyta", venue_link: "link" })
+
     assert_equal 123, game.game_id
     assert_equal "20122013", game.season
     assert_equal "Postseason", game.type
@@ -22,6 +24,23 @@ class GameTest < Minitest::Test
     assert_equal 1, game.home_goals
     assert_equal "Toyta", game.venue
     assert_equal "link", game.venue_link
+  end
+
+  def test_it_has_teams
+    Game.from_csv("./data/games.csv")
+    game = Game.all_games[0]
+
+     assert_equal 2012030221, game.game_id
+     assert_equal "20122013", game.season
+     assert_equal "Postseason", game.type
+     assert_equal "5/16/13", game.date_time
+     assert_equal 3, game.away_team_id
+     assert_equal 6, game.home_team_id
+     assert_equal 2, game.away_goals
+     assert_equal 3, game.home_goals
+     assert_equal "Toyota Stadium", game.venue
+     assert_equal "/api/v1/venues/null", game.venue_link
+     assert_instance_of Game, game
   end
 
 end
