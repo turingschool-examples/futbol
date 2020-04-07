479689067aa53e62e28d7a072db93496a21937c8
Jack Cullen <jpcullen20@gmail.com>
2020-04-07 15:02:18 -0600

    Add all game_team attributes as instance vars

--- lib/game_team.rb
+++ lib/game_team.rb
@@ -5,7 +5,16 @@ class GameTeam
               :hoa,
               :result,
               :settled_in,
-              :head_coach
+              :head_coach,
+              :goals,
+              :shots,
+              :tackles,
+              :pim,
+              :powerplayopportunities,
+              :powerplaygoals,
+              :faceoffwinpercentage,
+              :giveaways,
+              :takeaways
   def initialize(game_team_info)
     @game_id = game_team_info[:game_id].to_i
     @team_id = game_team_info[:team_id].to_i
@@ -13,5 +22,14 @@ class GameTeam
     @result = game_team_info[:result]
     @settled_in = game_team_info[:settled_in]
     @head_coach = game_team_info[:head_coach]
+    @goals = game_team_info[:goals].to_i
+    @shots = game_team_info[:shots].to_i
+    @tackles = game_team_info[:tackles].to_i
+    @pim = game_team_info[:pim].to_i
+    @powerplayopportunities = game_team_info[:powerplayopportunities].to_i
+    @powerplaygoals = game_team_info[:powerplaygoals].to_i
+    @faceoffwinpercentage = game_team_info[:faceoffwinpercentage].to_f
+    @giveaways = game_team_info[:giveaways].to_i
+    @takeaways = game_team_info[:takeaways].to_i
   end
 end
