d44199123b55db0fa85bb9fbac9e0f21540518bd
Max Mitrani <mjmitrani168@gmail.com>
2020-04-07 13:19:12 -0600

    Add stat_tracker initialize and tests of attributes

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,10 +1,14 @@
 class StatTracker
+  attr_reader :games, :teams, :game_teams
 
-  def initialize(file)
+  def initialize(data_files)
+    @games = data_files[:games]
+    @teams = data_files[:teams]
+    @game_teams = data_files[:game_teams]
   end
 
-  def self.from_csv(file)
-    StatTracker.new(file)
+  def self.from_csv(data_files)
+    StatTracker.new(data_files)
   end
 
 end
--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -32,4 +32,22 @@ class StatTrackerTest < Minitest::Test
     assert_instance_of StatTracker, stat_tracker
   end
 
+  def test_has_attributes
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+    stat_tracker = StatTracker.from_csv(locations)
+    assert_equal  './data/games.csv', stat_tracker.games
+    assert_equal  './data/teams.csv', stat_tracker.teams
+    assert_equal  './data/game_teams.csv', stat_tracker.game_teams
+  end
+
+
+
 end
