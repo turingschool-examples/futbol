793af88003c374b3cf4436d570da4ba21c8c4c79
Melanie Tran <melaniexx02@gmail.com>
2020-04-07 14:56:14 -0600

    Created test for attributes on team collection

--- test/team_collection_test.rb
+++ test/team_collection_test.rb
@@ -11,5 +11,13 @@ class TeamCollectionTest < Minitest::Test
     assert_instance_of TeamCollection, @team_collection
   end
 
-  
+  def test_it_has_attributes
+    assert_instance_of Array, @team_collection.teams
+    assert_equal 32, @team_collection.teams.length
+  end
+
+  def test_it_can_create_teams_from_csv
+    assert_instance_of Team, @team
+  end
+
 end
