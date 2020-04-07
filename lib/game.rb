ce25d3c4aa78134870e56a84392429649f1fa00f
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-07 12:43:15 -0600

    Add game class to convert games.csv into Ruby objecrts

---
+++ lib/game.rb
@@ -0,0 +1,42 @@
+require "csv"
+require "pry"
+
+class Game
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.from_csv(file_path)
+    csv = CSV.read("#{file_path}", headers: true, header_converters: :symbol)
+
+    @@all = csv.map do |row|
+      Game.new(row)
+    end
+  end
+
+  attr_reader :game_id,
+              :season,
+              :type,
+              :date_time,
+              :away_team_id,
+              :home_team_id,
+              :away_goals,
+              :home_goals,
+              :venue,
+              :venue_link
+
+  def initialize(game_stats)
+    @game_id = game_stats[:game_id].to_i
+    @season = game_stats[:season].to_i
+    @type = game_stats[:type]
+    @date_time = game_stats[:date_time]
+    @away_team_id = game_stats[:away_team_id].to_i
+    @home_team_id = game_stats[:home_team_id].to_i
+    @away_goals = game_stats[:away_goals].to_i
+    @home_goals = game_stats[:home_goals].to_i
+    @venue = game_stats[:venue]
+    @venue_link = game_stats[:venue_link]
+  end
+end
