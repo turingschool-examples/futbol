44b75a87e73de522a93778a78f2698f54732ac70
Eric Larson <ericlarsonbroom@gmail.com>
2020-04-06 18:02:15 -0600

    attempt to pull from csv file

--- lib/team.rb
+++ lib/team.rb
@@ -1,12 +1,26 @@
 class Team
+  @@all_teams= []
 
+  def self.all_teams
+    @@all_teams
+  end
+
+  def self.from_csv(file_path)
+      csv = CSV.read("#{file_path}", headers: true, header_converters: :symbol)
+
+      @@all_teams = csv.map do |row|
+         # require"pry";binding.pry
+          Team.new(row)
+           # require"pry";binding.pry
+      end
+    end
   #
   # CSV.read(./data/teams.csv)
-  attr_reader :team_id, :franchise_id, :name, :abbreviation, :stadium, :link
+  attr_reader :team_id, :franchise_id, :team_name, :abbreviation, :stadium, :link
   def initialize(info)
-    @team_id = info[:team_id]
-    @franchise_id = info[:franchise_id]
-    @name = info[:name]
+    @team_id = info[:team_id].to_i
+    @franchise_id = info[:franchise_id].to_i
+    @team_name = info[:team_name]
     @abbreviation = info[:abbreviation]
     @stadium = info[:stadium]
     @link = info[:link]
--- test/team_test.rb
+++ test/team_test.rb
@@ -7,19 +7,32 @@ class TeamTest < Minitest::Test
 
 
   def test_it_exists
-    team = Team.new({team_id: 123, franchise_id: 456, name: "Rockets", abbreviation: "RO", stadium: "Rocket Stadium", link: "link" })
+    team = Team.new({team_id: 123, franchiseId: 456, team_name: "Rockets", abbreviation: "RO", stadium: "Rocket Stadium", link: "link" })
     assert_instance_of Team, team
   end
 
   def test_it_has_attributes
-    team = Team.new({team_id: 123, franchise_id: 456, name: "Rockets", abbreviation: "RO", stadium: "Rocket Stadium", link: "link" })
+    team = Team.new({team_id: 123, franchiseId: 456, team_name: "Rockets", abbreviation: "RO", stadium: "Rocket Stadium", link: "link" })
     assert_equal 123, team.team_id
     assert_equal 456, team.franchise_id
-    assert_equal "Rockets", team.name
+    assert_equal "Rockets", team.team_name
     assert_equal "RO", team.abbreviation
     assert_equal "Rocket Stadium", team.stadium
     assert_equal "link", team.link
   end
+#"./test/fixtures/items_truncated.csv"
+  def test_it_has_teams
+    Team.from_csv("./data/teams.csv")
+    team = Team.all_teams[2]
+
+     # require"pry";binding.pry
+    assert_equal 26,team.team_id
+    # assert_equal 14, team.franchise_id
+    assert_equal "FC Cincinnati", team.team_name
 
 
+    assert_instance_of Team, team
+
+  end
+#26,14,FC Cincinnati,CIN,Nippert Stadium,/api/v1/teams/26
 end
