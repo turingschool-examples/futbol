13029fc2d22cd0dcaee1a6866ec0359b39afa0fd
Melanie Tran <melaniexx02@gmail.com>
2020-04-07 13:56:53 -0600

    Refactor setup test

--- test/team_test.rb
+++ test/team_test.rb
@@ -2,15 +2,14 @@ require_relative 'test_helper'
 
 class TeamTest < Minitest::Test
   def setup
-    team_info = {
+    @team = Team.new({
       :team_id => "1",
       :franchiseId => "23",
       :teamName => "Atlanta United",
       :abbreviation => "ATL",
       :Stadium => "Mercedes-Benz Stadium",
       :link => "/api/v1/teams/1"
-    }
-    @team = Team.new(team_info)
+    })
   end
 
   def test_it_exists
