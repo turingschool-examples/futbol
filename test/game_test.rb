3f19267f1cfdc17de89de16ffa3a2028647a1c27
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-08 15:26:02 -0600

    Update file path for new fixture file

--- test/game_test.rb
+++ test/game_test.rb
@@ -20,7 +20,7 @@ class GameTest < Minitest::Test
                 :venue => "Heaven",
                 :venue_link => "venue/link"})
 
-    Game.from_csv('./test/fixtures/short_games.csv')
+    Game.from_csv('./test/fixtures/games_20.csv')
     @game = Game.all[0]
   end
 
@@ -56,11 +56,11 @@ class GameTest < Minitest::Test
 
   def test_it_has_all
     assert_instance_of Array, Game.all
-    assert_equal 100, Game.all.length
+    assert_equal 20, Game.all.length
     assert_instance_of Game, Game.all.first
   end
 
   def test_it_can_count_games_by_season
-    assert_equal ({20122013=>57, 20162017=>4, 20142015=>17, 20152016=>16, 20132014=>6}), Game.count_of_games_by_season
+    assert_equal ({20122013=>2, 20162017=>5, 20142015=>6, 20132014=>4, 20152016=>2, 20172018=>1}), Game.count_of_games_by_season
   end
 end
