0a2b3612a8a00a28def99ce16a4b0ddf0e223d38
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-08 15:26:15 -0600

    Update tests based on new fixture files

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -12,9 +12,9 @@ require './lib/game_team'
 class StatTrackerTest < Minitest::Test
 
   def setup
-    @game_path = './test/fixtures/short_games.csv'
+    @game_path = './test/fixtures/games_20.csv'
     @team_path = './data/teams.csv'
-    @game_teams_path = './test/fixtures/short_game_teams.csv'
+    @game_teams_path = './test/fixtures/game_teams_40.csv'
 
     @locations = {
       games: @game_path,
@@ -30,8 +30,8 @@ class StatTrackerTest < Minitest::Test
   end
 
   def test_it_has_attributes
-    assert_equal "./test/fixtures/short_game_teams.csv", @stat_tracker.game_teams_path
-    assert_equal "./test/fixtures/short_games.csv", @stat_tracker.games_path
+    assert_equal "./test/fixtures/game_teams_40.csv", @stat_tracker.game_teams_path
+    assert_equal "./test/fixtures/games_20.csv", @stat_tracker.games_path
     assert_equal "./data/teams.csv", @stat_tracker.teams_path
   end
 
@@ -51,22 +51,22 @@ class StatTrackerTest < Minitest::Test
   end
 
   def test_it_can_find_number_of_home_games
-    assert_equal 50.00, @stat_tracker.home_games
+    assert_equal 20, @stat_tracker.home_games
   end
 
   def test_it_can_find_percent_home_wins
-    assert_equal 66.00, @stat_tracker.percentage_home_wins
+    assert_equal 60.00, @stat_tracker.percentage_home_wins
   end
 
   def test_it_can_find_percentage_visitor_wins
-    assert_equal 32.00, @stat_tracker.percentage_visitor_wins
+    assert_equal 20.00, @stat_tracker.percentage_visitor_wins
   end
 
   def test_it_can_find_percentage_ties
-    assert_equal 2.00, @stat_tracker.percentage_ties
+    assert_equal 25.00, @stat_tracker.percentage_ties
   end
 
   def test_it_can_count_games_in_a_season
-    assert_equal ({20122013=>57, 20162017=>4, 20142015=>17, 20152016=>16, 20132014=>6}), @stat_tracker.count_of_games_by_season
+    assert_equal ({20122013=>2, 20162017=>5, 20142015=>6, 20132014=>4, 20152016=>2, 20172018=>1}), @stat_tracker.count_of_games_by_season
   end
 end
