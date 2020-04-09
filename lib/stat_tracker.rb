7e26c0fde547391d1828dfdcbb7e76b74e606082
Eric Larson <ericlarsonbroom@gmail.com>
2020-04-09 15:59:59 -0600

    attempted to fix stattracker

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -7,6 +7,30 @@ class StatTracker
   #   teams: team_path,
   #   game_teams: game_teams_path
   # }
+#   locations = {
+#     games: game_path,
+#     teams: team_path,
+#     game_teams: game_teams_path
+#   }
+#
+#   @stat_tracker = StatTracker.from_csv(locations)
+# end
+
+def self.teams(file_path)
+  Team.from_csv(file_path)
+  Team.all_teams
+end
+
+def self.games(file_path)
+  Game.from_csv(file_path)
+  Game.all_games
+end
+
+def self.game_stats(file_path)
+  GameStats.from_csv(file_path)
+  game_stats = GameStats.all_game_stats
+
+end
 
 
   def self.from_csv(file_paths)
@@ -23,19 +47,123 @@ class StatTracker
     @game_teams_path = game_teams_path
   end
 
-  def teams(file_path)
-    Team.from_csv(team_path)
-    Team.all_teams
+
+
+  #   def games(file_path)
+  #     Game.from_csv(game_path)
+  #     Game.all_games
+  # end
+
+  # def game_stats(file_path)
+  #   GameStats.from_csv(file_path)
+  #   game_stats = GameStats.all_game_stats
+  #
+  # end
+
+  def team_info(id)
+    info_hash = Hash.new
+
+    Team.all_teams.each do |team|
+      if team.team_id == id
+      info_hash[:team_id] = team.team_id
+      info_hash[:franchise_id] = team.franchiseid
+      info_hash[:team_name] = team.teamname
+      info_hash[:abbreviation] = team.abbreviation
+      info_hash[:link] =  team.link
+      end
+    end
+      info_hash
   end
 
-    def games(file_path)
-      Game.from_csv(game_path)
-      Game.all_games
+  def highest_total_score(all_games)
+    require"pry";binding.pry
+    Game.highest_total_score
   end
 
-  def game_stats(file_path)
-    GameStats.from_csv(file_path)
-    game_stats = GameStats.all_game_stats
+
+# Season with the highest win percentage for a team.
+      # (win/total) *100
+
+  def best_season(id)
+
+    season_win_percent = Hash.new
+    count = 0
+    wins = Game.all_games.each do |game|
+
+      if game.away_team_id == id && (game.away_goals > game.home_goals) && (season_win_percent[game.season] == nil)
+         # && season_win_percent[game.season] == nil
+        season_win_percent[game.season] = 0
+        season_win_percent[game.season] += (1.to_f/(games_per_season(id, game.season)))
+      elsif game.home_team_id == id && (game.home_goals > game.away_goals) && (season_win_percent[game.season] == nil)
+        season_win_percent[game.season] = 0
+        season_win_percent[game.season] += (1.to_f/(games_per_season(id, game.season)))
+      elsif game.away_team_id == id && (game.away_goals > game.home_goals)
+        season_win_percent[game.season] += (1.to_f/(games_per_season(id, game.season)))
+      elsif game.home_team_id == id && (game.home_goals > game.away_goals)
+        season_win_percent[game.season] += (1.to_f/(games_per_season(id, game.season)))
+      end
+      # end
+    end
+    best_percent = season_win_percent.max_by do |key, value|
+      season_win_percent[key]
+    end
+      best_percent.first
+
 
   end
-end
+
+  def games_per_season(id, game_season)
+    games_per_season = 0
+    Game.all_games.each do |game|
+
+      if (game.away_team_id == id || (game.home_team_id == id)) && (game.season == game_season)
+        games_per_season += 1
+      end
+    end
+    games_per_season
+  end
+
+  def worst_season(id)
+    season_win_percent = Hash.new
+    count = 0
+    wins = Game.all_games.each do |game|
+
+      if game.away_team_id == id && (game.away_goals > game.home_goals) && (season_win_percent[game.season] == nil)
+         # && season_win_percent[game.season] == nil
+        season_win_percent[game.season] = 0
+        season_win_percent[game.season] += (1.to_f/(games_per_season(id, game.season)))
+      elsif game.home_team_id == id && (game.home_goals > game.away_goals) && (season_win_percent[game.season] == nil)
+        season_win_percent[game.season] = 0
+        season_win_percent[game.season] += (1.to_f/(games_per_season(id, game.season)))
+      elsif game.away_team_id == id && (game.away_goals > game.home_goals)
+        season_win_percent[game.season] += (1.to_f/(games_per_season(id, game.season)))
+      elsif game.home_team_id == id && (game.home_goals > game.away_goals)
+        season_win_percent[game.season] += (1.to_f/(games_per_season(id, game.season)))
+      end
+      # end
+    end
+    best_percent = season_win_percent.min_by do |key, value|
+      season_win_percent[key]
+    end
+      best_percent.first
+
+  end
+
+  # average_win_percentage	Average win percentage of all games for a team.
+
+  # def average_win_percentage(id)
+  #   win_percent = 0
+  #   GameStats.all_game_stats.each do |game|
+  #     require"pry";binding.pry
+  #     if (game.team_id == id) && (game.result == win)
+  #       win_percent += (1)
+  #     end
+  #   end
+  #   win
+  #   end
+  #
+
+
+
+
+  end
--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -14,14 +14,21 @@ class StatTrackerTest < Minitest::Test
   #   teams: team_path,
   #   game_teams: game_teams_path
   # }
-
-  def setup
-      @stat_tracker = StatTracker.from_csv({
-        :teams     => "./data/teams.csv",
-        :games => "./data/games.csv",
-        :game_teams => "./data/game_teams.csv"
-      })
-    end
+  #
+  # def setup
+  #     @stat_tracker = StatTracker.from_csv({
+  #       :teams     => "./data/teams.csv",
+  #       :games => "./data/games.csv",
+  #       :game_teams => "./data/game_teams.csv"
+  #     })
+  #   end
+    def setup
+        @stat_tracker = StatTracker.from_csv({
+          :teams     => StatTracker.teams("./data/teams.csv"),
+          :games => StatTracker.games("./data/games.csv"),
+          :game_teams => StatTracker.game_stats("./data/game_teams.csv")
+        })
+      end
 
   def test_it_exists
     assert_instance_of StatTracker, @stat_tracker
@@ -29,22 +36,70 @@ class StatTrackerTest < Minitest::Test
 
   def test_it_has_attributes
 
-    assert_equal "./data/teams.csv", @stat_tracker.team_path
-    assert_equal "./data/games.csv", @stat_tracker.game_path
-    assert_equal "./data/game_teams.csv", @stat_tracker.game_teams_path
+
+     assert_equal "ATL", @stat_tracker.team_path[0].abbreviation
+    assert_equal 2012030221, @stat_tracker.game_path[0].game_id
+    assert_equal 44.8, @stat_tracker.game_teams_path[0].faceoffwinpercentage
 
   end
 
   def test_it_has_teams
   expected = "Atlanta United"
-    assert_equal expected, @stat_tracker.teams(@stat_tracker.team_path)[0].teamname
+
+    assert_equal expected, @stat_tracker.team_path[0].teamname
   end
 
   def test_it_has_games
-    assert_equal 2012030222, @stat_tracker.games(@stat_tracker.game_path)[1].game_id
+    assert_equal 2012030222, @stat_tracker.game_path[1].game_id
   end
 
   def test_it_has_game_stats
-    assert_equal 2012030222, @stat_tracker.game_stats(@stat_tracker.game_teams_path)[2].game_id
+     # require "pry";binding.pry
+    assert_equal 2012030222, @stat_tracker.game_teams_path[2].game_id
+  end
+
+  def test_it_has_info
+
+    expected = {:team_id=>3, :franchise_id=>10, :team_name=>"Houston Dynamo", :abbreviation=>"HOU", :link=>"/api/v1/teams/3"}
+    assert_equal expected, @stat_tracker.team_info(3)
+  end
+
+  def test_best_season
+
+
+
+    expected = "20152016"
+    assert_equal expected, @stat_tracker.best_season(1)
+  end
+
+  def test_games_per_season
+
+    expected = 87
+    assert_equal expected, @stat_tracker.games_per_season(3, "20152016")
+  end
+
+  def test_teams_worst_season
+
+    expected = "20132014"
+    assert_equal expected, @stat_tracker.worst_season(2)
+  end
+
+  # def test_average_win_percentage
+  #   @stat_tracker.game_stats(@stat_tracker.game_teams_path)
+  #   expected = 0.0
+  #   assert_equal expected, @stat_tracker.average_win_percentage(5)
+  #
+  # end
+
+  def test_total_games_played
+
+
+  end
+
+  def test_highest_total_score
+
+    expected = 0
+    assert_equal expected, @stat_tracker.highest_total_score(all_games)
+
   end
 end
