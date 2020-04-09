df0dfb38529044a83058bf25a0d022866cbfa1ae
Danny Ramos <danramos21@gmail.com>
2020-04-08 21:35:36 -0600

    Game and Team

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,15 @@
 class Game
 
   def initialize(data)
-    @data = data
+    @id = data[:game_id]
+    @season = data[:season]
+    @type = data[:type]
+    @date_time = data[:abbreviation]
+    @away_team_id = data[:away_team_id]
+    @home_team_id = data[:home_team_id]
+    @away_goals = data[:away_goals]
+    @home_goals = data[:home_goals]
+    @venue = data[:venue]
+    @venue_link = data[:venue_link]
   end
 end
--- lib/team.rb
+++ lib/team.rb
@@ -3,6 +3,9 @@ class Team
   def initialize(data)
     @id = data[:team_id]
     @franchise_id = data[:franchiseid]
-
+    @name = data[:franchiseid]
+    @abbreviation = data[:abbreviation]
+    @stadium = data[:stadium]
+    @link = data[:link]
   end
 end
