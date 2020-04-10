f01d0a7f7bc5591f7f7bbb62ba6ea583292b6a6d
Melanie Tran <melaniexx02@gmail.com>
2020-04-10 10:13:48 -0600

    Added team info method

--- lib/team_collection.rb
+++ lib/team_collection.rb
@@ -23,4 +23,15 @@ class TeamCollection
   def find(team_id_number)
     @teams.find {|team| team.team_id == team_id_number}
   end
+  
+  def team_info(team_id_number)
+    team_information = {}
+    team_object = @teams.find {|team| team.team_id == team_id_number}
+    team_information[:team_id] = team_object.team_id
+    team_information[:franchiseid] = team_object.franchiseid
+    team_information[:teamname] = team_object.teamname
+    team_information[:abbreviation] = team_object.abbreviation
+    team_information[:link] = team_object.link
+    team_information
+  end
 end
