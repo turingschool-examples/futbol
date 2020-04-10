3ad1932b6e1ffd475d29e78e464c624c27174372
Melanie Tran <melaniexx02@gmail.com>
2020-04-10 10:51:18 -0600

    Created test for most and fewest goals

--- test/team_stats_test.rb
+++ test/team_stats_test.rb
@@ -13,4 +13,16 @@ class TeamStatsTest < Minitest::Test
     assert_equal 3, @team_stats.all_games_for(17).count
     assert_equal 4, @team_stats.all_games_for(5).count
   end
+
+  def test_can_get_most_goals_by_team_id
+    assert_equal 3, @team_stats.most_goals_scored(17)
+    assert_equal 1, @team_stats.most_goals_scored(5)
+    assert_equal 4, @team_stats.most_goals_scored(6)
+  end
+
+  def test_can_find_fewest_goals_by_team_id
+    assert_equal 1, @team_stats.fewest_goals_scored(17)
+    assert_equal 0, @team_stats.fewest_goals_scored(5)
+    assert_equal 1, @team_stats.fewest_goals_scored(6)
+  end
 end
