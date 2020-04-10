bba8227c2d9308722f79ac66e37233999856c759
Kiera Allen <kieraaallen@gmail.com>
2020-04-10 07:25:35 -0600

    Add team_statistics changes

--- lib/team_statistics.rb
+++ lib/team_statistics.rb
@@ -1,7 +1,6 @@
-# team_info
-# A hash with key/value pairs for the following attributes:
+# team_info (A hash with key/value pairs for the following attributes:
 # team_id, franchise_id, team_name,
-# abbreviation, and link
+# abbreviation, and link)
 # best_season
 # worst_season
 # average_win_percentage
@@ -13,7 +12,23 @@
 require 'csv'
 
 class TeamStatistics
+  attr_reader :teams
 
   def initialize(csv_file_path)
+    @teams = create_teams(csv_file_path)
   end
+
+  def create_teams(csv_file_path)
+    csv = CSV.read("#{csv_file_path}", headers: true, header_converters: :symbol)
+    csv.map do |row|
+      Team.new(row)
+    end
+    require "pry"; binding.pry
+  end
+
+  # def team_info(team_id)
+  #   team.team_id
+  # end
+
+
 end
