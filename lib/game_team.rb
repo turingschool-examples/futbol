bf5b785e9f03676c2ac9b9a066981e5d8895e386
Mad Hallé <mad.halle@icloud.com>
2020-04-09 21:14:02 -0600

    game team class updated to return integers and floats

--- lib/game_team.rb
+++ lib/game_team.rb
@@ -16,21 +16,21 @@ class GameTeam
               :takeaways
 
   def initialize(game_team_info)
-    @game_id = game_team_info[:game_id]
-    @team_id = game_team_info[:team_id]
+    @game_id = game_team_info[:game_id].to_i
+    @team_id = game_team_info[:team_id].to_i
     @hoa = game_team_info[:hoa]
     @result = game_team_info[:result]
     @settled_in = game_team_info[:settled_in]
     @head_coach = game_team_info[:head_coach]
-    @goals = game_team_info[:goals]
-    @shots = game_team_info[:shots]
-    @tackles = game_team_info[:tackles]
-    @pim = game_team_info[:pim]
-    @powerPlayOpportunities = game_team_info[:powerPlayOpportunities]
-    @powerPlayGoals = game_team_info[:powerPlayGoals]
-    @faceOffWinPercentage = game_team_info[:faceOffWinPercentage]
-    @giveaways = game_team_info[:giveaways]
-    @takeaways = game_team_info[:takeaways]
+    @goals = game_team_info[:goals].to_i
+    @shots = game_team_info[:shots].to_i
+    @tackles = game_team_info[:tackles].to_i
+    @pim = game_team_info[:pim].to_i
+    @powerPlayOpportunities = game_team_info[:powerPlayOpportunities].to_i
+    @powerPlayGoals = game_team_info[:powerPlayGoals].to_i
+    @faceOffWinPercentage = game_team_info[:faceOffWinPercentage].to_f
+    @giveaways = game_team_info[:giveaways].to_i
+    @takeaways = game_team_info[:takeaways].to_i
   end
 
 end
