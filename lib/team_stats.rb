c10f176d9b291a3adc39915ac4bce08123dfecdb
Melanie Tran <melaniexx02@gmail.com>
2020-04-10 10:50:03 -0600

    Created method for all games for

--- lib/team_stats.rb
+++ lib/team_stats.rb
@@ -11,4 +11,11 @@ class TeamStats
     team_stats_csv = CSV.read("#{file_path}", headers: true, header_converters: :symbol)
     team_stats_csv.map { |row| GameStats.new(row)}
   end
+
+  def all_games_for(team_id)
+    games_played = @team_stats.find_all do |team_stat|
+      team_stat.team_id == team_id
+    end
+    games_played
+  end
 end
