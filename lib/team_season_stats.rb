c0458fcb82c9891836d54d61e1862adeb8bafef7
Melanie Tran <melaniexx02@gmail.com>
2020-04-10 20:57:32 -0600

    Created method to find all home and away games by id

--- lib/team_season_stats.rb
+++ lib/team_season_stats.rb
@@ -5,6 +5,12 @@ class TeamSeasonStats < Collection
   attr_reader :team_stats
 
   def initialize(file_path)
-    @team_stats = create_objects(file_path, Game)
+    @games = create_objects(file_path, Game)
+  end
+
+  def all_games_for(id)
+    @games.find_all do |game|
+      (game.home_team_id == id) || (game.away_team_id == id)
+    end
   end
 end
--- test/team_season_stats_test.rb
+++ test/team_season_stats_test.rb
@@ -8,4 +8,8 @@ class TeamSeasonStatsTest < Minitest::Test
   def test_it_exists
     assert_instance_of TeamSeasonStats, @team_stats
   end
+
+  def test_can_find_games
+    assert_equal 29, @team_stats.all_games_for(17).count
+  end
 end
