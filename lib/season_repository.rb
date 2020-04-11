f6710a3a0b2a4db86450fec066a78ddac0005872
James Belta <james.belta@gmail.com>
2020-04-11 16:06:32 -0600

    Added winningest_coach method

--- lib/season_repository.rb
+++ lib/season_repository.rb
@@ -7,22 +7,59 @@ class SeasonRepository
     @game_collection = CsvHelper.generate_game_array(game_path)
     @game_team_collection = CsvHelper.generate_game_teams_array(game_team_path)
     @team_collection = CsvHelper.generate_team_array(team_path)
-    require 'pry'; binding.pry
-  end
 
-  def id_stats
-    @game_team_collection.to_h{|game| [[game.game_id], [game.head_coach, game.result]]}
   end
-require 'pry'; binding.pry
-  def tally_wins
-    require 'pry'; binding.pry
-    if @games_teams_collection.id_stats
+#
+#  def id_stats
+#    @game_team_collection.to_h{|game| [[game.game_id], [game.head_coach, game.result]]}
+#  end
+#require 'pry'; binding.pry
+#  def tally_wins
+#    require 'pry'; binding.pry
+#    if @games_teams_collection.id_stats
+#    end
+#  end
+  def winningst_coach(season)
+    game_array =  @game_collection.select do |game|
+      #require 'pry'; binding.pry
+      game.season == season
+    end
+    coach_win_percentage = Hash.new
+    count = 0
+    game_id_array = game_array.map do |game| game.game_id
+    end
+
+
+    @game_team_collection.each do |team|
+      game_id_array.each do |id|
+      if ((team.game_id == id) && (team.result == "WIN")) && (coach_win_percentage[team.head_coach] == nil)
+        #require 'pry'; binding.pry
+        coach_win_percentage[team.head_coach] = 0
+        coach_win_percentage[team.head_coach] += (1.to_f / (number_of_games(season, team.head_coach)))
+      elsif ((team.game_id == id) && (team.result == "WIN"))
+        coach_win_percentage[team.head_coach] += (1.to_f / (number_of_games(season, team.head_coach)))
+      end
     end
   end
-
-
-
-
-
-
+  coach_winner = coach_win_percentage.max_by{|key, value| coach_win_percentage[key]}
+  coach_winner.first
+  require 'pry'; binding.pry
 end
+
+    def number_of_games(season, coach)
+      game_array =  @game_collection.select do |game|
+
+        game.season == season
+      end
+      game_id_array = game_array.map do |game| game.game_id
+      end
+      count = 0
+
+     @game_team_collection.each do |game_team|
+        if  game_id_array.include?(game_team.game_id) && (game_team.head_coach == coach)
+          count += 1
+        end
+      end
+      count
+    end
+  end
--- test/season_repository_test.rb
+++ test/season_repository_test.rb
@@ -2,6 +2,5 @@ require './lib/season_repository'
 
 season_repository = SeasonRepository.new('./data/games.csv', './data/game_teams.csv', './data/teams.csv')
 
-def test_id_stats
-  require 'pry'; binding.pry
-end
+season_repository.winningst_coach("20122013")
+season_repository.number_of_games("20122013", "John Tortorella")
