f4f462f5b66ecad271c3e897d4ea2482946d6219
Melanie Tran <melaniexx02@gmail.com>
2020-04-12 15:02:35 -0600

    Created team stats class and method

---
+++ lib/team_stats.rb
@@ -0,0 +1,10 @@
+require 'csv'
+require_relative 'collection'
+
+class TeamStats < Collection
+  attr_reader :team_stats
+
+  def initialize(file_path)
+    @team_stats = create_objects(file_path, GameStats)
+  end
+end
