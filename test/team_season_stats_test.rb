c8ea6fe785041fe57e293d926116b699c7b9acbc
Melanie Tran <melaniexx02@gmail.com>
2020-04-11 21:20:39 -0600

    Updated team stats test

--- test/team_season_stats_test.rb
+++ test/team_season_stats_test.rb
@@ -10,7 +10,7 @@ class TeamSeasonStatsTest < Minitest::Test
   end
 
   def test_can_find_games
-    assert_equal 29, @team_stats.all_games(17).count
+    assert_equal 18, @team_stats.all_games(5).count
   end
 
   def test_can_sort_games_by_season
@@ -20,12 +20,30 @@ class TeamSeasonStatsTest < Minitest::Test
     game3 = mock('game 3')
 
     @team_stats.stubs(:all_games).returns([game1, game2, game3])
-
+    #{season => [game1, game2], season => [game2, game3]}
     assert_equal 2, @team_stats.all_games_by_season(5)
   end
 
   def test_can_count_games_by_season
-    expected = {"20122013"=>26, "20152016"=>5, "20142015"=>5}
-    assert_equal expected, @team_stats.count_all_games_in_season(5)
+    expected = {"20122013"=>11, "20152016"=>3, "20142015"=>4}
+    assert_equal expected, @team_stats.total_games_per_season(5)
+  end
+
+  def test_can_count_all_wins_in_season
+    expected = {"20122013"=>5, "20152016"=>3, "20142015"=>0}
+    assert_equal expected, @team_stats.wins_in_season(5)
+  end
+
+  def test_can_calculate_win_percentage
+    expected = {"20122013"=>45.0, "20152016"=>100.0, "20142015"=>0.0}
+    assert_equal expected, @team_stats.win_percentage(5)
+  end
+
+  def test_can_find_best_season
+    assert_equal "20152016", @team_stats.best_season(5)
+  end
+
+  def test_can_find_worst_season
+    assert_equal "20142015", @team_stats.worst_season(5)
   end
 end
