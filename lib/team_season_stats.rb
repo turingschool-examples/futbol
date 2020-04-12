e58aa6720d98b241f3e0b1c960ede11f151cdd6d
Melanie Tran <melaniexx02@gmail.com>
2020-04-12 15:17:54 -0600

    Created win percentage methods

--- lib/team_season_stats.rb
+++ lib/team_season_stats.rb
@@ -13,4 +13,36 @@ class TeamSeasonStats < Collection
       (game.home_team_id == id) || (game.away_team_id == id)
     end
   end
+
+  def all_games_by_season(id)
+    games_by_season = all_games(id).group_by {|game| game.season}
+    games_by_season
+  end
+
+  def total_games_per_season(id)
+    all_games_by_season(id).reduce({}) do |total, (season, games)|
+      total[season] = games.length
+      total
+    end
+  end
+
+  def wins_in_season(id)
+    all_games_by_season(id).reduce(Hash.new(0)) do |season_wins, (season, games)|
+      wins = games.count do |game|
+        home_wins = game.home_team_id == id && game.home_team_win?
+        away_wins = game.away_team_id == id && game.visitor_team_win?
+        home_wins || away_wins
+      end
+        season_wins[season] += wins
+        season_wins
+    end
+  end
+#h1.merge(h2) {|key, oldval, newval| newval - oldval}
+  def win_percentage(id)
+    wins = wins_in_season(id)
+    total = total_games_per_season(id)
+    wins.merge(total) do |season, wins, games|
+      (wins.to_f/games).round(2) * 100
+    end
+  end
 end
--- test/team_season_stats_test.rb
+++ test/team_season_stats_test.rb
@@ -12,4 +12,30 @@ class TeamSeasonStatsTest < Minitest::Test
   def test_can_find_games
     assert_equal 18, @team_stats.all_games(5).count
   end
+
+#   def test_can_sort_games_by_season
+#   skip
+#   game1 = mock('game 1')
+#   game2 = mock('game 2')
+#   game3 = mock('game 3')
+#
+#   @team_stats.stubs(:all_games).returns([game1, game2, game3])
+#   #{season => [game1, game2], season => [game2, game3]}
+#   assert_equal 2, @team_stats.all_games_by_season(5)
+# end
+
+  def test_can_count_games_by_season
+    expected = {"20122013"=>11, "20152016"=>3, "20142015"=>4}
+    assert_equal expected, @team_stats.total_games_per_season(5)
+  end
+
+  def test_can_count_all_wins_in_season
+    expected = {"20122013"=>5, "20152016"=>3, "20142015"=>0}
+    assert_equal expected, @team_stats.wins_in_season(5)
+  end
+
+  def test_can_calculate_win_percentage
+    expected = {"20122013"=>45.0, "20152016"=>100.0, "20142015"=>0.0}
+    assert_equal expected, @team_stats.win_percentage(5)
+  end
 end
