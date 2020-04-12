be6203a7de9fddcf194ec478f988c8e5afa8570e
Jack Puchalla <Jackpuchallalab@gmail.com>
2020-04-11 20:55:58 -0600

    added helper methods

--- lib/game_collection.rb
+++ lib/game_collection.rb
@@ -17,5 +17,91 @@ class GameCollection
     end
   end
 
+  def highest_total_score
+    total_scores.max
+  end
+
+  def lowest_total_score
+    total_scores.min
+  end
+
+  # percentage_home_wins Percentage of games that a home team has won (rounded to the nearest 100th)Float
+
+  def percentage_home_wins
+    # find total of home wins
+    # take total of home wins and divide by total games played
+    # make percentage
+    home_wins
+  end
+
+  def percentage_visitor_wins
+
+  end
+
+  def percentage_ties
+
+  end
+
+
+# helper methods
+
+def total_scores
+  @games_list.map { |game| game.home_goals.to_i + game.away_goals.to_i }
+end
+
+def home_wins
+  # tying to find all the home wins
+  @games_list.select { |game| game.home_goals > game.away_goals}.length
+end
+
+
+def away_wins
+  @games_list.select { |game| game.home_goals < game.away_goals}.length
+end
+
+def ties
+  @games_list.select { |game| game.home_goals == game.away_goals}.length
+
+end
+# percentage_visitor_wins Percentage of games that a visitor has won (rounded to the nearest 100th)Float
+#
+# percentage_ties Percentage of games that has resulted in a tie (rounded to the nearest 100th)Float
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  # count_of_games_by_season A hash with season names (e.g. 20122013) as keys and counts of games as valuesHash
+  #
+  # average_goals_per_game Average number of goals scored in a game across all seasons including both home and away goals (rounded to the nearest 100th)Float
+  #
+  # average_goals_by_season Average number of goals scored in a game organized in a hash with season names (e.g. 20122013) as keys and a float representing the average
+  #
+  # number of goals in a game for that season as values (rounded to the nearest 100th)Hash
+
+
+
 
 end
--- test/game_collection_test.rb
+++ test/game_collection_test.rb
@@ -13,9 +13,46 @@ class GameCollectionClass < Minitest::Test
   end
 
   def test_it_has_list_of_games
-  assert_instance_of Array, @game_collection.games_list
-  assert_instance_of Game, @game_collection.games_list.first
-  assert_equal 299, @game_collection.games_list.length
+    assert_instance_of Array, @game_collection.games_list
+    assert_instance_of Game, @game_collection.games_list.first
+    assert_equal 18, @game_collection.games_list.length
+  end
+
+  def test_total_scores
+    expected = [3, 5, 3, 8, 6, 6, 5, 5, 1, 2, 5, 4, 3, 7, 5, 3, 3, 3]
+    assert_equal expected, @game_collection.total_scores
+  end
+
+  def test_highest_total_score
+    assert_equal 8, @game_collection.highest_total_score
+  end
+
+  def test_lowest_total_score
+    assert_equal 1, @game_collection.lowest_total_score
+  end
+
+  def test_home_wins
+    assert_equal 12, @game_collection.home_wins
+  end
+
+  def test_away_wins
+    assert_equal 6, @game_collection.away_wins
+  end
+
+  def test_ties
+    assert_equal 0, @game_collection.ties
+  end
+
+  def test_percentage_home_wins
+    assert_equal 21.1, @game_collection.percentage_home_wins
+  end
+
+  def test_percentage_away_wins
+    skip
+  end
+
+  def test_percentage_ties
+    skip
   end
 
 end
