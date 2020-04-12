bc1ed792e1a9a7242af942301e613b2f5fde4f7f
Melanie Tran <melaniexx02@gmail.com>
2020-04-12 15:04:20 -0600

    Created method to find all games for team id

--- lib/team_stats.rb
+++ lib/team_stats.rb
@@ -7,4 +7,11 @@ class TeamStats < Collection
   def initialize(file_path)
     @team_stats = create_objects(file_path, GameStats)
   end
+
+  def all_games_for(team_id)
+    games_played = @team_stats.find_all do |team_stat|
+      team_stat.team_id == team_id
+    end
+    games_played
+  end
 end
--- test/team_stats_test.rb
+++ test/team_stats_test.rb
@@ -8,4 +8,8 @@ class TeamStatsTest < Minitest::Test
   def test_it_exists
     assert_instance_of TeamStats, @team_stats
   end
+
+  def test_can_find_all_games_for
+    assert_equal 6, @team_stats.all_games_for(6).count
+  end
 end
