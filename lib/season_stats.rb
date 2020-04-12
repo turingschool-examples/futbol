b3f8180dab40218579b0ae8409ff91241e0c8670
Michael Gallup <Gallup93@icloud.com>
2020-04-11 18:51:27 -0600

    added find_num_games_played_won_in_season

--- lib/season_stats.rb
+++ lib/season_stats.rb
@@ -12,4 +12,17 @@ class SeasonStats
     end
   end
 
+  def find_num_games_played_won_in_season(season, team_id)
+    results_tracker = {:games_played => 0, :games_won => 0}
+    games = get_games_of_season(season)
+    games.each do |game|
+      if game.team_id == team_id.to_i && game.result == "WIN"
+        results_tracker[:games_played] += 1
+        results_tracker[:games_won] += 1
+      elsif game.team_id == team_id.to_i
+        results_tracker[:games_played] += 1
+      end
+    end
+    results_tracker
+  end
 end
