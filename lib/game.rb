84a4b88d1765d43b3e2433a514b8fc6b7b13f13f
Melanie Tran <melaniexx02@gmail.com>
2020-04-12 15:13:59 -0600

    Added to_i to attributes

--- lib/game.rb
+++ lib/game.rb
@@ -2,12 +2,12 @@ class Game
   attr_reader :game_id, :season, :type, :away_team_id, :home_team_id, :away_goals, :home_goals
   def initialize(info)
 
-    @game_id= info[:game_id]
+    @game_id= info[:game_id].to_i
     @season= info[:season]
     @type= info[:type]
-    @away_team_id= info[:away_team_id]
-    @home_team_id= info[:home_team_id]
-    @away_goals= info[:away_goals]
-    @home_goals= info[:home_goals]
+    @away_team_id= info[:away_team_id].to_i
+    @home_team_id= info[:home_team_id].to_i
+    @away_goals= info[:away_goals].to_i
+    @home_goals= info[:home_goals].to_i
   end
 end
