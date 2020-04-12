4918bd33063dd4337bfdb12fdd3c07f9e88c1b98
Michael Gallup <Gallup93@icloud.com>
2020-04-11 18:54:43 -0600

    added winningest_coach

--- lib/season_stats.rb
+++ lib/season_stats.rb
@@ -31,4 +31,27 @@ class SeasonStats
     (season_results[:games_won].to_f / season_results[:games_played]).round(2)
   end
 
+  def winningest_coach(season)
+    team_ids = []
+    games = get_games_of_season(season)
+
+    games.each do |game|
+      team_ids << game.team_id if team_ids.any?{|id| id == game.team_id} == false
+    end
+
+    percentage_tracker = -1
+    id_tracker = nil
+
+    team_ids.each do |id|
+      percentage = calc_season_win_percentage(season, id)
+      if percentage > percentage_tracker
+        percentage_tracker = percentage
+        id_tracker = id
+      end
+    end
+
+    winner = games.find{|x| x.team_id == id_tracker}
+    winner.head_coach
+  end
+
 end
