cf85df689ac029e4c33bef49cda37607ec4a8bb3
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-11 20:44:12 -0600

    Add test for rival

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -158,4 +158,8 @@ class StatTrackerTest < Minitest::Test
     assert_equal "Houston Dynamo", @stat_tracker.favorite_opponent(6)
   end
 
+  def test_rival
+    assert_equal "FC Dallas", @stat_tracker.rival(3)
+  end 
+
 end
