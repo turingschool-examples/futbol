935883409cd78562cee9ce8d0fe64858b0844e1f
Antonio Jackson <antoniojackson@gmail.com>
2020-04-11 20:00:10 -0600

    Adding require_relative Collection inheritance to TeamCollection and updating teams initialization to use create_objects method from Collection superclass

--- lib/team_collection.rb
+++ lib/team_collection.rb
@@ -1,19 +1,12 @@
 require 'csv'
 require_relative 'team'
+require_relative 'collection'
 
-class TeamCollection
+class TeamCollection < Collection
   attr_reader :teams
 
   def initialize(csv_file_path)
-    @teams = create_teams(csv_file_path)
-  end
-
-  def create_teams(csv_file_path)
-    csv = CSV.read("#{csv_file_path}", headers: true, header_converters: :symbol)
-
-    csv.map do |row|
-      Team.new(row)
-    end
+    @teams = create_objects(csv_file_path, Team)
   end
 
   def count_of_teams
