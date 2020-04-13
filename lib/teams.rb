d4d236162c1507adf71401e4d246d36792c623dc
Kiera Allen <kieraaallen@gmail.com>
2020-04-12 23:48:05 -0600

    Test and add Teams class

---
+++ lib/teams.rb
@@ -0,0 +1,26 @@
+require_relative 'team'
+
+class Teams
+  attr_reader :teams
+
+  def initialize(csv_file_path)
+    @teams = create_teams(csv_file_path)
+  end
+
+  def create_teams(csv_file_path)
+    csv = CSV.read("#{csv_file_path}", headers: true, header_converters: :symbol)
+    csv.map do |row|
+      Team.new(row)
+    end
+  end
+
+  def all
+    @teams
+  end
+
+  def find_by_team_id(team_id)
+    @teams.find do |team|
+      team.team_id == team_id
+    end
+  end
+end
