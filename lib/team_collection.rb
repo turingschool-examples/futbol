d5c04ca3277757a527f25dd5e81df815962da9f5
Kiera Allen <kieraaallen@gmail.com>
2020-04-12 23:55:03 -0600

    Comment out code

--- lib/team_collection.rb
+++ lib/team_collection.rb
@@ -1,29 +1,31 @@
-require_relative 'team'
+# may not need this anymore
 
-class TeamCollection
-  attr_reader :teams
-
-  def initialize(csv_file_path)
-    @teams = create_teams(csv_file_path)
-  end
-
-  def create_teams(csv_file_path)
-    csv = CSV.read("#{csv_file_path}", headers: true, header_converters: :symbol)
-    csv.map do |row|
-      Team.new(row)
-    end
-  end
-
-  def team_info(team_id)
-     found_team = @teams.find do |team|
-       team.team_id == team_id
-     end
-     {
-       :team_id => found_team.team_id,
-       :franchise_id => found_team.franchise_id,
-       :team_name => found_team.team_name,
-       :abbreviation => found_team.abbreviation,
-       :link => found_team.link
-     }
-  end
-end
+# require_relative 'team'
+#
+# class TeamCollection
+#   attr_reader :teams
+#
+#   def initialize(csv_file_path)
+#     @teams = create_teams(csv_file_path)
+#   end
+#
+#   def create_teams(csv_file_path)
+#     csv = CSV.read("#{csv_file_path}", headers: true, header_converters: :symbol)
+#     csv.map do |row|
+#       Team.new(row)
+#     end
+#   end
+#
+#   def team_info(team_id)
+#      found_team = @teams.find do |team|
+#        team.team_id == team_id
+#      end
+#      {
+#        :team_id => found_team.team_id,
+#        :franchise_id => found_team.franchise_id,
+#        :team_name => found_team.team_name,
+#        :abbreviation => found_team.abbreviation,
+#        :link => found_team.link
+#      }
+#   end
+# end
