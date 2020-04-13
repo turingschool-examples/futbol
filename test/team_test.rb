017c4864dafe795d1ec346e7b5f759bfdc687207
Kiera Allen <kieraaallen@gmail.com>
2020-04-12 18:30:26 -0600

    Change setup info from one line to multiple lines

--- test/team_test.rb
+++ test/team_test.rb
@@ -6,7 +6,13 @@ require './lib/team'
 class TeamTest < Minitest::Test
 
   def setup
-    @team = Team.new({:team_id => 1, :franchise_id => 23, :team_name => "Atlanta United", :abbreviation => "ATL", :link => "/api/v1/teams/1"})
+    @team = Team.new({
+      :team_id => 1,
+      :franchise_id => 23,
+      :team_name => "Atlanta United",
+      :abbreviation => "ATL",
+      :link => "/api/v1/teams/1"
+    })
   end
 
   def test_it_exists
