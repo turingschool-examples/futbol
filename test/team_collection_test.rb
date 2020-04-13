c92326f6622aec98a863eade1df866e4a1d2118c
Kiera Allen <kieraaallen@gmail.com>
2020-04-12 18:11:28 -0600

    Combine teams_test.rb and team_collection_test.rb into one file

--- test/team_collection_test.rb
+++ test/team_collection_test.rb
@@ -21,6 +21,10 @@ class TeamCollectionTest < Minitest::Test
     assert_equal "Atlanta United", @team.teamname
   end
 
+  # def test_it_has_all
+  #   assert_equal @teams.teams, @teams.all
+  # end
+
   def test_it_has_team_info
     expected = {:team_id => 1,
                 :franchise_id => 23,
@@ -29,6 +33,10 @@ class TeamCollectionTest < Minitest::Test
                 :link => "/api/v1/teams/1"}
     assert_equal expected, @team_collection.team_info(1)
   end
+
+  # def test_find_by_team_id
+  #   assert_equal @team, @teams.find_by_team_id(@team.team_id)
+  # end
 
   def test_it_can_find_teamname_by_id
     assert_equal "Atlanta United", @team_collection.teamname_by_id(1)
