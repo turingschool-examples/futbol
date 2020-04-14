874d8f0e56f4c8f512696cc7e4e6370d7d69a688
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-14 17:37:20 -0600

    Deleted commented out code

--- lib/collection.rb
+++ lib/collection.rb
@@ -1,8 +1,6 @@
 require "csv"
 class Collection
 
-  # @@all = nil
-
   def self.from_csv(file_path, class_name)
     csv = CSV.read("#{file_path}", headers: true, header_converters: :symbol)
     @all = csv.map do |row|
