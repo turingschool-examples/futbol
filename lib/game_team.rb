e6bfedca7abf21f52eec7bfa3831e75be51368b1
michelle <foleymichelle9@gmail.com>
2020-04-14 17:43:00 -0600

    Fix

--- lib/game_team.rb
+++ lib/game_team.rb
@@ -6,189 +6,41 @@ class GameTeam
     @@all
   end
 
-  def self.find_by_team(team_id)
-    all.find_all{|game| game.team_id == team_id}
-  end
-
-  def self.find_by(id)
-    all.find_all{|game| game.game_id == id}
-  end
-
   def self.from_csv(csv_file_path)
     csv = CSV.read("#{csv_file_path}", headers: true, header_converters: :symbol)
     @@all = csv.map { |row| GameTeam.new(row) }
   end
 
   def self.home_games
-    (all.find_all {|gt| gt.hoa == "home" }).count
+    (@@all.find_all {|gt| gt.hoa == "home" }).count
   end
-#deliverable
+
   def self.percentage_home_wins
-    home_wins = (all.find_all {|gt| gt.hoa == "home" && gt.result == "WIN" }).count.to_f
-    ((home_wins / self.home_games)).round(2)
+    home_wins = (@@all.find_all {|gt| gt.hoa == "home" && gt.result == "WIN" }).count.to_f
+    ((home_wins / self.home_games) * 100).round(2)
   end
-#deliverable
+
   def self.percentage_visitor_wins
-    visitor_wins = (all.find_all {|gt| gt.hoa == "home" && gt.result == "LOSS" }).count.to_f
-    ((visitor_wins / self.home_games)).round(2)
+    visitor_wins = (@@all.find_all {|gt| gt.hoa == "home" && gt.result == "LOSS" }).count.to_f
+    ((visitor_wins / self.home_games) * 100).round(2)
   end
-#deliverable
+
   def self.percentage_ties
-    games_count = all.count.to_f
-    ties_count = (all.find_all { |gt| gt.result == "TIE"}).count.to_f
-    ((ties_count / games_count)).round(2)
+    games_count = @@all.count.to_f
+    ties_count = (@@all.find_all { |gt| gt.result == "TIE"}).count.to_f
+    ((ties_count / games_count) * 100).round(2)
   end
 
-  def self.coaches_in_season(season_id)
-    search_term = season_id.to_s[0..3]
-    game_teams_in_season = all.find_all do |gt|
-      gt.game_id.to_s[0..3] == search_term
-    end
-    game_teams_in_season.map {|gameteam| gameteam.head_coach}.uniq
+  def self.winningest_coach
   end
 
-  def self.results_by_coach(season_id)
-    search_term = season_id.to_s[0..3]
-    results_by_coach_by_season = Hash.new { |hash, key| hash[key] = [] }
-    coaches_in_season(season_id).each do |coach|
-      all.find_all do |gt|
-        if gt.game_id.to_s[0..3] == search_term && coach == gt.head_coach
-            results_by_coach_by_season[coach] << gt.result
-        end
-      end
-    end
-    results_by_coach_by_season
-  end
-
-  def self.total_games_coached(season_id)
-    total_games_coached_by_season = {}
-    results_by_coach(season_id).map do |coach, results|
-      total_games_coached_by_season[coach] = results.length
-    end
-    total_games_coached_by_season
-  end
-#deliverbale
-  def self.wins_by_coach(season_id)
-    wins_by_coach_by_season = Hash.new { |hash, key| hash[key] = 0 }
-    results_by_coach(season_id).each do |coach, results|
-      results.each do |result|
-        wins_by_coach_by_season[coach] += 1 if result == "WIN"
-      end
-    end
-    wins_by_coach_by_season
-  end
-
-#####30 Seconds
-  def self.winningest_coach(season_id)
-    coaches_in_season(season_id).max_by {|coach| (wins_by_coach(season_id)[coach].to_f / total_games_coached(season_id)[coach].to_f).round(2)}
-  end
-#####30 Seconds
-  def self.worst_coach(season_id)
-    coaches_in_season(season_id).min_by do |coach|
-      (wins_by_coach(season_id)[coach].to_f / total_games_coached(season_id)[coach].to_f).round(2)
-    end
-  end
-
-
-  def self.game_team_shots_goals_count(arr_games)
-    season = arr_games.first.game_id
-    self.find_by(season)
-  end
-
-  def self.get_goal_shots_by_game_team(game_teams)
-    #passes in an array of games
-    results = {}
-    #each game, iterating through and adding the team_id as the key and
-    #getting the shots and goals count
-    game_teams.each do |game_team|
-      results[game_team.team_id] ||= {"shots"=>0,"goals"=>0}
-      results[game_team.team_id]["shots"] += game_team.shots
-      results[game_team.team_id]["goals"] += game_team.goals
-    end
-    results
-  end
-
-  def self.least_accurate_team(season)
-    games_by_season = Game.grouped_by_season(season)
-    game_teams_by_season = []
-    games_by_season.each do |game|
-      game_teams_by_season << GameTeam.find_by(game.game_id)
-    end
-    game_teams_by_season.flatten!
-    least_accurate_team_id = nil
-    lowest_average = 5
-    grouped_game_teams_by_season = game_teams_by_season.group_by{|group|group.team_id}
-    grouped_game_teams_by_season.each do |team_id, team_games|
-      average = team_games.sum(&:goals) / team_games.sum(&:shots).to_f
-      if average < lowest_average
-        least_accurate_team_id = team_id
-        lowest_average = average
-      end
-    end
-    least_accurate_team_id
-  end
-
-  def self.most_accurate_team(season)
-    games_by_season = Game.grouped_by_season(season)
-    game_teams_by_season = []
-    games_by_season.each do |game|
-      game_teams_by_season << GameTeam.find_by(game.game_id)
-    end
-    game_teams_by_season.flatten!
-    most_accurate_team_id = nil
-    highest_average = 7
-    grouped_game_teams_by_season = game_teams_by_season.group_by{|group|group.team_id}
-    grouped_game_teams_by_season.each do |team_id, team_games|
-      average = team_games.sum(&:shots) / team_games.sum(&:goals).to_f
-      if average < highest_average
-        most_accurate_team_id = team_id
-        highest_average = average
-      end
-    end
-    most_accurate_team_id
-  end
-
-
-  # def self.least_accurate_team(season)
-  #   season = season.to_i
-  #    seasonal_hash = gets_team_shots_goals_count(season)
-  #    seasonal_hash.map do |key,value|
-  #      value["average"] = (value["goals"]/ value["shots"].to_f).round(2)
-  #    end
-  #    team_hash_with_highest_average = seasonal_hash.min_by do |key,value|
-  #      value["average"]
-  #    end
-  #    team_hash_with_highest_average[0]
-  # end
-  #
-  # def self.most_accurate_team(season)
-  #   season = season.to_i
-  #   seasonal_hash = gets_team_shots_goals_count(season)
-  #   seasonal_hash.map do |key,value|
-  #     value["average"] = (value["goals"]/ value["shots"].to_f).round(2)
-  #   end
-  #   team_hash_with_highest_average = seasonal_hash.max_by do |key,value|
-  #     value["average"]
-  #   end
-  #   team_hash_with_highest_average[0]
-  # end
-
-  def self.gets_team_shots_goals_count(season)
-    #passes in desired season, grabs the *games* for the season
-    season_games = Game.grouped_by_season(season)
-    matches = []
-    #gets the game_teams associated with the season
-    season_games.each do |game|
-      matches.concat(GameTeam.find_by(game.game_id))
-    end
-    #takes the game_teams and runs them through the below method
-    stats_by_team = get_goal_shots_by_game_team(matches)
+  def self.worst_coach
   end
 
 #Michelle Start
   def self.games_ids_by_season(season_id)
-    game_id_first = season_id.to_s[0..3]
-    all_games_by_id = all.find_all {|game| game.season_id == game_id_first}
+    game_id_first = season_id.to_s[0..3].to_i
+    all_games_by_id = all.find_all { |game| game.season_id == game_id_first }
     all_games_by_id.map { |game| game.game_id }
   end
 
@@ -214,110 +66,17 @@ class GameTeam
       end
       tackles_by_team
   end
-#####45 Seconds
-  def self.most_tackles(season_id)
-    most_tackles = tackles_by_team(season_id).max_by { |key, value| value}
-    most_tackles.first
-  end
-#####45 Seconds
-  def self.fewest_tackles(season_id)
-    fewest_tackles = tackles_by_team(season_id).min_by { |key, value| value}
-    fewest_tackles.first
-  end
 
-
-  def self.best_offense
-    #grouped by team_id with keys being the team_id and values is an array of games
-    grouped_team = all.group_by{|game| game.team_id}
-    #loop through the values (games) and set them equal to the average of goals
-    team_averaged_goals = grouped_team.map do |ids, games|
-      goals_per_game = games.map {|game| game.goals}
-      games = (goals_per_game.sum / goals_per_game.length.to_f)
+    def self.most_tackles(season_id)
+      most_tackles = tackles_by_team(season_id).max_by { |key, value| value}
+      most_tackles.first
     end
-    total_goals_per_team= Hash[grouped_team.keys.zip(team_averaged_goals)]
-    total_goals_per_team.key(total_goals_per_team.values.max)
-  end
 
-  def self.worst_offense
-
-    #grouped by team_id with keys being the team_id and values is an array of games
-    grouped_team = all.group_by{|game| game.team_id}
-    #loop through the values (games) and set them equal to the average of goals
-    team_averaged_goals = grouped_team.map do |ids, games|
-      goals_per_game = games.map {|game| game.goals}
-      games = (goals_per_game.sum / goals_per_game.length.to_f)
+    def self.fewest_tackles(season_id)
+      fewest_tackles = tackles_by_team(season_id).max_by { |key, value| -value}
+      fewest_tackles.first
     end
-    total_goals_per_team= Hash[grouped_team.keys.zip(team_averaged_goals)]
-    total_goals_per_team.key(total_goals_per_team.values.min)
-  end
-
-  def self.most_goals_scored(team_id)
-  total_game_teams_per_team_id = find_by_team(team_id)
-  results = {}
-  total_game_teams_per_team_id.each do |game_team|
-    results[game_team.game_id] ||= {"team_id"=>0, "goals"=>0}
-    results[game_team.game_id]["team_id"] = game_team.team_id
-    results[game_team.game_id]["goals"] = game_team.goals
-  end
-  max_goals = results.max_by{|key,value| value["goals"]}
-  return max_goals[1]["goals"]
-  end
-
-  def self.fewest_goals_scored(team_id)
-    total_game_teams_per_team_id = find_by_team(team_id)
-    results = {}
-    total_game_teams_per_team_id.each do |game_team|
-      results[game_team.game_id] ||= {"team_id"=>0, "goals"=>0}
-      results[game_team.game_id]["team_id"] = game_team.team_id
-      results[game_team.game_id]["goals"] = game_team.goals
-      end
-    min_goals = results.min_by{|key,value| value["goals"]}
-    return min_goals[1]["goals"]
-  end
-
-  def self.opponents_records(team_id)
-    game_ids = all.map {|gt| gt.game_id if gt.team_id == team_id }.compact
-    opponents_records = Hash.new { |hash, key| hash[key] = [] }
-    game_ids.each do |game_id|
-      all.find_all do |gt|
-        opponents_records[gt.team_id] << gt.result if gt.game_id == game_id && gt.team_id != team_id
-      end
-    end
-    opponents_records
-  end
-
-  def self.opponents_wins(team_id)
-    opponent_wins = Hash.new { |hash, key| hash[key] = 0 }
-    opponents_records(team_id).each do |team_id, record|
-      record.each do |result|
-        opponent_wins[team_id] += 1 if result == "WIN"
-      end
-    end
-    opponent_wins
-  end
-
-####1:15 seconds
-  def self.favorite_opponent_id(team_id)
-    team_id = team_id
-    record_length = {}
-    opponents_records(team_id).map do |team_id, record|
-      record_length[team_id] = record.length
-    end
-    opponents = opponents_records(team_id).keys
-    opponents.min_by {|opponent| (opponents_wins(team_id)[opponent].to_f / record_length[opponent].to_f).round(2)}
-  end
-####2:30 seconds
-  def self.rival_id(team_id)
-    team_id = team_id
-    record_length = {}
-    opponents_records(team_id).map do |team_id, record|
-      record_length[team_id] = record.length
-    end
-    opponents = opponents_records(team_id).keys
-    opponents.max_by {|opponent| (opponents_wins(team_id)[opponent].to_f / record_length[opponent].to_f).round(2)}
-  end
-#
-
+    #Michelle end
 
 
     attr_reader :game_id,
@@ -338,8 +97,8 @@ class GameTeam
                 :season_id
 
   def initialize(details)
-    @game_id = details[:game_id]
-    @team_id = details[:team_id]
+    @game_id = details[:game_id].to_i
+    @team_id = details[:team_id].to_i
     @hoa = details[:hoa]
     @result = details[:result]
     @settled_in = details[:settled_in]
@@ -353,12 +112,6 @@ class GameTeam
     @face_off_win_percentage = details[:faceoffwinpercentage].to_f.round(2)
     @giveaways = details[:giveaways].to_i
     @takeaways = details[:takeaways].to_i
-    @season_id = @game_id.to_s[0..3]
+    @season_id = @game_id.to_s[0..3].to_i
   end
-
-  def win?
-    return 1 if @result == "WIN"
-    0
-  end
-
 end
--- test/game_team_test.rb
+++ test/game_team_test.rb
@@ -6,7 +6,7 @@ require 'pry'
 require './lib/stat_tracker'
 require './lib/game_team'
 require 'mocha/minitest'
-
+#
 class GameTeamTest < Minitest::Test
 
   def setup
@@ -14,8 +14,8 @@ class GameTeamTest < Minitest::Test
     @game_teams = GameTeam.from_csv(file_path)
 
     @base_game_team = GameTeam.new({
-      :game_id => "2012030221",
-      :team_id => "3",
+      :game_id => 2012030221,
+      :team_id => 3,
       :hoa => "away",
       :result => "LOSS",
       :settled_in => "OT",
@@ -44,8 +44,8 @@ class GameTeamTest < Minitest::Test
   end
 
   def test_it_returns_attributes_of_base
-    assert_equal "2012030221", @base_game_team.game_id
-    assert_equal "3", @base_game_team.team_id
+    assert_equal 2012030221, @base_game_team.game_id
+    assert_equal 3, @base_game_team.team_id
     assert_equal "away", @base_game_team.hoa
     assert_equal "LOSS", @base_game_team.result
     assert_equal "OT", @base_game_team.settled_in
@@ -62,8 +62,8 @@ class GameTeamTest < Minitest::Test
   end
 
   def test_it_returns_attributes_from_collection
-    assert_equal "2012030221", @game_teams[0].game_id
-    assert_equal "3", @game_teams[0].team_id
+    assert_equal 2012030221, @game_teams[0].game_id
+    assert_equal 3, @game_teams[0].team_id
     assert_equal "away", @game_teams[0].hoa
     assert_equal "LOSS", @game_teams[0].result
     assert_equal "OT", @game_teams[0].settled_in
@@ -84,126 +84,47 @@ class GameTeamTest < Minitest::Test
   end
 
   def test_it_can_find_percent_home_wins
-    assert_equal 0.60, GameTeam.percentage_home_wins
+    assert_equal 60.00, GameTeam.percentage_home_wins
   end
 
   def test_it_can_find_percentage_visitor_wins
-    assert_equal 0.20, GameTeam.percentage_visitor_wins
+    assert_equal 20.00, GameTeam.percentage_visitor_wins
   end
 
   def test_it_can_find_percentage_ties
-    assert_equal 0.25, GameTeam.percentage_ties
-  end
-
-  def test_it_can_find_coaches_in_season
-    assert_equal ["Willie Desjardins", "Darryl Sutter", "Claude Julien", "Michel Therrien", "Joel Quenneville", "Jared Bednar", "Mike Babcock", "Bruce Cassidy", "Lindy Ruff"], GameTeam.coaches_in_season(2016030134)
-  end
-
-  def test_it_can_find_game_results_by_coach
-    assert_equal ({"Willie Desjardins"=>["TIE", "TIE"], "Darryl Sutter"=>["TIE", "LOSS"], "Claude Julien"=>["WIN", "WIN"], "Michel Therrien"=>["LOSS"], "Joel Quenneville"=>["WIN"], "Jared Bednar"=>["TIE"], "Mike Babcock"=>["TIE", "WIN"], "Bruce Cassidy"=>["WIN"], "Lindy Ruff"=>["WIN", "WIN"]}), GameTeam.results_by_coach(2016030134)
-  end
-
-  def test_it_can_find_total_games_coached
-    assert_equal ({"Willie Desjardins"=>2, "Darryl Sutter"=>2, "Claude Julien"=>2, "Michel Therrien"=>1, "Joel Quenneville"=>1, "Jared Bednar"=>1, "Mike Babcock"=>2, "Bruce Cassidy"=>1, "Lindy Ruff"=>2}), GameTeam.total_games_coached(2016030134)
-  end
-
-  def test_it_can_count_wins_by_coach
-    assert_equal ({"Claude Julien"=>2, "Joel Quenneville"=>1, "Mike Babcock"=>1, "Bruce Cassidy"=>1, "Lindy Ruff"=>2}), GameTeam.wins_by_coach(2016030134)
+    assert_equal 25.00, GameTeam.percentage_ties
   end
 
   def test_it_can_find_winninest_coach
-    assert_equal "Claude Julien", GameTeam.winningest_coach("2016030134")
+
   end
 
   def test_it_can_find_worst_coach
-    assert_equal "Willie Desjardins", GameTeam.worst_coach("2016030134")
-  end
-
-  def test_find_by_returns_games
-    assert_kind_of Array, GameTeam.find_by("2012030221")
-  end
-
-  def test_game_team_shots_goals_count
-    Game.from_csv('./test/fixtures/games_20.csv')
-    # arr_games = Game.all[0..2]
-    game_team1 = mock
-    game_team2 = mock
-    game_team3 = mock
-    game_team4 = mock
-    game_team1.stubs(:game_id).returns("2016020527")
-    game_team2.stubs(:game_id).returns("123")
-    game_team3.stubs(:game_id).returns("456")
-    game_team4.stubs(:game_id).returns("456")
-    passed_array = [game_team1,game_team2,game_team3,game_team4]
-    assert_kind_of Array , GameTeam.game_team_shots_goals_count(passed_array)
-  end
-
-  def test_get_goal_shots_by_game_team
-    game_team1 = mock
-    game_team2 = mock
-    game_team3 = mock
-    game_team4 = mock
-    game_team1.stubs(:team_id).returns("123")
-    game_team2.stubs(:team_id).returns("123")
-    game_team3.stubs(:team_id).returns("456")
-    game_team4.stubs(:team_id).returns("456")
-    game_team1.stubs(:goals).returns(3)
-    game_team2.stubs(:goals).returns(3)
-    game_team3.stubs(:goals).returns(2)
-    game_team4.stubs(:goals).returns(2)
-    game_team1.stubs(:shots).returns(6)
-    game_team2.stubs(:shots).returns(6)
-    game_team3.stubs(:shots).returns(4)
-    game_team4.stubs(:shots).returns(4)
-    passed_array = [game_team1,game_team2,game_team3,game_team4]
-
-    expected = {"123"=>{"shots"=>12, "goals"=>6}, "456"=>{"shots"=>8, "goals"=>4}}
-
-    assert_equal expected, GameTeam.get_goal_shots_by_game_team(passed_array)
-  end
-
-  def test_most_accurate_team
-    Game.from_csv('./test/fixtures/games_20.csv')
-    arr_games = Game.all[0..2]
-    assert_equal "6", GameTeam.most_accurate_team("20122013")
   end
 #Michelle Start
   def test_it_can_find_game_ids_by_season
-    expected = ["2012030221", "2012030221", "2012030222", "2012030222", "2012030237", "2012030237", "2012030121", "2012030121", "2012030322", "2012030322", "2012020035", "2012020035"]
-    assert_equal expected, GameTeam.games_ids_by_season("20122013")
+    expected = [2012030221, 2012030221, 2012030222, 2012030222, 2012030237, 2012030237, 2012030121, 2012030121, 2012030322, 2012030322, 2012020035, 2012020035]
+    assert_equal expected, GameTeam.games_ids_by_season(20122013)
   end
 
   def test_it_find_games_by_season_id
-    assert_equal 12, GameTeam.games_by_season_id("20122013").length
+    assert_equal 12, GameTeam.games_by_season_id(20122013).length
   end
 
   def test_it_can_find_games_by_team_name
-      assert_equal Hash, GameTeam.games_by_team_name("20122013").class
+      assert_equal Hash, GameTeam.games_by_team_name(20122013).class
   end
 
   def test_it_can_find_tackles_by_team
-    expected = {"3"=>77, "6"=>87, "17"=>38, "16"=>72, "9"=>31, "8"=>24, "26"=>46, "28"=>13, "22"=>10}
-    assert_equal expected, GameTeam.tackles_by_team("20122013")
+    assert_equal 77, GameTeam.tackles_by_team(20122013)[3]
   end
 
-  def test_team_with_most_tackles
-    assert_equal "6", GameTeam.most_tackles("20122013")
+  def test_most_tackles
+    assert_equal 6, GameTeam.most_tackles(20122013)
   end
 
-  def test_team_with_fewest_tackles
-    assert_equal "22", GameTeam.fewest_tackles("20122013")
+  def test_fewest_tackles
+    assert_equal 22, GameTeam.fewest_tackles(20122013)
   end
   #Michelle end
-
-  def test_least_accurate_team
-    Game.from_csv('./test/fixtures/games_20.csv')
-    arr_games = Game.all[0..2]
-    assert_equal "3", GameTeam.least_accurate_team("20122013")
-  end
-
-  def test_it_can_return_best_offense_team_number
-
-    assert_equal "28", GameTeam.best_offense
-  end
-
 end
