02e271db598e09075a2f40a29c721f64a00a0eeb
Antonio Jackson <antoniojackson@gmail.com>
2020-04-13 20:03:59 -0600

    Refactoring test_goals_by_team_id to include all, home, and away games
    Refactoring test_average_goals_by_team_id to include all, home, and away 
    games and removing associated tests for old methods that were removed
    
    Adding a test for find_team_id and removing the associated tests for old 
    methods that were removed

--- test/game_stats_collection_test.rb
+++ test/game_stats_collection_test.rb
@@ -36,16 +36,23 @@ class GameStatsCollectionTest < Minitest::Test
 
   def test_goals_by_team_id
     result = {3=>[2, 2, 1, 2, 1], 6=>[3, 2, 3, 3, 3, 4], 5=>[0]}
-    assert_equal result, @game_stats_collection.goals_by_team_id
+    assert_equal result, @game_stats_collection.goals_by_team_id("all")
+
+    result2 = {3=>[2, 2, 1], 6=>[2, 3, 3, 4]}
+    assert_equal result2, @game_stats_collection.goals_by_team_id("away")
+
+    result3 = {6=>[3, 3], 3=>[1, 2], 5=>[0]}
+    assert_equal result3, @game_stats_collection.goals_by_team_id("home")
   end
 
   def test_average_goals_by_team_id
     result = {3=>1.6, 6=>3.0, 5=>0.0}
-    assert_equal result, @game_stats_collection.average_goals_by_team_id
+    assert_equal result, @game_stats_collection.average_goals_by_team_id("all")
   end
 
-  def test_best_offense_id
-    assert_equal 6, @game_stats_collection.best_offense_id
+  def test_find_team_id
+    assert_equal 6, @game_stats_collection.find_team_id("all", "max")
+    assert_equal 5, @game_stats_collection.find_team_id("all", "min")
   end
 
   def test_find_team_name_by_id
@@ -56,63 +63,65 @@ class GameStatsCollectionTest < Minitest::Test
     assert_equal "FC Dallas", @game_stats_collection.best_offense
   end
 
-  def test_worst_offense_id
-    assert_equal 5, @game_stats_collection.worst_offense_id
-  end
-
   def test_worst_offense
     assert_equal "Sporting Kansas City", @game_stats_collection.worst_offense
   end
 
-  def test_away_goals_by_team_id
-    result = {3=>[2, 2, 1], 6=>[2, 3, 3, 4]}
-    assert_equal result, @game_stats_collection.away_goals_by_team_id
-  end
-
   def test_average_away_goals_by_team_id
+    skip
     result = {3=>1.67, 6=>3.0}
     assert_equal result, @game_stats_collection.average_away_goals_by_team_id
   end
 
   def test_highest_scoring_visitor_id
+    skip
     assert_equal 6, @game_stats_collection.highest_scoring_visitor_id
   end
 
   def test_highest_scoring_visitor
+    skip
     assert_equal "FC Dallas", @game_stats_collection.highest_scoring_visitor
   end
 
   def test_home_goals_by_team_id
+    skip
     result = {6=>[3, 3], 3=>[1, 2], 5=>[0]}
     assert_equal result, @game_stats_collection.home_goals_by_team_id
   end
 
   def test_average_home_goals_by_team_id
+    skip
     result = {6 => 3.00, 3 => 1.50, 5 => 0}
     assert_equal result, @game_stats_collection.average_home_goals_by_team_id
   end
 
   def test_highest_scoring_home_team_id
+    skip
     assert_equal 6, @game_stats_collection.highest_scoring_home_team_id
   end
 
   def test_highest_scoring_home_team
+    skip
     assert_equal "FC Dallas", @game_stats_collection.highest_scoring_home_team
   end
 
   def test_lowest_scoring_visitor_id
+    skip
     assert_equal 3, @game_stats_collection.lowest_scoring_visitor_id
   end
 
   def test_lowest_scoring_visitor
+    skip
     assert_equal "Houston Dynamo", @game_stats_collection.lowest_scoring_visitor
   end
 
   def test_lowest_scoring_home_team_id
+    skip
     assert_equal 5, @game_stats_collection.lowest_scoring_home_team_id
   end
 
   def test_lowest_scoring_home_team
+    skip
     assert_equal "Sporting Kansas City", @game_stats_collection.lowest_scoring_home_team
   end
 
