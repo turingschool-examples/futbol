5dc13582231bd6d284d8b05d3e3d477cfeaae529
Melanie Tran <melaniexx02@gmail.com>
2020-04-14 11:23:16 -0600

    Updated stat_tracker test with league stats methods

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -58,4 +58,32 @@ class StatTrackerTest < Minitest::Test
   def test_can_find_rival
     assert_equal "New England Revolution", @stat_tracker.rival(9)
   end
+
+  def test_can_count_of_teams
+    assert_equal 32, @stat_tracker.count_of_teams
+  end
+
+  def test_can_get_best_offense
+    assert_equal "Reign FC", @stat_tracker.best_offense
+  end
+
+  def test_can_get_worst_offense
+    assert_equal "Utah Royals FC", @stat_tracker.worst_offense
+  end
+
+  def test_can_get_highest_scoring_visitor
+    assert_equal "FC Dallas", @stat_tracker.highest_scoring_visitor
+  end
+
+  def test_can_get_highest_scoring__home_team
+    assert_equal "Reign FC", @stat_tracker.highest_scoring_home_team
+  end
+
+  def test_can_get_lowest_scoring_visitor
+    assert_equal "San Jose Earthquakes", @stat_tracker.lowest_scoring_visitor
+  end
+
+  def test_can_get_lowest_scoring_home_team
+    assert_equal "Utah Royals FC", @stat_tracker.lowest_scoring_home_team
+  end
 end
