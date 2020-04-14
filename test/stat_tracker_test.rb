398b56e88090b4e3dc32c7f6d7a3b0064bcc62bf
Melanie Tran <melaniexx02@gmail.com>
2020-04-14 11:08:10 -0600

    Added team stats test from stat tracker to the test

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -26,4 +26,36 @@ class StatTrackerTest < Minitest::Test
     assert_equal CSV.read(@game_teams_path, headers: true, header_converters: :symbol), @stat_tracker.game_stats
   end
 
+  def test_can_get_team_info
+    expected = {"team_id"=>"9", "franchise_id"=>"30", "team_name"=>"New York City FC", "abbreviation"=>"NYC", "link"=>"/api/v1/teams/9"}
+    assert_equal expected, @stat_tracker.team_info(9)
+  end
+
+  def test_can_get_most_goals_scored
+    assert_equal 5, @stat_tracker.most_goals_scored(9)
+  end
+
+  def test_can_get_fewest_goals_scored
+    assert_equal 0, @stat_tracker.fewest_goals_scored(9)
+  end
+
+  def test_can_average_win_percentage
+    assert_equal 0.35, @stat_tracker.average_win_percentage(9)
+  end
+
+  def test_can_find_best_season
+    assert_equal "20162017", @stat_tracker.best_season(9)
+  end
+
+  def test_can_find_worst_season
+    assert_equal "20172018", @stat_tracker.worst_season(9)
+  end
+
+  def test_can_find_favorite_opponent
+    assert_equal "FC Cincinnati", @stat_tracker.favorite_opponent(9)
+  end
+
+  def test_can_find_rival
+    assert_equal "New England Revolution", @stat_tracker.rival(9)
+  end
 end
