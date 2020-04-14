9e559e6fbeb0bf7d0b44c1689ace99b11c39a845
Alex Pariseau <arpariseau@gmail.com>
2020-04-13 18:35:26 -0600

    Adjusted stats to accommodate new season stats child class

--- lib/stats.rb
+++ lib/stats.rb
@@ -3,8 +3,9 @@ require_relative 'game'
 require_relative 'team'
 
 class Stats
-  
+
   attr_reader :games, :teams, :game_teams
+
   def self.from_csv(locations)
     games_path = locations[:games]
     teams_path = locations[:teams]
@@ -17,9 +18,27 @@ class Stats
     GameTeam.from_csv(game_teams_path)
     Team.from_csv(teams_path)
 
-    @games = Game.all
-    @teams = Team.all
-    @game_teams = GameTeam.all
+    @@games = Game.all
+    @@teams = Team.all
+    @@game_teams = GameTeam.all
+  end
+
+  def self.games
+    @@games
+  end
+
+  def self.teams
+    @@teams
+  end
+
+  def self.game_teams
+    @@game_teams
+  end
+
+  def team_games_by_season(season) # test
+    season_games = @@games.find_all{|game| game.season == season}
+    season_game_ids = season_games.map{|game| game.game_id}
+    @@game_teams.find_all{|team| season_game_ids.include?(team.game_id)}
   end
 
 end
