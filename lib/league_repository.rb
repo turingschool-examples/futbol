1c24bc283d26c8618e77b958cd11b52ba8d86aae
Jane Greene <janegreenecasa@gmail.com>
2020-04-14 19:09:49 -0600

    Initialized hashes with zero to save lines

--- lib/league_repository.rb
+++ lib/league_repository.rb
@@ -10,15 +10,15 @@ class LeagueRepository < Repository
   end
 
   def best_offense
-    average_team_goals = Hash.new
-    total_games = Hash.new
+    average_team_goals = Hash.new(0)
+    total_games = Hash.new(0)
     @game_team_collection.each do |game|
-      if total_games[game.team_id] == nil
-        total_games[game.team_id] = 0
+      # if total_games[game.team_id] == nil
+      #   total_games[game.team_id] = 0
+      #   total_games[game.team_id] += 1
+      # else
         total_games[game.team_id] += 1
-      else
-        total_games[game.team_id] += 1
-      end
+      # end
     end
     @game_team_collection.each do |game|
       if average_team_goals[game.team_id] == nil
