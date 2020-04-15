3987bed41f6eeffd8c1e74ac26b6a909de427687
Michael Gallup <Gallup93@icloud.com>
2020-04-15 12:20:28 -0600

    refactored info_by_season by breaking into helper methods

--- lib/modules/listable.rb
+++ lib/modules/listable.rb
@@ -7,50 +7,73 @@ module Listable
 
   def info_by_season(season, type)
     if type == :head_coach
-      season.map {|game| game.head_coach}.uniq
+      coaches_by_season(season)
     elsif type == :team_id
-      season.map {|game| game.team_id}.uniq
+      team_ids_by_season(season)
     elsif type == :team_tackles
-      teams_tackles = {}
-      season.each do |game|
-        if teams_tackles.any?{teams_tackles[game.team_id]} == false
-          teams_tackles[game.team_id] = game.tackles
-        else
-          teams_tackles[game.team_id] += game.tackles
-        end
-      end
-      teams_tackles
+      team_tackles_by_season(season)
     elsif type == :team_shots
-      teams_shots = {}
-      season.each do |game|
-        if teams_shots.any?{teams_shots[game.team_id]} == false
-          teams_shots[game.team_id] = game.shots
-        else
-          teams_shots[game.team_id] += game.shots
-        end
-      end
-      teams_shots
+      team_shots_by_season(season)
     elsif type == :team_goals
-      teams_goals = {}
-      season.each do |game|
-        if teams_goals.any?{teams_goals[game.team_id]} == false
-          teams_goals[game.team_id] = game.goals
-        else
-          teams_goals[game.team_id] += game.goals
-        end
-      end
-      teams_goals
+      team_goals_by_season(season)
     elsif type == :team_wins
-      teams_wins = {}
-      season.each do |game|
-        if teams_wins.any?{teams_wins[game.team_id]} == false && game.result == "WIN"
-          teams_wins[game.team_id] = 1
-        elsif game.result == "WIN"
-          teams_wins[game.team_id] += 1
-        end
-      end
-      require "pry";binding.pry
-      teams_wins
+      team_wins_by_season(season)
     end
+  end
+
+  def coaches_by_season(season)
+    season.map {|game| game.head_coach}.uniq
+  end
+
+  def team_ids_by_season(season)
+    season.map {|game| game.team_id}.uniq
+  end
+
+  def team_tackles_by_season(season)
+    teams_tackles = {}
+    season.each do |game|
+      if teams_tackles.any?{teams_tackles[game.team_id]} == false
+        teams_tackles[game.team_id] = game.tackles
+      else
+        teams_tackles[game.team_id] += game.tackles
+      end
+    end
+    teams_tackles
+  end
+
+  def team_shots_by_season(season)
+    teams_shots = {}
+    season.each do |game|
+      if teams_shots.any?{teams_shots[game.team_id]} == false
+        teams_shots[game.team_id] = game.shots
+      else
+        teams_shots[game.team_id] += game.shots
+      end
+    end
+    teams_shots
+  end
+
+  def team_goals_by_season(season)
+    teams_goals = {}
+    season.each do |game|
+      if teams_goals.any?{teams_goals[game.team_id]} == false
+        teams_goals[game.team_id] = game.goals
+      else
+        teams_goals[game.team_id] += game.goals
+      end
+    end
+    teams_goals
+  end
+
+  def team_wins_by_season(season)
+    teams_wins = {}
+    season.each do |game|
+      if teams_wins.any?{teams_wins[game.team_id]} == false && game.result == "WIN"
+        teams_wins[game.team_id] = 1
+      elsif game.result == "WIN"
+        teams_wins[game.team_id] += 1
+      end
+    end
+    teams_wins
   end
 end
