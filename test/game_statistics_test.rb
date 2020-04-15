da8b7aacbf3d043253bd30c4adffa020145fa3a8
Jack Gould <johnhilsmangould@gmail.com>
2020-04-15 17:20:48 -0600

    cleaned up game statistics tests

--- test/game_statistics_test.rb
+++ test/game_statistics_test.rb
@@ -41,32 +41,40 @@ class GameStatisticsTest < Minitest::Test
   def test_highest_total_score
     assert_equal 6, @game_statistics.total_score(:high)
   end
-  #
+
   def test_lowest_total_score
     assert_equal 1,  @game_statistics.total_score(:low)
   end
-  #
+
   def test_it_can_calculate_percentage_of_home_wins
     assert_equal 0.63, @game_statistics.percentage_outcomes("home")
   end
-  #
+
   def test_it_can_calculate_percentage_of_visitor_game_wins
     assert_equal 0.25, @game_statistics.percentage_outcomes("away")
   end
-  #
+
   def test_it_can_calculate_percenatage_ties
     assert_equal 0.13, @game_statistics.percentage_outcomes("tie")
   end
 
+  def test_it_can_group_by_season
+    seasons = ["20122013", "20132014", "20172018", "20162017", "20152016", "20142015"]
+    assert_equal seasons, @game_statistics.group_by_season.keys
+    assert_equal 6, @game_statistics.group_by_season.length
+  end
+
   def test_count_of_games_by_season
-    assert_equal ({"20122013"=>12, "20132014"=>3, "20172018"=>3, "20162017"=>3, "20152016"=>1, "20142015"=>5}), @game_statistics.count_of_games_by_season
+    count_by_season = {"20122013"=>12, "20132014"=>3, "20172018"=>3, "20162017"=>3, "20152016"=>1, "20142015"=>5}
+    assert_equal count_by_season, @game_statistics.count_of_games_by_season
   end
 
   def test_average_number_of_goals_per_game
-    assert_equal 4.11,@game_statistics.average_goals_per_game
+    assert_equal 4.11, @game_statistics.average_goals_per_game
   end
 
   def test_average_goals_by_season
-    assert_equal ({"20122013"=>4.0, "20132014"=>4.0, "20172018"=>3.67, "20162017"=>4.33, "20152016"=>4.0, "20142015"=>4.6}), @game_statistics.average_goals_by_season
+    goals_by_season = {"20122013"=>4.0, "20132014"=>4.0, "20172018"=>3.67, "20162017"=>4.33, "20152016"=>4.0, "20142015"=>4.6}
+    assert_equal goals_by_season, @game_statistics.average_goals_by_season
   end
 end
