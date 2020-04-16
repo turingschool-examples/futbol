95aabf0ddde2b518e9b6cc6a622890296edcadc9
Ross Perry <perryr16@gmail.com>
2020-04-15 18:54:20 -0600

    iteration 3 conditions

--- lib/game_team.rb
+++ lib/game_team.rb
@@ -2,18 +2,12 @@ require_relative 'collection'
 require_relative 'game'
 require_relative 'hashable'
 require_relative 'winable'
+require_relative 'gameteamable'
 
 class GameTeam < Collection
   extend Hashable
   include Winable
-
-  def self.find_by_team(team_id)
-    all.find_all{|game| game.team_id == team_id}
-  end
-
-  def self.home_games
-    (all.find_all {|gt| gt.hoa == "home" }).count
-  end
+  extend Gameteamable
 
   def self.percentage_home_wins
     home_wins = (all.find_all {|gt| gt.hoa == "home" && gt.result == "WIN" }).count.to_f
@@ -31,11 +25,6 @@ class GameTeam < Collection
     ((ties_count / games_count)).round(2)
   end
 
-  def self.coach_record(season_id)
-    game_teams_in_season = all.find_all {|gt| gt.season_id == season_id.to_s[0..3]}
-    hash_of_hashes(game_teams_in_season, :head_coach, :wins, :games_played, :gt_win?, 1)
-  end
-
   def self.winningest_coach(season_id)
     wins_by_coach = divide_hash_values(:wins, :games_played,coach_record(season_id))
     winningest = wins_by_coach.max_by {|coach, percent| percent}
@@ -47,14 +36,10 @@ class GameTeam < Collection
     worst = wins_by_coach.min_by {|coach, percent| percent}
     worst[0]
   end
-
+#what does this do?
   def self.game_team_shots_goals_count(arr_games)
     season = arr_games.first.game_id
     self.find_by(season)
-  end
-
-  def self.get_goal_shots_by_game_team(game_teams)
-    hash_of_hashes(game_teams,:team_id,:goals,:shots,:goals,:shots)
   end
 
   def self.least_accurate_team(season)
@@ -69,28 +54,6 @@ class GameTeam < Collection
     return seasonal_hash.max_by{|key,value| value[:average]}[0]
   end
 
-  def self.gets_team_shots_goals_count(season)
-    season_games = Game.grouped_by_season(season)
-    matches = []
-    season_games.each {|game|matches.concat(GameTeam.find_by(game.game_id))}
-    stats_by_team = get_goal_shots_by_game_team(matches)
-  end
-
-  def self.games_by_team_name(season_id)
-    game_id_first = season_id.to_s[0..3]
-    all_games_by_id = all.find_all {|game| game.season_id == game_id_first}
-    games_by_id = all_games_by_id.group_by { |game| game.team_id }
-  end
-
-  def self.tackles_by_team(season_id)
-    tackles_by_team = {}
-    games_by_team_name(season_id).each do |key, value|
-      total_tackles = value.sum { |value| value.tackles}
-      tackles_by_team[key] = total_tackles
-    end
-      tackles_by_team
-  end
-
   def self.most_tackles(season_id)
     most_tackles = tackles_by_team(season_id).max_by { |key, value| value}
     most_tackles.first
@@ -99,13 +62,6 @@ class GameTeam < Collection
   def self.fewest_tackles(season_id)
     fewest_tackles = tackles_by_team(season_id).min_by { |key, value| value}
     fewest_tackles.first
-  end
-
-  def self.total_goals_per_team
-    grouped_team = all.group_by{|game| game.team_id}
-    grouped_team.keys.each_with_object({}) do |team_id , hash|
-      hash[team_id] = (grouped_team[team_id].sum(&:goals) / grouped_team[team_id].length.to_f).round(2)
-    end
   end
 
   def self.best_offense
@@ -128,18 +84,6 @@ class GameTeam < Collection
     total_game_teams_per_team_id = find_by_team(team_id)
     game_team_with_min = total_game_teams_per_team_id.min_by{|game_team| game_team.goals}
     return game_team_with_min.goals
-  end
-
-  def self.game_teams_with_opponent(team_id)
-    game_ids = all.map {|gt| gt.game_id if gt.team_id == team_id }.compact
-    game_teams_with_these_ids = all.find_all do |gt|
-      game_ids.include?(gt.game_id) && gt.team_id != team_id
-    end
-    game_teams_with_these_ids
-  end
-
-  def self.opponents_records(team_id)
-    hash_of_hashes(game_teams_with_opponent(team_id), :team_id, :wins, :games_played, :gt_win?, 1)
   end
 
   def self.favorite_opponent_id(team_id)
---
+++ lib/gameteamable.rb
@@ -0,0 +1,62 @@
+module Gameteamable
+
+#used 3 times
+  def find_by_team(team_id)
+    all.find_all{|game| game.team_id == team_id}
+  end
+
+  def home_games
+    (all.find_all {|gt| gt.hoa == "home" }).count
+  end
+
+  def coach_record(season_id)
+    game_teams_in_season = all.find_all {|gt| gt.season_id == season_id.to_s[0..3]}
+    hash_of_hashes(game_teams_in_season, :head_coach, :wins, :games_played, :gt_win?, 1)
+  end
+
+  def get_goal_shots_by_game_team(game_teams)
+    hash_of_hashes(game_teams,:team_id,:goals,:shots,:goals,:shots)
+  end
+
+  def gets_team_shots_goals_count(season)
+    season_games = Game.grouped_by_season(season)
+    matches = []
+    season_games.each {|game|matches.concat(GameTeam.find_by(game.game_id))}
+    stats_by_team = get_goal_shots_by_game_team(matches)
+  end
+
+  def games_by_team_name(season_id)
+    game_id_first = season_id.to_s[0..3]
+    all_games_by_id = all.find_all {|game| game.season_id == game_id_first}
+    games_by_id = all_games_by_id.group_by { |game| game.team_id }
+  end
+
+  def tackles_by_team(season_id)
+    tackles_by_team = {}
+    games_by_team_name(season_id).each do |key, value|
+      total_tackles = value.sum { |value| value.tackles}
+      tackles_by_team[key] = total_tackles
+    end
+      tackles_by_team
+  end
+
+  def total_goals_per_team
+    grouped_team = all.group_by{|game| game.team_id}
+    grouped_team.keys.each_with_object({}) do |team_id , hash|
+      hash[team_id] = (grouped_team[team_id].sum(&:goals) / grouped_team[team_id].length.to_f).round(2)
+    end
+  end
+
+  def game_teams_with_opponent(team_id)
+    game_ids = all.map {|gt| gt.game_id if gt.team_id == team_id }.compact
+    game_teams_with_these_ids = all.find_all do |gt|
+      game_ids.include?(gt.game_id) && gt.team_id != team_id
+    end
+    game_teams_with_these_ids
+  end
+
+  def opponents_records(team_id)
+    hash_of_hashes(game_teams_with_opponent(team_id), :team_id, :wins, :games_played, :gt_win?, 1)
+  end
+
+end
