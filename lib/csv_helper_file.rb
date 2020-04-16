95482f8f03db529c9cddf545acbbe1c50e436846
Jane Greene <janegreenecasa@gmail.com>
2020-04-15 18:02:20 -0600

    removed stray comments of pry

--- lib/csv_helper_file.rb
+++ lib/csv_helper_file.rb
@@ -8,7 +8,6 @@ def self.generate_team_array(file_path)
     csv = CSV.read("#{file_path}", headers: true, header_converters: :symbol)
 
     teams_collection = csv.map do |row|
-        # require"pry";binding.pry
         team = Team.new(row)
     end
     teams_collection
@@ -17,9 +16,7 @@ def self.generate_team_array(file_path)
   def self.generate_game_array(file_path)
       csv = CSV.read("#{file_path}", headers: true, header_converters: :symbol)
       game_collection = csv.map do |row|
-        #require 'pry'; binding.pry
         game = Game.new(row)
-         # require 'pry'; binding.pry
       end
       game_collection
   end
