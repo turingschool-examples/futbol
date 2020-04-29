519cadb468c62437e548d135ba6c958c54dd4d99
Jose Lopez <joselopez11394@gmail.com>
2020-04-29 17:14:07 -0400

    made test for wins method

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -8,4 +8,14 @@ describe StatTracker do
   it "should exist" do
     expect(stat).to be_kind_of(StatTracker)
   end
+
+  it "should return 0 if left alone" do
+    expect(stat.wins).to eq(0)
+  end
+
+  it "should return the number assigned to it" do
+    stat.wins = 5
+
+    expect(stat.wins).to eq(5)
+  end
 end
