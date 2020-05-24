2eed5d881cc8cb1f96424ab7d64fdba17a8397af
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-24 11:44:02 -0600

    Change test to assert instance of and test attributes

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -9,17 +9,20 @@ class StatTrackerTest < MiniTest::Test
     team_path = './fixtures/teams_fixture.csv'
     game_teams_path = './fixtures/game_teams_fixture.csv'
 
-    locations = {
+    file_path_locations = {
       games: game_path,
       teams: team_path,
       game_teams: game_teams_path
     }
 
-    @stat_tracker = StatTracker.from_csv(locations)
+    @stat_tracker = StatTracker.from_csv(file_path_locations)
   end
 
-  def test_it_is_a_class
-    assert_equal Class, @stat_tracker.class
+  def test_it_exists_with_attributes
+    assert_instance_of StatTracker, @stat_tracker
+    assert_equal './fixtures/games_fixture.csv', @stat_tracker.games
+    assert_equal './fixtures/teams_fixture.csv', @stat_tracker.teams
+    assert_equal './fixtures/game_teams_fixture.csv', @stat_tracker.game_teams
   end
 
 end
