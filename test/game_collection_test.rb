4403a44450327008dd6f85f982f0fe4008e6108a
Norma Lopez <norma_l304@yahoo.com>
2020-05-25 16:05:48 -0600

    Add game_collection class with passing tests

--- test/game_collection_test.rb
+++ test/game_collection_test.rb
@@ -8,6 +8,14 @@ class GameCollectionTest < Minitest::Test
   end
 
   def test_it_exists
-    assert_instance_of GameCollection, @
+    assert_instance_of GameCollection, @games_collection
+  end
+
+  def test_it_has_attributes
+    assert_instance_of Array, @games_collection.games
+  end
+
+  def test_it_can_create_games_objects
+    assert_instance_of Game, @games_collection.games.first
   end
 end
