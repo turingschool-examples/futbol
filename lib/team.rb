716ea3bca00593f44ab40c7e59b1e6b7fc3f63c7
Rion Hutsell <st.gish@gmail.com>
2020-05-25 15:54:22 -0600

    Add attr_readers to Team class

--- lib/team.rb
+++ lib/team.rb
@@ -1,10 +1,16 @@
 require 'csv'
 
 class Team
+  attr_reader :team_id,
+              :franchise_id,
+              :team_name,
+              :abbreviation,
+              :link
+
   def initialize(team_params)
-    @team_id = team_params[:team_id]
-    @franchise_id = team_params[:franchise_id]
-    @team_name = team_params[:team_name]
+    @team_id = team_params[:team_id].to_i
+    @franchise_id = team_params[:franchiseId].to_i
+    @team_name = team_params[:teamName]
     @abbreviation = team_params[:abbreviation]
     @link = team_params[:link]
   end
--- test/team_test.rb
+++ test/team_test.rb
@@ -5,8 +5,8 @@ require './lib/team'
 class TeamTest < Minitest::Test
   def setup
     team_params = {team_id: "1",
-                  franchise_id: "23",
-                    team_name: "Atlanta United",
+                  franchiseId: "23",
+                    teamName: "Atlanta United",
                     abbreviation: "ATL",
                     link: "/api/v1/teams/1"}
     @team = Team.new(team_params)
@@ -14,5 +14,13 @@ class TeamTest < Minitest::Test
 
   def test_it_exists
     assert_instance_of Team, @team
+  end
+
+  def test_it_has_attributes
+    assert_equal 1, @team.team_id
+    assert_equal 23, @team.franchise_id
+    assert_equal "Atlanta United", @team.team_name
+    assert_equal "ATL", @team.abbreviation
+    assert_equal "/api/v1/teams/1", @team.link
   end
 end
