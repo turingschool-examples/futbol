ebca3efb21cfbf535942558de3f763daf5201578
Sienna Kopf <princess.kopf@gmail.com>
2020-05-24 22:11:35 -0600

    Add stat tracker has attributes

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,12 +1,12 @@
 class StatTracker
-  # attr_reader :games,
-  #             :teams,
-  #             :game_teams
+  attr_reader :games,
+              :teams,
+              :game_teams
 
   def initialize(data_files)
-    # @games = data_files[:games]
-    # @teams = data_files[:teams]
-    # @game_teams = data_files[:game_teams]
+    @games = data_files[:games]
+    @teams = data_files[:teams]
+    @game_teams = data_files[:game_teams]
   end
 
   def self.from_csv(data_files)
