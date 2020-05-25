bed351e351ee064a7b4835af7e37770e2a47cfaf
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-05-25 14:46:54 -0600

    Define game class and tests

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,9 @@
 class Game
-
+  attr_reader :game_id, :season, :away_team_id, :home_team_id
+  def initialize(game_params)
+    @game_id = game_params[:game_id].to_i
+    @season = game_params[:season].to_i
+    @away_team_id = game_params[:away_team_id].to_i
+    @home_team_id = game_params[:home_team_id].to_i
+  end
 end
--- test/game_test.rb
+++ test/game_test.rb
@@ -4,10 +4,21 @@ require './lib/game'
 
 class GameTest < Minitest::Test
   def setup
+    game_params = {game_id: "2012030221",
+                    season: "20122013",
+                    away_team_id: "3",
+                    home_team_id: "6"}
     @game = Game.new(game_params)
   end
 
   def test_it_exists
     assert_instance_of Game, @game
+  end
+
+  def test_it_has_attributes
+    assert_equal 2012030221, @game.game_id
+    assert_equal 20122013, @game.season
+    assert_equal 3, @game.away_team_id
+    assert_equal 6, @game.home_team_id
   end
 end
