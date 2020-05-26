2fb77c4a669353235c80927f0afef8cea7f84303
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-05-26 19:32:52 -0400

    Work on game collection get_all method and attributes

--- lib/game_collection.rb
+++ lib/game_collection.rb
@@ -5,4 +5,20 @@ class GameCollection
     @all = game_collection
   end
 
+  def self.get_all(game_file)
+    all_games = []
+    CSV.read(game_file, headers: true).each do |game|
+      game_hash = {:id,
+                  :season,
+                  :type,
+                  :date_time,
+                  :away_team_id,
+                  :home_team_id,
+                  :away_goals,
+                  :home_goals,
+                  :venue}
+      all_games << Game.new(merchant_hash)
+    end
+    all_games
+  end
 end
--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -28,8 +28,23 @@ class StatTrackerTest < MiniTest::Test
   end
 
   class GameStatisticsTest < StatTrackerTest
-    def test_it_can_calculate_highest_total_score
+    def setup
+      game_path = './data/games_fixture.csv'
+      team_path = './data/teams_fixture.csv'
+      game_teams_path = './data/game_teams_fixture.csv'
+
+      @locations = {
+        games: game_path,
+        teams: team_path,
+        game_teams: game_teams_path
+      }
+
+      @stat_tracker = StatTracker.from_csv(@locations)
+    end
+
+    def test_it_can_return_highest_total_score
       skip
+      
     end
   end
 end
