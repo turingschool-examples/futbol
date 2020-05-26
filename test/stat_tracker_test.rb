967827eb12d385aaf18a392d11fdf0759a5e2e97
twarbelow <taija.warbelow@google.com>
2020-05-26 16:12:04 -0700

    scaffold statistic test areas

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -3,27 +3,45 @@ require 'minitest/pride'
 require './lib/stat_tracker'
 
 class StatTrackerTest < MiniTest::Test
-  def setup
-    game_path = './data/games.csv'
-    team_path = './data/teams.csv'
-    game_teams_path = './data/game_teams.csv'
+  class BasicTest < StatTrackerTest
+    def setup
+      game_path = './data/games.csv'
+      team_path = './data/teams.csv'
+      game_teams_path = './data/game_teams.csv'
 
-    @locations = {
-      games: game_path,
-      teams: team_path,
-      game_teams: game_teams_path
-    }
+      @locations = {
+        games: game_path,
+        teams: team_path,
+        game_teams: game_teams_path
+      }
 
-    @stat_tracker = StatTracker.from_csv(@locations)
+      @stat_tracker = StatTracker.from_csv(@locations)
+    end
+
+    def test_it_exists
+      assert_instance_of StatTracker, @stat_tracker
+    end
+
+    def test_it_has_attributes
+      assert_equal './data/games.csv', @stat_tracker.games
+      assert_equal './data/teams.csv', @stat_tracker.teams
+      assert_equal './data/game_teams.csv', @stat_tracker.game_teams
+    end
   end
 
-  def test_it_exists
-    assert_instance_of StatTracker, @stat_tracker
+  class TeamStatisticsTest < StatTrackerTest
+
   end
 
-  def test_it_has_attributes
-    assert_equal './data/games.csv', @stat_tracker.games
-    assert_equal './data/teams.csv', @stat_tracker.teams
-    assert_equal './data/game_teams.csv', @stat_tracker.game_teams
+  class SeasonStatisticsTest < StatTrackerTest
+
   end
+
+  class LeagueStatisticsTest < StatTrackerTest
+  end
+
+  class GameStatisticsTest < StatTrackerTest
+
+  end
+
 end
