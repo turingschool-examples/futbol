c2254e0fd665031f5c732839cc4ea6e81cae7784
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-05-26 19:46:45 -0400

    Work on get_all method in GameCollection class

--- lib/game_collection.rb
+++ lib/game_collection.rb
@@ -5,19 +5,20 @@ class GameCollection
     @all = game_collection
   end
 
-  def self.get_all(game_file)
+  def get_all
     all_games = []
     CSV.read(game_file, headers: true).each do |game|
-      game_hash = {:id,
-                  :season,
-                  :type,
-                  :date_time,
-                  :away_team_id,
-                  :home_team_id,
-                  :away_goals,
-                  :home_goals,
-                  :venue}
-      all_games << Game.new(merchant_hash)
+      game_hash = {id: game["game_id"],
+        season: game["season"],
+        type: game["type"],
+        date_time: game["date_time"],
+        away_team_id: game["away_team_id"],
+        home_team_id: game["home_team_id"],
+        away_goals: game["away_goals"],
+        home_goals: game["home_goals"],
+        venue: game["venue"]
+      }
+      all_games << Game.new(game_hash)
     end
     all_games
   end
