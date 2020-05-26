31fb198d88d62cc79dab6802e9adf322bb25a0a3
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-05-26 12:53:15 -0600

    fetch data from the csv file and save it on a class variable.

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,9 +1,17 @@
 require "csv"
+require "./lib/game"
 
 class StatTracker
-
+  @@games =[]
   def self.from_csv(location)
-    self.new
+    games_data = CSV.read(location[:games], headers: true)
+    @@games = games_data.map do |row|
+       Game.new(row)
+     end
+    StatTracker.new
   end
 
+  def games
+    @@games
+  end
 end
