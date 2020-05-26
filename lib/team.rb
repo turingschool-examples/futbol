031556dbdcec7c2aca3867f24d38fcceda1168cd
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-05-26 13:19:58 -0400

    Add team test and class

--- lib/team.rb
+++ lib/team.rb
@@ -1,22 +1,18 @@
 class Team
 
-  # CSV.read(./data/teams.csv)
   attr_reader :id,
-              :franchiseid,
-              :teamname,
+              :franchise_id,
+              :name,
               :abbreviation,
               :stadium,
               :link
 
   def initialize(info)
-    @id           = info["team_id"]
-    @franchise_id = info["franchiseid"]
-    @name         = info["teamname"]
-    @abbreviation = info["abbreviation"]
-    @stadium      = info["stadium"]
-    @link         = info["link"]
+    @id           = info[:team_id]
+    @franchise_id = info[:franchiseid]
+    @name         = info[:teamname]
+    @abbreviation = info[:abbreviation]
+    @stadium      = info[:stadium]
+    @link         = info[:link]
   end
-
-  @@all_teams= []
-
 end
