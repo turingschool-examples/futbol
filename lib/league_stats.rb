c841b33efbbe4ea21b0329c743c34ee4f173dca7
Cam C <ckc.cameron@gmail.com>
2020-05-27 08:53:21 -0600

    Fixes setup for league_stats class and passes setup tests

--- lib/league_stats.rb
+++ lib/league_stats.rb
@@ -1,8 +1,14 @@
+require_relative './game'
+require_relative './team'
+require_relative './game_team'
+
 class LeagueStats
-  attr_reader :teams_collection,
+  attr_reader :games_collection,
+              :teams_collection,
               :game_teams_collection
 
   def initialize(file_path)
+    @games_collection = file_path[:games_collection]
     @teams_collection = file_path[:teams_collection]
     @game_teams_collection = file_path[:game_teams_collection]
   end
--- test/league_stats_test.rb
+++ test/league_stats_test.rb
@@ -1,18 +1,26 @@
 require './test/helper_test'
 require './lib/stat_tracker'
+require './lib/game'
+require './lib/game_collection'
 require './lib/team'
+require './lib/team_collection'
 require './lib/game_team'
+require './lib/game_team_collection'
 require './lib/league_stats'
 
 class LeagueStatsTest < Minitest::Test
   def setup
-    @locations = {
-      teams_collection: './data/teams.csv',
-      game_teams_collection: './test/fixtures/game_teams_truncated.csv'
+    games_collection = GameCollection.new("./test/fixtures/games_truncated.csv")
+    teams_collection = TeamCollection.new("./data/teams.csv")
+    game_teams_collection = GameTeamCollection.new("./test/fixtures/game_teams_truncated.csv")
+
+    locations = {
+      games_collection: games_collection,
+      teams_collection: teams_collection,
+      game_teams_collection: game_teams_collection
       }
 
-    @stat_tracker = StatTracker.from_csv(@locations)
-    @league_stats = LeagueStats.new(@locations)
+    @league_stats = LeagueStats.new(locations)
   end
 
   def test_it_exists
@@ -20,6 +28,7 @@ class LeagueStatsTest < Minitest::Test
   end
 
   def test_it_has_attributes
+    assert_instance_of GameCollection, @league_stats.games_collection
     assert_instance_of TeamCollection, @league_stats.teams_collection
     assert_instance_of GameTeamCollection, @league_stats.game_teams_collection
   end
