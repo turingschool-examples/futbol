e448084acf4e04b6a7176b307f05c2175fbdb3ee
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-05-26 21:13:00 -0600

    added Teams method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,18 +1,37 @@
 
 require "csv"
 require "./lib/game"
+require "./lib/team"
+require "./lib/game_team"
 
 class StatTracker
-  @@games =[]
+  @@games = []
+  @@teams = []
+  @@game_teams = []
   def self.from_csv(location)
-    games_data = CSV.read(location[:games], headers: true)
+    games_data = CSV.read(location[:games], headers: true, header_converters: :symbol)
     @@games = games_data.map do |row|
-       Game.new(row)
+      Game.new(row)
      end
+
+    teams_data = CSV.read(location[:teams], headers: true, header_converters: :symbol)
+    @@teams = teams_data.map do |row|
+      Team.new(row)
+    end
+    #
+    # game_teams_data = CSV.read(location[:game_teams], headers: true, header_converters: :symbol)
+    # @@games_team = games_team_data.map do |row|
+    #   GameTeam.new(row)
+    # end
+
     StatTracker.new
   end
 
   def games
     @@games
+  end
+
+  def teams
+    @@teams
   end
 end
