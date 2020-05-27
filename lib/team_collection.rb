136f85143df35b85ab9e1d6077531678abf615d2
Gaby Mendez <gmendez90@gmail.com>
2020-05-27 02:11:04 -0400

    Rearrange a couple lines in teamCollection

--- lib/team_collection.rb
+++ lib/team_collection.rb
@@ -3,11 +3,11 @@ require "csv"
 class TeamCollection
   def initialize(teams_csv)
     @teams_csv = teams_csv
-    @rows = CSV.read(@teams_csv, headers: true, header_converters: :symbol)
   end
 
   def all
-    @rows.reduce([]) do |teams, row|
+    rows = CSV.read(@teams_csv, headers: true, header_converters: :symbol)
+    rows.reduce([]) do |teams, row|
       teams << Team.new(row)
       teams
     end
