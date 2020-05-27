f19c3e3f923f3611c790643922818de564981ab3
Alex Desjardins <ardesjardins91@gmail.com>
2020-05-26 18:13:53 -0600

    Functional game class and test.

--- test/game_test.rb
+++ test/game_test.rb
@@ -1,5 +1,6 @@
 require './test/test_helper'
 require './lib/game'
+require 'csv'
 
 class GameTest < Minitest::Test
   def setup
@@ -16,6 +17,9 @@ class GameTest < Minitest::Test
     venue_link: '/api/v1/venues/null'}
 
     @game = Game.new(expected)
+    @game_data = CSV.read('./test/fixtures/games_fixture.csv', headers: true, header_converters: :symbol)
+
+    @game_1 = Game.new(@game_data[16])
   end
 
   def test_it_exists
@@ -36,7 +40,16 @@ class GameTest < Minitest::Test
   end
 
   def test_game_reads_from_CSV
-    
+    assert_equal 2013020674, @game_1.game_id
+    assert_equal "20132014", @game_1.season
+    assert_equal "Regular Season", @game_1.type
+    assert_equal "1/11/14", @game_1.date_time
+    assert_equal "19", @game_1.away_team_id
+    assert_equal "23", @game_1.home_team_id
+    assert_equal 1, @game_1.away_goals
+    assert_equal 2, @game_1.home_goals
+    assert_equal "Saputo Stadium", @game_1.venue
+    assert_equal '/api/v1/venues/null', @game_1.venue_link
   end
 
 end
