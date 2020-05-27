1b5c506935cc91694adb82894877752de598f67e
Eric Hale <eric_hale@comcast.net>
2020-05-27 15:25:08 -0600

    Add locations to stat_tracker

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,7 +1,4 @@
 class StatTracker
-<<<<<<< HEAD
-  
-=======
   attr_reader :games_file_path, :teams_file_path, :game_teams_file_path
 
   def initialize(argument)
@@ -14,7 +11,6 @@ class StatTracker
     games_file_path = locations[:games]
     teams_file_path = locations[:teams]
     game_teams_file_path = locations[:game_teams]
-    StatTracker.new
+    StatTracker.new(locations)
   end
->>>>>>> 474ae8a71fb19fad81eacf76d9241f1e9d52f3f3
 end
