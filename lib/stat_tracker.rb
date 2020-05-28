b52c97322cd8b2b5cd5c1b9f7b18bbc6fdfc5a07
Sienna Kopf <princess.kopf@gmail.com>
2020-05-28 13:18:33 -0600

    Modify winningest coach method to iterate through a game_team_collection

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -31,14 +31,6 @@ class StatTracker
     end
 
     total.home_goals + total.away_goals
-
-    # top_score = 0
-    # CSV.foreach(@games, headers: true, header_converters: :symbol) do |game|
-    #   if game[:away_goals].to_i + game[:home_goals].to_i > top_score
-    #     top_score = game[:away_goals].to_i + game[:home_goals].to_i
-    #   end
-    # end
-    # top_score
   end
 
   def lowest_total_score
@@ -47,25 +39,18 @@ class StatTracker
     end
 
     total.home_goals + total.away_goals
-
-    # lowest_score = 1_000_000_000
-    # CSV.foreach(@games, headers: true, header_converters: :symbol) do |game|
-    #   if game[:away_goals].to_i + game[:home_goals].to_i < lowest_score
-    #     lowest_score = game[:away_goals].to_i + game[:home_goals].to_i
-    #   end
-    # end
-    # lowest_score
   end
 
   def winningest_coach(season_id)
     coaches_stats = Hash.new(0)
-      CSV.foreach(@game_teams, headers: true, header_converters: :symbol) do |game_team|
-        if season_id.to_s.include?(game_team[:game_id].split(//).join[0..3])
-          if game_team[:result] == "LOSS"
-            coaches_stats[game_team[:head_coach]] -= 1
-          elsif game_team[:result] == "WIN"
-            coaches_stats[game_team[:head_coach]] += 1
-          elsif game_team[:result] == "TIE"
+
+      game_team_collection.all.each do |game_team|
+        if season_id.to_s.include?(game_team.game_id.to_s.split(//).join[0..3])
+          if game_team.result == "LOSS"
+            coaches_stats[game_team.head_coach] -= 1
+          elsif game_team.result == "WIN"
+            coaches_stats[game_team.head_coach] += 1
+          elsif game_team.result == "TIE"
             next
           end
       end
