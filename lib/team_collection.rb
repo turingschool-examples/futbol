fa06393b50ba83de72f032d048d14cbfa8e31e3f
Kiera Allen <kieraaallen@gmail.com>
2020-05-28 07:28:15 -0600

    Test and add team_info method

--- lib/team_collection.rb
+++ lib/team_collection.rb
@@ -10,9 +10,21 @@ class TeamCollection
 
   def create_teams(csv_file_path)
     csv = CSV.read('./data/teams.csv', headers: true, header_converters: :symbol)
-
     csv.map do |row|
       Team.new(row)
     end
+  end
+
+  def team_info(team_id)
+     found_team = @teams.find do |team|
+       team.team_id == team_id
+     end
+     {
+       :team_id => found_team.team_id,
+       :franchise_id => found_team.franchise_id,
+       :team_name => found_team.team_name,
+       :abbreviation => found_team.abbreviation,
+       :link => found_team.link
+     }
   end
 end
--- test/team_collection_test.rb
+++ test/team_collection_test.rb
@@ -4,7 +4,7 @@ class TeamCollectionTest < Minitest::Test
 
   def setup
     @team_collection = TeamCollection.new('./data/teams.csv')
-    @team = @team_collection.teams[8]
+    @team = @team_collection.teams.first
   end
 
   def test_it_exists
@@ -16,12 +16,21 @@ class TeamCollectionTest < Minitest::Test
     assert_equal 32, @team_collection.teams.length
   end
 
-  def test_it_can_create_teams_from_csv
-    assert_equal "8", @team.team_id
-    assert_equal "1", @team.franchise_id
-    assert_equal "New York Red Bulls", @team.team_name
-    assert_equal "NY", @team.abbreviation
-    assert_equal "Red Bull Arena", @team.stadium
-    assert_equal "/api/v1/teams/8", @team.link
+  def test_it_has_team_info
+    expected = {:team_id => "1",
+                :franchise_id => "23",
+                :team_name => "Atlanta United",
+                :abbreviation => "ATL",
+                :link => "/api/v1/teams/1"}
+    assert_equal expected, @team_collection.team_info("1")
   end
+
+  # def test_it_can_create_teams_from_csv
+  #   assert_equal "8", @team.team_id
+  #   assert_equal "1", @team.franchise_id
+  #   assert_equal "New York Red Bulls", @team.team_name
+  #   assert_equal "NY", @team.abbreviation
+  #   assert_equal "Red Bull Arena", @team.stadium
+  #   assert_equal "/api/v1/teams/8", @team.link
+  # end
 end
