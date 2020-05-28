0a27ca92ffc62f998dd0e11b02857d7c6c29a73f
Eric Hale <eric_hale@comcast.net>
2020-05-28 11:18:37 -0600

    Add stat tracker functionality

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,16 +1,16 @@
 class StatTracker
-  attr_reader :games_file_path, :teams_file_path, :game_teams_file_path
-
-  def initialize(argument)
-    @games_file_path
-    @teams_file_path
-    @game_teams_file_path
-  end
+  attr_reader :games_path, :teams_path, :game_teams_path
 
   def self.from_csv(locations)
-    games_file_path = locations[:games]
-    teams_file_path = locations[:teams]
-    game_teams_file_path = locations[:game_teams]
-    StatTracker.new(locations)
+    @games_file_path = locations[:games]
+    @teams_file_path = locations[:teams]
+    @game_teams_file_path = locations[:game_teams]
+    StatTracker.new(@games_file_path, @teams_file_path, @game_teams_file_path)
+  end
+
+  def initialize(games_path, teams_path, game_teams_path)
+    @games_path = games_path
+    @teams_path = teams_path
+    @game_teams_path = game_teams_path
   end
 end
