5b9e5375f3d0ef3377de0c53986d167029e4c6fd
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-05-27 23:00:26 -0600

    added to_hash method for use with the team_info method in stat_tracker

--- lib/team.rb
+++ lib/team.rb
@@ -13,4 +13,9 @@ attr_reader :team_id, :franchise_id, :team_name, :abbreviation,
     @stadium = data[:stadium]
     @link = data[:link]
   end
+
+    def to_hash
+      {team_id: @team_id, franchise_id: @franchise_id, team_name: @team_name, abbreviation: @abbreviation, link: @link}
+    end
+    
 end
