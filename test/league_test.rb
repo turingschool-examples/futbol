346e99b89445422d55a186c8284a248372b6d686
Amy Moller <amy.moller@yahoo.com>
2020-05-27 21:14:12 -0600

    Add first tests

---
+++ test/league_test.rb
@@ -0,0 +1,36 @@
+require './test/test_helper'
+require './lib/stat_tracker'
+require './lib/league'
+
+class LeagueTest < Minitest::Test
+  def setup
+
+
+
+
+
+
+
+
+
+  end
+
+  def test_it_exists
+    skip 
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
