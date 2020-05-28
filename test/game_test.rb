6c1fead84dbe17036e487bdd9fab9cb605471c75
Alex Desjardins <ardesjardins91@gmail.com>
2020-05-28 12:20:56 -0600

    Test loadable module functions

--- test/game_test.rb
+++ test/game_test.rb
@@ -17,9 +17,9 @@ class GameTest < Minitest::Test
     venue_link: '/api/v1/venues/null'}
 
     @game = Game.new(expected)
-    @game_data = CSV.read('./test/fixtures/games_fixture.csv', headers: true, header_converters: :symbol)
 
-    @game_1 = Game.new(@game_data[16])
+    Game.from_csv("./test/fixtures/games_fixture.csv")
+    @game_1 = Game.accumulator[5]
   end
 
   def test_it_exists
@@ -40,15 +40,15 @@ class GameTest < Minitest::Test
   end
 
   def test_game_reads_from_CSV
-    assert_equal 2013020674, @game_1.game_id
-    assert_equal "20132014", @game_1.season
-    assert_equal "Regular Season", @game_1.type
-    assert_equal "1/11/14", @game_1.date_time
-    assert_equal "19", @game_1.away_team_id
-    assert_equal "23", @game_1.home_team_id
-    assert_equal 1, @game_1.away_goals
+    assert_equal 2017030221, @game_1.game_id
+    assert_equal "20172018", @game_1.season
+    assert_equal "Postseason", @game_1.type
+    assert_equal "4/26/18", @game_1.date_time
+    assert_equal "5", @game_1.away_team_id
+    assert_equal "15", @game_1.home_team_id
+    assert_equal 3, @game_1.away_goals
     assert_equal 2, @game_1.home_goals
-    assert_equal "Saputo Stadium", @game_1.venue
+    assert_equal "Providence Park", @game_1.venue
     assert_equal '/api/v1/venues/null', @game_1.venue_link
   end
 
