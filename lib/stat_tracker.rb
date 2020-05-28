3380fd95c229f463654ccacfec453f28a0b16f06
Sienna Kopf <princess.kopf@gmail.com>
2020-05-28 13:20:13 -0600

    Modify worst coach test to reference a game team collection

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -63,13 +63,14 @@ class StatTracker
 
   def worst_coach(season_id)
     coaches_stats = Hash.new(0)
-      CSV.foreach(@game_teams, headers: true, header_converters: :symbol) do |game_team|
-        if season_id.to_s.include?(game_team[:game_id].split(//).join[0..3])
-          if game_team[:result] == "LOSS"
-            coaches_stats[game_team[:head_coach]] -= 1
-          elsif game_team[:result] == "WIN"
-            coaches_stats[game_team[:head_coach]] += 1
-          elsif game_team[:result] == "TIE"
+
+      game_team_collection.all.each do |game_team|
+        if season_id.to_s.include?(game_team.game_id.to_s.split(//).join[0..3])
+          if game_team.result == "LOSS"
+            coaches_stats[game_team.head_coach] -= 1
+          elsif game_team.result == "WIN"
+            coaches_stats[game_team.head_coach] += 1
+          elsif game_team.result == "TIE"
             next
           end
       end
