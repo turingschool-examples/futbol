9ce0987395a9960f3a6e6c2de4b1c8de004c9f15
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-29 12:52:48 -0600

    Convert statistical instance variables into integers

--- lib/games.rb
+++ lib/games.rb
@@ -17,8 +17,8 @@ class Games
     @date_time = games_params[:date_time]
     @away_team_id = games_params[:away_team_id]
     @home_team_id = games_params[:home_team_id]
-    @away_goals = games_params[:away_goals]
-    @home_goals = games_params[:home_goals]
+    @away_goals = games_params[:away_goals].to_i
+    @home_goals = games_params[:home_goals].to_i
     @venue = games_params[:venue]
     @venue_link = games_params[:venue_link]
   end
