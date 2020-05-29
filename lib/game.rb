ee2a59133265a9c706aa8a1b92deddfe50cea1a0
Phillip Strom <phillip.strom@gmail.com>
2020-05-29 11:34:35 -0600

    Add total_score variable

--- lib/game.rb
+++ lib/game.rb
@@ -2,7 +2,7 @@
 class Game
   attr_reader :game_id, :home_goals, :home_team_id, :season,
               :type, :date_time, :away_goals, :away_team_id,
-              :venue, :venue_link
+              :venue, :venue_link, :total_goals
   def initialize(data)
     @game_id = data[:game_id]
     @season = data[:season]
@@ -10,9 +10,10 @@ class Game
     @date_time = data[:date_time]
     @away_team_id = data[:away_team_id]
     @home_team_id = data[:home_team_id]
-    @away_goals = data[:away_goals]
-    @home_goals = data[:home_goals]
+    @away_goals = data[:away_goals].to_i
+    @home_goals = data[:home_goals].to_i
     @venue = data[:venue]
     @venue_link = data[:venue_link]
+    @total_goals = @away_goals + @home_goals
   end
 end
