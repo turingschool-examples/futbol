85b3bdc43855e0e3d8c5f849c37d3b0ae2137c93
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-29 12:36:37 -0600

    Create games class and pass all tests

---
+++ test/games_test.rb
@@ -0,0 +1,40 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/games"
+
+class GamesTest < MiniTest::Test
+
+	def setup
+		games_params = {
+			:game_id => "2012030221",
+			:season => "20122013",
+			:type => "Postseason",
+			:date_time => "5/16/13",
+			:away_team_id => "3",
+			:home_team_id => "6",
+			:away_goals => "2",
+			:home_goals => "3",
+			:venue => "Toyota Stadium",
+			:venue_link => "/api/v1/venues/null"
+		}
+    @games = Games.new(games_params)
+	end
+
+	def test_it_exists
+		assert_instance_of Games, @games
+	end
+
+	def test_it_has_attributes
+		assert_equal "2012030221", @games.game_id
+		assert_equal "20122013", @games.season
+		assert_equal "Postseason", @games.type
+		assert_equal "5/16/13", @games.date_time
+		assert_equal "3", @games.away_team_id
+		assert_equal "6", @games.home_team_id
+		assert_equal "2", @games.away_goals
+		assert_equal "3", @games.home_goals
+		assert_equal "Toyota Stadium", @games.venue
+		assert_equal "/api/v1/venues/null", @games.venue_link
+	end
+
+end
