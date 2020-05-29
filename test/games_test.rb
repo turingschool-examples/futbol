25c2b98bb48a5ee3450044815c20a5ee4b2be059
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-29 12:56:31 -0600

    Fix tests to reflect integer

--- test/games_test.rb
+++ test/games_test.rb
@@ -31,8 +31,8 @@ class GamesTest < MiniTest::Test
 		assert_equal "5/16/13", @games.date_time
 		assert_equal "3", @games.away_team_id
 		assert_equal "6", @games.home_team_id
-		assert_equal "2", @games.away_goals
-		assert_equal "3", @games.home_goals
+		assert_equal 2, @games.away_goals
+		assert_equal 3, @games.home_goals
 		assert_equal "Toyota Stadium", @games.venue
 		assert_equal "/api/v1/venues/null", @games.venue_link
 	end
