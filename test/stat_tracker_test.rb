67b996df21469091053c5d23fe6aa56b70a38a57
Judith Pillado <judith.pillado@gmail.com>
2020-05-29 19:07:29 -0500

    Add count of games by season test

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -64,4 +64,16 @@ class StatTrackerTest < Minitest::Test
     assert_equal 0.20, @stat_tracker.percentage_ties
   end
 
+  def test_count_of_games_by_season
+    expected = {
+      "20122013"=>806,
+      "20162017"=>1317,
+      "20142015"=>1319,
+      "20152016"=>1321,
+      "20132014"=>1323,
+      "20172018"=>1355
+    }
+    assert_equal expected, @stat_tracker.count_of_games_by_season
+  end
+
 end
