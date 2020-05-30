38c8e7ccd62b3e3739d190ff79e1fa7ed7804803
Michael Chickillo <truthvendor@gmail.com>
2020-05-30 16:20:20 -0600

    Checking small typo

--- lib/team.rb
+++ lib/team.rb
@@ -2,15 +2,19 @@ require 'pry'
 
 class Team
 
-attr_reader :team_id, :franchise_id, :team_name, :abbreviation,
-            :stadium, :link
+attr_reader :team_id,
+            :franchise_id,
+            :team_name,
+            :abbreviation,
+            :stadium,
+            :link
 
   def initialize(data)
-    @team_id = data[:team_id]
+    @team_id      = data[:team_id]
     @franchise_id = data[:franchiseid]
-    @team_name = data[:teamname]
+    @team_name    = data[:teamname]
     @abbreviation = data[:abbreviation]
-    @stadium = data[:stadium]
-    @link = data[:link]
+    @stadium      = data[:stadium]
+    @link         = data[:link]
   end
 end
