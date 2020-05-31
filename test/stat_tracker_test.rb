f538b8d27e368543f7a2051c13f1fe8b2f95dee6
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-05-31 14:11:45 -0600

    Prep for merge

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -18,11 +18,16 @@ class StatTrackerTest < MiniTest::Test
     @stat_tracker = StatTracker.from_csv(file_path_locations)
   end
 
-  def test_it_exists_with_attributes
-    assert_instance_of StatTracker, @stat_tracker
-    assert_equal './data/games.csv', @stat_tracker.games
-    assert_equal './data/teams.csv', @stat_tracker.teams
-    assert_equal './data/game_teams.csv', @stat_tracker.game_teams
+  # def test_it_exists_with_attributes
+  #   assert_instance_of StatTracker, @stat_tracker
+  #   assert_equal './data/games.csv', @stat_tracker.games
+  #   assert_equal './data/teams.csv', @stat_tracker.teams
+  #   assert_equal './data/game_teams.csv', @stat_tracker.game_teams
+  # end
+
+# Team Statistics best_season
+  def test_it_can_find_best_season
+    assert_equal "6", @stat_tracker.best_season("20132014")
   end
 
 end
