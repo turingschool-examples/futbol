49ee29dca063eb5d67e55528242c135b94ab1106
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-31 13:38:38 -0600

    Add test scaffold for development template

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -9,20 +9,194 @@ class StatTrackerTest < MiniTest::Test
     team_path = './data/teams.csv'
     game_teams_path = './data/game_teams.csv'
 
-    file_path_locations = {
+    locations = {
       games: game_path,
       teams: team_path,
       game_teams: game_teams_path
     }
 
-    @stat_tracker = StatTracker.from_csv(file_path_locations)
+    @stat_tracker = StatTracker.from_csv(locations)
   end
 
-  def test_it_exists_with_attributes
+  def test_it_exists
     assert_instance_of StatTracker, @stat_tracker
-    assert_equal './data/games.csv', @stat_tracker.games
-    assert_equal './data/teams.csv', @stat_tracker.teams
-    assert_equal './data/game_teams.csv', @stat_tracker.game_teams
+  end
+
+  def test_it_gets_highest_total_score
+    skip
+    assert_equal 11, @stat_tracker.highest_total_score
+  end
+
+  def test_it_gets_lowest_total_score
+    skip
+    assert_equal 0, @stat_tracker.lowest_total_score
+  end
+
+  def test_it_gets_percentage_home_wins
+    skip
+    assert_equal 0.44, @stat_tracker.percentage_home_wins
+  end
+
+  def test_it_gets_percentage_visitor_wins
+    skip
+    assert_equal 0.36, @stat_tracker.percentage_visitor_wins
+  end
+
+  def test_it_gets_percentage_ties
+    skip
+    assert_equal 0.20, @stat_tracker.percentage_ties
+  end
+
+  def test_it_gets_count_of_games_by_season
+    skip
+    expected = {
+      "20122013"=>806,
+      "20162017"=>1317,
+      "20142015"=>1319,
+      "20152016"=>1321,
+      "20132014"=>1323,
+      "20172018"=>1355
+    }
+    assert_equal expected, @stat_tracker.count_of_games_by_season
+  end
+
+  def test_it_gets_average_goals_per_game
+    skip
+    assert_equal 4.22, @stat_tracker.average_goals_per_game
+  end
+
+  def test_it_gets_average_goals_by_season
+    skip
+    expected = {
+      "20122013"=>4.12,
+      "20162017"=>4.23,
+      "20142015"=>4.14,
+      "20152016"=>4.16,
+      "20132014"=>4.19,
+      "20172018"=>4.44
+    }
+    assert_equal expected, @stat_tracker.average_goals_by_season
+  end
+
+  def test_it_gets_count_of_teams
+    skip
+    assert_equal 32, @stat_tracker.count_of_teams
+  end
+
+  def test_it_gets_best_offense
+    skip
+    assert_equal "Reign FC", @stat_tracker.best_offense
+  end
+
+  def test_it_gets_worst_offense
+    skip
+    assert_equal "Utah Royals FC", @stat_tracker.worst_offense
+  end
+
+  def test_it_gets_highest_scoring_visitor
+    skip
+    assert_equal "FC Dallas", @stat_tracker.highest_scoring_visitor
+  end
+
+  def test_it_gets_highest_scoring_home_team
+    skip
+    assert_equal "Reign FC", @stat_tracker.highest_scoring_home_team
+  end
+
+  def test_it_gets_lowest_scoring_visitor
+    skip
+    assert_equal "San Jose Earthquakes", @stat_tracker.lowest_scoring_visitor
+  end
+
+  def test_it_gets_lowest_scoring_home_team
+    skip
+    assert_equal "Utah Royals FC", @stat_tracker.lowest_scoring_home_team
+  end
+
+  def test_it_gets_team_info
+    skip
+    expected = {
+      "team_id" => "18",
+      "franchise_id" => "34",
+      "team_name" => "Minnesota United FC",
+      "abbreviation" => "MIN",
+      "link" => "/api/v1/teams/18"
+    }
+
+    assert_equal expected, @stat_tracker.team_info("18")
+  end
+
+  def test_it_gets_best_season
+    skip
+    assert_equal "20132014", @stat_tracker.best_season("6")
+  end
+
+  def test_it_gets_worst_season
+    skip
+    assert_equal "20142015", @stat_tracker.worst_season("6")
+  end
+
+  def test_it_gets_average_win_percentage
+    skip
+    assert_equal 0.49, @stat_tracker.average_win_percentage("6")
+  end
+
+  def test_it_gets_most_goals_scored
+    skip
+    assert_equal 7, @stat_tracker.most_goals_scored("18")
+  end
+
+  def test_it_gets_fewest_goals_scored
+    skip
+    assert_equal 0, @stat_tracker.fewest_goals_scored("18")
+  end
+
+  def test_it_gets_favorite_opponent
+    skip
+    assert_equal "DC United", @stat_tracker.favorite_opponent("18")
+  end
+
+  def test_it_gets_rival
+    skip
+    expected = ["Houston Dash", "LA Galaxy"]
+    assert_includes expected, @stat_tracker.rival("18")
+  end
+
+  def test_it_gets_winningest_coach
+    skip
+    assert_equal "Claude Julien", @stat_tracker.winningest_coach("20132014")
+    assert_equal "Alain Vigneault", @stat_tracker.winningest_coach("20142015")
+  end
+
+  def test_it_gets_worst_coach
+    skip
+    assert_equal "Peter Laviolette", @stat_tracker.worst_coach("20132014")
+    expected = ["Craig MacTavish", "Ted Nolan"]
+    assert_includes expected, @stat_tracker.worst_coach("20142015")
+  end
+
+  def test_it_gets_most_accurate_team
+    skip
+    assert_equal "Real Salt Lake", @stat_tracker.most_accurate_team("20132014")
+    assert_equal "Toronto FC", @stat_tracker.most_accurate_team("20142015")
+  end
+
+  def test_it_gets_least_accurate_team
+    skip
+    assert_equal "New York City FC", @stat_tracker.least_accurate_team("20132014")
+    assert_equal "Columbus Crew SC", @stat_tracker.least_accurate_team("20142015")
+  end
+
+  def test_it_gets_most_tackles
+    skip
+    assert_equal "FC Cincinnati", @stat_tracker.most_tackles("20132014")
+    assert_equal "Seattle Sounders FC", @stat_tracker.most_tackles("20142015")
+  end
+
+  def test_it_gets_fewest_tackles
+    skip
+    assert_equal "Atlanta United", @stat_tracker.fewest_tackles("20132014")
+    assert_equal "Orlando City SC", @stat_tracker.fewest_tackles("20142015")
   end
 
 end
