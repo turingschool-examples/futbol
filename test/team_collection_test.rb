5442fd48feab9ed184ffc4cb2aee7a10dbd98c6e
Kiera Allen <kieraaallen@gmail.com>
2020-05-31 11:08:52 -0600

    Test is can return total number of teams

--- test/team_collection_test.rb
+++ test/team_collection_test.rb
@@ -16,13 +16,16 @@ class TeamCollectionTest < Minitest::Test
 
   def test_it_has_attributes
     assert_instance_of Array, @team_collection.teams_array
-    assert_equal 32, @team_collection.teams_array.length
   end
 
-  def test_it_can_create_games_from_csv
+  def test_it_can_create_teams_from_csv
     assert_instance_of Team, @team
     assert_equal "1", @team.team_id
     assert_equal "Atlanta United", @team.team_name
+  end
+
+  def test_it_returns_total_number_of_teams
+    assert_equal 32, @team_collection.total_number_of_teams
   end
 
 end
