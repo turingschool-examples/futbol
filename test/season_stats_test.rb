51a45d84f4d62719f602cdbc65c52f5d0942f564
Norma Lopez <norma_l304@yahoo.com>
2020-05-30 22:53:48 -0600

    Add test for game_ids_by_season

--- test/season_stats_test.rb
+++ test/season_stats_test.rb
@@ -7,6 +7,7 @@ require './lib/team_collection'
 require './lib/game_team'
 require './lib/game_team_collection'
 require './lib/season_stats'
+require 'pry'
 
 class SeasonStatsTest < Minitest::Test
   def setup
@@ -33,19 +34,17 @@ class SeasonStatsTest < Minitest::Test
     assert_instance_of GameTeamCollection, @season_stats.game_teams_collection
   end
 
-  # def test_it_can_get_games_by_coach
-  #   assert_equal 4, @season_stats.games_by_coach("Claude Noel").count
-  # end
-
   def test_it_can_get_games_by_season
     assert_equal 40, @season_stats.games_by_season("20122013").count
   end
 
-  def test_it_can_get_game_teams_by_season
-    assert_equal 40, @season_stats.game_teams_by_season("20122013").count
-  end
-
-  def test_it_can_get_winningest_coach
-    assert_equal "Claude Noel", @season_stats.winningest_coach("20122013")
+  def test_game_ids_by_season
+    assert_instance_of Array, @season_stats.game_ids_by_season("20122013")
+    assert_equal 40, @season_stats.game_ids_by_season("20122013").count
+    assert_equal 40, @season_stats.game_ids_by_season("20132014").count
+    assert_equal 40, @season_stats.game_ids_by_season("20142015").count
+    assert_equal 40, @season_stats.game_ids_by_season("20152016").count
+    assert_equal 40, @season_stats.game_ids_by_season("20162017").count
+    assert_equal 40, @season_stats.game_ids_by_season("20172018").count
   end
 end
