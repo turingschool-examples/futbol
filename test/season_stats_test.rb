98e78cc49f2f07c472a7da721dca56cb90874c5a
Norma Lopez <norma_l304@yahoo.com>
2020-05-30 22:56:26 -0600

    Add test for game_teams_by_season

--- test/season_stats_test.rb
+++ test/season_stats_test.rb
@@ -47,4 +47,14 @@ class SeasonStatsTest < Minitest::Test
     assert_equal 40, @season_stats.game_ids_by_season("20162017").count
     assert_equal 40, @season_stats.game_ids_by_season("20172018").count
   end
+
+  def test_game_teams_by_season
+    assert_instance_of Array, @season_stats.game_teams_by_season("20122013")
+    assert_equal 40, @season_stats.game_teams_by_season("20122013").count
+    assert_equal 40, @season_stats.game_teams_by_season("20132014").count
+    assert_equal 40, @season_stats.game_teams_by_season("20142015").count
+    assert_equal 40, @season_stats.game_teams_by_season("20152016").count
+    assert_equal 40, @season_stats.game_teams_by_season("20162017").count
+    assert_equal 40, @season_stats.game_teams_by_season("20172018").count
+  end
 end
