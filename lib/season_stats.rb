e043f973be1dd4cee965fb613602a1c7416e17c4
Kiera Allen <kieraaallen@gmail.com>
2020-05-31 12:43:11 -0600

    Test collections of data are attributes

--- lib/season_stats.rb
+++ lib/season_stats.rb
@@ -1,6 +1,6 @@
 class SeasonStats
   attr_reader :game_collection,
-              :game_team_collection
+              :game_team_collection,
               :team_collection
 
   def initialize(game_collection, game_team_collection, team_collection)
--- test/season_stats_test.rb
+++ test/season_stats_test.rb
@@ -18,6 +18,12 @@ class SeasonStatsTest < Minitest::Test
     assert_instance_of SeasonStats, @season_stats
   end
 
+  def test_it_has_collections_as_attributes
+    assert_equal @team_collection, @season_stats.team_collection
+    assert_equal @game_collection, @season_stats.game_collection
+    assert_equal @game_team_collection, @season_stats.game_team_collection
+  end
+
   # def test_it_has_a_winningest_coach
   #   season_id = "20122013"
   #   assert_equal "Claude Julien", @season_stats.winningest_coach(season_id)
