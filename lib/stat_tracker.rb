5ad476d9ec70da55b444e8f03d525bb4be437cde
Sienna Kopf <princess.kopf@gmail.com>
2020-06-01 07:56:46 -0600

    Update winningest and worst coach methods to count and compare only wins

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -32,8 +32,32 @@ class StatTracker
     GameTeamCollection.new(@game_teams)
   end
 
+  def game_collection_to_use
+    game_coll = instantiate_game_collection
+  end
+
+  def instantiate_game_collection
+    @instantiate_game_collection ||= game_collection.all
+  end
+
+  def game_team_collection_to_use
+    instantiate_game_team_collection
+  end
+
+  def instantiate_game_team_collection
+    @instantiate_game_team_collection ||= game_team_collection.all
+  end
+
+  def team_collection_to_use
+    instantiate_team_collection
+  end
+
+  def instantiate_team_collection
+    @instantiate_team_collection ||= team_collection.all
+  end
+
   def highest_total_score
-    total = game_collection.all.max_by do |game|
+    total = game_collection_to_use.max_by do |game|
       game.away_goals.to_i + game.home_goals.to_i
     end
 
@@ -41,234 +65,154 @@ class StatTracker
   end
 
   def lowest_total_score
-    total = game_collection.all.min_by do |game|
+    total = game_collection_to_use.min_by do |game|
       game.away_goals.to_i + game.home_goals.to_i
     end
 
     total.home_goals.to_i + total.away_goals.to_i
   end
 
-
   def team_name_based_off_of_team_id(team_id)
-    team_collection.all.each do |team|
+    team_collection_to_use.each do |team|
       return team.team_name if team_id == team.team_id
     end
   end
 
-  # def filter_by_season(season_id)
-  #   games_grouped_by_season[season_id]
-  # end
-
-  def games_for_season(season_id)
-   game_collection.all.select do |game|
+  def games_by_season(season_id)
+   game_collection_to_use.select do |game|
      season_id == game.season
    end
-  end ###array of games in one season
-
-  def game_teams_for_season(season_id)
-    game_hash = games_for_season(season_id).group_by do |game|
-      game.game_id 
-    end
-
-    game_team_collection.all.select do |game_team|
-      game_hash.keys.include?(game_team.game_id)
-    end
   end
 
-  # def games_grouped_by_season(season_id)
-  #   game_collection.all.group_by do |game|
-  #     game.season
-  #   end
-  # end  #  @games_grouped_by_season ||=
-
-  def find_games_by_season_in_game_teams(season_id)
-    season_id_match_collection = filter_by_season(season_id).map do |game|
+  def season_games_grouped_by_team_id(season_id)
+    collection = games_by_season(season_id).group_by do |game|
       game.game_id
     end
-
-    game_team_season_collection = []
-    season_id_match_collection.each do |season_game_id|
-      game_team_collection.all.each do |game_team|
-        if game_team.game_id == season_game_id
-          game_team_season_collection << game_team
-        end
-      end
-    end
-    game_team_season_collection
   end
 
+  def game_teams_by_season(season_id)
+    game_team_collection_to_use.select do |game_team|
+      season_games_grouped_by_team_id(season_id).keys.include?(game_team.game_id)
+    end
+  end
 
   def winningest_coach(season_id)
-    coaches_wins = Hash.new(0)
-    find_games_by_season_in_game_teams(season_id).each do |game_team|
-      if game_team.result == "WIN"
-        coaches_wins[game_team.head_coach] += 1.0
-      elsif game_team.result == "LOSS"
-        coaches_wins[game_team.head_coach] += 0.0
-      elsif game_team.result == "TIE"
-        coaches_wins[game_team.head_coach] += 0.5
+    coaches_hash = game_teams_by_season(season_id).group_by do |game_team|
+      game_team.head_coach
+    end
+
+    coaches_hash.transform_values do |game_team_collection|
+      game_team_collection.keep_if do |game_team|
+        game_team.result == "WIN"
       end
     end
 
-    coaches_total_games = Hash.new(0)
-    find_games_by_season_in_game_teams(season_id).each do |game_team|
-      coaches_total_games[game_team.head_coach] += 1.0
+    coaches_hash.transform_values! do |game_team_collection|
+      game_team_collection.count
     end
 
-    ratio_of_wins_to_games = Hash.new(0)
-    coaches_total_games.each do |total_games_coach, total_games|
-      coaches_wins.each do |wins_coach, total_wins|
-        if total_games_coach == wins_coach
-          ratio_of_wins_to_games[wins_coach] = total_wins / total_games.to_f
-        end
-      end
-    end
-
-    best_coach = ratio_of_wins_to_games.max_by do |coach, ratio|
-      ratio
+    best_coach = coaches_hash.max_by do |coach, wins|
+      wins
     end
     best_coach[0]
   end
 
-  # def worst_coach(season_id)
-  #   coaches_wins = Hash.new(0)
-  #   game_team_collection.all.each do |game_team|
-  #     if season_id.include?(game_team.game_id.split(//).join[0..3])
-  #       if game_team.result == "WIN"
-  #         coaches_wins[game_team.head_coach] += 1.0
-  #       elsif game_team.result == "LOSS"
-  #         coaches_wins[game_team.head_coach] += 0.0
-  #       elsif game_team.result == "TIE"
-  #         coaches_wins[game_team.head_coach] += 0.5
-  #       end
-  #     end
-  #   end
-  #
-  #   coaches_total_games = Hash.new(0)
-  #   game_team_collection.all.each do |game_team|
-  #     if season_id.include?(game_team.game_id.split(//).join[0..3])
-  #       coaches_total_games[game_team.head_coach] += 1
-  #     end
-  #   end
-  #
-  #   ratio_of_wins_to_games = Hash.new(0)
-  #   coaches_total_games.each do |total_games_coach, total_games|
-  #     coaches_wins.each do |wins_coach, total_wins|
-  #       if total_games_coach == wins_coach
-  #         ratio_of_wins_to_games[wins_coach] = total_wins / total_games.to_f
-  #       end
-  #     end
-  #   end
-  #
-  #   worst_coach = ratio_of_wins_to_games.min_by do |coach, ratio|
-  #     ratio
-  #   end
-  #   worst_coach[0]
-  #
-  #   # coaches_stats = Hash.new(0)
-  #   #
-  #   # game_team_collection.all.each do |game_team|
-  #   #   if season_id.to_s.include?(game_team.game_id.to_s.split(//).join[0..3])
-  #   #     if game_team.result == "LOSS"
-  #   #       coaches_stats[game_team.head_coach] -= 1
-  #   #     elsif game_team.result == "WIN"
-  #   #       coaches_stats[game_team.head_coach] += 1
-  #   #     elsif game_team.result == "TIE"
-  #   #       next
-  #   #     end
-  #   #   end
-  #   # end
-  #   # worst_coach = coaches_stats.min_by do |coach, wins|
-  #   #   wins
-  #   # end
-  #   # worst_coach[0]
-  # end
-  #
-  # def most_accurate_team(season_id)
-  #   goals_for_season_by_team = Hash.new(0)
-  #   game_team_collection.all.each do |game_team|
-  #     if season_id.include?(game_team.game_id.split(//).join[0..3])
-  #       goals_for_season_by_team[game_team.team_id] += game_team.goals.to_i
-  #     end
-  #   end
-  #
-  #   shots_for_season_by_team = Hash.new(0)
-  #   game_team_collection.all.each do |game_team|
-  #     if season_id.include?(game_team.game_id.split(//).join[0..3])
-  #       shots_for_season_by_team[game_team.team_id] += game_team.shots.to_i
-  #     end
-  #   end
-  #
-  #   ratio_of_g_to_s_for_season_by_team = Hash.new(0)
-  #   goals_for_season_by_team.each do |g_team_id, goals|
-  #     shots_for_season_by_team.each do |s_team_id, shots|
-  #       if s_team_id == g_team_id
-  #         ratio_of_g_to_s_for_season_by_team[s_team_id] = goals.to_f / shots.to_f
-  #       end
-  #     end
-  #   end
-  #
-  #   best_team = ratio_of_g_to_s_for_season_by_team.max_by do |team_id, ratio|
-  #     ratio
-  #   end
-  #
-  #   team_name_based_off_of_team_id(best_team[0])
-  # end
-  #
-  # def least_accurate_team(season_id)
-  #   goals_for_season_by_team = Hash.new(0)
-  #   game_team_collection.all.each do |game_team|
-  #     if season_id.include?(game_team.game_id.split(//).join[0..3])
-  #       goals_for_season_by_team[game_team.team_id] += game_team.goals.to_i
-  #     end
-  #   end
-  #
-  #   shots_for_season_by_team = Hash.new(0)
-  #   game_team_collection.all.each do |game_team|
-  #     if season_id.include?(game_team.game_id.split(//).join[0..3])
-  #       shots_for_season_by_team[game_team.team_id] += game_team.shots.to_i
-  #     end
-  #   end
-  #
-  #   ratio_of_g_to_s_for_season_by_team = Hash.new(0)
-  #   goals_for_season_by_team.each do |g_team_id, goals|
-  #     shots_for_season_by_team.each do |s_team_id, shots|
-  #       if s_team_id == g_team_id
-  #         ratio_of_g_to_s_for_season_by_team[s_team_id] = goals.to_f / shots.to_f
-  #       end
-  #     end
-  #   end
-  #
-  #   worst_team = ratio_of_g_to_s_for_season_by_team.min_by do |team_id, ratio|
-  #     ratio
-  #   end
-  #
-  #   team_name_based_off_of_team_id(worst_team[0])
-  # end
-  #
-  # def most_tackles(season_id)
-  #   tackles_for_season_by_team = Hash.new(0)
-  #   find_games_by_season_in_game_teams(season_id).each do |game_team|
-  #     tackles_for_season_by_team[game_team.team_id] += game_team.tackles.to_i
-  #   end
-  #
-  #   best_team = tackles_for_season_by_team.max_by do |team_id, tackles|
-  #     tackles
-  #   end
-  #
-  #   team_name_based_off_of_team_id(best_team[0])
-  # end
-  #
-  # def fewest_tackles(season_id)
-  #   tackles_for_season_by_team = Hash.new(0)
-  #   find_games_by_season_in_game_teams(season_id).each do |game_team|
-  #     tackles_for_season_by_team[game_team.team_id] += game_team.tackles.to_i
-  #   end
-  #
-  #   worst_team = tackles_for_season_by_team.min_by do |team_id, tackles|
-  #     tackles
-  #   end
-  #
-  #   team_name_based_off_of_team_id(worst_team[0])
-  # end
+  def worst_coach(season_id)
+    coaches_hash = game_teams_by_season(season_id).group_by do |game_team|
+      game_team.head_coach
+    end
+
+    coaches_hash.transform_values do |game_team_collection|
+      game_team_collection.keep_if do |game_team|
+        game_team.result == "WIN"
+      end
+    end
+
+    coaches_hash.transform_values! do |game_team_collection|
+      game_team_collection.count
+    end
+
+    worst_coach = coaches_hash.min_by do |coach, wins|
+      wins
+    end
+    worst_coach[0]
+  end
+
+  def most_accurate_team(season_id)
+    goals_for_season_by_team = Hash.new(0)
+    game_teams_by_season(season_id).each do |game_team|
+      goals_for_season_by_team[game_team.team_id] += game_team.goals.to_i
+    end
+
+    shots_for_season_by_team = Hash.new(0)
+    game_teams_by_season(season_id).each do |game_team|
+      shots_for_season_by_team[game_team.team_id] += game_team.shots.to_i
+    end
+
+    ratio_of_g_to_s_for_season_by_team = Hash.new(0)
+      goals_for_season_by_team.each do |g_team_id, goals|
+        shots_for_season_by_team.each do |s_team_id, shots|
+          if s_team_id == g_team_id
+            ratio_of_g_to_s_for_season_by_team[s_team_id] = goals.to_f / shots.to_f
+          end
+        end
+      end
+
+    best_team = ratio_of_g_to_s_for_season_by_team.max_by do |team_id, ratio|
+      ratio
+    end
+
+    team_name_based_off_of_team_id(best_team[0])
+  end
+
+  def least_accurate_team(season_id)
+    goals_for_season_by_team = Hash.new(0)
+    game_teams_by_season(season_id).each do |game_team|
+      goals_for_season_by_team[game_team.team_id] += game_team.goals.to_i
+    end
+
+    shots_for_season_by_team = Hash.new(0)
+    game_teams_by_season(season_id).each do |game_team|
+      shots_for_season_by_team[game_team.team_id] += game_team.shots.to_i
+    end
+
+    ratio_of_g_to_s_for_season_by_team = Hash.new(0)
+      goals_for_season_by_team.each do |g_team_id, goals|
+        shots_for_season_by_team.each do |s_team_id, shots|
+          if s_team_id == g_team_id
+            ratio_of_g_to_s_for_season_by_team[s_team_id] = goals.to_f / shots.to_f
+          end
+        end
+      end
+
+    worst_team = ratio_of_g_to_s_for_season_by_team.min_by do |team_id, ratio|
+      ratio
+    end
+
+    team_name_based_off_of_team_id(worst_team[0])
+  end
+
+  def total_season_tackles_grouped_by_team(season_id)
+    tackles_for_season_by_team = Hash.new(0)
+    game_teams_by_season(season_id).each do |game_team|
+      tackles_for_season_by_team[game_team.team_id] += game_team.tackles.to_i
+    end
+    tackles_for_season_by_team
+  end
+
+  def most_tackles(season_id)
+    best_team = total_season_tackles_grouped_by_team(season_id).max_by do |team_id, tackles|
+      tackles
+    end
+
+    team_name_based_off_of_team_id(best_team[0])
+  end
+
+  def fewest_tackles(season_id)
+    worst_team = total_season_tackles_grouped_by_team(season_id).min_by do |team_id, tackles|
+      tackles
+    end
+
+    team_name_based_off_of_team_id(worst_team[0])
+  end
 end
--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -30,106 +30,128 @@ class StatTrackerTest < Minitest::Test
   end
 
   def test_it_has_attributes
-    skip
     assert_equal './data/fixtures/game_fixture.csv', @stat_tracker.games
     assert_equal './data/fixtures/team_fixture.csv', @stat_tracker.teams
     assert_equal './data/fixtures/game_teams_fixture.csv', @stat_tracker.game_teams
   end
 
   def test_it_can_have_game_collection
-    skip
     assert_instance_of GameCollection, @stat_tracker.game_collection
   end
 
   def test_it_can_have_team_collection
-    skip
     assert_instance_of TeamCollection, @stat_tracker.team_collection
   end
 
   def test_it_can_have_game_team_collection
-    skip
     assert_instance_of GameTeamCollection, @stat_tracker.game_team_collection
   end
 
-  def test_it_can_filer_by_season_to_season_id
-    skip
-    assert_equal 1, @stat_tracker.filter_by_season("20162017").count
-    assert_equal 2, @stat_tracker.filter_by_season("20122013").count
-    assert_equal 3, @stat_tracker.filter_by_season("20132014").count
+  def test_it_can_select_games_based_on_season
+    assert_equal 1, @stat_tracker.games_by_season("20162017").count
+    assert_equal 2, @stat_tracker.games_by_season("20122013").count
+    assert_equal 3, @stat_tracker.games_by_season("20132014").count
+    assert_equal Array, @stat_tracker.games_by_season("20162017").class
+    assert_equal Game, @stat_tracker.games_by_season("20162017").first.class
   end
 
-  def test_it_can_find_games_by_season_in_game_teams
-    skip
-    assert_equal 2, @stat_tracker.find_games_by_season_in_game_teams("20162017").count
-    assert_equal 6, @stat_tracker.find_games_by_season_in_game_teams("20142015").count
-    assert_equal 4, @stat_tracker.find_games_by_season_in_game_teams("20122013").count
+  def test_it_can_group_season_games_by_team_id
+    assert_equal 1, @stat_tracker.season_games_grouped_by_team_id("20162017").count
+    assert_equal 3, @stat_tracker.season_games_grouped_by_team_id("20132014").count
+    assert_equal Game, @stat_tracker.season_games_grouped_by_team_id("20122013").values[0][0].class
+    assert_equal "2016030235", @stat_tracker.season_games_grouped_by_team_id("20162017").keys[0]
   end
 
-  def test_it_can_group_by_head_coach
-    skip
-    assert_equal 2, @stat_tracker.group_by_head_coach("20162017").count
+  def test_it_can_select_game_teams_by_season
+    assert_equal 2, @stat_tracker.game_teams_by_season("20162017").count
+    assert_equal 6, @stat_tracker.game_teams_by_season("20132014").count
+    assert_equal GameTeam, @stat_tracker.game_teams_by_season("20162017")[0].class
   end
 
   def test_it_can_get_highest_total_score
-    skip
     assert_equal 8, @stat_tracker.highest_total_score
   end
 
   def test_it_can_get_lowest_total_score
-    skip
     assert_equal 1, @stat_tracker.lowest_total_score
   end
 
   def test_it_can_determine_the_winningest_coach
-    expected = ["Darryl Sutter", "Ralph Krueger"]
-    assert_includes expected, @stat_tracker.winningest_coach("20122013")
-    assert_equal "Alain Vigneault", @stat_tracker.winningest_coach("20142015")
+    expected1 = ["Darryl Sutter", "Ralph Krueger"]
+    assert_includes expected1, @stat_tracker.winningest_coach("20122013")
+    expected2 = ["Ken Hitchcock", "Alain Vigneault"]
+    assert_includes expected2, @stat_tracker.winningest_coach("20142015")
     assert_equal "Mike Yeo", @stat_tracker.winningest_coach("20162017")
   end
 
   def test_it_can_determine_the_worst_coach
-    skip
     expected = ["Patrick Roy", "Bruce Boudreau", "Ken Hitchcock"]
     assert_includes expected, @stat_tracker.worst_coach("20122013")
-    assert_equal "Jon Cooper", @stat_tracker.worst_coach("20142015")
+    assert_equal "Mike Yeo", @stat_tracker.worst_coach("20142015")
     assert_equal "Peter Laviolette", @stat_tracker.worst_coach("20162017")
   end
 
-  def test_it_can_determine_the_most_accurate_team
+  def test_it_can_count_up_goals_for_teams_over_a_season
     skip
+    assert_equal 4, @stat_tracker.total_season_goals_grouped_by_team("20122013")["22"]
+    assert_equal ["19", "26", "22", "30"], @stat_tracker.total_season_goals_grouped_by_team("20122013").keys
+    assert_equal [0,1,4,1], @stat_tracker.total_season_goals_grouped_by_team("20122013").values
+  end
+
+  def test_it_can_count_up_shots_for_teams_over_a_season
+    skip
+    assert_equal 9, @stat_tracker.total_season_shots_grouped_by_team("20122013")["30"]
+    assert_equal ["19", "26", "22", "30"], @stat_tracker.total_season_shots_grouped_by_team("20122013").keys
+    assert_equal [7,5,4,9], @stat_tracker.total_season_shots_grouped_by_team("20122013").values
+  end
+
+  def test_it_can_get_the_ratio_of_shots_to_goals_for_the_season
+    skip
+    assert_equal 0.2, @stat_tracker.season_ratio_goals_to_shots_grouped_by_team("20122013")["26"]
+    assert_equal 1.0, @stat_tracker.season_ratio_goals_to_shots_grouped_by_team("20122013")["22"]
+    assert_equal ["19", "26", "22", "30"], @stat_tracker.season_ratio_goals_to_shots_grouped_by_team("20122013").keys
+  end
+
+  def test_it_can_determine_the_most_accurate_team
     assert_equal "Washington Spirit FC", @stat_tracker.most_accurate_team("20122013")
     assert_equal "Houston Dynamo", @stat_tracker.most_accurate_team("20142015")
-    assert_equal "Philadelphia Union", @stat_tracker.most_accurate_team("20152016")
   end
 
   def test_it_can_determine_the_least_accurate_team
-    skip
     assert_equal "Philadelphia Union", @stat_tracker.least_accurate_team("20122013")
     assert_equal "Orlando City SC", @stat_tracker.least_accurate_team("20142015")
     assert_equal "Minnesota United FC", @stat_tracker.least_accurate_team("20162017")
   end
 
   def test_it_can_return_team_name_based_off_of_team_id
-    skip
     assert_equal "Philadelphia Union", @stat_tracker.team_name_based_off_of_team_id("19")
     assert_nil nil, @stat_tracker.team_name_based_off_of_team_id("5")
   end
 
-  def test_it_can_determine_the_team_with_the_most_tackles
-    skip
-    # assert_equal "FC Cincinnati", @stat_tracker.most_tackles("20132014")
+  def test_it_can_count_up_tackles_for_a_team_for_the_season
+    assert_equal 53, @stat_tracker.total_season_tackles_grouped_by_team("20122013")["26"]
+    assert_equal ["19", "26", "22", "30"], @stat_tracker.total_season_tackles_grouped_by_team("20122013").keys
+    assert_equal [39, 53, 15, 25], @stat_tracker.total_season_tackles_grouped_by_team("20122013").values
+  end
 
+  def test_it_can_determine_the_team_with_the_most_tackles
     assert_equal "FC Cincinnati", @stat_tracker.most_tackles("20122013")
     assert_equal "Orlando City SC", @stat_tracker.most_tackles("20142015")
     assert_equal "Philadelphia Union", @stat_tracker.most_tackles("20162017")
   end
 
   def test_it_can_determine_the_team_with_the_fewest_tackles
-    skip
-    # assert_equal "Atlanta United", @stat_tracker.fewest_tackles("20132014")
     assert_equal "Washington Spirit FC", @stat_tracker.fewest_tackles("20122013")
     assert_equal "DC United", @stat_tracker.fewest_tackles("20142015")
     assert_equal "Minnesota United FC", @stat_tracker.fewest_tackles("20162017")
   end
 
+  def test_creates_collections_once
+    collection1 = @stat_tracker.game_team_collection_to_use
+    collection2 = @stat_tracker.game_team_collection_to_use
+    assert_equal true, collection1 == collection2
+    assert_equal Array, @stat_tracker.game_collection_to_use.class
+    assert_equal Array, @stat_tracker.team_collection_to_use.class
+    assert_equal Array, @stat_tracker.game_team_collection_to_use.class
+  end
 end
