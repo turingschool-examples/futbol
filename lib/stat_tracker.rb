8f3ade6492bb988e7098e8c9a03fd4a4a8634478
twarbelow <taija.warbelow@google.com>
2020-06-02 09:28:19 -0700

    all methods for league stats

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -19,80 +19,157 @@ class StatTracker
   def self.from_csv(info)
     StatTracker.new(info)
   end
-  
-  
+
+
   # Game Statistics Methods
-  
-  def highest_total_score
-    games.map do |game|
-      game.away_goals.to_i + game.home_goals.to_i
-    end.max
+
+  # League Statistics Methods
+  def count_of_teams
+    teams = CSV.read(@teams, headers: true)
+    teams.count
   end
 
-  def lowest_total_score
-    games.map do |game|
-      game.away_goals.to_i + game.home_goals.to_i
-    end.min
-  end
+  def best_offense
+    game_stats = CSV.read(@game_teams, headers: true, header_converters: :symbol)
 
-  def percentage_home_wins
-    home_wins = 0
-    games.each do |game|
-      home_wins += 1 if game.home_goals.to_i > game.away_goals.to_i
+    each_game = game_stats.map do |row|
+      GameTeams.new(row)
     end
-    (home_wins.to_f / games.size).round(2)
-  end
 
-  def percentage_visitor_wins
-    visitor_wins = 0
-    games.each do |game|
-      visitor_wins += 1 if game.away_goals.to_i > game.home_goals.to_i
+    teams = CSV.read(@teams, headers: true, header_converters: :symbol)
+    each_team = teams.map do |row|
+      Team.new(row)
     end
-    (visitor_wins.to_f / games.size).round(2)
-  end
 
-  def percentage_ties
-    ties = 0
-    games.each do |game|
-      ties += 1 if game.away_goals.to_i == game.home_goals.to_i
+    each_game = each_game.group_by do |game_team|
+      game_team.team_id
     end
-    (ties.to_f / games.size).round(2)
-  end
 
-  def count_of_games_by_season
-    games_by_season = Hash.new(0)
-    games.each do |game|
-      games_by_season[game.season] += 1
+    average_goals_per_team = {}
+    each_game = each_game.each_value do |game_teams_array|
+      team_goals = game_teams_array.map do |game_team|
+        game_team.goals
+      end
+
+      game_number = team_goals.count
+      game_team = game_teams_array[0]
+      team_id = game_team.team_id
+      average_goals = (team_goals.sum.to_f)/game_number.to_f
+      average_goals_per_team[team_id] = average_goals
     end
+
+    worst_team = average_goals_per_team.each_pair.reduce do |memo, key_value|
+      if key_value[1] > memo[1]
+        key_value
+      else
+        memo
+      end
+    end
+
+    correct_team = nil
+    each_team.find do |team|
+      if team.id.to_i == worst_team[0]
+        correct_team = team.name
+      end
+    end
+
     correct_team
   end
 
-  def average_goals_per_game
-    average_goals = 0
-    games.each do |game|
-      average_goals += game.away_goals.to_i
-      average_goals += game.home_goals.to_i
+  def worst_offense
+    game_stats = CSV.read(@game_teams, headers: true, header_converters: :symbol)
+
+    each_game = game_stats.map do |row|
+      GameTeams.new(row)
     end
-    (average_goals.to_f / games.count).round(2)
+
+    teams = CSV.read(@teams, headers: true, header_converters: :symbol)
+    each_team = teams.map do |row|
+      Team.new(row)
+    end
+
+    each_game = each_game.group_by do |game_team|
+      game_team.team_id
+    end
+
+    average_goals_per_team = {}
+    each_game = each_game.each_value do |game_teams_array|
+      team_goals = game_teams_array.map do |game_team|
+        game_team.goals
+      end
+
+      game_number = team_goals.count
+      game_team = game_teams_array[0]
+      team_id = game_team.team_id
+      average_goals = (team_goals.sum.to_f)/game_number.to_f
+      average_goals_per_team[team_id] = average_goals
+    end
+
+    worst_team = average_goals_per_team.each_pair.reduce do |memo, key_value|
+      if key_value[1] < memo[1]
+        key_value
+      else
+        memo
+      end
+    end
+
+    correct_team = nil
+    each_team.find do |team|
+      if team.id.to_i == worst_team[0]
+        correct_team = team.name
+      end
+    end
+
+    correct_team
   end
 
-  def average_goals_by_season
-    goals_by_season = Hash.new(0)
-    games.each do |game|
-      goals_by_season[game.season] += (game.away_goals.to_i.to_f + game.home_goals.to_i)
+  def highest_scoring_visitor
+    game_stats = CSV.read(@game_teams, headers: true, header_converters: :symbol)
+
+    each_game = game_stats.map do |row|
+      GameTeams.new(row)
     end
-    goals_by_season.each do |season, count|
-      average_goals = (count / count_of_games_by_season[season])
-      average_goals.round(2)
-      goals_by_season[season] = average_goals.round(2)
+
+    teams = CSV.read(@teams, headers: true, header_converters: :symbol)
+    each_team = teams.map do |row|
+      Team.new(row)
     end
-    goals_by_season
+
+    each_game = each_game.group_by do |game_team|
+      game_team.team_id
+    end
+
+    average_goals_per_team = {}
+    each_game = each_game.each_value do |game_teams_array|
+      team_goals = game_teams_array.map do |game_team|
+        game_team.goals
+      end
+
+      game_number = team_goals.count
+      game_team = game_teams_array[0]
+      team_id = game_team.team_id
+      average_goals = (team_goals.sum.to_f)/game_number.to_f
+      average_goals_per_team[team_id] = average_goals
+    end
+
+    worst_team = average_goals_per_team.each_pair.reduce do |memo, key_value|
+      if key_value[1] < memo[1]
+        key_value
+      else
+        memo
+      end
+    end
+
+    correct_team = nil
+    each_team.find do |team|
+      if team.id.to_i == worst_team[0]
+        correct_team = team.name
+      end
+    end
+
+    correct_team
   end
 
-  
-  # League Statistics Methods
-  
-  
   def highest_scoring_visitor
     game_stats = CSV.read(@game_teams, headers: true, header_converters: :symbol)
 
@@ -288,6 +365,7 @@ class StatTracker
 
     correct_team
   end
+
 
   # Season Statistic methods
 
