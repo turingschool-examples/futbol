3600dd351d3247566d30223a8ff91d9eaf47fe23
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-06-02 12:50:35 -0400

    Add game teams collection file

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -3,6 +3,7 @@ require_relative './team'
 require_relative './game_teams'
 require_relative './game_collection'
 require_relative './team_collection'
+require_relative './game_teams_collection'
 require 'csv'
 
 class StatTracker
