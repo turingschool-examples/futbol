bc96f34d7f99e8d816ebafa136f34ecb3cf25b28
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-06-04 09:46:14 -0400

    Add season stats library

---
+++ lib/season_statistics_library.rb
@@ -0,0 +1,9 @@
+require_relative './game'
+require_relative './team'
+require_relative './game_teams'
+require_relative './game_collection'
+require_relative './team_collection'
+require_relative './game_teams_collection'
+
+class SeasonStatisticsLibrary < StatisticsLibrary
+end
