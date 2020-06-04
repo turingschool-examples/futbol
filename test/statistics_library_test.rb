11e3781e1d84dbeac3eab0c9674fd5d38a7d1e50
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-06-04 08:20:47 -0400

    Add stat lib test it exists

--- test/statistics_library_test.rb
+++ test/statistics_library_test.rb
@@ -7,6 +7,12 @@ require './lib/game'
 require './lib/team'
 require './lib/game_collection'
 require './lib/team_collection'
+require './lib/statistics_library'
 
 class StatisticsLibrayTest < MiniTest::Test
+  def test_it_exists
+    stats_lib = StatisticsLibray.new(game_file, team_file, game_teams_file)
+
+    assert_instance_of StatisticsLibray, stats_lib
+  end
 end
