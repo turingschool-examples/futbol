ca1f1559e39248de2553b42ce49efaf4cf82e1fb
Hashim Gari <hashmaster3k@gmail.com>
2020-07-17 11:44:12 -0600

    Changes to syntax as recommended by HoundCI

--- lib/game_team.rb
+++ lib/game_team.rb
@@ -3,7 +3,7 @@ class GameTeam
   attr_reader :game_id, :team_id, :hOa, :result, :settled_in, :head_coach, :goals, :shots, :tackles, :pim, :powerPlayOpportunities, :powerPlayGoals, :faceOffWinPercentage, :giveaways, :takeaways
 
   def initialize(line)
-    array = line.split(",")
+    array = line.split(',')
 
     @game_id = array[0].to_i
     @team_id = array[1].to_i
@@ -15,9 +15,9 @@ class GameTeam
     @shots = array[7].to_i
     @tackles = array[8].to_i
     @pim = array[9].to_i
-    @powerPlayOpportunities = array[10].to_i
-    @powerPlayGoals = array[11].to_i
-    @faceOffWinPercentage = array[12].to_f
+    @power_play_opportunities = array[10].to_i
+    @power_play_goals = array[11].to_i
+    @face_off_win_percentage = array[12].to_f
     @giveaways = array[13].to_i
     @takeaways = array[14].to_i
   end
--- test/game_team_test.rb
+++ test/game_team_test.rb
@@ -4,27 +4,27 @@ require './lib/game_team'
 
 class GameTeamTest < Minitest::Test
   def test_it_exists
-    test_1 = GameTeam.new("2012030221,3,away,LOSS,OT,John Tortorella,2,8,44,8,3,0,44.8,17,7")
+    test_1 = GameTeam.new('2012030221,3,away,LOSS,OT,John Tortorella,2,8,44,8,3,0,44.8,17,7')
 
     assert_instance_of GameTeam, test_1
   end
 
   def test_has_attributes
-    test_1 = GameTeam.new("2012030221,3,away,LOSS,OT,John Tortorella,2,8,44,8,3,0,44.8,17,7")
+    test_1 = GameTeam.new('2012030221,3,away,LOSS,OT,John Tortorella,2,8,44,8,3,0,44.8,17,7')
 
     assert_equal 2012030221, test_1.game_id
     assert_equal 3, test_1.team_id
-    assert_equal "away", test_1.hOa
-    assert_equal "LOSS", test_1.result
-    assert_equal "OT", test_1.settled_in
-    assert_equal "John Tortorella", test_1.head_coach
+    assert_equal 'away', test_1.hOa
+    assert_equal 'LOSS', test_1.result
+    assert_equal 'OT', test_1.settled_in
+    assert_equal 'John Tortorella', test_1.head_coach
     assert_equal 2, test_1.goals
     assert_equal 8, test_1.shots
     assert_equal 44, test_1.tackles
     assert_equal 8, test_1.pim
-    assert_equal 3, test_1.powerPlayOpportunities
-    assert_equal 0, test_1.powerPlayGoals
-    assert_equal 44.8, test_1.faceOffWinPercentage
+    assert_equal 3, test_1.power_play_opportunities
+    assert_equal 0, test_1.power_play_goals
+    assert_equal 44.8, test_1.face_off_win_percentage
     assert_equal 17, test_1.giveaways
     assert_equal 7, test_1.takeaways
   end
