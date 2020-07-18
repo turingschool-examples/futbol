195a6699fee9fa423736bfb0b4de125584ff856a
Logan Riffell <lkriffell@gmail.com>
2020-07-18 12:21:30 -0600

    Update create_stat_keys for scalability

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -3,11 +3,15 @@ class GameStatistics
 
   attr_reader :stat_hash
 
-  def initialize(game_stats)
-    @stat_hash = game_stats[:stat_hash]
+  def initialize()
+    @stat_hash = {}
   end
 
-  def create_stat_hash
-
+  def create_stat_keys(game_stats)
+    index = 0
+    game_stats[0].size.times do
+      @stat_hash[game_stats[0][index]] = 0
+      index += 1
+    end
   end
 end
