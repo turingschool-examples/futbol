84153dd6259ce937fb5272a08f0f66c24a61e174
Logan Riffell <lkriffell@gmail.com>
2020-07-18 09:06:49 -0600

    Add attr_reader for stat_tracker class

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -5,6 +5,8 @@ class StatTracker
     StatTracker.new(data)
   end
 
+  attr_reader :data #For testing. Eventually make a mock/stub so our test can pass without this
+
   def initialize(data)
     @data = data
   end
