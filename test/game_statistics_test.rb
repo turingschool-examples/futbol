51db27c51806ba10f4b302ad9efa9435d13e4e98
fieldstyler <tdfields93@gmail.com>
2020-07-18 10:49:43 -0600

    Add setup method for game_statistics_test

--- test/game_statistics_test.rb
+++ test/game_statistics_test.rb
@@ -6,9 +6,22 @@ require 'pry'
 
 class GameStatisticsTest < MiniTest::Test
 
+  def setup
+    game_path = './data/dummy_file_games.csv'
+    team_path = './data/dummy_file_teams.csv'
+    game_teams_path = './data/dummy_file_game_teams.csv'
+
+    @locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+  end
+  
   def test_it_exists
     game_statistics = GameStatistics.new
     assert_instance_of GameStatistics, game_statistics
   end
+
 
 end
