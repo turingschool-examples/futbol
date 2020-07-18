62f69b2d9a4247d060558216ebf760245f2a50f1
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-17 19:31:56 -0600

    Added attribute test

--- test/game_test.rb
+++ test/game_test.rb
@@ -7,10 +7,10 @@ class GameTest < MiniTest::Test
   def setup
     game_param = {game_id: "2012030221",
                   season: "20122013",
-                  away_team_id: 3,
-                  home_team_id: 6,
-                  away_goals: 2,
-                  home_goals: 3
+                  away_team_id: "3",
+                  home_team_id: "6",
+                  away_goals: "2",
+                  home_goals: "3"
                   }
 
   @game = Game.new(game_param)
@@ -20,6 +20,18 @@ class GameTest < MiniTest::Test
   def test_game_exists
 
     assert_instance_of Game, @game
+
+  end
+
+  def test_game_has_attributes
+
+    assert_equal "2012030221", @game.game_id
+    assert_equal "20122013", @game.season
+    assert_equal "3", @game.away_team_id
+    assert_equal "6", @game.home_team_id
+    assert_equal "2", @game.away_goals
+    assert_equal "3", @game.home_goals
+    assert_equal 5, @game.total_goals
 
   end
 
