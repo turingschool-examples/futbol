e6e4a9cd7802d32c84e1417f7668c7679df71616
Drew Williams <drewwilliams5280@gmail.com>
2020-07-18 14:41:22 -0600

    Adds completed games_coached method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -11,4 +11,21 @@ class StatTracker
     @data = data
   end
 
+  def games_coached(season_id)
+    coaches = []
+    season = CSV.read(@data[:games], headers: true, header_converters: :symbol).find_all {|row| row[:season] == season_id}
+    games = CSV.read(@data[:game_teams], headers: true, header_converters: :symbol)
+    season.each do |season|
+      games.each do |game|
+        if season[:game_id] == game[:game_id]
+          coaches << game[:head_coach]
+        end
+      end
+    end
+    games_coached = Hash.new(0)
+    coaches.each {|coach| games_coached[coach] += 1}
+    games_coached
+  end
+
+
 end
