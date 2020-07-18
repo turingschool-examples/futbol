b99ea5be8341d34c5ce085d229ae53b28e3ebe08
Logan Riffell <lkriffell@gmail.com>
2020-07-17 18:20:45 -0600

    Add minitest setup for stat_tracker_test

---
+++ test/stat_tracker_test.rb
@@ -0,0 +1,14 @@
+require "minitest/autorun"
+require "minitest/pride"
+#require "./lib/<file_name>"
+
+class ClassTest < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+end
