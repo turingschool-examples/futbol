5a738a32931e0cd309d696ead3392b867e253f46
Logan Riffell <lkriffell@gmail.com>
2020-07-18 13:26:03 -0600

    Remove methods from game_statistics class

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -1,28 +1,4 @@
 require 'pry'
 class GameStatistics
 
-  attr_reader :stat_hash
-
-  def initialize()
-    @stat_hash = {}
-  end
-
-  def create_stat_hash_keys(game_stats)
-    index = 0
-    game_stats[0].size.times do
-      @stat_hash[game_stats[0][index]] = []
-      key = @stat_hash[game_stats[0][index]]
-      create_stat_hash_values(game_stats, index, key)
-      index += 1
-    end
-  end
-
-  def create_stat_hash_values(game_stats, index, key)
-    line_index = 1
-    number = game_stats.size - 1
-    number.times do
-      key << game_stats[line_index][index]
-      line_index += 1
-    end
-  end
 end
