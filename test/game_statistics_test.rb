8f02802a141b629e13cb3536150f204f36844fdb
fieldstyler <tdfields93@gmail.com>
2020-07-18 10:46:24 -0600

    Add game_statistics instance variable

--- test/game_statistics_test.rb
+++ test/game_statistics_test.rb
@@ -7,6 +7,7 @@ require 'pry'
 class GameStatisticsTest < MiniTest::Test
 
   def test_it_exists
+    game_statistics = GameStatistics.new
     assert_instance_of GameStatistics, game_statistics
   end
 
