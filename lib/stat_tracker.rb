b69d99f61c12a1ff6badec02a0b912e94196cc9d
Arique Aguilar <arique1104@gmail.com>
2020-07-18 17:56:47 -0500

    Passing code for initial design.

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,20 +1,32 @@
 require "CSV"
+require "./lib/games"
+
 class StatTracker
- attr_reader  :locations
+  attr_reader :games
+
   def self.from_csv(locations)
     StatTracker.new(locations)
-    # game = CSV.foreach(locations[:games]) do |row|
-  
-    # end
-
-    game = CSV.read(locations[:games])
-
-    require "pry"; binding.pry
-
   end
 
   def initialize(locations)
-    @locations = locations
+    @games ||= turn_games_csv_data_into_games_objects(locations[:games])
+    # @teams = locations[:teams]
+    # @game_teams = locations[:game_teams]
   end
 
+  def turn_games_csv_data_into_games_objects(games_csv_data)
+    games_objects_collection = []
+    CSV.foreach(games_csv_data, headers: true, header_converters: :symbol) do |row|
+      games_objects_collection << Games.new(row)
+    end
+    games_objects_collection
+    # require "pry"; binding.pry
+  end
+
+  def highest_total_score
+    output = @games.max_by do |game|
+      game.total_game_score
+    end
+    output.total_game_score
+  end
 end
