e9255a5bfd30b76716f9c20dfcf033bb72445839
Drew Williams <drewwilliams5280@gmail.com>
2020-07-18 15:08:33 -0600

    Adds test for winningest_coach

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -73,4 +73,18 @@ class StatTrackerTest < MiniTest::Test
       assert_equal 0.333, stat_tracker.winning_percentage("20132014")["Peter Horachek"]
     end
 
+    def test_if_it_can_get_coach_with_highest_win_percent
+      game_path = './data/fake_games.csv'
+      team_path = './data/teams.csv'
+      game_teams_path = './data/fake_game_teams.csv'
+
+      locations = {
+        games: game_path,
+        teams: team_path,
+        game_teams: game_teams_path
+        }
+      stat_tracker = StatTracker.from_csv(locations)
+      assert_equal "Claude Noel", stat_tracker.winningest_coach("20132014")
+    end
+
   end
