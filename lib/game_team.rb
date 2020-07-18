2a166a673fceb59da0ed42bac16cfefa6efc1ecd
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-17 19:48:06 -0600

    Initialize and add attributes

---
+++ lib/game_team.rb
@@ -0,0 +1,16 @@
+class GameTeam
+
+  attr_reader :game_id, :team_id, :hoa, :result, :settled_in, :head_coach, :goals
+
+  def initialize(game_team_param)
+    @game_id = game_team_param[:game_id]
+    @team_id = game_team_param[:team_id]
+    @hoa = game_team_param[:hoa]
+    @result = game_team_param[:result]
+    @settled_in = game_team_param[:settled_in]
+    @head_coach = game_team_param[:head_coach]
+    @goals = game_team_param[:goals]
+  end
+
+
+end
