f3d702281f133e3ee6712db8c112d52f3bdeb367
Logan Riffell <lkriffell@gmail.com>
2020-07-18 13:10:26 -0600

    Add create_stat_hash_values method

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -10,13 +10,19 @@ class GameStatistics
   def create_stat_hash_keys(game_stats)
     index = 0
     game_stats[0].size.times do
-      @stat_hash[game_stats[0][index]] = 0
+      @stat_hash[game_stats[0][index]] = []
+      key = @stat_hash[game_stats[0][index]]
+      create_stat_hash_values(game_stats, index, key)
       index += 1
     end
-
   end
 
-  def create_stat_hash_values(stat_hash_keys)
-
+  def create_stat_hash_values(game_stats, index, key)
+    line_index = 1
+    number = game_stats.size - 1
+    number.times do
+      key << game_stats[line_index][index]
+      line_index += 1
+    end
   end
 end
