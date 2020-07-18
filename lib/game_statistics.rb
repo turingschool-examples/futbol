c8ffbbb1fd536d68ddde47e45772f06d19af3e01
Logan Riffell <lkriffell@gmail.com>
2020-07-18 17:12:10 -0600

    Add @score_total variable in initialize

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -3,14 +3,16 @@ class GameStatistics
 
   def initialize(games_hash)
     @games_hash = games_hash
+    @score_totals = []
   end
 
   def highest_total_score
     index = 0
-    acc -[]
+    acc = []
     @games_hash["away_goals"].size.times do
       acc << @games_hash["away_goals"][index] + @games_hash["away_goals"][index]
     require "pry"; binding.pry
     end
   end
+
 end
