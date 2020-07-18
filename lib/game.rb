a0e27dbeb46f04d77c64a8f1cbe651bc6b6cfaa0
Nico Rithner <nicorithner@gmail.com>
2020-07-18 15:09:25 -0600

    Combine Arique's set up and mine to improve set up

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,42 @@
-class Game
-  def initialize(data)
-    @away_goals = data["away_goals"].to_i
-    @home_goals = data["home_goals"].to_i
-    
+## Arique's set up
+class Games
+  attr_reader :game_id,
+              :season,
+              :type,
+              :date_time,
+              :away_team_id,
+              :home_team_id,
+              :away_goals,
+              :home_goals,
+              :venue,
+              :venue_link
+
+  def initialize(params)
+    @game_id = params[:game_id]
+    @season = params[:season]
+    @type = params[:type]
+    @date_time = params[:date_time]
+    @away_team_id = params[:away_team_id]
+    @home_team_id = params[:home_team_id]
+    @away_goals = params[:away_goals].to_i
+    @home_goals = params[:home_goals].to_i
+    @venue = params[:venue]
+    @venue_link = params[:venue_link]
   end
 
   def total_game_score
     @away_goals + @home_goals
   end
 end
+
+# class Game
+#   # def initialize(data)
+#   #   @away_goals = data["away_goals"].to_i
+#   #   @home_goals = data["home_goals"].to_i
+    
+#   # end
+
+#   def total_game_score
+#     @away_goals + @home_goals
+#   end
+# end
--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,82 +1,58 @@
 require "CSV"
-require_relative "./game"
-
+require "./lib/game"
+## => Arique's set up
 class StatTracker
-  ## This -below- is the class method (indicated by the self.)
-  def self.from_csv(locations) ##locations is a hash of the file paths.
-    all_games = []
-    games = CSV.foreach(locations[:games], :headers => true) do |row| ## this is our array of games
+  attr_reader :games
 
-      all_games << Game.new(row)
+  def self.from_csv(locations)
+    StatTracker.new(locations)
+  end
+
+  def initialize(locations)
+    @games ||= turn_games_csv_data_into_games_objects(locations[:games])
+    # @teams = locations[:teams]
+    # @game_teams = locations[:game_teams]
+  end
+
+  def turn_games_csv_data_into_games_objects(games_csv_data)
+    games_objects_collection = []
+    CSV.foreach(games_csv_data, headers: true, header_converters: :symbol) do |row|
+      games_objects_collection << Games.new(row)
     end
-    
-    StatTracker.new(all_games) ## this is creating an instance of the class
+    games_objects_collection
+    # require "pry"; binding.pry
   end
-
-  def initialize(games)
-    @games = games.to_a
-  end
-
   def highest_total_score
-    #  @games[0]["home_goals"].to_i + @games[0]["away_goals"].to_i
     output = @games.max_by do |game|
       game.total_game_score
     end
     output.total_game_score
   end
-
-
-
-
 end
 
-
-
-
-
+# require "CSV"
+# require_relative "./game"
 
 # class StatTracker
-#  attr_reader  :locations
-#   def self.from_csv(locations)
-#     StatTracker.new(locations)
-#     # game = CSV.foreach(locations[:games]) do |row|
-  
-#     # end
-
-#     game = CSV.foreach(locations[:games], :headers => true)
-#     game = game.to_a
-#     x = game.find {|game| game["date_time"] == "5/16/13"; puts game["venue"] }
-#     require "pry"; binding.pry
-    
-    
-
-
-
+#   ## This -below- is the class method (indicated by the self.)
+#   def self.from_csv(locations)   ##locations is a hash of the file paths.
+#     all_games = []
+#     games = CSV.foreach(locations[:games], :headers => true) do |row| ## this is our array of games
+#     all_games << Game.new(row)
+#     end
+#     StatTracker.new(all_games) ## this is creating an instance of the class
 #   end
 
-  
+#   def initialize(games)
+#     @games = games.to_a
+#   end
+
+  # def highest_total_score
+  #   #  @games[0]["home_goals"].to_i + @games[0]["away_goals"].to_i
+  #   output = @games.max_by do |game|
+  #     game.total_game_score
+  #   end
+  #   output.total_game_score
+  # end
 
 # end
-
-# =begin
-# require "CSV"
-# class StatTracker
-#  attr_reader  :locations
-#   def self.from_csv(locations)
-#     StatTracker.new(locations)
-#     # game = CSV.foreach(locations[:games]) do |row|
-  
-#     # end
-
-#     game = CSV.read(locations[:games])
-
-#     require "pry"; binding.pry
-
-#   end
-
-#   def initialize(locations)
-#     @locations = locations
-#   end
-
-# end  
-# =end
