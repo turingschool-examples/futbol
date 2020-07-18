93a3fe73add5ef5a7bfb8a2e3592ca568cf848f6
Logan Riffell <lkriffell@gmail.com>
2020-07-18 17:11:06 -0600

    Add accumulator for highest_total_score method

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -4,8 +4,13 @@ class GameStatistics
   def initialize(games_hash)
     @games_hash = games_hash
   end
-  
+
   def highest_total_score
+    index = 0
+    acc -[]
+    @games_hash["away_goals"].size.times do
+      acc << @games_hash["away_goals"][index] + @games_hash["away_goals"][index]
     require "pry"; binding.pry
+    end
   end
 end
