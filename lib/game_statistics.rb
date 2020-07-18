929dc125a1e9f5cbc1a850e26e4c21d174ca641f
Logan Riffell <lkriffell@gmail.com>
2020-07-18 17:15:03 -0600

    Add total_scores helper method for highest/lowest_total_score

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -3,16 +3,17 @@ class GameStatistics
 
   def initialize(games_hash)
     @games_hash = games_hash
-    @score_totals = []
   end
 
   def highest_total_score
+    total_scores.max
+  end
+
+  def total_scores
     index = 0
     acc = []
     @games_hash["away_goals"].size.times do
       acc << @games_hash["away_goals"][index] + @games_hash["away_goals"][index]
-    require "pry"; binding.pry
     end
   end
-
 end
