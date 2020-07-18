a839a14675f35f9c348bf3b20e97778717b5da96
Logan Riffell <lkriffell@gmail.com>
2020-07-17 18:25:15 -0600

    Update syntax for class minitest to StatTracker

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -2,7 +2,7 @@ require "minitest/autorun"
 require "minitest/pride"
 require "./lib/stat_tracker"
 
-class ClassTest < Minitest::Test
+class StatTrackerTest < Minitest::Test
 
   #def test_it_exists
     #assert_instance_of <Class>,
