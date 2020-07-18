3b7039c103e748265964762fef6775a602f566db
Logan Riffell <lkriffell@gmail.com>
2020-07-17 18:28:20 -0600

    Add test_it_exists for stat_tracker

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -4,9 +4,9 @@ require "./lib/stat_tracker"
 
 class StatTrackerTest < Minitest::Test
 
-  #def test_it_exists
-    #assert_instance_of <Class>,
-  #end
+  def test_it_exists
+    assert_instance_of StatTracker, stat_tracker
+  end
 
   #def test_it_has_attributes
   #end
