0c2d8f8fa7d62f11cc05c35e02e2f969d358797e
Hashim Gari <hashmaster3k@gmail.com>
2020-07-19 08:49:47 -0600

    Adds ability to read and instantiate objects

--- lib/game_teams_collection.rb
+++ lib/game_teams_collection.rb
@@ -1,8 +1,25 @@
+require './lib/game_team'
+require 'csv'
+
 #GameTeamsCollection
 class GameTeamsCollection
-  attr_reader :path
+
+  attr_reader :path, :all_game_teams
+
   def initialize(path)
     @path = path
     @all_game_teams = []
   end
+
+  def from_csv(path)
+    CSV.foreach(path, headers: true, header_converters: :symbol) do |game_team_data|
+      add_game_team(game_team_data)
+    end
+  end
+
+  def add_game_team(data)
+    @all_game_teams << GameTeam.new(data)
+  end
+
+
 end
--- test/game_teams_collection_test.rb
+++ test/game_teams_collection_test.rb
@@ -1,16 +1,36 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/game_teams_collection'
-#require './lib/game_team'
+require './lib/game_team'
 require 'csv'
 
 class GameTeamsCollectionTest < Minitest::Test
+
   def test_it_exists
     gtc = GameTeamsCollection.new('./test/fixtures/game_teams.csv')
     assert_instance_of GameTeamsCollection, gtc
   end
+
   def test_has_a_path
     gtc = GameTeamsCollection.new('./test/fixtures/game_teams.csv')
     assert_equal './test/fixtures/game_teams.csv', gtc.path
+  end
+
+  def test_it_can_read
+    gtc = GameTeamsCollection.new('./test/fixtures/game_teams.csv')
+    gtc.from_csv('./test/fixtures/game_teams.csv')
+    assert_equal 240, gtc.all_game_teams.length
+    assert_equal GameTeam, gtc.all_game_teams[0].class
+  end
+
+  def test_game_teams_empty
+    gtc = GameTeamsCollection.new('./test/fixtures/game_teams.csv')
+    assert_equal [], gtc.all_game_teams
+  end
+
+  def test_add_game_teams
+    gtc = GameTeamsCollection.new('./test/fixtures/game_teams.csv')
+    gtc.add_game_team({})
+    assert_equal 1, gtc.all_game_teams.length
   end
 end
