92fd847b92f95e4175e3dbf6b917cde9b4b5c6de
Hashim Gari <hashmaster3k@gmail.com>
2020-07-19 08:43:23 -0600

    Adds game teams collection class, test file and fixture file

---
+++ lib/game_teams_collection.rb
@@ -0,0 +1,3 @@
+#GameTeamsCollection
+class GameTeamsCollection
+end
---
+++ test/game_teams_collection_test.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/game_teams_collection'
+require './lib/game_team'
+require 'csv'
+
+class GameTeamsCollectionTest < Minitest::Test
+end
