7a517d86ef38396936d64afc3496aeae88ba5cdb
John Kim <abcdefghijohnk@gmail.com>
2020-07-19 11:22:12 -0600

    best offense team test created

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -9,4 +9,8 @@ class StatTrackerTest < MiniTest::Test
   def test_it_can_count_number_of_teams
     assert_equal 32, StatTracker.count_of_teams
   end
+
+  def test_it_can_get_the_best_offense_team
+   assert_equal "Reign FC", StatTracker.best_offense
+  end
 end
