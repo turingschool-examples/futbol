fcf1024aeed596eb7af58a46e01a6377e41e39ad
Hashim Gari <hashmaster3k@gmail.com>
2020-07-19 08:45:11 -0600

    Updates class with initialization

--- lib/game_teams_collection.rb
+++ lib/game_teams_collection.rb
@@ -1,3 +1,7 @@
 #GameTeamsCollection
 class GameTeamsCollection
+  def initialize(path)
+    @path = path
+    @all_game_teams = []
+  end
 end
--- test/game_teams_collection_test.rb
+++ test/game_teams_collection_test.rb
@@ -1,8 +1,12 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/game_teams_collection'
-require './lib/game_team'
+#require './lib/game_team'
 require 'csv'
 
 class GameTeamsCollectionTest < Minitest::Test
+  def test_it_exists
+    gtc = GameTeamsCollection.new('./test/fixtures/game_teams.csv')
+    assert_instance_of GameTeamsCollection, gtc
+  end
 end
