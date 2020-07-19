3f4b47487889ca6d3e12978f3a1c8b95b23e6180
Dani Coleman <danicoleman00@gmail.com>
2020-07-19 11:09:52 -0600

    Add all tests for #game stats portion. Need to double check our fixture sampling

--- test/game_stats_test.rb
+++ test/game_stats_test.rb
@@ -28,38 +28,47 @@ class GameStatsTest < Minitest::Test
   end
 
   def test_it_can_get_lowest_total_score
-    skip
-
+    assert_equal 0, @game_stats.lowest_total_score
   end
 
   def test_it_can_get_percentage_home_wins
-    skip
-
+    assert_equal 0.44, @game_stats.percentage_home_wins
   end
 
   def test_it_can_get_percentage_visitor_wins
-    skip
-
+    assert_equal 0.36, @game_stats.percentage_visitor_wins
   end
 
   def test_it_can_get_percentage_ties
-    skip
-
+    assert_equal 0.0, @game_stats.percentage_ties
   end
 
   def test_it_can_get_count_of_games_by_season
-    skip
-
+    #we need to get a better sampling if we use the fixture files
+    expected = {
+      "20122013"=>806,
+      "20162017"=>1317,
+      "20142015"=>1319,
+      "20152016"=>1321,
+      "20132014"=>1323,
+      "20172018"=>1355
+    }
+    assert_equal expected, @game_stats.count_of_games_by_season
   end
 
   def test_it_can_get_average_goals_per_game
-    skip
-
+    assert_equal 4, @game_stats.average_goals_per_game
   end
 
   def test_it_can_get_average_goals_by_season
-    skip
-
+    expected = {
+      "20122013"=>4.12,
+      "20162017"=>4.23,
+      "20142015"=>4.14,
+      "20152016"=>4.16,
+      "20132014"=>4.19,
+      "20172018"=>4.44
+    }
+    assert_equal expected, @game_stats.average_goals_by_season
   end
-
 end
