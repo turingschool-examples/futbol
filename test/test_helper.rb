99119881047dce97776fae19a543c982c1366a44
Dani Coleman <danicoleman00@gmail.com>
2020-07-19 11:46:43 -0600

    Add possible requires

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,9 @@ SimpleCov.start
 
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/game'
+require './lib/team'
+require './lib/game_team'
+require './test/test_helper'
+require './lib/stat_tracker'
+require 'pry'
