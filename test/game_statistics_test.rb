e0c69fee5b159ddd025dff0480ae7cf7b53b6ee3
fieldstyler <tdfields93@gmail.com>
2020-07-19 10:55:09 -0600

    Add game_statistics instance to test_lowest_total_score test

--- test/game_statistics_test.rb
+++ test/game_statistics_test.rb
@@ -36,6 +36,7 @@ class GameStatisticsTest < MiniTest::Test
   end
 
   def test_lowest_total_score
+    game_statistics = GameStatistics.new(@game.stat_hash)
     assert_equal 1, game_statistics.lowest_total_score
   end
 end
