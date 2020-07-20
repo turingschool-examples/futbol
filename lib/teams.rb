5faa6c49606322dd1a7734b1e5a5246ad85d719e
Daniel Lessenden <dless27@gmail.com>
2020-07-19 19:19:18 -0500

    Update variable names

--- lib/teams.rb
+++ lib/teams.rb
@@ -3,8 +3,8 @@ class Teams
 
   def initialize(team_data)
     @team_id = team_data[:team_id]
-    @franchise_id = team_data[:franchiseid]
-    @team_name = team_data[:teamname]
+    @franchiseid = team_data[:franchiseid]
+    @teamname = team_data[:teamname]
     @abbreviation = team_data[:abbreviation]
     @stadium = team_data[:stadium]
     @link = team_data[:link]
