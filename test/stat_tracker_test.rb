ce21adb648afd8b6e06704c4e2d4ab87178c5a10
Arique Aguilar <arique1104@gmail.com>
2020-07-19 20:55:04 -0500

    Adds place holder test for stat_tracker

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -18,15 +18,18 @@ class StatTrackerTest < MiniTest::Test
   end
 
   def test_it_exist
-
+    skip
     assert_instance_of StatTracker, @stat_tracker
   end
 
   def test_StatTracker_can_find_highest_total_score
-
+    skip
     assert_equal 11, @stat_tracker.highest_total_score
   end
 
-end
+  def test_it_can_calculate_highest_scoring_visitor
+    assert_equal "", @stat_tracker.highest_scoring_visitor
 
-# game.find {|game| game["date_time"] == "5/16/13"; return game["venue"] }
+  end
+
+end
