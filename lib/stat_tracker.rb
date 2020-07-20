95c21f7e0b90e1e52aab641f5fcbd2fe994dfa8f
Nico Rithner <nicorithner@gmail.com>
2020-07-20 15:21:53 -0600

    Add games_by_season and goals_per_season collections for average_goals_by_season method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -61,4 +61,31 @@ class StatTracker
     sum_of_goals_divided_by_game_count = (sum_of_goals / games_count).round(2)
     sum_of_goals_divided_by_game_count
   end
+
+  def count_of_games_by_season  
+    games_by_season = @games.group_by {|game| game.season}
+    game_count_per_season = {}
+    games_by_season.map {|season, game| game_count_per_season[season] = game.count}
+    game_count_per_season
+  end
+  
+
+  def average_goals_by_season
+    ## Average goals per each season: I need the total games by season. I need the total goals per season.
+
+    # Exists: count_of_games_by_season ## hash of season_name_string => total_games_integer
+    # Exists: total_game_score ## games method. Integer representing total goals in a game
+    # Need: goals_per_season ## sum of scores in the season
+    # Need: games_by_season to be accessed and obtain total goals per game so we can add them per season.
+    games_by_season = @games.group_by {|game| game.season} ##hash of games by season
+    games_by_season.delete_if { |key, value| key.nil? || value.nil? } 
+    goals_per_season = {} 
+    games_by_season.map {|season, games| goals_per_season[season] = [games.sum {|game| game.away_goals + game.home_goals}] }
+     
+    p goals_per_season.compact
+    # binding.pry
+    
+    
+  end
+
 end
