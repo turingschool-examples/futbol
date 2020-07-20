ba391c2ad928424c4015c66430f438c820b0e97a
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-19 20:09:40 -0600

    Remove extra spaces

--- lib/league_stats.rb
+++ lib/league_stats.rb
@@ -7,23 +7,19 @@ class LeagueStats < Stats
 
   def best_offense
     game_teams = @game_teams.group_by {|game| game.team_id}
-
     best_offense = game_teams.max_by do |team_id, game_teams|
       game_teams.sum{|game_team| game_team.goals} / game_teams.count.to_f
     end.first
 
     @teams.find{|team| team.team_id == best_offense}.team_name
-
   end
 
   def worst_offense
     game_teams = @game_teams.group_by {|game| game.team_id}
-
     worst_offense = game_teams.min_by do |team_id, game_teams|
       game_teams.sum{|game_team| game_team.goals} / game_teams.count.to_f
     end.first
 
     @teams.find{|team| team.team_id == worst_offense}.team_name
-
   end
 end
