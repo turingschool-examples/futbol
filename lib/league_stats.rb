75f14676409d86ba859d6559f1100e5150db0291
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-19 19:54:27 -0600

    Add best offense method

---
+++ lib/league_stats.rb
@@ -0,0 +1,18 @@
+require 'pry'
+class LeagueStats < Stats
+
+  def count_of_teams
+    @teams.count
+  end
+
+  def best_offense
+    game_teams = @game_teams.group_by {|game| game.team_id}
+
+    best_offense = game_teams.max_by do |team_id, game_teams|
+      game_teams.sum{|game_team| game_team.goals} / game_teams.count.to_f
+    end.first
+
+    @teams.find{|team| team.team_id == best_offense}.team_name
+
+  end
+end
