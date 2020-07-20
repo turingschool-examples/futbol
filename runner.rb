1c68d560a032bcec3383f6b7542d5c2ac6ee432a
Shaun James <ShaunDaneJames@Gmail.com>
2020-07-20 17:57:56 -0600

    Changes location to data

--- runner.rb
+++ runner.rb
@@ -1,15 +1,15 @@
 require './lib/stat_tracker'
 
-game_path = './data/games.csv'
-team_path = './data/teams.csv'
+game_path       = './data/games.csv'
+team_path       = './data/teams.csv'
 game_teams_path = './data/game_teams.csv'
 
-locations = {
+data = {
   games: game_path,
   teams: team_path,
   game_team: game_teams_path
 }
 
-stat_tracker = StatTracker.from_csv(locations)
+@stat_tracker = StatTracker.from_csv(data)
 
-require 'pry': binding.pry 
+require 'pry': binding.pry
