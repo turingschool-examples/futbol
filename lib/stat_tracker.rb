4c7f28d29a0303e038abcceecac4a488ca13060f
Daniel Lessenden <dless27@gmail.com>
2020-07-20 06:24:56 -0500

    Complete helper methods and count_of_games_by_season

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -56,27 +56,26 @@ class StatTracker
   end
 
   def season_names
-    @stat_tracker.games.map do |game|
+    @season_name = @games.map do |game|
       game.season
     end.uniq
+    @season_name
   end
 
   def season_games(season_id)
-    @stat_tracker.games.select do |game|
+    @games.select do |game|
       game.season == season_id
     end.count
   end #this works!!! need to make dynamic
 
   def count_of_games_by_season
-    @games_per_season = {}
-
-
-      season_names.each do |season|
+    games_per_season = {}
+      @season_name.each do |season|
         season_games(season)
-          @games_per_season[:"#{season_names}"] = season_games(season)
+          games_per_season["#{season}"] = season_games(season)
       end
-      @games_per_season
-    end
+      games_per_season
+    end #to make an interpolated symbol -- games_per_season[:"#{season}"
 
       #for each season_names iterate and count w/ season games
       #make dynamic, for each season_name array element make key,
