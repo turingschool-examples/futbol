25faaa75cde1b25de6131faf059c775ee234732b
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-20 12:14:27 -0600

    Add tests for highest/lowest scoring home/away teams

--- test/league_stats_test.rb
+++ test/league_stats_test.rb
@@ -34,6 +34,22 @@ class LeagueStatsTest < MiniTest::Test
 
   def test_worst_offense
     assert_equal "Utah Royals FC", @league_stats.worst_offense
-
   end
+
+  def test_can_get_highest_scoring_visitor
+    assert_equal "FC Dallas", @league_stats.highest_scoring_visitor
+  end
+
+  def test_losest_scoring_visitor
+    assert_equal "San Jose Earthquakes", @league_stats.lowest_scoring_visitor
+  end
+
+  def test_highest_scoring_home_team
+    assert_equal "Reign FC", @league_stats.highest_scoring_home_team
+  end
+
+  def test_lowest_scoring_home_team
+    assert_equal "Utah Royals FC", @league_stats.lowest_scoring_home_team
+  end 
+
 end
