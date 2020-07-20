b5068cf724ef565255dcda71185b432de618209d
Roberto Rodriguez <roberto.rodriguez@alumni.colostate.edu>
2020-07-20 12:14:07 -0600

    Add methods for highest/lowest scoring home/away teams

--- lib/league_stats.rb
+++ lib/league_stats.rb
@@ -5,9 +5,12 @@ class LeagueStats < Stats
     @teams.count
   end
 
+  def game_teams_by_team_id
+    @game_teams.group_by {|game| game.team_id}
+  end
+
   def best_offense
-    game_teams = @game_teams.group_by {|game| game.team_id}
-    best_offense = game_teams.max_by do |team_id, game_teams|
+    best_offense = game_teams_by_team_id.max_by do |team_id, game_teams|
       game_teams.sum{|game_team| game_team.goals} / game_teams.count.to_f
     end.first
 
@@ -15,11 +18,58 @@ class LeagueStats < Stats
   end
 
   def worst_offense
-    game_teams = @game_teams.group_by {|game| game.team_id}
-    worst_offense = game_teams.min_by do |team_id, game_teams|
+    worst_offense = game_teams_by_team_id.min_by do |team_id, game_teams|
       game_teams.sum{|game_team| game_team.goals} / game_teams.count.to_f
     end.first
 
     @teams.find{|team| team.team_id == worst_offense}.team_name
   end
+
+  def find_all_away_teams
+    @game_teams.find_all {|game| game.hoa == "away"}
+  end
+
+  def away_games_by_team_id
+    find_all_away_teams.group_by {|game| game.team_id}
+  end
+
+  def highest_scoring_visitor
+    best_away_team = away_games_by_team_id.max_by do |team_id, game_teams|
+      game_teams.sum{|game_team| game_team.goals} / game_teams.count.to_f
+    end.first
+
+    @teams.find{|team| team.team_id == best_away_team}.team_name
+  end
+  def lowest_scoring_visitor
+    worst_away_team = away_games_by_team_id.min_by do |team_id, game_teams|
+      game_teams.sum{|game_team| game_team.goals} / game_teams.count.to_f
+    end.first
+
+    @teams.find{|team| team.team_id == worst_away_team}.team_name
+  end
+
+  def find_all_home_teams
+    @game_teams.find_all {|game| game.hoa == "home"}
+  end
+
+  def home_games_by_team_id
+    find_all_home_teams.group_by {|game| game.team_id}
+  end
+
+  def highest_scoring_home_team
+    best_home_team = home_games_by_team_id.max_by do |team_id, game_teams|
+      game_teams.sum{|game_team| game_team.goals} / game_teams.count.to_f
+    end.first
+
+    @teams.find{|team| team.team_id == best_home_team}.team_name
+  end
+
+  def lowest_scoring_home_team
+    worst_home_team = home_games_by_team_id.min_by do |team_id, game_teams|
+      game_teams.sum{|game_team| game_team.goals} / game_teams.count.to_f
+    end.first
+
+    @teams.find{|team| team.team_id == worst_home_team}.team_name
+  end
+
 end
