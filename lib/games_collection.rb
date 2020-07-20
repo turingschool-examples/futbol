6b5e120624a4563d41bbd03478cf465fb1a7d854
RyanDBarnett <conner2468@gmail.com>
2020-07-20 12:07:55 -0600

    Add games collection class

---
+++ lib/games_collection.rb
@@ -0,0 +1,22 @@
+require './lib/game'
+require 'csv'
+
+class GamesCollection
+  attr_reader :path,
+              :all_games
+
+  def initialize(path)
+    @path      = path
+    @all_games = []
+  end
+
+  def from_csv(path)
+    CSV.foreach(path, headers: true, header_converters: :symbol) do |game_data|
+      add_game(game_data)
+    end
+  end
+
+  def add_game(data)
+    @all_games << Game.new(data)
+  end
+end
