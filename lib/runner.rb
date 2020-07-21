08d6520a6b242b47e3281fe57f89159804435eef
Drew Williams <drewwilliams5280@gmail.com>
2020-07-20 20:43:24 -0600

    Fixes spelling error in runner file

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,7 +1,7 @@
 require './lib/stat_tracker'
 require './lib/game'
 require './lib/team'
-require './lib/game_team'
+require './lib/game_teams'
 
 game_path = './data/games.csv'
 team_path = './data/teams.csv'
@@ -14,3 +14,4 @@ locations = {
 }
 
 stat_tracker = StatTracker.from_csv(locations)
+require "pry"; binding.pry
