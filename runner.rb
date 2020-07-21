7dd78ecfdae2c41ab915b580d57766fcb5adfac0
Dani Coleman <danicoleman00@gmail.com>
2020-07-20 18:01:33 -0600

    Revert data to locations and add require CSV

--- runner.rb
+++ runner.rb
@@ -1,15 +1,14 @@
+require 'CSV'
 require './lib/stat_tracker'
 
 game_path       = './data/games.csv'
 team_path       = './data/teams.csv'
 game_teams_path = './data/game_teams.csv'
 
-data = {
+locations = {
   games: game_path,
   teams: team_path,
   game_team: game_teams_path
 }
 
-@stat_tracker = StatTracker.from_csv(data)
-
-require 'pry': binding.pry
+stat_tracker = StatTracker.from_csv(locations)
