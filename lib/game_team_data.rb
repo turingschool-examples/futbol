531f8cf1fca83aefa3f65edeaf3a310562e1ece4
Chris Allbritton <cla.inbox@gmail.com>
2020-07-22 18:18:38 -0400

    Add attr_reader and initialize methods to game team data

--- lib/game_team_data.rb
+++ lib/game_team_data.rb
@@ -1,5 +1,26 @@
 class GameTeamData
+  attr_reader :game_id, :team_id, :hoa, :result,
+  :settled_in, :head_coach, :goals, :shots, :tackles,
+  :pim, :powerPlayOpportunities, :powerPlayGoals,
+  :faceOffWinPercentage, :giveaways, :takeaways
+
+  def initialize()
+    @game_id = game_id
+    @team_id = team_id
+    @hoa = hoa
+    @result = result
+    @settled_in = settled_in
+    @head_coach = head_coach
+    @goals = goals
+    @shots = shots
+    @tackles = tackles
+    @pim = pim
+    @powerPlayOpportunities = powerPlayOpportunities
+    @powerPlayGoals = powerPlayGoals
+    @faceOffWinPercentage = faceOffWinPercentage
+    @giveaways = giveaways
+    @takeaways = takeaways
+  end
 
 
-
-end 
+end
