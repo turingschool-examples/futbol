ea56ef602a1f569d1db0b72de4330588d2676cc1
Logan Riffell <lkriffell@gmail.com>
2020-07-22 15:57:49 -0600

    Add attr_reader and initialize method for team data class

--- lib/team_data.rb
+++ lib/team_data.rb
@@ -1,3 +1,15 @@
 class TeamData
 
+  attr_reader :team_id, :franchise_id, :team_name, :abbreviation,
+  :stadium, :link
+
+  def initialize()
+    @team_id = team_id
+    @franchise_id = franchise_id
+    @team_name = team_name
+    @abbreviation = abbreviation
+    @stadium = stadium
+    @link = link
+  end
+  
 end
