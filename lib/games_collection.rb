e903c4744b573d34243a5fb41a3b06857b41bdb2
RyanDBarnett <conner2468@gmail.com>
2020-07-21 18:52:33 -0600

    Update require to require relative for each collection

--- lib/games_collection.rb
+++ lib/games_collection.rb
@@ -1,4 +1,4 @@
-require './lib/game'
+require_relative './game'
 require 'csv'
 
 class GamesCollection
--- lib/teams_collection.rb
+++ lib/teams_collection.rb
@@ -1,4 +1,4 @@
-require './lib/team'
+require_relative './team'
 require 'csv'
 
 class TeamsCollection
