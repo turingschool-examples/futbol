a4f3f4e40b2d001a08809e5ae49e31be92c16b7d
Chris Allbritton <cla.inbox@gmail.com>
2020-07-22 18:15:17 -0400

    Update game team data test with test_it_exists

--- test/game_team_data_test.rb
+++ test/game_team_data_test.rb
@@ -5,9 +5,10 @@ require "csv"
 
 class GameTeamDataTest < Minitest::Test
 
-  #def test_it_exists
-    #assert_instance_of <Class>,
-  #end
+  def test_it_exists
+    game_team_data = GameTeamData.new
+    assert_instance_of GameTeamData, game_team_data
+  end
 
   #def test_it_has_attributes
   #end
