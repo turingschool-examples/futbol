f8f6c112bc0f5f662170c2afe6fad34ce2d0bdae
Priya Power <priyajag72@gmail.com>
2020-07-22 16:37:16 -0600

    Add minitest syntax to DataObjectCreatable Test

---
+++ test/data_object_creatable_test.rb
@@ -0,0 +1,15 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/data_object_creatable"
+include DataObjectCreatable
+
+class DataObjectCreatableTest < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+end
