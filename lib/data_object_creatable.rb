b3afea3736d54dfdfedf7dac25682c4450e7a7f6
Priya Power <priyajag72@gmail.com>
2020-07-22 16:42:02 -0600

    Update method create_instances_of_game for working code that creates array og game csv data

--- lib/data_object_creatable.rb
+++ lib/data_object_creatable.rb
@@ -6,5 +6,15 @@ require 'csv'
 module DataObjectCreatable
 
   def create_instances_of_game
+    table = CSV.parse(File.read('./data/dummy_file_games.csv'), headers: true, converters: :numeric)
+    line_index = 0
+    all_game_data = []
+    table.size.times do
+      game_data = GameData.new
+      game_data.create_attributes(table, line_index)
+      all_game_data << game_data
+      line_index += 1
+    end
+    all_game_data
   end
 end
