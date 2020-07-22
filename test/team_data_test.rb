2f639cfcee2ef5ca811762d0f2ff3353386adda3
Logan Riffell <lkriffell@gmail.com>
2020-07-22 15:53:48 -0600

    Add minitest syntax and test_it_exists

---
+++ test/team_data_test.rb
@@ -0,0 +1,17 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/team_data"
+require "csv"
+
+class TeamDataTest < Minitest::Test
+
+  def test_it_exists
+    team_data = TeamData.new
+
+    assert_instance_of TeamData, team_data
+  end
+
+  #def test_it_has_attributes
+  #end
+
+end
