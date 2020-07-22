c9fa083d8123d55bdf3c902cc3873aaf30efebad
Chris Allbritton <cla.inbox@gmail.com>
2020-07-22 18:13:43 -0400

    Update game team data test to add minitest syntax

--- test/game_team_data_test.rb
+++ test/game_team_data_test.rb
@@ -1,1 +1,15 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/game_team_data"
+require "csv"
 
+class GameTeamDataTest < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+end
