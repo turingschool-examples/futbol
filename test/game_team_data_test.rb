add7ea8a9f3dcc3ae0be2d4e3611bacc87c32bdd
Chris Allbritton <cla.inbox@gmail.com>
2020-07-22 18:17:35 -0400

    Add game team data test to include setup method

--- test/game_team_data_test.rb
+++ test/game_team_data_test.rb
@@ -5,12 +5,23 @@ require "csv"
 
 class GameTeamDataTest < Minitest::Test
 
+  def setup
+    game_path = './data/dummy_file_games.csv'
+    team_path = './data/dummy_file_teams.csv'
+    game_teams_path = './data/dummy_file_game_teams.csv'
+
+    @locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    @table = CSV.parse(File.read('./data/dummy_file_game_teams.csv'), headers: true)
+  end
+
   def test_it_exists
     game_team_data = GameTeamData.new
     assert_instance_of GameTeamData, game_team_data
   end
-
-  #def test_it_has_attributes
-  #end
 
 end
