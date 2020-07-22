9b56a2ffd7a281620c362c2f4aa7a7be201460d7
fieldstyler <tdfields93@gmail.com>
2020-07-22 15:18:20 -0600

    Add MiniTest syntax to game_data_test

---
+++ test/game_data_test.rb
@@ -0,0 +1,15 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/game_data"
+require "csv"
+
+class GameDataTest < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+end
