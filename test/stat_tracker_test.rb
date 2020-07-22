4f3e6c2948233a150e9d7a86423e6216a4424dc9
Travis McKinstry <mckinstrytg@gmail.com>
2020-07-22 17:40:39 -0600

    Create test to display rival

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -202,4 +202,19 @@ class StatTrackerTest < MiniTest::Test
     assert_equal "DC United", stats.favorite_opponent(18)
   end
 
+  def test_it_can_display_rival
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    stats = StatTracker.from_csv(locations)
+    assert_equal "Houston Dash", stats.rival(18)
+  end
+
 end
