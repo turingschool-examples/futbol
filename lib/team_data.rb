ccdbe64f8735509486aaa50224c1dce1a7b26eef
Logan Riffell <lkriffell@gmail.com>
2020-07-22 16:01:34 -0600

    Update create_attributes method to set attribute values

--- lib/team_data.rb
+++ lib/team_data.rb
@@ -12,7 +12,18 @@ class TeamData
     @link = link
   end
 
-  def create_attributes
-
+  def create_attributes(table, line_index)
+    index = 0
+    @team_id = table[line_index][index]
+    index += 1
+    @franchise_id = table[line_index][index]
+    index += 1
+    @team_name = table[line_index][index]
+    index += 1
+    @abbreviation = table[line_index][index]
+    index += 1
+    @stadium = table[line_index][index]
+    index += 1
+    @link = table[line_index][index]
   end
 end
