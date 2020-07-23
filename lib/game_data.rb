3f376f3b0b0751a528592b088887b05e1f832106
fieldstyler <tdfields93@gmail.com>
2020-07-23 13:29:10 -0600

    Update to refactor create_attributes method using specific keys as indeces

--- lib/game_data.rb
+++ lib/game_data.rb
@@ -29,4 +29,17 @@ class GameData
       end
       all_games
     end
+
+    def create_attributes(table, line_index)
+      @game_id = table[line_index]["game_id"]
+      @season = table[line_index]["season"]
+      @type = table[line_index]["type"]
+      @date_time = table[line_index]["date_time"]
+      @away_team_id = table[line_index]["away_team_id"]
+      @home_team_id = table[line_index]["home_team_id"]
+      @away_goals = table[line_index]["away_goals"]
+      @home_goals = table[line_index]["home_goals"]
+      @venue = table[line_index]["venue"]
+      @venue_link = table[line_index]["venue_link"]
+    end
 end
