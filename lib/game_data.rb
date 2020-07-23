bcf68b4335afcf256a7dc828f2f24c3f05ddbcff
fieldstyler <tdfields93@gmail.com>
2020-07-23 13:27:04 -0600

    Update to refactor initialize method with instance variables set to nil

--- lib/game_data.rb
+++ lib/game_data.rb
@@ -1,3 +1,20 @@
 class GameData
 
-end 
+  attr_reader :game_id, :season, :type,
+      :date_time, :away_team_id, :home_team_id,
+      :away_goals, :home_goals, :venue, :venue_link
+
+    def initialize()
+      @game_id = nil
+      @season = nil
+      @type = nil
+      @date_time = nil
+      @away_team_id = nil
+      @home_team_id = nil
+      @away_goals = nil
+      @home_goals = nil
+      @venue = nil
+      @venue_link = nil
+    end
+
+end
