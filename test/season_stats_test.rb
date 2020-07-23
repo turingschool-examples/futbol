3c1458b0ea91f2560be990208c966168512383d1
Shaun James <ShaunDaneJames@Gmail.com>
2020-07-22 18:12:21 -0600

    Adds and passes most accurate and least accurate

--- test/season_stats_test.rb
+++ test/season_stats_test.rb
@@ -25,6 +25,32 @@ class SeasonStatsTest < Minitest::Test
   end
 
   def test_cognizant_of_winningest_coach
-    assert_equal "Claude Julien", @season_stats.winningest_coach
+    assert_equal "Alain Vigneault", @season_stats.winningest_coach("20142015")
+    assert_equal "Claude Julien", @season_stats.winningest_coach("20132014")
   end
+
+  def test_it_can_get_worst_coach
+    assert_equal "Peter Laviolette", @season_stats.worst_coach("20132014")
+  end
+
+  def test_it_can_get_most_accurate_team
+
+    assert_equal "Real Salt Lake", @season_stats.most_accurate_team("20132014")
+  end
+
+  def test_it_can_get_least_accurate_team
+
+    assert_equal "New York City FC", @season_stats.least_accurate_team("20132014")
+  end
+
+  def test_it_can_get_most_tackles
+    skip
+    assert_equal "FC Cincinnati", @season_stats.most_tackles("20132014")
+  end
+
+  def test_it_knows_fewest_tackles
+    skip
+    assert_equal "Atlanta United", @season_stats.fewest_tackles("20132014")
+  end
+
 end
