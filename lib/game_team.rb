29710aa380bc12d92f403810de9f92c0e489c6a7
Dani Coleman <danicoleman00@gmail.com>
2020-07-23 13:51:44 -0600

    Add shot and tackles to initialize

--- lib/game_team.rb
+++ lib/game_team.rb
@@ -1,16 +1,24 @@
 class GameTeam
 
-  attr_reader :game_id, :team_id, :hoa, :result, :settled_in, :head_coach, :goals
+  attr_reader :game_id,
+              :team_id,
+              :hoa,
+              :result,
+              :settled_in,
+              :head_coach,
+              :goals,
+              :shots,
+              :tackles
 
   def initialize(game_team_param)
-    @game_id = game_team_param[:game_id]
-    @team_id = game_team_param[:team_id]
-    @hoa = game_team_param[:hoa]
-    @result = game_team_param[:result]
+    @game_id    = game_team_param[:game_id]
+    @team_id    = game_team_param[:team_id]
+    @hoa        = game_team_param[:hoa]
+    @result     = game_team_param[:result]
     @settled_in = game_team_param[:settled_in]
     @head_coach = game_team_param[:head_coach]
-    @goals = game_team_param[:goals].to_i
+    @goals      = game_team_param[:goals].to_i
+    @shots      = game_team_param[:shots].to_i
+    @tackles    = game_team_param[:tackles].to_i
   end
-
-
 end
