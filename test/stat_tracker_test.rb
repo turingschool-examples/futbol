5cbbd00f965a041e5e597ba25e997b1e59667a06
Daniel Lessenden <dless27@gmail.com>
2020-07-23 17:37:21 -0500

    Play around with refactoring

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -28,64 +28,11 @@ class StatTrackerTest < MiniTest::Test
     assert_instance_of StatTracker, @stat_tracker
   end
 
-  def test_StatTracker_can_find_highest_total_score
-    skip
-    assert_equal 11, @stat_tracker.highest_total_score
+  def test_find_the_fewest_tackles
+    #skip
+    assert_equal "Atlanta United", @stat_tracker.fewest_tackles("20132014")
+    assert_equal "Orlando City SC", @stat_tracker.fewest_tackles("20142015")
   end
-
-  def test_it_can_calculate_the_lowest_total_score
-    assert_equal 0, @stat_tracker.lowest_total_score
-  end
-
-  def test_average_goals_per_game
-    skip
-    assert_equal 4.22, @stat_tracker.average_goals_per_game
-  end
-
-  def test_average_goals_by_season
-    skip
-    @stat_tracker.average_goals_by_season
-    expected = {"20122013" => 4.12, "20162017" => 4.23, "20142015" => 4.14, "20152016" => 4.16, "20132014" => 4.19, "20172018" => 4.44 }
-    assert_equal expected, @stat_tracker.average_goals_by_season
-  end
-
-  def test_it_can_find_percentage_home_wins
-    skip
-    assert_equal 0.44, @stat_tracker.percentage_home_wins
-  end
-
-  def test_it_can_find_percentage_visitor_wins
-    skip
-    assert_equal 0.36, @stat_tracker.percentage_visitor_wins
-  end
-
-  def test_can_find_percentage_tie
-    skip
-     assert_equal 0.20, @stat_tracker.percentage_tie
-   end
-
-  def test_count_games_by_season
-    skip
-     expected = {"20122013" => 806,
-                 "20162017" => 1317,
-                 "20142015" => 1319,
-                 "20152016" => 1321,
-                 "20132014" => 1323,
-                 "20172018" => 1355
-                  }
-     assert_equal expected, @stat_tracker.count_of_games_by_season
-   end
-
-   def test_count_of_teams
-     skip
-     assert_equal 32, @stat_tracker.count_of_teams
-   end
-
-   def test_highest_scoring_home_team
-     skip
-     assert_equal "Reign FC", @stat_tracker.highest_scoring_home_team
-   end
-
 
     def test_find_the_most_tackles
       skip
@@ -93,11 +40,6 @@ class StatTrackerTest < MiniTest::Test
       assert_equal "Seattle Sounders FC", @stat_tracker.most_tackles("“20142015")
     end
 
-    def test_find_the_fewest_tackles
-      skip
-      assert_equal "Atlanta United", @stat_tracker.fewest_tackles("20132014")
-      assert_equal "Orlando City SC", @stat_tracker.fewest_tackles("20142015")
-    end
 
 
 
