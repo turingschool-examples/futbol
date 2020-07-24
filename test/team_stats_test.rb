f401f82aa0b2f34c7f31678175e07835e0db7d6f
Austin Aspaas <araspaas@live.com>
2020-07-23 20:27:27 -0600

    Add test for team info

---
+++ test/team_stats_test.rb
@@ -0,0 +1,39 @@
+require_relative 'test_helper'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/game'
+require './lib/team'
+require './lib/game_team'
+require './lib/stats'
+require './lib/game_stats'
+require './lib/season_stats'
+require './lib/team_stats'
+require 'pry'
+
+class TeamStatsTest < Minitest::Test
+
+  def setup
+    data = {
+            games:      './data/games.csv',
+            teams:      './data/teams.csv',
+            game_teams: './data/game_teams.csv'
+            }
+
+    @team_stats = TeamStats.new(data)
+  end
+
+  def test_it_exists
+    assert_instance_of TeamStats, @team_stats
+  end
+
+  def test_it_can_get_team_info
+    expected = {
+      "team_id" => "18",
+      "franchise_id" => "34",
+      "team_name" => "Minnesota United FC",
+      "abbreviation" => "MIN",
+      "link" => "/api/v1/teams/18"
+    }
+    assert_equal expected, @team_stats.team_info("18")
+  end
+end
