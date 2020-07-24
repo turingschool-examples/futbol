a03d1ec7a9d498daa62d030630427d421f6844c3
fieldstyler <tdfields93@gmail.com>
2020-07-24 11:47:41 -0600

    Add goals_by_id and games_by_id methods that create hashes by team_id

--- lib/league_statistics.rb
+++ lib/league_statistics.rb
@@ -4,11 +4,17 @@ class LeagueStatistics
 
   def initialize
     @team_name_by_id = Hash.new{}
+    @goals_by_id = Hash.new{ |hash, key| hash[key] = 0 }
+    @games_played_by_id = Hash.new{ |hash, key| hash[key] = 0 }
 
   end
 
   def all_teams
     TeamData.create_objects
+  end
+
+  def all_game_teams
+    GameTeamData.create_objects
   end
 
   def count_of_teams
@@ -23,6 +29,22 @@ class LeagueStatistics
   end
 
   def best_offense
+
+  end
+
+  def goals_by_id
+    all_game_teams.each do |game_team|
+      @goals_by_id[game_team.team_id] += game_team.goals
+    end
+    @goals_by_id
+  end
+
+  def games_by_id
+    all_game_teams.each do |game_team|
+      @games_played_by_id[game_team.team_id] += 1
+    end
+    require "pry"; binding.pry
+    @games_played_by_id
   end
 
 end
