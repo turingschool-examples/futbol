5b92ead70050f482a59b06cb3fc041322cff60b3
Austin Aspaas <araspaas@live.com>
2020-07-23 20:37:33 -0600

    Add worst season method and pass test

--- lib/team_stats.rb
+++ lib/team_stats.rb
@@ -23,4 +23,16 @@ class TeamStats < Stats
     end
     team_games_grouped_by_season.max_by {|_, win_percentage| win_percentage}.first
   end
+
+  def worst_season(team_id)
+    team_wins = @game_teams.find_all {|game| game.team_id == team_id && game.result == "WIN"}
+    winning_game_ids = team_wins.map {|game| game.game_id}
+    team_games = @games.find_all {|game| game.away_team_id == team_id || game.home_team_id == team_id}
+    team_games_grouped_by_season = team_games.group_by {|game| game.season}
+    team_games_grouped_by_season.each do |season, games_array|
+      wins = games_array.find_all {|game| winning_game_ids.include?(game.game_id)}.count
+      team_games_grouped_by_season[season] = wins.to_f / games_array.count
+    end
+    team_games_grouped_by_season.min_by {|_, win_percentage| win_percentage}.first
+  end
 end
