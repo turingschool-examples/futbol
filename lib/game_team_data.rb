413c457a7e518906b547b58756ff1f7780b30f66
Chris Allbritton <cla.inbox@gmail.com>
2020-07-24 12:03:17 -0400

    Update game team data test and game team data to include create objects method

--- lib/game_team_data.rb
+++ lib/game_team_data.rb
@@ -1,4 +1,15 @@
 class GameTeamData
 
-
-end 
+  def self.create_objects
+    table = CSV.parse(File.read('./data/dummy_file_game_teams.csv'), headers: true, converters: :numeric)
+    line_index = 0
+    all_game_teams = []
+    table.size.times do
+      game_team_data = GameTeamData.new
+      game_team_data.create_attributes(table, line_index)
+      all_game_teams << game_team_data
+      line_index += 1
+    end
+    all_game_teams
+  end
+end
--- test/game_team_data_test.rb
+++ test/game_team_data_test.rb
@@ -1,6 +1,7 @@
 require "minitest/autorun"
 require "minitest/pride"
 require "./lib/game_team_data"
+require "csv"
 
 class GameTeamDataTest < Minitest::Test
 
@@ -12,7 +13,7 @@ class GameTeamDataTest < Minitest::Test
   def test_it_can_create_many_objects
     game_team_data = GameTeamData.create_objects
 
-    assert_equal "away", game_team_data[0].hoa 
+    assert_equal "away", game_team_data[0].hoa
   end
 
 end
