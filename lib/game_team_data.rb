5eb2d374ba970dc21976905b416f2e2bcaa9309e
Chris Allbritton <cla.inbox@gmail.com>
2020-07-24 12:10:00 -0400

    Update game team data to include create attributes method

--- lib/game_team_data.rb
+++ lib/game_team_data.rb
@@ -12,4 +12,23 @@ class GameTeamData
     end
     all_game_teams
   end
+
+  def create_attributes(table, line_index)
+    @game_id = table[line_index]["game_id"]
+    @team_id = table[line_index]["team_id"]
+    @hoa = table[line_index]["HoA"]
+    @result = table[line_index]["result"]
+    @settled_in = table[line_index]["settled_in"]
+    @head_coach = table[line_index]["head_coach"]
+    @goals = table[line_index]["goals"]
+    @shots = table[line_index]["shots"]
+    @tackles = table[line_index]["tackles"]
+    @pim = table[line_index]["pim"]
+    @power_play_opportunites = table[line_index]["powerPlayOpportunities"]
+    @power_play_goals = table[line_index]["powerPlayGoals"]
+    @face_off_win_percentage = table[line_index]["faceOffWinPercentage"]
+    @giveaways = table[line_index]["giveaways"]
+    @takeaways = table[line_index]["takeaways"]
+
+  end
 end
