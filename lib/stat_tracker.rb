ab4e3ab26575ff7a117502ff92fa49117627a5be
Daniel Lessenden <dless27@gmail.com>
2020-07-24 17:57:39 -0500

    Add fewest tackles method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -620,4 +620,44 @@ end
   worst = worst.first + "#{math}"
   end#method
 
+
+  def fewest_tackles(season)
+     season_hash = @games.group_by {|games| games.season}
+     season_hash.delete_if {|k, v| k.nil?}
+
+     game_ids_by_season = {}
+       season_hash.map do |season, games|
+         game_ids_by_season[season] = games.map {|game| game.game_id}
+       end
+
+
+     games_by_season = {}
+     game_ids_by_season.map do |season, game_ids|
+       season_games = @game_teams.map do |game|
+         if game_ids.include?(game.game_id)
+           game
+         end
+       end
+       games_by_season[season] = season_games
+     end
+
+   season_games = games_by_season.map {|season, games| games}.flatten.compact
+
+   new_hash = Hash.new([])
+   game_ids_by_season.each do |k, v|
+     v.each do |game|
+     new_hash[k] += season_games.select {|season_game| season_game.game_id == game}
+     end
+   end
+
+   team_tackles = Hash.new(0)
+     new_hash[season].each do |game|
+       team_tackles[game.team_id] += game.tackles
+     end
+   team_tackles.delete_if {|k, v| k.nil? || k.nil?}
+
+   fewest = team_tackles.min_by {|k, v| v}
+   @teams.find {|team| team.team_id == fewest.first}.teamname
+ end#tackle method
+
 end#class
