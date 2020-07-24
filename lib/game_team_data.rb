2375a6eaa052c1f9905e6c94e4c74765f7716ab3
Chris Allbritton <cla.inbox@gmail.com>
2020-07-24 12:16:51 -0400

    Update game team data and test to include working attributes and applicable tests

--- lib/game_team_data.rb
+++ lib/game_team_data.rb
@@ -1,4 +1,37 @@
 class GameTeamData
+  attr_reader :game_id,
+              :team_id,
+              :hoa,
+              :result,
+              :settled_in,
+              :head_coach,
+              :goals,
+              :shots,
+              :tackles,
+              :pim,
+              :power_play_opportunites,
+              :power_play_goals,
+              :face_off_win_percentage,
+              :giveaways,
+              :takeaways
+
+  def initialize()
+    @game_id                 = nil
+    @team_id                 = nil
+    @hoa                     = nil
+    @result                  = nil
+    @settled_in              = nil
+    @head_coach              = nil
+    @goals                   = nil
+    @shots                   = nil
+    @tackles                 = nil
+    @pim                     = nil
+    @power_play_opportunites = nil
+    @power_play_goals        = nil
+    @face_off_win_percentage = nil
+    @giveaways               = nil
+    @takeaways               = nil
+  end
 
   def self.create_objects
     table = CSV.parse(File.read('./data/dummy_file_game_teams.csv'), headers: true, converters: :numeric)
