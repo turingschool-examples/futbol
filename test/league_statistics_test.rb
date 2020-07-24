2f203fef96a01715bfc4c3852cc3bd54ef04e291
fieldstyler <tdfields93@gmail.com>
2020-07-24 10:44:18 -0600

    Add setup and test_it_can_count_teams methods

--- test/league_statistics_test.rb
+++ test/league_statistics_test.rb
@@ -2,16 +2,21 @@ require "minitest/autorun"
 require "minitest/pride"
 require "./lib/league_statistics"
 require "./lib/team_data"
+require "./lib/game_team_data"
 require 'csv'
 
 class LeagueStatisticTest < Minitest::Test
 
-  def test_it_exists
+  def setup
     @league_statistics = LeagueStatistics.new
+  end
+
+  def test_it_exists
     assert_instance_of LeagueStatistics, @league_statistics
   end
 
-  #def test_it_has_attributes
-  #end
+  def test_it_can_count_teams
+    assert_equal 19, @league_statistics.count_of_teams
+  end
 
 end
