a351ba295a3edb7db9b35825cd8c9e3dd8a3eef9
Chris Allbritton <cla.inbox@gmail.com>
2020-07-24 11:50:38 -0400

    Updated variables in create attributes to match initialize for readability

--- lib/team_data.rb
+++ lib/team_data.rb
@@ -1,6 +1,19 @@
 class TeamData
+  attr_reader :team_id,
+              :franchise_id,
+              :team_name,
+              :abbreviation,
+              :stadium,
+              :link
 
-
+  def initialize()
+    @team_id      = nil
+    @franchise_id  = nil
+    @team_name     = nil
+    @abbreviation = nil
+    @stadium      = nil
+    @link         = nil
+  end
 
   def self.create_objects
     table = CSV.parse(File.read('./data/dummy_file_teams.csv'), headers: true, converters: :numeric)
@@ -16,12 +29,11 @@ class TeamData
   end
 
   def create_attributes(table, line_index)
-    @team_id = table[line_index]["team_id"]
-    @franchiseId = table[line_index]["franchiseId"]
-    @teamName = table[line_index]["teamName"]
+    @team_id      = table[line_index]["team_id"]
+    @franchise_id  = table[line_index]["franchiseId"]
+    @team_name     = table[line_index]["teamName"]
     @abbreviation = table[line_index]["abbreviation"]
-    @Stadium = table[line_index]["Stadium"]
-    @link = table[line_index]["link"]
+    @stadium      = table[line_index]["Stadium"]
+    @link         = table[line_index]["link"]
   end
-
 end
