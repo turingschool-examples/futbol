e1f6d5377f595111fd254e0e846b88b195c60821
fieldstyler <tdfields93@gmail.com>
2020-07-24 11:03:34 -0600

    Add can_get_team_name_by_id test and it passes

--- test/league_statistics_test.rb
+++ test/league_statistics_test.rb
@@ -21,7 +21,32 @@ class LeagueStatisticTest < Minitest::Test
 
   def test_find_best_and_worst_offense
     assert_equal "FC Dallas", @league_statistics.best_offense
-    assert_equal "Sporting Kansas City", @league_statistics.worst_offense 
+    assert_equal "Sporting Kansas City", @league_statistics.worst_offense
+  end
+
+  def test_can_get_team_name_by_id
+    expected = {
+                1=>"Atlanta United", 
+                4=>"Chicago Fire",
+                26=>"FC Cincinnati",
+                14=>"DC United",
+                6=>"FC Dallas",
+                3=>"Houston Dynamo",
+                5=>"Sporting Kansas City",
+                17=>"LA Galaxy",
+                28=>"Los Angeles FC",
+                18=>"Minnesota United FC",
+                23=>"Montreal Impact",
+                16=>"New England Revolution",
+                9=>"New York City FC",
+                8=>"New York Red Bulls",
+                30=>"Orlando City SC",
+                15=>"Portland Timbers",
+                19=>"Philadelphia Union",
+                24=>"Real Salt Lake",
+                27=>"San Jose Earthquakes"
+              }
+    assert_equal expected, @league_statistics.get_team_name_by_id
   end
 
 end
