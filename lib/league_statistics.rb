37350406752fc5463b6e31cee44d590290fa827f
fieldstyler <tdfields93@gmail.com>
2020-07-24 11:04:58 -0600

    Add team_name_by_id hash in initialize and get_team_name_by_id method that fills team_name_by_id hash

--- lib/league_statistics.rb
+++ lib/league_statistics.rb
@@ -3,6 +3,7 @@ require_relative "game_team_data"
 class LeagueStatistics
 
   def initialize
+    @team_name_by_id = Hash.new{}
 
   end
 
@@ -12,6 +13,16 @@ class LeagueStatistics
 
   def count_of_teams
     all_teams.size
+  end
+
+  def get_team_name_by_id
+    all_teams.each do |team|
+      @team_name_by_id[team.team_id] = team.team_name
+    end
+    @team_name_by_id
+  end
+
+  def best_offense
   end
 
 end
