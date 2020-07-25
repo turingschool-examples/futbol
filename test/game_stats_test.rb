c8df7790ee9b0ce5a536438a496d4fd71e9b62c2
Dani Coleman <danicoleman00@gmail.com>
2020-07-25 11:30:18 -0600

    Remove extra white space

--- test/game_stats_test.rb
+++ test/game_stats_test.rb
@@ -15,7 +15,6 @@ class GameStatsTest < Minitest::Test
       teams:      './fixtures/teams_fixture.csv',
       game_teams: './fixtures/game_teams_fixture.csv'
     }
-
     @game_stats = GameStats.new(data)
   end
 
@@ -44,16 +43,7 @@ class GameStatsTest < Minitest::Test
   end
 
   def test_it_can_get_count_of_games_by_season
-    #we need to get a better sampling if we use the fixture files
     expected = {"20122013"=>37}
-    # expected = {
-    #   "20122013"=>806,
-    #   "20162017"=>1317,
-    #   "20142015"=>1319,
-    #   "20152016"=>1321,
-    #   "20132014"=>1323,
-    #   "20172018"=>1355
-    # }
     assert_equal expected, @game_stats.count_of_games_by_season
   end
 
@@ -63,14 +53,6 @@ class GameStatsTest < Minitest::Test
 
   def test_it_can_get_average_goals_by_season
     expected = {"20122013"=>3.81}
-    # expected = {
-    #   "20122013"=>4.12,
-    #   "20162017"=>4.23,
-    #   "20142015"=>4.14,
-    #   "20152016"=>4.16,
-    #   "20132014"=>4.19,
-    #   "20172018"=>4.44
-    # }
     assert_equal expected, @game_stats.average_goals_by_season
   end
 end
