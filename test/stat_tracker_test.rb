2cf9c076f81c699fb2f28349f6fac8b8762fd1cb
Travis McKinstry <mckinstrytg@gmail.com>
2020-07-25 09:10:25 -0600

    Create new test for displaying fewest tackles per season

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -176,5 +176,12 @@ class StatTrackerTest < MiniTest::Test
     assert_equal "Seattle Sounders FC", @stat_tracker.most_tackles("20142015")
   end
 
+  def test_it_can_display_fewest_tackles
+
+    assert_equal "Atlanta United", @stat_tracker.fewest_tackles("20132014")
+    assert_equal "Orlando City SC", @stat_tracker.fewest_tackles("20142015")
+  end
+
+
 
 end
