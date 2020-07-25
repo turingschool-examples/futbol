fbb7fc6c4edff7b780953018e8fb23b3b11f1730
Dani Coleman <danicoleman00@gmail.com>
2020-07-25 11:29:07 -0600

    Condesnse methods into single line syntax

--- lib/game_stats.rb
+++ lib/game_stats.rb
@@ -5,63 +5,45 @@ class GameStats < Stats
   end
 
   def highest_total_score
-    @games.map do |game|
-      game.away_goals + game.home_goals
-    end.max
+    @games.map { |game| game.away_goals + game.home_goals}.max
   end
 
   def lowest_total_score
-    @games.map do |game|
-      game.away_goals + game.home_goals
-    end.min
+    @games.map { |game| game.away_goals + game.home_goals}.min
   end
 
   def percentage_home_wins
-    home_wins = @game_teams.find_all do |game|
-      game.hoa == "home" && game.result == "WIN"
-    end.count
+    home_wins = @game_teams.find_all { |game| game.hoa == "home" && game.result == "WIN"}.count
     (home_wins / unique_games.to_f).round(2)
   end
 
   def percentage_visitor_wins
-    visitor_wins = @game_teams.find_all do |game|
-      game.hoa == "away" && game.result == "WIN"
-    end.count
+    visitor_wins = @game_teams.find_all { |game| game.hoa == "away" && game.result == "WIN"}.count
     (visitor_wins / unique_games.to_f).round(2)
   end
 
   def percentage_ties
-    tie_games = @game_teams.find_all do |game|
-      game.result == "TIE"
-    end.count / 2
+    tie_games = @game_teams.select { |game|   game.result == "TIE"}.count / 2
     (tie_games.to_f / unique_games).round(2)
   end
 
   def count_of_games_by_season
-    grouped_by_season = @games.group_by do |game|
-      game.season
-    end
-    grouped_by_season.each do |season, games|
-      grouped_by_season[season] = games.count
-    end
+    grouped_by_season = @games.group_by { |game| game.season}
+    grouped_by_season.each { |season, games| grouped_by_season[season] = games.count}
   end
 
   def average_goals_per_game
-    gpg = @game_teams.sum do |game|
-      game.goals.to_f
-    end
+    gpg = @game_teams.sum { |game| game.goals.to_f}
     (gpg / unique_games).round(2)
   end
 
   def average_goals_by_season
-    grouped_by_season = @games.group_by do |game|
-      game.season
-    end
-    grouped_by_season.each do |season, games_array|
-      goals = games_array.sum do |game|
+    grouped_by_season = @games.group_by { |game| game.season}
+    grouped_by_season.each do |season, games|
+      goals = games.sum do |game|
         game.away_goals + game.home_goals
       end
-      grouped_by_season[season] = (goals.to_f / games_array.count).round(2)
+      grouped_by_season[season] = (goals.to_f / games.count).round(2)
     end
   end
 end
