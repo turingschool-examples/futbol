4255a0ad19c09b28f593fc294ecec15e2bbb754f
Priya Power <priyajag72@gmail.com>
2020-07-26 09:47:58 -0600

    Add access to csv methods to Season Statistics class

--- lib/season_statistics.rb
+++ lib/season_statistics.rb
@@ -1,6 +1,22 @@
+require_relative "game_data"
+require_relative "team_data"
+require_relative "game_team_data"
+
 class SeasonStatistics
 
   def initialize
+  end
+
+  def all_teams
+    TeamData.create_objects
+  end
+
+  def all_games
+    GameData.create_objects
+  end
+
+  def all_game_teams
+    GameTeamData.create_objects
   end
 
   def winningest_coach(season)
