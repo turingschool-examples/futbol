7add8c17fb3b3d872a076ed5e9aae92946bc5776
Priya Power <priyajag72@gmail.com>
2020-07-26 09:38:16 -0600

    Add minitest syntax to season statistics test

---
+++ test/season_statistics_test.rb
@@ -0,0 +1,18 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/season_statistics"
+require "./lib/team_data"
+require "./lib/game_data"
+require "./lib/game_team_data"
+require 'csv'
+
+class SeasonTest < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+end
