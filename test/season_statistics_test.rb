4cfeeccbd51fda8c2bdf468d128e66f14872671c
fieldstyler <tdfields93@gmail.com>
2020-07-27 17:10:26 -0600

    Update assert_equal to reflect spec_harness

--- test/season_statistics_test.rb
+++ test/season_statistics_test.rb
@@ -29,8 +29,8 @@ class SeasonStatisticsTest < Minitest::Test
   end
 
   def test_can_find_most_and_least_tackles_by_team_by_season
-    assert_equal "FC Dallas", @season_statistics.most_tackles("20122013")
-    assert_equal "LA Galaxy", @season_statistics.fewest_tackles("20122013")
+    assert_equal "Atlanta United", @season_statistics.fewest_tackles("20132014")
+    assert_equal "FC Dallas", @season_statistics.most_tackles("20132014")
   end
 
 end
