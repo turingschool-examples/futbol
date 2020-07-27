324f0950134111eae033419daff94d8ac3fcda5c
Priya Power <priyajag72@gmail.com>
2020-07-27 12:27:50 -0600

    Add helper method total_wins_by_season_by_team_id for DRY code for best season method

--- lib/team_statistics.rb
+++ lib/team_statistics.rb
@@ -9,7 +9,7 @@ class TeamStatistics
     @team_info_by_id = Hash.new
     @by_team_id_game_objects = []
     @total_games_by_season = Hash.new{ |hash, key| hash[key] = 0 }
-
+    @total_wins_by_season = Hash.new{ |hash, key| hash[key] = 0 }
   end
 
   def all_teams
@@ -53,27 +53,25 @@ class TeamStatistics
     end
   end
 
+  def total_wins_by_season_by_team_id(passed_id)
+    @by_team_id_game_objects.each do |game|
+      if passed_id == game.away_team_id.to_s && game.away_goals > game.home_goals
+        @total_wins_by_season[game.season] += 1
+      elsif passed_id == game.home_team_id.to_s && game.home_goals > game.away_goals
+        @total_wins_by_season[game.season] += 1
+      elsif passed_id == game.away_team_id.to_s && game.away_goals < game.home_goals
+        @total_wins_by_season[game.season] += 0
+      elsif passed_id == game.home_team_id.to_s && game.home_goals < game.away_goals
+        @total_wins_by_season[game.season] += 0
+      end
+    end
+  end
+
   def best_season(passed_id)
     collect_game_objects_by_team_id(passed_id)
     total_games_by_season_by_team_id
-    require "pry"; binding.pry
+    total_wins_by_season_by_team_id(passed_id)
 
-    # hash key:season value:wins that season
-    @total_wins_by_season_by_team_id = Hash.new{ |hash, key| hash[key] = 0 }
-
-    @by_team_id_game_objects.each do |game|
-      if passed_id == game.away_team_id.to_s && game.away_goals > game.home_goals
-        @total_wins_by_season_by_team_id[game.season] += 1
-      elsif passed_id == game.home_team_id.to_s && game.home_goals > game.away_goals
-        @total_wins_by_season_by_team_id[game.season] += 1
-      elsif passed_id == game.away_team_id.to_s && game.away_goals < game.home_goals
-        @total_wins_by_season_by_team_id[game.season] += 0
-      elsif passed_id == game.home_team_id.to_s && game.home_goals < game.away_goals
-        @total_wins_by_season_by_team_id[game.season] += 0
-      end
-    end
-
-    # Hash key:season value:avg win
     @win_percentage_by_season_by_team_id = Hash.new
 
     @total_wins_by_season_by_team_id.each do |season, total_win|
