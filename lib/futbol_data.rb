53d0c67637e4f5e010881df1e1737c084478744f
Priya Power <priyajag72@gmail.com>
2020-07-28 12:00:20 -0600

    Remove excess lines and comments and updates to match Mocks/Stubs in test

--- lib/futbol_data.rb
+++ lib/futbol_data.rb
@@ -1,66 +1,58 @@
 require "csv"
 class FutbolData
 
+  attr_reader :teams, :games, :game_teams
+
   def initialize(passed)
     @passed = passed
     @data_location = nil
     @teams = []
     @games = []
     @game_teams = []
-    # @collection_name = nil
-  end
-
-  #case method that takes the passed and returns both the file location and .new?
-  #self.create_objects passed param of case
-  #passed param will update file location and new
-  def chosen_data_set
-    case @passed
-      when "team"
-        @data_location = './data/teams.csv'
-      when "game"
-        @data_location = './data/games.csv'
-      when "game_team"
-        @data_location = './data/game_teams.csv'
-    end
+    create_objects
   end
 
   def create_objects
     chosen_data_set
     if @passed == 'team'
-      csv_data = CSV.read(@data_location, headers: true)
-        csv_data.each do |x|
-          @teams << x
-        end
-    elsif @passed == 'game'
-      csv_data = CSV.read(@data_location, headers: true)
-        csv_data.each do |x|
-          @games << x
-        end
-    elsif @passed == 'game_team'
-      csv_data = CSV.read(@data_location, headers: true)
-        csv_data.each do |x|
-          @game_teams << x
-        end
+      case_is_team
+    elsif @passed == 'games'
+      case_is_game
+    elsif @passed == 'game_teams'
+      case_is_game_teams
     end
   end
 
+  def chosen_data_set
+    case @passed
+      when "team"
+        @data_location = './data/teams.csv'
+      when "games"
+        @data_location = './data/games.csv'
+      when "game_teams"
+        @data_location = './data/game_teams.csv'
+    end
+  end
 
+  def case_is_team
+    csv_data = CSV.read(@data_location, headers: true)
+    csv_data.each do |x|
+      @teams << x
+    end
+  end
 
+  def case_is_game
+    csv_data = CSV.read(@data_location, headers: true)
+    csv_data.each do |x|
+      @games << x
+    end
+  end
 
+  def case_is_game_teams
+    csv_data = CSV.read(@data_location, headers: true)
+    csv_data.each do |x|
+      @game_teams << x
+    end
+  end
 
-
-
-
-
-
-    #   table = CSV.parse(File.read(@data_location), headers: true)
-    #   line_index = 0
-    #   all_data = []
-    #   table.size.times do
-    #     require "pry"; binding.pry
-    #     self.create_attributes(table, line_index)
-    #     all_data << collection_name
-    #     line_index += 1
-    # end
-    # all_data
 end
