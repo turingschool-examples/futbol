e17d1c1e9807f383defd29dc5c37fb8693a13d7a
Arique Aguilar <arique1104@gmail.com>
2020-07-27 19:59:26 -0500

    Worst Coacch test passes in our local but fails in speck harness

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,7 +1,7 @@
 require "CSV"
-require_relative "./games"
-require_relative "./teams"
-require_relative "./game_teams"
+require "./lib/games"
+require "./lib/teams"
+require "./lib/game_teams"
 
 class StatTracker
   attr_reader :games, :game_teams, :teams
--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -1,5 +1,4 @@
 require "./test/test_helper"
-require_relative "./games"
 # require 'minitest/autorun'
 
 
