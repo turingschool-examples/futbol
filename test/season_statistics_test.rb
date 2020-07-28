54a0d7b3bd55334e0be2db11724c74463167c6f7
Chris Allbritton <cla.inbox@gmail.com>
2020-07-28 09:16:35 -0400

    Update to match full game data

--- test/season_statistics_test.rb
+++ test/season_statistics_test.rb
@@ -19,18 +19,22 @@ class SeasonStatisticsTest < Minitest::Test
   # Update tests for all new helpers excluding suite
 
   def test_it_can_print_best_and_worst_coach_by_season
-    assert_equal "Claude Julien", @season_statistics.winningest_coach("20122013")
-    assert_equal "Dan Bylsma", @season_statistics.worst_coach("20122013")
+    assert_equal "Claude Julien", @season_statistics.winningest_coach("20132014")
+    assert_equal "Alain Vigneault", @season_statistics.worst_coach("20142015")
   end
 
   def test_can_find_most_and_least_accurate_teams_by_season
-    assert_equal "FC Dallas", @season_statistics.most_accurate_team("20122013")
-    assert_equal "Sporting Kansas City", @season_statistics.least_accurate_team("20122013")
+    assert_equal "Real Salt Lake", @season_statistics.most_accurate_team("20132014")
+    assert_equal "Toronto FC", @season_statistics.most_accurate_team("20142015")
+    assert_equal "New York City FC", @season_statistics.least_accurate_team("20132014")
+    assert_equal "Columbus Crew SC", @season_statistics.least_accurate_team("20142015")
   end
 
   def test_can_find_most_and_least_tackles_by_team_by_season
+    assert_equal "FC Cincinnati", @season_statistics.most_tackles("20132014")
+    assert_equal "Seattle Sounders FC", @season_statistics.most_tackles("20142015")
     assert_equal "Atlanta United", @season_statistics.fewest_tackles("20132014")
-    assert_equal "FC Dallas", @season_statistics.most_tackles("20132014")
+    assert_equal "Orlando City SC", @season_statistics.fewest_tackles("20142015")
   end
 
 end
