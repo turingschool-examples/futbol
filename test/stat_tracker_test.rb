d420e905f64645c1849df9437a0cae391e106c85
Aidan Murray <aidanhansm@gmail.com>
2020-09-03 17:42:14 -0600

    Add call for highest total score

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -3,13 +3,13 @@ require './lib/stat_tracker'
 
 class StatTrackerTest < Minitest::Test
   def test_it_exists
-    game_path = './data/games.csv'
-    team_path = './data/teams.csv'
+    games_path = './data/games.csv'
+    teams_path = './data/teams.csv'
     game_teams_path = './data/game_teams.csv'
 
     locations = {
-      games: game_path,
-      teams: team_path,
+      games: games_path,
+      teams: teams_path,
       game_teams: game_teams_path
     }
 
