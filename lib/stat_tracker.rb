3dce6bc9ac56ea94754f89a0d619647d8f57a135
Aidan Murray <aidanhansm@gmail.com>
2020-09-03 14:50:11 -0600

    Add attr_reader for paths

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -2,10 +2,12 @@
 
 # Stat tracker class
 class StatTracker
+  attr_reader :game_path, :team_path, :game_teams_path
+
   def initialize(locations)
     @game_path = locations[:games]
     @team_path = locations[:teams]
-    @game_team_path = locations[:game_teams]
+    @game_teams_path = locations[:game_teams]
   end
 
   def self.from_csv(locations)
