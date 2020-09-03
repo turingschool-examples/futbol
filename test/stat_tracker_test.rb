2774ef208291f5ac9b04fff78e56ae5e8d9506e3
Aidan Murray <aidanhansm@gmail.com>
2020-09-03 14:50:40 -0600

    Add tests for from_csv and attributes

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -1,8 +1,12 @@
 require './test/test_helper'
 require './lib/stat_tracker'
 
-class StatTrackerTest
+class StatTrackerTest < Minitest::Test
   def test_it_exists
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+
     locations = {
       games: game_path,
       teams: team_path,
@@ -10,6 +14,40 @@ class StatTrackerTest
     }
 
     stat_tracker = StatTracker.new(locations)
+
+    assert_instance_of StatTracker, stat_tracker
+  end
+
+  def test_attributes
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    stat_tracker = StatTracker.new(locations)
+
+    assert_equal game_path, stat_tracker.game_path
+    assert_equal team_path, stat_tracker.team_path
+    assert_equal game_teams_path, stat_tracker.game_teams_path
+  end
+
+  def test_from_csv
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    stat_tracker = StatTracker.from_csv(locations)
 
     assert_instance_of StatTracker, stat_tracker
   end
