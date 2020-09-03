722666cd5563909262a645060f2298becb27f491
Connor Ferguson <cpfergus1@gmail.com>
2020-09-03 15:02:47 -0600

    Created tests for stat_tracker class

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -1,2 +1,36 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/stat_tracker'
 
-require
+
+
+class TestStatTracker <Minitest::Test
+
+
+
+  def test_stat_tracker_has_attributes
+    stat_tracker = StatTracker.new
+
+    assert_equal nil, stat_tracker.game
+    assert_equal nil, stat_tracker.teams
+    assert_equal nil, stat_tracker.game_teams
+  end
+
+  def test_stat_tracker_can_pull_file_locations
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    stat_tracker = StatTracker.from_csv(locations)
+
+    assert_equal game_path, stat_tracker.games
+    assert_equal team_path, stat_tracker.teams
+    assert_equal game_teams_path, stat_tracker.game_teams
+  end
+
+end
