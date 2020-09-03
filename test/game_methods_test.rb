6c1e582b9fa664df421edd3a07159907c879c518
Aidan Murray <aidanhansm@gmail.com>
2020-09-03 17:48:47 -0600

    Add test for game methods

---
+++ test/game_methods_test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require './test/.test_helper'
+
+class GameMethodsTest < Minitest::Test
+  def test_it_exists
+    game_methods = GameMethods.new('location_of_file')
+
+    assert_instance_of GameMethods, game_methods
+
+    assert_equal 'location_of_file', game_methods.file_loc
+  end
+end
