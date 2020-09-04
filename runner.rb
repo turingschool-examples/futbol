31eafe6fc40a51197bd0a90d0eb74314bc9bf9ff
Joshua Carey <jdcarey128@gmail.com>
2020-09-04 09:37:08 -0600

    Add stat_tracker_test variable and setup

--- runner.rb
+++ runner.rb
@@ -1,12 +1,12 @@
 require './lib/stat_tracker'
 
-# game_path = './data/games.csv'
-# team_path = './data/teams.csv'
-# game_teams_path = './data/game_teams.csv'
+game_path = './data/games.csv'
+team_path = './data/teams.csv'
+game_teams_path = './data/game_teams.csv'
 
-game_path = './data/games_test.csv'
-team_path = './data/teams_test.csv'
-game_teams_path = './data/game_teams_test.csv'
+game_path_test = './data/games_test.csv'
+team_path_test = './data/teams_test.csv'
+game_teams_path_test = './data/game_teams_test.csv'
 
 locations = {
   games: game_path,
@@ -14,6 +14,13 @@ locations = {
   game_teams: game_teams_path
 }
 
-stattracker = StatTracker.from_csv(locations)
+locations_test = {
+  games: game_path_test,
+  teams: team_path_test,
+  game_teams: game_teams_path_test
+}
+
+stat_tracker_test = StatTracker.from_csv(locations_test)
+# stat_tracker = StatTracker.from_csv(locations)
 
 require 'pry'; binding.pry
