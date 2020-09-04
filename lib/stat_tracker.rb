0f56bd6b9e56a576a4ad4bf52c80d146b69c14ae
Jesse Mellinger <jesse.m.mellinger@gmail.com>
2020-09-03 18:14:28 -0600

    Create instance variables and add test for file paths

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,14 +1,17 @@
 class StatTracker
+  attr_reader :games, :teams, :game_teams
 
-  def initialize
-
+  def initialize(games, teams, game_teams)
+    @games = games
+    @teams = teams
+    @game_teams = game_teams
   end
 
   def self.from_csv(locations)
-    @games = locations[:games]
-    @teams = locations[:teams]
-    @game_teams = locations[:game_teams]
-    stat_tracker = self.new
+    games = locations[:games]
+    teams = locations[:teams]
+    game_teams = locations[:game_teams]
+    stat_tracker = self.new(games, teams, game_teams)
   end
 
 end
