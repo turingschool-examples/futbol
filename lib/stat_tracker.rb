cfa48cffe939786364e05c350b6202036a0daa00
Joshua Carey <jdcarey128@gmail.com>
2020-09-03 21:25:02 -0600

    Add stattracker instance variables, and foreach csv methods

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,24 +1,42 @@
 require "csv"
 
 class StatTracker
+  attr_reader :teams, :games, :game_teams
 
-  def initialize(stuff)
-    @data = stuff
+  def initialize(team_path, game_path, game_teams_path)
+    @teams = read_teams_from_csv(team_path)
+    @games = read_games_from_csv(game_path)
+    @game_teams = read_game_teams_from_csv(game_teams_path)
   end
-
-  game_path = './data/games.csv'
-  team_path = './data/teams.csv'
-  game_teams_path = './data/game_teams.csv'
-
-  locations = {
-    games: game_path,
-    teams: team_path,
-    game_teams: game_teams_path
-  }
 
   def self.from_csv(locations)
-    CSV.open(locations, headers: true, converters: :symbol) do |row|
-      puts row
-    end
+    self.new(locations[:teams], locations[:games], locations[:game_teams])
   end
+
+  CSV::Converters[:symbol] = ->(value) {value.to_sym rescue value}
+
+  def read_teams_from_csv(path)
+    teams = []
+    CSV.foreach(path, {headers: true, converters: :symbol}) do |row|
+      teams << row
+    end
+    teams
+  end
+
+  def read_games_from_csv(path)
+    games = []
+    CSV.foreach(path, {headers: true, converters: :symbol}) do |row|
+      games << row
+    end
+    games
+  end
+
+  def read_game_teams_from_csv(path)
+    game_teams = []
+    CSV.foreach(path, {headers: true, converters: :symbol}) do |row|
+      game_teams << row
+    end
+    game_teams
+  end
+
 end
