2432b0fee8b233fc4b4b884fe172e754b67705d9
Zach Stearns <zach.stearns92@gmail.com>
2020-09-04 11:55:40 -0600

    Add test for readable attributes

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -4,7 +4,14 @@ require './lib/stat_tracker'
 
 class StatTrackerTest < Minitest::Test
   def test_it_exists
-    stat_tracker = StatTracker.new
+    stat_tracker = StatTracker.new("games", "teams", "game_teams")
     assert_instance_of StatTracker, stat_tracker
+  end
+
+  def test_readable_attributes
+    stat_tracker = StatTracker.new("games", "teams", "game_teams")
+    assert_equal "games", stat_tracker.games
+    assert_equal "teams", stat_tracker.teams
+    assert_equal "game_teams", stat_tracker.game_teams
   end
 end
