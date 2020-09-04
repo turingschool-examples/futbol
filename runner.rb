4e2ecfe07cc59e63d9a8ab315abdd4c300f4ce9c
Joshua Carey <jdcarey128@gmail.com>
2020-09-04 09:50:10 -0600

    Change _test.csv files to _sample.csv and refactor

--- runner.rb
+++ runner.rb
@@ -4,9 +4,9 @@ game_path = './data/games.csv'
 team_path = './data/teams.csv'
 game_teams_path = './data/game_teams.csv'
 
-game_path_test = './data/games_test.csv'
-team_path_test = './data/teams_test.csv'
-game_teams_path_test = './data/game_teams_test.csv'
+game_path_sample = './data/games_sample.csv'
+team_path_sample = './data/teams_sample.csv'
+game_teams_path_sample = './data/game_teams_sample.csv'
 
 locations = {
   games: game_path,
@@ -14,13 +14,13 @@ locations = {
   game_teams: game_teams_path
 }
 
-locations_test = {
-  games: game_path_test,
-  teams: team_path_test,
-  game_teams: game_teams_path_test
+locations_sample = {
+  games: game_path_sample,
+  teams: team_path_sample,
+  game_teams: game_teams_path_sample
 }
 
-stat_tracker_test = StatTracker.from_csv(locations_test)
+stat_tracker_sample = StatTracker.from_csv(locations_sample)
 # stat_tracker = StatTracker.from_csv(locations)
 
 require 'pry'; binding.pry
