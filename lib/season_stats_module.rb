13e2c6f03d573ef3f57964c572f9776846ab2770
Sean Steel <seanrsteel@gmail.com>
2020-09-04 17:54:48 -0600

    Add coaches_and_game_id_hash; WIP

--- lib/season_stats_module.rb
+++ lib/season_stats_module.rb
@@ -1,5 +1,11 @@
 module SeasonStatistics
 
+  def coaches_and_game_id_hash
+    coach_and_game = Hash.new
+    @row.sort do |coach, game_id|
+      coach_and_game[:coach] = :game_id
+    end
+  end
 
 
-end 
+end
--- test/season_stats_module_test.rb
+++ test/season_stats_module_test.rb
@@ -1,10 +1,21 @@
 require './test/test_helper'
 require './lib/season_stats_module'
 
-# 
-# module SeasonStatisticsTest < Minitest::Test
-#
-#
-#
-#
-# end
+module SeasonStatisticsTest < Minitest::Test
+
+  def setup
+    @game_path = './data/games.csv'
+    @team_path = './data/teams.csv'
+    @game_teams_path = './data/game_teams.csv'
+    @locations = {
+      games: @game_path,
+      teams: @team_path,
+      game_teams: @game_teams_path
+    }
+
+    @stat_tracker = StatTracker.from_csv(@locations)
+
+
+
+
+end
