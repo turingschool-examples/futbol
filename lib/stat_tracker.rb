a44a196d73e0959d889695247d27e6574f8d2b47
Zach Stearns <zach.stearns92@gmail.com>
2020-09-04 11:55:26 -0600

    Initialize and make attribute readable

---
+++ lib/stat_tracker.rb
@@ -0,0 +1,9 @@
+class StatTracker
+  attr_reader :games, :teams, :game_teams
+
+  def initialize(games, teams, game_teams)
+    @games = games
+    @teams = teams
+    @game_teams = game_teams
+  end
+end
