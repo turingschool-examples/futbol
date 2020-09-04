f10e16cf21cf07a35f6232e0fcd2234bde959c1e
Curtis Bartell <cbartell980@gmail.com>
2020-09-04 08:50:30 -0600

    Update initialize to take hash argument

--- lib/team.rb
+++ lib/team.rb
@@ -1,11 +1,11 @@
 class Team
   attr_reader :team_id, :franchise_id, :team_name, :abbreviation, :stadium, :link
   def initialize(row)
-    @team_id = row[0].to_i
-    @franchise_id = row[1].to_i
-    @team_name = row[2]
-    @abbreviation = row[3]
-    @stadium = row[4]
-    @link = row[5]
+    @team_id = row[:team_id].to_i
+    @franchise_id = row[:franchise_id].to_i
+    @team_name = row[:team_name]
+    @abbreviation = row[:abbreviation]
+    @stadium = row[:stadium]
+    @link = row[:link]
   end
 end
