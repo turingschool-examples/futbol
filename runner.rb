ad25dcb3d80cdcb2a7412911ae97d002b56629f6
Joshua Carey <jdcarey128@gmail.com>
2020-09-03 21:21:59 -0600

    Add csv test file pathways from_csv argument

--- runner.rb
+++ runner.rb
@@ -1,8 +1,12 @@
 require './lib/stat_tracker'
 
-game_path = './data/games.csv'
-team_path = './data/teams.csv'
-game_teams_path = './data/game_teams.csv'
+# game_path = './data/games.csv'
+# team_path = './data/teams.csv'
+# game_teams_path = './data/game_teams.csv'
+
+game_path = './data/games_test.csv'
+team_path = './data/teams_test.csv'
+game_teams_path = './data/game_teams_test.csv'
 
 locations = {
   games: game_path,
