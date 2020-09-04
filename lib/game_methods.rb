24548b3fdb74f7fe1867acfc9b022cb7dd63f597
Aidan Murray <aidanhansm@gmail.com>
2020-09-03 18:16:02 -0600

    Add create table method

--- lib/game_methods.rb
+++ lib/game_methods.rb
@@ -4,9 +4,14 @@ require 'CSV'
 
 # Holds methods that get data out of a CSV file
 class GameMethods
-  attr_reader :file_loc
+  attr_reader :file_loc, :table
 
   def initialize(file_loc)
     @file_loc = file_loc
+    @table = create_table
+  end
+
+  def create_table
+    CSV.parse(File.read(@file_loc), headers: true)
   end
 end
