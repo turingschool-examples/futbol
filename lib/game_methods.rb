a5db88224a2d3dfc9d6e5179d7637128662c0b8d
Aidan Murray <aidanhansm@gmail.com>
2020-09-03 19:52:38 -0600

    Add highest total score method

--- lib/game_methods.rb
+++ lib/game_methods.rb
@@ -14,4 +14,13 @@ class GameMethods
   def create_table
     CSV.parse(File.read(@file_loc), headers: true)
   end
+
+  def highest_total_score
+    home_goals = @table['home_goals']
+    game_totals = @table['away_goals'].map.with_index do |away, idx|
+      away.to_i + home_goals[idx].to_i
+    end
+
+    game_totals.max
+  end
 end
