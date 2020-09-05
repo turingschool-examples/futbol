a83fbedadfaa326bcfbf3f03a59755c378d57a0d
Kevin Cuadros <kev040798@gmail.com>
2020-09-04 20:38:33 -0400

    Create class and test

---
+++ lib/stat_tracker.rb
@@ -0,0 +1,16 @@
+# frozen_literal_string: true
+
+require 'CSV'
+# Stat Tracker class
+class StatTracker
+  attr_reader :games, :teams, :game_teams
+  def initialize(locations)
+    @games = locations[:games]
+    @teams = locations[:teams]
+    @game_teams = locations[:game_teams]
+  end
+
+  def self.from_csv(locations)
+    StatTracker.new(locations)
+  end
+end
---
+++ test/stat_tracker_test.rb
@@ -0,0 +1,48 @@
+# frozen_relative_string
+
+require_relative 'test_helper'
+require './lib/stat_tracker'
+
+class StatTrackerTest < Minitest::Test
+  def test_it_exists
+    locations = {
+      games: './data/games.csv',
+      teams: './data/teams.csv',
+      game_teams: './data/game_teams.csv'
+    }
+
+    stat_tracker = StatTracker.new(locations)
+
+    assert_instance_of StatTracker, stat_tracker
+  end
+
+  def test_from_csv
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    stat_tracker = StatTracker.from_csv(locations)
+
+    assert_instance_of StatTracker, stat_tracker
+  end
+
+  def test_it_has_attributes
+    locations = {
+      games: './data/games.csv',
+      teams: './data/teams.csv',
+      game_teams: './data/game_teams.csv'
+    }
+
+    stat_tracker = StatTracker.new(locations)
+
+    assert_equal './data/games.csv', stat_tracker.games
+    assert_equal './data/teams.csv', stat_tracker.teams
+    assert_equal './data/game_teams.csv', stat_tracker.game_teams
+  end
+end
