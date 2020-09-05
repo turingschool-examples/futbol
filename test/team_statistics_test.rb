e1721ec04123f77fec45e4c072cc0883107073f4
Curtis Bartell <cbartell980@gmail.com>
2020-09-05 12:06:32 -0600

    Add test_team_info

--- test/team_statistics_test.rb
+++ test/team_statistics_test.rb
@@ -5,4 +5,18 @@ class TeamStatisticsTest < Minitest::Test
   def setup
     @stat_tracker = StatTracker.new("games", StatTracker.generate_teams('./data/teams.csv'), "game_teams")
   end
+
+  def test_team_info
+    expected ={
+           team_id: 1,
+      franchise_id: 23,
+         team_name: "Atlanta United",
+      abbreviation: "ATL",
+           stadium: "Mercedes-Benz Stadium",
+              link: "/api/v1/teams/1"
+    }
+    actual = @stat_tracker.team_info(1)
+
+    assert_equal expected, actual
+  end 
 end
