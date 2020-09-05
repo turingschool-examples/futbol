c9e5c04294041545468b99bfb465e08a0d8f3474
Hope Gochnour <hope.gochnour@gmail.com>
2020-09-05 15:52:29 -0600

    Add test to make sure that StatTracker can get games/teams/gameteams

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -12,7 +12,7 @@ class StatTracker
     @game_teams = game_teams
   end
 
-  def self.from_csv(locations = {games: './data/games.csv', teams: './data/teams_test.csv', game_teams: './data/game_teams_test.csv'})
+  def self.from_csv(locations = {games: './data/games_test.csv', teams: './data/teams_test.csv', game_teams: './data/game_teams_test.csv'})
     Games.from_csv(locations[:games])
     Teams.from_csv(locations[:teams])
     GameTeams.from_csv(locations[:game_teams])
--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -9,4 +9,13 @@ class StatTrackerTest < Minitest::Test
   def test_it_is_a_stat_tracker
     assert_instance_of StatTracker, @stats
   end
+
+  def test_it_has_access_to_other_classes
+    assert_instance_of Games, @stats.games[0]
+    assert_equal 6, @stats.games.count
+    assert_instance_of Teams, @stats.teams[0]
+    assert_equal 5, @stats.teams.count
+    assert_instance_of GameTeams, @stats.game_teams[0]
+    assert_equal 12, @stats.game_teams.count
+  end
 end
