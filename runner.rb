38b80ec6fb113976fc196abda83ff5136f09ed38
Zach Stearns <zach.stearns92@gmail.com>
2020-09-04 18:16:06 -0600

    Add quotation marks around ./data/x.csv file paths

--- runner.rb
+++ runner.rb
@@ -1,8 +1,8 @@
 require 'csv'
 require './lib/stat_tracker'
 
-teams = CSV.read(./data/teams.csv)
-games = CSV.read(./data/games.csv)
-games_teams = CSV.read(./data/games.csv)
+teams = CSV.read("./data/teams.csv")
+games = CSV.read("./data/games.csv")
+games_teams = CSV.read("./data/games.csv")
 
 stat_tracker = StatTracker.new(games, teams, games_teams)
