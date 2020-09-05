6df64e9575603e4be385615f922563ea30d5466d
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-04 20:31:34 -0500

    Add from_csv method to StatTracker class

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class StatTracker
   attr_reader :games, :teams, :game_teams
 
@@ -7,23 +9,30 @@ class StatTracker
     @game_teams = transform_game_teams(game_teams)
   end
 
+  def self.from_csv(locations)
+    games = CSV.read(locations[:games])
+    teams = CSV.read(locations[:teams])
+    game_teams = CSV.read(locations[:game_teams])
+
+    self.new(games, teams, game_teams)
+  end
+
   private
+
   Game = Struct.new(:game_id, :season, :type, :date_time, :away_team_id,
               :home_team_id, :away_goals, :home_goals, :venue, :venue_link)
-
   def transform_games(games)
-    games.map do |game|
-      Game.new(game[0], game[1], game[2], game[3], game[4], game[5],
-                game[6], game[7], game[8], game[9])
+    games.map do |cell|
+      Game.new(cell[0], cell[1], cell[2], cell[3], cell[4], cell[5],
+                cell[6], cell[7], cell[8], cell[9])
     end
   end
 
   Team = Struct.new(:team_id, :franchise_id, :team_name, :abbreviation,
                       :stadium, :link)
-
   def transform_teams(teams)
-    teams.map do |team|
-      Team.new(team[0], team[1], team[2], team[3], team[4], team[5])
+    teams.map do |cell|
+      Team.new(cell[0], cell[1], cell[2], cell[3], cell[4], cell[5])
     end
   end
 
@@ -32,11 +41,11 @@ class StatTracker
                         :power_play_opportunities, :power_play_goals,
                         :face_off_win_percentage, :giveaways, :takeaways)
   def transform_game_teams(game_teams)
-    game_teams.map do |game_team|
-      GameTeams.new(game_teams[0], game_teams[1], game_teams[2], game_teams[3],
-                    game_teams[4], game_teams[5], game_teams[6], game_teams[7],
-                    game_teams[8], game_teams[9], game_teams[10], game_teams[11],
-                    game_teams[12], game_teams[13], game_teams[14])
+    game_teams.map do |cell|
+      GameTeams.new(cell[0], cell[1], cell[2], cell[3],
+                    cell[4], cell[5], cell[6], cell[7],
+                    cell[8], cell[9], cell[10], cell[11],
+                    cell[12], cell[13], cell[14])
     end
   end
 end
