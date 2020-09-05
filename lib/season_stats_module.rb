d5db8ef3eb58610bbb6a22460dcaca85beb331ac
Sean Steel <seanrsteel@gmail.com>
2020-09-05 11:35:28 -0600

    Add array_of_game_id_from_season method in Module

--- lib/season_stats_module.rb
+++ lib/season_stats_module.rb
@@ -10,14 +10,22 @@ module SeasonStatistics
   end
 #above returns an array
 
-  def find_
-
-  def coaches_and_game_id_hash
-    coach_and_game = Hash.new
-    @row.sort do |coach, game_id|
-      coach_and_game[:coach] = :game_id
+  def array_of_game_id_from_season(season_id)
+    find_all_games_from_season(season_id).map do |season|
+      season.game_id
     end
   end
+
+  def find_game_teams_data_for_season(season_id)
+    
+  end
+
+  # def coaches_and_game_id_hash
+  #   coach_and_game = Hash.new
+  #   @row.sort do |coach, game_id|
+  #     coach_and_game[:coach] = :game_id
+  #   end
+  # end
 
 
 end
