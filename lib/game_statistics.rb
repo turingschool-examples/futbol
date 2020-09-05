acb88a76e6fa2a2d7cc2b4c4a3d5789146ff1d47
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-05 16:16:17 -0600

    Create GameStatistics test and class

---
+++ lib/game_statistics.rb
@@ -0,0 +1,20 @@
+class GameStatistics
+
+  def initialize
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+    locations = {
+    games: game_path,
+    teams: team_path,
+    game_teams: game_teams_path
+    }
+    @csv_game_table = StatTracker.from_csv(locations)
+    @instance_creation = StatTracker.new(locations)
+  end
+
+  def highest_total_score
+
+  end
+
+end
---
+++ test/game_statistics_test.rb
@@ -0,0 +1,24 @@
+require_relative 'test_helper'
+
+class GameStatistcsTest < Minitest::Test
+
+  def setup
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+
+    @locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+  end
+
+  def test_it_exists
+    stat_tracker = StatTracker.new(@locations)
+    game_statistics = GameStatistics.new
+
+    assert_instance_of GameStatistics, game_statistics
+  end
+
+end
