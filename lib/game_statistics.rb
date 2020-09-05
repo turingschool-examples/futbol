efbb16d49a2458c18cbfc09a070fdb69f97aa1ac
Greg Mitchell <g2mitchell3003@gmail.com>
2020-09-04 21:45:42 -0600

    add method and test for highest and lower score methods

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -9,4 +9,15 @@ module GameStatistics
       game.away_goals + game.home_goals
     end
   end
+
+  def lowest_total_score
+    min_score = lowest_total_score_helper
+    min_score.away_goals + min_score.home_goals
+  end
+
+  def lowest_total_score_helper
+    @games.min_by do |game|
+      game.away_goals + game.home_goals
+    end
+  end
 end
