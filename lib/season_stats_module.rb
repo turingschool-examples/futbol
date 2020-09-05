f4bc413e65597b963d0b8c104d040d37a152bb4c
Sean Steel <seanrsteel@gmail.com>
2020-09-05 12:13:29 -0600

    Add method to find game_teams data for season; WIP

--- lib/season_stats_module.rb
+++ lib/season_stats_module.rb
@@ -10,14 +10,20 @@ module SeasonStatistics
   end
 #above returns an array
 
+
+#maybe go back and try to just complare id in games
+#with id in game_teams
+
+
   def array_of_game_id_from_season(season_id)
+    season_hash = Hash.new
     find_all_games_from_season(season_id).map do |season|
-      season.game_id
+      season_hash[season_id] = season.game_id
     end
   end
 
   def find_game_teams_data_for_season(season_id)
-    
+    game_teams.
   end
 
   # def coaches_and_game_id_hash
