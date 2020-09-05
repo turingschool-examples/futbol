640fe777793e9ba76ada994739b9f721e5ecb52d
Roberto Basulto <roberto.basulto085@gmail.com>
2020-09-05 14:02:34 -0400

    Made find by id helper method and test

--- lib/team_methods.rb
+++ lib/team_methods.rb
@@ -1,17 +1,24 @@
 require 'CSV'
 class TeamMethods
-  attr_reader :file_loc, :table
+  attr_reader :teams_table, :teams
 
-  def initialize(file_loc)
-    @file_loc = file_loc
-    @table = create_table
+  def initialize(teams)
+    @teams = teams
+    @teams_table = create_table(@teams)
   end
 
-  def create_table
-    CSV.parse(File.read(@file_loc), headers: true)
+  def create_table(file)
+    CSV.parse(File.read(file), headers: true)
   end
 
   def count_of_teams
-    @table['teamName'].count
+    @teams_table['teamName'].count
+  end
+
+  def find_by_id(team_id)
+    team = @teams_table.find do |team|
+      team['team_id'] == team_id
+    end
+    team['teamName']
   end
 end
