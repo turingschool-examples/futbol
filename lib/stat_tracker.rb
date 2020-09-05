781edde54a209cb95bee1124cf641076c85aa35e
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-04 20:05:39 -0600

    Prototype - WIP

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -5,6 +5,7 @@ class StatTracker
   def initialize(locations)
     @games = {}
     CSV.foreach(locations[:games], headers: true) do |row|
+      require "pry"; binding.pry
       @games["game_data"] = Game.new({game_id: row["game_id"], season: row["season"], type: row["type"], date_time: row["date_time"], away_team_id: row["away_team_id"], home_team_id: row["home_team_id"], away_goals: row["away_goals"], home_goals: row["home_goals"], venue: row["venue"], venue_link: row["venue_link"]})
     end
     # @games = CSV.read(locations[:games], headers: true)
@@ -14,7 +15,19 @@ class StatTracker
     # end
 
     # @game_teams = CSV.read(locations[:game_teams])
-    end
+    # end
+
+    # def game_data
+    #   CSV.foreach(locations[:games], headers: true) do |row|
+    #     require "pry"; binding.pry
+    #     if @games[:game_id].nil?
+    #       @games[:game_id] = [row["game_id"]]
+    #     else
+    #       @games[:game_id] << row["game_id"]
+    #     end
+    #   end
+    # end
+
   end
 
   # def from_csv(locations)
@@ -23,4 +36,4 @@ class StatTracker
   #   @game_teams = CSV.read(locations[:game_teams])
   # end
 
-# end
+end
