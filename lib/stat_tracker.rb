bd8f35fc38b0bf3fa89be6d42205fc8300b9c16e
Shaun James <ShaunDaneJames@Gmail.com>
2020-09-05 20:41:12 -0600

    Adds game teams stats method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -22,4 +22,17 @@ class StatTracker
       row.delete(:venue_link)
     end
   end
+
+  def game_teams_stats
+    game_teams_data = CSV.read(@game_teams, {encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all})
+    hashed_game_teams_data = game_teams_data.map { |row| row.to_hash }
+    hashed_game_teams_data.each do |row|
+      row.delete(:pim)
+      row.delete(:powerPlayOpportunities)
+      row.delete(:powerPlayGoals)
+      row.delete(:faceOffWinPercentage)
+      row.delete(:giveaways)
+      row.delete(:takeaways)
+    end
+  end
 end
