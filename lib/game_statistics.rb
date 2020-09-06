76e2abc7a762ef348a832f7051f44bae795ae78b
Carson Jardine <carsonjardine@gmail.com>
2020-09-05 20:09:16 -0600

    Test it has attributes

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -1,15 +1,7 @@
-
 class GameStatistics
-  attr_reader :game_id, :season, :type, :date_time, :away_team_id, :home_team_id, :away_goals, :home_goals
+  attr_reader :game_id
 
-  def initialize(data)
-    @game_id = data[:game_id]
-    @season = data[:season]
-    @type = data[:type]
-    @date_time = data[:date_time]
-    @away_team_id = data[:away_team_id]
-    @home_team_id = data[:home_team_id]
-    @away_goals = data[:away_goals]
-    @home_goals = data[:home_goals]
+  def initialize(hashed_game_data)
+    @game_id = hashed_game_data
   end
 end
--- test/game_statistics_test.rb
+++ test/game_statistics_test.rb
@@ -18,23 +18,17 @@ class GameStatisticsTest < Minitest::Test
       teams: team_path,
       game_teams: game_teams_path
     }
-    data = {:game_id => [], :season => [], :type => [], :date_time => [], :away_team_id => [], :home_team_id => [], :away_goals => [], :home_goals => []}
-    @stat_tracker = StatTracker.from_csv(locations)
-    @game_statistics = GameStatistics.new(@stat_tracker.game_stats(data))
+
+    @stat_tracker_location = StatTracker.from_csv(locations)
+    @stat_tracker = @stat_tracker_location.game_stats
+    @game_statistics = GameStatistics.new(@stat_tracker)
   end
 
   def test_it_exists
     assert_instance_of GameStatistics, @game_statistics
   end
 
-  def test_it_has_stats
-    assert_equal "2012030221", @game_statistics.game_id[0]
-    assert_equal "20122013", @game_statistics.season[0]
-    assert_equal "Postseason", @game_statistics.type[0]
-    assert_equal "5/16/13", @game_statistics.date_time[0]
-    assert_equal "3", @game_statistics.away_team_id[0]
-    assert_equal "6", @game_statistics.home_team_id[0]
-    assert_equal 2, @game_statistics.away_goals[0]
-    assert_equal 3, @game_statistics.home_goals[0]
+  def test_it_has_attributes
+    assert_equal 7441, @game_statistics.game_id.length
   end
 end
