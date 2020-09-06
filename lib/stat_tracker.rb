295fdcff1bce982a75c5c8b535cf674a80cd962e
Nick King <nickmaxking@gmail.com>
2020-09-06 13:47:30 -0600

    Update methods to accept arguments. Add attr_readers for games, teams, game_teams

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,11 +1,21 @@
 require 'csv'
 
 class StatTracker
-  # def initialize
-  #
-  # end
+  attr_reader :games,
+              :teams,
+              :game_teams
 
-  def self.from_csv
-    new
+  def initialize(games, teams, game_teams)
+    @games = games
+    @teams = teams
+    @game_teams = game_teams
+  end
+
+  def self.from_csv(locations)
+    games = CSV.read(locations[:games], headers:true)
+    teams = CSV.read(locations[:teams], headers:true)
+    game_teams = CSV.read(locations[:game_teams], headers:true)
+
+    new(games, teams, game_teams)
   end
 end
