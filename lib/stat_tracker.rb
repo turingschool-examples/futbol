7a9ee54f5e12e272038b31a309a0b0288d80668a
Kate Tester <katemorris@gmail.com>
2020-09-06 16:33:57 -0600

    WIP - stat tracker, getting into a hash inside of stat_tracker

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -2,20 +2,22 @@ require 'csv'
 
 class StatTracker
 
-  attr_reader :games, :teams, :game_teams
+  attr_reader :games, :teams, :game_teams, :table
 
   class << self
-    self # => #<Class:StatTracker>
 
     def from_csv(locations)
-      @games = StatTracker.new(locations[:games])
-      @teams = StatTracker.new(locations[:teams])
-      @game_teams = StatTracker.new(locations[:game_teams])
+      grouping = {}
+      locations.each do |name, data|
+        # require "pry"; binding.pry
+        grouping[name] = read_stats(data)
+      end
+    end
+
+    def read_stats(data)
+      CSV.parse(File.read(data), headers: true)
     end
   end
 
-  def initialize(data)
-    # table = CSV.new
-    table = CSV.parse(File.read(data), headers: true)
-  end
+
 end
