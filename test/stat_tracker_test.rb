80fd1f8b829b4dfaed7bd6fdb566989b07ef0e12
Nick King <nickmaxking@gmail.com>
2020-09-06 13:46:36 -0600

    Add test for readable attributes, update early tests with arguments

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -4,15 +4,43 @@ require './lib/stat_tracker'
 
 class StatTrackerTest < Minitest::Test
   def test_it_exists
-    stat_tracker = StatTracker.new
-
+    games = "games"
+    teams = "teams"
+    game_teams = "game_teams"
+    stat_tracker = StatTracker.new(games, teams, game_teams)
+  
     assert_instance_of StatTracker, stat_tracker
   end
 
   def test_it_initializes_with_from_csv
-    stat_tracker = StatTracker.from_csv
+    game_path = './data/games_dummy.csv'
+    team_path = './data/teams_dummy.csv'
+    game_teams_path = './data/game_teams_dummy.csv'
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+    stat_tracker = StatTracker.from_csv(locations)
 
-  
+
     assert_instance_of StatTracker, stat_tracker
+  end
+
+  def test_it_can_inherit_csv_data
+    game_path = './data/games_dummy.csv'
+    team_path = './data/teams_dummy.csv'
+    game_teams_path = './data/game_teams_dummy.csv'
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    stat_tracker = StatTracker.from_csv(locations)
+
+    assert_instance_of CSV::Table, stat_tracker.games
+    assert_instance_of CSV::Table, stat_tracker.teams
+    assert_instance_of CSV::Table, stat_tracker.game_teams
   end
 end
