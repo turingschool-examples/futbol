69bd740fbce929eebf02eb29257f6671feef7c45
Curtis Bartell <cbartell980@gmail.com>
2020-09-05 20:36:54 -0600

    Add test_it_can_find_games_by_team_id

--- test/team_statistics_test.rb
+++ test/team_statistics_test.rb
@@ -3,7 +3,13 @@ require './lib/stat_tracker'
 
 class TeamStatisticsTest < Minitest::Test
   def setup
-    @stat_tracker = StatTracker.new("games", StatTracker.generate_teams('./data/teams.csv'), "game_teams")
+    locations = {
+      games: './fixtures/team_stats_fixture_games.csv',
+      teams: './data/teams.csv',
+      game_teams: './fixtures/team_stats_fixture_game_teams.csv'
+    }
+
+    @stat_tracker = StatTracker.from_csv(locations)
   end
 
   def test_team_info
@@ -16,6 +22,14 @@ class TeamStatisticsTest < Minitest::Test
               link: "/api/v1/teams/1"
     }
     actual = @stat_tracker.team_info(1)
+
+    assert_equal expected, actual
+  end
+
+  def test_it_can_find_games_by_team_id
+    games = @stat_tracker.games
+    expected = [games[11], games[12], games[13], games[14], games[15], games[16], games[17], games[28], games[45]]
+    actual = @stat_tracker.games_by_team_id(24)
 
     assert_equal expected, actual
   end
