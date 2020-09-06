fc72322570e089bcfb91f77933b2b965bd346be2
Carson Jardine <carsonjardine@gmail.com>
2020-09-05 20:16:31 -0600

    Update game stats

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -7,25 +7,19 @@ class StatTracker
     @games = locations[:games]
     @teams = locations[:teams]
     @game_teams = locations[:game_teams]
+
   end
 
   def self.from_csv(locations)
     StatTracker.new(locations)
   end
 
-  def game_stats(data)
-    CSV.read(@games, headers: true, header_converters: :symbol).map do |row|
-      data[:game_id] << row[:game_id]
-      data[:season] << row[:season]
-      data[:type] << row[:type]
-      data[:date_time] << row[:date_time]
-      data[:away_team_id] << row[:away_team_id]
-      data[:home_team_id] << row[:home_team_id]
-      data[:away_goals] << row[:away_goals].to_i
-      data[:home_goals] << row[:home_goals].to_i
-
-      game_stats = GameStatistics.new(data)
+  def game_stats
+    game_data = CSV.read(@games, {encoding: "UTF-8", headers: true, header_converters: :symbol, converters: :all})
+    hashed_game_data = game_data.map { |row| row.to_hash }
+    hashed_game_data.each do |row|
+      row.delete(:venue)
+      row.delete(:venue_link)
     end
-    data
   end
 end
