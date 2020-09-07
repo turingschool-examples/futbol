c0528b8e8aa39011524da29a5b5d80becb6a1fb5
Kevin Cuadros <kev040798@gmail.com>
2020-09-07 17:09:05 -0400

    First test and setup

---
+++ test/league_stats_test.rb
@@ -0,0 +1,29 @@
+require_relative 'test_helper'
+require './lib/game_statistics'
+require './lib/stat_tracker'
+require './lib/league_stats'
+
+class LeagueStatisticsTest < Minitest::Test
+  def setup
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    @stat_tracker_location = StatTracker.from_csv(locations)
+    @teams_stats = @stat_tracker_location.teams_stats
+    @stat_tracker = @stat_tracker_location.game_stats
+    @stat_game_teams_tracker = @stat_tracker_location.game_teams_stats
+    @game_statistics = GameStatistics.new(@stat_tracker, @stat_game_teams_tracker)
+    @league_stats = LeagueStatistics.new(@teams_stats)
+  end
+
+  def test_it_exits
+    assert_instance_of LeagueStatistics, @league_stats
+  end
+end
