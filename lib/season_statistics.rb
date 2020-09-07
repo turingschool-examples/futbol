b56f4e1f7ca93d31abc114d4d30e5d233bb64d74
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-07 12:09:59 -0600

    WIP - create coach game results hash

--- lib/season_statistics.rb
+++ lib/season_statistics.rb
@@ -2,17 +2,50 @@ require_relative "stat_tracker"
 
 class SeasonStatistics
   attr_reader :stat_tracker_copy
-  def initialize
-    game_path = './data/games.csv'
-    team_path = './data/teams.csv'
-    game_teams_path = './data/game_teams.csv'
-    locations = {
-    games: game_path,
-    teams: team_path,
-    game_teams: game_teams_path
-    }
-    @csv_game_table = StatTracker.from_csv(locations)
-    @stat_tracker_copy = StatTracker.new
+  def initialize(csv_files, stat_tracker)
+    @csv_game_teams_table = csv_files.game_teams
+    @stat_tracker_copy = stat_tracker
+  end
+
+  def coach_game_results
+    coach_hash = {}
+    @csv_game_teams_table.each do |key, value|
+      if coach_hash[value.head_coach]
+        coach_hash[value.head_coach] << value.result
+      else
+        coach_hash[value.head_coach] = [value.result]
+      end
+    end
+    coach_hash
+  end
+
+  def winningest_coach
+    coach_game_results.each do |key, value|
+      winningest_coach_name = nil
+      highest_percentage = 0
+      total_games = 0
+      total_wins = 0
+      total_losses = 0
+      total_ties = 0
+      # require "pry"; binding.pry
+      value.each do |game_result|
+        total_games += 1
+        if game_result == "WIN"
+          total_wins += 1
+        elsif game_result == "LOSS"
+          total_losses += 1
+        elsif game_result == "TIE"
+          total_ties += 1
+        end
+      end
+      p " #{key} +  #{(total_wins.to_f / total_games).round(2)}"
+      if (total_wins.to_f / total_games) > highest_percentage
+        highest_percentage = (total_wins.to_f / total_games)
+        winningest_coach_name = key
+        require "pry"; binding.pry
+      end
+    end
+    winningest_coach_name
   end
 
 end
--- test/season_statistics_test.rb
+++ test/season_statistics_test.rb
@@ -12,16 +12,29 @@ class SeasonStatistcsTest < Minitest::Test
       teams: team_path,
       game_teams: game_teams_path
     }
-    @instances = StatTracker.new
+    @stat = StatTracker.new
+
   end
 
   def test_it_exists
     stat_tracker = StatTracker.from_csv(@locations)
-    season_statistics = SeasonStatistics.new
+    season_statistics = SeasonStatistics.new(stat_tracker, @stat)
 
     assert_instance_of SeasonStatistics, season_statistics
   end
 
-  
+  def test_coach_game_results
+    stat_tracker = StatTracker.from_csv(@locations)
+    season_statistics = SeasonStatistics.new(stat_tracker, @stat)
+
+    assert_equal 60, season_statistics.coach_game_results.count
+  end
+
+  def test_winningest_coach
+    stat_tracker = StatTracker.from_csv(@locations)
+    season_statistics = SeasonStatistics.new(stat_tracker, @stat)
+
+    assert_equal "Dan Lacroix", season_statistics.winningest_coach
+  end
 
 end
