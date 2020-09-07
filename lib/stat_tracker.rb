a18a9932a96be4eeab259ecdd290b53502aef7cb
Will Dunlap <dunlapww@gmail.com>
2020-09-07 17:37:41 -0600

    Add Average Goals by Season Method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -18,4 +18,26 @@ class StatTracker
     GameTeam.from_csv(locations[:game_teams])
     self.new(Team.all_teams, Game.all_games, GameTeam.all_game_teams)
   end
+
+  def seasonal_game_data
+    seasonal_game_data = @games.group_by do |row|
+      row.season
+    end
+    seasonal_game_data
+  end
+
+  def avg_score_by_season
+    avg_score_by_season = {}
+    seasonal_game_data.each do |season, details|
+      game_count = 0
+      season_goals = 0
+      details.each do |row|
+        game_count += 1
+        season_goals += row.home_goals + row.away_goals
+      end
+      avg_score_by_season[season] = (season_goals / game_count.to_f).round(2)
+    end
+    avg_score_by_season
+  end
+
 end
