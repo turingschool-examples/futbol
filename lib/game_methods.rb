0b174cb892bd2a09c9a89b13b5875fd33cf75ee5
Megan Campbell <megan.campbell26@yahoo.com>
2020-09-07 10:28:27 -0600

    Add average goals per game method

--- lib/game_methods.rb
+++ lib/game_methods.rb
@@ -4,11 +4,13 @@ require 'CSV'
 
 # Holds methods that get data out of a CSV file
 class GameMethods
-  attr_reader :file_loc, :table
+  attr_reader :file_loc, :table, :home_goals, :away_goals
 
   def initialize(file_loc)
     @file_loc = file_loc
     @table = create_table
+    @home_goals = @table['home_goals']
+    @away_goals = @table['away_goals']
   end
 
   def create_table
@@ -31,5 +33,10 @@ class GameMethods
     end
 
     game_totals.min
+  end
+
+  def average_goals_per_game
+    total_goals = @away_goals.map(&:to_i).sum + @home_goals.map(&:to_i).sum
+    (total_goals.to_f / @away_goals.length).round(2)
   end
 end
