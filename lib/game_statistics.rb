12dd2f28a1d2e2e430013b38ad8f893eddf110a1
Cydnee Owens <cks410@yahoo.com>
2020-09-08 12:05:23 -0600

    Add sum team scores method and test

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -1,3 +1,33 @@
 class GameStatistics
+  attr_reader :stat_tracker
 
+  def initialize(stat_tracker)
+    @stat_tracker = stat_tracker
+  end
+
+  def sum_team_scores
+    grouping = {}
+    total_score_data_set.map do |array|
+      if grouping[array[0]] == nil
+        grouping[array[0]] = array[1].to_i
+      else
+        grouping[array[0]] += array[1].to_i
+      end
+    end
+    grouping
+  end
+
+  def total_score_data_set
+    data_set = @stat_tracker[:game_teams]["game_id"].zip(@stat_tracker[:game_teams]["goals"])
+  end
+
+  def highest_total_score
+    highest_game = sum_team_scores.max_by { |game_id, score| score }
+    highest_game[1]
+  end
+
+  def lowest_total_score
+    lowest_game = sum_team_scores.min_by { |game_id, score| score }
+    lowest_game[1]
+  end
 end
--- test/game_statistics_test.rb
+++ test/game_statistics_test.rb
@@ -5,9 +5,33 @@ require "./lib/game_statistics"
 require "pry";
 
 class GameStatisticsTest < Minitest::Test
-  def test_it_exists
-    game_statistics = GameStatistics.new
+  def setup
+    game_path = './data/dummy_game.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/dummy_game_teams.csv'
 
-    assert_instance_of GameStatistics, game_statistics
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    @stat_tracker = StatTracker.from_csv(locations)
+    @game_statistics = GameStatistics.new(@stat_tracker)
+  end
+
+  def test_it_exists
+    assert_instance_of GameStatistics, @game_statistics
+  end
+
+  def test_highest_total_score
+
+    assert_equal 5, @game_statistics.highest_total_score
+
+  end
+
+  def test_lowest_total_score
+
+    assert_equal 3, @game_statistics.lowest_total_score
   end
 end
