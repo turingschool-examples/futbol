70241a503ca8456f6b441e5d0a779fbcc256a78c
Kevin Cuadros <kev040798@gmail.com>
2020-09-07 20:19:20 -0400

    Helper methods

--- lib/league_stats.rb
+++ lib/league_stats.rb
@@ -1,10 +1,35 @@
 class LeagueStatistics
-  attr_reader :teams_data
-  def initialize(array_teams_data)
+  attr_reader :teams_data, :game_stats
+  def initialize(array_teams_data, game_stats)
     @teams_data = array_teams_data
+    @game_stats = game_stats
   end
 
   def count_of_teams
     @teams_data.length
+  end
+
+  def group_by_team_id
+    @game_stats.game_teams_data.group_by do |team|
+      team[:team_id]
+    end
+  end
+
+  def team_id_and_average_goals
+    hash = {}
+    group_by_team_id.each do |team, games|
+      total_games = games.map do |game|
+        game[:game_id]
+      end
+      total_goals = games.sum do |game|
+        game[:goals]
+      end
+      hash[team] = (total_goals.to_f / total_games.count).round(2)
+    end
+    hash
+  end
+
+  def best_offense
+
   end
 end
