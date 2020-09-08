f7e2e93d61bf23c4ef70c904fa53d635af29843f
Sean Steel <seanrsteel@gmail.com>
2020-09-07 18:37:38 -0600

    Fix method for finding game_teams data for season

--- lib/season_stats_module.rb
+++ lib/season_stats_module.rb
@@ -11,30 +11,42 @@ module SeasonStatistics
 #maybe go back and try to just complare id in games
 #with id in game_teams
 
-
   def array_of_game_id_from_season(season_id)
-    season_hash = Hash.new
-    find_all_games_from_season(season_id).map do |season|
-      season_hash[season_id] = season.game_id
+    find_all_games_from_season(season_id).map do |game|
+      game.game_id
     end
   end
 
   def game_teams_data_for_season(season_id)
-    @game_teams.find_all do |game_team|
-      game_team.game_id.include?(season_id[0..4])
+    @game_teams.find_all do |game|
+      game.game_id[0..3] == season_id[0..3]
     end
-  end
+  end 
 
-  # def find_game_teams_data_for_season(season_id)
-  #   game_teams.
-  # end
-
-  # def coaches_and_game_id_hash
-  #   coach_and_game = Hash.new
-  #   @row.sort do |coach, game_id|
-  #     coach_and_game[:coach] = :game_id
+  # def list_of_coaches_by_season(season_id)
+  #   coaches_by_season = {}
+  #   game_teams_data_for_season(season_id).sort_by do |key, value|
+  #     coaches_by_season[key.head_coach] = value
   #   end
   # end
+
+  def winningest_coach(season_id)
+    # win_count = game_teams_data_for_season(season_id).find do |season|
+    #   (season.head_coach && (season.result == WIN))
+    # end
+
+    coach_results = {}
+    game_teams_data_for_season(season_id).each do |season, game|
+      coach_results[season.head_coach] = []
+      game.each do |game|
+        coach_results[season.head_coach] << season.find do |data|
+          data == WIN
+        end
+      end
+    end
+
+
+  end
 
 
 end
--- test/season_stats_module_test.rb
+++ test/season_stats_module_test.rb
@@ -21,43 +21,27 @@ class SeasonStatisticsTest <Minitest::Test
 
   def test_find_all_games_from_season
 
-    expected = {
-                :away_goals => 2,
-                :away_team_id=>"3",
-                :date_time=>"5/16/13",
-                :game_id=>"2012030221",
-                :home_goals=>3,
-                :home_team_id=>"6",
-                :season=>"20122013",
-                :type=>"Postseason",
-                :venue=>"Toyota Stadium",
-                :venue_link=>"/api/v1/venues/null"}
-
-    assert_equal expected, @stat_tracker.find_all_games_from_season("20122013")[0]
+    assert_equal "5/16/13", @stat_tracker.find_all_games_from_season("20122013")[0].date_time
   end
 
   def test_array_of_game_id_from_season
 #should add more tests here
-    assert_equal "2012030221", @stat_tracker.array_of_game_id_from_season("20122013")[0]
+    assert_equal 806, @stat_tracker.array_of_game_id_from_season("20122013").length
   end
 
-  def test_find_game_teams_data_for_season
-    # expected = <GameTeams:0x00007f898640b318
-    #           @HoA="away",
-    #           @faceOffWinPercentage=44.8,
-    #           @game_id="2012030221",
-    #           @giveaways=17,
-    #           @goals=2,
-    #           @head_coach="John Tortorella",
-    #           @pim=8,
-    #           @powerPlayGoals=0,
-    #           @powerPlayOpportunities=3,
-    #           @result="LOSS",
-    #           @settled_in="OT",
-    #           @shots=8,
-    #           @tackles=44,
-    #           @takeaways=7,
-    #           @team_id="3">
-    assert_equal [], @stat_tracker.game_teams_data_for_season("20122013")[0]
+  def test_game_teams_data_for_season
+
+    assert_equal 1612, @stat_tracker.game_teams_data_for_season("20122013").length
   end
+
+  # def test_list_of_coaches_by_season
+  #
+  #   assert_equal "bob", @stat_tracker.list_of_coaches_by_season("20122013")
+  # end
+  #
+  def test_winningest_coach
+
+    # assert_equal [], @stat_tracker.winningest_coach("20122013")
+  end
+
 end
