41b5548b94f28bb9b356ec2a7b2690735c6928de
Kevin Cuadros <kev040798@gmail.com>
2020-09-08 20:35:20 -0400

    Create lowest visitor goals test and helper method test

--- test/league_stats_test.rb
+++ test/league_stats_test.rb
@@ -63,11 +63,19 @@ class LeagueStatisticsTest < Minitest::Test
     assert_equal 32, @league_stats.team_id_and_average_away_goals.count
   end
 
-  def test_team_away_goals
-    assert_equal 6, @league_stats.team_away_goals
+  def test_team_highest_away_goals
+    assert_equal 6, @league_stats.team_highest_away_goals
   end
 
   def test_highest_scoring_visitor
     assert_equal 'FC Dallas', @league_stats.highest_scoring_visitor
+  end
+
+  def test_team_lowest_away_goals
+    assert_equal 27, @league_stats.team_lowest_away_goals
+  end
+
+  def test_lowest_scoring_visitor
+    assert_equal 'San Jose Earthquakes', @league_stats.lowest_scoring_visitor
   end
 end
