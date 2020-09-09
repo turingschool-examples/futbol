e5629d7040f20aa29198b2c9562b61a362eb501b
Kate Tester <katemorris@gmail.com>
2020-09-09 07:53:01 -0600

    Adds assert instance test, passes.

--- test/season_statistics_test.rb
+++ test/season_statistics_test.rb
@@ -5,5 +5,23 @@ require './lib/game_statistics'
 require './lib/season_statistics'
 
 class SeasonStatisticsTest < Minitest::Test
+  def setup
+    game_path = './data/dummy_game.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/dummy_game_teams.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    @stat_tracker = StatTracker.from_csv(locations)
+    @season_statistics = SeasonStatistics.new(@stat_tracker)
+  end
+
+  def test_it_exists
+    assert_instance_of SeasonStatistics, @season_statistics
+  end
 
 end
