cfef74f73c9a9eba07952999b456dc236024c70b
George Soderholm <georgesoderholm@gmail.com>
2020-09-09 15:14:08 -0600

    Extract a test class for league_statistics from game_statistics

---
+++ test/league_statistics_test.rb
@@ -0,0 +1,43 @@
+require "minitest/autorun"
+require "minitest/pride"
+require './lib/stat_tracker'
+require "./lib/game_statistics"
+require "./lib/league_statistics"
+require "pry";
+
+class LeagueStatisticsTest < Minitest::Test
+  def setup
+    game_path = './data/dummy_game.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/dummy_game_teams.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    @stat_tracker = StatTracker.from_csv(locations)
+    @league_statistics = LeagueStatistics.new(@stat_tracker)
+  end
+
+  def test_it_exists
+
+    assert_instance_of LeagueStatistics, @league_statistics
+  end
+
+  def test_it_can_count_teams
+    # skip
+    assert_equal 32, @league_statistics.count_of_teams
+  end
+
+  def test_it_can_find_best_offense
+  # skip
+    assert_equal "FC Dallas", @league_statistics.best_offense
+  end
+
+  def test_it_can_find_worst_offense
+
+    assert_equal "FC Dallas", @league_statistics.worst_offense
+  end
+end
