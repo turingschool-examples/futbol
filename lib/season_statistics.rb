fb619c7c73c5e97f21f4efab92eefdf25ccc0358
Kate Tester <katemorris@gmail.com>
2020-09-09 19:20:07 -0600

    Went back to old way and figured it out!

--- lib/season_statistics.rb
+++ lib/season_statistics.rb
@@ -1,15 +1,12 @@
 class SeasonStatistics
   attr_reader :stat_tracker
 
-  def initialize(data)
-    @game_id = row[:game_id]
-    @season_id = ""
-    @team_id = row[:team_id]
-    @result = row[:result]
-    @head_coach = row[:head_coach]
-    @goals = row[:goals]
-    @shots = row[:shots]
-    @tackles = row[:tackles]
+  def initialize(stat_tracker)
+    @stat_tracker = stat_tracker
+  end
+
+  def games_teams_data(column)
+    @stat_tracker[:game_teams][column]
   end
 
   def game_id_to_season_id
@@ -23,83 +20,45 @@ class SeasonStatistics
     game_id_to_season_id.zip(games_teams_data("head_coach"),games_teams_data("result"))
   end
 
-  def grouped_by_season
-    head_coaches_and_result_data_set.group_by(&:first)
+  def grouped_by_season(data_set)
+    data_set.group_by(&:first)
   end
 
-  def starter_hash_for_coach
-    starter = {}
-    grouped_by_season.each do |season_id, data|
-      starter[season_id] = data.map do |full_data|
-        full_data.last(2)
-      end.to_h
-    end
-    starter
-  end
-
-  def game_result_hash
+  def game_result_hash(data_set,season_id)
     game_results = {}
-      array[1] = game_results
-      data.each do |array|
-        result = array[2]
-        if game_results[result.downcase].nil? && game_results["total_games"].nil?
-          game_results[result.downcase] = 1
-          game_results["total_games"] = 1
-        elsif game_results[result.downcase].nil?
-          game_results[result.downcase] = 1
-          game_results["total_games"] += 1
+    grouped_by_season(data_set)[season_id].each do |array|
+      result = array[2].downcase.to_sym
+      if game_results[array[1]].nil?
+        game_results[array[1]] = {:total => 1, result => 1}
+      else
+        if game_results.fetch(array[1])[result].nil?
+          game_results.fetch(array[1])[result] = 1
+          game_results.fetch(array[1])[:total] += 1
         else
-          game_results[result.downcase] += 1
-          game_results["total_games"] += 1
+          game_results.fetch(array[1])[result] += 1
+          game_results.fetch(array[1])[:total] += 1
         end
       end
     end
-
-
+    game_results
   end
 
-  def coaches_results_by_season_data
-    final_hash = starter_hash_for_coach
-    grouped_by_season.each do |season_id, data|
-      data.each do |array|
-        game_results = {}
-        final_hash[season_id][array[1]] =
-      end
-    end
-
-    final_hash
-  end
-
-  #data set for coaches
-  # games_teams join game_id, season conversion, head_coach, result
-  #List of coaches (games_teams)
-  #Find win percentage by counting number of games they had and dividing that
-  # with the number of those that are wins
-  # Dataset is hash with season as key, value is another hash of coach as key
-  # values as hash of wins key, losses key, total games key
-  # data_set = {
-    # "20122013" => {
-    #    "John Tortorella" => {wins: 22, losses: 27, ties: 11, total: 60},
-    #    "Claude Julien" => {wins: 23, losses: 25, ties: 12, total: 60}
-    #    }
-    # "20132014" => {
-    #    "John Tortorella" => {wins: 22, losses: 27, ties: 11, total: 60},
-    #    "Claude Julien" => {wins: 23, losses: 25, ties: 12, total: 60}
-    #    }
-    #}
-
-
-  def winningest_coach(season)
+  def winningest_coach(season_id)
     # winningest_coach 	Name of the Coach with the best win percentage for the season 	String
-    coach_hash = coaches_results_by_season_data[season].max_by do |coach, data|
-      data[:wins] / data[:total]
+    compare = {}
+    game_result_hash(head_coaches_and_result_data_set,season_id).each do |coach, data|
+      compare[coach] = ((data[:win].to_f / data[:total])*100)
     end
-    coach_hash.last
+    compare.max_by { |coach, percent| percent }[0]
   end
 
-  def worst_coach(season)
+  def worst_coach(season_id)
     # worst_coach 	Name of the Coach with the worst win percentage for the season 	String
-
+    compare = {}
+    game_result_hash(head_coaches_and_result_data_set,season_id).each do |coach, data|
+      compare[coach] = ((data[:win].to_f / data[:total])*100)
+    end
+    compare.min_by { |coach, percent| percent }[0]
   end
 
   #data set for teams
