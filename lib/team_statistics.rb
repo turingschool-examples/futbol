f5a23b9909cb26938378c1c84c6998605b91e0c1
Cydnee Owens <cks410@yahoo.com>
2020-09-09 20:55:52 -0600

    Add average win percentage methods and tests

--- lib/team_statistics.rb
+++ lib/team_statistics.rb
@@ -41,29 +41,36 @@ class TeamStatistics
 #     (count_of_total_wins.to_s / stat_tracker[:games]["game_id"].count).round(2)
 #   end
   def season_data_set
-    @stat_tracker[:game]["season"].zip(@stat_tracker[:game]["away_goals"], @stat_tracker[:game]["home_team_id"], @stat_tracker[:game]["away_goals"], @stat_tracker[:game]["home_goals"])
+    @stat_tracker[:games]["season"].zip(@stat_tracker[:games]["away_goals"], @stat_tracker[:games]["home_team_id"], @stat_tracker[:games]["away_goals"], @stat_tracker[:games]["home_goals"])
   end
 
-  # def same_season
-  #
-  # end
-
-  def best_season(team_id)
-    highest_wins_per_season = {}
-
-    season_data_set.find_all do |season|
-      # season[0] == season[0]
-      season[1] == team_id && season[2] == "WIN"
-    end.count
-    # returns [["2012030221", "6", "WIN"], ["2016030222", "6", "WIN"]]
-    season_data_set.map do |season|
-      if season[0].eql?(season[0])
-        season.find_all do |team|
-          team[1] == team_id && team[2] == "WIN"
-        end
-      end
-    end
-    binding.pry
+#   def find_season
+#     season_data_set.map do |season
+#   end
+#
+#   def best_season(team_id)
+#     season_data = {}
+#     titles = ["season", "away_team_id", "home_team_id", "away_goals", "home_goals"]
+#
+#     titles.each_with_index do |title, index|
+# binding.pry
+#       season_data[title] = season_data_set[index]
+#     end
+    # highest_wins_per_season = {}
+    #
+    # season_data_set.find_all do |season|
+    #   # season[0] == season[0]
+    #   season[1] == team_id && season[2] == "WIN"
+    # end.count
+    # # returns [["2012030221", "6", "WIN"], ["2016030222", "6", "WIN"]]
+    # season_data_set.map do |season|
+    #   if season[0].eql?(season[0])
+    #     season.find_all do |team|
+    #       team[1] == team_id && team[2] == "WIN"
+    #     end
+    #   end
+    # end
+    # binding.pry
   #   seasons.map do |season|
   #     if highest_wins_per_season[season[0]].nil?
   #        highest_wins_per_season[season[0]] = (season[1][0].to_f + season[1][1].to_f)
@@ -72,15 +79,24 @@ class TeamStatistics
   #     end
   #   end
   #   highest_wins_per_season
+
+  def season_win_data_set
+    @stat_tracker[:game_teams]["game_id"].zip(@stat_tracker[:game_teams]["team_id"], @stat_tracker[:game_teams]["result"])
   end
 
-  # def season_win_data_set
-  #   @stat_tracker[:game_teams]["game_id"].zip(@stat_tracker[:game_teams]["team_id"], @stat_tracker[:game_teams]["result"])
-  # end
-  #
-  # def average_win_percentage
-  #   season_data_set.find_all do |team|
-  #     team[1] == team_id && team[2] == "WIN"
-  #
-  # end
+  def total_games(team_id)
+    season_win_data_set.find_all do |team|
+      team[1] == team_id
+    end.count.to_f
+  end
+
+  def winning_games(team_id)
+    season_win_data_set.find_all do |team|
+      team[1] == team_id && team[2] == "WIN"
+    end.count.to_f
+  end
+
+  def average_win_percentage(team_id)
+    "#{(winning_games(team_id)/total_games(team_id) * 100).round(2)}"
+  end
 end
