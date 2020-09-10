a7edaf95d7332955b19daaf545dbe5e1c68d698e
Sage Gonzales <sagegonzales15@gmail.com>
2020-09-10 17:53:35 -0600

    creates game manager class

---
+++ lib/game_manager.rb
@@ -0,0 +1,15 @@
+class GameManager
+  attr_reader :games, :tracker #do we need attr_reader?
+  def initialize(path, tracker)
+    @games = []
+    @tracker = tracker
+    create_games(path)
+  end
+
+  def create_games(path)
+    games_data = CSV.read(path, headers:true) #may need to change .read to .load
+    @games = games_data.map do |data|
+      Game.new(data, self)
+    end
+  end
+end
