33b5030d82d1f15b2b35066815262b5007dcaad9
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-10 17:28:30 -0600

    Add find all seasons and coaches per season tests

--- test/season_statistics_test.rb
+++ test/season_statistics_test.rb
@@ -1,4 +1,5 @@
 require_relative 'test_helper'
+require 'mocha/minitest'
 
 class SeasonStatistcsTest < Minitest::Test
 
@@ -15,29 +16,19 @@ class SeasonStatistcsTest < Minitest::Test
     @stat = StatTracker.new(@locations)
 
   end
-#
+
   def test_it_exists
     @stat.coaches_per_season(@stat.find_all_seasons)
   end
-#
-#   def test_map_season_to_game_ids
-#     season_statistics = SeasonStatistics.new(@stat)
-#
-#     assert_equal 7441, season_statistics.map_season_to_game_ids.count
-#   end
-#
-#   def test_find_all_seasons
-#     season_statistics = SeasonStatistics.new(@stat)
-#
-#     assert_equal ["20122013", "20162017", "20142015", "20152016", "20132014", "20172018"], season_statistics.find_all_seasons
-#   end
-#
-#   def test_coach_game_results
-#     season_statistics = SeasonStatistics.new(@stat)
-#
-#     assert_equal 6, season_statistics.coach_game_results.count
-#   end
-#
+
+  def test_find_all_seasons
+    assert_equal ["20122013", "20162017", "20142015", "20152016", "20132014", "20172018"], @stat.find_all_seasons
+  end
+
+  def test_coaches_per_season
+    assert @stat.coaches_per_season(@stat.find_all_seasons)["20132014"].include?("Claude Julien")
+  end
+
   def test_winningest_coach
     assert_equal "Claude Julien", @stat.winningest_coach("20132014")
     assert_equal "Alain Vigneault", @stat.winningest_coach("20142015")
@@ -49,8 +40,6 @@ class SeasonStatistcsTest < Minitest::Test
   end
 #
 #   def test_most_accurate_team
-#     season_statistics = SeasonStatistics.new(@stat)
-#
 #     assert_equal "Real Salt Lake", season_statistics.get_team_name_from_game_id
 #
 #     assert_equal "Real Salt Lake", season_statistics.most_accurate_team("20132014")
