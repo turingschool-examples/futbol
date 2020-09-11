a8115531c6e50fc211133dfd5c4be2e166076949
Michael Foy <foymikek@gmail.com>
2020-09-11 17:23:02 -0600

    Add passing method count_of_teams

--- lib/league_statistics.rb
+++ lib/league_statistics.rb
@@ -1,3 +1,7 @@
-class LeagueStatistics
-  
+module LeagueStatistics
+
+  def count_of_teams
+    @team_table.length
+  end
+
 end
--- test/league_statistics_test.rb
+++ test/league_statistics_test.rb
@@ -4,8 +4,8 @@ class LeagueStatisticsTest < Minitest::Test
 
   def setup
     game_path = './data/games.csv'
-    @stat_tracker ||= StatTracker.from_csv({games: game_path})
-    @stat_tracker ||= StatTracker.from_csv({teams: game_path})
+    team_path = './data/teams.csv'
+    @stat_tracker ||= StatTracker.from_csv({games: game_path, teams: team_path})
   end
 
   def test_count_of_teams
