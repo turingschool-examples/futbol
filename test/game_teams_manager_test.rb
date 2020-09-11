5e68fafe5f88199ddbeccd780b14329c4743d8ae
Sage Gonzales <sagegonzales15@gmail.com>
2020-09-10 18:48:47 -0600

    adds game teams manager test class

---
+++ test/game_teams_manager_test.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'mocha/minitest'
+require './lib/stat_tracker'
+require './lib/game_teams_manager'
+
+class GameTeamsMangerTest < Minitest::Test
+
+end
