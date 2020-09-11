1edc96ffd6c52f014f517155aa955d7f388f7955
Nick King <nickmaxking@gmail.com>
2020-09-11 09:54:04 -0600

    Add initialize, create game teams, and attr_reader methods

--- lib/game_team_manager.rb
+++ lib/game_team_manager.rb
@@ -1,1 +1,18 @@
+require 'csv'
 
+class GameTeamManager
+  attr_reader :game_teams,
+              :tracker
+  def initialize(path, tracker)
+    @game_teams = []
+    create_underscore_game_teams(path)
+    @tracker = tracker
+  end
+
+  def create_underscore_game_teams(path)
+    game_teams_data = CSV.read(path, headers: true)
+    @game_teams = game_teams_data.map do |data|
+      GameTeam.new(data, self)
+    end
+  end
+end
