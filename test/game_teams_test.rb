5ded12fab9afc2259143148911946201c10ae800
Sage Gonzales <sagegonzales15@gmail.com>
2020-09-10 18:55:32 -0600

    adds game teams test class

---
+++ test/game_teams_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'mocha/minitest'
+require './lib/stat_tracker'
+require './lib/game_teams_manager'
+require './lib/game_teams'
+
+class GameTeamsTest < Minitest::Test
+
+end
