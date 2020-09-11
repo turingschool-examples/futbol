0ecbaec2003e46c15af61232214e3949021be231
Brian Liu <brian.b.liu@gmail.com>
2020-09-10 21:36:52 -0600

    Add test that game team class has attributes.

--- test/game_team_test.rb
+++ test/game_team_test.rb
@@ -11,4 +11,19 @@ class GameTeamTest < Minitest::Test
     assert_instance_of GameTeam, game_team
     assert_instance_of CSV::Table, data
   end
+
+  def test_it_has_attributes
+    data = CSV.read('./data/game_teams_dummy.csv', headers:true)
+    game_team = GameTeam.new(data[0], "manager")
+
+    assert_equal "2012030221", game_team.game_id
+    assert_equal "3", game_team.team_id
+    assert_equal "away", game_team.home_or_away
+    assert_equal "LOSS", game_team.result
+    assert_equal "OT", game_team.settled_in
+    assert_equal "John Tortorella", game_team.head_coach
+    assert_equal "2", game_team.goals
+    assert_equal "8", game_team.shots
+    assert_equal "44", game_team.tackles
+  end
 end
