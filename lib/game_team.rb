148b06bf3f4fdbf5a4f9edc1aa9a0d17f2ea1507
Brian Liu <brian.b.liu@gmail.com>
2020-09-10 21:39:49 -0600

    Add attr_reader, pass test it has attributes.

--- lib/game_team.rb
+++ lib/game_team.rb
@@ -1,6 +1,23 @@
 require 'csv'
 
 class GameTeam
+  attr_reader :manager,
+              :game_id,
+              :team_id,
+              :home_or_away,
+              :result,
+              :settled_in,
+              :head_coach,
+              :goals,
+              :shots,
+              :tackles,
+              :penalties_in_minutes,
+              :power_play_opportunities,
+              :power_play_goals,
+              :face_off_win_percentage,
+              :giveaways,
+              :takeaways
+
   def initialize(data, manager)
     @manager = manager
     @game_id = data['game_id']
