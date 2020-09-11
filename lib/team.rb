5ab91da55d095b48590cf0992e5ea7a3b31e33f8
Brian Liu <brian.b.liu@gmail.com>
2020-09-10 20:10:40 -0600

    Pass test it exists for team test , initialize team class.

---
+++ lib/team.rb
@@ -0,0 +1,14 @@
+require 'csv'
+
+class Team
+
+  def initialize(data, manager)
+    @manager = manager
+    @team_id = data["team_id"]
+    @franchise_id = data["franchiseId"]
+    @team_name = data["teamName"]
+    @abbreviation = data["abbreviation"]
+    @stadium = data["Stadium"]
+    @link = data["link"]
+  end
+end
--- test/team_test.rb
+++ test/team_test.rb
@@ -1,13 +1,18 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'Pry'
+#require './data/teams_dummy'
 require './lib/team'
 
 class TeamTest < Minitest::Test
   def test_it_exists
-    team = Team.new
+
+    data = CSV.read('./data/teams_dummy.csv', headers:true)
+    team = Team.new(data[0], "manager")
+    binding.pry
     assert_instance_of Team, team
+    assert_instance_of CSV::Table, data
   end
 
-  
+
 end
