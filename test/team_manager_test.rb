dd4cc67d602f86b81f3e5d07bc1b4915021381c5
Sage Gonzales <sagegonzales15@gmail.com>
2020-09-10 18:59:49 -0600

    Add team manager test class

---
+++ test/team_manager_test.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'mocha/minitest'
+require './lib/stat_tracker'
+require './lib/team_manager'
+
+class TeamManagerTest < Minitest::Test
+
+end
