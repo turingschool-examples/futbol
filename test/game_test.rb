b51d7e3216bcf0a7554e9956e6f1651d5637c9f5
Sage Gonzales <sagegonzales15@gmail.com>
2020-09-10 18:57:00 -0600

    adds game test class

---
+++ test/game_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'mocha/minitest'
+require './lib/stat_tracker'
+require './lib/game_manager'
+require './lib/game'
+
+class GameTest < Minitest::Test
+
+end
