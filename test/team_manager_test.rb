64d5d744def82e4771bb26f07b54b3769bf876ae
Nick King <nickmaxking@gmail.com>
2020-09-11 09:33:40 -0600

    Add create underscore teams method

--- test/team_manager_test.rb
+++ test/team_manager_test.rb
@@ -6,11 +6,15 @@ require './lib/team_manager'
 class TeamManagerTest < MiniTest::Test
   def test_it_exists
     team_path = './data/teams_dummy.csv'
-    team_manager = TeamManager.new("path", "tracker")
+    team_manager = TeamManager.new(team_path, "tracker")
 
     assert_instance_of TeamManager, team_manager
   end
 
   def test_create_underscore_teams
+    team_path = './data/teams_dummy.csv'
+    team_manager = TeamManager.new(team_path, "tracker")
+
+    assert_includes Team, team_manager.teams
   end
 end
