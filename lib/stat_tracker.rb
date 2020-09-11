f1fcaae1283615194b2bde319d34159d75deb69b
Nick King <nickmaxking@gmail.com>
2020-09-11 12:36:06 -0600

    Add manager creation, change indentation

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -5,7 +5,7 @@ class StatTracker
               :teams,
               :game_teams
 
-  def initialize(games, teams, game_teams)
+  def initialize(games, teams, game_teams, team_manager)
     @games = games
     @teams = teams
     @game_teams = game_teams
@@ -15,6 +15,9 @@ class StatTracker
     games = CSV.read(locations[:games], headers:true)
     teams = CSV.read(locations[:teams], headers:true)
     game_teams = CSV.read(locations[:game_teams], headers:true)
+    @game_manager = GameManager.new(locations[:games], self)
+    @game_team_manager = GameTeamManager.new(locations[:game_teams], self)
+    @team_manager = TeamManager.new(locations[:teams], self)
 
     new(games, teams, game_teams)
   end
@@ -455,9 +458,4 @@ class StatTracker
     end
     biggest_winner_name["teamName"]
   end
-
-
-
-
-
-  end
+end
