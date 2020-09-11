695b0908e3d373612ec60b0bd60ea02f65a6071b
Zach Stearns <zach.stearns92@gmail.com>
2020-09-11 17:49:01 -0600

    Add helper method for find_game_ids_for_season

--- lib/game_manager.rb
+++ lib/game_manager.rb
@@ -17,18 +17,18 @@ class GameManager
 
   #------------SeasonStats
 
-  def game_team_results_by_season(season)
-require "pry"; binding.pry
-    game_ids_in_season = games_of_season.map do |game|
-      game['game_id']
-    end
-    # games_results_per_season = game_teams.find_all do |team_result|
-    #   game_ids_in_season.include? team_result['game_id']
-    # end
-    # games_results_per_season
-  end
+  # def game_team_results_by_season(season)
+  #   games_results_per_season = game_teams.find_all do |team_result|
+  #     game_ids_in_season.include? team_result['game_id']
+  #   end
+  #   games_results_per_season
+  # end
 
   def games_of_season(season)
     @games.find_all {|game| game.season == season}
+  end
+
+  def find_game_ids_for_season(season)
+    games_of_season(season).map {|game| game.game_id }
   end
 end
