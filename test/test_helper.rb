dd96639c15a44a572a744cfae1534ee0a1702aae
Nick King <nickmaxking@gmail.com>
2020-09-11 16:14:24 -0600

    Move requires for lib files, pry, simplecov and minitest

---
+++ test/test_helper.rb
@@ -0,0 +1,12 @@
+require 'simplecov'
+SimpleCov.start
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'Pry'
+require './lib/stat_tracker'
+require './lib/team_manager'
+require './lib/game_team_manager'
+require './lib/game_manager'
+require './lib/team'
+require './lib/game'
+require './lib/game_team'
