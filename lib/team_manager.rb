307ae1dfce169851b124aadd262dac64d1de63f0
George Soderholm <georgesoderholm@gmail.com>
2020-09-11 20:12:52 -0600

    Add access to the team class

--- lib/team_manager.rb
+++ lib/team_manager.rb
@@ -1,15 +1,16 @@
+require_relative './team'
 class TeamsManager
   attr_reader :teams, :tracker
   def initialize(teams_path, tracker)
     @teams = []
     @tracker = tracker
-    # create_games(teams_path)
+    create_games(teams_path)
   end
 
-  # def create_games(teams_path)
-  #   teams_data = CSV.parse(File.read(teams_path), headers: true)
-  #   @teams = teams_data.map do |data|
-  #     Team.new(data, self)
-  #   end
-  # end
+  def create_games(teams_path)
+    teams_data = CSV.parse(File.read(teams_path), headers: true)
+    @teams = teams_data.map do |data|
+      Team.new(data, self)
+    end
+  end
 end
