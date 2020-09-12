0759a1326943683064a44f3c115ae08c9281c381
Jake Heft <jakeheft@gmail.com>
2020-09-11 19:20:29 -0600

    WIP - Refactor highest scoring visitor

--- lib/game_teams_manager.rb
+++ lib/game_teams_manager.rb
@@ -18,4 +18,44 @@ class GameTeamsManager
     end
   end
 
+  def avg_score(filtered_game_teams = @game_teams)
+    ratio(total_score(filtered_game_teams), total_game_teams(filtered_game_teams))
+  end
+
+  def total_game_teams(filtered_game_teams = @game_teams)
+    filtered_game_teams.count
+  end
+
+  def away_games_by_team
+    away_games.group_by do |game_team|
+      game_team.team_id
+    end
+  end
+
+  # USE THIS OR INDIVIDUAL ONES?
+  def home_or_away_games(where = "home")
+    @game_teams.select do |game|
+      game.hoa == where
+    end
+  end
+
+  def away_games
+    @game_teams.select do |game_team|
+      game_team.hoa == "away"
+    end
+  end
+
+  def home_games_by_team
+    home_games.group_by do |game_team|
+      game_team.team_id
+    end
+  end
+
+  def highest_scoring_visitor
+    highest_scoring_visitor = away_games_by_team.max_by do |team_id, details|
+      avg_score(details)
+    end[0]
+    team_id_to_team_name(highest_scoring_visitor)
+  end
+
 end
