b37a1ae46500f14d5ba4210383c683c127f6d8f4
Joshua Carey <jdcarey128@gmail.com>
2020-09-12 14:06:03 -0600

    Update test format

--- test/game_team_test.rb
+++ test/game_team_test.rb
@@ -4,11 +4,33 @@ require "./test/test_helper"
 class GameTeamTest < Minitest::Test
 
   def setup
-    @stat_tracker = StatTracker.from_csv
+    data = {
+      game_id: "2014020006",
+      team_id: "1",
+      hoa: "away",
+      result: "WIN",
+      settled_in: "REG",
+      head_coach: "Peter DeBoer",
+      goals: 4,
+      shots: 6,
+      pim: 10,
+      ppg: 0,
+      ppo: 3,
+      fowp: 42.9,
+      tackles: 36,
+      giveaways: 4,
+      takeaways: 5,
+    }
+    @game_team_1 = GameTeam.new(data)
   end
 
   def test_it_exists
-    assert_instance_of GameTeam, @stat_tracker.game_teams_manager.game_teams.first
+    assert_instance_of GameTeam, @game_team_1
+  end
+
+  def test_it_knows_a_home_and_away_game
+    assert_equal false, @game_team_1.home_game?
+    assert @game_team_1.away_game?
   end
 
 end
