25d0f4fe9dff104d2de04134e3dc58d2f62b1daf
Joshua Carey <jdcarey128@gmail.com>
2020-09-12 14:06:47 -0600

    Update test format

--- test/team_test.rb
+++ test/team_test.rb
@@ -5,10 +5,16 @@ class TeamTest < Minitest::Test
 
   def setup
     @stat_tracker = StatTracker.from_csv
+    @team = {
+      team_id: "1",
+      franchise_id: "23",
+      team_name: "Atlanta United",
+      abbreviation: "ATL"
+    }
   end
 
   def test_it_exists
-    assert_instance_of Team, @stat_tracker.teams_manager.teams.first
+    assert_instance_of Team, @team
   end
 
 end
