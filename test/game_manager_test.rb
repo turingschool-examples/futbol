502dd9c7d3fed9d242856fb95aa87244d37601f1
George Soderholm <georgesoderholm@gmail.com>
2020-09-12 11:39:11 -0600

    Add three tests that check for home win, tie and visitor win percentages

--- test/game_manager_test.rb
+++ test/game_manager_test.rb
@@ -25,5 +25,16 @@ class GamesManagerTest < Minitest::Test
   def test_it_can_find_the_lowest_total_score
     assert_equal 3, @games_manager.lowest_total_score
   end
-  
+
+  def test_it_can_find_percentage_home_wins
+    assert_equal 50.0, @games_manager.percentage_home_wins
+  end
+
+  def test_it_can_find_percentage_visitor_wins
+    assert_equal 33.33, @games_manager.percentage_visitor_wins
+  end
+
+  def test_it_can_find_percentage_ties
+    assert_equal 16.67, @games_manager.percentage_ties
+  end
 end
