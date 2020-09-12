d59e1822657f66b0964cdb3ff60a4761d62b0a1c
Nick King <nickmaxking@gmail.com>
2020-09-12 14:50:19 -0600

    Add test for average win percentage

--- test/game_team_manager_test.rb
+++ test/game_team_manager_test.rb
@@ -5,19 +5,21 @@ require './lib/game_team'
 require './lib/game_team_manager'
 
 class GameTeamManagerTest < MiniTest::Test
-  def test_it_exists
+  def setup
     game_team_path = './data/game_teams_dummy.csv'
-    game_team_manager = GameTeamManager.new(game_team_path, "tracker")
-
-    assert_instance_of GameTeamManager, game_team_manager
+    @game_team_manager = GameTeamManager.new(game_team_path, "tracker")
+  end
+  def test_it_exists
+    assert_instance_of GameTeamManager, @game_team_manager
   end
 
   def test_create_underscore_game_teams
-    game_team_path = './data/game_teams_dummy.csv'
-    game_team_manager = GameTeamManager.new(game_team_path, "tracker")
-
-    game_team_manager.game_teams.each do |game_team|
+    @game_team_manager.game_teams.each do |game_team|
       assert_instance_of GameTeam, game_team
     end
+  end
+
+  def test_it_can_test_for_alltime_winning_pct
+    assert_equal 0.88, @game_team_manager.average_win_percentage("6")
   end
 end
