61577f8e4492e0b55e81fbac196242bc40d66a53
Curtis Bartell <cbartell980@gmail.com>
2020-09-12 17:14:15 -0600

    Add game_ids_by_team

--- lib/game_team_manager.rb
+++ lib/game_team_manager.rb
@@ -3,16 +3,22 @@ require 'csv'
 
 class GameTeamManager
   attr_reader :game_teams
-  def initialize(locations, stat_tracker)
+  def initialize(location, stat_tracker) # I need a test
     @stat_tracker = stat_tracker
-    @game_teams = generate_game_teams(locations[:game_teams])
+    @game_teams = generate_game_teams(location)
   end
 
-  def generate_game_teams(location)
+  def generate_game_teams(location) # I need a test
     array = []
     CSV.foreach(location, headers: true) do |row|
       array << GameTeams.new(row.to_hash)
     end
     array
+  end
+
+  def game_ids_by_team(id)
+    game_teams.select do |game_team|
+      game_team.team_id == id
+    end.map(&:game_id)
   end
 end
