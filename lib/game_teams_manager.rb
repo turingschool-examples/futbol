8f4a449fde5ba61f0dcbf978ed9f5d873e270ad4
George Soderholm <georgesoderholm@gmail.com>
2020-09-11 19:36:36 -0600

    Add a game_teams_manager class

---
+++ lib/game_teams_manager.rb
@@ -0,0 +1,15 @@
+class GameTeamsManager
+  attr_reader :game_teams, :tracker
+  def initialize(game_teams_path, tracker)
+    @game_teams = []
+    @tracker = tracker
+    # create_games(game_teams_path)
+  end
+
+  # def create_games(game_teams_path)
+  #   game_teams_data = CSV.parse(File.read(game_teams_path), headers: true)
+  #   @game_teams = game_teams_data.map do |data|
+  #     GameTeam.new(data, self)
+  #   end
+  # end
+end
