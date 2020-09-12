a9db76b2af4c311bbc36ad798e16a80bc3d59509
Michael Foy <foymikek@gmail.com>
2020-09-11 18:50:15 -0600

    Add passing method best_offense

--- test/league_statistics_test.rb
+++ test/league_statistics_test.rb
@@ -17,3 +17,12 @@ class LeagueStatisticsTest < Minitest::Test
   end
 
 end
+# best_offense	Name of the team with the highest average number of goals
+#scored per game across all seasons.	String
+
+
+# worst_offense	Name of the team with the lowest average number of goals scored per game across all seasons.	String
+# highest_scoring_visitor	Name of the team with the highest average score per game across all seasons when they are away.	String
+# highest_scoring_home_team	Name of the team with the highest average score per game across all seasons when they are home.	String
+# lowest_scoring_visitor	Name of the team with the lowest average score per game across all seasons when they are a visitor.	String
+# lowest_scoring_home_team	Name of the team with the lowest average score per game across all seasons when they are at home.	String
