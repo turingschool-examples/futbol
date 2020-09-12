49f92b80487340fbe81b6108fb62b1d39484597e
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-12 15:38:02 -0500

    Add test for create_team_stats_hash in StatTracker class

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -4,6 +4,24 @@ require 'mocha/minitest'
 require './lib/stat_tracker'
 
 class StatTrackerTest < Minitest::Test
+  def setup
+    @game_path = './fixture/games_dummy.csv'
+    @team_path = './fixture/teams_dummy.csv'
+    @game_teams_path = './fixture/game_teams_dummy.csv'
+
+    @locations = {
+      games: @game_path,
+      teams: @team_path,
+      game_teams: @game_teams_path
+    }
+
+    @stat_tracker = StatTracker.from_csv(@locations)
+  end
+
+  def test_it_exists
+    assert_instance_of StatTracker, @stat_tracker
+  end
+
   def test_find_team_results_by_season
     game_path = './fixture/game_blank.csv'
     team_path = './fixture/team_blank.csv'
@@ -26,5 +44,25 @@ class StatTrackerTest < Minitest::Test
     game_3.stubs(:game_id).returns('789')
 
     assert_equal ["123", "456"], tracker.find_game_ids_for_season('20122013')
+  end
+
+  def test_it_can_return_team_stats_hash
+    game_path = './fixture/game_blank.csv'
+    team_path = './fixture/team_blank.csv'
+    game_teams_path = './fixture/game_teams_blank.csv'
+
+    tracker = StatTracker.new(game_path, team_path, game_teams_path)
+
+    team = mock('Team Object')
+    tracker.team_manager.teams << team
+
+    team.stubs(:team_id).returns('45')
+
+    expected = {
+      '45' => { total_games: 0, total_goals: 0, away_games: 0, home_games: 0,
+                away_goals: 0, home_goals: 0 }
+    }
+
+    assert_equal expected, tracker.create_team_stats_hash
   end
 end
