f44f4e4bf46e593a1442878630190b2e9b50265e
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-12 18:33:23 -0500

    Update LeagueStatistics and Findable modules

--- lib/findable.rb
+++ lib/findable.rb
@@ -1,6 +1,6 @@
 module Findable
   def find_team_by_team_id(id)
-    team_manager.teams.find do |team|
+    tracker.team_manager.teams.find do |team|
       team.team_id == id
     end.team_name
   end
