f9dd382f10fe7b41efa221776519d506194894a6
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-12 12:39:58 -0600

    Add lowest scoring visitor method

--- lib/league_statistics.rb
+++ lib/league_statistics.rb
@@ -93,4 +93,22 @@ module LeagueStatistics
     team_best_home_average.key(team_best_home_average.values.max)
   end
 
+  def lowest_scoring_visitor
+    team_best_away_average = {}
+    team_name_ids.each do |team_name, team_id|
+      game_count = 0
+      @game_table.each do |game_id, game|
+        if team_id.to_i == game.away_team_id && team_best_away_average[team_name].nil?
+           team_best_away_average[team_name] = game.away_goals
+           game_count += 1
+        elsif team_id.to_i == game.away_team_id
+          team_best_away_average[team_name] += game.away_goals
+          game_count += 1
+        end
+      end
+      team_best_away_average[team_name] = team_best_away_average[team_name]/game_count.to_f
+    end
+    team_best_away_average.key(team_best_away_average.values.min)
+  end
+
 end
