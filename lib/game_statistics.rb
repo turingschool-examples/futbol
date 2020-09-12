6dcce5aa191dc0537e5ad988674e65e21da5f79d
George Soderholm <georgesoderholm@gmail.com>
2020-09-12 13:01:55 -0600

    Edit out the methods that were used in alternative class

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -44,56 +44,56 @@ class GameStatistics
   #   lowest_game[1]
   # end
 
-  def home_away_data_set
-    stat_tracker[:games]["home_goals"].zip(stat_tracker[:games]["away_goals"])
-  end
+  # def home_away_data_set
+  #   stat_tracker[:games]["home_goals"].zip(stat_tracker[:games]["away_goals"])
+  # end
 
-  def percentage_home_wins
-    (count_of_result("home").to_f / winners.length * 100).round(2)
-  end
+  # def percentage_home_wins
+  #   (count_of_result("home").to_f / winners.length * 100).round(2)
+  # end
 
-  def winners
-    home_away_data_set.map do |pair|
-      if pair[0].to_i - pair[1].to_i > 0
-        "home"
-      elsif pair[0].to_i - pair[1].to_i < 0
-        "away"
-      else
-        "tie"
-      end
-    end
-  end
+  # def winners
+  #   home_away_data_set.map do |pair|
+  #     if pair[0].to_i - pair[1].to_i > 0
+  #       "home"
+  #     elsif pair[0].to_i - pair[1].to_i < 0
+  #       "away"
+  #     else
+  #       "tie"
+  #     end
+  #   end
+  # end
 
-  def count_of_result(result)
-    winners.count do |winner|
-      winner == result
-    end
-  end
+  # def count_of_result(result)
+  #   winners.count do |winner|
+  #     winner == result
+  #   end
+  # end
 
-  def percentage_visitor_wins
-    (count_of_result("away").to_f / winners.length * 100).round(2)
-  end
+  # def percentage_visitor_wins
+  #   (count_of_result("away").to_f / winners.length * 100).round(2)
+  # end
 
-  def percentage_ties
-    (count_of_result("tie").to_f / winners.length * 100).round(2)
-  end
+  # def percentage_ties
+  #   (count_of_result("tie").to_f / winners.length * 100).round(2)
+  # end
 
-  def count_of_games_by_season
-    games_per_season = stat_tracker[:games]["season"]
-    seasonal_counts = {}
-    games_per_season.each do |season|
-      if seasonal_counts[season]
-        seasonal_counts[season] += 1
-      else
-        seasonal_counts[season] = 1
-      end
-    end
-    seasonal_counts
-  end
+  # def count_of_games_by_season
+  #   games_per_season = stat_tracker[:games]["season"]
+  #   seasonal_counts = {}
+  #   games_per_season.each do |season|
+  #     if seasonal_counts[season]
+  #       seasonal_counts[season] += 1
+  #     else
+  #       seasonal_counts[season] = 1
+  #     end
+  #   end
+  #   seasonal_counts
+  # end
 
-  def goals_per_game_data_set
-    stat_tracker[:games]["game_id"].zip(home_away_data_set)
-  end
+  # def goals_per_game_data_set
+  #   stat_tracker[:games]["game_id"].zip(home_away_data_set)
+  # end
 
   def total_goals_per_game
     goals_per_game_data_set.map do |game|
