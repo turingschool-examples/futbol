f67cf3fbd190b6477ac5d047d3fd74e4a716e4fb
Jose Lopez <joselopez11394@gmail.com>
2020-09-12 00:01:36 -0400

    adds test for fewest goals scored

--- test/team_statistics_test.rb
+++ test/team_statistics_test.rb
@@ -38,4 +38,8 @@ class TeamStatisticsTest < Minitest::Test
   def test_most_goals_scored
     assert_equal 7, @stat_tracker.most_goals_scored("18")
   end
+
+  def test_fewest_goals_scored
+     assert_equal 0, @stat_tracker.fewest_goals_scored("18")
+  end
 end
