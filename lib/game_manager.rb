8a8adefab8d12bf4ad9f430948363ed6bd6bbe3a
Eugene Theriault <hybridbassist@gmail.com>
2020-09-12 12:59:25 -0700

    Create highest_total_score method in game_manager

--- lib/game_manager.rb
+++ lib/game_manager.rb
@@ -15,4 +15,11 @@ class GameManager
       Game.new(data, self)
     end
   end
+  
+  def highest_total_score
+    highest_score = @games.max_by do |game|
+      game.away_goals.to_i + game.home_goals.to_i
+    end
+    highest_score.away_goals.to_i + highest_score.home_goals.to_i
+  end
 end
