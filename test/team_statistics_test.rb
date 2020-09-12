829a729d38a1cbf81ecc2f7c2e4cdce29aa6ae71
Jose Lopez <joselopez11394@gmail.com>
2020-09-11 23:47:35 -0400

    adds test for most goals scored

--- test/team_statistics_test.rb
+++ test/team_statistics_test.rb
@@ -34,4 +34,8 @@ class TeamStatisticsTest < Minitest::Test
   def test_average_win_percentage
     assert_equal  0.49, @stat_tracker.average_win_percentage("6")
   end
+
+  def test_most_goals_scored
+    assert_equal 7, @stat_tracker.most_goals_scored("18")
+  end
 end
