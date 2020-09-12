903a124481dc9af928cb71f4b0da8da11ce2acaa
Eugene Theriault <hybridbassist@gmail.com>
2020-09-12 12:49:20 -0700

    Create code for team_info method in team_manager

--- lib/team_manager.rb
+++ lib/team_manager.rb
@@ -19,4 +19,17 @@ class TeamManager
   def count_of_teams
     @teams.count
   end
+  
+  def team_info(team_id)
+    found_team = @teams.find do |team|
+      team_id == team.team_id
+    end
+    found_team_info = {
+                      "team_id" => found_team.team_id,
+                      "franchise_id" => found_team.franchise_id,
+                      "team_name" => found_team.team_name,
+                      "abbreviation" => found_team.abbreviation,
+                      "link" => found_team.link
+                      }
+  end
 end
