0310bfb86c1c37ecd2c885ac213509f41cff50ed
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-12 16:15:59 -0500

    Add team_stats method to GameManager class

--- lib/game_manager.rb
+++ lib/game_manager.rb
@@ -28,11 +28,11 @@ class GameManager
   def team_stats
     tracker.create_team_stats_hash.each do |team_id, games_goals|
       games.each do |game|
-        if team_id == game['away_team_id'] || team_id == game['home_team_id']
-          games_goals[:away_games] += 1 if team_id == game['away_team_id']
-          games_goals[:home_games] += 1 if team_id == game['home_team_id']
-          games_goals[:away_goals] += game['away_goals'].to_i if team_id == game['away_team_id']
-          games_goals[:home_goals] += game['home_goals'].to_i if team_id == game['home_team_id']
+        if team_id == game.away_team_id || team_id == game.home_team_id
+          games_goals[:away_games] += 1 if team_id == game.away_team_id
+          games_goals[:home_games] += 1 if team_id == game.home_team_id
+          games_goals[:away_goals] += game.away_goals.to_i if team_id == game.away_team_id
+          games_goals[:home_goals] += game.home_goals.to_i if team_id == game.home_team_id
         end
       end
       games_goals[:total_games] = games_goals[:away_games] + games_goals[:home_games]
