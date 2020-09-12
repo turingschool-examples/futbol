e0e4c8709fb0d62067bf205fafd27b372d582bb3
George Soderholm <georgesoderholm@gmail.com>
2020-09-11 20:11:10 -0600

    Add require_relative of game class

--- lib/game_manager.rb
+++ lib/game_manager.rb
@@ -1,15 +1,17 @@
+require_relative './game'
+
 class GamesManager
   attr_reader :games, :tracker
   def initialize(games_path, tracker)
     @games = []
     @tracker = tracker
-    # create_games(games_path)
+    create_games(games_path)
   end
 
-  # def create_games(games_path)
-  #   games_data = CSV.parse(File.read(games_path), headers: true)
-  #   @games = games_data.map do |data|
-  #     Game.new(data, self)
-  #   end
-  # end
+  def create_games(games_path)
+    games_data = CSV.parse(File.read(games_path), headers: true)
+    @games = games_data.map do |data|
+      Game.new(data, self)
+    end
+  end
 end
