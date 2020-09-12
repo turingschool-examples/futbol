45a231a49c7e1286acdd82c3c7a83ffa1611cf6b
Eugene Theriault <hybridbassist@gmail.com>
2020-09-12 13:53:50 -0700

    Rename tests for clarity and raking

--- test/game_manager_test.rb
+++ test/game_manager_test.rb
@@ -20,19 +20,23 @@ class GameManagerTest < MiniTest::Test
     end
   end
 
-  def test_it_can_calculate_highest_total_score
+  def test_game_manager_can_calculate_highest_total_score
     assert_equal 7, @game_manager.highest_total_score
   end
 
-  def test_it_can_calculate_lowest_total_score
+  def test_game_manager_can_calculate_lowest_total_score
     assert_equal 1, @game_manager.lowest_total_score
   end
 
-  def test_it_can_calculate_percentage_home_wins
+  def test_game_manager_can_calculate_percentage_home_wins
    assert_equal 0.53, @game_manager.percentage_home_wins
   end
 
-  def test_it_can_calculate_percentage_visitor_wins
+  def test_game_manager_can_calculate_percentage_visitor_wins
     assert_equal 0.41, @game_manager.percentage_visitor_wins
+  end
+    
+  def test_game_manager_can_calculate_percentage_ties
+    assert_equal 0.06, @game_manager.percentage_ties
   end
 end
