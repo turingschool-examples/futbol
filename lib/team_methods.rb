a19254cd72d63da52bd8a73d75c40bfa78d787c6
John Kim <abcdefghijohnk@gmail.com>
2020-09-13 11:56:17 -0600

    Fix tests

--- lib/team_methods.rb
+++ lib/team_methods.rb
@@ -1,36 +1,11 @@
-
-
 require 'CSV'
 
 class TeamMethods
-  attr_reader :file_loc,:table
-
-  def initialize(file_loc)
-    @file_loc = file_loc
-    @table = create_table
-    @team_id = @table['team_id']
-    @franchiseId = @table['franchiseId']
-    @teamName = @table['teamName']
-    @abbreviation = @table['abbreviation']
-    @Stadium = @table['Stadium']
-    @link = @table['link']
-  end
-
-  def create_table
-    CSV.parse(File.read(@file_loc), headers: true)
-  end
-
-  def team_info
-    require 'pry'; binding.pry
-  end
-end
-require 'CSV'
-class TeamMethods
-  attr_reader :teams_table, :teams
+  attr_reader :teams_array, :teams
 
   def initialize(teams)
     @teams = teams
-    @teams_table = create_table(@teams)
+    @teams_array = create_table(@teams)
   end
 
   def create_table(file)
@@ -38,11 +13,11 @@ class TeamMethods
   end
 
   def count_of_teams
-    @teams_table['teamName'].count
+    @teams_array['teamName'].count
   end
 
   def find_by_id(team_id)
-    team = @teams_table.find do |team|
+    team = @teams_array.find do |team|
       team['team_id'] == team_id
     end
     team['teamName']
