f615a836888f6ecc33be467e319677658d65d670
Curtis Bartell <cbartell980@gmail.com>
2020-09-12 19:15:43 -0600

    Add game_team_info

--- lib/game_team_manager.rb
+++ lib/game_team_manager.rb
@@ -21,4 +21,13 @@ class GameTeamManager
       game_team.team_id == id
     end.map(&:game_id)
   end
+
+  def game_team_info(game_id)
+    game_teams.select do |game_team|
+      game_team.game_id == game_id
+    end.reduce({}) do |collector, game|
+      collector[game.team_id] = game.game_team_info
+      collector
+    end
+  end
 end
