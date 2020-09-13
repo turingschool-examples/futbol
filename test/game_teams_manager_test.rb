e1b06179ad99c49cafb30d7c32bced31fa75b482
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-13 16:26:40 -0600

    Test for average win percentage

--- test/game_teams_manager_test.rb
+++ test/game_teams_manager_test.rb
@@ -197,4 +197,31 @@ class GameTeamsManagerTest < Minitest::Test
     assert_equal ({:total => 3, :wins => 2, :ties => 0, :losses => 1}), tracker.game_teams_manager.result_totals_by_team('123')
     assert_equal ({:total => 1, :wins => 0, :ties => 1, :losses => 0}), tracker.game_teams_manager.result_totals_by_team('987')
   end
+
+  def test_it_can_find_average_win_percentage
+    game_path = './fixture/game_blank.csv'
+    team_path = './fixture/team_blank.csv'
+    game_teams_path = './fixture/game_teams_blank.csv'
+
+    tracker = StatTracker.new(game_path, team_path, game_teams_path)
+    game_teams_1 = mock("Game Team Object 1")
+    game_teams_2 = mock("Game Team Object 2")
+    game_teams_3 = mock("Game Team Object 3")
+    game_teams_4 = mock("Game Team Object 4")
+    tracker.game_teams_manager.game_teams << game_teams_1
+    tracker.game_teams_manager.game_teams << game_teams_2
+    tracker.game_teams_manager.game_teams << game_teams_3
+    tracker.game_teams_manager.game_teams << game_teams_4
+    game_teams_1.stubs(:team_id).returns('123')
+    game_teams_2.stubs(:team_id).returns('123')
+    game_teams_3.stubs(:team_id).returns('123')
+    game_teams_4.stubs(:team_id).returns('987')
+    game_teams_1.stubs(:result).returns('WIN')
+    game_teams_2.stubs(:result).returns('LOSS')
+    game_teams_3.stubs(:result).returns('WIN')
+    game_teams_4.stubs(:result).returns('TIE')
+
+    assert_equal 0.67, tracker.game_teams_manager.average_win_percentage('123')
+    assert_equal 0.0, tracker.game_teams_manager.average_win_percentage('987')
+  end
 end
