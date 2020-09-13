53dd124d305581df3dec69b03b4cbc28776761fc
Nick King <nickmaxking@gmail.com>
2020-09-13 10:23:01 -0600

    Modify fewest tackles method to function with extracted logic

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -160,25 +160,7 @@ class StatTracker
   end
 
   def fewest_tackles(season)
-    team_tackles = Hash.new(0)
-    games_in_season = @games.select do |game|
-      game["season"] == season
-    end
-    game_ids = games_in_season.map do |game|
-      game["game_id"]
-    end
-    @game_teams.each do |game|
-      if game_ids.include?(game["game_id"])
-        team_tackles[game["team_id"]] += game["tackles"].to_i
-      end
-    end
-    fewest_tackles_team = team_tackles.min_by do |team, tackles|
-      tackles
-    end[0]
-    fewest_tackles_team_name = @teams.find do |team|
-      team["team_id"] == fewest_tackles_team
-    end
-    fewest_tackles_team_name["teamName"]
+    @game_team_manager.fewest_tackles(season)
   end
 
   def team_info(team_id)
