dcd53a9e5da7d1cc10c7c9de319a61fafabd706d
Michael Foy <foymikek@gmail.com>
2020-09-13 10:07:45 -0600

    Add test for test_most_tackles_by_season

--- test/season_statistics_test.rb
+++ test/season_statistics_test.rb
@@ -43,4 +43,10 @@ class SeasonStatistcsTest < Minitest::Test
    assert_equal "FC Cincinnati", @stat.most_tackles("20132014")
    assert_equal "Seattle Sounders FC", @stat.most_tackles("20142015")
   end
+
+  def test_most_tackles_by_season
+   assert_equal "Atlanta United", @stat_tracker.fewest_tackles("20132014")
+   assert_equal "Orlando City SC", @stat_tracker.fewest_tackles("20142015")
+  end
+
 end
