c8f6e2718012788d7ceb6fd44417560d20906f00
Kevin Cuadros <kev040798@gmail.com>
2020-09-13 13:01:02 -0400

    Create test setup and initial tests

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -1,52 +1,32 @@
 require_relative 'test_helper'
 
 class StatTrackerTest < Minitest::Test
-  def test_it_exists
-    locations = {
+  def setup
+    @locations = {
       games: './data/games.csv',
       teams: './data/teams.csv',
       game_teams: './data/game_teams.csv'
     }
 
-    stat_tracker = StatTracker.new(locations)
+    @stat_tracker = StatTracker.new(@locations)
+    @stat_tracker = StatTracker.from_csv(@locations)
+  end
 
-    assert_instance_of StatTracker, stat_tracker
+  def test_it_exists
+   assert_instance_of StatTracker, @stat_tracker
   end
 
   def test_from_csv
-    game_path = './data/games.csv'
-    team_path = './data/teams.csv'
-    game_teams_path = './data/game_teams.csv'
-
-    locations = {
-      games: game_path,
-      teams: team_path,
-      game_teams: game_teams_path
-    }
-
-    stat_tracker = StatTracker.from_csv(locations)
-
-    assert_instance_of StatTracker, stat_tracker
+    assert_instance_of StatTracker, @stat_tracker
   end
 
   def test_it_has_attributes
-    game_path = './data/games.csv'
-    team_path = './data/teams.csv'
-    game_teams_path = './data/game_teams.csv'
-    
-    locations = {
-      games: game_path,
-      teams: team_path,
-      game_teams: game_teams_path
-    }
+    @game_manager = GameManager.new(@locations[:games], self)
+    @game_teams_manager = GameTeamsManager.new(@locations[:game_teams], self)
+    @team_manager = TeamManager.new(@locations[:teams], self)
 
-    stat_tracker = StatTracker.new(locations)
-    @game_manager = GameManager.new(locations[:games], self)
-    @game_teams_manager = GameTeamsManager.new(locations[:game_teams], self)
-    @team_manager = TeamManager.new(locations[:teams], self)
-
-    assert_instance_of GameManager, stat_tracker.game_manager
-    assert_instance_of TeamManager, stat_tracker.team_manager
-    assert_instance_of GameTeamsManager, stat_tracker.game_teams_manager
+    assert_instance_of GameManager, @stat_tracker.game_manager
+    assert_instance_of TeamManager, @stat_tracker.team_manager
+    assert_instance_of GameTeamsManager, @stat_tracker.game_teams_manager
   end
 end
