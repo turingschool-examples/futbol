2f97421523e967fa2284e82c84ce08f7e9aba333
Michael Foy <foymikek@gmail.com>
2020-09-14 13:40:15 -0600

    Add passing helper test  test_collect_seasons

--- test/team_stat_helper_test.rb
+++ test/team_stat_helper_test.rb
@@ -10,6 +10,9 @@ class TeamStatHelperTest < Minitest::Test
     @team_stat_helper ||= TeamStatHelper.new(@stat_tracker.game_table, @stat_tracker.team_table, @stat_tracker.game_team_table)
   end
 
-  
+  def test_collect_seasons
+    expected = ["20122013", "20172018", "20132014", "20142015", "20152016", "20162017"]
+    assert_equal expected, @team_stat_helper.collect_seasons("6").keys
+  end
 
 end
