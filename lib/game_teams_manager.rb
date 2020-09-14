85a3cea652c4e1a293f0c30a44a037f759bf5fd0
Shaunda Cunningham <shaunda.cunningham@gmail.com>
2020-09-13 21:44:43 -0600

    Add method for finding opponent win percentage

--- lib/game_teams_manager.rb
+++ lib/game_teams_manager.rb
@@ -144,4 +144,14 @@ class GameTeamsManager
     end
   end
 
+  def find_opponent_win_percentage(team_id)
+    opponent_games_by_team = find_opponent_games(team_id).group_by(&:team_id)
+    win_percentage = {}
+    opponent_games_by_team.each do |team_id, game_teams|
+      total = 0
+      opponent_wins = game_teams.count {|game_teams| game_teams.result == 'WIN'}
+      win_percentage[team_id] = total += (opponent_wins.to_f / game_teams.length).round(2)
+    end
+    win_percentage
+  end
 end
