1659005ea6bbffe19f02974a68a1b53319f43124
Kate Tester <katemorris@gmail.com>
2020-09-14 11:04:25 -0600

    Reordering for understanding.

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -23,23 +23,11 @@ class StatTracker
     @teams_manager = TeamsManager.new(locations[:teams], self)
   end
 
+# Game Statistics
 
-
-  def find_season_id(game_id)
-    @games_manager.find_season_id(game_id)
-  end
-
-  def find_team_name(team_number)
-    @teams_manager.find_team_name(team_number)
-  end
-
-  def average_number_of_goals_scored_by_team(team_id)
-    @game_teams_manager.average_number_of_goals_scored_by_team(team_id)
-  end
-
-  def average_number_of_goals_scored_by_team_by_type(team_id, home_away)
-    @game_teams_manager.average_number_of_goals_scored_by_team_by_type(team_id, home_away)
-  end
+  # def method_name
+  #
+  # end
 
 # League Statistics
   def count_of_teams
@@ -69,4 +57,21 @@ class StatTracker
   def lowest_scoring_home_team
     @teams_manager.lowest_scoring_home
   end
+
+  # Helpers
+    def find_season_id(game_id)
+      @games_manager.find_season_id(game_id)
+    end
+
+    def find_team_name(team_number)
+      @teams_manager.find_team_name(team_number)
+    end
+
+    def average_number_of_goals_scored_by_team(team_id)
+      @game_teams_manager.average_number_of_goals_scored_by_team(team_id)
+    end
+
+    def average_number_of_goals_scored_by_team_by_type(team_id, home_away)
+      @game_teams_manager.average_number_of_goals_scored_by_team_by_type(team_id, home_away)
+    end
 end
