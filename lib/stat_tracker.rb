4c3f2037fba4099580991b81b561c61fb213a5ed
todd-estes <elestes@gmail.com>
2020-09-14 13:37:02 -0600

    Refactor stat_tracker

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,31 +1,14 @@
-require 'csv'
-require_relative './game'
-require_relative './game_manager'
-require_relative './game_team_manager'
-require_relative './game_team'
-require_relative './team'
-require_relative './team_manager'
+require_relative './helper'
 
 class StatTracker
-  attr_reader :games,
-              :teams,
-              :game_teams
-
-  def initialize(games, teams, game_teams, locations)
-    @games = games
-    @teams = teams
-    @game_teams = game_teams
+  def initialize(locations)
     @game_manager = GameManager.new(locations[:games], self)
     @game_team_manager = GameTeamManager.new(locations[:game_teams], self)
     @team_manager = TeamManager.new(locations[:teams], self)
   end
 
   def self.from_csv(locations)
-    games = CSV.read(locations[:games], headers:true)
-    teams = CSV.read(locations[:teams], headers:true)
-    game_teams = CSV.read(locations[:game_teams], headers:true)
-
-    new(games, teams, game_teams, locations)
+    new(locations)
   end
   #-------traffic cop methods-------#
   def find_winningest_coach(game_ids, expected_result)
