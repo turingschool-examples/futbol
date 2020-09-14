f1e5171a6fac28a67817b3a3d150da8bc9cdaea5
Curtis Bartell <cbartell980@gmail.com>
2020-09-13 18:24:43 -0600

    Add test_it_can_find_a_teams_rival

--- test/team_manager_test.rb
+++ test/team_manager_test.rb
@@ -445,6 +445,69 @@ class TeamManagerTest < Minitest::Test
     }
     game_teams_info = [game1, game2, game3, game4, game5, game6, game7, game8]
     team_manager.stubs(:gather_game_team_info).returns(game_teams_info)
+
     assert_equal 'The Twos', team_manager.favorite_opponent('5')
+  end
+
+  def test_it_can_find_a_teams_rival
+    stat_tracker = mock('A totally legit stat_tracker')
+    team5 = mock('team 5')
+    team5.stubs(:team_id).returns('5')
+    team5.stubs(:team_info).returns({'team_name' => 'The Fives'})
+    team2 = mock('team 2')
+    team2.stubs(:team_id).returns('2')
+    team2.stubs(:team_info).returns({'team_name' => 'The Twos'})
+    team6 = mock('team 6')
+    team6.stubs(:team_id).returns('6')
+    team6.stubs(:team_info).returns({'team_name' => 'The Sixes'})
+    team7 = mock('team 7')
+    team7.stubs(:team_id).returns('7')
+    team7.stubs(:team_info).returns({'team_name' => 'The Sevens'})
+    team8 = mock('team 8')
+    team8.stubs(:team_id).returns('8')
+    team8.stubs(:team_info).returns({'team_name' => 'The Ochos'})
+    team12 = mock('team 12')
+    team12.stubs(:team_id).returns('12')
+    team12.stubs(:team_info).returns({'team_name' => 'The Twelves'})
+    CSV.stubs(:foreach).returns(nil)
+    team_manager = TeamManager.new('A totally legit path', stat_tracker)
+    teams = [team2, team5, team6, team7, team8, team12]
+    team_manager.stubs(:teams).returns(teams)
+    game1 = {
+      '5' => {result: 'LOSS'},
+      '6' => {result: 'WIN'}
+    }
+    game2 = {
+      '8' => {result: 'TIE'},
+      '5' => {result: 'TIE'}
+    }
+    game3 = {
+      '5' => {result: 'WIN'},
+      '12' => {result: 'LOSS'}
+    }
+    game4 = {
+      '5' => {result: 'WIN'},
+      '2' => {result: 'LOSS'}
+    }
+    game5 = {
+      '5' => {result: 'WIN'},
+      '7' => {result: 'LOSS'}
+    }
+    game6 = {
+      '5' => {result: 'LOSS'},
+      '6' => {result: 'WIN'}
+    }
+    game7 = {
+      '5' => {result: 'WIN'},
+      '6' => {result: 'LOSS'}
+    }
+    game8 = {
+      '5' => {result: 'LOSS'},
+      '6' => {result: 'WIN'}
+    }
+    game_teams_info = [game1, game2, game3, game4, game5, game6, game7, game8]
+    team_manager.stubs(:gather_game_team_info).returns(game_teams_info)
+
+    assert_equal 'The Sixes', team_manager.rival('5')
   end
 end
