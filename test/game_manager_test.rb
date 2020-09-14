3c6fa597058fe2ae7d229e7882c58688be87ac81
Sage Gonzales <sagegonzales15@gmail.com>
2020-09-14 10:58:36 -0600

    Alter test to equal expected for average goals by season

--- test/game_manager_test.rb
+++ test/game_manager_test.rb
@@ -174,14 +174,7 @@ class GameManagerTest < Minitest::Test
     game_3.stubs(:away_goals).returns(0)
     game_3.stubs(:home_goals).returns(0)
 
-      expected = {
-      "20122013"=>4.12,
-      "20162017"=>4.23,
-      "20142015"=>4.14,
-      "20152016"=>4.16,
-      "20132014"=>4.19,
-      "20172018"=>4.44
-    }
+      expected = {"20122013"=>0.0, "20132014"=>0.0}
 
     assert_equal expected, game_manager.average_goals_by_season
   end
