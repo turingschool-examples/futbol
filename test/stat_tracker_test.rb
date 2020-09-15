dd06c11b33f64584e3c664b2469c0c2e8617ab0a
Sage Gonzales <sagegonzales15@gmail.com>
2020-09-15 10:10:06 -0600

    Add test for percentage visitor wins method

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -75,4 +75,19 @@ class StatTrackerTest < Minitest::Test
 
     assert_equal 0.69, stat_tracker.percentage_home_wins
   end
+
+  def test_it_can_find_percentage_visitor_wins
+    game_path = './fixture/games_dummy.csv'
+    team_path = './fixture/teams_dummy.csv'
+    game_teams_path = './fixture/game_teams_dummy.csv'
+
+     locations = {
+       games: game_path,
+       teams: team_path,
+       game_teams: game_teams_path
+     }
+     stat_tracker = StatTracker.from_csv(locations)
+
+    assert_equal 0.31, stat_tracker.percentage_visitor_wins
+  end
 end
