4ea2e1dddf220991df00c9f2c919d9adf31b03d6
Kevin Cuadros <kev040798@gmail.com>
2020-09-15 13:12:16 -0400

    Created test for game test

--- test/game_test.rb
+++ test/game_test.rb
@@ -11,8 +11,34 @@ class GameTest < Minitest::Test
       teams: team_path,
       game_teams: game_teams_path
     }
-    @stat_tracker = StatTracker.from_csv(locations)
-    @game_stats   = GameStats.new('./data/games.csv', @stat_tracker)
-    @game         = Game.new(locations, @game_stats)
+
+    @hash = {
+       game_id: 2012030221,
+       season: 20122013,
+       type: "Postseason",
+       date_time: 5/16/13,
+       away_team_id: '3',
+       home_team_id: '6',
+       away_goals: 2,
+       home_goals: 3
+      }
+    @tracker = StatTracker.from_csv(locations)
+    @game_stats   = GameStats.new(@tracker)
+    @game         = Game.new(@hash)
+  end
+
+  def test_it_exists
+    assert_instance_of Game, @game
+  end
+
+  def test_attributes
+    assert_equal 2012030221, @game.game_id
+    assert_equal '20122013', @game.season
+    assert_equal "Postseason", @game.type
+    assert_equal 5/16/13, @game.date_time
+    assert_equal '3', @game.away_team_id
+    assert_equal '6', @game.home_team_id
+    assert_equal 2, @game.away_goals
+    assert_equal 3, @game.home_goals
   end
 end
