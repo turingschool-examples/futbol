d3ab47e4e826b7c506aa047d3352107152f9faa0
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-15 18:01:26 -0500

    Refacotr team_info method to be less than 10 lines

--- lib/team_manager.rb
+++ lib/team_manager.rb
@@ -29,13 +29,13 @@ class TeamManager
   def team_info(team_id)
     team_info = {}
     @teams.each do |team|
-      if team_id == team.team_id
-        team_info['team_id'] = team.team_id
-        team_info['franchise_id'] = team.franchise_id
-        team_info['team_name'] = team.team_name
-        team_info['abbreviation'] = team.abbreviation
-        team_info['link'] = team.link
-      end
+      next unless team_id == team.team_id
+
+      team_info['team_id'] = team.team_id
+      team_info['franchise_id'] = team.franchise_id
+      team_info['team_name'] = team.team_name
+      team_info['abbreviation'] = team.abbreviation
+      team_info['link'] = team.link
     end
     team_info
   end
