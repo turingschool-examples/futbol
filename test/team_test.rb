0f40b9b5526e3c844c79df2665928e8a4a7dfb9c
Taylor Phillips <taylorscottphillips@gmail.com>
2020-09-15 17:49:48 -0500

    Add testing for Team class

--- test/team_test.rb
+++ test/team_test.rb
@@ -6,10 +6,25 @@ require './lib/team_manager'
 require './test/test_helper'
 
 class TeamTest < Minitest::Test
+  def setup
+    @team_path = './data/teams.csv'
+
+    @team_manager = TeamManager.new(@team_path,nil)
+  end
+
   def test_it_exists
+    assert_instance_of Team, @team_manager.teams[0]
+  end
+
+  def test_it_has_attributes
     path = './data/teams.csv'
     team = Team.new(path, nil)
 
-    assert_instance_of Team, team
+    assert_equal '1', @team_manager.teams[0].team_id
+    assert_equal '23', @team_manager.teams[0].franchise_id
+    assert_equal 'Atlanta United', @team_manager.teams[0].team_name
+    assert_equal 'ATL', @team_manager.teams[0].abbreviation
+    assert_equal 'Mercedes-Benz Stadium', @team_manager.teams[0].stadium
+    assert_equal '/api/v1/teams/1', @team_manager.teams[0].link
   end
 end
