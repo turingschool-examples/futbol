129f6d525d3430e3f4993dc13065e58c31e4c8af
Joshua Carey <jdcarey128@gmail.com>
2020-09-14 18:26:54 -0600

    Change rounding in find_percent to 2

--- lib/manageable.rb
+++ lib/manageable.rb
@@ -2,7 +2,7 @@ module Manageable
   # Do we need this method?
   def find_percent(numerator, denominator)
     return 0.0 if denominator == 0
-    (numerator / denominator.to_f * 100).round(3)
+    (numerator / denominator.to_f * 100).round(2)
   end
 
   def ratio(numerator, denominator, rounding = 2)
