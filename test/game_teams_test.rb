b2ba71d6daedb9d9d0358d1b80c1d62356ba3b99
Kevin Cuadros <kev040798@gmail.com>
2020-09-15 20:55:51 -0400

    Complete game teams test

--- test/game_teams_test.rb
+++ test/game_teams_test.rb
@@ -5,7 +5,7 @@ class GameTeamsTest < Minitest::Test
     @hash = {
       game_id: '2012030221',
       team_id: '3',
-      HoA: 'away',
+      hoa: 'away',
       result: 'LOSS',
       settled_in: 'OT',
       head_coach: 'John Tortorella',
@@ -19,5 +19,17 @@ class GameTeamsTest < Minitest::Test
 
   def test_it_exists_with_attributes
     assert_instance_of GameTeams, @game_teams
+  end
+
+  def test_attributes
+    assert_equal 2012030221, @game_teams.game_id
+    assert_equal 3, @game_teams.team_id
+    assert_equal 'away', @game_teams.hoa
+    assert_equal 'LOSS', @game_teams.result
+    assert_equal 'OT', @game_teams.settled_in
+    assert_equal 'John Tortorella', @game_teams.head_coach
+    assert_equal 2, @game_teams.goals
+    assert_equal 8, @game_teams.shots
+    assert_equal 44, @game_teams.tackles
   end
 end
