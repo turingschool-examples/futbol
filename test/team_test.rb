67882bb52aeb4b438e8b878efa435ab9dd509111
Kevin Cuadros <kev040798@gmail.com>
2020-09-15 20:27:31 -0400

    Finish team test

--- test/team_test.rb
+++ test/team_test.rb
@@ -2,16 +2,6 @@ require_relative 'test_helper'
 
 class TeamTest < Minitest::Test
   def setup
-    game_path = './data/games.csv'
-    team_path = './data/teams.csv'
-    game_teams_path = './data/game_teams.csv'
-
-    locations = {
-      games: game_path,
-      teams: team_path,
-      game_teams: game_teams_path
-    }
-
     @hash = {
        team_id: '1',
        franchiseid: '23',
@@ -19,12 +9,18 @@ class TeamTest < Minitest::Test
        abbreviation: 'ATL',
        link: '/api/v1/teams/1'
       }
-    @tracker = StatTracker.from_csv(locations)
-    @game_stats   = GameStats.new(@tracker)
-    @team         = Team.new(@hash)
+    @team = Team.new(@hash)
   end
 
   def test_it_exists
     assert_instance_of Team, @team
+  end
+
+  def test_attributes
+    assert_equal '1', @team.team_id
+    assert_equal '23', @team.franchiseid
+    assert_equal 'ATL', @team.abbreviation
+    assert_equal 'Atlanta United', @team.teamname
+    assert_equal '/api/v1/teams/1', @team.link
   end
 end
