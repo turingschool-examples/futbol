efd7470eed19435f4ddcf95823dce36a3048151f
Nick King <nickmaxking@gmail.com>
2020-09-16 13:51:00 -0600

    Merge game_and_win_count and game_and_versus_count helper methods into one method. Change arguments in helper method and associated methods

--- lib/game_team_manager.rb
+++ lib/game_team_manager.rb
@@ -102,22 +102,36 @@ class GameTeamManager
   end
 
   def find_winningest_coach(game_ids)
-    total_wins_and_games = game_and_win_count(game_ids, "WIN")
+    total_wins_and_games = game_and_win_count(game_ids, :head_coach, "WIN", nil, true)
     sort_percentages(total_wins_and_games.first, total_wins_and_games.last).last[0]
   end
 
   def find_worst_coach(game_ids)
-    total_wins_and_games = game_and_win_count(game_ids, "WIN")
+    total_wins_and_games = game_and_win_count(game_ids, :head_coach, "WIN", nil, true)
     sort_percentages(total_wins_and_games.first, total_wins_and_games.last).first[0]
   end
 
-  def game_and_win_count(game_ids, expected_result)
+  def favorite_opponent(team_id)
+    game_ids = find_game_ids(team_id)
+    total_games_and_results = game_and_win_count(game_ids, :team_id, "LOSS", team_id)
+    biggest_loser = sort_percentages(total_games_and_results.first, total_games_and_results.last)
+    @tracker.get_team_name(biggest_loser.last[0])
+  end
+
+  def rival(team_id)
+    game_ids = find_game_ids(team_id)
+    total_games_and_results = game_and_win_count(game_ids, :team_id, "WIN", team_id)
+    biggest_winner = sort_percentages(total_games_and_results.first, total_games_and_results.last)
+    @tracker.get_team_name(biggest_winner.last[0])
+  end
+
+  def game_and_win_count(game_ids, stat_query, expected_result, team_id = nil, teams_removed = false)
     @game_teams.reduce([Hash.new, Hash.new(0)]) do |collectors, game|
-      if game_ids.include?(game.game_id)
-        collectors.first[game.head_coach] ||= 0
-        collectors.last[game.head_coach] += 1
+      if game_ids.include?(game.game_id) && (teams_removed || game.stats[stat_query] != team_id)
+        collectors.first[game.stats[stat_query]] ||= 0
+        collectors.last[game.stats[stat_query]] += 1
         if game.result == expected_result
-          collectors.first[game.head_coach] += 1
+          collectors.first[game.stats[stat_query]] += 1
         end
       end
       collectors
@@ -147,32 +161,6 @@ class GameTeamManager
   def find_game_ids(team_id)
     find_all_games(team_id).map do |game|
       game.game_id
-    end
-  end
-
-  def favorite_opponent(team_id)
-    total_games_and_results = game_and_versus_count(team_id, "LOSS")
-    biggest_loser = sort_percentages(total_games_and_results.first, total_games_and_results.last)
-    @tracker.get_team_name(biggest_loser.last[0])
-  end
-
-  def rival(team_id)
-    total_games_and_results = game_and_versus_count(team_id, "WIN")
-    biggest_winner = sort_percentages(total_games_and_results.first, total_games_and_results.last)
-    @tracker.get_team_name(biggest_winner.last[0])
-  end
-
-  def game_and_versus_count(team_id, expected_result)
-    game_ids = find_game_ids(team_id)
-    @game_teams.reduce([Hash.new, Hash.new(0)]) do |collectors, game|
-      if game_ids.include?(game.game_id) && game.team_id != team_id
-        collectors.first[game.team_id] ||= 0
-        collectors.last[game.team_id] += 1
-        if game.result == expected_result
-          collectors.first[game.team_id] += 1
-        end
-      end
-      collectors
     end
   end
 end
