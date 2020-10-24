04df55749999a51a868027d374759f7d099f3060
Catherine Dean <catherinemdean15@gmail.com>
2020-10-24 11:51:39 -0600

    add test for highest_total_score, add test for  sum_of_scores

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -5,4 +5,16 @@ class StatTracker
 
   def initialize(locations)
   end
+
+  def sum_of_scores
+    #array of ints
+    #(games.csv).map do |game|
+      #home_team_scores + away_team_scores
+    #end
+  end
+
+  def highest_total_score
+    # sum_of_scores.max
+  end
+
 end
