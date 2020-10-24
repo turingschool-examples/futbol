caa11e5ecb0adf979d5dd953c68b77445ce7bb73
Sam Yeo <yeosamuel95@gmail.com>
2020-10-24 14:37:12 -0600

    Add highest_total_score test

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -4,18 +4,6 @@ class StatTrackerTest < MiniTest::Test
 
 # Game Statistics Methods
   def test_highest_total_score
-    # Sum up Away and Home team's score per game_id
-    # [] games.csv .map each row
-      # Add winning + losing score
-    # [].max
-
-    #assert_equal , StatTracker.highest_total_score
+    assert_equal 5, StatTracker.highest_total_score
   end
-
-
-
-
-
-
-
 end
