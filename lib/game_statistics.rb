e851198c4715fd7214c57ab9bb5fdbf7f95e0545
Max Ribbans <ribbans.max@gmail.com>
2020-10-24 14:07:48 -0400

    Add comments for later abstraction

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -5,10 +5,12 @@ class GameStats
     @stats = stats
   end
 
+  ############### DATA CONVERSION HELPER
   def convert_to_i(array)
     array.map(&:to_i)
   end
 
+  ############### DATA CONVERSION HELPER
   def sum_data(data_key)
     convert_to_i(@stats[data_key]).sum
   end
