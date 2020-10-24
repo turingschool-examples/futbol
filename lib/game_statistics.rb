3266ada3d893d6e79eda423f266919eef4b3dcb8
Max Ribbans <ribbans.max@gmail.com>
2020-10-24 15:47:16 -0400

    Add capability to sum_data to take data, defaults to all data Add team_stats method: this method returns a table that has only the rows where the data_value is inside the header column

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -11,9 +11,17 @@ class GameStats
   end
 
   ############### DATA CONVERSION HELPER
-  def sum_data(data_key)
-    convert_to_i(@stats[data_key]).sum
+  def sum_data(data_key, data = @stats)
+    convert_to_i(data[data_key]).sum
   end
 
-
+  ##########################################################
+  # This method returns a table that has only the rows where the data_value is inside the header column#######
+  # SUPER IMPORTANT METHOD ################
+  def team_stats(header, data_value)
+    temp = @stats
+    temp.delete_if do |row|
+      row[header] != data_value
+    end
+  end
 end
