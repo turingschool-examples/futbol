cb6bf159c9b58200361440c181355af60812be34
Catherine Dean <catherinemdean15@gmail.com>
2020-10-24 15:36:52 -0600

    add percentage methods

--- lib/gamestats.rb
+++ lib/gamestats.rb
@@ -8,6 +8,7 @@ class GameStats
   def initialize(locations)
     @locations = locations #this is a hash
     @games_table = CSV.parse(File.read(@locations[:games]), headers: true)
+    @games_teams_table = CSV.parse(File.read(@locations[:game_teams]), headers: true)
   end
 
   def sum_of_scores
@@ -22,6 +23,28 @@ class GameStats
 
   def lowest_total_score
     sum_of_scores.min
+  end
+
+  def compare_hoa_to_result(hoa, result)
+    @games_teams_table.to_a.count do |game|
+      game[2] == hoa && game[3] == result
+    end.to_f
+  end
+
+  def total_games
+    @games_teams_table.count / 2
+  end
+
+  def percentage_home_wins
+    (compare_hoa_to_result("home", "WIN") / total_games * 100).round(2)
+  end
+
+  def percentage_away_wins
+    (compare_hoa_to_result("away", "WIN") / total_games  * 100).round(2)
+  end
+
+  def percentage_ties
+    (compare_hoa_to_result("away", "TIE") / total_games  * 100).round(2)
   end
 
 end
--- test/gamestats_test.rb
+++ test/gamestats_test.rb
@@ -41,4 +41,28 @@ class GameStatsTest < Minitest::Test
     assert_equal 5, @gamestats.lowest_total_score
   end
 
+  def test_it_calls_percentage_of_games_w_home_team_win
+    assert_instance_of Float, @gamestats.percentage_home_wins
+    @gamestats.stubs(:percentage_home_wins).returns(45.23)
+    assert_equal 45.23, @gamestats.percentage_home_wins
+  end
+
+  def test_it_calls_percentage_of_games_w_away_team_win
+    assert_instance_of Float, @gamestats.percentage_away_wins
+    @gamestats.stubs(:percentage_away_wins).returns(45.23)
+    assert_equal 45.23, @gamestats.percentage_away_wins
+  end
+
+  def test_it_calls_percentage_of_games_tied
+    assert_instance_of Float, @gamestats.percentage_ties
+    @gamestats.stubs(:percentage_ties).returns(11.55)
+    assert_equal 11.55, @gamestats.percentage_ties
+  end
+
+  def test_total_percentages_equals_100
+    assert_equal 100, (@gamestats.percentage_home_wins +
+                       @gamestats.percentage_away_wins +
+                       @gamestats.percentage_ties)
+  end
+  
 end
