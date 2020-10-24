8a282b2b10fa0ebf6ea454da719c2779adb30ec7
Max Ribbans <ribbans.max@gmail.com>
2020-10-24 13:51:48 -0400

    Add convert_to_i (array => array of integers) method

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -4,4 +4,15 @@ class GameStats
   def initialize(stats)
     @stats = stats
   end
+
+  def convert_to_i(array)
+    array.map(&:to_i)
+  end
+  # def sum_data(data_key)
+  #   @stats[data_key].map do |number|
+  #     number.to_i
+  #   end.sum
+  # end
+
+
 end
