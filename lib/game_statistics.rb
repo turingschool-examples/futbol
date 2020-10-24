7c3643f789ef7e04de9c6bb9c737e17c50e49c18
Max Ribbans <ribbans.max@gmail.com>
2020-10-24 13:54:06 -0400

    Add sum_data method (sums a column of data given the header)

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -8,11 +8,10 @@ class GameStats
   def convert_to_i(array)
     array.map(&:to_i)
   end
-  # def sum_data(data_key)
-  #   @stats[data_key].map do |number|
-  #     number.to_i
-  #   end.sum
-  # end
+
+  def sum_data(data_key)
+    convert_to_i(@stats[data_key]).sum
+  end
 
 
 end
