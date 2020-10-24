a005e3f8e0aa3320e032a883fecca213067810d8
Grayson Myers <graymyers10@gmail.com>
2020-10-24 11:26:31 -0600

    Add object data class

---
+++ lib/object_data.rb
@@ -0,0 +1,9 @@
+
+class ObjectData
+  attr_reader :games, :teams, :game_teams
+  def initialize(stat_tracker)
+    @games = stat_tracker.retrieve_games
+    @teams = stat_tracker.retrieve_teams
+    @game_teams = stat_tracker.retrieve_game_teams
+  end
+end
--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -7,35 +7,36 @@ class StatTracker
   attr_reader :games, :teams, :game_teams
   def initialize(locations)
     @locations = locations
-    @file_hashes = {}
-    @games = {}
-    @teams = {}
-    @game_teams = {}
+     #because we are creating an object without initialize, we have to manually return the instance that was created.
   end
 
-  def process_files
-    retrieve_game_teams
-    retrieve_games
-    retrieve_teams
+  def self.from_csv(locations)
+    StatTracker.new(locations)
   end
 
   def retrieve_game_teams
+    output_hash = {}
     CSV.foreach(@locations[:game_teams], headers: true) do |row|
-      @game_teams[row[0]] = {} if !@game_teams[row[0]]
-      @game_teams[row[0]][row[2]] = GameTeam.new(row)
+      output_hash[row[0]] = {} if !output_hash[row[0]]
+      output_hash[row[0]][row[2]] = GameTeam.new(row)
     end
+    output_hash
   end
 
   def retrieve_games
+    output_hash = {}
     CSV.foreach(@locations[:games], headers: true) do |row|
-      @games[row[0]] = Game.new(row)
+      output_hash[row[0]] = Game.new(row)
     end
+    output_hash
   end
 
   def retrieve_teams
+    output_hash = {}
     CSV.foreach(@locations[:teams], headers: true) do |row|
-      @teams[row[0]] = Team.new(row)
+      output_hash[row[0]] = Team.new(row)
     end
+    output_hash
   end
 
 end
