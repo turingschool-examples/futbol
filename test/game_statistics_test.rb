f2da5de3493fe2bce02609f2eff8197c2fa73c22
Max Ribbans <ribbans.max@gmail.com>
2020-10-24 13:29:17 -0400

    Add setup

--- test/game_statistics_test.rb
+++ test/game_statistics_test.rb
@@ -4,5 +4,24 @@ require './lib/stat_tracker'
 require './lib/game_statistics'
 
 class GameStats < Minitest::Test
+  def setup
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+    dummy_path = './data/dummy.csv'
 
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path,
+      dummy: dummy_path
+    }
+
+    stat_tracker = StatTracker.from_csv(locations)
+    dummy_stats = stat_tracker[:dummy]
+    @game = GameStats.new()
+  end
+
+  def test_it_exists_and_has_attributes
+  end
 end
