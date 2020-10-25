6fb04d9cbf71d23728b47262751c8ba212947566
Ely Hess <ely.hess@me.com>
2020-10-25 15:57:47 -0600

    Add all method test

--- test/team_manager_test.rb
+++ test/team_manager_test.rb
@@ -6,12 +6,28 @@ require_relative '../lib/team_manager'
 class TeamManagerTest < Minitest::Test
 
   def setup
-    @team_manager = TeamManager.new('./data/teams.csv')
+    @game_path = './data/games.csv'
+    @team_path = './data/teams.csv'
+    @game_teams_path = './data/game_teams.csv'
+
+    @file_locations = {
+                      games: @game_path,
+                      teams: @team_path,
+                      game_teams: @game_teams_path
+                        }
+
+    @team_manager = TeamManager.new(@file_locations)
   end
 
   def test_it_exists_and_has_attributes
     assert_instance_of TeamManager, @team_manager
     assert_equal './data/teams.csv', @team_manager.teams_data
   end
+
+  def test_it_gives_array_of_all_teams
+    assert_equal 32, @team_manager.all.count
+  end
+
+  
 
 end
