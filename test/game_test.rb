02375cbda10669e1cb9d7d5c156300aaf7df7e83
Phil McCarthy <philmcc2631@gmail.com>
2020-10-25 16:37:11 -0600

    Add initialization test for game class

--- test/game_test.rb
+++ test/game_test.rb
@@ -1,22 +1,24 @@
-require './lib/test_helper'
+require './test/test_helper'
 require './lib/game'
-class Test < Minitest::Test
-  #
-  # def setup
-  #   @game = Game.from_csv('./data/games.csv')
-  # end
+class GameTest < Minitest::Test
+  def setup
+    @game = Game.new({
+      game_id: 2012030221,
+      season: 20122013,
+      away_team_id: 3,
+      home_team_id: 6,
+      away_goals: 2,
+      home_goals: 3,
+    })
+  end
 
-  # def test_it_exists_and_has_attributes
-  #   assert_instance_of Game, @game
-  #   assert_equal x, game_id
-  #   assert_equal x, season
-  #   assert_equal x, type
-  #   assert_equal x, date_time
-  #   assert_equal x, away_team_id
-  #   assert_equal x, home_team_id
-  #   assert_equal x, away_goals
-  #   assert_equal x, home_goals
-  #   assert_equal x, venue
-  #   assert_equal x, venue_link
-  # end
+  def test_it_exists_and_has_attributes
+    assert_instance_of Game, @game
+    assert_equal 2012030221, @game.id
+    assert_equal 20122013, @game.season
+    assert_equal 3, @game.away_team_id
+    assert_equal 6, @game.home_team_id
+    assert_equal 2, @game.away_goals
+    assert_equal 3, @game.home_goals
+  end
 end
