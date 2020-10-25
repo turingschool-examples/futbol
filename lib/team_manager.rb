45b03b6560e7097600b1a49c0caa2312347c4fd7
Ely Hess <ely.hess@me.com>
2020-10-25 15:59:42 -0600

    Add all method

--- lib/team_manager.rb
+++ lib/team_manager.rb
@@ -7,4 +7,20 @@ class TeamManager
     @teams_data = file_locations[:teams]
   end
 
+  def all
+    teams = []
+    CSV.foreach(@teams_data, header_converters: :symbol, headers: true) do |row|
+      team_data = {
+        team_id: row[:team_id],
+        franchise_id: row[:franchise_id],
+        team_name: row[:team_name],
+        abbreviation: row[:abbreviation],
+        stadium: row[:stadium],
+        link: row[:link]
+      }
+      teams << Team.new(team_data)
+    end
+    teams
+  end
+
 end
