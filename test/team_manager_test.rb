21c1955cdc86682c49205dcdd51f79f5f0920a15
elyhess <46981974+elyhess@users.noreply.github.com>
2020-10-25 17:07:11 -0600

    Change team_data to team_attributes

--- test/team_manager_test.rb
+++ test/team_manager_test.rb
@@ -21,7 +21,7 @@ class TeamManagerTest < Minitest::Test
 
   def test_it_exists_and_has_attributes
     assert_instance_of TeamManager, @team_manager
-    assert_equal './data/teams.csv', @team_manager.teams_data
+    assert_equal './data/teams.csv', @team_manager.teams_attributes
   end
 
   def test_it_gives_array_of_all_teams
