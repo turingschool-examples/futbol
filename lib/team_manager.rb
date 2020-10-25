83e73b6a78f0e02e1227dd8a70f3a85fd1815fec
elyhess <46981974+elyhess@users.noreply.github.com>
2020-10-25 17:06:07 -0600

    Fix inconsistent attribute naming

--- lib/team_manager.rb
+++ lib/team_manager.rb
@@ -1,15 +1,15 @@
 require 'csv'
 
 class TeamManager
-  attr_reader :teams_data, :teams
+  attr_reader :teams_attributes, :teams
 
   def initialize(file_locations)
-    @teams_data = file_locations[:teams]
+    @teams_attributes = file_locations[:teams]
   end
 
   def all
     teams = []
-    CSV.foreach(@teams_data, header_converters: :symbol, headers: true) do |row|
+    CSV.foreach(@teams_attributes, header_converters: :symbol, headers: true) do |row|
       team_data = {
         team_id: row[:team_id],
         franchise_id: row[:franchise_id],
