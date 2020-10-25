f81e7c732867bedc2cc70b99c91705a58c1dcc46
Aiden Mendez <aidenmendez@gmail.com>
2020-10-25 16:04:06 -0600

    Add lowest_total_score

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -3,6 +3,8 @@
   # Col by winning score + Col by losing score
 # Parse CSV table initially and save as instance variable
 # Method will iterate through instance variable
+require 'csv'
+
 class StatTracker
   attr_reader :games, :game_teams, :teams
   def self.from_csv(locations)
@@ -22,6 +24,15 @@ class StatTracker
       most = total if total > most
     end
     most
+  end
+
+  def lowest_total_score
+    least = 1000
+    CSV.foreach(games, :headers => true, header_converters: :symbol) do |row|
+      total = row[:home_goals].to_i + row[:away_goals].to_i
+      least = total if total < least
+    end
+    least
   end
 
 end
