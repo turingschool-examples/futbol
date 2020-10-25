0ab28eb83af708f74c39b37ad332b3b66153b5db
elyhess <46981974+elyhess@users.noreply.github.com>
2020-10-25 17:04:44 -0600

    Update team_manager.rb
    Remove stadium attribute

--- lib/team_manager.rb
+++ lib/team_manager.rb
@@ -15,7 +15,6 @@ class TeamManager
         franchise_id: row[:franchise_id],
         team_name: row[:team_name],
         abbreviation: row[:abbreviation],
-        stadium: row[:stadium],
         link: row[:link]
       }
       teams << Team.new(team_data)
