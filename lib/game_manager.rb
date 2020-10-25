fc5256c66500ca9a91e59e3241e7d98fbab564be
Phil McCarthy <philmcc2631@gmail.com>
2020-10-25 16:42:29 -0600

    Add game_manager all method with test passing

--- lib/game_manager.rb
+++ lib/game_manager.rb
@@ -7,4 +7,19 @@ class GameManager
     @games_data_path = data
     @games = []
   end
+
+  def all
+    CSV.foreach(@games_data_path, headers: true, header_converters: :symbol) do |row|
+      game_attributes = {
+                          game_id: row[:game_id],
+                          season: row[:season],
+                          away_team_id: row[:away_team_id],
+                          home_team_id: row[:home_team_id],
+                          away_goals: row[:away_goals],
+                          home_goals: row[:home_goals],
+                        }
+      @games << (Game.new(game_attributes))
+    end
+    @games
+  end
 end
