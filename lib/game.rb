51daecd4e771d061a86669e5c735d4c8f53ff5f1
Phil McCarthy <philmcc2631@gmail.com>
2020-10-25 16:37:39 -0600

    Add game class to pass initial test

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,16 @@
-require 'csv'
-
 class Game
-  
+  attr_reader :id,
+              :season,
+              :away_team_id,
+              :home_team_id,
+              :away_goals,
+              :home_goals
+  def initialize(data)
+    @id = data[:game_id].to_i
+    @season = data[:season].to_i
+    @away_team_id = data[:away_team_id].to_i
+    @home_team_id = data[:home_team_id].to_i
+    @away_goals = data[:away_goals].to_i
+    @home_goals = data[:home_goals].to_i
+  end
 end
