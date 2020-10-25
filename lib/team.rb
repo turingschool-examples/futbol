f91717ee115ac48d7c4655a05c5f524921f0335e
Ely Hess <ely.hess@me.com>
2020-10-25 16:28:22 -0600

    Add team class

---
+++ lib/team.rb
@@ -0,0 +1,17 @@
+class Team
+  attr_reader :team_id,
+              :franchise_id,
+              :team_name,
+              :abbreviation,
+              :stadium,
+              :link
+
+  def initialize(data)
+    @team_id = data[:team_id]
+    @franchise_id = data[:franchise_id]
+    @team_name = data[:team_name]
+    @abbreviation = data[:abbreviation]
+    @stadium = data[:stadium]
+    @link = data[:link]
+  end
+end
