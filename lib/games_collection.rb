c52f035532d0fc622ba496b51467a6cd71fc66a5
James Fox-Collis <jlfoxcollis@gmail.com>
2020-10-26 10:02:56 -0700

    refactor game and game_collection initialize

--- lib/games_collection.rb
+++ lib/games_collection.rb
@@ -5,18 +5,11 @@ class GamesCollection
 
   def initialize(file_path)
     @games = []
+    create_games(file_path)
+  end
+
+  def create_games(file_path)
     CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
-      game_id = row[:game_id]
-      season = row[:season]
-      type = row[:type]
-      date_time = row[:date_time]
-      away_team_id = row[:away_team_id]
-      home_team_id = row[:home_team_id]
-      away_goals = row[:away_goals].to_i
-      home_goals = row[:home_goals].to_i
-      venue = row[:venue]
-      venue_link = row[:venue_link]
-      @games << Game.new(game_id,season,type,date_time,away_team_id,home_team_id,away_goals,home_goals,venue,venue_link)
-    end
+    @games << Game.new(row)
   end
 end
