66021c0eaf42dcb089c3a2fe056fc97eecb31ddf
James Fox-Collis <jlfoxcollis@gmail.com>
2020-10-26 10:15:51 -0700

    fixed error on games_collection, missing end

--- lib/games_collection.rb
+++ lib/games_collection.rb
@@ -10,6 +10,7 @@ class GamesCollection
 
   def create_games(file_path)
     CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
-    @games << Game.new(row)
+      @games << Game.new(row)
+    end
   end
 end
