326190efac756a5d53f7b1e148a5c07d35a32498
Saundra <saundracatalina@gmail.com>
2020-10-26 13:20:02 -0600

    Add percentage_away_wins method-passes test

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -48,4 +48,25 @@ class StatTracker
     end
     percentage = (home_wins.to_f / (home_wins + away_wins)* 100).round(2)
   end
+
+  def percentage_away_wins
+    home_wins = 0
+    away_wins = 0
+    CSV.foreach(game_teams, headers: true, header_converters: :symbol) do |row|
+      next if row [:result] == "LOSS"
+      if row[:hoa] == "away"
+        away_wins += 1
+      elsif row[:hoa] == "home"
+        home_wins += 1
+      end
+    end
+    percentage = (away_wins.to_f / (home_wins + away_wins)* 100).round(2)
+  end
+
+
+  # possible helper methods:
+  # total Games
+  # away_wins
+  # home_wins
+  # percentage_of_games method --pass in enumerator and total games
 end
