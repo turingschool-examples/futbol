996b53a484d7d6cd20fa9d5dbbf2f252c77f43d3
James Fox-Collis <jlfoxcollis@gmail.com>
2020-10-26 10:07:45 -0700

    finished refactoring game test setup

--- test/game_test.rb
+++ test/game_test.rb
@@ -1,5 +1,6 @@
 require_relative './test_helper'
 require './lib/game'
+require 'csv'
 
 class GameTest < Minitest::Test
 
@@ -12,15 +13,15 @@ class GameTest < Minitest::Test
 
   def test_it_exists_and_has_attributes
     game = @games.first
-    assert_instance_of Game, @game
-    assert_equal 2012030221, @game.game_id
-    assert_equal 20122013, @game.season
-    assert_equal "Postseason", @game.type
-    assert_equal "5/16/13", @game.date_time
-    assert_equal 3, @game.away_team_id
-    assert_equal 6, @game.home_team_id
-    assert_equal 2, @game.away_goals
-    assert_equal 3, @game.home_goals
-    assert_equal "Toyota Stadium", @game.venue
+    assert_instance_of Game, game
+    assert_equal 2012030221, game.game_id
+    assert_equal 20122013, game.season
+    assert_equal "Postseason", game.type
+    assert_equal "5/16/13", game.date_time
+    assert_equal 3, game.away_team_id
+    assert_equal 6, game.home_team_id
+    assert_equal 2, game.away_goals
+    assert_equal 3, game.home_goals
+    assert_equal "Toyota Stadium", game.venue
   end
 end
