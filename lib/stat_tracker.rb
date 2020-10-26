4f74cedfb8467c5701c6d2af603f0c073a4e7e8e
Ben Fulton <benjamin.fulton99@gmail.com>
2020-10-26 15:00:24 -0600

    Add passing team_info test suite

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -247,5 +247,20 @@ class StatTracker
     match.teamname
   end
   
+  def team_info(arg_id)
+    queried_team = Hash.new
+    @teams.find do |team|
+      
+      if team.team_id == arg_id
+      queried_team["team_id"] = team.team_id
+      queried_team["franchise_id"] = team.franchiseid
+      queried_team["team_name"] = team.teamname
+      queried_team["abbreviation"] = team.abbreviation
+      queried_team["link"] = team.link
+      end
+    end
+    
+  queried_team
+  end
 
 end
