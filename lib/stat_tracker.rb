bb70416b9e997ab881043f932db0fff9ec49b8cd
Max Ribbans <ribbans.max@gmail.com>
2020-10-26 16:31:47 -0400

    Update self.from_csv to take hash of locations, and stores data in @all_data as hash

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,19 +1,25 @@
 require 'CSV'
 class StatTracker
-  attr_reader :data
+  attr_reader :all_data
 
   def initialize(data)
-    @data = data
+    @all_data = data
   end
 
   def self.from_csv(locations)
-    @data = []
-    CSV.foreach(locations, headers: true, header_converters: :symbol) do |row|
-      @data << row
-    end
 
-    stat_tracker = StatTracker.new(@data)
-    require 'pry'; binding.pry
+    @all_data = {}
+
+    locations.each_pair do |key, location|
+        data = []
+        CSV.foreach(location, headers: true, header_converters: :symbol) do |row|
+        
+        
+            data << row
+        end
+        @all_data[key] = data
+    end
+    stat_tracker = StatTracker.new(@all_data)
   end
 
 end
