c009ba68968758d5be0266a6d683a38351ed61b3
Ely Hess <ely.hess@me.com>
2020-10-26 18:32:37 -0600

    Change file pathing

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,4 +1,7 @@
 require 'csv'
+require_relative './game_manager'
+require_relative './team_manager'
+require_relative './game_teams_manager'
 
 class StatTracker
   attr_reader :games,
@@ -9,9 +12,19 @@ class StatTracker
     @games = file_locations[:games]
     @teams = file_locations[:teams]
     @game_teams = file_locations[:game_teams]
+    @game_manager = GameManager.new(@games)
   end
 
-  def self.from_csv(data)
-    StatTracker.new(data)
+  def self.from_csv(file_locations)
+    StatTracker.new(file_locations)
   end
+
+  # def game_manager
+  #   @game_manager ||= GameManager.new(@games)
+  # end
+  #
+  # def highest_total_score
+  #   @game_manager.all
+  #   @game_manager.highest_total_score
+  # end
 end
