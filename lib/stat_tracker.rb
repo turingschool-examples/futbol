4db0e27d87e561f4e2b0ff2a72662a7297538fdb
Adam Etzion <adametzion90@gmail.com>
2020-10-26 18:38:16 -0600

    Add winningest and worst coach methods

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -246,6 +246,36 @@ class StatTracker
     end
     match.teamname
   end
-  
 
+  def game_teams_by_coach
+    @game_teams.group_by do |game|
+      game.head_coach
+    end
+  end
+
+  def winningest_coach(season_id)
+    season_games = @games.find_all { |game| game.season == season_id }
+    game_ids = season_games.map do |game|
+      game.game_id.to_s
+    end
+    win_rate = {}    
+    
+    game_teams_by_coach.map do |coach, games|
+      win_rate[coach] = (games.count {|game| (game.result == "WIN") && game_ids.include?(game.game_id)} / games.count)
+    end
+    win_rate.key(win_rate.values.max)
+  end
+
+  def worst_coach(season_id)
+    season_games = @games.find_all { |game| game.season == season_id }
+    game_ids = season_games.map do |game|
+      game.game_id.to_s
+      end
+    win_rate = {}
+    game_teams_by_coach.map do |coach, games|
+      win_rate[coach] = ((games.count {|game| (game.result == "WIN") && game_ids.include?(game.game_id)}).to_f / (games.count {|game| game_ids.include?(game.game_id)})).round(2)    
+    end
+    # require 'pry'; binding.pry
+    win_rate.key(win_rate.values.reject{|x| x.nan?}.min)
+  end
 end
