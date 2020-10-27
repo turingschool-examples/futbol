81109a8b867f790d08fc512320c3eac4cb942efe
Vanessa Arruda <wanessaarruda@gmail.com>
2020-10-27 15:55:59 -0600

    Add team stats test

--- test/game_statistics_test.rb
+++ test/game_statistics_test.rb
@@ -109,6 +109,14 @@ class GameStatsTest < Minitest::Test
     assert_equal expected, @game2.count_of_games_by_season
   end
 
+  def test_it_can_select_stats_by_header
+    team_id = "3"
+    team_3_goal_sum = 8
+    team_3_stats = @game.team_stats(:team_id, team_id)
+
+    assert_equal team_3_goal_sum, @game.sum_data(:goals, team_3_stats)
+  end
+
   def test_it_can_average_goals_by_season
     expected = {"20122013" => 3.78}
     assert_equal expected, @game2.average_goals_by_season
