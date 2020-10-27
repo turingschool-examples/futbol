26ce2239f84e4cdf8caf8ad75958e4bd6752ca39
Jesus Quezada-Guillen <maj3st1@gmail.com>
2020-10-27 16:42:28 -0500

    Add team_info method to TeamStatistics class.

--- lib/team_statistics.rb
+++ lib/team_statistics.rb
@@ -1,5 +1,14 @@
 class TeamStatistics
-
+  def team_info(teams, team_id)
+    team_data = teams[team_id]
+    new_hash = {
+      "franchise_id" => team_data.franchiseId.to_s,
+      "team_name" => team_data.teamName,
+      "abbreviation" => team_data.abbreviation,
+      "link" => team_data.link,
+      "team_id" => team_id
+    }
+  end
 
 
 
