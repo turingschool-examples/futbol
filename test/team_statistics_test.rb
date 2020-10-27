71028a2ac57a2fafb692b63b5e94a9e957953cbb
Jesus Quezada-Guillen <maj3st1@gmail.com>
2020-10-27 16:41:29 -0500

    Add test for team_info method of TeamStatistics class.

--- test/team_statistics_test.rb
+++ test/team_statistics_test.rb
@@ -23,4 +23,19 @@ class TeamStatisticsTest < Minitest::Test
   def test_it_exists
     assert_instance_of TeamStatistics, @team_statistics
   end
+
+  def test_team_info
+  expected16 = {"franchise_id" => "11", "team_name" => "New England Revolution", "abbreviation" => "NE", "link" => "/api/v1/teams/16", "team_id" => "16"}
+  assert_equal expected16, @team_statistics.team_info(@object_data.teams, "16")
+  expected18 = {
+    "team_id" => "18",
+    "franchise_id" => "34",
+    "team_name" => "Minnesota United FC",
+    "abbreviation" => "MIN",
+    "link" => "/api/v1/teams/18"
+  }
+  assert_equal expected18, @team_statistics.team_info(@object_data.teams, "18")
+end
+
+
 end
