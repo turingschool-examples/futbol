a2991013fc66d57f54a70b09c23d3d85ceb92adb
Catherine Dean <catherinemdean15@gmail.com>
2020-10-27 19:16:22 -0600

    put helper methods game_ids_per_season and count_coach_results back, add games_per_coach helper method

--- lib/seasonstats.rb
+++ lib/seasonstats.rb
@@ -11,16 +11,46 @@ class SeasonStats
     @games_table = CSV.parse(File.read(@locations[:games]), headers: true)
   end
 
-  def games_in_season(season)
-    @games_table.find_all do |game|
-      game["season"] == season
+  def game_ids_per_season
+    seasons_and_games = {}
+    @games_table.each do |game|
+      if seasons_and_games[game["season"]]
+        seasons_and_games[game["season"]] << game["game_id"]
+      else seasons_and_games[game["season"]] = [game["game_id"]]
+      end
     end
+    seasons_and_games
+  end
+
+  def games_per_coach
+    coaches_and_games = {}
+    @games_teams_table.each do |game|
+     if coaches_and_games[game["head_coach"]]
+       coaches_and_games[game["head_coach"]] << game
+     else coaches_and_games[game["head_coach"]] = [game]
+     end
+   end
   end
 
 
-  def winningest_coach(season)
+  def count_coach_results(season, result)
+    coaches_and_games = {}
+    game_ids_per_season[season].each do |game_id|
+      @games_teams_table.each do |game|
+      if coaches_and_games[game["head_coach"]] && (game["game_id"] == game_id) && (game["result"] == result)
+        coaches_and_games[game["head_coach"]] += 1
+      else (game["game_id"] == game_id) && (game["result"] == result)
+        coaches_and_games[game["head_coach"]] = 1
+      end
+      end
+    end
     require "pry"; binding.pry
-    games_in_season(season)
+  end
+
+  def winningest_coach(season)
+    count_coach_results(season, "WIN")
+    #coach_wins(season, WIN)
+    #coach_wins[key].
   end
 
 end
