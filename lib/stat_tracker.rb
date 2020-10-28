054f479d4bc7b43a0f9eb4d7816b5ce40cc75e40
Cydnee Owens <cks410@yahoo.com>
2020-10-28 10:05:47 -0600

    Refactor stat_tracker class methods

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,17 +1,15 @@
 require 'csv'
 
 class StatTracker
-  class << self
-    def from_csv(locations)
-      all_data = {}
-      locations.each do |file_name, data|
-        all_data[file_name] = read_data(data)
-      end
-      all_data
+  def self.from_csv(locations)
+    all_data = {}
+    locations.each do |file_name, data|
+      all_data[file_name] = read_data(data)
     end
+    all_data
+  end
 
-    def read_data(data)
-      CSV.parse(File.read(data), headers: true)
-    end
+  def self.read_data(data)
+    CSV.parse(File.read(data), headers: true)
   end
 end
