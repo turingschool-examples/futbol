bd179a0fe21b721059407dfd104b41ff0576638b
Max Ribbans <ribbans.max@gmail.com>
2020-10-28 18:45:38 -0400

    Add attr_reader for attributes

--- lib/game.rb
+++ lib/game.rb
@@ -1,13 +1,21 @@
 class Game
+  attr_reader :parent,
+              :game_id,
+              :season,
+              :type,
+              :away_team_id,
+              :home_team_id,
+              :away_goals,
+              :home_goals
 
   def initialize(row, parent)
     @parent = parent
     @game_id = row[:game_id]
     @season = row[:season]
     @type = row[:type]
-    @date_time = row[:date_time].to_date
     @away_team_id = row[:away_team_id].to_i
     @home_team_id = row[:home_team_id].to_i
     @away_goals = row[:away_goals].to_i
     @home_goals = row[:home_goals].to_i
+  end
 end
