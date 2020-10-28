7ced537f8414e86dd9f075c60f7386aaf0148021
Jesus Quezada-Guillen <maj3st1@gmail.com>
2020-10-27 21:41:28 -0500

    Add worst_season method and add_wins_and_losses method to TeamStatistics class.

--- lib/team_statistics.rb
+++ lib/team_statistics.rb
@@ -10,6 +10,33 @@ class TeamStatistics
     }
   end
 
+  def worst_season(games, team_id)
+    season_wins_and_losses = add_wins_and_losses(games, team_id)
+    highest_win_to_loss = season_wins_and_losses.min_by do |season, win_and_loss_hash|
+      win_and_loss_hash[:wins].to_f / win_and_loss_hash[:total]
+    end
+    highest_win_to_loss[0].to_s
+  end
+
+  def add_wins_and_losses(games, team_id)
+    win_percent_by_season = {}
+    games.each do |game_id, game|
+      win_percent_by_season[game.season] ||= {wins: 0, total: 0}
+      home_team_won = game.home_goals > game.away_goals
+      team_is_home = game.home_team_id == team_id.to_i
+      is_draw = game.home_goals == game.away_goals
+      team_is_playing = team_is_home || game.away_team_id == team_id.to_i
+      if team_is_playing
+        win_percent_by_season[game.season][:total] += 1
+        if (home_team_won == team_is_home) && !is_draw
+          win_percent_by_season[game.season][:wins] += 1
+        end
+      end
+     end
+     win_percent_by_season
+  end
+
+
 
 
 
