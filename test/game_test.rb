8f2fefa81a001e6a10795b4ee330874e5f85cdb9
Max Ribbans <ribbans.max@gmail.com>
2020-10-28 18:27:24 -0400

    Add setup and test it exists and has first two attributes

--- test/game_test.rb
+++ test/game_test.rb
@@ -1,9 +1,21 @@
+require 'CSV'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'mocha/minitest'
 require './lib/stat_tracker'
 require './lib/games_repo'
-require './lib/games'
+require './lib/game'
 
 class GameTest < Minitest::Test
+  def setup
 
+    row = CSV.readlines('./data/games.csv', headers: :true, header_converters: :symbol)[0]
+    @parent = mock()
+    @game1 = Game.new(row, @parent)
+  end
+
+  def test_it_exists_and_has_attributes
+    assert_equal 2012030221, @game1.game_id
+    assert_equal 20122013, @game1.season
+  end
 end
