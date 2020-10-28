fb10ba9d2a9a8a1de8931e65cfa790228af34b8d
Max Ribbans <ribbans.max@gmail.com>
2020-10-28 18:44:44 -0400

    Test all attributes exists

--- test/game_test.rb
+++ test/game_test.rb
@@ -10,12 +10,18 @@ class GameTest < Minitest::Test
   def setup
 
     row = CSV.readlines('./data/games.csv', headers: :true, header_converters: :symbol)[0]
-    @parent = mock()
+    @parent = mock('game_repo')
     @game1 = Game.new(row, @parent)
   end
 
   def test_it_exists_and_has_attributes
-    assert_equal 2012030221, @game1.game_id
-    assert_equal 20122013, @game1.season
+    assert_equal "2012030221", @game1.game_id
+    assert_equal "20122013", @game1.season
+    assert_equal "Postseason", @game1.type
+    assert_equal 3, @game1.away_team_id
+    assert_equal 6, @game1.home_team_id
+    assert_equal 2, @game1.away_goals
+    assert_equal 3, @game1.home_goals
+    assert mock(), @game1.parent
   end
 end
