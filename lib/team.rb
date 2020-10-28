d7853bea86b0220ce85fee9d680d06d0ae74a2b2
Michael Foy <foymikek@gmail.com>
2020-10-28 15:45:20 -0600

    Refactored model files

--- lib/team.rb
+++ lib/team.rb
@@ -1,9 +1,14 @@
 class Team
+  attr_reader :teamName,
+              :franchiseId,
+              :abbreviation,
+              :link,
+              :Stadium
   def initialize(row)
-    @franchiseId = row[1].to_i,
-    @teamName = row[2],
-    @abbreviation = row[3],
-    @Stadium = row[4],
+    @franchiseId = row[1]
+    @teamName = row[2]
+    @abbreviation = row[3]
+    @Stadium = row[4]
     @link = row[5]
   end
 end
