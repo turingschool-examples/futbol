250d345aad0518aa9d01490e552b7853bda4c486
Michael Foy <foymikek@gmail.com>
2020-10-28 15:32:15 -0600

    Add passing test_it_exists

---
+++ test/season_statistics_test.rb
@@ -0,0 +1,26 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/game_statistics'
+require './lib/object_data'
+require './lib/stat_tracker'
+require './lib/season_statistics'
+
+class SeasonStatisticsTest < Minitest::Test
+  def setup
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+    @stat_tracker = StatTracker.from_csv(locations)
+    @object_data ||= ObjectData.new(@stat_tracker)
+    @season_statistics = SeasonStatistics.new
+  end
+
+  def test_it_exists
+    assert_instance_of SeasonStatistics, @season_statistics
+  end
+end
