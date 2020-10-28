ca58901edfdc86cf32e25a52cc90eb51aacda3a6
Cydnee Owens <cks410@yahoo.com>
2020-10-28 08:17:11 -0600

    Add helper test for games per season total

--- test/team_statistics_test.rb
+++ test/team_statistics_test.rb
@@ -81,8 +81,12 @@ class TeamStatisticsTest < Minitest::Test
   end
 
   def test_it_can_find_total_games_per_season
-    skip
-    assert_equal '5', @team_statistics.total_games_by_game_id(@team_id)
+    # use a stub here when not using dummy csv
+    expected = {
+                "20122013"=>9.0,
+                "20132014"=>3.0
+              }
+    assert_equal expected, @team_statistics.total_games_by_game_id(@team_id)
   end
 
   def test_it_can_find_winning_games_per_season
