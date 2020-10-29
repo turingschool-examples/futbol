c97ce46b722c67e655957037d186a3c4f4c49291
Caryn Keffer <carynkeffer@gmail.com>
2020-10-28 20:46:34 -0400

    refactor seasonstats  tests and seasonstats class

--- lib/seasonstats.rb
+++ lib/seasonstats.rb
@@ -22,48 +22,54 @@ class SeasonStats
     seasons_and_games
   end
 
-  def games_per_coach
-    coaches_and_games = {}
-    @games_teams_table.each do |game|
-     if coaches_and_games[game["head_coach"]]
-       coaches_and_games[game["head_coach"]] << game
-     else coaches_and_games[game["head_coach"]] = [game]
-     end
-   end
-   coaches_and_games
-  end
-
-  def results_per_coach_per_season(season)
-    coaches_and_results_per_season = {}
-    games_per_coach.each do |coach, games|
-      games.each do |game|
-          require "pry"; binding.pry
-        if coaches_and_results_per_season[coach] && game_ids_per_season[season].include?(game["game_id"])
-          coaches_and_results_per_season[coach] << game["result"]
-        elsif game_ids_per_season[season].include?(game["game_id"])
-          coaches_and_results_per_season[coach] = [game["result"]]
-        end
+  def games_in_season(season)
+    @games_teams_table.find_all do |game|
+      game_ids_per_season[season].include?(game["game_id"])
       end
-    end
-    coaches_and_results_per_season
   end
 
-  def count_coach_results(season, result)
-    coaches_and_results = {}
-      results_per_coach_per_season.each do |coach, results|
-        results.count do |item|
-          item == result
-        end
-      end
-    # require "pry"; binding.pry
-  end
+  # def games_per_coach
+  #   coaches_and_games = {}
+  #   @games_teams_table.each do |game|
+  #    if coaches_and_games[game["head_coach"]]
+  #      coaches_and_games[game["head_coach"]] << game
+  #    else coaches_and_games[game["head_coach"]] = [game]
+  #    end
+  #  end
+  #  coaches_and_games
+  # end
 
-  def winningest_coach(season)
-    # require "pry"; binding.pry
-    most_wins = count_coach_results(season, "WIN")
-
-    #coach_wins(season, WIN)
-    #coach_wins[key].
-  end
+  # def results_per_coach_per_season(season)
+  #   coaches_and_results_per_season = {}
+  #   games_per_coach.each do |coach, games|
+  #     games.each do |game|
+  #         # require "pry"; binding.pry
+  #       if coaches_and_results_per_season[coach] && game_ids_per_season[season].include?(game["game_id"])
+  #         coaches_and_results_per_season[coach] << game["result"]
+  #       elsif game_ids_per_season[season].include?(game["game_id"])
+  #         coaches_and_results_per_season[coach] = [game["result"]]
+  #       end
+  #     end
+  #   end
+  #   coaches_and_results_per_season
+  # end
+  #
+  # def count_coach_results(season, result)
+  #   coaches_and_results = {}
+  #     results_per_coach_per_season(season).each do |coach, results|
+  #       results.count do |item|
+  #         item == result
+  #       end
+  #     end
+  #   # require "pry"; binding.pry
+  # end
+  #
+  # def winningest_coach(season)
+  #   # require "pry"; binding.pry
+  #   wins = count_coach_results(season, "WIN")
+  #
+  #   #coach_wins(season, WIN)
+  #   #coach_wins[key].
+  # end
 
 end
--- test/seasonstats_test.rb
+++ test/seasonstats_test.rb
@@ -6,9 +6,9 @@ require './lib/seasonstats'
 class SeasonStatsTest < Minitest::Test
 
   def setup
-    game_path = './data/games.csv'
+    game_path = './data/games_dummy.csv'
     team_path = './data/teams.csv'
-    game_teams_path = './data/game_teams.csv'
+    game_teams_path = './data/game_teams_dummy.csv'
 
     locations = {
       games: game_path,
@@ -24,26 +24,29 @@ class SeasonStatsTest < Minitest::Test
   end
 
   def test_game_ids_per_season
-    expected = { 20122013 => [2012030221, 2012030222, 2012030223]}
-    assert_instance_of Hash, @seasonstats.game_ids_per_season
-    @seasonstats.stubs(:game_ids_per_season).returns(expected)
+    expected = {"20122013"=> ["2012030221", "2012030222", "2012030223", "2012030224", "2012030225", "2012030311", "2012030312", "2012030313", "2012030314", "2012030231", "2012030232", "2012030233", "2012030234", "2012030235", "2012030236", "2012030237", "2012030121", "2012030122", "2012030123", "2012030124", "2012030125", "2012030151", "2012030152", "2012030153", "2012030154", "2012030155", "2012030181", "2012030182", "2012030183", "2012030184", "2012030185", "2012030186", "2012030161", "2012030162", "2012030163", "2012030164", "2012030165", "2012030166", "2012030167", "2012030111", "2012030112", "2012030113", "2012030114", "2012030115", "2012030116", "2012030131", "2012030132", "2012030133", "2012030134", "2012030135", "2012030136", "2012030137", "2012030321", "2012030322"], "20162017"=>["2016030165"], "20152016"=>["2015030311"]}
     assert_equal expected, @seasonstats.game_ids_per_season
   end
 
-  def test_games_per_coach
-    expected = { "Coach Name" => ["the whole line"]}
-    assert_instance_of Hash, @seasonstats.games_per_coach
-    @seasonstats.stubs(:games_per_coach).returns(expected)
-    assert_equal expected, @seasonstats.games_per_coach
+  def test_games_in_season
+    expected = 4
+    assert_equal expected, @seasonstats.games_in_season("20122013").count
   end
 
-  def test_results_per_coach_per_season
-    expected = { "Coach Name" => ["WIN", "TIE", "LOSS"]}
-    assert_instance_of Hash, @seasonstats.results_per_coach_per_season("20122013")
-    @seasonstats.stubs(:results_per_coach_per_season).returns(expected)
-    assert_equal expected, @seasonstats.results_per_coach_per_season("20122013")
-  end
+  # def test_games_per_coach
+  #   expected = { "Coach Name" => ["the whole line"]}
+  #   assert_instance_of Hash, @seasonstats.games_per_coach
+  #   @seasonstats.stubs(:games_per_coach).returns(expected)
+  #   assert_equal expected, @seasonstats.games_per_coach
+  # end
 
+  # def test_results_per_coach_per_season
+  #   expected = { "Coach Name" => ["WIN", "TIE", "LOSS"]}
+  #   assert_instance_of Hash, @seasonstats.results_per_coach_per_season("20122013")
+  #   @seasonstats.stubs(:results_per_coach_per_season).returns(expected)
+  #   assert_equal expected, @seasonstats.results_per_coach_per_season("20122013")
+  # end
+  #
   # def test_count_coach_results
   #   expected = { "Coach Name" => 55,
   #                "Another Coach" => 34}
@@ -51,7 +54,7 @@ class SeasonStatsTest < Minitest::Test
   #   @seasonstats.stubs(:count_coach_results).returns(expected)
   #   assert_equal expected, @seasonstats.count_coach_results("12345", "WIN")
   # end
-
+  #
   # def test_winningest_coach
   #   assert_instance_of String, @seasonstats.winningest_coach("20122013")
   #   @seasonstats.stubs(:winningest_coach).returns("Claude Julien")
