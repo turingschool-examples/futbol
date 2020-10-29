e57e31e09924ab004ec3abb3338002c1b7f436db
Phil McCarthy <philmcc2631@gmail.com>
2020-10-28 20:47:55 -0600

    Update game manager test for new csv handling

--- test/game_manager_test.rb
+++ test/game_manager_test.rb
@@ -4,51 +4,43 @@ require './lib/game_manager'
 
 class GameManagerTest < Minitest::Test
   def setup
-    @game_manager = GameManager.new('./data/games.csv')
+    @game_manager = GameManager.new('./data/games.csv', 'stat_tracker')
   end
 
   def test_it_exists_and_has_attributes
     assert_instance_of GameManager, @game_manager
-    assert_equal [], @game_manager.games
+    assert_equal Array, @game_manager.games.class
   end
 
   def test_it_can_add_array_of_all_game_objects
-    @game_manager.all
     assert_instance_of Game, @game_manager.games.first
   end
 
   def test_it_calculates_highest_total_score
-    @game_manager.all
     assert_equal 11, @game_manager.highest_total_score
   end
 
   def test_it_calculates_lowest_total_score
-    @game_manager.all
     assert_equal 0, @game_manager.lowest_total_score
   end
 
   def test_it_gives_percentage_of_home_wins
-    @game_manager.all
     assert_equal 0.44, @game_manager.percentage_home_wins
   end
 
   def test_it_gives_percentage_of_visitor_wins
-    @game_manager.all
     assert_equal 0.36, @game_manager.percentage_visitor_wins
   end
 
   def test_it_gives_percentage_of_ties
-    @game_manager.all
     assert_equal 0.20, @game_manager.percentage_ties
   end
 
   def test_it_gives_game_count
-    @game_manager.all
     assert_equal 806, @game_manager.game_count("20122013")
   end
 
   def test_it_gives_games_by_season_count
-    @game_manager.all
     expected = {
                 "20122013"=>806,
                 "20162017"=>1317,
@@ -61,18 +53,15 @@ class GameManagerTest < Minitest::Test
   end
 
   def test_it_gives_average_goals_per_game
-    @game_manager.all
     assert_equal 4.22, @game_manager.average_goals_per_game
   end
 
   def test_it_gives_goal_count_by_season
-    @game_manager.all
     assert_equal 3322, @game_manager.goal_count('20122013')
 
   end
 
   def test_it_gives_average_goals_by_season
-    @game_manager.all
     expected = {
       "20122013"=>4.12,
       "20162017"=>4.23,
@@ -83,6 +72,4 @@ class GameManagerTest < Minitest::Test
     }
     assert_equal expected, @game_manager.average_goals_by_season
   end
-
-
 end
