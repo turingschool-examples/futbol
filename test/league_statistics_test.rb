009e4c6238036a0df8c8a5eac998107be755672e
Yesi Meza <yesi.meza10@gmail.com>
2020-10-29 11:56:57 -0600

    create helper tests

--- test/league_statistics_test.rb
+++ test/league_statistics_test.rb
@@ -20,46 +20,104 @@ class LeagueStatisticsTest < Minitest::Test
   def test_it_exists
     assert_instance_of LeagueStatistics, @league_statistics
   end
-
+# League Statistic Tests
   def test_it_can_count_number_of_teams
     assert_equal 32, @league_statistics.count_of_teams
   end
 
   def test_it_knows_highest_average_goals_scored_across_season
-      #
-    	# Name of the team with the highest average
-      # number of goals scored per game across all seasons.
+	# Name of the team with the highest average number of goals scored per game across all seasons.
     assert_equal 'FC Dallas', @league_statistics.best_offense
   end
 
   def test_it_knows_lowest_average_goals_scored_across_season
-      #
-      # Name of the team with the highest average
-      # number of goals scored per game across all seasons.
+  # Name of the team with the highest average number of goals scored per game across all seasons.
     assert_equal 'Sporting Kansas City', @league_statistics.worst_offense
   end
 
   def test_it_knows_highest_average_away
-    	# Name of the team with the highest average score
-      # per game across all seasons when they are away.
+  # Name of the team with the highest average score per game across all seasons when they are away.
     assert_equal 'Real Salt Lake', @league_statistics.highest_scoring_visitor
   end
 
   def test_it_knows_highest_average_home
-      # Name of the team with the highest average score
-      # per game across all seasons when they are away.
+  # Name of the team with the highest average score per game across all seasons when they are away.
     assert_equal 'Real Salt Lake', @league_statistics.highest_scoring_home_team
   end
 
   def test_it_knows_lowest_average_away
-    	# Name of the team with the highest average score
-      # per game across all seasons when they are away.
+  # Name of the team with the highest average score per game across all seasons when they are away.
     assert_equal 'Sporting Kansas City', @league_statistics.lowest_scoring_visitor
   end
 
   def test_it_knows_lowest_average_home
-      # Name of the team with the highest average score
-      # per game across all seasons when they are away.
+  # Name of the team with the highest average score per game across all seasons when they are away.
     assert_equal 'Sporting Kansas City', @league_statistics.lowest_scoring_home_team
+  end
+
+# Helpe Tests
+  def test_it_can_find_highest_goal
+    skip
+    assert_equal 5, @league_statistics.find_highest_goal
+  end
+
+  def test_it_can_find_highest_goal
+    skip
+    assert_equal 5, @league_statistics.find_highest_goal
+  end
+
+  def test_it_can_find_highest_goal
+    skip
+    assert_equal 5, @league_statistics.find_highest_goal
+  end
+
+  def test_it_can_find_highest_goal
+    skip
+    assert_equal 5, @league_statistics.find_highest_goal
+  end
+
+  def test_it_can_find_highest_goal
+    skip
+    assert_equal 5, @league_statistics.find_highest_goal
+  end
+
+  def test_it_can_find_highest_goal
+    skip
+    assert_equal 5, @league_statistics.find_highest_goal
+  end
+
+  def test_it_can_find_highest_goal
+    skip
+    assert_equal 5, @league_statistics.find_highest_goal
+  end
+
+  def test_it_can_find_highest_goal
+    skip
+    assert_equal 5, @league_statistics.find_highest_goal
+  end
+
+  def test_it_can_find_highest_goal
+    skip
+    assert_equal 5, @league_statistics.find_highest_goal
+  end
+
+  def test_it_can_find_highest_goal
+    skip
+    assert_equal 5, @league_statistics.find_highest_goal
+  end
+
+  def test_it_can_find_highest_goal
+    skip
+    assert_equal 5, @league_statistics.find_highest_goal
+  end
+
+  def test_it_can_find_highest_goal
+    skip
+    assert_equal 5, @league_statistics.find_highest_goal
+  end
+  
+  def test_it_can_find_highest_goal
+    skip
+    assert_equal 5, @league_statistics.find_highest_goal
   end
 end
