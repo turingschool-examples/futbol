9a7f4267b2254aa033392d032626a9af55adec0d
Joseph Budina <joebud3@gmail.com>
2020-10-28 19:57:46 -0600

    Add create_games method instances game

--- lib/games_repo.rb
+++ lib/games_repo.rb
@@ -1,8 +1,18 @@
+require 'CSV'
+require './lib/game'
 class GamesRepo
   attr_reader :parent, :games 
   
   def initialize(path, parent)
     @parent = parent 
     @games = create_games(path)
+  end
+
+  def create_games(path)
+    rows = CSV.readlines('./data/games.csv', headers: :true , header_converters: :symbol)
+
+    rows.map do |row|
+      Game.new(row, self)
+    end
   end
 end
