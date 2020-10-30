d66a7cd7c646bacbcb965f54b31007d2f7665ee9
Yesi Meza <yesi.meza10@gmail.com>
2020-10-30 10:19:55 -0600

    created test for goals per team id away

--- test/league_statistics_test.rb
+++ test/league_statistics_test.rb
@@ -77,10 +77,10 @@ class LeagueStatisticsTest < Minitest::Test
     assert_equal expected, @league_statistics.total_games_per_team_id_home
   end
 
-  # def test_it_knows_total_goals_per_team_id_away
-  #   expected = {"3"=>5.0, "6"=>11.0, "5"=>1.0, "20"=>4.0, "24"=>6.0}
-  #   assert_equal expected, @league_statistics.total_goals_per_team_id_away
-  # end
+  def test_it_knows_total_goals_per_team_id_away
+    expected = {"3"=>5.0, "6"=>11.0, "5"=>1.0, "20"=>4.0, "24"=>6.0}
+    assert_equal expected, @league_statistics.total_goals_per_team_id_away
+  end
 
   # def test_it_knows_total_goals_per_team_id_home
   #
