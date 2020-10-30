1465e0c70b644e11dcd8346c6e6389b30b8def61
Cydnee Owens <cks410@yahoo.com>
2020-10-29 20:19:25 -0600

    Update formatting

--- test/game_team_test.rb
+++ test/game_team_test.rb
@@ -7,21 +7,21 @@ require 'mocha/minitest'
 class GameTeamTest < Minitest::Test
   def setup
     data = {
-            'game_id'      => '2012030221',
-            'team_id'       => '3',
-            'HoA'         => 'away',
-            'result'    => 'LOSS',
-            'settled_in' => 'OT',
-            'head_coach' => 'John Tortorella',
-            'goals'   => '2',
-            'shots'   => '8',
-            'tackles'        => '44',
-            'pim'   => '8',
+            'game_id'           => '2012030221',
+            'team_id'           => '3',
+            'HoA'               => 'away',
+            'result'            => 'LOSS',
+            'settled_in'        => 'OT',
+            'head_coach'        => 'John Tortorella',
+            'goals'             => '2',
+            'shots'             => '8',
+            'tackles'           => '44',
+            'pim'               => '8',
             'powerPlayOpportunities'   => '3',
-            'powerPlayGoals'   => '0',
-            'faceOffWinPercentage'        => '44.8',
-            'giveaways'   => '17',
-            'takeaways'   => '7'
+            'powerPlayGoals'    => '0',
+            'faceOffWinPercentage'     => '44.8',
+            'giveaways'         => '17',
+            'takeaways'         => '7'
           }
     loader = mock('GameTeamLoader')
     @game_team = GameTeam.new(data, loader)
