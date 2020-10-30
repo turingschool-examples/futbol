ab3f357412a672aa1109744a2cdef94a730221b7
Cydnee Owens <cks410@yahoo.com>
2020-10-29 18:13:10 -0600

    Update stat_tracker to load files

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,8 +1,19 @@
+require 'csv'
+
 class StatTracker
   def self.from_csv(locations)
     StatTracker.new(locations)
   end
 
+  attr_reader :argument
+
   def initialize(locations)
+    @load_files(locations)
+  end
+
+  def load_files(locations)
+    @games = Game.new(locations[:games], self)
+    @teams = Team.new(locations[:teams], self)
+    @game_teams = GameTeam.new(locations[:game_teams], self)
   end
 end
