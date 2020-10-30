231fa1c65a568eda441bb451cb1ab4305b1d9790
Saundra <saundracatalina@gmail.com>
2020-10-30 11:33:20 -0600

    Add manager and model dirs with files

--- lib/model/game_team.rb
+++ lib/model/game_team.rb
@@ -1,20 +1,20 @@
-class GameTeams
-  attr_reader :game_id, 
-              :team_id, 
-              :hoa, 
-              :result, 
-              :settled_in, 
-              :head_coach, 
-              :goals, 
-              :shots, 
+class GameTeam
+  attr_reader :game_id,
+              :team_id,
+              :hoa,
+              :result,
+              :settled_in,
+              :head_coach,
+              :goals,
+              :shots,
               :tackles,
-              :pim, 
-              :powerplayopportunities, 
-              :powerplaygoals, 
-              :faceoffwinpercentage, 
-              :giveaways, 
+              :pim,
+              :powerplayopportunities,
+              :powerplaygoals,
+              :faceoffwinpercentage,
+              :giveaways,
               :takeaways
-              
+
   def initialize(data)
     @game_id = data[:game_id].to_i
     @team_id = data[:team_id].to_i
--- lib/model/team.rb
+++ lib/model/team.rb
@@ -1,17 +1,17 @@
-class Teams
-  attr_reader :team_id, 
-              :franchise_id, 
+class Team
+  attr_reader :team_id,
+              :franchise_id,
               :team_name,
-              :abbreviation, 
-              :stadium, 
+              :abbreviation,
+              :stadium,
               :link
-              
+
   def initialize(data)
     @team_id = data[:team_id].to_i
     @franchise_id = data[:franchiseId].to_i
     @team_name = data[:teamName]
     @abbreviation = data[:abbreviation]
     @stadium = data[:stadium]
-    @link = data[:link]    
+    @link = data[:link]
   end
 end
