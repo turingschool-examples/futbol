5dd57efc18b73dadb3ac48312ae75b320031b3a8
Ely Hess <ely.hess@me.com>
2020-10-31 09:29:00 -0600

    Add team_goal_ratio test

--- test/game_teams_manager_test.rb
+++ test/game_teams_manager_test.rb
@@ -84,4 +84,10 @@ class GameTeamsManagerTest < Minitest::Test
     assert_equal "John Tortorella", @game_teams_manager.worst_coach([2012030222, 2012030223])
   end
 
+  def test_team_goal_ratio
+    expected = {3=>{:goals=>3, :shots=>15}, 6=>{:goals=>5, :shots=>16}}
+    assert_equal expected, @game_teams_manager.team_goal_ratio([2012030222, 2012030223])
+  end
+
+
 end
