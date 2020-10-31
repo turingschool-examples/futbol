8d7a20a51591b9c74808a638ee7ded278a1624f6
Aiden Mendez <aidenmendez@gmail.com>
2020-10-31 16:07:10 -0600

    Test verify in season

--- test/game_teams_manager_test.rb
+++ test/game_teams_manager_test.rb
@@ -2,10 +2,13 @@ require_relative './test_helper'
 
 class GameTeamsManagerTest < Minitest::Test
   def setup
-    locations = {}
-    controller = StatTracker.new(locations)
-    location = './data/fixture_files/game_teams.csv'
-    @game_teams_manager = GameTeamsManager.get_data(location, controller)
+    locations = {
+      games: './data/fixture_files/games.csv',
+      teams: './data/teams.csv',
+      game_teams: './data/fixture_files/game_teams.csv'
+    }
+    controller = StatTracker.from_csv(locations)
+    @game_teams_manager = controller.game_teams_manager
   end
 
   def test_it_exists	
@@ -18,7 +21,14 @@ class GameTeamsManagerTest < Minitest::Test
 
   def test_coaches
     coach_hash = {:games => 0, :wins => 0}
-    all_coaches = @game_teams_manager.coaches
+
+    all_coaches = @game_teams_manager.coaches_by_season(20152016)
+
     assert_equal coach_hash, all_coaches["Jack Capuano"]
+  end
+
+  def test_verify_in_season
+    assert @game_teams_manager.verify_in_season(20152016, 2015030143)
+    assert_equal false, @game_teams_manager.verify_in_season(20152016, 2013020293)
   end
 end
