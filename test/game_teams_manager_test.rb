2e1b69b5bcc55a1623e16d630484f3f59a374cb1
Aiden Mendez <aidenmendez@gmail.com>
2020-10-31 13:23:55 -0600

    Test coaches

--- test/game_teams_manager_test.rb
+++ test/game_teams_manager_test.rb
@@ -15,4 +15,10 @@ class GameTeamsManagerTest < Minitest::Test
   def test_winningest_coach
     assert_equal "Dan Bylsma", @game_teams_manager.winningest_coach(20152016)
   end
+
+  def test_coaches
+    coach_hash = {:games => 0, :wins => 0}
+    all_coaches = @game_teams_manager.coaches
+    assert_equal coach_hash, all_coaches["Jack Capuano"]
+  end
 end
