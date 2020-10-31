cf3e15400647a721a19c817ffb2a03c39a532c8e
Ely Hess <ely.hess@me.com>
2020-10-31 17:08:10 -0600

    Fix team_info test

--- test/team_test.rb
+++ test/team_test.rb
@@ -5,8 +5,8 @@ class TeamTest < Minitest::Test
 
   def setup
     @team_info = {"team_id" =>"1",
-                  "franchise_id" =>"23",
-                  "team_name" =>"Atlanta United",
+                  "franchiseId" =>"23",
+                  "teamName" =>"Atlanta United",
                   "abbreviation" =>"ATL",
                   "link"=> "/api/v1/teams/1"
                 }
@@ -15,7 +15,7 @@ class TeamTest < Minitest::Test
 
   def test_it_exists_and_has_attributes
     assert_instance_of Team, @team
-    assert_equal 1, @team.team_id
+    assert_equal "1", @team.team_id
     assert_equal "23", @team.franchise_id
     assert_equal "Atlanta United", @team.team_name
     assert_equal "ATL", @team.abbreviation
