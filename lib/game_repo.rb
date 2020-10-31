8889edb995de320f5b75f4d335eb5344aa701a04
Megan Campbell <megan.campbell26@yahoo.com>
2020-10-31 12:36:00 -0600

    Refactor make games

--- lib/game_repo.rb
+++ lib/game_repo.rb
@@ -6,18 +6,7 @@ class GameRepo
   def make_games(games_path)
     games = []
     CSV.foreach(games_path, headers: true, header_converters: :symbol) do |row|
-      game_id = row[:game_id]
-      season = row[:season]
-      type = row[:type]
-      date_time = row[:date_time]
-      away_team_id = row[:away_team_id].to_i
-      home_team_id = row[:home_team_id].to_i
-      away_goals = row[:away_goals].to_i
-      home_goals = row[:home_goals].to_i
-      venue = row[:venue]
-      venue_link = row[:venue_link]
-
-      games << Game.new(game_id, season, type, date_time, away_team_id, home_team_id, away_goals, home_goals, venue, venue_link)
+      games << Game.new(row)
     end
     games
   end
