f6d7f2e3bc5f08c43982a776499d6d17a742c471
Cydnee Owens <cks410@yahoo.com>
2020-10-31 18:19:58 -0600

    reorganize test and helper methods

--- lib/game_team_collection.rb
+++ lib/game_team_collection.rb
@@ -47,7 +47,11 @@ class GameTeamCollection
   def worst_offense
     @stat_tracker.find_team_name(find_lowest_goal_team_id)
   end
-  
+
+  def highest_scoring_visitor
+    @stat_tracker.find_team_name(highest_average_team_id_visitor)
+  end
+
   # League Statistics Helper Methods
   def find_highest_goal_team_id
     @game_teams.max_by {|goal| goal.goals}.team_id
@@ -57,23 +61,15 @@ class GameTeamCollection
     @game_teams.min_by {|goal| goal.goals}.team_id
   end
 
-
-
   def highest_average_team_id_visitor
     highest_visitor = Hash.new {|hash_obj, key| hash_obj[key] = []}
     @stat_tracker.total_goals_per_team_id_away.each do |team_id, num_goals|
       @stat_tracker.total_games_per_team_id_away.each do |id, num_games|
-        if team_id == id
-          highest_visitor[team_id] << (num_goals / num_games).round(2)
-        end
+        highest_visitor[team_id] << (num_goals / num_games).round(2) if team_id == id
       end
     end
      highest_visitor.max_by {|team_id, avg| avg}[0]
    end
-
-  def highest_scoring_visitor
-    @stat_tracker.find_team_name(highest_average_team_id_visitor)
-  end
 
    def highest_average_team_id_home
      highest_home = Hash.new {|hash_obj, key| hash_obj[key] = []}
