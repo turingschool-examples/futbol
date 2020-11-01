40956327f0b2682397c2b337df42fd23e0c02b80
Adam Etzion <adametzion90@gmail.com>
2020-11-01 11:26:33 -0700

    ADD Game_teams.rb update to connect to game_teams repo

--- lib/game_teams.rb
+++ lib/game_teams.rb
@@ -15,21 +15,21 @@ class GameTeams
                 :giveaways,
                 :takeaways
 
-    def initialize(game_id,team_id,hoa,result,settled_in,head_coach,goals,shots,tackles,pim,powerPlayOpportunities,powerPlayGoals,faceOffWinPercentage,giveaways,takeaways)
-        @game_id = game_id
-        @team_id = team_id
-        @hoa = hoa
-        @result = result
-        @settled_in = settled_in
-        @head_coach = head_coach
-        @goals = goals
-        @shots = shots
-        @tackles = tackles
-        @pim = pim
-        @powerPlayOpportunities = powerPlayOpportunities
-        @powerPlayGoals = powerPlayGoals
-        @faceOffWinPercentage = faceOffWinPercentage
-        @giveaways = giveaways
-        @takeaways = takeaways
-    end
+    def initialize(row)
+        @game_id = row[:game_id]
+        @team_id = row[:team_id]
+        @hoa = row[:hoa]
+        @result = row[:result]
+        @settled_in = row[:settled_in]
+        @head_coach = row[:head_coach]
+        @goals = row[:goals].to_i
+        @shots = row[:shots].to_i
+        @tackles = row[:tackles].to_i
+        @pim = row[:pim].to_i
+        @powerPlayOpportunities = row[:powerPlayOpportunities].to_i
+        @powerPlayGoals = row[:powerPlayGoals].to_i
+        @faceOffWinPercentage = row[:faceOffWinPercentage].to_f
+        @giveaways = row[:giveaways].to_i
+        @takeaways = row[:takeaways].to_i
+     end
 end
