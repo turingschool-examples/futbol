af8ef35e574f2d1c262984b9e1c6fb9472214f10
Grayson Myers <graymyers10@gmail.com>
2020-11-02 14:46:25 -0700

    Change GameTeam initialization index type from integer to string

--- lib/game_team.rb
+++ lib/game_team.rb
@@ -8,20 +8,14 @@ class GameTeam
 
 
 
+
   def initialize(row)
-    @team_id = row[1].to_i
-    @HoA = row[2]
-    @result = row[3]
-    @settled_in = row[4]
-    @head_coach = row[5]
-    @goals = row[6].to_i
-    @shots = row[7].to_i
-    @tackles = row[8].to_i
-    @pim = row[9].to_i
-    @powerPlayOpportunities = row[10].to_i
-    @powerPlayGoals = row[11].to_i
-    @faceOffWinPercentage = row[12].to_f
-    @giveaways = row[13].to_i
-    @takeaways = row[14].to_i
+    @team_id = row["team_id"].to_i
+    @HoA = row["HoA"]
+    @result = row["result"]
+    @head_coach = row["head_coach"]
+    @goals = row["goals"].to_i
+    @shots = row["shots"].to_i
+    @tackles = row["tackles"].to_i
   end
 end
