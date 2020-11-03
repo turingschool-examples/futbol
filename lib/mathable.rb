07bd6042d61289d66b9010e0abe7dbd93d1c5483
Ely Hess <ely.hess@me.com>
2020-11-03 07:59:15 -0700

    Change id to team_id

--- lib/mathable.rb
+++ lib/mathable.rb
@@ -22,4 +22,27 @@ module Mathable
     end
     win_pct_by_category
   end
+
+  def goals_scored(amount, team_id)
+    if amount == "most"
+      games_by_team(team_id).reduce(0) do |goals, game|
+        if game.away?(team_id)
+          goals = game.away_goals if game.away_goals > goals
+        else
+          goals = game.home_goals if game.home_goals > goals
+        end
+        goals
+      end
+    else
+      games_by_team(team_id).reduce(99) do |goals, game|
+        if game.away?(team_id)
+          goals = game.away_goals if game.away_goals < goals
+        else
+          goals = game.home_goals if game.home_goals < goals
+        end
+        goals
+      end
+    end 
+  end
+
 end
