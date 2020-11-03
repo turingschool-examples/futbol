fb2efb9b4fda22adb924ce8afeab52c84fa47294
Liam Cusack <LCusack8108@gmail.com>
2020-11-02 21:51:47 -0700

    Add test_high

--- test/calculator_test.rb
+++ test/calculator_test.rb
@@ -5,5 +5,11 @@ class CalculatorTest < Minitest::Test
   include Calculator
 
   def setup
+    @hashibl_lector = {"20122013"=> 12, "20132014"=> 1}
+  end
+
+  def test_high
+
+    assert_equal ["20122013", 12], high(@hashibl_lector)
   end
 end
