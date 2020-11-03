f98f5c0794627e42b57e33c5f6599e750a3471b5
Liam Cusack <LCusack8108@gmail.com>
2020-11-03 09:06:11 -0700

    Add test_combine

--- test/calculator_test.rb
+++ test/calculator_test.rb
@@ -6,6 +6,7 @@ class CalculatorTest < Minitest::Test
 
   def setup
     @hashibl_lector = {"20122013"=> 12, "20132014"=> 1}
+    @hashibl_lector2 = {"20122013"=> 16, "20132014"=> 2}
     @each_with_objectable = {"3"=>{:success=>38, :total=>8}, "6"=>{:success=>46, :total=>14}}
     @ble = {"17"=>{:success=>1, :total=>4}}
   end
@@ -38,5 +39,10 @@ class CalculatorTest < Minitest::Test
   def test_win_pct
 
     assert_equal 0.25, win_pct(@ble)
+  end
+
+  def test_combine
+
+    assert_equal ({"20122013"=>1.33, "20132014"=>2.0}), combine(@hashibl_lector, @hashibl_lector2)
   end
 end
