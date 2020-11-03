5773b1ca93b1db02118fde2c9fa1a14d482d3fd6
Cydnee Owens <cks410@yahoo.com>
2020-11-02 19:26:36 -0700

    add percentage mod method to game team game

--- lib/divisable.rb
+++ lib/divisable.rb
@@ -3,4 +3,8 @@ module Divisable
   def average(sum, total)
     (sum / total).round(2)
   end
-end 
+
+  def percentage(sum, total)
+    ((sum / total) * 100).round(2)
+  end
+end
--- lib/game_team_game.rb
+++ lib/game_team_game.rb
@@ -1,8 +1,9 @@
 require_relative './game_team'
 require_relative './game_team_collection'
+require_relative "./divisable"
 
 class GameTeamGame < GameTeamCollection
-
+  include Divisable
   def compare_hoa_to_result(hoa, result)
     @game_teams.count do |game|
       game.HoA == hoa && game.result == result
@@ -14,14 +15,14 @@ class GameTeamGame < GameTeamCollection
   end
 
   def percentage_home_wins
-    (compare_hoa_to_result("home", "WIN") / total_amount_games * 100).round(2)
+    percentage(compare_hoa_to_result("home", "WIN"), total_amount_games)
   end
 
   def percentage_visitor_wins
-    (compare_hoa_to_result("away", "WIN") / total_amount_games * 100).round(2)
+    percentage(compare_hoa_to_result("away", "WIN"), total_amount_games)
   end
 
   def percentage_ties
-    (compare_hoa_to_result("away", "TIE") / total_amount_games  * 100).round(2)
+    percentage(compare_hoa_to_result("away", "TIE"), total_amount_games)
   end
 end
