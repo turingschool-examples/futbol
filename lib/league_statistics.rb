80641000f76dcb6b75fc9205dcf88a3a34e81a20
Joe Ray <jray100.jr@gmail.com>
2021-02-22 19:14:36 -0700

    Added shells for league statictic files

---
+++ lib/league_statistics.rb
@@ -0,0 +1,43 @@
+
+class LeagueStatistics
+
+  # def initialize(argument)
+  #   @argument = argument
+  # end
+
+  def count_of_teams
+    # Description - Total number of teams in the data.
+    # Return Value - Integer
+  end
+
+  def best_offense
+    # Description - Name of the team with the highest average number of goals scored per game across all seasons.
+    # Return Value - String
+  end
+
+  def worst_offense
+    # Description - Name of the team with the lowest average number of goals scored per game across all seasons.
+    # Return Value - String
+  end
+
+  def highest_scoring_visitor
+    # Description - Name of the team with the highest average score per game across all seasons when they are away.
+    # Return Value - String
+  end
+
+  def highest_scoring_home_team
+    # Description - Name of the team with the highest average score per game across all seasons when they are home.
+    # Return Value - String
+  end
+
+  def lowest_scoring_visitor
+    # Description - Name of the team with the lowest average score per game across all seasons when they are a visitor.
+    # Return Value - String
+  end
+
+  def lowest_scoring_home_team
+    # Description - Name of the team with the lowest average score per game across all seasons when they are at home.
+    # Return Value - String
+  end
+
+end
