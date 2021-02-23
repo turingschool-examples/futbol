9aad70d3da6a0d4962a7188390e4aa1518048629
Alex Klick <aklick12@gmail.com>
2021-02-23 13:26:37 -0700

    forgot to save 2 small things

--- test/stat_test.rb
+++ test/stat_test.rb
@@ -7,13 +7,8 @@ class StatTrackerTest < Minitest::Test
     game_path = './data/games.csv'
     team_path = './data/teams.csv'
     game_teams_path = './data/game_teams.csv'
-    
-    locations = {
-      games: game_path,
-      teams: team_path,
-      game_teams: game_teams_path
-    }
-    tracker = StatTracker.from_csv(locations)
+  
+    tracker = StatTracker.from_csv('./data/teams_test.csv')
     refute_equal [], tracker[0][0]
     assert_equal tracker[0][0], {:game_id=>2012030221, :season=>20122013, :type=>"Postseason", :date_time=>"5/16/13", :away_team_id=>3, :home_team_id=>6, :away_goals=>2, :home_goals=>3, :venue=>"Toyota Stadium", :venue_link=>"/api/v1/venues/null"}
   end
