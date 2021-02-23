744de3738656bf3a8103fff1da1ddeb2f43170e9
Alicia Henzler <aliciahenzler@outlook.com>
2021-02-22 17:10:36 -0700

    Create test helper file

---
+++ test/test_helper.rb
@@ -0,0 +1,7 @@
+require 'simplecov'
+SimpleCov.start
+
+# Note: If SimpleCov starts after your application code is already loaded (via require), it won't be able to track your files and their coverage! The SimpleCov.start must be issued before any of your application code is required!
+
+# SimpleCov github link
+# https://github.com/simplecov-ruby/simplecov
