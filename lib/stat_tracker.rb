210aaccc019af4f86d769210c624156922f752c5
kevn7n <kevn_nguyen@yahoo.com>
2021-02-23 01:53:28 -0700

    Update StatTracker class with .new instance

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,10 +1,16 @@
 class StatTracker
 
-def self.from_csv(data)
+  def self.from_csv(data)
+    #Read data
+    #StatTracker.new(data)
+  end
+
+  def initialize(data)
+    #OR Read data here
+    #require "pry"; binding.pry
+    @data = data
+  end
+
 end
 
-def initialize(data)
-  @data = data
-end
-
-end
+# HASHED out lines are updates from Turing kickoff video
