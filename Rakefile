0f510402aaf8cb6d39ea6a707324bd4d4596f84e
Alex Klick <aklick12@gmail.com>
2021-02-23 13:12:03 -0700

    testing the test, cleaned up coverage and added to gitignore

--- Rakefile
+++ Rakefile
@@ -2,7 +2,6 @@ require 'rake/testtask'
 
 Rake::TestTask.new do |t|
     t.pattern = "test/*_test.rb"
-    #t.test_files = FileList["test/*_test.rb"]
 end
 
 task default: ["test"]
--- test/stat_test.rb
+++ test/stat_test.rb
@@ -1,8 +1,7 @@
-#require './test/test_helper'
+require './test/test_helper'
 require './lib/stat_tracker'
-require 'minitest/autorun'
-require 'minitest/pride'
 require 'active_support'
+
 class StatTrackerTest < Minitest::Test
   def test_has_attributes
     game_path = './data/games.csv'
@@ -14,9 +13,10 @@ class StatTrackerTest < Minitest::Test
       teams: team_path,
       game_teams: game_teams_path
     }
-    #require 'pry'; binding.pry
     tracker = StatTracker.from_csv(locations)
     refute_equal [], tracker[0][0]
     assert_equal tracker[0][0], {:game_id=>2012030221, :season=>20122013, :type=>"Postseason", :date_time=>"5/16/13", :away_team_id=>3, :home_team_id=>6, :away_goals=>2, :home_goals=>3, :venue=>"Toyota Stadium", :venue_link=>"/api/v1/venues/null"}
   end
+
+
 end
