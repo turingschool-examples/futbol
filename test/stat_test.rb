0b0d615c7c8b6f46508ea4c8c5d9320b89492a37
Alex Klick <aklick12@gmail.com>
2021-02-23 13:36:03 -0700

    setup with dummy csv (teams_test.rb)

--- test/stat_test.rb
+++ test/stat_test.rb
@@ -4,13 +4,10 @@ require 'active_support'
 
 class StatTrackerTest < Minitest::Test
   def test_has_attributes
-    game_path = './data/games.csv'
-    team_path = './data/teams.csv'
-    game_teams_path = './data/game_teams.csv'
-  
-    tracker = StatTracker.from_csv('./data/teams_test.csv')
+    locations = {path: './data/teams_test.csv'}
+    tracker = StatTracker.from_csv(locations)
     refute_equal [], tracker[0][0]
-    assert_equal tracker[0][0], {:game_id=>2012030221, :season=>20122013, :type=>"Postseason", :date_time=>"5/16/13", :away_team_id=>3, :home_team_id=>6, :away_goals=>2, :home_goals=>3, :venue=>"Toyota Stadium", :venue_link=>"/api/v1/venues/null"}
+    assert_equal tracker[0][0], {:team_id=>1, :franchiseid=>23, :teamname=>"Atlanta United", :abbreviation=>"ATL", :stadium=>"Mercedes-Benz Stadium", :link=>"/api/v1/teams/1"}
   end
 
 
