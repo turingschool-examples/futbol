b162217115e5fb8ab63148fb951c15cc8f3412d0
Logan Anderson <nagolisback@gmail.com>
2021-02-22 19:12:57 -0700

    Created shell for season stats lib/test

---
+++ test/season_statistics_test.rb
@@ -0,0 +1,12 @@
+require './test/test_helper'
+
+class SeasonStatisticsTest < Minitest::Test
+  # def test_it_exists
+  # end
+  #
+  # def test_it_has_attributes
+  # end
+  #
+  # def test_it_can_have_different_attributes
+  # end
+end
