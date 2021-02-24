3764ec143d07f06c14861409f2d6ef903361fbc1
Kseniya Kudzelich <kseniya.kudelich@gmail.com>
2021-02-23 22:04:36 -0700

    Add runner file

---
+++ runner.rb
@@ -0,0 +1,16 @@
+require 'CSV'
+require './test/test_helper.rb'
+
+game_path = './data/games_truncated.csv'
+team_path = './data/teams.csv'
+game_teams_path = './data/game_teams_truncated.csv'
+
+locations = {
+  games: game_path,
+  teams: team_path,
+  game_teams: game_teams_path
+}
+
+stat_tracker = StatTracker.from_csv(locations)
+
+require 'pry'; binding.pry
