ae67db55b84334540f7a9ca06383d96b49a78223
ignored-comment <info.konham@gmail.com>
2021-02-23 22:11:34 -0500

    Create global variables for CSV files

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,12 +1,11 @@
 require 'CSV'
 
 class StatTracker
-  attr_reader :games
-
-  def initialize
-  end
 
   def self.from_csv(locations)
     StatTracker.new
+    @games = CSV.parse(File.read("./dummy_data/games_dummy.csv"), headers: true)
+    @game_teams = CSV.parse(File.read("./dummy_data/game_teams_dummy.csv"), headers: true)
+    @teams = CSV.parse(File.read("./dummy_data/teams_dummy.csv"), headers: true)
   end
 end
