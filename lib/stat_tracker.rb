181d6f8c0b96ebd063dc8a39c35763291bae22d5
Alex Klick <aklick12@gmail.com>
2021-02-23 19:52:04 -0700

    1 more time

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -6,17 +6,17 @@ class StatTracker
 
   def self.from_csv(csv_files)
     data = []
-    csv_files.map{ |file|
-      CSV.parse(File.read(file[1]), headers: true, converters: :numeric, header_converters: :symbol).map{
-      |row| 
-      headers ||= row.headers
-      data << row.to_h
-    }}
-    # import_data.map {
-    #   |csv_file| 
-    #   symbol = csv_file[0]
-    #   CSV.parse(File.read(csv_file[1]),converters: :numeric).map{|item| item}
-    # }
+    # csv_files.map{ |file|
+    #   CSV.parse(File.read(file[1]), headers: true, converters: :numeric, header_converters: :symbol).map{
+    #   |row| 
+    #   headers ||= row.headers
+    #   data << row.to_h
+    # }}
+    csv_files.map {
+      |csv_file| 
+      symbol = csv_file[0]
+      CSV.parse(File.read(csv_file[1]),converters: :numeric).map{|item| item}
+    }
     require 'pry'; binding.pry
   end
   # CSV.parse(File.read(csv_file[1]), headers: true, converters: :numeric).map{|line| line}
