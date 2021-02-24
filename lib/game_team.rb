92e5bd1f1231d468c6098954459f3f1d2f13a4dd
Aidan McGuire <>
2021-02-23 17:58:10 -0700

    Read in game_test csv data, create game_test class

---
+++ lib/game_team.rb
@@ -0,0 +1,26 @@
+class GameTeam
+  attr_reader :game_id, :team_id, :home_or_away, :result,
+              :settled_in, :head_coach, :goals, :shots, 
+              :tackles, :pim, :power_play_opportunities,
+              :power_play_goals, :face_off_win_percentage,
+              :giveaways, :takeaways
+
+   def initialize(raw_data)
+    @raw_data = raw_data
+    @game_id = raw_data[:game_id]
+    @team_id = raw_data[:team_id]
+    @home_or_away = raw_data[:home_or_away]
+    @result = raw_data[:result]
+    @settled_in = raw_data[:settled_in]
+    @head_coach = raw_data[:head_coach]
+    @goals = raw_data[:goals].to_i
+    @shots = raw_data[:shots].to_i
+    @tackles = raw_data[:tackles].to_i
+    @pim = raw_data[:pim].to_i
+    @power_play_opportunities = raw_data[:power_play_opportunities].to_i
+    @power_play_goals = raw_data[:power_play_goals].to_i
+    @face_off_win_percentage = raw_data[:face_off_win_percentage].to_f
+    @giveaways = raw_data[:giveaways].to_i
+    @takeaways = raw_data[:takeaways].to_i
+   end
+end
---
+++ test/game_team_test.rb
@@ -0,0 +1,33 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/game_team'
+
+class GameTeamTest < MiniTest::Test
+
+ def test_it_exists_with_attributes
+    fake_data = {game_id: "2012030221", team_id: "3", home_or_away: "away", 
+                result: "LOSS", settled_in: "OT", 
+                head_coach: "John Tortorella", goals: 2, shots: 8, tackles: 44, 
+                pim: 8, power_play_opportunities: 3, power_play_goals: 0, 
+                face_off_win_percentage: 44.8, giveaways: 17, takeaways: 7}
+
+    game_team = GameTeam.new(fake_data)
+
+    assert_instance_of GameTeam, game_team
+    assert_equal "2012030221", game_team.game_id
+    assert_equal "3", game_team.team_id
+    assert_equal "away", game_team.home_or_away
+    assert_equal "LOSS", game_team.result
+    assert_equal "OT", game_team.settled_in
+    assert_equal "John Tortorella", game_team.head_coach
+    assert_equal 2, game_team.goals
+    assert_equal 8, game_team.shots
+    assert_equal 44, game_team.tackles
+    assert_equal 8, game_team.pim
+    assert_equal 3, game_team.power_play_opportunities
+    assert_equal 0, game_team.power_play_goals
+    assert_equal 44.8, game_team.face_off_win_percentage
+    assert_equal 17, game_team.giveaways
+    assert_equal 7, game_team.takeaways
+  end
+end
