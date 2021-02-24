80d2b8a449b1762130060c27068f856f87987fe8
kevn7n <kevn_nguyen@yahoo.com>
2021-02-23 20:48:56 -0700

    Update stat-tracker, bug with key names

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,19 +1,33 @@
 require 'CSV'
 require './lib/game'
+require './lib/team'
+require './lib/game_team'
 
 class StatTracker
 
-  def initialize
-    # @games = []
+  def initialize(games, teams, game_teams)
+    @games = games
+    @teams = teams
+    @game_teams = game_teams
+    require "pry"; binding.pry
   end
 
   def self.from_csv(data_locations)
-    @games = []
-    stat_tracker = StatTracker.new
+    games = []
+    teams = []
+    game_teams = []
     CSV.foreach(data_locations[:games], headers: true, header_converters: :symbol) do |row|
-      @games << Game.new(row)
+      games << Game.new(row)
     end
-    require "pry"; binding.pry
+    CSV.foreach(data_locations[:teams], headers: true, header_converters: :symbol) do |row|
+      teams << Team.new(row)
+    end
+    CSV.foreach(data_locations[:game_teams], headers: true, header_converters: :symbol) do |row|
+      game_teams << GameTeam.new(row)
+    end
+    stat_tracker = StatTracker.new(games, teams, game_teams)
+
   end
+
 
 end
