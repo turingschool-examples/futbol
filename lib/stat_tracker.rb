5951c4ebbf393dd81617ab32b9d6f443648457d8
Wyatt <wyattwicks@gmail.com>
2021-02-23 19:21:18 -0700

    Add instance variables for data csv conversion

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -2,13 +2,17 @@ require 'csv'
 
 class StatTracker
 
-
   def self.from_csv(csv_file)
     @data = csv_file.map {
       |csv_file|
       CSV.parse(File.read(csv_file[1]), headers: true, converters: :numeric)
     }
-    require "pry"; binding.pry
-    # teams = CSV.parse(File.read(csv_file[1]), headers: true, converters: :numeric)
+
+    @games = @data[0]
+    @teams = @data[1]
+    @game_teams = @data[2]
+    @teams_test = @data[3]
+
+     require "pry";binding.pry
   end
 end
