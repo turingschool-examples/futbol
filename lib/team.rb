a2358164d4659106fd7887df67f94d73e39e6164
Aphilosopher30 <Ashafer1@fordham.edu>
2021-02-23 19:47:02 -0700

    Update team instance variable names

--- lib/team.rb
+++ lib/team.rb
@@ -1,12 +1,12 @@
 class Team
-  attr_reader :team_id, :franchiseId, :teamName,
+  attr_reader :team_id, :franchise_id, :team_name,
               :abbreviation, :stadium, :link
 
   def initialize(team_data)
     @team_data = team_data
     @team_id = team_data[:team_id]
-    @franchiseId = team_data[:franchiseId]
-    @teamName = team_data[:teamName]
+    @franchise_id = team_data[:franchiseId]
+    @team_name = team_data[:teamName]
     @abbreviation = team_data[:abbreviation]
     @stadium = team_data[:Stadium]
     @link = team_data[:link]
