535533d4e752fc2cb0ac2331c5d82aaeb8479009
ignored-comment <info.konham@gmail.com>
2021-02-23 22:50:57 -0500

    Try spacing between arguments for readability

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -4,8 +4,8 @@ class StatTracker
 
   def self.from_csv(locations)
     StatTracker.new
-    @games = CSV.parse(File.read(locations[:games]), headers: true)
-    @game_teams = CSV.parse(File.read(locations[:game_teams]), headers: true)
-    @teams = CSV.parse(File.read(locations[:teams]), headers: true)
+    @games = CSV.parse( File.read( locations[:games] ), headers: true)
+    @game_teams = CSV.parse( File.read(locations[:game_teams] ), headers: true)
+    @teams = CSV.parse( File.read( locations[:teams] ), headers: true)
   end
 end
