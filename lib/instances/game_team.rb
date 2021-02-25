238a50626bdec54b0fd42fc52afb28db1b04e9a9
Alex Klick <aklick12@gmail.com>
2021-02-25 15:52:52 -0700

    fixed formatting error on game_team

--- lib/instances/game_team.rb
+++ lib/instances/game_team.rb
@@ -19,19 +19,19 @@ class GameTeam
   def initialize(data)
     data
     @game_id = data[:game_id]
-    # @team_id = data.team_id
-    # @HoA = data.HoA
-    # @result = data.result
-    # @settled_in = data.settled_in
-    # @head_coach = data.head_coach
-    # @goals = data.goals
-    # @shots = data.shots
-    # @tackles= data.tackles
-    # @pim = data.pim
-    # @powerPlayOpportunities = data.powerPlayOpportunities
-    # @powerPlayGoals = data.powerPlayGoals
-    # @faceOffWinPercentage = data.faceOffWinPercentage
-    # @giveaways = data.giveaways
-    # @takeaways = data.takeaways
+    @team_id = data[:team_id]
+    @HoA = data[:HoA]
+    @result = data[:result]
+    @settled_in = data[:settled_in]
+    @head_coach = data[:head_coach]
+    @goals = data[:goals]
+    @shots = data[:shots]
+    @tackles= data[:tackles]
+    @pim = data[:pim]
+    @powerPlayOpportunities = data[:powerPlayOpportunities]
+    @powerPlayGoals = data[:powerPlayGoals]
+    @faceOffWinPercentage = data[:faceOffWinPercentage]
+    @giveaways = data[:giveaways]
+    @takeaways = data[:takeaways]
   end
 end
