da2b927374e8c6e46f2c057d73bf1f1e55f76c7e
Aphilosopher30 <Ashafer1@fordham.edu>
2021-02-25 11:46:34 -0700

    ajust test

--- test/game_manager_test.rb
+++ test/game_manager_test.rb
@@ -19,13 +19,17 @@ class GameManagerTest < Minitest::Test
   def test_it_has_attributes
 
     start_tracker = mock
-    path = "./fixture/games_dummy15.csv"
+    # path = "./fixture/games_dummy15.csv"
     game_manager = GamesManager.new(path , start_tracker)
-    csv = mock
-    csv.stubs(:foreach).returns([])
+
+    # csv = mock
+    # csv.stubs(:foreach).returns([])
 
     assert_equal path, game_manager.data_path
-    # assert_equal [], game_manager.games
+    assert_equal 15, game_manager.games.length
+    assert_instance_of Game, game_manager.games[0]
+    assert_instance_of Game, game_manager.games[-1]
+
     # assert_instance_of StatTracker, stat_tracker
   end
 end
