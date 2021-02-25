222922ed314f3e05a55135bc233131a97fa136c8
Aphilosopher30 <Ashafer1@fordham.edu>
2021-02-24 21:28:30 -0700

    create new game manager class, supporting archetecture, and being transfering functionality

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,31 +1,30 @@
 require 'CSV'
-require './lib/game'
-require './lib/team'
-require './lib/game_team'
+require './lib/game_manager'
+require './lib/team_manager'
+require './lib/game_team_manager'
 
 class StatTracker
 
-  def initialize(games, teams, game_teams)
-    @games = games
-    @teams = teams
-    @game_teams = game_teams
-    # require "pry"; binding.pry
+  def initialize(games_path, team_path, game_teams_path)
+    @games = GamesManager.new(games_path)
+    @teams = TeamsManager.new(team_path)
+    @game_teams = GameTeamsManager(game_team_path)
+
+     # require "pry"; binding.pry
   end
 
   def self.from_csv(data_locations)
-    games = []
-    teams = []
-    game_teams = []
-    CSV.foreach(data_locations[:games], headers: true, header_converters: :symbol) do |row|
-      games << Game.new(row)
-    end
-    CSV.foreach(data_locations[:teams], headers: true, header_converters: :symbol) do |row|
-      teams << Team.new(row)
-    end
-    CSV.foreach(data_locations[:game_teams], headers: true, header_converters: :symbol) do |row|
-      game_teams << GameTeam.new(row)
-    end
-    stat_tracker = StatTracker.new(games, teams, game_teams)
+
+    games_path = data_locations[:games]
+    teams_path = data_locations[:teams]
+    game_teams_path = data_locations[:game_teams]
+    # CSV.foreach(data_locations[:teams], headers: true, header_converters: :symbol) do |row|
+    #   teams << Team.new(row)
+    # end
+    # CSV.foreach(data_locations[:game_teams], headers: true, header_converters: :symbol) do |row|
+    #   game_teams << GameTeam.new(row)
+    # end
+    StatTracker.new(games_path, teams_path, game_teams_path)
   end
 
   ####### Game Stats ########
---
+++ test/game_manager_test.rb
@@ -0,0 +1,18 @@
+require './test/test_helper'
+require './lib/game_manager'
+
+
+class GameManagerTest < Minitest::Test
+
+  def test_it_exists
+    # fake_data = {game_id: 2012030221, season: 20122013, type: "Postseason", date_time: "5/16/13", away_team_id: 3, home_team_id: 6, away_goals: 2, home_goals: 3, venue: "Toyota Stadium", venue_link: "/api/v1/venues/null"}
+    #
+    # game = Game.new(fake_data)
+
+    assert_instance_of GameManager, ------
+  end
+
+  def test_it_has_attributes
+
+  end
+end
