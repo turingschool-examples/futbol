6cde60bcf59706c5e166342d46ebd487226ced05
Aphilosopher30 <Ashafer1@fordham.edu>
2021-02-26 09:47:07 -0700

    add ability to get team name from game_id

--- lib/team.rb
+++ lib/team.rb
@@ -1,10 +1,10 @@
 class Team
-  attr_reader :team_id
+  attr_reader :team_id,:team_name
 
   def initialize(team_data)
     @team_id = team_data[:team_id]
     # @franchise_id = team_data[:franchiseid]
-    # @team_name = team_data[:teamname]
+    @team_name = team_data[:teamname]
     # @abbreviation = team_data[:abbreviation]
     # @stadium = team_data[:stadium]
     # @link = team_data[:link]
--- test/team_test.rb
+++ test/team_test.rb
@@ -18,9 +18,29 @@ class TeamTest < Minitest::Test
 
     assert_equal "1", team.team_id
     # assert_equal "23", team.franchise_id
-    # assert_equal "Atlanta United", team.team_name
+    assert_equal "Atlanta United", team.team_name
     # assert_equal "ATL", team.abbreviation
     # assert_equal "Mercedes-Benz Stadium", team.stadium
     # assert_equal "/api/v1/teams/1", team.link
+  end
+
+  def test_get_names_hash
+    teams_manager = TeamsManager.new('./fixture/game_teams_dummy15.csv')
+
+    team_hash = teams_manager.get_names_hash
+
+    assert_equal "Atlanta United", team_hash["1"]
+    assert_equal "FC Dallas", team_hash["6"]
+    assert_equal "Minnesota United", team_hash["18"]
+    assert_equal "Orlando City", team_hash["30"]
+  end
+
+  def test_get_name
+    teams_manager = TeamsManager.new('./fixture/game_teams_dummy15.csv')
+
+    assert_equal "Atlanta United", teams_manager.get_name("1")
+    assert_equal "FC Dallas", teams_manager.get_name("6")
+    assert_equal "Minnesota United", teams_manager.get_name("18")
+    assert_equal "Orlando City", teams_manager.get_name("30")
   end
 end
