065bb1c3e20317ec2f535df713d507789566d554
jaharaclark <jaharamclark@gmail.com>
2021-02-26 09:29:44 -1000

    Adds all changes for two methods and Spec

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,6 +1,6 @@
-require './lib/game'
-require './lib/team'
-require './lib/game_team'
+require_relative './game'
+require_relative './team'
+require_relative './game_team'
 require_relative 'csv_loadable'
 
 class StatTracker
