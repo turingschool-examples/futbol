94344585ad6d8e15c70f5de5ddb1dd789446368f
jaharaclark <jaharamclark@gmail.com>
2021-02-26 09:27:33 -1000

    Copy and paste code

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -5,9 +5,12 @@ class StatTrackerTest < Minitest::Test
 
   def setup
     @locations = {
-      games: './data/games_truncated.csv',
+      games: './data/games.csv',
       teams: './data/teams.csv',
-      game_teams: './data/game_teams_truncated.csv'
+      game_teams: './data/game_teams.csv'
+      # games: './data/games_truncated.csv',
+      # game_teams: './data/game_teams_truncated.csv'
+
     }
 
     @stat_tracker = StatTracker.from_csv(@locations)
