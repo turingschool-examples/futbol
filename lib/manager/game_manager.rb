56cdcf64aff5432635668991b0343ee49cbe86cf
Austin <>
2021-02-26 14:00:32 -0500

    Remove random headers that shouldn't exist in our files

--- lib/manager/game_manager.rb
+++ lib/manager/game_manager.rb
@@ -35,7 +35,6 @@ class GameManager
   end
 
   def calculate_percentage_home_wins
-<<<<<<< HEAD:lib/game_manager.rb
     ((home_wins.count.to_f / @games.count) * 100).round(2)
   end
 
@@ -79,7 +78,7 @@ class GameManager
     # Result: Hash
     # average goals per game = total goals /
     season_names_and_average_goals = {}
-    average_goals_per_match 
+    average_goals_per_match
     @games.each do |game|
       if season_names_and_average_goals[game.season].nil?
          season_names_and_average_goals[game.season] = 1
@@ -89,50 +88,4 @@ class GameManager
     end
     season_names_and_games
   end
-
-=======
-    # number of home wins / total number games
-    (home_wins.count.to_f / @games.count) * 100
-  end
->>>>>>> main:manager/game_manager.rb
-
-  # def highest_total_score
-  #   # Description: Highest sum of the winning and losing teams’ scores
-  #   # Return Value: Integer
-  # end
-  #
-  # def lowest_total_score
-  #   # Description: Lowest sum of the winning and losing teams’ scores
-  #   # Return Value: Integer
-  # end
-  #
-  # def percentage_home_wins
-  #   # Description: Percentage of games that a home team has won (rounded to the nearest 100th)
-  #   # Return Value: Float
-  # end
-  #
-  # def percentage_visitor_wins
-  #   # Description: Percentage of games that a visitor has won (rounded to the nearest 100th)
-  #   # Return Value: Float
-  # end
-  #
-  # def percentage_ties
-  #   # Percentage of games that has resulted in a tie (rounded to the nearest 100th)
-  #   # Return Value: Float
-  # end
-  #
-  # def count_of_games_by_season
-  #   # A hash with season names (e.g. 20122013) as keys and counts of games as values.
-  #   # Return Value: Hash
-  # end
-  #
-  # def average_goals_per_game
-  #   # Average number of goals scored in a game across all seasons including both home and away goals (rounded to the nearest 100th)
-  #   # Return Value: Float
-  # end
-  #
-  # def average_goals_by_season
-  #   # Average number of goals scored in a game organized in a hash with season names (e.g. 20122013) as keys and a float representing the average number of goals in a game for that season as values (rounded to the nearest 100th)
-  #   # Return Value: Hash
-  # end
 end
--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -19,7 +19,6 @@ class StatTracker
     @game_manager.highest_scoring_game.total_score
   end
 
-<<<<<<< HEAD
   def lowest_total_score
     #        game object               tack on total_score
     @game_manager.lowest_scoring_game.total_score
@@ -44,52 +43,4 @@ class StatTracker
   def average_goals_per_game
     @game_manager.average_goals_per_match
   end
-
-=======
->>>>>>> main
-
-  # def self.from_csv(locations)
-  #   # require "pry";binding.pry
-  #   # data = locations.map do |key, path|
-  #     CSV.foreach(path, :headers => true,
-  #     header_converters: :symbol) do |row|
-  #       # require "pry";binding.pry
-  #       if key == :games
-  #         GameManager.new(row)
-  #         # require "pry"; binding.pry
-  #       elsif key == :teams
-  #         TeamManager.new(row)
-  #       else
-  #         GameTeamManager.new(row)
-  #       end
-  #     end
-  #   # end
-  #   data
-  #   require "pry";binding.pry
-  # end
-
-  # def initialize(teams, games, game_teams)
-  #   @teams = teams
-  #   @games = games
-  #   @game_teams = @game_teams
-  #   @seasons = seasons
-  # end
-
-  # def self.from_csv(locations)
-  #   # require "pry";binding.pry
-  #   locations.each do |key, path|
-  #     CSV.foreach(path, :headers => true,
-  #     header_converters: :symbol) do |row|
-  #       if key == :games
-  #         GameManager.new(row)
-  #         # require "pry"; binding.pry
-  #       elsif key == :teams
-  #         TeamManager.new(row)
-  #       else
-  #         GameTeamManager.new(row)
-  #       end
-  #     end
-  #   end
-  # end
-
 end
