8a3ed71b74ada8e9b92d14469f4de1f2d78cb957
Gaelyn Cooper <gaelyn.cooper@gmail.com>
2021-02-26 21:23:34 -0600

    Add team_info method and test

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,10 +1,15 @@
 require 'CSV'
 
 class StatTracker
-  attr_reader :games_manager
+  attr_reader :games_data,
+              :team_data,
+              :game_teams_data
 
   def initialize(locations)
     # @locations = locations
+    @games_data = CSV.parse(File.read('./data/games.csv'), headers: true, header_converters: :symbol)
+    @team_data = CSV.parse(File.read('./data/teams.csv'), headers: true, header_converters: :symbol)
+    @game_teams_data = CSV.parse(File.read('./data/game_teams.csv'), headers: true, header_converters: :symbol)
     # @team_manager = TeamsManager.new(CSV.parse(File.read(locations[:teams]), headers: true, header_converters: :symbol), self)
     # @games_manager = GamesManager.new(CSV.parse(File.read(locations[:games]), headers: true, header_converters: :symbol), self)
     # @game_team_manager = GameTeamsManager.new(CSV.parse(File.read(locations[:game_teams]), headers: true, header_converters: :symbol), self)
@@ -16,7 +21,7 @@ class StatTracker
 
 # Game Statistics
   def highest_total_score
-    games_data = CSV.parse(File.read('./dummy_data/games_dummy.csv'), headers: true, header_converters: :symbol)
+    # games_data = CSV.parse(File.read('./dummy_data/games_dummy.csv'), headers: true, header_converters: :symbol)
     highest_score = 0
     games_data.each do |game|
       sum = game[:home_goals].to_i + game[:away_goals].to_i
@@ -29,7 +34,7 @@ class StatTracker
 
   # League Statistics
   def count_of_teams
-    team_data = CSV.parse(File.read('./dummy_data/teams_dummy.csv'), headers: true, header_converters: :symbol)
+    # team_data = CSV.parse(File.read('./dummy_data/teams_dummy.csv'), headers: true, header_converters: :symbol)
     number_of_teams = team_data.map do |team|
       team
     end
@@ -38,8 +43,8 @@ class StatTracker
 
   # Season Statistics
   def most_tackles
-    game_teams_data = CSV.parse(File.read('./dummy_data/game_teams_dummy.csv'), headers: true, header_converters: :symbol)
-    team_data = CSV.parse(File.read('./dummy_data/teams_dummy.csv'), headers: true, header_converters: :symbol)
+    # game_teams_data = CSV.parse(File.read('./dummy_data/game_teams_dummy.csv'), headers: true, header_converters: :symbol)
+    # team_data = CSV.parse(File.read('./dummy_data/teams_dummy.csv'), headers: true, header_converters: :symbol)
 
     result = game_teams_data.max_by do |game_team|
       game_team[:tackles]
@@ -50,7 +55,22 @@ class StatTracker
     end
     name_result[:teamname]
   end
+  # Team Statistics
+  # A hash with key/value pairs for the following attributes: team_id,
+  # franchise_id, team_name, abbreviation, and link
+  def team_info(team_id)
+    team_data_string_headers = CSV.parse(File.read('./data/teams.csv'), headers: true)
+    team_info = team_data_string_headers.find do |team|
+      # require "pry"; binding.pry
+      team["team_id"] == team_id
+    end
+
+    team_deets = team_info.to_h
+    team_deets.delete("Stadium")
+    team_deets["team_name"] = team_deets["teamName"]
+    team_deets.delete("teamName")
+    team_deets["franchise_id"] = team_deets["franchiseId"]
+    team_deets.delete("franchiseId")
+    team_deets
+  end
 end
-
-
-# Team Statistics
