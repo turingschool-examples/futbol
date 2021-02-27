dc40610f549ba9a1797dfff56e492e9fa2690489
Klaudia Stewart <klaudiastewart@gmail.com>
2021-02-26 18:21:47 -0700

    Add percentage game methods

--- lib/tables/game_table.rb
+++ lib/tables/game_table.rb
@@ -13,11 +13,29 @@ class GameTable
 
   def highest_total_score
     @game_data.map {|game| game.away_goals + game.home_goals}.max
-    # @csv_file.each do |csv_file|
-# goes through and gets highest total score of home goals AND the highest total score of the away goals
-#
-# highest away goals plus home goals
+  end
 
+  def lowest_total_score
+    @game_data.map { |game| game.away_goals + game.home_goals}.min
+  end
 
+  def percentage_home_wins
+    wins = 0
+    total = @game_data.map { |game| wins += 1 if game.home_goals > game.away_goals }.count
+    percentage = (wins.to_f / @game_data.count).round(2)
+    # require "pry"; binding.pry
+    percentage
+    end
+
+  def percentage_away_wins
+    wins = 0
+    total = @game_data.map { |game| wins += 1 if game.home_goals < game.away_goals }.count
+    percentage = (wins.to_f / @game_data.count).round(2)
+  end
+
+  def percentage_ties
+    wins = 0
+    total = @game_data.map { |game| wins += 1 if game.home_goals == game.away_goals }.count
+    percentage = (wins.to_f / @game_data.count).round(2)
   end
 end
--- test/game_table_test.rb
+++ test/game_table_test.rb
@@ -18,7 +18,22 @@ include CsvToHash
   end
 
   def test_highest_total_score
-
     assert_equal 11, @game_table.highest_total_score
+  end
+
+  def test_lowest_total_score
+    assert_equal 0, @game_table.lowest_total_score
+  end
+
+  def test_percentage_of_home_wins
+    assert_equal 0.44, @game_table.percentage_home_wins
+  end
+
+  def test_percentage_of_away_wins
+    assert_equal 0.36, @game_table.percentage_away_wins
+  end
+
+  def test_percentage_of_ties
+    assert_equal 0.20, @game_table.percentage_ties
   end
 end
