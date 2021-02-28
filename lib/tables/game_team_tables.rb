1a6f330dd8c3076df2d637fca6fcdb0a6524f165
Klaudia Stewart <klaudiastewart@gmail.com>
2021-02-27 19:36:33 -0700

    Add most accurate team method

--- lib/tables/game_team_tables.rb
+++ lib/tables/game_team_tables.rb
@@ -9,29 +9,83 @@ class GameTeamTable
     @stat_tracker = stat_tracker
   end
 
-  def winningest_coach(season)
-    
+  def get_season(season)
     season = @stat_tracker.game_by_season[season.to_i].map do |season|
       season.game_id
     end
-   
     ids = @game_team_data.map do |gameteam|
       gameteam.game_id
     end
-
     overlap = season & ids
+  end
 
+  def winningest_coach(season)
+    season = @stat_tracker.game_by_season[season.to_i].map do |season|
+      season.game_id
+    end
+    ids = @game_team_data.map do |gameteam|
+      gameteam.game_id
+    end
+    overlap = season & ids
     games_by_season= @game_team_data.find_all do |games|
       overlap.include?(games.game_id)
     end
-
     winning_coach_hash =games_by_season.group_by do |game|
        game.head_coach if game.result == "WIN"
-      end  
-
+      end
     win_count = winning_coach_hash.each { |k, v| winning_coach_hash[k] = v.count}.reject{|coach| coach == nil}
-    # require 'pry'; binding.pry
     win_count.max_by{|coach, win| win}[0]
+  end
 
+  def worst_coach(season)
+    season = @stat_tracker.game_by_season[season.to_i].map do |season|
+      season.game_id
+    end
+    ids = @game_team_data.map do |gameteam|
+      gameteam.game_id
+    end
+    overlap = season & ids
+    games_by_season = @game_team_data.find_all do |games|
+      overlap.include?(games.game_id)
+    end
+    winning_coach_hash = games_by_season.group_by do |game|
+       game.head_coach if game.result == "LOSS"
+      end
+    win_count = winning_coach_hash.each { |k, v| winning_coach_hash[k] = v.count}.reject{|coach| coach == nil}
+    win_count.min_by{|coach, win| win}[0]
+  end
+
+  def most_accurate_team(season)
+    season = @stat_tracker.game_by_season[season.to_i].map do |season|
+      season.game_id
+    end
+    ids = @game_team_data.map do |gameteam|
+      gameteam.game_id
+    end
+    overlap = season & ids
+    games_by_season = @game_team_data.find_all do |games|
+      overlap.include?(games.game_id)
+    end
+    # goals = games_by_season.map do |game|
+    #   game.goals.each do |game|
+    #     game.goal
+    #   end
+    # end
+    # shots = games_by_season.map do |game|
+    #   game.shots
+    # end
+    games_by_team_id_hash = games_by_season.group_by do |game|
+      game.team_id #if (game.goals.to_f / game.shots.to_f).round(2)
+    end
+
+    ratio_of_s_to_g = games_by_team_id_hash.each do |team, ratio|
+      games_by_team_id_hash[team] = ratio.map do |array|
+        (array.goals.to_f / array.shots.to_f).round(2)
+      end
+    end
+
+    r1 = ratio_of_s_to_g.max_by {|team_id, ratio| ratio}[0]
+# win_count.min_by{|coach, win| win}[0]
+  require "pry"; binding.pry
   end
 end
--- test/game_team_table_test.rb
+++ test/game_team_table_test.rb
@@ -15,8 +15,17 @@ class GameTeamTableTest < Minitest::Test
   end
 
   def test_winningest_coach
-    
     assert_equal "Claude Julien", @game_table.winningest_coach("20132014")
     assert_equal "Alain Vigneault", @game_table.winningest_coach("20142015")
   end
+
+  def test_worst_coach
+    assert_equal "Peter Laviolette", @game_table.worst_coach("20132014")
+    assert_equal "Craig MacTavish" || "Ted Nolan", @game_table.worst_coach("20142015")
+  end
+
+  def test_most_accurate_team
+    assert_equal "Real Salt Lake", @game_table.most_accurate_team("20132014")
+    assert_equal "Toronto FC", @game_table.most_accurate_team("20142015")
+  end 
 end
