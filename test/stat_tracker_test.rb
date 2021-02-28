75e518b10a120c2f7fded51d9b6136df964db515
Andrew Smith <andrewpatrick138@gmail.com>
2021-02-28 14:59:40 -0800

    organize test methods

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -16,7 +16,6 @@ class StatTrackerTest < Minitest::Test
       game_teams: @game_teams_path
     }
     @stat_tracker = StatTracker.from_csv(@locations)
-    # @stat_tracker_instance = StatTracker.new(@locations)
   end
 
   def test_it_exists
@@ -27,11 +26,10 @@ class StatTrackerTest < Minitest::Test
   # Game Statistics Tests
   def test_it_can_find_highest_score
     skip
-    # stat_tracker = StatTracker.from_csv(@locations)
     assert_equal 11, @stat_tracker.highest_total_score
   end
 
-def test_you_can_find_lowest_total_score
+  def test_you_can_find_lowest_total_score
     skip
     assert_equal 0, @stat_tracker.lowest_total_score
   end
@@ -49,6 +47,19 @@ def test_you_can_find_lowest_total_score
   def test_percentage_ties
     skip
     assert_equal 0.20, @stat_tracker.percentage_ties
+  end
+
+  def test_count_games_by_season
+    skip
+    expected = {
+      "20122013"=>806,
+      "20162017"=>1317,
+      "20142015"=>1319,
+      "20152016"=>1321,
+      "20132014"=>1323,
+      "20172018"=>1355
+      }
+    assert_equal expected, @stat_tracker.count_of_games_by_season
   end
 
   def test_average_goals_per_game
@@ -76,45 +87,14 @@ def test_you_can_find_lowest_total_score
     assert_equal 32, @stat_tracker.count_of_teams
   end
 
-  def test_it_can_name_team_with_most_tackles
+  def test_it_can_calculate_best_offense
     skip
-    assert_equal "FC Cincinnati", @stat_tracker.most_tackles("20132014")
-    assert_equal "Seattle Sounders FC", @stat_tracker.most_tackles("20142015")
+    assert_equal "Reign FC", @stat_tracker.best_offense
   end
 
-  #Team Statistics
-  def test_it_can_list_team_info
-    skip
-
-    expected = {
-     "team_id" => "18",
-     "franchise_id" => "34",
-     "team_name" => "Minnesota United FC",
-     "abbreviation" => "MIN",
-     "link" => "/api/v1/teams/18"
-      }
-
-   assert_equal expected, @stat_tracker.team_info("18")
-  end
-
-  def test_fewest_goals_scored
-    skip
-    assert_equal 0, @stat_tracker.fewest_goals_scored("18")
-  end
-
-  def test_most_goals_scored
-    skip
-    assert_equal 7, @stat_tracker.most_goals_scored("18")
-  end
-
-  def test_highest_scoring_home_team
-    skip
-    assert_equal "Reign FC", @stat_tracker.highest_scoring_home_team
-  end
-
-  def test_lowest_scoring_home_team
-    skip
-    assert_equal "Utah Royals FC", @stat_tracker.lowest_scoring_home_team
+  def test_it_can_calculate_worst_offense
+        skip
+    assert_equal "Utah Royals FC", @stat_tracker.worst_offense
   end
 
   def test_highest_scoring_visitor
@@ -122,10 +102,22 @@ def test_you_can_find_lowest_total_score
     assert_equal "FC Dallas", @stat_tracker.highest_scoring_visitor
   end
 
+  def test_highest_scoring_home_team
+    skip
+    assert_equal "Reign FC", @stat_tracker.highest_scoring_home_team
+  end
+
   def test_lowest_scoring_visitor
     skip
     assert_equal "San Jose Earthquakes", @stat_tracker.lowest_scoring_visitor
   end
+
+  def test_lowest_scoring_home_team
+    skip
+    assert_equal "Utah Royals FC", @stat_tracker.lowest_scoring_home_team
+  end
+
+  #Season Statistics
 
   def test_winningest_coach
     skip
@@ -155,7 +147,7 @@ def test_you_can_find_lowest_total_score
     assert_equal "Columbus Crew SC", @stat_tracker.least_accurate_team("20142015")
   end
 
-  def test_most_tackles
+  def test_it_can_name_team_with_most_tackles
     skip
     assert_equal "FC Cincinnati", @stat_tracker.most_tackles("20132014")
     assert_equal "Seattle Sounders FC", @stat_tracker.most_tackles("20142015")
@@ -167,42 +159,43 @@ def test_you_can_find_lowest_total_score
     assert_equal "Orlando City SC", @stat_tracker.fewest_tackles("20142015")
   end
 
-  def test_you_can_find_lowest_total_score
+  #Team Statistics
+  def test_it_can_list_team_info
     skip
-    assert_equal 0, @stat_tracker.lowest_total_score
-  end
 
-  def test_percentage_of_home_wins
-    skip
-    assert_equal 0.44, @stat_tracker.percentage_home_wins
-  end
-
-  def test_percentage_of_visitor_wins
-    skip
-    assert_equal 0.36, @stat_tracker.percentage_visitor_wins
-  end
-
-  def test_count_games_by_season
-    skip
     expected = {
-      "20122013"=>806,
-      "20162017"=>1317,
-      "20142015"=>1319,
-      "20152016"=>1321,
-      "20132014"=>1323,
-      "20172018"=>1355
+     "team_id" => "18",
+     "franchise_id" => "34",
+     "team_name" => "Minnesota United FC",
+     "abbreviation" => "MIN",
+     "link" => "/api/v1/teams/18"
       }
-    assert_equal expected, @stat_tracker.count_of_games_by_season
+
+   assert_equal expected, @stat_tracker.team_info("18")
   end
 
-  def test_it_can_calculate_best_offense
+  def test_best_season
+    # WORK IN PROGRESS
+    assert_equal "20132014", @stat_tracker.best_season("6")
+  end
+
+  def test_worst_season
+    assert_equal "20142015", @stat_tracker.worst_season("6")
+  end
+
+  def test_average_win_percentage
+    # skip
+    assert_equal 0.49, @stat_tracker.average_win_percentage("6")
+  end
+
+  def test_most_goals_scored
     skip
-    assert_equal "Reign FC", @stat_tracker.best_offense
+    assert_equal 7, @stat_tracker.most_goals_scored("18")
   end
 
-  def test_it_can_calculate_worst_offense
-        skip
-    assert_equal "Utah Royals FC", @stat_tracker.worst_offense
+  def test_fewest_goals_scored
+    skip
+    assert_equal 0, @stat_tracker.fewest_goals_scored("18")
   end
 
   def test_it_can_have_a_favorite_opponent
@@ -214,19 +207,5 @@ def test_you_can_find_lowest_total_score
     skip
     # assert_equal "Houston Dash", @stat_tracker.rival("18")
     assert_equal "LA Galaxy", @stat_tracker.rival("18")
-  end
-
-  def test_average_win_percentage
-    # skip
-    assert_equal 0.49, @stat_tracker.average_win_percentage("6")
-  end
-
-  def test_best_season
-    # WORK IN PROGRESS
-    assert_equal "20132014", @stat_tracker.best_season("6")
-  end
-
-  def test_worst_season
-    assert_equal "20142015", @stat_tracker.worst_season("6")
   end
 end
