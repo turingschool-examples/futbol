09121ca3619137a9fd3173440c171f156f09c2b2
“Alicia <>
2021-03-01 14:53:34 -0700

    removed comments

--- lib/teams_manager.rb
+++ lib/teams_manager.rb
@@ -16,7 +16,7 @@ class TeamsManager
     list_of_data
   end
 
-  def count_of_teams #ask intructors if they want this
+  def count_of_teams
     teams.uniq.count
   end
 
@@ -31,21 +31,13 @@ class TeamsManager
   def get_team_name(team_id)
     name_hash = get_names_hash
     name_hash[team_id]
-    #refactor decision
-  #alternate replacement without hashing.
-  #get_name(id)
-    # team = @teams.find do |team|
-    #   team.team_id == id
-    # end
-    # team.name
-  #end
   end
 
   def team_info(team_id)
     desired_team = @teams.find do |team|
       team.team_id == team_id
     end
-    
+
     info = { "team_id" => desired_team.team_id,
              "franchise_id" => desired_team.franchise_id,
              "team_name" => desired_team.team_name,
