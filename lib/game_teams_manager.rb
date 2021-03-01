d24c8e9d6ee412f1e3333ff71af634a4a4f7e26d
Aphilosopher30 <Ashafer1@fordham.edu>
2021-03-01 02:11:27 -0700

    refactor Game_teams_manager to have more helper methods.

--- lib/game_teams_manager.rb
+++ lib/game_teams_manager.rb
@@ -16,20 +16,6 @@ class GameTeamsManager
     list_of_data
   end
 
-
-  def get_team_tackle_hash(season_games_ids)
-    season_list = @game_teams.find_all do |game_team|
-      season_games_ids.include?(game_team.game_id)
-    end
-
-    team_id = Proc.new{|game_team| game_team.team_id}
-    tackles = Proc.new{|game_team| game_team.tackles}
-
-    team_tackles_totals = create_hash_int(team_id, tackles, season_list)
-
-    team_tackles_totals
-  end
-
   def create_hash_int(key, value, data_list)
     hash = Hash.new(0)
     data_list.each do |datum|
@@ -50,7 +36,6 @@ class GameTeamsManager
     hash = numerator_hash.merge(denominator_hash) do |key, numerator, denominator|
       (numerator/denominator.to_f).round(2)
     end
-    hash
   end
 
   def get_proportion_hash_list(hash, criterion = "WIN")
@@ -60,9 +45,7 @@ class GameTeamsManager
   end
 
   def score_ratios_hash(season_games_ids)#new
-    data_list = @game_teams.find_all do |game_team|
-      season_games_ids.include?(game_team.game_id)
-    end
+    data_list = select_season_games(season_games_ids)
 
     team_id_key = Proc.new{|game_team| game_team.team_id}
     goal_value = Proc.new{|game_team| game_team.goals}
@@ -74,30 +57,38 @@ class GameTeamsManager
     get_proportion_hash(goals_hash, shots_hash)
   end
 
-  def winningest_coach(season_games)
-    data_list = @game_teams.find_all do |game_team|
-      season_games.include?(game_team.game_id)
+  def select_season_games(season_games_ids)
+    @game_teams.find_all do |game_team|
+      season_games_ids.include?(game_team.game_id)
     end
+  end
 
-    coach_key = Proc.new{|game_team| game_team.head_coach}
-    result_value = Proc.new{|game_team| game_team.result}
-    coach_hash_win_list = create_hash_list(coach_key, result_value, data_list)
-    coach_hash_win_ratio = get_proportion_hash_list(coach_hash_win_list)
+  def get_team_tackle_hash(season_games_ids)
+    season_list = select_season_games(season_games_ids)
 
-    coach_hash_win_ratio.key(coach_hash_win_ratio.values.max)
+    team_id = Proc.new{|game_team| game_team.team_id}
+    tackles = Proc.new{|game_team| game_team.tackles}
+
+    create_hash_int(team_id, tackles, season_list)
+  end
+
+  def winningest_coach(season_games)
+    data_list = select_season_games(season_games_ids)
+    coach_hash = all_coachs_records(season)
+    coach_hash.key(coach_hash.values.max)
   end
 
   def worst_coach(season_games)
-    data_list = @game_teams.find_all do |game_team|
-      season_games.include?(game_team.game_id)
-    end
+    data_list = select_season_games(season_games_ids)
+    coach_hash = all_coachs_records(season)
+    coach_hash.key(coach_hash.values.min)
+  end
 
+  def all_coachs_records(season)
     coach_key = Proc.new{|game_team| game_team.head_coach}
     result_value = Proc.new{|game_team| game_team.result}
     coach_hash_win_list = create_hash_list(coach_key, result_value, data_list)
     coach_hash_win_ratio = get_proportion_hash_list(coach_hash_win_list)
-
-    coach_hash_win_ratio.key(coach_hash_win_ratio.values.min)
   end
 
   def favorite_opponent(team_id)
@@ -162,138 +153,3 @@ class GameTeamsManager
     average.key(average.values.min)
   end
 end
-
-  # def score_and_shots_by_team(season_games_ids)
-  #   hash = Hash.new { |hash, key| hash[key] = [0,0] }
-  #   @game_teams.each do |game_team|
-  #     if season_games_ids.include?(game_team.game_id)
-  #       hash[game_team.team_id][0] += game_team.goals
-  #       hash[game_team.team_id][1] += game_team.shots
-  #     end
-  #   end
-  #   hash
-  # end
-
-  # def score_ratios_hash(season_games_ids)   ##Refactor?: 'hash' to 'accuracy'
-  #   hash = score_and_shots_by_team(season_games_ids)
-  #   hash.each do |team_id, pair|
-  #     ratio = calculate_ratios(pair)
-  #     hash[team_id] = ratio
-  #   end
-  #   hash
-  # end
-  #
-  # def calculate_ratios(pair)
-  #   pair[0].to_f/pair[1].to_f
-  # end
-
-  # def winningest_coach(season_games)   ##Refactor?: change 'hash' to 'coach'
-  #   hash = Hash.new { |hash, team| hash[team] = [0,0] }
-  #   @game_teams.each do |game_team|
-  #     if season_games.include?(game_team.game_id)
-  #       hash[game_team.head_coach][1] += 1
-  #       hash[game_team.head_coach][0] += 1 if game_team.result == "WIN"
-  #     end
-  #   end
-  #   hash.each do |team_id, pair|
-  #     ratio = calculate_ratios(pair)
-  #     hash[team_id] = ratio
-  #   end
-  #   hash.key(hash.values.max)
-  # end
-
-  # def worst_coach(season_games)   ##Refactor?:  change 'hash' to 'coach'
-  #   coach = Hash.new { |hash, team| hash[team] = [0,0] }
-  #   @game_teams.each do |game_team|
-  #     if season_games.include?(game_team.game_id)
-  #       coach[game_team.head_coach][1] += 1
-  #       coach[game_team.head_coach][0] += 1 if game_team.result == "WIN"
-  #     end
-  #   end
-  #   coach.each do |team_id, pair|
-  #     ratio = calculate_ratios(pair)
-  #     coach[team_id] = ratio
-  #   end
-  #   coach.key(coach.values.min)
-  # end
-
-  # def favorite_opponent(team_id)
-  #   rivals = Hash.new { |rivals, team| rivals[team] = [0,0] }
-  #   played = @game_teams.find_all do |game_team|
-  #     team_id == game_team.team_id
-  #   end
-  #   played.each do |game_A|
-  #     opponent_game = @game_teams.find do |game_B|
-  #       game_A.game_id == game_B.game_id && game_A.team_id != game_B.team_id
-  #     end
-  #     rivals[opponent_game.team_id][1] += 1
-  #     rivals[opponent_game.team_id][0] += 1 if opponent_game.result == "WIN"
-  #   end
-  #   rivals.each do |rival, pair|
-  #     rivals[rival] = calculate_ratios(pair)
-  #   end
-  #   rivals.key(rivals.values.min)
-  # end
-  #
-  # def rival(team_id)
-  #   rivals = Hash.new { |rivals, team| rivals[team] = [0,0] }
-  #   played = @game_teams.find_all do |game_team|
-  #     team_id == game_team.team_id
-  #   end
-  #   played.each do |game_A|
-  #     opponent_game = @game_teams.find do |game_B|
-  #       game_A.game_id == game_B.game_id && game_A.team_id != game_B.team_id
-  #     end
-  #     rivals[opponent_game.team_id][1] += 1
-  #     rivals[opponent_game.team_id][0] += 1 if opponent_game.result == "WIN"
-  #   end
-  #   rivals.each do |rival, pair|
-  #     rivals[rival] = calculate_ratios(pair)
-  #   end
-  #   rivals.key(rivals.values.max)
-  # end
-
-# def total_goals_by_team
-#     goals_by_team_id = {}
-#     game_teams.each do |game| #should this be @game_teams?
-#       if goals_by_team_id[game.team_id].nil?
-#         goals_by_team_id[game.team_id] = game.goals
-#       else
-#         goals_by_team_id[game.team_id] += game.goals
-#       end
-#     end
-#     goals_by_team_id
-#   end
-#
-#   def total_games_by_team
-#     games_by_team_id = {}
-#     game_teams.each do |game|
-#       if games_by_team_id[game.team_id].nil?
-#         games_by_team_id[game.team_id] = 1
-#       else
-#         games_by_team_id[game.team_id] += 1
-#       end
-#     end
-#     games_by_team_id
-#   end
-#
-#   def best_offense
-#     averages = total_goals_by_team.merge(total_games_by_team) do |team_id, goals, games|
-#       (goals/games.to_f).round(2)
-#     end
-#     average_max = averages.max_by do |team_id, average|
-#       average
-#     end
-#      average_max[0]
-#   end
-#
-#   def worst_offense
-#     averages = total_goals_by_team.merge(total_games_by_team) do |team_id, goals, games|
-#       (goals/games.to_f).round(2)
-#     end
-#     average_min = averages.min_by do |team_id, average|
-#       average
-#     end
-#      average_min[0]
-#   end
-# end
