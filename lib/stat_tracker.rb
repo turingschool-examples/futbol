3b877b289c1a9e464dec7adad28808f8262f6fe2
Joey Haas <josephthaas@gmail.com>
2021-03-02 10:51:40 -0700

    updated tracker references

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -5,10 +5,14 @@ class StatTracker
   include ReturnTeamable
   attr_reader :games, :game_teams, :teams
   def initialize
-    require 'pry'; binding.pryq
+    
     @games = GameTable.new('./data/games.csv')
     @game_teams = GameTeamTable.new('./data/game_teams.csv', self)
     @teams = TeamsTable.new('./data/teams.csv')
+  end
+
+  def self.from_csv(locations)
+    StatTracker.new(locations)
   end
 
   def game_by_season
@@ -78,45 +82,53 @@ class StatTracker
   def count_of_teams
     @teams.count_of_teams
   end
+
   def worst_offense
-    return_team(@game_teams.worst_offense, @teams.team_data).teamname
+    @game_teams.worst_offense
   end
+
   def highest_scoring_home_team
-    return_team(@game_teams.highest_scoring_home_team, @teams.team_data).teamname
+    @game_teams.highest_scoring_home_team
   end
+
   def lowest_scoring_home_team
-    return_team(@game_teams.lowest_scoring_home_team, @teams.team_data).teamname
+    @game_teams.lowest_scoring_home_team
   end
+
   def team_info(team_id_str)
     @teams.team_info(return_team(team_id_str.to_i, @teams.team_data))
   end
+
   def worst_season(team_id_str)
     year = @game_teams.worst_season(team_id_str.to_i)
     #add back in the 2nd year of season
     year + (year.to_i + 1).to_s
   end
+
   def most_goals_scored(team_id_str)
     @game_teams.most_goals_scored(team_id_str)
   end
+
   def favorite_opponent(team_id_str)
     #sends array [game_id,result]
     games = @game_teams.find_team_games(team_id_str).map{|game|[game.game_id,game.result]}
     @games.favorite_opponent(games)
   end
+
   def game_by_season(season)
     @game_teams.game_by_season(season)
   end
 
   def best_offense
-    return_team(@game_teams.best_offense, @teams.team_data).teamname
+    @game_teams.best_offense, @teams.team_data
   end
 
   def highest_scoring_visitor
-    return_team(@game_teams.highest_scoring_visitor, @teams.team_data).teamname
+    @game_teams.highest_scoring_visitor
   end
 
   def lowest_scoring_visitor
-    return_team(@game_teams.lowest_scoring_visitor, @teams.team_data).teamname
+    @game_teams.lowest_scoring_visitor, @teams.team_data
   end
 
   def best_season(team_id_str)
@@ -133,4 +145,7 @@ class StatTracker
     @games.rival(games)
   end
 
+  def average_win_percentage(team_id)
+    @game_teams.average_win_percentage(team_id)
+  end
 end
