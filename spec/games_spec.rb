889ae5c114bd87b79a4c1bff41998e03e842b452
Matthew Kimball <mekimba6@gmail.com>
2021-07-26 17:48:56 -0600

    adds tests for games spec

--- spec/games_spec.rb
+++ spec/games_spec.rb
@@ -2,5 +2,24 @@ require './lib/games.rb'
 
 RSpec.describe Game do
 
+  it 'exists' do
+    info = ["2012030221","20122013","Postseason","5/16/13","3","6","2","3","Toyota Stadium","/api/v1/venues/null"]
+    game = Game.new(info)
+    expect(game).to be_a(Game)
+  end
 
+  it 'has data' do
+    info = ["2012030221","20122013","Postseason","5/16/13","3","6","2","3","Toyota Stadium","/api/v1/venues/null"]
+    game = Game.new(info)
+    expect(game.data).to be_a(Hash)
+  end
+
+  it 'data has game ID' do
+    info = ["2012030221","20122013","Postseason","5/16/13","3","6","2","3","Toyota Stadium","/api/v1/venues/null"]
+    game = Game.new(info)
+    expect(game.data[:game_id]).to eq(2012030221)
+    expect(game.data[:type]).to eq("Postseason")
+    expect(game.data[:date_time]).to eq("5/16/13")
+    expect(game.data[:venue_link]).to eq("/api/v1/venues/null")
+  end
 end
