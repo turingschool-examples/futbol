409a82dfbdd2a40ae7e85a81e875e0175c0a8b37
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-07-26 22:25:24 -0600

    Add best_season, worst_season, average_win methods

--- lib/games_processor.rb
+++ lib/games_processor.rb
@@ -24,12 +24,52 @@ module GamesProcessor
   end
 
   def best_season(team_id)
-    games = all_team_games(team_id)
+    season_avg = seasons_win_count(team_id)
+    season_avg.each.max_by do |season, stats|
+      stats[:wins].fdiv(stats[:total])
+    end.first
+  end
+
+  def worst_season(team_id)
+    season_avg = seasons_win_count(team_id)
+    season_avg.each.min_by do |season, stats|
+      stats[:wins].fdiv(stats[:total])
+    end.first
   end
 
   def team_games_by_season(team_id)
     all_team_games(team_id).group_by do |game|
       game[:season]
     end
+  end
+
+  def seasons_win_count(team_id)
+    season_avg = {}
+
+    @games.each do |game|
+      season = game[:season]
+      if game[:home_team_id] == team_id || game[:away_team_id] == team_id
+        season_avg[season] ||= {wins: 0, total: 0}
+        if game[:home_team_id] == team_id
+          home_win = game[:home_goals] > game[:away_goals]
+          season_avg[season][:wins] += 1 if home_win
+        else
+          away_win = game[:home_goals] < game[:away_goals]
+          season_avg[season][:wins] += 1 if away_win
+        end
+        season_avg[season][:total] += 1
+      end
+    end
+    season_avg
+  end
+
+  def average_win_percentage(team_id)
+    wins = 0
+    games = 0
+    seasons_win_count(team_id).each do |season, stats|
+      wins += stats[:wins]
+      games += stats[:total]
+    end
+    (wins.fdiv(games)).round(2)
   end
 end
