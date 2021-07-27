64abfa47f6ccc4425c816c2864006ef2051903d4
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-07-26 18:39:37 -0600

    Add attributes

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,6 +1,16 @@
 class StatTracker
 
+  attr_reader :games,
+              :teams,
+              :game_teams
+
+  def initialize(locations)
+    @games = locations[:games]
+    @teams = locations[:teams]
+    @game_teams = locations[:game_teams]
+  end
+
   def self.from_csv(locations)
-    StatTracker.new 
+    StatTracker.new(locations)
   end
 end
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,7 +1,5 @@
 require 'spec_helper'
 
-
-
 RSpec.describe StatTracker do
   it "exists" do
     game_path = './data/mini_games.csv'
@@ -17,5 +15,23 @@ RSpec.describe StatTracker do
     stat_tracker = StatTracker.from_csv(locations)
 
     expect(stat_tracker).to be_a(StatTracker)
+  end
+
+  it "has attributes" do
+    game_path = './data/mini_games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/mini_game_teams.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    stat_tracker = StatTracker.from_csv(locations)
+
+    expect(stat_tracker.games).to eq(locations[:games])
+    expect(stat_tracker.teams).to eq(locations[:teams])
+    expect(stat_tracker.game_teams).to eq(locations[:game_teams])
   end
 end
