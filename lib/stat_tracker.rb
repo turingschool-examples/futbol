51390f89e8d0a9a8319344232ed29ce4605cc07b
Jamie Pace <jamiejpace@gmail.com>
2021-07-27 14:59:45 -0600

    Finish adding game stats methods

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -68,4 +68,54 @@ class StatTracker
     end.length
     (home_wins.to_f / (games.length)).round(2)
   end
+
+  def percentage_visitor_wins
+    vistor_wins = @games.find_all do |game|
+      game.away_goals > game.home_goals
+    end.length
+    (vistor_wins.to_f / (games.length)).round(2)
+  end
+
+  def percentage_ties
+    ties = @games.find_all do |game|
+      game.away_goals == game.home_goals
+    end.length
+    (ties.to_f / (games.length)).round(2)
+  end
+
+  def count_of_games_by_season
+    count = {}
+    @games.each do |game|
+      if count[game.season.to_s].nil?
+        count[game.season.to_s] = 1
+      else
+        count[game.season.to_s] += 1
+      end
+    end
+    count
+  end
+
+  def average_goals_per_game
+    total_goals = @games.sum do |game|
+      game.away_goals + game.home_goals
+    end
+    (total_goals.to_f / (@games.length)).round(2)
+  end
+
+  def average_goals_by_season
+    hash = {}
+    @games.each do |game|
+      if hash[game.season].nil?
+        hash[game.season] = [1, game.home_goals + game.away_goals]
+      else
+        hash[game.season][0] += 1
+        hash[game.season][1] += game.home_goals + game.away_goals
+      end
+    end
+    results = {}
+    hash.each do |season, stats|
+      results[season.to_s] = (stats[1].to_f / stats[0]).round(2)
+    end
+    results
+  end
 end
