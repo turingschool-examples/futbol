2cc26655e698c3830f44e116b426c4e179e38b23
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-07-26 18:09:46 -0600

    Add stat_tracker class and test

---
+++ spec/stat_tracker_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+
+
+RSpec.describe StatTracker do
+  it "exists" do
+    game_path = './data/mini_games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/mini_game_teams.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    stat_tracker = StatTracker.from_csv(locations)
+
+    expect(stat_tracker).to be_a(StatTracker)
+  end
+end
