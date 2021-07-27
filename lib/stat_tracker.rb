6b1e96b639462097a1bd1ef65bb51179227dff3c
Weston Ellis <ellisweston112@gmail.com>
2021-07-27 14:27:21 -0600

    Change data format to array of hashes instead of array of array

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,24 +1,34 @@
 class StatTracker
   attr_reader :games, :teams, :game_teams
-
-  def initialize(games, teams, game_teams)
+  def initialize (games, teams, game_teams)
     @games = games
     @teams = teams
     @game_teams = game_teams
   end
 
   def self.from_csv(locations)
-    # require "pry"; binding.pry
-    games = {}
-    teams = {}
-    game_teams = {}
+    games = []
+    teams = []
+    game_teams = []
 
-    games[:games] = CSV.read(locations[:games])
-    teams[:teams] =  CSV.read(locations[:teams])
-    game_teams[:game_teams] =  CSV.read(locations[:game_teams])
+    CSV.foreach(locations[:games], headers: true, header_converters: :symbol) do |row|
+      headers ||= row.headers
+
+      games << row.to_h
+    end
+
+    CSV.foreach(locations[:teams], headers: true, header_converters: :symbol) do |row|
+      headers ||= row.headers
+
+      teams << row.to_h
+    end
+
+    CSV.foreach(locations[:game_teams], headers: true, header_converters: :symbol) do |row|
+      headers ||= row.headers
+
+      game_teams << row.to_h
+    end
 
     StatTracker.new(games, teams, game_teams)
   end
-
-
 end
---
+++ spec/stat_tracker_spec.rb
@@ -0,0 +1,22 @@
+require 'simplecov'
+require './lib/stat_tracker'
+
+
+SimpleCov.start
+RSpec.describe StatTracker do
+
+  it 'exists' do
+    stat_tracker = StatTracker.new([], [], [])
+
+    expect(stat_tracker).to be_a(StatTracker)
+  end
+
+  it 'can access instance variables' do
+    stat_tracker = StatTracker.new(['a'], ['b'], ['c'])
+
+
+    expect(stat_tracker.games).to eq(['a'])
+    expect(stat_tracker.teams).to eq(['b'])
+    expect(stat_tracker.game_teams).to eq(['c'])
+  end
+end
