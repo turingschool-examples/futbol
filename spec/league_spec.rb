a0119c1282eef971677201b1e6205604b3c87d93
Weston Ellis <ellisweston112@gmail.com>
2021-07-27 16:41:36 -0600

    Update tests

--- spec/league_spec.rb
+++ spec/league_spec.rb
@@ -6,22 +6,43 @@ require "./lib/league"
 
 SimpleCov.start
 RSpec.describe League do
-  # before(:each) do
+  before(:each) do
+    game_path = './data/games_test.csv'
+    team_path = './data/teams_test.csv'
+    game_teams_path = './data/game_teams_test.csv'
 
-  it 'exists and can read data' do
-    league = League.new([], [], [])
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
 
-    expect(league).to be_a(League)
-    expect(league.games).to eq([])
-    expect(league.teams).to eq([])
-    expect(league.game_teams).to eq([])
+    @stat_tracker = StatTracker.from_csv(locations)
   end
 
-  it 'can count numbers of teams' do
-    team = [{}, {}, {}, {}, {}]
-    league = League.new([], team, [])
+    it 'exists and can read data' do
+      league = League.new([], [], [])
 
-    expect(league.count_of_teams).to eq(5)
+      expect(league).to be_a(League)
+      expect(league.games).to eq([])
+      expect(league.teams).to eq([])
+      expect(league.game_teams).to eq([])
+    end
+
+    it 'can count numbers of teams' do
+      team = @stat_tracker.teams
+      league = League.new(@stat_tracker.games, @stat_tracker.teams, @stat_tracker.game_teams)
+
+
+      expect(league.count_of_teams).to eq(32)
+    end
+
+    it 'can get best offense' do
+      league = League.new(@stat_tracker.games, @stat_tracker.teams, @stat_tracker.game_teams)
+
+
+    
   end
+
 
 end
