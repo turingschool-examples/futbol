9383e1d609345a8b4535c3940241823bfae0242d
Tanner Dale <tanner@tannerdale.com>
2021-07-27 14:09:31 -0500

    Add favorite team, rival methods

--- lib/games_processor.rb
+++ lib/games_processor.rb
@@ -60,4 +60,49 @@ module GamesProcessor
     end
     (wins.fdiv(games)).round(2)
   end
+
+  def opponent_win_loss(team_id)
+    win_loss = {}
+    @games.each do |game|
+      if game[:home_team_id] == team_id || game[:away_team_id] == team_id
+        teams = [game[:home_team_id], game[:away_team_id]]
+        goals = [game[:home_goals], game[:away_goals]]
+        team_index = teams.index(team_id)
+        opp_index = team_index - 1
+
+        win_loss[teams[opp_index]] ||= {wins: 0, total: 0}
+        if goals[team_index] > goals[opp_index]
+          win_loss[teams[opp_index]][:wins] += 1
+        end
+        win_loss[teams[opp_index]][:total] += 1
+      end
+    end
+    win_loss
+  end
+
+  def calculate_win_percent(team_id)
+    win_loss = opponent_win_loss(team_id)
+    win_loss.each.map do |team, results|
+      avg = results[:wins].fdiv(results[:total])
+      [team, avg]
+    end.to_h
+  end
+
+  def favorite_opponent(team_id)
+    win_loss = calculate_win_percent(team_id)
+    fav_team = win_loss.each.max_by do |team, result|
+      result
+    end.first
+
+    team_info(fav_team)["team_name"]
+  end
+
+  def rival(team_id)
+    win_loss = calculate_win_percent(team_id)
+    rival_team = win_loss.each.min_by do |team, result|
+      result
+    end.first
+
+    team_info(rival_team)["team_name"]
+  end
 end
