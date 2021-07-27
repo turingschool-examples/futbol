49cf58b5dddfb1001d544fd70d28920b842ce81d
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-07-26 18:40:22 -0600

    Add parsing module files

---
+++ lib/teams_processor.rb
@@ -0,0 +1,18 @@
+require 'csv'
+
+module TeamsProcessor
+  def parse(file_path)
+    teams = []
+
+    CSV.foreach(file_path, headers: true) do |row|
+      teams << {
+        team_id: row["team_id"],
+        franchise_id: row["franchiseId"],
+        team_name: row["teamName"],
+        abbrev: row["abbreviation"],
+        link: row["link"]
+      }
+    end
+    teams 
+  end
+end
