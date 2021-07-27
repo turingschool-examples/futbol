d88aa0ba4b2efedcdeb15610152614cdd5f8f70c
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-07-26 20:04:58 -0600

    Add team_info test

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -30,9 +30,17 @@ RSpec.describe StatTracker do
 
     stat_tracker = StatTracker.from_csv(locations)
 
+    expected = {
+      "team_id" => "18",
+      "franchise_id" => "34",
+      "team_name" => "Minnesota United FC",
+      "abbreviation" => "MIN",
+      "link" => "/api/v1/teams/18"
+    }
+
     expect(stat_tracker.teams).not_to be_empty
     expect(stat_tracker.game_teams).not_to be_empty
-    expect(stat_tracker.team_info).not_to be_empty
     expect(stat_tracker.games).not_to be_empty
+    expect(stat_tracker.team_info("18")).to eq(expected)
   end
 end
