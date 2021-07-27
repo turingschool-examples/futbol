1f7bb0604320ef0fe5f02afeed5dbb8b58d53e63
Kaitlyn Fox <ksmaddox3@gmail.com>
2021-07-26 20:05:54 -0600

    Add best_season and team_games_by_season methods

--- lib/games_processor.rb
+++ lib/games_processor.rb
@@ -16,4 +16,20 @@ module GamesProcessor
     end
     games
   end
+
+  def all_team_games(team_id)
+    @games.find_all do |game|
+      game[:home_team_id] == team_id || game[:away_team_id] == team_id
+    end
+  end
+
+  def best_season(team_id)
+    games = all_team_games(team_id)
+  end
+
+  def team_games_by_season(team_id)
+    all_team_games(team_id).group_by do |game|
+      game[:season]
+    end
+  end
 end
