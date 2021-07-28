2505f282501c8d6e3ff86246a58cd5edff3923ad
Andrew Massey <acmassey@ncsu.edu>
2021-07-28 12:22:42 -0400

    Quick refactor of game_stats to make data accessible in the same way across classes

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -1,15 +1,17 @@
 require_relative './stat_tracker'
 
 class GameStatistics
-  attr_reader :data
+  attr_reader :games, :teams, :game_teams
 
-  def initialize(data)
-    @data = data
+  def initialize(games, teams, game_teams)
+    @games = games
+    @teams = teams
+    @game_teams = game_teams
   end
 
   def highest_total_score
     highest_scoring_game =
-    @data.games.max_by do |game|
+    @games.max_by do |game|
       game[:away_goals].to_i + game[:home_goals].to_i
     end
     highest_scoring_game[:away_goals].to_i + highest_scoring_game[:home_goals].to_i
@@ -17,27 +19,27 @@ class GameStatistics
 
   def lowest_total_score
     lowest_scoring_game =
-    @data.games.min_by do |game|
+    @games.min_by do |game|
       game[:away_goals].to_i + game[:home_goals].to_i
     end
     lowest_scoring_game[:away_goals].to_i + lowest_scoring_game[:home_goals].to_i
   end
 
   def percentage_home_wins
-    (home_team_wins.fdiv(@data.games.length) * 100).round(2)
+    (home_team_wins.fdiv(@games.length)).round(2)
   end
 
   def percentage_visitor_wins
-    (visitor_team_wins.fdiv(@data.games.length) * 100).round(2)
+    (visitor_team_wins.fdiv(@games.length)).round(2)
   end
 
   def percentage_ties
-    (ties.fdiv(@data.games.length) * 100).round(2)
+    (ties.fdiv(@games.length)).round(2)
   end
 
   def home_team_wins
     home_wins =
-    @data.games.count do |game|
+    @games.count do |game|
       game[:home_goals] > game[:away_goals]
     end
     home_wins
@@ -45,7 +47,7 @@ class GameStatistics
 
   def visitor_team_wins
     visitor_wins =
-    @data.games.count do |game|
+    @games.count do |game|
       game[:home_goals] < game[:away_goals]
     end
     visitor_wins
@@ -53,7 +55,7 @@ class GameStatistics
 
   def ties
     ties =
-    @data.games.count do |game|
+    @games.count do |game|
       game[:home_goals] == game[:away_goals]
     end
     ties
@@ -61,7 +63,7 @@ class GameStatistics
 
   def count_of_games_by_season
     games_by_season = Hash.new(0)
-    @data.games.each do |game|
+    @games.each do |game|
       game.each do |key, value|
         if key == :season
           games_by_season[value] += 1
@@ -73,7 +75,7 @@ class GameStatistics
 
   def average_goals_per_game
     goals = []
-    @data.games.each do |game|
+    @games.each do |game|
       goals << game[:home_goals].to_i + game[:away_goals].to_i
     end
     goals.sum.fdiv(goals.length).round(2)
@@ -81,7 +83,7 @@ class GameStatistics
 
   def total_goals_by_season
     goals_by_season = Hash.new(0)
-    @data.games.each do |game|
+    @games.each do |game|
       game.each do |key, value|
         if key == :season
           goals_by_season[value] += game[:home_goals].to_i + game[:away_goals].to_i
