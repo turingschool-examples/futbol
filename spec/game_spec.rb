6e8326cd28a74e0d6a856ded47510090f6dd08e7
Jessica Jones <daddehjones@gmail.com>
2021-07-28 13:12:59 -0400

    Test run game2.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,6 @@
-require_relative './lib/game'
-require_relative './data/games_sample'
+require './lib/game'
+# require './data/games_sample'
+# require './lib/stat_tracker'
 
 RSpec.describe Game do
   context "it's a futbol game" do
@@ -14,6 +15,17 @@ RSpec.describe Game do
       home_goals: "1"
     })
 
+    game2 = Game.new({
+      game_id: "2012020251",
+      season: "20122013",
+      type: "Regular Season",
+      date_time: "2/23/13",
+      away_team_id: "52",
+      home_team_id: "4",
+      away_goals: "3",
+      home_goals: "3"
+    })
+
     it "exists" do
       expect(game).to be_a(Game)
     end
@@ -24,6 +36,10 @@ RSpec.describe Game do
 
     it "calculates total game score" do
       expect(game.total_game_score).to eq(1)
+    end
+
+    it "calculates total games" do
+      expect(game.total_games).to eq(2)
     end
   end
 end
