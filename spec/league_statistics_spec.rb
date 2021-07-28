b8d39e0ff4792ce66bcfb592e938dc7c67cd1687
Michael Mattson <michael.p.mattson@gmail.com>
2021-07-28 11:59:52 -0600

    Add best and worst offense

--- spec/league_statistics_spec.rb
+++ spec/league_statistics_spec.rb
@@ -20,4 +20,28 @@ RSpec.describe StatTracker do
   it 'total number of teams in the data' do
     expect(@stat_tracker.count_of_teams).to eq(32)
   end
+
+  it 'returns total goals all seasons per team' do
+    expect(@stat_tracker.total_goals_all_seasons(3)).to eq(8)
+  end
+
+  it 'returns total games all seasons per team' do
+    expect(@stat_tracker.total_games_all_seasons(3)).to eq(5)
+  end
+
+  it 'returns average goals for all seasons per team' do
+    expect(@stat_tracker.average_goals_all_seasons(3)).to eq(1.60)
+  end
+
+  it 'team by id' do
+    expect(@stat_tracker.team_by_id(3)).to eq("Houston Dynamo")
+  end
+
+  it 'returns team with highest average goals across seasons' do
+    expect(@stat_tracker.best_offense).to eq("FC Dallas")
+  end
+
+  it 'returns team with lowest average goals across seasons' do
+    expect(@stat_tracker.worst_offense).to eq("Sporting Kansas City")
+  end
 end
