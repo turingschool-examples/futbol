efe49dbc9fcfa02d4d70defa8f54572d6e9d840f
Henry Schmid <henry.schmid1@gmail.com>
2021-07-27 18:05:24 -0600

    Move average goals per game and season to games_processor

--- lib/game_teams_processor.rb
+++ lib/game_teams_processor.rb
@@ -47,30 +47,4 @@ module GameTeamsProcessor
     end
     (goals.fdiv(@game_teams.size) * 2).round(2)
   end
-
-  def average_goals_by_season
-    goals_per_season.reduce({}) do |acc, season_goals|
-      acc[season_goals[0]] = season_goals[1].fdiv(games_per_season(season_goals[0])).round(2)
-      acc
-    end
-  end
-
-  def goals_per_game(game)
-    game[:away_goals].to_i + game[:home_goals].to_i
-  end
-
-  def goals_per_season
-    goals = {}
-    @games.each do |game|
-      goals[game[:season]] ||= 0
-      goals[game[:season]] += goals_per_game(game)
-    end
-    goals
-  end
-
-  def games_per_season(season)
-    @games.count do |game|
-      game if game[:season] == season
-    end
-  end
 end
