d57ad70451ea1f0f0aae048f0febb1c303c78dad
Tanner Dale <tanner@tannerdale.com>
2021-07-28 17:21:30 -0500

    Begin transfering functionality to game manager class

--- lib/games_processor.rb
+++ lib/games_processor.rb
@@ -1,22 +1,6 @@
 require 'csv'
 
 module GamesProcessor
-  def parse_games_file(file_path)
-    games = []
-
-    CSV.foreach(file_path, headers: true) do |row|
-      games << {
-        game_id: row["game_id"],
-        season: row["season"],
-        away_team_id: row["away_team_id"],
-        home_team_id: row["home_team_id"],
-        away_goals: row["away_goals"],
-        home_goals: row["home_goals"]
-      }
-    end
-    games
-  end
-
   def best_season(team_id)
     get_season_averages(team_id).max_by do |season, average|
       average
@@ -36,21 +20,9 @@ module GamesProcessor
     end.to_h
   end
 
-  def get_home_or_away(team_id, game)
-    teams = [game[:home_team_id], game[:away_team_id]]
-    goals = [game[:home_goals], game[:away_goals]]
-    team_index = teams.index(team_id)
-    opp_index = team_index - 1
-    {
-      team_goals: goals[team_index],
-      opp_id: teams[opp_index],
-      opp_goals: goals[opp_index]
-    }
-  end
-
-  def seasons_win_count(team_id)
+  def seasons_win_count(team_id, games)
     season_average = {}
-    @games.each do |game|
+    games.each do |game|
       season = game[:season]
       if game[:home_team_id] == team_id || game[:away_team_id] == team_id
         data = get_home_or_away(team_id, game)
@@ -63,49 +35,6 @@ module GamesProcessor
       end
     end
     season_average
-  end
-
-  def opponent_win_count(team_id)
-    win_loss = {}
-    @games.each do |game|
-      if game[:home_team_id] == team_id || game[:away_team_id] == team_id
-        data = get_home_or_away(team_id, game)
-
-        win_loss[data[:opp_id]] ||= {wins: 0, total: 0}
-        if data[:team_goals] > data[:opp_goals]
-          win_loss[data[:opp_id]][:wins] += 1
-        end
-        win_loss[data[:opp_id]][:total] += 1
-      end
-    end
-    win_loss
-  end
-
-  def average_win_percentage(team_id)
-    wins = 0
-    games = 0
-    seasons_win_count(team_id).each do |season, stats|
-      wins += stats[:wins]
-      games += stats[:total]
-    end
-    (wins.fdiv(games)).round(2)
-  end
-
-  def calculate_win_percents(team_id)
-    win_loss = opponent_win_count(team_id)
-    win_loss.map do |team, results|
-      avg = results[:wins].fdiv(results[:total])
-      [team, avg]
-    end.to_h
-  end
-
-  def favorite_opponent(team_id)
-    win_loss = calculate_win_percents(team_id)
-    fav_team = win_loss.max_by do |team, result|
-      result
-    end.first
-
-    team_info(fav_team)["team_name"]
   end
 
   def rival(team_id)
