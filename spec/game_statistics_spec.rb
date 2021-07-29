750def30cb5a5efa20337102dd4c1a4384d0d367
Andrew Massey <acmassey@ncsu.edu>
2021-07-29 18:08:49 -0400

    Recalculate expect values for game_stats_spec

--- spec/game_statistics_spec.rb
+++ spec/game_statistics_spec.rb
@@ -26,7 +26,7 @@ RSpec.describe GameStatistics do
   end
 
   it 'finds highest total score' do
-    expect(@game_stats.highest_total_score).to eq(5)
+    expect(@game_stats.highest_total_score).to eq(7)
   end
 
   it 'finds lowest total score' do
@@ -34,23 +34,23 @@ RSpec.describe GameStatistics do
   end
 
   it 'counts home wins' do
-    expect(@game_stats.home_team_wins).to eq(21)
+    expect(@game_stats.home_team_wins).to eq(49)
   end
 
   it 'finds percentage of home wins' do
-    expect(@game_stats.percentage_home_wins).to eq(0.72)
+    expect(@game_stats.percentage_home_wins).to eq(0.55)
   end
 
   it 'finds visitor wins' do
-    expect(@game_stats.visitor_team_wins).to eq(7)
+    expect(@game_stats.visitor_team_wins).to eq(37)
   end
 
   it 'finds percentage of visitor wins' do
-    expect(@game_stats.percentage_visitor_wins).to eq(0.24)
+    expect(@game_stats.percentage_visitor_wins).to eq(0.42)
   end
 
   it 'finds number of ties' do
-    expect(@game_stats.ties).to eq(1)
+    expect(@game_stats.ties).to eq(3)
   end
 
 
@@ -59,18 +59,36 @@ RSpec.describe GameStatistics do
   end
 
   it 'finds the count of games by season' do
-    expect(@game_stats.count_of_games_by_season).to eq({ '20122013' => 29 })
+    expect(@game_stats.count_of_games_by_season).to eq({
+      "20122013"=>57,
+      "20132014"=>6,
+      "20142015"=>6,
+      "20152016"=>16,
+      "20162017"=>4
+      })
   end
 
-  it 'finds average goals by game' do
-    expect(@game_stats.average_goals_per_game).to eq(3.69)
+  it 'finds average goals per game' do
+    expect(@game_stats.average_goals_per_game).to eq(3.91)
   end
 
   it 'finds total goals by season' do
-    expect(@game_stats.total_goals_by_season).to eq({ '20122013' =>107 })
+    expect(@game_stats.total_goals_by_season).to eq({
+      "20122013"=>220,
+      "20132014"=>26,
+      "20142015"=>21,
+      "20152016"=>62,
+      "20162017"=>19
+      })
   end
 
-  it 'finds average_goals_per_game' do
-    expect(@game_stats.average_goals_by_season).to eq({ '20122013' => 3.69 })
+  it 'finds average_goals_per_game in a season' do
+    expect(@game_stats.average_goals_by_season).to eq({
+      "20122013"=>3.86,
+      "20132014"=>4.33,
+      "20142015"=>3.5,
+      "20152016"=>3.88,
+      "20162017"=>4.75
+      })
   end
 end
