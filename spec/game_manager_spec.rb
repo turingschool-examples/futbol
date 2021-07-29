a68793fb97710598ad3635767d3c1009ab8683ba
Laura Court <laura.mcourt@gmail.com>
2021-07-28 20:56:18 -0400

    Add lowest total score

--- spec/game_manager_spec.rb
+++ spec/game_manager_spec.rb
@@ -22,8 +22,19 @@ RSpec.describe GameManager do
     expect(@game_manager.games).to be_an(Array)
   end
 
-  it "is an array of season numbers" do
-    result = ["20122013", "20152016", "20132014", "20142015", "20172018", "20162017"]
-    expect(@game_manager.array_of_seasons).to eq(result)
+  it "adds team scores together for total score" do
+    expect(@game_manager.total_game_score).to eq([5, 6, 4, 1, 5, 7, 3, 5, 6, 4, 4, 3, 4, 4, 6, 5, 5, 5, 5])
   end
+
+  it "finds the higest total score" do
+    expect(@game_manager.highest_total_score).to eq(7)
+  end
+
+  it "finds the lowest total score" do
+    expect(@game_manager.lowest_total_score).to eq(1)
+  end
+  # it "is an array of season numbers" do
+  #   result = ["20122013", "20152016", "20132014", "20142015", "20172018", "20162017"]
+  #   expect(@game_manager.array_of_seasons).to eq(result)
+  # end
 end
