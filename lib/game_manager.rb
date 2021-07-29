4341f79de39596b01404f2dcc045ee1cb1ec56b6
Laura Court <laura.mcourt@gmail.com>
2021-07-28 20:56:39 -0400

    Add lowest total score

--- lib/game_manager.rb
+++ lib/game_manager.rb
@@ -7,10 +7,32 @@ class GameManager
     @games = Game.read_file(locations[:games])
   end
 
-
-  def array_of_seasons
+  def total_game_score
     @games.map do |game|
-      game.season
-    end.uniq
+      game.away_goals.to_i + game.home_goals.to_i
+    end
+  end
+
+  def highest_total_score
+    total_game_score.max_by do |score|
+      score
+    end
+  end
+
+  def lowest_total_score
+    total_game_score.min_by do |score|
+      score
+    end
   end
 end
+
+
+
+
+
+
+  # def array_of_seasons
+  #   @games.map do |game|
+  #     game.season
+  #   end.uniq
+  # end
