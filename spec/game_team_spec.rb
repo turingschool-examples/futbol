c2863704f16cbb3a28578c36d3b5a0b6231d5fa4
Rowan DeLong <rowanwinzer@gmail.com>
2021-07-29 11:38:02 -0600

    Fix instance variable tests

--- spec/game_team_spec.rb
+++ spec/game_team_spec.rb
@@ -11,8 +11,8 @@ RSpec.describe GameTeam do
   it 'game_team has data' do
     info = {game_id:"2012030221", team_id:"3", hoa:"away", result:"LOSS", settled_in:"OT", head_coach:"John Tortorella", goals:"2", shots:"8", tackles:"44", pim:"8", powerplayopportunities:"3", powerplaygoals:"0", faceoffwinpercentage:"44.8", giveaways:"17", takeaways:"7"}
     game_team = GameTeam.new(info)
-    expect(game_team.game_id).to eq(2012030221)
-    expect(game_team.team_id).to eq(3)
+    expect(game_team.game_id).to eq("2012030221")
+    expect(game_team.team_id).to eq("3")
     expect(game_team.result).to eq("LOSS")
     expect(game_team.settled_in).to eq("OT")
     expect(game_team.head_coach).to eq("John Tortorella")
