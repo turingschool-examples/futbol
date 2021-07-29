0cf9477628ee00e727707b8ee0043fedb5b2465d
Laura Court <laura.mcourt@gmail.com>
2021-07-28 21:27:40 -0400

    Add percentage and count tests

--- spec/game_manager_spec.rb
+++ spec/game_manager_spec.rb
@@ -33,6 +33,35 @@ RSpec.describe GameManager do
   it "finds the lowest total score" do
     expect(@game_manager.lowest_total_score).to eq(1)
   end
+
+  it 'counts total games' do
+    expect(@game_manager.total_games).to eq(19)
+  end
+
+  it 'has home wins count' do
+    expect(@game_manager.home_wins_count).to eq(12)
+  end
+
+  it 'has home win percents' do
+    expect(@game_manager.percent_home_wins).to eq(63.2)
+  end
+
+  it 'has visitor wins count' do
+    expect(@game_manager.visitor_wins_count).to eq(2)
+  end
+
+  it 'has visitor win percents' do
+    expect(@game_manager.percent_visitor_wins).to eq(10.5)
+  end
+
+  it 'has a tie count' do
+    expect(@game_manager.tie_count).to eq(5)
+  end
+
+  it 'has tie percent' do
+    expect(@game_manager.percent_ties).to eq(26.3)
+
+  end
   # it "is an array of season numbers" do
   #   result = ["20122013", "20152016", "20132014", "20142015", "20172018", "20162017"]
   #   expect(@game_manager.array_of_seasons).to eq(result)
