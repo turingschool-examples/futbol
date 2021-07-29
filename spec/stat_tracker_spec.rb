16f5eeb196e09074025b8f9c87107528e3e8a184
Blake Dempsey <bdempsey864@gmail.com>
2021-07-29 11:22:59 -0600

    Trim context season stats methods

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -148,198 +148,99 @@ RSpec.describe StatTracker do
   end
 
   context 'Season stats methods' do
+    game_path = './spec/fixture_files/test_games.csv'
+    team_path = './spec/fixture_files/test_teams.csv'
+    game_teams_path = './spec/fixture_files/test_game_teams.csv'
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+    stat_tracker = StatTracker.from_csv(locations)
+    
     it '#winningest_coach' do
-      game_path = './spec/fixture_files/test_games.csv'
-      team_path = './spec/fixture_files/test_teams.csv'
-      game_teams_path = './spec/fixture_files/test_game_teams.csv'
-      locations = {
-        games: game_path,
-        teams: team_path,
-        game_teams: game_teams_path
-      }
-      stat_tracker = StatTracker.from_csv(locations)
       expect(stat_tracker.winningest_coach("20132014")).to eq("Claude Noel")
     end
 
     it '#worst_coach' do
-      game_path = './spec/fixture_files/test_games.csv'
-      team_path = './spec/fixture_files/test_teams.csv'
-      game_teams_path = './spec/fixture_files/test_game_teams.csv'
-      locations = {
-        games: game_path,
-        teams: team_path,
-        game_teams: game_teams_path
-      }
-      stat_tracker = StatTracker.from_csv(locations)
       expect(stat_tracker.worst_coach("20132014")).to eq("Mike Babcock")
     end
 
     it '#game_ids_by_season' do
-      game_path = './spec/fixture_files/test_games.csv'
-      team_path = './spec/fixture_files/test_teams.csv'
-      game_teams_path = './spec/fixture_files/test_game_teams.csv'
-      locations = {
-        games: game_path,
-        teams: team_path,
-        game_teams: game_teams_path
-      }
-      stat_tracker = StatTracker.from_csv(locations)
       expect(stat_tracker.game_ids_by_season("20122013").size).to eq(111)
     end
 
     it '#game_teams_by_season' do
-      game_path = './spec/fixture_files/test_games.csv'
-      team_path = './spec/fixture_files/test_teams.csv'
-      game_teams_path = './spec/fixture_files/test_game_teams.csv'
-      locations = {
-        games: game_path,
-        teams: team_path,
-        game_teams: game_teams_path
-      }
-      stat_tracker = StatTracker.from_csv(locations)
       expect(stat_tracker.game_teams_by_season("20122013").size).to eq(220)
     end
 
     it '#coach_stats_by_season' do
-      game_path = './spec/fixture_files/test_games.csv'
-      team_path = './spec/fixture_files/test_teams.csv'
-      game_teams_path = './spec/fixture_files/test_game_teams.csv'
-      locations = {
-        games: game_path,
-        teams: team_path,
-        game_teams: game_teams_path
-      }
-      stat_tracker = StatTracker.from_csv(locations)
       expect(stat_tracker.coach_stats_by_season("20122013")["John Tortorella"]).to eq([14, 2])
     end
 
     it '#team_shots_by_season' do
-      game_path = './spec/fixture_files/test_games.csv'
-      team_path = './spec/fixture_files/test_teams.csv'
-      game_teams_path = './spec/fixture_files/test_game_teams.csv'
-      locations = {
-        games: game_path,
-        teams: team_path,
-        game_teams: game_teams_path
-      }
-      stat_tracker = StatTracker.from_csv(locations)
       expect(stat_tracker.team_shots_by_season("20122013")[3]).to eq([19, 100])
     end
 
     it '#most_accurate_team' do
-      game_path = './spec/fixture_files/test_games.csv'
-      team_path = './spec/fixture_files/test_teams.csv'
-      game_teams_path = './spec/fixture_files/test_game_teams.csv'
-      locations = {
-        games: game_path,
-        teams: team_path,
-        game_teams: game_teams_path
-      }
-      stat_tracker = StatTracker.from_csv(locations)
       expect(stat_tracker.most_accurate_team("20132014")).to eq "Utah Royals FC"
       expect(stat_tracker.most_accurate_team("20142015")).to eq "Toronto FC"
     end
 
     it "#least_accurate_team" do
-      game_path = './spec/fixture_files/test_games.csv'
-      team_path = './spec/fixture_files/test_teams.csv'
-      game_teams_path = './spec/fixture_files/test_game_teams.csv'
-      locations = {
-        games: game_path,
-        teams: team_path,
-        game_teams: game_teams_path
-      }
-      stat_tracker = StatTracker.from_csv(locations)
       expect(stat_tracker.least_accurate_team("20132014")).to eq "Sky Blue FC"
       expect(stat_tracker.least_accurate_team("20142015")).to eq "North Carolina Courage"
     end
 
     it "#tackles_by_season" do
-      game_path = './spec/fixture_files/test_games.csv'
-      team_path = './spec/fixture_files/test_teams.csv'
-      game_teams_path = './spec/fixture_files/test_game_teams.csv'
-      locations = {
-        games: game_path,
-        teams: team_path,
-        game_teams: game_teams_path
-      }
-      stat_tracker = StatTracker.from_csv(locations)
       expect(stat_tracker.tackles_by_season("20132014")[3]).to eq 102
     end
 
     it "#most_tackles" do
-      game_path = './spec/fixture_files/test_games.csv'
-      team_path = './spec/fixture_files/test_teams.csv'
-      game_teams_path = './spec/fixture_files/test_game_teams.csv'
-      locations = {
-        games: game_path,
-        teams: team_path,
-        game_teams: game_teams_path
-      }
-      stat_tracker = StatTracker.from_csv(locations)
       expect(stat_tracker.most_tackles("20132014")).to eq "FC Cincinnati"
       expect(stat_tracker.most_tackles("20142015")).to eq "DC United"
     end
 
     it "#fewest_tackles" do
-      game_path = './spec/fixture_files/test_games.csv'
-      team_path = './spec/fixture_files/test_teams.csv'
-      game_teams_path = './spec/fixture_files/test_game_teams.csv'
-      locations = {
-        games: game_path,
-        teams: team_path,
-        game_teams: game_teams_path
-      }
-      stat_tracker = StatTracker.from_csv(locations)
       expect(stat_tracker.fewest_tackles("20132014")).to eq "Sky Blue FC"
       expect(stat_tracker.fewest_tackles("20142015")).to eq "North Carolina Courage"
     end
   end
 
   context 'Team stats methods' do
+    game_path = './spec/fixture_files/test_games.csv'
+    team_path = './spec/fixture_files/test_teams.csv'
+    game_teams_path = './spec/fixture_files/test_game_teams.csv'
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+    stat_tracker = StatTracker.from_csv(locations)
+
     it '#team_info(team_id)' do
-      game_path = './spec/fixture_files/test_games.csv'
-      team_path = './spec/fixture_files/test_teams.csv'
-      game_teams_path = './spec/fixture_files/test_game_teams.csv'
-      locations = {
-        games: game_path,
-        teams: team_path,
-        game_teams: game_teams_path
-      }
-      stat_tracker = StatTracker.from_csv(locations)
       expected = {
-      "team_id" => "18",
-      "franchise_id" => "34",
-      "team_name" => "Minnesota United FC",
-      "abbreviation" => "MIN",
-      "link" => "/api/v1/teams/18"
-      }
+        "team_id" => "18",
+        "franchise_id" => "34",
+        "team_name" => "Minnesota United FC",
+        "abbreviation" => "MIN",
+        "link" => "/api/v1/teams/18"
+        }
       expect(stat_tracker.team_info("18")).to eq(expected)
     end
 
     it "#find_win_count" do
-      game_path = './spec/fixture_files/test_games.csv'
-      team_path = './spec/fixture_files/test_teams.csv'
-      game_teams_path = './spec/fixture_files/test_game_teams.csv'
-      locations = {
-        games: game_path,
-        teams: team_path,
-        game_teams: game_teams_path
-      }
-      stat_tracker = StatTracker.from_csv(locations)
-      expect(stat_tracker.find_win_count("6")).to eq({20122013=>[11, 10], 20172018=>[16, 10], 20132014=>[8, 6], 20142015=>[10, 4], 20152016=>[3, 1], 20162017=>[2, 0]})
+      expect(stat_tracker.find_win_count("6")).to eq({
+        20122013=>[11, 10], 
+        20172018=>[16, 10], 
+        20132014=>[8, 6], 
+        20142015=>[10, 4], 
+        20152016=>[3, 1], 
+        20162017=>[2, 0]
+      })
     end
 
     it "#best_season(team_id)" do
-      game_path = './spec/fixture_files/test_games.csv'
-      team_path = './spec/fixture_files/test_teams.csv'
-      game_teams_path = './spec/fixture_files/test_game_teams.csv'
-      locations = {
-        games: game_path,
-        teams: team_path,
-        game_teams: game_teams_path
-      }
-      stat_tracker = StatTracker.from_csv(locations)
       expect(stat_tracker.best_season("6")).to eq "20122013"
     end
 
