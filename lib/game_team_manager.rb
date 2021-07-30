c5f47cf8f5f93df7eb84112507e4bdabc8353de0
matthewjholmes <ilookup@gmail.com>
2021-07-30 16:20:06 -0600

    helper methods for #best_offense

--- lib/game_team_manager.rb
+++ lib/game_team_manager.rb
@@ -21,5 +21,29 @@ class GameTeamManager
     end
   end
 
+  def total_games_all_seasons(team_id)
+    total = 0
+    @game_teams.each do |game_id, teams|
+      teams.each do |hoa, team|
+        total += 1 if team_id == team.team_id.to_i
+      end
+    end
+    total
+  end
+
+  def total_goals_all_seasons(team_id)
+    total = 0
+    @game_teams.each do |game_id, teams|
+      teams.each do |hoa, team|
+        total += team.goals.to_i if team_id == team.team_id.to_i
+      end
+    end
+    total
+  end
+
+  def average_goals_all_seasons(team_id)
+    average = total_goals_all_seasons(team_id) / total_games_all_seasons(team_id).to_f
+    average.round(2)
+  end
 
 end
