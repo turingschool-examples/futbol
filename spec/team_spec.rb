e05acade23880648d6809da188860cb11625be86
Weston Ellis <ellisweston112@gmail.com>
2021-07-31 13:21:24 -0600

    Fix test

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe Team do
 
   it 'exists and can take a hash' do
     team = Team.new({
-      team_name: 'name',
+      teamname: 'name',
       team_id: 1
       })
 
     expect(team).to be_a(Team)
     expect(team.team_id).to eq(1)
-    expect(team.name).to eq('name')
+    expect(team.team_name).to eq('name')
   end
 
 end
