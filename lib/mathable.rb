7841afbfd921bcde9a77e46da9798aeeb265262f
Henry Schmid <henry.schmid1@gmail.com>
2021-07-31 17:53:32 -0600

    Rename average method to hash_average method, add new average method

--- lib/mathable.rb
+++ lib/mathable.rb
@@ -1,6 +1,10 @@
 module Mathable
-  def average(data)
+  def hash_average(data)
     count, total = data.keys
     data[count].fdiv(data[total])
+  end
+
+  def average(num1, num2)
+    num1.fdiv(num2)
   end
 end
