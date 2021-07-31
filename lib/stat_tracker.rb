2b6a3da15cc1590410171feb529968f03d637f9b
Christina Delpone <cdelpone@gmail.com>
2021-07-30 18:12:51 -0600

    Clean stat_tracker

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,24 +1,9 @@
-require_relative './teams_processor'
-require_relative './games_processor'
-require_relative './game_teams_processor'
-
 class StatTracker
-  include TeamsProcessor
-  include GamesProcessor
-  include GameTeamsProcessor
-
-  attr_reader :games,
-              :teams,
-              :game_teams
 
   def initialize(locations)
-    @games = parse_games_file(locations[:games])
-    @teams = parse_teams_file(locations[:teams])
-    @game_teams = parse_game_teams_file(locations[:game_teams])
   end
 
   def self.from_csv(locations)
     StatTracker.new(locations)
   end
 end
-##########move elsewhere
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,18 +1,7 @@
 require 'spec_helper'
 
 RSpec.describe StatTracker do
-  context 'initialize' do
-    game_path = './data/mini_games.csv'
-    team_path = './data/teams.csv'
-    game_teams_path = './data/mini_game_teams.csv'
-    locations = {
-      games: game_path,
-      teams: team_path,
-      game_teams: game_teams_path
-    }
-    stat_tracker = StatTracker.from_csv(locations)
-
-    it "exists" do
-      expect(stat_tracker).to be_a(StatTracker)
-    end
+  it "exists" do
+    expect(stat_tracker).to be_a(StatTracker)
   end
+end
