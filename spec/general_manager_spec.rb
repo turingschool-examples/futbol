d6de6c4ba499a397f614ca2df86bab50930bbf98
Christina Delpone <cdelpone@gmail.com>
2021-07-31 09:51:32 -0600

    Add games mgr methods

--- spec/general_manager_spec.rb
+++ spec/general_manager_spec.rb
@@ -41,6 +41,54 @@ RSpec.describe GeneralManager do
     it 'has a teams count in a league' do
       expect(@general_manager.count_of_teams).to eq(32)
     end
+  end
 
+  context 'games' do
+    it 'has highest and lowest total scored' do
+      expect(@general_manager.highest_total_score).to eq(7)
+      expect(@general_manager.lowest_total_score).to eq(1)
+    end
+
+    it 'counts games per season' do
+      expected = {
+        '20132014' => 6,
+        '20142015' => 19,
+        '20152016' => 9,
+        '20162017' => 17
+      }
+      expect(@general_manager.count_of_games_by_season).to eq(expected)
+    end
+
+    it 'has average goals by season' do
+      expected = {
+        "20142015"=>3.63,
+        "20152016"=>4.33,
+        "20132014"=>4.67,
+        "20162017"=>4.24
+      }
+      expect(@general_manager.average_goals_by_season).to eq(expected)
+    end
+
+    it 'has average goals per game' do
+      expect(@general_manager.average_goals_per_game).to eq(4.08)
+    end
+
+    it 'highest scoring vistor and home team' do
+      expect(@general_manager.highest_scoring_visitor).to eq("5")
+      expect(@general_manager.highest_scoring_home_team).to eq("24")
+    end
+
+    it 'lowest scoring vistor and home team' do
+      expect(@general_manager.lowest_scoring_visitor).to eq("13")
+      expect(@general_manager.lowest_scoring_home_team).to eq("13")
+    end
+
+    it 'has a favourite opponent' do
+      expect(@general_manager.favorite_opponent("15")).to eq("2")
+    end
+
+    it 'has a rival' do
+      expect(@general_manager.rival("15")).to eq("10")
+    end
   end
 end
