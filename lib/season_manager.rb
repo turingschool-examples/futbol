500b3f9cd07b975f5c47a8fd97f9110d47a6fadf
Erika Kischuk <erika.kischuk@gmail.com>
2021-07-31 23:19:59 -0600

    Refactor game_id_by_season to return a single array and utilize helper method games_in_season

--- lib/season_manager.rb
+++ lib/season_manager.rb
@@ -1,18 +1,15 @@
-require_relative './game'
-require_relative './game_team'
-require_relative './team'
-
-
 class SeasonManager
   def initialize(hash)
     @seasons = hash
   end
 
-  def game_id_by_season
-    @seasons.transform_values do |games|
-      games.map do |game|
-        game.game_id
-      end
+  def games_in_season(season)
+    @seasons[season]
+  end
+
+  def game_id_by_season(season)
+    games_in_season(season).map do |game|
+      game.game_id
     end
   end
 end
