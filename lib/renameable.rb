cd61619dc0f403646830c3e32f27feefe83f85ea
Ozzie <mikeosmonson@gmail.com>
2021-08-01 10:09:02 -0600

    Add #hash_generator

--- lib/renameable.rb
+++ lib/renameable.rb
@@ -3,4 +3,21 @@ module Renameable
   def season_verification(game, season)
     game.game_id.start_with?(season[0..3])
   end
+
+  def hash_generator(data_set, key, value, season)
+    hash = Hash.new(0)
+    data_set.each do |game|
+      if season_verification(game, season)
+        hash[game.send(key)] += game.send(value).to_i
+      end
+    end
+    hash
+  end
+
+  def team_identifier(id)
+    matching_team = @teams.find do |team|
+      team.team_id == id
+    end
+    matching_team.team_name
+  end
 end
