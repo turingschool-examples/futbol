05377d18dc27e7d176fc46f359a8156057094456
Erika Kischuk <erika.kischuk@gmail.com>
2021-07-31 23:50:51 -0600

    Add coaches method to return all coaches for a collection of game ids

--- lib/game_team_manager.rb
+++ lib/game_team_manager.rb
@@ -7,14 +7,23 @@ class GameTeamManager
     @game_teams = GameTeam.read_file(locations[:game_teams])
   end
 
-  def by_game_id(id)
+  def by_game_id(game_id)
     @game_teams.filter do |game_team|
-      game_team.game_id == id
+      game_team.game_id == game_id
     end
   end
 
-  def winning_coach(id)
-    winner = by_game_id(id).find do |game_team|
+  def coaches(season_game_ids)
+    season_game_ids.flat_map do |game_id|
+      game_team_pair = by_game_id(game_id)
+      game_team_pair.map do |game_team|
+        game_team.head_coach
+      end
+    end
+  end
+
+  def winning_coach(game_id)
+    winner = by_game_id(game_id).find do |game_team|
         game_team.result == "WIN"
       end
     winner.head_coach
