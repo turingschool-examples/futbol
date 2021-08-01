aa8043dcb7ceae44659835703aca40b7468a7ed6
Erika Kischuk <erika.kischuk@gmail.com>
2021-08-01 15:38:01 -0600

    Add team_id_by_season to return array of team ids within a given season

--- lib/season_manager.rb
+++ lib/season_manager.rb
@@ -1,6 +1,6 @@
 class SeasonManager
   def initialize(hash)
-    @seasons = hash
+    @seasons = hash # keys = seasons, values = array of game objects
   end
 
   def games_in_season(season)
@@ -11,5 +11,14 @@ class SeasonManager
     games_in_season(season).map do |game|
       game.game_id
     end
+  end
+
+  def team_id_by_season(season)
+    team_ids = []
+    games_in_season(season).each do |game|
+      team_ids << game.away_team_id
+      team_ids << game.home_team_id
+    end
+    team_ids.uniq
   end
 end
