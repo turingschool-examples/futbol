f00f2f1f06f1349912130f3a5e550fd4b11e3634
Laura Court <laura.mcourt@gmail.com>
2021-08-02 22:58:57 -0400

    Add methods for game stats

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -37,6 +37,26 @@ class StatTracker
     @game_manager.percentage_home_wins
   end
 
+  def percentage_visitor_wins
+    @game_manager.percentage_visitor_wins
+  end
+
+  def percentage_ties
+    @game_manager.percent_ties
+  end
+
+  def count_of_games_by_season
+    @game_manager.count_of_games_by_season
+  end
+
+  def average_goals_per_game
+    @game_manager.average_goals_per_game
+  end
+
+  def average_goals_by_season
+    @game_manager.average_goals_by_season
+  end
+  
   # Season methods
   def winningest_coach(season)
     season_games = @season_manager.game_id_by_season(season) #returns array of game ids
@@ -101,7 +121,4 @@ class StatTracker
     @team_manager.team_name(@game_team_manager.worst_average_score_team_home)
   end
 
-  def percentage_ties
-    @game_manager.percent_ties
-  end
 end
