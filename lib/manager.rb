294a2705f419b9c5fd722ac060296403f8b6dd87
Michael Mattson <michael.p.mattson@gmail.com>
2021-08-02 18:44:12 -0600

    Swap to map enumerable

--- lib/manager.rb
+++ lib/manager.rb
@@ -1,11 +1,8 @@
 class Manager
   def load(file_path, object)
-    objects = []
     data = CSV.read(@file_path, headers: true)
-    data.each do |row| #change this to map later
-      new_object = object.new(row)
-      objects.push(new_object)
+    data.map do |row|
+      object.new(row)
     end
-    objects
   end
 end
