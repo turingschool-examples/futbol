2cfbd4938c92a085d1b196fb91411fc97c510f27
Kevin Gloss <kgloss89@gmail.com>
2021-11-01 17:06:23 -0600

    Create outline for stat_tracker_spec

---
+++ spec/stat_tracker_spec.rb
@@ -0,0 +1,14 @@
+require './lib/stat_tracker'
+
+RSpec.describe StatTracker do
+
+  before(:all) do
+    @stat_tracker = StatTracker.from_csv(locations)
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@stat_tracker).to be_an_instance_of(StatTracker)
+    end
+  end
+end
