69177e8913fa118023391527f0487df479e18a55
Eldridge Turambi <eturambi95@gmail.com>
2021-11-01 16:58:55 -0600

    Update data transfer to class

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,4 +1,19 @@
+require 'csv'
+
 class StatTracker
 
-  
+  def initialize
+    # @game_path = locations[:games]
+    # @team_path = locations[:teams]
+    # @game_teams_path = locations[:game_teams]
+  end
+
+  def self.from_csv(files)
+    StatTracker.new(files)
+  end
+
+  def game_data(file_name)
+    rows = CSV.read(file_name, headers: true)
+    require "pry"; binding.pry
+  end
 end
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,12 +1,24 @@
 require './lib/stat_tracker'
 require 'simplecov'
+require 'csv'
 # SimpleCov.start
 
 RSpec.describe StatTracker do
 
   it 'exists' do
+    # locations = {
+    #   games: game_path,
+    #   teams: team_path,
+    #   game_teams: game_teams_path
+    # }
+
     stat_tracker = StatTracker.new
 
     expect(stat_tracker).to be_an_instance_of(StatTracker)
+  end
+
+  it '#game_data' do
+    stat_tracker = StatTracker.new
+    expect(stat_tracker.game_data('./data/games_test.csv')).to be(Array)
   end
 end
