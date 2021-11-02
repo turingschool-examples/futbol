fb3cc60a2695e9dc297e76fcb52e27609590a09c
Josh Walsh <dmaggioarcangeli@yahoo.com>
2021-11-02 15:53:46 -0600

    Create creator file

---
+++ lib/creator.rb
@@ -0,0 +1,19 @@
+class Creator
+  attr_reader :league
+
+  def initialize(league)
+    @league = league
+  end
+
+  def self.create_objects(game_data, team_data, game_team_data)
+    # stat_objects = stat_obj_creator(game_team_data)
+    league = "this is league"
+    self.new(league)
+  end
+
+  def stat_obj_creator(game_team_data)
+    require "pry"; binding.pry
+  end
+
+
+end
