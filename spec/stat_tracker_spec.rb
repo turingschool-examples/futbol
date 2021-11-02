343706fb9984a7e3194d555cbcac526e5784a55d
Kevin Gloss <kgloss89@gmail.com>
2021-11-02 13:13:46 -0600

    Add lowest_total_score method

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -60,4 +60,10 @@ RSpec.describe StatTracker do
       expect(@stat_tracker.highest_total_score).to eq(11)
     end
   end
+
+  describe '#lowest_total_score' do
+    it 'will find the lowest sum of the team scores from all the games' do
+      expect(@stat_tracker.lowest_total_score).to eq(0)
+    end
+  end
 end
