3892737db3daa0da7b056ea9d2870f9d4d788430
Paul Leonard <paulpaulleonard@gmail.com>
2021-11-02 15:51:02 -0400

    Figure out self.from_csv and rewrote tests

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -5,10 +5,10 @@ class StatTracker
               :teams,
               :game_teams
 
-  def initialize(locations)
-    @games      = CSV.read(locations[:games], headers: true)
-    @teams      = CSV.read(locations[:teams], headers: true)
-    @game_teams = CSV.read(locations[:game_teams], headers: true)
+  def initialize(game_path, team_path, game_team_path)
+    @games      = CSV.read(game_path)
+    @teams      = CSV.read(team_path)
+    @game_teams = CSV.read(game_team_path)
   end
 
   def to_array(file_path)
@@ -17,6 +17,15 @@ class StatTracker
     CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
       rows << row.to_h
     end
+    # require "pry"; binding.pry
     rows
+  end
+
+  def self.from_csv(locations)
+    game_path = locations[:games]
+    team_path = locations[:teams]
+    game_team_path = locations[:game_teams]
+    # require "pry"; binding.pry
+    self.new(game_path, team_path, game_team_path)
   end
 end
