703fa7d214f45030699c2e6a94215e7ed3d7b8c3
Kevin Gloss <kgloss89@gmail.com>
2021-11-01 19:59:51 -0600

    Remove conversion to integer for ids

--- lib/team.rb
+++ lib/team.rb
@@ -2,8 +2,8 @@ class Team
   attr_reader :team_id, :franchise_id, :team_name, :abbreviation, :stadium, :link
 
   def initialize(data)
-    @team_id = data["team_id"].to_i
-    @franchise_id = data["franchiseId"].to_i
+    @team_id = data["team_id"]
+    @franchise_id = data["franchiseId"]
     @team_name = data["teamName"]
     @abbreviation = data["abbreviation"]
     @stadium = data["Stadium"]
