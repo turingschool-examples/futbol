0a512e9d882e30b914d48c1250e26952af332d5e
Eric Chon <echon92@gmail.com>
2021-11-01 19:56:20 -0600

    Updated veriables to integers

--- lib/games.rb
+++ lib/games.rb
@@ -1,3 +1,4 @@
+require 'csv'
 class Games
   attr_reader :game_id, :season, :type, :date_time, :away_team_id, :home_team_id, :away_goals, :home_goals, :venue, :venue_link
 
@@ -8,8 +9,8 @@ class Games
     @date_time = data['date_time']
     @away_team_id = data['away_team_id']
     @home_team_id = data['home_team_id']
-    @away_goals = data['away_goals']
-    @home_goals = data['home_goals']
+    @away_goals = data['away_goals'].to_i
+    @home_goals = data['home_goals'].to_i
     @venue = data['venue']
     @venue_link = data['venue_link']
   end
