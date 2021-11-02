d80c7163eb2e8f6c61d1ba94bcdd9b60b71079e8
Leland Curtis <lelandcurtis88@gmail.com>
2021-11-02 15:47:11 -0600

    Update stat_tracker and spec to use Hash data holders, not arrays.

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -7,9 +7,9 @@ class StatTracker
   attr_accessor :games, :teams, :game_teams
 
   def initialize()
-    @games = []
-    @teams = []
-    @game_teams = []
+    @games = Hash.new(0)
+    @teams = Hash.new(0)
+    @game_teams = Hash.new(0)
   end
 
   def self.from_csv(locations)
@@ -17,68 +17,69 @@ class StatTracker
     rows = CSV.read(locations[:teams], headers: true)
     rows.each do |row|
       team = Teams.new(row)
-      stat_tracker.teams << team
+      stat_tracker.teams[team.team_id] = team
     end
 
     rows = CSV.read(locations[:games], headers: true)
     rows.each do |row|
-      games = Games.new(row)
-      stat_tracker.games << games
+      game = Games.new(row)
+      stat_tracker.games[game.game_id] = game
     end
 
     rows = CSV.read(locations[:game_teams], headers: true)
     rows.each do |row|
-      game_teams = GameTeams.new(row)
-      stat_tracker.game_teams << game_teams
+      game_team = GameTeams.new(row)
+      combo_id = game_team.game_id + '_' + game_team.team_id
+      stat_tracker.game_teams[combo_id] = game_team
     end
     return stat_tracker
   end
 
   #Game Statistics Methods
   def highest_total_score
-    game_score = @games.map {|game| game.away_goals + game.home_goals}
+    game_score = @games.values.map {|game| game.away_goals + game.home_goals}
     game_score.max
   end
 
   def lowest_total_score
-    game_score = @games.map {|game| game.away_goals + game.home_goals}
+    game_score = @games.values.map {|game| game.away_goals + game.home_goals}
     game_score.min
   end
 
   def percentage_visitor_wins
     visitor_wins = []
-    @games.each do |game|
+    @games.values.each do |game|
       if game.away_goals > game.home_goals
         visitor_wins.push(game)
       end
     end
-    (visitor_wins.count.to_f / @games.count.to_f).round(2)
+    (visitor_wins.count.to_f / @games.values.count.to_f).round(2)
   end
 
   def percentage_home_wins
     home_wins = []
-    @games.each do |game|
+    @games.values.each do |game|
       if game.home_goals > game.away_goals
         home_wins.push(game)
       end
     end
-    (home_wins.count.to_f / @games.count.to_f).round(2)
+    (home_wins.count.to_f / @games.values.count.to_f).round(2)
   end
 
   def percentage_ties
     tie_games = []
-    @games.each do |game|
+    @games.values.each do |game|
       if game.home_goals == game.away_goals
         tie_games.push(game)
       end
     end
-    (tie_games.count.to_f / @games.count.to_f).round(2)
+    (tie_games.count.to_f / @games.values.count.to_f).round(2)
   end
 
   # A hash with season names (e.g. 20122013) as keys and counts of games as values
   def count_of_games_by_season
     count_of_games_by_season = Hash.new(0)
-    games_by_season = @games.group_by{|game| game.season}
+    games_by_season = @games.values.group_by{|game| game.season}
     games_by_season.keys.each do |season|
       count_of_games_by_season[season] = games_by_season[season].length
     end
@@ -89,7 +90,7 @@ class StatTracker
   # Average number of goals scored in a game across all seasons including
   # both home and away goals (rounded to the nearest 100th) - float
   def avgerage_goals_per_game
-    total_goals = @games.map{|game| game.home_goals + game.away_goals}
+    total_goals = @games.values.map{|game| game.home_goals + game.away_goals}
     avg_goals_per_game = (total_goals.sum.to_f/total_goals.length.to_f).round(2)
   end
 
@@ -98,7 +99,7 @@ class StatTracker
   # representing the average number of goals in a game for that season
   # as values (rounded to the nearest 100th)	- Hash
 
-  # def avgerage_goals_per_season
+  def avgerage_goals_per_season
   #   avg_goals_per_season = Hash.new(0)
   #   games_by_season = @games.group_by do|game|
   #     game.season
@@ -108,9 +109,9 @@ class StatTracker
   #     avg_goals_per_season[season] = (goals_by_season.sum.to_f / goals_by_season.length.to_f).round(2)
   #   end
   #   return avg_goals_per_season
-  # end
-  #
-  # def highest_scoring_visitor
+  end
+
+   def highest_scoring_visitor
 
 
 
