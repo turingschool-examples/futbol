66c6ebef9682335051f16f3739de4e06312cf361
Eric Chon <echon92@gmail.com>
2021-11-01 20:26:03 -0600

    Spelling correction

--- spec/games_spec.rb
+++ spec/games_spec.rb
@@ -11,8 +11,8 @@ RSpec.describe Games do
     @season = '20122013'
     @type = 'Postseason'
     @date_time = '5/16/13'
-    @away_team_id = 3
-    @home_team_id = 6
+    @away_team_id = '3'
+    @home_team_id = '6'
     @away_goals = 2
     @home_goals = 3
     @venue = 'Toyota Stadium'
