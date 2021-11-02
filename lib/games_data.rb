980911dcdbae027a22bbfaca29b805ec2dfaa0bd
David Kassin <david.j.kassin@gmail.com>
2021-11-02 17:05:31 -0600

    feat: Add highest_total_score method

--- lib/games_data.rb
+++ lib/games_data.rb
@@ -6,14 +6,25 @@ class GamesData < StatTracker
 
   def initialize(current_stat_tracker)
     @game_data = current_stat_tracker.games
-    # @game_id = nil
-    # @season = nil
-    # @type = nil
-    # @data_time = nil
-    # away_team_id = nil
-    # home_team_id = nil
-    # away_goals = nil
-    # home_goals = nil
-    # venue_link = nil
+    # @game_id = @game_data["game_id"]
+    # @season = @game_data["season"]
+    # @type = @game_data["type"]
+    # @data_time = @game_data["date_time"]
+    # away_team_id = @game_data["away_team_id"]
+    # home_team_id = @game_data["home_team_id"]
+    # away_goals = @game_data["away_goals"].to_i
+    # home_goals = @game_data["home_goals"].to_i
+    # venue = @game_data["venue"]
+  end
+
+  def highest_total_score
+  highest_score = 0
+    @game_data.each do |row|
+      total_score = row['away_goals'].to_i + row['home_goals'].to_i
+      if total_score > highest_score
+        highest_score = total_score
+      end
+    end
+    highest_score
   end
 end
