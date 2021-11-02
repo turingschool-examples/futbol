fb183e4cce102e9f898348f65c14b73eb0a77491
Kevin Gloss <kgloss89@gmail.com>
2021-11-01 18:46:11 -0600

    Update initialize method

--- lib/team.rb
+++ lib/team.rb
@@ -1,12 +1,12 @@
 class Team
   attr_reader :team_id, :franchise_id, :team_name, :abbreviation, :stadium, :link
 
-  def initialize(team_id, franchise_id, team_name, abbreviation, stadium, link)
-    @team_id = team_id
-    @franchise_id = franchise_id
-    @team_name = team_name
-    @abbreviation = abbreviation
-    @stadium = stadium
-    @link = link
+  def initialize(row)
+    @team_id = row["team_id"]
+    @franchise_id = row["franchiseId"]
+    @team_name = row["teamName"]
+    @abbreviation = row["abbreviation"]
+    @stadium = row["Stadium"]
+    @link = row["link"]
   end
 end
