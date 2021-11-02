d16af014004323d2c41abe3a4374fd935fb39e36
David Kassin <david.j.kassin@gmail.com>
2021-11-02 14:20:41 -0600

    test: Fix StatTracker exists test

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -5,7 +5,6 @@ require 'csv'
 
 RSpec.describe StatTracker do
   before(:each) do
-    @new_instance = StatTracker.new
     @game_path = './data/games_test.csv'
     @team_path = './data/teams_test.csv'
     @game_teams_path = './data/game_teams_test.csv'
@@ -24,27 +23,8 @@ RSpec.describe StatTracker do
     #   game_teams: game_teams_path
     # }
 
-    stat_tracker = StatTracker.new
+    stat_tracker = StatTracker.from_csv(@locations)
 
     expect(stat_tracker).to be_an_instance_of(StatTracker)
-  end
-
-
-  it '#from_csv returns hash with all data' do
-
-
-    expect(StatTracker.from_csv(@locations)).to be_a (Hash)
-
-  end
-
-  it '#game_data' do
-    stat_tracker = StatTracker.from_csv(@locations)
-    games_data = StatTracker.get_data(:games)
-    teams_data = StatTracker.get_data(:teams)
-
-
-    expect(stat_tracker).to be_a (Hash)
-    expect(games_data).to be_a (Array)
-    expect(teams_data).to be_a (Array)
   end
 end
