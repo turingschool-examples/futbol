29fd675d1891ca09fb12b64fbea98e20430ad1c5
Kevin Gloss <kgloss89@gmail.com>
2021-11-01 19:48:33 -0600

    Update id variables to integers

--- lib/team.rb
+++ lib/team.rb
@@ -1,12 +1,12 @@
 class Team
   attr_reader :team_id, :franchise_id, :team_name, :abbreviation, :stadium, :link
 
-  def initialize(row)
-    @team_id = row["team_id"]
-    @franchise_id = row["franchiseId"]
-    @team_name = row["teamName"]
-    @abbreviation = row["abbreviation"]
-    @stadium = row["Stadium"]
-    @link = row["link"]
+  def initialize(data)
+    @team_id = data["team_id"].to_i
+    @franchise_id = data["franchiseId"].to_i
+    @team_name = data["teamName"]
+    @abbreviation = data["abbreviation"]
+    @stadium = data["Stadium"]
+    @link = data["link"]
   end
 end
