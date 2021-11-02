71b2e97396305be5226571c0152bfefcaea9209b
Seth Perna <gseth26@gmail.com>
2021-11-02 17:20:13 -0400

    method for highest total score

--- lib/game_stats.rb
+++ lib/game_stats.rb
@@ -1,8 +1,24 @@
 require 'csv'
 require 'simplecov'
-require './stat_tracker'
+require './lib/stat_tracker'
 
 SimpleCov.start
 
-class Game_stats
+class GameStats
+  attr_reader :game_data
+  def initialize(game_data)
+    #@game_data = CSV.read("./data/sample_games.csv")
+  end
+
+  def highest_total_score
+    @game_data = CSV.parse(File.read("./data/sample_games.csv"), headers: true)
+    max_score = 0
+    @game_data.each do |game|
+      sum = game["away_goals"].to_i + game["home_goals"].to_i
+      if sum > max_score
+        max_score = sum
+      end
+    end
+    max_score
+  end
 end
--- spec/league_stats_spec.rb
+++ spec/league_stats_spec.rb
@@ -2,10 +2,10 @@ require 'csv'
 require 'simplecov'
 require './lib/league_stats'
 
-RSpec.describe League_stats do
+RSpec.describe LeagueStats do
   it 'exists' do
-    league_stats = League_stats.new
+    league_stats = LeagueStats.new
 
-    expect(league_stats).to be_an_instance_of(League_stats)
+    expect(league_stats).to be_an_instance_of(LeagueStats)
   end
 end
