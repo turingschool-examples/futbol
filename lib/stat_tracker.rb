ef44e441048692050f2d45cede7f503072f10914
Joel Grant <joelmgrant@me.com>
2021-11-02 13:48:04 -0600

    Feat: Add methods to read each file

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,3 +1,7 @@
+require './lib/game_team'
+require './lib/team'
+require './lib/game'
+
 class StatTracker
   attr_accessor :location
   def initialize(location)
@@ -6,8 +10,29 @@ class StatTracker
 
   def self.from_csv(locations)
     locations.map do |location|
-      require 'pry'; binding.pry
+      # require 'pry'; binding.pry
       StatTracker.new(location)
     end
   end
+
+  def read_game_stats(file)
+    CSV.foreach(file, headers: true, header_converters: :symbol) do |row|
+      game = Game.new(row)
+    end
+  end
+
+  def read_team_stats(file)
+    CSV.foreach(file, headers: true, header_converters: :symbol) do |row|
+      team = Team.new(row)
+    end
+  end
+
+  def read_game_teams_stats(file)
+    CSV.foreach(file, headers: true, header_converters: :symbol) do |row|
+      game_team = GameTeam.new(row)
+      require 'pry'; binding.pry
+    end
+  end
+
+
 end
