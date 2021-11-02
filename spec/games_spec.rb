d07400853142d8b9fcdd0304fa317a23868922c4
Eric Chon <echon92@gmail.com>
2021-11-01 20:11:00 -0600

    completed spec file for games

---
+++ spec/games_spec.rb
@@ -0,0 +1,42 @@
+require './lib/games.rb'
+require 'csv'
+
+RSpec.describe Games do
+
+  before :each do
+    @rows = CSV.read('./data/games.csv', headers: true)
+    @row = @rows[0]
+
+    @game_id = '2012030221'
+    @season = '20122013'
+    @type = 'Postseason'
+    @date_time = '5/16/13'
+    @away_team_id = 3
+    @home_team_id = 6
+    @away_goals = 2
+    @home_goals = 3
+    @venue = 'Toyota Stadium'
+    @venue_link = '/api/v1/venues/null'
+
+    @game1 = Games.new(@row)
+  end
+
+  describe 'initialize' do
+    it "exists" do
+      expect(@game1).to be_a(Games)
+    end
+
+    it "checks that all the attributes" do
+      expect(@game1.game_id).to eq('2012030221')
+      expect(@game1.season).to eq('20122013')
+      expect(@game1.type).to eq('Postseason')
+      expect(@game1.date_time).to eq('5/16/13')
+      expect(@game1.away_team_id).to eq('3')
+      expect(@game1.home_team_id).to eq('6')
+      expect(@game1.away_goals).to eq(2)
+      expect(@game1.home_goals).to eq(3)
+      expect(@game1.venue).to eq('Toyota Stadium')
+      expect(@game1.venue_link).to eq('/api/v1/venues/null')
+    end
+  end
+end
