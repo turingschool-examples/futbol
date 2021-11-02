7800df58f962f23e500c27a6e611347649714488
Robin Dittrich <robinodittrich@gmail.com>
2021-11-01 20:13:02 -0700

    Prepare test files.

--- spec/team_stats_spec.rb
+++ spec/team_stats_spec.rb
@@ -1,3 +1,11 @@
 require 'csv'
 require 'simplecov'
 require './lib/team_stats'
+
+RSpec.describe Team_stats do
+  it 'exists' do
+    team_stats = Team_stats.new
+
+    expect(team_stats).to be_an_instance_of(Team_stats)
+  end
+end
