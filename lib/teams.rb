f97b7cce5a74b15cc276bb85f5ad6fc8cc15cc76
Kevin Gloss <kgloss89@gmail.com>
2021-11-01 20:07:16 -0600

    Update file name to match class

---
+++ lib/teams.rb
@@ -0,0 +1,12 @@
+class Teams
+  attr_reader :team_id, :franchise_id, :team_name, :abbreviation, :stadium, :link
+
+  def initialize(data)
+    @team_id = data["team_id"]
+    @franchise_id = data["franchiseId"]
+    @team_name = data["teamName"]
+    @abbreviation = data["abbreviation"]
+    @stadium = data["Stadium"]
+    @link = data["link"]
+  end
+end
