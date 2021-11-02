8d40da4a2ff1b2a12215c0e6c3cc9b893dd4ad37
Hannah Warren <hannahkwarren@gmail.com>
2021-11-02 18:29:24 -0400

    Add count_of_games_by_season

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -5,11 +5,10 @@ require './lib/stat_tracker'
 
    before(:each) do
    @game_path = './data/games_test.csv'
-
    @team_path = './data/teams.csv'
    @game_teams_path = './data/game_teams.csv'
 
-   @locations = {
+   @filenames = {
      games: @game_path,
      teams: @team_path,
      game_teams: @game_teams_path
@@ -19,7 +18,7 @@ require './lib/stat_tracker'
 
    it 'exists' do
      expect(@stat_tracker).to be_an_instance_of(StatTracker)
-     expect(StatTracker.from_csv(@locations)).to be_an_instance_of(StatTracker)
+     expect(StatTracker.from_csv(@filenames)).to be_an_instance_of(StatTracker)
    end
 
    it 'can access csv data' do
@@ -28,9 +27,11 @@ require './lib/stat_tracker'
    end
 
    it 'can find the highest total score' do
-     expect(@stat_tracker.highest_total_score).to eq(5)
+     expect(@stat_tracker.highest_total_score).to eq(8)
    end
 
- 
+   it 'counts games by season' do
+     expect(@stat_tracker.count_of_games_by_season).to eq({"20122013"=> 9, "20162017" => 1, "20152016" => 2, "20172018" => 1})
+   end
 
  end
