e25f5d980df0b235916a0c3188b2f6356d31f8ca
Joel Grant <joelmgrant@me.com>
2021-11-02 17:25:51 -0600

    Update: Setup StatTracker dataflow
    Co-authored-by: Brad Breiten <jonathan@breiten.us>
    Co-authored-by: Parker Lockhart <parkerlockhart42@gmail.com>
    Co-authored-by: Jessica Grazulis <jlgrazulis@gmail.com>

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -6,8 +6,11 @@ class StatTracker
   attr_accessor :location
   def initialize(location)
     @location = location
+    @games = {}
+    @teams = {}
+    @game_teams = {}
   end
- 
+
   def self.from_csv(locations)
     locations.map do |location|
       StatTracker.new(location)
@@ -17,6 +20,7 @@ class StatTracker
   def read_game_stats(file)
     CSV.foreach(file, headers: true, header_converters: :symbol) do |row|
       game = Game.new(row)
+      @games[game.game_id] = game
     end
   end
 
@@ -30,6 +34,17 @@ class StatTracker
     CSV.foreach(file, headers: true, header_converters: :symbol) do |row|
       game_team = GameTeam.new(row)
     end
+  end
+
+  def highest_total_score
+    high_score = 0
+    @games.each_value do |game|
+      high = game.away_goals + game.home_goals
+      if (game.away_goals + game.home_goals) > high_score
+        high_score = game.away_goals + game.home_goals
+      end
+    end
+    high_score
   end
 
 
