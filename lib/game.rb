e849f10fdba599d5aabe0db188b265058ffad085
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-11-03 12:03:20 -0600

    Start Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,17 @@
 class Game
-  
+
+  attr_reader :game_id,
+              :season,
+              :type,
+              :date_time,
+              :away_team_id,
+              :home_team_id,
+              :away_goals,
+              :home_goals,
+              :venue,
+              :venue_link
+
+  def initialize(arg1, arg2, arg3)
+  end
+
 end
