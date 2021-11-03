a72727115434392d8bc21983268219025789fb61
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-11-03 17:06:03 -0600

    Start self.team_obj_creator

--- lib/creator.rb
+++ lib/creator.rb
@@ -1,5 +1,6 @@
 require './tg_stat'
 require './game'
+require './team'
 
 class Creator
   attr_reader :league
@@ -11,7 +12,7 @@ class Creator
   def self.create_objects(game_data, team_data, game_team_data)
     stats_hash = self.stat_obj_creator(game_team_data)
     games_hash = self.game_obj_creator(game_data, stats_hash)
-
+    @seasons_hash = self.season_obj_creator(games_hash)
 
     league = "this is league"
     self.new(league)
@@ -42,11 +43,27 @@ class Creator
       game.season
     end
   end
+  # def self.postseason_obj_creator
+  #   season_type_hash = games_hash.values.group_by do |game|
+  #     game.type
+  #   end
+  # end
 
-  def team_obj_creator
+  def self.team_obj_creator(team_data, games_hash)
+    teams_hash = {}
+    team_data.each do |team|
+      # stuff wrong here
+      team_games = team_games.group_by { |game| game.game_id }
+      teams_hash[team[:team_id]] = Team.new(team, team_games)
+    end
+    teams_hash
   end
+  # team_games = games_hash.values.find_all do |game|
+  #   require "pry"; binding.pry
+  #   (game.home_team_id == team[:team_id]) || (game.away_team_id == team[:team_id])
+  # end
 
-  def league_obj_creator
+  def self.league_obj_creator
   end
 
 end
