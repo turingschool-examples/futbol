a333d6fae17ea032dc484e9187750ed3b9d77d2b
Katy <m.catherine.harrod@gmail.com>
2021-11-03 15:36:09 -0600

    Added code to game class and spec

--- lib/game.rb
+++ lib/game.rb
@@ -3,17 +3,25 @@ require './lib/stat_tracker'
 
 class Game
   def initialize(game_path)
-    @game_id    = @game_path[:game_id]
-    @season     = @game_path[:season]
-    @game_path  = game_path
+    # @game_id    = @game_path[:game_id]
+    # require "pry"; binding.pry
+    # @season     = @game_path[:season]
+    # @game_path  = game_path
   end
 
-  def to_array
-    rows = []
+  # def to_cells
+  # end
 
-    CSV.foreach(@game_path, headers: true, header_converters: :symbol) do |row|
-      rows << row.to_h
-    end
-    rows
-    require "pry"; binding.pry
-  end
+    # for each |row|
+    # something = Game.new
+
+  # def to_array
+  #   rows = []
+  #
+  #   CSV.foreach(@game_path, headers: true, header_converters: :symbol) do |row|
+  #     rows << row.to_h
+  #   end
+  #   rows
+  #   require "pry"; binding.pry
+  # end
+end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,2 +1,29 @@
-require './lib/stat_tracker'
+require 'csv'
 require './lib/game.rb'
+
+RSpec.describe Game do
+
+before(:each) do
+  @game_path       = './data/games_sample.csv'
+
+end
+
+
+  def to_array
+    rows = []
+
+    CSV.foreach(@game_path, headers: true, header_converters: :symbol) do |row|
+      rows << row.to_h
+    end
+    game_board = rows.map do |row|
+      Game.new(row)
+      require "pry"; binding.pry
+    end
+  end
+
+  it 'exists' do
+    game = Game.new(@game_path)
+    game.to_array
+    expect()
+  end
+end
