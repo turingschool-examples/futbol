8e8e6d6fd7e1125f2fb28c7b45f1d44a48dfa66f
Robin Dittrich <robinodittrich@gmail.com>
2021-11-02 21:01:49 -0700

    Refactor count_of_teams_class.

--- spec/league_stats_spec.rb
+++ spec/league_stats_spec.rb
@@ -2,10 +2,22 @@ require 'csv'
 require 'simplecov'
 require './lib/league_stats'
 
-RSpec.describe League_stats do
-  it 'exists' do
-    league_stats = League_stats.new
+SimpleCov.start
 
-    expect(league_stats).to be_an_instance_of(League_stats)
+RSpec.describe LeagueStats do
+  before :each do
+    @league_path = './data/sample_game_teams.csv'
+
+    @league_stats = LeagueStats.new(@league_path)
+  end
+
+  it 'exists' do
+
+    expect(@league_stats).to be_an_instance_of(LeagueStats)
+  end
+
+  it '#count_of_teams' do
+
+    expect(@league_stats.count_of_teams).to eq(5)
   end
 end
