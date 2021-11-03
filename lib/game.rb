e1607bf93b8c65345089ea46ffa4931a4e32a2fc
Sam Iavri <sammi80134@gmail.com>
2021-11-03 11:32:11 -0600

    Turn ID into integers

--- lib/game.rb
+++ lib/game.rb
@@ -7,12 +7,12 @@ class Game
     # and call the keys as strings on the row hash argument
     # determine what TYPE each instance variable is. ids are probably INT
     # headers are values and rows hold the keys
-    @game_id = row['game_id']
-    @season = row['season']
+    @game_id = row['game_id'].to_i
+    @season = row['season'].to_i
     @type = row['type']
     @date_time = row['date_time']
-    @away_team_id = row['away_team_id']
-    @home_team_id = row['home_team_id']
+    @away_team_id = row['away_team_id'].to_i
+    @home_team_id = row['home_team_id'].to_i
     @away_goals = row['away_goals'].to_i
     @home_goals = row['home_goals'].to_i
     @venue = row['venue']
