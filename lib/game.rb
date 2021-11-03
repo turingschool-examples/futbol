51f7c6c6bd19be6aabc56d043b47aee49bd4ad14
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-11-03 15:32:58 -0600

    Finish Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,4 @@
+require './lib/stat_tracker'
 class Game
 
   attr_reader :game_id,
@@ -9,9 +10,23 @@ class Game
               :away_goals,
               :home_goals,
               :venue,
-              :venue_link
+              :venue_link,
+              :home_team_stat,
+              :away_team_stat
 
-  def initialize(arg1, arg2, arg3)
+  def initialize(game, home_team_stat, away_team_stat)
+    @game_id = game[:game_id]
+    @season = game[:season]
+    @type = game[:type]
+    @date_time = game[:date_time]
+    @away_team_id = game[:away_team_id]
+    @home_team_id = game[:home_team_id]
+    @away_goals = game[:away_goals].to_i
+    @home_goals = game[:home_goals].to_i
+    @venue = game[:venue]
+    @venue_link = game[:venue_link]
+    @home_team_stat = home_team_stat
+    @away_team_stat = away_team_stat
   end
 
 end
