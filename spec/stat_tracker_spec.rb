d70bd2317e267a5a07549578229741164db230b6
Brad Breiten <jonathan@breiten.us>
2021-11-03 15:22:43 -0600

    Add percentage_home_wins method to StatTracker Gamestats

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,12 +1,80 @@
-require_relative './lib/stat_tracker'
 require 'csv'
-require_relative './lib/game_team'
-require_relative './lib/game'
-require_relative './lib/team'
-require_relative './runner'
+require './lib/stat_tracker'
+require './lib/game_team'
+require './lib/game'
+require './lib/team'
+require './runner'
 
 
 # Game Statistics Tests
+
+RSpec.describe StatTracker do
+  before(:each) do
+    @game_path = './data/games.csv'
+    @team_path = './data/teams.csv'
+    @game_teams_path = './data/game_teams.csv'
+
+    @locations = {
+      games: @game_path,
+      teams: @team_path,
+      game_teams: @game_teams_path
+    }
+    @stat_tracker = StatTracker.from_csv(@locations)
+    @stat_tracker[0].read_game_stats(@game_path)
+    @stat_tracker[0].read_team_stats(@team_path)
+    @stat_tracker[0].read_game_teams_stats(@game_teams_path)
+  end
+
+  xdescribe '#initialize' do
+    it 'is an instance of StatTracker' do
+      expect(@stat_tracker).to be_an_instance_of Array
+    end
+
+    it 'can read games.csv' do
+      expect(@stat_tracker[0].games).to be_an_instance_of(Hash)
+    end
+
+    it 'can read teams.csv' do
+      expect(@stat_tracker[0].teams).to be_an_instance_of(Hash)
+    end
+
+    it 'can read game_teams.csv' do
+      expect(@stat_tracker[0].game_teams).to be_an_instance_of(Hash)
+    end
+  end
+
+  xdescribe '#highest_total_score' do
+    it 'returns an integer' do
+      expect(@stat_tracker[0].highest_total_score).to be_an_instance_of Integer
+    end
+
+    it 'returns highest total of winning and losing team score by game' do
+      expect(@stat_tracker[0].highest_total_score).to eq 11
+    end
+  end
+
+  xdescribe '#lowest_total_score' do
+    it 'returns an integer' do
+      expect(@stat_tracker[0].lowest_total_score).to be_an_instance_of Integer
+    end
+
+    it 'returns lowest total of winning and losing team score by game' do
+      expect(@stat_tracker[0].lowest_total_score).to eq 0
+    end
+  end
+
+  describe '#percentage_home_wins' do
+    it 'returns a float' do
+      expect(@stat_tracker[0].percentage_home_wins).to be_an_instance_of Float
+    end
+
+    it 'percentage of games won by the home team' do
+      expect(@stat_tracker[0].percentage_home_wins).to eq 0
+    end
+  end
+end
+
+
 
 
 # League Statistics Tests
