faf48b4eb2c349fe8573260f9b6ff7401284129c
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-11-03 17:06:30 -0600

    Start and complete Team class

--- lib/team.rb
+++ lib/team.rb
@@ -1,3 +1,21 @@
 class Team
-  
+
+  attr_reader :team_id,
+              :franchise_id,
+              :team_name,
+              :abbreviation,
+              :stadium,
+              :link,
+              :game_objects
+
+  def initialize(team_data, team_games)
+    @team_id = team_data[:team_id]
+    @franchise_id = team_data[:franchiseid]
+    @team_name = team_data[:teamname]
+    @abbreviation = team_data[:abbreviation]
+    @stadium = team_data[:stadium]
+    @link = team_data[:link]
+    @game_objects = team_games
+  end
+
 end
