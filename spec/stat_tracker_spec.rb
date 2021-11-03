b0b37e1713a1ebdb0d6f9ad247c46b1db48b3322
Sam Iavri <sammi80134@gmail.com>
2021-11-03 07:04:40 -0600

    Rewrite Game class into a hash to hold in an array.

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,36 +1,36 @@
 require './lib/stat_tracker'
 
+RSpec.describe StatTracker do
+  before(:each) do
+    @game_path = './data/games_test.csv'
+    @team_path = './data/teams.csv'
+    @game_teams_path = './data/game_teams.csv'
 
- RSpec.describe StatTracker do
+    @filenames = {
+      games: @game_path,
+      teams: @team_path,
+      game_teams: @game_teams_path
+    }
+    @stat_tracker = StatTracker.from_csv(@filenames)
+  end
 
-   before(:each) do
-   @game_path = './data/games_test.csv'
+  it 'exists' do
+    expect(@stat_tracker).to be_an_instance_of(StatTracker)
+    expect(StatTracker.from_csv(@filenames)).to be_an_instance_of(StatTracker)
+  end
 
-   @team_path = './data/teams.csv'
-   @game_teams_path = './data/game_teams.csv'
+  it 'can access csv data' do
+    expect(StatTracker.from_csv(@filenames)).to be_an_instance_of(StatTracker)
+  end
 
-   @locations = {
-     games: @game_path,
-     teams: @team_path,
-     game_teams: @game_teams_path
-   }
-   @stat_tracker = StatTracker.from_csv(@filenames)
-   end
+  it 'can find the highest total score' do
+    expect(@stat_tracker.highest_total_score).to eq(5)
+  end
 
-   it 'exists' do
-     expect(@stat_tracker).to be_an_instance_of(StatTracker)
-     expect(StatTracker.from_csv(@locations)).to be_an_instance_of(StatTracker)
-   end
-
-   it 'can access csv data' do
-
-     expect(StatTracker.from_csv(@filenames)).to be_an_instance_of(StatTracker)
-   end
-
-   it 'can find the highest total score' do
-     expect(@stat_tracker.highest_total_score).to eq(5)
-   end
-
- 
-
- end
+  it 'can find the lowest total score' do
+    expected = 7
+    mock_games = OpenStruct.new(lowest_total_score: expected)
+    actual = StatTracker.new(games: mock_games).lowest_total_score
+    expect(actual).to eq(expected)
+  end
+end
