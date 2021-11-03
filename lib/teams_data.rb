8e5958dc0991ec16da3ae95ec57acb6c58849907
Ruben Pasillas <hobbbes@protonmail.com>
2021-11-03 14:46:06 -0600

    Create method skeletons

--- lib/teams_data.rb
+++ lib/teams_data.rb
@@ -5,43 +5,44 @@ class TeamsData < StatTracker
   def initialize(current_stat_tracker)
     @teamData = current_stat_tracker.teams
     #
-    # @team_id = @teams["team_id"]
-    # @franchise_id = @teams["franchise_id"]
-    # @team_name = @teams["teamName"]
-    # @abbreviation = @teams["abbreviation"]
-    # @stadium = @teams["Stadium"]
-    # @link = @teams["link"]
-  end
-
-  def team_info
 
   end
 
-  def best_season
+  def team_info(team_id)
+    @team_id = @teams["team_id"]
+    @franchise_id = @teams["franchise_id"]
+    @team_name = @teams["teamName"]
+    @abbreviation = @teams["abbreviation"]
+    @link = @teams["link"]
+
 
   end
 
-  def worst_season
+  def best_season(team_id)
 
   end
 
-  def average_win_percentage
+  def worst_season(team_id)
 
   end
 
-  def most_goals_scored
+  def average_win_percentage(team_id)
 
   end
 
-  def fewest_goals_scored
+  def most_goals_scored(team_id)
 
   end
 
-  def favorite_opponent
+  def fewest_goals_scored(team_id)
 
   end
 
-  def rival
+  def favorite_opponent(team_id)
+
+  end
+
+  def rival(team_id)
 
   end
 end
