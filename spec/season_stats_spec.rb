9b52eef4b38fa3d622140b213e18fabf96cf9a88
Robin Dittrich <robinodittrich@gmail.com>
2021-11-03 15:39:45 -0700

    Create winningest_coach test.

--- spec/season_stats_spec.rb
+++ spec/season_stats_spec.rb
@@ -15,4 +15,9 @@ RSpec.describe SeasonStats do
 
     expect(@season_stats).to be_an_instance_of(SeasonStats)
   end
+
+  it 'winningest_coach' do
+    
+    expect(@season_stats.winningest_coach).to eq("Mike Babcock")
+  end
 end
