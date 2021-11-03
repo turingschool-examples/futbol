c30328bb41b9a60069763440acb1f0be5503f05c
Jackson Valdez <jacksonvaldez64@gmail.com>
2021-11-03 12:02:33 -0600

    Complete game_obj_creator method

--- lib/creator.rb
+++ lib/creator.rb
@@ -1,4 +1,6 @@
 require './tg_stat'
+require './game'
+
 class Creator
   attr_reader :league
 
@@ -7,12 +9,15 @@ class Creator
   end
 
   def self.create_objects(game_data, team_data, game_team_data)
-    # stat_objects = stat_obj_creator(game_team_data)
+    stats_hash = self.stat_obj_creator(game_team_data)
+    games_hash = self.game_obj_creator(game_data, stats_hash)
+
+
     league = "this is league"
     self.new(league)
   end
 
-  def stat_obj_creator(game_team_data)
+  def self.stat_obj_creator(game_team_data)
     stats_hash = {}
     game_team_data.each do |stat|
       stat_name = "#{stat[:game_id] + '_' + stat[:team_id]}"
@@ -20,4 +25,24 @@ class Creator
     end
     stats_hash
   end
+
+  def self.game_obj_creator(game_data, stats_hash)
+    games_hash = {}
+    game_data.each do |game|
+      home_team = "#{game[:game_id] + '_' + game[:home_team_id]}".to_sym
+      away_team = "#{game[:game_id] + '_' + game[:away_team_id]}".to_sym
+      games_hash[game[:game_id]] = Game.new(game, stats_hash[home_team], stats_hash[away_team])
+    end
+    games_hash
+  end
+
+  def season_obj_creator
+  end
+
+  def team_obj_creator
+  end
+
+  def league_obj_creator
+  end
+
 end
