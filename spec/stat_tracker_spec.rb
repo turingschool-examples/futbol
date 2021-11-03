0549f271db21fed9064c37530382399fc71c961a
Jacob Jenner <jennerjacob@gmail.com>
2021-11-02 19:28:51 -0600

    Feat: Modify open files test and create highest total score method

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,15 +1,27 @@
 require '/spec_helper.rb'
 require './lib/stat_tracker.rb'
+require './runner.rb'
 
 RSpec.describe StatTracker do
 
-  let(:stat_tracker) {StatTracker.new}
+  games = './data/games_dummy.csv'
+  teams = './data/teams_dummy.csv'
+  game_results = './data/game_results_dummy.csv'
+  let(:stat_tracker) {StatTracker.new(games, teams, game_results)}
+  let(:games) {CSV.parse(File.read(game_path), headers: true)}
+  let(:teams) {CSV.parse(File.read(team_path), headers: true)}
+  let(:game_results) {CSV.parse(File.read(game_results), headers: true)}
+
 
   it 'exists' do
     expect(stat_tracker).to be_instance_of(StatTracker)
   end
 
   it 'opens files' do
-    expect(from_csv(locations)).to be_a(StatTracker)
+    expect(stat_tracker.games).to eq('./data/games_dummy.csv')
+  end
+
+  it 'it can display the highest total score' do
+    expect(highest_total_score).to eq(7)
   end
 end
