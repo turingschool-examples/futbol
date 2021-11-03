1c964d27285c5d20c482950a087d73470acfcb4c
Robin Dittrich <robinodittrich@gmail.com>
2021-11-02 20:59:01 -0700

    Create count_of_teams method

--- lib/league_stats.rb
+++ lib/league_stats.rb
@@ -3,5 +3,21 @@ require 'simplecov'
 
 SimpleCov.start
 
-class League_stats
+class LeagueStats
+  attr_reader :league_data
+  def initialize(league_data)
+    #@league_data = CSV.read("./data/sample_game_teams.csv")
+    @league_data = CSV.parse(File.read("./data/sample_game_teams.csv"), headers: true)
+  end
+
+  def count_of_teams
+    team_ids = []
+    @league_data.each do |league|
+      if team_ids.include?(league["team_id"])
+      else
+        team_ids << league["team_id"]
+      end
+    end
+    p team_ids.count
+  end
 end
