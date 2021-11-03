446e045b5a4202b0fb8832ae6b48a2f19e9ac6ee
Robin Dittrich <robinodittrich@gmail.com>
2021-11-02 21:00:40 -0700

    Create worst_offense method.

--- lib/league_stats.rb
+++ lib/league_stats.rb
@@ -47,4 +47,31 @@ class LeagueStats
     end
     best_offense_team
   end
+
+  def worst_offense
+    teams_games_goals_avg = {}
+    @league_data.each do |league|
+        if league["HoA"] == "home"
+          if teams_games_goals_avg.keys.include? (league["team_id"])
+          else
+            teams_games_goals_avg[league["team_id"]] = [0,0]
+          end
+
+          current_goals = league["goals"].to_i
+          first = (teams_games_goals_avg[league["team_id"]][0] += 1)
+          second = (teams_games_goals_avg[league["team_id"]][1] += current_goals)
+          teams_games_goals_avg[league["team_id"]] = [first, second]
+      end
+    end
+    worst_offense_team_average = 100
+    worst_offense_team = nil
+    teams_games_goals_avg.each do |key, value|
+      value[2] = (value[1].to_f / value[0].to_f)
+      if value[2] < worst_offense_team_average
+        worst_offense_team_average = value[2]
+        worst_offense_team = key
+      end
+    end
+    worst_offense_team
+  end
 end
