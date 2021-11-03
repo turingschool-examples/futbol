1ec061652820fb1ad494226931b3ce323c4140bb
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2021-11-03 16:11:44 -0600

    Test: add tests for intializing Games class with attributes

---
+++ spec/games_spec.rb
@@ -0,0 +1,47 @@
+require 'CSV'
+require './lib/games'
+
+RSpec.describe Games do
+  it 'exists' do
+    game = Games.new({
+          :game_id       => '2012030221',
+          :season        => '20122013',
+          :type          =>  'Postseason',
+          :date_time     =>  '5/16/13',
+          :away_team_id  =>  '3',
+          :home_team_id  =>  '6',
+          :away_goals    =>  '2',
+          :home_goals    =>  '3',
+          :venue         =>  "Toyota Stadium",
+          :venue_link    =>  "/api/v1/venues/null"
+      })
+
+    expect(game).to be_an_instance_of(Games)
+  end
+
+  it 'has attributes' do
+    game = Games.new({
+          :game_id       => '2012030221',
+          :season        => '20122013',
+          :type          =>  'Postseason',
+          :date_time     =>  '5/16/13',
+          :away_team_id  =>  '3',
+          :home_team_id  =>  '6',
+          :away_goals    =>  '2',
+          :home_goals    =>  '3',
+          :venue         =>  "Toyota Stadium",
+          :venue_link    =>  "/api/v1/venues/null"
+      })
+
+    expect(game.game_id).to eq(2012030221)
+    expect(game.season).to eq(20122013)
+    expect(game.date_time).to eq('5/16/13')
+    expect(game.type).to eq('Postseason')
+    expect(game.away_team_id).to eq(3)
+    expect(game.home_team_id).to eq(6)
+    expect(game.away_goals).to eq(2)
+    expect(game.home_goals).to eq(3)
+    expect(game.venue).to eq("Toyota Stadium")
+    expect(game.venue_link).to eq("/api/v1/venues/null")
+  end
+end
