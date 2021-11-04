653a27d1eb389f06f06c7ee888769070cb11e2b7
Chris Hewitt <agop5134@gmail.com>
2021-11-04 13:26:23 -0600

    Clean up stattracker spec

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -3,36 +3,27 @@ require './league_stats'
 
 SimpleCov.start
 SimpleCov.command_name 'Unit Tests'
-require_relative './modules/league_stats'
+require_relative '../modules/league_stats'
 
 
 RSpec.describe StatTracker do
-  let(:stattracker) { StatTracker.new { include LeagueStats } }
+  game_path = './data/games.csv'
+  team_path = './data/teams.csv'
+  game_teams_path = './data/game_teams.csv'
+  locations = {
+    games: game_path,
+    teams: team_path,
+    game_teams: game_teams_path
+  }
+  let(:stattracker) { StatTracker.from_csv(locations) }
 
   it "attributes" do
-    game_path = './data/games.csv'
-    team_path = './data/teams.csv'
-    game_teams_path = './data/game_teams.csv'
-    locations = {
-      games: game_path,
-      teams: team_path,
-      game_teams: game_teams_path
-    }
-    stattracker = StatTracker.from_csv(locations)
     expect(stattracker.game_data).to eq(CSV.read(locations[:games], headers: true, header_converters: :symbol))
     expect(stattracker.team_data).to eq(CSV.read(locations[:teams], headers: true, header_converters: :symbol))
     expect(stattracker.game_team_data).to eq(CSV.read(locations[:game_teams], headers: true, header_converters: :symbol))
   end
 
   it "self from csv and initialize" do
-    game_path = './data/games.csv'
-    team_path = './data/teams.csv'
-    game_teams_path = './data/game_teams.csv'
-    locations = {
-      games: game_path,
-      teams: team_path,
-      game_teams: game_teams_path
-    }
     expect(StatTracker.from_csv(locations)).to be_a(StatTracker)
   end
 
