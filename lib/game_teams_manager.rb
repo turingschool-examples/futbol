87e12e6fd2a1533224a9d5c74c1088636d654c1f
Wade Naughton <wadenaughton@gmail.com>
2021-11-03 21:14:09 -0400

    add game_teams_manager and game_teams

---
+++ lib/game_teams_manager.rb
@@ -0,0 +1,22 @@
+require 'CSV'
+require 'pry'
+
+require_relative './teams'
+
+class GameTeamManager
+  attr_reader :game_team_objects, :game_teams_path
+
+  def initialize(game_teams_path)
+    @game_teams_path = './data/game_teams.csv'
+    @game_team_objects = (
+      objects = []
+      CSV.foreach(game_teams_path, headers: true, header_converters: :symbol) do |row|
+        objects << GameTeams.new(row)
+        
+      end
+      objects)
+
+  end
+
+
+end
