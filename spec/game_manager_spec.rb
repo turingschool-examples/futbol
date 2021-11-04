f859441444775624e1f077d9e5485dfe2cc90f51
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-11-03 20:41:51 -0400

    Edit to match changes to GameManager class

--- spec/game_manager_spec.rb
+++ spec/game_manager_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe GameManager do
     expect(game_manager).to be_an_instance_of(GameManager)
   end
 
-  it 'starts with no game objects' do
+  xit 'starts with no game objects' do
     game_path = './data/games.csv'
     game_manager = GameManager.new(game_path)
 
@@ -21,8 +21,9 @@ RSpec.describe GameManager do
     game_path = './data/games.csv'
     game_manager = GameManager.new(game_path)
 
-    expect(game_manager.create_game_manager).to be_an(Array)
+    expect(game_manager.game_objects[0]).to be_an(Games)
     # expect(game_manager.add_objects).to be_an_instance_of(GameManager)
-    expect(game_manager.create_game_manager.count).to eq(7441)
+    expect(game_manager.game_objects.count).to eq(7441)
+    expect(game_manager.highest_total_score).to eq(0)
   end
 end
