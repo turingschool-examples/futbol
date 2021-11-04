4b665f6a8d75bfcff1a8dd014fbb0b8b03cfff1d
Russell Rockwood <russell.rockwood@gmail.com>
2021-11-04 18:48:13 -0500

    add most/least scored goals tests

--- spec/teams_data_spec.rb
+++ spec/teams_data_spec.rb
@@ -3,9 +3,9 @@ require './lib/teams_data'
 
 RSpec.describe TeamsData do
   before(:each) do
-    @game_path = './data/games.csv'
+    @game_path = './data/games_test.csv'
     @team_path = './data/teams.csv'
-    @game_teams_path = './data/game_teams.csv'
+    @game_teams_path = './data/game_teams_test.csv'
 
     @locations = {
       games: @game_path,
@@ -18,32 +18,49 @@ RSpec.describe TeamsData do
 
 
 
-  it 'is exists' do
+  # it 'is exists' do
+  #   team_obj = TeamsData.new(@stat_tracker)
+  #   expect(team_obj).to be_instance_of(TeamsData)
+  # end
+  #
+  # it 'can store and access teams data' do
+  #   team_obj = TeamsData.new(@stat_tracker)
+  #
+  #   expect(team_obj.teamData).to eq(@stat_tracker.teams)
+  # end
+
+  # it 'can return #team_info' do
+  #   team_obj = TeamsData.new(@stat_tracker)
+  #
+  #   expected = {
+  #     team_id: 18,
+  #     franchiseId: 34,
+  #     teamName: "Minnesota United FC",
+  #     abbreviation: "MIN",
+  #     link: "/api/v1/teams/18"
+  #   }
+  #   expected(TeamsData.team_info(@team_id)).to eq(expected)
+  # end
+
+  # it 'finds all team games' do
+  #   team_obj = TeamsData.new(@stat_tracker)
+  #   expect(team_obj.all_games_by_team(6).count).to eq(3)
+  # end
+
+  # it 'finds best season by team' do
+  #   team_obj = TeamsData.new(@stat_tracker)
+  #   expect(team_obj.best_season(6)).to eq("20122013")
+  # end
+
+
+
+  it 'finds most scored goals by team' do
     team_obj = TeamsData.new(@stat_tracker)
-    expect(team_obj).to be_instance_of(TeamsData)
+    expect(team_obj.most_goals_scored(6)).to eq(4)
   end
 
-  it 'can store and access teams data' do
+  it 'finds lowest scoring game by team' do
     team_obj = TeamsData.new(@stat_tracker)
-
-    expect(team_obj.teamData).to eq(@stat_tracker.teams)
-  end
-
-  it 'can return #team_info' do
-    team_obj = TeamsData.new(@stat_tracker)
-
-    expected = {
-      team_id: 18,
-      franchiseId: 34,
-      teamName: "Minnesota United FC",
-      abbreviation: "MIN",
-      link: "/api/v1/teams/18"
-    }
-    expected(TeamsData.team_info(team_id)).to eq(expected)
-  end
-
-  it 'finds best season by team' do
-    team_obj = TeamsData.new(@stat_tracker)
-    expect(team_obj.best_season(9)).to eq('20142015')
+    expect(team_obj.most_goals_scored(6)).to eq(1)
   end
 end
