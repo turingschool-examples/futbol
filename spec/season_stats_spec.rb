ba0803b0e61fc4a55e6f34cfd4116667082317d9
Robin Dittrich <robinodittrich@gmail.com>
2021-11-03 22:14:05 -0700

    Create most and least accurate team methods.

--- spec/season_stats_spec.rb
+++ spec/season_stats_spec.rb
@@ -16,27 +16,27 @@ RSpec.describe SeasonStats do
     expect(@season_stats).to be_an_instance_of(SeasonStats)
   end
 
-  xit 'winningest_coach' do
+  it 'winningest_coach' do
 
-    expect(@season_stats.winningest_coach).to eq("Mike Babcock")
+    expect(@season_stats.winningest_coach).to eq("Claude Julien")
   end
 
-  xit 'worst_coach' do
+  it 'worst_coach' do
 
-    expect(@season_stats.worst_coach).to eq("Dan Bylsma")
+    expect(@season_stats.worst_coach).to eq("John Tortorella")
   end
 
-  xit 'most_accurate_team' do
+  it 'most_accurate_team' do
 
-    expect(@season_stats.most_accurate_team).to eq("17")
+    expect(@season_stats.most_accurate_team).to eq("3")
   end
 
-  xit 'least_accurate_team' do
+  it 'least_accurate_team' do
 
-    expect(@season_stats.least_accurate_team).to eq("5")
+    expect(@season_stats.least_accurate_team).to eq("16")
   end
 
-  xit 'most_tackles' do
+  it 'most_tackles' do
 
     expect(@season_stats.most_tackles).to eq("6")
   end
