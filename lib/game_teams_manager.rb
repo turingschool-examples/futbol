a29de465c2739a693ef8e3f84deb834bee948411
Wade Naughton <wadenaughton@gmail.com>
2021-11-03 21:36:19 -0400

    typo fix

--- lib/game_teams_manager.rb
+++ lib/game_teams_manager.rb
@@ -4,15 +4,15 @@ require 'pry'
 require_relative './teams'
 
 class GameTeamManager
-  attr_reader :game_team_objects, :game_teams_path
+  attr_reader :game_teams_objects, :game_teams_path
 
   def initialize(game_teams_path)
     @game_teams_path = './data/game_teams.csv'
-    @game_team_objects = (
+    @game_teams_objects = (
       objects = []
       CSV.foreach(game_teams_path, headers: true, header_converters: :symbol) do |row|
         objects << GameTeams.new(row)
-        
+
       end
       objects)
 
