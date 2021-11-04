c67c32a2b1e81ab7dcdee9cf40d60b0886e1eac5
Wade Naughton <wadenaughton@gmail.com>
2021-11-03 22:36:27 -0400

    update stat_tracker and stat_tracker_spec

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,10 +1,10 @@
 require 'CSV'
 require './lib/game_teams'
 require './lib/games'
-# require './lib/teams'
+require './lib/teams'
 require './lib/game_teams_manager'
 require './lib/game_manager'
-# require './lib/teams_manager'
+require './lib/teams_manager'
 
 class StatTracker
   def self.from_csv(locations)
@@ -14,7 +14,7 @@ class StatTracker
   def initialize(locations)
     @games = GameManager.new(locations[:games])
     @teams = TeamManager.new(locations[:teams])
-    @game_teams = GamesTeamsManager.new(locations[:game_teams])
+    @game_teams = GameTeamsManager.new(locations[:game_teams])
   end
 
   def highest_total_score
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -18,6 +18,14 @@ RSpec.describe StatTracker do
   end
 
   xit '#highest_total_score' do
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+    locations = {
+              games: game_path,
+              teams: team_path,
+              game_teams: game_teams_path
+                }
 
     stat_tracker = StatTracker.from_csv(locations)
 
