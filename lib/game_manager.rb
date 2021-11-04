e4c219e73b3d962e58f61cd044e66f13da3c0464
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-11-03 20:38:12 -0400

    Add access to games.csv

--- lib/game_manager.rb
+++ lib/game_manager.rb
@@ -1,19 +1,34 @@
 require 'CSV'
 require 'pry'
 
+require_relative './games'
+
 class GameManager
   attr_reader :game_objects, :game_path
+
   def initialize(game_path)
     @game_path = './data/games.csv'
     # @stat_tracker = stat_tracker
-    @game_objects = []
+    @game_objects = (
+      objects = []
+      CSV.foreach(game_path, headers: true, header_converters: :symbol) do |row|
+        objects << Games.new(row)
+      end
+      objects)
   end
 
-  def create_game_manager
-    objects = []
-    x = CSV.foreach(game_path, headers: true, header_converters: :symbol) do |row|
-      binding.pry
-      objects << GameManager.new(row)
-    end
+  #   def create_game_manager
+  #     x = CSV.read(game_path, headers: true, header_converters: :symbol) do |row|
+  #     x.map do |row|
+  #       Games.new(row)
+  #       end
+  #     end
+  #   end
+  # # binding.pry
+
+  def highest_total_score
+    @game_objects.map do |object|
+      object.venue
+    end.uniq
   end
 end
