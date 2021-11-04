ab15ddbf3dc40131613256762f0da1a9faf8b1d8
Sierra Tucker <sierra.tucker121@gmail.com>
2021-11-04 13:31:41 -0600

    EDIT: Fix merge issues

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe StatTracker do
   let(:stat_tracker) {StatTracker.new(games, teams, game_results)}
 
 
-  xit 'exists' do
+  it 'exists' do
     expect(stat_tracker).to be_instance_of(StatTracker)
   end
 
@@ -21,40 +21,24 @@ RSpec.describe StatTracker do
     expect(stat_tracker.games).to eq('./data/games_dummy.csv')
   end
 
-  xit 'can display the highest total score' do
+  it 'can display the highest total score' do
     expect(stat_tracker.highest_total_score).to eq(5)
   end
 
-  xit 'it can display lowest total score' do
-    expect(stat_tracker.lowest_total_score).to eq(3)
-  end
-
-  xit 'it can display total wins by home team as percentage' do
-    expect(stat_tracker.percentage_home_wins).to eq(60.00)
-  end
-
-  it 'can find the percentage of games that a visitor has won' do
-    expect(stat_tracker.percentage_visitor_wins).to eq(40.00)
-  end
-
-  it 'can find the percentage of games that resulted in a tie' do
-
-    expect(stat_tracker.percentage_ties).to eq(20.00)
-    
   it 'it can display lowest total score' do
     expect(stat_tracker.lowest_total_score).to eq(3)
   end
 
   it 'it can display total wins by home team as percentage' do
-    expect(stat_tracker.percentage_home_wins).to eq(40.00)
+    expect(stat_tracker.percentage_home_wins).to eq(33.34)
   end
 
-  xit 'can find the percentage of games that a visitor has won' do
-    expect(stat_tracker.percentage_visitor_wins).to eq(40.00)
+  it 'can find the percentage of games that a visitor has won' do
+    expect(stat_tracker.percentage_visitor_wins).to eq(33.34)
   end
 
-  xit 'can find the percentage of games that resulted in a tie' do
-    expect(stat_tracker.percentage_ties).to eq(20.00)
+  it 'can find the percentage of games that resulted in a tie' do
+    expect(stat_tracker.percentage_ties).to eq(33.34)
   end
 
   it 'can sort the number of games attributed to each season' do
@@ -64,7 +48,7 @@ RSpec.describe StatTracker do
   end
 
   it 'can calculate average goals per game across seasons' do
-    expect(stat_tracker.averagee_goals_per_game),to eq(3.70)
+    expect(stat_tracker.averagee_goals_per_game).to eq(3.70)
   end
 
   it 'can organize average goals per game by season' do
