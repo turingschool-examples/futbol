1f1fd8f25824856eb38ad36dcc30905456a21b4c
Joel Grant <joelmgrant@me.com>
2021-11-04 20:36:57 -0600

    Modify tests for new structure

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -24,7 +24,7 @@ describe StatTracker do
     it 'exists' do
       expect(@stat_tracker).to be_instance_of(StatTracker)
     end
-end
+
 # Game Statistics Tests
 
 
@@ -35,3 +35,58 @@ end
 
 
 # Team Statistics Tests
+  describe '#team_info' do
+    it 'returns team_id, franchise_id, team_name, abbr., and link' do
+      expected = {
+        :team_id => 27,
+        :franchise_id => 28,
+        :team_name => "San Jose Earthquakes",
+        :abbreviation => "SJ",
+        :link => "/api/v1/teams/27"
+      }
+      expect(@stat_tracker.team_info(27)).to eq(expected)
+    end
+  end
+
+  describe '#best_season' do
+    it 'returns the season with the highest win percentage for a team' do
+      expect(@stat_tracker.best_season(3)).to eq("20142015")
+    end
+  end
+
+  describe '#worst_season' do
+    it 'returns the season with the lowest win percentage for a team' do
+      expect(@stat_tracker.worst_season(3)).to eq("20172018")
+    end
+  end
+
+  describe '#average_win_percentage' do
+    xit 'returns the average win percentage of all games for a team' do
+      expect(@stat_tracker.worst_season(3)).to eq()
+    end
+  end
+
+  describe '#most_goals_scored' do
+    xit 'returns the highest nuber of goals a particular team has scored in a single game' do
+
+    end
+  end
+
+  describe '#fewest_goals_scored' do
+    it 'returns the lowest numer of goals a particular team has scored in a single game' do
+
+    end
+  end
+
+  describe '#favorite_opponent' do
+    xit 'returns the name of the opponent that has the lowest win percentage against the given team' do
+
+    end
+  end
+
+  describe '#rival' do
+    xit 'returns the name of the opponent that has the highest win percentage against the given team' do
+
+    end
+  end
+end
