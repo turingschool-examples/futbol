8ae9f867c3b7fc3dfcd2a38a0cd9aff2a0ac9b6f
Hannah Warren <hannahkwarren@gmail.com>
2021-11-04 23:15:24 -0400

    Add highest and lowest scoring teams methods, for home and away

--- lib/league_methods.rb
+++ lib/league_methods.rb
@@ -4,7 +4,7 @@ module LeagueMethods
     teams.count
   end
 
-  def calc_average_goals_alltime(game_teams, team_id)
+  def calc_avg_goals_alltime(game_teams, team_id)
 
     goals = []
 
@@ -15,14 +15,14 @@ module LeagueMethods
     end
     sum = goals.sum.to_f
     count = goals.count.to_f
-    average = (sum / count).to_f
+    average = (sum / count)
     average
   end
 
   def best_offense(game_teams, teams)
     averages = []
     teams.each do |team|
-      averages << [team, self.calc_average_goals_alltime(game_teams, team.team_id)]
+      averages << [team, self.calc_avg_goals_alltime(game_teams, team.team_id)]
     end
     max = averages.max {|a, b| a[1] <=> b[1]}
     max[0].teamName
@@ -31,9 +31,80 @@ module LeagueMethods
   def worst_offense(game_teams, teams)
     averages = []
     teams.each do |team|
-      averages << [team, self.calc_average_goals_alltime(game_teams, team.team_id)]
+      averages << [team, self.calc_avg_goals_alltime(game_teams, team.team_id)]
     end
     min = averages.min {|a, b| a[1] <=> b[1]}
     min[0].teamName
   end
+
+  def calc_avg_goals_away_alltime(game_teams, team_id)
+
+    goals = []
+
+    game_teams.each do |game_team|
+      if game_team.team_id == team_id && game_team.HoA == "away"
+        goals << game_team.goals
+      end
+    end
+    sum = goals.sum.to_f
+    count = goals.count.to_f
+    average = (sum / count)
+    average
+  end
+
+  def calc_avg_goals_home_alltime(game_teams, team_id)
+
+    goals = []
+
+    game_teams.each do |game_team|
+      if game_team.team_id == team_id && game_team.HoA == "home"
+        goals << game_team.goals
+      end
+    end
+    sum = goals.sum.to_f
+    count = goals.count.to_f
+    average = (sum / count)
+    average
+  end
+
+  #team with highest average score per game, all-time, when playing away
+  def highest_scoring_visitor(game_teams, teams)
+    averages = []
+    teams.each do |team|
+      averages << [team, self.calc_avg_goals_away_alltime(game_teams, team.team_id)]
+    end
+    max = averages.max {|a, b| a[1] <=> b[1]}
+    max[0].teamName
+  end
+
+  #team with the highest average score per game, all-time, at home
+  def highest_scoring_home_team(game_teams, teams)
+    averages = []
+    teams.each do |team|
+      averages << [team, self.calc_avg_goals_home_alltime(game_teams, team.team_id)]
+    end
+    max = averages.max {|a, b| a[1] <=> b[1]}
+    max[0].teamName
+  end
+
+  #team with lowest average score per game, all-time, when playing away
+  def lowest_scoring_visitor(game_teams, teams)
+    averages = []
+    teams.each do |team|
+      averages << [team, self.calc_avg_goals_away_alltime(game_teams, team.team_id)]
+    end
+    min = averages.min {|a, b| a[1] <=> b[1]}
+    min[0].teamName
+  end
+
+  #team with the lowest average score per game, all-time, at home
+  def lowest_scoring_home_team(game_teams, teams)
+    averages = []
+    teams.each do |team|
+      averages << [team, self.calc_avg_goals_home_alltime(game_teams, team.team_id)]
+    end
+    min = averages.min {|a, b| a[1] <=> b[1]}
+    min[0].teamName
+  end
+
 end
--- spec/league_methods_spec.rb
+++ spec/league_methods_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe StatTracker do
   end
 
   it 'can #calc_average_goals_alltime' do
-    expect(@stat_tracker.calc_average_goals_alltime(@stat_tracker.game_teams, 4)).to eq(2.0377358490566038)
+    expect(@stat_tracker.calc_avg_goals_alltime(@stat_tracker.game_teams, 4)).to eq(2.0377358490566038)
   end
 
   it 'can return team name of team with the best offense' do
@@ -29,6 +29,26 @@ RSpec.describe StatTracker do
 
   it 'can return team name of team with the worst offense' do
     expect(@stat_tracker.worst_offense(@stat_tracker.game_teams, @stat_tracker.teams)).to eq("Utah Royals FC")
+  end
+
+  it 'can return the average all-time goals when playing away' do
+    expect(@stat_tracker.calc_avg_goals_away_alltime(@stat_tracker.game_teams, 6)).to eq(2.2450592885375493)
+  end
+
+  it 'can return team name of team with highest all-time average score when playing away' do
+    expect(@stat_tracker.highest_scoring_visitor(@stat_tracker.game_teams, @stat_tracker.teams)).to eq("FC Dallas")
+  end
+
+  it 'can return team name of team with highest all-time average score when playing at home' do
+    expect(@stat_tracker.highest_scoring_home_team(@stat_tracker.game_teams, @stat_tracker.teams)).to eq("Reign FC")
+  end
+
+  it 'can return team name of team with lowest all-time average score when playing away' do
+    expect(@stat_tracker.lowest_scoring_visitor(@stat_tracker.game_teams, @stat_tracker.teams)).to eq("San Jose Earthquakes")
+  end
+
+  it 'can return team name of team with lowest all-time average score when playing at home' do
+    expect(@stat_tracker.lowest_scoring_home_team(@stat_tracker.game_teams, @stat_tracker.teams)).to eq("Utah Royals FC")
   end
 
 end
