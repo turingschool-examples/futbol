f26f75e25122182e0ca6f1f20724d40f08c74f91
Seth Perna <gseth26@gmail.com>
2021-11-06 13:26:15 -0400

    writing game correctly

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -11,9 +11,9 @@ class StatTracker
   attr_accessor :game_teams_path, :teams_path, :games_path
 
   def initialize(locations)
-    # @game_teams_path = game_teams(locations[:game_teams])
-    # @teams_path = teams(locations[:teams])
     @games_path = games(locations[:games])
+    # @teams_path = teams(locations[:teams])
+    # @game_teams_path = game_teams(locations[:game_teams])
   end
 
   def self.from_csv(locations)
@@ -35,23 +35,155 @@ class StatTracker
   end
 
 
-    def game_teams(game_teams_stats)
-      rows = CSV.read(game_stats, headers: true)
-      rows.map do |row|
-        GameTeams.new(row)
+  def game_teams(game_teams_stats)
+    rows = CSV.read(game_stats, headers: true)
+    rows.map do |row|
+      GameTeams.new(row)
+    end
+  end
+
+  def highest_total_score
+    max_score = 0
+    @games_path.each do |game|
+      sum = game.away_goals + game.home_goals
+      if sum > max_score
+        max_score = sum
       end
     end
+    max_score
+  end
 
-  # def highest_total_score
-  #   game_stats = GameStats.new(@games_path)
-  #   game_stats.highest_total_score
+  def lowest_total_score
+     low_score = 100
+     @games_path.each do |game|
+       sum = game.away_goals + game.home_goals
+       if sum < low_score
+         low_score = sum
+       end
+     end
+     low_score
+   end
+
+   def percentage_home_wins
+    home_wins = 0
+    total_game = 0
+    @games_path.each do |game|
+      total_game += 1
+      if game.home_goals > game.away_goals
+        home_wins += 1
+      else
+      end
+    end
+    x = (home_wins.to_f / total_game.to_f).round(2)
+  end
+
+  def percentage_visitor_wins
+    away_wins = 0
+    total_game = 0
+    @games_path.each do |game|
+      total_game += 1
+      if game.away_goals > game.home_goals
+        away_wins += 1
+      else
+      end
+    end
+    x = (away_wins.to_f / total_game.to_f).round(2)
+  end
+
+  def percentage_ties
+    total_game = 0
+    total_ties = 0
+    @games_path.each do |game|
+      total_game += 1
+      if game.home_goals == game.away_goals
+        total_ties += 1
+      else
+      end
+    end
+    x = (total_ties.to_f / total_game.to_f).round(2)
+  end
+
+  def count_of_games_by_season
+    tgps = Hash.new(0)
+    @games_path.each do |game|
+      tgps[game.season_id] += 1
+    end
+    tgps
+  end
+
+  def average_goals_per_game
+    total_game = 0.0
+    away_goals = 0.0
+    home_goals = 0.0
+    @games_path.each do |game|
+      total_game += 1
+      away_goals += (game.away_goals).to_f
+      home_goals += (game.home_goals).to_f
+    end
+    sum = away_goals + home_goals
+    avg_goal_per_game = sum / total_game
+    avg_goal_per_game.round(2)
+  end
+
+  def average_goals_by_season
+     avg = {}
+     count_of_games_by_season.each_pair do |season, games|
+       avg[season] = (total_goals(season) / games).round(2)
+     end
+     avg
+   end
+
+   def season_games
+     @games_path.group_by do |game|
+       game.season_id
+     end
+   end
+
+   def total_goals(season)
+     (total_away_goals(season) + total_home_goals(season)).sum
+   end
+
+   def total_away_goals(season)
+     away = @games_path.select do |game|
+       game.include?(season)
+     end
+
+     away.map do |goal|
+       goal.to_f
+     end
+   end
+
+   def total_home_goals(season)
+     home = @games_path.select do |game|
+       @games_path.include?(season)
+     end
+     home.map do |goal|
+       goal.to_f
+     end
+   end
+ end
+ # #
+  # def average_goals_by_season
+  #   avg = 0.0
+  #   total = 0.0
+  #   avg_hash = Hash.new(0)
+  #   @games_path.group_by do |game|
+  #     count_of_games_by_season.each do |sea|
+  #       require "pry"; binding.pry
+  #       if game.season_id == sea[0]
+  #         total = (game.away_goals + game.home_goals).to_f
+  #       end
+  #       avg = (total / sea.last.to_f).round(2)
+  #       avg_hash[sea.first] = avg
+  #     end
+  #   end
+  #   avg_hash
   # end
-  #
-  # def lowest_total_score
-  #   game_stats = GameStats.new(@games_path)
-  #   game_stats.lowest_total_score
-  # end
-  #
+
+
+
+
+
   # def percentage_home_wins
   #   game_stats = GameStats.new(@games_path)
   #   game_stats.percentage_home_wins
@@ -87,6 +219,3 @@ class StatTracker
   #   league_stats = LeagueStats.new(@game_teams_path)
   #   league_stats.count_of_teams
   # end
-
-
-end
--- spec/game_stats_spec.rb
+++ spec/game_stats_spec.rb
@@ -15,36 +15,37 @@ RSpec.describe GameStats do
 
     expect(@game_stats).to be_an_instance_of(GameStats)
   end
+end 
 
-  it "#highest_total_score" do
-
-    expect(@game_stats.highest_total_score).to eq(5)
-  end
-
-  xit "lowest_total_score" do
-
-    expect(@game_stats.lowest_total_score).to eq(0)
-  end
-
-  xit "#percentage_home_wins" do
-
-    expect(@game_stats.percentage_home_wins).to eq(55.56)
-  end
-
-  xit "#percentage_away_wins" do
-
-    expect(@game_stats.percentage_away_wins).to eq(33.33)
-  end
-
-  xit "#percentage_ties" do
-
-    expect(@game_stats.percentage_ties).to eq(11.11)
-  end
-
-  xit "count_of_games_by_season" do
-
-    expect(@game_stats.count_of_games_by_season).to eq(Hash)
-  end
-
-
-end
+#   it "#highest_total_score" do
+#
+#     expect(@game_stats.highest_total_score).to eq(5)
+#   end
+#
+#   xit "lowest_total_score" do
+#
+#     expect(@game_stats.lowest_total_score).to eq(0)
+#   end
+#
+#   xit "#percentage_home_wins" do
+#
+#     expect(@game_stats.percentage_home_wins).to eq(55.56)
+#   end
+#
+#   xit "#percentage_away_wins" do
+#
+#     expect(@game_stats.percentage_away_wins).to eq(33.33)
+#   end
+#
+#   xit "#percentage_ties" do
+#
+#     expect(@game_stats.percentage_ties).to eq(11.11)
+#   end
+#
+#   xit "count_of_games_by_season" do
+#
+#     expect(@game_stats.count_of_games_by_season).to eq(Hash)
+#   end
+#
+#
+# end
