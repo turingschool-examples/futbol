ae15a1ffc24a3d4bfa177b9b263f11ebeb143b9f
Josh Walsh <dmaggioarcangeli@yahoo.com>
2021-11-06 12:57:25 -0600

    Create tests for TeamStats

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -34,10 +34,60 @@ RSpec.describe StatTracker do
     it "league count_of_teams" do
       expect(stattracker.count_of_teams).to eq(32)
     end
-  
+
     it "best offense" do
       expect(stattracker.best_offense).to eq("FC Dallas")
     end
   end
 
+
+  describe 'TeamStats' do
+    describe '#team_info' do
+      it "returns the info for each team" do
+        expected = {
+          "team_id" => "18",
+          "franchise_id" => "34",
+          "team_name" => "Minnesota United FC",
+          "abbreviation" => "MIN",
+          "link" => "/api/v1/teams/18"
+        }
+        expect(stattracker.team_info("18")).to eq expected
+      end
+    end
+    describe '#best_season' do
+      it "returns the best season for the team id given" do
+        expect(stattracker.best_season("6")).to eq "20122013"
+      end
+    end
+
+    describe '#worst_season' do
+      it "returns the worst season for the team id given" do
+        expect(stattracker.worst_season("3")).to eq "20122013"
+      end
+    end
+
+    describe '#average_win_percentage' do
+      it "returns the average win/loss percentage for the team id given" do
+        expect(stattracker.average_win_percentage("6")).to eq 100.0
+      end
+    end
+
+    describe '#least_goals_scored' do
+      it "returns the least goals scored for the team id given" do
+        expect(stattracker.fewest_goals_scored("6")).to eq 1
+      end
+    end
+
+    describe '#favorite_opponent' do
+      it "returns the team name that has the most losses against the team id given" do
+        expect(stattracker.favorite_opponent("6")).to eq "Houston Dynamo"
+      end
+    end
+
+    describe '#rival' do
+      it "returns the team name that has the most wins against the team id given" do
+        expect(stattracker.rival("5")).to eq "FC Dallas"
+      end
+    end
+  end
 end
