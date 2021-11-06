2b0ed542bd27508de418b895bdd5015732854900
Kelly Anderson <kean9528@colorado.edu>
2021-11-06 11:18:45 -0600

    feat: write methods for percent home and away wins

--- lib/game_team.rb
+++ lib/game_team.rb
@@ -1,4 +1,5 @@
 class GameTeam
+  attr_reader :game_id, :team_id, :home_or_away, :result, :settled_in, :head_coach, :goals, :shots, :tackles, :pim, :power_play_opportunities, :power_play_goals, :face_off_win_percentage, :giveaways, :takeaways
   def initialize(data)
     @game_id = data["game_id"]
     @team_id = data["team_id"]
@@ -16,4 +17,19 @@ class GameTeam
     @giveaways = data["giveaways"]
     @takeaways = data["takeaways"]
   end
+
+  # def highest_total_score
+  #   grouped_by_game = @game_teams.group_by do |row|
+  #     row["game_id"]
+  #   end
+  #   game_sum_goals = []
+  #   grouped_by_game.each_pair do |key, value|
+  #     first_team_goals = value.first["goals"].to_i
+  #     second_team_goals = value.last["goals"].to_i
+  #     game_sum_goals << first_team_goals + second_team_goals
+  #   end
+  #   game_sum_goals.max
+  # end
+
+
 end
--- lib/league.rb
+++ lib/league.rb
@@ -11,11 +11,29 @@ class League
     @games = data[:games]
     @teams = data[:teams]
     @game_teams = data[:game_teams]
-    require "pry"; binding.pry
   end
 
   def count_of_teams
     @teams.count
   end
 
+  def percentage_home_wins
+    home_games = @game_teams.find_all do |game_team|
+      game_team.home_or_away["home"]
+    end
+    home_game_wins = @game_teams.find_all do |game_team|
+      game_team.home_or_away["home"] && game_team.result == "WIN"
+    end
+    ((home_game_wins.length.to_f)/(home_games.length.to_f)).round(2)
+  end
+
+  def percentage_away_wins
+    away_games = @game_teams.find_all do |game_team|
+      game_team.home_or_away["away"]
+    end
+    away_game_wins = @game_teams.find_all do |game_team|
+      game_team.home_or_away["away"] && game_team.result["WIN"]
+    end
+    ((away_game_wins.length.to_f)/(away_games.length.to_f)).round(2)
+  end
 end
