e8eb0bb6b0d9978d9a45c383c1fbf6e79da1c7a7
Wade Naughton <wadenaughton@gmail.com>
2021-11-06 12:05:42 -0400

    branch conflicts resolve

--- lib/game_teams.rb
+++ lib/game_teams.rb
@@ -17,25 +17,6 @@ class GameTeams
               :giveaways,
               :takeaways
 
-<<<<<<< HEAD
-  def initialize(info_hash)
-    @game_id                = info_hash[:game_id].to_i
-    @team_id                = info_hash[:team_id].to_i
-    @hoa                    = info_hash[:HoA]
-    @result                 = info_hash[:result]
-    @settled_in             = info_hash[:settled_in]
-    @head_coach             = info_hash[:head_coach]
-    @goals                  = info_hash[:goals].to_i
-    @shots                  = info_hash[:shots].to_i
-    @tackles                = info_hash[:tackles].to_i
-    @pim                    = info_hash[:pim].to_i
-    @powerplayopportunities = info_hash[:powerPlayOpportunities].to_i
-    @powerplaygoals         = info_hash[:powerPlayGoals].to_i
-    @faceoffwinpercentage   = info_hash[:faceOffWinPercentage].to_f
-    @giveaways              = info_hash[:giveaways].to_i
-    @takeaways              = info_hash[:takeaways].to_i
-=======
-
   def initialize(row)
     @row = row[:game_id]
     @team_id = row[:team_id].to_i
@@ -53,6 +34,5 @@ class GameTeams
     @giveaways = row[:giveaways].to_i
     @takeaways = row[:takeaways].to_i
 
->>>>>>> 6272cf6 (commit to save changes and switch branch)
   end
 end
