31cc23d3b54b27ebc855ee1ffa42905dc0de020f
Sierra Tucker <sierra.tucker121@gmail.com>
2021-11-06 16:40:29 -0600

    Feat: Add #average_goals_per_game and #average_goals_by_season

--- lib/league.rb
+++ lib/league.rb
@@ -41,4 +41,15 @@ class League
     ((sum_of_goals_each_game.sum.to_f) / (@game_teams.uniq {|game_team| game_team.game_id}.length.to_f)).round(2)
   end
 
+  def average_goals_by_season
+    goals = []
+    games_by_season = @games.group_by {|game| game.season}
+
+    goals << away = games_by_season.map {|season, game| game.map {|game| game.away_goals.to_f}.inject(:+)}
+    goals << home = games_by_season.map {|season, game| game.map {|game| game.home_goals.to_f}.inject(:+)}
+    total_games_season = games_by_season.map {|season, game| game.map {|game| game.game_id}.count}
+    goals_array = goals.transpose.map(&:sum)
+    average_goals_per_game_season = goals_array.zip(total_games_season).map {|thing| thing.inject(:/).round(2)}
+    average_goals_by_season_hash = Hash[games_by_season.keys.zip(average_goals_per_game_season)]
+  end
 end
