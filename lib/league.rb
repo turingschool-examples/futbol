ed8fe64bea50616f097ad1e62982319f10c855e7
Kelly Anderson <kean9528@colorado.edu>
2021-11-06 16:36:03 -0600

    feat: create best and worst offense methods

--- lib/league.rb
+++ lib/league.rb
@@ -37,7 +37,6 @@ class League
       sum_of_goals_each_game << value[0].goals.to_i + value[1].goals.to_i
     end
     sum_of_goals_each_game.min
-
   end
 
   def count_of_teams
@@ -62,6 +61,7 @@ class League
       game_team.home_or_away["away"] && game_team.result["WIN"]
     end
     ((away_game_wins.length.to_f)/(away_games.length.to_f)).round(2)
+  end
 
   def percentage_ties
     tie_games = @game_teams.select {|game| game.result == "TIE"}
@@ -71,6 +71,28 @@ class League
   def count_of_games_by_season
     games_by_season = @games.group_by {|game| game.season}
     games_by_season.transform_values! {|value| value.count}
-    #require "pry"; binding.pry
   end
+
+  def combined_hash_team_goals
+    away_team_hash = @games.group_by {|game| game.away_team_id}
+    away_added_goals = away_team_hash.map {|id, games| games.map {|game| game.away_goals.to_i}.inject(:+)}
+    away_hash = Hash[away_team_hash.keys.zip(away_added_goals)]
+    home_team_hash = @games.group_by {|game| game.home_team_id}
+    home_added_goals = home_team_hash.map {|id, games| games.map {|game| game.home_goals.to_i}.inject(:+)}
+    home_hash = Hash[home_team_hash.keys.zip(home_added_goals)]
+    home_hash.merge(away_hash){|key, home_value, away_value| home_value + away_value}
+  end
+
+  def best_offense
+    highest_scoring_team = combined_hash_team_goals.index(combined_hash_team_goals.values.max)
+    best_team = @teams.find {|team| team.team_id == highest_scoring_team}
+    best_team.team_name
+  end
+
+  def worst_offense
+    lowest_scoring_team = combined_hash_team_goals.index(combined_hash_team_goals.values.min)
+    worst_team = @teams.find {|team| team.team_id == lowest_scoring_team}
+    worst_team.team_name
+  end
+
 end
