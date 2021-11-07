f29aff0b89d84f9523ef8fbbd87034163cd304cf
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-11-07 00:39:29 -0400

    Add team_info method

--- lib/team_manager.rb
+++ lib/team_manager.rb
@@ -21,4 +21,15 @@ class TeamManager
   def count_of_teams
     @team_objects.count
   end
+
+  def team_info(team_id)
+  pick = @team_objects.find {|team| team.team_id.to_s == team_id}
+{
+  "team_id" => pick.team_id.to_s,
+  "franchise_id" => pick.franchiseid.to_s,
+  "team_name" => pick.teamname,
+  "abbreviation" => pick.abbreviation,
+  "link" => pick.link
+}
+  end
 end
