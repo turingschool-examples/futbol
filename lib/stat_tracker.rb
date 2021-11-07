d176fa7a05235ccd1219171d0dc48f2dd2820c93
Jessica Grazulis <jlgrazulis@gmail.com>
2021-11-07 16:30:21 -0700

    Add #least_accurate team

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -194,6 +194,118 @@ class StatTracker
     end
     team_name.team_name
   end
+
+  def least_accurate_team(season)
+    season_games = []
+     @games.each do |game|
+       if game.season == season
+         season_games << game
+       end
+    end
+
+    filtered_teams = []
+    @teams.each do |team|
+      @game_teams.each do |game_team|
+        season_games(season).each do |game|
+          if game_team.team_id == team.team_id && game.game_id == game_team.game_id
+            filtered_teams << team
+          end
+        end
+      end
+    end
+
+    goal_count = Hash.new(0)
+    filtered_teams.each do |game_team|
+      goal_count[game_team.team_id] += 1
+    end
+
+    shots_count = Hash.new(0)
+    filtered_teams.each do |game_team|
+      shots_count[game_team.team_id] += 1
+    end
+
+    worst_ratio = Hash.new(0)
+    filtered_teams.each do |game_team|
+      goal_count.each_value do |goal|
+        shots_count.each_value do |shot|
+          if goal <= shot
+            worst_ratio[game_team.team_id] += 1
+          end
+        end
+      end
+    end
+
+    worst_team_id = worst_ratio.min_by { |key, value| value }[0]
+
+    team_name = @teams.find do |team|
+      team.team_id == worst_team_id
+    end
+    team_name.team_name
+  end
+
+  def most_tackles(season)
+    season_games = []
+     @games.each do |game|
+       if game.season == season
+         season_games << game
+       end
+    end
+
+    filtered_game_teams = []
+    @game_teams.each do |game_team|
+      season_games(season).each do |game|
+        if game.game_id == game_team.game_id
+          filtered_game_teams << game_team
+        end
+      end
+    end
+
+    total_team_tackles = Hash.new(0)
+    filtered_game_teams.each do |game_team|
+      total_team_tackles[game_team.team_id] += game_team.tackles
+    end
+
+    team_id_tackles = total_team_tackles.max_by { |key, value| value }[0]
+
+    team_name = @teams.find do |team|
+      team.team_id == team_id_tackles
+    end
+
+    team_name.team_name
+  end
+
+  def fewest_tackles(season)
+    season_games = []
+    @games.each do |game|
+      if game.season == season
+        season_games << game
+      end
+    end
+
+    filtered_game_teams = []
+    @game_teams.each do |game_team|
+      season_games(season).each do |game|
+        if game.game_id == game_team.game_id
+          filtered_game_teams << game_team
+        end
+      end
+    end
+
+    total_team_tackles = Hash.new(0)
+    filtered_game_teams.each do |game_team|
+      total_team_tackles[game_team.team_id] += game_team.tackles
+    end
+
+    team_id_tackles = total_team_tackles.min_by { |key, value| value }[0]
+
+    team_name = @teams.find do |team|
+      team.team_id == team_id_tackles
+    end
+
+    team_name.team_name
+
+  end
 end
+
 
   # Team Statistics
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,13 +1,13 @@
 require 'csv'
 require './lib/stat_tracker'
-require './lib/game_team'
 require './lib/game'
 require './lib/team'
+require './lib/game_team'
 require './runner'
+require_relative 'stat_tracker_spec'
 
-describe StatTracker do
+RSpec.describe StatTracker do
   before(:each) do
-
     game_path = './data/games.csv'
     team_path = './data/teams.csv'
     game_teams_path = './data/game_teams.csv'
@@ -21,10 +21,15 @@ describe StatTracker do
     @stat_tracker = StatTracker.from_csv(locations)
   end
 
-
+  describe '#instance of stat_tracker' do
     it 'exists' do
       expect(@stat_tracker).to be_instance_of(StatTracker)
     end
+  end
+
+
+
+
 # Game Statistics Test
 
 
@@ -34,35 +39,64 @@ describe StatTracker do
   describe '#winningest_coach' do
 
     it 'returns a string' do
-      expect(@stat_tracker.winningest_coach("20122013")).to eq("Dan Lacroix")
+      expect(@stat_tracker.winningest_coach("20122013")).to eq("coach name")
     end
     #
     it 'gives name of Coach with best win percentage of season' do
-      expect(@stat_tracker.winningest_coach("20122013")).to eq("Dan Lacroix")
+      expect(@stat_tracker.winningest_coach("20122013")).to eq("coach name")
     end
   end
 
   describe '#worst_coach' do
+
     it 'returns a string' do
-      expect(@stat_tracker.worst_coach("20122013")).to eq("Martin Raymond")
+      expect(@stat_tracker.worst_coach("20122013")).to eq("coach name")
     end
 
     it 'gives name of Coach with the worst win percentage for the season' do
-      expect(@stat_tracker.worst_coach("20122013")).to eq("Martin Raymond")
+      expect(@stat_tracker.worst_coach("20122013")).to eq("coach name")
     end
   end
 
   describe '#most_accurate_team' do
     it 'returns a string' do
-      expect(@stat_tracker.most_accurate_team("20122013")).to eq("Houston Dynamo")
+      expect(@stat_tracker.most_accurate_team("20122013")).to eq("team name")
     end
 
     it 'gives name of team with the best ratio of shots to goals for the season' do
-      expect(@stat_tracker.most_accurate_team("20122013")).to eq("Houston Dynamo")
+      expect(@stat_tracker.most_accurate_team("20122013")).to eq("team name")
+    end
+  end
+
+  describe '#least_accurate_team' do
+    it 'returns a string' do
+      expect(@stat_tracker.least_accurate_team("20122013")).to be_a(String)
     end
 
+    it 'gives name of team with the worst ratio of shots to goals for the season' do
+      expect(@stat_tracker.least_accurate_team("20122013")).to eq("team name")
+    end
+  end
 
- end
+  describe '#most_tackles' do
+    it 'returns a string' do
+      expect(@stat_tracker.most_tackles("20122013")).to be_a(String)
+    end
+
+    it 'gives name of team with the most tackles in the season' do
+      expect(@stat_tracker.most_tackles("20122013")).to eq("team name")
+    end
+  end
+
+  describe '#fewest_tackles' do
+    it 'returns a string' do
+      expect(@stat_tracker.fewest_tackles("20122013")).to be_a(String)
+    end
+
+    it 'gives name of team with the fewest tackles in the season' do
+      expect(@stat_tracker.fewest_tackles("20122013")).to eq("team name")
+    end
+  end
 end
 # Season Statistics Tests
 
