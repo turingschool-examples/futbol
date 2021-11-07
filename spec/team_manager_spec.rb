e21902d64343ff0e56298a97f107ab6798359bda
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-11-07 00:21:11 -0400

    Add test for count_of_teams method

--- spec/team_manager_spec.rb
+++ spec/team_manager_spec.rb
@@ -16,7 +16,13 @@ RSpec.describe TeamManager do
 
     expect(team_manager.team_objects[0]).to be_an(Teams)
     # expect(game_manager.add_objects).to be_an_instance_of(GameManager)
-    expect(team_manager.team_objects.count).to eq(32)
     # expect(team_manager.highest_total_score).to eq(0)
+  end
+
+  it 'counts the number of teams' do
+    team_path = './data/teams.csv'
+    team_manager = TeamManager.new(team_path)
+
+    expect(team_manager.count_of_teams).to eq(32)
   end
 end
