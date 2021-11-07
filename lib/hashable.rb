ec62f6175037e117df8c29dac6737427db0bb73e
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-11-06 23:13:51 -0600

    Add functionality to Hashable module, refactored highest/lowest home/vistor

--- lib/hashable.rb
+++ lib/hashable.rb
@@ -15,10 +15,27 @@ module Hashable
     away_hash = Hash[away_team_hash.keys.zip(away_added_goals)]
   end
 
+
+  def away_team_goals_per_game_avg
+    away_team_hash = @games.group_by {|game| game.away_team_id}
+    away_team_hash.transform_values! {|value| value.count}
+    combined_average = away_teams_goals_by_id.merge(away_team_hash){|key, goals_value, games_value| goals_value.to_f / games_value.to_f}
+  end
+
   def home_team_goals_by_id
     home_team_hash = @games.group_by {|game| game.home_team_id}
     home_added_goals = home_team_hash.map {|id, games| games.map {|game| game.home_goals.to_i}.inject(:+)}
     home_hash = Hash[home_team_hash.keys.zip(home_added_goals)]
+  end
+
+  def home_team_goals_per_game_avg
+    home_team_hash = @games.group_by {|game| game.home_team_id}
+    home_team_hash.transform_values! {|value| value.count}
+    combined_average = home_team_goals_by_id.merge(home_team_hash){|key, goals_value, games_value| goals_value.to_f / games_value.to_f}
+  end
+
+  def team_name_from_id(team_id)
+    @teams.select {|team| team.team_id == team_id}.map {|team| team.team_name}[0]
   end
 
   def combined_goals_by_team_id
@@ -32,30 +49,30 @@ module Hashable
     team_id = combined_average.index(combined_average.values.max)
     return @teams.select {|team| team.team_id == team_id}.map {|team| team.team_name}[0]
   end
-
-  def highest_scoring_home_team
-    home_team_hash = @games.group_by {|game| game.home_team_id}
-    home_team_hash.transform_values! {|value| value.count}
-    combined_average = home_team_goals_by_id.merge(home_team_hash){|key, goals_value, games_value| goals_value.to_f / games_value.to_f}
-    team_id = combined_average.index(combined_average.values.max)
-    #require "pry"; binding.pry
-    return @teams.select {|team| team.team_id == team_id}.map {|team| team.team_name}[0]
-  end
-
-  def lowest_scoring_visitor
-    away_team_hash = @games.group_by {|game| game.away_team_id}
-    away_team_hash.transform_values! {|value| value.count}
-    combined_average = away_teams_goals_by_id.merge(away_team_hash){|key, goals_value, games_value| goals_value.to_f / games_value.to_f}
-    team_id = combined_average.index(combined_average.values.min)
-    return @teams.select {|team| team.team_id == team_id}.map {|team| team.team_name}[0]
-  end
-
-  def lowest_scoring_home_team
-    home_team_hash = @games.group_by {|game| game.home_team_id}
-    home_team_hash.transform_values! {|value| value.count}
-    combined_average = home_team_goals_by_id.merge(home_team_hash){|key, goals_value, games_value| goals_value.to_f / games_value.to_f}
-    team_id = combined_average.index(combined_average.values.min)
-    #require "pry"; binding.pry
-    return @teams.select {|team| team.team_id == team_id}.map {|team| team.team_name}[0]
-  end
+  #
+  # def highest_scoring_home_team
+  #   home_team_hash = @games.group_by {|game| game.home_team_id}
+  #   home_team_hash.transform_values! {|value| value.count}
+  #   combined_average = home_team_goals_by_id.merge(home_team_hash){|key, goals_value, games_value| goals_value.to_f / games_value.to_f}
+  #   team_id = combined_average.index(combined_average.values.max)
+  #   #require "pry"; binding.pry
+  #   return @teams.select {|team| team.team_id == team_id}.map {|team| team.team_name}[0]
+  # end
+  #
+  # def lowest_scoring_visitor
+  #   away_team_hash = @games.group_by {|game| game.away_team_id}
+  #   away_team_hash.transform_values! {|value| value.count}
+  #   combined_average = away_teams_goals_by_id.merge(away_team_hash){|key, goals_value, games_value| goals_value.to_f / games_value.to_f}
+  #   team_id = combined_average.index(combined_average.values.min)
+  #   return @teams.select {|team| team.team_id == team_id}.map {|team| team.team_name}[0]
+  # end
+  #
+  # def lowest_scoring_home_team
+  #   home_team_hash = @games.group_by {|game| game.home_team_id}
+  #   home_team_hash.transform_values! {|value| value.count}
+  #   combined_average = home_team_goals_by_id.merge(home_team_hash){|key, goals_value, games_value| goals_value.to_f / games_value.to_f}
+  #   team_id = combined_average.index(combined_average.values.min)
+  #   #require "pry"; binding.pry
+  #   return @teams.select {|team| team.team_id == team_id}.map {|team| team.team_name}[0]
+  # end
 end
