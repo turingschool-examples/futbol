9767dc33660b91131b6e138db890b43dfa67fbf4
craig cardwell <wizeduvcc@gmail.com>
2021-11-07 21:11:30 -0700

    test: edit team info test and add remaining tests for Team Statistics methods best season down to rival

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -78,13 +78,41 @@ RSpec.describe StatTracker do
   it "#team_info" do
     expected = {
       "team_id" => "18",
-      "franchise_id" => "34",
+      "franchiseId" => "34",
       "team_name" => "Minnesota United FC",
       "abbreviation" => "MIN",
       "link" => "/api/v1/teams/18"
     }
 
     expect(@stat_tracker.team_info("18")).to eq expected
+  end
+
+  it "#best_season" do
+    expect(@stat_tracker.best_season("6")).to eq "20132014"
+  end
+
+  xit "#worst_season" do
+    expect(@stat_tracker.worst_season("6")).to eq "20142015"
+  end
+
+  xit "#average_win_percentage" do
+    expect(@stat_tracker.average_win_percentage("6")).to eq 0.49
+  end
+
+  xit "#most_goals_scored" do
+    expect(@stat_tracker.most_goals_scored("18")).to eq 7
+  end
+
+  xit "#fewest_goals_scored" do
+    expect(@stat_tracker.fewest_goals_scored("18")).to eq 0
+  end
+
+  xit "#favorite_opponent" do
+    expect(@stat_tracker.favorite_opponent("18")).to eq "DC United"
+  end
+
+  xit "#rival" do
+    expect(@stat_tracker.rival("18")).to eq("Houston Dash").or(eq("LA Galaxy"))
   end
 
 end
