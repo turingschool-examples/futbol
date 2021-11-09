5259deeff90c81836fe5551ec69f5941f156fec1
John Napier <jcee.napier@gmail.com>
2021-11-09 14:00:23 -0700

    Add highest and Lowest scoring visitor methods

--- lib/league_stats.rb
+++ lib/league_stats.rb
@@ -5,9 +5,10 @@ class LeagueStats < Team
   attr_reader :game_teams, 
               :games 
 
-  def initialize(file_1, file_2)
+  def initialize(file_1, file_2, file_3)
     @game_teams = self.format(file_1)
     @teams      = self.format2(file_2)
+    @games      = self.format3(file_3)
   end 
 
   def format(file)
@@ -21,6 +22,13 @@ class LeagueStats < Team
     league_file = CSV.read(file, headers: true, header_converters: :symbol)
     league_file.map do |row|
       Teams.new(row)
+    end
+  end
+
+  def format3(file)
+    league_file = CSV.read(file, headers: true, header_converters: :symbol)
+    league_file.map do |row|
+      Game.new(row)
     end
   end
 
@@ -72,5 +80,27 @@ class LeagueStats < Team
     end
     averages
     find_team_name(averages.key(averages.values.min))
+  end
+  
+  def away_teams_list
+   @game_teams.find_all do |game|
+      game.hoa == "away"
+    end
+  end
+
+  def highest_scoring_visitor
+    scores = away_teams_list.reduce({}) do |hash, team|
+      hash[team.team_id] = team.goals.to_i
+      hash
+    end
+    find_team_name(scores.key(scores.values.max))
+  end
+
+  def lowest_scoring_visitor
+    scores = away_teams_list.reduce({}) do |hash, team|
+      hash[team.team_id] = team.goals.to_i
+      hash
+    end
+    find_team_name(scores.key(scores.values.min))
   end
 end
