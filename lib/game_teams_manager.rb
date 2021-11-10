73d2236e8f700d2f07facfb6e3d51057135dfba3
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-11-09 19:34:14 -0500

    Refactor best_offense, worst_offense, highest_scoring_visitor, highest_scoring_home_team, lowest_scoring_home_team methods

--- lib/game_teams_manager.rb
+++ lib/game_teams_manager.rb
@@ -9,16 +9,8 @@ class GameTeamsManager
 
   def initialize(game_teams_path)
     @game_teams_path = './data/game_teams.csv'
-    @game_teams_objects = (
-      objects = []
-      CSV.foreach(game_teams_path, headers: true, header_converters: :symbol) do |row|
-        objects << GameTeams.new(row)
-      end
-      objects)
+    @game_teams_objects = CSV.read(game_teams_path, headers: true, header_converters: :symbol).map {|row| GameTeams.new(row)}
     @teams = TeamManager.new('./data/teams.csv').team_objects
-    # team_manager = TeamManager.new('./data/teams.csv')
-    # @teams = team_manager.team_objects
-
   end
 
   def games_by_team_id(team_id, hoa = nil)
@@ -40,58 +32,34 @@ class GameTeamsManager
   def average_goals_per_game_by_id(team_id, hoa = nil)
     average = total_goals_by_team(team_id, hoa) / games_by_team_id(team_id, hoa).count.to_f
     average.round(2)
-
   end
 
-  # def wins_by_team_id(team_id, hoa = nil)
-  #   games_by_team_id(team_id, hoa).find_all do |game|
-  #       game.result == "WIN"
-  #   end
-  # end
-  #
-  # def win_percent_by_id(team_id, hoa = nil)
-  #   win_percent = wins_by_team_id(team_id, hoa).count
-  #    other_percent = games_by_team_id(team_id, hoa).count
-  #    win_percent.to_f/other_percent
-  #   require "pry"; binding.pry
-  # end
-
-
-
   def best_offense
-    best_team = teams.max_by { |team| average_goals_per_game_by_id(team.team_id) }
-    best_team.teamname
-
+    teams.max_by { |team| average_goals_per_game_by_id(team.team_id) }.teamname
   end
 
   def worst_offense
-    worst_team = teams.min_by { |team| average_goals_per_game_by_id(team.team_id) }
-    worst_team.teamname
-
+    teams.min_by { |team| average_goals_per_game_by_id(team.team_id) }.teamname
   end
 
   def highest_scoring_visitor
-    highest_visitor = teams.max_by { |team| average_goals_per_game_by_id(team.team_id, "away") }
-    highest_visitor.teamname
+    teams.max_by { |team| average_goals_per_game_by_id(team.team_id, "away") }.teamname
   end
 
   def highest_scoring_home_team
-    highest_home = teams.max_by { |team| average_goals_per_game_by_id(team.team_id, "home") }
-    highest_home.teamname
+    teams.max_by { |team| average_goals_per_game_by_id(team.team_id, "home") }.teamname
   end
 
   def lowest_scoring_visitor
-    lowest_visitor = teams.min_by { |team| average_goals_per_game_by_id(team.team_id, "away") }
-    lowest_visitor.teamname
+    teams.min_by { |team| average_goals_per_game_by_id(team.team_id, "away") }.teamname
   end
 
   def lowest_scoring_home_team
-    lowest_home = teams.min_by { |team| average_goals_per_game_by_id(team.team_id, "home") }
-    lowest_home.teamname
+    teams.min_by { |team| average_goals_per_game_by_id(team.team_id, "home") }.teamname
   end
 
   def most_goals_scored(team_id)
-    games_by_team_id = @game_teams_objects.find_all { |game| game.team_id == team_id.to_i  }
+    games_by_team_id = @game_teams_objects.find_all { |game| game.team_id == team_id.to_i }
     most = games_by_team_id.max_by do |game|
       game.goals
     end
@@ -105,9 +73,4 @@ class GameTeamsManager
     end
     fewest.goals
   end
-
-  # def test
-  #
-  #   require "pry"; binding.pry
-  # end
 end
