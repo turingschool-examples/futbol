b488d2ae3c0cb19b02ac61a4cfeed3e11de04d27
Arnaldo Aparicio <apaarn2211@gmail.com>
2021-11-09 19:36:58 -0500

    Clean up unused code

--- spec/game_manager_spec.rb
+++ spec/game_manager_spec.rb
@@ -12,19 +12,11 @@ RSpec.describe GameManager do
     expect(game_manager).to be_an_instance_of(GameManager)
   end
 
-  xit 'starts with no game objects' do
-    game_path = './data/games.csv'
-    game_manager = GameManager.new(game_path)
-
-    expect(game_manager.game_objects).to eq([])
-  end
-
   it 'can create game objects' do
     game_path = './data/games.csv'
     game_manager = GameManager.new(game_path)
 
     expect(game_manager.game_objects[0]).to be_an(Games)
-    # expect(game_manager.add_objects).to be_an_instance_of(GameManager)
     expect(game_manager.game_objects.count).to eq(7441)
   end
 
