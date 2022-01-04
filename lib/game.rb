6566fe544a56d4b5d5e0407b7c90502eb72932d9
Adrian Campos <adriancampos2992@yahoo.com>
2022-01-03 17:54:14 -0800

    Feature: lowest total score method and test

--- lib/game.rb
+++ lib/game.rb
@@ -14,4 +14,14 @@ class Game
     end
     total_scores.max
   end
+
+
+  def lowest_total_score
+    total_scores = []
+    @contents.each do |row|
+      score =  row[:away_goals].to_i + row[:home_goals].to_i
+      total_scores << score
+    end
+    total_scores.min
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,18 +4,27 @@ RSpec.describe do Game
   it 'exists' do
     game_path = './data/games_stub.csv'
     locations = {games: game_path}
-     game = Game.new(locations[:games])
+    game = Game.new(locations[:games])
      expect(game).to be_an_instance_of(Game)
   end
 
 
-  it 'will give results' do
+  it 'give highest total score' do
     game_path = './data/games_stub.csv'
     locations = {games: game_path}
     game = Game.new(locations[:games])
     expect(game.highest_total_score).to be(5)
-
   end
+
+  it 'give lowest total score' do
+    game_path = './data/games_stub.csv'
+    locations = {games: game_path}
+    game = Game.new(locations[:games])
+    expect(game.lowest_total_score).to be(1)
+  end
+
+
+
   # it '' do
   #
   # end
