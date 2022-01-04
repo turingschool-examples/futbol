a36ffebfe0ae5743601068870a757982fa8af291
Michael Hicks <hicks.michael04@gmail.com>
2022-01-04 15:38:25 -0700

    feat: Add lowest_total_score

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -12,6 +12,7 @@ class StatTracker
     StatTracker.new(places) #creating an instance of StatTracker holding the hash as locations
   end
 
+
   def highest_total_score
     games = CSV.read @locations[:games], headers: true, header_converters: :symbol
     scores_array = []
@@ -19,13 +20,15 @@ class StatTracker
       scores_array << row[:away_goals].to_i + row[:home_goals].to_i
     end
     scores_array.max
+  end
 
-    # binding.pry
-    # highest_total_score = games[:away_goals].each do |key|
-    # end
-    #
-    # highest_home_score = games[:home_goals].to_i
-
+  def lowest_total_score
+    games = CSV.read @locations[:games], headers: true, header_converters: :symbol
+    scores_array = []
+    games.each do |row|
+      scores_array << row[:away_goals].to_i + row[:home_goals].to_i
+    end
+    scores_array.min
   end
 
 end
