124854a68d7d58aae94a8572fe71252970e36958
Adrian Campos <adriancampos2992@yahoo.com>
2022-01-03 17:17:41 -0800

    Feature: highest total score method and test

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,17 @@
+require 'csv'
 class Game
   def initialize(path)
-    @path = path 
+    @path = path
+    @contents = CSV.open "#{path}", headers:true, header_converters: :symbol
+
   end
 
+  def highest_total_score
+    total_scores = []
+    @contents.each do |row|
+      score =  row[:away_goals].to_i + row[:home_goals].to_i
+      total_scores << score
+    end
+    total_scores.max
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,9 +7,15 @@ RSpec.describe do Game
      game = Game.new(locations[:games])
      expect(game).to be_an_instance_of(Game)
   end
-  # it '' do
-  #
-  # end
+
+
+  it 'will give results' do
+    game_path = './data/games_stub.csv'
+    locations = {games: game_path}
+    game = Game.new(locations[:games])
+    expect(game.highest_total_score).to be(5)
+
+  end
   # it '' do
   #
   # end
