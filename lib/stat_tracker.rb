dd04990f1013e8cfca4c76a968dce19a4f630ea8
Jacob Jenner <jennerjacob@gmail.com>
2022-01-04 14:14:05 -0700

    Add: from_csv method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,4 +1,15 @@
+
 class StatTracker
-  def initialize
+  attr_reader :games, :teams, :game_teams
+
+  def initialize(locations)
+    @games = CSV.read locations[:games], headers: true, header_converters: :symbol
+    @teams = CSV.read locations[:teams], headers: true, header_converters: :symbol
+    @game_teams = CSV.read locations[:game_teams], headers: true, header_converters: :symbol
   end
+
+  def self.from_csv(locations)
+    StatTracker.new(locations)
+  end
+  
 end
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,12 +1,33 @@
-# lines 3-4 could be moved to a spec_helper.rb rile --- and then we add
-# require 'spec/spec_helper' to avoid adding these lines to every test file
-require 'simplecov'
-SimpleCov.start
+require './spec_helper'
 require './lib/stat_tracker'
+require 'csv'
 
 RSpec.describe StatTracker do
+  before(:each) do
+    @game_path = './data/games_dummy.csv'
+    @team_path = './data/teams.csv'
+    @game_teams_path = './data/game_teams_dummy.csv'
+    @locations = {
+      games: @game_path,
+      teams: @team_path,
+      game_teams: @game_teams_path
+    }
+    # @stat_tracker = StatTracker.from_csv(@locations)
+  end
+
   it 'exists' do
-    stat_tracker = StatTracker.new
+    stat_tracker = StatTracker.new(@locations)
     expect(stat_tracker).to be_instance_of StatTracker
   end
+
+  it 'loads from csv' do
+    stat_tracker = StatTracker.from_csv(@locations)
+    # require "pry"; binding.pry
+    expect(stat_tracker).to be_instance_of StatTracker
+    expect(stat_tracker.games.class).to be CSV::Table
+    expect(stat_tracker.teams.class).to be CSV::Table
+    expect(stat_tracker.game_teams.class).to be CSV::Table
+  end
+
+
 end
