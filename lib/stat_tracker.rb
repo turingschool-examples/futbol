303b6e0d5b7608fd8a23e2fbc253c104cd40fb4e
Michael Hicks <hicks.michael04@gmail.com>
2022-01-03 17:46:52 -0700

    feat: from CSV method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,2 +1,21 @@
+require 'pry'
+
 class StatTracker
+
+
+  def initialize(locations)
+    @locations = locations
+  end
+
+  def self.from_csv(locations) #add .to_a changes to an array
+    StatTracker.new(locations) #creating an instance of StatTracker holding the hash as locations
+  end
+
+
 end
+
+
+# locations.each do |location|
+#   location.key.to_s = StatTracker.new(location.value)
+#   binding.pry
+# end
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,11 +1,12 @@
 require './lib/stat_tracker'
 require 'pry'
 require 'simplecov'
-SimpleCov.start
+# SimpleCov.start
 
 RSpec.describe  StatTracker do
   it 'exists' do
     stat_tracker = StatTracker.new
+    binding.pry
     expect(stat_tracker).to be_a(StatTracker)
   end
 end
