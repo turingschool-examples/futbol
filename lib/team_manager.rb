b6ef51bc4b40df65c7091f0a2584b2eac76e4bfd
Aedan Yturralde <aedanyturralde@gmail.com>
2022-01-04 18:30:46 -0700

    refactor: refactor TeamManager class to more efficiently access csv data

--- lib/team_manager.rb
+++ lib/team_manager.rb
@@ -1,36 +1,15 @@
 require 'csv'
 
 class TeamManager
-  attr_accessor :team_id,
-                :franchise_id,
-                :team_name,
-                :abbreviation,
-                :stadium,
-                :link
+  attr_accessor :data
+  
   def initialize
-      @data = load_file
-      @hash_data = Hash.new {|h,k| h[k] = []}
-      @labeled_data = label_data
+    @data = load_file(path)
   end
 
-  def load_file()
-    data = CSV.read('./data/teams.csv')
-    headers = data.shift.map {|header| header.to_sym }
-    string_data = data.map {|row| row.map {|cell| cell.to_s} }
-  end
-
-  def label_data()
-    @data.each do |row|
-      @hash_data[:team_id] << row[0]
-      @hash_data[:franchiseid] << row[1]
-      @hash_data[:teamname] << row[2]
-      @hash_data[:abbreviation] << row[3]
-      @hash_data[:stadium] << row[4]
-      @hash_data[:link] << row[5]
+  def load_file(path)
+    CSV.read(path)[1..-1].collect do |row|
+      Team.new(row)
     end
-    return @hash_data
   end
 end
-
-team = TeamManager.new
-require "pry"; binding.pry
