83ba3629629cab1094c69437524270ee1c89d753
Joseph Galvin <jwgalvin@gmail.com>
2022-01-05 12:28:05 -0700

    Feat:average goals by season running into block.

--- lib/game_tracker.rb
+++ lib/game_tracker.rb
@@ -32,81 +32,95 @@ class GameTracker
 
     def highest_total_score
       total_scores = []
-      @contents.each do |row|
-        score =  row[:away_goals].to_i + row[:home_goals].to_i
+      @@games.each do |game|
+        # binding.pry
+        score =  game.away_goals.to_i + game.home_goals.to_i
         total_scores << score
       end
+
       total_scores.max
     end
 
 
-  #   def lowest_total_score
-  #     total_scores = []
-  #     @contents.each do |row|
-  #       score =  row[:away_goals].to_i + row[:home_goals].to_i
-  #       total_scores << score
-  #     end
-  #     total_scores.min
-  #   end
-  #
-  #   def percentage_home_wins
-  #     total_games = 0
-  #     home_wins = 0
-  #     @contents.each do |row|
-  #       total_games += 1
-  #       row[:home_goals].to_i > row[:away_goals].to_i ? home_wins += 1 : next
-  #     end
-  #     ((home_wins.to_f / total_games) * 100).round(2)
-  #   end
-  #
-  #   def percentage_vistor_wins
-  #     total_games = 0
-  #     visitor_wins = 0
-  #     @contents.each do |row|
-  #       total_games += 1
-  #       row[:home_goals].to_i < row[:away_goals].to_i ? visitor_wins += 1 : next
-  #     end
-  #     ((visitor_wins.to_f / total_games) * 100).round(2)
-  #   end
-  #
-  #   def percentage_ties
-  #     total_games = 0
-  #     ties = 0
-  #     @contents.each do |row|
-  #       total_games += 1
-  #       row[:home_goals].to_i == row[:away_goals].to_i ? ties += 1 : next
-  #     end
-  #     ((ties.to_f / total_games) * 100).round(2)
-  #   end
-  #
-  #   def count_of_games_by_season
-  #     game_count = Hash.new(0)
-  #     @contents.each do |row|
-  #       game_count[row[:season]] += 1
-  #     end
-  #     game_count
-  #   end
-  #
-  #   def average_goals_per_game
-  #     total_games = 0
-  #     total_scores = 0
-  #     @contents.each do |row|
-  #       total_games += 1
-  #       total_scores += (row[:home_goals].to_i + row[:away_goals].to_i)
-  #     end
-  #     (total_scores.to_f / total_games).round(2)
-  #   end
-  #
-  #   # def average_goals_by_season
-  #   #   # want key value pair to give total number of games, then want total number of goals. Will then return hash that has season name with the value of average goals
-  #   #   season_games = count_of_games_by_season
-  #   #   require "pry"; binding.pry
-  #   #
-  #   #     @contents.each do |row|
-  #   #       # count_of_games_by_season[pair[0]]=((row[:home_goals].to_f + row[:away_goals].to_f) / pair[1])
-  #   #     end
-  #   #   end
-  #   # end
-  #
-  # end
+    def lowest_total_score
+      total_scores = []
+      @@games.each do |game|
+        # binding.pry
+        score =  game.away_goals.to_i + game.home_goals.to_i
+        total_scores << score
+      end
+
+      total_scores.min
+    end
+
+    def percentage_home_wins
+      total_games = 0
+      home_wins = 0
+      @@games.each do |game|
+        total_games += 1
+        game.home_goals.to_i > game.away_goals.to_i ? home_wins += 1 : next
+      end
+      ((home_wins.to_f / total_games) * 100).round(2)
+    end
+
+    def percentage_vistor_wins
+      total_games = 0
+      visitor_wins = 0
+        @@games.each do |game|
+        total_games += 1
+        game.home_goals.to_i < game.away_goals.to_i ? visitor_wins += 1 : next
+      end
+      ((visitor_wins.to_f / total_games) * 100).round(2)
+    end
+
+    def percentage_ties
+      total_games = 0
+      ties = 0
+      @@games.each do |game|
+        total_games += 1
+        game.home_goals.to_i == game.away_goals.to_i ? ties += 1 : next
+      end
+      ((ties.to_f / total_games) * 100).round(2)
+    end
+
+    def count_of_games_by_season
+      game_count = Hash.new(0)
+      @@games.each do |game|
+        game_count[game.season] += 1
+      end
+      game_count
+    end
+
+    def average_goals_per_game
+      total_games = 0
+      total_scores = 0
+      @@games.each do |game|
+        total_games += 1
+        total_scores += (game.home_goals.to_i + game.away_goals.to_i)
+      end
+      (total_scores.to_f / total_games).round(2)
+    end
+
+    def average_goals_by_season
+      # games_by_season = Hash.new {|hash, key| hash[key] = 0}
+
+      season_hash = @@games.group_by do |game|
+        game.season
+      end
+      game_average = {}
+      average_goals_by_season = season_hash.each_pair do |season, games|
+        total_games = 0
+        total_scores = 0
+        games.map do |game|
+          total_games += 1
+          total_scores += (game.home_goals.to_i + game.away_goals.to_i)
+          (total_scores.to_f) #/ total_games).round(2)
+        end
+        games = total_scores / total_games
+
+        games
+      end
+      average_goals_by_season
+      # binding.pry
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,6 @@
 require './lib/game'
 require './lib/game_tracker'
+require 'pry'
 RSpec.describe do Game
   it 'exists' do
     game_path = './data/games_stub.csv'
@@ -16,50 +17,50 @@ RSpec.describe do Game
     expect(gametracker.highest_total_score).to be(5)
   end
 
-  xit 'give lowest total score' do
+  it 'give lowest total score' do
     game_path = './data/games_stub.csv'
     locations = {games: game_path}
     gametracker = GameTracker.new(game_path)
     expect(gametracker.lowest_total_score).to be(1)
   end
 
-  xit 'percentage_home_wins' do
+  it 'percentage_home_wins' do
     game_path = './data/games_stub.csv'
     locations = {games: game_path}
     gametracker = GameTracker.new(game_path)
     expect(gametracker.percentage_home_wins).to be(68.18)
   end
 
-  xit 'percentage_visitor_wins' do
+  it 'percentage_visitor_wins' do
     game_path = './data/games_stub.csv'
     locations = {games: game_path}
     gametracker = GameTracker.new(game_path)
     expect(gametracker.percentage_vistor_wins).to be(27.27)
   end
 
-  xit 'percentage_ties' do
+  it 'percentage_ties' do
     game_path = './data/games_stub.csv'
     locations = {games: game_path}
     gametracker = GameTracker.new(game_path)
     expect(gametracker.percentage_ties).to be(4.55)
   end
 
-  xit 'count_of_games_by_season' do
-    game_path = './data/games_stub.csv'
+  it 'count_of_games_by_season' do
+    game_path = './data/games_stub_2.csv'
     locations = {games: game_path}
     gametracker = GameTracker.new(game_path)
     expect(gametracker.count_of_games_by_season).to eq({'20122013' => 57, '20162017' => 4, '20142015' => 6})
   end
 
-  xit 'average_goals_per_game' do
+  it 'average_goals_per_game' do
     game_path = './data/games_stub.csv'
     locations = {games: game_path}
     gametracker = GameTracker.new(game_path)
     expect(gametracker.average_goals_per_game).to eq(3.77)
   end
 
-  xit 'average_goals_by_season' do
-    game_path = './data/games_stub.csv'
+  it 'average_goals_by_season' do
+    game_path = './data/average_goals_by_season.csv'
     locations = {games: game_path}
     gametracker = GameTracker.new(game_path)
     expect(gametracker.average_goals_by_season).to eq({'20122013' => 4.5, '20162017' => 4.75, '20142015' => 4})
