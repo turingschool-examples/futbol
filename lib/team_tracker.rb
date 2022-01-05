8530fbb10e2a40c94bea03666234c701156ae4da
Jimmy Page <jpage@knights.ucf.edu>
2022-01-05 15:50:53 -0700

    created new file paths

--- lib/team_tracker.rb
+++ lib/team_tracker.rb
@@ -10,6 +10,29 @@ class TeamTracker
     @@teams = create_teams
   end
 
+  def create_games
+    @games = []
+    contents = CSV.open './data/game_teams_stub.csv', headers:true, header_converters: :symbol
+    contents.each do |row|
+      team_id = row[:team_id]
+      game_id = row[:game_id]
+      hoa = row[:HoA]
+      result = row[:result]
+      settled_in = row[:settled_in]
+      head_coach = row[:head_coach]
+      goals = row[:goals]
+      shots = row[:shots]
+      tackles = row[:tackles]
+      pim = row[:pim]
+      powerPlayOpportunities = row[:powerPlayOpportunities]
+      powerPlayGoals = row[:powerPlayGoals]
+      faceOffWinPercentage = row[:faceOffWinPercentage]
+      giveaways = row[:giveaways]
+      takeaways = row[:takeaways]
+    end
+    @games
+  end
+
   def create_teams
     @teams = []
     contents = CSV.open './data/teams.csv', headers:true, header_converters: :symbol
