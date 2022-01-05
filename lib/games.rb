a30b35f6e05a0d1c89fedf5e1ee67ba59487f1a5
Richard LaBrecque <RichardlaBreque@Gmail.com>
2022-01-05 17:56:16 -0500

    Feat: games class reading csv

--- lib/games.rb
+++ lib/games.rb
@@ -1,12 +1,13 @@
 require 'pry'
 class Games
-  attr_reader :games_file, :readfile
+  attr_reader :games_file, :read_file
   def initialize(games_file)
     @games_file = games_file
+    @read_games = []
+    read_file
   end
 
-  def readfile
-    read_games = CSV.read @games_file, headers: true, header_converters: :symbol
-    binding.pry
+  def read_file
+    @read_games = CSV.read @games_file, headers: true, header_converters: :symbol
   end
 end
