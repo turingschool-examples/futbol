826174e02ae56310eca84eebded10161d8e4ac94
Aedan Yturralde <aedanyturralde@gmail.com>
2022-01-04 18:25:49 -0700

    feature: create Team class with attributes that correlate to headers

---
+++ lib/team.rb
@@ -0,0 +1,12 @@
+class Team
+  attr_reader :team_id,
+
+  def initialize(row)
+    @team_id = row[0]
+    @franchise_id = row[1]
+    @team_name = row[2]
+    @abbreviation = row[3]
+    @stadium = row[4]
+    @link = row[5]
+  end
+end 
