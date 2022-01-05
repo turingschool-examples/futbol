d63da6d780cf29b17f38fe1b31db11f2803a7836
Jimmy Page <jpage@knights.ucf.edu>
2022-01-05 15:06:55 -0700

    reorganized initialize method

--- lib/league_tracker.rb
+++ lib/league_tracker.rb
@@ -5,18 +5,23 @@ require './lib/team_tracker'
 require './lib/game_tracker'
 
 class LeagueTracker
-  attr_reader :teams
+  attr_reader :teams, :games
 
-  def initialize
-    tracker = TeamTracker.new
-    @teams = tracker.teams
+  def initialize(path)
+    team_tracker = TeamTracker.new
+    game_tracker = GameTracker.new(path)
+    @teams = team_tracker.teams
+    @games = game_tracker.games
   end
 
   def count_of_teams
-    @teams.uniq.count 
+    @teams.uniq.count
+  end
+
+  def best_offense
+
   end
 
 
 
 end
-tracker = LeagueTracker.new
