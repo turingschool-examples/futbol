bb9244122927b59dece59bd2fe548ddf38c32b53
Sam Iavri <sammi80134@gmail.com>
2022-01-05 17:48:14 -0700

    Added in Highest and lowest total score with specs

--- spec/game_stat_spec.rb
+++ spec/game_stat_spec.rb
@@ -1,20 +1,27 @@
 require_relative 'spec_helper'
-require './lib/game_child'
-require './lib/futbol_data'
-require './lib/stat_tracker'
+require 'ostruct'
+require './lib/game_statistics'
 
-RSpec.describe StatTracker do
+RSpec.describe GameStatistics do
   before(:each) do
-    @game_path = './data/games.csv'
-    @team_path = './data/teams.csv'
-    @game_teams_path = './data/game_teams.csv'
+    mock_game_1 = OpenStruct.new({ home_score: 1, away_score: 3, total_score: 4 })
+    mock_game_2 = OpenStruct.new({ home_score: 2, away_score: 6, total_score: 8 })
+    mock_game_manager = OpenStruct.new({ data: [mock_game_1, mock_game_2] })
+    @game_statistics = GameStatistics.new(mock_game_manager)
+  end
+  describe '#highest_total_score' do
+    it 'has a highest winning score' do
+      actual = @game_statistics.highest_total_score
+      expected = 8
+      expect(actual).to eq(expected)
+    end
+  end
 
-    @locations = {
-      games: @game_path,
-      teams: @team_path,
-      game_teams: @game_teams_path
-    }
-    @stat_tracker = StatTracker.from_csv(@locations)
-    @game_statistics = GameStatistics.new(@locations)
+  describe '#lowest_total_score' do
+    it 'has a lowest score' do
+      actual = @game_statistics.lowest_total_score
+      expected = 4
+      expect(actual).to eq(expected)
+    end
   end
 end
