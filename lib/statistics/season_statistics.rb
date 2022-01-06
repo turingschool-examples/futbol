b22e0484765936af8a946fcff07d2ad7532fa679
Michael Brandt <msbrandt00@gmail.com>
2022-01-06 15:16:38 -0700

    Feature: #coaches_by_win_percentage, #winningest_coach, #worst_coach and accompanying testing

--- lib/statistics/season_statistics.rb
+++ lib/statistics/season_statistics.rb
@@ -1,4 +1,4 @@
-require './lib/managers/game_team_manager'
+require './lib/managers/game_teams_manager'
 require 'pry'
 # THIS IS MICHAEL'S
 # Season Statistics
@@ -7,12 +7,12 @@ class SeasonStatistics
   def initialize(game_team_manager)
     @gtmd = game_team_manager # accronym for gameteammanagerdata
   end
-  def winningest_coach(season_id) #best win percentage for the season)
-    #season_coaches(season_id).max_by {|coach| coaches_by_win_percentage(season_id, coach) }
+  def winningest_coach(season_id)
+    season_coaches(season_id).max_by {|coach| coaches_by_win_percentage(season_id, coach) }
   end
 
-  def worst_coach
-    # Name of the Coach with the worst win percentage for the season (String)
+  def worst_coach(season_id)
+    season_coaches(season_id).min_by {|coach| coaches_by_win_percentage(season_id, coach) }
 
   end
 
@@ -38,10 +38,22 @@ class SeasonStatistics
     game_teams_data_by_season(season_id).map { |game| game.head_coach }.uniq
   end
 
-  def game_teams_data_by_season(season_id) # currently requires a string do we want to change to integers?
+  def game_teams_data_by_season(season_id)
     @gtmd.data.find_all {|game| game.game_id[0..3] == season_id[0..3]}
   end
+
+  def coaches_by_win_percentage(season_id, coach)
+    won_games_by_coach = []
+    count = 0
+    game_teams_data_by_season(season_id).each do |game|
+      won_games_by_coach << game if game.head_coach == coach && game.result == 'WIN'
+      count += 1 if game.head_coach == coach
+    end
+    ((won_games_by_coach.count.to_f / count) * 100).round(2)
+  end
+
+
 end
 
 a = SeasonStatistics.new($game_team_manager_data)
-binding.pry
+# binding.pry
