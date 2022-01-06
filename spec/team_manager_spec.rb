752bd880f8522d458e3088716b9e386732185a43
Aedan Yturralde <aedanyturralde@gmail.com>
2022-01-06 11:34:05 -0700

    refactor: minor changes to specs

--- spec/team_manager_spec.rb
+++ spec/team_manager_spec.rb
@@ -1,13 +1,21 @@
 require 'RSpec'
+require 'ostruct'
 require './lib/team_manager'
 
 RSpec.describe TeamManager do
-  before(:each) do
-    @manager = TeamManager.new("./data/teams.csv")
-  end
+  # before(:each) do
+  #
+  # end
 
   it "exists" do
     expect(@manager).to be_a(TeamManager)
   end
+
+  it "reads CSV data and returns each row as a team object " do
+    require "pry"; binding.pry
+    expect(@manager.data).to include()
+  end
+
+
 
 end
