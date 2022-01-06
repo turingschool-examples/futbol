20a42d23e18a1d7f8b55da5b99cca048187f818f
Jimmy Page <jpage@knights.ucf.edu>
2022-01-06 14:55:37 -0700

    refactored initialize

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,5 @@
 require 'csv'
+
 class Game
   attr_reader :game_id,
               :type,
@@ -10,17 +11,18 @@ class Game
               :venue,
               :venue_link,
               :season
-  def initialize(game_id,type,date_time,away_team_id,home_team_id,away_goals,home_goals,venue,venue_link,season)
-    @game_id = game_id
-    @type = type
-    @date_time = date_time
-    @away_team_id = away_team_id
-    @home_team_id = home_team_id
-    @away_goals = away_goals
-    @home_goals = home_goals
-    @venue = venue
-    @venue_link = venue
-    @season = season
+
+  def initialize(row)
+    @game_id = row[:game_id]
+    @type = row[:type]
+    @date_time = row[:date_time]
+    @away_team_id = row[:away_team_id]
+    @home_team_id = row[:home_team_id]
+    @away_goals = row[:away_goals]
+    @home_goals = row[:home_goals]
+    @venue = row[:venue]
+    @venue_link = row[:venue]
+    @season = row[:season]
   end
 
 end
