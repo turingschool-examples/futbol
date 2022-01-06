a6f3d969a4e79562dbbbbd59bd6c278430227367
benistta <92293363+benistta@users.noreply.github.com>
2022-01-06 15:24:58 -0700

    Feat: stattracker class with Team method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,15 +1,25 @@
+require 'csv'
 require 'pry'
 
 class StatTracker
+  # include LeagueStatistics[@teams, @games, @game_teams]
   attr_reader :game_path, :team_path, :game_teams_path
 
   def initialize(files)
     @game_path = files[:games]
     @team_path = files[:teams]
     @game_teams_path = files[:game_teams]
+    @teams = extract_team_data(@team_path)
   end
 
   def self.from_csv(files)
     stats = StatTracker.new(files)
+  end
+
+  def extract_team_data(team_path)
+    data = CSV.read(team_path, :headers => true)
+    data.each do |row|
+      Team.new(row["team_id"])
+    end
   end
 end
