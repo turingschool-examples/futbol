07634042c114d723cabb27d3b78baee39e6df99d
Ian Ross <ianrossdrums@gmail.com>
2022-01-06 12:50:01 -0700

    feat: League stats structure

---
+++ lib/league_statistics.rb
@@ -0,0 +1,34 @@
+class LeagueStatistics
+
+  def initialize(argument)
+    @argument = argument
+  end
+
+  def count_of_teams #total number of teams in data
+
+  end
+
+  def best_offense #team with highest average goals per game for all seasons
+
+  end
+
+  def worst_offense #team with lowext average goals per game for all seasons
+
+  end
+
+  def highest_scoring_visitor #team with highest average score per game for all seasons when away
+
+  end
+
+  def highest_scoring_home_team #team with highest average score per game for all seasons when home
+
+  end
+
+  def lowest_scoring_visitor #team with lowest average score per game for all seasons when away
+
+  end
+
+  def lowest_scoring_home_team #team with lowest average score per game for all seasons when home
+
+  end
+end
