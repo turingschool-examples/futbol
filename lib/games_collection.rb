14b3554daf48c8439c4a158cf5d33f7f0c1c5e8d
Richard LaBrecque <RichardlaBreque@Gmail.com>
2022-01-06 17:43:45 -0500

    abandonship

---
+++ lib/games_collection.rb
@@ -0,0 +1,14 @@
+require 'pry'
+class GamesCollection
+  attr_reader :games
+  def initialize(games_file)
+    @games = read_file(games_file)
+  end
+
+  def read_file(games_file)
+    data = CSV.read(games_file, headers: true, header_converters: :symbol)
+    data.map do |row|
+      Game.new(row)
+    end
+  end
+end
