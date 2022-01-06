9a86c215328a1a1eb93a83745149690e0da82dc6
Greg Flaherty <gregoryjflaherty@gmaul.com>
2022-01-06 18:44:23 -0500

    Refactor: make class adhere to inheritence of statistics class

--- lib/game_team.rb
+++ lib/game_team.rb
@@ -1,22 +1,21 @@
 class GameTeam
-  
   attr_reader :team_id, :game_id, :hoa, :result, :settled_in, :head_coach, :goals, :shots, :tackles, :pim, :powerPlayOpportunities, :powerPlayGoals, :faceOffWinPercentage, :giveaways, :takeaways
 
-  def initialize(team_id, game_id, hoa, result, settled_in, head_coach, goals, shots, tackles, pim, powerPlayOpportunities, powerPlayGoals, faceOffWinPercentage, giveaways, takeaways)
-    @team_id = team_id
-    @game_id = game_id
-    @hoa = hoa
-    @result = result
-    @settled_in = settled_in
-    @head_coach = head_coach
-    @goals = goals
-    @shots = shots
-    @tackles = tackles
-    @pim = pim
-    @powerPlayOpportunities = powerPlayOpportunities
-    @powerPlayGoals = powerPlayGoals
-    @faceOffWinPercentage = faceOffWinPercentage
-    @giveaways = giveaways
-    @takeaways = takeaways
+  def initialize(row)
+    @team_id = row[:team_id]
+    @game_id = row[:game_id]
+    @hoa = row[:hoa]
+    @result = row[:result]
+    @settled_in = row[:settled_in]
+    @head_coach = row[:head_coach]
+    @goals = row[:goals]
+    @shots = row[:shots]
+    @tackles = row[:tackles]
+    @pim = row[:pim]
+    @powerPlayOpportunities = row[:powerPlayOpportunities]
+    @powerPlayGoals = row[:powerPlayGoals]
+    @faceOffWinPercentage = row[:faceOffWinPercentage]
+    @giveaways = row[:giveaways]
+    @takeaways = row[:takeaways]
   end
 end
