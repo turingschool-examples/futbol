4fd01f1203b361eb6515e3d833f2458ad7100293
Jimmy Page <jpage@knights.ucf.edu>
2022-01-06 14:58:06 -0700

    changed initialize method

---
+++ lib/statistics.rb
@@ -0,0 +1,17 @@
+require 'csv'
+require_relative 'game'
+require_relative 'team'
+require_relative 'game_team'
+
+class Statistics
+  attr_reader :games, :teams, :game_teams
+
+  def initialize(locations)
+    @games = CSV.read(locations[:games], headers: true, header_converters: :symbol).map {|row| Game.new(row)}
+    # @teams = CSV.read(locations[:teams], headers: true, header_converters: :symbol).map {|row| Team.new(row)}
+    # @game_teams = CSV.read(locations[:game_teams], headers: true, header_converters: :symbol).map {|row| GameTeam.new(row)}
+  end
+
+
+
+end
