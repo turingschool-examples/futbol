250a398a1c363e8289ee5f4327081151cf653aa4
Aedan Yturralde <aedanyturralde@gmail.com>
2022-01-06 19:24:11 -0700

    create tests that will be used in another file that can access game_team_manager

--- spec/statistics/team_statistics_spec.rb
+++ spec/statistics/team_statistics_spec.rb
@@ -1,6 +1,10 @@
+# require 'spec_helper'
+require 'simplecov'
 require 'RSpec'
 require 'ostruct'
-require './lib/team_statistics'
+require './lib/statistics/team_statistics'
+require './lib/statistics/season_statistics.rb'
+# require './lib/statistics/game_teams_manager.rb'
 
 RSpec.describe TeamStatistics do
   before(:each) do
@@ -26,6 +30,34 @@ RSpec.describe TeamStatistics do
       # team_info method currently returns with rocket syntax. problem?
       expected = {:team_id => 1, :franchise_id => 23, :team_name => "Atlanta United", :abbreviation => "ATL", :link => "/api/v1/teams/1" }
       # expected = {team_id: 1, franchise_id: 23, team_name: "Atlanta United", abbreviation: "ATL", link: "/api/v1/teams/1" }
+      expect(actual).to eq(expected)
+    end
+  end
+end
+# placeholder name TeamGame doesn't exist yet
+# This is for the #win_percentage. Needs game_team_manager
+RSpec.describe TeamGame do
+  before(:each) do
+    mock_game_team_1 = OpenStruct.new({game_id: 2012030221, team_id: 3, HOA: "away", result: "LOSS",
+       settled_in: "OT", head_coach: "John Tortorella" })
+    mock_game_team_2 = OpenStruct.new({game_id: 2012030222, team_id: 3, HOA: "home", result: "WIN",
+       settled_in: "REG", head_coach: "John Tortorella" })
+    mock_game_team_3 = OpenStruct.new({game_id: 2012030221, team_id: 4, HOA: "away", result: "LOSS",
+       settled_in: "OT", head_coach: "John Tortorella" })
+    mock_game_team_manager = OpenStruct.new({ data: [mock_team_1, mock_team_2, mock_team_3] })
+    @game_team_statistics = SeasonStatistics.new(mock_game_team_manager)
+  end
+
+  describe 'exists' do
+    it "exists" do
+      expect(@game_team_statistics).to be_a(TeamGame)
+    end
+  end
+
+  describe '#win_percentage' do
+    it 'can return the win percentage of all games for a given team' do
+      actual = @game_team_statistics.win_percentage(3)
+      expected = 50
       expect(actual).to eq(expected)
     end
   end
