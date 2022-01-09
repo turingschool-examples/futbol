fcc97355c3ea3491bd4126e1e0dccdf8ba1cf0d7
Greg Flaherty <gregoryjflaherty@gmaul.com>
2022-01-08 20:04:40 -0500

    Feat: create rival method and tests- all tests passing

--- lib/team_tracker.rb
+++ lib/team_tracker.rb
@@ -134,4 +134,25 @@ class TeamTracker < Statistics
     end
     find_name_by_ID(most_win_hash.key(most_win_hash.values.max))[0].teamname
   end
+
+  def rival(team_id) ###REFACTOR VARIABLE NAMES 
+    all_games = @games.find_all {|game| game.home_team_id == team_id || game.away_team_id == team_id }
+    opponent_lost_hash = Hash.new { |hash, key| hash[key] = [] }
+    total_opponent_games = Hash.new { |hash, key| hash[key] = 0 }
+    all_games.each do |game|
+      opponent_lost_hash[game.away_team_id] << game if (game.home_team_id == team_id) && (game.home_goals > game.away_goals)
+      opponent_lost_hash[game.home_team_id] << game if (game.away_team_id == team_id) && (game.home_goals < game.away_goals)
+      total_opponent_games[game.home_team_id] += 1 if (game.away_team_id == team_id)
+      total_opponent_games[game.away_team_id] += 1 if (game.home_team_id == team_id)
+    end
+    most_win_hash = Hash.new
+    opponent_lost_hash.each_pair do |lost_opponent, won_games|
+      total_opponent_games.each_pair do |any_opponent, all_games|
+        if lost_opponent == any_opponent
+          most_win_hash[lost_opponent] = won_games.length / all_games.to_f
+        end
+      end
+    end
+    find_name_by_ID(most_win_hash.key(most_win_hash.values.min))[0].teamname
+  end
 end
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -93,6 +93,13 @@ RSpec.describe do TeamTracker
     expect(team_tracker.favorite_opponent("18")).to eq("DC United")
   end
 
-
-
+  it 'rival' do
+    game_path = './data/game_teams.csv'
+    locations = {
+      games: './data/games.csv',
+      teams: './data/teams.csv',
+      game_teams: game_path}
+    team_tracker = TeamTracker.new(locations)
+    expect(team_tracker.rival("18")).to eq("Houston Dash").or(eq("LA Galaxy"))
+  end
 end
