85db7889f3497d3561f81e9808508fef45da36d8
oscar santos <oscar303data@gmail.com>
2022-01-08 17:34:47 -0700

    Feat: best_offense block is created

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -36,20 +36,24 @@ RSpec.describe StatTracker do
     expect(@stat_tracker.best_offense).to eq("Reign FC")
   end
 
-  it '#worst_offense can find the team with lowest average number of goals scored' do
+  xit '#worst_offense can find the team with lowest average number of goals scored' do
     expect(@stat_tracker.worst_offense).to eq("Utah Royals FC")
   end
 
-  it '#highest_scoring_visitor can find the team with highest average score per game' do
+  xit '#highest_scoring_visitor can find the team with highest average score(away)' do
     expect(@stat_tracker.highest_scoring_visitor).to eq("FC Dallas")
   end
 
-  it '#highest_scoring_home_team can find the team with highest average score per game' do
+  xit '#highest_scoring_home_team can find the team  with highest average score(home) ' do
     expect(@stat_tracker.highest_scoring_home_team).to eq("Reign FC")
   end
 
-  it '#lowest_scoring_visitor can find the team with highest average score per game' do
+  xit '#lowest_scoring_visitor can find the team with lowest average score(visitor)' do
     expect(@stat_tracker.lowest_scoring_visitor).to eq("San Jose Earthquakes")
+  end
+
+  xit '#lowest_scoring_home_team can find the team with lowest average score(home)' do
+    expect(@stat_tracker.lowest_scoring_home_team).to eq("Utah Royals FC")
   end
 
 
