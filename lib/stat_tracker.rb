15086266ee02a1821bf6e3ed29c021c4b3d220dd
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-01-10 13:03:29 -0500

    Feat: Feat create stat_tracker class and initialize methods

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,7 +1,19 @@
-require_relative './helper_class'
+require 'csv'
+require_relative './statistics'
+require_relative './game_tracker'
+require_relative './team_tracker'
+require_relative './season_tracker'
+require_relative './game_team_tracker'
 
 class StatTracker
-  def initialize
-    @argument = argument
+  def self.from_csv(locations)
+    StatTracker.new(locations)
+  end
+
+  def initialize(locations)
+    @game_tracker = GameTracker.new(locations)
+    @team_tracker = TeamTracker.new(locations)
+    @season_tracker = SeasonTracker.new(locations)
+    @game_team_tracker = GameTeamTracker.new(locations)
   end
 end
