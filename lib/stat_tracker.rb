990c54d488c3572227d7f1a9a60e6fed90571f57
Richard LaBrecque <RichardlaBreque@Gmail.com>
2022-01-09 20:01:37 -0500

    feat: make space for season stat methods

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -289,4 +289,22 @@ class StatTracker
     end
     average_goals_by_season
   end
+
+  def winningest_coach (season_id)
+  end
+
+  def worst_coach (season_id)
+  end
+
+  def most_accurate_team (season_id)
+  end
+
+  def least_accurate_team (season_id)
+  end
+  
+  def most_tackles (season_id)
+  end
+
+  def fewest_tackles (season_id)
+  end
 end
