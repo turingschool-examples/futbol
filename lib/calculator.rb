19f413c5afc2fdb236dc049b05499e9e39c574a6
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-01-11 23:52:59 -0500

    Refactor: Add feat to average method - can take integers as argument

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,5 +1,9 @@
 module Calculator
-  def average(result)
-    (result.to_f / @games.length).round(2)
+  def average(result, data)
+    if data.class == Integer
+      (result.to_f / data).round(2)
+    else
+      (result.to_f / data.length).round(2)
+    end
   end
 end
