2f63356490dcc2694f9ed80c1b19072f6bdce3d2
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-01-12 02:02:03 -0500

    Refactor: consolidate average_win_percentage method

--- lib/team_tracker.rb
+++ lib/team_tracker.rb
@@ -27,31 +27,14 @@ class TeamTracker < Statistics
   end
 
   def average_win_percentage(team_id)
-    all_games = @games.find_all do |game|
-      game.home_team_id == team_id || game.away_team_id == team_id
+    games_by_team = @game_teams.find_all {|game| game.team_id == team_id}
+    total_wins = games_by_team.count do |game|
+      game.result == "WIN"
     end
-    games_by_season = all_games.group_by do |game|
-      game.season
-    end
-    win_hash = Hash.new { |hash, key| hash[key] = [] }
-    games_by_season.each_pair do |season, games|
-      games.each do |game|
-        @game_teams.find_all do |game_2|
-          win_hash[season] << game_2 if game.game_id == game_2.game_id && game_2.team_id == team_id
-        end
-      end
-    end
-    hash = Hash.new
-    win_hash.each_pair do |season, games|
-      wins = games.count do |game|
-        game.result == "WIN"
-      end
-      hash[season] = (wins.to_f / win_hash[season].length).round(2)
-    end
-    ((hash.values.sum) / hash.values.length).round(2)
+    average(total_wins, games_by_team)
   end
 
-  def most_goals_scored(team_id)
+  def most_goals_scored(team_id) ###SOMEONE ELSE REFACTOR
     team_goal_max = @game_teams.find_all do |game|
       game.team_id == team_id
     end
@@ -61,7 +44,7 @@ class TeamTracker < Statistics
     game.goals
   end
 
-  def fewest_goals_scored(team_id)
+  def fewest_goals_scored(team_id) ###SOMEONE ELSE REFACTOR
     team_goal_min = @game_teams.find_all do |game|
       game.team_id == team_id
     end
