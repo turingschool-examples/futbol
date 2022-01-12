4b73285eff983235ae42a983c1df0c4508ef842b
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-01-12 01:45:39 -0500

    Refactor: refactor best/worst offense methods

--- lib/game_team_tracker.rb
+++ lib/game_team_tracker.rb
@@ -1,41 +1,30 @@
 class GameTeamTracker < Statistics
+  # include DataCollector
   def count_of_teams
     unique = @game_teams.map {|game|game.team_id}
     unique.uniq.count
   end
 
-  def best_offense # look at breaking into different methods
+  def best_offense
     best_team = {}
-    max = 0
     sorted = @game_teams.group_by {|game| game.team_id}
     key_hash = {}
-    #goal_average_calculator(team_hash)
     sorted.map do |key, sorted|
       goals = sorted.sum {|game| game.goals}
-      total_games = sorted.count
-      average = goals.to_f / total_games
-      key_hash[key] = average
-      key_hash = key_hash.to_h
-      max = key_hash.values.max
-      best_team = key_hash.select {|k,v| v == max}
+      key_hash[key] = goals.to_f / sorted.count
+      best_team = key_hash.select {|k,v| v == key_hash.values.max}
     end
     find_name_by_ID(best_team.keys[0])[0].team_name
   end
 
-  def worst_offense # look at breaking into different methods
+  def worst_offense
     worst_team = {}
-    min = 0
     sorted = @game_teams.group_by {|game| game.team_id}
     key_hash = {}
-    #goal_average_calculator(team_hash)
     sorted.map do |key, sorted|
       goals = sorted.sum {|game| game.goals}
-      total_games = sorted.count
-      average = goals.to_f / total_games
-      key_hash[key] = average
-      key_hash = key_hash.to_h
-      min = key_hash.values.min
-      worst_team = key_hash.select {|k,v| v == min}
+      key_hash[key] = goals.to_f / sorted.count
+      worst_team = key_hash.select {|k,v| v == key_hash.values.min}
     end
     find_name_by_ID(worst_team.keys[0])[0].team_name
   end
