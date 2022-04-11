bae08ad9ec50ee4860aebafa79c476b5f6f1bfa9
Zach Prince <zachary.s.prince@outlook.com>
2022-04-11 19:03:51 -0400

    Add season methods

---
+++ modules/season.rb
@@ -0,0 +1,17 @@
+require './lib/game'
+require './lib/game_teams'
+require './lib/team_stats'
+
+module Season
+  def winningest_coach; end
+
+  def worst_coach; end
+
+  def most_accurate_team; end
+
+  def least_accurate_team; end
+
+  def most_tackles; end
+
+  def fewest_tackles; end
+end
