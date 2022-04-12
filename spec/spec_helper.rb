5a48065102d721a5ab26e57c41310621e886900a
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-04-12 14:15:14 -0600

    Refactor: simplecov addition to futbol directory

---
+++ spec/spec_helper.rb
@@ -0,0 +1,8 @@
+require 'simplecov'
+SimpleCov.start 
+
+require_relative 'stat_tracker_spec'
+
+#makes a helper file to pull in existing spec file and now simplecov lives here
+
+#spec/spec_helper - will run tests in required order in this file -> require them in this file to get the return in one big percentage
