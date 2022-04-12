d683f6d61b8d92e2255c91ad170afb61a7996903
“Brad <hutch13531@gmail.com>
2022-04-12 15:08:49 -0400

    Feat: hightest_scoring_visitor

--- modules/league_statistics.rb
+++ modules/league_statistics.rb
@@ -23,7 +23,15 @@ module LeagueStats
     game_teams.each do |team_id, goals|
       scores << goals.sum / goals.length if team_id == team_id
     end
-    scores.[0]
+    scores[0]
+  end
+
+  def highest_scoring_visitor
+    game_teams = GameTeams.create_list_of_games(@game_teams)
+    scores = []
+      scores << goals.sum / goals.length if team_id == team_id && if hoa == hoa
+    end
+    scores.sort[-1]
   end
 
 
