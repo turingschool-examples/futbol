59cf39464bb732985f34a182aae1acdb9ff89cea
Paker Thomson <pdthomson1239@gmail.com>
2022-04-12 14:58:14 -0400

    feat/test:added the first 4 methods and the tests to go with them

--- modules/game_statistics.rb
+++ modules/game_statistics.rb
@@ -1,8 +1,27 @@
 require './lib/game'
+require './lib/game_teams'
 
 module GameStats
   def highest_total_score
     games = Game.create_list_of_games(@games)
     games.map { |game| game.away_goals + game.home_goals }.sort[-1]
   end
+# require 'pry' ;binding pry
+  def lowest_total_score
+    games = Game.create_list_of_games(@games)
+    games.map { |game| game.away_goals + game.home_goals }.sort[0]
+  end
+
+  def percentage_home_wins
+    games = Game.create_list_of_games(@games)
+    ((games.count{ |game| game.home_goals > game.away_goals}) / games.length.to_f).round(2)
+  end
+
+
+  def percentage_visitor_wins
+    games = Game.create_list_of_games(@games)
+    ((games.count{ |game| game.home_goals < game.away_goals}) / games.length.to_f).round(2)
+  end
+
+
 end
---
+++ spec/game_statistics_spec.rb
@@ -0,0 +1,38 @@
+require './lib/game'
+require './lib/game_teams'
+require './modules/game_statistics'
+require './lib/stat_tracker'
+require 'rspec'
+
+describe GameStats do
+  before(:all) do
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    @stat_tracker = StatTracker.from_csv(locations)
+  end
+
+  it "#lowest_total_score" do
+      expect(@stat_tracker.lowest_total_score).to eq 0
+    end
+
+  it "#percentage_home_wins" do
+    expect(@stat_tracker.percentage_home_wins).to eq 0.44
+  end
+
+  it "sees the home win percentage" do
+    expect(@stat_tracker.percentage_home_wins).to eq 0.44
+  end
+
+  it "#percentage_visitor_wins" do
+    expect(@stat_tracker.percentage_visitor_wins).to eq 0.36
+  end
+
+end
