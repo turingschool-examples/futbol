260d27e631edab1a61487f8e2fef349d2250321d
“Brad <hutch13531@gmail.com>
2022-04-12 14:59:15 -0400

    Feat: worst_offese

--- modules/league_statistics.rb
+++ modules/league_statistics.rb
@@ -1,4 +1,5 @@
 require './lib/game_teams'
+require './lib/team_stats'
 
 module LeagueStats
 
@@ -6,5 +7,26 @@ module LeagueStats
     teams = TeamStats.create_list_of_teams(@teams)
     teams.map { |team| team.length}
   end
+
+  def best_offense
+    game_teams = GameTeams.create_list_of_games(@game_teams)
+    scores = []
+    game_teams.each do |team_id, goals|
+      scores << goals.sum / goals.length if team_id == team_id
+    end
+    scores.sort[-1]
+  end
+
+  def worst_offense
+    game_teams = GameTeams.create_list_of_games(@game_teams)
+    scores = []
+    game_teams.each do |team_id, goals|
+      scores << goals.sum / goals.length if team_id == team_id
+    end
+    scores.[0]
+  end
+
+
+
 
 end
