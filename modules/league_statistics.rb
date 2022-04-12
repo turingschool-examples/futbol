8f984c0c2d0d3b6a8e71e0f4e7426022f4ff6ab7
“Brad <hutch13531@gmail.com>
2022-04-12 13:03:28 -0400

    Module: Created LeagueStats

--- modules/league_statistics.rb
+++ modules/league_statistics.rb
@@ -1,9 +1,5 @@
-require './lib/team_stats'
 require './lib/game_teams'
 
 module LeagueStats
-  def count_of_teams
-    teams = TeamStats.create_list_of_teams(@teams)
-    teams.map {|team| team.length}
-  end
+
 end
