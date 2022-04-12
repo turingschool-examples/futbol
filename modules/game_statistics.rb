e9a6a399c564216ac7d91f39bc8c39a46f5bbcea
Paker Thomson <pdthomson1239@gmail.com>
2022-04-12 16:33:28 -0400

    'feat/test: add tests and method for all of game_statistics'

--- modules/game_statistics.rb
+++ modules/game_statistics.rb
@@ -17,7 +17,6 @@ module GameStats
     ((games.count{ |game| game.home_goals > game.away_goals}) / games.length.to_f).round(2)
   end
 
-
   def percentage_visitor_wins
     games = Game.create_list_of_games(@games)
     ((games.count{ |game| game.home_goals < game.away_goals}) / games.length.to_f).round(2)
@@ -29,8 +28,39 @@ module GameStats
   end
 
   def count_of_games_by_season
-
+    games_by_season = {}
+    games = Game.create_list_of_games(@games)
+    games.each do |game|
+      if games_by_season[game.season].nil?
+        games_by_season[game.season] = 1
+      else
+        games_by_season[game.season] += 1
+      end
+    end
+      games_by_season
   end
 
-  
+  def average_goals_per_game
+    games = Game.create_list_of_games(@games)
+    (games.flat_map{ |game| game.home_goals + game.away_goals}.sum / games.length.to_f).round(2)
+  end
+
+  def average_goals_by_season
+    games = Game.create_list_of_games(@games)
+    games_in_season = count_of_games_by_season
+    goals_in_season = {}
+    games.each do |game|
+      if goals_in_season[game.season].nil?
+        goals_in_season[game.season] = (game.home_goals + game.away_goals)
+      else
+        goals_in_season[game.season] += (game.home_goals + game.away_goals)
+      end
+    end
+    average_goals = {}
+    goals_in_season.each do |season, goals|
+      average_goals[season] = (goals / games_in_season[season].to_f).round(2)
+    end
+    average_goals
+  end
+
 end
--- spec/game_statistics_spec.rb
+++ spec/game_statistics_spec.rb
@@ -39,4 +39,47 @@ describe GameStats do
       expect(@stat_tracker.percentage_ties).to eq 0.20
   end
 
+  it "#count_of_games_by_season" do
+    # expected = {
+    #   "20122013"=>806,
+    #   "20162017"=>1317,
+    #   "20142015"=>1319,
+    #   "20152016"=>1321,
+    #   "20132014"=>1323,
+    #   "20172018"=>1355
+    # }
+    expect(@stat_tracker.count_of_games_by_season).to eq({
+      "20122013"=>806,
+      "20162017"=>1317,
+      "20142015"=>1319,
+      "20152016"=>1321,
+      "20132014"=>1323,
+      "20172018"=>1355
+    })
+  end
+
+  it "#average_goals_per_game" do
+    expect(@stat_tracker.average_goals_per_game).to eq 4.22
+  end
+
+
+  it "#average_goals_by_season" do
+    # expected = {
+    #   "20122013"=>4.12,
+    #   "20162017"=>4.23,
+    #   "20142015"=>4.14,
+    #   "20152016"=>4.16,
+    #   "20132014"=>4.19,
+    #   "20172018"=>4.44
+    # }
+    expect(@stat_tracker.average_goals_by_season).to eq ({
+      "20122013"=>4.12,
+      "20162017"=>4.23,
+      "20142015"=>4.14,
+      "20152016"=>4.16,
+      "20132014"=>4.19,
+      "20172018"=>4.44
+  })
+  end
+
 end
