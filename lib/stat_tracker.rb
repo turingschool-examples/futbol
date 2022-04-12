27759f94dabec914f2db4144407a53404843533b
Rue Zheng <99001315+ruezheng@users.noreply.github.com>
2022-04-12 15:02:17 -0600

    Feat: initialize attributes in Team class and test objects exist in team_spec.rb
    add create_teams method in stat_tracker.rb and tests we can return an index value in an instance of StatTracker

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,12 +1,33 @@
 require 'csv'
-class StatTracker
+require './lib/team'
 
-  def self.from_csv(locations)
-    games = CSV.open(locations[:games], headers: true, header_converters: :symbol)
-    teams = CSV.open(locations[:teams], headers: true, header_converters: :symbol)
-    game_teams = CSV.open(locations[:game_teams], headers: true, header_converters: :symbol)
-    stat_tracker1 = self.new
-    # require "pry";binding.pry
-  end
-  
+class StatTracker
+	attr_reader :games, :teams, :game_teams
+
+	def initialize(games, teams, game_teams)
+		@games = games
+		@teams = create_teams(teams)
+		@game_teams = game_teams
+	end
+
+	def self.from_csv(locations)
+	 games = CSV.open(locations[:games], headers: true, header_converters: :symbol)
+	 teams = CSV.open(locations[:teams], headers: true, header_converters: :symbol)
+	 game_teams = CSV.open(locations[:game_teams], headers: true, header_converters: :symbol)
+	 stat_tracker1 = self.new(games, teams, game_teams)
+	end
+
+	def create_teams(teams)
+		team_arr = Array.new
+		teams.each do |row|
+			team_id = row[:team_id]
+			franchise_id = row[:franchise_id]
+			team_name = row[:team_name]
+			abbreviation = row[:abbreviation]
+			stadium = row[:stadium] # do symbols always return all lowercase or the same case as we assign it???
+			link = row[:link]
+			team_arr << Team.new(team_id, franchise_id, team_name, abbreviation, stadium, link)
+		end
+		return team_arr
+	end
 end
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,6 +1,8 @@
 require 'simplecov'
 SimpleCov.start
 require './lib/stat_tracker'
+require './lib/team'
+
 
 describe StatTracker do
   before :each do
@@ -15,8 +17,13 @@ describe StatTracker do
     }
     @stat_tracker = StatTracker.from_csv(@locations)
   end
-  it 'exists' do
 
-  expect(@stat_tracker).to be_an_instance_of(StatTracker)
+  it 'exists' do
+	  expect(@stat_tracker).to be_an_instance_of(StatTracker)
   end
+
+	it "has a team" do
+		@team1 = Team.new('1', '23', 'Atlanta United', 'ATL', 'Mercedes-Benz Stadium', 'link')
+		expect(@stat_tracker.team[0]).to eq '1'
+	end
 end
