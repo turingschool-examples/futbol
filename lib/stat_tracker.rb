dd26fbf3fb0e732b0a4c5c9cbc0d01043bdc824d
Caden Jarrett <caden.jarrett99@gmail.com>
2022-04-12 12:10:01 -0600

    WIP: config run

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,9 +1,30 @@
 require 'pry'
+require 'csv'
 
 class StatTracker
 
-  attr_reader :name
-  def initialize(name)
-    @name = name
+  attr_reader :game_statistics, :stat_tracker
+  def initialize(stat_tracker)
+    @stat_tracker = stat_tracker
   end
+
+  def self.from_csv(locations)
+    info = []
+    locations.each do |key, path|
+      data = CSV.open path, headers: true, header_converters: :symbol
+      info << data
+    end
+    return info
+  end
+
+  def highest_total_score
+    total = 0
+    @game_statistics.each do |row|
+      if total < row[:home_goals].to_i + row[:away_goals].to_i
+        total = row[:home_goals].to_i + row[:away_goals].to_i
+      end
+    end
+    return total
+  end
+
 end
--- runner.rb
+++ runner.rb
@@ -4,7 +4,7 @@ require './lib/stat_tracker'
 
 game_path = './data/games.csv'
 team_path = './data/teams.csv'
-game_teams_path = './data.game_teams.csv'
+game_teams_path = './data/game_teams.csv'
 
 locations = {
   games: game_path,
@@ -13,5 +13,5 @@ locations = {
 }
 
 stat_tracker = StatTracker.from_csv(locations)
-
+# p StatTracker.from_csv(locations)
 require 'pry'; binding.pry
