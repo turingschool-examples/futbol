751882df239234b4044c1c4ad4c35e047d175da2
Sage Skaff <skaffsage@gmail.com>
2022-04-12 12:57:25 -0700

    Add runner, update StatTracker

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,1 +1,9 @@
-require './spec_helper'
+require './spec/spec_helper'
+
+RSpec.describe StatTracker do
+  it "exists" do
+    stat_tracker = StatTracker.new
+
+    expect(stat_tracker).to be_a(StatTracker)
+  end
+end
