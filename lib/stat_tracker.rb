f69eea076727a28ffa1661717bf4fc23632187cd
Eric Espindola <eric_e_2012@hotmail.com>
2022-04-12 12:57:52 -0700

    fix csv path issue and update highest_total_score

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -3,23 +3,21 @@ require 'csv'
 
 class StatTracker
 
-  attr_reader :game_statistics, :stat_tracker
-  def initialize(stat_tracker)
-    @stat_tracker = stat_tracker
+  def initialize(stats)
+    @stats = stats
   end
 
   def self.from_csv(locations)
     info = []
     locations.each do |key, path|
-      data = CSV.open path, headers: true, header_converters: :symbol
-      info << data
+      info << CSV.read(path, headers: true, header_converters: :symbol)
     end
-    return info
+    StatTracker.new(info)
   end
 
   def highest_total_score
     total = 0
-    @game_statistics.each do |row|
+    @stats[0].each do |row|
       if total < row[:home_goals].to_i + row[:away_goals].to_i
         total = row[:home_goals].to_i + row[:away_goals].to_i
       end
