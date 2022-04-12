cea6a38d2b866ee246893f0ab7452d0ee8fe6eaf
Jim Riddle <jimriddle0@gmail.com>
2022-04-12 14:33:43 -0500

    create intialize and from_csv methods

---
+++ runner.rb
@@ -0,0 +1,16 @@
+# runner.rb
+require './lib/stat_tracker'
+
+game_path = './data/games_sample.csv'
+team_path = './data/teams.csv'
+game_teams_path = './data/games_teams_sample.csv'
+
+locations = {
+  games: game_path,
+  teams: team_path,
+  game_teams: game_teams_path
+}
+
+stat_tracker = StatTracker.from_csv(locations)
+
+require 'pry'; binding.pry
