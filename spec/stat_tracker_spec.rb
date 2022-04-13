33c26902585568aede339a31513a0a0204744cd6
Sarah Hall <swatson1994@yahoo.com>
2022-04-13 16:56:30 -0400

    "Test: Add test for tie percentage"

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -104,6 +104,9 @@ describe StatTracker do
   it 'returns a percentage of how many wins were away wins' do
     expect(@stat_tracker.percentage_visitor_wins).to eq(55.56)
   end
+  it 'returns a percentage of how many ties there were' do
+    expect(@stat_tracker.percentage_ties).to eq(0.00)
+  end
 
 
 
