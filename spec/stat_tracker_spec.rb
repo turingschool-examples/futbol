3dc23e1a7d89ba33cf34f5a475a1459bde8b05da
Jim Riddle <jimriddle0@gmail.com>
2022-04-13 17:29:24 -0500

    wrote test to give me all games in a given season

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,11 +1,16 @@
+require 'simplecov'
+SimpleCov.start
+
 require './lib/stat_tracker'
 require 'csv'
+
 
 RSpec.describe StatTracker do
 
 
-
   it 'exists' do
+
+
     game_path = './data/games_15_rows.csv'
     team_path = './data/teams.csv'
     game_teams_path = './data/games_teams_15_rows.csv'
@@ -37,6 +42,7 @@ RSpec.describe StatTracker do
   end
 
   it 'can give me the highest_total_score' do
+
     game_path = './data/games_15_rows.csv'
     team_path = './data/teams.csv'
     game_teams_path = './data/games_teams_15_rows.csv'
@@ -49,6 +55,41 @@ RSpec.describe StatTracker do
 
     stat_tracker = StatTracker.from_csv(locations)
     expect(stat_tracker.highest_total_score).to eq(7)
+  end
+
+  xit 'can give me the winningest coach given a specific season' do
+    game_path = './data/games_sample.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/games_teams_sample.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    stat_tracker = StatTracker.from_csv(locations)
+
+    expect(stat_tracker.winningest_coach(20132014)).to eq("Claude Julien")
+
+  end
+
+  it 'gives me all the games given the season' do
+
+    game_path = './data/games_15_rows.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/games_teams_sample.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    stat_tracker = StatTracker.from_csv(locations)
+
+    expect(stat_tracker.games_by_season(20172018).count).to eq(6)
+
   end
 
 end
