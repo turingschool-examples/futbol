9d30e8b57084066d905632dfd688864244b9b71d
Andrew King <dev.andrewking@gmail.com>
2022-04-13 17:16:21 -0500

    add highest total score method and test

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,13 @@
 # game_id,season,type,date_time,away_team_id,home_team_id,away_goals,home_goals,venue,venue_link
 class Game
 
-attr_reader :game_id, :season, :type, :date_time, :away_team_id, :home_team_id, :away_goals, :home_goals, :venue, :venue_link
-    def initialize(game_id,season,type,date_time,away_team_id,home_team_id,away_goals,home_goals,venue,venue_link)
+attr_reader :game_id, :season, :type, :date_time, :away_team_id, :home_team_id,
+ :away_goals, :home_goals, :venue, :venue_link
+
+    def initialize(game_id,season,type,date_time,away_team_id,
+                    home_team_id,away_goals,home_goals,venue,
+                    venue_link)
+                    
         @game_id = game_id
         @season = season
         @type = type
@@ -12,6 +17,6 @@ attr_reader :game_id, :season, :type, :date_time, :away_team_id, :home_team_id, 
         @away_goals = away_goals
         @home_goals = home_goals
         @venue = venue
-        @venue_link = venue_link    
+        @venue_link = venue_link
     end
 end
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -2,37 +2,34 @@ require './lib/stat_tracker'
 
 
 RSpec.describe StatTracker do
+  before :each do
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+
+    @locations = {
+        games: game_path,
+        teams: team_path,
+        game_teams: game_teams_path
+      }
+      @stat_tracker = StatTracker.from_csv(@locations)
+
+  end
 
     it 'exists and has attributes' do
-      game_path = './data/games.csv'
-      team_path = './data/teams.csv'
-      game_teams_path = './data/game_teams.csv'
-
-      locations = {
-          games: game_path,
-          teams: team_path,
-          game_teams: game_teams_path
-        }
-        stat_tracker = StatTracker.from_csv(locations)
-
-        expect(stat_tracker).to be_an_instance_of(StatTracker)
+        expect(@stat_tracker).to be_an_instance_of(StatTracker)
     end
 
     it 'can call #from_csv on self' do
-        game_path = './data/games.csv'
-        team_path = './data/teams.csv'
-        game_teams_path = './data/game_teams.csv'
-
-        locations = {
-            games: game_path,
-            teams: team_path,
-            game_teams: game_teams_path
-          }
-          stat_tracker = StatTracker.from_csv(locations)
-          #require 'pry';binding.pry
-
-        expect(stat_tracker.games.count).to eq(7441)
-        expect(stat_tracker.teams.count).to eq(32)
-        expect(stat_tracker.game_teams.count).to eq(14882)
+        expect(@stat_tracker.games.count).to eq(7441)
+        expect(@stat_tracker.teams.count).to eq(32)
+        expect(@stat_tracker.game_teams.count).to eq(14882)
     end
+
+    it '#highest_total_score' do
+      expect(@stat_tracker.highest_total_score).to eq(6)
+    end
+
+
+
 end
