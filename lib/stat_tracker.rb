ac499208097f235182ae7854a6a2fed707f87d30
Jim Riddle <jimriddle0@gmail.com>
2022-04-13 17:29:58 -0500

    wrote method to pass games by season test

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -23,25 +23,57 @@ class StatTracker
     game_score.max
   end
 
-  # def initialize(locations)
-  #   @games = CSV.open ""
-  #   @teams = CSV.open ""
-  #   @game_teams = CSV.open ""
-  # end
-  #
-  # def self.from_csv(locations)
-  #   StatTracker.new(locations)
-  # end
+  def games_by_season(season)
+    @games.select do |row|
+      row[:season] == season.to_s
+    end
+  end
+
+  def winningest_coach(season)
+    season_games = games_by_season(season)
+    coaches_records = {}
+
+    season_game_teams = []
+    @game_teams.each do |row|
+      season_games.each do |game|
+        if row[:game_id] == game[:game_id]
+          season_game_teams << row
+        end
+      end
+    end
+
+    season_game_teams.each do |row|
+      coaches_records[row[:head_coach]] = [0,0,0,0.to_f]
+    end
+
+    season_game_teams.each do |row|
+      if row[:result] == "WIN"
+        coaches_records[row[:head_coach]][0] += 1
+      elsif row[:result] == "LOSS"
+        coaches_records[row[:head_coach]][1] += 1
+      elsif row[:result] == "TIE"
+        coaches_records[row[:head_coach]][2] += 1
+      end
+    end
+
+    coaches_records.keys.each do |key|
+      total_games = coaches_records[key][0] + coaches_records[key][1] + coaches_records[key][2]
+      coaches_records[key][3] = coaches_records[key][0]/total_games.to_f
+    end
+
+    winning_coach = ""
+    winning_coach_percentage = 0.to_f
+
+    coaches_records.keys.each do |key|
+      if coaches_records[key][3] > winning_coach_percentage
+        winning_coach_percentage = coaches_records[key][3]
+        winning_coach = key
+      end
+    end
+
+    require 'pry'; binding.pry
+
+    return winning_coach
+  end
 
 end
-
-
-# contents = CSV.open "./data/event_attendees.csv", headers: true, header_converters: :symbol
-# # puts contents
-#
-# attendees = []
-#
-# big_contents[0] do |row|
-#  p row[:game_id]
-#
-# end
