fecf1f6fb12008fc11babe8275cf121cb4bcee4a
Jennifer Halloran <jenniferlhalloran@gmail.com>
2022-04-14 11:55:16 -0400

    Add highest and lowest total score methods

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,9 +1,33 @@
 require './spec/spec_helper'
 
 RSpec.describe StatTracker do
-  it "exists" do
-    stat_tracker = StatTracker.new
 
-    expect(stat_tracker).to be_a(StatTracker)
+  before(:all) do
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    @stat_tracker = StatTracker.from_csv(locations)
+  end
+
+  it "exists" do
+
+    expect(@stat_tracker).to be_a(StatTracker)
+  end
+
+  it "finds highest_total_score" do
+
+    expect(@stat_tracker.highest_total_score).to eq 11
+  end
+
+  it "finds lowest_total_score" do
+
+    expect(@stat_tracker.lowest_total_score).to eq 0
   end
 end
