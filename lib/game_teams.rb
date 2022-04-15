934688ddc59d3fdd8a0233ca0e5b24081bb80376
Matt Deming <vballmatt44@gmail.com>
2022-04-15 09:59:27 -0600

    tests: Update game_teams spec file and GameTeams class

---
+++ lib/game_teams.rb
@@ -0,0 +1,36 @@
+
+
+class GameTeams
+  attr_reader :game_id,
+              :team_id,
+              :hoa,
+              :result,
+              :settled_in,
+              :head_coach,
+              :goals,
+              :shots,
+              :tackles,
+              :pim,
+              :powerplayopportunities,
+              :powerplaygoals,
+              :faceoffwinpercentage,
+              :giveaways,
+              :takeaways
+  def initialize(data)
+    @game_id = data[:game_id]
+    @team_id = data[:team_id]
+    @hoa = data[:hoa]
+    @result = data[:result]
+    @settled_in = data[:settled_in]
+    @head_coach = data[:head_coach]
+    @goals = data[:goals]
+    @shots = data[:shots]
+    @tackles = data[:tackles]
+    @pim = data[:pim]
+    @powerplayopportunities = data[:powerplayopportunities]
+    @powerplaygoals = data[:powerplaygoals]
+    @faceoffwinpercentage = data[:faceoffwinpercentage]
+    @giveaways = data[:giveaways]
+    @takeaways = data[:takeaways]
+  end
+end
