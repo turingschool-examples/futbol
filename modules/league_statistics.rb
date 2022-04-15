cefc482a122a5dd2caf7759e31b9f16a068e3159
Bryce Wein <bryce.wein@gmail.com>
2022-04-15 13:32:31 -0400

    Refactor: Add require relative for spec harness

--- modules/league_statistics.rb
+++ modules/league_statistics.rb
@@ -1,6 +1,6 @@
-require './lib/team_stats'
-require './lib/game'
-require './lib/game_teams'
+require_relative '../lib/team_stats'
+require_relative '../lib/game'
+require_relative '../lib/game_teams'
 
 module LeagueStats
   def count_of_teams
