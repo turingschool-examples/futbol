f138d10b8756dff369342e4229c32be889d3b1a4
Andrew King <dev.andrewking@gmail.com>
2022-04-14 19:05:20 -0500

    add:finished adding class methods into stat_tracker init method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -36,43 +36,5 @@ class StatTracker
 
 
 
-  def fill_team_array
-    @teams.each do |row|
-      team_id = row[:team_id]
-      franchise_id = row[:franchise_id]
-      team_name = row[:team_name]
-      abbreviation = row[:abbreviation]
-      stadium = row[:stadium]
-      link = row[:link]
-      teams_array << Team.new(team_id, franchise_id, team_name,
-         abbreviation, stadium, link)
-      end
-  end
-
-  def fill_game_teams_array
-    @game_teams.each do |row|
-      game_id = row[:game_id]
-      team_id = row[:team_id]
-      hoa = row[:hoa]
-      result = row[:result]
-      settled_in = row[:settled_in]
-      head_coach = row[:head_coach]
-      goals = row[:goals]
-      shots = row[:shots]
-      tackles = row[:tackles]
-      pim = row[:pim]
-      power_play_opportunities = row[:power_play_opportunities]
-      power_play_goals = row[:power_play_goals]
-      face_off_win_percentage = row[:face_off_win_percentage]
-      giveaways = row[:giveaways]
-      takeaways = row[:takeaways]
-      game_teams_array << GameTeams.new(game_id, team_id, hoa, result, settled_in, head_coach, goals,
-        shots, tackles, pim, power_play_opportunities, power_play_goals,
-        face_off_win_percentage, giveaways, takeaways)
-      end
-  end
-
-
-
 
   end
