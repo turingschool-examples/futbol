1bc131b376d467cd00e1f655869ed3808d0ed7e9
Sarah Hall <swatson1994@yahoo.com>
2022-04-16 14:42:58 -0400

    "Fix: Allow custon CSV modifications"

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -5,14 +5,14 @@ require './lib/stat_tracker'
 describe StatTracker do
   before(:each) do
 
-    game_path = './data/games_test.csv'
-    team_path = './data/teams_test.csv'
-    game_teams_path = './data/game_teams_test.csv'
+    @game_path = './data/games_test.csv'
+    @team_path = './data/teams_test.csv'
+    @game_teams_path = './data/game_teams_test.csv'
 
     locations = {
-      games: game_path,
-      teams: team_path,
-      game_teams: game_teams_path
+      games: @game_path,
+      teams: @team_path,
+      game_teams: @game_teams_path
     }
 
     @stat_tracker = StatTracker.from_csv(locations)
