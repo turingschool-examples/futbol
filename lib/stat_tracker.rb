0bf611723465d4d95cb2e92b6f77bfd6e043bd8f
philmarcu <philmark18@gmail.com>
2022-04-17 18:52:11 -0600

    Finish favorite opp/rival method / test

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -192,7 +192,6 @@ end
   def worst_season(id)
     team_games = game_teams_by_team(id.to_i)
     hash = Hash.new{|h,k| h[k] = [0,0,0.to_f] }
-    # hash {wins => 0, games => 0, avg_win_pct => 0.to_f}
       team_games.each do |game|
         hash[game.game_id.to_s[0..3]][0]+=1
         if game.result == "WIN"
@@ -203,7 +202,6 @@ end
       season_id = hash.min_by do |season|
         season[1][2]
       end[0].to_i
-      # require "pry"; binding.pry
       return  "#{season_id}#{season_id+1}"
   end
 
@@ -227,11 +225,68 @@ end
         game_total += 1
         if game.result == "WIN"
           win_total += 1
-          # hash[game.game_id][1]+=1
-          # hash[game.game_id][2] = hash[game.game_id][1]/hash[game.game_id][0].to_f
         end
       end
       # require "pry"; binding.pry
       return (win_total/game_total.to_f).round(2)
     end
+    def games_by_team(team_id)
+      @games.select do |game|
+        game.home_team_id == team_id.to_i || game.away_team_id == team_id.to_i
+      end
+    end
+
+  def favorite_opponent(id)
+    hash = Hash.new{|h,k| h[k] = [0,0,0.to_f] }
+    team_games = games_by_team(id)
+    team_games.each do |game|
+      if game.away_team_id == id.to_i
+        hash[game.home_team_id][0]+=1
+        if game.away_goals > game.home_goals
+          hash[game.home_team_id][1]+=1
+        end
+      else
+        hash[game.away_team_id][0]+=1
+        if game.home_goals > game.away_goals
+          hash[game.away_team_id][1]+=1
+        end
+      end
+    end
+    hash.keys.each do |key|
+      hash[key][2] = hash[key][1]/hash[key][0].to_f
+    end
+    fav_opp_team_id = hash.max_by do |team|
+      team[1][2]
+    end[0]
+    return team_name(fav_opp_team_id)
+  end
+
+  def rival(id)
+    hash = Hash.new{|h,k| h[k] = [0,0,0.to_f] }
+    team_games = games_by_team(id)
+    team_games.each do |game|
+      if game.away_team_id == id.to_i
+        hash[game.home_team_id][0]+=1
+        if game.away_goals > game.home_goals
+          hash[game.home_team_id][1]+=1
+        end
+      else
+        hash[game.away_team_id][0]+=1
+        if game.home_goals > game.away_goals
+          hash[game.away_team_id][1]+=1
+        end
+      end
+    end
+    hash.keys.each do |key|
+      hash[key][2] = hash[key][1]/hash[key][0].to_f
+    end
+    rival_id = hash.min_by do |team|
+      team[1][2]
+    end[0]
+    return team_name(rival_id)
+  end
+
 end
+# --- PS NOTES --- #
+# hash {wins => 0, games => 0, avg_win_pct => 0.to_f}  --- < Better way to
+# label key value pairs vs line's 176's less readable way
