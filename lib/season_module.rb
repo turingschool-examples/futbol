e007091fe19e6c8379c2f59af8a68fa4e73681a8
Clay Ash <claycourtneyash@gmail.com>
2022-04-19 20:01:38 -0600

    Refactor: move tackles_hash to season module

--- lib/season_module.rb
+++ lib/season_module.rb
@@ -2,4 +2,22 @@ require './required_files'
 
 module SeasonModule
 
+  def SeasonModule.tackles_hash(season_id, game_teams)
+    game_season = []
+    game_teams.each do |game|
+      if season_id[0..3] == game.game_id[0..3]
+        game_season << game
+      end
+    end
+    tackles_hash = {}
+    game_season.each do |game|
+      if tackles_hash[game.team_id] == nil
+        tackles_hash[game.team_id] = game.tackles.to_i
+      else
+        tackles_hash[game.team_id] += game.tackles.to_i
+      end
+    end
+    tackles_hash
+  end
+
 end
