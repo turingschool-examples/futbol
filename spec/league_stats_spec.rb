e178e3ccadcf38fb0b4a3ac62030d03d7768b997
Andrew King <dev.andrewking@gmail.com>
2022-04-20 20:12:55 -0500

    refactor league stats

--- spec/league_stats_spec.rb
+++ spec/league_stats_spec.rb
@@ -2,12 +2,12 @@ require './lib/league_stats'
 require './lib/stat_tracker'
 require './lib/csv_reader'
 
-RSpec.describe LeagueStats do 
+RSpec.describe LeagueStats do
     before :each do
         @game_path = './data/dummy_games.csv'
         @team_path = './data/teams.csv'
         @game_teams_path = './data/dummy_game_teams.csv'
-        
+
         @locations = {
           games: @game_path,
           teams: @team_path,
@@ -18,39 +18,40 @@ RSpec.describe LeagueStats do
     end
 
     describe 'League Statistics' do
-        it 'exists' do 
+        it 'exists' do
             expect(@league_stats).to be_a(LeagueStats)
         end
 
         it '#count_of_teams can count teams' do
         expect(@league_stats.count_of_teams).to eq(32)
         end
-  
+
         it '#best_offense finds team with the best offense' do
-          expect(@league_stats.best_offense).to eq(["Atlanta United",
-             "Orlando City SC", "Portland Timbers", "San Jose Earthquakes"])
+          expect(@league_stats.best_offense).to eq("Atlanta United, " +
+             "Orlando City SC, Portland Timbers, San Jose Earthquakes")
         end
-  
+
         it '#worst_offense finds team with the worst offense' do
           expect(@league_stats.worst_offense).to eq("Seattle Sounders FC")
         end
 
         it '#hightest_scoring_visitor finds the team with the hightest scoring visitor' do
-            expect(@league_stats.highest_scoring_visitor).to eq( ["Portland Timbers", "Real Salt Lake", "Atlanta United", "FC Dallas"])
+            expect(@league_stats.highest_scoring_visitor).to eq(
+              "Portland Timbers, Real Salt Lake, Atlanta United, FC Dallas")
         end
-  
+
         it '#lowest_scoring_visitor finds team with the lowest average away goals' do
           expect(@league_stats.lowest_scoring_visitor).to eq("Sporting Kansas City")
         end
-  
+
         it '#lowest_scoring_home_team finds team with the lowest average home goals' do
           expect(@league_stats.lowest_scoring_home_team).to eq("Seattle Sounders FC")
         end
-  
+
         it '#highest_scoring_home_team finds team with the highest average home goals' do
-          expect(@league_stats.highest_scoring_home_team).to eq(["Orlando City SC", "San Jose Earthquakes"])
+          expect(@league_stats.highest_scoring_home_team).to eq("Orlando City SC, San Jose Earthquakes")
         end
-  
+
         it '#team_name_helper finds team name via team_id' do
           expect(@league_stats.team_name_helper("3")).to eq("Houston Dynamo")
         end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,8 @@ require_relative 'team_spec'
 require_relative 'game_spec'
 require_relative 'game_teams_spec'
 require_relative 'game_stat_spec'
+require_relative 'league_stats_spec'
+require_relative 'season_stats_spec'
 #makes a helper file to pull in existing spec file and now simplecov lives here
 
 #spec/spec_helper - will run tests in required order in this file -> require them in this file to get the return in one big percentage
