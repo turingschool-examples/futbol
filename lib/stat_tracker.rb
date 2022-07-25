6a6c258ce0e694d039c5838285ac70a0c26f2277
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-07-25 16:30:43 -0600

    has data test and method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,6 +1,21 @@
-class StatTracker 
+require 'csv'
+
+class StatTracker
+  attr_reader :games,
+              :teams,
+              :game_teams
+
+  def initialize(games, teams, game_teams)
+    @games = games
+    @teams = teams
+    @game_teams = game_teams
+  end
 
   def self.from_csv(locations)
-    StatTracker.new
+    games = CSV.open(locations[:games], headers: true, header_converters: :symbol )
+    teams = CSV.open(locations[:teams], headers: true, header_converters: :symbol)
+    game_teams = CSV.open(locations[:game_teams], headers: true, header_converters: :symbol)
+    StatTracker.new(games, teams, game_teams)
   end
+
 end
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -2,10 +2,30 @@ require './lib/stat_tracker'
 
 RSpec.describe StatTracker do
   context 'Iteration 1' do
-    it 'exists' do
-      stat_tracker = StatTracker.new
+    before :each do
+      game_path = './data/games.csv'
+      team_path = './data/teams.csv'
+      game_teams_path = './data/game_teams.csv'
 
-      expect(stat_tracker).to be_an_instance_of(StatTracker)
+      locations = {
+        games: game_path,
+        teams: team_path,
+        game_teams: game_teams_path
+      }
+
+      @stat_tracker = StatTracker.from_csv(locations)
+    end
+    
+    it 'exists' do
+
+      expect(@stat_tracker).to be_an_instance_of(StatTracker)
+    end
+
+    it 'has data' do
+
+      expect(@stat_tracker.games).to be_an_instance_of(CSV)
+      expect(@stat_tracker.teams).to be_an_instance_of(CSV)
+      expect(@stat_tracker.game_teams).to be_an_instance_of(CSV)
     end
   end
 end
