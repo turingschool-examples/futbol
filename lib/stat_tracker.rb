3afe2e5eab2e69aaad281d085cd7c5fa1c4284ce
Carter Ball <carter.k.ball@gmail.com>
2022-07-25 18:38:17 -0400

    Create second test for StatTracker, add from_csv method to StatTracker with no contents

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,3 +1,12 @@
 class StatTracker
-  
+
+  def initialize
+
+  end
+
+  def self.from_csv(locations)
+    
+  end
+
+
 end
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -10,6 +10,18 @@ require './lib/stat_tracker.rb'
     expect(@stat_tracker).to be_a(StatTracker)
   end
 
+  it 'has the right class' do
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+    expect(StatTracker.from_csv(locations)).to be_a(StatTracker)
+  end
+
 
 
 
