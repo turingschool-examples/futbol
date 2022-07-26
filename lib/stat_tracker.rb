ef6f0d20dd167dfd6c7dd70d18ee947b24ad6d3b
A.J. Krumholz <krumholz.alan@gmail.com>
2022-07-26 10:21:28 -0400

    Revise instance variables in #initialize

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,22 +1,29 @@
 require 'csv'
 
 class StatTracker
-  attr_reader 
-  def initialize(locations)#games, teams, game_teams)
-    # @games = games
-    # @teams = teams
-    # @game_teams = game_teams
+  attr_reader :locations,
+              :games_data,
+              :teams_data,
+              :game_teams_data
+
+  def initialize(locations)
     @locations = locations
+    @games_data = CSV.open(locations[:games], headers: true, header_converters: :symbol)
+    @teams_data = CSV.open(locations[:teams], headers: true, header_converters: :symbol)
+    @game_teams_data = CSV.open(locations[:game_teams], headers: true, header_converters: :symbol)
   end
 
   def self.from_csv(locations)
-    # games_data = CSV.open locations[:games], headers: true, header_converters: :symbol
-    # teams_data = CSV.open locations[:teams], headers: true, header_converters: :symbol
-    # game_teams_data = CSV.open locations[:game_teams], headers: true, header_converters: :symbol
     new_stat_tracker = StatTracker.new(locations)#games_data, teams_data, game_teams_data)
-    # require 'pry';binding.pry
   end
 
+  # highest_total_score	Highest sum of the winning and losing teams’ scores	Integer  
+
+  def self.highest_total_score
+    
+    require 'pry'; binding.pry
+
+  end
 
 
 end
