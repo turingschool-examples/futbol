3a101035d41822ee1fe4b2a4d60a262531c8dfb9
Gavin Carew <gjcarew@gmail.com>
2022-07-26 14:31:01 -0700

    feat: winningest_coach method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -11,4 +11,19 @@ class StatTracker
     locations.map {|key, filepath| [key, StatTracker.new(filepath)]}.to_h
   end
 
+  def winningest_coach
+    coaches_and_results = @data[:result].zip @data[:head_coach]
+    wins = Hash.new(0)
+    losses = Hash.new(0)
+    coaches_and_results.each do |result, coach|
+      wins[coach] += 1 if result == "WIN"
+      losses[coach] += 1 if result == "LOSS"
+    end
+    win_percentage = Hash.new
+    wins.map do |coach, num_wins|
+      win_percentage[coach] = num_wins.to_f / (losses[coach] + num_wins)
+    end
+
+    win_percentage.max_by{|coach, percentage| percentage}.first
+  end
 end
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -30,9 +30,34 @@ RSpec.describe StatTracker do
     }
 
     stat_tracker = StatTracker.from_csv(locations)
-    
+
     expect(stat_tracker).to be_a Hash
     expect(stat_tracker.keys).to eq([:games, :teams, :game_teams])
     expect(stat_tracker.values).to all be_an_instance_of StatTracker
   end
+
+  context 'Season statistics' do
+    it 'S1. has a method for winningest_coach' do
+      team_path = './data/game_teams_dummy.csv'
+      location = team_path
+      stat_tracker = StatTracker.new(location)
+
+      expect(stat_tracker.data[:head_coach]).to include(stat_tracker.winningest_coach)
+      expect(stat_tracker.winningest_coach). to eq "Claude Julien"
+    end
+
+    
+
+
+#   Season Statistics
+# These methods each take a season id as an argument and return the values described below.
+#
+# Method	Description	Return Value
+# winningest_coach	Name of the Coach with the best win percentage for the season	String
+# worst_coach	Name of the Coach with the worst win percentage for the season	String
+# most_accurate_team	Name of the Team with the best ratio of shots to goals for the season	String
+# least_accurate_team	Name of the Team with the worst ratio of shots to goals for the season	String
+# most_tackles	Name of the Team with the most tackles in the season	String
+# fewest_tackles	Name of the Team with the fewest tackles in the season	String
+end
 end
