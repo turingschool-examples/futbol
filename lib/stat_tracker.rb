a251ea7bc5c5649fa2b58287be3d9428a791e0aa
Aleisha Mork <aleisha.mork@gmail.com>
2022-07-25 23:47:43 -0600

    test: add test for csv files
    Co-authored-by: Kristen <kristen.nestler@gmail.com>
    Co-authored_by: Stephen <fabianstephen@gmail.com>

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -2,17 +2,19 @@ require 'csv'
 
 class StatTracker
 
-  def initialize #(game, team, game_teams)
-    @games = []
-    @teams = []
-    @game_teams = []
+  attr_reader :games, :teams, :game_teams
+  def initialize (games, teams, game_teams)
+    @games = games
+    @teams = teams
+    @game_teams = game_teams
   end
 
     def self.from_csv(locations)
-      @game = CSV.table(locations[:games], headers: true, header_converters: :symbol)
-      @team = CSV.table(locations[:teams], headers: true, header_converters: :symbol)
-      @game_teams = CSV.table(locations[:game_teams], headers: true, header_converters: :symbol)
-      StatTracker.new
-      require "pry"; binding.pry
+      games = CSV.table(locations[:games], headers: true, header_converters: :symbol)
+      teams = CSV.table(locations[:teams], headers: true, header_converters: :symbol)
+      game_teams = CSV.table(locations[:game_teams], headers: true, header_converters: :symbol)
+
+      StatTracker.new(games, teams, game_teams)
+      # require "pry"; binding.pry
     end
 end
