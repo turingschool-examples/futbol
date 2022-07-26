478e71f028e58615372797c33ae32889ae06937c
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-07-26 12:17:39 -0600

    feat/add teams_dummy.csv data, runner_dummy.rb, and stat_tracker_dummy_spec.rb

---
+++ spec/stat_tracker_dummy_spec.rb
@@ -0,0 +1,29 @@
+require './lib/stat_tracker'
+
+describe StatTracker do
+  before(:all) do
+    game_path = './data/games_dummy.csv'
+    team_path = './data/teams_dummy.csv'
+    game_teams_path = './data/game_teams_dummy.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    @stat_tracker = StatTracker.from_csv(locations)
+  end
+
+  it "exists" do
+    expect(@stat_tracker).to be_an_instance_of StatTracker
+  end
+
+  it "#highest_total_score" do
+    expect(@stat_tracker.highest_total_score).to eq 6
+  end
+
+  it '#lowest_total_score' do
+    expect(@stat_tracker.lowest_total_score).to eq 1
+  end
+end
