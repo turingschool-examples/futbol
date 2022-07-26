38730aa407d0b7b1b5b17b4685b43921c5204ada
Kevin Ta <kvnta@yahoo.com>
2022-07-26 13:17:20 -0600

    Fix stat tracker with before :each so we don't need to have it for each test

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -3,39 +3,35 @@ require_relative 'spec_helper.rb'
 
 
 describe StatTracker do
+    before :each do 
+          @game_path = './data/games_dummy.csv'
+          @team_path = './data/teams.csv'
+          @game_teams_path = './data/game_teams_dummy.csv'
 
+          @locations = {
+          games: @game_path,
+          teams: @team_path,
+          game_teams: @game_teams_path
+          }
+          @stat_tracker = StatTracker.from_csv(@locations)
+        end 
     describe '.from_csv(locations)' do
         it 'returns an instance of StatTracker' do
 
-            game_path = './data/games_dummy.csv'
-            team_path = './data/teams.csv'
-            game_teams_path = './data/game_teams_dummy.csv'
-
-            locations = {
-            games: game_path,
-            teams: team_path,
-            game_teams: game_teams_path
-            }
-
-            expect(StatTracker.from_csv(locations)).to be_an_instance_of(StatTracker)
+            expect(StatTracker.from_csv(@locations)).to be_an_instance_of(StatTracker)
 
         end
 
         it "read csv files" do
-          game_path = './data/games_dummy.csv'
-          team_path = './data/teams.csv'
-          game_teams_path = './data/game_teams_dummy.csv'
-
-          locations = {
-          games: game_path,
-          teams: team_path,
-          game_teams: game_teams_path
-          }
-          stat_tracker = StatTracker.from_csv(locations)
-
-          expect(stat_tracker.games).to eq(CSV.table(game_path))
-          expect(stat_tracker.teams).to eq(CSV.table(team_path))
-          expect(stat_tracker.game_teams).to eq(CSV.table(game_teams_path))
+         
+          expect(@stat_tracker.games).to eq(CSV.table(@game_path))
+          expect(@stat_tracker.teams).to eq(CSV.table(@team_path))
+          expect(@stat_tracker.game_teams).to eq(CSV.table(@game_teams_path))
         end
+    end
+    describe 'Game Statistics' do 
+         
+
+
     end
 end
