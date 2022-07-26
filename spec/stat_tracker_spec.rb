c1d854c69e01598de00c610fbfe77d8c91bf9e03
Aleisha Mork <aleisha.mork@gmail.com>
2022-07-26 13:45:07 -0600

    test: add test for highest total score method

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -37,5 +37,20 @@ describe StatTracker do
           expect(stat_tracker.teams).to eq(CSV.table(team_path))
           expect(stat_tracker.game_teams).to eq(CSV.table(game_teams_path))
         end
+
+        it "finds highest total score" do
+          game_path = './data/games_dummy.csv'
+          team_path = './data/teams.csv'
+          game_teams_path = './data/game_teams_dummy.csv'
+
+          locations = {
+          games: game_path,
+          teams: team_path,
+          game_teams: game_teams_path
+          }
+          stat_tracker = StatTracker.from_csv(locations)
+
+          expect(stat_tracker.highest_total_score).to eq 7
+        end
     end
 end
