eba599941561fcffe39a85d782a184bb73ae008f
Rob Schlereth <robert.schlereth@gmail.com>
2022-07-26 16:17:54 -0500

    average goals by season, II

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -4,6 +4,7 @@ class StatTracker
   attr_reader :games,
               :teams,
               :game_teams
+              :percentage_visitor_wins
 
   def initialize(games, teams, game_teams)
     @games = games
@@ -19,21 +20,51 @@ class StatTracker
   end
 
 
+  def total_scores_per_game
+    games[:away_goals].sum + games[:home_goals].sum
+  end
+
+  def lowest_total_score
+    @games.map {|game| game[:away_goals] + game[:home_goals]}.min
+  end
+
+  def highest_total_score
+    @games.map{|game| game[:away_goals] + game[:home_goals]}.max
+  end
+
+  def average_goals_per_game
+    total_goals = @games.sum{ |game| game[:away_goals] + game[:home_goals] }
+    (total_goals.to_f / @games.length).round(2)
+  end
+
+  def percentage_ties
+    total_tie_games = @games.count{ |game| game[:away_goals] == game[:home_goals] }
+    (total_tie_games.to_f / @games.length).round(2)
+  end
+
+  def percentage_home_wins
+    home_wins = @game_teams.count{ |game_team| game_team[:hoa] == "home" && game_team[:result] == "WIN" }
+    total_games = @games.length
+    (home_wins.to_f / total_games).round(2)
+  end
+
   def percentage_visitor_wins
-    
-    # require 'pry';binding.pry
-    total_away_wins = @game_teams.count do |game_team| 
+    total_away_wins = @game_teams.count do |game_team|
       game_team[:hoa] == "away" && game_team[:result] == "WIN"
     end
     (total_away_wins/@games.length.to_f).round(2)
-    # (game[:away_wins]/(total_away_games.to_f).round(2))
+  end
+  
+  def count_of_games_by_season
+   games_by_season = Hash.new(0)
+
+   @games.each do |game|
+     games_by_season[game[:season]] += 1
+    end
+   games_by_season
   end
 
-  def average_goals_by_season
-    goals_by_season = Hash.new(0)
-    @games.count do |season, game|
-      (game[:away_goals] + game[:home_goals])/(@games.length.to_f).round(2)
-    end
-
+  def count_of_teams
+    @teams[:team_id].uniq.count
   end
 end
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe StatTracker do
   context 'Iteration 1' do
     before :each do
       game_path = './data/games_dummy.csv'
-      team_path = './data/teams.csv'
+      team_path = './data/teams_dummy.csv'
       game_teams_path = './data/game_teams_dummy.csv'
 
       locations = {
@@ -17,23 +17,53 @@ RSpec.describe StatTracker do
     end
 
     it 'exists' do
-
       expect(@stat_tracker).to be_an_instance_of(StatTracker)
     end
 
     it 'has data' do
-
       expect(@stat_tracker.games).to be_an_instance_of(CSV::Table)
       expect(@stat_tracker.teams).to be_an_instance_of(CSV::Table)
       expect(@stat_tracker.game_teams).to be_an_instance_of(CSV::Table)
     end
 
-    it 'can calculate percentage visitor wins' do
-        expect(@stat_tracker.percentage_visitor_wins).to eq(0.22)
+
+
+    it 'has a total score' do
+      expect(@stat_tracker.total_scores_per_game).to eq(34)
     end
 
-    it 'can calculate average goals by season' do
-        expect(@stat_tracker.average_goals_by_season).to eq (9)
+    it 'has a lowest total score' do
+      expect(@stat_tracker.lowest_total_score).to eq(1)
+    end
+
+    it 'show highest_total_score' do
+     expect(@stat_tracker.highest_total_score).to eq(5)
+    end
+
+    it 'can return the number average_goals_per_game' do
+      expect(@stat_tracker.average_goals_per_game).to eq(3.78)
+    end
+
+
+    it 'show percentage of tie games' do
+      expect(@stat_tracker.percentage_ties).to eq(0.0)
+    end
+
+    it 'shows the percentage_home_wins' do
+      expect(@stat_tracker.percentage_home_wins).to eq(0.56)
+    end
+
+    it 'can calculate percentage visitor wins' do
+      expect(@stat_tracker.percentage_visitor_wins).to eq(0.44)
+    end
+
+    it 'can return the count of games per season' do
+
+     expect(@stat_tracker.count_of_games_by_season).to eq({20122013=>5, 20142015=>4})
+    end
+
+    it 'can count the total teams' do
+      expect(@stat_tracker.count_of_teams).to eq(3)
     end
   end
 end
