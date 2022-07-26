3ba8ac132a2d8cffb7727532de7335f474de9f24
Gavin Carew <gjcarew@gmail.com>
2022-07-25 22:18:55 -0700

    feat: Add self.from_csv method to load multiples CSVs

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -4,12 +4,11 @@ class StatTracker
  attr_reader :data
 
   def initialize(filepath)
-    @data = CSV.read(filepath, headers: true,
-header_converters: :symbol)
+    @data = CSV.read(filepath, headers: true, header_converters: :symbol)
   end
 
-  def self.from_csv
-
+  def self.from_csv(locations)
+    locations.map {|key, filepath| [key, StatTracker.new(filepath)]}.to_h
   end
 
 end
