9bed4b6a9ec54f404b548b69a5218ce71aa45bc2
Andrew Mullins <mullinsand15@gmail.com>
2022-07-25 21:34:12 -0600

    fix: update stat_tracker class and add new method initialize

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -2,10 +2,20 @@ require 'csv'
 
 class StatTracker
   def self.from_csv(locations)
-    all_data_hash = Hash.new
-    all_data_hash[:games] = CSV.open locations[:games], headers: true, header_converters: :symbol
-    all_data_hash[:teams] = CSV.open locations[:teams], headers: true, header_converters: :symbol
-    all_data_hash[:game_teams] = CSV.open locations[:game_teams], headers: true, header_converters: :symbol
-    all_data_hash
+    all_data_hash = Hash.new{ |h, k| h[k] = [] }
+    CSV.foreach(locations[:games], headers: true, header_converters: :symbol) do |row|
+      all_data_hash[:games] << row
+    end
+    CSV.foreach(locations[:teams], headers: true, header_converters: :symbol) do |row|
+      all_data_hash[:teams] << row
+    end
+    CSV.foreach(locations[:game_teams], headers: true, header_converters: :symbol) do |row|
+      all_data_hash[:game_teams] << row
+    end
+  new(all_data_hash)
+  end
+
+  def initialize(all_data_hash)
+    @all_data_hash = all_data_hash
   end
 end
