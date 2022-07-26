394e65a85d9354c8bdcdb37b0bc63f513add27fd
Madeline Mauser <madeline.mauser@gmail.com>
2022-07-25 20:17:08 -0600

    Add count_of_teams method

--- lib/league_statistics.rb
+++ lib/league_statistics.rb
@@ -1,2 +1,15 @@
 class LeagueStatistics
+  attr_reader :data_set
+
+  def initialize(data_set)
+    @data_set = data_set
+  end
+
+  def count_of_teams
+    team_id_array = []
+    @data_set.each do |row|
+      team_id_array << row[1]
+    end
+    team_id_array.uniq.count
+  end
 end
--- spec/league_statistics_spec.rb
+++ spec/league_statistics_spec.rb
@@ -1,11 +1,20 @@
 require './lib/league_statistics'
 require 'csv'
+require 'pry'
 
 RSpec.describe(LeagueStatistics) do
   before :each do
-    @league_statistics = LeagueStatistics.new
+    @data = CSV.open "data/dummy_game_teams.csv", headers: true
+    @league_statistics = LeagueStatistics.new(@data)
   end
   it 'exists' do
     expect(@league_statistics).to be_an_instance_of(LeagueStatistics)
+  end
+  it 'has a CSV data set' do
+    expect(@league_statistics.data_set).to be_a(CSV)
+    expect(@league_statistics.data_set).to eq(@data)
+  end
+  it 'has a count of teams' do
+    expect(@league_statistics.count_of_teams).to eq(11)
   end
 end
