b41f009a8bbc6ffdef000dcbd0876a820a831bd9
dominicod <dominicodonnell99@gmail.com>
2022-07-27 17:31:22 -0500

    mid-way through best-season

--- lib/team_statistics.rb
+++ lib/team_statistics.rb
@@ -2,10 +2,24 @@ class TeamStatistics
     def initialize(statistics)
         @statistics = statistics
     end
-
     def team_info(team_id)
-        # team_id, franchise_id, team_name, abbreviation, and link
+        # returns hash with team_id, franchise_id, team_name, abbreviation, and link
         team_index = @statistics.teams[:team_id].index(team_id)
         team_info_return = (@statistics.teams[team_index].to_h).reject {|key, _value| key == :stadium}
+    end
+    def best_season(team_id)
+        # returns season with the highest win percentage for a team as string
+        win_percentage(team_id, :highest_win)
+    end
+    def season_by_id(team_id)
+        (@statistics.games.find_all {|row| row[:home_team_id] == team_id || row[:away_team_id] == team_id}).sort_by {|obj| obj[:season]}
+    end
+    def win_percentage(team_id, value)
+        games = @statistics.games
+        season_by_id = season_by_id(team_id)
+        unique_total_seasons = season_by_id.uniq {|season| season[:season]}
+        
+
+        require 'pry'; binding.pry
     end
 end
--- spec/team_statistics_spec.rb
+++ spec/team_statistics_spec.rb
@@ -5,9 +5,9 @@ require 'csv'
 
 RSpec.describe TeamStatistics do
     before(:all) do
-        game_path = './data/fixture_data_team_satistics/fixture_games.csv'
+        game_path = './data/games.csv'
         team_path = './data/teams.csv'
-        game_teams_path = './data/fixture_data_team_satistics/fixture_game_teams.csv'
+        game_teams_path = './data/game_teams.csv'
 
         locations = {
             games: game_path,
@@ -31,38 +31,28 @@ RSpec.describe TeamStatistics do
         context "team_info returns a hash" do
             hash_output = {
                 team_id: "1",
-                franchise_id: "23",
-                team_name: "Atlanta United",
+                franchiseid: "23",
+                teamname: "Atlanta United",
                 abbreviation: "ATL",
                 link: "/api/v1/teams/1"
             }
 
-            it 'returns a hash' do
-                @team_statistics.team_info("1")
-                # expect(@team_statistics.team_info("1")).to be_in(Hash)
-            end
-            xit 'returns team_id, franchise_id, team_name, abbreviation, and link' do
+            it 'returns team_id, franchise_id, team_name, abbreviation, and link' do
                 expect(@team_statistics.team_info("1")).to eq(hash_output)
             end
         end
     end
 
-    # describe ".best_season" do
-    #     context "best_season returns a string" do
-    #         xit 'returns a string' do
-    #             expect(team_statistics.best_season("6")).to be_in(String)
-    #         end
-    #         xit 'returns highest win percentage' do
-    #             expect(team_statistics.best_season("6").to eq("20132014"))
-    #         end
-    #     end
-    # end
+    describe ".best_season" do
+        context "best_season returns a string" do
+            it 'returns highest win percentage' do
+                expect(@team_statistics.best_season("6")).to eq("20132014")
+            end
+        end
+    end
 
     # describe ".worst_season" do
     #     context "worst_season returns a string" do
-    #         xit 'returns a string' do
-    #             expect(team_statistics.worst_season("6")).to be_in(String)
-    #         end
     #         xit 'returns highest win percentage' do
     #             expect(team_statistics.worst_season("6").to eq("20142015"))
     #         end
@@ -71,9 +61,6 @@ RSpec.describe TeamStatistics do
 
     # describe ".average_win_percentage" do
     #     context "average_win_percentage returns a float" do
-    #         xit 'returns a float' do
-    #             expect(team_statistics.average_win_percentage("6")).to be_in(Float)
-    #         end
     #         xit 'returns average win percentage' do
     #             expect(team_statistics.average_win_percentage("6")).to eq(0.49)
     #         end
