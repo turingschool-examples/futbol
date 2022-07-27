af304723fc78017439b494873b8b061c130f8ab1
Madeline Mauser <madeline.mauser@gmail.com>
2022-07-27 15:36:14 -0600

    Add test: "can return average goals by team"

--- spec/league_statistics_spec.rb
+++ spec/league_statistics_spec.rb
@@ -1,20 +1,27 @@
+require './spec_helper'
 require './lib/league_statistics'
 require 'csv'
 require 'pry'
 
 RSpec.describe(LeagueStatistics) do
   before :each do
-    @data = CSV.open "data/dummy_game_teams.csv", headers: true
-    @league_statistics = LeagueStatistics.new(@data)
+    @game_teams_data = CSV.read "data/fixture_data_league_statistics/dummy_game_teams.csv", headers: true, header_converters: :symbol
+    @teams_data = CSV.read "data/teams.csv", headers: true, header_converters: :symbol
+
+    @league_statistics = LeagueStatistics.new(@game_teams_data)
   end
   it 'exists' do
     expect(@league_statistics).to be_an_instance_of(LeagueStatistics)
   end
   it 'has a CSV data set' do
-    expect(@league_statistics.data_set).to be_a(CSV)
-    expect(@league_statistics.data_set).to eq(@data)
+    expect(@league_statistics.data_set).to be_a(CSV::Table)
+    expect(@league_statistics.data_set).to eq(@game_teams_data)
   end
   it 'has a count of teams' do
     expect(@league_statistics.count_of_teams).to eq(11)
+  end
+  it 'can return a hash of rounded average goals by team id' do
+    expect(@league_statistics.total_team_goal_averages).to be_a(Hash)
+    expect(@league_statistics.total_team_goal_averages[19]).to eq(3)
   end
 end
