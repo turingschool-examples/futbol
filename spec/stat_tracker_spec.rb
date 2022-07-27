75c029ad67642631ea70557d76871aba05ec569e
Kyongnam Kang <kangkna@gmail.com>
2022-07-27 17:56:53 -0400

    Add test lines for most tackles made

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -141,6 +141,11 @@ describe StatTracker do
     expect(@stat_tracker.least_accurate_team("20142015")).to eq "Columbus Crew SC"
   end
 
+  it 'can name the team with the most tackles made' do
+    expect(@stat_tracker.most_tackles("20132014")).to eq "FC Cincinnati"
+    expect(@stat_tracker.most_tackles("20142015")).to eq "Seattle Sounders FC"
+  end
+
 
 
 end
