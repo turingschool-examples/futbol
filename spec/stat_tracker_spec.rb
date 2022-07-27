c48d0b6602920fede4f4e656386d4526c994d499
Astrid Hecht <silentaviary@gmail.com>
2022-07-26 20:36:08 -0600

    Add spec file for PR

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -20,27 +20,27 @@ RSpec.describe do
     expect(@stat_tracker).to be_a(StatTracker)
   end
 
-  xit "#highest_total_score" do
+  it "#highest_total_score" do
     expect(@stat_tracker.highest_total_score).to eq 11
   end
 
-  xit "#lowest_total_score" do
+  it "#lowest_total_score" do
     expect(@stat_tracker.lowest_total_score).to eq 0
   end
 
-  xit "#percentage_home_wins" do
+  it "#percentage_home_wins" do
     expect(@stat_tracker.percentage_home_wins).to eq 0.44
   end
 
-  xit "#percentage_visitor_wins" do
+  it "#percentage_visitor_wins" do
     expect(@stat_tracker.percentage_visitor_wins).to eq 0.36
   end
 
-  xit "#percentage_ties" do
+  it "#percentage_ties" do
     expect(@stat_tracker.percentage_ties).to eq 0.20
   end
 
-  xit "#count_of_games_by_season" do
+  it "#count_of_games_by_season" do
     expected = {
       "20122013"=>806,
       "20162017"=>1317,
@@ -52,11 +52,11 @@ RSpec.describe do
     expect(@stat_tracker.count_of_games_by_season).to eq expected
   end
 
-  xit "#average_goals_per_game" do
+  it "#average_goals_per_game" do
     expect(@stat_tracker.average_goals_per_game).to eq 4.22
   end
 
-  xit "#average_goals_by_season" do
+  it "#average_goals_by_season" do
     expected = {
       "20122013"=>4.12,
       "20162017"=>4.23,
