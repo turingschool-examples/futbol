2a49c9e0f0a82f9d3c65afb34abbbee93423a12d
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-07-26 20:08:12 -0600

    update with code/tests for pull request

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -2,23 +2,7 @@ require './lib/stat_tracker'
 
 RSpec.describe StatTracker do
 
-  it '1. exists' do
-    team_path = './data/teams.csv'
-    location = team_path
-    stat_tracker = StatTracker.new(location)
-
-    expect(stat_tracker).to be_an_instance_of StatTracker
-  end
-
-  it '2. can load filepath' do
-    team_path = './data/teams.csv'
-    location = team_path
-    stat_tracker = StatTracker.new(location)
-    # require "pry"; binding.pry
-    expect(stat_tracker.data.headers).to eq [:team_id,:franchiseid,:teamname,:abbreviation,:stadium,:link]
-  end
-
-  it '3. can load an array of multiple CSVs' do
+  before :each do
     game_path = './data/games_dummy.csv'
     team_path = './data/teams.csv'
     game_teams_path = './data/game_teams_dummy.csv'
@@ -29,13 +13,18 @@ RSpec.describe StatTracker do
       game_teams: game_teams_path
     }
 
-    stat_tracker = StatTracker.from_csv(locations)
-    
-    expect(stat_tracker).to be_a Hash
-    expect(stat_tracker.keys).to eq([:games, :teams, :game_teams])
-    expect(stat_tracker.values).to all be_an_instance_of StatTracker
+    @stat_tracker = StatTracker.from_csv(locations)
   end
 
+  it '1. exists' do
+    expect(@stat_tracker).to be_an_instance_of StatTracker
+  end
+
+  it '3. can load an array of multiple CSVs' do
+    expect(@stat_tracker.games).to be_a(CSV::Table)
+    expect(@stat_tracker.teams).to be_a(CSV::Table)
+    expect(@stat_tracker.game_teams).to be_a(CSV::Table)
+  end
   describe 'League Methods' do
     xit 'can count teams' do
       game_path = './data/games_dummy.csv'
