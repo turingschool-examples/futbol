26fcdbd7b23090aec6ff657fadc0d46fa456f89f
Alex M <Alexm717@hotmail.com>
2022-07-27 15:08:29 -0700

    method for pecentage home wins

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,7 +1,8 @@
-require 'csv'
+require "csv"
+require "pry"
 
 class StatTracker
- attr_reader :games, :teams, :game_teams
+  attr_reader :games, :teams, :game_teams
 
   def initialize(games, teams, game_teams)
     @games = games
@@ -10,11 +11,34 @@ class StatTracker
   end
 
   def self.from_csv(locations)
-    StatTracker.new(
-    CSV.read(locations[:games], headers: true, header_converters: :symbol),
-    CSV.read(locations[:teams], headers: true, header_converters: :symbol),
-    CSV.read(locations[:game_teams], headers: true, header_converters: :symbol)
-  )
+    StatTracker.new(CSV.read(locations[:games],     headers: true,     header_converters: :symbol), CSV.read(locations[:teams],     headers: true,     header_converters: :symbol), CSV.read(locations[:game_teams],     headers: true,     header_converters: :symbol))
   end
 
+  def highest_total_score
+    highest_total = @games.map do |row|
+      row[:away_goals].to_i + row[:home_goals].to_i
+    end
+
+    highest_total.max
+  end
+
+  def lowest_total_score
+    lowest_total = @games.map do |row|
+      row[:away_goals].to_i + row[:home_goals].to_i
+    end
+
+    lowest_total.min
+  end
+
+  def percentage_home_wins
+    home_wins = []
+    all_home_games = []
+
+    @game_teams.each do |row|
+      home_wins << row if row[:hoa] == "home" && row[:result] == "WIN"
+      all_home_games << row if row[:hoa] == "home"
+    end
+
+    ((home_wins.count / all_home_games.count.to_f).round(2))
+  end
 end
