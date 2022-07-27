2eae1cb9dd46ce7752a00e30582863ed8c3c51ea
Ryan Smith <ryan.chris.smith@gmail.com>
2022-07-27 07:43:42 -0600

    feat: add calculation to return average goals across all seasons

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -96,7 +96,8 @@ class StatTracker
   end
 
   def highest_scoring_home_team
-    high_scoring = teams_and_goals.max_by{|teams, stats| stats[:total_home_goals].to_f}
+    high_scoring = teams_and_goals.max_by{|team, stats| stats[:total_home_goals].to_f / stats[:total_home_games]}
     high_scoring[1][:team_name]
+    require 'pry' ; binding.pry
   end
 end
