088aa84bfb7fa6eb4753bf2bf0bbb117019e9ee2
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-07-27 16:42:25 -0600

    add #id_team_hash, #number_of_games_played, #goals_by_id and setup for #best_offense, #worst_offense, #hightest_scoring_visitor, #highest_scoring_home_team, #lowest_scoring_home_team.

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,4 +1,5 @@
 require 'csv'
+require 'pry'
 
 class StatTracker
   attr_reader :games, :teams, :game_teams
@@ -15,5 +16,138 @@ class StatTracker
     CSV.read(locations[:teams], headers: true, header_converters: :symbol),
     CSV.read(locations[:game_teams], headers: true, header_converters: :symbol)
   )
+  end
+
+  def count_of_teams
+    @teams.count
+  end
+
+  def id_team_hash # desired return {id: {name: "teamname", home_goals: num, away_goals: num}}
+    @id_and_team = {}
+
+    @teams.each do |team|
+      @id_and_team[team[:team_id]] = team[:teamname]
+    end
+    # binding.pry
+    @id_and_team
+  end
+
+  def number_of_games_played
+    id_team_hash
+    number_played = @id_and_team
+
+    number_played.each do |id, team_name|
+      games_played = Hash.new{0}
+
+      @games.each do |game|
+        if game[:home_team_id] == id #|| game[:away_team_id] == id
+          games_played[:name] = team_name
+          games_played[:home_games] += 1
+        end
+        if game[:away_team_id] == id #|| game[:away_team_id] == id
+          games_played[:name] = team_name
+          games_played[:away_games] += 1
+        end
+      end
+      number_played[id] = games_played
+    end
+    binding.pry
+  end
+
+  def goals_by_id
+    id_team_hash #method id_team_hash called to have access to @id_and_team
+    goals_scored = @id_and_team
+    # binding.pry
+
+    goals_scored.each do |id, team_name|
+      goals = Hash.new{0}
+
+      @games.each do |game|
+        if game[:home_team_id] == id
+          goals[:name] = team_name
+          goals[:home_goals] += game[:home_goals].to_i
+          # goals[:number_of_games] += 1
+        end
+        if game[:away_team_id] == id
+          goals[:name] = team_name
+          goals[:away_goals] += game[:away_goals].to_i
+          # goals[:games_played] += 1
+        end
+      end
+      goals_scored[id] = goals
+    end
+    goals_scored
+  end
+
+  def best_offense
+    id_team_hash
+    goals_by_id
+    @id_and_team
+    the_best = @id_and_team.sort_by do |id, team_info|
+      team_info[:home_goals]
+    end
+    binding.pry
+    the_best[-1][1].values[0]
+  end
+
+  def best_offense#does not account for ties in offenses with same number of points
+  # Name of the team with the highest average number of goals scored per game across all seasons.
+    id_team_hash
+    goals_by_id
+    @id_and_team
+    the_best = @id_and_team.sort_by do |id, team_info|
+      team_info[:home_goals]
+    end
+    the_best[-1][1].values[0]
+  end
+
+  def worst_offense
+    id_team_hash
+    goals_by_id
+    @id_and_team
+    the_worst = @id_and_team.sort_by do |id, team_info|
+      team_info[:home_goals]
+    end
+    the_worst[0][1].values[0]
+  end
+
+  def highest_scoring_visitor
+    id_team_hash
+    goals_by_id
+    @id_and_team
+    the_best = @id_and_team.sort_by do |id, team_info|
+      team_info[:away_goals]
+    end
+    the_best[-1][1].values[0]
+  end
+  
+  def highest_scoring_home_team
+    id_team_hash
+    goals_by_id
+    @id_and_team
+    the_best = @id_and_team.sort_by do |id, team_info|
+      team_info[:home_goals]
+    end
+    the_best[-1][1].values[0]
+  end
+
+  def lowest_scoring_visitor
+    id_team_hash
+    goals_by_id
+    @id_and_team
+    the_lowest = @id_and_team.sort_by do |id, team_info|
+      team_info[:away_goals]
+    end
+    the_lowest[0][1].values[0]
+  end
+
+  def lowest_scoring_home_team
+    id_team_hash
+    goals_by_id
+    @id_and_team
+    the_lowest = @id_and_team.sort_by do |id, team_info|
+      team_info[:home_goals]
+    end
+    the_lowest[0][1].values[0]
   end
 end
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,4 +1,5 @@
 require './lib/stat_tracker'
+require 'pry'
 
 RSpec.describe StatTracker do
 
@@ -25,110 +26,43 @@ RSpec.describe StatTracker do
     expect(@stat_tracker.teams).to be_a(CSV::Table)
     expect(@stat_tracker.game_teams).to be_a(CSV::Table)
   end
+
   describe 'League Methods' do
-    xit 'can count teams' do
-      game_path = './data/games_dummy.csv'
-      team_path = './data/teams.csv'
-      game_teams_path = './data/game_teams_dummy.csv'
-  
-      locations = {
-        games: game_path,
-        teams: team_path,
-        game_teams: game_teams_path
-      }
-  
-      stat_tracker = StatTracker.from_csv(locations)
-      expect(stat_tracker.count_of_teams).to eq 32
+
+    it 'can count teams' do
+      # binding.pry
+      @stat_tracker.number_of_games_played
+      expect(@stat_tracker.count_of_teams).to eq 32
     end
 
     xit 'can find best offense' do
-      game_path = './data/games_dummy.csv'
-      team_path = './data/teams.csv'
-      game_teams_path = './data/game_teams_dummy.csv'
-  
-      locations = {
-        games: game_path,
-        teams: team_path,
-        game_teams: game_teams_path
-      }
-  
-      stat_tracker = StatTracker.from_csv(locations)
-      expect(stat_tracker.best_offense).to eq "Name of the team with the highest average number of goals scored per game across all seasons."
+#does not account for ties in offenses with same number of points
+      expect(@stat_tracker.best_offense).to eq "Sporting Kansas City"
     end
 
     xit 'can find worst offense' do
-      game_path = './data/games_dummy.csv'
-      team_path = './data/teams.csv'
-      game_teams_path = './data/game_teams_dummy.csv'
-  
-      locations = {
-        games: game_path,
-        teams: team_path,
-        game_teams: game_teams_path
-      }
-  
-      stat_tracker = StatTracker.from_csv(locations)
-      expect(stat_tracker.worst_offense).to eq "Name of the team with the lowest average number of goals scored per game across all seasons."
+    #does not account for ties in offenses with same number of points
+      expect(@stat_tracker.worst_offense).to eq "Columbus Crew SC"
     end
 
     xit 'can find highest scoring visitor' do
-      game_path = './data/games_dummy.csv'
-      team_path = './data/teams.csv'
-      game_teams_path = './data/game_teams_dummy.csv'
-  
-      locations = {
-        games: game_path,
-        teams: team_path,
-        game_teams: game_teams_path
-      }
-  
-      stat_tracker = StatTracker.from_csv(locations)
-      expect(stat_tracker.highest_scoring_visitor).to eq "Name of the team with the highest average score per game across all seasons when they are away."
+     
+      expect(@stat_tracker.highest_scoring_visitor).to eq "Name of the team with the highest average score per game across all seasons when they are away."
     end
 
     xit 'can find highest scoring home team' do
-      game_path = './data/games_dummy.csv'
-      team_path = './data/teams.csv'
-      game_teams_path = './data/game_teams_dummy.csv'
-  
-      locations = {
-        games: game_path,
-        teams: team_path,
-        game_teams: game_teams_path
-      }
-  
-      stat_tracker = StatTracker.from_csv(locations)
-      expect(stat_tracker.highest_scoring_home_team).to eq "Name of the team with the highest average score per game across all seasons when they are home."
+     
+      expect(@stat_tracker.highest_scoring_home_team).to eq "Name of the team with the highest average score per game across all seasons when they are home."
     end
 
     xit 'can find lowest scoring visitor' do
-      game_path = './data/games_dummy.csv'
-      team_path = './data/teams.csv'
-      game_teams_path = './data/game_teams_dummy.csv'
-  
-      locations = {
-        games: game_path,
-        teams: team_path,
-        game_teams: game_teams_path
-      }
-  
-      stat_tracker = StatTracker.from_csv(locations)
-      expect(stat_tracker.lowest_scoring_visitor).to eq "Name of the team with the lowest average score per game across all seasons when they are a visitor."
+
+      expect(@stat_tracker.lowest_scoring_visitor).to eq "Name of the team with the lowest average score per game across all seasons when they are a visitor."
     end
 
     xit 'can find lowest scoring home team' do
-      game_path = './data/games_dummy.csv'
-      team_path = './data/teams.csv'
-      game_teams_path = './data/game_teams_dummy.csv'
   
-      locations = {
-        games: game_path,
-        teams: team_path,
-        game_teams: game_teams_path
-      }
-  
-      stat_tracker = StatTracker.from_csv(locations)
-      expect(stat_tracker.lowest_scoring_home_team).to eq "Name of the team with the lowest average score per game across all seasons when they are at home."
+      expect(@stat_tracker.lowest_scoring_home_team).to eq "Name of the team with the lowest average score per game across all seasons when they are at home."
     end
   end
 end
