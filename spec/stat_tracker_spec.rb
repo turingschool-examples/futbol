a0a61040373f8b8b0b5bc2c54ab73e048ced5b08
Kyongnam Kang <kangkna@gmail.com>
2022-07-27 17:53:07 -0400

    Add test lines for best accuracy

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -126,6 +126,16 @@ describe StatTracker do
     expect(@stat_tracker.winningest_coach("20142015")).to eq("Alain Vigneault")
   end
 
+  it 'can name the coach with the worst winnig percentage' do
+    expect(@stat_tracker.worst_coach("20132014")).to eq "Peter Laviolette"
+    expect(@stat_tracker.worst_coach("20142015")).to eq("Craig MacTavish").or(eq("Ted Nolan"))
+  end
+
+  it 'can name the team with the best shot accuracy' do
+    expect(@stat_tracker.most_accurate_team("20132014")).to eq "Real Salt Lake"
+    expect(@stat_tracker.most_accurate_team("20142015")).to eq "Toronto FC"
+  end
+
 
 
 end
