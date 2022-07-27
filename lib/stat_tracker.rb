a2e84c4997a408e1526f772bf10c39684be670ba
Krisetn Nestler <kristen.nestler@gmail.com>
2022-07-26 22:10:27 -0400

    feat: #4 percentage_home_wins complete

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -30,5 +30,35 @@ class StatTracker
       total_score.max
     end
 
+    def percentage_home_wins
+      percentage = home_wins/home_games
+      p percentage
+    end
+
+    def game_win
+      wins = []
+      @game_teams[:result].map {|row| wins << row   if row == "WIN"}; p wins.count.to_f
+    end 
+  
+    def game_loss
+      losses = []
+      @game_teams[:result].map {|row| losses << row   if row == "LOSS"}; p losses.count.to_f
+    end
+  
+    def home_games
+      home = []
+      @game_teams[:hoa].map {|row| home << row   if row == "home" }; p home.count.to_f
+    end
+  
+    def away_games
+      away = []
+      @game_teams[:hoa].map {|row| away << row   if row == "away"}; p away.count.to_f
+    end
+  
+    def home_wins #(home, wins)
+      home_win = [] 
+      @game_teams.values_at(:result, :hoa).flat_map {|row| home_win << row if row == ["WIN", "home"]}; p home_win.count.to_f
+    end
 end
+
 
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -38,10 +38,36 @@ describe StatTracker do
 
         it "finds highest total score" do
           expect(@stat_tracker.highest_total_score).to eq 7
-       end
+        end
          
         it 'can return the lowest score' do 
           expect(@stat_tracker.lowest_total_score).to eq(1)
         end
+
+        it "tracks wins" do
+          expect(@stat_tracker.game_win).to eq 29
+        end
+  
+        it "tracks losses" do
+          expect(@stat_tracker.game_loss).to eq 29
+        end
+  
+        it "tracks home games" do
+          expect(@stat_tracker.home_games).to eq 30
+        end
+  
+        it "tracks away games" do
+          expect(@stat_tracker.away_games).to eq 30
+        end
+  
+        it "calculates home wins" do
+           expect(@stat_tracker.home_wins).to eq 21
+        end
+  
+        it ' calculates percentage wins' do 
+          expect(@stat_tracker.percentage_home_wins).to eq 0.7
+        end
+
+
     end
 end
