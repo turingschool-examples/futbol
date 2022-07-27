eba5d7971bb52f71260844170461ae9ca54dc2b8
Aleisha Mork <aleisha.mork@gmail.com>
2022-07-26 19:08:58 -0600

    test/refactor: add test for percentage of tied games addressing issue #6, and clarified name of total score helper method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -9,26 +9,29 @@ class StatTracker
     @game_teams = game_teams
   end
 
-    def self.from_csv(locations)
-      games = CSV.table(locations[:games])
-      teams = CSV.table(locations[:teams])
-      game_teams = CSV.table(locations[:game_teams])
-      StatTracker.new(games, teams, game_teams)
-    end
+  def self.from_csv(locations)
+    games = CSV.table(locations[:games])
+    teams = CSV.table(locations[:teams])
+    game_teams = CSV.table(locations[:game_teams])
+    StatTracker.new(games, teams, game_teams)
+  end
 
-    def total_score
-      @games.values_at(:away_goals, :home_goals).map do |game|
-        game[0] + game[1]
-      end
+  def total_scores_by_game
+    @games.values_at(:away_goals, :home_goals).map do |game|
+      game[0] + game[1]
     end
+  end
 
-    def lowest_total_score 
-        total_score.min
-    end
+  def lowest_total_score
+      total_scores_by_game.min
+  end
+
+  def highest_total_score
+    total_scores_by_game.max
+  end
+
+  def percentage_ties
   
-    def highest_total_score
-      total_score.max
-    end
+  end
 
 end
-
