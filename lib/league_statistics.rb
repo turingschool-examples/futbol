4a6362e081f409f284f1be851d39b4641e9324fe
Madeline Mauser <madeline.mauser@gmail.com>
2022-07-27 15:45:53 -0600

    Fix data set issues

--- lib/league_statistics.rb
+++ lib/league_statistics.rb
@@ -7,9 +7,13 @@ class LeagueStatistics
 
   def count_of_teams
     team_id_array = []
-    @data_set.each do |row|
+    @data_set[0].each do |row|
       team_id_array << row[1]
     end
     team_id_array.uniq.count
+  end
+
+  def total_team_goal_averages
+
   end
 end
--- spec/league_statistics_spec.rb
+++ spec/league_statistics_spec.rb
@@ -7,15 +7,16 @@ RSpec.describe(LeagueStatistics) do
   before :each do
     @game_teams_data = CSV.read "data/fixture_data_league_statistics/dummy_game_teams.csv", headers: true, header_converters: :symbol
     @teams_data = CSV.read "data/teams.csv", headers: true, header_converters: :symbol
+    @data_sets = [@game_teams_data, @teams_data]
 
-    @league_statistics = LeagueStatistics.new(@game_teams_data)
+    @league_statistics = LeagueStatistics.new(@data_sets)
   end
   it 'exists' do
     expect(@league_statistics).to be_an_instance_of(LeagueStatistics)
   end
   it 'has a CSV data set' do
-    expect(@league_statistics.data_set).to be_a(CSV::Table)
-    expect(@league_statistics.data_set).to eq(@game_teams_data)
+    expect(@league_statistics.data_set[0]).to be_a(CSV::Table)
+    expect(@league_statistics.data_set[0]).to eq(@game_teams_data)
   end
   it 'has a count of teams' do
     expect(@league_statistics.count_of_teams).to eq(11)
