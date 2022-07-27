c1c46f5598b2bed6332db81c23b41afc6725a583
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-07-26 20:14:10 -0600

    merge code from main

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,14 +1,19 @@
 require 'csv'
 
 class StatTracker
- attr_reader :data
+  attr_reader :games, :teams, :game_teams
 
-  def initialize(filepath)
-    @data = CSV.read(filepath, headers: true, header_converters: :symbol)
+  def initialize(games, teams, game_teams)
+    @games = games
+    @teams = teams
+    @game_teams = game_teams
   end
 
   def self.from_csv(locations)
-    locations.map {|key, filepath| [key, StatTracker.new(filepath)]}.to_h
+    StatTracker.new(
+    CSV.read(locations[:games], headers: true, header_converters: :symbol),
+    CSV.read(locations[:teams], headers: true, header_converters: :symbol),
+    CSV.read(locations[:game_teams], headers: true, header_converters: :symbol)
+  )
   end
-
 end
