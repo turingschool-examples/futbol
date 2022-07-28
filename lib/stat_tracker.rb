923bd6db842ea3c9ad26bae2ffb6c7698a92da12
Thomas Musselman <tmusselman@gmail.com>
2022-07-28 12:07:54 -0600

    remove old average_goals_per_game method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -38,10 +38,7 @@ class StatTracker
     @games.map{|game| game[:away_goals] + game[:home_goals]}.max
   end
 
-  def average_goals_per_game
-    total_goals = @games.sum{ |game| game[:away_goals] + game[:home_goals] }
-    (total_goals.to_f / @games.length).round(2)
-  end
+
 
   def percentage_ties
     total_tie_games = @games.count{ |game| game[:away_goals] == game[:home_goals] }
