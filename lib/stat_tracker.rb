0f7fa6f4c4778bd1b9d972c62489f2009e124609
Astrid Hecht <silentaviary@gmail.com>
2022-07-27 21:16:17 -0600

    refactor: make helper method for #favorite and #rival

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -137,20 +137,15 @@ class StatTracker
     @game_teams.find_all { |game| game[:team_id] == team_id }.min_by { |game| game[:goals] }[:goals].to_i
   end
 
-  def favorite_opponent(team_id) 
-    home_games = @games.find_all { |game| game[:home_team_id] == team_id}
-    away_games = @games.find_all { |game| game[:away_team_id] == team_id}
-    opponents = (home_games.map { |game| game[:away_team_id]} + away_games.map { |game| game[:home_team_id]}).uniq.sort_by { |num| num.to_i}
-    ordered_win_rate = []
-    opponents.each do |opponent, win_counter = 0|
-      win_counter += home_games.count { |game| game[:away_team_id] == opponent && game[:home_goals] > game[:away_goals]}
-      win_counter += away_games.count { |game| game[:home_team_id] == opponent && game[:home_goals] < game[:away_goals]}
-      ordered_win_rate << (win_counter.to_f / versus_games_counter(team_id, opponent))
-    end
-    @teams.find { |team| team[:team_id] == Hash[opponents.zip(ordered_win_rate)].max_by { |_k, v| v }[0] }[:team_name]
+  def favorite_opponent(team_id)
+    @teams.find { |team| team[:team_id] == win_hash(team_id).max_by { |_k, v| v }[0] }[:team_name]
   end
 
   def rival(team_id)
+    @teams.find { |team| team[:team_id] == win_hash(team_id).min_by { |_k, v| v }[0] }[:team_name]
+  end
+
+  def win_hash(team_id)
     home_games = @games.find_all { |game| game[:home_team_id] == team_id}
     away_games = @games.find_all { |game| game[:away_team_id] == team_id}
     opponents = (home_games.map { |game| game[:away_team_id]} + away_games.map { |game| game[:home_team_id]}).uniq.sort_by { |num| num.to_i}
@@ -160,7 +155,7 @@ class StatTracker
       win_counter += away_games.count { |game| game[:home_team_id] == opponent && game[:home_goals] < game[:away_goals]}
       ordered_win_rate << (win_counter.to_f / versus_games_counter(team_id, opponent))
     end
-    @teams.find { |team| team[:team_id] == Hash[opponents.zip(ordered_win_rate)].min_by { |_k, v| v }[0] }[:team_name]
+    Hash[opponents.zip(ordered_win_rate)]
   end
 
   def versus_games_counter(team_id, opponent)
