61f78e2bafd33648fce4227cc6aaed1d87b2ca22
Thomas Hayes <hayes.thomas610@gmail.com>
2022-07-27 20:38:13 -0600

    Spacing fix

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,16 +1,16 @@
 require 'csv'
 class StatTracker
-    attr_reader :game_teams,
-                :games,
-                :teams
+  attr_reader :game_teams,
+              :games,
+              :teams
 
-    def initialize(locations)
-      game_teams = CSV.read locations[:game_teams], headers: true, header_converters: :symbol
-      games = CSV.read locations[:games], headers: true, header_converters: :symbol
-      teams = CSV.read locations[:teams], headers: true, header_converters: :symbol
-    end
+  def initialize(locations)
+    game_teams = CSV.read locations[:game_teams], headers: true, header_converters: :symbol
+    games = CSV.read locations[:games], headers: true, header_converters: :symbol
+    teams = CSV.read locations[:teams], headers: true, header_converters: :symbol
+  end
 
-    def self.from_csv(locations)
-      StatTracker.new(locations)
-    end
+  def self.from_csv(locations)
+    StatTracker.new(locations)
+  end
 end
