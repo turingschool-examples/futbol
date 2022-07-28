411f309b657866ac224270c87beae039ba41f794
Thomas Musselman <tmusselman@gmail.com>
2022-07-28 16:28:58 -0600

    clean up files from previous merges. move old methods to new folder

--- lib/game_stats.rb
+++ lib/game_stats.rb
@@ -23,22 +23,6 @@ module GameStats
     end
     (total_away_wins/@games.length.to_f).round(2)
   end
-  
-  def average_goals_by_season
-    average_goals_per_season = Hash.new(0)
-    seasons = @games[:season].uniq
-    seasons.each do |season|
-      total_goals_in_season = 0
-      total_games_in_season = 0
-      @games.each do |game|
-        if game[:season] == season
-          total_goals_in_season += game[:away_goals] + game[:home_goals]
-          total_games_in_season += 1
-        end
-      end
-      average_goals_per_season[season] = total_goals_in_season.to_f / total_games_in_season
-    end
-    average_goals_per_season
 
   def percentage_ties
     total_tie_games = @games.count{ |game| game.winner == :tie }
---
+++ lib/old_stat_tracker_methods.rb
@@ -0,0 +1,134 @@
+def count_of_games_by_season
+  games_by_season = Hash.new(0)
+
+  @games.each do |game|
+    games_by_season[game[:season]] += 1
+   end
+  games_by_season
+ end
+
+ def count_of_teams
+   @teams[:team_id].uniq.count
+ end
+
+ def average_goals_by_season
+   average_goals_per_season = Hash.new(0)
+   seasons = @games[:season].uniq
+   seasons.each do |season|
+     total_goals_in_season = 0
+     total_games_in_season = 0
+     @games.each do |game|
+       if game[:season] == season
+         total_goals_in_season += game[:away_goals] + game[:home_goals]
+         total_games_in_season += 1
+       end
+     end
+     average_goals_per_season[season] = total_goals_in_season.to_f / total_games_in_season
+   end
+   average_goals_per_season
+ end
+
+ def teams_and_goals #=> League Stats Helper Method
+   teams_and_goals_hash = {}
+   @teams.each do |team|
+     teams_and_goals_hash[team[:team_id]] = {
+       team_name: team[:teamname],
+       total_goals: 0,
+       total_games: 0,
+       total_home_goals: 0,
+       total_away_goals: 0,
+       total_home_games: 0,
+       total_away_games: 0
+     }
+   end
+   @game_teams.each do |game_team|
+     teams_and_goals_hash[game_team[:team_id]][:total_goals] += game_team[:goals]
+     teams_and_goals_hash[game_team[:team_id]][:total_games] += 1
+     if game_team[:hoa] == 'home'
+       teams_and_goals_hash[game_team[:team_id]][:total_home_goals] += game_team[:goals]
+       teams_and_goals_hash[game_team[:team_id]][:total_home_games] += 1
+     else
+       teams_and_goals_hash[game_team[:team_id]][:total_away_goals] += game_team[:goals]
+       teams_and_goals_hash[game_team[:team_id]][:total_away_games] += 1
+     end
+   end
+   teams_and_goals_hash
+ end
+
+ def worst_offense
+   result = teams_and_goals.min_by do |team, stats|
+     stats[:total_goals].to_f / stats[:total_games]
+   end
+   result[1][:team_name]
+ end
+
+ def best_offense
+   result = teams_and_goals.max_by do |team, stats|
+     stats[:total_goals].to_f / stats[:total_games]
+   end
+   result[1][:team_name]
+ end
+
+ def highest_scoring_home_team
+   high_scoring = teams_and_goals.max_by{|team, stats| stats[:total_home_goals].to_f / stats[:total_home_games]}
+   high_scoring[1][:team_name]
+ end
+
+ def lowest_scoring_home_team
+   low_scoring = teams_and_goals.min_by{|teams, stats| stats[:total_home_goals].to_f / stats[:total_home_games]}
+   low_scoring[1][:team_name]
+ end
+
+ def highest_scoring_visitor
+   high_scoring = teams_and_goals.max_by{|teams, stats| stats[:total_away_goals].to_f / stats[:total_away_games]}
+   high_scoring[1][:team_name]
+ end
+
+ def lowest_scoring_visitor
+   low_scoring = teams_and_goals.min_by{|teams, stats| stats[:total_away_goals].to_f / stats[:total_away_games]}
+   low_scoring[1][:team_name]
+ end
+
+ def tackles_by_team(season) #helper method, returns hash with keys team_id and values number of tackles
+   season_game_ids = []
+   @games.each do |game|
+     season_game_ids << game[:game_id] if season.to_i == game[:season]
+   end
+   team_tackle_data = Hash.new(0)
+   @game_teams.each do |game_team|
+     season_game_ids.each do |game_id|
+       if game_team[:game_id] == game_id
+         team_tackle_data[game_team[:team_id]] += game_team[:tackles]
+       end
+     end
+   end
+   team_tackle_data
+ end
+
+ def fewest_tackles(season)
+   result_team_id = tackles_by_team(season).min_by do |team_id, tackles|
+     tackles
+   end.first
+   result_team_name = ""
+   @teams.each{ |team| result_team_name = team[:teamname] if team[:team_id] == result_team_id }
+   result_team_name
+ end
+
+ def most_tackles(season)
+   result_team_id = tackles_by_team(season).max_by do |team_id, tackles|
+     tackles
+   end.first
+   result_team_name = ""
+   @teams.each{ |team| result_team_name = team[:teamname] if team[:team_id] == result_team_id }
+   result_team_name
+ end
+
+ def team_info(team)
+   team_info = Hash.new()
+   @teams.each do |individual_team|
+     if individual_team[:teamname] == team
+       team_info = {"team_id" => individual_team[:team_id].to_s, "franchise_id" => individual_team[:franchiseid].to_s, "team_name" => individual_team[:teamname], "abbreviation" => individual_team[:abbreviation], "link" => individual_team[:link]}
+     end
+   end
+   team_info
+ end
