cd2ff540cd7d23cce6a9cc57ca89540fe239e330
Mary Ballantyne <mballantyne777@gmail.com>
2022-07-27 18:57:57 -0600

    complete new methods in league statistics

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -142,5 +142,59 @@ class StatTracker
     avg_season_goals
   end
 
-  #league stats
+  def count_of_teams
+    team_count = 0
+    contents = CSV.open(@team_path, headers: true, header_converters: :symbol)
+    contents.each do |row|
+      teams = row[:team_id].to_i
+      if teams != 0
+        team_count += 1
+      end
+    end
+    team_count
+  end
+
+  def best_offense
+    game_teams_csv = CSV.open(@game_teams_path, headers: true, header_converters: :symbol)
+    goals_by_team_id = Hash.new(0)
+    game_teams_csv.each do |row|
+      goals_by_team_id[row[:team_id].to_i] += row[:goals].to_i
+    end
+    best_offense_team_id = goals_by_team_id.max_by { |team_id, goals| goals }[0]
+    teams_csv = CSV.open(@team_path, headers: true, header_converters: :symbol)
+    team_row = teams_csv.find do |row|
+      row[:team_id].to_i == best_offense_team_id
+    end
+    team_row[:teamname]
+  end
+
+  def worst_offense
+    game_teams_csv = CSV.open(@game_teams_path, headers: true, header_converters: :symbol)
+    goals_by_team_id = Hash.new(0)
+    game_teams_csv.each do |row|
+      goals_by_team_id[row[:team_id].to_i] += row[:goals].to_i
+    end
+    #in the method below we are checking the lowest scoring team by accessing the key at the end "[0]"
+    best_offense_team_id = goals_by_team_id.min_by { |team_id, goals| goals }[0]
+    teams_csv = CSV.open(@team_path, headers: true, header_converters: :symbol)
+    team_row = teams_csv.find do |row|
+      row[:team_id].to_i == best_offense_team_id
+    end
+    team_row[:teamname]
+  end
+
+  def highest_scoring_visitor
+    games_csv = CSV.open(@game_path, headers: true, header_converters: :symbol)
+    away_goals_by_team_id = Hash.new(0)
+    games_csv.each do |row|
+      away_goals_by_team_id[row[:away_team_id].to_i] += row[:away_goals].to_i
+    end
+    best_away_team_id = away_goals_by_team_id.max_by { |away_team_id, goals| goals }[0]
+    teams_csv = CSV.open(@team_path, headers: true, header_converters: :symbol)
+    visitor_team_row = teams_csv.find do |row|
+      row[:team_id].to_i == best_away_team_id
+    end
+    visitor_team_row[:teamname]
+  end
+
 end
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,13 +1,15 @@
 require './lib/stat_tracker'
 
 RSpec.describe StatTracker do
-  let(:locations) do {
-    games: './data/dummy_games.csv',
-    teams: './data/dummy_teams.csv',
-    game_teams: './data/dummy_game_teams.csv'
-  } end
+  let(:locations) do
+    {
+      games: './data/dummy_games.csv',
+      teams: './data/teams.csv',
+      game_teams: './data/dummy_game_teams.csv'
+    }
+  end
 
-  let(:stat_tracker) {StatTracker.from_csv(locations)}
+  let(:stat_tracker) { StatTracker.from_csv(locations) }
 
   it 'StatTracker exists' do
     expect(stat_tracker).to be_a(StatTracker)
@@ -34,7 +36,7 @@ RSpec.describe StatTracker do
   end
 
   it 'returns count of games by season' do
-    expect(stat_tracker.count_of_games_by_season).to eq({'20122013' => 7})
+    expect(stat_tracker.count_of_games_by_season).to eq({ '20122013' => 7 })
   end
 
   it 'returns average goals per game' do
@@ -42,12 +44,61 @@ RSpec.describe StatTracker do
   end
 
   it 'returns total goals by season' do
-    expect(stat_tracker.total_goals_by_season).to eq({'20122013' => 30})
+    expect(stat_tracker.total_goals_by_season).to eq({ '20122013' => 30 })
   end
 
   it 'returns average goals by season' do
-    expect(stat_tracker.average_goals_by_season).to eq({'20122013' => 4.29})
+    expect(stat_tracker.average_goals_by_season).to eq({ '20122013' => 4.29 })
   end
 
-  #league stats
+  it 'returns count of teams' do
+    expect(stat_tracker.count_of_teams).to eq(32)
+  end
+
+  it 'returns team with best offense' do
+    expect(stat_tracker.best_offense).to eq("FC Dallas")
+  end
+
+  it 'returns team with worst offense' do
+    expect(stat_tracker.worst_offense).to eq("Houston Dynamo")
+  end
+
+  it 'returns highest scoring visitor team' do
+    #Data has been mocked below since initial dummy data set did not have varying seasons.
+    fake_data = [
+      {
+        :season => "20142015",
+        :away_team_id => "3",
+        :home_team_id => "6",
+        :away_goals => "2",
+        :home_goals => "3",
+      },
+      {
+        :season => "20122013",
+        :away_team_id => "3",
+        :home_team_id => "6",
+        :away_goals => "2",
+        :home_goals => "3",
+      },
+      {
+        :season => "20142015",
+        :away_team_id => "6",
+        :home_team_id => "3",
+        :away_goals => "3",
+        :home_goals => "1",
+      },
+      {
+        :season => "20122013",
+        :away_team_id => "6",
+        :home_team_id => "3",
+        :away_goals => "3",
+        :home_goals => "2",
+      }
+    ]
+    #calls to the original CSV file where specified, in this case it was the second half of the method
+    allow(CSV).to receive(:open).with(any_args).and_call_original
+    #allows us to utilize the fake data instead of the referenced CSV in order to perform our test
+    allow(CSV).to receive(:open).with(locations[:games], any_args).and_return(fake_data)
+    expect(stat_tracker.highest_scoring_visitor).to eq("FC Dallas")
+  end
 end
