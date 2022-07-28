86a258e61c0e98bfd5d7eb70db1872c6b2fbe72e
Thomas Musselman <tmusselman@gmail.com>
2022-07-28 12:06:49 -0600

    clean up old tests and write new test for average_goals_per_game

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -21,96 +21,13 @@ RSpec.describe StatTracker do
     end
 
     it 'has data' do
-      expect(@stat_tracker.games).to be_an_instance_of(CSV::Table)
-      expect(@stat_tracker.teams).to be_an_instance_of(CSV::Table)
-      expect(@stat_tracker.game_teams).to be_an_instance_of(CSV::Table)
+      expect(@stat_tracker.games).to all(be_an(Game))
+      expect(@stat_tracker.teams.values).to all(be_an(Team))
+      expect(@stat_tracker.seasons.values).to all(be_an(Season))
     end
 
-    it 'has a total score' do
-      expect(@stat_tracker.total_scores_per_game).to eq(34)
-    end
-
-    # it 'has a lowest total score' do
-    #   expect(@stat_tracker.lowest_total_score).to eq(1)
-    # end
-    #
-    # it 'show highest_total_score' do
-    #  expect(@stat_tracker.highest_total_score).to eq(5)
-    # end
-
-    it 'can return the number average_goals_per_game' do
-      expect(@stat_tracker.average_goals_per_game).to eq(3.78)
-    end
-
-
-    it 'show percentage of tie games' do
-      expect(@stat_tracker.percentage_ties).to eq(0.0)
-    end
-
-    it 'shows the percentage_home_wins' do
-      expect(@stat_tracker.percentage_home_wins).to eq(0.56)
-    end
-
-    it 'can calculate percentage visitor wins' do
-      expect(@stat_tracker.percentage_visitor_wins).to eq(0.44)
-    end
-
-    it 'can return the count of games per season' do
-
-     expect(@stat_tracker.count_of_games_by_season).to eq({20122013=>5, 20142015=>4})
-    end
-
-    it 'can count the total teams' do
-      expect(@stat_tracker.count_of_teams).to eq(3)
-    end
-
-    it 'can calculate average goals by season' do
-        expect(@stat_tracker.average_goals_by_season).to eq ({20122013 => 4.4, 20142015 => 3.0})
-    end
-
-    it 'can return a hash of team and goal data' do
-      expect(@stat_tracker.teams_and_goals).to be_an(Hash)
-      expect(@stat_tracker.teams_and_goals[3][:total_goals]).to eq(8)
-    end
-
-
-    it 'can return the team with the worst offense' do
-      expect(@stat_tracker.worst_offense).to eq("Sporting Kansas City")
-    end
-
-    it 'can return the team with the best offense' do
-      expect(@stat_tracker.best_offense).to eq("FC Dallas")
-    end
-
-    it 'can show the highest scoring home team' do
-      expect(@stat_tracker.highest_scoring_home_team).to eq('FC Dallas')
-    end
-
-    it 'can show the lowest scoring home team' do
-      expect(@stat_tracker.lowest_scoring_home_team).to eq('Sporting Kansas City')
-    end
-
-    it 'can show the highest scoring visiting team' do
-      expect(@stat_tracker.highest_scoring_visitor).to eq('FC Dallas')
-    end
-
-    it 'can show the lowest scoring visiting team' do
-      expect(@stat_tracker.lowest_scoring_visitor).to eq('Sporting Kansas City')
-    end
-
-    it 'can return the team with the fewest tackles in a given season' do
-      expect(@stat_tracker.fewest_tackles("20122013")).to eq("FC Dallas")
-    end
-
-    it 'can return the team with the most tackles in a given season' do
-      expect(@stat_tracker.most_tackles("20122013")).to eq("Houston Dynamo")
-    end
-
-    it 'can show the team info' do
-      expect(@stat_tracker.team_info('FC Dallas')).to eq({"team_id" => "6", "franchise_id" => "6",
-        "team_name" => "FC Dallas",
-        "abbreviation" => "DAL",
-        "link" => "/api/v1/teams/6"})
+    it 'can return average_goals_per_game' do
+      expect(@stat_tracker.average_goals_per_game).to eq(3.85)
     end
 
   end
