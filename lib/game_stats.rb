02c3ebab47b478ec2794f017dbc4fa8a5627f257
Thomas Musselman <tmusselman@gmail.com>
2022-07-28 12:26:51 -0600

    feat: implement new percentage_home_wins method

--- lib/game_stats.rb
+++ lib/game_stats.rb
@@ -4,5 +4,10 @@ module GameStats
     total_goals = @games.sum{ |game| game.total_goals }
     (total_goals.to_f / @games.length).round(2)
   end
+
+  def percentage_home_wins
+    home_wins = @games.count{ |game| game.winner == :home_team }
+    (home_wins.to_f / @games.length).round(2)
+  end
   
 end
