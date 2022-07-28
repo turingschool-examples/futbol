3c542a735f0be709d93322787ef6f90a5e3b61ca
Thomas Musselman <tmusselman@gmail.com>
2022-07-28 16:17:54 -0600

    Update stat_tracker.rb

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -26,29 +26,6 @@ class StatTracker
     StatTracker.new(games, teams, game_teams)
   end
 
-  def average_goals_per_game
-    total_goals = @games.sum{ |game| game[:away_goals] + game[:home_goals] }
-    (total_goals.to_f / @games.length).round(2)
-  end
-
-  def percentage_ties
-    total_tie_games = @games.count{ |game| game[:away_goals] == game[:home_goals] }
-    (total_tie_games.to_f / @games.length).round(2)
-  end
-
-  def percentage_home_wins
-    home_wins = @game_teams.count{ |game_team| game_team[:hoa] == "home" && game_team[:result] == "WIN" }
-    total_games = @games.length
-    (home_wins.to_f / total_games).round(2)
-  end
-  
-  def percentage_visitor_wins
-    total_away_wins = @game_teams.count do |game_team|
-      game_team[:hoa] == "away" && game_team[:result] == "WIN"
-    end
-    (total_away_wins/@games.length.to_f).round(2)
-  end
-
   def count_of_games_by_season
    games_by_season = Hash.new(0)
 
