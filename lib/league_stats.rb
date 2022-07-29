7a35642233bada27f16392157b8464662dd76b0d
Thomas Musselman <tmusselman@gmail.com>
2022-07-28 22:00:11 -0600

    fixed best/worst offense methods to convert to float

--- lib/league_stats.rb
+++ lib/league_stats.rb
@@ -1,11 +1,11 @@
 module LeagueStats
 
   def worst_offense
-    @teams.values.min_by{ |team| team.total_overall_goals / team.games_participated_in.length }.team_name
+    @teams.values.min_by{ |team| team.total_overall_goals.to_f / team.games_participated_in.length }.team_name
   end
 
   def best_offense
-    @teams.values.max_by{ |team| team.total_overall_goals / team.games_participated_in.length }.team_name
+    @teams.values.max_by{ |team| team.total_overall_goals.to_f / team.games_participated_in.length }.team_name
   end
 
   def highest_scoring_home_team
