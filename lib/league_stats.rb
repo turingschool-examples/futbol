cbe4f8251e6340675a96fced430cb381cbc8073a
Thomas Musselman <tmusselman@gmail.com>
2022-07-28 19:20:13 -0600

    fix: fixed worst offense method to reflect average goals instead of total goals

--- lib/league_stats.rb
+++ lib/league_stats.rb
@@ -1,7 +1,7 @@
 module LeagueStats
 
   def worst_offense
-    @teams.values.min_by{ |team| team.total_overall_goals }.team_name
+    @teams.values.min_by{ |team| team.total_overall_goals / team.games_participated_in.length }.team_name
   end
 
 end
