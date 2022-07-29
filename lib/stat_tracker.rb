3a7166370901ece28180cf9b9b85047624045b8f
Thomas Musselman <tmusselman@gmail.com>
2022-07-28 19:58:19 -0600

    add most/least tackles to the stat_tracker

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -26,4 +26,7 @@ class StatTracker
     StatTracker.new(games, teams, game_teams)
   end
 
+  def most_tackles(season) @seasons[season.to_i].most_tackles end
+  def least_tackles(season) @seasons[season.to_i].least_tackles end
+
 end
