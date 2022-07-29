ae36e7544bad8c62848593541d499acf4af71a3a
Shawn Lee <Shawnl93@hotmail.com>
2022-07-28 19:53:00 -0600

    finish more of best season

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -34,38 +34,42 @@ class StatTracker
 
   def best_season(search_team_id)
     ##search input##
-    game_info = @game_teams.select do |game_team|
+    game_info = @game_teams.find do |game_team|
       game_team[1] = search_team_id
     end
-    ## groups all games into one season
-    games_id_season = @games[:season].zip(@games[:game_id])
-    games_id_season_h = Hash.new{|hash, key| hash[key] = []}
-    games_id_season.each do |season, game|
-      games_id_season_h[season] << game
-    end
-    ## finds all the winning games
-    all_win_info = []
 
-    @game_teams.each do |game_team|
-      if game_team[:result] == "WIN" && game_team[:team_id] == search_team_id
-      all_win_info << game_team[:game_id]
+    all_win_info = []
+      @game_teams.each do |game_team|
+        if game_team[3] == "WIN" && game_team[1] == search_team_id
+          all_win_info << game_team[0]
+        end
       end
-      require "pry"; binding.pry
+    all_win_info
+
+    season_won = []
+    @games.each do |game|
+      all_win_info.each do |per_game|
+      if per_game == game[0]
+        season_won << game[1]
+        # require "pry"; binding.pry
+        end
+      end
     end
-    ## takeing 1 win from the list of wins
-    # season_won = []
-    # if all_win_info.each do |game_won|
-    #   @games.each do |game|
-    #     require "pry"; binding.pry
-    #     game[:game_id].include?(game_won) ##should be false.. but when true.. print season
-    #     end
-    #   end
-    # end
+    season_won
   end
 
 
 
 
 
-
 end
+
+
+
+
+
+  # games_id_season = @games[:season].zip(@games[:game_id])
+  # games_id_season_h = Hash.new{|hash, key| hash[key] = []}
+  # games_id_season.each do |season, game|
+  #   games_id_season_h[season] << game
+  # end
