7a712f8c216fe795332cdcb186c20d34a1560499
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-07-29 16:20:20 -0600

    refactor: moved league stats to league_stats.rb

--- lib/season_stats.rb
+++ lib/season_stats.rb
@@ -1,1 +1,10 @@
-class SeasonStats < StatTracker
+class SeasonStats
+    attr_reader :game,
+                :teams,
+                :game_teams
+    def inititalize(game, teams, game_teams)
+        @game = game
+        @teams = teams
+        @game_teams = game_teams
+    end
+end
