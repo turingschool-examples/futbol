9579b3f8f43b6692b9ef86ee906e1ac391dc56b9
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-07-29 20:39:45 -0600

    Update leauge_stats to refactored branch being worked on upon latest PR. league_stats: get rid of unnecessary instance variables, refactor methods using unnecessary instance variables to use methods, refactor #total_goals, #total_games, #best_offense, #worst_offense. add #goals_per_game. In calculable: update #id_team_hash to get rid of instance variable. In stat_tracker.spec: update league stats tests to reflect those in spec harness.

--- lib/calculable.rb
+++ lib/calculable.rb
@@ -1,9 +1,10 @@
 module Calculable
 
   def id_team_hash
-    @id_and_team = {}
+    id_and_team = {}
     @teams.each do |team|
-      @id_and_team[team[:team_id]] = team[:teamname]
+      id_and_team[team[:team_id]] = team[:teamname]
     end
+    id_and_team
   end
 end
