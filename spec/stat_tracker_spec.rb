c4dda8bd2832105f5431b768a63bf97ec7c5d605
Noah van Ekdom <noahvanekdom@gmail.com>
2022-07-30 09:48:02 -0600

    Add new tests

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -7,121 +7,121 @@ RSpec.describe StatTracker do
 
   let!(:stat_tracker) { StatTracker.from_csv(locations) }
   
-#   context 'stat_tracker instantiates' do
-#    it 'should have a class' do
-#   expect(stat_tracker).to be_a StatTracker
-#    end
+  context 'stat_tracker instantiates' do
+   it 'should have a class' do
+  expect(stat_tracker).to be_a StatTracker
+   end
 
-#    it 'self method should be an instance of the class' do
-#   expect(StatTracker.from_csv(locations)).to be_a StatTracker
-#    end
-#   end
+   it 'self method should be an instance of the class' do
+  expect(StatTracker.from_csv(locations)).to be_a StatTracker
+   end
+  end
  
-#    it '#highest_total_score' do
-#   expect(stat_tracker.highest_total_score).to eq 11
-#    end
+   it '#highest_total_score' do
+  expect(stat_tracker.highest_total_score).to eq 11
+   end
 
-#   it "#lowest_total_score" do
-#     expect(stat_tracker.lowest_total_score).to eq 0
-#   end
+  it "#lowest_total_score" do
+    expect(stat_tracker.lowest_total_score).to eq 0
+  end
 
-#   it "#percentage_home_wins" do
-#     expect(stat_tracker.percentage_home_wins).to eq 0.44
-#   end
+  it "#percentage_home_wins" do
+    expect(stat_tracker.percentage_home_wins).to eq 0.44
+  end
 
-#   it '#percentage_visitor_wins' do
-#     expect(stat_tracker.percentage_visitor_wins).to eq 0.36
-#   end
+  it '#percentage_visitor_wins' do
+    expect(stat_tracker.percentage_visitor_wins).to eq 0.36
+  end
 
-#   it '#percentage_ties' do
-#     expect(stat_tracker.percentage_ties).to eq 0.20
-#   end
+  it '#percentage_ties' do
+    expect(stat_tracker.percentage_ties).to eq 0.20
+  end
 
-#   it '#count_of_games_by_season' do
-#     expected = {
-#       "20122013"=>806,
-#       "20162017"=>1317,
-#       "20142015"=>1319,
-#       "20152016"=>1321,
-#       "20132014"=>1323,
-#       "20172018"=>1355
-#     }
-#     expect(stat_tracker.count_of_games_by_season).to eq expected
-#   end
+  it '#count_of_games_by_season' do
+    expected = {
+      "20122013"=>806,
+      "20162017"=>1317,
+      "20142015"=>1319,
+      "20152016"=>1321,
+      "20132014"=>1323,
+      "20172018"=>1355
+    }
+    expect(stat_tracker.count_of_games_by_season).to eq expected
+  end
 
-#   it '#average_goals_per_game' do
-#    expect(stat_tracker.average_goals_per_game).to eq 4.22
-#   end
+  it '#average_goals_per_game' do
+   expect(stat_tracker.average_goals_per_game).to eq 4.22
+  end
 
-#   it '#average_goals_by_season' do
-#    expected = {
-#     "20122013"=>4.12,
-#     "20162017"=>4.23,
-#     "20142015"=>4.14,
-#     "20152016"=>4.16,
-#     "20132014"=>4.19,
-#     "20172018"=>4.44
-#    }
-#    expect(stat_tracker.average_goals_by_season).to eq expected
-#   end
+  it '#average_goals_by_season' do
+   expected = {
+    "20122013"=>4.12,
+    "20162017"=>4.23,
+    "20142015"=>4.14,
+    "20152016"=>4.16,
+    "20132014"=>4.19,
+    "20172018"=>4.44
+   }
+   expect(stat_tracker.average_goals_by_season).to eq expected
+  end
 
 
-# # # # League Statistics
+# # # League Statistics
 
-#   it '#count_of_teams' do
-#    expect(stat_tracker.count_of_teams).to eq 32
-#   end
+  it '#count_of_teams' do
+   expect(stat_tracker.count_of_teams).to eq 32
+  end
 
-#   it '#best_offense' do
-#    expect(stat_tracker.best_offense).to eq "Reign FC"
-#   end
+  it '#best_offense' do
+   expect(stat_tracker.best_offense).to eq "Reign FC"
+  end
 
-#   it '#worst_offense' do
-#    expect(stat_tracker.worst_offense).to eq "Utah Royals FC"
-#   end
+  it '#worst_offense' do
+   expect(stat_tracker.worst_offense).to eq "Utah Royals FC"
+  end
 
-#   it '#highest_scoring_visitor' do
-#    expect(stat_tracker.highest_scoring_visitor).to eq "FC Dallas"
-#   end
+  it '#highest_scoring_visitor' do
+   expect(stat_tracker.highest_scoring_visitor).to eq "FC Dallas"
+  end
 
-#   it '#highest_scoring_home_team' do
-#    expect(stat_tracker.highest_scoring_home_team).to eq "Reign FC"
-#   end
+  it '#highest_scoring_home_team' do
+   expect(stat_tracker.highest_scoring_home_team).to eq "Reign FC"
+  end
 
-#   it '#lowest_scoring_visitor' do
-#    expect(stat_tracker.lowest_scoring_visitor).to eq "San Jose Earthquakes"
-#   end
+  it '#lowest_scoring_visitor' do
+   expect(stat_tracker.lowest_scoring_visitor).to eq "San Jose Earthquakes"
+  end
 
-#   it '#lowest_scoring_home_team' do
-#    expect(stat_tracker.lowest_scoring_home_team).to eq "Utah Royals FC"
-#   end
+  it '#lowest_scoring_home_team' do
+   expect(stat_tracker.lowest_scoring_home_team).to eq "Utah Royals FC"
+  end
 
-# # # # Season Statistics
+# # # Season Statistics
 
-#   it '#winningest_coach' do
-#     expect(stat_tracker.winningest_coach).to eq("Dan Lacroix")
-#   end
+  it '#winningest_coach' do
+    expect(stat_tracker.winningest_coach).to eq("Dan Lacroix")
+  end
 
-#   it '#worst_coach' do
-#     expect(stat_tracker.worst_coach).to eq("Martin Raymond")
-#   end
+  it '#worst_coach' do
+    expect(stat_tracker.worst_coach).to eq("Martin Raymond")
+  end
 
-#   it '#most_accurate_team' do
-#     expect(stat_tracker.most_accurate_team).to eq("Chicago Red Stars")
-#   end
+  it '#most_accurate_team' do
+    expect(stat_tracker.most_accurate_team).to eq("Chicago Red Stars")
+  end
 
-#   it '#least_accurate_team' do
-#     expect(stat_tracker.least_accurate_team).to eq("New England Revolution")
-#   end
+  it '#least_accurate_team' do
+    expect(stat_tracker.least_accurate_team).to eq("New England Revolution")
+  end
 
-#   it '#most_tackles' do
+  it '#most_tackles' do
 
-#     expect(stat_tracker.most_tackles).to eq("Orlando Pride")
-#   end
+    expect(stat_tracker.most_tackles).to eq("Orlando Pride")
+  end
 
-#   it '#fewest_tackles' do
-#     expect(stat_tracker.fewest_tackles).to eq("Philadelphia Union")
-#   end
+  it '#fewest_tackles' do
+    expect(stat_tracker.fewest_tackles).to eq("Philadelphia Union")
+  end
 
 # # # Team Statistics
 
