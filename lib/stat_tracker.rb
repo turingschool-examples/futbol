7025713c08c35ee44e4e22b39725c35f0a2376e9
Aleisha Mork <aleisha.mork@gmail.com>
2022-07-31 17:45:21 -0600

    updtated coach by team id to include all coaches by season

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -215,7 +215,19 @@ class StatTracker
 
   def coach_by_team_id #Provides hash of coach names by team id - Helper method for issue #27
     # Example hash: {3=>"John Tortorella", 6=>"Claude Julien",  5=>"Dan Bylsma",
-    @game_teams.values_at(:team_id, :head_coach).to_h
+    coaches = Hash.new {|h, k| h[k] = {}}
+    games_by_season.each do |season, games|
+      @game_teams.each do |row|
+        if games.include?(row[:game_id])
+          if !coaches[row[:team_id]].keys.include?(season)
+            coaches[row[:team_id]][season] = [row[:head_coach]]
+          elsif !coaches[row[:team_id]][season].include?(row[:head_coach])
+            coaches[row[:team_id]][season] << row[:head_coach]
+          end
+        end
+      end
+    end
+    coaches
   end
 
   def team_win_percent_by_season # Provides hash of seasons with array of hashes for team id
