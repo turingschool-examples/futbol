726c9634c8c0292edca249271372eb062f4fd331
Chris Lyman <christopher.lyman0@gmail.com>
2022-08-01 19:19:21 -0400

    create team_spec

---
+++ spec/team_spec.rb
@@ -0,0 +1,54 @@
+require './lib/team'
+
+RSpec.describe Team do
+  before do
+    info = {
+      team_id: "1",
+      franchiseid: "23",
+      teamname: "Atlanta United",
+      abbreviation: "ATL",
+      stadium: "Mercedes_Benz Stadium",
+      link: "api/v1/teams/1"
+    }
+    @team = Team.new(info)
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+
+      expect(@team).to be_an_instance_of(Team)
+    end
+  end
+
+  describe '@attributes' do
+    it 'has a team id' do
+
+      expect(@team.team_id).to eq("1")
+    end
+
+    it 'has a franchise id' do
+
+      expect(@team.franchise_id).to eq("23")
+    end
+
+    it 'has a team name' do
+
+      expect(@team.team_name).to eq("Atlanta United")
+    end
+
+    it 'has an abbreviation' do
+
+      expect(@team.abbreviation).to eq("ATL")
+    end
+
+    it 'has a stadium' do
+
+      expect(@team.stadium).to eq("Mercedes_Benz Stadium")
+    end
+
+    it 'has a link' do
+
+      expect(@team.link).to eq("api/v1/teams/1")
+    end
+  end
+end
