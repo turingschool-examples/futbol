8e06c644d542ee173bed8ece69ecf4b16ecb376c
Erik Riggs <riggs.erik@gmail.com>
2022-08-01 16:02:19 -0600

    updated spec_helper to run simplecov

--- lib/team.rb
+++ lib/team.rb
@@ -2,8 +2,8 @@ class Team
   attr_reader :team_id, :franchise_id, :team_name, :abbv, :link
   def initialize(row)
     @team_id = row[:team_id]
-    @franchise_id = row[:franchiseId]
-    @team_name = row[:teamName]
+    @franchise_id = row[:franchiseid]
+    @team_name = row[:teamname]
     @abbv = row[:abbreviation]
     @link = row[:link]
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,7 @@
 require 'simplecov'
 SimpleCov.start
-
+# puts "SimpleCov started successfully!"
 require_relative 'stat_tracker_spec'
+require_relative 'team_spec'
+require_relative 'game_spec'
+require_relative 'game_team_spec'
