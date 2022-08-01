7a7bdd52cf21058c8865b709be6b10b44979510e
A.J. Krumholz <krumholz.alan@gmail.com>
2022-08-01 12:39:46 -0400

    fix instance variable usage

--- lib/season_statistics.rb
+++ lib/season_statistics.rb
@@ -6,53 +6,50 @@ class SeasonStatistics
   include Seasonable
   include TeamNameable
 
-  attr_reader :season
-
-  def initialize(locations, season)
-    @season = season
+  def initialize(locations)
     @locations = locations
     @games_data = CSV.read(@locations[:games], headers: true, header_converters: :symbol)
     @teams_data = CSV.read(@locations[:teams], headers: true, header_converters: :symbol)
     @game_teams_data = CSV.read(@locations[:game_teams], headers: true, header_converters: :symbol)
   end
 
-  def self.from_csv(locations, season)
-    SeasonStatistics.new(locations, season)
-  end
+  # def self.from_csv(locations)
+  #   SeasonStatistics.new(locations)
+  # end
 
-  def winningest_coach
-    records = coach_records(@season)
-    populate_coach_records(@season, records)
+  def winningest_coach(season)
+    records = coach_records(season)
+    populate_coach_records(season, records)
     winning_record(records).max_by { |team, win_percent| win_percent }[0].to_s
   end
 
-  def worst_coach
-    records = coach_records(@season)
-    populate_coach_records(@season, records)
+  def worst_coach(season)
+    records = coach_records(season)
+    populate_coach_records(season, records)
     winning_record(records).min_by { |team, win_percent| win_percent }[0].to_s
   end
 
-  def most_accurate_team
-    records = accuracy_records(@season)
-    populate_accuracy_records(@season, records)
+  def most_accurate_team(season)
+    records = accuracy_records(season)
+    populate_accuracy_records(season, records)
     find_team_name_by_id(most_accurate(records))
   end
 
-  def least_accurate_team
-    records = accuracy_records(@season)
-    populate_accuracy_records(@season, records)
+  def least_accurate_team(season)
+    records = accuracy_records(season)
+    populate_accuracy_records(season, records)
     find_team_name_by_id(least_accurate(records))
   end
 
-  def most_tackles
-    records = tackle_records(@season)
-    populate_tackle_records(@season, records)
+  def most_tackles(season)
+    records = tackle_records(season)
+    populate_tackle_records(season, records)
     find_team_name_by_id(best_tackling_team(records))
   end
 
-  def fewest_tackles
-    records = tackle_records(@season)
-    populate_tackle_records(@season, records)
+  def fewest_tackles(season)
+    records = tackle_records(season)
+    populate_tackle_records(season, records)
     find_team_name_by_id(worst_tackling_team(records))
   end
 end
