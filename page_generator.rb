e37a9a80b3e1b69bfa7f889adb4f94da94d3d7c9
Thomas Musselman <tmusselman@gmail.com>
2022-08-01 13:01:03 -0600

    remove dummy file path from main and add gitignore

--- page_generator.rb
+++ page_generator.rb
@@ -1,9 +1,9 @@
 require './lib/stat_tracker'
 require 'erb'
 
-game_path = './data/games_dummy.csv'
-team_path = './data/teams_dummy.csv'
-game_teams_path = './data/game_teams_dummy.csv'
+game_path = './data/games.csv'
+team_path = './data/teams.csv'
+game_teams_path = './data/game_teams.csv'
 
 locations = {
   games: game_path,
