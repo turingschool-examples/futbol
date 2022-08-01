61a3b2c542801185cef2a4e8d866f54297f9cf96
Carter Ball <carter.k.ball@gmail.com>
2022-08-01 10:03:38 -0400

    Set up GameTeamsStats class and get it passing existence test

---
+++ spec/game_teams_stats_spec.rb
@@ -0,0 +1,25 @@
+require './lib/game_teams_stats'
+require './lib/game'
+require './lib/game_teams'
+require './lib/teams'
+
+describe GameTeamsStats do
+  before :each do
+    game_teams_path = "./data/game_teams.csv"
+    @game_teams_stats = GameTeamsStats.from_csv(game_teams_path)
+  end
+  
+  it 'exists' do
+    expect(@game_teams_stats).to be_a(GameTeamsStats)
+  end
+
+  it "can calculate which team had the best offense" do
+    expect(@game_teams_stats.best_offense).to eq "Reign FC"
+  end
+
+
+
+
+
+
+end
