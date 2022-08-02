0e4e9503bd0d6f1e9099f1aaa500934cc6117b75
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-08-01 19:10:30 -0600

    All methods moved to game teams stats except for two

--- lib/game_teams_stats.rb
+++ lib/game_teams_stats.rb
@@ -17,6 +17,42 @@ class GameTeamsStats
     GameTeamsStats.new(game_teams_as_objects)
   end
 
-  #methods go here
+  def most_goals_scored(team_id)
+    goals_by_game = []
+    @game_teams.each do |game|
+      goals_by_game << game.goals.to_i if team_id == game.team_id
+    end
+    goals_by_game.max
+  end
 
+  def fewest_goals_scored(team_id)
+    goals_by_game = []
+    @game_teams.each do |game|
+      goals_by_game << game.goals.to_i if team_id == game.team_id
+    end
+    goals_by_game.min
+  end
+
+
+  def best_offense
+    team_scores = Hash.new { |h, k| h[k] = [] }
+    @game_teams.each { |game_team| team_scores[game_team.team_id] << game_team.goals.to_f }
+
+    team_scores_average =
+      team_scores.map do |id, scores|
+        [id, ((scores.sum) / (scores.length)).round(2)] #create an average out of the scores
+      end
+  end
+
+  def worst_offense
+    team_scores = Hash.new { |h, k| h[k] = [] }
+    @game_teams.each { |game_team| team_scores[game_team.team_id] << game_team.goals.to_f }
+    team_scores_average =
+      team_scores.map do |id, scores|
+        [id, ((scores.sum) / (scores.length)).round(2)] #creat an average out of the scores
+      end
+  end
+
+
+  #methods go here
 end
--- spec/game_teams_stats_spec.rb
+++ spec/game_teams_stats_spec.rb
@@ -14,8 +14,22 @@ describe GameTeamsStats do
     expect(@game_teams_stats).to be_a(GameTeamsStats)
   end
 
-  xit "can calculate which team had the best offense" do
-    expect(@game_teams_stats.best_offense).to eq "Reign FC"
+  it "can tell the most goals a team has scored in a game across all seasons" do
+    expect(@game_teams_stats.most_goals_scored("18")).to eq(7)
+  end
+
+  it "can tell the fewest goals a team has scored in a game across all seasons" do
+    expect(@game_teams_stats.fewest_goals_scored("18")).to eq(0)
+  end
+
+  it "can calculate which team had the best offense" do
+    expect(@game_teams_stats.best_offense.size).to eq 32
+  end
+
+  it "can calculate which team had the worst offense" do
+    expect(@game_teams_stats.worst_offense.size).to eq 32
+    expect(@game_teams_stats.worst_offense[0][0].to_i).to eq 3
+
   end
 
 end
