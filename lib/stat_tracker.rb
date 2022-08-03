5b893a264477b4c69460a50cd122129b939b1eab
Krisetn Nestler <kristen.nestler@gmail.com>
2022-08-03 01:51:58 -0400

    fix bugs on StatTracker

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -22,35 +22,35 @@ class StatTracker
   end
 
   def highest_total_score
-    @games_stats.highest_total_score
+    @game_stats.highest_total_score
   end
 
   def lowest_total_score
-    @games_stats.lowest_total_score
+    @game_stats.lowest_total_score
   end
 
   def percentage_home_wins 
-    @games_stats.percentage_home_wins
+    @game_stats.percentage_home_wins
   end
 
   def percentage_visitor_wins
-    @games_stats.percentage_visitor_wins
+    @game_stats.percentage_visitor_wins
   end
 
   def percentage_ties 
-    @games_stats.percentage_ties
+    @game_stats.percentage_ties
   end
 
   def count_of_games_by_season
-    @games_stats.count_of_games_by_season
+    @game_stats.count_of_games_by_season
   end
 
   def average_goals_per_game
-    @games_stats.average_goals_per_game
+    @game_stats.average_goals_per_game
   end
 
   def average_goals_by_season
-    @games_stats.average_goals_by_season
+    @game_stats.average_goals_by_season
   end
 
   def count_of_teams
@@ -99,7 +99,8 @@ class StatTracker
 
   def most_tackles(season_id)
     @season_stats.most_tackles(season_id)
-
+  end 
+  
   def fewest_tackles(season_id)
     @season_stats.fewest_tackles(season_id)
   end
