5492073782a3b6c741aff517b05e501c19f85670
Chris Lyman <christopher.lyman0@gmail.com>
2022-08-02 23:46:12 -0400

    feat/add tests for home, vistor and tie

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -75,4 +75,25 @@ RSpec.describe Game do
       expect(@game.venue_link).to eq("/api/v1/venues/null")
     end
   end
+
+  describe '#home_win?' do
+    it 'returns true with a home win' do
+
+      expect(@game.home_win?).to be true
+    end
+  end
+
+  describe '#visitor_win?' do
+    it 'returns true with a visitor win' do
+
+      expect(@game.visitor_win?).to be false
+    end
+  end
+
+  describe '#tie' do
+    it "returns true with a tie" do
+
+      expect(@game.tie?).to be false
+    end
+  end
 end
