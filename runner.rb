10fb10fdbd880d9ef66ca3aca4ed6543ec0f8bd8
Sid Mann <sjmann2@outlook.com>
2022-08-03 09:32:11 -0500

    add p methods to runner file

--- runner.rb
+++ runner.rb
@@ -13,10 +13,10 @@ locations = {
 
 stat_tracker = StatTracker.from_csv(locations)
 
-p stat_tracker.highest_scoring_home_team
-p stat_tracker.lowest_scoring_home_team
-p stat_tracker.most_accurate_team("20142015")
-p stat_tracker.most_goals_scored("18")
-p stat_tracker.best_offense
+puts stat_tracker.highest_scoring_home_team
+puts stat_tracker.lowest_scoring_home_team
+puts stat_tracker.most_accurate_team("20142015")
+puts stat_tracker.most_goals_scored("18")
+puts stat_tracker.best_offense
 
 
