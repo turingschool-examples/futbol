4c40b32d088aa51dbde850a17b398be63ab50321
Bryan Keener <bkeener7@gmail.com>
2022-09-12 15:52:25 -0600

    Create stat_tracker.rb and modifications to runner.rb

---
+++ lib/stat_tracker.rb
@@ -0,0 +1,7 @@
+class StatTracker
+
+    def self.from_csv(location)
+        location        
+    end
+    
+end
---
+++ runner.rb
@@ -0,0 +1,16 @@
+# runner.rb
+require './lib/stat_tracker'
+require 'csv'
+
+game_path = CSV.open './data/games.csv', headers: true, header_converters: :symbol
+team_path = CSV.open './data/teams.csv', headers: true, header_converters: :symbol
+game_teams_path = CSV.open './data/game_teams.csv', headers: true, header_converters: :symbol
+
+locations = {
+  games: game_path,
+  teams: team_path,
+  game_teams: game_teams_path
+}
+
+stat_tracker = StatTracker.from_csv(locations)
+require 'pry'; binding.pry
