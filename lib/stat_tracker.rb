9b1d11d69abe0320e732475044746b859ebe3519
Joseph Hilby <joseph.hilby@gmail.com>
2022-09-12 16:52:33 -0500

    WIP add from_csv method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,4 +1,18 @@
+require 'csv'
+
 class StatTracker
+  attr_reader :games, :teams, :game_teams
   def initialize
+    @games = nil
+    @teams = nil
+    @game_teams = nil
+  end
+
+  def self.from_csv(csv_hash)
+    @games = CSV.open csv_hash[:games], headers: true, header_converters: :symbol
+    @teams = CSV.open csv_hash[:teams], headers: true, header_converters: :symbol
+    @game_teams = CSV.open csv_hash[:game_teams], headers: true, header_converters: :symbol
   end
 end
+
+# csv_preview_data = CSV.foreach(csv_path, headers: false).take(100)
