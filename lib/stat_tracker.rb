daf860d281957b25bfef731a0faaf9a79594a2fe
Annie Pulzone <ajpulzone@gmail.com>
2022-09-12 20:07:10 -0700

    Test: StatTracker parse test debugging

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,3 +1,4 @@
+require "csv"
 
 class StatTracker
   attr_reader :teams,
@@ -11,5 +12,15 @@ class StatTracker
   end
 
   def self.from_csv()
+  end
+
+  def self.team_import(file_name)
+    content = CSV.open file_name, headers: false, header_converters: :symbol
+  # require "pry";binding.pry
+    content.each do |line|
+          require "pry";binding.pry
+    @teams << Team.new(line)
+    end
+
   end
 end
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,4 +1,6 @@
+require "rspec"
 require './lib/stat_tracker'
+require "./lib/team"
 
 RSpec.describe StatTracker do
   let(:stat_tracker) {StatTracker.new}
@@ -12,5 +14,11 @@ RSpec.describe StatTracker do
     expect(stat_tracker.games).to eq([])
     expect(stat_tracker.game_teams).to eq([])
   end
-end
 
+  it "3. can parse CSV data" do
+    # content = CSV.open "./data/team_dummy.csv", headers: true, header_converters: :symbol
+    # expect(File).to receive(:open).with("./data/team_dummy.csv", "r", headers: true, header_converters: :symbol, :universal_newline=>false).and_return(content)
+    StatTracker.team_import("./data/team_dummy.csv")
+    expect(stat_tracker_1.teams[1]).to eq([1, 23, "Atlanta United", "ATL" , "Mercedes-Benz Stadium" , "/api/v1/teams/1"])
+  end
+end
