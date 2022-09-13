aaf025d6078fa71f3c4983a55099ff1a37fa6868
Bryan Flanagan <bflanagan78@yahoo.com>
2022-09-13 16:39:05 -0600

    Add total_games helper method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -13,18 +13,19 @@ class StatTracker
     games_input = CSV.read(csv_hash[:games], headers: true, header_converters: :symbol)
     teams_input = CSV.read(csv_hash[:teams], headers: true, header_converters: :symbol)
     game_teams_input = CSV.read(csv_hash[:game_teams], headers: true, header_converters: :symbol)
-    stats_tracker = StatTracker.new(games_input, teams_input, game_teams_input)
+    stat_tracker = StatTracker.new(games_input, teams_input, game_teams_input)
   end
   
   def total_games
-    games_teams.length
+    @games.count
+    # binding.pry
   end
 
-  def ties
-    games_teams.each {|game_team| game_team[:result]}
-      if game_team == TIE 
-    end
-  end
+  # def ties
+  #   game_teams.each {|game_team| game_team[:result]}
+  #     if game_team == TIE 
+  #   end
+  # end
 
   # take game_teams -> HoA = home -> if win, +=1. Divide by total number of games, multiply by 100.
   # def percentage_home_wins
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -3,27 +3,51 @@ require 'pry'
 require './lib/stat_tracker'
 
 RSpec.describe StatTracker do
-  it "Test 1: exists" do
-    tracker = StatTracker.new
+  before(:each) do
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+  
+    @stat_tracker = StatTracker.from_csv(locations)
+  end
+
+  it "exists" do
+    tracker = StatTracker.new('file_games', 'file_teams', 'file_game_teams')
 
     expect(tracker).to be_a(StatTracker)
   end
 
-  it "has a total number of games played" do
-    tracker = StatTracker.new
+  it "has readable attributes" do
+    tracker = StatTracker.new('file_games', 'file_teams', 'file_game_teams')
 
-    expect(tracker.total_games).to eq 14882
+    expect(tracker.games).to eq('file_games')
+    expect(tracker.teams).to eq('file_teams')
+    expect(tracker.game_teams).to eq('file_game_teams')
+  end
+
+  it "has a total number of games played" do
+    # tracker = StatTracker.new('file_games', 'file_teams', 'file_game_teams')
+    
+    # binding.pry
+    expect(@stat_tracker.total_games).to eq 7441
+  #  binding.pry
   end
 
   # it '#percentage_ties' do
-  #   tracker = StatTracker.new
+  #   tracker = StatTracker.new('file_games', 'file_teams', 'file_game_teams')
 
   #   expect(tracker.percentage_ties).to eq .20
   # end
 
 
   # it "#percentage_home_wins" do
-  #   tracker = StatTracker.new
+  #   tracker = StatTracker.new('file_games', 'file_teams', 'file_game_teams')
 
   #   expect(tracker.percentage_home_wins).to eq .44
   # end
