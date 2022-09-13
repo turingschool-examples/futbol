448ed8695ae21c085b05efae49c6f1117a972b96
Sean Culliton <smculliton@gmail.com>
2022-09-13 13:55:49 -0600

    Create spec files

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -15,11 +15,22 @@ RSpec.describe StatTracker do
 
     @stat_tracker = StatTracker.from_csv(locations)
   end
+
   describe '#initialize' do
   end
+
   describe '#highest_total_score' do
     it 'returns highest total score of seasons games' do
       expect(@stat_tracker.highest_total_score).to eq(5)
+    end
+  end
+
+  describe '#return_column' do 
+    it 'is an integer' do 
+      # allow(fake_data).to receive(:)
+      # return_column(fake_data, header)
+      
+
     end
   end
 end
