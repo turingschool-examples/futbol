484a8295eea21b400fc43aa7da2c2560487d13c7
Amanda Ross <amikaross@gmail.com>
2022-09-13 13:56:41 -0600

    Add sorted_wins_by_coach and games_by_season methods

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -39,7 +39,6 @@ class StatTracker
 
   def total_games
     @games_data.count
-    
   end
 
   def percentage_home_wins
@@ -52,5 +51,35 @@ class StatTracker
 
   def percentage_ties
     (total_ties / total_games.to_f).round(2)
+  end
+
+  def winningest_coach(season_id)
+    sorted_wins_by_coach(season_id)[0][0]
+  end
+
+  def worst_coach(season_id)
+    sorted_wins_by_coach(season_id)[-1][0]
+  end
+
+  def games_by_season
+    games_by_season = Hash.new(0)
+    @games.each do |row|
+      if games_by_season.keys.include?(row["season"])
+        games_by_season[row["season"]] << row["game_id"]
+      else 
+        games_by_season[row["season"]] = [row["game_id"]]
+      end 
+    end
+    games_by_season
+  end
+
+  def sorted_wins_by_coach(season_id) 
+    wins_by_coach = Hash.new(0)
+    @game_teams_data.each do |row|
+      row["head_coach"].each do |coach|
+        wins_by_coach[coach] += 1 if row["result"] == "WIN"
+      end
+    end
+    wins_by_coach.sort_by { |coach, wins| wins }.reverse
   end
 end
