df63153d2490c820d2725f3cbd0ddbe1b8399ac9
Joseph Hilby <joseph.hilby@gmail.com>
2022-09-13 12:59:53 -0500

    Fix from_csv method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,20 +1,17 @@
 require 'csv'
 
 class StatTracker
-  def initialize
+  attr_reader :games, :teams, :game_teams
+  def initialize(games, teams, game_teams)
+    @games = games
+    @teams = teams
+    @game_teams = game_teams
   end
 
   def self.from_csv(csv_hash)
-    # @games_input = CSV.open csv_hash[:games], headers: true, header_converters: :symbol
-    @games_input = CSV.foreach(csv_hash[:games], headers: true, header_converters: :symbol).take(200)
-    # @teams_input = CSV.open csv_hash[:teams], headers: true, header_converters: :symbol
-    @teams_input = CSV.foreach(csv_hash[:teams], headers: true, header_converters: :symbol).take(200)
-    # @game_teams_input = CSV.open csv_hash[:game_teams], headers: true, header_converters: :symbol
-    @game_teams_input = CSV.foreach(csv_hash[:game_teams], headers: true, header_converters: :symbol).take(200)
-    @stats = {
-      games: @games_input.map {|row| row},
-      teams: @teams_input.map {|row| row},
-      game_teams: @game_teams_input.map {|row| row}
-    }
+    games_input = CSV.read(csv_hash[:games], headers: true, header_converters: :symbol).take(200)
+    teams_input = CSV.read(csv_hash[:teams], headers: true, header_converters: :symbol).take(200)
+    game_teams_input = CSV.read(csv_hash[:game_teams], headers: true, header_converters: :symbol).take(200)
+    stats_tracker = StatTracker.new(games_input.map {|row| row}, teams_input.map {|row| row}, game_teams_input.map {|row| row})
   end
 end
