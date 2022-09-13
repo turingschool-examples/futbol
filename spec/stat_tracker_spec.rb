3fbb9951ecdb26038e160aedca0367f1bc9ad350
Krisetn Nestler <kristen.nestler@gmail.com>
2022-09-12 23:24:44 -0400

    feat: highest total score and fix: retrival of data

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -19,31 +19,23 @@ RSpec.describe StatTracker do
 
   describe '#initialize' do
     it 'exists' do
-      stat_tracker = StatTracker.new
-      expect(stat_tracker).to be_instance_of StatTracker
-    end
-
-    it 'has no game data to begin' do
-      stat_tracker = StatTracker.new
-      expect(stat_tracker.game_data).to eq nil
-    end
-    
-    it 'has no team data to begin' do
-      stat_tracker = StatTracker.new
-      expect(stat_tracker.team_data).to eq nil
-    end
-    
-    it 'has no game team data to begin' do
-      stat_tracker = StatTracker.new
-      expect(stat_tracker.game_teams_data).to eq nil
+      # stat_tracker = StatTracker.new
+      expect(@stat_tracker).to be_instance_of StatTracker
     end
   end
-  
+
   describe '#self.from_csv' do
     it 'makes data accessible in class' do
-      expect(@stat_tracker[0]).to be_instance_of CSV
-      expect(@stat_tracker[1]).to be_instance_of CSV
-      expect(@stat_tracker[2]).to be_instance_of CSV
+      expect(@stat_tracker.games_data).to be_instance_of CSV
+      expect(@stat_tracker.teams_data).to be_instance_of CSV
+      expect(@stat_tracker.game_teams_data).to be_instance_of CSV
     end
   end
+
+  describe '#game stats' do
+    it 'can calculate the highest sum of the winning and losing teams scores' do 
+        expect(@stat_tracker.highest_total_score).to eq 11
+    end
+  end
+
 end
