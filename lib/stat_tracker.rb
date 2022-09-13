e7872d9c3f0b6854dd382bc07803fc8207c8ab88
Krisetn Nestler <kristen.nestler@gmail.com>
2022-09-13 10:06:40 -0400

    fix: initialize block locations

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -3,10 +3,10 @@ require 'csv'
 class StatTracker
   attr_reader :games_data, :teams_data, :game_teams_data
   def initialize(locations)
-    @locations = locations
-    @games_data = CSV.read(@locations[:games], headers: true, header_converters: :symbol)
-    @teams_data = CSV.read(@locations[:teams], headers: true, header_converters: :symbol)
-    @game_teams_data = CSV.read(@locations[:game_teams], headers: true, header_converters: :symbol)
+    @games_data = CSV.read(locations[:games], headers: true, header_converters: :symbol)
+    @teams_data = CSV.read(locations[:teams], headers: true, header_converters: :symbol)
+    @game_teams_data = CSV.read(locations[:game_teams], headers: true, header_converters: :symbol)
+  
   end
   
   def self.from_csv(locations)
