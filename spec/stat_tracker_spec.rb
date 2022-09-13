3ba1d8a135c9d57d77937e277df49224ea989329
Darby Smith <newman.darby2@gmail.com>
2022-09-12 20:54:15 -0400

    Tests for initialize and self.from_csv methods

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,38 +1,49 @@
 require 'rspec'
+require 'simplecov'
 require './lib/stat_tracker'
 
 RSpec.describe StatTracker do
+  before(:all) do
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    @stat_tracker = StatTracker.from_csv(locations)
+  end
+
   describe '#initialize' do
     it 'exists' do
       stat_tracker = StatTracker.new
       expect(stat_tracker).to be_instance_of StatTracker
     end
 
-    it '' do
-
+    it 'has no game data to begin' do
+      stat_tracker = StatTracker.new
+      expect(stat_tracker.game_data).to eq nil
+    end
+    
+    it 'has no team data to begin' do
+      stat_tracker = StatTracker.new
+      expect(stat_tracker.team_data).to eq nil
+    end
+    
+    it 'has no game team data to begin' do
+      stat_tracker = StatTracker.new
+      expect(stat_tracker.game_teams_data).to eq nil
     end
   end
   
-  
   describe '#self.from_csv' do
     it 'makes data accessible in class' do
-      game_path = './data/games.csv'
-      team_path = './data/teams.csv'
-      game_teams_path = './data/game_teams.csv'
-
-      locations = {
-        games: game_path,
-        teams: team_path,
-        game_teams: game_teams_path
-      }
-
-      StatTracker.from_csv(locations)
-      # expect(@game_data).to be CSV
-      # expect(@team_data).to be CSV
-      # expect(@game_team_data).to be CSV
-      expect(@game_data).not_to eq nil
-      expect(@team_data).not_to eq nil
-      expect(@game_team_data).not_to eq nil
+      expect(@stat_tracker[0]).to be_instance_of CSV
+      expect(@stat_tracker[1]).to be_instance_of CSV
+      expect(@stat_tracker[2]).to be_instance_of CSV
     end
   end
 end
