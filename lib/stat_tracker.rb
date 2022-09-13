49354407c789cbf821ccc115b59c65f876cbe909
Sean Culliton <smculliton@gmail.com>
2022-09-13 11:19:46 -0600

    create add_rows method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -12,6 +12,18 @@ class StatTracker
    games = CSV.open locations[:games], headers: true, header_converters: :symbol
    teams = CSV.open locations[:teams], headers: true, header_converters: :symbol
    game_teams = CSV.open locations[:game_teams], headers: true, header_converters: :symbol
-   StatTracker.new(games, teams, game_teams)
+   make_rows(games, teams, game_teams)
+  end
+
+  def self.make_rows(games, teams, game_teams)
+    
+    games_rows = []
+
+    games.each do |row|
+      games_rows << row
+    end
+    require 'pry'; binding.pry
+    StatTracker.new(games_rows, teams, game_teams)
+
   end
 end
