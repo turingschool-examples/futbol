e3031c14109cc63a331bd46dd1072eaf154f84b4
Naomi Yocum <naomiyocum24@gmail.com>
2022-09-13 09:01:12 -0600

    feat: add locations arg to intialize, complete highest_total_score

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,27 +1,25 @@
 require 'csv'
 
 class StatTracker
-  attr_reader :games, 
+  attr_reader :games,
               :teams,
               :game_teams
 
-  def initialize
-    @games = CSV.read './data/games.csv', headers: true, header_converters: :symbol
-    @teams = CSV.read './data/teams.csv', headers: true, header_converters: :symbol
-    @game_teams = CSV.read './data/game_teams.csv', headers: true, header_converters: :symbol
+  def initialize(locations)
+    @games = CSV.read locations[:games], headers: true, header_converters: :symbol
+    @teams = CSV.read locations[:teams], headers: true, header_converters: :symbol
+    @game_teams = CSV.read locations[:game_teams], headers: true, header_converters: :symbol
   end
 
   def self.from_csv(locations)
     StatTracker.new(locations)
   end
 
-  # def return_game_id
-  #   game_id = []
-  #   @games.each do |game|
-  #     game_id << 
-  #   end
-  # end
+  def highest_total_score
+    scores = @games.map do |row|
+      row[:away_goals].to_i + row[:home_goals].to_i
+    end
+    scores.max
+  end
+
 end
-
-
-
