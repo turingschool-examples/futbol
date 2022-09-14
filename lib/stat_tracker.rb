016b0255e2d3df2a193b1ea581753aa825e83456
Joseph Hilby <joseph.hilby@gmail.com>
2022-09-14 14:17:17 -0500

    Add comments

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -29,7 +29,8 @@ class StatTracker
     lowest_scoring_game[:away_goals].to_i + lowest_scoring_game[:home_goals].to_i
   end
 
-  # recomend move methids like this to a mixin 'calculator'
+  # Method is used in average_scores_for_all_visitors & average_scores_for_all_home_teams
+  # Recomend refactor by mixin 'calculator'
   def average_score_per_game(game_teams_selection)
     goals = game_teams_selection.sum {|game| game[:goals].to_f}
     # You need to / 2. The game_teams CSV has 2 lines to represent one game.
@@ -37,7 +38,8 @@ class StatTracker
     goals / games
   end
 
-  def away_games
+  # Method is used in average_scores_for_all_visitors
+  def away_games_by_team_id
     away_games_list = @game_teams.find_all {|game| game[:hoa] == "away"}
     away_games_hash = Hash.new([])
     away_games_list.each do |game|
@@ -46,7 +48,8 @@ class StatTracker
     away_games_hash
   end
 
-  def home_games
+  # Method is used in used in average_scores_for_all_home_teams
+  def home_games_by_team_id
     home_games_list = @game_teams.find_all {|game| game[:hoa] == "home"}
     home_games_hash = Hash.new([])
     home_games_list.each do |game|
@@ -55,14 +58,25 @@ class StatTracker
     home_games_hash
   end
 
+  # Method is used in highest_scoring_visitor & lowest_scoring_visitor
   def average_scores_for_all_visitors
     @visitor_hash = {}
-    away_games.each do |team_id, games_array|
+    away_games_by_team_id.each do |team_id, games_array|
       @visitor_hash[team_id] = average_score_per_game(games_array)
     end
     @visitor_hash
   end
 
+  # Method is used in highest_scoring_home_team & lowest_scoring_home_team
+  def average_scores_for_all_home_teams
+    @home_hash = {}
+    home_games_by_team_id.each do |team_id, games_array|
+      @home_hash[team_id] = average_score_per_game(games_array)
+    end
+    @home_hash
+  end
+
+  # Origional method from Iteration 2
   def highest_scoring_visitor
     average_scores_for_all_visitors
     highest_scoring_team = @teams.find do |team|
@@ -71,20 +85,30 @@ class StatTracker
     highest_scoring_team[:teamname]
   end
 
+  # Origional method from Iteration 2
   def lowest_scoring_visitor
     average_scores_for_all_visitors
     lowest_scoring_team = @teams.find do |team|
-      # require 'pry'; binding.pry
       team[:team_id].to_i == @visitor_hash.key(@visitor_hash.values.min)
     end
     lowest_scoring_team[:teamname]
   end
 
+  # Origional method from Iteration 2
   def highest_scoring_home_team
-
+    average_scores_for_all_home_teams
+    highest_scoring_team = @teams.find do |team|
+      team[:team_id].to_i == @home_hash.key(@home_hash.values.max)
+    end
+    highest_scoring_team[:teamname]
   end
 
+  # Origional method from Iteration 2
   def lowest_scoring_home_team
-
+    average_scores_for_all_home_teams
+    highest_scoring_team = @teams.find do |team|
+      team[:team_id].to_i == @home_hash.key(@home_hash.values.min)
+    end
+    highest_scoring_team[:teamname]
   end
 end
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -16,6 +16,7 @@ RSpec.describe StatTracker do
 
     @stat_tracker = StatTracker.from_csv(locations)
   end
+
   it "exists" do
     tracker = StatTracker.new('file input 1', 'file input 2', 'file input 3')
 
@@ -30,37 +31,52 @@ RSpec.describe StatTracker do
     expect(tracker.game_teams).to eq('file input 3')
   end
 
-  it "can find the highest scoring game" do
+  it "can find the highest_total_score" do
     expect(@stat_tracker.highest_total_score).to eq(11)
   end
 
-  it "can find the lowest scoring game" do
+  it "can find the lowest_total_score" do
     expect(@stat_tracker.lowest_total_score).to eq(0)
   end
 
-  it "can find the average score per game" do
+  it "can find the average_score_per_game" do
+    # Stat_tracker has 2 lines / game. That is wy there are 10 lines and only 5.0 games.
     expect(@stat_tracker.average_score_per_game(@stat_tracker.game_teams.take(10))).to eq(22.0/5.0)
   end
 
-  xit "can list away games in a hash" do
-    expect(@stat_tracker.average_score_per_game(@stat_tracker.game_teams.take(10))).to eq(22.0/5.0)
+  it "can list away_games_by_team_id in a hash" do
+    expect(@stat_tracker.away_games_by_team_id.length).to eq(@stat_tracker.teams.length)
+    expect(@stat_tracker.away_games_by_team_id).to be_a(Hash)
   end
 
-  xit "can list home games in a hash" do
-
+  it "can list home_games_by_team_id in a hash" do
+    expect(@stat_tracker.home_games_by_team_id.length).to eq(@stat_tracker.teams.length)
+    expect(@stat_tracker.home_games_by_team_id).to be_a(Hash)
   end
 
-  it "can find the highest_scoring_visitor_team" do
+  it "can find the average_scores_for_all_visitors" do
+    expect(@stat_tracker.average_scores_for_all_visitors.length).to eq(@stat_tracker.teams.length)
+    expect(@stat_tracker.average_scores_for_all_visitors).to be_a(Hash)
+  end
+
+  it "can find the average_scores_for_all_home_teams" do
+    expect(@stat_tracker.average_scores_for_all_home_teams.length).to eq(@stat_tracker.teams.length)
+    expect(@stat_tracker.average_scores_for_all_home_teams).to be_a(Hash)
+  end
+
+  it "can find the highest_scoring_visitor team" do
     expect(@stat_tracker.highest_scoring_visitor).to eq("FC Dallas")
   end
 
-  it "#lowest_scoring_visitor" do
-    expect(@stat_tracker.lowest_scoring_visitor).to eq "San Jose Earthquakes"
+  it "can find the lowest_scoring_visitor team" do
+    expect(@stat_tracker.lowest_scoring_visitor).to eq("San Jose Earthquakes")
   end
 
-  xit "can find the highest_scoring_home_team" do
-    expect(@stat_tracker.highest_scoring_home_team).to eq "Reign FC"
+  it "can find the highest_scoring_home_team" do
+    expect(@stat_tracker.highest_scoring_home_team).to eq("Reign FC")
   end
 
-
+  it "can find the lowest_scoring_home_team" do
+    expect(@stat_tracker.lowest_scoring_home_team).to eq("Utah Royals FC")
+  end
 end
