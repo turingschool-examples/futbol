95f0e67f5072d34b8ee8ee308374230e16d7a4ea
Sean Culliton <smculliton@gmail.com>
2022-09-14 15:42:42 -0600

    Fix file paths

--- runner.rb
+++ runner.rb
@@ -1,8 +1,8 @@
 require './lib/stat_tracker'
 
-game_path = './data/games.csv'
-team_path = './data/teams.csv'
-game_teams_path = './data/game_teams.csv'
+game_path = './data/games_dummy.csv'
+team_path = './data/teams_dummy.csv'
+game_teams_path = './data/game_teams_dummy.csv'
 
 locations = {
   games: game_path,
@@ -12,4 +12,3 @@ locations = {
 
 stat_tracker = StatTracker.from_csv(locations)
 
-p stat_tracker.least_accurate_team('20132014')
