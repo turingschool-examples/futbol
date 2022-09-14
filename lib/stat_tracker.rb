39ecc41ec827129b8f0a055b946030dd9352a1a4
Bryan Keener <bkeener7@gmail.com>
2022-09-14 12:57:56 -0600

    Hotfix the CSV open to CSV read

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -8,9 +8,9 @@ class StatTracker
     @game_path = game_path
     @team_path = team_path
     @game_teams_path = game_teams_path
-    @game_csv = CSV.open(@game_path, headers: true, header_converters: :symbol)
-    @team_csv = CSV.open(@team_path, headers: true, header_converters: :symbol)
-    @game_teams_csv = CSV.open(@game_teams_path, headers: true, header_converters: :symbol)
+    @game_csv = CSV.read(@game_path, headers: true, header_converters: :symbol)
+    @team_csv = CSV.read(@team_path, headers: true, header_converters: :symbol)
+    @game_teams_csv = CSV.read(@game_teams_path, headers: true, header_converters: :symbol)
   end
 
   def self.from_csv(locations)
