6704dce753939dd2cadf37d7714ff9b81e689bb9
Ryan Loughry <thecoderanger5280@icloud.com>
2022-09-15 10:30:15 -0600

    implement code for the lowest scoring visitory

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -205,7 +205,7 @@ class StatTracker
     # find @teams the team_id that corresponds to the maximum value in the team_id_goals_hash
     @teams.find { |x| x.fetch(:team_id) == team_id_goals_hash.max_by { |k,v| v }.first }[:teamname]
   end
-  
+
   def highest_scoring_visitor
     away_team_score = Hash.new(0)
     away_team_count = Hash.new(0)
@@ -226,5 +226,27 @@ class StatTracker
       team[:team_id] == team_id_highest_average
     end
     team_highest_average[:teamname]
+  end
+
+  def lowest_scoring_visitor
+    away_team_score = Hash.new(0)
+    away_team_count = Hash.new(0)
+    @games.map do |game| 
+      away_team_score[game[:away_team_id]] += game[:away_goals].to_i
+      away_team_count[game[:away_team_id]] += 1
+    end
+    away_score_average = away_team_score.map do |id, score|
+      {id => (score.to_f / away_team_count[id].to_f).round(2)}
+    end
+    away_score_hash = {}
+    away_score_average.each do |average|
+      away_score_hash[average.keys[0]] = average.values[0]
+    end
+    team_id_lowest_average = away_score_hash.key(away_score_hash.values.min)
+
+    team_lowest_average = @teams.find do |team|
+      team[:team_id] == team_id_lowest_average
+    end
+    team_lowest_average[:teamname]
   end
 end
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -34,25 +34,25 @@ RSpec.describe StatTracker do
 
   describe '#percentage_ties' do
     it 'returns the percent that the games have ended in a tie' do
-      expect(@stat_tracker.percentage_ties).to eq(5.88)
+      expect(@stat_tracker.percentage_ties).to eq(0.06)
     end
   end
 
   describe '#average_goals_per_game' do
     it 'returns the average number of goals scored in a game across all seasons' do
-      expect(@stat_tracker.average_goals_per_game).to eq(3.78)
+      expect(@stat_tracker.average_goals_per_game).to eq(3.9)
     end
   end
 
   describe '#percentage_home_wins' do
     it 'returns the percentage of the home team wins' do
-      expect(@stat_tracker.percentage_home_wins).to eq(55.56)
+      expect(@stat_tracker.percentage_home_wins).to eq(0.5)
     end
   end
 
   describe '#percentage_visitor_wins' do
     it 'returns the percentage of the visitor team wins' do
-      expect(@stat_tracker.percentage_visitor_wins).to eq(44.44)
+      expect(@stat_tracker.percentage_visitor_wins).to eq(0.5)
     end
   end
 
@@ -98,7 +98,7 @@ RSpec.describe StatTracker do
   describe '#average_win_percentage' do 
     it 'calculates average win percentage of a single team' do 
       expect(@stat_tracker.average_win_percentage(3)).to eq (0)
-      expect(@stat_tracker.average_win_percentage(16)).to eq (42.86)
+      expect(@stat_tracker.average_win_percentage(16)).to eq (0.43)
     end
   end
 
@@ -117,6 +117,12 @@ RSpec.describe StatTracker do
   describe '#highest_scoring_visitor' do
     it 'returns the name of the team with the highest average score per game across all seasons when they are away' do
       expect(@stat_tracker.highest_scoring_visitor).to eq('FC Dallas')
+    end
+  end
+
+  describe '#lowest_scoring_visitor' do
+    it 'returns the name of the team with the lowest average score per game across all seasons when they are away' do
+      expect(@stat_tracker.lowest_scoring_visitor).to eq('Sporting Kansas City')
     end
   end
 end
