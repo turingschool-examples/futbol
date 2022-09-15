149a078015934f94ac1502ac6f4421d3b20068ee
Darby Smith <newman.darby2@gmail.com>
2022-09-15 18:24:29 -0400

    Add tests for highest and lowest scoring teams

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -233,5 +233,21 @@ RSpec.describe StatTracker do
     it 'returns the worst offense' do
       expect(@stat_tracker.worst_offense).to eq "Utah Royals FC"
     end
+
+    it "#highest_scoring_visitor" do
+      expect(@stat_tracker.highest_scoring_visitor).to eq "FC Dallas"
+    end
+
+    xit "#highest_scoring_home_team" do
+      expect(@stat_tracker.highest_scoring_home_team).to eq "Reign FC"
+    end
+
+    xit "#lowest_scoring_visitor" do
+      expect(@stat_tracker.lowest_scoring_visitor).to eq "San Jose Earthquakes"
+    end
+
+    xit "#lowest_scoring_home_team" do
+      expect(@stat_tracker.lowest_scoring_home_team).to eq "Utah Royals FC"
+    end
   end
 end
