8980000d4b1ab4027346cc1401f04f5a7b9d9e3a
Krisetn Nestler <kristen.nestler@gmail.com>
2022-09-15 19:33:28 -0400

    refactor game stats

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -6,52 +6,39 @@ class StatTracker
     @games_data = CSV.read(locations[:games], headers: true, header_converters: :symbol)
     @teams_data = CSV.read(locations[:teams], headers: true, header_converters: :symbol)
     @game_teams_data = CSV.read(locations[:game_teams], headers: true, header_converters: :symbol)
-  
   end
   
   def self.from_csv(locations)
     StatTracker.new(locations)
   end
 
+  def total_game_goals
+    @games_data.map {|row| row[:away_goals].to_i + row[:home_goals].to_i}
+  end
+
   def highest_total_score
-    @games_data.map do |row| 
-    row[:away_goals].to_i + row[:home_goals].to_i
-    end.max
+    total_game_goals.max
   end
 
   def lowest_total_score
-    @games_data.map do |row| 
-    row[:away_goals].to_i + row[:home_goals].to_i
-    end.min
+    total_game_goals.min
   end
 
   def percentage_home_wins
     wins =0
-    @games_data.each do |row| 
-      if row[:away_goals].to_i < row[:home_goals].to_i
-        wins += 1
-      end
-    end
+    @games_data.each { |row| wins += 1 if row[:away_goals].to_i < row[:home_goals].to_i}
     (wins.to_f/games_data.count).round(2)
   end
 
   def percentage_visitor_wins
     wins =0
-    @games_data.each do |row| 
-      if row[:away_goals].to_i > row[:home_goals].to_i
-        wins += 1
-      end
-    end
+    @games_data.each { |row| wins += 1 if row[:away_goals].to_i > row[:home_goals].to_i}
     (wins.to_f/games_data.count).round(2)
   end
 
   def percentage_ties
     ties =0 
-    @games_data.each do |row| 
-      if row[:away_goals].to_i == row[:home_goals].to_i
-        ties += 1
-      end
-    end
+    @games_data.each {|row| ties += 1 if row[:away_goals].to_i == row[:home_goals].to_i}
     (ties.to_f/games_data.count).round(2)
   end
 
@@ -66,34 +53,29 @@ class StatTracker
     end
     games_by_season
   end
-
-  def average_goals_per_game
-    total_goals = @games_data.map do |row| 
-      (row[:away_goals].to_i + row[:home_goals].to_i)
-    end
-    (total_goals.sum / @games_data.count.to_f).round(2)
-  end
-
-  def average_goals_by_season
-    average_goals = {}
-    goals_by_season = {}
   
-    @games_data.each do |row|
+  def count_of_goals_by_season
+    goals_by_season = {}
+    @games_data.each { |row|
       if goals_by_season.include?(row[:season])
-        goals_by_season[row[:season]] += ([row[:away_goals].to_f]+[row[:home_goals].to_i]).sum
+        goals_by_season[row[:season]] += ([row[:away_goals].to_f]+[row[:home_goals].to_f]).sum
       elsif goals_by_season.include?(row[:season]) == false
-        goals_by_season[row[:season]] = ([row[:away_goals].to_f]+[row[:home_goals].to_i]).sum
+        goals_by_season[row[:season]] = ([row[:away_goals].to_f]+[row[:home_goals].to_f]).sum
       end
-    end
-    
-    goals_by_season.each do |season, wins|
-      count_of_games_by_season.each do |season_number, games|
-        if season_number == season
-          games
-          average_goals[season_number] = (wins/games).round(2)
-        end 
-      end
-    end
+    }
+    goals_by_season
+  end
+  
+  def average_goals_per_game
+    (total_game_goals.sum / @games_data.count.to_f).round(2)
+  end
+  
+  def average_goals_by_season
+    average_goals = {}  
+    count_of_goals_by_season.each {|season, goals|
+      count_of_games_by_season.each {|season_number, games|
+        average_goals[season_number] = (goals/games).round(2) if season_number == season
+        }}
     average_goals
   end
 
