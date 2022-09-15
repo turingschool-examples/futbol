3655785efba6e46bc2b4f348e417a9f52a6a596d
Lacey Weaver <jlweave89@gmail.com>
2022-09-14 20:22:46 -0600

    Add best_offense and helper methods

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -33,17 +33,44 @@ class StatTracker
       total_goals += game[:away_goals].to_f
       total_goals += game[:home_goals].to_f
     end
-    (total_goals / @games_reader.count).round(2)  
+    (total_goals / @games_reader.count).round(2)
   end
-  
+
   def average_goals_by_season
     goals_per_season = Hash.new(0)
     @games_reader.each do |game|
-      goals_per_season[game[:season]] += (game[:away_goals]).to_f 
+      goals_per_season[game[:season]] += (game[:away_goals]).to_f
       goals_per_season[game[:season]] += (game[:home_goals]).to_f
     end
     goals_per_season.update(goals_per_season) do |season, total_goals|
       total_goals / ((@games_reader[:season].find_all {|element| element == season}).count)
     end
+  end
+
+  def best_offense
+    teams_hash = total_goals_by_team
+    teams_hash.update(teams_hash) do |team_id, total_goals|
+      total_goals / ((@games_reader[:away_team_id].find_all {|element| element == team_id}).count +
+    @games_reader[:home_team_id].find_all {|element| element == team_id}.count)
+    end
+    team_name_from_id(teams_hash.key(teams_hash.values.max))
+  end
+
+  def total_goals_by_team
+    teams_hash = Hash.new(0)
+    @teams_reader[:team_id].each do |team|
+      @games_reader.each do |line|
+        if line[:away_team_id] == team
+          teams_hash[team] += line[:away_goals].to_f
+        elsif line[:home_team_id] == team
+          teams_hash[team] += line[:home_goals].to_f
+        end
+      end
+    end
+    teams_hash
+  end
+
+  def team_name_from_id(team_id)
+    @teams_reader[:teamname][@teams_reader[:team_id].index('20')]
   end
 end
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -37,7 +37,7 @@ RSpec.describe StatTracker do
 
     expect(stat_tracker1.average_goals_per_game).to eq(5.00)
   end
-  
+
   it "#average_goals_by_season" do
     dummy_filepath = {teams: "./data/team_dummy.csv",
                       games: './data/games_dummy1.csv',
@@ -45,11 +45,46 @@ RSpec.describe StatTracker do
 
     }
     stat_tracker1 = StatTracker.from_csv(dummy_filepath)
-    
+
     result = {
       '20122013' => 5.00,
       '20132014' => 5.00
     }
     expect(stat_tracker1.average_goals_by_season).to eq(result)
+  end
+
+  it '#best_offense Name of the team with the highest average number of goals scored per game across all seasons.' do
+    dummy_filepath = {teams: "./data/teams.csv",
+                      games: './data/games_dummy1.1.csv',
+                      game_teams: './data/game_teams.csv'
+
+    }
+    stat_tracker1 = StatTracker.from_csv(dummy_filepath)
+
+    expect(stat_tracker1.best_offense).to eq("Toronto FC")
+
+  end
+
+  it '#total_goals_by_team' do
+    dummy_filepath = {teams: "./data/teams.csv",
+                      games: './data/games_dummy1.1.csv',
+                      game_teams: './data/game_teams.csv'
+
+    }
+    stat_tracker1 = StatTracker.from_csv(dummy_filepath)
+
+    result = {"6"=>4.0, "3"=>6.0, "5"=>5.0, "30"=>0.0, "24"=>6.0, "20"=>9.0, "21"=>2.0}
+    expect(stat_tracker1.total_goals_by_team).to eq(result)
+  end
+
+  it '#team_name_from_id' do
+    dummy_filepath = {teams: "./data/teams.csv",
+                      games: './data/games_dummy1.1.csv',
+                      game_teams: './data/game_teams.csv'
+
+    }
+    stat_tracker1 = StatTracker.from_csv(dummy_filepath)
+
+    expect(stat_tracker1.team_name_from_id('20')).to eq('Toronto FC')
   end
 end
