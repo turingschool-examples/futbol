b9e83d80c779d088be4e5c54129cbc52477335ec
Krisetn Nestler <kristen.nestler@gmail.com>
2022-09-15 20:00:30 -0400

    game stats refactor

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -25,42 +25,39 @@ class StatTracker
   end
 
   def percentage_home_wins
-    wins =0
-    @games_data.each { |row| wins += 1 if row[:away_goals].to_i < row[:home_goals].to_i}
-    (wins.to_f/games_data.count).round(2)
+    home_wins =0
+    @games_data.each { |game| home_wins += 1 if game[:away_goals].to_i < game[:home_goals].to_i}
+    (home_wins.to_f/games_data.count).round(2)
   end
 
   def percentage_visitor_wins
-    wins =0
-    @games_data.each { |row| wins += 1 if row[:away_goals].to_i > row[:home_goals].to_i}
-    (wins.to_f/games_data.count).round(2)
+    visitor_wins =0
+    @games_data.each { |game| visitor_wins += 1 if game[:away_goals].to_i > game[:home_goals].to_i}
+    (visitor_wins.to_f/games_data.count).round(2)
   end
 
   def percentage_ties
     ties =0 
-    @games_data.each {|row| ties += 1 if row[:away_goals].to_i == row[:home_goals].to_i}
+    @games_data.each {|game| ties += 1 if game[:away_goals].to_i == game[:home_goals].to_i}
     (ties.to_f/games_data.count).round(2)
   end
 
   def count_of_games_by_season
     games_by_season = {}
-    @games_data.each do |row|
-      if games_by_season.include?(row[:season])
-        games_by_season[row[:season]] += 1
-      elsif games_by_season.include?(row[:season]) == false
-        games_by_season[row[:season]] = 1
-      end
-    end
+    @games_data.each {|game|
+      games_by_season[game[:season]] += 1 if games_by_season.include?(game[:season])
+      games_by_season[game[:season]] = 1 if !games_by_season.include?(game[:season])
+    }
     games_by_season
   end
   
   def count_of_goals_by_season
     goals_by_season = {}
-    @games_data.each { |row|
-      if goals_by_season.include?(row[:season])
-        goals_by_season[row[:season]] += ([row[:away_goals].to_f]+[row[:home_goals].to_f]).sum
-      elsif goals_by_season.include?(row[:season]) == false
-        goals_by_season[row[:season]] = ([row[:away_goals].to_f]+[row[:home_goals].to_f]).sum
+    @games_data.each { |game|
+      if goals_by_season.include?(game[:season])
+        goals_by_season[game[:season]] += ([game[:away_goals].to_f]+[game[:home_goals].to_f]).sum
+      elsif !goals_by_season.include?(game[:season])
+        goals_by_season[game[:season]] = ([game[:away_goals].to_f]+[game[:home_goals].to_f]).sum
       end
     }
     goals_by_season
