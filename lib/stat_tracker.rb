65ae75c6a352221e48eb801c95712e4865f92902
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2022-09-16 12:07:40 -0400

    feat:season games grouped into an set array

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -299,47 +299,142 @@ class StatTracker
 #end
   #SeasonClass
 
-  #Method groups game_id by season in a hash
-  def season_games
-    season = {}
-    @games_data.each do |row|
-      season[row[:season]] #= row[:game_id]
-      require 'pry';binding.pry
-    end
-  end
 
-
-#Method returns the name Coach with the best win percentage for the season in a string
+  #Method returns the name Coach with the best win percentage for the season in a string
   #look at every season
   #find coach name
   #accumulate coach's win/loss/tie
   #compare coach records with other for each season
-  def winningest_coach(season)
+  def winningest_coach(campaign)
     
-    #different seasons individually to compare them to each other
-    #Method compares a season
+    campaign = "2014020109"
     
+    # season_all_game_id
+    coach_games = Hash.new(0)
+    wins = 0
+    games = 0
+    games_played = 
+    season = Set.new
+
+    @game_teams_data.each do |row|      
+      #hash of coach (key) and count of games coached in a season (value)
+      # require 'pry';binding.pry
+      row.find_all do |game_id|
+        if campaign.scan(/.{4}/).shift == row[:game_id].scan(/.{4}/).shift
+          season << row[:game_id]
+            end
+          end
+        end
+                season
+
+      coaches_for_each_game.each do |game, coaches|
+        if season.include?(game)
+        # require 'pry';binding.pry
+          if coach_games .include?(game[:head_coach])
+            coach_games [game[:head_coach]] += 1 
+          else
+            coach_games [game[:head_coach]] = 1 
+          end
+        end
+        coach_games 
+        require 'pry';binding.pry
+      end
+    # @game_teams_data.each do |row|
+    #   if season == row[:game_id]
+    #     row.each do |coaches|
 
 
 
 
+                  # coach_games 
+                  require 'pry';binding.pry
 
+          
+                            # require 'pry';binding.pry
+          #  coaches.each do |coach|
+          # require 'pry';binding.pry
+          # if coach_games.include?(row[:head_coach])
+          #   coach_games[row[:head_coach]] = [row[:game_id]]
+          # else
+          #   coach_games[row[:season]] = [row[:game_id]]
+          # end
+          #     end
 
+  end
 
+            # if coach_games.include?(game[:head_coach])
+            #   coach_games[game[:head_coach]] = (games += 1)
+            #       else
+            #         coach_games[game[:head_coach]] = (games + 1) 
+            #       end
+            #       coach_games
+            #       require 'pry';binding.pry
+                # end
+
+          
+          # end
+          # coaches_for_each_game.each do |game, result|
+          #     # if row[:result] == "WIN"
+          #     require 'pry';binding.pry
+          #     if coach.include?(game[:head_coach])
+          #       coach[game[:head_coach]] += 1 
+          #       # && row[:result] == "WIN" 
+          #           # wins += 1
+          #     else
+          #       coach[game[:head_coach]] = 1 
+          #     end
+          #     coach
+          #     require 'pry';binding.pry
+          #   end
+          # end
+        # end
+      # end
+          #   if row[:result] == "WIN" 
+          #     wins += 1
+          #  coach [ wins, total_games ]
+          #         values[0]/values[1]
+          #   coach [%]
+
+    
+  
+  
+    
   #method creates hash-season(key) and all games_id(values) in string
-  def season_all_game_id(season)
-    season_games = {}
+  def season_all_game_id
+  season_games = {}
     @games_data.flat_map do |row|
-      if season == row[:season]
         if season_games.include?(row[:season])
           season_games[row[:season]].push(row[:game_id])
-        elsif #!season_games.include?(row[:season])
+        else #!season_games.include?(row[:season])
           season_games[row[:season]] = [row[:game_id]]
         end
-      end
     end
     season_games
   end
 
+  #method returns a hash: game_id(key) = [team_id (away), team_id (home)]
+  def game_match_up
+    game_ids = {}
+    @game_teams_data.each do |row|
+      if game_ids.include?(row[:game_id])
+        game_ids[row[:game_id]].push(row[:team_id])
+      else #!game_ids.include?(row[:season])
+        game_ids[row[:game_id]] = [row[:team_id]]
+      end
+    end
+    game_ids
+  end
 
+  #method returns hash: game_id(key) coaches for those games(values)
+  def coaches_for_each_game
+    coach_games = Hash.new
+    @game_teams_data.each do |row|
+          if coach_games.include?(row[:game_id])
+            coach_games[row[:game_id]].push(row[:head_coach])
+          else# !coach_games.include?(row[:season])
+            coach_games[row[:game_id]] = [row[:head_coach]]
+          end
+    end
+        coach_games
+  end
 end
