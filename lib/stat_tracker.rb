7da925630673d6406124971ff4f99cba3f1eb69f
Annie Pulzone <ajpulzone@gmail.com>
2022-09-17 15:18:54 -0700

    Feature: winningest_coach method written; games_by_head coach, coach results helper methods written

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -224,4 +224,35 @@ class StatTracker
       end
     unique_goal_totals.min
   end
-end 
+
+  def coach_results(result, season_id)
+    coaches = Hash.new(0)
+    @game_teams_reader.each do |row|
+      if row[:game_id][0..3] == season_id[0..3] && row[:result] == result
+        coaches[row[:head_coach]] += 1
+      else
+        coaches[row[:head_coach]] += 0
+      end
+    end
+    coaches
+  end
+  
+  def games_by_head_coach(season_id)
+    games_by_coach = Hash.new(0)
+    @game_teams_reader.each do |row|
+      if row[:game_id][0..3] == season_id[0..3]
+        games_by_coach[row[:head_coach]] += 1
+      end
+    end
+    games_by_coach
+  end
+
+  def winningest_coach(season_id)
+    coaches = coach_results("WIN", season_id)
+    games_by_coach = games_by_head_coach(season_id)
+    coaches.update(coaches) do |coach, wins|
+      wins.to_f / games_by_coach[coach]
+    end
+    coaches.key(coaches.values.max)
+  end
+end
