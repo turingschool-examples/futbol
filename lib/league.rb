572c13e0bfdbe0e268a39d5ef3b3a82c6b8e977c
Darby Smith <newman.darby2@gmail.com>
2022-09-18 14:35:50 -0400

    Add initialize attributes for files

--- lib/league.rb
+++ lib/league.rb
@@ -1,11 +1,9 @@
-require 'csv'
 class League
-  attr_reader :data
+  attr_reader :teams_data, :game_teams_data
   
-  def initialize(locations)
-    @games_data = CSV.read(locations[:games], headers: true, header_converters: :symbol)
-    @teams_data = CSV.read(locations[:teams], headers: true, header_converters: :symbol)
-    @game_teams_data = CSV.read(locations[:game_teams], headers: true, header_converters: :symbol)
+  def initialize(teams_data, game_teams_data)
+    @teams_data = teams_data
+    @game_teams_data = game_teams_data
   end
 
   def count_of_teams
