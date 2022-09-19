6d5920bf743de6b155f2e4ecba26054e1bd505fe
Ashley <ashuhleyt@gmail.com>
2022-09-19 10:55:27 -0700

    feat: league module completed

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,8 +1,11 @@
 require 'csv'
 require_relative 'team_stats'
+require_relative 'league_stats'
 
 class StatTracker
   include TeamStatistics
+  include LeagueStatistics 
+
   attr_reader :games,
               :teams,
               :game_teams
@@ -113,119 +116,12 @@ class StatTracker
     avg = Hash[count_of_games_by_season.keys.zip(arr)]
   end
 
-  def count_of_teams
-    @teams.count
-  end
-
-  def average_goals
-    team_goals = Hash.new(0)
-    @game_teams.map do |row|
-      team_id = row[:team_id]
-      goals = row[:goals].to_f
-      team_goals[team_id] += goals
-    end
-    team_game = @game_teams.map {|row| row[:team_id]}.tally
-    nested_arr = team_game.values.zip(team_goals.values)
-    arr = nested_arr.map {|array| array[1] / array[0]}
-    avg = Hash[team_game.keys.zip(arr)]
-  end
-
   def team_name(team_id)
     name = @teams.filter_map {|row| row[:teamname] if row[:team_id].to_i == team_id }
     return name[0]
   end
 
-  def best_offense
-    best = average_goals.max_by {|key,value| value}
-    hash = Hash.new
-    @teams.map do |row|
-      team_id = row[:team_id]
-      team_name = row[:teamname]
-      hash[team_id] = team_name
-    end
-    num1 = hash.filter_map {|key,value| value if key == best[0] }
-    num1[0]
-  end
-
-  def worst_offense
-    worst = average_goals.min_by {|key,value| value}
-    hash = Hash.new
-    @teams.map do |row|
-      team_id = row[:team_id]
-      team_name = row[:teamname]
-      hash[team_id] = team_name
-    end
-    the_worst = hash.filter_map {|key,value| value if key == worst[0] }
-    the_worst[0]
-  end
-
-  def highest_scoring_visitor
-    team_goals = Hash.new(0)
-    @game_teams.map do |row|
-      if row[:hoa] == 'away'
-        team_id = row[:team_id]
-        goals = row[:goals].to_f
-        team_goals[team_id] += goals
-      end
-    end
-    team_game = @game_teams.filter_map {|row| row[:team_id] if row[:hoa] == 'away'}.tally
-    nested_arr = team_game.values.zip(team_goals.values)
-    arr = nested_arr.map {|array| array[1] / array[0]}
-    away_avg = Hash[team_game.keys.zip(arr)]
-    best_away = away_avg.max_by {|key,value| value}
-    team_name(best_away[0].to_i)
-  end
-
-  def highest_scoring_home_team
-    team_goals = Hash.new(0)
-    @game_teams.map do |row|
-      if row[:hoa] == 'home'
-        team_id = row[:team_id]
-        goals = row[:goals].to_f
-        team_goals[team_id] += goals
-      end
-    end
-    team_game = @game_teams.filter_map {|row| row[:team_id] if row[:hoa] == 'home'}.tally
-    nested_arr = team_game.values.zip(team_goals.values)
-    arr = nested_arr.map {|array| array[1] / array[0]}
-    home_avg = Hash[team_game.keys.zip(arr)]
-    best_home = home_avg.max_by {|key,value| value}
-    team_name(best_home[0].to_i)
-  end
-
-  def lowest_scoring_visitor
-    team_goals = Hash.new(0)
-    @game_teams.map do |row|
-      if row[:hoa] == 'away'
-        team_id = row[:team_id]
-        goals = row[:goals].to_f
-        team_goals[team_id] += goals
-      end
-    end
-    team_game = @game_teams.filter_map {|row| row[:team_id] if row[:hoa] == 'away'}.tally
-    nested_arr = team_game.values.zip(team_goals.values)
-    arr = nested_arr.map {|array| array[1] / array[0]}
-    away_avg = Hash[team_game.keys.zip(arr)]
-    best_away = away_avg.min_by {|key,value| value}
-    team_name(best_away[0].to_i)
-  end
-
-  def lowest_scoring_home_team
-    team_goals = Hash.new(0)
-    @game_teams.map do |row|
-      if row[:hoa] == 'home'
-        team_id = row[:team_id]
-        goals = row[:goals].to_f
-        team_goals[team_id] += goals
-      end
-    end
-    team_game = @game_teams.filter_map {|row| row[:team_id] if row[:hoa] == 'home'}.tally
-    nested_arr = team_game.values.zip(team_goals.values)
-    arr = nested_arr.map {|array| array[1] / array[0]}
-    home_avg = Hash[team_game.keys.zip(arr)]
-    best_home = home_avg.min_by {|key,value| value}
-    team_name(best_home[0].to_i)
-  end
+ 
 
   def team_info(team_id)
     team_hash = Hash.new
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -74,38 +74,6 @@ RSpec.describe StatTracker do
     expect(@stat_tracker.average_goals_by_season).to eq({"20122013"=>3.5, "20142015"=>4.5, "20152016"=>4.0})
   end
 
-  it '#count_of_teams' do
-    expect(@stat_tracker.count_of_teams).to eq(32)
-  end
-
-  it '#average_goals' do
-    expect(@stat_tracker.average_goals).to eq({"16"=>1.0, "17"=>2.0, "24"=>2.0, "25"=>2.0, "26"=>3.0, "8"=>2.5, "9"=>1.0})
-  end
-
-  it '#best_offense' do
-    expect(@stat_tracker.best_offense).to eq('FC Cincinnati')
-  end
-
-  it '#worst_offense' do
-    expect(@stat_tracker.worst_offense). to eq('New England Revolution')
-  end
-
-  it '#highest_scoring_visitor' do
-    expect(@stat_tracker.highest_scoring_visitor).to eq('FC Cincinnati')
-  end
-
-  it '#highest_scoring_home_team' do
-    expect(@stat_tracker.highest_scoring_home_team).to eq('New York Red Bulls')
-  end
-
-  it '#lowest_scoring_visitor' do
-    expect(@stat_tracker.lowest_scoring_visitor).to eq('New England Revolution')
-  end
-
-  it '#lowest_scoring_home_team' do
-    expect(@stat_tracker.lowest_scoring_home_team).to eq('New York City FC')
-  end
-
   it '#most_goals_scored' do
     expect(@stat_tracker.most_goals_scored('8')).to eq(3)
   end
