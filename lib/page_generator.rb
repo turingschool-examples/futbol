2487b8e157d3047d8f63eeb78df0f79e9724ea9c
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-09-20 18:15:28 -0600

    Add lib/page_generator.rb site/index.html and update runner

--- lib/page_generator.rb
+++ lib/page_generator.rb
@@ -1,9 +1,9 @@
-require "erb"
-require "./lib/stat_tracker"
+require 'erb'
+require './lib/stat_tracker'
 
-game_path = './data/games_dummy.csv'
-team_path = './data/teams_dummy.csv'
-game_teams_path = './data/game_teams_dummy.csv'
+game_path = './data/games.csv'
+team_path = './data/teams.csv'
+game_teams_path = './data/game_teams.csv'
 
 locations = {
   games: game_path,
@@ -16,52 +16,167 @@ stat_tracker = StatTracker.from_csv(locations)
 template = %(
 <!doctype html>
 <html lang="en">
-
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="">
-  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-  <meta name="generator" content="Hugo 0.72.0">
-  <title>Dashboard Template · Bootstrap</title>
-
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css"
-    integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-    crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js"
-    integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/"
-    crossorigin="anonymous"></script>
-
-
-</head>
-
-<body>
-
-  <div class="container-fluid">
-  <h1>Hello World</h1>
-  <div>cool code goes here </div>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container-lg">
+<div class="container-lg">
+  <div class="row">
+    <div class="col bg-color bg-primary text-light p-3 d-flex justify-content-center">
+      <h3>Team Ryan, Sean, Yuji, Michael</h3>
+    </div>
+  </div>
+  <div class="row d-flex flex-column m-3">
+    <h1>Game Statistics</h1>
+    <div class="col border border-1">
+      <h3>highest_total_score</h3>
+      <%= stat_tracker.highest_total_score %>
+    </div>
+    <div class="col border border-1">
+      <h3>lowest_total_score</h3>
+      <%= stat_tracker.lowest_total_score %>
+    </div>
+    <div class="col border border-1">
+      <h3>percentage_home_wins</h3>
+      <%= stat_tracker.percentage_home_wins %>
+    </div>
+    <div class="col border border-1">
+      <h3>percentage_visitor_wins</h3>
+      <%= stat_tracker.percentage_visitor_wins %>
+    </div>
+    <div class="col border border-1">
+      <h3>percentage_ties</h3>
+      <%= stat_tracker.percentage_ties %>
+    </div>
+    <div class="col border border-1">
+      <h3>count_of_games_by_season</h3>
+      <%= stat_tracker.count_of_games_by_season %>
+    </div>
+    <div class="col border border-1">
+      <h3>average_goals_per_game</h3>
+      <%= stat_tracker.average_goals_per_game %>
+    </div>
+    <div class="col border border-1">
+      <h3>average_goals_by_season</h3>
+      <%= stat_tracker.average_goals_by_season %>
+    </div>
+  </div>
+  <div class="row d-flex flex-column m-3">
+    <h1>League Statistics</h1>
+    <div class="col border border-1">
+      <h3>count_of_teams</h3>
+      <%= stat_tracker.count_of_teams %>
+    </div>
+    <div class="col border border-1">
+      <h3>best_offense</h3>
+      <%= stat_tracker.best_offense %>
+    </div>
+    <div class="col border border-1">
+      <h3>worst_offense</h3>
+      <%= stat_tracker.worst_offense %>
+    </div>
+    <div class="col border border-1">
+      <h3>highest_scoring_visitor</h3>
+      <%= stat_tracker.highest_scoring_visitor %>
+    </div>
+    <div class="col border border-1">
+      <h3>highest_scoring_home_team</h3>
+      <%= stat_tracker.highest_scoring_home_team %>
+    </div>
+    <div class="col border border-1">
+      <h3>lowest_scoring_visitor</h3>
+      <%= stat_tracker.lowest_scoring_visitor %>
+    </div>
+    <div class="col border border-1">
+      <h3>lowest_scoring_home_team</h3>
+      <%= stat_tracker.lowest_scoring_home_team %>
+    </div>
+  </div>
+  <div class="row d-flex flex-column m-3">
+    <h1>Season Statistics</h1>
+    <div class="col border border-1">
+      <h3>winningest_coach</h3>
+      <%= stat_tracker.winningest_coach("20122013") %>
+    </div>
+    <div class="col border border-1">
+      <h3>worst_coach</h3>
+      <%= stat_tracker.worst_coach("20122013") %>
+    </div>
+    <div class="col border border-1">
+      <h3>most_accurate_team</h3>
+      <%= stat_tracker.most_accurate_team("20122013") %>
+    </div>
+    <div class="col border border-1">
+      <h3>least_accurate_team</h3>
+      <%= stat_tracker.least_accurate_team("20122013") %>
+    </div>
+    <div class="col border border-1">
+      <h3>most_tackles</h3>
+      <%= stat_tracker.most_tackles("20122013") %>
+    </div>
+    <div class="col border border-1">
+      <h3>fewest_tackles</h3>
+      <%= stat_tracker.fewest_tackles("20122013") %>
+    </div>
+  </div>
+  <div class="row d-flex flex-column m-3">
+    <h1>Team Statistic</h1>
+    <div class="col border border-1">
+      <h3>team_info</h3>
+      <%= stat_tracker.team_info('1') %>
+    </div>
+    <div class="col border border-1">
+      <h3>best_season</h3>
+      <%= stat_tracker.best_season('1') %>
+    </div>
+    <div class="col border border-1">
+      <h3>worst_season</h3>
+      <%= stat_tracker.worst_season('1') %>
+    </div>
+    <div class="col border border-1">
+      <h3>average_win_percentage</h3>
+      <%= stat_tracker.average_win_percentage('1') %>
+    </div>
+    <div class="col border border-1">
+      <h3>most_goals_scored</h3>
+      <%= stat_tracker.most_goals_scored('1') %>
+    </div>
+    <div class="col border border-1">
+      <h3>fewest_goals_scored</h3>
+      <%= stat_tracker.fewest_goals_scored('1') %>
+    </div>
+    <div class="col border border-1">
+      <h3>favorite_opponent</h3>
+      <%= stat_tracker.favorite_opponent('1') %>
+    </div>
+    <div class="col border border-1">
+      <h3>fewest_goals_scored</h3>
+      <%= stat_tracker.fewest_goals_scored('1') %>
+    </div>
   </div>
 
 
-  <script src="/docs/5.0/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-DBjhmceckmzwrnMMrjI7BvG2FmRuxQVaTfFYHgfnrdfqMhxKt445b7j3KBQLolRl"
-    crossorigin="anonymous"></script>
+</div>
 
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.24.1/feather.min.js"
-    integrity="sha384-EbSscX4STvYAC/DxHse8z5gEDaNiKAIGW+EpfzYTfQrgIlHywXXrM9SUIZ0BlyfF"
-    crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"
-    integrity="sha384-i+dHPTzZw7YVZOx9lbH5l6lP74sLRtMtwN2XjVqjf3uAGAREAF4LMIUDTWEVs4LI"
-    crossorigin="anonymous"></script>
-  <script src="dashboard.js"></script>
-</body>
 
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  </body>
 </html>
+
 )
 
 html = ERB.new(template).result(binding)
 puts html
 
-File.open('site/index.html.erb', 'w') { |x| x.write html }
+File.write('site/index.html.erb', html)
