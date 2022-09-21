8c334ec65399b9db3cf77f0f0bbca244f1569ac4
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2022-09-21 11:04:18 -0400

    feat: refactoring of methods

--- lib/season.rb
+++ lib/season.rb
@@ -1,8 +1,6 @@
 require './lib/futbol_data.rb'
 
 class Season < FutbolData
-
-  #Method returns the name Coach with the best win percentage for the season in a string
   def winningest_coach(campaign)
     game_results_percentage = Hash.new
 
@@ -10,13 +8,11 @@ class Season < FutbolData
     game_results_percentage.update(season_coached_games(campaign),season_coach_wins(campaign)) do |coach, games_coached, games_won| 
       (games_won.fdiv(games_coached)).round(4)
     end
-    
+  
     winning_coach = highest_success_rate(game_results_percentage) 
-
     winning_coach[0]
   end
 
-  #Coach with the worst win percentage for the season
   def worst_coach(campaign)
     coached_games = Hash.new(0)
     coach_loss_tie = Hash.new(0)
@@ -24,7 +20,7 @@ class Season < FutbolData
     # method returns 2 hash: coach (key),count fo RESULT(WIN) (value) and total games in season
     season_data(campaign).select do |row|
       coached_games[row[:head_coach]] += 1
-      coach_loss_tie [row[:head_coach]] += 1 if row[:result] != "WIN"
+      coach_loss_tie[row[:head_coach]] += 1 if row[:result] != "WIN"
     end
     coach_loss_tie
     coached_games 
@@ -36,20 +32,18 @@ class Season < FutbolData
     worst_coach[0]
   end
 
-  #Team with the best ratio of shots to goals for the season (goals/shots)
   def most_accurate_team(campaign)
     team_name_from_team_id(highest_success_rate(team_accuracy(campaign)))
   end
 
-  #Team with the worst ratio of shots to goals for the season
   def least_accurate_team(campaign)
     team_name_from_team_id(lowest_success_rate(team_accuracy(campaign)))
   end
 
+  #returns two Hashes with team (key) and shots taken (value) and goals made (value)
   def team_accuracy(campaign)
     season_goals = Hash.new(0)
     team_shots = Hash.new(0)
-
     season_data(campaign).select do |row|
       season_goals[row[:team_id]] += row[:goals].to_i
       team_shots[row[:team_id]] += row[:shots].to_i
@@ -60,19 +54,17 @@ class Season < FutbolData
     merge_hashes_and_divide(season_goals,team_shots)
   end
 
-  #Team with the most tackles in the season
   def most_tackles(campaign)
     team_name_from_team_id(highest_success_rate(team_tackles(campaign)))
   end
 
-  #Team with the fewest tackles in the season
   def fewest_tackles(campaign)    
     team_name_from_team_id(lowest_success_rate(team_tackles(campaign)))
   end
 
+  #returns the total number of team tackles in a season
   def team_tackles(campaign)
     team_total_tackles = Hash.new(0)
-      
     season_data(campaign).select do |row|
       tackles = row[:tackles].to_i
       team_total_tackles[row[:team_id]] += tackles
@@ -99,42 +91,40 @@ class Season < FutbolData
     end
     season 
   end
-
+  
   def highest_success_rate(data)
     data.max_by { |team,percentage| percentage }
   end
-
+  
   def lowest_success_rate(data)
     data.min_by { |team,percentage| percentage }
   end
 
   def merge_hashes_and_divide(objective, attempts)
     efficiency = Hash.new
-
     efficiency.update(objective,attempts) do |team,goal,attempts|
       goal.fdiv(attempts).round(4)
     end
     efficiency
   end
 
-    # method returns hash: coach (key),count fo RESULT(WIN) (value)
-    def season_coach_wins(campaign)
-      coach_wins = Hash.new(0)
-      season_data(campaign).select do |row|
-        coach_wins[row[:head_coach]] += 1 if row[:result] == "WIN"
-      end
-      coach_wins 
+  # method returns hash: coach (key),count fo RESULT(WIN) (value)
+  def season_coach_wins(campaign)
+    coach_wins = Hash.new(0)
+    season_data(campaign).select do |row|
+      coach_wins[row[:head_coach]] += 1 if row[:result] == "WIN"
     end
-  
-    # method return a hash: coach(key), count of games coached in a season (value)-if coach had a WIN
-    def  season_coached_games(campaign)
-      coached_games = Hash.new(0)
-      season_data(campaign).find_all do |row|
-        if season_coach_wins(campaign).has_key?(row[:head_coach])
-          coached_games[row[:head_coach]] += 1
-        end
-      end
-      coached_games 
-    end
+    coach_wins 
+  end
 
+  # method return a hash: coach(key), count of games coached in a season (value)-if coach had a WIN
+  def  season_coached_games(campaign)
+    coached_games = Hash.new(0)
+    season_data(campaign).find_all do |row|
+      if season_coach_wins(campaign).has_key?(row[:head_coach])
+        coached_games[row[:head_coach]] += 1
+      end
+    end
+    coached_games 
+  end
 end
