314569f7aa335693ab75a3209ffa690a76c5c1bb
Brad Dunlap <braddunlapdpt@gmail.com>
2023-01-02 18:38:44 -0500

    feat: update stattracker

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,5 @@ require 'pry'
 require 'rspec'
 require './lib/stattracker'
 require 'simplecov'
+require 'csv'
 SimpleCov.start
--- spec/stattracker_spec.rb
+++ spec/stattracker_spec.rb
@@ -2,20 +2,16 @@ require_relative 'spec_helper'
 
 describe StatTracker do
 	before :each do
-	@stat1 = StatTracker.new(2012030221, 20122013, "Postseason", "5/16/13", 3, 6, 2, 3, "Toyota Stadium", "/api/v1/venues/null")
-	@stat2 = StatTracker.new(2012030222, 20122013, "Postseason", "5/19/13", 3, 6, 2, 3, "Toyota Stadium", "/api/v1/venues/null")
-	@stat3 = StatTracker.new(2012030223, 20122013, "Postseason", "5/21/13", 6, 3, 2, 1, "BBVA Stadium", "/api/v1/venues/null")
+	@stat1 = StatTracker.new
 	end
 
 	it 'exists' do
+		# require 'pry'; binding.pry
 		expect(@stat1).to be_a StatTracker
-		expect(@stat2).to be_a StatTracker
-		expect(@stat3).to be_a StatTracker
+		
 	end
 
-	it 'has an ID' do
-		expect(@stat1.game_id).to eq(2012030221)
-		expect(@stat2.game_id).to eq(2012030222)
-		expect(@stat3.game_id).to eq(2012030223)
+	it 'has can return IDs' do
+		expect(@stat1.game_ids).to eq([2012030221, 2012030222, 2012030223])
 	end
 end
