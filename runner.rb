1ee9f9152149f84c9db9b0962045c83cfe02b1ac
Tori Enyart <torienyart@gmail.com>
2023-01-02 18:46:33 -0700

    Set up runner file with information from iteration

--- runner.rb
+++ runner.rb
@@ -2,20 +2,16 @@ require './lib/stat_tracker'
 require 'csv'
 require './lib/game'
 
-game_path = './data/games.csv'
-team_path = './data/teams.csv'
-game_teams_path = './data/game_teams.csv'
-
-locations = {
-  games: game_path,
-  teams: team_path,
-  game_teams: game_teams_path
-}
 #15=19 should go to stattracker
+game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
 
-games = []
-CSV.foreach(locations[:games], headers: true) do |info|
-  games << Game.new(info)
-end
-require 'pry'; binding.pry
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
 stat_tracker = StatTracker.from_csv(locations)
+require 'pry'; binding.pry
