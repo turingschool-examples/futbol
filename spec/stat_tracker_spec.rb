74e21e72207f585af23d91311c8dad64ceddb127
KaraJH <karas.jones@yahoo.com>
2023-01-03 15:18:21 -0700

    test: first 3 moethods and helper methods tests

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,11 +1,10 @@
 require_relative 'spec_helper'
 
-
 RSpec.describe StatTracker do
   before(:all) do
-    game_path = './data/games.csv'
-    team_path = './data/teams.csv'
-    game_teams_path = './data/game_teams.csv'
+    game_path = './spec/fixtures/games_fixture.csv'
+    team_path = './spec/fixtures/teams_fixture.csv'
+    game_teams_path = './spec/fixtures/game_teams_fixture.csv'
 
     locations = {
       games: game_path,
@@ -14,9 +13,39 @@ RSpec.describe StatTracker do
     }
 
     @stat_tracker = StatTracker.from_csv(locations)
-    end
+  end
  
-    it "exists" do 
-        expect(@stat_tracker).to be_an_instance_of(StatTracker)
+  it "exists" do 
+   expect(@stat_tracker).to be_an_instance_of(StatTracker)
+  end
+  
+  describe '#highest_total_score' do 
+    it 'returns the highest sum of the winning and losing teams scores' do 
+      expect(@stat_tracker.highest_total_score).to eq(6)
     end
+  end
+
+  describe '#lowest_total_score' do 
+    it ' is the lowest sum of the wining and losing teams scores' do 
+      expect(@stat_tracker.lowest_total_score).to eq(1)
+    end
+  end
+
+  describe '#all_scores' do 
+    it 'is a helper method for highest and lowest total score methods' do 
+      expect(@stat_tracker.all_scores.class).to eq(Array)
+    end
+  end
+
+  describe '#percentage_home_wins' do 
+    it 'is the percentage of games that a home team has won' do 
+      expect(@stat_tracker.percentage_home_wins).to eq(0.53)
+    end
+  end
+
+  describe '#home_wins_array' do 
+    it 'is a helper method for percentage_home_wins, array of home goals greater than away goals' do 
+      expect(@stat_tracker.home_wins_array.class).to eq(Array)
+    end
+  end
 end
