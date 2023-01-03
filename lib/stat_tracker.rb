ab719f886265754e34b0a6c889c9a78c04505c48
Matt Enyeart <menyeart1@gmail.com>
2023-01-03 15:13:08 -0700

    Add the self from csv and intialize methods

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,5 +1,21 @@
-class StatTracker
-  def self.from_csv(locations)
+require 'csv'
 
+class StatTracker
+  attr_reader :data_array,
+              :games,
+              :teams,
+              :game_teams
+
+
+  def self.from_csv(locations)
+    @data_array = locations.values.map {|location| location }
+    @stat_tracker = StatTracker.new(@data_array)
   end
-end
+
+  def initialize(data_array)
+    @data_array = data_array
+    @games = CSV.read @data_array[0], headers: true, header_converters: :symbol
+    @teams = CSV.read @data_array[1], headers: true, header_converters: :symbol
+    @game_teams = CSV.read @data_array[2], headers: true, header_converters: :symbol
+  end
+
