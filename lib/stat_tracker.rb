df0ce5ee8d00afac377e474d5144e367da6d53de
Matt Enyeart <menyeart1@gmail.com>
2023-01-03 15:17:20 -0700

    Add the highest total score method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -19,3 +19,9 @@ class StatTracker
     @game_teams = CSV.read @data_array[2], headers: true, header_converters: :symbol
   end
 
+  def highest_total_score
+    @games.map {|row| row[:home_goals].to_i + row[:away_goals].to_i}.max
+  end
+
+
+
