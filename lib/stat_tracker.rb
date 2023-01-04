9b7f66aff5d86bcf586db42186392c888c0f0feb
Isaac Alter <i_alter@yahoo.com>
2023-01-04 16:10:34 -0700

    Adds feat: feature completion of i/o setup of itertion 1 with accompanying succesful tests

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,38 +1,16 @@
 require "csv"
 class StatTracker
-	attr_reader :game_teams,
-              :games,
-              :teams
+	attr_accessor :game_teams,
+                :games,
+                :teams
 
-	def initialize
-    @game_teams = Hash.new
-    @games = Hash.new
-    @teams = Hash.new
+	def initialize(locations)
+    @game_teams = CSV.read locations[:game_teams], headers: true, header_converters: :symbol 
+    @games = CSV.read locations[:games], headers: true, header_converters: :symbol
+    @teams = CSV.read locations[:teams], headers: true, header_converters: :symbol
 	end
   
 	def self.from_csv(locations)
-    stat_tracker = new 
-    teams_csv_reader(locations, stat_tracker)
-    require 'pry'; binding.pry
-    games_csv_reader(locations, stat_tracker)
-    game_teams_csv_reader(locations, stat_tracker)
-
-    stat_tracker
-  end
-
-  def self.teams_csv_reader(locations, stat_tracker)
-    row_database = []
-    CSV.foreach(locations[:teams], headers: true, header_converters: :symbol) do |row|
-      row_database << row
-    end
-    return row_database
-  end
-
-  def self.games_csv_reader(locations, stat_tracker)
-    contents = CSV.open locations[:games], headers: true, header_converters: :symbol
-  end
-
-  def self.game_teams_csv_reader(locations, stat_tracker)
-    contents = CSV.open locations[:game_teams], headers: true, header_converters: :symbol
+    StatTracker.new(locations)
   end
 end
