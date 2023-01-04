343c3f877d6a52ab3381926864eb14eae031974f
Weston Sandfort <sandfortw@gmail.com>
2023-01-03 18:10:27 -0700

    Creates league statistics category in the spec file, as well as adds a test for count_of_teams

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -103,6 +103,23 @@ describe StatTracker do
     it "can determine lowest_total_score" do
       expect(stat_tracker.lowest_total_score).to eq(1)
     end
+  end
+
+  describe 'league statistics' do
+    let(:game_path_2){'./data/fixtures/games_i2.csv'}
+    #note that we will need to edit team/game_team paths if new fixture data is created for use in these tests
+    
+    let(:locations_2){{
+      games: game_path_2,
+      teams: team_path,
+      game_teams: game_teams_path
+    }}
+
+    let(:stat_tracker) {StatTracker.from_csv(locations_2)}
+
+    it 'can count the number of teams' do
+      expect(stat_tracker.count_of_teams).to eq(19)
+    end
 
   end
 
