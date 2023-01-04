c9aab9fafa6a929dcced3968f49e81ded41f9559
Hady Matar <hadyematar@gmail.com>
2023-01-04 11:14:22 -0700

    Prior to making changes and removing objects

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -12,70 +12,78 @@ class StatTracker
   end
 
   def self.from_csv(locations)
-    game_teams = game_teams_from_csv(locations)
+    # game_teams = game_teams_from_csv(locations)
     games = games_from_csv(locations)
     teams = teams_from_csv(locations)
     StatTracker.new(game_teams, games, teams)
   end
 
-  def self.game_teams_from_csv(locations)
-    game_teams_array = []
-    CSV.foreach(locations[:game_teams], headers: true) do |info|
-      info = info.to_h
-      hash2= info.map do |value, key|
-        [value.to_sym, key]
-      end
-      info = hash2.to_h
-      info_updated= []
-      info.each do |key, value|
-        if key == :game_id
-          info_updated << [key, value.to_i]
-        elsif key == :team_id
-          info_updated << [key, value.to_i]
-        elsif key == :HoA
-          info_updated << [key, value]
-        elsif key == :result
-          info_updated << [key, value]
-        elsif key == :settled_in
-          info_updated << [key, value]
-        elsif key == :head_coach
-          info_updated << [key, value]
-        elsif key == :goals
-          info_updated << [key, value.to_i]
-        elsif key == :shots
-          info_updated << [key, value.to_i]
-        elsif key == :tackles
-          info_updated << [key, value.to_i]
-        elsif key == :pim
-          info_updated << [key, value.to_i]
-        elsif key == :powerPlayOpportunities
-          info_updated << [key, value.to_i]
-        elsif key == :powerPlayGoals
-          info_updated << [key, value.to_i]
-        elsif key == :faceOffWinPercentage
-          info_updated << [key, value.to_f]
-        elsif key == :giveaways
-          info_updated << [key, value.to_i]
-        elsif key == :takeaways
-          info_updated << [key, value.to_i]
-        end
-      end 
-      info = info_updated.to_h
-      game_teams_array << GameTeam.new(info)
-    end
-    game_teams_array
-    end
+  # def self.game_teams_from_csv(locations)
+  #   game_teams_array = []
+  #   CSV.foreach(locations[:game_teams], headers: true) do |info|
+  #     info = info.to_h
+  #     hash2= info.map do |value, key|
+  #       [value.to_sym, key]
+  #     end
+  #     info = hash2.to_h
+  #     info_updated= []
+  #     info.each do |key, value|
+  #       if key == :game_id
+  #         info_updated << [key, value.to_i]
+  #       elsif key == :team_id
+  #         info_updated << [key, value.to_i]
+  #       elsif key == :HoA
+  #         info_updated << [key, value]
+  #       elsif key == :result
+  #         info_updated << [key, value]
+  #       elsif key == :settled_in
+  #         info_updated << [key, value]
+  #       elsif key == :head_coach
+  #         info_updated << [key, value]
+  #       elsif key == :goals
+  #         info_updated << [key, value.to_i]
+  #       elsif key == :shots
+  #         info_updated << [key, value.to_i]
+  #       elsif key == :tackles
+  #         info_updated << [key, value.to_i]
+  #       elsif key == :pim
+  #         info_updated << [key, value.to_i]
+  #       elsif key == :powerPlayOpportunities
+  #         info_updated << [key, value.to_i]
+  #       elsif key == :powerPlayGoals
+  #         info_updated << [key, value.to_i]
+  #       elsif key == :faceOffWinPercentage
+  #         info_updated << [key, value.to_f]
+  #       elsif key == :giveaways
+  #         info_updated << [key, value.to_i]
+  #       elsif key == :takeaways
+  #         info_updated << [key, value.to_i]
+  #       end
+  #     end 
+  #     info = info_updated.to_h
+  #     game_teams_array << GameTeam.new(info)
+  #   end
+  #   game_teams_array
+  #   end
   
 
   def self.games_from_csv(locations)
     games_array = []
     CSV.foreach(locations[:games], headers: true) do |info|
-      info = info.to_h
-      hash2= info.map do |value, key|
-        [value.to_sym, key]
-      end
-      info = hash2.to_h
-      games_array << Game.new(info)
+      
+      new_info = {
+        game_id: info["game_id"].to_i, 
+        season: info["season"], 
+        type: info[]
+
+
+      }  
+
+      games_array << new_info
+      require 'pry'; binding.pry
+
+       
+      # games_array << Game.new(info)
     end
     games_array
   end
