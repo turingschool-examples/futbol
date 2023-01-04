71df0c788a09d57f7dda205434cd3f960f9a9d75
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-01-03 17:00:52 -0700

    Add: #most_goals_scored method and #all_game_scores_by_team helper method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -12,9 +12,9 @@ class StatTracker
 	end
 
 	def self.from_csv(multiple_data_paths) # in hash format
-		games_data = CSV.open multiple_data_paths[:games], headers: true, header_converters: :symbol
-		teams_data = CSV.open multiple_data_paths[:teams], headers: true, header_converters: :symbol
-		game_teams_data = CSV.open multiple_data_paths[:game_teams], headers: true, header_converters: :symbol
+		games_data = CSV.read multiple_data_paths[:games], headers: true, header_converters: :symbol
+		teams_data = CSV.read multiple_data_paths[:teams], headers: true, header_converters: :symbol
+		game_teams_data = CSV.read multiple_data_paths[:game_teams], headers: true, header_converters: :symbol
 		StatTracker.new(games_data, teams_data, game_teams_data)
 	end
 
@@ -38,5 +38,17 @@ class StatTracker
 		total_scores.min
 	end
 
+	def most_goals_scored(team_id)
+		all_game_scores_by_team[team_id.to_s].max.to_i
+	end
+
+	def all_game_scores_by_team
+		hash = Hash.new {|k, v| k[v] = []}
+		@games.each do |row|
+			hash[row[:home_team_id]] << row[:home_goals]
+			hash[row[:away_team_id]] << row[:away_goals]
+		end
+		hash
+	end
   
 end
