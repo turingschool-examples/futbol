eda973dcab1101e5e8b57e171ebcf5a93e53b8f1
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-01-03 21:31:36 -0700

    Add attributes

--- lib/team.rb
+++ lib/team.rb
@@ -1,6 +1,17 @@
-
 class Team
-    def initialize
+    attr_reader :team_id,
+                :franchise_id,
+                :team_name,
+                :abbreviation,
+                :stadium,
+                :link
 
+    def initialize(info)
+        @team_id = info[:team_id]
+        @franchise_id = info[:franchiseID]
+        @team_name = info[:teamName]
+        @abbreviation = info[:abbreviation]
+        @stadium = info[:Stadium]
+        @link = info[:link]
     end
 end
