ac69d8b5f6454154cdb7487003fb6532a5c18e2d
Matt Enyeart <menyeart1@gmail.com>
2023-01-03 17:05:04 -0700

    Add initial setup and first test for #method

---
+++ spec/stat_tracker_spec.rb
@@ -0,0 +1,22 @@
+require './lib/stat_tracker'
+
+RSpec.describe StatTracker do
+  before(:all) do
+    game_path = './data/fake_games.csv'
+    team_path = './data/fake_teams.csv'
+    game_teams_path = './data/fake_game_teams.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    @stat_tracker = StatTracker.from_csv(locations)
+  end
+
+  it "exists" do
+    expect(@stat_tracker).to be_an_instance_of StatTracker
+  end
+
+end
