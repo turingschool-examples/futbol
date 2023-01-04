b4638ce13dd19e534b15aa5a8311a4d4b7d5751a
David Marino <davidjmarino8@gmail.com>
2023-01-03 21:14:31 -0500

    add instance methods to initialize

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -16,9 +16,9 @@ describe StatTracker do
       expect(stat_tracker.games).to be_a(CSV::Table)
       expect(stat_tracker.teams).to be_a(CSV::Table)
       expect(stat_tracker.game_teams).to be_a(CSV::Table)
+      expect(stat_tracker.game_id).to be_a(Array)
     end
   end
-# end
 
 
 
@@ -202,16 +202,5 @@ describe StatTracker do
       stat_tracker.count_of_teams
       expect(stat_tracker.count_of_teams).to eq 32
     end
-  end
-
-  describe '#best_offense' do
-    it 'names a team with the highest avg of goals for the season' do
-      expect(stat_tracker.best_offense).to eq "Reign FC"
-    end
-
-    # it 'names a team with the highest avg of goals for the season' do
-    #   stat_tracker.season_goals(away, home)
-    #   expect(stat_tracker.season_goals).to eq "Reign FC"
-    # end
   end
 end
