1eeb7b32d9c0cc6079b9de63ce4719df859add2f
David Marino <davidjmarino8@gmail.com>
2023-01-03 20:09:25 -0500

    refine gameinfo spec

--- lib/game_info.rb
+++ lib/game_info.rb
@@ -13,13 +13,13 @@ class GameInfo
   def initialize(info)
     @game_id = info[:game_id]
     @season = info[:season]
-    @type = info[:type]
+    @type = info[:type].to_s
     @date_time = info[:date_time]
     @away_team_id = info[:away_team_id]
     @home_team_id = info[:home_team_id]
     @away_goals = info[:away_goals]
     @home_goals = info[:home_goals]
-    @venue = info[:venue]
-    @venue_link = info[:venue_link]
+    @venue = info[:venue].to_s
+    @venue_link = info[:venue_link].to_s
   end
 end
