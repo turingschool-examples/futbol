4c6a79743235fcb1c74575771c887b5de733c34b
David Marino <davidjmarino8@gmail.com>
2023-01-05 14:23:36 -0500

    Add: data sets

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -12,8 +12,6 @@ class StatTracker
     @teams = CSV.read(locations[:teams], headers: true, header_converters: :symbol)
     @game_teams = CSV.read(locations[:game_teams], headers: true, header_converters: :symbol)
     @total_score_array = total_score
-    @games_by_season = count_of_games_by_season
-
     @game_id = @games[:game_id]
     @season = @games[:season]
     @type = @games[:type].to_s
@@ -24,7 +22,6 @@ class StatTracker
     @home_goals = @games[:home_goals]
     @venue = @games[:venue].to_s
     @venue_link = @games[:venue_link].to_s
-    @total_scores = []
   end
 
   def self.from_csv(locations)
@@ -69,81 +66,82 @@ class StatTracker
     end
     (count.to_f / @games.size).round(2)
   end
-end
 
-def best_offense
-  teams = []
-  @games.each do |row|
-    teams.push([row[:home_team_id], row[:home_goals]])
-    teams.push([row[:away_team_id], row[:away_goals]])
-  end
 
-  hash = Hash.new {|hash, key| hash[key] = []}
-  teams.each do |array|
-    hash[array[0]] << array[1].to_i
-  end
-
-  hash.each do |k, v|
-    if v.size > 1
-      hash[k] = v.sum.to_f/v.size
-    else
-      hash[k] = v[0].to_f/1
+  def best_offense
+    teams = []
+    @games.each do |row|
+      teams.push([row[:home_team_id], row[:home_goals]])
+      teams.push([row[:away_team_id], row[:away_goals]])
     end
-  end
 
-  max_team = hash.max_by do |k, v|
-    v
-  end
-
-  best_offense = nil
-  @teams.each do |team|
-    best_offense = team[:teamname] if team[:team_id] == max_team[0]
-  end
-  best_offense
-end
-
-def worst_offense
-  teams = []
-  @games.each do |row|
-    teams.push([row[:home_team_id], row[:home_goals]])
-    teams.push([row[:away_team_id], row[:away_goals]])
-  end
-
-  hash = Hash.new {|hash, key| hash[key] = []}
-  teams.each do |array|
-    hash[array[0]] << array[1].to_i
-  end
-
-  hash.each do |k, v|
-    if v.size > 1
-      hash[k] = v.sum.to_f/v.size
-    else
-      hash[k] = v[0].to_f/1
+    hash = Hash.new {|hash, key| hash[key] = []}
+    teams.each do |array|
+      hash[array[0]] << array[1].to_i
     end
-  end
 
-  min_team = hash.min_by do |k, v|
-    v
-  end
-
-  worst_offense = nil
-  @teams.each do |team|
-    worst_offense = team[:teamname] if team[:team_id] == min_team[0]
-  end
-  worst_offense
-end
-
-def highest_scoring_visitor
-  visitor_goals = Hash.new(0)
-  @teams.each do |team|
-    @games.each do |game|
-      if game[:away_team_id] == team[:team_id]
-        visitor_goals[team[:teamname]] += game[:away_goals].to_i
+    hash.each do |k, v|
+      if v.size > 1
+        hash[k] = v.sum.to_f/v.size
+      else
+        hash[k] = v[0].to_f/1
       end
     end
+
+    max_team = hash.max_by do |k, v|
+      v
+    end
+
+    best_offense = nil
+    @teams.each do |team|
+      best_offense = team[:teamname] if team[:team_id] == max_team[0]
+    end
+    best_offense
   end
-  best_team = visitor_goals.max_by {|team, goals| goals}
-  best_team[0]
+
+  def worst_offense
+    teams = []
+    @games.each do |row|
+      teams.push([row[:home_team_id], row[:home_goals]])
+      teams.push([row[:away_team_id], row[:away_goals]])
+    end
+
+    hash = Hash.new {|hash, key| hash[key] = []}
+    teams.each do |array|
+      hash[array[0]] << array[1].to_i
+    end
+
+    hash.each do |k, v|
+      if v.size > 1
+        hash[k] = v.sum.to_f/v.size
+      else
+        hash[k] = v[0].to_f/1
+      end
+    end
+
+    min_team = hash.min_by do |k, v|
+      v
+    end
+
+    worst_offense = nil
+    @teams.each do |team|
+      worst_offense = team[:teamname] if team[:team_id] == min_team[0]
+    end
+    worst_offense
+  end
+
+  def highest_scoring_visitor
+    visitor_goals = Hash.new(0)
+    @teams.each do |team|
+      @games.each do |game|
+        if game[:away_team_id] == team[:team_id]
+          visitor_goals[team[:teamname]] += game[:away_goals].to_i
+        end
+      end
+    end
+    best_team = visitor_goals.max_by {|team, goals| goals}
+    best_team[0]
+  end
 end
 
 
