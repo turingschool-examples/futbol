dd37fd937d8963f6bb95b4da54ec189d9132a286
Elle Majors <hellolovely@ellemajors>
2023-01-05 15:39:33 -0700

    test: #most_tackles

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -243,4 +243,8 @@ describe StatTracker do
     end
   end
 
+  it "#most_tackles returns team with the most tackles in the season " do
+    expect(@stat_tracker.most_tackles("20132014")).to eq("FC Cincinnati")
+    expect(@stat_tracker.most_tackles("20142015")).to eq("Seattle Sounders FC")
+  end
 end
