15bac45f61b92a7bf124cfd374445bcdb062a460
Matt Enyeart <menyeart1@gmail.com>
2023-01-05 09:16:05 -0700

    Refactor tests

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe StatTracker do
   it "exists" do
     expect(@stat_tracker).to be_an_instance_of StatTracker
   end
-  # Game
+
   it "can calculate the highest total game score" do
     expect(@stat_tracker.highest_total_score).to eq(5)
   end
@@ -27,18 +27,20 @@ RSpec.describe StatTracker do
     expect(@stat_tracker.lowest_total_score).to eq(3)
   end
 
-  # League
   it "can list the total number of teams" do
     expect(@stat_tracker.count_of_teams).to eq(5)
   end
 
   it "can name the team with the highest average score" do
-    require 'pry'; binding.pry
     expect(@stat_tracker.best_offense).to eq("FC Dallas")
   end
 
   it "can name the team with the lowest average score" do
-    expect(@stat_tracker.worst_offense).to eq("Houston Dynamo")
+    expect(@stat_tracker.worst_offense).to eq("Sporting Kansas City")
+  end
+
+  it "can name the team the highest number of tackles in a season" do
+    expect(@stat_tracker.worst_offense).to eq("Sporting Kansas City")
   end
 
 
