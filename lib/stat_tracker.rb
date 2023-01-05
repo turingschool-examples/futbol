600e9141b84aeaef3b25951f7866db5ca7716fd7
Harrison Ryan <hwryan12@gmail.com>
2023-01-04 19:33:22 -0600

    docs: Initialize stat_tracker

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -17,29 +17,43 @@ class StatTracker
     end
 
     def self.from_csv(location_paths)
-        StatTracker.new(location_paths)
+        new(location_paths)
     end
 
     def create_games_array(game_path)
         games = []
-        CSV.foreach(game_path, headers: true) do |info|
-            games << Game.new(info)
+        CSV.foreach(game_path, headers: true, header_converters: :symbol) do |info|
+            game = info.to_h
+            game[:away_goals] = game[:away_goals].to_i
+            game[:home_goals] = game[:home_goals].to_i
+            games << game
         end
         games
     end
 
     def create_teams_array(team_path)
       team = []
-      CSV.foreach(team_path, headers: true) do |info|
-          team << Team.new(info)
+      CSV.foreach(team_path, headers: true, header_converters: :symbol) do |info|
+          team << info.to_h
       end
       team
     end
 
     def create_game_teams_array(game_team_path)
       game_teams = []
-      CSV.foreach(game_team_path, headers: true) do |info|
-          game_teams << GameTeam.new(info)
+      CSV.foreach(game_team_path, headers: true, header_converters: :symbol) do |info|
+          game_team = info.to_h
+          game_team[:goals] = game_team[:goals].to_i
+          game_team[:shots] = game_team[:shots].to_i
+          game_team[:tackles] = game_team[:tackles].to_i
+          game_team[:pim] = game_team[:pim].to_i
+          game_team[:powerplayopportunities] = game_team[:powerplayopportunities].to_i
+          game_team[:powerplaygoals] = game_team[:powerplaygoals].to_i
+          game_team[:faceoffwinpercentage] = game_team[:faceoffwinpercentage].to_f
+          game_team[:giveaways] = game_team[:giveaways].to_i
+          game_team[:takeaways] = game_team[:takeaways].to_i
+          game_teams << game_team
+        #   require "pry"; binding.pry
       end
       game_teams
     end
