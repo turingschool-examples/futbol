58ff08e94d741cee3fdcec8e9bfcb12ae7ae1296
Melony Franchini <meltravelz@gmail.com>
2023-01-05 18:47:34 -0700

    Fix all sytax on spec/class files after merge

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -21,7 +21,6 @@ class StatTracker
   def self.game_teams_from_csv(locations)
     game_teams_array = []
     CSV.foreach(locations[:game_teams], headers: true) do |info|
-  
       new_info = {
         game_id: info["game_id"].to_i,
         team_id: info["team_id"].to_i, 
@@ -39,9 +38,7 @@ class StatTracker
         giveaways: info["giveaways"].to_i,
         takeaways: info["takeaways"].to_i
       }  
-
       game_teams_array << new_info
-    
     end
     game_teams_array
   end
@@ -49,7 +46,6 @@ class StatTracker
   def self.games_from_csv(locations)
     games_array = []
     CSV.foreach(locations[:games], headers: true) do |info|
-  
       new_info = {
         game_id: info["game_id"].to_i, 
         season: info["season"], 
@@ -62,9 +58,7 @@ class StatTracker
         venue: info["venue"],
         venue_link: info["venue_link"]
       }  
-
       games_array << new_info
-    
     end
     games_array
   end
@@ -72,7 +66,6 @@ class StatTracker
   def self.teams_from_csv(locations)
     teams_array = []
     CSV.foreach(locations[:teams], headers: true) do |info|
-
       new_info = {
         team_id: info["team_id"].to_i,
         franchise_id: info["franchiseId"].to_i,
@@ -81,105 +74,101 @@ class StatTracker
         stadium: info["Stadium"],
         link: info["link"]
       }
-
       teams_array << new_info
     end
     teams_array
   end
- #DO NOT CHANGE ANYTHING ABOVE THIS POINT ^
 
+ ################## Game Statisics ##################
 
-  # Highest sum of the winning and losing teams’ scores
+  # Highest sum of the winning and losing teams scores
   def highest_total_score
     total_scores.last
   end
 
-  # Lowest sum of the winning and losing teams’ scores
+  # Lowest sum of the winning and losing teams scores
   def lowest_total_score
     total_scores.first
   end
 
-  # HELPER: Array of the winning and losing teams’ scores
+  # HELPER: for highest/lowest total score
   def total_scores
     game_sums = @games.map do |game|
       game[:away_goals] + game[:home_goals]
     end.sort
   end
 
-  #Total number of teams in the data
-  def count_of_teams
-    @teams.count
-  end
-
-  #Team name w/ highest avg num of goals scored (per game across all seasons)
-  def best_offense
-    team_id_all_goals = Hash.new { |hash, key| hash[key] = [] }
-    @game_teams.each do |info_line|
-      team_id_all_goals[info_line[:team_id]] << info_line[:goals]
-    end
-  
-    team_id_avg_goals = Hash.new { |hash, key| hash[key] = 0 }
-    team_id_all_goals.each do |team_id, goals_scored|
-      team_id_avg_goals[team_id] = (goals_scored.sum.to_f / goals_scored.length.to_f).round(2)
-    end
-
-    # if 2 teams have the same avg this will return ONLY the FIRST one:
-    @teams.first do |info_line|
-      if info_line[:team_id] == team_id_avg_goals.key(team_id_avg_goals.values.max)
-        return info_line[:team_name] 
+ # Percentage of games that a home team has won (rounded to the nearest 100th)
+  def percentage_home_wins
+    total_of_home_games = 0
+    wins_at_home = 0 
+    @game_teams.each do | k, v |
+      if k[:hoa] == "home"
+        total_of_home_games += 1
+      end
+      if k[:hoa] == "home" && k[:result] == "WIN"
+        wins_at_home += 1
       end
     end
+    percent_win = ((wins_at_home / total_of_home_games.to_f)).round(2)
   end
 
-  #Team name w/ lowest avg num of goals scored (per game across all seasons)
-  def worst_offense
-    team_id_all_goals = Hash.new { |hash, key| hash[key] = [] }
-    @game_teams.each do |info_line|
-      team_id_all_goals[info_line[:team_id]] << info_line[:goals]
+  # Percentage of games that a visitor has won (rounded to the nearest 100th)
+  def percentage_visitor_wins
+    total_of_home_games = 0
+    losses_at_home = 0 
+    @game_teams.each do | k, v |
+      if k[:hoa] == "home"
+        total_of_home_games += 1
+      end
+      if k[:hoa] == "home" && k[:result] == "LOSS"
+        losses_at_home += 1
+      end
     end
-  
-    team_id_avg_goals = Hash.new { |hash, key| hash[key] = 0 }
-    team_id_all_goals.each do |team_id, goals_scored|
-      team_id_avg_goals[team_id] = (goals_scored.sum.to_f / goals_scored.length.to_f).round(2)
+    percent_loss = ((losses_at_home / total_of_home_games.to_f)).round(2)
+  end
+ 
+  # Percentage of games that has resulted in a tie (rounded to the nearest 100th)
+  def percentage_ties
+    ties = 0 
+    total_of_games = @game_teams.count
+    @game_teams.each do | k, v |
+      if k[:result] == "TIE"
+        ties += 1
+      end
     end
-
-    # if 2 teams have the same avg this will return ONLY the FIRST one:
-    @teams.first do |info_line|
-      if info_line[:team_id] == team_id_avg_goals.key(team_id_avg_goals.values.min)
-
-
- #Percentage of games that a home team has won (rounded to the nearest 100th)
-
-#  Method count of games by season
- def count_of_games_by_season
-  new_hash = Hash.new(0) 
-
-  games.each do |game|
-    new_hash[game[:season]] += 1
+    percent_ties = ((ties / total_of_games.to_f)).round(2)
   end
-  return new_hash
- end
 
-# Method of average goals per game  
- def average_goals_per_game 
-  goals = 0 
-  games.each do |game|
-    goals += (game[:away_goals] + game[:home_goals]) 
+  # Method count of games by season
+  def count_of_games_by_season
+    new_hash = Hash.new(0) 
+    games.each do |game|
+      new_hash[game[:season]] += 1
+    end
+    return new_hash
   end
-  average = (goals.to_f/(games.count.to_f)).round(2)
-  return average
- end
-# Method average goals by season
- def average_goals_by_season
-  new_hash = Hash.new(0) 
 
-  games.each do |game|
-    new_hash[game[:season]]  = season_goals(game[:season])
+  # Method of average goals per game  
+  def average_goals_per_game 
+    goals = 0 
+    games.each do |game|
+      goals += (game[:away_goals] + game[:home_goals]) 
+    end
+    average = (goals.to_f/(games.count.to_f)).round(2)
+    return average
   end
-  return new_hash
- end
+ 
+  # Method average goals by season
+  def average_goals_by_season
+    new_hash = Hash.new(0) 
+    games.each do |game|
+      new_hash[game[:season]]  = season_goals(game[:season])
+    end
+    return new_hash
+  end
 
-# Helper method to average goals by season 
+  # HELPER: for average goals by season 
  def season_goals(season)
   number = 0
   goals = 0
@@ -192,34 +181,150 @@ class StatTracker
   average = (goals.to_f/number.to_f).round(2)
  end
 
-#  WINNINGNEST COACH AND HELPER METHODS BELOW 
+ ################## League Statisics ##################
 
+  #Total number of teams in the data
+  def count_of_teams
+    @teams.count
+  end
+
+  #Team name w/ highest avg num of goals scored (per game across all seasons)
+  def best_offense
+    team_id_all_goals = Hash.new { |hash, key| hash[key] = [] }
+    @game_teams.each do |info_line|
+      team_id_all_goals[info_line[:team_id]] << info_line[:goals]
+    end
+    team_id_avg_goals = Hash.new { |hash, key| hash[key] = 0 }
+    team_id_all_goals.each do |team_id, goals_scored|
+      team_id_avg_goals[team_id] = (goals_scored.sum.to_f / goals_scored.length.to_f).round(2)
+    end
+    # if 2 teams have the same avg this will return ONLY the FIRST one:
+    @teams.find do |info_line|
+      if info_line[:team_id] == team_id_avg_goals.key(team_id_avg_goals.values.max)
+        return info_line[:team_name] 
+      end
+    end
+  end
+
+  #Team name w/ lowest avg num of goals scored (per game across all seasons)
+  def worst_offense
+    team_id_all_goals = Hash.new { |hash, key| hash[key] = [] }
+    @game_teams.each do |info_line|
+      team_id_all_goals[info_line[:team_id]] << info_line[:goals]
+    end
+
+    team_id_avg_goals = Hash.new { |hash, key| hash[key] = 0 }
+    team_id_all_goals.each do |team_id, goals_scored|
+      team_id_avg_goals[team_id] = (goals_scored.sum.to_f / goals_scored.length.to_f).round(2)
+    end
+
+    # if 2 teams have the same avg this will return ONLY the FIRST one:
+    @teams.find do |info_line|
+      if info_line[:team_id] == team_id_avg_goals.key(team_id_avg_goals.values.min)
+        return info_line[:team_name] 
+      end
+    end
+  end
+
+  # Name of the team with the highest average score per game across all seasons when they are away.	
+  def highest_scoring_visitor
+    id_goals = Hash.new { |hash, key| hash[key] = [] }
+    @game_teams.each do | k, v |
+      if k[:hoa] == "away"
+        id_goals[k[:team_id]] << k[:goals]
+      end
+    end
+    id_goals_avg = Hash.new { |hash, key| hash[key] = 0 }
+    id_goals.each do |team_id, goals_scored|
+      id_goals_avg[team_id] = (goals_scored.sum.to_f / goals_scored.length.to_f).round(2)
+    end
+    @teams.find do |info_line|
+      if info_line[:team_id] == id_goals_avg.key(id_goals_avg.values.max)
+        return info_line[:team_name] 
+      end
+    end
+  end
+  
+  # Name of the team with the highest average score per game across all seasons when they are home. 
+  def highest_scoring_home_team
+    id_goals = Hash.new { |hash, key| hash[key] = [] }
+    @game_teams.each do | k, v |
+      if k[:hoa] == "home"
+        id_goals[k[:team_id]] << k[:goals]
+      end
+    end
+    id_goals_avg = Hash.new { |hash, key| hash[key] = 0 }
+    id_goals.each do |team_id, goals_scored|
+      id_goals_avg[team_id] = (goals_scored.sum.to_f / goals_scored.length.to_f).round(2)
+    end
+    @teams.find do |info_line|
+      if info_line[:team_id] == id_goals_avg.key(id_goals_avg.values.max)
+        return info_line[:team_name] 
+      end
+    end
+  end
+
+  # Name of the team with the lowest average score per game across all seasons when they are a visitor.
+  def lowest_scoring_visitor
+    id_goals = Hash.new { |hash, key| hash[key] = [] }
+    @game_teams.each do | k, v |
+      if k[:hoa] == "away"
+        id_goals[k[:team_id]] << k[:goals]
+      end
+    end
+    id_goals_avg = Hash.new { |hash, key| hash[key] = 0 }
+    id_goals.each do |team_id, goals_scored|
+      id_goals_avg[team_id] = (goals_scored.sum.to_f / goals_scored.length.to_f).round(2)
+    end
+    @teams.find do |info_line|
+      if info_line[:team_id] == id_goals_avg.key(id_goals_avg.values.min)
+        return info_line[:team_name] 
+      end
+    end
+  end
+
+  # Name of the team with the lowest average score per game across all seasons when they are at home.
+  def lowest_scoring_home_team
+    id_goals = Hash.new { |hash, key| hash[key] = [] }
+    @game_teams.each do | k, v |
+    if k[:hoa] == "home"
+        id_goals[k[:team_id]] << k[:goals]
+      end
+    end
+    id_goals_avg = Hash.new { |hash, key| hash[key] = 0 }
+    id_goals.each do |team_id, goals_scored|
+      id_goals_avg[team_id] = (goals_scored.sum.to_f / goals_scored.length.to_f).round(2)
+    end
+    @teams.find do |info_line|
+      if info_line[:team_id] == id_goals_avg.key(id_goals_avg.values.min)
+
+        return info_line[:team_name] 
+      end
+    end
+  end
+
+ ################## Season Statisics ##################
+
+  #  WINNINGNEST/WORST COACH AND HELPER METHODS BELOW 
   def winningest_coach(season)
-
     raw_hash_values(season, "winning")
-
   end 
 
   def worst_coach(season)
-
     raw_hash_values(season, "worst")
-
   end
 
   def raw_hash_values(season, type)
     coach_games = Hash.new(0)
     coach_victories = Hash.new(0)
-
     total_games = determine_games(season)
     all_coaches = total_games.group_by do |game|
       game[:head_coach]
     end
-    
     all_coaches.keys.each do |coach|
       coach_games[coach] = 0
       coach_victories[coach] = 0
   end
-
     games.each do |game|
       game_teams.each do |game_team|
         if game[:season] == season 
@@ -243,11 +348,9 @@ class StatTracker
     games_in_season = games.find_all do |game| 
       game[:season] == season
     end 
-  
     games_in_season_gameid = games_in_season.map do |game| 
       game[:game_id]
     end 
-    
     total_relevant_games = []
     games_in_season_gameid.each do |game_id| 
       game_teams.each do |game_team|
@@ -260,160 +363,111 @@ class StatTracker
   end 
 
   def sort_coach_percentages(coach_games, coach_victories, type)
-
     additional_hash = {}
-    
     coach_games.each do |key, value|
-    coach_victories.each do |key_v, value_v|
-      if key == key_v 
-        percent = (value_v / value.to_f) 
-      additional_hash[key] = percent
+      coach_victories.each do |key_v, value_v|
+        if key == key_v 
+          percent = (value_v / value.to_f) 
+        additional_hash[key] = percent
+        end
       end
     end
-    end
     sorted_array = additional_hash.sort_by do |key, value|
-    value
+      value
     end
     determine_coach(sorted_array, type)
   end 
-  
+
   def determine_coach(sorted_array, type)
     if type == "winning"
       sorted_array = sorted_array.reverse
     elsif type == "worst"
-    sorted_array = sorted_array
-  
+      sorted_array = sorted_array
     end 
-  
     result = sorted_array[0][0]
     return result 
   end 
+  #  WINNINGNEST/WORST COACH AND HELPER METHODS ABOVE 
 
-end
+  #  MOST/LEAST ACCURATE TEAM METHODS BELOW
+  # Name of the Team with the best ratio of shots to goals for the season
+  def most_accurate_team
+    #Not sure if I need this info?: 
+    team_ids_array = @teams.map do |info_line|
+      info_line[:team_id]
+    end
+    # This returns: [1, 4, 26, 14, 6, 3, 5, 17, 28, 18, 23, 16, 9, 8, 30, ... # these are team_id numbers
 
- 
- # Percentage of games that a home team has won (rounded to the nearest 100th)
-  def percentage_home_wins
-    total_of_home_games = 0
-    wins_at_home = 0 
-    @game_teams.each do | k, v |
-      if k[:hoa] == "home"
-        total_of_home_games += 1
-      end
-      if k[:hoa] == "home" && k[:result] == "WIN"
-        wins_at_home += 1
-      end
+    game_ids_by_season = Hash.new {|hash, key| hash[key] = []}
+    @games.group_by do |game| 
+      game_ids_by_season[game[:season]] << game[:game_id]
     end
-    percent_win = ((wins_at_home / total_of_home_games.to_f)).round(2)
-  end
-  # Percentage of games that a visitor has won (rounded to the nearest 100th)
-  def percentage_visitor_wins
-    total_of_home_games = 0
-    losses_at_home = 0 
-    @game_teams.each do | k, v |
-      if k[:hoa] == "home"
-        total_of_home_games += 1
-      end
-      if k[:hoa] == "home" && k[:result] == "LOSS"
-        losses_at_home += 1
-      end
-    end
-    percent_loss = ((losses_at_home / total_of_home_games.to_f)).round(2)
-  end
-   #  Percentage of games that has resulted in a tie (rounded to the nearest 100th)
-  def percentage_ties
-    ties = 0 
-    total_of_games = @game_teams.count
-    @game_teams.each do | k, v |
-      if k[:result] == "TIE"
-        ties += 1
-      end
-    end
-    percent_ties = ((ties / total_of_games.to_f)).round(2)
-  end
+    # This returns: {season_id=>[game_id, 2016020207, 2016020663, 2016020917, 2016020956, 2016020598, 2016020874],
+    # 20172018=>[2017020484, 2017020385, 2017020694, 2017020992, 2017020124, ...
+    
+    ########## NOT FINISHED!! JUST BRAINSTORMING HERE: ############
+    # @game_ids_by_season.each do |game_id_array|
+    #   game_id_array.each do |game_id|
+    #     game_id[:game_id] 
+    #     game_id[:team_id]
+    #     game_id [[team_id, tackles], [team_id, tackles]]
+    #   end
+    # end
 
-  # Name of the team with the highest average score per game across all seasons when they are away.	
-  def highest_scoring_visitor
-    id_goals = Hash.new { |hash, key| hash[key] = [] }
-    @game_teams.each do | k, v |
-      if k[:hoa] == "away"
-        id_goals[k[:team_id]] << k[:goals]
-      end
-    end
+    # @games.each do |info_line|
+    #   info_line[:game_id] == game_id
+    # end
 
-    id_goals_avg = Hash.new { |hash, key| hash[key] = 0 }
-    id_goals.each do |team_id, goals_scored|
-      id_goals_avg[team_id] = (goals_scored.sum.to_f / goals_scored.length.to_f).round(2)
-    end
+    # ratios_by_game_id = Hash.new {|hash, key| hash[key] = []}
+    # @game_teams.group_by do |info_line|
+    #   ratios_by_game_id[info_line[:game_id]] << (info_line[:goals].to_f / info_line[:shots]).round(2)
+    # end
 
-    @teams.find do |info_line|
-      if info_line[:team_id] == id_goals_avg.key(id_goals_avg.values.max)
-        return info_line[:team_name] 
-      end
-    end
-  end
-  
-  # Name of the team with the highest average score per game across all seasons when they are home. 
-  def highest_scoring_home_team
-    id_goals = Hash.new { |hash, key| hash[key] = [] }
-    @game_teams.each do | k, v |
-      if k[:hoa] == "home"
-        id_goals[k[:team_id]] << k[:goals]
-      end
-    end
-
-    id_goals_avg = Hash.new { |hash, key| hash[key] = 0 }
-    id_goals.each do |team_id, goals_scored|
-      id_goals_avg[team_id] = (goals_scored.sum.to_f / goals_scored.length.to_f).round(2)
-    end
-
-    @teams.find do |info_line|
-      if info_line[:team_id] == id_goals_avg.key(id_goals_avg.values.max)
-        return info_line[:team_name] 
-      end
-    end
+    # season_game_ids = games_in_season.map do |game| 
+    #   game[:game_id]
+    # end
+      
+    #PSEUDO CODE: 
+    # team takes 10 shots - 9 make it, then ratio = 90% - Most accurate
+    # team takes 10 shots - 2 make it, then ratio = 20% - Least accurate
+    # find each game_id (with diff team_ids cuz 2 each in CSV file) in array divide goals/shots(float) 
+    # take(sum) floats and divide by how many games(.count)
+    # game_ids_by_season.each do |season_id|
+    #   season_id.each do |game_id_array|
+    # From Dani?? @game_teams.find_all {|game| season_game_ids.include?(game.game_id)}
   end
 
-  # Name of the team with the lowest average score per game across all seasons when they are a visitor.
-  def lowest_scoring_visitor
-    id_goals = Hash.new { |hash, key| hash[key] = [] }
-    @game_teams.each do | k, v |
-      if k[:hoa] == "away"
-        id_goals[k[:team_id]] << k[:goals]
-      end
-    end
-  
-    id_goals_avg = Hash.new { |hash, key| hash[key] = 0 }
-    id_goals.each do |team_id, goals_scored|
-      id_goals_avg[team_id] = (goals_scored.sum.to_f / goals_scored.length.to_f).round(2)
-    end
-  
-    @teams.find do |info_line|
-      if info_line[:team_id] == id_goals_avg.key(id_goals_avg.values.min)
-        return info_line[:team_name] 
-      end
-    end
+  # def least_accurate_team
+  # end
+  # MOST/LEAST ACCURATE TEAM METHODS ABOVE
+
+  def most_tackles
   end
-  # Name of the team with the lowest average score per game across all seasons when they are at home.
-  def lowest_scoring_home_team
-    id_goals = Hash.new { |hash, key| hash[key] = [] }
-    @game_teams.each do | k, v |
-    if k[:hoa] == "home"
-        id_goals[k[:team_id]] << k[:goals]
-      end
-    end
 
-    id_goals_avg = Hash.new { |hash, key| hash[key] = 0 }
-    id_goals.each do |team_id, goals_scored|
-      id_goals_avg[team_id] = (goals_scored.sum.to_f / goals_scored.length.to_f).round(2)
-    end
+  def fewest_tackles
+  end
 
-    @teams.find do |info_line|
-      if info_line[:team_id] == id_goals_avg.key(id_goals_avg.values.min)
+ ################## Team Statisics ##################
 
-        return info_line[:team_name] 
-      end
-    end
+  def team_info
+  end
+
+  def best_season
+  end
+
+  def worst_season
+  end
+
+  def average_win_percentage
+  end
+
+  def most_goals_scored
+  end
+
+  def favorite_opponent
+  end
+
+  def rival
   end
 
 end
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -20,51 +20,49 @@ RSpec.describe StatTracker do
     end
 
     it "creates three arrays which house the information" do
-      
       expect(stat_tracker.game_teams).to be_an_instance_of(Array)
-
       expect(stat_tracker.games).to be_an_instance_of(Array)
-
       expect(stat_tracker.teams).to be_an_instance_of(Array)
     end
 
     it "all of the objects in the respective arrays are of one specific object type consistent with the array" do 
-
       expect(stat_tracker.game_teams[0..51]).to all(be_an_instance_of(Hash))
-
       expect(stat_tracker.games[0..47]).to all(be_an_instance_of(Hash))
-        
       expect(stat_tracker.teams[0..32]).to all(be_an_instance_of(Hash))
-
     end 
-
 
     it "the objects all have the correct attributes" do 
-    
       expect(stat_tracker.game_teams[0][:game_id]).to eq(2012030225)
-
       expect(stat_tracker.game_teams[rand(0..51)][:game_id]).to be_a(Integer)
-
       expect(stat_tracker.games[0][:home_goals]).to eq(1)
-
       expect(stat_tracker.teams[0][:team_id]).to eq(1)
-
       expect(stat_tracker.games[0][:venue]).to eq("Providence Park")
-
       expect(stat_tracker.teams[0][:abbreviation]).to eq("ATL")
-
     end 
   end
-  #DO NOT CHANGE ANYTHING ABOVE THIS POINT ^
   
   describe "Game Statisics" do
+    it "#highest_total_score" do
+      expect(stat_tracker.highest_total_score).to eq(9)
+    end
+
+    it "#lowest_total_score" do
+      expect(stat_tracker.lowest_total_score).to eq(1)
+    end
+
     it "#percentage_home_wins" do
       expect(stat_tracker.percentage_home_wins).to eq(0.43)
     end
 
+    it "#percentage_visitor_wins" do
+      expect(stat_tracker.percentage_visitor_wins).to eq(0.43)
+    end
 
-    it "counts the games by season" do 
+    it "#percentage_ties" do
+      expect(stat_tracker.percentage_ties).to eq(0.22)
+    end
 
+    it "#count_of_games_by_season" do 
       expect(stat_tracker.count_of_games_by_season).to eq({
         "20162017" => 7, 
         "20172018" => 10, 
@@ -75,23 +73,53 @@ RSpec.describe StatTracker do
       })
     end
 
-    it "is able to find the average number of goals across all seasons" do 
+    it "#average_goals_per_game" do 
       expect(stat_tracker.average_goals_per_game).to eq(4.45)
     end
 
-    it "Average number of goals scored in a game organized in a hash with season names (e.g. 20122013) as keys and a float representing the average number of goals in a game for that season as values (rounded to the nearest 100th)" do 
+    it "#average_goals_by_season" do 
       expect(stat_tracker.average_goals_by_season).to eq({
-        
         "20162017" => 5.14, 
         "20172018" => 4.10, 
         "20152016" => 6.00, 
         "20142015" => 3.90, 
         "20122013" => 5.00, 
         "20132014" => 4.31, 
-
       })
     end
-    context "Season Statistics" do 
+  end
+
+  describe "League Statistics" do
+    it "#count_of_teams" do
+      expect(stat_tracker.count_of_teams).to eq(32)
+    end
+
+    it "#best_offense" do
+      expect(stat_tracker.best_offense).to eq("Reign FC")
+    end
+
+    it "#worst_offense" do
+      expect(stat_tracker.worst_offense).to eq("Orlando City SC")
+    end
+
+    it "#highest_scoring_visitor" do
+      expect(stat_tracker.highest_scoring_visitor).to eq("Philadelphia Union")
+    end
+
+    it "#highest_scoring_home_team" do
+      expect(stat_tracker.highest_scoring_home_team).to eq("Reign FC")
+    end
+
+    it "#lowest_scoring_visitor" do
+      expect(stat_tracker.lowest_scoring_visitor).to eq("Orlando City SC")
+    end
+
+    it "#lowest_scoring_home_team" do
+      expect(stat_tracker.lowest_scoring_home_team).to eq("Seattle Sounders FC")
+    end
+  end
+
+  describe "Season Statistics" do 
     it "winningest_coach" do 
       game_path = './data/games.csv'
       team_path = './data/teams_fixture.csv'
@@ -110,52 +138,23 @@ RSpec.describe StatTracker do
       expect(stat_tracker.winningest_coach("20172018")).to eq("Peter Laviolette").or(eq("Gerard Gallant")).or(eq("Paul Maurice"))
     end
 
-    # it "worst coach" do 
-    #   game_path = './data/games.csv'
-    #   team_path = './data/teams_fixture.csv'
-    #   game_teams_path = './data/game_teams_fixture.csv' 
-    #   locations = 
-    #     {
-    #     games: game_path,
-    #     teams: team_path,
-    #     game_teams: game_teams_path
-    #     }
+    it "worst coach" do 
+      game_path = './data/games.csv'
+      team_path = './data/teams_fixture.csv'
+      game_teams_path = './data/game_teams_fixture.csv' 
+      locations = 
+        {
+        games: game_path,
+        teams: team_path,
+        game_teams: game_teams_path
+        }
       
-    #   stat_tracker = StatTracker.from_csv(locations) 
+      stat_tracker = StatTracker.from_csv(locations) 
     
-    #   expect(stat_tracker.worst_coach("20152016")).to eq("Peter Laviolette").or(eq("Barry Trotz"))
-    #   expect(stat_tracker.worst_coach("20172018")).to eq("Glen Gulutzan").or(eq("Peter DeBoer")).or(eq("Mike Sullivan"))
-
-
-    # end
-      end 
-
-
-  describe "Game Statistics" do
-    it "#highest_total_score" do
-      expect(stat_tracker.highest_total_score).to eq(9)
+      expect(stat_tracker.worst_coach("20152016")).to eq("Peter Laviolette").or(eq("Barry Trotz"))
+      expect(stat_tracker.worst_coach("20172018")).to eq("Glen Gulutzan").or(eq("Peter DeBoer")).or(eq("Mike Sullivan"))
     end
-
-    it "#lowest_total_score" do
-      expect(stat_tracker.lowest_total_score).to eq(1)
-    end
-  end
-
-  describe "League Statistics" do
-    it "#count_of_teams" do
-      expect(stat_tracker.count_of_teams).to eq(32)
-    end
-
-    it "#best_offense" do
-      expect(stat_tracker.best_offense).to eq("Reign FC")
-    end
-
-    it "#worst_offense" do
-      expect(stat_tracker.worst_offense).to eq("Orlando City SC")
-    end
-  end
-
-  describe "Season Statistics" do
+    
     xit "#most_accurate_team" do
       expect(stat_tracker.most_accurate_team).to eq()
     end
@@ -164,34 +163,46 @@ RSpec.describe StatTracker do
       expect(stat_tracker.least_accurate_team).to eq()
     end
 
-
-
-    it "#percentage_visitor_wins" do
-      expect(stat_tracker.percentage_visitor_wins).to eq(0.43)
+    xit "#most_tackles" do
+      expect().to eq()
     end
 
-    it "#percentage_ties" do
-      expect(stat_tracker.percentage_ties).to eq(0.22)
+    xit "#fewest_tackles" do
+      expect().to eq()
     end
   end
-
-
-  describe "League Statistics" do
-    it "#highest_scoring_visitor" do
-      expect(stat_tracker.highest_scoring_visitor).to eq("Philadelphia Union")
+  
+  describe "Team Statistics" do
+    xit "#team_info" do
+      expect().to eq()
     end
 
-    it "#highest_scoring_home_team" do
-      expect(stat_tracker.highest_scoring_home_team).to eq("Reign FC")
+    xit "#best_season" do
+      expect().to eq()
     end
 
-    it "#lowest_scoring_visitor" do
-      expect(stat_tracker.lowest_scoring_visitor).to eq("Orlando City SC")
+    xit "#worst_season" do
+      expect().to eq()
     end
 
-    it "#lowest_scoring_home_team" do
-      expect(stat_tracker.lowest_scoring_home_team).to eq("Seattle Sounders FC")
+    xit "#average_win_percentage" do
+      expect().to eq()
     end
 
+    xit "#most_goals_scored" do
+      expect().to eq()
+    end
+
+    xit "#fewest_goals_scored" do
+      expect().to eq()
+    end
+
+    xit "#favorite_opponent" do
+      expect().to eq()
+    end
+
+    xit "#rival" do
+      expect().to eq()
+    end
   end
 end
