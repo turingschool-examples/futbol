4af1aa114fa1fa1d1a82c2926fae66cb685b5573
Brad Dunlap <braddunlapdpt@gmail.com>
2023-01-06 12:38:49 -0500

    formatting of game methods as group

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -40,6 +40,20 @@ RSpec.describe StatTracker do
     end
   end  
 
+	describe 'checks percentage of wins/ties' do
+		it "#percentage_home_wins" do
+		expect(stat_tracker.percentage_home_wins).to eq 0.44
+	end
+
+		it "#percentage_visitor_wins" do
+			expect(stat_tracker.percentage_visitor_wins).to eq 0.36
+		end
+
+		it "#percentage_ties" do
+			expect(stat_tracker.percentage_ties).to eq 0.20
+		end
+	end
+
   describe 'compares total scores' do
     it 'finds total score' do
       expect(stat_tracker.total_score).to be_a(Array)
@@ -106,18 +120,4 @@ RSpec.describe StatTracker do
       expect(stat_tracker.average_goals_by_season).to eq(expected_hash)
     end
   end
-
-	describe 'checks percentage of wins/ties' do
-		it "#percentage_home_wins" do
-    expect(stat_tracker.percentage_home_wins).to eq 0.44
-  end
-
-  	it "#percentage_visitor_wins" do
-   	 expect(stat_tracker.percentage_visitor_wins).to eq 0.36
-  	end
-
-  	it "#percentage_ties" do
-   	 expect(stat_tracker.percentage_ties).to eq 0.20
-  	end
-	end
 end
