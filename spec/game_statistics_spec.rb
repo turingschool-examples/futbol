4d7fa35283adfca18f0ebf3b34cc5642d2ed6db1
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-01-06 16:43:13 -0700

    Add: initialize test

---
+++ spec/game_statistics_spec.rb
@@ -0,0 +1,18 @@
+require './lib/game_statistics'
+require './lib/games'
+
+describe GameStats do
+	before do
+		game_path = './data/games_sample.csv'
+		@games = Game.create_games(game_path)
+	end
+
+	let(:game) {@games[0]}
+	let(:stat) {GameStats.new(@games)}
+
+	describe '' do
+		it '' do
+			expect(stat.games_by_season(20132014)).to be_a('')
+		end
+	end
+end
