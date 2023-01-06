4a217e26725172bdc7da9fdb6418b0fa085d282e
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-01-06 16:06:31 -0700

    Fix: #initialize has attributes to reflect returning a hash

--- spec/games_spec.rb
+++ spec/games_spec.rb
@@ -4,25 +4,25 @@ describe Game do
 	before do
 		game_path = './data/games_sample.csv'
 		@games = Game.create_games(game_path)
-		@game = @games[0]
 	end
+	let(:game) {@games[0]}
 
 	describe '#initialize' do
 		it 'exists' do
-			expect(@game).to be_a(Game)
+			expect(game).to be_a(Game)
 		end
 
 		it 'has attributes' do
-			expect(@game.game_id).to eq(2012030221)
-			expect(@game.season).to eq(20122013)
-			expect(@game.type).to eq("Postseason")
-			expect(@game.date_time).to eq("5/16/13")
-			expect(@game.away_team_id).to eq(3)
-			expect(@game.home_team_id).to eq(6)
-			expect(@game.away_goals).to eq(2)
-			expect(@game.home_goals).to eq(3)
-			expect(@game.venue).to eq("Toyota Stadium")
-			expect(@game.venue_link).to eq("/api/v1/venues/null")
+			expect(game.info[:game_id]).to eq(2012030221)
+			expect(game.info[:season]).to eq(20122013)
+			expect(game.info[:type]).to eq("Postseason")
+			expect(game.info[:date_time]).to eq("5/16/13")
+			expect(game.info[:away_team_id]).to eq(3)
+			expect(game.info[:home_team_id]).to eq(6)
+			expect(game.info[:away_goals]).to eq(2)
+			expect(game.info[:home_goals]).to eq(3)
+			expect(game.info[:venue]).to eq("Toyota Stadium")
+			expect(game.info[:venue_link]).to eq("/api/v1/venues/null")
 		end
 	end
 end
