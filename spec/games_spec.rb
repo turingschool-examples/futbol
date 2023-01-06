2c98fb6319bf0c6b2325dc6b66be5bb001c205d4
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-01-06 14:49:19 -0700

    Add: attribute tests for #initialize

--- spec/games_spec.rb
+++ spec/games_spec.rb
@@ -9,11 +9,20 @@ describe Game do
 
 	describe '#initialize' do
 		it 'exists' do
-			expect(@games).to include(Game)
+			expect(@game).to be_a(Game)
 		end
 
 		it 'has attributes' do
-			expect(@game.game_id).to eq("2012030221")
+			expect(@game.game_id).to eq(2012030221)
+			expect(@game.season).to eq(20122013)
+			expect(@game.type).to eq("Postseason")
+			expect(@game.date_time).to eq("5/16/13")
+			expect(@game.away_team_id).to eq(3)
+			expect(@game.home_team_id).to eq(6)
+			expect(@game.away_goals).to eq(2)
+			expect(@game.home_goals).to eq(3)
+			expect(@game.venue).to eq("Toyota Stadium")
+			expect(@game.venue_link).to eq("/api/v1/venues/null")
 		end
 	end
 end
