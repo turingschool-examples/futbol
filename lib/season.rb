bab22c7d9824ad5ac1fd0ba8a712a7c5d78bca16
Hady Matar <hadyematar@gmail.com>
2023-01-09 09:20:50 -0700

    Basic organization of iteration 3

---
+++ lib/season.rb
@@ -0,0 +1,12 @@
+# CREATE FIRST AN OBJECT OF SEASON THAT IS FOR EACH SEASON, ALL OF THE STATISTICS 
+
+# winningest_coach	Name of the Coach with the best win percentage for the season	String
+# worst_coach	Name of the Coach with the worst win percentage for the season	String
+# most_accurate_team	Name of the Team with the best ratio of shots to goals for the season	String
+# least_accurate_team	Name of the Team with the worst ratio of shots to goals for the season	String
+# most_tackles	Name of the Team with the most tackles in the season	String
+# fewest_tackles	Name of the Team with the fewest tackles in the season	String
+
+# STAGE 2: 
+
+# FINDING COMMON METHODS BETWEEN SEASON AND TEAM, LEAGUE, GAME THAT WE CAN PUT INTO A MODULE. 
