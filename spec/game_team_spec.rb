1b7bf456185ea9002c982ac33e5864699a9e1d42
Brad Dunlap <braddunlapdpt@gmail.com>
2023-01-09 13:20:28 -0500

    feat: tests that Game_team class initializes with attributes

--- spec/game_team_spec.rb
+++ spec/game_team_spec.rb
@@ -1,11 +1,46 @@
-require spec_helper
+require_relative 'spec_helper'
 
 RSpec.describe Game_team do
-	let(:game_team_row1){ Game_team.new("2012030221","3","away","LOSS","OT","John Tortorella","2","8","44","8","3","0","44.8","17","7") }
+	info = {
+		game_id: "2012030221", 
+		team_id: "3",
+		hoa: "away",
+		result: "LOSS",
+		settled_in: "OT",
+		head_coach: "John Tortorella",
+		goals: "2",
+		shots: "8",
+		tackles: "44",
+		pim: "8",
+		powerplayopportunities: "3",
+		powerplaygoals: "0",
+		faceoffwinpercentage: "44.8",
+		giveaways: "17",
+		takeaways: "7"
+	}
 
+	let(:game_team) {Game_team.new(info)}
 	describe '#initialize' do
 		it 'exists' do
-			expect(game_team_row1).to be_instance_of(Game_team)
+			expect(game_team).to be_a(Game_team)
+		end
+
+		it 'initializes with attributes' do
+			expect(game_team.game_id).to eq("2012030221")
+			expect(game_team.team_id).to eq("3")
+			expect(game_team.hoa).to eq("away")
+			expect(game_team.result).to eq("LOSS")
+			expect(game_team.settled_in).to eq("OT")
+			expect(game_team.head_coach).to eq("John Tortorella")
+			expect(game_team.goals).to eq("2")
+			expect(game_team.shots).to eq("8")
+			expect(game_team.tackles).to eq("44")
+			expect(game_team.pim).to eq("8")
+			expect(game_team.power_play_opportunities).to eq("3")
+			expect(game_team.power_play_goals).to eq("0")
+			expect(game_team.face_off_win_percentage).to eq("44.8")
+			expect(game_team.giveaways).to eq("17")			
+			expect(game_team.takeaways).to eq("7")			
 		end
 	end
 end
