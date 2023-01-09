9af169bf4f85a76028f324cefa9b05661fe3c800
Tori Enyart <torienyart@gmail.com>
2023-01-09 16:36:29 -0700

    refactor: Move data factory methods to class file

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -10,37 +10,7 @@ class StatTracker
     @teams = teams
     @game_teams = game_teams
   end
-  
-  def self.from_csv(locations)
-    games = games_csv(locations)
-    teams = teams_csv(locations)
-    game_teams = game_teams_csv(locations)
-    self.new(games, teams, game_teams)
-  end
 
-  def self.games_csv(locations)
-    games = []
-    CSV.foreach(locations[:games], headers: true, header_converters: :symbol) do |info|
-      games << Game.new(info)
-    end
-    games
-  end
-
-  def self.teams_csv(locations)
-    teams = []
-    CSV.foreach(locations[:teams], headers: true, header_converters: :symbol) do |info|
-      teams << Team.new(info)
-    end
-    teams
-  end
-
-  def self.game_teams_csv(locations)
-    game_teams = []
-    CSV.foreach(locations[:game_teams], headers: true, header_converters: :symbol) do |info|
-      game_teams << GameTeam.new(info)
-    end
-    game_teams
-  end
 
   class Game
     attr_reader :game_id,
