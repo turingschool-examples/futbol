0628a7f2c687ed0bfcf60fae12b2ff41005f8e6e
Tori Enyart <torienyart@gmail.com>
2023-01-09 16:39:05 -0700

    refactor: Move game class to respective file

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -11,31 +11,7 @@ class StatTracker
     @game_teams = game_teams
   end
 
-
-  class Game
-    attr_reader :game_id,
-                :season,
-                :type,
-                :date_time,
-                :away_team_id,
-                :home_team_id,
-                :away_goals,
-                :home_goals,
-                :venue,
-                :venue_link
-
-    def initialize(info)
-      @game_id = info[:game_id]
-      @season = info[:season]
-      @type = info[:type]
-      @date_time = info[:date_time]
-      @away_team_id = info[:away_team_id]
-      @home_team_id = info[:home_team_id]
-      @away_goals = info[:away_goals]
-      @home_goals = info[:home_goals]
-      @venue = info[:venue]
-      @venue_link = info[:venue_link]
-    end
+    
 
     def goals_per_game
       away_goals.to_i + home_goals.to_i
