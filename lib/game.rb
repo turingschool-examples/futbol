5847785c634bbbde439cc7c45469c52b7473a575
Brad Dunlap <braddunlapdpt@gmail.com>
2023-01-09 12:41:16 -0500

    Refactor: change data to info

--- lib/game.rb
+++ lib/game.rb
@@ -11,15 +11,15 @@ class Game
                 :venue_link
 
     def initialize(info)
-        @game_id = data[:game_id]
-        @season = data[:season]
-        @type = data[:type]
-        @date_time = data[:date_time]
-        @away_team_id = data[:away_team_id]
-        @home_team_id = data[:home_team_id]
-        @away_goals = data[:away_goals]
-        @home_goals = data[:home_goals]
-        @venue = data[:venue]
-        @venue_link = data[:venue_link]
+        @game_id = info[:game_id]
+        @season = info[:season]
+        @type = info[:type]
+        @date_time = info[:date_time]
+        @away_team_id = info[:away_team_id]
+        @home_team_id = info[:home_team_id]
+        @away_goals = info[:away_goals]
+        @home_goals = info[:home_goals]
+        @venue = info[:venue]
+        @venue_link = info[:venue_link]
     end
 end
