3f8d9bd823a38d2e1b16170485ad6613c207b9f0
Brad Dunlap <braddunlapdpt@gmail.com>
2023-01-09 20:45:22 -0500

    Refactor: games class

--- lib/game.rb
+++ lib/game.rb
@@ -1,25 +1,33 @@
 class Game
-    attr_reader :game_id,
-                :season,
-                :type,
-                :date_time,
-                :away_team_id,
-                :home_team_id,
-                :away_goals,
-                :home_goals,
-                :venue,
-                :venue_link
+  attr_reader :game_id,
+              :season,
+							:type,
+							:date_time,
+							:away_team_id,
+							:home_team_id,
+							:away_goals,
+							:home_goals,
+							:venue,
+							:venue_link
 
-    def initialize(info)
-        @game_id = info[:game_id]
-        @season = info[:season]
-        @type = info[:type]
-        @date_time = info[:date_time]
-        @away_team_id = info[:away_team_id]
-        @home_team_id = info[:home_team_id]
-        @away_goals = info[:away_goals]
-        @home_goals = info[:home_goals]
-        @venue = info[:venue]
-        @venue_link = info[:venue_link]
-    end
+	def initialize(info)
+			@game_id = info[:game_id]
+			@season = info[:season]
+			@type = info[:type]
+			@date_time = info[:date_time]
+			@away_team_id = info[:away_team_id]
+			@home_team_id = info[:home_team_id]
+			@away_goals = info[:away_goals]
+			@home_goals = info[:home_goals]
+			@venue = info[:venue]
+			@venue_link = info[:venue_link]
+	end
+
+	def self.all_games(location)
+		games = []
+		CSV.foreach location[:games], headers: true, header_converters: :symbol do |row|
+			games << Game.new(row)
+		end
+		games
+	end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,11 +1,11 @@
 require 'spec_helper'
 
 RSpec.describe Game do
-    games = CSV.read './data/games.csv', headers: true, header_converters: :symbol
-
-    # let(:game){ Game.new( { game_id: "2012030221",season: "20122013",type: "Postseason",date_time: "5/16/13",away_team_id: "3",home_team_id: "6",away_goals: "2",home_goals: "3",venue: "Toyota Stadium",venue_link: "/api/v1/venues/null" } ) }
-
-    let(:game){ Game.new(games[0]) }
+	before do
+    location = './data/games.csv'
+		@games = Game.all_games(location)
+	end
+    let(:game){@games[0]}
 
     describe "#initialize" do
       it 'exists' do
