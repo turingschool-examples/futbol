4be309ed100bad1437cab7e15c27673820b33747
Weston Sandfort <sandfortw@gmail.com>
2023-01-09 19:24:13 -0700

    feature: Add missing helper method tests (blank)

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -268,88 +268,104 @@ describe StatTracker do
           expect(stat_tracker4.rival("4")).to eq("Portland Timbers")
       end
     end
+  end
 
-    describe 'helper methods' do
+  describe 'helper methods' do
 
-      it "can determine #home_wins" do
-        expect(stat_tracker.home_wins).to eq(20)
+    it "can determine #home_wins" do
+      expect(stat_tracker.home_wins).to eq(20)
+    end
+
+    it "can determine #away_wins" do
+      expect(stat_tracker.away_wins).to eq(23)
+    end
+
+    it "can determine #tie_games" do
+      expect(stat_tracker.tie_games).to eq(6)
+    end
+
+    describe '#team_score_averages' do
+      it 'can calculate #team_score_averages array length' do
+        expect(stat_tracker.team_score_averages.length).to eq(31)
       end
 
-      it "can determine #away_wins" do
-        expect(stat_tracker.away_wins).to eq(23)
+      #Refactor
+      it 'can return #team_score_averages array' do
+        expected_array = [["5", 0.5], ["8", 1.3333], ["53", 1.5], ["26", 1.6], ["22", 1.6667],
+        ["18", 1.75], ["21", 1.75], ["10", 2.0], ["2", 2.0], ["30", 2.0], ["17", 2.0], ["28", 2.0],
+        ["27", 2.0], ["1", 2.0], ["20", 2.0], ["15", 2.2], ["52", 2.2], ["6", 2.25], ["13", 2.3333],
+        ["12", 2.3333], ["3", 2.5], ["9", 2.5], ["25", 2.5], ["19", 2.6667], ["16", 2.75],
+        ["24", 2.8], ["14", 2.8571], ["23", 3.0], ["29", 3.0], ["7", 3.0], ["4", 3.3333]]
+        expect(stat_tracker.team_score_averages).to eq(expected_array)
       end
+    end
+    
+    it '#visitor_score_averages' do
 
-      it "can determine #tie_games" do
-        expect(stat_tracker.tie_games).to eq(6)
-      end
-      it '#game_ids_seasons helper method for #best_season and #worst_season' do
-        expect(stat_tracker.game_ids_seasons("6")).to be_a(Hash)
-        expect(stat_tracker.game_ids_seasons("6")["20122013"]).to be_an(Array)
-        expect(stat_tracker.game_ids_seasons("6")["20122013"][0]).to be_a(String)
-        expect(stat_tracker.game_ids_seasons("6")["20122013"][0].length).to eq(10)
-      end
+    end
 
-      it '#seasons_perc_win helper method for #best_season and #worst_season' do
-        expect(stat_tracker.seasons_perc_win("6")).to eq([["20122013", 1.0]])
-      end
+    it '#home_score_averages' do
 
-      it 'can generate #goals_scored_sorted as an array' do
-        expect(stat_tracker.goals_scored_sorted("6")).to be_an(Array)
-        expect(stat_tracker.goals_scored_sorted("6").first).to be_an(Integer)
-        expect(stat_tracker.goals_scored_sorted("6").last).to be_an(Integer)
-      end
+    end
 
-      it 'can #find_team_name(team_id)' do
-        expect(stat_tracker.find_team_name("6")).to eq("FC Dallas")
-        expect(stat_tracker.find_team_name("17")).to eq("LA Galaxy")
+    it 'can produce an #array_of_gameids by season' do
+      expect(stat_tracker.array_of_gameids_by_season("20122013")).to be_an(Array)
+      expect(stat_tracker.array_of_gameids_by_season("20122013")[0]).to be_a(String)
+      expect(stat_tracker.array_of_gameids_by_season("20122013")[0].length).to eq(10)
+    end
 
-      end
+    it 'can produce an #array_of_game_teams by season' do
+      expect(stat_tracker.array_of_game_teams_by_season("20122013")).to be_an(Array)
+      expect(stat_tracker.array_of_game_teams_by_season("20122013")[0]).to be_a(GameTeam)
+    end
 
-      it 'can #find_game_id_arr(team_id)' do
-        expected_arr = ["2012030311", "2012030312", "2012030313", "2012030314"]
-        expect(stat_tracker.find_game_id_arr("5")).to eq(expected_arr)
-      end
+    it '#coaches_win_percentages_hash' do
+      expect(stat_tracker.coaches_win_percentages_hash("20122013")).to be_a(Hash)
+      expect(stat_tracker.coaches_win_percentages_hash("20122013").first[1]).to be_a(Float)
+    end
 
-      it 'can give #opponents_win_percenage(team_id) array of arrays' do
-        expect(stat_tracker.opponents_win_percentage("6")).to eq([["3", 0.0], ["5", 0.0]])
-        expect(stat_tracker4.opponents_win_percentage("17")).to eq([["22", 0.0], ["29", 0.0], 
-                                                                    ["26", 0.0], ["30", 0.0],
-                                                                    ["19", 0.3333333333333333], ["25", 0.5], 
-                                                                    ["20", 1.0], ["16", 1.0]])
-      end
+    it 'can generate #goals_scored_sorted as an array' do
+      expect(stat_tracker.goals_scored_sorted("6")).to be_an(Array)
+      expect(stat_tracker.goals_scored_sorted("6").first).to be_an(Integer)
+      expect(stat_tracker.goals_scored_sorted("6").last).to be_an(Integer)
+    end
 
-      describe '#team_score_averages' do
-        it 'can calculate #team_score_averages array length' do
-          expect(stat_tracker.team_score_averages.length).to eq(31)
-        end
-
-        #Refactor
-        it 'can return #team_score_averages array' do
-          expected_array = [["5", 0.5], ["8", 1.3333], ["53", 1.5], ["26", 1.6], ["22", 1.6667],
-          ["18", 1.75], ["21", 1.75], ["10", 2.0], ["2", 2.0], ["30", 2.0], ["17", 2.0], ["28", 2.0],
-          ["27", 2.0], ["1", 2.0], ["20", 2.0], ["15", 2.2], ["52", 2.2], ["6", 2.25], ["13", 2.3333],
-          ["12", 2.3333], ["3", 2.5], ["9", 2.5], ["25", 2.5], ["19", 2.6667], ["16", 2.75],
-          ["24", 2.8], ["14", 2.8571], ["23", 3.0], ["29", 3.0], ["7", 3.0], ["4", 3.3333]]
-          expect(stat_tracker.team_score_averages).to eq(expected_array)
-        end
-      end
-
-      it 'can produce an #array_of_gameids by season' do
-        expect(stat_tracker.array_of_gameids_by_season("20122013")).to be_an(Array)
-        expect(stat_tracker.array_of_gameids_by_season("20122013")[0]).to be_a(String)
-        expect(stat_tracker.array_of_gameids_by_season("20122013")[0].length).to eq(10)
-      end
-
-      it 'can produce an #array_of_game_teams by season' do
-        expect(stat_tracker.array_of_game_teams_by_season("20122013")).to be_an(Array)
-        expect(stat_tracker.array_of_game_teams_by_season("20122013")[0]).to be_a(GameTeam)
-      end
-
-      it '#coaches_win_percentages_hash' do
-        expect(stat_tracker.coaches_win_percentages_hash("20122013")).to be_a(Hash)
-        expect(stat_tracker.coaches_win_percentages_hash("20122013").first[1]).to be_a(Float)
-      end
+    it '#find_team_id(team_id)' do 
       
     end
+
+    it '#team_ratio_hash(season)' do
+
+    end
+
+    it 'can #find_game_id_arr(team_id)' do
+      expected_arr = ["2012030311", "2012030312", "2012030313", "2012030314"]
+      expect(stat_tracker.find_game_id_arr("5")).to eq(expected_arr)
+    end
+
+    it 'can give #opponents_win_percentage(team_id) array of arrays' do
+      expect(stat_tracker.opponents_win_percentage("6")).to eq([["3", 0.0], ["5", 0.0]])
+      expect(stat_tracker4.opponents_win_percentage("17")).to eq([["22", 0.0], ["29", 0.0], 
+                                                                  ["26", 0.0], ["30", 0.0],
+                                                                  ["19", 0.3333333333333333], ["25", 0.5], 
+                                                                  ["20", 1.0], ["16", 1.0]])
+    end
+
+    it 'can #find_team_name(team_id)' do
+      expect(stat_tracker.find_team_name("6")).to eq("FC Dallas")
+      expect(stat_tracker.find_team_name("17")).to eq("LA Galaxy")
+    end
+
+    it '#game_ids_seasons helper method for #best_season and #worst_season' do
+      expect(stat_tracker.game_ids_seasons("6")).to be_a(Hash)
+      expect(stat_tracker.game_ids_seasons("6")["20122013"]).to be_an(Array)
+      expect(stat_tracker.game_ids_seasons("6")["20122013"][0]).to be_a(String)
+      expect(stat_tracker.game_ids_seasons("6")["20122013"][0].length).to eq(10)
+    end
+
+    it '#seasons_perc_win helper method for #best_season and #worst_season' do
+      expect(stat_tracker.seasons_perc_win("6")).to eq([["20122013", 1.0]])
+    end
   end
+  
 end
