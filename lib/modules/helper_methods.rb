58149b5931c74a7d3f325213e9074d51925b679f
Elle Majors <hellolovely@ellemajors.com>
2023-01-09 18:08:54 -0700

    Add helpers methods

---
+++ lib/modules/helper_methods.rb
@@ -0,0 +1,17 @@
+#home_wins
+#away_wins
+#tie_games
+
+#team_score_averages'
+
+#array_of_gameids
+#array_of_game_teams
+#coaches_win_percentages_hash
+
+#game_ids_seasons
+#seasons_perc_win
+
+#goals_scored_sorted
+#find_team_name(team_id)
+#find_game_id_arr(team_id)
+#opponents_win_percenage(team_id)
