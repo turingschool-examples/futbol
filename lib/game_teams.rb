e65d8e252d47ada51675d0d5f4660ced637c2851
Sam Walker <sg.walker327@gmail.com>
2023-01-10 14:17:41 -0700

    I3: finished moving methods and tests

--- lib/game_teams.rb
+++ lib/game_teams.rb
@@ -51,7 +51,7 @@ class GameTeams
 		average_home_goals_per_team
 	end
 
-  def highest_scoring_visitor #moved to gameteams
+  def highest_scoring_visitor 
 		visitor_highest = visitor_scores_hash.max_by {|k,v| v }
 		@team_path.map do |team| 
       if team[:team_id] == visitor_highest[0]
@@ -86,6 +86,12 @@ class GameTeams
         end
     end.compact.pop
 	end
+
+  def games_by_season
+    @games_by_season ||= @game_path.group_by do |row|
+      row[:season] 
+    end
+  end
 
   def games_by_game_id
     #memoization this @games_by_game_id ||= [everything below]
@@ -155,8 +161,6 @@ class GameTeams
     hash
   end
 
-  
-
   def get_ratios_by_season_id(season_id)
     merged_hash = team_shots_by_season(season_id).merge(team_goals_by_season(season_id)) {|key, old_val, new_val| new_val.sum / old_val.sum.to_f}
   end
@@ -209,7 +213,12 @@ class GameTeams
     @game_teams_path.group_by do |row|
       row[:team_id]
     end
-    require 'pry'; binding.pry
+  end
+
+  def games_by_id_game_path
+    @games_by_id_game_path ||= @game_path.group_by do |row|
+      row[:game_id]
+    end
   end
 
   def pair_teams_with_results(team_id)
