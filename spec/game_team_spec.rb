206cf74272fe8ce7f72f2888473bcb5dec6fc47a
Kassandra Leyba <kassandra.leyba@gmail.com>
2023-01-09 18:11:22 -0700

    Add formatting changes

--- spec/game_team_spec.rb
+++ spec/game_team_spec.rb
@@ -22,27 +22,29 @@ RSpec.describe GameTeam do
             }
             @game_team = GameTeam.new(info)
         end
+        
+        describe "#initialize" do
+            it "exists" do
+                expect(@game_team).to be_instance_of GameTeam
+            end
 
-        it "exists" do
-            expect(@game_team).to be_instance_of GameTeam
-        end
-
-        it "has attributes" do
-            expect(@game_team.game_id).to eq("2012030314")
-            expect(@game_team.team_id).to eq("5")
-            expect(@game_team.hoa).to eq("away")
-            expect(@game_team.result).to eq("LOSS")
-            expect(@game_team.settled_in).to eq("REG")
-            expect(@game_team.head_coach).to eq("Dan Bylsma")
-            expect(@game_team.goals).to eq(0)
-            expect(@game_team.shots).to eq(6)
-            expect(@game_team.tackles).to eq(33)
-            expect(@game_team.pim).to eq("8")
-            expect(@game_team.power_play_opportunities).to eq("3")
-            expect(@game_team.power_play_goals).to eq("0")
-            expect(@game_team.face_off_win_percentage).to eq("47.5")
-            expect(@game_team.giveaways).to eq("8")
-            expect(@game_team.takeaways).to eq("1")
+            it "has attributes" do
+                expect(@game_team.game_id).to eq("2012030314")
+                expect(@game_team.team_id).to eq("5")
+                expect(@game_team.hoa).to eq("away")
+                expect(@game_team.result).to eq("LOSS")
+                expect(@game_team.settled_in).to eq("REG")
+                expect(@game_team.head_coach).to eq("Dan Bylsma")
+                expect(@game_team.goals).to eq(0)
+                expect(@game_team.shots).to eq(6)
+                expect(@game_team.tackles).to eq(33)
+                expect(@game_team.pim).to eq("8")
+                expect(@game_team.power_play_opportunities).to eq("3")
+                expect(@game_team.power_play_goals).to eq("0")
+                expect(@game_team.face_off_win_percentage).to eq("47.5")
+                expect(@game_team.giveaways).to eq("8")
+                expect(@game_team.takeaways).to eq("1")
+            end
         end
     end
 end
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -13,16 +13,18 @@ RSpec.describe Team do
         @team = Team.new(info)
     end
 
-    it "exists" do
-        expect(@team).to be_instance_of Team
-    end
-    
-    it "has attributes" do
-        expect(@team.team_id).to eq("1")
-        expect(@team.franchise_id).to eq("23")
-        expect(@team.team_name).to eq("Atlanta United")
-        expect(@team.abbreviation).to eq("ATL")
-        expect(@team.stadium).to eq("Mercedes_Benz Stadium")
-        expect(@team.link).to eq("api/v1/teams/1")
+    describe "#initialize" do
+        it "exists" do
+            expect(@team).to be_instance_of Team
+        end
+        
+        it "has attributes" do
+            expect(@team.team_id).to eq("1")
+            expect(@team.franchise_id).to eq("23")
+            expect(@team.team_name).to eq("Atlanta United")
+            expect(@team.abbreviation).to eq("ATL")
+            expect(@team.stadium).to eq("Mercedes_Benz Stadium")
+            expect(@team.link).to eq("api/v1/teams/1")
+        end
     end
 end
