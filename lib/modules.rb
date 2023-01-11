e7a027f7864cb842749f454d94eaa1b5d0430149
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-01-10 17:50:42 -0700

    Refactor: move modules around

--- lib/modules.rb
+++ lib/modules.rb
@@ -1,20 +1,26 @@
 module Sortable
 
 	def games_played_by_season
-		@games_played_by_season ||= @games.group_by do |game|
+		@games_played_by_season ||= games.group_by do |game|
 			game.info[:season]
 		end
   end
 
 	def games_by_game_id
-		@games_by_game_id ||= @game_teams.group_by do |row|
-			row.info[:game_id]
+		@games_by_game_id ||= game_teams.group_by do |game_team|
+			game_team.info[:game_id]
+		end
+	end
+	
+	def games_by_team_id
+		@games_by_team_id ||= game_teams.group_by do |row|
+			row.info[:team_id] 
 		end
 	end
 
 	def find_team_by_id
-		@find_team_by_id ||= @teams.group_by do |row|
-			row.info[:team_id]
+		@find_team_by_id ||= teams.group_by do |team|
+			team.info[:team_id]
 		end
 	end
 
@@ -22,11 +28,6 @@ module Sortable
 		find_team_by_id[id].first.info[:team_name]
 	end
 
-	def games_by_team_id
-		@games_by_team_id ||= @game_teams.group_by do |row|
-			row.info[:team_id] 
-		end
-	end
 
 	def game_ids_for_season(season_id)
 		games = games_played_by_season[season_id.to_i]
