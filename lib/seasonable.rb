3f3ec9a0bc77a104a6cd08240d1f5c13d1db8b0b
Melony Franchini <meltravelz@gmail.com>
2023-01-10 19:29:47 -0700

    Delete extra comments

--- lib/seasonable.rb
+++ lib/seasonable.rb
@@ -7,7 +7,6 @@ module Seasonable
   def sums_of_home_away_goals
     @sums_of_home_away_goals ||= @games.map { |game| (game.away_goals + game.home_goals)}
   end
-  #=> returns array of all home/away goals per game (count7000-ish)
 
   # def team_id_all_goals
   #   @team_id_all_goals ||= @game_teams.group_by { |game_teams| [game_teams.team_id] << game_teams.goals.to_f }
