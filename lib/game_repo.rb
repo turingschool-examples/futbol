bb35ec31263c1079f2dba7dc58313ff85530696f
Harrison Ryan <hwryan12@gmail.com>
2023-01-11 14:13:24 -0600

    refactor: Save progress on fav/opp and rival

--- lib/game_repo.rb
+++ lib/game_repo.rb
@@ -92,4 +92,60 @@ class GameRepo
     end
     goals_by_season 
   end
+
+  def best_season(team_id)
+    games_list = []
+    @games.each do |game|
+      games_list.push(game) if game.home_team_id == team_id || game.away_team_id == team_id
+      end
+      games_in_a_season = games_list.group_by { |game| game.season }
+      season_win_percent = Hash.new(0)
+      games_in_a_season.each do |season, games|
+        home_wins = games.find_all do |game|
+          game.home_team_id == team_id && game.home_goals.to_i > game.away_goals.to_i
+        end
+        away_wins = games.find_all do |game|
+          game.away_team_id == team_id && game.home_goals.to_i < game.away_goals.to_i
+        end
+        season_win_percent[season] = ((home_wins.length.to_f + away_wins.length) / games.length).round(2)
+    end
+    season_win_percent ||= season_win_percent.team_id.to_i
+    season_win_percent.key(season_win_percent.values.max_by { |percentage| percentage })
+  end
+
+  def worst_season(team_id)
+    games_list = []
+    @games.each do |game|
+      games_list.push(game) if game.home_team_id == team_id || game.away_team_id == team_id
+      end
+      games_in_a_season = games_list.group_by { |game| game.season }
+      season_win_percent = Hash.new(0)
+      games_in_a_season.each do |season, games|
+        home_wins = games.find_all do |game|
+          game.home_team_id == team_id && game.home_goals.to_i > game.away_goals.to_i
+        end
+        away_wins = games.find_all do |game|
+          game.away_team_id == team_id && game.home_goals.to_i < game.away_goals.to_i
+        end
+        season_win_percent[season] = ((home_wins.length.to_f + away_wins.length) / games.length).round(2)
+    end
+    season_win_percent ||= season_win_percent.team_id.to_i
+    season_win_percent.key(season_win_percent.values.min_by { |percentage| percentage })
+  end
+  
+  # Use for reference on fav/rival:
+  
+  # def winningest_coach(for_season) 
+  #   game_teams = @game_teams.find_all { |game| game.game_id.to_s[0,4] == for_season[0,4] }
+  #   games_coached = game_teams.group_by { |game| game.head_coach }
+  #   games_coached.each do |coach, games|
+  #     coach_win_percentage = games.count{|game| game.result == "WIN"}/games.length.to_f
+  #     games_coached[coach] = coach_win_percentage
+  #   end
+  #   games_coached.key(games_coached.values.max)
+  # end
+
+  def favorite_opponent(team_id)
+    rival_games = 
+  end
 end
--- lib/team_repo.rb
+++ lib/team_repo.rb
@@ -19,4 +19,15 @@ class TeamRepo
       team.team_id == team_id
     end.team_name
   end
+  
+  def team_info(team_id)
+    team_info = @teams.find { |team| team.team_id == team_id }
+    {
+      'team_id' => team_info.team_id,
+      'franchise_id' => team_info.franchise_id,
+      'team_name' => team_info.team_name,
+      'abbreviation' => team_info.abbreviation,
+      'link' => team_info.link
+    }
+  end
 end
