55f64ee5dc0a14c89d4a26d73ab1ad50df4d8004
Dawson Timmons <DawsonTimmons@gmail.com>
2023-01-11 10:40:32 -0700

    refactor away/home team goals to utilize memoization

--- lib/goals_utility.rb
+++ lib/goals_utility.rb
@@ -1,8 +1,8 @@
 module GoalsUtility
 
   def sums_of_home_away_goals
-      @sums_of_home_away_goals ||= @games.map { |game| (game.away_goals + game.home_goals)}
-    end
+    @sums_of_home_away_goals ||= @games.map { |game| (game.away_goals + game.home_goals)}
+  end
   
   def team_id_all_goals
     team_id_all_goals_hash = Hash.new { |hash, key| hash[key] = [] }
@@ -10,6 +10,10 @@ module GoalsUtility
       team_id_all_goals_hash[game_teams.team_id] << game_teams.goals.to_f
     end
     @team_id_all_goals ||= team_id_all_goals_hash
+  end
+
+  def hoa_all_game_teams
+    @hoa_all_game_teams ||= @game_teams.group_by { |game_team| game_team.hoa }
   end
 
 end
