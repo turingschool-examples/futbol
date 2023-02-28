d8cf164a872504a29ba66c4b7fb2c933853474fb
Young Koh <youngheon6@gmail.com>
2023-02-28 13:42:54 -0700

    refactor: Add additional game_team and game_team_data classes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,6 @@ require './lib/game'
 require './lib/game_data'
 require './lib/team'
 require './lib/team_data'
+require './lib/game_team'
+require './lib/game_team_data'
 
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
-describe Team do
-  it 'is an instance of Team' do
+# describe Team do
+#   it 'is an instance of Team' do
     
-  end
-end
+#   end
+# end
