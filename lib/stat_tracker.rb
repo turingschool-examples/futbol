c67acf1835556e6af509197f3438a1e421de9fde
Reilly Miller <reillyjmiller31@gmail.com>
2023-02-28 13:41:55 -0700

    make stat tracker a class

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,15 +1,18 @@
-require './lib/stat_tracker'
+# require './lib/stat_tracker'
+class StatTracker
 
-game_path = './data/games.csv'
-team_path = './data/teams.csv'
-game_teams_path = './data/game_teams.csv'
+  game_path = './data/games.csv'
+  team_path = './data/teams.csv'
+  game_teams_path = './data/game_teams.csv'
 
-locations = {
-  games: game_path,
-  teams: team_path,
-  game_teams: game_teams_path
-}
+  locations = {
+    games: game_path,
+    teams: team_path,
+    game_teams: game_teams_path
+  }
 
-stat_tracker = StatTracker.from_csv(locations)
+  # stat_tracker = StatTracker.from_csv(locations)
 
-require 'pry'; binding.pry
+  require 'pry'; binding.pry
+
+end
