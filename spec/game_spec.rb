f6f8c08b058beb670f5143a4c0d49e66d005480e
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-28 18:31:41 -0500

    test: add initialize test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,23 @@
 require_relative './spec_helper'
 
 describe Game do
+  before(:each) do
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
 
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+    @stat_tracker = StatTracker.from_csv(locations)
+    @game = Game.new(@stat_tracker.games_data, @stat_tracker.game_team_data)
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@game).to be_a Game
+    end
+  end
 end
