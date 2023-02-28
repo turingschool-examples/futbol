f0732f5e96d73287c915e916d87187062e2cba9b
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-28 18:38:01 -0500

    feat: add games and game team data as attributes

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,9 @@
 class Game
-  def initialize(games_data, game_team_data)
+  attr_reader :games_data,
+              :game_team_data
 
+  def initialize(games_data, game_team_data)
+    @games_data = games_data
+    @game_team_data = game_team_data
   end
 end
