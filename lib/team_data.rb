f9559886e317ffaf260ed818b40c75a355dd1d72
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-02-28 15:18:57 -0500

    add team and teamdata classes

---
+++ lib/team_data.rb
@@ -0,0 +1,22 @@
+require 'csv'
+
+class TeamData
+  attr_reader 
+
+  def initialize
+    @teams = []
+  end
+
+  def add_teams
+    teams = CSV.open './data/teams.csv', headers: true, header_converters: :symbol
+    teams.each do |row|
+      team_id = row[:team_id]
+      francise_id = row[:franchise_id]
+      team_name = row[:team_name]
+      abbreviation = row[:abbreviation]
+      stadium = row[:stadium]
+      link = row[:link]
+      @teams << Team.new(team_id, franchise_id, team_name, abbreviation, stadium, link)
+    end
+  end
+end
