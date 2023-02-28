1c86e970c2f3fd8111e5de9bea4b79cdaa3cfb6d
Sean Cowans <zedesia@yahoo.com>
2023-02-27 18:06:58 -0700

    feat: Add #game and #game_data class

---
+++ lib/game.rb
@@ -0,0 +1,27 @@
+class Game
+attr_reader :id,
+            :season,
+            :type,
+            :date_time,
+            :away,
+            :home,
+            :away_goals,
+            :home_goals,
+            :venue,
+            :venue_link
+
+  def initialize(id, season, type, date_time, away, home, away_goals, home_goals, venue, venue_link)
+    @id = id
+    @season = season
+    @type = type
+    @date_time = date_time
+    @away = away
+    @home = home
+    @away_goals = away_goals
+    @home_goals = home_goals
+    @venue = venue
+    @venue_link = venue_link
+  end
+  # @name = first + " " + last
+  # @zipcode = zip.to_s.rjust(5,"0")[0..4]
+end
---
+++ lib/game_data.rb
@@ -0,0 +1,24 @@
+require 'csv'
+
+class GameData 
+  def initialize
+    @games = []
+  end
+
+  def add_games 
+    games = CSV.open 'data/games.csv' headers: true, header_converters: :symbol
+    games.each do |row|
+      id = row[:game_id]
+      season = row[:season]
+      type = row[:type]
+      date_time = row[:date_time]
+      away = row[:away_team_id]
+      home = row[:home_team_id]
+      away_goals = row[:away_goals]
+      home_goals = row[:home_goals]
+      venue = row[:venue]
+      venue_link = row[:venue_link]
+      @games << Game.new(id, season, type, date_time, away, home, away_goals, home_goals, venue, venue_link)
+    end
+  end
+end
