364b44d4944356dfe33d68099843e5d23694123c
Thomas Hawley <thawley22@turing.edu>
2023-03-01 14:39:15 -0700

    test: Add game_teams tests in stats_spec

--- lib/stats.rb
+++ lib/stats.rb
@@ -1,15 +1,17 @@
 require 'csv'
 require './lib/team'
 require './lib/game'
+require './lib/game_teams'
 
 class Stats
   attr_reader :teams,
-              :games
+              :games,
+              :game_teams
   
   def initialize(files)
     @teams = CSV.open(files[:teams], headers: true, header_converters: :symbol).map { |row| Team.new(row) }
     @games = CSV.open(files[:games], headers: true, header_converters: :symbol).map { |row| Game.new(row) }
-    @game_teams = CSV.open(files[:games], headers: true, header_converters: :symbol).map { |row| GameTeams.new(row) }
+    @game_teams = CSV.open(files[:game_teams], headers: true, header_converters: :symbol).map { |row| GameTeams.new(row) }
     
   end
 end
--- spec/stats_spec.rb
+++ spec/stats_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe Stats do
       expect(@stats.teams.first).to be_a(Team)
     end
 
-    it 'has team attributes' do
+    it 'team has attributes' do
       expect(@stats.teams.first.team_name).to eq('Atlanta United')
       expect(@stats.teams.first.team_id).to eq('1')
     end
@@ -42,6 +42,25 @@ RSpec.describe Stats do
       expect(@stats.games.first.home_goals).to eq('3')
       expect(@stats.games.first.away_goals).to eq('2')
       expect(@stats.games.first.total_score).to eq(5)
+    end
+  end
+
+  describe '#initialize GameTeams' do
+    it 'exists' do
+      expect(@stats.game_teams.first).to be_a(GameTeams)
+    end
+
+    it 'game_teams has attributes' do
+      
+      expect(@stats.game_teams.first.coach).to eq('John Tortorella')
+      expect(@stats.game_teams.first.goals).to eq('2')
+      expect(@stats.game_teams.first.shots).to eq('8')
+      expect(@stats.game_teams.first.tackles).to eq('44')
+      expect(@stats.game_teams.first.team_id).to eq('3')
+      expect(@stats.game_teams.first.game_id).to eq('2012030221')
+      expect(@stats.game_teams.first.season_id).to eq('2012')
+      expect(@stats.game_teams.first.result).to eq('LOSS')
+      expect(@stats.game_teams.first.home_away).to eq('away')
     end
   end
 end
