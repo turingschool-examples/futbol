4e970740397f55990f2dcdaf0c6be3b25f3e07da
Reid Miller <reid.s.miller@gmail.com>
2023-03-01 15:11:24 -0700

    feat: Add highest and lowest scoring visitor methods

--- lib/league_stats.rb
+++ lib/league_stats.rb
@@ -20,7 +20,13 @@ module LeagueStats
   end
 
   def highest_scoring_visitor
-
+    away_teams = @games.group_by(&:away_team_id)
+    away_teams_ave_score = {}
+    away_teams.each do |team, values|
+      away_teams_ave_score[team] = average_away_goals(values)
+    end
+    highest_scoring = away_teams_ave_score.max_by {|_, value| value}
+    convert_to_team_name(highest_scoring[0])
   end
 
   def highest_scoring_home_team
@@ -28,7 +34,13 @@ module LeagueStats
   end
   
   def lowest_scoring_visitor
-
+    away_teams = @games.group_by(&:away_team_id)
+    away_teams_ave_score = {}
+    away_teams.each do |team, values|
+      away_teams_ave_score[team] = average_away_goals(values)
+    end
+    highest_scoring = away_teams_ave_score.min_by {|_, value| value}
+    convert_to_team_name(highest_scoring[0])
   end
 
   def lowest_scoring_home_team
