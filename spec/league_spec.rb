76b4b84ad88f7f40694cf51233b0f4a2b72caeec
Julian Beldotti <julian.beldotti@gmail.com>
2023-02-28 22:04:07 -0500

    Revert "Feat: Add league class and test init"
    This reverts commit f708399b14980e1e1299254b898f91a689e649c1.

--- spec/league_spec.rb
+++ spec/league_spec.rb
@@ -1,27 +1,12 @@
 require 'rspec'
 require 'spec_helper'
 
-RSpec.describe League do
+RSpec.describe league do
   describe '#initialize' do
-    it 'initializes and has attributes' do
-      league1 = League.new({
-        team_id: 3,
-        team_name: 'Blueberry Beagles',
-        season: 20122013,
-        type: 'Postseason',
-        away_goals: 5,
-        home_goals: 16,
-        goals: 22
-      })
-      
+    it 'initilaizes and has attributes' do
+      league1 = League.new
+
       expect(league1).to be_a(League)
-      expect(league1.team_id).to eq(3)
-      expect(league1.team_name).to eq('Blueberry Beagles')
-      expect(league1.season).to eq(20122013)
-      expect(league1.type).to eq('Postseason')
-      expect(league1.away_goals).to eq(5)
-      expect(league1.home_goals).to eq(16)
-      expect(league1.goals).to eq(22)
     end
   end
 end
