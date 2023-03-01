1832f3562c5dac77322f1cd9cbeb5336be60ded4
margdaly <margaret.daly@gmail.com>
2023-03-01 10:31:41 -0700

    test: Add test for game_process_data

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,48 +1,47 @@
 require_relative '../spec/spec_helper'  
 
 RSpec.describe StatTracker do
- 
+  before(:each) do 
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+    
+    @locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+    
+    @team_data = {
+      team_id: "1",
+      franchiseId: "23",
+      teamName: "Atlanta United",
+      abbreviation: "ATL",
+      Stadium: "Mercedes-Benz Stadium"
+    }
+    
+    @team1 = Team.new(@team_data)
+    @stat_tracker = StatTracker.from_csv(@locations)
+  end
   
   describe 'initialize' do
-    before(:each) do 
-      game_path = './data/games.csv'
-      team_path = './data/teams.csv'
-      game_teams_path = './data/game_teams.csv'
-      
-      @locations = {
-        games: game_path,
-        teams: team_path,
-        game_teams: game_teams_path
-      }
-
-      @team_data = {
-        team_id: "1",
-        franchiseId: "23",
-        teamName: "Atlanta United",
-        abbreviation: "ATL",
-        Stadium: "Mercedes-Benz Stadium"
-      }
-
-      @team1 = Team.new(@team_data)
-
-      @stat_tracker = StatTracker.from_csv(@locations)
-    # require 'pry'; binding.pry
-    end
-
     it 'exists' do
       expect(@stat_tracker).to be_an_instance_of StatTracker
-
     end
     
     it 'processed team data, retrieves data from teams' do
       expect(@stat_tracker.processed_teams_data(@locations)).to all(be_a(Team))
-      # require 'pry'; binding.pry
+    end
 
+    it 'processed team data, retrieves data from teams' do
+      expect(@stat_tracker.processed_games_data(@locations)).to all(be_a(Game))
+    end
+
+    it 'processed team data, retrieves data from teams' do
+      expect(@stat_tracker.processed_game_teams_data(@locations)).to all(be_a(GameTeam))
     end
 
     xit 'can parse data into a string of objects' do
-      
-      
       # expect(@stat_tracker[games]).to be_a(Array)
       # expect(@stat_tracker[games]).to all(be_a(Game))
     end
