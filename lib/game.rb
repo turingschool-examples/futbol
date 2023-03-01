b76709a198cfdb1647b34bf4fc4715c3561aa1ff
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-03-01 15:47:48 -0700

    finish game_spec and remove unnecessary code from stat_tracker_spec

--- lib/game.rb
+++ lib/game.rb
@@ -1,22 +1,22 @@
 class Game
-  attr_reader :id,
+  attr_reader :game_id,
               :season,
               :type,
               :date_time,
-              :away_id,
-              :home_id,
+              :away_team_id,
+              :home_team_id,
               :away_goals,
               :home_goals,
               :venue
 
   def initialize(game_data)
-    @id = game_data[:game_id]
+    @game_id = game_data[:game_id]
     @season = game_data[:season]
     @type = game_data[:type]
-    @date_date = game_data[:date_time]
-    @away_id = game_data[:away_team_id]
-    @home_id = game_data[:home_team_id]
-    @away_goals = game_data[:away_team_goals]
+    @date_time = game_data[:date_time]
+    @away_team_id = game_data[:away_team_id]
+    @home_team_id = game_data[:home_team_id]
+    @away_goals = game_data[:away_goals]
     @home_goals = game_data[:home_goals]
     @venue = game_data[:venue]
   end
