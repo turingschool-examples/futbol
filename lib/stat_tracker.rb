f9c919c81ddd7c1ad181b95118c4f5454864e8d6
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-03-01 10:35:15 -0700

    finish: game stats

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -39,4 +39,45 @@ class StatTracker
       season[:goals].fdiv(season[:games]).round(2) 
     end
   end
+
+  def percent_home_wins
+    home_wins = 0
+    @games.each do |game|
+      home_wins += 1 if game[:home_goals].to_i > game[:away_goals].to_i
+    end
+    home_wins.fdiv(@games.length - 1).round(2)
+  end
+
+  def percent_away_wins
+    away_wins = 0
+    @games.each do |game|
+      away_wins += 1 if game[:away_goals].to_i > game[:home_goals].to_i
+    end
+    away_wins.fdiv(@games.length - 1).round(2)
+  end
+
+  def percent_ties
+    ties = 0
+    @games.each do |game|
+      ties += 1 if game[:away_goals].to_i == game[:home_goals].to_i
+    end
+    ties.fdiv(@games.length - 1).round(2)
+  end
+
+  def count_of_games_by_season
+    season_games = Hash.new(0)
+    @games.each do |game|
+      season = game[:season].to_i
+      season_games[season] += 1
+    end
+    season_games
+  end
+
+  def average_goals_per_game
+    goals = 0
+    @games.each do |game|
+      goals += game[:home_goals].to_i + game[:away_goals].to_i
+    end
+    goals.fdiv(@games.length - 1).round(2)
+  end
 end
