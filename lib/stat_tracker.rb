538cb9e3ec87e08cda4b38f1c59ddabbf2cedfa6
Grace Joh <graceehjoh@gmail.com>
2023-03-01 17:26:40 -0600

    feat: Add to StatTracker #initialize to read CSV data

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -9,7 +9,11 @@ class StatTracker
     StatTracker.new(locations)
   end
 
-  def initialize(locations)
+  attr_reader :game_data, :team_data, :game_teams_data
 
+  def initialize(locations)
+    @game_data = CSV.read locations[:games], headers: true, header_converters: :symbol
+    @team_data = CSV.read locations[:teams], headers: true, header_converters: :symbol
+    @game_teams_data = CSV.read locations[:game_teams], headers: true, header_converters: :symbol
   end
 end
