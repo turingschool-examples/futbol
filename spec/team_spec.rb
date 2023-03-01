1e73d2a54f907017f839675943732278aff8f4d5
Nick Sacco <nicksacco10@gmail.com>
2023-02-28 19:56:50 -0800

    Feat: Init Team class tests

---
+++ spec/team_spec.rb
@@ -0,0 +1,29 @@
+require 'rspec'
+require './lib/game'
+require './lib/team'
+
+RSpec.describe Team do
+  before(:each) do
+    hash = {
+      :team_id => 1,
+      :franchiseId => 23,
+      :teamName => "Atlanta United"
+      :abbreviation => "ATL"
+      :link => "/api/v1/teams/1"
+    } #are the above strings actually strings in the csv? arrays right?
+    #can we fix the camel case?
+    @team = Team.new(hash)
+  end
+
+  it 'exists' do
+    expect(@team).to be_a(Team)
+  end
+
+  it 'the team has attributes' do
+    expect(@team.team_id).to eq(1)
+    expect(@team.franchiseId).to eq(23)
+    expect(@team.teamName).to eq("Atlanta United")
+    expect(@team.abbreviation).to eq("ATL")
+    expect(@team.link).to eq("/api/v1/teams/1")
+  end
+end
