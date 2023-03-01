591ef81f5994248d03fb4c9fb64968337ff3645b
Logan Wilson <loganwilson777@gmail.com>
2023-03-01 09:43:57 -0700

    test: adds game_team unit tests, and edits spec helper

---
+++ spec/game_team_spec.rb
@@ -0,0 +1,41 @@
+require_relative './spec_helper'
+
+describe GameTeam do
+  before(:each) do
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+    @stat_tracker = StatTracker.from_csv(locations)
+    @test_game_teams = @stat_tracker.game_teams[0]
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@test_game_teams).to be_a GameTeam
+    end
+
+    it 'has attributes' do
+      expect(@test_game_teams.game_id).to eq("2012030221")
+      expect(@test_game_teams.team_id).to eq("3")
+      expect(@test_game_teams.hoa).to eq("away")
+      expect(@test_game_teams.result).to eq("LOSS")
+      expect(@test_game_teams.settled_in).to eq("OT")
+      expect(@test_game_teams.head_coach).to eq("John Tortorella")
+      expect(@test_game_teams.goals).to eq("2")
+      expect(@test_game_teams.shots).to eq("8")
+      expect(@test_game_teams.tackles).to eq("44")
+      expect(@test_game_teams.pim).to eq("8")
+      expect(@test_game_teams.powerplayopportunities).to eq("3")
+      expect(@test_game_teams.powerplaygoals).to eq("0")
+      expect(@test_game_teams.faceoffwinpercentage).to eq("44.8")
+      expect(@test_game_teams.giveaways).to eq("17")
+      expect(@test_game_teams.takeaways).to eq("7")
+    end
+  end
+end
