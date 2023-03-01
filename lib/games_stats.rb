16e77d18ab6bb8f98cf007a09f9f3982133a4b8b
Crow Rising <miscbyrd@gmail.com>
2023-03-01 15:56:26 -0700

    fix: teams test suite

--- lib/games_stats.rb
+++ lib/games_stats.rb
@@ -6,5 +6,8 @@ class GamesStats
     @games = CSV.open('./data/games.csv', headers: true, header_converters: :symbol).map { |row| Game.new(row) }
   end
 
-  
+  def highest_total_score
+    high_score = @games.max_by{ |game| game.total_score}
+    high_score.total_score
+  end
 end
--- lib/team.rb
+++ lib/team.rb
@@ -1,5 +1,6 @@
 class Team
-  attr_reader :team_id, :team_name
+  attr_reader :team_id, 
+              :team_name
   
   def initialize(details)
     @team_id = details[:team_id]
