de7b3fed1f6f8c3826f79c69b8add4fae74cd139
Andrew Bingham <andrewgbingham@gmail.com>
2023-02-28 20:05:54 -0700

    fix: correct team_spec init test

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -3,10 +3,15 @@ require './lib/team'
 
 
 class StatTracker
+  ####do we need a collector??####
+  def initialize
+    @teams = []
+  end
   def self.from_csv(locations)
     teams = CSV.open locations[:teams], headers: true, header_converters: :symbol
     teams.each do |team| 
-      new_team = Team.new(team)
+      # new_team = Team.new(team)
+      @teams << Team.new(team)
       require 'pry'; binding.pry
     end
   end
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 RSpec.describe Team do
   describe '#initialize' do
     it 'exists' do
-      team = Team.new
+      details = {team_id: "1", teamname: "Atlanta United"}
+      team = Team.new(details)
       expect(team).to be_a(Team)
     end
   end
