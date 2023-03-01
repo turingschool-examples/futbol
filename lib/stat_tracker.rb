2839af226c809c6fe217673722b1ef875a8e8bdb
Brian Guthrie <takingcareofthedeadfish@gmail.com>
2023-02-28 17:27:47 -0700

    Add tests for lowest and highest total score and code

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,17 +1,32 @@
 require_relative './stat_tracker'
+require 'csv'
 class StatTracker
 
-  game_path = './data/games.csv'
-  team_path = './data/teams.csv'
-  game_teams_path = './data/game_teams.csv'
-
-  locations = {
-    games: game_path,
-    teams: team_path,
-    game_teams: game_teams_path
-  }
-
-  # stat_tracker = StatTracker.from_csv(locations)
-
-  require 'pry'; binding.pry
+  # def self.from_csv(locations)
+  def initialize(locations)
+    @games = CSV.open locations[:games], headers: true, header_converters: :symbol
+    @teams = CSV.open locations[:teams], headers: true, header_converters: :symbol
+    @game_teams = CSV.open locations[:game_teams], headers: true, header_converters: :symbol
+  end
+  # end
+  
+  def highest_total_score
+    max = 0
+    @games.each do |row|
+      total = row[:home_goals].to_i + row[:away_goals].to_i
+      max = [max, total].max
+    end
+    max
+  end
+  
+  def lowest_total_score
+    min = 10
+    require 'pry'; binding.pry
+    @games.each do |row|
+      require 'pry'; binding.pry
+      score = row[:home_goals].to_i + row[:away_goals].to_i
+      min = [min, score].min
+    end
+    min
+  end
 end
