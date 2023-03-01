c5aebd15a8b51bc274f61042ccd5280af989bd4a
margdaly <margaret.daly@gmail.com>
2023-03-01 08:49:50 -0700

    Add team_data to tests in stat_tracker

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -8,24 +8,36 @@ RSpec.describe StatTracker do
       game_path = './data/games.csv'
       team_path = './data/teams.csv'
       game_teams_path = './data/game_teams.csv'
+      
+      @locations = {
+        games: game_path,
+        teams: team_path,
+        game_teams: game_teams_path
+      }
 
-    locations = {
-      games: game_path,
-      teams: team_path,
-      game_teams: game_teams_path
-    }
+      @team_data = {
+        team_id: "1",
+        franchiseId: "23",
+        teamName: "Atlanta United",
+        abbreviation: "ATL",
+        Stadium: "Mercedes-Benz Stadium"
+      }
 
-    @stat_tracker = StatTracker.from_csv(locations)
-    
-  end
-    it 'exists' do
-      expect(@stat_tracker).to be_an_instance_of StatTracker
-      # require 'pry'; binding.pry
-      p @stat_tracker
+      @team1 = Team.new(@team_data)
+
+      @stat_tracker = StatTracker.from_csv(@locations)
+    # require 'pry'; binding.pry
     end
 
+    it 'exists' do
+      expect(@stat_tracker).to be_an_instance_of StatTracker
+
+    end
+    
     it 'processed team data, retrieves data from teams' do
-      expect(@stat_tracker.processed_team_data(locations)).to eq([])
+      expect(@stat_tracker.processed_teams_data(@locations)).to all(be_a(Team))
+      # require 'pry'; binding.pry
+
     end
 
     xit 'can parse data into a string of objects' do
