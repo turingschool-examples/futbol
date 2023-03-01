3fbbce5dc1bd4e6ba44d8d4945b9395068010f7d
Crow Rising <miscbyrd@gmail.com>
2023-03-01 15:42:06 -0700

    test: Add highest total score

--- lib/games_stats.rb
+++ lib/games_stats.rb
@@ -5,4 +5,6 @@ class GamesStats
   def initialize
     @games = CSV.open('./data/games.csv', headers: true, header_converters: :symbol).map { |row| Game.new(row) }
   end
+
+  
 end
--- spec/games_stats_spec.rb
+++ spec/games_stats_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 RSpec.describe GamesStats do
-  
+  before(:each) do 
+    @game_stat = GamesStats.new
+  end  
   describe '#initialize' do
     it 'exists' do
-      game_stat = GamesStats.new
-      expect(game_stat).to be_a(GamesStats)
+      expect(@game_stat).to be_a(GamesStats)
+    end
+  end
+
+  describe '#highest_total_score' do
+    it 'has highest_total_score' do
+      expect(@game_stat.highest_total_score).to eq(11)
     end
   end
 end
