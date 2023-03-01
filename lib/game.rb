c07fccb5faa336290b44cbd018ddf43f2e9e2ec8
Caroline Peri <caroline.anne.peri@gmail.com>
2023-02-28 19:59:32 -0500

    feat: add initialize method and attributes

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,15 @@
 class Game
-  attr_reader :games_data,
-              :game_team_data
+  attr_reader :game_id, :season, :type, :date_time, :away_team_id, :home_team_id, :away_goals, :home_goals, :venue
 
-  def initialize(games_data, game_team_data)
-    @games_data = games_data
-    @game_team_data = game_team_data
+  def initialize(details)
+    @game_id = details[:game_id]
+    @season  = details[:season]
+    @type = details[:type]
+    @date_time = details[:date_time]
+    @away_team_id = details[:away_team_id]
+    @home_team_id = details[:home_team_id]
+    @away_goals = details[:away_goals]
+    @home_goals = details[:home_goals]
+    @venue = details[:venue]
   end
 end
