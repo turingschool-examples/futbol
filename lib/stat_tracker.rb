ff60d3899665223d2f4293a2322f40b2f3fbb72c
margdaly <margaret.daly@gmail.com>
2023-03-01 10:30:49 -0700

    feat: Add process_data for game and game_teams

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,12 +1,16 @@
 require_relative '../spec/spec_helper'
 
 class StatTracker 
-
-    attr_reader :data, :teams
+    attr_reader :data, 
+                :teams, 
+                :games, 
+                :game_teams
 
   def initialize(data)
     @data = data
     @teams = teams
+    @games = games
+    @game_teams = game_teams
   end
   
   def self.from_csv(locations)
@@ -25,20 +29,23 @@ class StatTracker
     end
     @teams = all_teams
   end
+  
+  def processed_games_data(locations)
+    all_games = []
+    games = @data[:games]
+    games.each do |row|
+      all_games << Game.new(row)
+    end
+    @games = all_games
+  end
+
+  def processed_game_teams_data(locations)
+    all_game_teams = []
+    game_teams = @data[:game_teams]
+    game_teams.each do |row|
+      all_game_teams << GameTeam.new(row)
+    end
+    @game_teams = all_game_teams
+  end
 end
-
-#   def initialize(locations)
-#     @games = processed_game_data(locations[:games])
-#     # @teams = processed_teams_data(locations[:teams])
-#     # @game_teams = processed_game_teams_data(locations[:game_teams])
-#   end
-
-#   def processed_game_data(game_data)
-#     all_games = []
-#     parsed_data = CSV.open game_data, headers: true, header_converters: :symbol
-#     parsed_data.each do |game_info|
-#       all_games << Game.new(game_info)
-#     end
-#     all_games
-#   end
 
