89e966cddab19b12459b5e643fb5f7e83ae43927
Reid Miller <reid.s.miller@gmail.com>
2023-03-01 15:10:22 -0700

    Add convert_to_team_name and average_away_goals methods to helper

--- lib/helper.rb
+++ lib/helper.rb
@@ -1,3 +1,11 @@
 module Helper
+  def convert_to_team_name(input_team_id)
+    team = @teams.find {|team| team.team_id == input_team_id}
+    team.team_name
+  end
 
+  def average_away_goals(input_games)
+    sum_away_goals = input_games.sum(&:away_goals)
+    sum_away_goals.to_f/input_games.length.to_f
+  end
 end
