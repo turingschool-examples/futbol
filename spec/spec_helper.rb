98c2ef43d9bb7607df4bda73de058daff1513ff8
Julian Beldotti <julian.beldotti@gmail.com>
2023-02-28 22:17:38 -0500

    Feat: Add league class and test init

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,4 @@
 require 'simplecov'
 SimpleCov.start
 
-# require_relative './game'
-# require_relative './team'
-# require_relative './stats'
 
-require './lib/league'
-require './lib/stat_tracker'
