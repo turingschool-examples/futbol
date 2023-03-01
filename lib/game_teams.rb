9cde9b828f507773ab8d6b1e9f1c68e7126538f4
Crow Rising <miscbyrd@gmail.com>
2023-03-01 13:16:34 -0700

    feat: Create games class with initialize methods

---
+++ lib/game_teams.rb
@@ -0,0 +1,5 @@
+class Games_Teams
+  def initialize
+    
+  end
+end
---
+++ lib/games.rb
@@ -0,0 +1,21 @@
+class Games
+  attr_reader :game_id,
+              :season_year,
+              :season_type,
+              :away_team_id,
+              :home_team_id,
+              :home_goals,
+              :away_goals,
+              :total_score
+
+  def initialize(details)
+    @game_id = details[:game_id]
+    @season_year = details[:season_year]
+    @season_type = details[:season_type]
+    @away_team_id = details[:away_team_id]
+    @home_team_id = details[:home_team_id]
+    @home_goals = details[:home_goals]
+    @away_goals = details[:away_goals]
+    @total_score = @home_goals + @away_goals
+  end
+end
