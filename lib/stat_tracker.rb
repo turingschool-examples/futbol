538681e596bc6db6920eae99dfb191718da31c7d
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-03-01 19:30:06 -0700

    Modify: modify format and organization.

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,44 +1,22 @@
 require 'csv'
+require './lib/game'
+require './lib/team'
 require './lib/game_teams'
+require_relative 'stats'
 
-class StatTracker
-  @@games = []
-  @@teams = []
-  @@game_teams = []
 
+class StatTracker 
   attr_reader :games, :teams, :game_teams
 
-  def initialize(games, teams, game_teams)
+  def initialize(locations)
     @games = games
     @teams = teams
     @game_teams = game_teams
   end
   
   def self.from_csv(locations)
-    StatTracker.new(locations[:games], locations[:teams], locations[:game_teams])
-  end
-  
-  def games
-    games = CSV.open @games, headers: true, header_converters: :symbol
-    games.each do |game|
-      @@games << game = Game.new(game) 
-    end
-    @@games
-  end
-
-  def teams
-    teams = CSV.open @teams, headers: true, header_converters: :symbol
-    teams.each do |team|
-      @@teams << teams = GameTeams.new(team)
-    end
-    @@teams
-  end
-
-  def game_teams
-    game_teams = CSV.open @game_teams, headers: true, header_converters: :symbol
-    game_teams.each do |game_teams|
-      @@game_teams << game_teams = GameTeams.new(game_teams)
-    end
-     @@game_teams
+    Stats.new(locations)
+    StatTracker.new(locations)
   end
 end
+
