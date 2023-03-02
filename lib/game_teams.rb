a2dd818fc338edfb000a6d5469ac5543709afecc
Andrew Bowman <andrewtbowman@gmail.com>
2023-03-01 19:55:00 -0500

    feat/test: finish game_teams methods+tests

--- lib/game_teams.rb
+++ lib/game_teams.rb
@@ -3,12 +3,12 @@ class GameTeam
     file = CSV.read locations[:game_teams], headers: true, header_converters: :symbol
     @game_id = file[:game_id]
     @team_id = file[:team_id]
-    @hoa = file[:HoA]
+    @hoa = file[:hoa]
     @result = file[:result]
     @settled_in = file[:settled_in]
     @head_coach = file[:head_coach]
     @goals = file[:goals]
-  
+    # require 'pry'; binding.pry
   end
 
   def best_offense
@@ -16,15 +16,49 @@ class GameTeam
     (0..@team_id.count).each do |i|
       offenses[@team_id[i]] += @goals[i].to_i
     end
-    offenses.key(offenses.values.max)
+    # offenses.key(offenses.values.max)
     offenses.max_by{|k, v| v}[0]
   end
-
+  
   def worst_offense
     offenses = Hash.new(0)
     (0..@team_id.count).each do |i|
       offenses[@team_id[i]] += @goals[i].to_i
     end
-    offenses.find_all{|k, v| v > 0}.min_by{|k, v| v}[0]
+    offenses.select{|k, v| v > 0}.min_by{|k, v| v}[0]
+  end
+
+  def highest_scoring_visitor
+    away_teams = Hash.new(0)
+    (0..@team_id.count).each do |i|
+      away_teams[@team_id[i]] += @goals[i].to_i if @hoa[i] == 'away'
+    end
+    team_id = away_teams.max_by{|k, v| v}[0]
+    require 'pry'; binding.pry
+  end
+
+  def lowest_scoring_visitor
+    away_teams = Hash.new(0)
+    (0..@team_id.count).each do |i|
+      away_teams[@team_id[i]] += @goals[i].to_i if @hoa[i] == 'away'
+    end
+    team_id = away_teams.min_by{|k, v| v}[0]
+  end
+
+  def highest_scoring_home_team
+    home_teams = Hash.new(0)
+    (0..@team_id.count).each do |i|
+      home_teams[@team_id[i]] += @goals[i].to_i if @hoa[i] == 'home'
+    end
+    team_id = home_teams.max_by{|k, v| v}[0]
+    require 'pry'; binding.pry
+  end
+
+  def lowest_scoring_home_team
+    home_teams = Hash.new(0)
+    (0..@team_id.count).each do |i|
+      home_teams[@team_id[i]] += @goals[i].to_i if @hoa[i] == 'home'
+    end
+    home_teams.min_by{|k, v| v}[0]
   end
 end
