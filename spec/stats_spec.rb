e4c0f3413d0db083ed2dfa0f63ed8313efe8ae98
Katie Lonsdale <katie952@gmail.com>
2023-03-01 20:13:37 -0700

    test: add initialize tests

--- spec/stats_spec.rb
+++ spec/stats_spec.rb
@@ -13,43 +13,18 @@ RSpec.describe StatTracker do
     @stat = Stats.new(@locations)
   end
 
-  # describe '#initialize' do
-  #   it 'exists and has attributes' do
+  describe '#initialize' do
+    it 'exists and has attributes' do
+      expect(@stat).to be_a Stats
+    end
 
-  #   end
-  # end
-# end
-
-# describe '#games' do
-#     xit 'returns an array of game instances' do
-#       expect(@stat_tracker.games).to be_a array
-#       expect(@stat_tracker.games.sample).to be_a Game
-#     end
-#     xit 'adds each instance to games class array' do
-#       # need to clear array?
-#       expect(@@games.empty?).to be false
-#     end
-#   end
-
-#   describe '#teams' do
-#     xit 'returns an array of team instances' do
-#       expect(@stat_tracker.teams).to be_a array
-#       expect(@stat_tracker.teams.sample).to be_a Team
-#     end
-#     xit 'adds each instance to games class array' do
-#       # need to clear array?
-#       expect(@@games.empty?).to be false
-#     end
-#   end
-
-#   describe '#game_teams' do
-#     xit 'returns an array of game_team instances' do
-#       expect(@stat_tracker.game_team).to be_a array
-#       expect(@stat_tracker.game_team.sample).to be_a GameTeam 
-#       # what are we calling this? season?
-#     end
-#     xit 'adds each instance to games class array' do
-#       # need to clear array?
-#       expect(@@games.empty?).to be false
-#     end
+    it 'has instance variables that contain arrays of objects' do
+      expect(@stat.teams).to be_a Array 
+      expect(@stat.teams.sample).to be_a Team
+      expect(@stat.games).to be_a Array
+      expect(@stat.games.sample).to be_a Game
+      expect(@stat.game_teams).to be_a Array
+      expect(@stat.game_teams.sample).to be_a GameTeams
+    end
+  end
   end
