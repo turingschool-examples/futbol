0981a5effe60323904d23c6ff2257f335607d663
Boston Lowrey <bostonlowrey@gmail.com>
2023-03-02 15:21:08 -0500

    feat: #fewest_tackles works

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -111,7 +111,16 @@ class StatTracker
   end
 
   def fewest_tackles(season_id)
-  
+    team_and_tackles = Hash.new(0) 
+    seasons =  get_reg_and_post_seasons(season_id)
+    seasons.each do |season|
+      season.games.each do |game|
+        team_and_tackles[game.refs[:home_team].name] += game.team_stats[:home_team][:tackles].to_i
+        team_and_tackles[game.refs[:away_team].name] += game.team_stats[:away_team][:tackles].to_i
+      end
+    end
+
+    team_and_tackles.min_by{ |team, tackles| tackles}.first
   end
 end
 
