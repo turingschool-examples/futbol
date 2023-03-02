f8a8fbbf1217b7d92c4cb2167f4264156bcee3b1
Thomas Hawley <thawley22@turing.edu>
2023-03-02 12:24:13 -0700

    test: Add #initialize test for SeasonStats class

---
+++ spec/season_stats_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+RSpec.describe SeasonStats do
+  before(:each) do
+    @season_stat = SeasonStats.new
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@season_stat).to be_a(SeasonStats)
+    end
+  end
+end
