bc96ff15d0a6a90c6c5cbb69ee21a96c76398df5
Grace Joh <graceehjoh@gmail.com>
2023-03-02 00:07:30 -0600

    fix: Team #initialize team and franchise ids to be integer

--- lib/team.rb
+++ lib/team.rb
@@ -7,8 +7,8 @@ class Team
   attr_accessor :games
 
   def initialize(row)
-    @team_id = row[:team_id]
-    @franchise_id = row[:franchiseId]
+    @team_id = row[:team_id].to_i
+    @franchise_id = row[:franchiseId].to_i
     @team_name = row[:teamName]
     @abbreviation = row[:abbreviation]
     @stadium = row[:Stadium]
