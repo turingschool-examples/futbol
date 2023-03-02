bafe662dca49de8a1ecb20b95db40b715fdc0dfe
Reid Miller <reid.s.miller@gmail.com>
2023-03-01 21:15:47 -0700

    feat: Add ave_accuracy method

--- lib/helper.rb
+++ lib/helper.rb
@@ -18,11 +18,17 @@ module Helper
 
   def average_away_goals(input_games)
     sum_away_goals = input_games.sum(&:away_goals)
-    sum_away_goals.to_f/input_games.length.to_f
+    sum_away_goals.fdiv(input_games.count)
   end
 
   def average_home_goals(input_games)
     sum_home_goals = input_games.sum(&:home_goals)
     sum_home_goals.fdiv(input_games.count)
+  end
+
+  def average_accuracy(input_games)
+    sum_shots = input_games.sum(&:shots)
+    sum_goals = input_games.sum(&:goals)
+    sum_goals.fdiv(sum_shots)*100
   end
 end
