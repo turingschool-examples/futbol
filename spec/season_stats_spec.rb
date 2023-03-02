68795a5f98dbaaed2c6f33fce1f9ccc4dac37c97
Thomas Hawley <thawley22@turing.edu>
2023-03-02 13:09:56 -0700

    test: Add #most_accurate_team and #least_accurate_team tests

--- spec/season_stats_spec.rb
+++ spec/season_stats_spec.rb
@@ -10,4 +10,18 @@ RSpec.describe SeasonStats do
       expect(@season_stat).to be_a(SeasonStats)
     end
   end
+
+  describe '#most_accurate_team' do
+    it 'can return the team with the most accuracy per season' do
+      expect(@stat_tracker.most_accurate_team('20132014')).to eq('Real Salt Lake')
+      expect(@stat_tracker.most_accurate_team('20142015')).to eq('Toronto FC')
+    end
+  end
+
+  describe '#least_accurate_team' do
+    it 'can return the team with the least accuracy pr season' do
+      expect(@stat_tracker.least_accurate_team('20132014')).to eq('New York City FC')
+      expect(@stat_tracker.least_accurate_team('20142015')).to eq('Columbus Crew SC')
+    end
+  end
 end
