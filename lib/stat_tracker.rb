a0504d45dba3cc790f9fc610d8213eea132c691b
Grace Joh <graceehjoh@gmail.com>
2023-03-02 00:10:19 -0600

    refactor: StatTracker #all_games and #all_teams

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -6,7 +6,7 @@ class StatTracker
   #include modules
 
   def self.from_csv(locations)
-    StatTracker.new(locations)
+    new(locations)
   end
 
   attr_reader :game_data, :team_data, :game_teams_data
@@ -18,37 +18,16 @@ class StatTracker
   end
 
   def all_games
-    games = []
-    @game_data.each do |row|
-      game_details = {
-        id: row[:game_id].to_i,
-        season: row[:season],
-        season_type: row[:type],
-        date: row[:date_time],
-        away_id: row[:away_team_id].to_i,
-        home_id: row[:home_team_id].to_i,
-        away_goals: row[:away_goals].to_i,
-        home_goals: row[:home_goals].to_i
-      }
-      games << Game.new(game_details)
+    @game_data.map do |row|
+      Game.new(row)
     end
-    games
   end
 
   def all_teams
-    teams = []
-    @team_data.each do |row|
-      team_details = {
-        team_id: row[:team_id].to_i,
-        franchise_id: row[:franchiseid],
-        team_name: row[:teamname],
-        abbreviation: row[:abbreviation],
-        stadium: row[:stadium]
-      }
-      team = Team.new(team_details)
+    teams = @team_data.map do |row|
+      team = Team.new(row)
       team.games = all_games.select { |game| game.home_id == team.team_id || game.away_id == team.team_id }
-      teams << team
+      team
     end
-    teams
   end
 end
