9cc2ad31080739ab937fd2bf654bfb87ec07bf8e
Grace Joh <graceehjoh@gmail.com>
2023-03-01 23:35:06 -0600

    refactor: Game and Team #initialize

--- lib/game.rb
+++ lib/game.rb
@@ -8,14 +8,14 @@ class Game
               :away_goals,
               :home_goals
 
-  def initialize(game_details)
-    @id = game_details[:id]
-    @season = game_details[:season]
-    @season_type = game_details[:season_type]
-    @date = game_details[:date]
-    @away_id = game_details[:away_id]
-    @home_id = game_details[:home_id]
-    @away_goals = game_details[:away_goals]
-    @home_goals = game_details[:home_goals]
+  def initialize(row)
+    @id = row[:id].to_i
+    @season = row[:season]
+    @season_type = row[:season_type]
+    @date = row[:date]
+    @away_id = row[:away_id].to_i
+    @home_id = row[:home_id].to_i
+    @away_goals = row[:away_goals].to_i
+    @home_goals = row[:home_goals].to_i
   end
 end
--- lib/team.rb
+++ lib/team.rb
@@ -6,12 +6,12 @@ class Team
               :stadium
   attr_accessor :games
 
-  def initialize(team_data)
-    @team_id = team_data[:team_id]
-    @franchise_id = team_data[:franchiseid]
-    @team_name = team_data[:teamname]
-    @abbreviation = team_data[:abbreviation]
-    @stadium = team_data[:stadium]
+  def initialize(row)
+    @team_id = row[:team_id]
+    @franchise_id = row[:franchiseid]
+    @team_name = row[:teamname]
+    @abbreviation = row[:abbreviation]
+    @stadium = row[:stadium]
     @games = []
   end
 end
