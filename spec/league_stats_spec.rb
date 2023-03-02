1038a44d358e7f01c816291ef130ee1f3b8c1167
Brandon Johnson <120131327+johnsonbw2010@users.noreply.github.com>
2023-03-02 08:25:12 -0500

    test: unskip tests that were failing due to incorrect game_team paths, all tests passing

--- spec/league_stats_spec.rb
+++ spec/league_stats_spec.rb
@@ -23,7 +23,7 @@ describe LeagueStats do
     expect(stat_tracker.league_stats.count_of_teams).to eq(32)
   end
 
-  xit 'can store team total goals' do
+  it 'can store team total goals' do
 
     expect(stat_tracker.league_stats.lowest_scoring_home).to eq("Reign FC")
     expect(stat_tracker.league_stats.lowest_scoring_away).to eq("Reign FC")
@@ -37,11 +37,11 @@ describe LeagueStats do
     expect(stat_tracker.league_stats.worst_offense).to eq("Utah Royals FC")
   end
 
-  xit 'can find the highest scoring home team' do
+  it 'can find the highest scoring home team' do
     expect(stat_tracker.league_stats.highest_scoring_home_team).to eq('Sporting Kansas City')
   end
 
-  xit 'can find highest scoring away team' do
+  it 'can find highest scoring away team' do
     expect(stat_tracker.league_stats.highest_scoring_away_team).to eq('Sporting Kansas City')
   end
 end
