8b602c3d534ad09459337b738771b8846868e495
Andrew Bingham <andrewgbingham@gmail.com>
2023-03-02 18:48:45 -0700

    test: add fewest tackles test

--- spec/season_stats_spec.rb
+++ spec/season_stats_spec.rb
@@ -81,5 +81,9 @@ RSpec.describe SeasonStats do
       expect(@stat_tracker.most_tackles("20142015")).to eq "Seattle Sounders FC"
     end
 
-    
+    it "#fewest_tackles" do
+      expect(@stat_tracker.fewest_tackles("20132014")).to eq "Atlanta United"
+      expect(@stat_tracker.fewest_tackles("20142015")).to eq "Orlando City SC"
+    end
+  end
 end
