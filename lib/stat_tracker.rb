5ca411570f999eafbda7286a17ed4861457fac00
Crow Rising <miscbyrd@gmail.com>
2023-03-03 14:37:19 -0700

    fix: Add missing methods

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -20,40 +20,49 @@ class StatTracker
     @season_stats = SeasonStats.new(files)
   end
 
-  # Game Stats
-    def highest_total_score
-      @game_stats.highest_total_score
-    end
+# Game Stats
+  def highest_total_score
+    @game_stats.highest_total_score
+  end
 
-    def lowest_total_score
-      @game_stats.lowest_total_score
-    end
+  def lowest_total_score
+    @game_stats.lowest_total_score
+  end
 
-    def percentage_home_wins
-      @game_stats.percentage_home_wins
-    end
+  def percentage_home_wins
+    @game_stats.percentage_home_wins
+  end
 
-    def percentage_visitor_wins
-      @game_stats.percentage_visitor_wins
-    end
+  def percentage_visitor_wins
+    @game_stats.percentage_visitor_wins
+  end
 
-    def percentage_ties
-      @game_stats.percentage_ties
-    end
+  def percentage_ties
+    @game_stats.percentage_ties
+  end
 
-    def count_of_games_by_season
-      @game_stats.count_of_games_by_season
-    end
+  def count_of_games_by_season
+    @game_stats.count_of_games_by_season
+  end
 
-    def average_goals_per_game
-      @game_stats.average_goals_per_game
-    end
+  def average_goals_per_game
+    @game_stats.average_goals_per_game
+  end
 
-    def average_goals_by_season
-      @game_stats.average_goals_by_season
-    end
+  def average_goals_by_season
+    @game_stats.average_goals_by_season
+  end
 
   # League Stats
+
+  def count_of_teams
+    @league_stats.count_of_teams
+  end
+
+  def best_offense
+    @league_stats.best_offense
+  end
+  
   def worst_offense
     @league_stats.worst_offense
   end
