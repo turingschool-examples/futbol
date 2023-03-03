d6f4a10390a00fec307cfebb9c05778dc9128127
margdaly <margaret.daly@gmail.com>
2023-03-02 18:51:38 -0700

    Refactor for branch purposes

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -140,9 +140,5 @@ RSpec.describe StatTracker do
     it 'has #best_offense' do
       expect(@stat_tracker.best_offense).to eq "Reign FC"
     end
-
-    it "#worst_offense" do
-      expect(@stat_tracker.worst_offense).to eq "Utah Royals FC"
-    end
   end
 end
