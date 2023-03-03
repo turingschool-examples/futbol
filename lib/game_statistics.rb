5e28b90d9121856674d3902d089d0c846b7c6ceb
Katie Lonsdale <katie952@gmail.com>
2023-03-02 20:27:27 -0700

    feat: add percentage ties method

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -22,4 +22,13 @@ class GameStatistics < Stats
     percentage = (visitor_wins.to_f / @games.count.to_f) * 100
     percentage.round(2)
   end
+
+  def percentage_ties
+    ties = 0
+    @games.each do |game|
+      ties += 1 if game.home_goals == game.away_goals
+    end
+    percentage = (ties.to_f / @games.count.to_f) * 100
+    percentage.round(2)
+  end
 end
