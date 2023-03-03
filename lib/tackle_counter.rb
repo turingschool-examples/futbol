7c73f8d930584ea381a0f2779c621f29ff347006
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-03-03 11:48:31 -0700

    add tackle_counter module to dry out #most_tackles and #least_tackles

---
+++ lib/tackle_counter.rb
@@ -0,0 +1,12 @@
+module TackleCounter
+
+  def count_tackles(season_id)
+    tackles = Hash.new(0)
+
+    @seasons_by_id[season_id][:game_teams].each do |game_team|
+      tackles[game_team.team_id] += game_team.tackles.to_i
+    end
+    tackles
+  end
+
+end
