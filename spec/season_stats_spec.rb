0fdf6703e8d20274481d3d1e373eef62552d57c4
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-03 12:14:14 -0500

    refactor: edit season stats spec to remove unnecessary dummy data + set larger games dummy var

--- spec/season_stats_spec.rb
+++ spec/season_stats_spec.rb
@@ -12,45 +12,40 @@ describe SeasonStats do
       game_teams: game_teams_path
     }
     @stat_tracker = StatTracker.from_csv(locations)
-    @test_game_teams1 = @stat_tracker.game_teams[0]
-    @test_game_teams2 = @stat_tracker.game_teams[1]
-    @test_game1 = @stat_tracker.games[0]
-    @test_game2 = @stat_tracker.games[1]
-    @test_team1 = @stat_tracker.teams[0]
-    @test_team2 = @stat_tracker.teams[1]
 
     @test_games = @stat_tracker.games[0..9]
+    @test_games_larger = @stat_tracker.games[0..100]
     @test_game_teams = @stat_tracker.game_teams[0..9]
   end
 
   it 'exists' do
     expect(@stat_tracker.season_rspec_test).to be true
   end
-  
+
   it 'can determine the best coach' do
-    @stat_tracker.games = @stat_tracker.games[0..100]
-    
+    @stat_tracker.games = @test_games_larger
+
     expect(@stat_tracker.winningest_coach("20122013")).to eq("Claude Julien")
     expect(@stat_tracker.winningest_coach("20152016")).to eq("Mike Sullivan")
   end
-  
+
   it 'can determine the worst coach' do
-    @stat_tracker.games = @stat_tracker.games[0..100]
-    
+    @stat_tracker.games = @test_games_larger
+
     expect(@stat_tracker.worst_coach("20122013")).to eq("John Tortorella")
     expect(@stat_tracker.worst_coach("20152016")).to eq("Alain Vigneault")
   end
-  
+
   it 'can determine the most accurate team' do
-    @stat_tracker.games = @stat_tracker.games[0..100]
-    
+    @stat_tracker.games = @test_games_larger
+
     expect(@stat_tracker.most_accurate_team("20122013")).to eq("New York City FC")
     expect(@stat_tracker.most_accurate_team("20152016")).to eq("Sporting Kansas City")
   end
-  
+
   it 'can determine the least accurate team' do
-    @stat_tracker.games = @stat_tracker.games[0..100]
-  
+    @stat_tracker.games = @test_games_larger
+
     expect(@stat_tracker.least_accurate_team("20122013")).to eq("Houston Dynamo")
     expect(@stat_tracker.least_accurate_team("20152016")).to eq("Chicago Fire")
   end
