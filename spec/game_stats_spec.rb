1ab018f7a5d87799773be8d5c2dd1d1c185af652
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-03 12:07:34 -0500

    refactor: delete additional commented-out lines

--- spec/game_stats_spec.rb
+++ spec/game_stats_spec.rb
@@ -12,17 +12,9 @@ describe GameStats do
       game_teams: game_teams_path
     }
     @stat_tracker = StatTracker.from_csv(locations)
-    # @test_game_teams1 = @stat_tracker.game_teams[0]
-    # @test_game_teams2 = @stat_tracker.game_teams[1]
-    # @test_game1 = @stat_tracker.games[0]
-    # @test_game2 = @stat_tracker.games[1]
-    # @test_team1 = @stat_tracker.teams[0]
-    # @test_team2 = @stat_tracker.teams[1]
 
     @test_games = @stat_tracker.games[0..9]
     @test_games_larger = @stat_tracker.games[0..50]
-    #@test_game_teams = @stat_tracker.game_teams[0..9]
-    #@test_game_teams_larger = @stat_tracker.game_teams[0..50]
   end
 
   it 'exists' do
