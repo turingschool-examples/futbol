e9e0ae58b415caddbaf269dbb4ea82f03ffcaae9
Katie Lonsdale <katie952@gmail.com>
2023-03-02 20:46:33 -0700

    refactor: percentage ties and wins methods

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -10,8 +10,7 @@ class GameStatistics < Stats
     @games.each do |game|
       home_wins += 1 if game.home_goals > game.away_goals
     end
-    percentage = (home_wins.to_f / @games.count.to_f) * 100
-    percentage.round(2)
+    (home_wins.to_f / @games.count.to_f).round(2)
   end
 
   def percentage_visitor_wins
@@ -19,8 +18,7 @@ class GameStatistics < Stats
     @games.each do |game|
       visitor_wins += 1 if game.home_goals < game.away_goals
     end
-    percentage = (visitor_wins.to_f / @games.count.to_f) * 100
-    percentage.round(2)
+    (visitor_wins.to_f / @games.count.to_f).round(2)
   end
 
   def percentage_ties
@@ -28,7 +26,6 @@ class GameStatistics < Stats
     @games.each do |game|
       ties += 1 if game.home_goals == game.away_goals
     end
-    percentage = (ties.to_f / @games.count.to_f) * 100
-    percentage.round(2)
+    percentage = (ties.to_f / @games.count.to_f).round(2)
   end
 end
