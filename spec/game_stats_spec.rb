c2a2260c1ef10d2edc43f28d12eeea7cf4a3aeff
Caroline Peri <caroline.anne.peri@gmail.com>
2023-03-03 12:06:42 -0500

    refactor: edit game stats spec to reorder tests + remove unneeded dummy data

--- spec/game_stats_spec.rb
+++ spec/game_stats_spec.rb
@@ -12,17 +12,17 @@ describe GameStats do
       game_teams: game_teams_path
     }
     @stat_tracker = StatTracker.from_csv(locations)
-    @test_game_teams1 = @stat_tracker.game_teams[0]
-    @test_game_teams2 = @stat_tracker.game_teams[1]
-    @test_game1 = @stat_tracker.games[0]
-    @test_game2 = @stat_tracker.games[1]
-    @test_team1 = @stat_tracker.teams[0]
-    @test_team2 = @stat_tracker.teams[1]
+    # @test_game_teams1 = @stat_tracker.game_teams[0]
+    # @test_game_teams2 = @stat_tracker.game_teams[1]
+    # @test_game1 = @stat_tracker.games[0]
+    # @test_game2 = @stat_tracker.games[1]
+    # @test_team1 = @stat_tracker.teams[0]
+    # @test_team2 = @stat_tracker.teams[1]
 
     @test_games = @stat_tracker.games[0..9]
     @test_games_larger = @stat_tracker.games[0..50]
-    @test_game_teams = @stat_tracker.game_teams[0..9]
-    @test_game_teams_larger = @stat_tracker.game_teams[0..50]
+    #@test_game_teams = @stat_tracker.game_teams[0..9]
+    #@test_game_teams_larger = @stat_tracker.game_teams[0..50]
   end
 
   it 'exists' do
@@ -41,6 +41,32 @@ describe GameStats do
     expect(@stat_tracker.lowest_total_score).to eq(1)
   end
 
+  it 'can determine percentage of home wins' do
+    @stat_tracker.games = @test_games
+
+    expect(@stat_tracker.percentage_home_wins).to eq(0.60)
+  end
+
+  it 'can determine percentage of visitor wins' do
+    @stat_tracker.games = @test_games
+    expect(@stat_tracker.percentage_visitor_wins).to eq(0.40)
+  end
+
+  it 'can determine percentage of ties' do
+    @stat_tracker.games = @test_games_larger
+
+    expect(@stat_tracker.percentage_ties).to eq(0.02)
+  end
+
+  it 'can count games by season' do
+    @stat_tracker.games = @test_games
+    expected = ({'20122013' => 10})
+    expect(@stat_tracker.count_of_games_by_season).to be_a Hash
+    expect(@stat_tracker.count_of_games_by_season.keys[0]).to be_a String
+    expect(@stat_tracker.count_of_games_by_season.values[0]).to be_a Integer
+    expect(@stat_tracker.count_of_games_by_season).to eq(expected)
+  end
+
   it 'can determine the average goals per game' do
     @stat_tracker.games = @test_games
 
@@ -56,31 +82,5 @@ describe GameStats do
     expect(@stat_tracker.average_goals_by_season.keys[0]).to be_a String
     expect(@stat_tracker.average_goals_by_season.values[0]).to be_a Float
     expect(@stat_tracker.average_goals_by_season).to eq(expected)
-  end
-
-  it 'can determine percentage of home wins' do
-    @stat_tracker.games = @test_games
-
-    expect(@stat_tracker.percentage_home_wins).to eq(0.60)
-  end
-
-  it 'can determine percentage of visitor wins' do
-    @stat_tracker.games = @test_games
-    expect(@stat_tracker.percentage_visitor_wins).to eq(0.40)
-  end
-
-  it 'can count games by season' do 
-    @stat_tracker.games = @test_games
-    expected = ({'20122013' => 10})
-    expect(@stat_tracker.count_of_games_by_season).to be_a Hash
-    expect(@stat_tracker.count_of_games_by_season.keys[0]).to be_a String
-    expect(@stat_tracker.count_of_games_by_season.values[0]).to be_a Integer
-    expect(@stat_tracker.count_of_games_by_season).to eq(expected)
-  end
-  
-  it 'can determine percentage of ties' do
-    @stat_tracker.games = @test_games_larger
-
-    expect(@stat_tracker.percentage_ties).to eq(0.02)
   end
 end
