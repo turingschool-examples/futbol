f8a19fdebb02715cad481f65a0e4b183417929be
Reid Miller <reid.s.miller@gmail.com>
2023-03-03 11:45:41 -0700

    test: Add team_info test

---
+++ spec/team_stats_spec.rb
@@ -0,0 +1,29 @@
+require_relative './spec_helper'
+
+RSpec.describe Team do
+  before(:each) do
+    before(:each) do
+      game_path = './data/games.csv'
+      team_path = './data/teams.csv'
+      game_teams_path = './data/game_teams.csv'
+  
+      locations = {
+        games: game_path,
+        teams: team_path,
+        game_teams: game_teams_path
+      }
+      @stat_tracker = StatTracker.from_csv(locations)
+  
+      @test_games = @stat_tracker.games[0..9]
+      @test_games_larger = @stat_tracker.games[0..100]
+      @test_game_teams = @stat_tracker.game_teams[0..9]
+  end
+
+  it 'can create team hash' do
+    expect(@stat_tracker.team_info("1")[team_id]).to eq("1")
+    expect(@stat_tracker.team_info("1")[franchise_id]).to eq("23")
+    expect(@stat_tracker.team_info("1")[team_name]).to eq("Atlanta United")
+    expect(@stat_tracker.team_info("1")[abbreviation]).to eq("ATL")
+    expect(@stat_tracker.team_info("1")[link]).to eq("/api/vi/teams/1")
+  end
+end
