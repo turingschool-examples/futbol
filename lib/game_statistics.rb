abb4d74813543bf4d497f875fff1c44342bd2cf8
Katie Lonsdale <katie952@gmail.com>
2023-03-02 20:21:04 -0700

    feat: add percentage visitor wins method

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -13,4 +13,13 @@ class GameStatistics < Stats
     percentage = (home_wins.to_f / @games.count.to_f) * 100
     percentage.round(2)
   end
+
+  def percentage_visitor_wins
+    visitor_wins = 0
+    @games.each do |game|
+      visitor_wins += 1 if game.home_goals < game.away_goals
+    end
+    percentage = (visitor_wins.to_f / @games.count.to_f) * 100
+    percentage.round(2)
+  end
 end
