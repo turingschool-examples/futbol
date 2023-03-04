a77c2c6465906749976e8e94ad54eb351e0b4f25
Thomas Hawley <thawley22@turing.edu>
2023-03-03 22:50:46 -0700

    test: Add tests for helper methods

--- spec/game_stats_spec.rb
+++ spec/game_stats_spec.rb
@@ -32,12 +32,27 @@ RSpec.describe GameStats do
   end
 
   describe '#percentages' do
+    it 'can return an array of all home wins' do
+      expect(@game_stat.all_home_wins[0]).to be_a(Game)
+      expect(@game_stat.all_home_wins.length).to eq(3237)
+    end
+
     it 'has percentage_home_wins' do
       expect(@game_stat.percentage_home_wins).to eq(0.44)
     end
 
+    it 'can return an array of all away wins' do
+      expect(@game_stat.all_away_wins[0]).to be_a(Game)
+      expect(@game_stat.all_away_wins.length).to eq(2687)
+    end
+
     it 'has percentage_visitor_wins' do
       expect(@game_stat.percentage_visitor_wins).to eq(0.36)
+    end
+
+    it 'can return an array of all tie games' do
+      expect(@game_stat.all_ties[0]).to be_a(Game)
+      expect(@game_stat.all_ties.length).to eq(1517)
     end
 
     it 'has percentage_ties' do
@@ -63,6 +78,9 @@ RSpec.describe GameStats do
       expect(@game_stat.average_goals_per_game).to eq(4.22)
     end
 
+    it 'can return a hash of goals by season' do
+      expect(@game_stat.goals_by_season).to be_a(Hash)
+    end
     it 'can return a hash of average goals by season' do
       expect(@game_stat.average_goals_by_season).to eq({
         "20122013"=>4.12,
