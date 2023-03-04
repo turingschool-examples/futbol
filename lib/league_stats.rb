b79863aa9a5b60b73d305e2311adb4db6e6635c5
Angel Byun <abyun22@turing.edu>
2023-03-04 13:22:33 -0700

    Refactor: modified highest_scoring_home_team and lowest_scoring_home_team method in league_stats class with helper methods

--- lib/league_stats.rb
+++ lib/league_stats.rb
@@ -68,21 +68,32 @@ class LeagueStats < Stats
   end
 
   def highest_scoring_home_team
+    @teams.each do |team|
+      if team.team_id == average_goal_by_home.key(average_goal_by_home.values.max)
+        return team.team_name
+      end
+    end
+  end
+
+  def total_goals_by_home_team
     total_goals_by_home_team = Hash.new(0)
-    total_home_games_by_team = Hash.new(0)
     @games.each do |game|
       total_goals_by_home_team[game.home_team_id] += game.home_goals.to_i
     end
+    total_goals_by_home_team
+  end
+
+  def total_home_games_by_team
+    total_home_games_by_team = Hash.new(0)
     @games.each do |game|
       total_home_games_by_team[game.home_team_id] += 1 
     end
+    total_home_games_by_team
+  end
+
+  def average_goal_by_home
     total_goals_by_home_team.merge!(total_home_games_by_team) do |home_team_id, home_goals, home_games|
       home_goals.to_f / home_games
-    end
-    @teams.each do |team|
-      if team.team_id == total_goals_by_home_team.key(total_goals_by_home_team.values.max)
-        return team.team_name
-      end
     end
   end
 
@@ -106,19 +117,8 @@ class LeagueStats < Stats
   end
 
   def lowest_scoring_home_team
-    total_goals_by_home_team = Hash.new(0)
-    total_home_games_by_team = Hash.new(0)
-    @games.each do |game|
-      total_goals_by_home_team[game.home_team_id] += game.home_goals.to_i
-    end
-    @games.each do |game|
-      total_home_games_by_team[game.home_team_id] += 1 
-    end
-    total_goals_by_home_team.merge!(total_home_games_by_team) do |home_team_id, home_goals, home_games|
-      home_goals.to_f / home_games
-    end
     @teams.each do |team|
-      if team.team_id == total_goals_by_home_team.key(total_goals_by_home_team.values.min)
+      if team.team_id == average_goal_by_home.key(average_goal_by_home.values.min)
         return team.team_name
       end
     end
