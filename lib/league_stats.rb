7e7a8f3b7c6642698ed71c7918ce0fca389f834c
Reid Miller <reid.s.miller@gmail.com>
2023-03-04 21:26:02 -0700

    refactor: Change methods to use attribute team_name instead of conversion

--- lib/league_stats.rb
+++ lib/league_stats.rb
@@ -12,62 +12,62 @@ module LeagueStats
   end
 
   def best_offense
-    info_by_team = @game_teams.group_by(&:team_id)
+    info_by_team = @game_teams.group_by(&:team_name)
     team_goals_average = {}
     info_by_team.map do |team, games|  
       team_goals_average[team] = average_of_goals(games)
     end
     da_best_of_all = team_goals_average.max_by{|_, values| values}
-    convert_to_team_name(da_best_of_all[0])
+    da_best_of_all[0]
   end
 
   def worst_offense
-    info_by_team = @game_teams.group_by(&:team_id)
+    info_by_team = @game_teams.group_by(&:team_name)
     team_goals_average = {}
     info_by_team.map do |team, games|  
       team_goals_average[team] = average_of_goals(games)
     end
     da_worst_of_all = team_goals_average.min_by{|_, values| values}
-    convert_to_team_name(da_worst_of_all[0])
+    da_worst_of_all[0]
   end
 
   def highest_scoring_visitor
-    away_teams = @games.group_by(&:away_team_id)
+    away_teams = @games.group_by(&:away_team_name)
     away_teams_ave_score = {}
     away_teams.each do |team, values|
       away_teams_ave_score[team] = average_away_goals(values)
     end
     highest_scoring = away_teams_ave_score.max_by {|_, value| value}
-    convert_to_team_name(highest_scoring[0])
+    highest_scoring[0]
   end
 
   def highest_scoring_home_team
-    home_teams = @games.group_by(&:home_team_id)
+    home_teams = @games.group_by(&:home_team_name)
     home_teams_avg = {}
     home_teams.each do |team, values|
       home_teams_avg[team] = average_home_goals(values)
     end
     highest_scoring = home_teams_avg.max_by{|_, value| value}
-    convert_to_team_name(highest_scoring[0])
+    highest_scoring[0]
   end
 
   def lowest_scoring_visitor
-    away_teams = @games.group_by(&:away_team_id)
+    away_teams = @games.group_by(&:away_team_name)
     away_teams_ave_score = {}
     away_teams.each do |team, values|
       away_teams_ave_score[team] = average_away_goals(values)
     end
     lowest_scoring = away_teams_ave_score.min_by {|_, value| value}
-    convert_to_team_name(lowest_scoring[0])
+    lowest_scoring[0]
   end
 
   def lowest_scoring_home_team
-    home_teams = @games.group_by(&:home_team_id)
+    home_teams = @games.group_by(&:home_team_name)
     home_teams_avg = {}
     home_teams.each do |team, values|
       home_teams_avg[team] = average_home_goals(values)
     end
     lowest_scoring = home_teams_avg.min_by{|_, value| value}
-    convert_to_team_name(lowest_scoring[0])
+    lowest_scoring[0]
   end
 end
