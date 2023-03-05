a9e63373eb8779638437b3c0a1c4eb6fd0ba94f0
Amy Spears <amspears007@gmail.com>
2023-03-05 16:43:47 -0700

    Added game_stats class and spec and moved methods over from stat_tracker

--- lib/game_stats.rb
+++ lib/game_stats.rb
@@ -2,13 +2,18 @@ require_relative 'stat_data'
 
 class GameStats < StatData
 
-  def self.highest_total_score
+  def initialize(locations)
+    super(locations)
+  end
+
+  def highest_total_score
+    # super(all_games)
    all_games.map do |game|
       game.total_score
     end.max
   end
 
-  def self.lowest_total_score
+  def lowest_total_score
     all_games.map do |game|
       game.total_score
     end.min
---
+++ lib/objectable_spec.rb
@@ -0,0 +1,3 @@
+module Objectable
+  
+end
