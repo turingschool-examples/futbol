f53e18d77a4c6546bccd37f2f3eb0fa8c15cc2e3
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-03-04 18:35:17 -0700

    make our rspec file run in under 3 seconds.

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -2,45 +2,24 @@ require_relative 'spec_helper'
 require './lib/stat_tracker'
 
 RSpec.describe StatTracker do
-  before(:each) do 
+  before(:all) do
     game_path = './data/games.csv'
     team_path = './data/teams.csv'
     game_teams_path = './data/game_teams.csv'
-    @locations = {
+
+    locations = {
       games: game_path,
       teams: team_path,
       game_teams: game_teams_path
     }
-    @stat_tracker = StatTracker.from_csv(@locations)
+
+    @stat_tracker = StatTracker.from_csv(locations)
   end
   
   describe 'initialize' do
     it 'exists' do
       expect(@stat_tracker).is_a?(StatisticsGenerator)
       expect(@stat_tracker).to be_an_instance_of StatTracker
-    end
-    
-    it 'processed team data, retrieves data from teams' do
-      expect(@stat_tracker.processed_teams_data(@locations)).to all(be_a(Team))
-    end
-
-    it 'processed team data, retrieves data from teams' do
-      expect(@stat_tracker.processed_games_data(@locations)).to all(be_a(Game))
-    end
-
-    it 'processed season data, creates hash of season info' do
-      expect(@stat_tracker.seasons_by_id).to be_a(Hash)
-      expect(@stat_tracker.seasons_by_id["20122013"][:games]).to all(be_a(Game))
-      expect(@stat_tracker.seasons_by_id["20122013"][:game_teams]).to all(be_a(GameTeam))
-    end
-
-    it 'can parse data into a string of objects' do
-      expect(@stat_tracker.games).to be_a(Array)
-      expect(@stat_tracker.games).to all(be_a(Game))
-    end
-
-    it 'processed team data, retrieves data from teams' do
-      expect(@stat_tracker.processed_game_teams_data(@locations)).to all(be_a(GameTeam))
     end
   end
 
