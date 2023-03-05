840377465e24965df90aa6155b968fceb3fc0836
Amy Spears <amspears007@gmail.com>
2023-03-05 13:39:11 -0700

    delete unneccesary files and specs

--- lib/season.rb
+++
@@ -1,9 +0,0 @@
-class Season
-  attr_reader :years, :type, :games
-
-  def initialize(season_details)
-    @years = season_details[:season]
-    @type = season_details[:type]
-    @games = []
-  end
-end
