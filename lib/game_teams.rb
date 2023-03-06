0e43ed2503984e4a62979715dc7b7d227747e7bb
Andrew Bowman <andrewtbowman@gmail.com>
2023-03-06 00:48:20 -0500

    feat: most/fewest goals

--- lib/game_teams.rb
+++ lib/game_teams.rb
@@ -45,9 +45,26 @@ class GameTeams < StatBook
     win_loss_checker(team)[:worst_szn]
   end
 
+  def average_win_percentage(team)
+    wins, games = 0, 0
+    win_loss_counter[team].each do |_, szn|
+      wins += szn[:wins]
+      games += szn[:games]
+    end
+    wins.fdiv(games).round(2)
+  end
+
+  def most_goals_scored(team)
+    goals_checker(team)[:most_goals]
+  end
+
+  def fewest_goals_scored(team)
+    goals_checker(team)[:least_goals]
+  end
+
   def winningest_coach(season)
     coaches = super_nested_hash
-    (0..@team_id.count).each do |i|
+    (0..@game_id.count).each do |i|
       coaches[@head_coach[i]][@game_id[i]&.slice(0..3)][:wins] += 1 if @result[i] == 'WIN'
       coaches[@head_coach[i]][@game_id[i]&.slice(0..3)][:games] += 1
     end
@@ -65,7 +82,7 @@ class GameTeams < StatBook
 
   def worst_coach(season)
     coaches = super_nested_hash
-    (0..@team_id.count).each do |i|
+    (0..@game_id.count).each do |i|
       coaches[@head_coach[i]][@game_id[i]&.slice(0..3)][:wins] += 1 if @result[i] == 'WIN'
       coaches[@head_coach[i]][@game_id[i]&.slice(0..3)][:games] += 1
     end
@@ -83,7 +100,7 @@ class GameTeams < StatBook
 
   def least_accurate_team(season)
     teams = super_nested_hash
-    (0..@team_id.count).each do |i|
+    (0..@game_id.count).each do |i|
       teams[@team_id[i]][@game_id[i]&.slice(0..3)][:shots] += @shots[i].to_i
       teams[@team_id[i]][@game_id[i]&.slice(0..3)][:goals] += @goals[i].to_i
     end
@@ -101,7 +118,7 @@ class GameTeams < StatBook
 
   def most_accurate_team(season)
     teams = super_nested_hash
-    (0..@team_id.count).each do |i|
+    (0..@game_id.count).each do |i|
       teams[@team_id[i]][@game_id[i]&.slice(0..3)][:shots] += @shots[i].to_i
       teams[@team_id[i]][@game_id[i]&.slice(0..3)][:goals] += @goals[i].to_i
     end
@@ -128,7 +145,7 @@ class GameTeams < StatBook
   end
 
   def tackle_counter(hash)
-    (0..@team_id.count).each do |i|
+    (0..@game_id.count).each do |i|
       hash[@team_id[i]][@game_id[i]&.slice(0..3)] += @tackles[i].to_i
     end
     hash
@@ -154,7 +171,7 @@ class GameTeams < StatBook
 
   def goals_counter
     goals = nested_hash
-    (0..@team_id.count).each do |i|
+    (0..@game_id.count).each do |i|
       goals[@team_id[i]][:goals] += @goals[i].to_i
       goals[@team_id[i]][:away] += @goals[i].to_i if @hoa[i] == 'away'
       goals[@team_id[i]][:home] += @goals[i].to_i if @hoa[i] == 'home'
@@ -166,7 +183,7 @@ class GameTeams < StatBook
 
   def win_loss_counter
     games = super_nested_hash
-    (0..@team_id.count).each do |i|
+    (0..@game_id.count).each do |i|
       games[@team_id[i]][@game_id[i]&.slice(0..3)][:wins] += 1 if @result[i] == 'WIN'
       games[@team_id[i]][@game_id[i]&.slice(0..3)][:losses] += 1 if @result[i] == 'LOSS'
       games[@team_id[i]][@game_id[i]&.slice(0..3)][:games] += 1
@@ -190,5 +207,20 @@ class GameTeams < StatBook
     end
     {:best_szn => "#{best_szn}#{(best_szn.to_i + 1).to_s}", 
     :worst_szn => "#{worst_szn}#{(worst_szn.to_i + 1).to_s}"}
+  end
+
+  def goals_checker(team)
+    most_goals, least_goals = 0, 10
+    (0..@game_id.count).each do |i|
+      if @team_id[i] == team 
+        goals = @goals[i].to_i
+        if goals > most_goals
+          most_goals = goals
+        elsif goals < least_goals
+          least_goals = goals
+        end
+      end
+    end
+    {:most_goals => most_goals, :least_goals => least_goals}
   end
 end
