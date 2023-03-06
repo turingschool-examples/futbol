dcbd2420742f447869d7b5cfa8a588567673723b
DysonBreakstone <121531791+DysonBreakstone@users.noreply.github.com>
2023-03-05 19:02:57 -0700

    add test for TeamSeasonEvaluator module

---
+++ spec/team_season_evaluator_spec.rb
@@ -0,0 +1,21 @@
+require team_season_evaluator
+
+RSpec.describe TeamSeasonEvaluator do
+  before(:each) do 
+      game_path = './data/games.csv'
+      team_path = './data/teams.csv'
+      game_teams_path = './data/game_teams.csv'
+  
+      locations = {
+        games: game_path,
+        teams: team_path,
+        game_teams: game_teams_path
+      }
+  
+      @stat_tracker = StatTracker.from_csv(locations)
+  end
+
+  it "delivers the same result as the non-modularized version of #best_season" do
+    expect(@stat_tracker.best_season("6")).to eq "20132014"
+  end
+end
