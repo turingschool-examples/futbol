7418b2d9685fd9c153d280a3bda912c794bdfafe
Andrew Bowman <andrewtbowman@gmail.com>
2023-03-06 00:09:50 -0500

    feat: best/worst seasons

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -94,6 +94,54 @@ class StatTracker
     get_teamname(@game_teams.least_tackles(season))
   end
 
+  def team_info(team)
+    @teams.team_info(team)
+  end
+
+  def best_season(team)
+    @game_teams.best_season(team)
+  end
+
+  def worst_season(team)
+    @game_teams.worst_season(team)
+  end
+
+  def average_win_percentage(team)
+
+  end
+
+  def most_goals_scored(team)
+
+  end
+
+  def fewest_goals_scored(team)
+
+  end
+
+  def favorite_opponent(team)
+
+  end
+
+  def rival(team)
+
+  end
+
+  def biggest_team_blowout(team)
+
+  end
+
+  def worst_loss(team)
+
+  end
+
+  def head_to_head(team)
+
+  end
+
+  def seasonal_summary(team)
+
+  end
+
   # Helper method
 
   def get_teamname(method)
@@ -101,3 +149,29 @@ class StatTracker
     @teams.teamname[index]
   end
 end
+
+# Method	Description	Return Value
+# team_info	
+    # A hash with key/value pairs for the following attributes: team_id, franchise_id, team_name, abbreviation, and link	Hash
+# best_season	
+    # Season with the highest win percentage for a team.	String
+# worst_season	
+    # Season with the lowest win percentage for a team.	String
+# average_win_percentage	
+    # Average win percentage of all games for a team.	Float
+# most_goals_scored	
+    # Highest number of goals a particular team has scored in a single game.	Integer
+# fewest_goals_scored	
+    # Lowest number of goals a particular team has scored in a single game.	Integer
+# favorite_opponent	
+    # Name of the opponent that has the lowest win percentage against the given team.	String
+# rival	
+    # Name of the opponent that has the highest win percentage against the given team.	String
+# biggest_team_blowout	
+    # Biggest difference between team goals and opponent goals for a win for the given team.	Integer
+# worst_loss	
+    # Biggest difference between team goals and opponent goals for a loss for the given team.	Integer
+# head_to_head	
+    # Record (as a hash - win/loss) against all opponents with the opponents’ names as keys and the win percentage against that opponent as a value.	Hash
+# seasonal_summary	
+    # For each season that the team has played, a hash that has two keys (:regular_season and :postseason), that each point to a hash with the following keys: :win_percentage, :total_goals_scored, :total_goals_against, :average_goals_scored, :average_goals_against.	Hash
