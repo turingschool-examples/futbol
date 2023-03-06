81f8a45a162924305188d4a067423fa2ecc63f8a
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-03-06 10:42:04 -0700

    refactor coach_wins method

--- lib/coach.rb
+++ lib/coach.rb
@@ -2,24 +2,16 @@ module Coach
   
   def coach_wins(season_id)
     season_games = @seasons_by_id[season_id][:game_teams]
-    games_won_coach = Hash.new(0)
-    games_played_coach = Hash.new(0)
+    coach_stats = Hash.new {|coach, games| coach[games] = [0, 0]}
     coach_win_percentage = Hash.new
     season_games.each do |game|
-      games_played_coach[game.head_coach] += 1
+      coach_stats[game.head_coach][0] += 1
       if game.result == "WIN"
-      games_won_coach[game.head_coach] += 1
+      coach_stats[game.head_coach][1] += 1
       end
     end
-    games_played_coach.each do |coach, games|
-      games_won_coach.each do |won_coach, won_games|
-        if coach == won_coach
-          coach_win_percentage[coach] = (won_games / games.to_f)
-        end
-        if !games_won_coach.include?(coach)
-          coach_win_percentage[coach] = 0
-        end
-      end
+    coach_stats.each do |coach, stats|
+     coach_win_percentage[coach] = stats[1] / stats[0].to_f
     end
     coach_win_percentage
   end
