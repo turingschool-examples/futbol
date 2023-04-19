61f40b5429c408ba10655808ea5b4bcd18e4f65c
Myles Nottingham <MylesNottingham@gmail.com>
2023-04-19 12:59:09 -0600

    Refactor: Update StatTracker class to open games.

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,3 +1,17 @@
-class StatTracker
+require "csv"
+require_relative './game'
 
+class StatTracker
+  attr_reader :games
+
+  def self.from_csv(files)
+    StatTracker.new(files)
+  end
+
+  def initialize(files)
+    @games = (CSV.open files[:games], headers: true, header_converters: :symbol).map do |row|
+      game = Game.new(row)
+    end
+    require 'pry'; binding.pry
+  end
 end
