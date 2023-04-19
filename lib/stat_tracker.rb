826b81de8ef9ea1e81dd73bf9930bd79a0acbcc8
Jimmy Jurado <jurado140@gmail.com>
2023-04-19 14:23:31 -0400

    Feat: Adds runner file with starter code, also adds StatTracker Class lib file. Still need to pull the data from CSV files. Pending.

---
+++ lib/stat_tracker.rb
@@ -0,0 +1,21 @@
+require "csv"
+
+class StatTracker
+
+  def initialize(locations)
+
+  end
+
+  def self.from_csv(locations)
+require 'pry'; binding.pry
+  end
+
+
+  # def from_csv(locations)
+  #   {
+  #     locations[:games]: {game_path},
+  #     teams: {team_path},
+  #     game_teams: {game_teams_path}
+  #   }
+  # end
+end
