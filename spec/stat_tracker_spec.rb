9d5569d527b4f9c2990c72dc932502b314a4b25b
Steven Stackhouse <stackmm@gmail.com>
2023-04-19 13:45:17 -0600

    add initialize test

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -12,12 +12,11 @@ RSpec.describe StatTracker do
       game_teams: game_teams_path
     }
     @stat_tracker = StatTracker.from_csv(@locations)
-    @stat_tracker1 =StatTracker.new
   end
+
   describe "initialize" do
     it "exists" do
-      expect(@stat_tracker1).to be_a(StatTracker)
-      expect(@stat_tracker1.read_csv(@locations)).to be_a(StatTracker)
+      expect(@stat_tracker).to be_a(StatTracker)
     end
   end
 
