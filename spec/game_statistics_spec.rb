f6e5a6fddeec22cc408eee44e64727b403f4ede3
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-04-19 07:47:01 -0600

    Feat: add describe and it blocks for tests.

---
+++ spec/game_statistics_spec.rb
@@ -0,0 +1,63 @@
+
+
+
+RSpec.describe StatTracker do 
+  
+  describe "#highest_total_score" do
+    it " finds the score of the highest scoring game" do 
+
+      # return value integer
+    end
+  end
+
+  describe "#lowest_total_score" do
+    it "  finds the score of the lowest scoring game" do 
+
+      # return value integer
+    end
+  end
+
+  describe "#percentage_home_wins" do
+    it " calculates the percentage of games that a home team has won (rounded to the nearest 100th)" do 
+
+      # return value float
+    end
+  end
+
+  describe "#percentage_visitor_wins" do
+    it " calculates the percentage of games that a visitor has won (rounded to the nearest 100th)" do 
+
+      # return value float
+    end
+  end
+
+  describe "#percentage_ties" do
+    it " calculates the percentage of games that have resulted in a tie (rounded to the nearest 100th)" do 
+
+      # return value float
+    end
+  end
+
+  describe "#count_of_games_by_season" do
+    it " lists each season with the amount of games played for each season" do 
+
+      # return value hash
+    end
+  end
+
+  describe "#average_goals_per_game" do
+    it " calculates the average number of goals scored in a game across all seasons including both home and away goals (rounded to the nearest 100th)" do 
+
+      # return value float
+    end
+  end
+
+  describe "#average_goals_by_season" do
+    it " lists each season with the average number of goals in a game for that season (rounded to the nearest 100th)" do 
+
+      # return value hash
+    end
+  end
+
+
+end
---
+++ spec/league_statistics_spec.rb
@@ -0,0 +1,56 @@
+
+
+
+RSpec.describe StatTracker do 
+  
+  describe "#count_of_teams" do
+    it " counts the total number of teams in the data" do 
+
+      # return value integer
+    end
+  end
+
+  describe "#best_offense" do
+    it " names the team with the highest average number of goals scored per game across all seasons" do 
+
+      # return value string
+    end
+  end
+
+  describe "#worst_offense" do
+    it " names the team with the lowest average number of goals scored per game across all seasons" do 
+
+      # return value string
+    end
+  end
+
+  describe "#highest_scoring_visitor" do
+    it " names the team with the highest average score per game across all seasons when they are away" do 
+
+      # return value string
+    end
+  end
+
+  describe "#highest_scoring_home_team" do
+    it " names the team with the highest average score per game across all seasons when they are home" do 
+
+      # return value string
+    end
+  end
+
+  describe "#lowest_scoring_visitor" do
+    it " names the team with the lowest average score per game across all seasons when they are away" do 
+
+      # return value string
+    end
+  end
+
+  describe "#lowest_scoring_home_team" do
+    it " names the team with the lowest average score per game across all seasons when they are home" do 
+
+      # return value string
+    end
+  end
+
+
+end
