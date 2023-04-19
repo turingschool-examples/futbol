4c5ced8a5bd111fb49d9b64c86d4999dd912ebc9
K.D. Hubbard <hubbard.kd@gmail.com>
2023-04-19 12:06:51 -0600

    wip

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -11,7 +11,6 @@ class StatTracker
       (CSV.open location, headers: true, header_converters: :symbol)
     end
     # @teams << all_open_csvs[1]
-    require 'pry'; binding.pry
     # @games << all_open_csvs[0]
     # @games << all_open_csvs[2]
   end
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -2,7 +2,6 @@ require "./spec/spec_helper"
 
 RSpec.describe StatTracker do 
   before(:each) do 
-    @stat_tracker = StatTracker.new
     @game_path = './data/games.csv'
     @team_path = './data/teams.csv'
     @game_teams_path = './data/game_teams.csv'
@@ -11,12 +10,14 @@ RSpec.describe StatTracker do
       teams: @team_path,
       game_teams: @game_teams_path
     }
+    @stat_tracker = StatTracker.from_csv(@locations)
   end
 
   describe "#initialize" do 
     it "can initialize with attributes" do 
+      require 'pry'; binding.pry
       expect(@stat_tracker).to be_a(StatTracker)
-      @stat_tracker = StatTracker.from_csv(@locations)
+      
     end
   end
 end
