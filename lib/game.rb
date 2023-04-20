45d805b5e6bc07a8b96dd0fce488c9dcc91e258f
Nick Tassinari <ntass7800@gmail.com>
2023-04-19 19:50:16 -0600

    fixes the variables to integers

--- lib/game.rb
+++ lib/game.rb
@@ -7,11 +7,11 @@ class Game
               :home_goals
 
   def initialize(stats)
-    @game_id = stats[:game_id].to_s
-    @season = stats[:season].to_s
-    @away_team_id = stats[:away_team_id].to_s
-    @home_team_id = stats[:home_team_id].to_s
-    @away_goals = stats[:away_goals]
-    @home_goals = stats[:home_goals]
+    @game_id = stats[:game_id]
+    @season = stats[:season]
+    @away_team_id = stats[:away_team_id]
+    @home_team_id = stats[:home_team_id]
+    @away_goals = stats[:away_goals].to_i
+    @home_goals = stats[:home_goals].to_i
   end
 end
