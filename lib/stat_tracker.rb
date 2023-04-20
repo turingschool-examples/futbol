7bfe9f49e899b5ebe3833491392b6dab34094a6e
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-19 21:53:16 -0400

    add example to stat tracker class

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,13 +1,17 @@
 require "csv"
 
 class StatTracker
-  attr_reader :game_stats
+  # attr_reader :game_stats
   
   def self.from_csv(files)
     StatTracker.new
   end
 
-  def initialize(files)
-    @game_stats = GameStats.new(files)
-  end
+  # def initialize(files)
+  #   @game_stats = GameStatistics.new(files)
+  # end
+
+  #def method from game_statistics class
+      #method ex: @games_stats.highest_score
+  #end
 end
