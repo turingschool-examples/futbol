b1027ede5d6e6c39991935de5567daa4764a7da0
Jesse Thomas <jgthomas3@gmail.com>
2023-04-20 15:56:23 -0600

    Test: delete comments and clean up.

--- runner.rb
+++ runner.rb
@@ -11,7 +11,10 @@ locations = {
   teams: team_path,
   game_teams: game_teams_path
 }
-  # CSV.readlines(game_path, headers:true, header_converters: :symbol).map do |row|
-  # require 'pry'; binding.pry
-  # end
+
 stat_tracker = StatTracker.from_csv(locations)
+# require 'pry'; binding.pry
+# in spec file?:
+# expect stat_tracker to be an array
+# expect stat_tracker[0] to be a Game
+stat_tracker.highest_total_score
