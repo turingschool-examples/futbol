84fdd308e89366ee0df527ebff325ecb7ac6efd6
Jeff Nelson <jpnelson85@gmail.com>
2023-04-19 19:58:38 -0500

    add team spec test

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -2,9 +2,13 @@ require 'spec_helper'
 
 RSpec.describe Team do
   it 'exists' do
-    team = Team.new
+    team = Team.new(team)
     expect(team).to be_a(Team)
   end
 
-  
+  it 'has attributes' do
+    team = Team.new(team)
+    expect(team.team_id).to eq()
+    expect(team.name).to eq()
+  end
 end
