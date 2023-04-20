b0bd1d87fe443e6315448ebbf85a15c0e5880d06
Lauren Golden <llgolden@gmail.com>
2023-04-19 22:01:01 -0600

    Update stat tracker methods

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,7 +1,43 @@
 require_relative "csv"
 
 class StatTracker
+  def initialize(files)
+    @game_stats = GameStatistics.new(files)
+   end
+
   def self.from_csv(csv)
     new_stat = StatTracker.new
+  end
+
+  def highest_total_score
+    @game_stats.highest_total_score
+  end
+
+  def lowest_total_score
+    @game_stats.lowest_total_score
+  end
+
+  def percentage_home_wins
+    @game_stats.percentage_home_wins
+  end
+
+  def percentage_visitor_wins
+    @game_stats.percentage_visitor_wins
+  end
+
+  def percentage_ties
+    @game_stats.percentage_ties
+  end
+
+  def count_of_games_by_season
+    @game_stats.count_of_games_by_season
+  end
+
+  def average_goals_per_game
+    @game_stats.average_goals_per_game
+  end
+
+  def average_goals_by_season
+    @game_stats.average_goals_by_season
   end
 end
