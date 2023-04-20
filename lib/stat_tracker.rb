92783ef71fde770a2823e4b4306ad6b44bc33851
Ashley Plasket <ashley.plasket@gmail.com>
2023-04-19 20:21:02 -0400

    add spec for stat_tracker to run tests, and write initialize statement for stat_tracker

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,7 +1,18 @@
 require "csv"
+require "spec_helper"
 
 class StatTracker
-  def self.from_csv(csv)
+  attr_reader :game_stats,
+              :league_stats
+              :team_stats
+  
+  def self.from_csv(files)
     new_stat = StatTracker.new
+  end
+
+  def initialize(files)
+    @game_stats = GameStats.new(files)
+    @league_stats = LeagueStats.new(files)
+    @team_stats = SeasonStats.new(files)
   end
 end
