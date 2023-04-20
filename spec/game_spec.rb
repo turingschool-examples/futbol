cedd9f818efc27afb683f37619c48e48fa00238c
Justin Pasko <jpasko1112@gmail.com>
2023-04-19 21:53:04 -0400

    Add initialize tests.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,12 +1,20 @@
 require 'spec_helper'
 
-RSpec.describe  do
+RSpec.describe Game do
   before(:each) do
-
+    @game = Futbol.new(CSV.open './spec/fixtures/games.csv', headers: true, header_converters: :symbol)
+    @game = Futbol.new(CSV.open './spec/fixtures/game_teams.csv', headers: true, header_converters: :symbol)
+    @game_1 = Game.new(id: )
   end
 
-  describe '' do
-    it '' do
+  describe '#initialize' do
+    it 'can initialize with attributes' do
+      expect(@game).to be_a(Game)
+      expect(@game.id).to eq("")
+      expect(@game.home_team_id).to eq("")
+      expect(@game.away_team_id).to eq("")
+      expect(@game.home_team_goals).to eq("")
+      expect(@game.away_team_goals).to eq("")
     end
   end
 end
