a1cf3775854b5a005bc1da65607003b7a1e672ec
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-04-20 16:31:11 -0600

    Test: #games, #game_teams, #teams

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -16,9 +16,22 @@ RSpec.describe StatTracker do
   end
   
   describe '#initialize' do 
-    it 'exists and has readable attributes' do 
-
+    it 'exists' do 
       expect(@stat_tracker).to be_a(StatTracker)
+    end
+
+    it 'has an array of games' do 
+      expect(@stat_tracker.games).to be_a Array
+      expect(@stat_tracker.games[0]).to be_a Game
+    end
+    
+    it 'has an array of gameteams' do    
+      expect(@stat_tracker.game_teams).to be_a Array
+      expect(@stat_tracker.game_teams[0]).to be_a GameTeam
+    end
+    it 'has an array of teams' do 
+      expect(@stat_tracker.teams).to be_a Array
+      expect(@stat_tracker.teams[0]).to be_a Team
     end
   end
 end
