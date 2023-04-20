19f099f4308b5fd3540fa72906cd061ff9f37789
Jeff Nelson <jpnelson85@gmail.com>
2023-04-20 16:32:20 -0500

    update team lib file

--- lib/team.rb
+++ lib/team.rb
@@ -2,12 +2,6 @@ class Team
   attr_reader :team_id,
               :name
 
-  contents = CSV.open "teams.csv", headers: true, header_converters: :symbol
-  contents.each do |row|
-    team_id = row[:team_id]
-    name = row[:name]
-  end
-
   def initialize(team)
     @team_id = team[:team_id]
     @name = team[:teamname]
