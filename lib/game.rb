fef01a43a067cd71d60bf5c2f4d5f9e4927bcfb2
Lauren Golden <llgolden@gmail.com>
2023-04-19 21:53:17 -0600

    Update game initialize

--- lib/game.rb
+++ lib/game.rb
@@ -1,1 +1,18 @@
 # use require_relative
+class Game 
+initialize games.csv
+row, example:
+
+  def initialize(row_info)
+    @game_id = row_info[:game_id]
+    @season = row_info[:season]
+    @type = row_info[:type]
+    @date_time = row_info[:date_time]
+    @away_team_id = row_info[:away_team_id]
+    @home_team_id = row_info[:home_team_id]
+    @away_goals = row_info[:away_goals]
+    @home_goals = row_info[:home_goals]
+    @venue = row_info[:venue]
+    @venue_link = row_info[:venue_link]
+  end
+end
--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -1,1 +1,39 @@
 # use require_relative
+class GameStatistics
+
+  def initialize(files)
+    @games = CSV.open(<file> headers: true, header_converters: :symbol)
+  end
+
+  def highest_total_score
+    method
+  end
+
+  def lowest_total_score
+    method
+  end
+
+  def percentage_home_wins
+    @games. (method for calculating this)
+  end
+
+  def percentage_visitor_wins
+    method
+  end
+
+  def percentage_ties
+    method
+  end
+
+  def count_of_games_by_season
+    method
+  end
+
+  def average_goals_per_game
+    method
+  end
+
+  def average_goals_by_season
+    method
+  end
+end
