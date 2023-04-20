d63ab433174a589ed9dd868fc3fe6ed97fc2fe84
Steven Stackhouse <stackmm@gmail.com>
2023-04-19 21:22:24 -0600

    add highest and lowest_total_score methods

--- lib/game.rb
+++ lib/game.rb
@@ -4,5 +4,17 @@ class Game
   def initialize(data)
     @game_data = data
   end
+
+  def highest_total_score
+    @game_data.map do |row|
+      row[:away_goals].to_i + row[:home_goals].to_i
+    end.max
+  end
+
+  def lowest_total_score
+    @game_data.map do |row|
+      row[:away_goals].to_i + row[:home_goals].to_i
+    end.min
+  end
   
 end
