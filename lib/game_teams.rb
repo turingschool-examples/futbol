cd0af26d9e7a2057ff42b44c62c88eff7457d418
William Zale <williamzale@gmail.com>
2023-04-20 22:51:40 -0400

    Feat: Add GameTeams class.

---
+++ lib/game_teams.rb
@@ -0,0 +1,19 @@
+class GameTeams
+  attr_reader :game_id,
+              :team_id,
+              :hoa,
+              :result,
+              :head_coach,
+              :goals,
+              :tackles
+              
+  def initialize(game_id, team_id, hoa, result, head_coach, goals, tackles)
+    @game_id = game_id
+    @team_id = team_id
+    @hoa = hoa
+    @result = result
+    @head_coach = head_coach
+    @goals = goals
+    @tackles = tackles
+  end
+end
