a81a49489ce270cbb9906af2f704c32e26a38652
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-04-21 13:21:20 -0600

    Fix: edit data types for attributes.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,12 +4,12 @@ require "spec_helper"
 RSpec.describe Game do
   let(:game) {Game.new(row)}
   let(:row) do {
-      game_id: 2012030221,
-      season: 20122013,
+      game_id: "2012030221",
+      season: "20122013",
       type: "Postseason",
       date_time: "5/16/13",
-      away_team_id: 3,
-      home_team_id: 6,
+      away_team_id: "3",
+      home_team_id: "6",
       away_goals: 2,
       home_goals: 3,
       venue: "Toyota Stadium",
@@ -23,11 +23,11 @@ RSpec.describe Game do
     end
     
     it " has attributes" do
-      expect(game.game_id).to eq(2012030221)
-      expect(game.game_id).to be_a(Integer)
+      expect(game.game_id).to eq("2012030221")
+      expect(game.game_id).to be_a(String)
 
-      expect(game.season).to eq(20122013)
-      expect(game.season).to be_a(Integer)
+      expect(game.season).to eq("20122013")
+      expect(game.season).to be_a(String)
 
       expect(game.type).to eq("Postseason")
       expect(game.type).to be_a(String)
@@ -35,11 +35,11 @@ RSpec.describe Game do
       expect(game.date_time).to eq("5/16/13")
       expect(game.date_time).to be_a(String)
 
-      expect(game.away_team_id).to eq(3)
-      expect(game.away_team_id).to be_a(Integer)
+      expect(game.away_team_id).to eq("3")
+      expect(game.away_team_id).to be_a(String)
 
-      expect(game.home_team_id).to eq(6)
-      expect(game.home_team_id).to be_a(Integer)
+      expect(game.home_team_id).to eq("6")
+      expect(game.home_team_id).to be_a(String)
 
       expect(game.away_goals).to eq(2)
       expect(game.away_goals).to be_a(Integer)
