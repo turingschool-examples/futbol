8f39a12cdc2eaf2bf12f60fbbc178bd4b991d8b2
Justin Pasko <jpasko1112@gmail.com>
2023-04-21 12:28:21 -0400

    Fix syntax, spelling and 'highest_score' test.

--- spec/games_stats_spec.rb
+++ spec/games_stats_spec.rb
@@ -2,18 +2,19 @@ require 'spec_helper'
 
 RSpec.describe GamesStats do
   before(:each) do
-    game_path = './data/games.csv'
-    team_path = './data/teams.csv'
-    game_teams_path = './data/game_teams.csv'
+    @game_path = './data/games.csv'
+    @team_path = './data/teams.csv'
+    @game_teams_path = './data/game_teams.csv'
 
-    locations = {
-    games: game_path,
-    teams: team_path,
-    game_teams: game_teams_path
+    @locations = {
+    games: @game_path,
+    teams: @team_path,
+    game_teams: @game_teams_path
     }
 
-    @games_stats = GamesStats.new(locations)
+    @games_stats = GamesStats.new(@locations)
     @games_stats.merge_game_game_teams
+    # @games_stats.merge_team_to_game_game_teams
   end
 
   describe '#initialize' do
@@ -24,6 +25,8 @@ RSpec.describe GamesStats do
 
   # highest sum of winning and losing teams score
   describe '#highest_total_score' do
-    expect(@games_stats.highest_total_score).to be_a(String)
+    it 'checks highest total score of a game' do
+    expect(@games_stats.highest_total_score).to eq(11)
+    end
   end
 end
