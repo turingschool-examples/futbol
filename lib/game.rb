06e7b2ddbb06b3d4b7791302e273e0fc8a9a0152
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-04-21 10:33:22 -0600

    Fix: removed to_i from @game_id & @season.

--- lib/game.rb
+++ lib/game.rb
@@ -14,8 +14,8 @@ attr_reader :game_id,
             :venue_link
 
   def initialize(row)
-    @game_id = row[:game_id].to_i
-    @season = row[:season].to_i
+    @game_id = row[:game_id]
+    @season = row[:season]
     @type = row[:type]
     @date_time = row[:date_time]
     @away_team_id = row[:away_team_id].to_i
