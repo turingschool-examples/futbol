8e48b4ef4a9ca055bafc182e8c883b08c313593a
Jesse Thomas <jgthomas3@gmail.com>
2023-04-21 11:54:23 -0600

    Refactor: Adjust GameTeams initialize.

--- lib/game_teams.rb
+++ lib/game_teams.rb
@@ -7,13 +7,13 @@ class GameTeams
               :goals,
               :tackles
 
-  def initialize(game_id, team_id, hoa, result, head_coach, goals, tackles)
-    @game_id = game_id
-    @team_id = team_id
-    @hoa = hoa
-    @result = result
-    @head_coach = head_coach
-    @goals = goals.to_i
-    @tackles = tackles
+  def initialize(row)
+    @game_id = row[:game_id]
+    @team_id = row[:team_id]
+    @hoa = row[:hoa]
+    @result = row[:result]
+    @head_coach = row[:head_coach]
+    @goals = row[:goals].to_i
+    @tackles = row[:tackles].to_i
   end
 end
