e543cbc4d4dd036c065917fd4bd14aec5ad53510
Jesse Sorman <jessesorman@gmail.com>
2023-04-21 16:27:43 -0400

    feat most and least accurate teams

--- lib/season.rb
+++ lib/season.rb
@@ -4,5 +4,32 @@ class Season
   def initialize(data)
     @season_data = data
   end
+
+  def team_accuracy
+    team_goal_accuracy = Hash.new
+    @season_data.each do |game|
+      accuracy = game[:goals].to_i / game[:shots].to_i.to_f
+      team_id = game[:team_id]
+      team_goal_accuracy[team_id] = accuracy
+    end
+    team_goal_accuracy
+  end
   
+  def most_accurate_team
+    teams = CSV.read('./data_dummy/teams_dummy.csv', headers: true, header_converters: :symbol)
+    teams.each do |team|
+      if team[:team_id] == team_accuracy.key(team_accuracy.values.max)
+        return name = team[:teamname]
+      end
+    end
+  end
+  
+  def least_accurate_team
+    teams = CSV.read('./data_dummy/teams_dummy.csv', headers: true, header_converters: :symbol)
+    teams.each do |team|
+      if team[:team_id] == team_accuracy.key(team_accuracy.values.min)
+        return name = team[:teamname]
+      end
+    end
+  end
 end
--- spec/season_spec.rb
+++ spec/season_spec.rb
@@ -4,20 +4,27 @@ RSpec.describe Season do
   before(:each) do
     game_path = './data_dummy/games_dummy.csv'
     games_data = CSV.read(game_path, headers: true, header_converters: :symbol)
-    @season = League.new(games_data)
+    @season = Season.new(games_data)
 
     game_teams_path = './data_dummy/game_teams_dummy.csv'
     games_teams_data = CSV.read(game_teams_path, headers: true, header_converters: :symbol)
-    @season2 = League.new(games_teams_data)
+    @season2 = Season.new(games_teams_data)
 
     teams_path = './data_dummy/teams_dummy.csv'
     teams_data = CSV.read(teams_path, headers: true, header_converters: :symbol)
-    @season3 = League.new(teams_data)
+    @season3 = Season.new(teams_data)
   end
 
   describe "initialize" do
     it "exists" do
       expect(@season).to be_a(Season)
+    end
+  end
+
+  describe "team accuracy" do
+    it "returns most accurate team" do
+      expect(@season2.most_accurate_team).to eq("New England Revolution")
+      expect(@season2.least_accurate_team).to be_a("Sporting Kansas City")
     end
   end
 end
