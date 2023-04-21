1fd12473f0f1fdddf6c26fdae62e3ed9cc9d8f30
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-04-21 14:44:56 -0500

    Add methods to game_statistics.rb for game_teams.rb

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -20,22 +20,33 @@ class GameStatistics < StatHelper
   end
 
   def percentage_home_wins
-    games_played = 0 
-    home_games_won = 0
-    @game_teams.map do |game_team|
-      game_team.hoa
+    home_wins = 0
+    @game_teams.find_all do |row|
+      home_wins += 1 if (row[:hoa]) == "home" && row[:result] == "win" ||
+      (row[:hoa]) =="away" && row[:result] == "loss"
     end
+    (home_wins.to_f / @game_teams.count.to_f).round(2)
   end
 
-  # def percentage_visitor_wins
-    
-  # end
+  def percentage_visitor_wins
+    visitor_wins = 0
+    @game_teams.find_all do |row|
+      visitor_wins += 1 if (row[:hoa]) == "away" && row[:result] == "win" ||
+      (row[:hoa]) =="home" && row[:result] == "loss"
+    end
+    (visitor_wins.to_f / @game_teams.count.to_f).round(2)
+  end
 
-  # def percentage_ties
-  #   method
-  # end
+  def percentage_ties
+    no_lose = 0
+    @game_teams.find_all do |row|
+      no lose += 1 if (row[:hoa]) == "home" && row[:result] == "tie" ||
+      (row[:hoa]) =="away" && row[:result] == "tie"
+    end
+    (no_lose.to_f / @game_teams.count. to_f).round(2)
+  end
 
-  # def count_of_games_by_season
+    # def count_of_games_by_season
   #   method
   # end
 
