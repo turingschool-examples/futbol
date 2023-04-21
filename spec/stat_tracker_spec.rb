8c861f3d88bd6e9b7ba2659aa777088ebdbe256f
Bradley Milton <bmilton2412@gmail.com>
2023-04-20 20:56:23 -0700

    add tests for percentage_ties and count of games by season methods

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -61,5 +61,18 @@ RSpec.describe StatTracker do
       allow(@stat_tracker).to receive(:games) {@test_games}
       expect(@stat_tracker.percentage_visitor_wins).to eq(0.36)
     end
-  end   
+  end
+
+  describe '#percentage_ties method' do
+    it 'shows the percentage of ties per season' do 
+      expect(@stat_tracker.percentage_ties).to eq(0.2)
+    end
+  end
+
+  describe '#count_of_games_by_season method' do
+    it 'counts number of games played per season' do
+      hash = {"20122013"=>806, "20132014"=>1323, "20142015"=>1319, "20152016"=>1321, "20162017"=>1317, "20172018"=>1355}
+      expect(@stat_tracker.count_of_games_by_season).to eq(hash)
+    end
+  end
 end
