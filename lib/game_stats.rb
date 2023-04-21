6f9edc5ee96a6576c52bdef00f8f85b029d67554
Sarah Garlock <garlock.sarah10@gmail.com>
2023-04-20 20:14:56 -0600

    add highest/lowest score methods

--- lib/game_stats.rb
+++ lib/game_stats.rb
@@ -1,7 +1,11 @@
 module GameStats
   
   def highest_total_score
+    games.map { |game| game.home_goals + game.away_goals }.max
+  end
 
+  def lowest_total_score
+    games.map { |game| game.away_goals + game.home_goals }.min
   end
 
   def percentage_home_wins 
