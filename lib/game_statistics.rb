d1f8fdd7823b5d2d3db31eb6ca61b142ea8aafb3
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-04-21 15:58:47 -0500

    Fix percentage_home_wins, percentage_visitor_wins, and percentage_ties methods

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -21,8 +21,7 @@ class GameStatistics < StatHelper
   def percentage_home_wins
     home_wins = 0
     @game_teams.find_all do |row|
-      home_wins += 1 if (row[:hoa]) == "home" && row[:result] == "win" ||
-      (row[:hoa]) =="away" && row[:result] == "loss"
+      home_wins += 1 if row.hoa == "home" && row.result == "win" || row.hoa == "away" && row.result == "loss"
     end
     (home_wins.to_f / @game_teams.count.to_f).round(2)
   end
@@ -30,8 +29,7 @@ class GameStatistics < StatHelper
   def percentage_visitor_wins
     visitor_wins = 0
     @game_teams.find_all do |row|
-      visitor_wins += 1 if (row[:hoa]) == "away" && row[:result] == "win" ||
-      (row[:hoa]) =="home" && row[:result] == "loss"
+      visitor_wins += 1 if row.hoa == "away" && row.result == "win" || row.hoa == "home" && row.result == "loss"
     end
     (visitor_wins.to_f / @game_teams.count.to_f).round(2)
   end
@@ -39,10 +37,9 @@ class GameStatistics < StatHelper
   def percentage_ties
     no_lose = 0
     @game_teams.find_all do |row|
-      no lose += 1 if (row[:hoa]) == "home" && row[:result] == "tie" ||
-      (row[:hoa]) =="away" && row[:result] == "tie"
+      no lose += 1 if row.hoa == "home" && row.result == "tie" || row.hoa == "away" && row.result == "tie"
     end
-    (no_lose.to_f / @game_teams.count. to_f).round(2)
+    (no_lose.to_f / @game_teams.count.to_f).round(2)
   end
 
   # def count_of_games_by_season
