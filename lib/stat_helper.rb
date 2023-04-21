0ef94864fc4034054b5ce50818d48863d15a91d1
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-04-21 07:14:59 -0600

    Feature: add stat helper class.

---
+++ lib/stat_helper.rb
@@ -0,0 +1,17 @@
+require_relative "./game"
+require_relative "./game_teams"
+require_relative "./team"
+
+class StatHelper
+  attr_reader :games,
+              :teams,
+              :game_teams
+
+  def initialize(files)
+    @games = (CSV.open files[:games], headers: true, header_converters: :symbol).map { |row| Game.new(row) }
+    @game_teams = (CSV.open files[:game_teams], headers: true, header_converters: :symbol).map { |row| GameTeams.new(row) }
+
+  end
+
+
+end
