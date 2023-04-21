68ce6420c3f64d995b1e9a5ba41307ce8accb0b2
Jeff Nelson <jpnelson85@gmail.com>
2023-04-21 16:11:35 -0500

    add helper methods

--- lib/league_stats.rb
+++ lib/league_stats.rb
@@ -8,11 +8,27 @@ class LeagueStats < Futbol
 
   def count_of_teams
     @games.map(&:game_id).uniq.length
-    end
   end
 
   def best_offense
+    best_offense = Hash.new(0)
+    @games.each do |game|
+      best_offense[game_team.team_id] = average_number_of_goals(game_team)
+    end
+    sorted = best_offense.max_by{|team, avg_goals| avg_goals}
+    team_id_string = sorted[0]
+    team_id_converter(team_id_string)
+  
+  
+    total_goals = (total_goals_by_team(game_team.team_id))
+    total_goals.fdiv(total_games_by_team(game_team.team_id))
+  
 
+    total_goals = 0
+    @game_teams.each do |game_team|
+      total_goals += game_team.goals if game_team.team_id == team_id
+    end
+    total_goals
   end
 
   def worst_offense
@@ -66,5 +82,56 @@ class LeagueStats < Futbol
       end
       count_of_goals / count_of_games
     end.min()
+  end
+
+  #Helper Methods
+  def average_number_of_goals(game)
+    total_goals = (total_goals_by_team(game_team.team_id))
+    total_goals.fdiv(total_games_by_team(game_team.team_id))
+  end
+
+  def total_goals_by_team(team_id)
+    total_goals = 0
+    @games.each do |game|
+      if game.team_id == team_id
+      total_goals += game_team.goals
+      end
+    end
+    total_goals
+  end
+  
+  def total_games_by_team(team_id)
+    total_games = 0
+    @games.each do |game|
+      if game.team_id == team_id
+      total_games += 1 
+      end
+    end
+    total_games
+  end
+
+  def team_id_converter(team_id)
+    best_team = " "
+    @teams.each{|team| best_team = team.team_name if team.team_id == team_id}
+    best_team
+  end
+
+  def avg_score_away_games(sorted_teams)
+    sorted_teams.transform_values do |games|
+      goals = games.sum{|game| game.goals}
+      goals.fdiv(games.length)
+    end
+  end
+
+  def filter_away_games(sorted_teams)
+    sorted_teams.transform_values do |games|
+      games.select{|game| game.hoa == "away"}
+    end
+  end
+
+  def filter_home_games(sorted_teams)
+    sorted_teams.transform_values do |games|
+      games.select{|game| game.hoa == "home"}
+    end
   end
 end
