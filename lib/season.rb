c991c9ade911136f861f2e4fef9e9aadc9059c9a
Steven Stackhouse <stackmm@gmail.com>
2023-04-21 12:36:24 -0600

    create worst_coach, most_tackles, fewest_tackles methods

--- lib/season.rb
+++ lib/season.rb
@@ -5,4 +5,19 @@ class Season
     @season_data = data
   end
   
+  # name of the coach with the worst win percentage for the season
+  def worst_coach(season)
+
+  end
+
+  # name of the team with the most tackles in the season
+  def most_tackles(season)
+
+  end
+
+  # name of the team with the fewest tackles in the season
+  def fewest_tackles(season)
+
+  end
+  
 end
