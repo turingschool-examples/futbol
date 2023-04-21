fcdf6f3e27e7b7dc2f241330f0fbac9ae9c6bbea
Garrett Varenhorst <garrettvarenhorst@gmail.com>
2023-04-21 14:47:57 -0500

    Remove percentage_home_wins, percentage_visitor_wins, and percentage_ties from file

--- lib/game_teams.rb
+++ lib/game_teams.rb
@@ -35,31 +35,4 @@ class GameTeams
     @giveaways =      row[:giveaways].to_i
     @takeaways =      row[:takeaways].to_i
   end
-
-  def percentage_home_wins
-    home_wins = 0
-    @game_teams.find_all do |row|
-      home_wins += 1 if (row[:hoa]) == "home" && row[:result] == "WIN" ||
-      (row[:hoa]) =="away" && row[:result] == "LOSS"
-    end
-    (home_wins.to_f / @game_teams.count.to_f).round(2)
-  end
-
-  def percentage_visitor_wins
-    visitor_wins = 0
-    @game_teams.find_all do |row|
-      visitor_wins += 1 if (row[:hoa]) == "away" && row[:result] == "WIN" ||
-      (row[:hoa]) =="home" && row[:result] == "LOSS"
-    end
-    (visitor_wins.to_f / @game_teams.count.to_f).round(2)
-  end
-
-  def percentage_ties
-    no_lose = 0
-    @game_teams.find_all do |row|
-      no lose += 1 if (row[:hoa]) == "home" && row[:result] == "TIE" ||
-      (row[:hoa]) =="away" && row[:result] == "TIE"
-    end
-    (no_lose.to_f / @game_teams.count. to_f).round(2)
-  end
 end
