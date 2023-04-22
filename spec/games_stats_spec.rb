dee327b4d092772c10aeb902f0c461d409bf3b62
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-04-22 14:32:02 -0600

    Add avg_goal_season test

--- spec/games_stats_spec.rb
+++ spec/games_stats_spec.rb
@@ -69,8 +69,19 @@ RSpec.describe GamesStats do
   end
 
   describe 'GameStat helper methods' do
-    it '#average_goals_per_game' do
+    xit '#average_goals_per_game' do
       expect(@games_stats.average_goals_per_game).to eq(4.22)
+    end
+  
+    it '#average_goals_by_season' do
+      expect(@games_stats.average_goals_by_season).to eq({
+        "20122013"=>4.12,
+        "20162017"=>4.23,
+        "20142015"=>4.14,
+        "20152016"=>4.16,
+        "20132014"=>4.19,
+        "20172018"=>4.44
+      })
     end
   end
 
