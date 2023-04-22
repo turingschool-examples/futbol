2c482b92a07bf8b83c3efeda92500bbc8367224d
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-04-22 14:42:52 -0600

    Complete GamesStats helper method tests

--- spec/games_stats_spec.rb
+++ spec/games_stats_spec.rb
@@ -14,24 +14,22 @@ RSpec.describe GamesStats do
 
     @games_stats = GamesStats.new(@locations)
     @games_stats.merge_game_game_teams
-    # @games_stats.merge_team_to_game_game_teams
   end
 
   describe '#initialize' do
-    xit 'exists' do
+    it 'exists' do
       expect(@games_stats).to be_a(GamesStats)
     end
   end
 
-  # highest sum of winning and losing teams score
   describe '#highest_total_score' do
-    xit 'checks highest total score of a game' do
+    it 'checks highest total score of a game' do
       expect(@games_stats.highest_total_score).to eq(11)
     end
   end
 
   describe '#count_of_games_by_season' do
-    xit 'returns a hash of total games in a season' do
+    it 'returns a hash of total games in a season' do
       expect(@games_stats.count_of_games_by_season).to be_a(Hash)
       expect(@games_stats.count_of_games_by_season).to eq({
         "20122013"=>806,
@@ -45,35 +43,39 @@ RSpec.describe GamesStats do
   end
 
   describe '#lowest_total_score' do
-    xit 'checks lowest total score of a game' do
+    it 'checks lowest total score of a game' do
       expect(@games_stats.lowest_total_score).to eq(0)
     end
   end
 
   describe '#percentage_home_wins' do 
-    xit 'Percentage of games that all home teams have won (rounded to the nearest 100th)' do 
+    it 'Percentage of games that all home teams have won (rounded to the nearest 100th)' do 
       expect(@games_stats.percentage_home_wins).to eq(0.44)
     end
   end
 
   describe '#percentage_visitor_wins' do 
-    xit 'Percentage of games that a visitor has won (rounded to the nearest 100th)'do 
+    it 'Percentage of games that a visitor has won (rounded to the nearest 100th)'do 
     expect(@games_stats.percentage_visitor_wins).to eq(0.36)
     end
   end
   
   describe '#percentage_ties' do 
-    xit 'Percentage of games that has resulted in a tie (rounded to the nearest 100th)'do 
+    it 'Percentage of games that has resulted in a tie (rounded to the nearest 100th)'do 
     expect(@games_stats.percentage_ties).to eq(0.20)
     end
   end
 
   describe 'GameStat helper methods' do
-    xit '#average_goals_per_game' do
+    it '#average_goals_per_game' do
       expect(@games_stats.average_goals_per_game).to eq(4.22)
     end
   
-    it '#average_goals_by_season' do
+    it '#returns a hash of season goals' do
+    expect(@games_stats.average_goals_by_season).to be_a(Hash)
+    end
+    
+    it '#average_goals_by_season values' do
       expect(@games_stats.average_goals_by_season).to eq({
         "20122013"=>4.12,
         "20162017"=>4.23,
@@ -82,9 +84,10 @@ RSpec.describe GamesStats do
         "20132014"=>4.19,
         "20172018"=>4.44
       })
+    end 
+    
+    it '#goalie_goals' do
+      expect(@games_stats.goalie_goals).to be_a(Hash)
     end
   end
-
-
-
 end
