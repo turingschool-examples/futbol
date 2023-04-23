e6aba44acb8f6312267a8816cf37c57423bd14b8
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-04-23 14:59:21 -0600

    Add season_stats module files and file paths to spec_helper

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -17,20 +17,20 @@ RSpec.describe StatTracker do
   end
   
   describe '#initialize' do 
-    xit 'exists' do 
+    it 'exists' do 
       expect(@stat_tracker).to be_a(StatTracker)
     end
 
-    xit 'has an array of games' do 
+    it 'has an array of games' do 
       expect(@stat_tracker.games).to be_a Array
       expect(@stat_tracker.games[0]).to be_a Game
     end
     
-    xit 'has an array of gameteams' do    
+    it 'has an array of gameteams' do    
       expect(@stat_tracker.game_teams).to be_a Array
       expect(@stat_tracker.game_teams[0]).to be_a GameTeam
     end
-    xit 'has an array of teams' do 
+    it 'has an array of teams' do 
       expect(@stat_tracker.teams).to be_a Array
       expect(@stat_tracker.teams[0]).to be_a Team
     end
@@ -38,7 +38,7 @@ RSpec.describe StatTracker do
 
 #game class
   describe 'game averages' do 
-    xit '#average_goals_per_game' do 
+    it '#average_goals_per_game' do 
       expect(@stat_tracker.average_goals_per_game).to eq(4.22)
     end
 
@@ -53,33 +53,33 @@ RSpec.describe StatTracker do
       })
     end
 
-    xit 'can collect the sum of highest winning scores' do
+    it 'can collect the sum of highest winning scores' do
       expect(@stat_tracker.highest_total_score).to eq(11)
     end
 
-    xit 'can collect the sum of lowest winning scores' do
+    it 'can collect the sum of lowest winning scores' do
       expect(@stat_tracker.lowest_total_score).to eq(0)
     end
   end
 
   describe 'percentage of wins' do 
-    xit 'can calculate the percentage of home wins' do 
+    it 'can calculate the percentage of home wins' do 
       expect(@stat_tracker.percentage_home_wins).to eq(0.44)
     end 
 
-    xit 'can calculate the percentage of visitor wins' do 
+    it 'can calculate the percentage of visitor wins' do 
       expect(@stat_tracker.percentage_visitor_wins).to eq(0.36)
     end
   end
 
   describe '#percentage_ties method' do
-    xit 'shows the percentage of ties per season' do 
+    it 'shows the percentage of ties per season' do 
       expect(@stat_tracker.percentage_ties).to eq(0.2)
     end
   end
 
   describe '#count_of_games_by_season method' do
-    xit 'counts number of games played per season' do
+    it 'counts number of games played per season' do
       hash = {"20122013"=>806, "20132014"=>1323, "20142015"=>1319, "20152016"=>1321, "20162017"=>1317, "20172018"=>1355}
       expect(@stat_tracker.count_of_games_by_season).to eq(hash)
     end
@@ -88,39 +88,39 @@ RSpec.describe StatTracker do
 #game_teams/league stats
 
   describe 'count of teams' do 
-    xit 'can count the total number of teams in the data' do 
+    it 'can count the total number of teams in the data' do 
       expect(@stat_tracker.count_of_teams).to eq(32)
     end
   end
 
   describe 'best offense' do 
-    xit 'can return team with highest average goals per game' do 
+    it 'can return team with highest average goals per game' do 
       expect(@stat_tracker.best_offense).to eq('Reign FC')
     end
   end
 
   describe 'worst offense' do 
-    xit 'can return team with lowest average goals per game' do 
+    it 'can return team with lowest average goals per game' do 
       expect(@stat_tracker.worst_offense).to eq('Utah Royals FC')
     end
   end
 
   describe 'highest scoring avg' do 
-    xit 'can return name of team with highest avg score per game when they are away' do 
+    it 'can return name of team with highest avg score per game when they are away' do 
       expect(@stat_tracker.highest_scoring_visitor).to eq('FC Dallas')
     end
   
-    xit 'can return name of team with highest avg score per game when they are home' do 
+    it 'can return name of team with highest avg score per game when they are home' do 
       expect(@stat_tracker.highest_scoring_home_team).to eq('Reign FC')
     end
   end
 
   describe 'lowest scoring avg' do 
-    xit 'can return name of team with lowest score per game when they are away' do 
+    it 'can return name of team with lowest score per game when they are away' do 
       expect(@stat_tracker.lowest_scoring_visitor).to eq('San Jose Earthquakes')
     end
 
-    xit 'can return name of team with lowest score per game when they are home' do
+    it 'can return name of team with lowest score per game when they are home' do
       expect(@stat_tracker.lowest_scoring_home_team).to eq('Utah Royals FC')
     end
   end
