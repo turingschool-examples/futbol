5debedacc625cdc1b33da23d4b82a541f4c1dd6e
Bradley Milton <bmilton2412@gmail.com>
2023-04-23 15:28:01 -0700

    add the least accurate team method test to stat_tracker_spec.rb file

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -126,6 +126,13 @@ RSpec.describe StatTracker do
     end
   end
 
+  describe 'least accurate team' do
+    it "#least_accurate_team" do
+      expect(@stat_tracker.least_accurate_team("20132014")).to eq "New York City FC"
+      expect(@stat_tracker.least_accurate_team("20142015")).to eq "Columbus Crew SC"
+    end
+  end
+
   describe '#fewest_tackles' do 
     it 'can track the team with the least tackles per season' do 
       expect(@stat_tracker.fewest_tackles("20132014")).to eq "Atlanta United"
