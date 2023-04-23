de5c470cf5a0640a96778f289db1912e7d450cb2
Bradley Milton <bmilton2412@gmail.com>
2023-04-23 15:36:47 -0700

    fixes least accurate team test to most accurate team test on line 129 in the stat tracker spec.rb file

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -126,7 +126,7 @@ RSpec.describe StatTracker do
     end
   end
 
-  describe 'least accurate team' do
+  describe 'most accurate team' do
     it "#most_accurate_team" do
       expect(@stat_tracker.most_accurate_team("20132014")).to eq "Real Salt Lake"
       expect(@stat_tracker.most_accurate_team("20142015")).to eq "Toronto FC"
