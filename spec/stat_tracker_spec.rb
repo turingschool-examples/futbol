072c926dc198d70bc0b4d36f751be81a949c2aea
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-04-24 09:14:57 -0600

    Test: #worst_coach

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -17,25 +17,25 @@ RSpec.describe StatTracker do
   end
   
   describe '#initialize' do 
-    it 'exists' do 
+    xit 'exists' do 
       expect(@stat_tracker).to be_a(StatTracker)
     end
 
-    it 'has an array of games' do 
+    xit 'has an array of games' do 
       expect(@stat_tracker.games).to be_a Array
       expect(@stat_tracker.games[0]).to be_a Game
     end
     
-    it 'has an array of gameteams' do    
+    xit 'has an array of gameteams' do    
       expect(@stat_tracker.game_teams).to be_a Array
       expect(@stat_tracker.game_teams[0]).to be_a GameTeam
     end
 
-    it 'game_teams have seasons by assignment' do 
+    xit 'game_teams have seasons by assignment' do 
       expect(@stat_tracker.game_teams[0].season).to eq('20122013')
     end
 
-    it 'has an array of teams' do 
+    xit 'has an array of teams' do 
       expect(@stat_tracker.teams).to be_a Array
       expect(@stat_tracker.teams[0]).to be_a Team
     end
@@ -44,11 +44,11 @@ RSpec.describe StatTracker do
 #game class
 
   describe 'game averages' do 
-    it '#average_goals_per_game' do 
+    xit '#average_goals_per_game' do 
       expect(@stat_tracker.average_goals_per_game).to eq(4.22)
     end
 
-    it '#average_goals_by_season' do 
+    xit '#average_goals_by_season' do 
       expect(@stat_tracker.average_goals_by_season).to eq({
           "20122013"=>4.12,
           "20162017"=>4.23,
@@ -59,33 +59,33 @@ RSpec.describe StatTracker do
       })
     end
 
-    it 'can collect the sum of highest winning scores' do
+    xit 'can collect the sum of highest winning scores' do
       expect(@stat_tracker.highest_total_score).to eq(11)
     end
 
-    it 'can collect the sum of lowest winning scores' do
+    xit 'can collect the sum of lowest winning scores' do
       expect(@stat_tracker.lowest_total_score).to eq(0)
     end
   end
 
   describe 'percentage of wins' do 
-    it 'can calculate the percentage of home wins' do 
+    xit 'can calculate the percentage of home wins' do 
       expect(@stat_tracker.percentage_home_wins).to eq(0.44)
     end 
 
-    it 'can calculate the percentage of visitor wins' do 
+    xit 'can calculate the percentage of visitor wins' do 
       expect(@stat_tracker.percentage_visitor_wins).to eq(0.36)
     end
   end
 
   describe '#percentage_ties method' do
-    it 'shows the percentage of ties per season' do 
+    xit 'shows the percentage of ties per season' do 
       expect(@stat_tracker.percentage_ties).to eq(0.2)
     end
   end
 
   describe '#count_of_games_by_season method' do
-    it 'counts number of games played per season' do
+    xit 'counts number of games played per season' do
       hash = {"20122013"=>806, "20132014"=>1323, "20142015"=>1319, "20152016"=>1321, "20162017"=>1317, "20172018"=>1355}
       expect(@stat_tracker.count_of_games_by_season).to eq(hash)
     end
@@ -94,66 +94,74 @@ RSpec.describe StatTracker do
 #game_teams/league stats
 
   describe 'count of teams' do 
-    it 'can count the total number of teams in the data' do 
+    xit 'can count the total number of teams in the data' do 
       expect(@stat_tracker.count_of_teams).to eq(32)
     end
   end
 
   describe 'best offense' do 
-    it 'can return team with highest average goals per game' do 
+    xit 'can return team with highest average goals per game' do 
       expect(@stat_tracker.best_offense).to eq('Reign FC')
     end
   end
 
   describe 'worst offense' do 
-    it 'can return team with lowest average goals per game' do 
+    xit 'can return team with lowest average goals per game' do 
       expect(@stat_tracker.worst_offense).to eq('Utah Royals FC')
     end
   end
 
   describe 'highest scoring avg' do 
-    it 'can return name of team with highest avg score per game when they are away' do 
+    xit 'can return name of team with highest avg score per game when they are away' do 
       expect(@stat_tracker.highest_scoring_visitor).to eq('FC Dallas')
     end
   
-    it 'can return name of team with highest avg score per game when they are home' do 
+    xit 'can return name of team with highest avg score per game when they are home' do 
       expect(@stat_tracker.highest_scoring_home_team).to eq('Reign FC')
     end
   end
 
   describe 'lowest scoring avg' do 
-    it 'can return name of team with lowest score per game when they are away' do 
+    xit 'can return name of team with lowest score per game when they are away' do 
       expect(@stat_tracker.lowest_scoring_visitor).to eq('San Jose Earthquakes')
     end
 
-    it 'can return name of team with lowest score per game when they are home' do
+    xit 'can return name of team with lowest score per game when they are home' do
       expect(@stat_tracker.lowest_scoring_home_team).to eq('Utah Royals FC')
     end
   end
 
   describe 'most accurate team' do
-    it "#most_accurate_team" do
+    xit "#most_accurate_team" do
       expect(@stat_tracker.most_accurate_team("20132014")).to eq "Real Salt Lake"
       expect(@stat_tracker.most_accurate_team("20142015")).to eq "Toronto FC"
     end
   end
 
   describe 'least accurate team' do
-    it "#least_accurate_team" do
+    xit "#least_accurate_team" do
       expect(@stat_tracker.least_accurate_team("20132014")).to eq "New York City FC"
       expect(@stat_tracker.least_accurate_team("20142015")).to eq "Columbus Crew SC"
     end
   end
 
   describe '#fewest_tackles' do 
-    it 'can track the team with the least tackles per season' do 
+    xit 'can track the team with the least tackles per season' do 
       expect(@stat_tracker.fewest_tackles("20132014")).to eq "Atlanta United"
       expect(@stat_tracker.fewest_tackles("20142015")).to eq "Orlando City SC"
     end
   end
 
+  #season_stats
+
+  describe 'coach quality' do
+    it "#worst_coach" do
+      expect(@stat_tracker.worst_coach("20122013")).to eq ('Martin Raymond')
+    end
+  end
+
   describe 'helpers' do 
-    it '#total_goals' do 
+    xit '#total_goals' do 
       expect(@stat_tracker.total_goals(@test_games)).to eq(397)
     end
   end
