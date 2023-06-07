979f93b3971b8994de8ad76f30e410496f4df00c
Dani Wilson <danirae.powell@gmail.com>
2023-06-07 16:57:34 -0600

    feat: Add games won method

--- lib/team.rb
+++ lib/team.rb
@@ -1,10 +1,20 @@
 class Team
 
 attr_reader :id,
-            :team_name
+            :team_name,
+            :games_won
 
   def initialize(team_data)
     @id = team_data[:team_id]
     @team_name = team_data[:teamname]
+    @games_won = 0
+    # parse_stats(game_team_data)
   end 
+
+  def parse_stats(game_team_data)
+    game_lines = CSV.open game_team_data, headers: true, header_converters: :symbol
+    @games_won = game_lines.count do |line|
+      line[:team_id] == @id && line[:result] == "WIN"
+    end
+  end
 end
