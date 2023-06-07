f8f92a6f2b033d24787e6c452eab809e9220021a
Gabriel Torres <thomastgtorres21@gmail.com>
2023-06-07 18:24:39 -0500

    test: create stat tracker spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,7 @@
 require 'simplecov'
+require './lib/stat_tracker'
+require './lib/games'
+require './lib/game_teams'
+require './lib/teams'
+
 SimpleCov.start
---
+++ spec/stat_tracker_spec.rb
@@ -0,0 +1,23 @@
+require './spec_helper'
+
+
+RSpec.describe 'StatTracker' do
+  before(:each) do
+    game_path = './data/games.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    @stat_tracker = StatTracker.from_csv(locations)
+    require 'pry'; binding.pry
+  end
+
+  it 'exists' do
+    expect(@stat_tracker).to be_an_instance_of(Hash)
+  end
+end
