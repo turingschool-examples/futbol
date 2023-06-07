74cf82d6ed27549047b25827b5878081db91c145
Artemy Gibson <artemygibson@gmail.com>
2023-06-06 18:32:16 -0700

    test: Set up game class initialization

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,8 +1,32 @@
-require './lib/game'
-require 'rspec'
+require "./lib/game"
+require "rspec"
+require "csv"
 
 describe Game do
   before do
-    @game = Game.new
+    @game_file = "./data/game_sampl.csv"
+    @game_lines = CSV.open @game_file, headers: true, header_converters: :symbol
+    @games = []
+    @game_lines.each do |line|
+      @games << Game.new(line)
+    end
+    @game_1 = @games[0] #2012030221,20122013,Postseason,5/16/13,3,6,2,3,Toyota Stadium,/api/v1/venues/null
+    @game_2 = @games[2] #2012030222,20122013,Postseason,5/19/13,3,6,2,3,Toyota Stadium,/api/v1/venues/null
+    @game_3 = @games[12] #2012020225,20122013,Regular Season,2/19/13,29,24,2,3,Rio Tinto Stadium,/api/v1/venues/null
+  end
+
+  it "exists" do
+    expect(@game).to be_a(Game)
+  end
+
+  it "has game_id, season, season_type, away_team_id, home_team_id, away_goals, home_goals" do
+    expect(@game_1.game_id).to eq(2012030221)
+    expect(@game_1.season).to eq(20122013)
+    expect(@game_1.season_type).to eq("Postseason")
+    expect(@game_3.season_type).to eq("Regular Season")
+    expect(@game_1.away_team_id).to eq(3)
+    expect(@game_1.home_team_id).to eq(6)
+    expect(@game_1.away_goals).to eq(2)
+    expect(@game_1.home_goals).to eq(3)
   end
 end
