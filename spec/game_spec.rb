475701ddec789f80e0bee423e67f043a519ae20b
Matthew Lim <matlim200@gmail.com>
2023-06-07 14:49:54 -0700

    Test: Fixed formatting

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,15 +3,15 @@ require 'spec_helper'
 RSpec.describe Game do
   before do
     @data = {
-      :game_id => 2012030221,
-      :season => 20122013,
-      :type => "Postseason",
-      :date_time => "5/16/13",
+      :game_id      => 2012030221,
+      :season       => 20122013,
+      :type         => "Postseason",
+      :date_time    => "5/16/13",
       :away_team_id => 3,
       :home_team_id => 6,
-      :away_goals => 2,
-      :home_goals => 3,
-      :venue => "Toyota Stadium"
+      :away_goals   => 2,
+      :home_goals   => 3,
+      :venue        => "Toyota Stadium"
       # :venue_link => /api/v1/venues/null
     }
     @game = Game.new(@data)
