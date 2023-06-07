866c615a6c18bdbf1db071ad9165a413aa67a990
Artemy Gibson <artemygibson@gmail.com>
2023-06-06 19:42:58 -0700

    test: Separate attribute tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -15,21 +15,45 @@ describe Game do
     @game_3 = games[11] 
     @game_4 = games[5] 
     @game_5 = games[12]
+    @game_6 = games[9]
   end
 
   it "exists" do
     expect(@game_1).to be_a(Game)
   end
 
-  it "has game_id, season, season_type, away_team_id, home_team_id, away_goals, home_goals" do
+  it "has a game_id" do
     expect(@game_1.game_id).to eq("2012030221")
+    expect(@game_2.game_id).to eq("2012030223")
+  end
+
+  it "has a season" do
     expect(@game_1.season).to eq("20122013")
+    expect(@game_2.season).to eq("20122013")
+    expect(@game_6.season).to eq("20132014")
+  end
+
+  it "has a season type" do
     expect(@game_1.season_type).to eq("Postseason")
     expect(@game_3.season_type).to eq("Regular Season")
+  end
+
+  it "has team ids" do
     expect(@game_1.away_team_id).to eq("3")
     expect(@game_1.home_team_id).to eq("6")
+    expect(@game_2.away_team_id).to eq("6")
+    expect(@game_2.home_team_id).to eq("3")
+    expect(@game_3.away_team_id).to eq("29")
+    expect(@game_3.home_team_id).to eq("24")
+  end
+
+  it "has goals" do
     expect(@game_1.away_goals).to eq(2)
     expect(@game_1.home_goals).to eq(3)
+    expect(@game_3.away_goals).to eq(2)
+    expect(@game_3.home_goals).to eq(3)
+    expect(@game_5.away_goals).to eq(3)
+    expect(@game_5.home_goals).to eq(3)
   end
 
   it "has a total goals scored" do
