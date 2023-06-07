866bfd8be1037df753e7242ff101fa81ed0bb272
Artemy Gibson <artemygibson@gmail.com>
2023-06-06 19:12:44 -0700

    feat: Add winner method

--- lib/game.rb
+++ lib/game.rb
@@ -15,8 +15,14 @@ class Game
     @away_goals = game_info[:away_goals].to_i
     @home_goals = game_info[:home_goals].to_i
   end
-
+  #StatTracker will reference total_goals for highest_total_score and lowest_total_score
   def total_goals
     @away_goals + @home_goals
+  end
+
+  def winner
+    return "home" if @home_goals > @away_goals
+    return "away" if @away_goals > @home_goals
+    return "tie" if @home_goals == @away_goals
   end
 end
