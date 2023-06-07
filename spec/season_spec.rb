02614df7316a9a4a4662abe8964d3bcd9eec089b
Dani Wilson <danirae.powell@gmail.com>
2023-06-07 16:07:01 -0600

    test: Add game ids method tests

--- spec/season_spec.rb
+++ spec/season_spec.rb
@@ -7,7 +7,7 @@ require 'csv'
 RSpec.describe Season do
     before do
         game_file = "./data/games_sampl.csv"
-        game_team_file = "./game_teams_sampl.csv"
+        game_team_file = "./data/game_teams_sampl.csv"
         @season = Season.new(game_file, game_team_file, "20122013", "Postseason")
     end
 
@@ -26,16 +26,21 @@ RSpec.describe Season do
         expect(@season.games.sample).to be_a(Game)
     end
 
-    # xit "has a coach for a given team in a given season" do
-
-    # end
-
     it "can count each game in a season" do
         expect(@season.games_count).to eq(9)
     end
 
     it "has average goals per season" do
         expect(@season.average_goals_per_game).to eq(1.89)
+    end
+
+    it "can generate team stats" do
+        expect(@season.team_stats).to be_an(Array)
+    end
+
+    it "can keep track of all game ids" do
+        expect(@season.game_ids).to be_an(Array)
+        expect(@season.game_ids).to eq(["2012030221", "2012030222", "2012030223", "2012030224", "2012030225", "2012030311", "2012030312", "2012030313", "2012030314"]) 
     end
     
 end
