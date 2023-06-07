50bf3ab19bda32509c48a44f489f15b296fb92b4
Dani Wilson <danirae.powell@gmail.com>
2023-06-07 16:59:14 -0600

    style: Fix quotation marks

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Team do
     @team_lines = CSV.open @team_data, headers: true, header_converters: :symbol
     teams = []
     @team_lines.each do |line|
-      teams << Team.new(line)
+      teams << Team.new(line, @game_team_data)
     end
     @team1 = teams[0]
     @team2 = teams[1]
@@ -17,25 +17,25 @@ RSpec.describe Team do
     @team5 = teams[5]
   end
 
-  it 'exists' do
+  it "exists" do
   expect(@team1).to be_instance_of(Team)
   expect(@team2).to be_instance_of(Team)
   expect(@team3).to be_instance_of(Team)
   end
 
-  it 'has an id' do 
+  it "has an id" do 
     expect(@team1.id).to eq("1")
     expect(@team2.id).to eq("4")
     expect(@team3.id).to eq("26") 
   end
 
-  it 'has a team_name' do
+  it "has a team_name" do
     expect(@team1.team_name).to eq("Atlanta United")
     expect(@team2.team_name).to eq("Chicago Fire")
     expect(@team3.team_name).to eq("FC Cincinnati")
   end
 
-  it 'parses stats' do
+  it "parses stats" do
     @team4.parse_stats(@game_team_data)
     expect(@team4.games_won).to eq(4)
     expect(@team5.games_won).to eq(0)
