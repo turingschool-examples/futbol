9d1ebdc511d71ab92390c9a1875c2084c85903ee
Dani Wilson <danirae.powell@gmail.com>
2023-06-07 17:18:16 -0600

    fix: Remove changes to team class

--- lib/team.rb
+++ lib/team.rb
@@ -1,20 +1,10 @@
 class Team
 
 attr_reader :id,
-            :team_name,
-            :games_won
+            :team_name
 
-  def initialize(team_data, game_team_data)
+  def initialize(team_data)
     @id = team_data[:team_id]
     @team_name = team_data[:teamname]
-    @games_won = 0
-    parse_stats(game_team_data)
-  end 
-
-  def parse_stats(game_team_data)
-    game_lines = CSV.open game_team_data, headers: true, header_converters: :symbol
-    @games_won = game_lines.count do |line|
-      line[:team_id] == @id && line[:result] == "WIN"
-    end
   end
 end
