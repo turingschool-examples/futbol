004b9a1117a7270daf9218191f171461339c0a7f
Dani Wilson <danirae.powell@gmail.com>
2023-06-07 15:05:32 -0600

    feat: Add generate games method

--- lib/season.rb
+++ lib/season.rb
@@ -1,9 +1,23 @@
 require 'csv'
 
 class Season
-    attr_reader :games
+    attr_reader :games,
+                :type,
+                :season
 
     def initialize(game_file, game_team_file, season, type)
         @games = []
+        @season = season
+        @type = type
+        generate_games(game_file)
+    end
+
+    def generate_games(game_file)
+        game_lines = CSV.open game_file, headers: true, header_converters: :symbol
+        game_lines.each do |line|
+            if line[:season] == @season && line[:type] == @type
+                @games << Game.new(line)
+            end
+        end
     end
 end
