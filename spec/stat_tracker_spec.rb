85f4a1471261128b46c2cc9ad5b1b453052d00b0
Parker Boeing <parker.boeing@gmail.com>
2023-06-08 17:01:44 -0600

    Test: ensure functionality of best_offense method and average_goals_by_team helper method

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -90,14 +90,40 @@ RSpec.describe StatTracker do
       "20132014" => 25,
       "20142015" => 2,
       "20162017" => 5
-      }
+    }
     expect(@stat_tracker.count_of_games_by_season).to eq(expected)
     end
   end
 
   describe '#league statics' do
+    it 'can determine #average_goals_by_team' do
+
+    expected = {
+      "3"=>1.8064516129032258,
+      "6"=>2.727272727272727,
+      "5"=>1.9,
+      "17"=>1.9285714285714286,
+      "16"=>2.1379310344827585,
+      "9"=>2.1818181818181817,
+      "8"=>1.7272727272727273,
+      "30"=>1.875,
+      "26"=>2.130434782608696,
+      "19"=>1.6666666666666667,
+      "24"=>2.3529411764705883,
+      "2"=>1.8333333333333333,
+      "15"=>1.6923076923076923,
+      "20"=>1.75,
+      "14"=>1.9230769230769231,
+      "28"=>2.4,
+      "4"=>1.0,
+      "21"=>1.7142857142857142,
+      "25"=>2.5
+    }
+    expect(@stat_tracker.average_goals_by_team).to eq(expected)
+    end
+
     it 'can determine which team has #best_offense' do
-    expect(@stat_tracker.best_offense).to eq("Chicago Red Stars")
+    expect(@stat_tracker.best_offense).to eq("FC Dallas")
     end
 
     xit 'can determine which team has #worst_offense' do
