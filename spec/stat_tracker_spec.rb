3a0bd9b0f002988a572be3ffca1d4ce4667beb99
Artemy Gibson <artemygibson@gmail.com>
2023-06-08 13:14:08 -0700

    test: Test count_of_teams method

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -12,49 +12,48 @@ RSpec.describe StatTracker do
     game_path = './data/games_sampl.csv'
     teams_path = './data/teams_sampl.csv'
     game_teams_path = './data/game_teams_sampl.csv'
-    @stat_tracker_1 = StatTracker.from_csv(game_path, teams_path, game_teams_path)
+    @stat_tracker = StatTracker.from_csv(game_path, teams_path, game_teams_path)
   end
 
   it "exists" do
-    expect(@stat_tracker_1).to be_a(StatTracker)
+    expect(@stat_tracker).to be_a(StatTracker)
   end
 
   it "can calculate the highest total score of all games" do
-    expect(@stat_tracker_1.highest_total_score).to eq(6)
+    expect(@stat_tracker.highest_total_score).to eq(6)
   end
 
   it "can calculate the lowest total score of all games" do
-    expect(@stat_tracker_1.lowest_total_score).to eq(1)
+    expect(@stat_tracker.lowest_total_score).to eq(1)
   end
 
   it "can calculate the home win percentage" do
-    expect(@stat_tracker_1.percentage_home_wins).to eq(0.54)
+    expect(@stat_tracker.percentage_home_wins).to eq(0.54)
   end
 
   it "can calculate the visitor win percentage" do
-    expect(@stat_tracker_1.percentage_visitor_wins).to eq(0.38)
+    expect(@stat_tracker.percentage_visitor_wins).to eq(0.38)
   end
 
   it "can calculate the pecentage of games that end up in a tie" do
-    expect(@stat_tracker_1.percentage_ties).to eq(0.08)
+    expect(@stat_tracker.percentage_ties).to eq(0.08)
   end
 
   it "can count games in each season" do
-    expect(@stat_tracker_1.count_of_games_by_season).to eq({"20122013"=>11, "20132014"=>2})
+    expect(@stat_tracker.count_of_games_by_season).to eq({"20122013"=>11, "20132014"=>2})
   end
 
   #may have misunderstood wording in instructions-- may need to refactor average goals methods after checking against spec harness (remove goals_averaged per game, use total goals instead)
   it "can count average goals per game" do
-    expect(@stat_tracker_1.average_goals_per_game).to eq(2.04)
+    expect(@stat_tracker.average_goals_per_game).to eq(2.04)
   end
 
   it "can count average goals by season" do
-    expect(@stat_tracker_1.average_goals_by_season).to eq({"20122013"=>2.05, "20132014"=>2.0})
+    expect(@stat_tracker.average_goals_by_season).to eq({"20122013"=>2.05, "20132014"=>2.0})
   end
 
   it "can count all teams" do
-    #count_of_teams
-    
+    expect(@stat_tracker.count_of_teams).to eq(9)
   end
 
 
