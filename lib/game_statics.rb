6dd7db1d94fa8696dcf9974be667700d453bdecc
Dylan Timmons <dylanstimmons@icloud.com>
2023-06-07 19:10:42 -0600

    Feat: highest total score for gamestatics class

--- lib/game_statics.rb
+++ lib/game_statics.rb
@@ -1,6 +1,19 @@
 class GameStatics
 
     def initialize(game_object_array)
-        @games = game_object_array
+      @games = game_object_array
     end
+  
+    def highest_total_score
+        highest_score = 0
+      
+        @games.each do |game|
+          total_score = game["home_goals"].to_i + game["away_goals"].to_i
+          highest_score = total_score if total_score > highest_score
+        end
+      
+        highest_score
+    end
+           
 end
+  
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require 'simplecov'
 require './lib/game'
+require './lib/game_statics'
 require './lib/team'
 require './lib/game_by_team'
 require './lib/stat_tracker'
