8464e631a89e163501838bbf9af9732d729b490c
andi lovetto <andilovetto@gmail.com>
2023-06-08 14:49:33 -0600

    test: Remove test specs not needed

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -7,25 +7,7 @@ RSpec.describe 'Stat_Tracker' do
   let(:path) { './data/games.csv' }
   let(:path_2) { './data/teams.csv' }
   let(:path_3) { './data/game_teams.csv' }
-  
-  it 'exists as a csv hash' do
 
-  end
-
-  it 'can establish data from csv' do
-    csv = CSV.parse(File.read('./data/teams.csv'))
-    p = StatTracker.new
-    p.from_csv(csv)
-    expect(p.from_csv(csv)).to be_a(Array)
-    expect(p.from_csv(csv)[1]).to be_a(Hash)
-  end
-
-  it 'can sort to which csv method is needed' do
-    csv = CSV.parse(File.read('./data/teams.csv'))
-    expect(csv[0][1]).to eq("franchiseId")
-    csv2 = CSV.parse(File.read('./data/game_teams.csv'))
-    expect(csv2[0][1]).to eq("team_id")
-  end
 
   it 'creates GameFactory with games as a hash' do
     stat_tracker = StatTracker.new
