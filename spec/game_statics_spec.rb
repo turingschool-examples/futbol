46b91fc7f33c4d0cc5d3d63e8b451aaa2eee19fc
Matthew Lim <matlim200@gmail.com>
2023-06-08 10:41:49 -0700

    Test: Add #percentage_home_wins test

--- spec/game_statics_spec.rb
+++ spec/game_statics_spec.rb
@@ -4,8 +4,8 @@ require 'spec_helper.rb'
 RSpec.describe GameStatics do
     before do
         games_csv = './spec/feature/game_test.csv'
-        game_array = CSV.read(games_csv, headers: true).map(&:to_h) 
-        @game_statics = GameStatics.new(game_array) 
+        game_array = CSV.read(games_csv, headers: true).map(&:to_h)
+        @game_statics = GameStatics.new(game_array)
       end
 
     it "#highest_total_score" do
@@ -15,4 +15,10 @@ RSpec.describe GameStatics do
     it "#lowest_total_score" do
       expect(@game_statics.lowest_total_score).to eq(3)
     end
+
+  describe "#percentage_home_wins" do
+    it "can find the percentage of home wins" do
+      expect(@game_statics.percentage_home_wins).to eq(0.08)
+    end
+  end
 end
