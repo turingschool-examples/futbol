6528c65d1842bebf1a2278f179ace48bb88203e4
Parker Boeing <parker.boeing@gmail.com>
2023-06-08 11:12:33 -0600

    Feat: add percentage_ties and count_of_games_by_season methods to GameStatics class

--- lib/game_statics.rb
+++ lib/game_statics.rb
@@ -1,6 +1,17 @@
 class GameStatics
 
-    def initialize(game_object_array)
-        @games = game_object_array
-    end
+  def initialize(game_object_array)
+    @games = game_object_array
+  end
+
+  def percentage_ties
+    tie_count = @games.count { |game| game.away_goals.to_i == game.home_goals.to_i }
+    percentage = (tie_count.to_f / @games.count.to_f).round(4) * 100
+    p percentage
+  end
+
+  def count_of_games_by_season
+   season_games = @games.each_with_object(Hash.new(0)) {|game, hash| hash[game.season] += 1}
+   p season_games
+  end
 end
