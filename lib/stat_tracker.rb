54bb81458b72488d13c2398dc533fcd8db8cca5e
pcbennett108 <pcbennett42@gmail.com>
2023-06-08 11:56:09 -0600

    feat: Add empty methods for all statistics

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -21,6 +21,28 @@ class StatTracker
   # class GameStats < RawStats
   end
 
+  
+  # Game Statistics
+  def highest_total_score
+    
+  end
+
+  def lowest_total_score
+
+  end
+
+  def percentage_home_wins
+
+  end
+
+  def percentage_visitor_wins
+
+  end
+
+  def percentage_ties
+
+  end
+
   def count_of_games_by_season
     result = {}
     @games.each do |game|
@@ -33,6 +55,70 @@ class StatTracker
     result
   end
 
+  def average_goals_per_game
+
+  end
+
+  def average_goals_by_season
+
+  end
+
+  # League Statistics
+
+  def count_of_teams
+
+  end
+
+  def best_offense
+
+  end
+
+  def worst_offense
+
+  end
+
+  def highest_scoring_visitor
+
+  end
+
+  def highest_scoring_home_team
+
+  end
+
+  def lowest_scoring_visitor
+
+  end
+
+  def lowest_scoring_home_team
+
+  end
+
+  # Season Statistics
+
+  def winningest_coach
+
+  end
+
+  def worst_coach
+
+  end
+
+  def most_accurate_team
+
+  end
+
+  def least_accurate_team
+
+  end
+
+  def most_tackles
+
+  end
+
+  def fewest_tackles
+
+  end
+  
   # Implement the remaining methods for statistics calculations
   # ...
 
