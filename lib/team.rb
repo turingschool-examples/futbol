f2e28ad0962b85b354be88ca77f53be61691fc53
Anna Wiley <awiley33@gmail.com>
2023-06-08 15:49:35 -0500

    feat: Add initialize for team class

--- lib/team.rb
+++ lib/team.rb
@@ -1,2 +1,12 @@
+
+
 class Team
+  def initialize(data)
+    @team_id = data[:team_id]
+    @franchise_id = data[:franchiseid]
+    @name = data[:teamname]
+    @abbreviation = data[:abbreviation]
+    @stadium = data[:stadium]
+    @link = data[:link]
+  end
 end
