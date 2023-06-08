9b5cfb583fa753efdfc375f72c76fa3f8ceb2e6d
Zanna Fitch <zannafitch2004@gmail.com>
2023-06-08 15:47:03 -0500

    add initialize with access to csv

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -7,6 +7,5 @@ class StatTracker
     @games = CSV.read(data[:games], headers: true, header_converters: :symbol).map {|row| Game.new(row) }
     @teams = CSV.read(data[:teams], headers: true, header_converters: :symbol).map {|row| Team.new(row) }
     @game_teams = CSV.read(data[:game_teams], headers: true, header_converters: :symbol).map {|row| GameTeam.new(row) }
-
   end
 end
