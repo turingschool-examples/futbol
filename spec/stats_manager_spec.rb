7dbcb3d7cd1e7e83dabbafceec23ee4140db407f
Daniel Gallagher <Daniel.Gallagher92@gmail.com>
2023-06-08 13:30:29 -0600

    Test: StatsManager spec

---
+++ spec/stats_manager_spec.rb
@@ -0,0 +1,14 @@
+require './lib/stats_manager'
+
+RSpec.describe StatsManager do 
+  #it can recieve data from csv file
+  #it can read game_id
+  # can read season
+  # expect type to be Postseason
+  #expect away team id to be date
+  #expect home/away team id to be int
+  #expect home/away goals to be int
+  #expect venue to be a string
+  #expect venue_link to be string
+
+end
