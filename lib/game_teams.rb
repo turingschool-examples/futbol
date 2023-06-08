548699e7e3032e6c591f50f058d7f4f89abb4a07
Dani Wilson <danirae.powell@gmail.com>
2023-06-08 14:53:09 -0600

    refactor: Add to attr_reader, fix data naming

--- lib/game_teams.rb
+++ lib/game_teams.rb
@@ -1,16 +1,22 @@
 require "csv"
 
 class GameTeam
-    attr_reader
+    attr_reader :team_id,
+                :home_or_away,
+                :result,
+                :head_coach,
+                :goals,
+                :shots,
+                :tackles
 
     def initialize(game_team_data)
-        @team_id = game_team_file[:team_id]
-        @home_or_away = game_team_file[:hoa]
-        @result = game_team_file[:result]
-        @head_coach = game_team_file[:head_coach]
-        @goals = game_team_file[:goals].to_i
-        @shots = game_team_file[:shots].to_i
-        @tackles = game_team_file[:tackles].to_i
+        @team_id = game_team_data[:team_id].to_i
+        @home_or_away = game_team_data[:hoa]
+        @result = game_team_data[:result]
+        @head_coach = game_team_data[:head_coach]
+        @goals = game_team_data[:goals].to_i
+        @shots = game_team_data[:shots].to_i
+        @tackles = game_team_data[:tackles].to_i
     end
 
 
