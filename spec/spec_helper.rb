6005986820e0a2448b2fa6c6fc2e4e4a7151c5dd
Weston Schutt <wtschutt@gmail.com>
2023-06-07 18:38:48 -0600

    Test: Add Game class tests and Update file names to singular object convention

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,9 @@
-# require 'simplecov'
-# SimpleCov.start
+require 'simplecov'
+SimpleCov.start
 
 require 'csv'
-require './lib/game_teams'
-require './lib/games'
+require './lib/team'
+require './lib/game'
+require './lib/game_team'
+require './lib/raw_stats'
 require './lib/stat_tracker'
-require './lib/teams'
