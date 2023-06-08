287b953d8762a4a1f9e3f0d2205732595789faca
Justin Pasko <jpasko1112@gmail.com>
2023-06-08 18:13:51 -0400

    feat: Add initialize method and instance variables.

--- lib/season.rb
+++ lib/season.rb
@@ -1,2 +1,24 @@
 # This class will pull from game_teams.csv.
 # We need game_id, team_id, HoA, result, head_coach, goals, shots, tackles.
+class Season
+  attr_reader :game_id,
+              :team_id,
+              :HoA,
+              :result,
+              :head_coach,
+              :goals,
+              :shots,
+              :tackles
+
+  def initialize(data)
+    @game_id = data[:game_id]
+    @team_id = data[:team_id]
+    @HoA = data[:HoA]
+    @result = data[:result]
+    @head_coach = data[:head]
+    @goals = data[:goals]
+    @shots = data[:shots]
+    @tackles = data[:tackles]
+  end
+end
+
