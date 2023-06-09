521aa714e23a95b99d799e89b16a7578bc097aab
Anna Wiley <awiley33@gmail.com>
2023-06-08 20:58:40 -0500

    feat: Add attr reader for game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,14 @@
 class Game
-  
+  attr_reader :game_id,
+              :season,
+              :type,
+              :date_time,
+              :away_team_id,
+              :home_team_id,
+              :away_goals,
+              :home_goals,
+              :venue
+
   def initialize(data)
     @game_id = data[:game_id]
     @season = data[:season]
@@ -7,8 +16,8 @@ class Game
     @date_time = data[:date_time]
     @away_team_id = data[:away_team_id]
     @home_team_id = data[:home_team_id]
-    @away_goals = data[:away_goals]
-    @home_goals = data[:home_goals]
+    @away_goals = data[:away_goals].to_i
+    @home_goals = data[:home_goals].to_i
     @venue = data[:venue]
     @venue_link = data[:venue_link]
   end
