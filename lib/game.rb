fb6d7d6f2a3f61852c7a8d2bb8308f638e63f44f
Anna Wiley <awiley33@gmail.com>
2023-06-09 10:35:06 -0500

    feat: Update var names to group norms

--- lib/game.rb
+++ lib/game.rb
@@ -1,24 +1,25 @@
 class Game
   attr_reader :game_id,
-              :season,
-              :type,
-              :date_time,
+              :season_id,
+              :season_type,
+              :game_date,
               :away_team_id,
               :home_team_id,
               :away_goals,
               :home_goals,
-              :venue
+              :venue_name,
+              :venue_link
 
   def initialize(data)
     @game_id = data[:game_id]
-    @season = data[:season]
-    @type = data[:type]
-    @date_time = data[:date_time]
+    @season_id = data[:season]
+    @season_type = data[:type]
+    @game_date = data[:date_time]
     @away_team_id = data[:away_team_id]
     @home_team_id = data[:home_team_id]
     @away_goals = data[:away_goals].to_i
     @home_goals = data[:home_goals].to_i
-    @venue = data[:venue]
+    @venue_name = data[:venue]
     @venue_link = data[:venue_link]
   end
 end
