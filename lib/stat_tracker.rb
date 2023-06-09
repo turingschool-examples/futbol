f92186fbdc7a508226d591cedf51754fdd02102b
Derek Chavez <frrenose17@yahoo.com>
2023-06-09 17:40:39 -0600

    feat: refactor most/fewest tackles methods

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -243,27 +243,15 @@ class StatTracker
 
   def most_tackles(season)
     season_data = find_season(season)
-    
-    team_id_with_most_tackles = season_data.game_teams.max_by { |game_team| game_team.tackles }.team_id
+    team_id_with_most_tackles = season_data.team_tackles.max_by { |team_id, tackles| tackles }[0]
     team_with_most_tackles = @teams.find { |team| team.id == team_id_with_most_tackles }
-
     team_with_most_tackles.team_name
   end
 
   def fewest_tackles(season)
     season_data = find_season(season)
-    team_tackles = Hash.new(0)
-  
-    season_data.game_teams.each do |game_team|
-      team_id = game_team.team_id
-      tackles = game_team.tackles
-  
-      team_tackles[team_id] += tackles
-    end
-  
-    team_id_with_fewest_tackles = team_tackles.min_by { |_, tackles| tackles }[0]
+    team_id_with_fewest_tackles = season_data.team_tackles.min_by { |team_id, tackles| tackles }[0]
     team_with_fewest_tackles = @teams.find { |team| team.id == team_id_with_fewest_tackles }
-  
     team_with_fewest_tackles.team_name
   end
 end
