f53db2b799ed43575e0fc5eebb3d8b6f32fc3379
Anna Wiley <awiley33@gmail.com>
2023-06-09 10:35:22 -0500

    feat: Update var names to group norms

--- lib/game_team.rb
+++ lib/game_team.rb
@@ -2,14 +2,14 @@ class GameTeam
   attr_reader :game_id,
               :team_id,
               :home_or_away,
-              :result,
-              :settled_in,
-              :head_coach,
+              :game_result,
+              :reg_or_ot,
+              :coach,
               :goals,
               :shots,
               :tackles,
               :pim,
-              :power_play_opportunities,
+              :power_play_opp,
               :power_play_goals,
               :face_off_win_percentage,
               :giveaways,
@@ -19,14 +19,14 @@ class GameTeam
     @game_id = data[:game_id]
     @team_id = data[:team_id]
     @home_or_away = data[:hoa]
-    @result = data[:result]
-    @settled_in = data[:settled_in]
-    @head_coach = data[:head_coach]
+    @game_result = data[:result]
+    @reg_or_ot = data[:settled_in]
+    @coach = data[:head_coach]
     @goals = data[:goals]
     @shots = data[:shots]
     @tackles = data[:tackles]
     @pim = data[:pim]
-    @power_play_opportunities = data[:powerplayopportunities]
+    @power_play_opp = data[:powerplayopportunities]
     @power_play_goals = data[:powerplaygoals]
     @face_off_win_percentage = data[:faceoffwinpercentage]
     @giveaways = data[:giveaways]
