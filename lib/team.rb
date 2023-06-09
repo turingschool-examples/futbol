9ffc91cbbe076f38da147d7049ce9ec2bcbe0503
Anna Wiley <awiley33@gmail.com>
2023-06-09 10:35:36 -0500

    feat: Update var names to group norms

--- lib/team.rb
+++ lib/team.rb
@@ -3,16 +3,17 @@
 class Team
   attr_reader :team_id,
               :franchise_id,
-              :name,
-              :abbreviation,
-              :stadium
-              
+              :team_name,
+              :team_abbreviation,
+              :team_stadium,
+              :team_link
+
   def initialize(data)
     @team_id = data[:team_id]
     @franchise_id = data[:franchiseid]
-    @name = data[:teamname]
-    @abbreviation = data[:abbreviation]
-    @stadium = data[:stadium]
-    @link = data[:link]
+    @team_name = data[:teamname]
+    @team_abbreviation = data[:abbreviation]
+    @team_stadium = data[:stadium]
+    @team_link = data[:link]
   end
 end
