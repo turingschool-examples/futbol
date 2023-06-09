29524b8954c1fbfb538da5f19035f949036b0694
Ethan Black <ethanrossblack@gmail.com>
2023-06-09 15:03:46 -0600

    fix: rename variables

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -25,8 +25,9 @@ RSpec.describe StatTracker do
   end
 
   describe "#Create_games" do 
-    before(:each) do 
-      @game_data = {
+
+    it "can create games" do 
+      expected_game_data = {
         away_goals: "2",
         away_team_id: "3",
         date_time: "5/16/13",
@@ -38,28 +39,26 @@ RSpec.describe StatTracker do
         venue: "Toyota Stadium",
         venue_link: "/api/v1/venues/null"
       }
-    end
-
-    it "can create games" do 
+      
       test_game = @stat_tracker.games.first
 
       expect(test_game).to be_a(Game)
-      expect(test_game.away_goals).to eq(@game_data[:away_goals])
-      expect(test_game.away_team_id).to eq(@game_data[:away_team_id])
-      expect(test_game.date_time).to eq(@game_data[:date_time])
-      expect(test_game.game_id).to eq(@game_data[:game_id])
-      expect(test_game.home_goals).to eq(@game_data[:home_goals])
-      expect(test_game.home_team_id).to eq(@game_data[:home_team_id])
-      expect(test_game.season).to eq(@game_data[:season])
-      expect(test_game.type).to eq(@game_data[:type])
-      expect(test_game.venue).to eq(@game_data[:venue])
-      expect(test_game.venue_link).to eq(@game_data[:venue_link])
+      expect(test_game.away_goals).to eq(expected_game_data[:away_goals])
+      expect(test_game.away_team_id).to eq(expected_game_data[:away_team_id])
+      expect(test_game.date_time).to eq(expected_game_data[:date_time])
+      expect(test_game.game_id).to eq(expected_game_data[:game_id])
+      expect(test_game.home_goals).to eq(expected_game_data[:home_goals])
+      expect(test_game.home_team_id).to eq(expected_game_data[:home_team_id])
+      expect(test_game.season).to eq(expected_game_data[:season])
+      expect(test_game.type).to eq(expected_game_data[:type])
+      expect(test_game.venue).to eq(expected_game_data[:venue])
+      expect(test_game.venue_link).to eq(expected_game_data[:venue_link])
     end
   end
   
   describe "#Create_teams" do
     it "can create a team" do 
-      team_data = {
+      expected_team_data = {
         abbreviation: "ATL",
         franchise_id: "23",
         link: "/api/v1/teams/1",
@@ -71,19 +70,19 @@ RSpec.describe StatTracker do
       test_team = @stat_tracker.teams.first
 
       expect(test_team).to be_a(Team)
-      expect(test_team.abbreviation).to eq(team_data[:abbreviation])
-      expect(test_team.franchise_id).to eq(team_data[:franchise_id])
-      expect(test_team.link).to eq(team_data[:link])
-      expect(test_team.stadium).to eq(team_data[:stadium])
-      expect(test_team.team_id).to eq(team_data[:team_id])
-      expect(test_team.team_name).to eq(team_data[:team_name])
+      expect(test_team.abbreviation).to eq(expected_team_data[:abbreviation])
+      expect(test_team.franchise_id).to eq(expected_team_data[:franchise_id])
+      expect(test_team.link).to eq(expected_team_data[:link])
+      expect(test_team.stadium).to eq(expected_team_data[:stadium])
+      expect(test_team.team_id).to eq(expected_team_data[:team_id])
+      expect(test_team.team_name).to eq(expected_team_data[:team_name])
 
     end
   end
 
   describe "#Create_game_teams" do
     it "can create a game_team" do 
-      game_team_data = {
+      expected_game_team_data = {
         face_off_win_percentage: "44.8",
         game_id: "2012030221",
         giveaways: "17",
@@ -104,21 +103,21 @@ RSpec.describe StatTracker do
       test_game_team = @stat_tracker.game_teams.first
 
       expect(test_game_team).to be_a(GameTeam)
-      expect(test_game_team.face_off_win_percentage).to eq(game_team_data[:face_off_win_percentage])
-      expect(test_game_team.game_id).to eq(game_team_data[:game_id])
-      expect(test_game_team.giveaways).to eq(game_team_data[:giveaways])
-      expect(test_game_team.goals).to eq(game_team_data[:goals])
-      expect(test_game_team.head_coach).to eq(game_team_data[:head_coach])
-      expect(test_game_team.hoa).to eq(game_team_data[:hoa])
-      expect(test_game_team.pim).to eq(game_team_data[:pim])
-      expect(test_game_team.power_play_goals).to eq(game_team_data[:power_play_goals])
-      expect(test_game_team.power_play_opportunities).to eq(game_team_data[:power_play_opportunities])
-      expect(test_game_team.result).to eq(game_team_data[:result])
-      expect(test_game_team.settled_in).to eq(game_team_data[:settled_in])
-      expect(test_game_team.shots).to eq(game_team_data[:shots])
-      expect(test_game_team.tackles).to eq(game_team_data[:tackles])
-      expect(test_game_team.takeaways).to eq(game_team_data[:takeaways])
-      expect(test_game_team.team_id).to eq(game_team_data[:team_id])
+      expect(test_game_team.face_off_win_percentage).to eq(expected_game_team_data[:face_off_win_percentage])
+      expect(test_game_team.game_id).to eq(expected_game_team_data[:game_id])
+      expect(test_game_team.giveaways).to eq(expected_game_team_data[:giveaways])
+      expect(test_game_team.goals).to eq(expected_game_team_data[:goals])
+      expect(test_game_team.head_coach).to eq(expected_game_team_data[:head_coach])
+      expect(test_game_team.hoa).to eq(expected_game_team_data[:hoa])
+      expect(test_game_team.pim).to eq(expected_game_team_data[:pim])
+      expect(test_game_team.power_play_goals).to eq(expected_game_team_data[:power_play_goals])
+      expect(test_game_team.power_play_opportunities).to eq(expected_game_team_data[:power_play_opportunities])
+      expect(test_game_team.result).to eq(expected_game_team_data[:result])
+      expect(test_game_team.settled_in).to eq(expected_game_team_data[:settled_in])
+      expect(test_game_team.shots).to eq(expected_game_team_data[:shots])
+      expect(test_game_team.tackles).to eq(expected_game_team_data[:tackles])
+      expect(test_game_team.takeaways).to eq(expected_game_team_data[:takeaways])
+      expect(test_game_team.team_id).to eq(expected_game_team_data[:team_id])
     end
   end
 end
