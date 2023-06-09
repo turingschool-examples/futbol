b3152d2dbc781b4a57832fae0dc855ab80b40c34
Connor Richmond <richmond.connor@gmail.com>
2023-06-09 14:26:23 -0400

    Fix: Rename to fit new StatTracker class
    Co-authored-by: Connor <richmond.connor@gmail.com>
    Co-authored-by: Ethan <ethanrossblack@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require 'simplecov'
 SimpleCov.start
 # keep at top
-require './lib/stats_manager'
+require './lib/stat_tracker'
 require './lib/game'
 require './lib/team'
 require './lib/game_team'
