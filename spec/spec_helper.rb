2ffa4ed5b956fb20f8a071500177a0648bc6ddee
Zanna Fitch <zannafitch2004@gmail.com>
2023-06-09 10:17:08 -0500

    add initial set up

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,11 @@
 require 'simplecov'
 SimpleCov.start
+
+require 'RSpec'
+require 'pry'
+require './lib/team'
+require './lib/game'
+require './lib/game_team'
+require 'CSV'
+# require 'runner'
+require 'stat_tracker'
