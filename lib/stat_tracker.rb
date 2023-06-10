848bab0532acdf12e620e4f3eb2b8ded38b2563d
Captainlearyo <clayoleary@gmail.com>
2023-06-10 17:55:39 -0500

    added sums

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -94,5 +94,42 @@ class StatTracker
     team_goals
   end
 
+  def highest_sum(arr) 
+    hash = {}
+    arr.each do |i|
+    if hash.key?(i[:home_team_id])
+      hash[i[:home_team_id]] += i[:home_goals].to_i
+    else
+      hash[i[:home_team_id]] = i[:home_goals].to_i
+    end
+
+    if hash.key?(i[:away_team_id])
+      hash[i[:away_team_id]] += i[:away_goals].to_i
+    else
+      hash[i[:away_team_id]] = i[:away_goals].to_i
+    end
+
+    end
+    hash.values.max
+  end
+
+  def lowest_sum(arr) 
+    hash = {}
+    arr.each do |i|
+    if hash.key?(i[:home_team_id])
+      hash[i[:home_team_id]] += i[:home_goals].to_i
+    else
+      hash[i[:home_team_id]] = i[:home_goals].to_i
+    end
+
+    if hash.key?(i[:away_team_id])
+      hash[i[:away_team_id]] += i[:away_goals].to_i
+    else
+      hash[i[:away_team_id]] = i[:away_goals].to_i
+    end
+
+    end
+    hash.values.min
+  end
   
 end
