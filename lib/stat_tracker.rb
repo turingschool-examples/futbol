efe115ea85cea5e50a19e073ca0ae73a104a8282
Will Chen <wfchen1990@gmail.com>
2023-08-01 16:35:34 -0600

    Add helper class

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,4 +1,5 @@
-require 'csv'
+require_relative 'helper_class'
+
 class StatTracker
   attr_reader :data, 
               :team_file,
@@ -6,13 +7,20 @@ class StatTracker
               :game_team_file
 
   def initialize(data)
-    # @data = data
+    @data = data
     @game_file = CSV.open data[:games], headers: true, header_converters: :symbol
     @team_file = CSV.open data[:teams], headers: true, header_converters: :symbol
     @game_team_file = CSV.open data[:game_teams], headers: true, header_converters: :symbol
+    @teams = []
   end
   
   def self.from_csv(locations)
     StatTracker.new(locations)
+  end
+
+  def create_teams
+    @team_file.each do |team|
+      @teams << Teams.new(team)
+    end
   end
 end
