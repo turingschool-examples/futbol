92cef1969f10fd88a2e1bb965be96a383c9fc57d
Will Chen <wfchen1990@gmail.com>
2023-08-01 16:22:54 -0600

    Add team spec

---
+++ spec/team_spec.rb
@@ -0,0 +1,24 @@
+require_relative 'spec_helper'
+
+RSpec.describe Team do
+  let(:team) { Team.new({team_id: 1, teamname: "Atlanta United"}) }
+
+  describe '#exists' do
+    it 'initializes' do
+      expect(team).to be_a Team
+    end
+  end
+
+  describe '#teamname' do
+    it 'reads the team name' do
+      expect(team.teamname).to eq("Atlanta United")
+    end
+    
+  end
+  
+  describe '#team_id' do
+    it 'reads the team id' do
+      expect(team.team_id).to eq(1)
+    end  
+  end
+end
