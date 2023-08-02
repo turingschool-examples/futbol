11128857b9fe78b4ceaf0259e77bbe7507957040
Antoine Aube <aaube3@gmail.com>
2023-08-02 12:02:10 -0600

    feat: team class initialized with attributes

---
+++ lib/team.rb
@@ -0,0 +1,17 @@
+class Team
+  attr_reader :team_id, 
+              :franchise_id, 
+              :team_name, 
+              :abbreviation, 
+              :stadium, 
+              :link
+
+  def initialize(row)
+    @team_id = row[:team_id]
+    @franchise_id = row[:franchiseid]
+    @team_name = row[:teamname]
+    @abbreviation = row[:abbreviation]
+    @stadium = row[:stadium]
+    @link = row[:link]
+  end
+end
