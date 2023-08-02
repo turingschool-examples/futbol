2194ecd586aaa0c592fc4b7cab6555a818fde745
Joop Stark <stark.joop@gmail.com>
2023-08-01 22:18:40 -0600

    refactor: fix spaces

--- spec/game_team_spec.rb
+++ spec/game_team_spec.rb
@@ -1,7 +1,7 @@
 require_relative 'spec_helper'
 
 describe GameTeam do
-  let (gameteam1) {gameteam1 = Game.new({ game_id: 123456, 
+  let (:gameteam1) {gameteam1 = GameTeam.new({ game_id: 123456, 
                                           hoa: "home",
                                           team_id: 7337,
                                           results: "WIN",
@@ -14,12 +14,14 @@ describe GameTeam do
     it "exists" do
       expect(gameteam1).to be_a GameTeam
     end
+
     it "has attributes" do
       expect(gameteam1.game_id).to eq(123456)
       expect(gameteam1.team_id).to eq(7337)
       expect(gameteam1.home).to be true
       expect(gameteam1.result).to eq("WIN")
       expect(gameteam1.coach).to eq("Hank Hill")
+      expect(gameteam1.goals).to eq(2)
       expect(gameteam1.teamname).to eq("chinchillas")
     end
   end
