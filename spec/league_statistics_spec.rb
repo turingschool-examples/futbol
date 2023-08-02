1becb72b72f6a0343176ea2c3991275555d6254c
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-08-02 14:16:36 -0600

    Make adjustment to file path to create correct data

--- spec/league_statistics_spec.rb
+++ spec/league_statistics_spec.rb
@@ -2,12 +2,21 @@ require "spec_helper"
 
 RSpec.describe LeagueStatistics do
   before(:each) do 
+    @teams_csv_file_path = ("./data/teams.csv")
     @game_teams_fixture_file_path = ("./fixture/game_teams_fixture.csv")
+    @game_fixture_file_path = ("./fixture/game_fixture.csv")
+
   end
 
   it "can show total number of teams" do 
-    league = LeagueStatistics.new(@game_teams_fixture_file_path)
+    league = LeagueStatistics.new(@teams_csv_file_path)
 
     expect(league.count_of_teams).to eq(19)
+  end
+
+  xit "can show the name of the team with the highest average number of goals scored per game across all seasons " do 
+    league = LeagueStatistics.new(@game_teams_fixture_file_path)
+
+    expect(league.best_offense).to eq("")
   end
 end
