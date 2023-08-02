6a4636189bfe87887b57f8e4b753d48529c3e18c
Robert deLaguna <rjdelaguna@yahoo.com>
2023-08-02 16:51:20 -0700

    add highest total score to seasons module

--- lib/modules/seasons_module.rb
+++ lib/modules/seasons_module.rb
@@ -1,5 +1,12 @@
 require './lib/helper_class'
 
 module Seasons
-  
+  def highest_total_score
+    away_goals = Season.seasons.map { |season| season.away_goals }.flatten
+    home_goals = Season.seasons.map { |season| season.home_goals }.flatten
+    totals = [away_goals, home_goals].transpose.map { |each| each.sum }
+    totals.max
+  end
+
+
 end
