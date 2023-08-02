60ffe0295f836234503aaa4b02aca76128c8d83c
Joop Stark <stark.joop@gmail.com>
2023-08-01 20:19:22 -0600

    test: create game1 test object

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,6 +2,12 @@ require_relative 'spec_helper'
 
 describe Game do
   describe "set-up" do
+    let (:game1) {game1 = Game.new{ game_id: 123456, 
+                                    season: 20202021,
+                                    away_team_id: 7337,
+                                    away_team_goals: 100,
+                                    home_team_id: 4008,
+                                    home_team_goals: 2 }}
     it "exists" do
       expect(game1).to be_a Game
     end
