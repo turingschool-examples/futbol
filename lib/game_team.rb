7355679973879df3ebf550881dcfb3bc07082425
Joop Stark <stark.joop@gmail.com>
2023-08-01 22:14:28 -0600

    feat: initialize

---
+++ lib/game_team.rb
@@ -0,0 +1,34 @@
+require_relative 'helper_class'
+
+class GameTeam
+  attr_reader :game_id,
+              :team_id,
+              :home,
+              :result,
+              :coach,
+              :teamname
+
+  def initialize(game_team_file, team_file)
+    @game_id = game_team_file[:game_id]
+    @team_id = game_team_file[:team_id]
+    @home = game_team_file[:hoa] == "home"
+    @result = game_team_file[:results]
+    @coach = game_team_file[:coach]
+    @goals = game_team_file[:goals]
+    @teamname = find_teamname(team_file)
+  end
+
+
+
+  private
+
+  def find_teamname(team_file)
+    name = nil
+    team_file.each do |team| if team[:team_id] == @away_team_id
+      name = team[:teamname]
+      end
+    end
+    name
+  end
+
+end
