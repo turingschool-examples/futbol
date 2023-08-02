cc0e7dc2851348af50fd45845ef1e5a872f69442
Antoine Aube <aaube3@gmail.com>
2023-08-02 15:43:38 -0600

    fix: tweak some file pathing for spec harness

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,17 +1,23 @@
 require 'csv'
 require_relative 'team'
+require_relative 'game'
+require_relative "game_team"
 
 class StatTracker
   attr_reader :games, :teams, :game_teams
   def initialize(files)
-    @games = (CSV.open files[:games], headers: true, header_converters: :symbol).map do |row|
+    @games = (CSV.foreach files[:games], headers: true, header_converters: :symbol).map do |row|
       Game.new(row)
     end
-    @teams = (CSV.open files[:teams], headers: true, header_converters: :symbol).map do |row|
+    @teams = (CSV.foreach files[:teams], headers: true, header_converters: :symbol).map do |row|
       Team.new(row)
     end
-    @game_teams = (CSV.open files[:game_teams], headers: true, header_converters: :symbol).map do |row|
+    @game_teams = (CSV.foreach files[:game_teams], headers: true, header_converters: :symbol).map do |row|
       GameTeam.new(row)
     end
+  end
+
+  def self.from_csv(files)
+    StatTracker.new(files)
   end
 end
