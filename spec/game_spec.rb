a9a03aade2ff703cd499ead836a7eb6ac4033a1b
Joop Stark <stark.joop@gmail.com>
2023-08-01 20:43:09 -0600

    bug: put parenthesis around parameter

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,15 +2,17 @@ require_relative 'spec_helper'
 
 describe Game do
   describe "set-up" do
-    let (:game1) {game1 = Game.new{ game_id: 123456, 
+    let (:game1) {game1 = Game.new({ game_id: 123456, 
                                     season: 20202021,
                                     away_team_id: 7337,
                                     away_team_goals: 100,
                                     home_team_id: 4008,
-                                    home_team_goals: 2 }}
+                                    home_team_goals: 2 })}
+
     it "exists" do
       expect(game1).to be_a Game
     end
+
     it "has attribures" do
       expect(game1.game_id).to eq(123456)
       expect(game1.season).to eq(20202021)
