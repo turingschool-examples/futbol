ad4734039771e570ba487c2d16f67ad320a0453f
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-08-02 14:05:08 -0600

    Pass first spec test for 'count_of_teams' method

--- spec/league_statistics_spec.rb
+++ spec/league_statistics_spec.rb
@@ -2,10 +2,12 @@ require "spec_helper"
 
 RSpec.describe LeagueStatistics do
   before(:each) do 
-    @league = LeagueStatistics.new
+    @game_teams_fixture_file_path = ("./fixture/game_teams_fixture.csv")
   end
 
   it "can show total number of teams" do 
-    expect(@league.count_of_teams).to eq(20)
+    league = LeagueStatistics.new(@game_teams_fixture_file_path)
+
+    expect(league.count_of_teams).to eq(19)
   end
 end
