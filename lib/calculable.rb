80856d1e514e70d5b47d67bbf540d7f6496f05d1
Reed Hillmar <reedhillmar@gmail.com>
2023-08-01 21:19:45 -0600

    feat: add Calculable.average with filtering for edge cases

--- lib/calculable.rb
+++ lib/calculable.rb
@@ -10,4 +10,13 @@ module Calculable
       p "Inputs must be integers and/or floats."
     end
   end
+
+  def average(array)
+    # This method got a little complicated. The line below is meant to be a filter that removes any input that isn't an array OR is empty OR is an array that contains elements that aren't either an integer or float 
+    if !array.kind_of?(Array) || array.empty? || !array.reject {|element| element.kind_of?(Integer) || element.kind_of?(Float)}.empty?
+      p "Input must be an array of integers and/or floats."
+    else
+      (array.sum.to_f / array.count).round(2)
+    end
+  end
 end
