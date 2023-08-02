19d44041478415b52418f403c096d82cf5b79188
Joop Stark <stark.joop@gmail.com>
2023-08-01 20:09:59 -0600

    test: attributes

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,4 +2,17 @@ require_relative 'spec_helper'
 
 describe Game do
   describe "set-up" do
-    
+    it "exists" do
+      expect(game1).to be_a Game
+    end
+    it "has attribures" do
+      expect(game1.game_id).to eq(123456)
+      expect(game1.season).to eq(20202021)
+      expect(game1.away_team_id).to eq(7337)
+      expect(game1.away_team_goals).to eq(100)
+      expect(game1.home_team_id).to eq(4008)
+      expect(game1.home_team_goals).to eq(2)
+    end
+  end
+end
+
