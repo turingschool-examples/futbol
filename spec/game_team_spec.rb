3a01bc4e6a574b616584c56f48f20f371d3a02c3
Joop Stark <stark.joop@gmail.com>
2023-08-01 22:02:32 -0600

    test: add  gameteam1 test object

--- spec/game_team_spec.rb
+++ spec/game_team_spec.rb
@@ -1,9 +1,25 @@
 require_relative 'spec_helper'
 
 describe GameTeam do
-  let (gameteam1)
+  let (gameteam1) {game1 = Game.new({ game_id: 123456, 
+                                      hoa: "home",
+                                      team_id: 7337,
+                                      results: "WIN",
+                                      coach: "Hank Hill",
+                                      goals: 2 }, 
+                                      [{teamname: "chinchillas", team_id: 7337},
+                                      {teamname: "sloths", team_id: 4008}])}
+
   describe "set-up" do
-    it "exists"
-    it "has attributes"
+    it "exists" do
+      expect(gameteam1).to be_a GameTeam
+    end
+    it "has attributes" do
+      expect(gameteam1.game_id).to eq(123456)
+      expect(gameteam1.team_id).to eq(7337)
+      expect(gameteam1.home).to be true
+      expect(gameteam1.result).to eq(4008)
+      expect(gameteam1.coach).to eq(2)
+      expect(gameteam1.teamname).to eq("chinchillas")
   end
 end
