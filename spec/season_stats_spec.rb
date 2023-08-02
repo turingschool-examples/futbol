f4f2a916eaff1c678edf1704a83c78f8e939e812
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-08-02 15:04:24 -0500

    add ruby standard and update class names

--- spec/season_stats_spec.rb
+++ spec/season_stats_spec.rb
@@ -1,8 +1,7 @@
 require "csv"
 require "./lib/season_stats"
 
-
-RSpec.describe Season_Stats do
+RSpec.describe SeasonStats do
   before :each do
     # @person_1 = Attendee.new(1, "Allison", "Nguyen", "20010")
     # @person_2 = Attendee.new(4, "David", "Thomas", "7306")
@@ -18,19 +17,19 @@ RSpec.describe Season_Stats do
       # expect(@person_4).to be_an Attendee
     end
 
-    it 'has an ID number' do
+    it "has an ID number" do
       # expect(@person_1.id).to eq 1
       # expect(@person_2.id).to eq 4
       # expect(@person_3.id).to eq 5
       # expect(@person_4.id).to eq 7
     end
-    it 'has a first and last name' do
+    it "has a first and last name" do
       # expect(@person_1.name).to eq "Allison Nguyen"
       # expect(@person_2.name).to eq "David Thomas"
       # expect(@person_3.name).to eq "Chris Sackett"
       # expect(@person_4.name).to eq "Mary Kate Curry"
     end
-    it 'has a valid zip code' do
+    it "has a valid zip code" do
       # expect(@person_1.zipcode).to eq "20010"
       # expect(@person_2.zipcode).to eq "07306"
       # expect(@person_3.zipcode).to eq "00000"
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,11 +1,10 @@
-require './lib/stat_tracker'
+require "./lib/stat_tracker"
 
-game_path = './data/games.csv'
-team_path = './data/teams.csv'
-game_teams_path = './data/game_teams.csv'
-games_fixture_path = './data/games_fixture.csv'
-games_teams_fixture_path = './data/games_teams_fixture.csv'
-
+game_path = "./data/games.csv"
+team_path = "./data/teams.csv"
+game_teams_path = "./data/game_teams.csv"
+games_fixture_path = "./data/games_fixture.csv"
+games_teams_fixture_path = "./data/games_teams_fixture.csv"
 
 locations = {
   games: game_path,
@@ -21,127 +20,106 @@ stat_tracker = StatTracker.from_csv(locations)
 RSpec.describe StatTracker do
   describe "#highest_total_score" do
     it "" do
-
     end
   end
 
   describe "#lowest_total_score" do
     it "" do
-      
     end
   end
 
   describe "#percentage_home_wins" do
     it "" do
-      
     end
   end
 
   describe "#percentage_visitor_wins" do
     it "" do
-      
     end
   end
 
   describe "#percentage_ties" do
     it "" do
-      
     end
   end
 
   describe "#count_of_games_by_season" do
     it "" do
-      
     end
   end
 
   describe "#average_goals_per_game" do
     it "" do
-      
     end
   end
 
   describe "#average_goals_by_season" do
     it "" do
-      
     end
   end
 
   describe "#count_of_teams" do
     it "" do
-      
     end
   end
 
   describe "#best_offense" do
     it "" do
-      
     end
   end
 
   describe "#worst_offense" do
     it "" do
-      
     end
   end
 
   describe "#highest_scoring_visitor" do
     it "" do
-      
     end
   end
 
   describe "#highest_scoring_home_team" do
     it "" do
-      
     end
   end
 
   describe "#lowest_scoring_visitor" do
     it "" do
-      
     end
   end
 
   describe "#lowest_scoring_home_team" do
     it "" do
-      
     end
   end
 
   describe "#winningest_coach" do
     it "" do
-      
     end
   end
 
   describe "#worst_coach" do
     it "" do
-      
     end
   end
 
   describe "#most_accurate_team" do
     it "" do
-      
     end
   end
 
   describe "#least_accurate_team" do
     it "" do
-      
     end
   end
 
   describe "#most_tackles" do
     it "" do
-      
     end
   end
 
   describe "#fewest_tackles" do
     it "" do
-      
     end
   end
 end
