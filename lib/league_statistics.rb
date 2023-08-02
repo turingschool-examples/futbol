56b807100aa1813b85b5a90609256d0fb309fc35
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-08-02 13:49:57 -0600

    Begin creating basic class setup

--- lib/league_statistics.rb
+++ lib/league_statistics.rb
@@ -1,3 +1,16 @@
+require 'csv'
+
 class LeagueStatistics
-  
+  def initialize(file)
+    @file = file
+  end
+
+  def read_csv(file)
+    CSV.read(file, headers:true)
+  end
+
+  def count_of_teams 
+    teams = read_csv(@file)
+    teams.size
+  end
 end
