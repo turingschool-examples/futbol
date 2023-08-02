02c2834b87ac697c7df825632deb6363edf57613
Joop Stark <stark.joop@gmail.com>
2023-08-01 22:04:46 -0600

    test: attributes

--- spec/game_team_spec.rb
+++ spec/game_team_spec.rb
@@ -1,14 +1,14 @@
 require_relative 'spec_helper'
 
 describe GameTeam do
-  let (gameteam1) {game1 = Game.new({ game_id: 123456, 
-                                      hoa: "home",
-                                      team_id: 7337,
-                                      results: "WIN",
-                                      coach: "Hank Hill",
-                                      goals: 2 }, 
-                                      [{teamname: "chinchillas", team_id: 7337},
-                                      {teamname: "sloths", team_id: 4008}])}
+  let (gameteam1) {gameteam1 = Game.new({ game_id: 123456, 
+                                          hoa: "home",
+                                          team_id: 7337,
+                                          results: "WIN",
+                                          coach: "Hank Hill",
+                                          goals: 2 }, 
+                                          [{teamname: "chinchillas", team_id: 7337},
+                                          {teamname: "sloths", team_id: 4008}])}
 
   describe "set-up" do
     it "exists" do
@@ -18,8 +18,9 @@ describe GameTeam do
       expect(gameteam1.game_id).to eq(123456)
       expect(gameteam1.team_id).to eq(7337)
       expect(gameteam1.home).to be true
-      expect(gameteam1.result).to eq(4008)
-      expect(gameteam1.coach).to eq(2)
+      expect(gameteam1.result).to eq("WIN")
+      expect(gameteam1.coach).to eq("Hank Hill")
       expect(gameteam1.teamname).to eq("chinchillas")
+    end
   end
 end
