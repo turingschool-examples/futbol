2473017658849a1f3503636c7d40d6f7da9b2236
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-08-02 13:43:25 -0600

    Begin creating spec tests for 'league_statistics' class

--- spec/league_statistics_spec.rb
+++ spec/league_statistics_spec.rb
@@ -1,5 +1,11 @@
 require "spec_helper"
 
 RSpec.describe LeagueStatistics do
-  
+  before(:each) do 
+    @league = LeagueStatistics.new
+  end
+
+  it "can show total number of teams" do 
+    
+  end
 end
