f6a5c7d5e8b07305dc80ebdb35f7c594a45c65ad
Reed Hillmar <reedhillmar@gmail.com>
2023-08-02 14:09:47 -0600

    test: add teams class test code for initialize

--- lib/teams_factory.rb
+++ lib/teams_factory.rb
@@ -1,1 +1,18 @@
 #./lib/teams_factory.rb
+
+class TeamsFactory
+  attr_reader :teams
+
+  def initialize
+    @teams = []
+  end
+
+  def create_teams
+    contents = CSV.open "./data/teams.csv", headers: true, header_converters: :symbol
+require 'pry';binding.pry
+    contents.each do |team|
+      team_id = team[:team_id]
+      franchise_id = team[:franc]
+    end
+  end
+end
