687a35db1f1bc12490f420d95e2c3b1619a94a27
Nick Spencer <strangebrew202@gmail.com>
2023-08-02 12:10:01 -0600

    Add: Spec file for Game class,includes #initialize method with required readable attribute tests

---
+++ spec/game_spec.rb
@@ -0,0 +1,21 @@
+require './lib/game'
+
+describe Game do 
+  let(:game) {game = Game.new({ season:'20122013', away_goals: '2', home_goals: '3'})}
+  describe "#initialize" do 
+    it "has readable season" do
+
+      expect(game.season).to eq("20122013")
+    end
+
+    it "has readable away_goals" do
+
+      expect(game.away_goals).to eq(2)
+    end
+
+    it "has readable home_goals" do
+
+      expect(game.home_goals).to eq(3)
+    end
+  end
+end
