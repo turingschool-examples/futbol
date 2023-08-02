4464be348ac1f6c2a2caa104df63250708bb66f3
Terry Lamb <terrylamb@gmail.com>
2023-08-02 17:54:11 -0400

    edit: formatting tweaks / edits

--- lib/game.rb
+++ lib/game.rb
@@ -2,7 +2,15 @@
 # We will use game_id, season, away_team_id, hone_team_id, away_goals, home_goals
 
 class Game 
-  attr_reader :game_id, :season, :type, :date_time, :away_team_id, :home_team_id, :away_goals, :home_goals, :venue
+  attr_reader :game_id, 
+              :season, 
+              :type, 
+              :date_time, 
+              :away_team_id, 
+              :home_team_id, 
+              :away_goals, 
+              :home_goals, 
+              :venue
   
   def initialize(game_id, season, type, date_time, away_team_id, home_team_id, away_goals, home_goals, venue)
     @game_id = game_id
