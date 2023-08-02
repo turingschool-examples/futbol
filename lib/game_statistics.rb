9a1233e8b240d95bc5642dd70509a4d3ddb54b61
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-08-02 14:55:56 -0500

    feat: test setup and highest_total_score

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -1,3 +1,29 @@
+require 'csv'
 class GameStatistics
   
+  def highest_total_score
+    highest_total_score = 0
+  game_contents = CSV.open './fixture/games_fixture.csv', headers: true, header_converters: :symbol
+  game_contents.each do |row|
+    # game_id = row[:game_id]
+    # season = row[:season]
+    # type = row[:type]
+    # date_time = row[:date_time]
+    # away_team_id = row[:away_team_id]
+    # home_team_id = row[:home_team_id]
+    away_goals = row[:away_goals].to_i
+    home_goals = row[:home_goals].to_i
+    # venue = row[:venue]
+    # venue_link= row[:venue_link]
+    total_score = away_goals + home_goals
+    if total_score > highest_total_score
+      highest_total_score = total_score
+    end
+    # total_score.max_by |score| do
+  end
+  highest_total_score
+
+  end
+
+
 end
--- spec/game_statistics_spec.rb
+++ spec/game_statistics_spec.rb
@@ -1,5 +1,15 @@
 require "spec_helper"
 
 RSpec.describe GameStatistics do
-  
+  before :each do
+    @game_stats = GameStatistics.new
+  end
+
+  describe "#highest_total_score" do
+    it "finds the highest total score from stat data" do
+      expect(@game_stats.highest_total_score).to eq(5)
+    end
+  end
+
+
 end
