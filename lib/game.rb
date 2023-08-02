0af0244a4fe3b91af54c608efeda247ff5d9c0fd
Jimmy Trulock <ttrulock23@gmail.com>
2023-08-02 13:46:26 -0600

    feat: add game class and attributes

---
+++ lib/game.rb
@@ -0,0 +1,25 @@
+class Game
+  attr_reader :game_id, 
+              :season, 
+              :type, 
+              :date_time, 
+              :away_team_id, 
+              :home_team_id,
+              :away_goals, 
+              :home_goals, 
+              :venue, 
+              :venue_link
+
+  def initialize(row)
+    @game_id = row[:game_id]
+    @season = row[:season]
+    @type = row[:type]
+    @date_time = row[:date_time]
+    @away_team_id = row[:away_team_id]
+    @home_team_id = row[:home_team_id]
+    @away_goals = row[:away_goals]
+    @home_goals = row[:home_goals]
+    @venue = row[:venue]
+    @venue_link = row[:venue_link]
+  end
+end
