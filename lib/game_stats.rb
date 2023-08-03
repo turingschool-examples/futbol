f2bd5b076cdc4e0d57f2083e522385ee98952a93
Victor Sanchez <vsanantone@gmail.com>
2023-08-03 11:52:00 -0500

    feat: add methods for game_stats.rb

--- lib/game_stats.rb
+++ lib/game_stats.rb
@@ -2,7 +2,8 @@ require_relative 'game'
 require 'csv'
 
 class GameStats
-  attr_reader :games
+  attr_reader :games,
+              :counter
 
   def initialize(data)
     @data = CSV.parse (File.read(data)), headers: true, header_converters: :symbol
@@ -14,5 +15,66 @@ class GameStats
     data.each do |row|
       @games << Game.new(row)
     end
+  end
+
+  def highest_total_score
+    max = @games.max_by do |game|
+      game.away_goals + game.home_goals
+    end
+    max.away_goals + max.home_goals
+  end
+
+  def lowest_total_score
+    max = @games.min_by do |game|
+      game.away_goals + game.home_goals
+    end
+    max.away_goals + max.home_goals
+  end
+
+  def percentage_home_wins
+    # wins = []
+    #   percent = @games.each do |game|
+    #     if game.home_goals > game.away_goals
+    #       wins << game.home_goals
+    #     end
+    #   end
+    #   x = (wins.length.to_f) / (@games.length.to_f)
+    #   x.round(2)
+    percent = @games.find_all do |game|
+      game.home_goals > game.away_goals  
+    end
+    ((percent.length.to_f) / (@games.length.to_f)).round(2)
+    # x = (percent.length.to_f) / (@games.length.to_f)
+    # x.round(2)
+  end
+
+  def percentage_visitor_wins
+    percent = @games.find_all do |game|
+
+      game.home_goals < game.away_goals  
+    end
+    ((percent.length.to_f) / (@games.length.to_f)).round(2)
+  end
+   
+  def percentage_ties
+    percent = @games.find_all do |game|
+
+      game.home_goals == game.away_goals  
+    end
+    ((percent.length.to_f) / (@games.length.to_f)).round(2)
+  end
+
+  def average_goals_per_game
+    percent = @games.find_all do |game|
+
+      game.home_goals + game.away_goals  
+    end
+    ((percent.length.to_f) / (@games.length.to_f)).round(2)
+  end
+
+  def average_goals_per_game
+  end
+    
+  def average_goals_by_season
   end
 end
--- spec/game_stats_spec.rb
+++ spec/game_stats_spec.rb
@@ -10,56 +10,56 @@ describe GameStats do
       expect(game_stats).to be_a(GameStats)
     end
     
-    it "reads in a csv and creats game objects" do
+    it "reads in a csv and creates game objects" do
 
       expect(game_stats.games).to all be_a(Game)
     end
   end
 
   describe "#highest_total_score" do
-    xit "returns the highest combined game score" do
+    it "returns the highest combined game score" do
 
       expect(game_stats.highest_total_score).to eq(11)
     end
   end
 
   describe "#lowest_total_score" do
-    xit "returns the lowest combined game score" do
+    it "returns the lowest combined game score" do
 
       expect(game_stats.lowest_total_score).to eq(0)
     end
   end
 
   describe "#percentage_home_wins" do 
-    xit "returns a percentage representing the ratio that home team wins the game" do
+    it "returns a percentage representing the ratio that home team wins the game" do
 
       expect(game_stats.percentage_home_wins).to eq(0.44)
     end
   end
 
   describe "#percentage_visitor_wins" do
-    xit "returns the raio of games won by the visiting team" do
+    it "returns the ratio of games won by the visiting team" do
 
       expect(game_stats.percentage_visitor_wins).to eq(0.36)
     end
   end
 
   describe "#percentage_ties" do
-    xit "returns the ratio of games ending in a tie" do
+    it "returns the ratio of games ending in a tie" do
       
       expect(game_stats.percentage_ties).to eq(0.20)
     end
   end
 
   describe "#average_goals_per_game" do
-    xit "returns average goals scored per game" do
+    it "returns average goals scored per game" do
 
       expect(game_stats.average_goals_per_game).to eq(4.22)
     end
   end
 
   describe "#average_goals_by_season" do
-    xit "returns hash with average goals per game for each season" do
+    it "returns hash with average goals per game for each season" do
       expected = {
       "20122013"=>4.12,
       "20162017"=>4.23,
