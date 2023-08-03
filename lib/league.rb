8d439fdcd56306819f40a549cdee9c2c5ef04cb7
Will Chen <wfchen1990@gmail.com>
2023-08-02 22:18:12 -0600

    Refactor League

--- lib/league.rb
+++ lib/league.rb
@@ -2,21 +2,13 @@ require_relative 'helper_class'
 
 class League
   @@games = []
-  attr_reader :game_id,
-              :season,
-              :away_team_id,
+  attr_reader :away_team_id,
               :away_team_goals,
-              :away_team_name,
               :home_team_id,
               :home_team_goals,
-              :home_team_name
 
   def initialize(test_game_file)
-    # @game_id = test_game_file[:game_id]
-    # @season = test_game_file[:season]
     @away_team_id = test_game_file[:away_team_id]
-    # @home_team_name = test_game_file[:home_team_name]
-    # @away_team_name = test_game_file[:away_team_name]
     @away_team_goals = test_game_file[:away_goals].to_i
     @home_team_id = test_game_file[:home_team_id]
     @home_team_goals = test_game_file[:home_goals].to_i
