106e6ede3a113c9cc6b7bd3cbeba27550069a6f6
Jimmy Trulock <ttrulock23@gmail.com>
2023-08-02 21:05:42 -0600

    fix: change the testing data

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -10,8 +10,8 @@ RSpec.describe Game do
           date_time: "5/16/13",
           away_team_id: "3",
           home_team_id: "6",
-          away_goals: "2",
-          home_goals: "3",
+          away_goals: 2,
+          home_goals: 3,
           venue: "Toyota Stadium",
           venue_link: "/api/v1/venues/null"
         }
@@ -29,8 +29,8 @@ RSpec.describe Game do
           date_time: "5/16/13",
           away_team_id: "3",
           home_team_id: "6",
-          away_goals: "2",
-          home_goals: "3",
+          away_goals: 2,
+          home_goals: 3,
           venue: "Toyota Stadium",
           venue_link: "/api/v1/venues/null"
         }
@@ -42,8 +42,8 @@ RSpec.describe Game do
         expect(game1.date_time).to eq("5/16/13")
         expect(game1.away_team_id).to eq("3")
         expect(game1.home_team_id).to eq("6")
-        expect(game1.away_goals).to eq("2")
-        expect(game1.home_goals).to eq("3")
+        expect(game1.away_goals).to eq(2)
+        expect(game1.home_goals).to eq(3)
         expect(game1.venue).to eq("Toyota Stadium")
         expect(game1.venue_link).to eq("/api/v1/venues/null")
     end
