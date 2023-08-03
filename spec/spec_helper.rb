027376dc8beb486f8a0a9ce00b0d4c263a9b6d60
Eliza Keating <ek.elizakeating@gmail.com>
2023-08-02 21:17:29 -0600

    feat: add require csv to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,5 +2,6 @@ require "./lib/game_statistics"
 require "./lib/league_statistics"
 require "./lib/season_statistics"
 require "./lib/stat_tracker"
+require "csv"
 require 'simplecov'
 SimpleCov.start
