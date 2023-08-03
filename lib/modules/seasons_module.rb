3f9cc18e967b6363f2533d9b8b1d77a92748955b
Robert deLaguna <rjdelaguna@yahoo.com>
2023-08-02 17:07:45 -0700

    add the lowest total score method

--- lib/modules/seasons_module.rb
+++ lib/modules/seasons_module.rb
@@ -2,10 +2,17 @@ require './lib/helper_class'
 
 module Seasons
   def highest_total_score
-    away_goals = Season.seasons.map { |season| season.away_goals }.flatten
-    home_goals = Season.seasons.map { |season| season.home_goals }.flatten
+    away_goals = Season.seasons.map { |season| season.away_goals.map(&:to_i) }.flatten
+    home_goals = Season.seasons.map { |season| season.home_goals.map(&:to_i) }.flatten
     totals = [away_goals, home_goals].transpose.map { |each| each.sum }
     totals.max
+  end
+
+  def lowest_total_score
+    away_goals = Season.seasons.map { |season| season.away_goals.map(&:to_i) }.flatten
+    home_goals = Season.seasons.map { |season| season.home_goals.map(&:to_i) }.flatten
+    totals = [away_goals, home_goals].transpose.map { |each| each.sum }
+    totals.min
   end
 
 
