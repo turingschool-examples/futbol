610ea0a4568b4b565bf78d5c7daf76d66c5db352
Eliza Keating <ek.elizakeating@gmail.com>
2023-08-02 19:57:47 -0600

    feat: add least_accurate_team method

--- lib/season_statistics.rb
+++ lib/season_statistics.rb
@@ -127,7 +127,45 @@ class SeasonStatistics
     team = @teams_data.find do |row|
       row[:team_id] == accurate_team[0]
     end
-    
+
+    team[:teamname]
+  end
+
+  def least_accurate_team(season_id)
+    season_games = []
+
+    @game_data.each do |row|
+      if row[:season] == season_id
+        season_games << row
+      end
+    end
+
+    season_game_teams = []
+
+    @game_team_data.each do |row|
+      season_games.each do |game|
+        if game[:game_id] == row[:game_id]
+          season_game_teams << row
+        end
+      end
+    end
+
+    goals = Hash.new(0)
+    shots = Hash.new(0)
+
+    season_game_teams.each do |game|
+      goals[game[:team_id]] += game[:goals].to_i
+      shots[game[:team_id]] += game[:shots].to_i
+    end
+
+    least_accurate_team = goals.min_by do |team, goals|
+      (goals.to_f/shots[team].to_f * 100).to_f
+    end
+
+    team = @teams_data.find do |row|
+      row[:team_id] == least_accurate_team[0]
+    end
+
     team[:teamname]
   end
 end
