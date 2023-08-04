8503e27ed11c2a5908e2519ae1121017205785a7
Edward Rodriguez <rodriguez.avery@gmail.com>
2023-08-03 22:35:17 -0500

    update season_stats methods

--- lib/season_stats.rb
+++ lib/season_stats.rb
@@ -1,49 +1,25 @@
-require './lib/stat_daddy'
+require "./lib/stat_daddy"
 require "csv"
 require "pry"
 
 class SeasonStats < StatDaddy
-
-  # kind of pseudo coded out this stuff
-  # @games.season.sort(season)
-  #   season 12 = @games.season("20122013")
-  #   season 13 = @games.season("20132014")
-  #   season 14 = @games.season("20142015")
-  #   season 15 = @games.season("20152016")
-  #   season 16 = @games.season("20162017")
-  #   season 17 = @games.season("20172018")
-
-  def winningest_coach()
-    # percentage_home_wins
-    # percentage_visitor_wins
-    # percentage_ties
-
-  #   @games.team_id.each do |data|
-  #     loss = data.result("loss")
-  #     win = data.result("win")
-  #     tie = data.result("tie")
-
-  #     # lost + win + tie and then the percentage somehow
-
-  #   home_win_percentage = (home_wins.to_f / total_games) * 100
-  #   home_win_percentage.round(2)
-  # end
-
-   
+  def winningest_coach(season)
+    coach_wins = Hash.new(0)
+    coach_games = Hash.new(0)
   end
 
-    # Name of the Coach with the worst win percentage for the season
+  def worst_coach(season)
   end
 
-  def most_accurate_team()
+  def most_accurate_team(season)
     # Name of the Team with the best ratio of shots to goals for the season
   end
 
-  def least_accurate_team()
+  def least_accurate_team(season)
     # Name of the Team with the worst ratio of shots to goals for the season
   end
 
-  def most_tackles
+  def most_tackles(season)
     team_tackles = []
     # Name of the Team with the most tackles in the season
     @games_teams_fixture_path.each do |data|
@@ -57,7 +33,7 @@ class SeasonStats < StatDaddy
     end
   end
 
-  def fewest_tackles()
+  def fewest_tackles(season)
     @games_teams_fixture_path.each do |data|
       # Name of the Team with the fewest tackles in the season
     end
