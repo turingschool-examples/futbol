6522096e640401090c601da54b5b2c2ae0b5b7cc
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-08-04 14:37:10 -0500

    refacotor: csv open to read

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -7,9 +7,9 @@ class GameStatistics
 
   def initialize(locations)
     @locations = locations
-    @game_data = CSV.open locations[:games], headers: true, header_converters: :symbol
-    @teams_data = CSV.open locations[:teams], headers: true, header_converters: :symbol
-    @game_team_data = CSV.open locations[:game_teams], headers: true, header_converters: :symbol  
+    @game_data = CSV.read locations[:games], headers: true, header_converters: :symbol
+    @teams_data = CSV.read locations[:teams], headers: true, header_converters: :symbol
+    @game_team_data = CSV.read locations[:game_teams], headers: true, header_converters: :symbol  
   end
 
   def percentage_calculator(portion, whole)
