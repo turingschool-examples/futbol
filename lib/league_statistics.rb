6140f919cbff8d2c581554a9d3e0065433755d90
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-08-03 21:46:57 -0600

    Initial progress for helper method

--- lib/league_statistics.rb
+++ lib/league_statistics.rb
@@ -19,38 +19,10 @@ class LeagueStatistics
     team_ids.size
   end
 
+  def average_goals_by_team
+    total_goals_by_team = Hash.new(0)
+    total_games_by_team = Hash.new(0)
 
-  # def best_offense
-  #   games = read_csv(@file)
-
-  #   offense = Hash.new { |hash, key| hash[key] = [] }
     
-  #   games.each do |row|
-  #     team = row["team_id"].to_i
-  #     goals = row["goals"].to_i
-  #     offense[team] << goals
-  #   end
-
-  #   best_offense_team_id = nil
-  #   highest_average_goals = -1
-
-  #   offense.each do |team_id, goals|
-  #     average_goals = goals.sum.to_f / goals.size
-  #     if average_goals > highest_average_goals
-  #       highest_average_goals = average_goals
-  #       best_offense_team_id = team_id
-  #     end
-  #   end
-
-  #   best_offense_team = nil
-  #   teams = read_csv("./data/teams.csv")
-
-  #   teams.each do |row|
-  #     if row["team_id"].to_i == best_offense_team_id
-  #       best_offense_team = row["teamName"]
-  #     end
-  #   end
-
-  #   best_offense_team
-  # end
+  end
 end
