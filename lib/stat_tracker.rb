1986fc97fcd1713511f7d4655f7e21e718d1725b
Charles Ren <cren04@syr.edu>
2023-08-04 14:32:29 -0700

    docs: removed pseudocode

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -24,56 +24,39 @@ include LeagueStatable
   end
 
   def most_accurate_team(season)
-    # name of team with the best ratio of shots to goals for the season 
-    # 1. pull out all game_id for a season in @games as an array
-    # 2. iterate over @game_teams using our created array of game_id as reference
-      # make hash of team_id as keys and set values to [goals + itself, shots + itself]
-    # 3. use transform_values on hash to give goals / shots .round(4)
-    # 4. Find the max value and subsequent key and return the team_name with team_list
-
-    # 1. array of all game_id from a season
     all_season_game_id = @games.map do |game|
       game.game_id if game.season == season
     end.compact
 
-    # 2. hash of team_id as keys and [goals, values] as values
     team_id_goals_shots = @game_teams.each_with_object(Hash.new([0,0])) do |game, hash|
-      # if game.game_id exists in our all_season_game_id array, then create key-value pair
       if all_season_game_id.include?(game.game_id)
         hash[game.team_id] = [game.goals + hash[game.team_id][0], game.shots + hash[game.team_id][1]]
       end
     end
     
-    # 3. Calculate the average of goals/shots and assign to values
     avg_goals_made = team_id_goals_shots.transform_values do |value|
       (value[0] / value[1].to_f).round(4)
     end
 
-    # 4. Find max value and subsequent key to return the team_name with helper method team_list
     team_name = avg_goals_made.key(avg_goals_made.values.max)
     team_list[team_name]
   end
 
   def least_accurate_team(season)
-    # 1. array of all game_id from a season
     all_season_game_id = @games.map do |game|
       game.game_id if game.season == season
     end.compact
     
-    # 2. hash of team_id as keys and [goals, values] as values
     team_id_goals_shots = @game_teams.each_with_object(Hash.new([0,0])) do |game, hash|
-      # if game.game_id exists in our all_season_game_id array, then create key-value pair
       if all_season_game_id.include?(game.game_id)
         hash[game.team_id] = [game.goals + hash[game.team_id][0], game.shots + hash[game.team_id][1]]
       end
     end
     
-    # 3. Calculate the average of goals/shots and assign to values
     avg_goals_made = team_id_goals_shots.transform_values do |value|
       (value[0] / value[1].to_f).round(4)
     end
     
-    # 4. Find min value and subsequent key to return the team_name with helper method team_list
     team_name = avg_goals_made.key(avg_goals_made.values.min)
     team_list[team_name]
   end
