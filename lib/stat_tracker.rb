1323a8ac3f5d4bf69e3633449e5db0bb5bfc2739
Antoine Aube <aaube3@gmail.com>
2023-08-03 20:48:51 -0600

    feat: #highest_scoring_team_name

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -21,6 +21,30 @@ include GameStatable
     end
   end
 
+  def highest_scoring_visitor 
+    away_games_by_team_id = @games.each_with_object(Hash.new(0.0)) do |game, hash|
+      hash[game.away_team_id] += 1
+    end
+
+    away_goals_by_team_id = @games.each_with_object(Hash.new(0.0)) do |game, hash|
+      hash[game.away_team_id] += game.away_goals
+    end
+    
+    average_away_goals_by_team_id = Hash.new(0.0)
+    away_goals_by_team_id.each do |key, value|
+      average_away_goals_by_team_id[key] = (value/ away_games_by_team_id[key]).round(2)
+    end
+    average_away_goals_by_team_id
+    highest_average_scoring_team_id = average_away_goals_by_team_id.max_by {|k, v| v}.first
+    
+    highest_scoring_team = @teams.find do |team|
+      team.team_name if team.team_id == highest_average_scoring_team_id
+    end
+    team = highest_scoring_team.team_name
+    require 'pry';binding.pry
+  end
+
+
   def self.from_csv(files)
     StatTracker.new(files)
   end
