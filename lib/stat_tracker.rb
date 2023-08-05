99a981731bda4e80fe69b5153a33d9a893ad2631
Antoine Aube <aaube3@gmail.com>
2023-08-05 10:57:51 -0600

    feat: data_parsable module

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,30 +1,17 @@
-require 'csv'
+# require 'csv'
 require_relative 'team'
 require_relative 'game'
 require_relative "game_team"
 require_relative 'game_statable'
 require_relative 'league_statable'
 require_relative 'season_statable'
+require_relative 'data_parsable'
 
 class StatTracker
+include DataParsable
 include GameStatable
 include LeagueStatable
 include SeasonStatable
-
-  attr_reader :games, :teams, :game_teams
-
-  def initialize(files)
-    @games = (CSV.foreach files[:games], headers: true, header_converters: :symbol).map do |row|
-      Game.new(row)
-    end
-    @teams = (CSV.foreach files[:teams], headers: true, header_converters: :symbol).map do |row|
-      Team.new(row)
-    end
-    @game_teams = (CSV.foreach files[:game_teams], headers: true, header_converters: :symbol).map do |row|
-      GameTeam.new(row)
-    end
-  end
-
 
   def self.from_csv(files)
     StatTracker.new(files)
