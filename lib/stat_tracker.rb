157c4b07adfcbae714e1619ff2ef46497e943d3a
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-09-19 17:42:39 -0600

    feat: allow all instances of StatTracker to access CSV data

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -2,11 +2,23 @@
 require './spec/spec_helper'
 
 class StatTracker
-  attr_reader :team_data, :game, :game_teams
+  
   def initialize(locations)
-    @game = game_data_parser(locations[:games])
-    @team_data = team_data_parser(locations[:teams])
-    @game_teams = game_teams_data_parser(locations[:game_teams])
+    @@game = game_data_parser(locations[:games])
+    @@team_data = team_data_parser(locations[:teams])
+    @@game_teams = game_teams_data_parser(locations[:game_teams])
+  end
+  
+  def game
+    @@game
+  end
+
+  def team_data
+    @@team_data
+  end
+
+  def game_teams
+    @@game_teams
   end
 
   def self.from_csv(locations)
