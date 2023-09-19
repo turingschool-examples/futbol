248375ee3e0054ab13eae8ddafedf6dab893e15b
trpubz <taylor.pubins@me.com>
2023-09-18 17:19:41 -0700

    Create spec_helper.rb

---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+require "simplecov"
+SimpleCov.start do
+  enable_coverage :branch
+  add_filter "/spec/"
+  add_filter "/data/"
+end
+
+require "rspec"
+require "csv"
+require "./lib/stat_tracker"
+require "./data/"
