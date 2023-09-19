2ca3d7ed119bad07112f77e1ad84122ab9352507
Justin Lee <justinslee201100@yahoo.com>
2023-09-19 16:35:36 -0600

    add from csv method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,3 +1,20 @@
+require 'csv'
+require './runner'
+
+
 class StatTracker
 
+  def initialize
+   
+  end
+
+  def self.from_csv(locations)
+    game_data = CSV.open locations.games, headers: true, header_converters: :symbol
+    game_data.map do |row|
+      row
+    end
+    StatTracker.new
+  end
+  
 end
+
