c3edea6ff2cb28373dd4c60cbfedac1b0a39fe7a
Blaine Kennedy <blainekennedy@gmail.com>
2023-09-18 21:05:13 -0500

    feat: add #highest_total_score and #lowest_total_score

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,1 +1,41 @@
 
+require 'csv'
+
+class StatTracker
+  def self.from_csv(locations)
+    games_data = CSV.read(locations[:games], headers: true, header_converters: :symbol)
+    teams_data = CSV.read(locations[:teams], headers: true, header_converters: :symbol)
+    game_teams_data = CSV.read(locations[:game_teams], headers: true, header_converters: :symbol)
+require 'pry'; binding.pry
+    new(games_data, teams_data, game_teams_data)
+  end
+
+  def initialize(games_data, teams_data, game_teams_data)
+    @games_data = games_data
+    @teams_data = teams_data
+    @game_teams_data = game_teams_data
+  end
+
+  def highest_total_score 
+    highest_score = 0
+
+    @games_data.each do |game|
+      total_score = (game[:home_goals].to_i + game[:away_goals].to_i)
+
+      highest_score = total_score if total_score > highest_score
+    end
+    highest_score
+  end
+  def lowest_total_score 
+    lowest_score = 0 
+
+    @games_data.each do |game| 
+      total_score = (game[:home_goals].to_i + game[:away_goals].to_i)
+
+      lowest_score = total_score if total_score < lowest_score
+    end
+    lowest_score
+  end
+
+
+end
