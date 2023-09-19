e9afe13ca59575f90bd7a0e9ee0b7dbcf1cfdc0f
Kam Kennedy <kameronk013@gmail.com>
2023-09-19 15:07:39 -0600

    sync versions

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,1 +1,10 @@
+require "./spec/spec_helper"
+require "./lib/runner"
 
+RSpec.describe "StatTracker" do
+  describe "#percentage_home_wins" do
+    it "returns the percentage of home wins" do
+      expect
+    end
+  end
+end
