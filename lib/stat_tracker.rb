130519a3c0490f94c193ee360854801d56be1838
Taylor Pubins <tpubz@icloud.com>
2023-09-18 20:10:30 -0700

    Update mock data files

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,1 +1,20 @@
+require "csv"
 
+class StatTracker
+  attr_reader :games_data, :teams_data, :game_teams_data
+
+  def initialize(games_data, teams_data, game_teams_data)
+    @games_data = games_data
+    @teams_data = teams_data
+    @game_teams_data = game_teams_data
+  end
+
+  def self.from_csv(locations)
+    games_data = CSV.read(locations[:games], headers: true, header_converters: :symbol)
+    teams_data = CSV.read(locations[:teams], headers: true, header_converters: :symbol)
+    game_teams_data = CSV.read(locations[:game_teams], headers: true, header_converters: :symbol)
+
+    new(games_data, teams_data, game_teams_data)
+  end
+
+end
