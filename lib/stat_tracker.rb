08f3a7fdb0cbe474537d905ada314a11428bf721
Sam Tran <samuelttran@gmail.com>
2023-09-19 09:21:11 -0600

    Add initialize method, add ::from_csv

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,7 +1,15 @@
 class StatTracker
+  attr_reader :all_data
+  def initialize(all_data)
+    @all_data = all_data
+  end
 
-
-
-
-
+  def self.from_csv(locations)
+    all_data = {}
+    locations.each do |name, location|
+      formatted_csv = CSV.open location, headers: true, header_converters: :symbol
+      all_data[name] = formatted_csv
+    end
+    StatTracker.new(all_data)
+  end
 end
