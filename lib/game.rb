f24da06d4fcd76cb85aaf6f44be6595e64f59785
Joseph Lee <jhjlee702@gmail.com>
2023-09-20 14:55:28 -0600

    write attributes for game class

---
+++ lib/game.rb
@@ -0,0 +1,14 @@
+class Game
+  def initialize(game_info)
+    @game_id = game_info[:game_id].to_i
+    @season = game_info[:season].to_i
+    @type = game_info[:type]
+    @date_time = game_info[:date_time]
+    @away_team_id = game_info[:away_team_id].to_i
+    @home_team_id = game_info[:home_team_id].to_i
+    @away_goals = game_info[:away_goals].to_i
+    @home_goals = game_info[:home_goals].to_i
+    @venue = game_info[:venue]
+    @venue_link = game_info[:venue_link]
+  end
+end
