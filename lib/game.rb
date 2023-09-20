d298f7fc914cd7e067febe5826a4136fe06a0548
Joseph Lee <jhjlee702@gmail.com>
2023-09-20 15:08:46 -0600

    clear records to get small sample of data

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,15 @@
 class Game
+  attr_reader :game_id,
+              :season,
+              :type,
+              :date_time,
+              :away_team_id,
+              :home_team_id,
+              :away_goals,
+              :home_goals,
+              :venue,
+              :venue_link
+              
   def initialize(game_info)
     @game_id = game_info[:game_id].to_i
     @season = game_info[:season].to_i
