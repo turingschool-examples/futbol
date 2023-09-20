524f62f9540aeedc628f31486f938f2251cec6b2
Nathan Trautenberg <Natester123@mac.com>
2023-09-20 15:49:18 -0600

    Created percentage home wins method.

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -43,4 +43,34 @@ class StatTracker
     end
     sum.min
   end
+
+  def percentage_home_wins
+    hash = Hash.new{ |hash, key| hash[key] = [] }
+    stat_tracker.game_teams.each do |game|
+      @key = game[:game_id].to_i
+      @value_array = []
+      @value2 = game[:hoa]
+      @value1 = game[:goals].to_f
+      @value_array << @value1
+      @value_array << @value2
+      if hash.values.first == nil
+        hash[@key] = @value_array
+      elsif hash.values.flatten.first < @value1
+        hash[@key] = @value_array
+      else
+        hash
+      end
+    end
+    @home_win = 0
+    @total_games = 0
+    hash.values.each do |winner|
+      if winner[1] == "home"
+        @home_win += 1.0
+        @total_games += 1.0
+      else
+        @total_games += 1.0
+      end
+    end
+    @home_win / @total_games
+  end
 end
