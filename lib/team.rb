0c0d7fd196da8e9dc4210b9c49e8390f295121e8
Joseph Lee <jhjlee702@gmail.com>
2023-09-20 15:12:37 -0600

    create Team class

---
+++ lib/team.rb
@@ -0,0 +1,17 @@
+class Team
+  attr_reader :team_id,
+              :franchise_id,
+              :name,
+              :abbreviation,
+              :stadium,
+              :link
+              
+  def initialize(team_info)
+    @team_id = team_info[:team_id]
+    @franchise_id = team_info[:franchiseid]
+    @name = team_info[:teamname]
+    @abbreviation = team_info[:abbreviation]
+    @stadium = team_info[:stadium]
+    @link = team_info[:link]
+  end
+end
