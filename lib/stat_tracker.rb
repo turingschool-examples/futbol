c3f284cdc1361d085b8fccac09c61e5ad6e48b7d
Xander Hendry <vhendry.coding@gmail.com>
2023-09-20 10:49:33 -0400

    Change csv.read to csv.readlines

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -4,9 +4,9 @@ class StatTracker
   
   def self.from_csv(locations)
     content = {}
-    content[:teams] = CSV.read(locations[:teams], headers: true, header_converters: :symbol),
-    content[:games] = CSV.read(locations[:games], headers: true, header_converters: :symbol),
-    content[:game_teams] = CSV.read(locations[:game_teams], headers: true, header_converters: :symbol)
+    content[:teams] = CSV.readlines(locations[:teams], headers: true, header_converters: :symbol),
+    content[:games] = CSV.readlines(locations[:games], headers: true, header_converters: :symbol),
+    content[:game_teams] = CSV.readlines(locations[:game_teams], headers: true, header_converters: :symbol)
   
   end
 ##in pry you can then do stat_tracker[:team_id] and it will print stuff
