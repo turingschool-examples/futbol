1c79081badb496b2a0672d9528a029858d265810
Xander Hendry <vhendry.coding@gmail.com>
2023-09-19 23:11:21 -0400

    Add more thurough setup

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,12 +1,18 @@
 require 'spec_helper'
 RSpec.describe StatTracker do
-  let(:teams) { './data/teams.csv' } 
-  let(:stat_tracker) { StatTracker.from_csv(teams) } 
+  let(:game_path) { './data/test_games.csv' }
+  let(:team_path) { './data/test_teams.csv' } 
+  let(:game_teams_path) { './data/test_teams.csv' } 
+  let(:test_locations) { 
+    {games: game_path,
+    teams: team_path,
+    game_teams: game_teams_path}
+    } 
+  let(:stat_tracker) { StatTracker.from_csv(test_locations) } 
 
   describe "::from_csv" do 
     it 'will create a new instance object using data from the given csv' do
       expect(stat_tracker).to be_a(StatTracker)
-      expect(stat_tracker.contents).to eq(teams)
     end
   end
 end 
