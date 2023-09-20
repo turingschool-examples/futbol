c784df5f8a5820102e6c49415d2f881cd79e6731
Xander Hendry <vhendry.coding@gmail.com>
2023-09-20 10:40:12 -0400

    Add csv test samples, edit ::from_csv and runner file

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -2,8 +2,12 @@ require "csv"
 class StatTracker
 
   
-  def self.from_csv(file_name)
-    contents = CSV.read file_name, headers: true, header_converters: :symbol
+  def self.from_csv(locations)
+    content = {}
+    content[:teams] = CSV.read(locations[:teams], headers: true, header_converters: :symbol),
+    content[:games] = CSV.read(locations[:games], headers: true, header_converters: :symbol),
+    content[:game_teams] = CSV.read(locations[:game_teams], headers: true, header_converters: :symbol)
+  
   end
 ##in pry you can then do stat_tracker[:team_id] and it will print stuff
 end
