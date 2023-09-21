a44f575f486b110c5ee6527f5510d1281dad6347
Eric Belongea <eric.belongea@gmail.com>
2023-09-20 21:18:45 -0600

    Feat: starts winningest coach and refactors season_spec and stat_tracker

--- lib/season.rb
+++ lib/season.rb
@@ -1,6 +1,27 @@
 class Season
-  def initialize(game_team_data, team_data)
+  def initialize(game_team_data, game_data)
     @game_team_data = game_team_data
-    @team_data = team_data
+    @game_data = game_data
+  end
+
+  def winningest_coach(season)
+    total_wins = 0
+    total_games = 0
+
+    coach_hash =  Hash.new(0)
+    @game_data.each do |game_row|
+      # require 'pry'; binding.pry
+      game_row[:season] == season
+      total_games += 1
+      season_game_id = game_row[:game_id]
+      @game_team_data.each do |row|
+        if row[:game_id] == season_game_id && row[:result] == "WIN"
+          head_coach = row[:head_coach]
+          coach_hash[head_coach] += 1
+        end
+      end
+    end
+    coach_hash
+    require 'pry'; binding.pry
   end
 end
--- spec/season_spec.rb
+++ spec/season_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe StatTracker do
     }
 
     @stat_tracker = StatTracker.from_csv(locations)
-    @season = Season.new(@stat_tracker.game_team_data, @stat_tracker.team_data)  
+    @season = Season.new(@stat_tracker.game_team_data, @stat_tracker.game_data)  
   end
 
   it "exists" do
@@ -28,27 +28,27 @@ RSpec.describe StatTracker do
     expect(@season.winningest_coach("20142015")).to eq "Alain Vigneault"
   end
 
-  it "#worst_coach" do
+  xit "#worst_coach" do
     expect(@season.worst_coach("20132014")).to eq "Peter Laviolette"
     expect(@season.worst_coach("20142015")).to eq("Craig MacTavish").or(eq("Ted Nolan"))
   end
 
-  it "#most_accurate_team" do
+  xit "#most_accurate_team" do
     expect(@season.most_accurate_team("20132014")).to eq "Real Salt Lake"
     expect(@season.most_accurate_team("20142015")).to eq "Toronto FC"
   end
 
-  it "#least_accurate_team" do
+  xit "#least_accurate_team" do
     expect(@season.least_accurate_team("20132014")).to eq "New York City FC"
     expect(@season.least_accurate_team("20142015")).to eq "Columbus Crew SC"
   end
 
-  it "#most_tackles" do
+  xit "#most_tackles" do
     expect(@season.most_tackles("20132014")).to eq "FC Cincinnati"
     expect(@season.most_tackles("20142015")).to eq "Seattle Sounders FC"
   end
 
-  it "#fewest_tackles" do
+  xit "#fewest_tackles" do
     expect(@season.fewest_tackles("20132014")).to eq "Atlanta United"
     expect(@season.fewest_tackles("20142015")).to eq "Orlando City SC"
   end
