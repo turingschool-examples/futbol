70415e1d81f02818f2bbd7869d63cb710231e8bb
Kevin Zolman <kevin.zolman@gmail.com>
2023-09-21 14:02:24 -0600

    Feat: add logic to #highest_scoring_visitor in league (still bugged)

--- lib/league.rb
+++ lib/league.rb
@@ -18,14 +18,37 @@ class League
   # games and scores, total scores and 
 
   def highest_scoring_visitor
-    # Name of the team with the highest average score per game across all seasons when they are away
     # calc average score for each team's away goals, store in hash with team_id => ave_score
-    # determine highest average
-    # link team_id to team_name
-    hash = {}
-    @game_data.each do |game|
-      
-      team_id = game[:team_id] 
+
+    # count total goals per game
+    # this could potentially be a helper method
+    visitor_goals = Hash.new(0)
+    @game_team_data.each do |game|
+      home_or_away = game[:hoa]
+      goals = game[:goals].to_i
+      team_id = game[:team_id]
+      if home_or_away == "away"
+        visitor_goals[team_id] += goals
+      end
     end
+
+    # count total games per team
+    
+
+    # determine highest average
+    # this could potentially be a helper method
+    highest_scoring_id = visitor_goals.max_by do |team_id, goals|
+      goals
+    end
+
+    # link team_id to team_name
+    # this could potentially be a helper method
+    highest_scoring_name = ""
+    @team_data.each do |team|
+      if highest_scoring_id[0] == team[:team_id]
+        highest_scoring_name += team[:team_name]
+      end
+    end
+    highest_scoring_name
   end
 end
