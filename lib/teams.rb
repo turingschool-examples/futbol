f834fef703604078d4f1c73adc5fec9b33bb81ab
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-09-21 14:45:39 -0600

    Debug: Standardize game_team and team

--- lib/teams.rb
+++ lib/teams.rb
@@ -1,17 +1,17 @@
 class Team
   attr_reader :game_id,
-              :franchiseId,
-              :teamName,
+              :franchise_id,
+              :team_name,
               :abbreviation,
-              :Stadium,
+              :stadium,
               :link
 
   def initialize(attributes)
-    @game_id = attributes[:game_id]
-    @franchiseId = attributes[:franchiseId]
-    @teamName = attributes[:teamName]
+    @game_id      = attributes[:game_id]
+    @franchise_id = attributes[:franchiseid]
+    @team_name    = attributes[:teamname]
     @abbreviation = attributes[:abbreviation]
-    @Stadium = attributes[:Stadium]
-    @link = attributes[:link]
+    @stadium      = attributes[:stadium]
+    @link         = attributes[:link]
   end
 end
