46071c3efc02a00c097216d40897ddd82cf1c054
Eric Belongea <eric.belongea@gmail.com>
2023-09-21 12:38:51 -0600

    Fix: spelling errors in winningest_coach

--- lib/season.rb
+++ lib/season.rb
@@ -5,32 +5,28 @@ class Season
   end
 
   def winningest_coach(season)
-    
-    total_games = 0
-
-    coach_hash_winnnings =  Hash.new(0)
     coach_hash_total = Hash.new(0)
-
+    coach_hash_winnings =  Hash.new(0)
+    # total_games = 0
     @game_data.each do |game_row|
-      # require 'pry'; binding.pry
       if game_row[:season] == season
-        #Total Games is only for ensuring that the season has been successfully isolated.
-        total_games += 1 
+        # total_games += 1 #Total Games is only for ensuring that the season has been successfully isolated.
         season_game_id = game_row[:game_id]
         @game_team_data.each do |row|
           if row[:game_id] == season_game_id && row[:result] == "WIN"
             head_coach = row[:head_coach]
-            coach_hash_winnnings[head_coach] += 1
-          elsif row[:game_id] == season_game_id 
-            head_coach = row[:head_coach]
-            coach_hash_total[head_coach] += 1
-           #Perhaps I want to make an object that accumulates the wins and losses?
+            coach_hash_winnings[head_coach] += 1
+            if row[:game_id] == season_game_id 
+              head_coach = row[:head_coach]
+              coach_hash_total[head_coach] += 1
+            end
           end
         end
       end
     end
-    coach_hash_winnnings
-    coach_hash_total
+    coach_hash_winnings.sort
+    coach_hash_total.sort
     require 'pry'; binding.pry
   end
 end
+
