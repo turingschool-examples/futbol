46db7b283c74adafaa6b11eb7cf6cb5a3fb79988
Nathan Trautenberg <Natester123@mac.com>
2023-09-21 13:30:38 -0600

    Made new branch to merge method to main.

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -101,8 +101,7 @@ class StatTracker
     x = @tie / @total_games
     (x * 100).round(2)
   end
-#will return a hash with season names as keys, 
-#and a float representing the average number of goals in a game for that season as values
+
   def average_goals_by_season
     hash = Hash.new{ |hash, key| hash[key] = 0.00 }
     @total_goals = 0.00
