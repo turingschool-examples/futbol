ac4f5aea47ee2a2b213b2de10966d0fb60006b2a
Miranda Delaney <delaney.miranda1@gmail.com>
2023-09-20 22:12:54 -0400

    fix: merging duplication and order errors

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -40,28 +40,6 @@ class StatTracker
     contents.readlines
   end
 
-  def percentage_visitor_wins
-    count = 0
-    game.each do |single_game|
-      if single_game[:away_goals].to_i > single_game[:home_goals].to_i
-        count +=1
-      end
-    end
-    percentage = (count.to_f / game.length).round(2)
-  end
-
-  def percentage_home_wins #(testing = false)
-    #testing ? data = game.take(10) : data = game
-    count = 0
-    game.each do |single_game|
-      if single_game[:home_goals].to_i > single_game[:away_goals].to_i
-        count += 1
-      end
-    end
-    percentage = (count.to_f / game.count).round(2)
-    
-  end
-
   def highest_total_score
     highest_score = 0
     game.each do |goals|
@@ -74,6 +52,36 @@ class StatTracker
     highest_score
   end 
 
+  def lowest_total_score
+    total_scores = game.map do |game|
+      home_score = game[:home_goals].to_i 
+      away_score = game[:away_goals].to_i
+      total_score = home_score + away_score
+    end
+    total_scores.sort.first
+  end
+
+  def percentage_home_wins 
+    count = 0
+    game.each do |single_game|
+      if single_game[:home_goals].to_i > single_game[:away_goals].to_i
+        count += 1
+      end
+    end
+    percentage = (count.to_f / game.count).round(2)
+    
+  end
+
+  def percentage_visitor_wins
+    count = 0
+    game.each do |single_game|
+      if single_game[:away_goals].to_i > single_game[:home_goals].to_i
+        count +=1
+      end
+    end
+    percentage = (count.to_f / game.length).round(2)
+  end
+
   def percentage_ties 
     count = 0
     game.each do |single_game|
@@ -84,21 +92,18 @@ class StatTracker
     percentage = (count.to_f / game.count).round(2)
   end
   
-  def lowest_total_score(testing = false)
-    testing ? data = game.take(20) : data = game
-    total_scores = data.map do |game|
-      home_score = game[:home_goals].to_i 
-      away_score = game[:away_goals].to_i
-      total_score = home_score + away_score
+  def count_of_games_by_season
+    counts = Hash.new(0)
+    game.each do |single_game|
+      counts[single_game[:season]] += 1
     end
-    total_scores.sort.first
+    counts
   end
 
-  def average_goals_per_game(testing = false)
-    testing ? data = game_teams.take(20) : data = game_teams
-    numerator = data.sum { |game| game[:goals].to_i }.to_f
-    numerator / data.count
-  end 
+  def average_goals_per_game
+    numerator = game_teams.sum { |game| game[:goals].to_i }.to_f
+    (numerator / game_teams.count).round(2)
+  end
 
   def average_goals_by_season
     goals = Hash.new { |hash, season| hash[season] = [] }
@@ -118,6 +123,18 @@ class StatTracker
 
   def count_of_teams
     team_data.count
+  end
+
+  def best_offense
+    team_number = games_and_scores.sort_by { |team, data| data[:average] }.last[0]
+    
+    best_offense = ""
+    team_data.each do |team|
+      if team[:team_id] == team_number
+        best_offense << team[:teamname] 
+      end
+    end
+    best_offense
   end
 
   def worst_offense
@@ -163,14 +180,6 @@ class StatTracker
       end 
     end
     total_score
-  end
-
-  def count_of_games_by_season
-    counts = Hash.new(0)
-    game.each do |single_game|
-      counts[single_game[:season]] += 1
-    end
-    counts
   end
 
   def lowest_scoring_home_team
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -27,27 +27,9 @@ RSpec.describe StatTracker do
     end
   end
 
-  describe '#percentage_visitor_wins' do
-    it 'calculates the percentage of visitor wins' do
-      expect(stat_tracker.percentage_visitor_wins).to eq(0.20)
-    end
-  end
-
-  describe '#percentage_home_wins' do
-    it 'calculates percentage home wins' do
-      expect(stat_tracker.percentage_home_wins).to eq(0.45)
-    end
-  end
-
   describe '#highest_total_score' do
     it 'returns the highest sum of the winning and losing teams scores' do
       expect(stat_tracker.highest_total_score).to eq(6)
-    end
-  end
-
-  describe '#percentage_ties' do
-    it 'calculates the percentage of tied games' do
-      expect(stat_tracker.percentage_ties).to eq(0.35)
     end
   end
 
@@ -57,17 +39,38 @@ RSpec.describe StatTracker do
     end
   end
 
+  describe '#percentage_home_wins' do
+    it 'calculates percentage home wins' do
+      expect(stat_tracker.percentage_home_wins).to eq(0.45)
+    end
+  end
+
+  describe '#percentage_visitor_wins' do
+    it 'calculates the percentage of visitor wins' do
+      expect(stat_tracker.percentage_visitor_wins).to eq(0.20)
+    end
+  end
+
+  describe '#percentage_ties' do
+    it 'calculates the percentage of tied games' do
+      expect(stat_tracker.percentage_ties).to eq(0.35)
+    end
+  end
+
+  describe '#count_of_games_by_season' do
+    it 'counts games by season' do
+      expected = {
+        "20122013" => 6,
+        "20132014" => 9,
+        "20142015" => 5,
+      }
+      expect(stat_tracker.count_of_games_by_season).to eq(expected)
+    end
+  end
 
   describe '#average_goals_per_game' do
     it 'returns the average number of goals scored by a single team' do
       expect(stat_tracker.average_goals_per_game).to eq(1.98)
-    end
-  end
-
-  describe '#average_goals_by_season' do
-    it 'returns the average goals scored per season' do
-      expected_value = { '20122013' => 3.67, '20132014' => 3.78, '20142015' => 4.60 }
-      expect(stat_tracker.average_goals_by_season).to eq(expected_value)
     end
   end
 
@@ -84,10 +87,9 @@ RSpec.describe StatTracker do
     end
   end
 
-  describe '#average_goals_by_season' do
-    it 'returns the average goals scored per season' do
-      expected_value = { '20122013' => 3.86, '20142015' => 3.5, '20162017' => 4.75 }
-      expect(stat_tracker.average_goals_by_season).to eq(expected_value)
+  describe '#best_offense' do
+    it 'list best offense' do
+      expect(stat_tracker.best_offense).to eq("Houston Dynamo")
     end
   end
 
@@ -97,28 +99,13 @@ RSpec.describe StatTracker do
     end
   end
 
-  describe '#count_of_games_by_season' do
-    it 'counts games by season' do
-      expected = {
-        "20122013" => 6,
-        "20132014" => 9,
-        "20142015" => 5,
-      }
-      expect(stat_tracker.count_of_games_by_season).to eq(expected)
-    end
-  end
+  # Add tests for best/worst offence helper methods
 
   describe '#lowest_scoring_home_team' do
     it 'returns name of the team with the lowest average score per home game across all seasons' do
       expect(stat_tracker.lowest_scoring_home_team).to eq('Seattle Sounders FC')
     end
   end
-  # Test for League Statistics
 
-  describe '#best_offense' do
-    it 'list best offense' do
-      expect(stat_tracker.best_offense).to eq("Houston Dynamo")
-    end
-  end
-
+  # Add test for home team helper method
 end
