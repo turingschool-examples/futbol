4f6608dca85201d57bd924ea555eb864bd4fe832
Billy Burch <billyburch40@gmail.com>
2023-09-22 12:25:17 -0600

    ref: cleaned up code

--- lib/league.rb
+++ lib/league.rb
@@ -1,5 +1,4 @@
 class League 
-  # I'm putting all the data in here for total teams. Not sure if this is correct
   def initialize(game_data, team_data, game_team_data)
     @game_data = game_data
     @team_data = team_data
