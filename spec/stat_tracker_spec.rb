c36894090269da980aad4a95db17647c32b44c25
Martin Chavez <martinchavez1203@gmail.com>
2023-09-22 14:43:44 -0600

    Updated test for game data

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -2,7 +2,7 @@ require './spec/spec_helper'
 
 RSpec.describe StatTracker do
   before :each do
-    game_path = './fixture/games_fixture.csv'
+    game_path = './data/games.csv'
     team_path = './data/teams.csv'
     game_teams_path = './fixture/game_teams_fixture.csv'
 
@@ -16,14 +16,20 @@ RSpec.describe StatTracker do
 
   describe "#percent ties" do
     it "finds percntage of tied away and home games" do
-      expect(@game_stats.percentage_ties).to eq(0.05)
+      expect(@game_stats.percentage_ties).to eq(0.20)
     end
   end
 
   describe "#count_of_games_by_season" do 
     it "#count_of_games_by_season" do
     expected = {
-      "20122013"=>19,
+      "20122013"=>806,
+      "20162017"=>1317,
+      "20142015"=>1319,
+      "20152016"=>1321,
+      "20132014"=>1323,
+      "20172018"=>1355
+
     }
     expect(@game_stats.count_of_games_by_season).to eq expected
     end 
@@ -32,7 +38,12 @@ RSpec.describe StatTracker do
   describe "##average_goals_by_season" do
     it "#average_goals_by_season" do
     expected = {
-      "20122013"=> 3.68
+      "20122013"=>4.12,
+      "20162017"=>4.23,
+      "20142015"=>4.14,
+      "20152016"=>4.16,
+      "20132014"=>4.19,
+      "20172018"=>4.44
     }
     expect(@game_stats.average_goals_by_season).to eq expected
     end
