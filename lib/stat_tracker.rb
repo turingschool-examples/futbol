2d1aa2b946b101984f573beb3a1dee5842dac877
Taylor Pubins <tpubz@icloud.com>
2023-09-21 18:46:19 -0700

    Modify #teams_hash to not recieve arg; args handled at [stat_tracker.rb] level

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -144,5 +144,12 @@ class StatTracker < Stats
 
   ###=== TEAM QUERIES ===###
 
+  def biggest_team_blowout(team_id)
+    teams_hash[team_id][:goal_diffs].max
+  end
+
+  def worst_loss(team_id)
+    teams_hash[team_id][:goal_diffs].min
+  end
   ###=== TEAM QUERIES ===###
 end
