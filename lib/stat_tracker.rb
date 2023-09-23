138a49be493ae632f37e6195b04e3e2487927e06
Matt Darlington <matthewsdarlington@gmail.com>
2023-09-23 03:22:39 -0600

    Redefine methods in stat_tracker to return values that matched those in spec_harness, clean up stat_tracker to conform to ruby convention's spacing, variable names, remove pry stmts

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -6,164 +6,175 @@
 
 
 class StatTracker
-attr_reader :locations, :team_data, :game_data, :game_teams_data
+  attr_reader :locations, :team_data, :game_data, :game_teams_data
 
-def initialize(locations)
-  @game_data = create_games(locations[:games])
-  @game_teams_data = create_game_teams(locations[:game_teams])
-  @team_data = create_teams(locations[:teams])
-end
-
-# CREATOR METHODS
-
-def create_games(path)
-  Game.reset
-  Game.reset
-  data = CSV.parse(File.read(path), headers: true, header_converters: :symbol)
-  data.map do |row|
-    Game.new(row)
+  def initialize(locations)
+    @game_data = create_games(locations[:games])
+    @game_teams_data = create_game_teams(locations[:game_teams])
+    @team_data = create_teams(locations[:teams])
   end
-end
 
-def create_game_teams(path)
-  GameTeam.reset
-  data = CSV.parse(File.read(path), headers: true, header_converters: :symbol)
-  data.map do |row| 
-    GameTeam.new(row)
-  end
-end
+  # CREATOR METHODS
 
-def create_teams(path)
-  data = CSV.parse(File.read(path), headers: true, header_converters: :symbol)
-  data.map do |row|
-    Team.new(row)
-  end
-end
-
-def self.from_csv(locations)
-  StatTracker.new(locations)
-end
-
-
-# GAME METHODS
-
-def highest_total_score
-  most_goals_game = @game_data.reduce(0) do |goals, game|
-    game_goals = game.home_goals.to_i + game.away_goals.to_i
-    if game_goals > goals
-      goals = game_goals
+  def create_games(path)
+    Game.reset
+    Game.reset
+    data = CSV.parse(File.read(path), headers: true, header_converters: :symbol)
+    data.map do |row|
+      Game.new(row)
     end
-    goals
   end
-  most_goals_game
-end
 
-def lowest_total_score
-  fewest_goals_game = @game_data.reduce(0) do |goals, game|
-    game_goals = game.home_goals.to_i + game.away_goals.to_i
-    if game_goals < goals
-      goals = game_goals
+  def create_game_teams(path)
+    GameTeam.reset
+    data = CSV.parse(File.read(path), headers: true, header_converters: :symbol)
+    data.map do |row| 
+      GameTeam.new(row)
     end
-    goals
   end
-  fewest_goals_game
-end
+
+  def create_teams(path)
+    data = CSV.parse(File.read(path), headers: true, header_converters: :symbol)
+    data.map do |row|
+      Team.new(row)
+    end
+  end
+
+  def self.from_csv(locations)
+    StatTracker.new(locations)
+  end
 
 
+  # GAME METHODS
 
+  def highest_total_score
+    most_goals_game = @game_data.reduce(0) do |goals, game|
+      game_goals = game.home_goals.to_i + game.away_goals.to_i
+      if game_goals > goals
+        goals = game_goals
+      end
+      goals
+    end
+    most_goals_game
+  end
 
+  def lowest_total_score
+    fewest_goals_game = @game_data.reduce(0) do |goals, game|
+      game_goals = game.home_goals.to_i + game.away_goals.to_i
+      if game_goals < goals
+        goals = game_goals
+      end
+      goals
+    end
+    fewest_goals_game
+  end
 
-def percentage_calculator(portion, whole)
-  percentage = (portion/whole).round(2)
-end
+  def percentage_calculator(portion, whole)
+    percentage = (portion/whole).round(2)
+  end
 
-def count_of_games_by_season 
-  # require 'pry'; binding.pry
-  games_seasons = Hash.new(0)
-  
-  @game_data.each do |row|
-    season = row.season
-    games_seasons[season] += 1
-  end 
-  games_seasons
-end
-
-def percentage_ties 
-  ties = @game_data.count do |game|
+  def count_of_games_by_season 
     # require 'pry'; binding.pry
-    game.away_goals.to_f == game.home_goals.to_f
-  end.to_f
-  (ties/@game_data.count).round(2)
-end
-
-def average_goals_by_season
-  season_hash =@game_data.group_by{|game| game.season }
-  av_goals = {}
-  
-  season_hash.each do |season,games|
-    total_goals = games.map {|game| game.home_goals.to_i + game.away_goals.to_i}
-    av_goals[season] = (total_goals.sum.to_f / games.length).round(2)
+    games_seasons = Hash.new(0)
+    
+    @game_data.each do |row|
+      season = row.season
+      games_seasons[season] += 1
+    end 
+    games_seasons
   end
-  av_goals
-end
 
-def percentage_visitor_wins
-  away_wins = GameTeam.gameteam.count do |game|
-    game.HoA == "away" && game.result == "WIN"
-  end 
-  (away_wins.to_f / Game.games.count.to_f).round(2)
-end
-
-def seasons_sorted
-  season_sorted = Game.games.group_by {|game| game.season}
-  # require 'pry'; binding.pry
-end
-  
-def team_info
-  teams = GameTeam.gameteam.group_by {|team| team.team_id}
-  # require 'pry'; binding.pry
-end
-  
-def most_tackles(season)
-  #returns array of all games for specific season
-  games_by_season = @game_data.find_all do |game|
-    game if game.season == season
+  def percentage_ties 
+    ties = @game_data.count do |game|
+      # require 'pry'; binding.pry
+      game.away_goals.to_f == game.home_goals.to_f
+    end.to_f
+    (ties/@game_data.count).round(2)
   end
-  #returns array of all game_teams for specific season, game_teams track tackles
-  game_teams_by_season = []
-  games_by_season.each do |game|
-    @game_teams_data.each do |game_team|
-      game_teams_by_season.push(game_team) if game.game_id == game_team.game_id
+
+  def average_goals_by_season
+    season_hash =@game_data.group_by{|game| game.season }
+    av_goals = {}
+    
+    season_hash.each do |season,games|
+      total_goals = games.map {|game| game.home_goals.to_i + game.away_goals.to_i}
+      av_goals[season] = (total_goals.sum.to_f / games.length).round(2)
     end
+    av_goals
   end
-  #creats empty acumulator hash, sort teams by id as key, add all tackles for each team as value
-  tackles_by_team = Hash.new(0)
-  game_teams_by_season.each do |game_team|
-    tackles_by_team[game_team.team_id] += game_team.tackles.to_i
-  end
-  #find team id with most tackles 
-  team_id_most_tackles = tackles_by_team.max_by {|team_id, tackles| tackles}.first
-  #find team object by id found above
-  team_with_most_tackles = @team_data.find do |team|
-    team_id_most_tackles == team.franchise_id
-  end
-  #call on team name attribute of team object
-  team_with_most_tackles.team_name
-end  
-  
 
-def fewest_tackles(season)
-  #returns array of all games for specific season
-  games_by_season = @game_data.find_all do |game|
-    game if game.season == season
+  def percentage_home_wins
+    home_wins = GameTeam.gameteam.count do |game|
+      game.hoa == "home" && game.result == "WIN"
+    end 
+    (home_wins.to_f / Game.games.count.to_f).round(2)
   end
+
+  def percentage_visitor_wins
+    away_wins = GameTeam.gameteam.count do |game|
+      game.hoa == "away" && game.result == "WIN"
+    end 
+    (away_wins.to_f / Game.games.count.to_f).round(2)
+  end
+
+  def seasons_sorted
+    season_sorted = Game.games.group_by {|game| game.season}
+    # require 'pry'; binding.pry
+  end
+    
+  def team_info(team_id)
+    team = @team_data.find do |team|
+      team.team_id == team_id
+    end
+    {
+      "team_id" => team.team_id,
+      "franchise_id" => team.franchise_id,
+      "team_name" => team.team_name,
+      "abbreviation" => team.abbreviation,
+      "link" => team.link
+    }
+  end
+    
+  def most_tackles(season)
+    #returns array of all games for specific season
+    games_by_season = @game_data.find_all do |game|
+      game if game.season == season
+    end
     #returns array of all game_teams for specific season, game_teams track tackles
-  game_teams_by_season = []
-  games_by_season.each do |game|
-    @game_teams_data.each do |game_team|
-      game_teams_by_season.push(game_team) if game.game_id == game_team.game_id
+    game_teams_by_season = []
+    games_by_season.each do |game|
+      @game_teams_data.each do |game_team|
+        game_teams_by_season.push(game_team) if game.game_id == game_team.game_id
+      end
     end
-  end
+    #creats empty acumulator hash, sort teams by id as key, add all tackles for each team as value
+    tackles_by_team = Hash.new(0)
+    game_teams_by_season.each do |game_team|
+      tackles_by_team[game_team.team_id] += game_team.tackles.to_i
+    end
+    #find team id with most tackles 
+    team_id_most_tackles = tackles_by_team.max_by {|team_id, tackles| tackles}.first
+    #find team object by id found above
+    team_with_most_tackles = @team_data.find do |team|
+      team_id_most_tackles == team.franchise_id
+    end
+    #call on team name attribute of team object
+    team_with_most_tackles.team_name
+  end  
+  
+
+  def fewest_tackles(season)
+    #returns array of all games for specific season
+    games_by_season = @game_data.find_all do |game|
+      return game if game.season == season
+    end
+    #returns array of all game_teams for specific season, game_teams track tackles
+    game_teams_by_season = []
+    games_by_season.each do |game|
+      @game_teams_data.each do |game_team|
+        return game_teams_by_season.push(game_team) if game.game_id == game_team.game_id
+      end
+    end
     #creats empty acumulator hash, sort teams by id as key, add all tackles for each team as value
     tackles_by_team = Hash.new(0)
     game_teams_by_season.each do |game_team|
@@ -201,20 +212,11 @@ def fewest_tackles(season)
     end
     average = total_goals.to_f / total_games.uniq.count
     average.round(2)
+
+
+    
   end
 
-  # def highest_scoring_visitor
-  #   team_information = {}
-  #   season_goals = 0
-  #   @game_teams_data.find_all do |row|
-  #     season_goals += row[:goals].to_i
-  #     team_information[row[:team_id]] = season_goals + 
-  #     # require 'pry'; binding.pry
-  #   # require 'pry'; binding.pry
-  #   # row[:goals]
-  #   # row[:team_id]
-  #   # row[:game_id]
-  #   # row[:hoa]
 
   def team_goals(home_or_away)
     teams = @game_teams_data.group_by { |row| row.team_id}
@@ -257,7 +259,7 @@ def fewest_tackles(season)
     average_goals = Hash.new
     team_goals(team_side).each do |key, value|
       if games_by_team(team_side)[key]
-        average_goals[key] = (value.to_f / games_by_team(team_side)[key].to_f).round(2) 
+        average_goals[key] = ((value/ games_by_team(team_side)[key].to_f)*100/100).round(3)
       end
     end
     average_goals
@@ -266,42 +268,73 @@ def fewest_tackles(season)
   def highest_scoring_visitor
     team = average_goals_per_team("away")
     most_avg = average_goals_per_team("away").values.max
-    # require 'pry'; binding.pry
-    return team.key(most_avg)
+    team_id = team.reduce("") do |team_id, el|
+      if el.last == most_avg
+        team_id = el.first
+      end
+      team_id
+    end
+    team_name = get_team_name(team_id)
   end
-  def lowest_scoring_visitor
-    team = average_goals_per_team("away")
-    lowest_average = average_goals_per_team("away").values.min
-    return team.key(lowest_average)
-  end
+
   def highest_scoring_home_team
     team = average_goals_per_team("home")
-    highest_average = average_goals_per_team("home").values.max
-    return team.key(highest_average)
+    highest_avg = average_goals_per_team("home").values.max
+    team_id = team.reduce("") do |team_id, el|
+      if el.last == highest_avg
+        team_id = el.first
+      end
+      team_id
+    end
+    team_name = get_team_name(team_id)
   end
+
+  def lowest_scoring_visitor
+    team = average_goals_per_team("away")
+    lowest_avg = average_goals_per_team("away").values.min
+    team_id = team.reduce("") do |team_id, el|
+      if el.last == lowest_avg
+        team_id = el.first
+      end
+      team_id
+    end
+    team_name = get_team_name(team_id)
+  end
+
   def lowest_scoring_home_team
     team = average_goals_per_team("home")
-    least_average = average_goals_per_team("home").values.min
-    return team.key(least_average)
+    most_avg = average_goals_per_team("home").values.min
+    team_id = team.reduce("") do |team_id, el|
+      if el.last == most_avg
+        team_id = el.first
+      end
+      team_id
+    end
+    team_name = get_team_name(team_id)
   end
 
   def count_of_teams
     @team_data.count
   end
-end
 
-def seasons_sorted
-  season_sorted = Game.games.group_by {|game| game.season}
-  game_ids = []
-  season_game_ids = Hash.new
-  season_sorted.each do |season|
-    season.last.each do |data|
-      game_ids << data.game_id
+  def seasons_sorted
+    season_sorted = Game.games.group_by {|game| game.season}
+    game_ids = []
+    season_game_ids = Hash.new
+    season_sorted.each do |season|
+      season.last.each do |data|
+        game_ids << data.game_id
+      end
+      season_game_ids[season.first] = game_ids
     end
-    season_game_ids[season.first] = game_ids
+    season_game_ids
   end
-  season_game_ids
-end
 
+  def get_team_name(team_id)
+    @team_data.find do |team|
+      team.team_id == team_id
+    end.team_name
+  end
+end
 
 
