c715a24c5547bffa05c465f43a7de22fbaa3ad24
Quin Nordmark <quinnordmark@gmail.com>
2023-11-07 14:28:59 -0800

    fixed initalize misspell

--- lib/team.rb
+++ lib/team.rb
@@ -1,7 +1,7 @@
 class Team
     attr_reader :team_id, :franchise_id, :team_name, :abbreviation, :stadium, :link
 
-    def initalize(team_id, franchise_id, team_name, abbreviation, stadium, link)
+    def initialize(team_id, franchise_id, team_name, abbreviation, stadium, link)
         @team_id = team_id
         @franchise_id = franchise_id
         @team_name = team_name
