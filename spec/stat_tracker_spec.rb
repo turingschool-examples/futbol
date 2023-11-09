db16bd04f6f9142a079c60ee8ab7a1f984a8531b
Martin Chavez <martinchavez1203@gmail.com>
2023-11-08 17:26:41 -0700

    set test for Stat_tracker

---
+++ spec/stat_tracker_spec.rb
@@ -0,0 +1,27 @@
+require './lib/stat_tracker'
+
+
+
+RSpec.describe StatTracker do
+  before(:each) do
+    
+    game_path = './data/games_fixture.csv'
+    team_path = './data/teams.csv'
+    game_teams_path = './data/game_teams_fixture.csv'
+    
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+    
+    @stat_tracker = StatTracker.from_csv(locations)
+
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@stat_tracker).to be_a(StatTracker)
+    end
+  end
+end
