d190ddc5dfbe915a26f5d0e85c8105779f0568ae
Barry Allahyar <barrya@gmail.com>
2023-11-09 15:02:32 -0600

    fix: Makes sure create_games has a parameter for class

--- spec/game_teams_spec.rb
+++ spec/game_teams_spec.rb
@@ -1,19 +1,6 @@
 require 'spec_helper'
 
 RSpec.describe GameTeams do
-  before :each do
-    game_path = './data/games_subset.csv'
-    team_path = './data/teams.csv'
-    game_teams_path = './data/game_teams_subset.csv'
-
-    locations = {
-      games: game_path,
-      teams: team_path,
-      game_teams: game_teams_path
-    }
-
-    stat_tracker = StatTracker.from_csv(locations)
-  end
 
   it "exists" do
     expect(game_teams = GameTeams.new("2012030221","20122013","home","LOSS","Tom",6,2,3)).to be_a GameTeams
@@ -32,6 +19,7 @@ RSpec.describe GameTeams do
   end
 
   it "has a create games class method" do
+    game_teams_path = './data/game_teams.csv'
     expect(GameTeams.create_game_teams(game_teams_path)).to be_an Array
     GameTeams.create_game_teams(game_teams_path).each do |game_team|
       expect(game_team).to be_a GameTeams
--- spec/teams_spec.rb
+++ spec/teams_spec.rb
@@ -1,19 +1,6 @@
 require 'spec_helper'
 
 RSpec.describe Teams do
-  before :each do
-    game_path = './data/games_subset.csv'
-    team_path = './data/teams.csv'
-    game_teams_path = './data/game_teams_subset.csv'
-
-    locations = {
-      games: game_path,
-      teams: team_path,
-      game_teams: game_teams_path
-    }
-
-    stat_tracker = StatTracker.from_csv(locations)
-  end
 
   it "exists" do
     expect(teams = Teams.new("Rabbits","20122013")).to be_a Teams
@@ -26,6 +13,7 @@ RSpec.describe Teams do
   end
 
   it "has a create games class method" do
+    team_path = './data/teams.csv'
     expect(Teams.create_teams(team_path)).to be_an Array
     Teams.create_teams(team_path).each do |team|
       expect(team).to be_a Teams
