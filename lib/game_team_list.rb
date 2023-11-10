83482ea8d5d5f8027441bd93bdfcdb657c350b85
Quin Nordmark <quinnordmark@gmail.com>
2023-11-10 12:35:42 -0800

    game_team_list class 1

--- lib/game_team_list.rb
+++ lib/game_team_list.rb
@@ -1,19 +1,20 @@
-require_relative './helper_class'
 require 'CSV'
 require './lib/game_team'
 
 class GameTeamList
-  attr_reader :game_teams
+  attr_reader :game_teams, :stat_tracker
 
   def initialize(path, stat_tracker)
-      @game_teams = create_teams(path)
+    @stat_tracker = stat_tracker
+    @game_teams = create_game_team_list(path)
   end
 
-  def create_game_teams(path)
-      data = CSV.parse(File.read(path), headers: true, header_converters: :symbol)
-      data.map do |datum|
-          GameTeams.new(datum,self)
-      end
-  end
-
+    def create_game_team_list(path)
+        arr = []
+        data = CSV.parse(File.read(path), headers: true, header_converters: :symbol)
+        data.map do |datum|
+            arr.append(GameTeam.new(datum,self))
+        end
+        arr
+    end
 end
