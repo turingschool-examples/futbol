ea95d0560bb53a577459cac527f4a7a2d934d6b1
Logan Finnegan <logantfinnegan@protonmail.com>
2023-11-10 18:37:46 -0600

    add highest/lowest score methods and tests

--- lib/game_list.rb
+++ lib/game_list.rb
@@ -2,10 +2,11 @@ require 'CSV'
 require_relative './game'
 
 class GameList
-  attr_reader :games
+  attr_reader :games, :stat_tracker
 
   def initialize(path, stat_tracker)
-      @games = create_games(path)
+    @games = create_games(path)
+    @stat_tracker = stat_tracker
   end
 
   def create_games(path)
@@ -13,10 +14,17 @@ class GameList
       data.map do |datum|
         Game.new(datum, self)
       end
+    end
+
+  def highest_total_score
+    @games.max_by do |game|
+      game.total_score
+    end.total_score
   end
 
-  # def highest_total_score
-
-  # end
-
+  def lowest_total_score
+    @games.min_by do |game|
+      game.total_score
+    end.total_score
+  end
 end
