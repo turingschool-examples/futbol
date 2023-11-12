328a2e3b6cd310e448156a433751f5851dc2b2ee
Sam Puttman <daputter2@gmail.com>
2023-11-12 15:22:42 -0500

    fix most_tackles method to take season as a parameter

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -558,7 +558,7 @@ def lowest_scoring_home_team
     # games.csv - game_id, season 
     # teams.csv - team_id, teamName
 
-  def least_accurate_team
+  def least_accurate_team(season)
     season_data = {}
         
     @game_teams.each do |game_team|
@@ -601,40 +601,23 @@ def lowest_scoring_home_team
     # game_teams.csv - game_id, team_id, tackles
     # games.csv - game_id, season
     # teams.csv - team_id, teamName
-  def most_tackles
+  def most_tackles(season)
     season_tackles = {}
     
     @game_teams.each do |game_team|
       game = find_game_by_id(game_team.game_id)
-      next unless game
+      next unless game && game.season == season
     
-      season = game.season
       team_id = game_team.team_id
       tackles = game_team.tackles.to_i
         
-      #initialize nested hash for each new season or team
 
-      season_tackles[season] ||= {}
-      season_tackles[season][game_team.team_id] ||= 0
-    
-      season_tackles[season][team_id] += tackles
-
+      season_tackles[team_id] ||= 0   
+      season_tackles[team_id] += tackles
     end
     
-    teams_with_most_tackles_per_season = {}
-    season_tackles.each do |season, teams|
-      most_tackles = 0
-      team_with_most_tackles = nil
-
-      teams.each do |team_id, tackles|
-        if tackles > most_tackles
-          most_tackles = tackles
-          team_with_most_tackles = team_id
-        end
-      end
-      teams_with_most_tackles_per_season[season] = team_name(team_with_most_tackles)
-    end
-    teams_with_most_tackles_per_season
+    team_with_most_tackles = season_tackles.max_by { |team_id, tackles| tackles }.first
+    team_name(team_with_most_tackles)
   end
 
   # least_tackles # SAM
