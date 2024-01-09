794b5baaf98003ac7b77ebe88a9a13e5d3febfe7
Joey Reyes <joeyreyes105@gmail.com>
2024-01-09 15:18:47 -0800

    Create initialize methods with data access points may need to refactor

--- lib/game_stats.rb
+++ lib/game_stats.rb
@@ -1,3 +1,13 @@
 class GameStats 
-    
+    attr_reader :highest_score,
+                :lowest_score,
+                :home_team, 
+                :away_team
+                
+    def initialize(highest_score, lowest_score, home_team, away_team)
+        @highest_score = highest_score
+        @lowest_score = lowest_score
+        @home_team = home_team
+        @away_team = away_team
+    end 
 end
