fe4ca3ac4f0fbd0ddcb5756a4a1bd1b67c3deff4
Joey Reyes <joeyreyes105@gmail.com>
2024-01-09 14:47:55 -0800

    Initial commit, add files for game statistics

---
+++ spec/game_stats_spec.rb
@@ -0,0 +1,9 @@
+require './spec/spec_helper.rb'
+
+RSpec.describe GameStats do 
+    describe '#initialize' do 
+        it 'exists' do 
+            stats = GameStats.new 
+        end
+    end
+end
