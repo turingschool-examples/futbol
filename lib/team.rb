4ad272e3a62847e006e6ce19615f82389a077a9d
Jack Sweeney <jackcsweeney@gmail.com>
2024-01-10 15:34:00 -0800

    feat: Add initialize to Team

---
+++ lib/team.rb
@@ -0,0 +1,19 @@
+class Team
+
+    attr_reader :team_id,
+                :franchise_id,
+                :team_name,
+                :abbreviation,
+                :stadium,
+                :link
+
+    def initialize(info)
+        @team_id = info[:team_id]
+        @franchise_id = info[:franchise_id]
+        @team_name = info[:team_name]
+        @abbreviation = info[:abbreviation]
+        @stadium = info[:stadium]
+        @link = info[:link]
+    end
+end
+        
