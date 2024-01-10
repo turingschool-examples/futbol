9862f477ebf1298233f9a75d9ccba35004773ff2
Jack Sweeney <jackcsweeney@gmail.com>
2024-01-10 15:52:26 -0800

    test: Add tests for create_teams to TeamFactory

--- spec/team_factory_spec.rb
+++ spec/team_factory_spec.rb
@@ -3,11 +3,11 @@ require './lib/team_factory'
 require 'pry'
 
 RSpec.describe TeamFactory do
+    before do
+        @file_path = './data/teams_fixture.csv'
+        @team_factory = TeamFactory.new(@file_path)
+    end
     describe '#initialize' do
-        before do
-            @file_path = './data/teams_fixture.csv'
-            @team_factory = TeamFactory.new(@file_path)
-        end
 
         it 'exists' do
 
@@ -19,6 +19,26 @@ RSpec.describe TeamFactory do
 
             expect(@team_factory.file_path).to eq(@file_path)
 
+        end
+    end
+
+    describe '#create_teams' do
+        it 'creates team objects from the data stored in its file_path attribute' do
+
+            expect(@team_factory.create_teams).to be_a(Array)
+            expect(@team_factory.create_teams.all? {|team| team.class == Team}).to eq(true)
+
+        end
+
+        it 'creates objects with the necessary attributes' do
+
+            expect(@team_factory.create_teams.first.team_id).to eq(1)
+            expect(@team_factory.create_teams.first.franchise_id).to eq(23)
+            expect(@team_factory.create_teams.first.team_name).to eq("Atlanta United")
+            expect(@team_factory.create_teams.first.abbreviation).to eq("ATL")
+            expect(@team_factory.create_teams.first.stadium).to eq("Mercedes-Benz Stadium")
+            expect(@team_factory.create_teams.first.link).to eq("/api/v1/teams/1")
+            
         end
     end
 end
