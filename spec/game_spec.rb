ec61435d5b59bb838d2eead0680bcc9da2219036
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-01-10 16:13:11 -0500

    test: include before do block for tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,8 +1,19 @@
 require 'spec_helper'
 
 RSpec.describe Game do
+   before(:each) do
+      @game_details = {
+         game_id: 2012030221,
+         season: 20122013,
+         away_team_id: 3,
+         home_team_id: 6,
+         away_goals: 2,
+         home_goals: 3
+      }
+   end
+
    it 'exists' do
-      game = Game.new(2012030221, 20122013, 3, 6, 2, 3)
+      game = Game.new(@game_details)
 
       expect(game).to be_a Game
    end
