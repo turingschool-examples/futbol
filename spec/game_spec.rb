2f868c04d36ea6945abe708ecb55160138b5b2a3
Joey Reyes <joeyreyes105@gmail.com>
2024-01-10 15:15:49 -0800

    Finish initialize for game file attributes, test pass

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -13,6 +13,13 @@ RSpec.describe Game do
                 home_goals: '3'})
 
             expect(game).to be_a Game
+            expect(game.game_id).to eq(2012030221)
+            expect(game.season).to eq(20122013)
+            expect(game.type).to eq('Postseason')
+            expect(game.away_team_id).to eq(3)
+            expect(game.home_team_id).to eq(6)
+            expect(game.away_goals).to eq(2)
+            expect(game.home_goals).to eq(3)
         end 
     end
 end
