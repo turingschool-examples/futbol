224841cf42b7244e7d42a64274f5e02c3c45535c
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-01-10 15:58:34 -0500

    feat: read_games_csv(location) to create Game objects in StatTracker

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -12,4 +12,24 @@ class StatTracker
       @data_game_teams = read_game_teams_csv(locations[:game_teams])
    end
 
+   def read_games_csv(location)
+      games_data = []
+      CSV.foreach(location, headers: true, header_converters: :symbol) do |row|
+         game_id = row[:game_id]
+         season = row[:season]
+         away_team_id = row[:away_team_id]
+         home_team_id = row[:home_team_id]
+         away_goals = row[:away_goals]
+         home_goals = row[:home_goals]
+         
+         games_data << Game.new(game_id, season, away_team_id, home_team_id, away_goals, home_goals)
+      end
+      games_data
+   end
+end
+#equire 'CSV'
+# headers: true & header_converter: :symbol are optional arguments
+# CSV.foreach('./data/animal_lovers.csv', headers: true, header_converters: :symbol) do |row|
+#    require 'pry'; binding.pry
+#  end
 end
