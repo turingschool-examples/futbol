b34f59a1b3ba9096adb3d256d3919b31bfb76c20
Mel Langhoff <mlanghoff@uwalumni.com>
2024-01-10 08:22:05 -0700

    Fixed typo

--- lib/games.rb
+++ lib/games.rb
@@ -35,7 +35,7 @@ class Game
             home_goals = row[:home_goals]
             venue = row[:venue]
             venue_link = row[:venue_link]
-            game_instance = Game.new(game_id, season, type, date_time, away_team_id, home_team_id, away_goals, home_goals, venue, venue_link)
+            game_instance = Games.new(game_id, season, type, date_time, away_team_id, home_team_id, away_goals, home_goals, venue, venue_link)
         end
     end
 end
