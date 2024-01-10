65dd06b1d0404b4fffbe7275c0925c5c73f40135
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-01-10 16:14:02 -0500

    refactor: remove unnecessary code

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -15,21 +15,16 @@ class StatTracker
    def read_games_csv(location)
       games_data = []
       CSV.foreach(location, headers: true, header_converters: :symbol) do |row|
-         game_id = row[:game_id]
-         season = row[:season]
-         away_team_id = row[:away_team_id]
-         home_team_id = row[:home_team_id]
-         away_goals = row[:away_goals]
-         home_goals = row[:home_goals]
-         
-         games_data << Game.new(game_id, season, away_team_id, home_team_id, away_goals, home_goals)
+         game_details = {
+            game_id: row[:game_id],
+            season: row[:season],
+            away_team_id: row[:away_team_id],
+            home_team_id: row[:home_team_id],
+            away_goals: row[:away_goals],
+            home_goals: row[:home_goals]
+         }
+         games_data << Game.new(game_details)
       end
       games_data
    end
-end
-#equire 'CSV'
-# headers: true & header_converter: :symbol are optional arguments
-# CSV.foreach('./data/animal_lovers.csv', headers: true, header_converters: :symbol) do |row|
-#    require 'pry'; binding.pry
-#  end
 end
