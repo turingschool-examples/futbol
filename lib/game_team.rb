7df6cf7c67737a8e0fb170ec6fb7ade1f53d5274
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-01-10 18:09:58 -0500

    fix: fix typo

--- lib/game_team.rb
+++ lib/game_team.rb
@@ -2,9 +2,9 @@ class GameTeam
    def initialize(game_team_details)
       @game_id = game_team_details[:game_id]
       @team_id = game_team_details[:team_id]
-      @home_or_away = game_team_details[:HoA]
+      @hoa = game_team_details[:hoa]
       @result = game_team_details[:result]
-      @coach = game_team_details[:head_coach]
+      @head_coach = game_team_details[:head_coach]
       @goals = game_team_details[:goals]
       @shots = game_team_details[:shots]
       @tackles = game_team_details[:tackles]
