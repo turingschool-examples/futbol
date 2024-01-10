0af2f7ad29b9f95b1cf86ec01028ab4632640b59
Igor Magalhaes <magalhaess.igor@gmail.com>
2024-01-10 18:02:30 -0500

    test: create test initialize for game-team

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -37,7 +37,7 @@ RSpec.describe StatTracker do
 
    describe '#read_game_teams_csv' do
       it 'create game_team objects' do
-         location = './data/team_fixture.csv'
+         location = './data/game_teams_fixture.csv'
 
          expect(@stat_tracker.read_game_teams_csv(location)[0]).to be_a GameTeam
       end
