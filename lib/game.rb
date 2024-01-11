eff255b1472fa231117030a87d92f97d7bab4570
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2024-01-11 10:55:19 -0500

    Create the game class describing the csv file for game

---
+++ lib/game.rb
@@ -0,0 +1,53 @@
+class Game
+    attr_reader :game_id,
+                :season, 
+                :type, 
+                :data_time, 
+                :away_team_id, 
+                :home_team_id,
+                :away_goals,
+                :home_goals,
+                :venue
+    def initialize(data_attributes)
+        @game_id = data_attributes[:game_id]
+        @season = data_attributes[:season]
+        @type = data_attributes[:type]
+        @data_time = data_attributes[:data_time]
+        @away_team_id = data_attributes[:away_team_id]
+        @home_team_id = data_attributes[:home_team_id]
+        @away_goals = data_attributes[:away_goals]
+        @home_goals = data_attributes[:home_goals]
+        @venue = data_attributes[:venue]
+    end 
+
+    def highest_total_score
+        
+    end
+
+    def lowest_total_score
+
+    end
+    
+    def percentage_home_wins
+        
+    end
+
+    def percentage_visitor_wins
+
+    end 
+
+    def percentage_ties
+    end
+
+    def count_of_games_by_season
+
+    end
+
+    def average_goals_per_game
+
+    end
+
+    def average_goals_by_season
+
+    end
+end
