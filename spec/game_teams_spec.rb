56378947ede949528fbaf8a514bab5f929a1aa36
Nico Shanstrom <nicoshanstrom@gmail.com>
2024-01-11 10:01:55 -0500

    Added s to the end of game_teams

--- spec/game_teams_spec.rb
+++ spec/game_teams_spec.rb
@@ -1,116 +1,116 @@
 require './lib/game_teams'
 require 'pry'
 
-RSpec.describe Game_Team do
+RSpec.describe GameTeams do
     describe '#initialize' do
 
         before do
             
-            @game_team_info = { 
+            @game_teams_info = { 
                             game_id: 2012030221, team_id: 3, HoA: "away", result: "LOSS", 
                             settled_in: "OT", head_coach: "John Tortorella", goals: 2, shots: 8, 
                             tackles: 44, pim: 8, powerPlayOpportunities: 3, powerPlayGoals: 0, 
                             faceOffWinPercentage: 44.8, giveaways: 8, takeaways: 17 
                             }
             
-            @game_team = Game_Team.new(@game_team_info)
+            @game_teams = GameTeams.new(@game_teams_info)
 
         end
 
         it 'exists' do
             
-            expect(@game_team).to be_a(Game_Team)
+            expect(@game_teams).to be_a(GameTeams)
 
         end
 
         it 'has a game_id' do
 
-            expect(@game_team.game_id).to eq(2012030221)
+            expect(@game_teams.game_id).to eq(2012030221)
 
         end
 
         it 'has a team_id' do
             
-            expect(@game_team.team_id).to eq(3)
+            expect(@game_teams.team_id).to eq(3)
 
         end
 
         it 'can return if a team is home or away' do
 
-            expect(@game_team.hoa).to eq("away")
+            expect(@game_teams.hoa).to eq("away")
 
         end
 
         it 'can return game result' do
 
-            expect(@game_team.result).to eq("LOSS")
+            expect(@game_teams.result).to eq("LOSS")
 
         end
 
         it 'can return how the game was settled' do
 
-            expect(@game_team.settled_in).to eq("OT")
+            expect(@game_teams.settled_in).to eq("OT")
 
         end
 
         it 'returns a teams head coach' do
 
-            expect(@game_team.head_coach).to eq("John Tortorella")
+            expect(@game_teams.head_coach).to eq("John Tortorella")
 
         end
 
         it 'returns goals by specific team in a game' do
 
-            expect(@game_team.goals).to eq(2)
+            expect(@game_teams.goals).to eq(2)
 
         end
 
         it 'returns how many shots a team attempted' do
 
-            expect(@game_team.shots).to eq(8)
+            expect(@game_teams.shots).to eq(8)
 
         end
 
         it 'returns how many tackles a team made' do
 
-            expect(@game_team.tackles).to eq(44)
+            expect(@game_teams.tackles).to eq(44)
 
         end
 
         it 'returns pim aka penulty minutes for a team' do
 
-            expect(@game_team.pim).to eq(8)
+            expect(@game_teams.pim).to eq(8)
         
         end
 
         it 'returns power play opportunities for a team' do
             #should i keep the method name consistent with the hash key, or change the hash key
             # and the method name to something like power_play_opps?
-            expect(@game_team.powerPlayOpportunities).to eq(3)
+            expect(@game_teams.powerPlayOpportunities).to eq(3)
 
         end
 
         it 'returns power play goals for a team' do
             #same question as above
-            expect(@game_team.powerPlayGoals).to eq(0)
+            expect(@game_teams.powerPlayGoals).to eq(0)
 
         end
 
         it 'returns a faceOffWinPercentage float for a team' do
             #same question as above
-            expect(@game_team.faceOffWinPercentage).to eq(44.8)
+            expect(@game_teams.faceOffWinPercentage).to eq(44.8)
 
         end
 
         it 'returns the number of giveaways for a team' do
 
-            expect(@game_team.giveaways).to eq(8)
+            expect(@game_teams.giveaways).to eq(8)
 
         end
 
         it 'returns the number of takeaways for a team' do
 
-            expect(@game_team.takeaways).to eq(17)
+            expect(@game_teams.takeaways).to eq(17)
 
         end
     end
