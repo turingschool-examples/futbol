e96c49d71b743bc6ecd16a5d383505775ecce572
Mohammad Faisal Nazari <mfaisalnaari@gmail.com>
2024-01-10 20:54:35 -0700

    Attribute tests

--- spec/game_team_spec.rb
+++ spec/game_team_spec.rb
@@ -17,9 +17,18 @@ RSpec.describe GameTeam do
          @game_teams << GameTeam.new(game_team_details)
       end
       @game_teams
+      @game_team_0 = @game_teams[0]
    end
 
    it 'exists' do
       expect(@game_teams[0]).to be_a GameTeam
+      expect(@game_team_0.game_id).to eq("2012030221")
+      expect(@game_team_0.team_id).to eq("3")
+      expect(@game_team_0.hoa).to eq("away")
+      expect(@game_team_0.result).to eq("LOSS")
+      expect(@game_team_0.head_coach).to eq("John Tortorella")
+      expect(@game_team_0.goals).to eq("2")
+      expect(@game_team_0.shots).to eq("8")
+      expect(@game_team_0.tackles).to eq("44")
    end
 end
