a8fb4dfa99f4fd5f9cdd45fcb9d918081c3a19f3
Odell McFarland IV <odell.mac4@gmail.com>
2024-01-10 23:46:24 -0800

    Feat: add attr_reader to create team id and team name methods and convert team id to integer

--- lib/team.rb
+++ lib/team.rb
@@ -1,6 +1,9 @@
 class Team
+
+   attr_reader :team_id, :team_name
+
    def initialize(team_id, team_name)
-      @team_id = team_id
+      @team_id = team_id.to_i
       @team_name = team_name
    end
 end
