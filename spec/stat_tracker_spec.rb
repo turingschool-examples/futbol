6f82ad0db274473a3dea8c9980087df54f6ebdbf
Mohammad Faisal Nazari <mfaisalnaari@gmail.com>
2024-01-10 21:30:17 -0700

    game_team_spec updated

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -38,8 +38,17 @@ RSpec.describe StatTracker do
    describe '#read_game_teams_csv' do
       it 'create game_team objects' do
          location = './data/game_teams_fixture.csv'
+         game_team_0 = @stat_tracker.read_game_teams_csv(location)[0]
 
          expect(@stat_tracker.read_game_teams_csv(location)[0]).to be_a GameTeam
+         expect(game_team_0.game_id).to eq("2012030221")
+         expect(game_team_0.team_id).to eq("3")
+         expect(game_team_0.hoa).to eq("away")
+         expect(game_team_0.result).to eq("LOSS")
+         expect(game_team_0.head_coach).to eq("John Tortorella")
+         expect(game_team_0.goals).to eq("2")
+         expect(game_team_0.shots).to eq("8")
+         expect(game_team_0.tackles).to eq("44")
       end
    end
 end
