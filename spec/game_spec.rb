84e8df89821d2df16284487a7c9d722f0c4445af
lance butler <lancej2004@gmail.com>
2024-01-11 11:22:57 -0700

    Refactor: Tests different games in array and made some formating changes

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,38 +3,58 @@ require 'simplecov'
 
 RSpec.describe Game do
    before(:each) do
-      @game_details = {
-         game_id: 2012030221,
-         season: 20122013,
-         away_team_id: 3,
-         home_team_id: 6,
-         away_goals: 2,
-         home_goals: 3
-      }
+   @games = []
+      CSV.foreach('data/games_fixture.csv', headers: true, header_converters: :symbol) do |row|
+         game_details = {
+         game_id: row[:game_id],
+         season: row[:season],
+         away_team_id: row[:away_team_id],
+         home_team_id: row[:home_team_id],
+         away_goals: row[:away_goals],
+         home_goals: row[:home_goals]
+         }
+         @games << Game.new(game_details)
+      end
+      @games
    end
 
    describe '#initialize' do
       before(:each) do
-         @game = Game.new(@game_details)
+         @game_1 = @games[0]
+         @game_2 = @games[-1]
+         #require 'pry'; binding.pry
       end
 
       it 'exists' do
-         expect(@game).to be_a Game
+         expect(@game_1).to be_a Game
       end
 
-      it 'has a game id and season number' do
-         expect(@game.game_id).to eq(2012030221)
-         expect(@game.season).to eq(20122013)
+      it 'has a game id' do
+         expect(@game_1.game_id).to eq("2012030221")
+         
+         expect(@game_2.game_id).to eq("2012030124")
       end
       
-      it "has different home and away team id's" do
-         expect(@game.away_team_id).to eq(3)
-         expect(@game.home_team_id).to eq(6)
+      it "has a season" do
+         expect(@game_1.season).to eq("20122013")
+         
+         expect(@game_2.season).to eq("20122013")
       end
 
-      it "has different home and away goals" do
-         expect(@game.away_goals).to eq(2)
-         expect(@game.home_goals).to eq(3)
+      it "has different home and away team id" do
+         expect(@game_1.away_team_id).to eq(3)
+         expect(@game_1.home_team_id).to eq(6)
+         
+         expect(@game_2.home_team_id).to eq(9)
+         expect(@game_2.away_team_id).to eq(8)
+      end
+
+      it "can identify home and away goals" do
+         expect(@game_1.away_goals).to eq(2)
+         expect(@game_1.home_goals).to eq(3)
+
+         expect(@game_2.away_goals).to eq(2)
+         expect(@game_2.home_goals).to eq(3)
       end
    end
 end
