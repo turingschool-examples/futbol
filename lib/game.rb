33dd5d9b8e1ce5cb015b2520c096cb1d84e7592f
LJ9332 <149629952+LJ9332@users.noreply.github.com>
2024-01-11 15:26:27 +0000

    Update: Changes all game_details  return values from string to int

--- lib/game.rb
+++ lib/game.rb
@@ -7,11 +7,11 @@ class Game
                :home_goals
    
    def initialize(game_details)
-      @game_id = game_details[:game_id]
-      @season = game_details[:season]
-      @away_team_id = game_details[:away_team_id]
-      @home_team_id = game_details[:home_team_id]
-      @away_goals = game_details[:away_goals]
-      @home_goals = game_details[:home_goals]
+      @game_id = game_details[:game_id].to_i
+      @season = game_details[:season].to_i
+      @away_team_id = game_details[:away_team_id].to_i
+      @home_team_id = game_details[:home_team_id].to_i
+      @away_goals = game_details[:away_goals].to_i
+      @home_goals = game_details[:home_goals].to_i
    end
 end
