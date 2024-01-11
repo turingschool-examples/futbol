15f3a41e179a73abbcdcb81334a37ad2241a64ed
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-01-10 21:48:07 -0600

    Feat: create array of objects

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,9 +1,19 @@
 require 'csv'
+# require_relative './lib/game'
+# require_relative './lib/team'
+# require_relative './lib/game_team'
 
 class StatTracker
+    @@games = []
+    @@teams = []
+    @@game_teams = []
+
+    # class method for StatTracker
     def self.from_csv(locations)
-        #iterate through each hash key/value pair
+        #iterate through each hash key/value pair (key elements -> file_name, value elements -> file_path)
         locations.each do |file_name, file_path|
+            
+            #for each hash value (file_path) goes through and converts the row to be another hash with the column headers as keys and the row values as the key's values.
             CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
                 if file_name == :games
                     game_id = row[:game_id]
@@ -15,13 +25,14 @@ class StatTracker
                     away_goals = row[:away_goals]
                     home_goals = row[:home_goals]
                     venue = row[:venue]
-                    @game = Game.new(game_id, season, type, date_time, away_team_id, home_team_id, away_goals, home_goals, venue)
+                    game = Game.new(game_id, season, type, date_time, away_team_id, home_team_id, away_goals, home_goals, venue)
+                    @@games << game
                 elsif file_name == :teams
                     team_id = row[:team_id]
                     franchise_id = row[:franchise]
                     abbreviation = row[:abbreviation]
                     stadium = row[:stadium]
-                    @team = Team.new(team_id, franchise_id, abbreviation, stadium)
+                    @@teams << team = Team.new(team_id, franchise_id, abbreviation, stadium)
                 elsif file_name == :game_teams
                     game_id = row[:game_id]
                     team_id = row[:team_id]
@@ -38,8 +49,8 @@ class StatTracker
                     face_off_win_percentage = row[:faceOffWinPercentage]
                     give_aways = row[:giveaways]
                     take_aways = row[:takeaway]
-                    #game_teams arguments needed
-                    @game_team = GameTeam()
+                    game_team = GameTeam(game_id, team_id, home_or_away_game, result, settled_in, head_coach, goals, shots, tackles, pentalty_infraction_min, power_play_opportunities, power_play_goals, face_off_win_percentage, give_aways, take_aways)
+                    @@game_teams << game_team
                 else
                     puts "I don't have access to #{file_name}, sorry."
                 end
