9f02a6fd72a7a551df5600df6096466ca030798c
lance butler <lancej2004@gmail.com>
2024-01-10 19:56:48 -0700

    Test: adds new describe initialize block and tests all attributes

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'simplecov'
 
 RSpec.describe Game do
    before(:each) do
@@ -12,9 +13,22 @@ RSpec.describe Game do
       }
    end
 
-   it 'exists' do
-      game = Game.new(@game_details)
+   describe '#initialize' do
+      it 'exists' do
+         game = Game.new(@game_details)
 
-      expect(game).to be_a Game
+         expect(game).to be_a Game
+      end
+
+      it 'has attributes' do
+         game = Game.new(@game_details)
+
+         expect(game.game_id).to eq(2012030221)
+         expect(game.season).to eq(20122013)
+         expect(game.away_team_id).to eq(3)
+         expect(game.home_team_id).to eq(6)
+         expect(game.away_goals).to eq(2)
+         expect(game.home_goals).to eq(3)
+      end
    end
 end
