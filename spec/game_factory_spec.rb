2053f0a9f7e55d2ca1c271b6c61e248ffd859e89
Laura Vega Vargas <ing.laurarvega@gmail.com>
2024-01-11 15:17:23 -0700

    Test: Add starts with no games created test for GameFactory class

--- spec/game_factory_spec.rb
+++ spec/game_factory_spec.rb
@@ -14,16 +14,24 @@ RSpec.describe GameFactory do
     it 'exists' do
       expect(@game_factory).to be_a(GameFactory)
     end
-  end
-
+    
     it 'has a file_path attributre' do
-    expect(@game_factory.file_path).to eq(@file_path)
+      expect(@game_factory.file_path).to eq(@file_path)
     end
+
+    it 'starts with no games created' do
+      expect(@game_factory.games).to eq([])
+    end
+  end
 
   describe 'create_games' do
     it 'creates game objects from data stored in the file_path attribute' do
       expect(@game_factory.create_games).to be_a(Array)
       expect(@game_factory.create_games.all? {|game| game.class == Game}).to be true
     end
+  end
+
+  describe 'count_of_goals' do
+    
   end
 end
