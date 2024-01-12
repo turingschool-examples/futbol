8229c9d0e99f9664778fc667f2174588492ec220
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2024-01-12 05:23:10 -0500

    Nothing

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,13 +1,12 @@
 require './spec/spec_helper'
 
 RSpec.describe Game do
-
     it 'exits' do
         game = Game.new({
             :game_id => 2012030221, 
             :season => 20122013, 
             :type => "Postseason", 
-            :data_time => 5/16/13, 
+            :date_time => 5/16/13, 
             :away_team_id => 3, 
             :home_team_id => 6, 
             :away_goals => 2, 
@@ -17,4 +16,20 @@ RSpec.describe Game do
 
         expect(game).to be_an_instance_of Game
     end 
+
+    it 'can have the highest total score' do
+        game = Game.new({
+            :game_id => 2012030221, 
+            :season => 20122013, 
+            :type => "Postseason", 
+            :date_time => 5/16/13, 
+            :away_team_id => 3, 
+            :home_team_id => 6, 
+            :away_goals => 2, 
+            :home_goals => 3, 
+            :venue => "Toyota Stadium"
+        })
+
+        expect(game.highest_total_score).to eq(5)
+    end
 end 
