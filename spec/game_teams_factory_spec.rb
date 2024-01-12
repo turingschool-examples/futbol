ad947de9539db5dd127e885ea86fcfe379613629
Jack Sweeney <jackcsweeney@gmail.com>
2024-01-11 17:24:04 -0800

    test: Add test for win_percentage_by_coach_by_season to GameTeamFactory.rb

--- spec/game_teams_factory_spec.rb
+++ spec/game_teams_factory_spec.rb
@@ -62,12 +62,11 @@ RSpec.describe GameTeamFactory do
     describe '#ratio_of_shots_to_goals_by_season(season)' do
         it 'can give you a hash with the shot to goal ratio of each team by their team id' do
             @game_team_factory.create_game_team
-
+            
             expect(@game_team_factory.ratio_of_shots_to_goals_by_season(20122013)).to eq({3 => 21.05, 6 => 31.58, 5 => 6.25, 17 => 20.00, 16 => 20.00})
         end
     end
     
-
     describe '#ratio_of_shots_to_goals' do
     # should edit fixture to test this with multiple seasons
         it 'can tell you the ratio of shots to goals for all teams by all seasons' do
@@ -76,7 +75,8 @@ RSpec.describe GameTeamFactory do
             expect(@game_team_factory.ratio_of_shots_to_goals).to eq({20122013 => {3 => 21.05, 6 => 31.58, 5 => 6.25, 17 => 20.00, 16 => 20.00}})
         end
     end
-
+    
+    
     describe '#game_result_by_hoa' do
         it 'returns an array of strings with the team that won (home, away, or tie)' do
             @game_team_factory.create_game_team
@@ -113,6 +113,12 @@ RSpec.describe GameTeamFactory do
         end
     end
             
-
+    describe '#win_percentage_by_coach_by_season(season)' do
+        it 'can give you a hash with the shot to goal ratio of each team by their team id' do
+            @game_team_factory.create_game_team
+            
+            expect(@game_team_factory.win_percentage_by_coach_by_season(20122013)).to eq({"John Tortorella" => 0.00, "Claude Julien" => 100.00, "Dan Bylsma" => 0.00, "Mike Babcock" => 0.00, "Joel Quenneville" => 100.00})
+        end
+    end
 
 end
