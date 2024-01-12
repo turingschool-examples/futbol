dd17038e8df8e1f6f5b9d97529522d067bcdc4f3
Selena Hawamdeh <selenahawamdeh@gmail.com>
2024-01-12 14:55:55 -0700

    Resolve: update file

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,9 @@
 class Game
     attr_reader :game_id,
-                :season, 
-                :type, 
-                :data_time, 
-                :away_team_id, 
+                :season,
+                :type,
+                :data_time,
+                :away_team_id,
                 :home_team_id,
                 :away_goals,
                 :home_goals,
@@ -19,36 +19,5 @@ class Game
         @away_goals = away_goals.to_i
         @home_goals = home_goals.to_i
         @venue = venue
-    end 
-
-    def total_score
-        @home_goals + @away_goals
-    end
-
-    def lowest_total_score
-
-    end
-    
-    def percentage_home_wins
-        
-    end
-
-    def percentage_visitor_wins
-
-    end 
-
-    def percentage_ties
-    end
-
-    def count_of_games_by_season
-
-    end
-
-    def average_goals_per_game
-
-    end
-
-    def average_goals_by_season
-
     end
 end
