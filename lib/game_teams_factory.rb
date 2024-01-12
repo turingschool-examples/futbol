e48d93e6fcdc0e14ae25385da85b5ae008ca8ac1
Jack Sweeney <jackcsweeney@gmail.com>
2024-01-11 16:32:50 -0800

    feat: Add ratio_of_shots_to_goals and ratio_of_shots_to_goals_by_team(team_id) to GameTeamsFactory

--- lib/game_teams_factory.rb
+++ lib/game_teams_factory.rb
@@ -34,15 +34,15 @@ class GameTeamFactory
     end
 
 
-    # def ratio_of_shots_to_goals_by_season(season)
-    #     ratios = {}
-    #     @game_teams.each do |game_team|
-    #         if game_team.get_season_from_game_id == season
-    #             shots = 0
-    #             goals = 0
-
-    #     end
-    # end
+    def ratio_of_shots_to_goals_by_season(season)
+        ratios = {}
+        @game_teams.each do |game_team|
+            if game_team.get_season_from_game_id == season
+                ratios[game_team.team_id] = ratio_of_shots_to_goals_by_team(game_team.team_id)
+            end
+        end
+        ratios
+    end
 
     def ratio_of_shots_to_goals_by_team(team_id)
         shots = 0
@@ -50,11 +50,19 @@ class GameTeamFactory
         @game_teams.each do |game_team|
             shots += game_team.shots if game_team.team_id == team_id
             goals += game_team.goals if game_team.team_id == team_id
-            # binding.pry
         end
-        (goals / shots)
+        ((goals.to_f / shots.to_f) * 100).round(2)
+    end
+
+    def ratio_of_shots_to_goals
+        hash = {}
+        @game_teams.each do |game_team|
+            hash[game_team.get_season_from_game_id] = ratio_of_shots_to_goals(game_team.get_season_from_game_id)
+        end
+        hash
     end
     
 end
+# binding.pry
 
 
