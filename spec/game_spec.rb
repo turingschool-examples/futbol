bbd469b0c92404529cf94388f4a318da1a8561fa
Selena Hawamdeh <selenahawamdeh@gmail.com>
2024-01-12 14:58:06 -0700

    Test: update test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,25 +2,23 @@ require './spec/spec_helper'
 
 RSpec.describe Game do
     before(:each) do
-        @game = Game.new({
-            :game_id => 2012030221, 
-            :season => 20122013, 
-            :type => "Postseason", 
-            :data_time => 5/16/13, 
-            :away_team_id => 3, 
-            :home_team_id => 6, 
-            :away_goals => 2, 
-            :home_goals => 3, 
-            :venue => "Toyota Stadium"
-        })
+        @game = Game.new(
+           2012030221,
+           20122013,
+           "Postseason",
+            5/16/13,
+            3,
+            6,
+            2,
+            3,
+            "Toyota Stadium"
+        )
     end
-    
-    end
-    it 'exits' do
-        expect(@game).to be_an_instance_of Game
-    end 
 
-    it 'calculates total score' do
-        expect(@game.total_score).to eq(5)
+
+    it 'exits' do
+        expect(@game).to be_an_instance_of(Game)
     end
-end 
+
+
+end
