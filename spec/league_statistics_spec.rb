bcdfc55d76f9e43d9ba801408f10994f5fb5da9b
Mel Langhoff <mlanghoff@uwalumni.com>
2024-01-12 11:44:53 -0700

    Fixed formatting of setups

--- spec/league_statistics_spec.rb
+++ spec/league_statistics_spec.rb
@@ -1,17 +1,16 @@
 RSpec.describe LeagueStatistics do
   before :each do
   @game_teams = [
-      { game_id: 2012030221, team_id: 3, HoA: 'away', result: 'LOSS', settled_in: 'OT', head_coach: 'John Tortorella', goals: 2, shots: 8, tackles: 44, pim: 8, powerPlayOpportunities: 3, powerPlayGoals: 0, faceOffWinPercentage: 44.8, giveaways: 17, takeaways: 7 },
-      { game_id: 2012030221, team_id: 6, HoA: 'home', result: 'WIN', settled_in: 'OT', head_coach: 'Claude Julien', goals: 3, shots: 12, tackles: 51, pim: 6, powerPlayOpportunities: 4, powerPlayGoals: 1, faceOffWinPercentage: 55.2, giveaways: 4, takeaways: 5 },
-      { game_id: 2012030222, team_id: 3, HoA: 'away', result: 'LOSS', settled_in: 'REG', head_coach: 'John Tortorella', goals: 2, shots: 9, tackles: 33, pim: 11, powerPlayOpportunities: 5, powerPlayGoals: 0, faceOffWinPercentage: 51.7, giveaways: 1, takeaways: 4 },
-      { game_id: 2012030222, team_id: 6, HoA: 'home', result: 'WIN', settled_in: 'REG', head_coach: 'Claude Julien', goals: 3, shots: 8, tackles: 36, pim: 19, powerPlayOpportunities: 1, powerPlayGoals: 0, faceOffWinPercentage: 48.3, giveaways: 16, takeaways: 6 }
-      # Add more game_teams if needed
+      { game_id: 2012030221, team_id: 3, hoa: 'away', result: 'LOSS', settled_in: 'OT', head_coach: 'John Tortorella', goals: 2, shots: 8, tackles: 44, pim: 8, powerplayopportunities: 3, powerplaygoals: 0, facepffwinpercentage: 44.8, giveaways: 17, takeaways: 7 },
+      { game_id: 2012030221, team_id: 6, hoa: 'home', result: 'WIN', settled_in: 'OT', head_coach: 'Claude Julien', goals: 3, shots: 12, tackles: 51, pim: 6, powerplayopportunities: 4, powerplaygoals: 1, facepffwinpercentage: 55.2, giveaways: 4, takeaways: 5 },
+      { game_id: 2012030222, team_id: 3, hoa: 'away', result: 'LOSS', settled_in: 'REG', head_coach: 'John Tortorella', goals: 2, shots: 9, tackles: 33, pim: 11, powerplayopportunities: 5, powerplaygoals: 0, facepffwinpercentage: 51.7, giveaways: 1, takeaways: 4 },
+      { game_id: 2012030222, team_id: 6, hoa: 'home', result: 'WIN', settled_in: 'REG', head_coach: 'Claude Julien', goals: 3, shots: 8, tackles: 36, pim: 19, powerplayopportunities: 1, powerplaygoals: 0, facepffwinpercentage: 48.3, giveaways: 16, takeaways: 6 }
     ]
   @teams = [
-      { team_id: 1, franchiseId: 23, teamName: 'Atlanta United', abbreviation: 'ATL', Stadium: 'Mercedes-Benz Stadium', link: '/api/v1/teams/1' },
-      { team_id: 4, franchiseId: 16, teamName: 'Chicago Fire', abbreviation: 'CHI', Stadium: 'SeatGeek Stadium', link: '/api/v1/teams/4' },
-      { team_id: 26, franchiseId: 14, teamName: 'FC Cincinnati', abbreviation: 'CIN', Stadium: 'Nippert Stadium', link: '/api/v1/teams/26' },
-      { team_id: 14, franchiseId: 31, teamName: 'DC United', abbreviation: 'DC', Stadium: 'Audi Field', link: '/api/v1/teams/14' }
+      { team_id: 1, franchiseid: 23, teamname: 'Atlanta United', abbreviation: 'ATL', stadium: 'Mercedes-Benz Stadium', link: '/api/v1/teams/1' },
+      { team_id: 4, franchiseid: 16, teamname: 'Chicago Fire', abbreviation: 'CHI', stadium: 'SeatGeek Stadium', link: '/api/v1/teams/4' },
+      { team_id: 26, franchiseid: 14, teamname: 'FC Cincinnati', abbreviation: 'CIN', stadium: 'Nippert Stadium', link: '/api/v1/teams/26' },
+      { team_id: 14, franchiseid: 31, teamname: 'DC United', abbreviation: 'DC', stadium: 'Audi Field', link: '/api/v1/teams/14' }
     ]
   end
 
