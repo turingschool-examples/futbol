0cadd203c2947d75eec72704de63030d6a5f4290
lance butler <lancej2004@gmail.com>
2024-01-12 16:11:37 -0700

    Fix: adds season_id as an arrgument into best and worst coach

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -154,8 +154,8 @@ class StatTracker
       avg_goals_by_season
    end
 
-   def winingest_coach
-      best_coach = coach_game_stats.max_by do |coach, stats|
+   def winingest_coach(season_id)
+      best_coach = coach_game_stats(season_id).max_by do |coach, stats|
          win_percentage = stats[:games_won].to_f / stats[:number_of_games]
          win_percentage 
       end
@@ -163,8 +163,8 @@ class StatTracker
       #require 'pry' ; binding.pry
    end
 
-   def worst_coach
-      worst_coach = coach_game_stats.min_by do |coach, stats|
+   def worst_coach(season_id)
+      worst_coach = coach_game_stats(season_id).min_by do |coach, stats|
          lose_percentage = stats[:games_won].to_f / stats[:number_of_games]
          lose_percentage 
       end
@@ -207,12 +207,14 @@ class StatTracker
       team_stats
    end
 
-   def coach_game_stats
+   def coach_game_stats(season_id)
       game_stats = Hash.new {|hash, key| hash[key] = {number_of_games: 0, games_won: 0 }}
       @data_game_teams.each do |game_team|
-         game_stats[game_team.head_coach][:number_of_games] += 1
-         if game_team.result == "WIN"
-         game_stats[game_team.head_coach][:games_won] += 1
+         if game_team.game_id == convert_season_id_to_game_id(season_id)
+            game_stats[game_team.head_coach][:number_of_games] += 1
+            if game_team.result == "WIN"
+               game_stats[game_team.head_coach][:games_won] += 1
+            end
          end
       end
       game_stats
@@ -228,4 +230,13 @@ class StatTracker
       end
       team_stats
    end
+
+   def convert_season_id_to_game_id(season_id)
+      team = @data_games.find do |team|
+         team.season == season_id
+      end
+      team.game_id
+      #require 'pry' ; binding.pry
+   end
+
 end
