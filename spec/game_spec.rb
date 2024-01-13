087b188df0fbe66f0ab664254e9b5b37821e8c48
Luis Aparicio <lfelipeaparicio2004@gmail.com>
2024-01-13 03:48:02 -0500

    Fix the test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,21 +2,10 @@ require './spec/spec_helper'
 
 RSpec.describe Game do
     before(:each) do
-        @game = Game.new({
-            :game_id => 2012030221, 
-            :season => 20122013, 
-            :type => "Postseason", 
-            :date_time => 5/16/13, 
-            :away_team_id => 3, 
-            :home_team_id => 6, 
-            :away_goals => 2, 
-            :home_goals => 3, 
-            :venue => "Toyota Stadium"
-        })
+        @game = Game.new(2012030221, 20122013, "Postseason", 5/16/13, 3, 6, 2, 3, "Toyota Stadium")
     end
     
-    end
-    it 'exits' do
+    it 'exists' do
         expect(@game).to be_an_instance_of Game
     end 
 
