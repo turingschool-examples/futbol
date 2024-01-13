908e422246c1e6b140bd789764283eee3ff312d9
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-01-13 12:08:30 -0600

    Test: game attributes

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,23 +2,24 @@ require './spec/spec_helper'
 
 RSpec.describe Game do
     before(:each) do
-        @game = Game.new({
-            :game_id => 2012030221, 
-            :season => 20122013, 
-            :type => "Postseason", 
-            :data_time => 5/16/13, 
-            :away_team_id => 3, 
-            :home_team_id => 6, 
-            :away_goals => 2, 
-            :home_goals => 3, 
-            :venue => "Toyota Stadium"
-        })
+        @game = Game.new("2012030221", "20122013", "Postseason", "5/16/13", 3, 6, 2, 3, "Toyota Stadium")
     end
     
-    end
     it 'exits' do
         expect(@game).to be_an_instance_of Game
-    end 
+    end
+
+    it "has attributes" do
+        expect(@game.game_id).to eq(2012030221)
+        expect(@game.season).to eq(20122013)
+        expect(@game.type).to eq("Postseason")
+        expect(@game.date_time).to eq("5/16/13")
+        expect(@game.away_team_id).to eq(3)
+        expect(@game.home_team_id).to eq(6)
+        expect(@game.away_goals).to eq(2)
+        expect(@game.home_goals).to eq(3)
+        expect(@game.venue).to eq("Toyota Stadium")
+    end
 
     it 'calculates total score' do
         expect(@game.total_score).to eq(5)
