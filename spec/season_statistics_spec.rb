d44f22657e198d3e6405e93a5b4607fb7bc29859
Hoa Dam <hoangocdam@gmail.com>
2024-01-12 23:10:52 -0800

    fix:test results

--- spec/season_statistics_spec.rb
+++ spec/season_statistics_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe SeasonStatistics do
       expect(season_stats.game_teams).to be_a(Array)
       expect(season_stats.games.length).to eq(4)
       expect(season_stats.teams.length).to eq(6)
-      expect(season_stats.game_teams.length).to eq(4)
+      expect(season_stats.game_teams.length).to eq(8)
     end
   end
 
@@ -29,13 +29,13 @@ RSpec.describe SeasonStatistics do
 
   describe '#calculate_win_percentage' do
     it 'returns hash where each coach is associated with their win percentage for the season' do
-      expect(season_stats.calculate_win_percentage(20122013)).to eq({"John Tortorella"=> 0.00, "Claude Julien"=> 100.00})
+      expect(season_stats.calculate_win_percentage(20122013)).to eq({"Claude Julien"=>100.0, "Joel Quenneville"=>50.0, "John Tortorella"=>0.0, "Mike Babcock"=>50.0})
     end
   end
 
   describe '#calculate_accurate_shots_pct' do
     it "returns the hash where each team ID is associated with the accurate shots percentage" do
-      expect(season_stats.calculate_accurate_shots_pct(20122013)).to eq({3=>23.53, 6=>30.0})
+      expect(season_stats.calculate_accurate_shots_pct(20122013)).to eq({3=>23.53, 6=>30.0, 17=>25.0, 16=>20.0})
     end
   end
 
@@ -47,13 +47,13 @@ RSpec.describe SeasonStatistics do
 
   describe '#least_accurate_team' do
     it 'returns the name of team with the worst ratio of shots to goals for the season' do
-      expect(season_stats.least_accurate_team(20122013)).to eq("Houston Dynamo")
+      expect(season_stats.least_accurate_team(20122013)).to eq("New England Revolution")
     end
   end
 
   describe '#tackles_by_teams' do
     it 'returns the hash where each team ID is associated with the total tackles in the season' do
-      expect(season_stats.tackles_by_teams(20122013)).to eq({3=>77, 6=>87})
+      expect(season_stats.tackles_by_teams(20122013)).to eq({3=>77, 6=>87, 17=>69, 16=>60})
     end
   end
 
@@ -65,7 +65,7 @@ RSpec.describe SeasonStatistics do
 
   describe '#fewest_tackles' do
     it 'returns the name of team with the most tackles in the season' do
-      expect(season_stats.fewest_tackles(20122013)).to eq("Houston Dynamo")
+      expect(season_stats.fewest_tackles(20122013)).to eq("New England Revolution")
     end
   end
 end
