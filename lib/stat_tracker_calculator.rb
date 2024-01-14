b6cc95fe545e51435f9bf8e5b796e1a6711f9f41
Dana Howell <howelld115@gmail.com>
2024-01-14 12:16:43 -0700

    feat: Add #average_goals_per_game

--- lib/stat_tracker_calculator.rb
+++ lib/stat_tracker_calculator.rb
@@ -59,4 +59,16 @@ class StatTrackerCalculator
   def percentage_ties
     (@game_team_factory.game_results_count_by_result("TIE").to_f / @game_factory.count_of_games.to_f).round(2)
   end
+
+  def count_of_games_by_season
+    count_of_games_by_season = {}
+    @game_factory.games.each do |game|
+      count_of_games_by_season[game.season] = @game_factory.season_games(game.season)
+    end
+    count_of_games_by_season
+  end
+
+  def average_goals_per_game
+    (@game_factory.total_score.sum.to_f / @game_factory.count_of_games.to_f).round(2)
+  end
 end
