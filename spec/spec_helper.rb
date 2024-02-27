3214b6f9a1338d6351311d6077e3378509091d5b
Steddman Bell <steddfast@gmail.com>
2024-02-27 14:09:28 -0800

    Initial creation of classes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,10 @@
 require 'simplecov'
+require 'pry'
+require 'rspec'
+
+RSpec.configure do |config|
+  config.formatter = :documentation
+end
 SimpleCov.start
 
 # Previous content of test helper now starts here
