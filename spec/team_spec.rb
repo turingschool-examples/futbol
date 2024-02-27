de36d78b938f5e81d9fc736e2bdac9f3d892e7dc
Grant Davis <grantdavis303@gmail.com>
2024-02-27 16:59:01 -0700

    Add test for Team having name and id attribute

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -3,13 +3,14 @@ require './spec/spec_helper'
 RSpec.describe Team do
 
   it 'exists' do
-    new_team = Team.new
-    expect(new_team).to be_a Team
-  end
-
-  it 'creates a team object' do
     new_team = Team.new("1", "Atlanta United")
     expect(new_team).to be_an_instance_of Team
+  end
+
+  it 'has a name and an id' do
+    new_team = Team.new("1", "Atlanta United")
+    expect(new_team.id).to eq 1
+    expect(new_team.name).to eq "Atlanta United"
   end
 
 end
