91b9a806b34d37e4089c5b0fc7beea38aece925e
Billy Wallace <wallace.billy1@gmail.com>
2024-02-27 15:48:16 -0700

    Create stat_tracker class, spec files, and runner file

---
+++ spec/gameteam_spec.rb
@@ -0,0 +1,5 @@
+require './spec_helper'
+
+describe GameTeam do
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 require 'simplecov'
 require 'pry'
-require 'rspec'
+require '../lib/game'
+require '../lib/gameteam'
+require '../lib/stat_tracker'
+require '../lib/team'
 
 RSpec.configure do |config|
   config.formatter = :documentation
