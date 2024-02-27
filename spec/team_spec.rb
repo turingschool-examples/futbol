418d74b017c3464ded0318639e20ddd15daa185e
Grant Davis <grantdavis303@gmail.com>
2024-02-27 16:54:47 -0700

    Add test for Team class creating new objects

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -1,5 +1,15 @@
-require './spec_helper'
+require './spec/spec_helper'
 
-describe Team do
+RSpec.describe Team do
+
+  it 'exists' do
+    new_team = Team.new
+    expect(new_team).to be_a Team
+  end
+
+  it 'creates a team object' do
+    new_team = Team.new("1", "Atlanta United")
+    expect(new_team).to be_an_instance_of Team
+  end
 
 end
