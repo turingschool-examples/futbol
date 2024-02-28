4eb19f4710e1237b5369481efd4eb4347cdf4c7c
Billy Wallace <wallace.billy1@gmail.com>
2024-02-28 07:48:49 -0700

    Add tests for initializing gameteam objects and displaying attributes

--- spec/gameteam_spec.rb
+++ spec/gameteam_spec.rb
@@ -1,5 +1,31 @@
-require './spec_helper'
+require './spec/spec_helper'
 
-describe GameTeam do
+RSpec.describe GameTeam do
+  before(:each) do
+    gameteam_data = { game_id: 2_012_030_221,
+    team_id: 3,
+    hoa: "away",
+    result: "LOSS",
+    head_coach: "John Tortorella",
+    goals: 2,
+    shots: 8,
+    tackles: 44
+    }
+    @gameteam1 = GameTeam.new(gameteam_data)
+  end
+
+  it 'exists' do
+    expect(@gameteam1).to be_an_instance_of GameTeam
+  end
+
+  it 'has attributes that can be read' do
+    expect(@gameteam1.team_id).to eq 3
+    expect(@gameteam1.hoa).to eq "away"
+    expect(@gameteam1.result).to eq "LOSS"
+    expect(@gameteam1.head_coach).to eq "John Tortorella"
+    expect(@gameteam1.goals).to eq 2
+    expect(@gameteam1.shots).to eq 8
+    expect(@gameteam1.tackles).to eq 44
+  end
 
 end
