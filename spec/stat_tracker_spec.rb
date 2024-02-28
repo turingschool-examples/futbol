555d3704c4bae3e0441e4d5abf2361bfd76a77ed
Billy Wallace <wallace.billy1@gmail.com>
2024-02-28 15:10:05 -0700

    Update before block and add tests for initializing data using from_csv method

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,10 +1,29 @@
 require './spec/spec_helper'
 
 RSpec.describe StatTracker do
+  before(:each) do
+    games_file = './data/games_dummy.csv'
+    teams_file = './data/teams.csv'
+    game_teams_file = './data/game_teams_dummy.csv'
+
+    locations = {
+      games: games_file,
+      teams: teams_file,
+      game_teams: game_teams_file
+    }
+
+    @stat_tracker = StatTracker.from_csv(locations)
+  end
 
   it 'exists' do
     stat_tracker = StatTracker.new
-    expect(stat_tracker).to be_a StatTracker
+    expect(@stat_tracker).to be_a StatTracker
+  end
+
+  it 'initializes data from a CSV file' do
+    expect(@stat_tracker.games.first).to be_a Game
+    expect(@stat_tracker.teams.first).to be_a Team
+    expect(@stat_tracker.game_teams.first).to be_a GameTeam
   end
 
 end
