881682f4076602948b87e52fa6c136d170752da7
Grant Davis <grantdavis303@gmail.com>
2024-02-28 14:15:55 -0700

    Format Game with Hash

--- lib/game.rb
+++ lib/game.rb
@@ -1,32 +1,38 @@
+require './spec/spec_helper'
+
 class Game
-    attr_reader :game_id,
-                :season,
-                :type,
-                :away_team_id,
-                :home_team_id,
-                :away_goals,
-                :home_goals
+  attr_reader :game_id,
+              :season,
+              :type,
+              :away_team_id,
+              :home_team_id,
+              :away_goals,
+              :home_goals
 
-    def initialize(game_id, season, type, away_team_id, home_team_id, away_goals, home_goals)
-        @game_id = game_id
-        @season = season
-        @type = type.to_s
-        @away_team_id = away_team_id
-        @home_team_id = home_team_id
-        @away_goals = away_goals
-        @home_goals = home_goals
-    end
+  def initialize(game_data)
+    @game_id = game_data[:game_id]
+    @season = game_data[:season]
+    @type = game_data[:type].to_s
+    @away_team_id = game_data[:away_team_id]
+    @home_team_id = game_data[:home_team_id]
+    @away_goals = game_data[:away_goals]
+    @home_goals = game_data[:home_goals]
+  end
 
-    def self.create_from_csv(file_path)
-        CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
-            game_id = row[:game_id]
-            season = row[:season]
-            type = row[:type]
-            away_team_id = row[:away_team_id]
-            home_team_id = row[:home_team_id]
-            away_goals = row[:away_goals]
-            home_goals = row[:home_goals]
-            Game.new(game_id, season, type, away_team_id, home_team_id, away_goals, home_goals)
-        end
+  def self.create_from_csv(file_path)
+    games = []
+    CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
+      game_Data = {
+        game_id: row["game_id"],
+        season: row["season"],
+        type: row["type"],
+        away_team_id: row["away_team_id"],
+        home_team_id: row["home_team_id"],
+        away_goals: row["away_goals"],
+        home_goals: row["home_goals"]
+    }
+    games << Game.new(game_id, season, type, away_team_id, home_team_id, away_goals, home_goals)
     end
+    games
+  end
 end
