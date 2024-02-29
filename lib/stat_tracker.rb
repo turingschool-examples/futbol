e0460186067d5d50d67ec6e36805ded1e219ad90
Grant Davis <grantdavis303@gmail.com>
2024-02-28 17:59:16 -0700

    Create Percentage_Home_Wins Method & RSpec Test

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -14,16 +14,25 @@ class StatTracker
     StatTracker.new(games, teams, game_teams)
   end
 
+  def percentage_home_wins
+    total_game_count = self.games.length # 7441
+    win_counter = 0 # 3237
+    self.games.each do |game|
+      if game.home_goals > game.away_goals
+        win_counter += 1
+      end
+    end
+    (win_counter / total_game_count.to_f).round(2)
+  end
+
   def percentage_ties
     total_game_count = self.games.length # 7441
-    tie_counter = 0 #1517
-
+    tie_counter = 0 # 1517
     self.games.each do |game|
       if game.away_goals == game.home_goals
         tie_counter += 1
       end
     end
-
     (tie_counter / total_game_count.to_f).round(2)
   end
 
