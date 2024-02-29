410c9b612bed0c827be71a36e2d39370c2c929de
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-29 14:58:03 -0700

    Add class methods to complete testing

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -17,4 +17,34 @@ class StatTracker
     game_teams = GameTeam.create_from_csv(locations[:game_teams])
     StatTracker.new(games, teams, game_teams)
   end
+
+  def self.highest_total_score
+    Game.highest_total_score
+  end
+
+  def self.lowest_total_score
+    Game.lowest_total_score
+  end
+
+  def self.count_of_teams
+    Team.count_of_teams
+  end
+
+  def self.best_offense
+    GameTeams.best_offense
+  end
+
+  def self.worst_offense
+    GameTeams.worst_offense
+  end
+
+  def self.most_tackles
+    # Game.most_tackles
+  end
+
+
+
+
+
+
 end
