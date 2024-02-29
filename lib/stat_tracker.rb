60ea77db16175e2ad7d71c097715aa2aa8dc87c8
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-28 18:23:43 -0700

    Add some required files and create #highest_total_score to access an array of scores summed and find the highest one

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,17 +1,28 @@
+require_relative "./game"
+require_relative "./team"
+require_relative "./gameteam"
+
 class StatTracker
   attr_reader :games, :teams, :game_teams
-  
+
   def initialize(games, teams, game_teams)
     @games = games
     @teams = teams
     @game_teams = game_teams
   end
-  
+
   def self.from_csv(locations)
     games = Game.create_from_csv(locations[:games])
     teams = Team.create_from_csv(locations[:teams])
     game_teams = GameTeam.create_from_csv(locations[:game_teams])
     StatTracker.new(games, teams, game_teams)
+  end
+
+  def highest_total_score
+    totals = games.map do |game|
+      game.away_goals + game.home_goals
+    end
+    totals.max
   end
 
 end
