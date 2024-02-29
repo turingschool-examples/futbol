e7b26c9feade51cb8a03cabdd6a804ec3beb239d
Aurora Sanderson <AuroraTuring@gmail.com>
2024-02-29 14:42:31 -0700

    Remove unnecessary tests and methods

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -25,20 +25,4 @@ RSpec.describe StatTracker do
     expect(@stat_tracker.teams.first).to be_a Team
     expect(@stat_tracker.game_teams.first).to be_a GameTeam
   end
-
-  it '#percentage_home_wins returns correct return value' do
-    expect(@stat_tracker.percentage_home_wins).to be_a Float
-    expect(@stat_tracker.percentage_home_wins).to eq 0.44
-  end
-
-  it '#percentage_visitor_wins returns correct return value' do
-    expect(@stat_tracker.percentage_visitor_wins).to be_a Float
-    expect(@stat_tracker.percentage_visitor_wins).to eq 0.36
-  end
-
-  it '#percentage_ties returns correct return value' do
-    expect(@stat_tracker.percentage_ties).to be_a Float
-    expect(@stat_tracker.percentage_ties).to eq 0.20
-  end
-
 end
