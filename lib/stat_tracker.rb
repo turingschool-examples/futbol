29b775f6f023929eec37807fefb496778a48506f
Grant Davis <grantdavis303@gmail.com>
2024-02-28 17:48:04 -0700

    Add Percentage_Ties Method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -14,4 +14,17 @@ class StatTracker
     StatTracker.new(games, teams, game_teams)
   end
 
+  def percentage_ties
+    total_game_count = self.games.length # 7441
+    tie_counter = 0
+
+    self.games.each do |game|
+      if game.away_goals == game.home_goals
+        tie_counter += 1 #1517
+      end
+    end
+
+    (tie_counter / total_game_count.to_f).round(2)
+  end
+
 end
