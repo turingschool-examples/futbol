2e7595a2a27cbcb35f645124cfe9b2e389cb72b2
Billy Wallace <wallace.billy1@gmail.com>
2024-03-03 09:40:38 -0700

    Add test for fewest_tackles

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -56,4 +56,9 @@ RSpec.describe StatTracker do
     expect(goals_by_season.count).to eq 6
   end
 
+  it '#fewest_tackles' do
+    expect(@stat_tracker.fewest_tackles("20122013")).to eq "Sporting Kansas City"
+    expect(@stat_tracker.fewest_tackles("20132014")).to eq "New England Revolution"
+  end
+
 end
