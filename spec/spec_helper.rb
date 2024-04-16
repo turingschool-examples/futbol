555cff6698661b9723e9b4cd4dfb3b9aaa92fd71
James McHugh <jdmchugh111@gmail.com>
2024-04-16 17:17:35 -0400

    Added simplecov gem and included modules in StatTracker

---
+++ spec/spec_helper.rb
@@ -0,0 +1,11 @@
+require 'simplecov'
+SimpleCov.start 
+
+require './lib/game_stats.rb'
+require './lib/game_team.rb'
+require './lib/game.rb'
+require './lib/league_stats.rb'
+require './lib/season_stats.rb'
+require './lib/stat_tracker.rb'
+require './lib/team.rb'
+
