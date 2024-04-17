b19f68f38c74706d9bf2fe7b356aadef240888b2
Lance Nelson <lancen04@gmail.com>
2024-04-17 16:11:30 -0400

    added game stats methods

--- lib/game_stats.rb
+++ lib/game_stats.rb
@@ -1,4 +1,39 @@
 
 
+
+
 module GameStats
+
+	def highest_total_score(season) # integer
+		# require 'pry'; binding.pry
+	end
+
+	def lowest_total_score(season) # integer
+
+	end
+
+	def percentage_home_wins(season) # float
+
+	end
+
+	def percentage_visitor_wins(season) # float
+
+	end
+
+	def percentage_ties(season) # float
+
+	end
+
+	def count_of_games_by_season(season) # hash
+
+	end
+
+	def average_goals_per_game(game_id) # float
+
+	end
+
+	def average_goals_by_season(season) # hash
+
+	end
+
 end
