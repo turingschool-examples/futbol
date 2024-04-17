09e1c797a3cf426a641f0e3f2f3db4ab3780a26e
Jan Lehigh <janlehigh461437@gmail.com>
2024-04-17 15:38:28 -0600

    Add basic infrastructure for game_stats_spec file

---
+++ spec/game_stats_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper.rb'
+
+RSpec.describe LeagueStats do
+  before(:each) do
+    @game_path = './data/games_fixture.csv'
+    @team_path = './data/teams.csv'
+    @game_teams_path = './data/game_teams_fixture.csv'
+    @locations = {
+      games: @game_path,
+      teams: @team_path,
+      game_teams: @game_teams_path
+    }
+    @stat_tracker = StatTracker.from_csv(@locations)
+  end
+end
