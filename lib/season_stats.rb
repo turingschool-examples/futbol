4a62075277f4611b5bb25d9a7ecb4308fd985fdc
James McHugh <jdmchugh111@gmail.com>
2024-04-16 23:26:01 -0400

    Wrote and passed tests for winningest_coach and worst_coach

--- lib/season_stats.rb
+++ lib/season_stats.rb
@@ -1,28 +1,39 @@
 module SeasonStats
     
     def winningest_coach(season_id)
+        get_all_games(season_id)
+        load_coach_array
+        load_coach_hash
+        coach_percentage_hash
+        @coach_percentages.sort_by {|k, v| -v}.first[0]
+    end
+
+    def get_all_games(season_id)
         @all_season_games = []
         @game_teams.each do |game_team|
             if game_team.season_id == season_id
                 @all_season_games << game_team
             end
         end
-        coach_array = []
-        coach_hash = {}
+        @all_season_games
+    end
+
+    def load_coach_array
+        @coach_array = []
         @all_season_games.each do |game|
-            if !coach_array.include?(game.head_coach)
-                coach_array << game.head_coach
+            if !@coach_array.include?(game.head_coach)
+                @coach_array << game.head_coach
             end
         end
-        coach_array.each do |coach|
-            coach_hash[coach] = coach_subhash(coach)
+        @coach_array
+    end
+
+    def load_coach_hash
+        @coach_hash = {}
+        @coach_array.each do |coach|
+            @coach_hash[coach] = coach_subhash(coach)
         end
-        coach_percentages = {}
-        coach_hash.each do |coach, subhash|
-            coach_percentages[coach] = subhash[:wins].to_f / subhash[:total_games].to_f
-        end
-        sorted_percentages = coach_percentages.sort_by {|k, v| -v}
-        sorted_percentages.first[0]
+        @coach_hash
     end
 
     def coach_subhash(coach)
@@ -39,29 +50,20 @@ module SeasonStats
         {wins: win_tally, total_games: total_game_tally}
     end
 
+    def coach_percentage_hash
+        @coach_percentages = {}
+        @coach_hash.each do |coach, subhash|
+            @coach_percentages[coach] = subhash[:wins].to_f / subhash[:total_games].to_f
+        end
+        @coach_percentages
+    end
+
     def worst_coach(season_id)
-        @all_season_games = []
-        @game_teams.each do |game_team|
-            if game_team.season_id == season_id
-                @all_season_games << game_team
-            end
-        end
-        coach_array = []
-        coach_hash = {}
-        @all_season_games.each do |game|
-            if !coach_array.include?(game.head_coach)
-                coach_array << game.head_coach
-            end
-        end
-        coach_array.each do |coach|
-            coach_hash[coach] = coach_subhash(coach)
-        end
-        coach_percentages = {}
-        coach_hash.each do |coach, subhash|
-            coach_percentages[coach] = subhash[:wins].to_f / subhash[:total_games].to_f
-        end
-        sorted_percentages = coach_percentages.sort_by {|k, v| -v}
-        sorted_percentages.last[0]
+        get_all_games(season_id)
+        load_coach_array
+        load_coach_hash
+        coach_percentage_hash
+        @coach_percentages.sort_by {|k, v| v}.first[0]
     end
 
 end
--- spec/season_stats_spec.rb
+++ spec/season_stats_spec.rb
@@ -1,1 +1,63 @@
 require 'spec_helper'
+
+RSpec.describe SeasonStats do
+    before(:all) do
+        @locations = {games: './data/games_fixture.csv', 
+                    teams: './data/teams.csv', 
+                    game_teams: './data/game_teams_fixture.csv'}
+        @stat_tracker = StatTracker.from_csv(@locations)
+    end
+
+    describe "#winningest_coach" do
+        it "can return winningest coach" do
+            expect(@stat_tracker.winningest_coach("20122013")).to eq("Claude Julien")
+        end
+    end
+
+    describe "#get_all_games" do
+        it "returns an empty array if season_id doesn't match any objects" do
+            expect(@stat_tracker.get_all_games("20152016")).to eq([])
+        end
+        it "can make an array of all game_team objects" do
+            expect(@stat_tracker.get_all_games("20122013").count).to eq(20)
+            expect(@stat_tracker.get_all_games("20122013")).to be_all(GameTeam)
+        end
+    end
+
+    describe "#load_coach_array" do
+        it "can load all coach names into an array" do
+            expect(@stat_tracker.load_coach_array).to eq(["John Tortorella", "Claude Julien", "Dan Bylsma", "Mike Babcock", "Joel Quenneville"])
+        end
+    end
+
+    describe "#load_coach_hash" do
+        it "can create a hash with coach names as keys" do
+            expect(@stat_tracker.load_coach_hash.keys).to eq(["John Tortorella", "Claude Julien", "Dan Bylsma", "Mike Babcock", "Joel Quenneville"])
+        end
+    end
+
+    describe "#coach_subhash" do
+        it "can create a subhash with wins and total games" do
+            expect(@stat_tracker.coach_subhash("Dan Bylsma")).to eq({wins: 0, total_games: 4})
+        end
+    end
+
+    describe "#coach_percentage_hash" do
+        it "can create a hash with coaches as keys and win percentage as values" do
+            expected_hash = {"John Tortorella" => 0.0, 
+                            "Claude Julien" => 1.0, 
+                            "Dan Bylsma" => 0.0, 
+                            "Mike Babcock" => 0.0, 
+                            "Joel Quenneville" => 1.0}
+
+
+            expect(@stat_tracker.coach_percentage_hash).to eq(expected_hash)
+        end
+    end
+
+    describe "#worst_coach" do
+        it "can return worst coach" do
+            expect(@stat_tracker.worst_coach("20122013")).to eq("John Tortorella")
+        end
+    end
+end
