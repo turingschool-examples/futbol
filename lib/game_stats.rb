f3894d3141e169248c13de836060f47fa8fdbd5a
Jan Lehigh <janlehigh461437@gmail.com>
2024-04-17 19:56:55 -0600

    Add tests for game_stats averag_goals_by_season method and method

--- lib/game_stats.rb
+++ lib/game_stats.rb
@@ -6,9 +6,9 @@ module GameStats
         games_per_season = Hash.new(0)
         @games.each do |game|
             if games_per_season.has_key?(game.season_id)
-            games_per_season[game.season_id] += 1
+                games_per_season[game.season_id] += 1
             else
-            games_per_season[game.season_id] = 1
+                games_per_season[game.season_id] = 1
             end
         end
         games_per_season
@@ -20,10 +20,16 @@ module GameStats
             average_of_games << game.away_goals.to_f
             average_of_games << game.home_goals.to_f
         end
-        average_of_games.sum/ average_of_games.size
+        average_of_games = average_of_games.sum / average_of_games.size
+        average_of_games.round(2)
     end
 
-    # def average_goals_by_season
-
-    # end
+    def average_goals_by_season
+        average_goals_per_season = {}
+        @games.each do |game|
+            if average_goals_per_season.has_key?(game.season_id)
+                games_per_season[game.season_id] += average_goals_per_game
+            end
+        end
+    end
 end
