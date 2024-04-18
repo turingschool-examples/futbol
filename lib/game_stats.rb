886e80b33a64db8ea92a0f24fad9ebbbd8b1c241
Jan Lehigh <janlehigh461437@gmail.com>
2024-04-17 19:37:06 -0600

    Add average_goals_per_game method to game_stats module

--- lib/game_stats.rb
+++ lib/game_stats.rb
@@ -13,33 +13,15 @@ module GameStats
         end
         games_per_season
     end
-      
 
-
-
-
-    # games_of_season = []
-    #     @games.map do |game|
-    #         games_of_season = game.season_id
-    #     end
-    #     games_of_season
-    # end
-
-
-
-
-
-    # def count_of_games_by_season(games)
-    #     # games_by_season = {}
-    #     # @games.each do |game|
-    #     #     games_by_season.store(game.season_id, game.season_id.count)
-    #     # end
-    #     # games_by_season
-    # end
-
-    # def average_goals_per_game
-        
-    # end
+    def average_goals_per_game
+        average_of_games = []
+        @games.each do |game|
+            average_of_games << game.away_goals.to_f
+            average_of_games << game.home_goals.to_f
+        end
+        average_of_games.sum/ average_of_games.size
+    end
 
     # def average_goals_by_season
 
