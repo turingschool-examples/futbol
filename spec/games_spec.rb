51d625400bf2e4896fc8102876539631c606b9c7
James-Cochran <jameslikesfruit@gmail.com>
2024-06-05 16:06:39 -0400

    corrected game.rb file to games.rb.  rewrote games class to be instances instead of a hash.  rewrote games_spec to correspond to games being instances.

--- spec/games_spec.rb
+++ spec/games_spec.rb
@@ -4,22 +4,31 @@ RSpec.configure do |config|
   config.formatter = :documentation
 end
 
-RSpec.describe Game do
-  before(:each) do
+RSpec.describe Games do
+  describe '#initialize' do
+    it "exists with attributes" do
+      game = Games.new(2012030221, 20122013, "Postseason", 5/16/13, 3, 6, 2, 3, "Toyota Stadium", "/api/v1/venues/null")
 
-    hash = {:game_id => 2012030221,
-            :sesason => 20122013,
-            :type => "Postseason",
-            :date_time => 5/16/13,
-            :away_team_id => 3,
-            :home_team_id => 6,
-            :away_goals => 2,
-            :home_goals => 3,
-            :venue => "Toyota Stadium",
-            :venue_link => /api/v1/venues/null}
-
-            @game = Game.new(hash)
+      expect(game).to be_a Games
+      expect(game.game_id).to be 2012030221
+      expect(game.season).to be 20122013
+      expect(game.type).to eq("Postseason")
+      expect(game.date_time).to be 5/16/13
+      expect(game.away_team_id).to be 3
+      expect(game.home_team_id).to be 6
+      expect(game.away_goals).to be 2
+      expect(game.home_goals).to be 3
+      expect(game.venue).to eq("Toyota Stadium")
+      expect(game.venue_link).to eq("/api/v1/venues/null")
+    end
   end
-
+end
+  
+  
+  
+  
+  
+  
+ 
 
 
