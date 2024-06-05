0cad7c6f9d1f5ce260c910ebbb34c8b5419cbc47
Candice Cirbo <thecirbos@icloud.com>
2024-06-05 15:59:34 -0600

    adds instance variables to team class and game-teams class

--- lib/team.rb
+++ lib/team.rb
@@ -1,9 +1,21 @@
 require 'csv'
 
 class Team
-    attr_reader :team_data
+    attr_reader :team_id,
+                :franchiseId,
+                :teamName,
+                :abbreviation,
+                :Stadium,
+                :link
+
+
 
     def initialize(team_data)
-        @team_data = team_data
+        @team_id = team_data[:team_id]
+        @franchiseId = team_data[:franchiseId]
+        @teamName = team_data[:teamName]
+        @abbreviation = team_data[:abbreviation]
+        @Stadium = team_data[:Stadium]
+        @link = team_data[:link]
     end
 end
