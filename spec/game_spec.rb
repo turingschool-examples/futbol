a2837909cdbcb9fde15e7cf5628c12114e8b40a0
Joel Davalos <jsdavalos@gmail.com>
2024-06-05 20:27:34 -0600

    Finished bulding game tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,7 +1,33 @@
 require 'spec_helper'
+require 'rspec'
+require 'csv'
 
 RSpec.describe Game do
-    it 'exists' do
+   before(:all) do
+      @games = []
+      CSV.foreach('./data/games.csv', headers: true) do |row|
+        p row
+        @games << Game.new(row)
+      end
+   end
 
-    end
+   describe 'Game instance' do
+      it 'exists' do
+        expect(@games.first).to be_an_instance_of(Game)
+      end
+
+      it 'has correct attributes' do
+        game = @games.first
+        expect(game.game_id).to eq('2012030221')
+      expect(game.season).to eq('20122013')
+      expect(game.type).to eq('Postseason')
+      expect(game.date_time).to eq('2013-05-16')
+      expect(game.away_team_id).to eq('3')
+      expect(game.home_team_id).to eq('6')
+      expect(game.away_goals).to eq('2')
+      expect(game.home_goals).to eq('3')
+      expect(game.venue).to eq('Toyota Stadium')
+      expect(game.venue_link).to eq('/api/v1/venues/null')
+      end
+   end
 end
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,7 +1,58 @@
 require 'spec_helper'
+require 'rspec'
+
 
 RSpec.describe StatTracker do
-    it 'exists' do
-
+    before(:all) do 
+      game_path = './data/games.csv'
     end
+
+      locations = {
+        games: game_path
+      }
+
+      @stat_tracker = StatTracker.from_csv(locations)
+
+
+  describe '#highest_total_score' do 
+  it 'returns and integer' do
+      expect(@stat_tracker.highest_total_score).to be_a(Integer)
+    end
+  end
+
+  descrbie '#lowest_total_score' do 
+    it 'returns an integer' do
+      espect(@stat_tracker.lowest_total_score).to be_a(Integer)
+    end
+  end
+
+  describe '#percentage_home_wins' do
+    it 'returns a float' do
+      expect(@stat_tracker.percentage_home_wins).to be_a(Float)
+    end
+  end
+  
+  describe '#percentage_ties' do 
+    it 'returns a float' do 
+      expect(@stat_tracker.percentage_ties).to be_a(Float)
+    end
+  end
+
+  describe '#count_of_games_by_season' do 
+    it 'returns a hash' do
+      expect(@stat_tracker.count_of_games_by_season).to be_a(Hash)
+    end
+  end
+
+  describe '#average_goals_per_game' do
+    it 'returns a float' do 
+      expect(@stat_tracker.count_of_games_per_game).to be_a(Float)
+    end
+  end
+
+  describe '#average_goals_by_season' do
+    it 'returns a hash' do
+      expect(@stat_tracker.average_goals_by_season).to be_a(Hash)
+    end
+  end
 end
