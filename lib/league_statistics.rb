198664418b09807b7eab0ff241b1d31eb84ec65b
Karl Fallenius <smilodon.p@gmail.com>
2024-06-06 12:00:34 -0600

    minimal progress on league stat methods

--- lib/league_statistics.rb
+++ lib/league_statistics.rb
@@ -1,13 +1,25 @@
-require 'csv'
+require_relative 'csv'
 
 class LeagueStatistics
   attr_reader 
-  def initialize()
 
+  def initialize()
+    @teams = Teams.create_teams_data_objects(path)
+    @game_teams = GameTeams.create_game_teams_data_objects(path)
   end
   
   def count_of_teams
-    teams.count
+    @teams.count
   end
 
+  def average_goals(team)
+    total_goals = game_teams[team].goals
+    total_goals.inject(0.0) {|sum, goals| sum + goals}/total_goals.size
+  end
+
+  def best_offense
+    game_teams.max_by {|team, average_goals| ###}.first.team_name
+
+  end
 end
+#.MAP!
--- spec/league_statistics_spec.rb
+++ spec/league_statistics_spec.rb
@@ -15,6 +15,34 @@ describe LeagueStatistics do
       expect(teams).to be_all Teams
     end
 
+    xit "can count the total number of goals a team has scored" do
+      path = "./data/test_game_teams.csv"
+      game_teams = GameTeams.create_game_teams_data_objects(path)
+
+      
+    end
+
+    xit "can determine a team's average number of goals per game" do
+      path = "./data/test_game_teams.csv"
+      game_teams = GameTeams.create_game_teams_data_objects(path)
+      team = Teams.new(1, 23, "Atlanta United", "ATL", "Mercedes-Benz Stadium", "/api/v1/teams/1")
+      #Insert test logic
+      expect(game_teams.team.average_goals).to eq()
+    end
+    
+    xit "can determine which team has had the best offense" do
+      path = "./data/test_game_teams.csv"
+      game_teams = GameTeams.create_game_teams_data_objects(path)
+      #Insert test logic
+      
+    end
+    
+    xit "can determine which team has had the worst offense" do
+      path = "./data/test_game_teams.csv"
+      game_teams = GameTeams.create_game_teams_data_objects(path)
+      #Insert test logic
+      
+    end
 
   end
 end
