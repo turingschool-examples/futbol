b193f85ebd93ab23c41338c118b15dba16b65716
Tyler Noble <tmnoble11@gmail.com>
2024-06-06 12:02:03 -0600

    Added: the rest of the methods in league_stat.rb.

--- lib/league_stats.rb
+++ lib/league_stats.rb
@@ -7,12 +7,10 @@ class LeagueStats
         @team_data = team_data
     end
 
-    #total num of teams in the data
     def count_of_teams
         @game_teams_data.map {|game| game.team_id}.uniq.count
     end
 
-    #best offense. Team with highest average number of goals scored per game across all seasons.
     def best_offense
         team_goals = @game_teams_data.group_by {|game| game.team_id}
         offense = team_goals.transform_values do |games|
@@ -24,7 +22,6 @@ class LeagueStats
         team
     end
 
-    #worst offense. Team with lowest average number of goals scored per game across all seasons.
     def worst_offense
         team_goals = @game_teams_data.group_by {|game| game.team_id}
         offense = team_goals.transform_values do |games|
@@ -36,7 +33,6 @@ class LeagueStats
         team
     end
 
-    #highest scoring visitor. Team with highest average number of goals scored per game when they are away.
     def highest_scoring_visitor
         visitors = @game_teams_data.select {|game| game.hoa == "away"}
         visitor_goals = visitors.group_by {|game| game.team_id}
@@ -46,6 +42,43 @@ class LeagueStats
         end
         visitor_goals = visitor_goals.max_by {|k, v| v}
         team = @team_data.find {|team| team.team_id == visitor_goals[0]}.team_name
+        team
+    end
+
+       
+    def highest_scoring_home_team
+        home = @game_teams_data.select {|game| game.hoa == "home"}
+        home_goals = home.group_by {|game| game.team_id}
+        home_goals = home_goals.transform_values do |games|
+            goals = games.sum {|game| game.goals.to_f}
+            goals / games.count
+        end
+        home_goals = home_goals.max_by {|k, v| v}
+        team = @team_data.find {|team| team.team_id == home_goals[0]}.team_name
+        team
+    end
+
+    def lowest_scoring_visitor
+        visitors = @game_teams_data.select {|game| game.hoa == "away"}
+        visitor_goals = visitors.group_by {|game| game.team_id}
+        visitor_goals = visitor_goals.transform_values do |games|
+            goals = games.sum {|game| game.goals.to_f}
+            goals / games.count
+        end
+        visitor_goals = visitor_goals.min_by {|k, v| v}
+        team = @team_data.find {|team| team.team_id == visitor_goals[0]}.team_name
+        team
+    end
+
+    def lowest_scoring_home_team
+        home = @game_teams_data.select {|game| game.hoa == "home"}
+        home_goals = home.group_by {|game| game.team_id}
+        home_goals = home_goals.transform_values do |games|
+            goals = games.sum {|game| game.goals.to_f}
+            goals / games.count
+        end
+        home_goals = home_goals.min_by {|k, v| v}
+        team = @team_data.find {|team| team.team_id == home_goals[0]}.team_name
         team
     end
 end
