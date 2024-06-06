f1ac2cace8e2d8247acf442a1b097ef0eaa22cc9
Cameron Pittman <166861602+CPPittman@users.noreply.github.com>
2024-06-06 10:55:16 -0600

    fix indention

--- lib/game_team.rb
+++ lib/game_team.rb
@@ -1,4 +1,4 @@
-class Game_team
+class GameTeam
     attr_reader :game_id, :team_id, :HoA, :result, :settled_in, :head_coach, :goals, :shots, :tackles, :pim, :powerPlayOpportunities, :powerPlayGoals, :faceOffWinPercentage, :giveaways, :takeaways
     #readable instance variables
 
