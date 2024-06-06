ce2c8583ade713f268da60d0f73be9bf7af0060b
Tyler Noble <tmnoble11@gmail.com>
2024-06-06 11:08:43 -0600

    Added: worst_offense, highest_scoring_visitor methods.

--- lib/league_stats.rb
+++ lib/league_stats.rb
@@ -23,4 +23,29 @@ class LeagueStats
         team = @team_data.find {|team| team.team_id == offense[0]}.team_name
         team
     end
+
+    #worst offense. Team with lowest average number of goals scored per game across all seasons.
+    def worst_offense
+        team_goals = @game_teams_data.group_by {|game| game.team_id}
+        offense = team_goals.transform_values do |games|
+            goals = games.sum {|game| game.goals.to_f}
+            goals / games.count
+        end
+        offense = offense.min_by {|k, v| v}
+        team = @team_data.find {|team| team.team_id == offense[0]}.team_name
+        team
+    end
+
+    #highest scoring visitor. Team with highest average number of goals scored per game when they are away.
+    def highest_scoring_visitor
+        visitors = @game_teams_data.select {|game| game.hoa == "away"}
+        visitor_goals = visitors.group_by {|game| game.team_id}
+        visitor_goals = visitor_goals.transform_values do |games|
+            goals = games.sum {|game| game.goals.to_f}
+            goals / games.count
+        end
+        visitor_goals = visitor_goals.max_by {|k, v| v}
+        team = @team_data.find {|team| team.team_id == visitor_goals[0]}.team_name
+        team
+    end
 end
