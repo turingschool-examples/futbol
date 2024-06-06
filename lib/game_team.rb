3ac92877cb20ee5dbe0a7a8341ce087c975fb540
Kaelin Salazar <kaelinpsalazar@gmail.com>
2024-06-05 20:35:51 -0600

    I have added some methods and a couple new pages

---
+++ lib/game_team.rb
@@ -0,0 +1,11 @@
+require 'csv'
+
+class Game_team
+    attr_reader :game_teams_data
+
+    def initialize(game_teams_data)
+        @game_teams_data = game_teams_data
+    end
+
+
+end
---
+++ lib/season_stat.rb
@@ -0,0 +1,10 @@
+class SeasonStat
+  attr_reader :game_teams_data
+
+  def initialize(game_teams_data)
+    @game_teams_data = game_teams_data
+  end
+
+  
+  
+end
