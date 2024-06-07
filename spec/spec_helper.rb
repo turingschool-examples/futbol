6137c8ce04aaa3b2a5feae340a43405fa4852402
Candice Cirbo <thecirbos@icloud.com>
2024-06-07 00:04:44 -0600

    adds addtional files to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,9 @@ SimpleCov.start
 
 require './lib/stat_tracker'
 require  './lib/game'
-require './lib/league'
-require './lib/season'
+require './lib/game_stat'
+require './lib/league_stat'
+require './lib/season_stat'
+require './lib/team'
+require './lib/game_team'
 require 'pry'
