e23a78b99738b5a5286c34fc6170690593277ad5
Karl Fallenius <smilodon.p@gmail.com>
2024-06-07 21:24:57 -0600

    moved league stats into stat tracker, fixed formatting issues, and worked on some methods

--- spec/league_statistics_spec.rb
+++ spec/league_statistics_spec.rb
@@ -1,48 +1,9 @@
-require 'spec_helper.rb'
+require './spec/spec_helper'
 
 RSpec.configure do |config|
   config.formatter = :documentation
 end
 
-describe LeagueStatistics do
-  
-  describe "class methods" do
-    it "can count the total number of teams" do
-      path = "./data/teams.csv"
-      teams = Teams.create_teams_data_objects(path)
+RSpec.describe StatTracker do
 
-      expect(teams.count).to be 32
-      expect(teams).to be_all Teams
-    end
-
-    xit "can count the total number of goals a team has scored" do
-      path = "./data/test_game_teams.csv"
-      game_teams = GameTeams.create_game_teams_data_objects(path)
-
-      
-    end
-
-    xit "can determine a team's average number of goals per game" do
-      path = "./data/test_game_teams.csv"
-      game_teams = GameTeams.create_game_teams_data_objects(path)
-      team = Teams.new(1, 23, "Atlanta United", "ATL", "Mercedes-Benz Stadium", "/api/v1/teams/1")
-      #Insert test logic
-      expect(game_teams.team.average_goals).to eq()
-    end
-    
-    xit "can determine which team has had the best offense" do
-      path = "./data/test_game_teams.csv"
-      game_teams = GameTeams.create_game_teams_data_objects(path)
-      #Insert test logic
-      
-    end
-    
-    xit "can determine which team has had the worst offense" do
-      path = "./data/test_game_teams.csv"
-      game_teams = GameTeams.create_game_teams_data_objects(path)
-      #Insert test logic
-      
-    end
-
-  end
-end
+ 
--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,4 +1,4 @@
-require './spec/spec_helper'
+require 'spec_helper'
 
 RSpec.configure do |config|
   config.formatter = :documentation
@@ -29,10 +29,84 @@ RSpec.describe StatTracker do
       expect(stat_tracker.games.first.game_id).to eq(2012030221)
       expect(stat_tracker.teams.first.team_id).to eq(1)
       expect(stat_tracker.game_teams.first.game_id).to eq(2012030221)
+    end
   end
+
+
+  describe '#initialize' do
+    it "exists" do 
+      stat_tracker = StatTracker.new([], [], [])
+      expect(stat_tracker).to be_a StatTracker
+    end
+  end
+
+  #League Stats Spec
+
+  # before(:each) do
+  #   teams_data = Teams.create_teams_data_objects("./data/teams.csv")
+  #   game_teams_data = GameTeams.create_game_teams_data_objects("./data/test_game_teams.csv")
+  #   stat_tracker = StatTracker.new(game_teams, game_data, teams)
+  # end
+
+  describe 'count_of_teams' do
+    it "counts all of the teams in the league" do
+      teams = Teams.create_teams_data_objects("./data/teams.csv")
+
+      expect(teams.count).to be 32
+      expect(teams).to be_all Teams
+    end
+  end
+
+  describe "total goals ever" do
+    it "calculates the total number of goals a team has scored across all seasons" do
+      team = Teams.new(6,6,"FC Dallas","DAL","Toyota Stadium","/api/v1/teams/6")
+      teams = Teams.create_teams_data_objects("./data/teams.csv")
+      expect(team.total_goals_ever).to be 24
+    end
+  end
+
+  describe "average goals" do
+    xit "calculates a team's average number of goals per game across all seasons" do
+      expect(team.average_goals).to be 2.67
+    end
+  end
+
+  describe 'best_offense' do
+    xit "determines the name of the team with the highest average number of goals per game" do
+      
+      expect(game_team_stats.best_offense).to be_a(string)
+    end
+  end
+
+  describe 'worst_offense' do
+    xit "determines the name of the team with the lowest average number of goals per game" do
+      
+    end
+  end
+
+#   describe 'highest_scoring_visitor' do
+#     xit "determines the name of the team with the most average goals per away game" do
+    
+#     end   
+#   end
+
+
+#   describe 'highest_scoring_home_team' do
+#     xit "determines the name of the team with the most average goals per home game" do
+    
+#     end   
+#   end
+
+#   describe 'lowest_scoring_visitor' do
+#     xit "determines the name of the team with the least average goals per away game" do
+    
+#     end
+#   end
+
+#   describe 'lowest_scoring_home_team' do
+#     xit "determines the name of the team with the least average goals per home game" do
+      
+#     end
+#   end 
+
 end
-
-
-
-end
-
