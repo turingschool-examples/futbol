887dfa952b6a86e0a3480ab67a5f499ffc5b2400
Cory Bretsch <corybretsch@gmail.com>
2024-06-08 08:03:24 -0500

    made many minor adjustments to allow spec_harness to work.

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe StatTracker do
         game_teams: game_teams_path
         }
       
-      stat_tracker = StatTracker.new(locations)
+      stat_tracker = StatTracker.from_csv(locations)
       
       expect(stat_tracker).to be_a StatTracker
       expect(stat_tracker.games).to be_all Games
@@ -26,13 +26,100 @@ RSpec.describe StatTracker do
       expect(stat_tracker.games.first).to be_a Games
       expect(stat_tracker.teams.first).to be_a Teams
       expect(stat_tracker.game_teams.first).to be_a GameTeams
-      expect(stat_tracker.games.first.game_id).to eq(2012030221)
+      expect(stat_tracker.games.first.game_id).to eq("2012030221")
       expect(stat_tracker.teams.first.team_id).to eq(1)
-      expect(stat_tracker.game_teams.first.game_id).to eq(2012030221)
+      expect(stat_tracker.game_teams.first.game_id).to eq("2012030221")
+    end
   end
-end
 
+  ######### GAME STATS ##############
 
+  describe '#highest_total_score' do
+    it "returns the highest sum of the winning and losing teams scores" do
+      game_data = Games.create_games_data_objects("./data/games_test.csv")
+      game_teams = GameTeams.create_game_teams_data_objects("./data/test_game_teams.csv")
+      teams = Teams.create_teams_data_objects("./data/teams.csv")
+      stat_tracker = StatTracker.new(game_teams, game_data, teams)           
+      expect(stat_tracker.highest_total_score).to eq 5 # Calculated manually
+    end
+  end
 
+  describe '#lowest_total_score' do
+    it "returns the lowest sum of the winning and losing teams scores" do
+      game_data = Games.create_games_data_objects("./data/games_test.csv")
+      game_teams = GameTeams.create_game_teams_data_objects("./data/test_game_teams.csv")
+      teams = Teams.create_teams_data_objects("./data/teams.csv")
+      stat_tracker = StatTracker.new(game_teams, game_data, teams) 
+      expect(stat_tracker.lowest_total_score).to eq(1) # Calculated manually
+    end
+  end
+
+  describe '#percentage_home_wins' do
+    it "returns the percentage of home team wins" do
+      game_data = Games.create_games_data_objects("./data/games_test.csv")
+      game_teams = GameTeams.create_game_teams_data_objects("./data/test_game_teams.csv")
+      teams = Teams.create_teams_data_objects("./data/teams.csv")
+      stat_tracker = StatTracker.new(game_teams, game_data, teams) 
+      total_games = stat_tracker.games.length
+      home_wins = stat_tracker.games.count { |game| game.home_goals > game.away_goals }
+      expect(stat_tracker.percentage_home_wins).to eq(0.6)
+    end
+  end
+
+  describe '#percentage_visitor_wins' do
+    it "returns the percentage of away team wins" do
+      game_data = Games.create_games_data_objects("./data/games_test.csv")
+      game_teams = GameTeams.create_game_teams_data_objects("./data/test_game_teams.csv")
+      teams = Teams.create_teams_data_objects("./data/teams.csv")
+      stat_tracker = StatTracker.new(game_teams, game_data, teams) 
+      total_games = stat_tracker.games.length
+      away_wins = stat_tracker.games.count { |game| game.away_goals > game.home_goals }
+      expect(stat_tracker.percentage_visitor_wins).to eq(0.4)
+    end   
+  end
+
+  describe '#percentage_ties' do
+    it "returns the percentage of tie games" do
+      game_data = Games.create_games_data_objects("./data/games_test.csv")
+      game_teams = GameTeams.create_game_teams_data_objects("./data/test_game_teams.csv")
+      teams = Teams.create_teams_data_objects("./data/teams.csv")
+      stat_tracker = StatTracker.new(game_teams, game_data, teams) 
+      total_games = stat_tracker.games.length
+      tie_games = stat_tracker.games.count { |game| game.home_goals == game.away_goals }
+      expect(stat_tracker.percentage_ties).to eq(0.00)
+    end   
+  end
+
+  describe '#count_of_games_by_season' do
+    it "returns a hash with season id's as keys and a count of games as values" do
+      game_data = Games.create_games_data_objects("./data/games_test.csv")
+      game_teams = GameTeams.create_game_teams_data_objects("./data/test_game_teams.csv")
+      teams = Teams.create_teams_data_objects("./data/teams.csv")
+      stat_tracker = StatTracker.new(game_teams, game_data, teams) 
+      expect(stat_tracker.count_of_games_by_season).to eq({ "20122013" => 10 })
+    end
+  end
+
+  describe '#average_goals_per_game' do
+    it "returns average number of goals scored per game from all seasons" do
+      game_data = Games.create_games_data_objects("./data/games_test.csv")
+      game_teams = GameTeams.create_game_teams_data_objects("./data/test_game_teams.csv")
+      teams = Teams.create_teams_data_objects("./data/teams.csv")
+      stat_tracker = StatTracker.new(game_teams, game_data, teams) 
+      total_goals = game_data.sum { |game| game.home_goals + game.away_goals }
+      total_games = game_data.length
+      expect(stat_tracker.average_goals_per_game).to eq(3.70)
+    end
+  end 
+
+  describe '#average_goals_by_season' do
+    it "returns a hash with season id's as keys and a float of average number of goals per game as value" do
+      game_data = Games.create_games_data_objects("./data/games_test.csv")
+      game_teams = GameTeams.create_game_teams_data_objects("./data/test_game_teams.csv")
+      teams = Teams.create_teams_data_objects("./data/teams.csv")
+      stat_tracker = StatTracker.new(game_teams, game_data, teams) 
+      expect(stat_tracker.average_goals_by_season).to eq({ "20122013" => 3.70 })
+    end
+  end
 end
 
