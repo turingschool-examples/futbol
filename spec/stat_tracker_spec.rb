93fafff8ca0e222e6919b96b5fef603b48e682ed
Karl Fallenius <smilodon.p@gmail.com>
2024-06-09 11:32:32 -0600

    added some league stats tests + skeletons

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -121,5 +121,61 @@ RSpec.describe StatTracker do
       expect(stat_tracker.average_goals_by_season).to eq({ "20122013" => 3.70 })
     end
   end
+
+######League Statistics######
+
+  describe 'count_of_teams' do
+    it "counts all of the teams in the league" do
+      teams = Teams.create_teams_data_objects("./data/teams.csv")
+      expect(teams.count).to be 32
+      expect(teams).to be_all Teams
+    end
+  end
+  describe "total goals ever" do
+    it "calculates the total number of goals a team has scored across all seasons" do
+      team = Teams.new(6,6,"FC Dallas","DAL","Toyota Stadium","/api/v1/teams/6")
+      game_teams = GameTeams.create_game_teams_data_objects("./data/test_game_teams.csv")
+      teams = Teams.create_teams_data_objects("./data/teams.csv")
+
+      expect(team.total_goals_ever).to be 24
+    end
+  end
+  describe "average goals" do
+    xit "calculates a team's average number of goals per game across all seasons" do
+      expect(team.average_goals).to be 2.67
+    end
+  end
+  describe 'best_offense' do
+    xit "determines the name of the team with the highest average number of goals per game" do
+      
+      expect(game_team_stats.best_offense).to be_a(string)
+    end
+  end
+  describe 'worst_offense' do
+    xit "determines the name of the team with the lowest average number of goals per game" do
+      
+    end
+  end
+#   describe 'highest_scoring_visitor' do
+#     xit "determines the name of the team with the most average goals per away game" do
+    
+#     end   
+#   end
+#   describe 'highest_scoring_home_team' do
+#     xit "determines the name of the team with the most average goals per home game" do
+    
+#     end   
+#   end
+#   describe 'lowest_scoring_visitor' do
+#     xit "determines the name of the team with the least average goals per away game" do
+    
+#     end
+#   end
+#   describe 'lowest_scoring_home_team' do
+#     xit "determines the name of the team with the least average goals per home game" do
+      
+#     end
+#   end 
+end
 end
 
