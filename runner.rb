c0ee2d81c35e3a33e3f4540734bd5bcbedbb5d42
Cory Bretsch <corybretsch@gmail.com>
2024-06-08 20:36:45 -0500

    adding high/low scoring away team with tests. passes with spec_harness. Also added before(:each) block in stat_tracker_spec to clean things up

--- runner.rb
+++ runner.rb
@@ -1,3 +1,5 @@
+require './lib/stat_tracker.rb'
+
 game_path = './data/games.csv'
 team_path = './data/teams.csv'
 game_teams_path = './data/game_teams.csv'
@@ -10,4 +12,4 @@ locations = {
 
 stat_tracker = StatTracker.from_csv(locations)
 
-# require 'pry'; binding.pry
+require 'pry'; binding.pry
