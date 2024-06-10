663825163cf23e6ead0f9c6b89fad78146d03c46
Evan Davis <evandavis427@gmail.com>
2024-06-10 10:12:48 -0600

    Implemented simplecov and example functionality in the runner.rb file

--- runner.rb
+++ runner.rb
@@ -12,4 +12,11 @@ locations = {
 
 stat_tracker = StatTracker.from_csv(locations)
 
+# Display Highest Total Score
+puts "Highest Total Score: #{stat_tracker.highest_total_score}"
 
+# Display Lowest Total Score
+puts "Lowest Total Score: #{stat_tracker.lowest_total_score}"
+
+# Display Best Offense Team
+puts "Best Offense Team: #{stat_tracker.best_offense}"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
+require 'simplecov'
 require 'pry'
 require './lib/game_team'
 require './lib/game'
 require './lib/stat_tracker'
 require './lib/team'
+SimpleCov.start
