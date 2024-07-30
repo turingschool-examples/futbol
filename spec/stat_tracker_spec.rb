82cedeaa4b8c7f37b0f94822b9afb8fc23e10908
Shane Galvin <Sgalvin36@gmail.com>
2024-07-30 13:19:34 -0600

    Add initial test and functionality

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -1,5 +1,12 @@
-require 'spec_helper'
+require_relative './spec_helper'
 
 RSpec.configure do |config| 
  config.formatter = :documentation 
+end
+
+RSpec.describe StatTracker do
+    it 'exists' do
+        stat_tracker = StatTracker.new
+        expect(stat_tracker).to be_a StatTracker
+    end
 end
