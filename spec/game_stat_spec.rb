164b99566f946e67701b1fe59a6ebf10f5a104ab
Candice Cirbo <thecirbos@icloud.com>
2024-07-30 17:29:10 -0600

    Feat/started set up to dummy files for methods and tests

--- spec/game_stat_spec.rb
+++ spec/game_stat_spec.rb
@@ -1,11 +1,24 @@
 require 'spec_helper'
 
 RSpec.configure do |config| 
- config.formatter = :documentation 
+  config.formatter = :documentation 
 end
 
-RSpec.describe *Class* do 
- before(:each) do 
- end
+
+
+RSpec.describe Game do 
+  before(:each) do 
+    game_path = './data/dummy_games.csv'
+    team_path = './data/dummy_teams.csv'
+    game_teams_path = './data/dummy_game_teams.csv'
+
+    locations = {
+      games: game_path,
+      teams: team_path,
+      game_teams: game_teams_path
+    }
+
+    
+  end
 end
 
