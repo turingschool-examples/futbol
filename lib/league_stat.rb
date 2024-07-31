cc15d85cdfcb24209b6f8aad3cfa2b5a421cdaad
Cameron Pittman <166861602+CPPittman@users.noreply.github.com>
2024-07-31 15:08:51 -0600

    added method structure

--- lib/league_stat.rb
+++ lib/league_stat.rb
@@ -1,3 +1,30 @@
 module LeagueStatistics
-    
+    def count_of_teams
+        @teams.count.to_i
+    end
+
+    def best_offense
+        @goals.max.to_s
+    end
+
+    def worst_offense
+
+    end
+
+    def highest_scoring_visitor
+
+    end
+
+    def highest_scoring_home_team
+
+    end
+
+    def lowest_scoring_visitor
+
+    end
+
+    def lowest_scoring_home_team
+
+    end
+
 end
