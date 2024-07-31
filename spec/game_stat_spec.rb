cc09a000d4a5398bd4197b455673f822e9e9a982
Jim Macur <jim.macur@gmail.com>
2024-07-31 13:28:11 -0600

    initial commit for game stat and spec

--- spec/game_stat_spec.rb
+++ spec/game_stat_spec.rb
@@ -4,21 +4,27 @@ RSpec.configure do |config|
   config.formatter = :documentation 
 end
 
+RSpec.describe GameStat do
+    it 'exists' do
+        expect(GameStat.new).to be_a GameStat
+    end
+end
 
 
-RSpec.describe Game do 
-  before(:each) do 
-    game_path = './data/dummy_games.csv'
-    team_path = './data/dummy_teams.csv'
-    game_teams_path = './data/dummy_game_teams.csv'
 
-    locations = {
-      games: game_path,
-      teams: team_path,
-      game_teams: game_teams_path
-    }
+# RSpec.describe Game do 
+#   before(:each) do 
+#     game_path = './data/dummy_games.csv'
+#     team_path = './data/dummy_teams.csv'
+#     game_teams_path = './data/dummy_game_teams.csv'
+
+#     locations = {
+#       games: game_path,
+#       teams: team_path,
+#       game_teams: game_teams_path
+#     }
 
     
-  end
-end
+#   end
+# end
 
