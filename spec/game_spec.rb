1a0b1e00822c015bb5eca687eb6cc86aba416179
Stefan Bloom <stefanjbloom88@gmail.com>
2024-07-30 19:22:35 -0600

    Test: final testing for data type conversions are green

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe Game do
   before(:each) do
     game_path = './data/games.csv'
 
+    # This game data is simply an example/mock? pulled from line 2 of games.csv
     @game_data = {
       :game_id => "2012030221",
       :season => "20122013",
@@ -22,13 +23,31 @@ RSpec.describe Game do
     }
 
     @game = Game.new(@game_data)
-
   end
+
     it 'exists' do
         expect(@game).to be_a Game
     end
-# create variables of the games stuff below
-# 2012030221,20122013,Postseason,5/16/13,3,6,2,3,Toyota Stadium,/api/v1/venues/null
-# 2012030222,20122013,Postseason,5/19/13,3,6,2,3,Toyota Stadium,/api/v1/venues/null
 
+    it 'contains game data' do
+      expect(@game.game_id).to eq("2012030221")
+      expect(@game.season).to eq("20122013")
+      expect(@game.type).to eq("Postseason")
+      expect(@game.date_time).to eq("5/16/13")
+      expect(@game.away_team_id).to eq("3")
+      expect(@game.home_team_id).to eq("6")
+      expect(@game.away_goals).to eq(2)
+      expect(@game.home_goals).to eq(3)
+      expect(@game.venue).to eq("Toyota Stadium")
+      expect(@game.venue_link).to eq("/api/v1/venues/null")
+    end
+
+    it 'coverts home goals and aways goals from strings to ints' do
+      expect(@game.away_goals).to be_a Integer
+      expect(@game.away_goals).to eq(2)
+
+      expect(@game.home_goals).to be_a Integer
+      expect(@game.home_goals).to eq (3) 
+      # This was tested by changing the before(:each) lines 17/18 from int back to string. Then back again.  
+    end
 end
