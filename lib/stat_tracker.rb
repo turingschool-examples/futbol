708a94eb7fdb2a62859f24d47087f4fa99d3a97b
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-08-01 18:35:02 -0400

    Created method for average goals per game

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -56,10 +56,24 @@ class StatTracker
   end
 
   def average_goals_per_game
+    total_goals = 0
+    total_games = 0
+    @games.each do |game|
+      total_goals += game.away_goals + game.home_goals
+      total_games += 1
+    end
 
+    if total_games > 0
+      average_goals = total_goals.to_f / total_games
+    else
+      average_goals = 0.0
+    end
+    average_goals_rounded = average_goals.round(2)
+    average_goals_rounded
   end
+  
 
    def average_goals_by_season
-    
+
    end
 end
