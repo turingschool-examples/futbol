73628f65216af24c114e5f50fc40fc34d9b10bcf
Kyle Delaney <khdelaney99@gmail.com>
2024-08-01 20:03:34 -0400

    feat: complete test and imp for highest_scoring_visitor

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -179,11 +179,7 @@ RSpec.describe StatTracker do
     
     describe 'highest_scoring_visitor' do
         it 'returns name of the team with the highest average score per game across all seasons when they are away.' do
-            hash_of_games = @stat_tracker.instance_variable_get(:@game_stats_data)
-
-            hash_of_games[2012030221].instance_variable_set(:@away_goals, 50000)
-
-            expect(@stat_tracker.highest_scoring_visitor).to eq("Houston Dynamo")
+            expect(@stat_tracker.highest_scoring_visitor).to eq("FC Dallas")
         end
     end
     
