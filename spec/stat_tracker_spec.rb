ad066848e15a00b5dad8e5ff070c84d91e8f938b
Cameron Pittman <166861602+CPPittman@users.noreply.github.com>
2024-08-02 14:11:25 -0600

    removed some pseudocode

--- spec/stat_tracker_spec.rb
+++ spec/stat_tracker_spec.rb
@@ -509,27 +509,3 @@ RSpec.describe StatTracker do
 
     end
 end
-## games per season
-# team_tackles
-# teams.max_by
-# get team_name
-
-
-
-# games per season
-# modify team_shot_goal so that it is just team_tackles
-# modify team_id_hash to be an integer instead of an array
-# modify update_shot goal - to be modify tackles - find key and incriment value by however many tackles
-# 
-# modify most/least accurate
-# teams max_by
-# teams min_by
-# no need for ratio method
-
-
-# describe '#fewest_tackles' do
-# games per season
-# team_tackles
-# teams.min_by
-# get team_name
-# end
