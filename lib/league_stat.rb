7792b0b4a8f5eb69537cf80f542516b6fbbcfeca
Candice Cirbo <thecirbos@icloud.com>
2024-08-02 18:01:16 -0600

    Feat/adds method highest scoring visitor

--- lib/league_stat.rb
+++ lib/league_stat.rb
@@ -13,22 +13,8 @@ module LeagueStatistics
     end
 
     def highest_scoring_visitor
-        visitor_totals = Hash.new { |hash, key| hash[key] = { goals: 0, games: 0 } }
-        @game_teams.each do |game|
-          if game.hoa == "away" 
-            visitor_totals[game.team_id][:goals] += game.goals
-            visitor_totals[game.team_id][:games] += 1
-          end
-        end
-        visitor_goals = {}
-        visitor_totals.each do |team_id, data|
-        visitor_goals[team_id] = data[:goals].to_f / data[:games]
-        end
-        highest_scoring_team = visitor_goals.max_by { |team_id, avg_goals| avg_goals }
-        highest_scoring_team_id = highest_scoring_team[0]
-        visitor_team_name = ''
         @teams.each do |team|
-            visitor_team_name = team.team_name if team.team_id == highest_scoring_team_id
+            return team.team_name if team.team_id == team_goals_and_games("away", "highest")
         end
         visitor_team_name
     end
