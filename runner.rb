7e0d88d9b7172918ded0940f184f355a58616d9c
Candice Cirbo <thecirbos@icloud.com>
2024-08-03 17:54:22 -0600

    Feat/runner file updated with statics to demo

--- runner.rb
+++ runner.rb
@@ -12,4 +12,31 @@ locations = {
 
 stat_tracker = StatTracker.from_csv(locations)
 
-require 'pry'; binding.pry
+
+puts "\n\n** Game Statistics **\n"
+puts "\nPercentage of games that a home team has won:"
+puts stat_tracker.percentage_home_wins
+# puts "\n"
+
+puts "\nPercentage of games that a visitor has won:"
+puts stat_tracker.percentage_visitor_wins
+puts "\n"
+
+puts "\n** League Statistics **\n"
+puts "\nTeam with the best offense:"
+puts stat_tracker.best_offense
+# puts "\n"
+
+puts "\nTeam with the worst offense:"
+puts stat_tracker.worst_offense
+puts "\n"
+
+puts "\n** Season Statistics **\n"
+puts "\nCoach with the best win percentage for the 2012-2013 season:"
+puts stat_tracker.winningest_coach("20122013")
+# puts "\n"
+
+puts "\nCoach with the worst win percentage for the 2012-2013 season:"
+puts stat_tracker.worst_coach("20122013")
+puts "\n\n"
+
