f8f081a02f135aa002309b636cb82878d14e1b51
Candice Cirbo <thecirbos@icloud.com>
2024-08-02 18:02:32 -0600

    Feat/adds lowest scoring home team

--- lib/league_stat.rb
+++ lib/league_stat.rb
@@ -16,28 +16,12 @@ module LeagueStatistics
         @teams.each do |team|
             return team.team_name if team.team_id == team_goals_and_games("away", "highest")
         end
-        visitor_team_name
     end
 
     def highest_scoring_home_team
-    home_totals = Hash.new { |hash, key| hash[key] = { goals: 0, games: 0 } }
-    @game_teams.each do |game|
-        if game.hoa == "home" 
-        home_totals[game.team_id][:goals] += game.goals
-        home_totals[game.team_id][:games] += 1
+        @teams.each do |team|
+            return team.team_name if team.team_id == team_goals_and_games("home", "highest")
         end
-    end
-    home_goals = {}
-    home_totals.each do |team_id, data|
-    home_goals[team_id] = data[:goals].to_f / data[:games]
-    end
-    highest_scoring_team = home_goals.max_by { |team_id, avg_goals| avg_goals }
-    highest_scoring_team_id = highest_scoring_team[0]
-    home_team_name = ''
-    @teams.each do |team|
-        home_team_name = team.team_name if team.team_id == highest_scoring_team_id
-    end
-    home_team_name
     end
 
     def lowest_scoring_visitor
