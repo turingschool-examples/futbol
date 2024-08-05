564502996f7f4ee9c494b936df2d948a5028da62
Jim Macur <jim.macur@gmail.com>
2024-08-04 19:56:29 -0600

    final touches on code cleanup

--- runner.rb
+++ runner.rb
@@ -12,20 +12,17 @@ locations = {
 
 stat_tracker = StatTracker.from_csv(locations)
 
-
 puts "\n\n** Game Statistics **\n"
 puts "\nPercentage of games that a home team has won:"
-puts stat_tracker.percentage_home_wins
-# puts "\n"
+puts "#{(stat_tracker.percentage_home_wins * 100)}%"
 
 puts "\nPercentage of games that a visitor has won:"
-puts stat_tracker.percentage_visitor_wins
+puts "#{(stat_tracker.percentage_visitor_wins * 100)}%"
 puts "\n"
 
 puts "\n** League Statistics **\n"
 puts "\nTeam with the best offense:"
 puts stat_tracker.best_offense
-# puts "\n"
 
 puts "\nTeam with the worst offense:"
 puts stat_tracker.worst_offense
@@ -34,13 +31,15 @@ puts "\n"
 puts "\n** Season Statistics **\n"
 puts "\nCoach with the best win percentage for the 2012-2013 season:"
 puts stat_tracker.winningest_coach("20122013")
-# puts "\n"
 
 puts "\nCoach with the worst win percentage for the 2012-2013 season:"
 puts stat_tracker.worst_coach("20122013")
 puts "\n\n"
 
 puts "\n** Team Statistics **\n"
-puts "\nTeam Infor for Chicago Fire:"
-puts stat_tracker.team_info("Chicago Fire")
-
+puts "\nTeam Info for Chicago Fire:"
+team_info = stat_tracker.team_info("4")
+team_info.each do |key, value|
+    puts "#{key}: #{value}"
+end
+puts "\n\n"
