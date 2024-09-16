4f08be824c61df1806048a93db4801360523a1de
Danielle Cardona <danielle.cardona23@gmail.com>
2024-09-16 17:12:12 -0600

    refactor: modify stat_tracker class to read games csv file

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,17 +1,39 @@
 require 'CSV'
 
 class StatTracker
-  attr_reader :stats
+    attr_reader
 
-  def initialize
-    @stats = []
-  end
-  
-  def from_csv(file_path)
-    CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
-      stat_tracker = StatTracker.new
-      @stats << stat_tracker
+    def initialize(game_id, season, type, date_time, away_team, home_team_id, away_goals, home_goals, venue, venue_link)
+        @game_id = game_id
+        @season = season
+        @type = type
+        @date_time = date_time
+        @away_team = away_team
+        @home_team_id = home_team_id
+        @away_goals = away_goals
+        @home_goals = home_goals
+        @venue = venue
+        @venue_link = venue_link
     end
-    @stats
-  end
+
+    def self.from_csv(locations)
+    stats = []
+
+    CSV.foreach(locations[:games], headers: true, header_converters: :symbol) do |row|
+        stat_tracker = StatTracker.new(
+            row[:game_id].to_i,
+            row[:season],
+            row[:type],
+            row[:date_time],
+            row[:away_team],
+            row[:home_team_id],
+            row[:away_goals].to_i,
+            row[:home_goals].to_i,
+            row[:venue],
+            row[:venue_link]
+        )
+        stats << stat_tracker
+    end
+    stats
+    end
 end
