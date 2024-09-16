391a889ac889331021778a5db868af7cdf5f2c69
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-09-16 18:33:48 -0500

    test: modified tests to use instance variables and to test for each variable properly

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper.rb'
 
-Rspec.describe Game do
+RSpec.describe Game do
   before(:each) do
-    game1 = Game.new(2012030221, 20122013, 'postseason', '5/16/13', 3, 6, 2, 3, 'Toyota Stadium', '/api/v1/venues/null')
-    game2 = Game.new(2012030222, 20122013, 'Postseason', '5/19/13', 3, 6, 2, 3, 'Toyota Stadium', '/api/v1/venues/null)')
+    @game1 = Game.new(2012030221, 20122013, 'Postseason', '5/16/13', 3, 6, 2, 3, 'Toyota Stadium', '/api/v1/venues/null')
+    @game2 = Game.new(2012030222, 20122013, 'Postseason', '5/19/13', 3, 6, 2, 3, 'Toyota Stadium', '/api/v1/venues/null')
   end
   
 
@@ -14,54 +14,54 @@ Rspec.describe Game do
     end
 
     it 'has game_id' do
-      expect(@game1).to eq(2012030221)
-      expect(@game2).to eq(2012030222)
+      expect(@game1.game_id).to eq(2012030221)
+      expect(@game2.game_id).to eq(2012030222)
 
     end
 
     it 'has season' do
-      expect(@game1).to eq(20122013)
-      expect(@game2).to eq(20122013)
+      expect(@game1.season).to eq(20122013)
+      expect(@game2.season).to eq(20122013)
     end
 
     it 'has type' do
-      expect(@game1).to eq('Postseason')
-      expect(@game2).to eq('Postseason')
+      expect(@game1.type).to eq('Postseason')
+      expect(@game2.type).to eq('Postseason')
     end
 
     it 'has date_time' do
-      expect(@game1).to eq('5/16/13')
-      expect(@game2).to eq('5/19/13')
+      expect(@game1.date_time).to eq('5/16/13')
+      expect(@game2.date_time).to eq('5/19/13')
     end
 
     it 'has away_team_id' do
-      expect(@game1).to eq(3)
-      expect(@game2).to eq(3)
+      expect(@game1.away_team_id).to eq(3)
+      expect(@game2.away_team_id).to eq(3)
     end
 
     it 'has home_team_id' do
-      expect(@game1).to eq(6)
-      expect(@game2).to eq(6)
+      expect(@game1.home_team_id).to eq(6)
+      expect(@game2.home_team_id).to eq(6)
     end
 
     it 'has away_goals' do
-      expect(@game1).to eq(2)
-      expect(@game2).to eq(2)
+      expect(@game1.away_goals).to eq(2)
+      expect(@game2.away_goals).to eq(2)
     end
 
     it 'has home_goals' do
-      expect(@game1).to eq(3)
-      expect(@game2).to eq(3)
+      expect(@game1.home_goals).to eq(3)
+      expect(@game2.home_goals).to eq(3)
     end
 
     it 'has a venue' do
-      expect(@game1).to eq('Toyota Stadium')
-      expect(@game2).to eq('Toyota Stadium')
+      expect(@game1.venue).to eq('Toyota Stadium')
+      expect(@game2.venue).to eq('Toyota Stadium')
     end
 
     it 'has venue_link' do
-      expect(@game1).to eq('/api/v1/venues/null')
-      expect(@game2).to eq('/api/v1/venues/null')
+      expect(@game1.venue_link).to eq('/api/v1/venues/null')
+      expect(@game2.venue_link).to eq('/api/v1/venues/null')
     end
   end
 end
