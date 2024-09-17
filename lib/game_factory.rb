412c725114e375e8a4ba82bf7e1660033c31723e
Eric DeShetler <ejdesh@gmail.com>
2024-09-17 13:31:35 -0700

    fix: removed unnecessary change from other files

--- lib/game_factory.rb
+++ lib/game_factory.rb
@@ -1,14 +1,15 @@
-require './lib/game'
 class GameFactory
   attr_reader :games
 
   def initialize
     @games = []
   end
-
   def create_games(source)
+    
     CSV.foreach(source, headers: true, header_converters: :symbol) do |row|
       @games << Game.new(row)
     end
+
   end
+
 end
--- lib/teams_factory.rb
+++ lib/teams_factory.rb
@@ -3,17 +3,18 @@ require_relative 'team'
 
 class Teams_factory
   attr_reader :teams
-
   def initialize
     @teams = []
   end
 
   def create_teams(file_path)
+
     custom_header_converter = lambda do |header|
       header.to_sym
     end
 
-    CSV.foreach(file_path, headers: true, header_converters: custom_header_converter) do |row|
+    CSV.foreach(file_path, headers:true, header_converters: custom_header_converter) do |row|
+
       team = Team.new(row)
       @teams << team
     end
