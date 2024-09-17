6feac95353502eff1b93faa81799e839face64af
Michael OBrien <obrien.michael.t@gmail.com>
2024-09-17 18:59:05 -0400

    Updated to use dummy data from csv

--- spec/game_statistics_spec.rb
+++ spec/game_statistics_spec.rb
@@ -2,33 +2,17 @@ require './spec/spec_helper'
 
 RSpec.describe GameStatistics do
     before(:each) do
-        @fake_game_data = [
-      {
-      game_id: '2012030221', team_id: 3, HoA: 'away', result: 'LOSS', settled_in: 'OT', head_coach: 'John Tortorella', goals: 2, shots: 8, tackles: 44, pim: 8, powerPlayOpportunities: 3, powerPlayGoals: 0, faceOffWinPercentage: 44.8, giveaways: 17, takeaways: 7,
-      game_id: '2012030221', team_id: 6, HoA: 'home', result: 'WIN', settled_in: 'OT', head_coach: 'Claude Julien', goals: 3, shots: 12, tackles: 51, pim: 6, powerPlayOpportunities: 4, powerPlayGoals: 1, faceOffWinPercentage: 55.2, giveaways: 4, takeaways: 5,
-      game_id: '2012030222', team_id: 3, HoA: 'away', result: 'LOSS', settled_in: 'REG', head_coach: 'John Tortorella', goals: 2, shots: 9, tackles: 33, pim: 11, powerPlayOpportunities: 5, powerPlayGoals: 0, faceOffWinPercentage: 51.7, giveaways: 1, takeaways: 4,
-      game_id: '2012030222', team_id: 6, HoA: 'home', result: 'WIN', settled_in: 'REG', head_coach: 'Claude Julien', goals: 3, shots: 8, tackles: 36, pim: 19, powerPlayOpportunities: 1, powerPlayGoals: 0, faceOffWinPercentage: 48.3, giveaways: 16, takeaways: 6,
-      game_id: '2012030223', team_id: 6, HoA: 'away', result: 'WIN', settled_in: 'REG', head_coach: 'Claude Julien', goals: 2, shots: 8, tackles: 28, pim: 6, powerPlayOpportunities: 0, powerPlayGoals: 0, faceOffWinPercentage: 61.8, giveaways: 10, takeaways: 7,
-      game_id: '2012030223', team_id: 3, HoA: 'home', result: 'LOSS', settled_in: 'REG', head_coach: 'John Tortorella', goals: 1, shots: 6, tackles: 37, pim: 2, powerPlayOpportunities: 2, powerPlayGoals: 0, faceOffWinPercentage: 38.2, giveaways: 7, takeaways: 9,
-      game_id: '2012030224', team_id: 6, HoA: 'away', result: 'WIN', settled_in: 'OT', head_coach: 'Claude Julien', goals: 3, shots: 10, tackles: 24, pim: 8, powerPlayOpportunities: 4, powerPlayGoals: 2, faceOffWinPercentage: 53.7, giveaways: 8, takeaways: 6,
-      game_id: '2012030224', team_id: 3, HoA: 'home', result: 'LOSS', settled_in: 'OT', head_coach: 'John Tortorella', goals: 2, shots: 8, tackles: 40, pim: 8 powerPlayOpportunities: 4, powerPlayGoals: 1, faceOffWinPercentage: 46.3, giveaways: 9, takeaways: 7
-      }
-    ]
-
-    @fake_season_data = [
-        {
-          game_id: '2012030325', season: 20122013, type: 'Postseason', date_time: 6/9/13, away_team_id: 26, home_team_id: 16, away_goals: 3, home_goals: 2, venue: 'Gillette Stadium', venue_link: '/api/v1/venues/null',
-          game_id: '2016030172', season: 20162017, type: 'Postseason', date_time: 4/16/17, away_team_id: 20, home_team_id: 24, away_goals: 2, home_goals: 3, venue: 'Rio Tinto Stadium', venue_link: '/api/v1/venues/null',
-          game_id: '2016030173', season: 20162017, type: 'Postseason', date_time: 4/18/17, away_team_id: 24, home_team_id: 20, away_goals: 3, home_goals: 2, venue: 'BMO Field', venue_link: '/api/v1/venues/null',
-          game_id: '2016030174', season: 20162017, type: 'Postseason', date_time: 4/20/17, away_team_id: 24, home_team_id: 20, away_goals: 3, home_goals: 1, venue: 'BMO Field', venue_link: '/api/v1/venues/null',
-          game_id: '2014030411', season: 20142015, type: 'Postseason', date_time: 6/4/15, away_team_id: 16, home_team_id: 14, away_goals: 2, home_goals: 1, venue: 'Audi Field', venue_link: '/api/v1/venues/null',
-          game_id: '2014030412', season: 20142015, type: 'Postseason', date_time: 6/6/15, away_team_id: 16, home_team_id: 14, away_goals: 3, home_goals: 2, venue: 'Audi Field', venue_link: '/api/v1/venues/null',
-          game_id: '2016030171', season: 20162017, type: 'Postseason', date_time: 4/14/17, away_team_id: 20, home_team_id: 24, away_goals: 2, home_goals: 3, venue: 'Rio Tinto Stadium', venue_link: '/api/v1/venue/null',
-          game_id: '2014030413', season: 20142015, type: 'Postseason', date_time: 6/9/15, away_team_id: 14, home_team_id: 16, away_goals: 3, home_goals: 2, venue: 'Gillette Stadium', venue_link: '/api/v1/venues/null'
+        game_path = './data/games_dummy.csv'
+        team_path = './data/teams_dummy.csv'
+        game_teams_path = './data/game_teams_dummy.csv'
+    
+        locations = {
+          games: game_path,
+          teams: team_path,
+          game_teams: game_teams_path
         }
-      ]
 
-    @game_stats = SeasonStatistics.new(@fake_game_data, @fake_team_data, @fake_season_data)
+    @game_stats = Gametatistics.new(locations[:games], locations[:teams], locations[:game_teams])
     end
 
     describe '#total score stats' do
