275ad447efa98badfeda164c5c17faa526015f4c
Paul Knapp <ppankluap@gmail.com>
2024-09-17 10:28:51 -0500

    feat: percent home wins method started

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,5 +1,22 @@
 require 'CSV'
 
 class Stattracker
-    
+    s
+
+
+
+
+
+
+
+
+
+
+    def percentage_home_wins
+        total_games = @games.length
+        home_wins = @games.count { |game| game.home_goals > game.away_goals }
+            
+        percentage = (home_wins.to_f / total_games) * 100
+        percentage.round(2)
+    end
 end
