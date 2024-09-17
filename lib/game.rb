1985798b796bb4d8857cca25299acd210b991f86
BrodyNics77 <brodynichols777@gmail.com>
2024-09-17 12:17:55 -0500

    Feat: Added Game class & attr_reader

---
+++ lib/game.rb
@@ -0,0 +1,13 @@
+class Game
+    attr_reader :game_id,
+                :seasons
+                :type,
+                :date_time,
+                :away,
+                :home_team_id,
+                :away_goals,
+                :home_goals,
+                :venue,
+                :venue_link
+
+end
