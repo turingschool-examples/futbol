615688f6f015418f1dc9eef95e53ca6c8838438c
Montana Pfeifer <montanapfeifer@hotmail.com>
2024-09-17 16:26:00 -0600

    un commented method that made 96% tests complete

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -44,13 +44,13 @@ class Stattracker
     stattracker
   end
 
-  # def percentage_home_wins
-  #   total_games = @games.length
-  #   home_wins = @games.count { |game| game.home_goals > game.away_goals }
+  def percentage_home_wins
+    total_games = @games.length
+    home_wins = @games.count { |game| game.home_goals > game.away_goals }
           
-  #   percentage = (home_wins.to_f / total_games) * 100
-  #   percentage.round(2)
-  # end
+    percentage = (home_wins.to_f / total_games) * 100
+    percentage.round(2)
+  end
 
   def highest_total_score
     scores = @all_games.map do |game|
