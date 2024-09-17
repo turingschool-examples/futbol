fed0041955e0f1b7e7fb11827f52f478d4f3cc4d
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-09-17 11:43:43 -0500

    refactored formatting

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -35,9 +35,9 @@ class Stattracker
 
   def percentage_home_wins
     total_games = @games.length
-        home_wins = @games.count { |game| game.home_goals > game.away_goals }
-            
-        percentage = (home_wins.to_f / total_games) * 100
-        percentage.round(2)
-    end
+    home_wins = @games.count { |game| game.home_goals > game.away_goals }
+          
+    percentage = (home_wins.to_f / total_games) * 100
+    percentage.round(2)
+  end
 end
