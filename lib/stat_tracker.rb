abfc80360cd8a7c5dfa0a74d36b01d0848fdee55
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-09-17 11:27:31 -0500

    feat: added self.from_csv

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,19 +1,40 @@
 require 'CSV'
+require './lib/game.rb'
+require './lib/game_factory.rb'
+require './lib/team.rb'
+require './lib/teams_factory.rb'
+require './lib/game_team.rb'
+require './lib/game_team_factory.rb'
 
 class Stattracker
-    s
+  attr_reader :game_teams_factory,
+              :teams_factory,
+              :game_factory
 
+  def initialize
+      @game_teams_factory = GameTeamFactory.new
+      @teams_factory = Teams_factory.new    
+      @game_factory = GameFactory.new
+  end
 
+  def self.from_csv(source)
+    stattracker = Stattracker.new
 
+    source.each do |key, value|
+        case key
+        when :games
+          stattracker.game_factory.create_games(value)
+        when :teams
+          stattracker.teams_factory.create_teams(value)
+        when :game_teams
+          stattracker.game_teams_factory.create_game_teams(value)
+        end
+      end
+      stattracker 
+  end
 
-
-
-
-
-
-
-    def percentage_home_wins
-        total_games = @games.length
+  def percentage_home_wins
+    total_games = @games.length
         home_wins = @games.count { |game| game.home_goals > game.away_goals }
             
         percentage = (home_wins.to_f / total_games) * 100
