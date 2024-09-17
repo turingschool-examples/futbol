821d7265697da12a1eaa7c9dff6b8156f1b00197
Alora Riley <alora.riley@gmail.com>
2024-09-17 09:38:54 -0600

    feat: Create new methods for each set of data and add a way to use the CSV file data

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,26 +1,30 @@
+require_relative 'game'
 require 'CSV'
 
 class StatTracker
-    attr_reader
+    attr_reader :games, :teams, :game_teams
 
-    def initialize(game_id, season, type, date_time, away_team, home_team_id, away_goals, home_goals, venue, venue_link)
-        @game_id = game_id
-        @season = season
-        @type = type
-        @date_time = date_time
-        @away_team = away_team
-        @home_team_id = home_team_id
-        @away_goals = away_goals
-        @home_goals = home_goals
-        @venue = venue
-        @venue_link = venue_link
+    def initialize
+      @games = []
+      @teams = []
+      @game_teams = []
     end
 
     def self.from_csv(locations)
-    stats = []
-
-    CSV.foreach(locations[:games], headers: true, header_converters: :symbol) do |row|
-        stat_tracker = StatTracker.new(
+       # Create a new instance of the StatTracker class
+      stat_tracker = StatTracker.new
+      # Call the load_games method on the new StatTracker instance
+      # Pass in the value associated with the :games key from the locations hash
+      # This value should be the file path to the games CSV file
+      stat_tracker.load_games(locations[:games])
+      stat_tracker.load_teams(locations[:teams])
+      stat_tracker.load_game_teams(locations[:game_teams])
+      stat_tracker
+    end
+    
+    def load_games(file_path)
+      CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
+        @games << Game.new(
             row[:game_id].to_i,
             row[:season],
             row[:type],
@@ -32,8 +36,40 @@ class StatTracker
             row[:venue],
             row[:venue_link]
         )
-        stats << stat_tracker
+      end
     end
-    stats
+
+    def load_teams(file_path)
+      CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
+        @teams << Team.new(
+          row[:team_id], 
+          row[:franchiseid], 
+          row[:teamname], 
+          row[:abbreviation], 
+          row[:stadium], 
+          row[:link]
+          )
+      end
+    end
+
+    def load_game_teams(file_path)
+      CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
+        @game_teams << GameTeam.new(
+          row[:game_id], 
+          row[:team_id], 
+          row[:hoa], 
+          row[:result], 
+          row[:settled_in], 
+          row[:head_coach], 
+          row[:goals].to_i, 
+          row[:shots].to_i, 
+          row[:tackles].to_i, 
+          row[:pim].to_i, 
+          row[:powerplayopportunities].to_i, 
+          row[:powerplaygoals].to_i, 
+          row[:faceoffwinpercentage].to_f, 
+          row[:giveaways].to_i, 
+          row[:takeaways].to_i)
+      end
     end
 end
