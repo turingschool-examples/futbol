a8b0cc1cd1b103d11fd1faeb089591487cd9e3bf
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-09-16 19:39:20 -0500

    refactored for readability

--- lib/game_factory.rb
+++ lib/game_factory.rb
@@ -17,7 +17,7 @@ class GameFactory
       home_goals = row[:home_goals]
       venue = row[:venue]
       venue_link = row[:venue_link]
-      @games << Game.new(game_id, season, type, date_time, away_team_id, home_team_id, away_goals, home_goals, venue, venue_link)
+      @games << Game.new(row)
     end
 
   end
