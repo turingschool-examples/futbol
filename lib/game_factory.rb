4c9a76ff40e7379420c0360d68cba736fb39fb0d
Jeremiah Ross <crosswolf.20work@gmail.com>
2024-09-16 19:17:55 -0500

    feat: added game factory able to create games

---
+++ lib/game_factory.rb
@@ -0,0 +1,25 @@
+class GameFactory
+  attr_reader :games
+
+  def initialize
+    @games = []
+  end
+  def create_games(source)
+    
+    CSV.foreach(source, headers: true, header_converters: :symbol) do |row|
+      game_id = row[:game_id]
+      season = row[:season]
+      type = row[:type]
+      date_time = row[:date_time]
+      away_team_id = row[:away_team_id]
+      home_team_id = row[:home_team_id]
+      away_goals = row[:away_goals]
+      home_goals = row[:home_goals]
+      venue = row[:venue]
+      venue_link = row[:venue_link]
+      @games << Game.new(game_id, season, type, date_time, away_team_id, home_team_id, away_goals, home_goals, venue, venue_link)
+    end
+
+  end
+
+end
