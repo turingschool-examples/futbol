56589a960d76bb37b2121cf8e55bfb1d18d14599
Eric DeShetler <ejdesh@gmail.com>
2024-09-17 08:35:05 -0700

    initial commit

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,5 +1,36 @@
 require 'CSV'
 
 class Stattracker
-    
+  def average_goals_per_game
+    # TODO: required
+    # Average number of goals scored in a game across all
+    # seasons including both home and away goals (rounded to the nearest 100th)
+    # returns float
+  end
+
+  def average_goals_season
+    # TODO: required
+    # Average number of goals scored in a game organized
+    # in a hash with season names (e.g. 20122013) as keys
+    # and a float representing the average number of goals
+    # in a game for that season as values (rounded to the nearest 100th)
+    # returns hash
+  end
+
+  def get_average(scores)
+    # TODO: helper method
+    # returns float
+  end
+
+  def get_sum(scores)
+    # TODO: helper method
+    # returns int
+  end
+
+  def get_scores(team_id, season)
+    # TODO: helper method
+    # returns array of scores
+    # season parameter option, set to all seasons by default
+  end
 end
+
