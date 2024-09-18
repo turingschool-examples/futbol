5f01c99729987336036f8a8db050eca5d7cb038d
Michael OBrien <obrien.michael.t@gmail.com>
2024-09-18 07:24:39 -0400

    Removed all allow statements from tests

--- spec/game_statistics_spec.rb
+++ spec/game_statistics_spec.rb
@@ -33,48 +33,40 @@ RSpec.describe GameStatistics do
 
     describe '#total score stats' do
         it 'knows the highest total score' do
-        allow(@game_stats).to receive(:highest_total_score).and_return(11)
         expect(@game_stats.highest_total_score).to eq(11)
         end
 
         it 'knows the lowest total score' do
-        allow(@game_stats).to receive(:lowest_total_score).and_return(7)
         expect(@game_stats.lowest_total_score).to eq(7)
         end
     end
 
     describe '#win, loss, and tie percentages' do
         it 'knows the percentage of home wins' do
-        allow(@game_stats).to receive(:percentage_home_wins).and_return(.50)
         expect(@game_stats.percentage_home_wins).to eq(.50)
         end
 
         it 'knows the percentage of visitor wins' do
-        allow(@game_stats).to receive(:percentage_visitor_wins).and_return(.50)
         expect(@game_stats.percentage_visitor_wins).to eq(.50)
         end
 
         it 'knows the percentage of ties' do
-            allow(@game_stats).to receive(:percentage_ties).and_return(0.0)
             expect(@game_stats.percentage_ties).to eq(0.0)
         end
     end
 
     describe '#knows the number of games in a season' do
         it 'counts the games in a season' do
-        allow(@game_stats).to receive(:count_of_games_by_season).and_return({season: 20122013, games: 1}, {season: 20162017, games: 4}, {season: 20142015, games: 3})
         expect(@game_stats.count_of_games_by_season).to eq({season: 20122013, games: 1}, {season: 20162017, games: 4}, {season: 20142015, games: 3})
         end
     end
 
     describe '#calculates average goals ' do
         it 'can average the goals scored per game by both teams in every season combined' do
-        allow(@game_stats).to receive(:average_goals_per_game).and_return(4.62)
         expect(@game_stats.average_goals_per_game).to eq(4.62)
         end
 
         it 'can average the goals scored per game by both teams in a single season' do
-            allow(@game_stats).to receive(:average_goals_by_season).and_return({season: 20122013, goals: 5}, {season: 20162017, goals: 4.75}, {season: 20142015, goals: 4.33})
             expect(@game_stats.average_goals_by_season).to eq({season: 20122013, goals: 5}, {season: 20162017, goals: 4.75}, {season: 20142015, goals: 4.33})
         end
     end
