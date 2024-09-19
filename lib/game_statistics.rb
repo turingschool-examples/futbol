270ef1d9fa08e41c59428784aec30aa9f0bce1ba
Michael OBrien <obrien.michael.t@gmail.com>
2024-09-19 15:50:00 -0400

    Completes average_goals_per_game - TEST PASSES

--- lib/game_statistics.rb
+++ lib/game_statistics.rb
@@ -2,10 +2,14 @@ require 'csv'
 require 'game.rb'
 
 class GameStatistics
+    attr_reader :game_data,
+                :team_data,
+                :stat_tracker
 
-    def initialize(game_data, team_data)
+    def initialize(game_data, team_data, stat_tracker)
         @game_data = game_data
         @team_data = team_data
+        @stat_tracker = stat_tracker
     end
 
     def percent_home_wins
@@ -25,7 +29,13 @@ class GameStatistics
     end
 
     def average_goals_per_game
-        binding.pry
-        
+        total_goals = 0
+        games = 0
+        @game_data.each do |game|
+            total_goals += game.away_goals.to_i + game.home_goals.to_i
+            games += 1
+        end
+        average_goals = total_goals / games.to_f
+        average_goals.round(2)
     end
 end
