87790bbba7305256c33537ca2486d346328336c2
Seth Verrill <verrill.seth@gmail.com>
2024-09-19 08:54:27 -0600

    feat: Adjusted best and worst offense to work with updated teams method

--- lib/league_statistics.rb
+++ lib/league_statistics.rb
@@ -1,23 +1,33 @@
 require 'csv'
 require_relative 'game'
+require_relative 'team'
+require_relative 'game_team'
 
 class LeagueStatistics
   attr_reader :games,
               :teams,
-              :game_teams
+              :game_teams,
+              :stat_tracker
 
-  def initialize(games, teams, game_teams)
+  def initialize(games, teams, game_teams, stat_tracker)
     @games = games
     @teams = teams
     @game_teams = game_teams
+    @stat_tracker = stat_tracker
   end
 
-  # def count_of_teams
-  #   @teams.size
-  # end
+  def count_of_teams
+    @teams.size
+  end
 
   def best_offense
-    @stat_tracker.team_name(team_avg_goals.max_by { |team_id, avg_goals| avg_goals}[0])
+    team_id = team_avg_goals.max_by { |team_id, avg_goals| avg_goals}[0]
+    @stat_tracker.team_name(team_id)
+  end
+
+  def worst_offense
+     team_id = team_avg_goals.min_by { |team_id, avg_goals| avg_goals}[0]
+    @stat_tracker.team_name(team_id)
   end
   
   def team_avg_goals
@@ -26,16 +36,18 @@ class LeagueStatistics
 
     @game_teams.each do |game_team|
       team_id = game_team.team_id
-      total_goals_by_team[team_id] += game_team.goals
+      total_goals_by_team[team_id] += game_team.goals.to_i
       total_games_by_team[team_id] += 1
     end
 
     total_goals_by_team.transform_values do |total_goals|
+      team_id = total_games_by_team.key(total_goals)
       total_goals.to_f / total_games_by_team[team_id]
     end
   end
 
-  # def team_name(team_id)
-  #   @teams[team_id]&.team_name
-  # end
+  def team_name(team_id)
+    team = @team.find { |team| team.team_id == team_id}
+    team.team_name
+  end
 end
