1a5dcb26d2f4737cc5325db3341844369682e291
Michael OBrien <obrien.michael.t@gmail.com>
2024-09-19 20:25:52 -0400

    Added rquire './lib/team_statistics' to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'pry'
 require './lib/season_statistics'
 require './lib/game_statistics'
 require './lib/league_statistics'
+require './lib/team_statistics'
 require './lib/stat_tracker'
 require 'simplecov'
 SimpleCov.start
