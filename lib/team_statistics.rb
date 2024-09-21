0027abcf19371df88a1bd35aa975bf568eb8b463
Jonathan Atkins <jonathanjatkins89@gmail.com>
2024-09-21 08:42:04 -0600

    feat:updates

--- lib/team_statistics.rb
+++ lib/team_statistics.rb
@@ -1,11 +1,13 @@
 class TeamStatistics
     attr_reader :teams,
                 :stat_tracker,
-                :games
+                :games,
+                :game_teams
 
     def initialize(teams,games, stat_tracker)
         @teams = teams
         @games = games
+        @game_teams = game_teams
         @stat_tracker = stat_tracker
     end
 
@@ -44,4 +46,34 @@ class TeamStatistics
         end
         goals
     end
+
+    def team_games(team_id)
+        @games.select { |game| game.away_team_id == team_id || game.home_team_id == team_id }
+      end
+
+    #def team_wins(team_id)
+        #total_wins = Hash.new(0)
+        #@game_teams.each do |game|
+            #total_wins[team_id] += 1 if game.result == 'WIN'
+        #end 
+        #total_wins
+    #end
+
+    #def team_losses(team_id)
+        #total_losses = Hash.new(0)
+        #@game_teams.each do |game|
+            #total_losses[team_id] += 1 if game.result == 'LOSS'
+        #end 
+        #total_losses
+    #end
+
+    #
+    
+    #def worst_season
+    #end
+    
+    
+    
+    
+    
 end
