93d1bf3ca1e013131487cf5c716fa313411e0694
Michael OBrien <obrien.michael.t@gmail.com>
2024-09-22 12:59:06 -0400

    Removed binding.pry from file

--- runner.rb
+++ runner.rb
@@ -11,6 +11,3 @@ locations = {
 }
 
 stat_tracker = StatTracker.from_csv(locations)
-
-require 'pry'; binding.pry
-# This is recommened for when we start analyzing data.  It should be able to be removed for production.
